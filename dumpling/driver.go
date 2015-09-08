5467670b072242739ad7ce239d122d22abc8da21
ngaut <ngaut@126.com>
2015-09-08 20:47:07 +0800

    [dumpling] session: Experimental transaction retry Support query like: set a=a+1

--- dumpling/driver.go
+++ dumpling/driver.go
@@ -29,12 +29,14 @@ import (
 	"sync"
 
 	"github.com/juju/errors"
+	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/model"
 	mysql "github.com/pingcap/tidb/mysqldef"
 	"github.com/pingcap/tidb/rset"
 	"github.com/pingcap/tidb/sessionctx"
 	qerror "github.com/pingcap/tidb/util/errors"
 	"github.com/pingcap/tidb/util/types"
+	"github.com/reborndb/go/errors2"
 )
 
 const (
@@ -239,8 +241,13 @@ func (c *driverConn) Commit() error {
 	if c.s == nil {
 		return qerror.ErrCommitNotInTransaction
 	}
+	_, err := c.s.Execute(txCommitSQL)
 
-	if _, err := c.s.Execute(txCommitSQL); err != nil {
+	if errors2.ErrorEqual(err, kv.ErrConditionNotMatch) {
+		return c.s.Retry()
+	}
+
+	if err != nil {
 		return err
 	}
 
