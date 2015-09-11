ad068996ddd26d0357787a7b2ff1aa9e1f20cacb
shenli <shenli3514@gmail.com>
2015-09-11 13:57:57 +0800

    [dumpling] plans: Add test case for show variables
    Add test case for show variables

--- dumpling/plan/plans/show_test.go
+++ dumpling/plan/plans/show_test.go
@@ -11,4 +11,103 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package plans
+package plans_test
+
+import (
+	"fmt"
+
+	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb"
+	"github.com/pingcap/tidb/expression/expressions"
+	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/plan/plans"
+	"github.com/pingcap/tidb/sessionctx/variable"
+	"github.com/pingcap/tidb/stmt"
+)
+
+type testShowSuit struct {
+	txn  kv.Transaction
+	vars map[string]interface{}
+}
+
+// implement Context interface
+func (p *testShowSuit) GetTxn(forceNew bool) (kv.Transaction, error) { return p.txn, nil }
+
+func (p *testShowSuit) FinishTxn(rollback bool) error { return nil }
+
+// SetValue saves a value associated with this context for key
+func (p *testShowSuit) SetValue(key fmt.Stringer, value interface{}) {
+	p.vars[key.String()] = value
+}
+
+// Value returns the value associated with this context for key
+func (p *testShowSuit) Value(key fmt.Stringer) interface{} {
+	return p.vars[key.String()]
+}
+
+// ClearValue clears the value associated with this context for key
+func (p *testShowSuit) ClearValue(key fmt.Stringer) {}
+
+var _ = Suite(&testShowSuit{})
+
+func (p *testShowSuit) SetUpSuite(c *C) {
+	var err error
+	store, err = tidb.NewStore(tidb.EngineGoLevelDBMemory)
+	c.Assert(err, IsNil)
+	p.vars = map[string]interface{}{}
+	p.txn, _ = store.Begin()
+	variable.BindSessionVars(p)
+}
+
+func (p *testShowSuit) TestShowVariables(c *C) {
+	pln := &plans.ShowPlan{
+		Target:      stmt.ShowVariables,
+		GlobalScope: true,
+		Pattern: &expressions.PatternLike{
+			Pattern: &expressions.Value{
+				Val: "character_set_results",
+			},
+		},
+	}
+	fls := pln.GetFields()
+	c.Assert(fls, HasLen, 2)
+	c.Assert(fls[0].Name, Equals, "Variable_name")
+	c.Assert(fls[1].Name, Equals, "Value")
+
+	sessionVars := variable.GetSessionVars(p)
+	ret := map[string]string{}
+	pln.Do(p, func(id interface{}, data []interface{}) (bool, error) {
+		ret[data[0].(string)] = data[1].(string)
+		return true, nil
+	})
+	c.Assert(ret, HasLen, 1)
+	v, ok := ret["character_set_results"]
+	c.Assert(ok, IsTrue)
+	c.Assert(v, Equals, "latin1")
+	// Set session variable to utf8
+	sessionVars.Systems["character_set_results"] = "utf8"
+	pln.Do(p, func(id interface{}, data []interface{}) (bool, error) {
+		ret[data[0].(string)] = data[1].(string)
+		return true, nil
+	})
+	c.Assert(ret, HasLen, 1)
+	v, ok = ret["character_set_results"]
+	c.Assert(ok, IsTrue)
+	// Show global varibale get latin1
+	c.Assert(v, Equals, "latin1")
+
+	pln.GlobalScope = false
+	pln.Do(p, func(id interface{}, data []interface{}) (bool, error) {
+		ret[data[0].(string)] = data[1].(string)
+		return true, nil
+	})
+	c.Assert(ret, HasLen, 1)
+	v, ok = ret["character_set_results"]
+	c.Assert(ok, IsTrue)
+	// Show session varibale get utf8
+	c.Assert(v, Equals, "utf8")
+}
+
+func (p *testShowSuit) TearDownSuite(c *C) {
+	p.txn.Commit()
+}
