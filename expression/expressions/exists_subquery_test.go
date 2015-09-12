aab38d7f3d5f22366ffe99e955c1a440c1326af7
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2015-09-12 14:48:56 +0800

    expression/expressions: add exists subquery test.

--- expression/expressions/exists_subquery_test.go
+++ expression/expressions/exists_subquery_test.go
@@ -12,3 +12,59 @@
 // limitations under the License.
 
 package expressions
+
+import (
+	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb/util/types"
+)
+
+var _ = Suite(&testExistsSubQuerySuite{})
+
+type testExistsSubQuerySuite struct {
+}
+
+func (s *testExistsSubQuerySuite) convert(v interface{}) interface{} {
+	switch x := v.(type) {
+	case nil:
+		return nil
+	case int:
+		return int64(x)
+	}
+
+	return v
+}
+
+func (s *testExistsSubQuerySuite) TestExistsSubQuery(c *C) {
+	tbl := []struct {
+		in     []interface{}
+		not    bool
+		result int64 // 0 for false, 1 for true.
+	}{
+		{[]interface{}{1}, true, 0},
+		{[]interface{}{1}, false, 1},
+		{[]interface{}{nil}, true, 0},
+		{[]interface{}{nil}, false, 1},
+	}
+
+	for _, t := range tbl {
+		in := make([][]interface{}, 0, len(t.in))
+		for _, v := range t.in {
+			in = append(in, []interface{}{s.convert(v)})
+		}
+
+		sq := newMockSubQuery(in, []string{"c"})
+		expr := NewExistsSubQuery(sq, t.not)
+
+		c.Assert(expr.IsStatic(), IsFalse)
+
+		str := expr.String()
+		c.Assert(len(str), Greater, 0)
+
+		v, err := expr.Eval(nil, nil)
+		c.Assert(err, IsNil)
+
+		val, err := types.ToBool(v)
+		c.Assert(err, IsNil)
+		c.Assert(val, Equals, t.result)
+	}
+}
