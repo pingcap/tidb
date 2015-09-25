bd10b86bb346d1ec1293e25693aac5457cf976e3
siddontang <siddontang@gmail.com>
2015-09-25 10:23:45 +0800

    tidb: add test for #261

--- tidb_test.go
+++ tidb_test.go
@@ -800,6 +800,15 @@ func (s *testSessionSuite) TestSelect(c *C) {
 	row, err = r.FirstRow()
 	c.Assert(err, IsNil)
 	match(c, row, `'a'`, `"a"`, `pingcap '-->' tidb`)
+
+	mustExecSQL(c, se, "drop table if exists t")
+	mustExecSQL(c, se, "create table t (c varchar(20))")
+	mustExecSQL(c, se, `insert t values("pingcap '-->' tidb")`)
+
+	r = mustExecSQL(c, se, `select * from t where c like 'pingcap ''-->'' tidb'`)
+	row, err = r.FirstRow()
+	c.Assert(err, IsNil)
+	match(c, row, `pingcap '-->' tidb`)
 }
 
 func (s *testSessionSuite) TestSubQuery(c *C) {
