fe6f12f9e5d329cfa5680d510dc7c6038ed0b09a
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2015-10-13 20:35:55 +0800

    expression: complete date add function.

--- expression/date_add.go
+++ expression/date_add.go
@@ -14,7 +14,13 @@
 package expression
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/juju/errors"
 	"github.com/pingcap/tidb/context"
+	mysql "github.com/pingcap/tidb/mysqldef"
+	"github.com/pingcap/tidb/util/types"
 )
 
 // DateAdd is for time date_add function.
@@ -26,29 +32,67 @@ type DateAdd struct {
 }
 
 // Clone implements the Expression Clone interface.
-func (e *DateAdd) Clone() Expression {
-	n := *e
+func (da *DateAdd) Clone() Expression {
+	n := *da
 	return &n
 }
 
 // Eval implements the Expression Eval interface.
-func (e *DateAdd) Eval(ctx context.Context, args map[interface{}]interface{}) (interface{}, error) {
-	// TODO
-	return nil, nil
+func (da *DateAdd) Eval(ctx context.Context, args map[interface{}]interface{}) (interface{}, error) {
+	dv, err := da.Date.Eval(ctx, args)
+	if dv == nil || err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	sv, err := types.ToString(dv)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	f := types.NewFieldType(mysql.TypeDatetime)
+	f.Decimal = mysql.MaxFsp
+
+	dv, err = types.Convert(sv, f)
+	if dv == nil || err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	t, ok := dv.(mysql.Time)
+	if !ok {
+		return nil, errors.Errorf("need time type, but got %T", dv)
+	}
+
+	iv, err := da.Interval.Eval(ctx, args)
+	if iv == nil || err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	format, err := types.ToString(iv)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	years, months, days, durations, err := extractTimeValue(da.Unit, format)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	t.Time = t.Time.Add(durations)
+	t.Time = t.Time.AddDate(int(years), int(months), int(days))
+	return t, nil
 }
 
 // IsStatic implements the Expression IsStatic interface.
-func (e *DateAdd) IsStatic() bool {
-	return e.Date.IsStatic() && e.Interval.IsStatic()
+func (da *DateAdd) IsStatic() bool {
+	return da.Date.IsStatic() && da.Interval.IsStatic()
 }
 
 // String implements the Expression String interface.
-func (e *DateAdd) String() string {
-	// TODO
-	return ""
+func (da *DateAdd) String() string {
+	return fmt.Sprintf("DATE_ADD(%s, INTERVAL %s %s)", da.Date, da.Interval, strings.ToUpper(da.Unit))
 }
 
 // Accept implements the Visitor Accept interface.
-func (e *DateAdd) Accept(v Visitor) (Expression, error) {
-	return v.VisitDateAdd(e)
+func (da *DateAdd) Accept(v Visitor) (Expression, error) {
+	return v.VisitDateAdd(da)
 }
