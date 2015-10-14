a727bdae4f3ec122f67d8eff6e969ff85a637701
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2015-10-14 16:18:38 +0800

    [dumpling] expression: add more test.

--- dumpling/expression/date_add_test.go
+++ dumpling/expression/date_add_test.go
@@ -25,6 +25,40 @@ type testDateAddSuite struct {
 
 func (t *testDateAddSuite) TestExtract(c *C) {
 	input := "2011-11-11 10:10:10"
+	e := &DateAdd{
+		Unit:     "DAY",
+		Date:     Value{Val: input},
+		Interval: Value{Val: "1"},
+	}
+	c.Assert(e.String(), Equals, `DATE_ADD("2011-11-11 10:10:10", INTERVAL "1" DAY)`)
+	c.Assert(e.Clone(), NotNil)
+	c.Assert(e.IsStatic(), IsTrue)
+
+	_, err := e.Eval(nil, nil)
+	c.Assert(err, IsNil)
+
+	// Test null.
+	e = &DateAdd{
+		Unit:     "DAY",
+		Date:     Value{Val: nil},
+		Interval: Value{Val: "1"},
+	}
+
+	v, err := e.Eval(nil, nil)
+	c.Assert(err, IsNil)
+	c.Assert(v, IsNil)
+
+	e = &DateAdd{
+		Unit:     "DAY",
+		Date:     Value{Val: input},
+		Interval: Value{Val: nil},
+	}
+
+	v, err = e.Eval(nil, nil)
+	c.Assert(err, IsNil)
+	c.Assert(v, IsNil)
+
+	// Test eval.
 	tbl := []struct {
 		Unit     string
 		Interval interface{}
@@ -69,5 +103,42 @@ func (t *testDateAddSuite) TestExtract(c *C) {
 		value, ok := v.(mysql.Time)
 		c.Assert(ok, IsTrue)
 		c.Assert(value.String(), Equals, t.Expect)
+	}
+
+	// Test error.
+	errInput := "20111111 10:10:10"
+	errTbl := []struct {
+		Unit     string
+		Interval interface{}
+	}{
+		{"MICROSECOND", "abc1000"},
+		{"MICROSECOND", ""},
+		{"SECOND_MICROSECOND", "10"},
+		{"MINUTE_MICROSECOND", "10.0000"},
+		{"MINUTE_MICROSECOND", "10:10:10.0000"},
+
+		// MySQL support, but tidb not.
+		{"HOUR_MICROSECOND", "10:10.0000"},
+		{"YEAR_MONTH", "10 1"},
+	}
+
+	for _, t := range errTbl {
+		e := &DateAdd{
+			Unit:     t.Unit,
+			Date:     Value{Val: input},
+			Interval: Value{Val: t.Interval},
+		}
+
+		_, err := e.Eval(nil, nil)
+		c.Assert(err, NotNil)
+
+		e = &DateAdd{
+			Unit:     t.Unit,
+			Date:     Value{Val: errInput},
+			Interval: Value{Val: t.Interval},
+		}
+
+		v, err := e.Eval(nil, nil)
+		c.Assert(err, NotNil, Commentf("%s", v))
 	}
 }
