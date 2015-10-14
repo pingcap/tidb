9f2bb45e3a3a8389cc521a2434d8fcd64f39f9e1
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2015-10-14 15:15:56 +0800

    expression: add date_add function test.

---
+++ expression/date_add_test.go
@@ -0,0 +1,73 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package expression
+
+import (
+	. "github.com/pingcap/check"
+	mysql "github.com/pingcap/tidb/mysqldef"
+)
+
+var _ = Suite(&testDateAddSuite{})
+
+type testDateAddSuite struct {
+}
+
+func (t *testDateAddSuite) TestExtract(c *C) {
+	input := "2011-11-11 10:10:10"
+	tbl := []struct {
+		Unit     string
+		Interval interface{}
+		Expect   string
+	}{
+		{"MICROSECOND", "1000", "2011-11-11 10:10:10.001000"},
+		{"MICROSECOND", 1000, "2011-11-11 10:10:10.001000"},
+		{"SECOND", "10", "2011-11-11 10:10:20"},
+		{"MINUTE", "10", "2011-11-11 10:20:10"},
+		{"HOUR", "10", "2011-11-11 20:10:10"},
+		{"DAY", "11", "2011-11-22 10:10:10"},
+		{"WEEK", "2", "2011-11-25 10:10:10"},
+		{"MONTH", "2", "2012-01-11 10:10:10"},
+		{"QUARTER", "4", "2012-11-11 10:10:10"},
+		{"YEAR", "2", "2013-11-11 10:10:10"},
+		{"SECOND_MICROSECOND", "10.00100000", "2011-11-11 10:10:20.100000"},
+		{"SECOND_MICROSECOND", "10.0010000000", "2011-11-11 10:10:30"},
+		{"SECOND_MICROSECOND", "10.0010000010", "2011-11-11 10:10:30.000010"},
+		{"MINUTE_MICROSECOND", "10:10.100", "2011-11-11 10:20:20.100000"},
+		{"MINUTE_SECOND", "10:10", "2011-11-11 10:20:20"},
+		{"HOUR_MICROSECOND", "10:10:10.100", "2011-11-11 20:20:20.100000"},
+		{"HOUR_SECOND", "10:10:10", "2011-11-11 20:20:20"},
+		{"HOUR_MINUTE", "10:10", "2011-11-11 20:20:10"},
+		{"DAY_MICROSECOND", "11 10:10:10.100", "2011-11-22 20:20:20.100000"},
+		{"DAY_SECOND", "11 10:10:10", "2011-11-22 20:20:20"},
+		{"DAY_MINUTE", "11 10:10", "2011-11-22 20:20:10"},
+		{"DAY_HOUR", "11 10", "2011-11-22 20:10:10"},
+		{"YEAR_MONTH", "11-1", "2022-12-11 10:10:10"},
+		{"YEAR_MONTH", "11-11", "2023-10-11 10:10:10"},
+	}
+
+	for _, t := range tbl {
+		e := &DateAdd{
+			Unit:     t.Unit,
+			Date:     Value{Val: input},
+			Interval: Value{Val: t.Interval},
+		}
+
+		v, err := e.Eval(nil, nil)
+		c.Assert(err, IsNil)
+
+		value, ok := v.(mysql.Time)
+		c.Assert(ok, IsTrue)
+		c.Assert(value.String(), Equals, t.Expect)
+	}
+}
