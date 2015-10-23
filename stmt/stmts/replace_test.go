ccc0ffdfe939abc4fc85367d05a5077e10aeab45
xia <zimu_xia@126.com>
2015-10-23 20:29:08 +0800

    stmt: add replace primary key and unique index test

--- stmt/stmts/replace_test.go
+++ stmt/stmts/replace_test.go
@@ -25,33 +25,32 @@ func (s *testStmtSuite) TestReplace(c *C) {
     replace replace_test (c1) values (1),(2),(NULL);`
 	mustExec(c, s.testDB, testSQL)
 
-	errReplaceSelectSQL := `replace replace_test (c1) values ();`
+	errReplaceSQL := `replace replace_test (c1) values ();`
 	tx := mustBegin(c, s.testDB)
-	_, err := tx.Exec(errReplaceSelectSQL)
+	_, err := tx.Exec(errReplaceSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
 
-	errReplaceSelectSQL = `replace replace_test (c1, c2) values (1,2),(1);`
+	errReplaceSQL = `replace replace_test (c1, c2) values (1,2),(1);`
 	tx = mustBegin(c, s.testDB)
-	_, err = tx.Exec(errReplaceSelectSQL)
+	_, err = tx.Exec(errReplaceSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
 
-	errReplaceSelectSQL = `replace replace_test (xxx) values (3);`
+	errReplaceSQL = `replace replace_test (xxx) values (3);`
 	tx = mustBegin(c, s.testDB)
-	_, err = tx.Exec(errReplaceSelectSQL)
+	_, err = tx.Exec(errReplaceSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
 
-	errReplaceSelectSQL = `replace replace_test_xxx (c1) values ();`
+	errReplaceSQL = `replace replace_test_xxx (c1) values ();`
 	tx = mustBegin(c, s.testDB)
-	_, err = tx.Exec(errReplaceSelectSQL)
+	_, err = tx.Exec(errReplaceSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
 
 	replaceSetSQL := `replace replace_test set c1 = 3;`
 	mustExec(c, s.testDB, replaceSetSQL)
-
 	stmtList, err := tidb.Compile(replaceSetSQL)
 	c.Assert(err, IsNil)
 	c.Assert(stmtList, HasLen, 1)
@@ -61,28 +60,64 @@ func (s *testStmtSuite) TestReplace(c *C) {
 	c.Assert(testStmt.IsDDL(), IsFalse)
 	c.Assert(len(testStmt.OriginText()), Greater, 0)
 
-	errReplaceSelectSQL = `replace replace_test set c1 = 4, c1 = 5;`
+	errReplaceSetSQL := `replace replace_test set c1 = 4, c1 = 5;`
 	tx = mustBegin(c, s.testDB)
-	_, err = tx.Exec(errReplaceSelectSQL)
+	_, err = tx.Exec(errReplaceSetSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
 
-	errReplaceSelectSQL = `replace replace_test set xxx = 6;`
+	errReplaceSetSQL = `replace replace_test set xxx = 6;`
 	tx = mustBegin(c, s.testDB)
-	_, err = tx.Exec(errReplaceSelectSQL)
+	_, err = tx.Exec(errReplaceSetSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
 
-	replaceSelectSQL := `create table replace_test_1 (id int, c1 int); replace replace_test_1 select id, c1 from replace_test;`
+	replaceSelectSQL := `create table replace_test_1 (id int, c1 int); 
+	replace replace_test_1 select id, c1 from replace_test;`
 	mustExec(c, s.testDB, replaceSelectSQL)
 
 	replaceSelectSQL = `create table replace_test_2 (id int, c1 int); 
 	replace replace_test_1 select id, c1 from replace_test union select id * 10, c1 * 10 from replace_test;`
 	mustExec(c, s.testDB, replaceSelectSQL)
 
-	errReplaceSelectSQL = `replace replace_test_1 select c1 from replace_test;`
+	errReplaceSelectSQL := `replace replace_test_1 select c1 from replace_test;`
 	tx = mustBegin(c, s.testDB)
 	_, err = tx.Exec(errReplaceSelectSQL)
 	c.Assert(err, NotNil)
 	tx.Rollback()
+
+	replaceUniqueIndexSQL := `create table replace_test_3 (c1 int, c2 int, UNIQUE INDEX (c2)); 
+	replace into replace_test_3 set c2=1;`
+	mustExec(c, s.testDB, replaceUniqueIndexSQL)
+	replaceUniqueIndexSQL = `replace into replace_test_3 set c2=1;`
+	ret := mustExec(c, s.testDB, replaceUniqueIndexSQL)
+	rows, err := ret.RowsAffected()
+	c.Assert(err, IsNil)
+	c.Assert(rows, Equals, int64(2))
+
+	replaceUniqueIndexSQL = `replace into replace_test_3 set c2=NULL;`
+	mustExec(c, s.testDB, replaceUniqueIndexSQL)
+	replaceUniqueIndexSQL = `replace into replace_test_3 set c2=NULL;`
+	ret = mustExec(c, s.testDB, replaceUniqueIndexSQL)
+	rows, err = ret.RowsAffected()
+	c.Assert(err, IsNil)
+	c.Assert(rows, Equals, int64(1))
+
+	replaceUniqueIndexSQL = `create table replace_test_4 (c1 int, c2 int, c3 int, UNIQUE INDEX (c1, c2)); 
+	replace into replace_test_4 set c2=NULL;`
+	mustExec(c, s.testDB, replaceUniqueIndexSQL)
+	replaceUniqueIndexSQL = `replace into replace_test_4 set c2=NULL;`
+	ret = mustExec(c, s.testDB, replaceUniqueIndexSQL)
+	rows, err = ret.RowsAffected()
+	c.Assert(err, IsNil)
+	c.Assert(rows, Equals, int64(1))
+
+	replacePrimaryKeySQL := `create table replace_test_5 (c1 int, c2 int, c3 int, PRIMARY KEY (c1, c2)); 
+	replace into replace_test_5 set c1=1, c2=2;`
+	mustExec(c, s.testDB, replacePrimaryKeySQL)
+	replacePrimaryKeySQL = `replace into replace_test_5 set c1=1, c2=2;`
+	ret = mustExec(c, s.testDB, replacePrimaryKeySQL)
+	rows, err = ret.RowsAffected()
+	c.Assert(err, IsNil)
+	c.Assert(rows, Equals, int64(2))
 }
