dd26f59b123a40d10a073a8a122c1fa4d8b1bf86
Shen Li <shenli3514@gmail.com>
2015-10-26 21:54:21 +0800

    [dumpling] expression: Remove useless comment.

--- dumpling/expression/like.go
+++ dumpling/expression/like.go
@@ -104,11 +104,9 @@ func (p *PatternLike) Eval(ctx context.Context, args map[interface{}]interface{}
 		spattern, err := types.ToString(pattern)
 		if err != nil {
 			return nil, errors.Trace(err)
-			//errors.Errorf("Pattern should be string or []byte in LIKE: %v (Value of type %T)", pattern, pattern)
 		}
 		p.patChars, p.patTypes = compilePattern(spattern, p.Escape)
 	}
-
 	match := doMatch(sexpr, p.patChars, p.patTypes)
 	if p.Not {
 		return !match, nil
