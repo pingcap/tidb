ca8f66a114b740163466c24972276ffa40614b05
xia <zimu_xia@126.com>
2015-10-30 11:28:10 +0800

    [dumpling] expression: add date_sub operation

--- dumpling/expression/date_cast.go
+++ dumpling/expression/date_cast.go
@@ -16,6 +16,7 @@ package expression
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/juju/errors"
 	"github.com/pingcap/tidb/context"
@@ -23,60 +24,62 @@ import (
 	"github.com/pingcap/tidb/util/types"
 )
 
-// DateAdd is for time date_add function.
+const (
+	add = "ADD"
+	sub = "SUB"
+)
+
+// DateCast is used for dealing with addition and substraction of time.
+// If the Op value is ADD, then do date_add function.
 // See: https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html#function_date-add
-type DateAdd struct {
+// If the Op value is SUB, then do date_sub function.
+// See: https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html#function_date-sub
+type DateCast struct {
+	Op       string
 	Unit     string
 	Date     Expression
 	Interval Expression
 }
 
+func (dc *DateCast) isAdd() bool {
+	if dc.Op == add {
+		return true
+	}
+
+	return false
+}
+
 // Clone implements the Expression Clone interface.
-func (da *DateAdd) Clone() Expression {
-	n := *da
+func (dc *DateCast) Clone() Expression {
+	n := *dc
 	return &n
 }
 
+// IsStatic implements the Expression IsStatic interface.
+func (dc *DateCast) IsStatic() bool {
+	return dc.Date.IsStatic() && dc.Interval.IsStatic()
+}
+
+// Accept implements the Visitor Accept interface.
+func (dc *DateCast) Accept(v Visitor) (Expression, error) {
+	return v.VisitDateCast(dc)
+}
+
+// String implements the Expression String interface.
+func (dc *DateCast) String() string {
+	return fmt.Sprintf("DATE_%s(%s, INTERVAL %s %s)", dc.Op, dc.Date, dc.Interval, strings.ToUpper(dc.Unit))
+}
+
 // Eval implements the Expression Eval interface.
-func (da *DateAdd) Eval(ctx context.Context, args map[interface{}]interface{}) (interface{}, error) {
-	dv, err := da.Date.Eval(ctx, args)
-	if dv == nil || err != nil {
+func (dc *DateCast) Eval(ctx context.Context, args map[interface{}]interface{}) (interface{}, error) {
+	t, years, months, days, durations, err := dc.evalArgs(ctx, args)
+	if t.IsZero() || err != nil {
 		return nil, errors.Trace(err)
 	}
 
-	sv, err := types.ToString(dv)
-	if err != nil {
-		return nil, errors.Trace(err)
+	if !dc.isAdd() {
+		years, months, days, durations = -years, -months, -days, -durations
 	}
-
-	f := types.NewFieldType(mysql.TypeDatetime)
-	f.Decimal = mysql.MaxFsp
-
-	dv, err = types.Convert(sv, f)
-	if dv == nil || err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	t, ok := dv.(mysql.Time)
-	if !ok {
-		return nil, errors.Errorf("need time type, but got %T", dv)
-	}
-
-	iv, err := da.Interval.Eval(ctx, args)
-	if iv == nil || err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	format, err := types.ToString(iv)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	years, months, days, durations, err := mysql.ExtractTimeValue(da.Unit, strings.TrimSpace(format))
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-
 	t.Time = t.Time.Add(durations)
 	t.Time = t.Time.AddDate(int(years), int(months), int(days))
 
@@ -88,17 +91,39 @@ func (da *DateAdd) Eval(ctx context.Context, args map[interface{}]interface{}) (
 	return t, nil
 }
 
-// IsStatic implements the Expression IsStatic interface.
-func (da *DateAdd) IsStatic() bool {
-	return da.Date.IsStatic() && da.Interval.IsStatic()
-}
+func (dc *DateCast) evalArgs(ctx context.Context, args map[interface{}]interface{}) (
+	mysql.Time, int64, int64, int64, time.Duration, error) {
+	dv, err := dc.Date.Eval(ctx, args)
+	if dv == nil || err != nil {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Trace(err)
+	}
+	sv, err := types.ToString(dv)
+	if err != nil {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Trace(err)
+	}
+	f := types.NewFieldType(mysql.TypeDatetime)
+	f.Decimal = mysql.MaxFsp
+	dv, err = types.Convert(sv, f)
+	if dv == nil || err != nil {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Trace(err)
+	}
+	t, ok := dv.(mysql.Time)
+	if !ok {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Errorf("need time type, but got %T", dv)
+	}
 
-// String implements the Expression String interface.
-func (da *DateAdd) String() string {
-	return fmt.Sprintf("DATE_ADD(%s, INTERVAL %s %s)", da.Date, da.Interval, strings.ToUpper(da.Unit))
-}
+	iv, err := dc.Interval.Eval(ctx, args)
+	if iv == nil || err != nil {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Trace(err)
+	}
+	format, err := types.ToString(iv)
+	if err != nil {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Trace(err)
+	}
+	years, months, days, durations, err := mysql.ExtractTimeValue(dc.Unit, strings.TrimSpace(format))
+	if err != nil {
+		return mysql.ZeroTimestamp, 0, 0, 0, 0, errors.Trace(err)
+	}
 
-// Accept implements the Visitor Accept interface.
-func (da *DateAdd) Accept(v Visitor) (Expression, error) {
-	return v.VisitDateAdd(da)
+	return t, years, months, days, durations, nil
 }
