20070e616c8e87d6c7ae8b84d460a938d58e4873
disksing <i@disksing.com>
2015-11-02 14:43:24 +0800

    hbase-store: merge master

---
+++ store/hbase/kv.go
@@ -0,0 +1,139 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hbasekv
+
+import (
+	"strings"
+
+	"sync"
+
+	"github.com/c4pt0r/go-hbase"
+	"github.com/juju/errors"
+	"github.com/pingcap/go-themis"
+	"github.com/pingcap/tidb/kv"
+)
+
+const (
+	// ColFamily is the hbase column family name.
+	ColFamily = "f"
+	// Qualifier is the hbase column name.
+	Qualifier = "q"
+	// FmlAndQual is a shortcut.
+	FmlAndQual = ColFamily + ":" + Qualifier
+	// HbaseTableName is the table name in hbase which is invisible to SQL layer.
+	HbaseTableName = "tidb"
+)
+
+var (
+	_ kv.Storage = (*hbaseStore)(nil)
+)
+
+var (
+	// ErrZkInfoErr is returned when zookeeper info is invalid.
+	ErrZkInfoErr = errors.New("zk info error")
+)
+
+type storeCache struct {
+	mu    sync.Mutex
+	cache map[string]*hbaseStore
+}
+
+var mc storeCache
+
+func init() {
+	mc.cache = make(map[string]*hbaseStore)
+}
+
+type hbaseStore struct {
+	mu        sync.Mutex
+	zkInfo    string
+	storeName string
+	cli       hbase.HBaseClient
+}
+
+func (s *hbaseStore) Begin() (kv.Transaction, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	t := themis.NewTxn(s.cli)
+	txn := newHbaseTxn(t, s.storeName)
+	txn.UnionStore = kv.NewUnionStore(newHbaseSnapshot(t, s.storeName))
+	return txn, nil
+}
+
+func (s *hbaseStore) GetSnapshot(ver kv.Version) (kv.MvccSnapshot, error) {
+	//TODO: support snapshot
+	return nil, errors.New("not implemented")
+}
+
+func (s *hbaseStore) Close() error {
+	mc.mu.Lock()
+	defer mc.mu.Unlock()
+
+	delete(mc.cache, s.zkInfo)
+	return s.cli.Close()
+}
+
+func (s *hbaseStore) UUID() string {
+	return "hbase." + s.storeName + "." + s.zkInfo
+}
+
+func (s *hbaseStore) CurrentVersion() (kv.Version, error) {
+	t := themis.NewTxn(s.cli)
+	return kv.Version{Ver: t.GetStartTS()}, nil
+}
+
+// Driver implements engine Driver.
+type Driver struct {
+}
+
+// Open opens or creates a storage database with given path.
+func (d Driver) Open(zkInfo string) (kv.Storage, error) {
+	mc.mu.Lock()
+	defer mc.mu.Unlock()
+
+	if len(zkInfo) == 0 {
+		return nil, ErrZkInfoErr
+	}
+	if store, ok := mc.cache[zkInfo]; ok {
+		// TODO: check the cache store has the same engine with this Driver.
+		return store, nil
+	}
+	zks := strings.Split(zkInfo, ",")
+	if len(zks) == 0 {
+		return nil, ErrZkInfoErr
+	}
+	c, err := hbase.NewClient(zks, "/hbase")
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	if !c.TableExists(HbaseTableName) {
+		// Create new hbase table for store.
+		t := hbase.NewTableDesciptor(hbase.NewTableNameWithDefaultNS(HbaseTableName))
+		cf := hbase.NewColumnFamilyDescriptor(ColFamily)
+		cf.AddStrAddr("THEMIS_ENABLE", "true")
+		t.AddColumnDesc(cf)
+		//TODO: specify split?
+		if err := c.CreateTable(t, nil); err != nil {
+			return nil, errors.Trace(err)
+		}
+	}
+	s := &hbaseStore{
+		zkInfo:    zkInfo,
+		storeName: HbaseTableName,
+		cli:       c,
+	}
+	mc.cache[zkInfo] = s
+	return s, nil
+}
---
+++ store/hbase/snapshot.go
@@ -0,0 +1,163 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hbasekv
+
+import (
+	"github.com/c4pt0r/go-hbase"
+	"github.com/juju/errors"
+	"github.com/ngaut/log"
+	"github.com/pingcap/go-themis"
+	"github.com/pingcap/tidb/kv"
+)
+
+var (
+	_ kv.Snapshot     = (*hbaseSnapshot)(nil)
+	_ kv.MvccSnapshot = (*hbaseSnapshot)(nil)
+	_ kv.Iterator     = (*hbaseIter)(nil)
+)
+
+const hbaseBatchSize = 1000
+
+type hbaseSnapshot struct {
+	txn       *themis.Txn
+	storeName string
+	cache     map[string][]byte
+}
+
+func newHbaseSnapshot(txn *themis.Txn, storeName string) *hbaseSnapshot {
+	return &hbaseSnapshot{
+		txn:       txn,
+		storeName: storeName,
+		cache:     make(map[string][]byte),
+	}
+}
+
+func (s *hbaseSnapshot) Get(k kv.Key) ([]byte, error) {
+	if b, ok := s.cache[string(k)]; ok {
+		return b, nil
+	}
+
+	s.cache = make(map[string][]byte)
+	scanner := s.txn.GetScanner([]byte(s.storeName), k, nil, hbaseBatchSize)
+	for i := 0; i < hbaseBatchSize; i++ {
+		r := scanner.Next()
+		if r != nil && len(r.Columns) > 0 {
+			s.cache[string(r.Row)] = r.Columns[FmlAndQual].Value
+		} else {
+			break
+		}
+	}
+	scanner.Close()
+
+	if b, ok := s.cache[string(k)]; ok {
+		return b, nil
+	}
+	return nil, kv.ErrNotExist
+}
+
+// MvccGet returns the specific version of given key, if the version doesn't
+// exist, returns the nearest(lower) version's data.
+func (s *hbaseSnapshot) MvccGet(k kv.Key, ver kv.Version) ([]byte, error) {
+	g := hbase.NewGet([]byte(k))
+	g.AddColumn([]byte(ColFamily), []byte(Qualifier))
+	g.TsRangeFrom = 0
+	g.TsRangeTo = ver.Ver + 1
+	v, err := internalGet(s, g)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return v, nil
+}
+
+func internalGet(s *hbaseSnapshot, g *hbase.Get) ([]byte, error) {
+	r, err := s.txn.Get(s.storeName, g)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	if r == nil || len(r.Columns) == 0 {
+		return nil, kv.ErrNotExist
+	}
+	return r.Columns[FmlAndQual].Value, nil
+}
+
+func (s *hbaseSnapshot) NewIterator(param interface{}) kv.Iterator {
+	k, ok := param.([]byte)
+	if !ok {
+		log.Errorf("hbase iterator parameter error, %+v", param)
+		return nil
+	}
+
+	scanner := s.txn.GetScanner([]byte(s.storeName), k, nil, hbaseBatchSize)
+	return newInnerScanner(scanner)
+}
+
+// MvccIterator seeks to the key in the specific version's snapshot, if the
+// version doesn't exist, returns the nearest(lower) version's snaphot.
+func (s *hbaseSnapshot) NewMvccIterator(k kv.Key, ver kv.Version) kv.Iterator {
+	scanner := s.txn.GetScanner([]byte(s.storeName), k, nil, hbaseBatchSize)
+	scanner.SetTimeRange(0, ver.Ver+1)
+	return newInnerScanner(scanner)
+}
+
+func newInnerScanner(scanner *themis.ThemisScanner) kv.Iterator {
+	it := &hbaseIter{
+		ThemisScanner: scanner,
+	}
+	it.Next()
+	return it
+}
+
+func (s *hbaseSnapshot) Release() {
+	if s.txn != nil {
+		s.txn.Release()
+		s.txn = nil
+	}
+}
+
+// Release releases this snapshot.
+func (s *hbaseSnapshot) MvccRelease() {
+	s.Release()
+}
+
+type hbaseIter struct {
+	*themis.ThemisScanner
+	rs    *hbase.ResultRow
+	valid bool
+}
+
+func (it *hbaseIter) Next() (kv.Iterator, error) {
+	it.rs = it.ThemisScanner.Next()
+	it.valid = it.rs != nil && len(it.rs.Columns) > 0 && !it.ThemisScanner.Closed()
+	return it, nil
+}
+
+func (it *hbaseIter) Valid() bool {
+	return it.valid
+}
+
+func (it *hbaseIter) Key() string {
+	return string(it.rs.Row)
+}
+
+func (it *hbaseIter) Value() []byte {
+	return it.rs.Columns[FmlAndQual].Value
+}
+
+func (it *hbaseIter) Close() {
+	if it.ThemisScanner != nil {
+		it.ThemisScanner.Close()
+		it.ThemisScanner = nil
+	}
+	it.rs = nil
+}
