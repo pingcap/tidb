09394250cf17e5f380bbb74140587d727a59b595
siddontang <siddontang@gmail.com>
2015-11-03 08:00:49 +0800

    [dumpling] localstore: fix bolted deadlock

--- dumpling/store/localstore/txn.go
+++ dumpling/store/localstore/txn.go
@@ -192,6 +192,13 @@ func (txn *dbTxn) doCommit() error {
 			txn.store.unLockKeys(key)
 		}
 	}()
+
+	// Release read lock before write. Workaround for BoltDB.
+	// We must Release the snapshot before tryConditionLockKey.
+	// another writing has already locked store lock -> boltdb lock, and
+	// here we lock botldb lock -> store lock, deadlock!
+	txn.Snapshot.Release()
+
 	// Check locked keys
 	for k, v := range txn.snapshotVals {
 		err := txn.store.tryConditionLockKey(txn.tid, k, v)
@@ -223,8 +230,6 @@ func (txn *dbTxn) doCommit() error {
 	}
 	// Update commit version.
 	txn.version = curVer
-	// Release read lock before write. Workaround for BoltDB.
-	txn.Snapshot.Release()
 	return txn.store.writeBatch(b)
 }
 
