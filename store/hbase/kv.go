8e094c0e55ea9ee0e286481d75508358b4f46ba7
dongxu <i@huangdx.net>
2015-11-16 10:26:23 +0800

    hbase-store: use rand instead of timestamp for selecing connection randomly

--- store/hbase/kv.go
+++ store/hbase/kv.go
@@ -14,6 +14,7 @@
 package hbasekv
 
 import (
+	"math/rand"
 	"strings"
 	"sync"
 	"time"
@@ -59,6 +60,7 @@ var mc storeCache
 
 func init() {
 	mc.cache = make(map[string]*hbaseStore)
+	rand.Seed(time.Now().UnixNano())
 }
 
 type hbaseStore struct {
@@ -72,7 +74,7 @@ func (s *hbaseStore) Begin() (kv.Transaction, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// get random conn
-	hbaseCli := s.conns[time.Now().UnixNano()%hbaseConnPoolSize]
+	hbaseCli := s.conns[rand.Intn(hbaseConnPoolSize)]
 	t := themis.NewTxn(hbaseCli)
 	txn := newHbaseTxn(t, s.storeName)
 	txn.UnionStore = kv.NewUnionStore(newHbaseSnapshot(t, s.storeName))
@@ -81,7 +83,7 @@ func (s *hbaseStore) Begin() (kv.Transaction, error) {
 
 func (s *hbaseStore) GetSnapshot(ver kv.Version) (kv.MvccSnapshot, error) {
 	// get random conn
-	hbaseCli := s.conns[time.Now().UnixNano()%hbaseConnPoolSize]
+	hbaseCli := s.conns[rand.Intn(hbaseConnPoolSize)]
 	t := themis.NewTxn(hbaseCli)
 	return newHbaseSnapshot(t, s.storeName), nil
 }
@@ -108,7 +110,7 @@ func (s *hbaseStore) UUID() string {
 }
 
 func (s *hbaseStore) CurrentVersion() (kv.Version, error) {
-	hbaseCli := s.conns[time.Now().UnixNano()%hbaseConnPoolSize]
+	hbaseCli := s.conns[rand.Intn(hbaseConnPoolSize)]
 	t := themis.NewTxn(hbaseCli)
 	defer t.Release()
 
