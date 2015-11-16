c869f3ce7a5ffac6012397f02b2eb7d766b3f80d
siddontang <siddontang@gmail.com>
2015-11-16 20:34:49 +0800

    ddl: don't remove reorg info.
    if we remove reorg info but don’t update the DDL status when crashed,
    another server will start a whole reorganization again.

--- ddl/index.go
+++ ddl/index.go
@@ -345,7 +345,7 @@ func (d *ddl) addTableIndex(t table.Table, indexInfo *model.IndexInfo, reorgInfo
 		if err != nil {
 			return errors.Trace(err)
 		} else if len(handles) == 0 {
-			return errors.Trace(reorgInfo.RemoveHandle())
+			return nil
 		}
 
 		seekHandle = handles[len(handles)-1] + 1
