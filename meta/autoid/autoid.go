70078490adc1949da5cef91e9f850b82091017fe
Ewan Chou <coocood@gmail.com>
2015-12-17 17:55:17 +0800

    meta: toggle alloc id log.

--- meta/autoid/autoid.go
+++ meta/autoid/autoid.go
@@ -68,7 +68,9 @@ func (alloc *allocator) Alloc(tableID int64) (int64, error) {
 	}
 
 	alloc.base++
-	log.Infof("Alloc id %d, table ID:%d, from %p, database ID:%d", alloc.base, tableID, alloc, alloc.dbID)
+	if kv.LogTxn {
+		log.Debugf("Alloc id %d, table ID:%d, from %p, database ID:%d", alloc.base, tableID, alloc, alloc.dbID)
+	}
 	return alloc.base, nil
 }
 
