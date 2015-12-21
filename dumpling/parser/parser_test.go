e1b7fb4141199c8a74d38904a4f64c695d137a7a
shenli <shenli3514@gmail.com>
2015-12-21 11:13:45 +0800

    [dumpling] parser: fix parsing curtime
    1. It can be used as an identifier
    2. It should be called with parentheses

--- dumpling/parser/parser_test.go
+++ dumpling/parser/parser_test.go
@@ -41,6 +41,7 @@ func (s *testParserSuite) TestSimple(c *C) {
 		"collation", "comment", "avg_row_length", "checksum", "compression", "connection", "key_block_size",
 		"max_rows", "min_rows", "national", "row", "quarter", "escape", "grants", "status", "fields", "triggers",
 		"delay_key_write", "isolation", "repeatable", "committed", "uncommitted", "only", "serializable", "level",
+		"curtime",
 	}
 	for _, kw := range unreservedKws {
 		src := fmt.Sprintf("SELECT %s FROM tbl;", kw)
@@ -398,7 +399,6 @@ func (s *testParserSuite) TestBuiltin(c *C) {
 		{"select current_time", true},
 		{"select current_time()", true},
 		{"select current_time(6)", true},
-		{"select curtime", true},
 		{"select curtime()", true},
 		{"select curtime(6)", true},
 
