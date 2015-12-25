f7c82e27cebb18353b2a86a87862ef7473e11148
ngaut <ngaut@126.com>
2015-12-25 19:22:18 +0800

    Address comments

--- kv/bufpool.go
+++ kv/bufpool.go
@@ -1,3 +1,16 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package kv
 
 import (
@@ -13,7 +26,8 @@ type cache struct {
 	mu    sync.Mutex
 	name  string
 	saved []MemBuffer
-	new   func() MemBuffer
+	// factory
+	fact func() MemBuffer
 }
 
 func (c *cache) put(x MemBuffer) {
@@ -21,7 +35,7 @@ func (c *cache) put(x MemBuffer) {
 	if len(c.saved) < cap(c.saved) {
 		c.saved = append(c.saved, x)
 	} else {
-		log.Warning(c.name, "is full, you may need to increase pool size")
+		log.Warnf("%s is full, size: %d, you may need to increase pool size", c.name, len(c.saved))
 	}
 	c.mu.Unlock()
 }
@@ -31,7 +45,7 @@ func (c *cache) get() MemBuffer {
 	n := len(c.saved)
 	if n == 0 {
 		c.mu.Unlock()
-		return c.new()
+		return c.fact()
 	}
 	x := c.saved[n-1]
 	c.saved = c.saved[0 : n-1]
@@ -39,6 +53,6 @@ func (c *cache) get() MemBuffer {
 	return x
 }
 
-func newCache(name string, cap int, f func() MemBuffer) *cache {
-	return &cache{name: name, saved: make([]MemBuffer, 0, cap), new: f}
+func newCache(name string, cap int, fact func() MemBuffer) *cache {
+	return &cache{name: name, saved: make([]MemBuffer, 0, cap), fact: fact}
 }
