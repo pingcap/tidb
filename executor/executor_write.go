8729b45bb6c42ee9f6c35b56683727c61249d71a
xia <zimu_xia@126.com>
2016-03-05 17:29:31 +0800

    *: add tests

--- executor/executor_write.go
+++ executor/executor_write.go
@@ -18,7 +18,6 @@ import (
 	"strings"
 
 	"github.com/juju/errors"
-	"github.com/ngaut/log"
 	"github.com/pingcap/tidb/ast"
 	"github.com/pingcap/tidb/column"
 	"github.com/pingcap/tidb/context"
@@ -555,8 +554,9 @@ func (e *InsertValues) getRows(cols []*column.Col) (rows [][]types.Datum, err er
 	}
 
 	rows = make([][]types.Datum, len(e.Lists))
+	length := len(e.Lists[0])
 	for i, list := range e.Lists {
-		if err = e.checkValueCount(len(e.Lists[i]), len(list), i, cols); err != nil {
+		if err = e.checkValueCount(length, len(list), i, cols); err != nil {
 			return nil, errors.Trace(err)
 		}
 		e.currRow = i
@@ -641,7 +641,7 @@ func (e *InsertValues) fillRowData(cols []*column.Col, vals []types.Datum) ([]ty
 }
 
 func (e *InsertValues) initDefaultValues(row []types.Datum, marked map[int]struct{}) error {
-	var retryInfo bool
+	var rewriteValueCol *column.Col
 	var defaultValueCols []*column.Col
 	for i, c := range e.Table.Cols() {
 		if row[i].Kind() != types.KindNull {
@@ -667,9 +667,8 @@ func (e *InsertValues) initDefaultValues(row []types.Datum, marked map[int]struc
 				// `insert t (c1) values(1),(2),(3);`
 				// Last insert id will be 1, not 3.
 				variable.GetSessionVars(e.ctx).SetLastInsertID(uint64(recordID))
-				// if column value is not nil and it's retried, the last insert ID sets to column value.
-				retryInfo = true
-				log.Warnf("row:%v, no.%d, col:%d, lastInsertID:%v", e.currRow, i, row[i], recordID)
+				// It's used for retry.
+				rewriteValueCol = c
 			}
 		} else {
 			var err error
@@ -685,17 +684,49 @@ func (e *InsertValues) initDefaultValues(row []types.Datum, marked map[int]struc
 		return errors.Trace(err)
 	}
 
-	if !retryInfo {
+	// It's used for retry.
+	if rewriteValueCol == nil {
 		return nil
 	}
-	log.Warnf("row11111:%v", e.Lists[e.currRow][0])
-	cols := make([]ast.ExprNode, len(row))
-	for i, col := range row {
-		cols[i] = ast.NewValueExpr(col.GetValue())
-		log.Warnf("row==========:%v, no.%v", cols[i], i)
+	if len(e.Setlist) > 0 {
+		val := &ast.Assignment{
+			Column: &ast.ColumnName{Name: rewriteValueCol.Name},
+			Expr:   ast.NewValueExpr(row[rewriteValueCol.Offset].GetValue())}
+		if len(e.Setlist) < rewriteValueCol.Offset+1 {
+			e.Setlist = append(e.Setlist, val)
+			return nil
+		}
+		setlist := make([]*ast.Assignment, 0, len(e.Setlist)+1)
+		setlist = append(setlist, e.Setlist[:rewriteValueCol.Offset]...)
+		setlist = append(setlist, val)
+		e.Setlist = append(setlist, e.Setlist[rewriteValueCol.Offset:]...)
+		return nil
 	}
-	e.Lists[e.currRow] = cols
-	log.Warnf("row22222:%v, row1:%v, row2:%v", e.Lists[e.currRow][0], e.Lists[e.currRow][1], row)
+
+	// records the values of each row.
+	vals := make([]ast.ExprNode, len(row))
+	for i, col := range row {
+		vals[i] = ast.NewValueExpr(col.GetValue())
+	}
+	if len(e.Lists) <= e.currRow {
+		e.Lists = append(e.Lists, vals)
+	} else {
+		e.Lists[e.currRow] = vals
+	}
+
+	// records the column name only once.
+	if e.currRow != len(e.Lists)-1 {
+		return nil
+	}
+	if len(e.Columns) < rewriteValueCol.Offset+1 {
+		e.Columns = append(e.Columns, &ast.ColumnName{Name: rewriteValueCol.Name})
+		return nil
+	}
+	cols := make([]*ast.ColumnName, 0, len(e.Columns)+1)
+	cols = append(cols, e.Columns[:rewriteValueCol.Offset]...)
+	cols = append(cols, &ast.ColumnName{Name: rewriteValueCol.Name})
+	e.Columns = append(cols, e.Columns[rewriteValueCol.Offset:]...)
+
 	return nil
 }
 
