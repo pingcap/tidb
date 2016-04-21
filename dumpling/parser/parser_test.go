0752642f41b6dd32baf6faa3b385f1ea93b99ad8
zimulala <zimu_xia@126.com>
2016-04-21 18:05:50 +0800

    [dumpling] parser: Support truncate t (#1136)
    * parser: support truncate t

--- dumpling/parser/parser_test.go
+++ dumpling/parser/parser_test.go
@@ -293,6 +293,10 @@ func (s *testParserSuite) TestDMLStmt(c *C) {
 		{"CREATE TABLE sbtest (id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, k integer UNSIGNED DEFAULT '0' NOT NULL, c char(120) DEFAULT '' NOT NULL, pad char(60) DEFAULT '' NOT NULL, PRIMARY KEY  (id) )", true},
 		{"create table test (create_date TIMESTAMP NOT NULL COMMENT '创建日期 create date' DEFAULT now());", true},
 
+		// For truncate statement
+		{"TRUNCATE TABLE t1", true},
+		{"TRUNCATE t1", true},
+
 		// For delete statement
 		{"DELETE t1, t2 FROM t1 INNER JOIN t2 INNER JOIN t3 WHERE t1.id=t2.id AND t2.id=t3.id;", true},
 		{"DELETE FROM t1, t2 USING t1 INNER JOIN t2 INNER JOIN t3 WHERE t1.id=t2.id AND t2.id=t3.id;", true},
