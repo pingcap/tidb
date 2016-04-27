0f6450320eadaa14532d6c2eed7f97966951c5b9
Ewan Chou <coocood@gmail.com>
2016-04-27 11:28:25 +0800

    store/tikv: amend package comment.

--- store/tikv/client.go
+++ store/tikv/client.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package tikv provides tcp connection to kvserver.
+// Package tikv provides tcp connection to kvserver.
 package tikv
 
 import (
