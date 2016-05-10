890f588eeea94ccd12f8e55ef0bca0aadb18401a
disksing <i@disksing.com>
2016-05-10 17:26:13 +0800

    [dumpling] store/tikv: update tests with mocktikv (#1216)
    * store/tikv: update RegionCache test with mocktikv
    
    * store/tikv: update coprocessor tests with mocktikv
    
    * store/tikv: update scan test with mocktikv

--- dumpling/store/tikv/mock-tikv/cluster_manipulate.go
+++ dumpling/store/tikv/mock-tikv/cluster_manipulate.go
@@ -16,9 +16,33 @@ package mocktikv
 import "fmt"
 
 // BootstrapWithSingleStore initializes a Cluster with 1 Region and 1 Store.
-func BootstrapWithSingleStore(cluster *Cluster) {
+func BootstrapWithSingleStore(cluster *Cluster) (storeID, regionID uint64) {
 	ids := cluster.AllocIDs(2)
-	storeID, regionID := ids[0], ids[1]
+	storeID, regionID = ids[0], ids[1]
 	cluster.AddStore(storeID, fmt.Sprintf("store%d", storeID))
 	cluster.Bootstrap(regionID, []uint64{storeID}, storeID)
+	return
+}
+
+// BootstrapWithMultiStores initializes a Cluster with 1 Region and n Stores.
+func BootstrapWithMultiStores(cluster *Cluster, n int) (storeIDs []uint64, regionID uint64, leaderStore uint64) {
+	ids := cluster.AllocIDs(n + 1)
+	regionID, leaderStore, storeIDs = ids[0], ids[1], ids[1:]
+	for _, storeID := range storeIDs {
+		cluster.AddStore(storeID, fmt.Sprintf("store%d", storeID))
+	}
+	cluster.Bootstrap(regionID, storeIDs, leaderStore)
+	return
+}
+
+// BootstrapWithMultiRegions initializes a Cluster with multiple Regions and 1
+// Store. The number of Regions will be len(splitKeys) + 1.
+func BootstrapWithMultiRegions(cluster *Cluster, splitKeys ...[]byte) (storeID uint64, regionIDs []uint64) {
+	var firstRegionID uint64
+	storeID, firstRegionID = BootstrapWithSingleStore(cluster)
+	regionIDs = append([]uint64{firstRegionID}, cluster.AllocIDs(len(splitKeys))...)
+	for i, k := range splitKeys {
+		cluster.Split(regionIDs[i], regionIDs[i+1], k, storeID)
+	}
+	return
 }
