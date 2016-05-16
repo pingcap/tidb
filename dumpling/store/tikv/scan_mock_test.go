50a70c34646c21fd53b4e316c305dd88dfb43211
disksing <i@disksing.com>
2016-05-16 12:28:32 +0800

    [dumpling] store/tikv: update scanner (#1233)
    * store/tikv: update scanner
    
    * store/tikv: update tests
    
    * add delete lock test

--- dumpling/store/tikv/scan_mock_test.go
+++ dumpling/store/tikv/scan_mock_test.go
@@ -40,9 +40,7 @@ func (s *testScanMockSuite) TestScanMultipleRegions(c *C) {
 	txn, err = store.Begin()
 	c.Assert(err, IsNil)
 	snapshot := newTiKVSnapshot(store, kv.Version{Ver: txn.StartTS()})
-	region, err := store.getRegion(nil)
-	c.Assert(err, IsNil)
-	scanner, err := newScanner(region, []byte("a"), txn.StartTS(), *snapshot, 10)
+	scanner, err := newScanner(snapshot, []byte("a"), 10)
 	c.Assert(err, IsNil)
 	for ch := byte('a'); ch <= byte('z'); ch++ {
 		c.Assert([]byte{ch}, BytesEquals, []byte(scanner.Key()))
@@ -52,29 +50,4 @@ func (s *testScanMockSuite) TestScanMultipleRegions(c *C) {
 	}
 	c.Assert(scanner.Next(), NotNil)
 	c.Assert(scanner.Valid(), IsFalse)
-}
-
-func (s *testScanMockSuite) TestStaleRegionEpoch(c *C) {
-	store, cluster := createMockStoreCluster()
-	_, _, regionID := mocktikv.BootstrapWithSingleStore(cluster)
-
-	txn, err := store.Begin()
-	c.Assert(err, IsNil)
-	for ch := byte('a'); ch <= byte('z'); ch++ {
-		err = txn.Set([]byte{ch}, []byte{ch})
-		c.Assert(err, IsNil)
-	}
-	err = txn.Commit()
-	c.Assert(err, IsNil)
-
-	txn, err = store.Begin()
-	c.Assert(err, IsNil)
-	snapshot := newTiKVSnapshot(store, kv.Version{Ver: txn.StartTS()})
-	region, err := store.getRegion(nil)
-	c.Assert(err, IsNil)
-
-	newPeerID := cluster.AllocID()
-	cluster.Split(regionID, cluster.AllocID(), []byte("m"), []uint64{newPeerID}, newPeerID)
-	_, err = newScanner(region, []byte("a"), txn.StartTS(), *snapshot, 10)
-	c.Assert(err, NotNil)
 }
