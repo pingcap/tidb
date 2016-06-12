aec8d4f6bb29dffea98d038a6474a95788e67134
shenli <shenli3514@gmail.com>
2016-06-12 15:55:39 +0800

    *: Address comment

--- executor/executor_xapi.go
+++ executor/executor_xapi.go
@@ -909,6 +909,8 @@ func (b *executorBuilder) binopToPBExpr(client kv.Client, expr *ast.BinaryOperat
 		tp = tipb.ExprType_Or
 	case opcode.Plus:
 		tp = tipb.ExprType_Plus
+	case opcode.Div:
+		tp = tipb.ExprType_Div
 	default:
 		return nil
 	}
