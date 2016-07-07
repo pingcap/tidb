f2e65b4167aac7db1bfd61125e82df8f98a51b01
hanfei19910905 <hanfei19910905@gmail.com>
2016-07-07 16:09:44 +0800

    fmt code.

--- expression/expression.go
+++ expression/expression.go
@@ -69,7 +69,7 @@ type Column struct {
 	// Position means the position of this column that appears in the select fields.
 	// e.g. SELECT name as id , 1 - id as id , 1 + name as id, name as id from src having id = 1;
 	// There are four ids in the same schema, so you can't identify the column through the FromID and ColName.
-	Position    int
+	Position int
 	// IsAggOrSubq means if this column is referenced to a Aggregation column or a Subquery column.
 	// If so, this column's name will be the plain sql text.
 	IsAggOrSubq bool
