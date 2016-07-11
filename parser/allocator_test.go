ae8f87c00b660beaf73daead60be4d7a82f05336
tiancaiamao <tiancaiamao@gmail.com>
2016-07-11 15:53:06 +0800

    add allocator tests

--- parser/allocator_test.go
+++ parser/allocator_test.go
@@ -14,9 +14,14 @@
 package parser
 
 import (
+	random "math/rand"
+	"runtime"
+
 	. "github.com/pingcap/check"
-	"github.com/pingcap/tidb/mysql"
+	"github.com/pingcap/tidb/ast"
+	"github.com/pingcap/tidb/model"
 	"github.com/pingcap/tidb/util/testleak"
+	"github.com/pingcap/tidb/util/types"
 )
 
 var _ = Suite(&testAllocator{})
@@ -28,12 +33,81 @@ func (t *testAllocator) TestSimple(c *C) {
 	defer testleak.AfterTest(c)()
 	ac := newAllocator()
 
-	ac.newFieldType(mysql.TypeBlob)
-	ac.newFieldType(mysql.TypeDate)
-	ac.newFieldType(mysql.TypeEnum)
-	ac.reset()
-	for i := 0; i < 3; i++ {
-		ft := ac.allocFieldType()
-		c.Assert(ft.Tp, Equals, byte(0))
+	var table = []struct {
+		alloc func(*allocator) interface{}
+		zero  interface{}
+		use   func(interface{})
+	}{
+		{
+			func(ac *allocator) interface{} { return ac.allocFieldType() },
+			&types.FieldType{},
+			func(v interface{}) { r := v.(*types.FieldType); r.Tp = 7 },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocValueExpr() },
+			&ast.ValueExpr{},
+			func(v interface{}) { r := v.(*ast.ValueExpr); r.SetType(ac.newFieldType(23)) },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocInsertStmt() },
+			&ast.InsertStmt{},
+			func(v interface{}) { r := v.(*ast.InsertStmt); r.Priority = 7 },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocSelectStmt() },
+			&ast.SelectStmt{},
+			func(v interface{}) { r := v.(*ast.SelectStmt); r.LockTp = 42 },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocJoin() },
+			&ast.Join{},
+			func(v interface{}) { r := v.(*ast.Join); r.Tp = 54 },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocTableName() },
+			&ast.TableName{},
+			func(v interface{}) { r := v.(*ast.TableName); r.Name = model.NewCIStr("hello") },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocTableSource() },
+			&ast.TableSource{},
+			func(v interface{}) { r := v.(*ast.TableSource); r.AsName = model.NewCIStr("world") },
+		},
+		{
+			func(ac *allocator) interface{} { return ac.allocTableRefsClause() },
+			&ast.TableRefsClause{},
+			func(v interface{}) { r := v.(*ast.TableRefsClause); r.TableRefs = ac.allocJoin() },
+		},
+	}
+
+	for i := 0; i < 10000; i++ {
+		r := random.Intn(len(table))
+		if r == 0 {
+			ac.reset()
+		}
+
+		data := table[r].alloc(ac)
+		c.Assert(data, DeepEquals, table[r].zero)
+		table[r].use(data)
+	}
+}
+
+func (t *testAllocator) TestSafeAfterGC(c *C) {
+	defer testleak.AfterTest(c)()
+	ac := newAllocator()
+
+	ts := &ast.TableSource{}
+	ref := make([]*ast.TableRefsClause, 0, 10000)
+	for i := 0; i < 10000; i++ {
+		v := ac.allocTableRefsClause()
+		v.TableRefs = &ast.Join{
+			Left: ts,
+		}
+		ref = append(ref, v)
+	}
+	runtime.GC()
+
+	for _, v := range ref {
+		c.Assert(v.TableRefs.Left, NotNil)
 	}
 }
