1a33045c230171ed235e169695297dd29c6b5ffe
tiancaiamao <tiancaiamao@gmail.com>
2016-07-28 17:17:52 +0800

    add LICENSE & avoid allocation

--- parser/lexer.go
+++ parser/lexer.go
@@ -1,3 +1,16 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package parser
 
 import (
@@ -51,14 +64,14 @@ func (s *Scanner) Lex(v *yySymType) int {
 	tok, pos, lit := s.scan()
 	v.offset = pos.Offset
 	if tok == identifier {
-		if tok1, ok := tokenMap[strings.ToUpper(lit)]; ok {
+		if tok1 := isTokenIdentifier(lit, &s.buf); tok1 != 0 {
 			return tok1
 		}
 	}
-	v.item = lit
 	if tok == unicode.ReplacementChar && s.r.eof() {
 		return 0
 	}
+	v.item = lit
 	return tok
 }
 
--- parser/misc.go
+++ parser/misc.go
@@ -1,4 +1,23 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package parser
+
+import (
+	"bytes"
+
+	"github.com/pingcap/tidb/util/hack"
+)
 
 func isWhitespace(ch byte) bool {
 	return ch == ' ' || ch == '\t' || ch == '\n'
@@ -378,6 +397,21 @@ var tokenMap = map[string]int{
 	"CASCADE":             cascade,
 	"NO":                  no,
 	"ACTION":              action,
+}
+
+func isTokenIdentifier(s string, buf *bytes.Buffer) int {
+	buf.Reset()
+	buf.Grow(len(s))
+	data := buf.Bytes()[:len(s)]
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 'a' && s[i] <= 'z' {
+			data[i] = s[i] + 'A' - 'a'
+		} else {
+			data[i] = s[i]
+		}
+	}
+	tok := tokenMap[hack.String(data)]
+	return tok
 }
 
 func isTokenByte(ch byte) int {
