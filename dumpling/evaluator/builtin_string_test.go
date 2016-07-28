e0c30cb1912f2b002f33bbda9eb1cbc7134a60e7
HuaiyuXu <391585975@qq.com>
2016-07-28 13:53:54 +0800

    [dumpling] Add builtin function hex (#1504)

--- dumpling/evaluator/builtin_string_test.go
+++ dumpling/evaluator/builtin_string_test.go
@@ -658,3 +658,29 @@ func (s *testEvaluatorSuite) TestTrim(c *C) {
 		c.Assert(r, testutil.DatumEquals, types.NewDatum(v.result))
 	}
 }
+func (s *testEvaluatorSuite) TestHexFunc(c *C) {
+
+	defer testleak.AfterTest(c)()
+	tbl := []struct {
+		Input  interface{}
+		Expect string
+	}{
+		{12, "C"},
+		{12.3, "C"},
+		{12.5, "D"},
+		{-12.3, "FFFFFFFFFFFFFFF4"},
+		{-12.5, "FFFFFFFFFFFFFFF3"},
+		{"12", "3132"},
+		{0x12, "12"},
+		{"", ""},
+	}
+
+	dtbl := tblToDtbl(tbl)
+
+	for _, t := range dtbl {
+		d, err := builtinHex(t["Input"], nil)
+		c.Assert(err, IsNil)
+		c.Assert(d, testutil.DatumEquals, t["Expect"][0])
+
+	}
+}
