349341da54feb6aa6c1d4615f99c25926e6ed0db
xia <zimu_xia@126.com>
2016-07-29 13:50:32 +0800

    *: pass make race

--- store/tikv/coprocessor.go
+++ store/tikv/coprocessor.go
@@ -99,9 +99,7 @@ func (c *CopClient) Send(req *kv.Request) kv.Response {
 	}
 	it.errChan = make(chan error, 1)
 	if len(it.tasks) == 0 {
-		it.mu.Lock()
 		it.Close()
-		it.mu.Unlock()
 	}
 	it.run()
 	return it
@@ -275,10 +273,10 @@ func (it *copIterator) Next() (io.ReadCloser, error) {
 				break
 			}
 		}
+		it.mu.Unlock()
 		if task == nil {
 			it.Close()
 		}
-		it.mu.Unlock()
 		if task == nil {
 			return nil, nil
 		}
@@ -294,12 +292,12 @@ func (it *copIterator) Next() (io.ReadCloser, error) {
 	it.mu.Lock()
 	defer it.mu.Unlock()
 	if err != nil {
-		it.Close()
+		it.finished = true
 		return nil, errors.Trace(err)
 	}
 	it.respGot++
 	if it.respGot == len(it.tasks) {
-		it.Close()
+		it.finished = true
 	}
 	return ioutil.NopCloser(bytes.NewBuffer(resp.Data)), nil
 }
@@ -394,7 +392,9 @@ func (it *copIterator) rebuildCurrentTask(bo *Backoffer, task *copTask) error {
 }
 
 func (it *copIterator) Close() error {
+	it.mu.Lock()
 	it.finished = true
+	it.mu.Unlock()
 	return nil
 }
 
