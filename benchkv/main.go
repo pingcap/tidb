b70d46d173882399d8529f2b93f36a9377a0a844
ngaut <ngaut@126.com>
2016-08-29 15:16:55 +0800

    add missing file

---
+++ benchkv/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"sync"
+	"time"
+
+	"github.com/ngaut/log"
+	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/store/tikv"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	store     kv.Storage
+	dataCnt   = flag.Int("N", 1000000, "data num")
+	workerCnt = flag.Int("C", 300, "concurrent num")
+	pdAddr    = flag.String("pd", "localhost:2379", "pd address:localhost:2379")
+
+	txnCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "tikv",
+			Subsystem: "txn",
+			Name:      "total",
+			Help:      "Counter of txns.",
+		}, []string{"type"})
+
+	txnRolledbackCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "tikv",
+			Subsystem: "txn",
+			Name:      "failed_total",
+			Help:      "Counter of rolled back txns.",
+		}, []string{"type"})
+
+	txnDurations = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "tikv",
+			Subsystem: "txn",
+			Name:      "durations_histogram_seconds",
+			Help:      "Txn latency distributions.",
+			Buckets:   prometheus.ExponentialBuckets(0.0005, 2, 13),
+		}, []string{"type"})
+)
+
+func Init() {
+	driver := tikv.Driver{}
+	var err error
+	store, err = driver.Open(fmt.Sprintf("tikv://%s?cluster=1", *pdAddr))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prometheus.MustRegister(txnCounter)
+	prometheus.MustRegister(txnRolledbackCounter)
+	prometheus.MustRegister(txnDurations)
+
+	go http.ListenAndServe(":9191", nil)
+}
+
+// without conflict
+func batchRW() {
+	wg := sync.WaitGroup{}
+	base := *dataCnt / *workerCnt
+	wg.Add(*workerCnt)
+	for i := 0; i < *workerCnt; i++ {
+		go func(i int) {
+			defer wg.Done()
+			for j := 0; j < base; j++ {
+				txnCounter.WithLabelValues("txn").Inc()
+				start := time.Now()
+				k := base*i + j
+				txn, err := store.Begin()
+				if err != nil {
+					log.Fatal(err)
+				}
+				key := fmt.Sprintf("key_%d", k)
+				txn.Set([]byte(key), []byte("value"))
+				err = txn.Commit()
+				if err != nil {
+					txnRolledbackCounter.WithLabelValues("txn").Inc()
+					txn.Rollback()
+				}
+
+				txnDurations.WithLabelValues("txn").Observe(time.Since(start).Seconds())
+			}
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	flag.Parse()
+	log.SetLevelByString("error")
+	Init()
+
+	t := time.Now()
+	batchRW()
+	fmt.Printf("\nelapse:%v\n", time.Since(t))
+}
