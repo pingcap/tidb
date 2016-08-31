525fa1d81d354d188f866a7b9a85b126a431edb0
tiancaiamao <tiancaiamao@gmail.com>
2016-08-31 23:18:07 +0800

    parser: recognise \r\n as whitespace

--- parser/misc.go
+++ parser/misc.go
@@ -21,10 +21,6 @@ import (
 	"github.com/pingcap/tidb/util/hack"
 )
 
-func isWhitespace(ch rune) bool {
-	return ch == ' ' || ch == '\t' || ch == '\n'
-}
-
 func isLetter(ch rune) bool {
 	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
 }
