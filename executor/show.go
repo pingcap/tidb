4cc095e0c83853431fd0639ac33121c37d19aa4d
shenli <shenli3514@gmail.com>
2016-09-05 17:09:22 +0800

    *: fix show create table bug

--- executor/show.go
+++ executor/show.go
@@ -411,10 +411,8 @@ func (e *ShowExec) fetchShowCreateTable() error {
 	if pkCol != nil {
 		// If PKIsHanle, pk info is not in tb.Indices(). We should handle it here.
 		buf.WriteString(",\n")
-		buf.WriteString(fmt.Sprintf(" PRIMARY KEY (`%s`) ", pkCol.Name.O))
-	}
-
-	if len(tb.Indices()) > 0 {
+		buf.WriteString(fmt.Sprintf(" PRIMARY KEY (`%s`), ", pkCol.Name.O))
+	} else if len(tb.Indices()) > 0 {
 		buf.WriteString(",\n")
 	}
 
