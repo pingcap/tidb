5cf2c28e5a7e4b8f2bae0c185f54ca7490690fd0
disksing <i@disksing.com>
2016-09-21 10:32:53 +0800

    store/tikv: add metrics. (#1741)

--- store/tikv/txn.go
+++ store/tikv/txn.go
@@ -15,6 +15,7 @@ package tikv
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/juju/errors"
 	"github.com/ngaut/log"
@@ -54,6 +55,10 @@ func newTiKVTxn(store *tikvStore) (*tikvTxn, error) {
 // Implement transaction interface.
 func (txn *tikvTxn) Get(k kv.Key) ([]byte, error) {
 	log.Debugf("Get key[%q] txn[%d]", k, txn.StartTS())
+	txnCmdCounter.WithLabelValues("get").Inc()
+	start := time.Now()
+	defer func() { txnCmdHistogram.WithLabelValues("get").Observe(time.Since(start).Seconds()) }()
+
 	ret, err := txn.us.Get(k)
 	if err != nil {
 		return nil, errors.Trace(err)
@@ -63,6 +68,8 @@ func (txn *tikvTxn) Get(k kv.Key) ([]byte, error) {
 
 func (txn *tikvTxn) Set(k kv.Key, v []byte) error {
 	log.Debugf("Set key[%q] txn[%d]", k, txn.StartTS())
+	txnCmdCounter.WithLabelValues("set").Inc()
+
 	txn.dirty = true
 	return txn.us.Set(k, v)
 }
@@ -73,17 +80,27 @@ func (txn *tikvTxn) String() string {
 
 func (txn *tikvTxn) Seek(k kv.Key) (kv.Iterator, error) {
 	log.Debugf("Seek key[%q] txn[%d]", k, txn.StartTS())
+	txnCmdCounter.WithLabelValues("seek").Inc()
+	start := time.Now()
+	defer func() { txnCmdHistogram.WithLabelValues("seek").Observe(time.Since(start).Seconds()) }()
+
 	return txn.us.Seek(k)
 }
 
 // SeekReverse creates a reversed Iterator positioned on the first entry which key is less than k.
 func (txn *tikvTxn) SeekReverse(k kv.Key) (kv.Iterator, error) {
 	log.Debugf("SeekReverse key[%q] txn[%d]", k, txn.StartTS())
+	txnCmdCounter.WithLabelValues("seek_reverse").Inc()
+	start := time.Now()
+	defer func() { txnCmdHistogram.WithLabelValues("seek_reverse").Observe(time.Since(start).Seconds()) }()
+
 	return txn.us.SeekReverse(k)
 }
 
 func (txn *tikvTxn) Delete(k kv.Key) error {
 	log.Debugf("Delete key[%q] txn[%d]", k, txn.StartTS())
+	txnCmdCounter.WithLabelValues("delete").Inc()
+
 	txn.dirty = true
 	return txn.us.Delete(k)
 }
@@ -103,6 +120,10 @@ func (txn *tikvTxn) Commit() error {
 	defer txn.close()
 
 	log.Debugf("[kv] start to commit txn %d", txn.StartTS())
+	txnCmdCounter.WithLabelValues("commit").Inc()
+	start := time.Now()
+	defer func() { txnCmdHistogram.WithLabelValues("commit").Observe(time.Since(start).Seconds()) }()
+
 	if err := txn.us.CheckLazyConditionPairs(); err != nil {
 		return errors.Trace(err)
 	}
@@ -134,10 +155,13 @@ func (txn *tikvTxn) Rollback() error {
 	}
 	txn.close()
 	log.Warnf("[kv] Rollback txn %d", txn.StartTS())
+	txnCmdCounter.WithLabelValues("rollback").Inc()
+
 	return nil
 }
 
 func (txn *tikvTxn) LockKeys(keys ...kv.Key) error {
+	txnCmdCounter.WithLabelValues("lock_keys").Inc()
 	for _, key := range keys {
 		txn.lockKeys = append(txn.lockKeys, key)
 	}
