fd2b82e2b8a7a3c257abdfa3fb7020a16d9fcd37
shenli <shenli3514@gmail.com>
2016-10-31 17:17:59 +0800

    *: Support Show processlist

--- executor/show_test.go
+++ executor/show_test.go
@@ -83,6 +83,7 @@ func (s *testSuite) TestShow(c *C) {
 
 	tk.MustQuery("SHOW PROCEDURE STATUS WHERE Db='test'").Check(testkit.Rows())
 	tk.MustQuery("SHOW TRIGGERS WHERE Trigger ='test'").Check(testkit.Rows())
+	tk.MustQuery("SHOW processlist;").Check(testkit.Rows())
 }
 
 type stats struct {
