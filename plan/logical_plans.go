fdb74960a44852e8553553906500eadf1d2e820f
Han Fei <hanfei19910905@gmail.com>
2016-11-17 12:42:12 +0800

    add a new expression type: Correlated Column. (#1973)

--- plan/logical_plans.go
+++ plan/logical_plans.go
@@ -93,11 +93,11 @@ type Selection struct {
 type Apply struct {
 	baseLogicalPlan
 
-	InnerPlan   LogicalPlan
-	OuterSchema expression.Schema
-	Checker     *ApplyConditionChecker
-	// outerColumns is the columns that not belong to this plan.
-	outerColumns []*expression.Column
+	InnerPlan        LogicalPlan
+	Checker          *ApplyConditionChecker
+	corColsInCurPlan []*expression.CorrelatedColumn
+	// corColsInOuterPlan is the correlated columns that don't belong to this plan.
+	corColsInOuterPlan []*expression.CorrelatedColumn
 }
 
 // Exists checks if a query returns result.
