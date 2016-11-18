322a6e620db7467ac90902634b0e7364eafd4f5c
Han Fei <hanfei19910905@gmail.com>
2016-11-18 13:24:20 +0800

    [dumpling] plan: remove old validator logic (#2013)

--- dumpling/plan/validator.go
+++ dumpling/plan/validator.go
@@ -21,7 +21,6 @@ import (
 	"github.com/pingcap/tidb/ast"
 	"github.com/pingcap/tidb/mysql"
 	"github.com/pingcap/tidb/parser"
-	"github.com/pingcap/tidb/parser/opcode"
 	"github.com/pingcap/tidb/util/types"
 )
 
@@ -68,30 +67,12 @@ func (v *validator) Leave(in ast.Node) (out ast.Node, ok bool) {
 	switch x := in.(type) {
 	case *ast.AggregateFuncExpr:
 		v.inAggregate = false
-	case *ast.BetweenExpr:
-		v.checkAllOneColumn(x.Expr, x.Left, x.Right)
-	case *ast.BinaryOperationExpr:
-		v.checkBinaryOperation(x)
-	case *ast.ByItem:
-		v.checkAllOneColumn(x.Expr)
 	case *ast.CreateTableStmt:
 		v.checkAutoIncrement(x)
-	case *ast.CompareSubqueryExpr:
-		v.checkSameColumns(x.L, x.R)
-	case *ast.FieldList:
-		v.checkFieldList(x)
-	case *ast.HavingClause:
-		v.checkAllOneColumn(x.Expr)
-	case *ast.IsNullExpr:
-		v.checkAllOneColumn(x.Expr)
-	case *ast.IsTruthExpr:
-		v.checkAllOneColumn(x.Expr)
 	case *ast.ParamMarkerExpr:
 		if !v.inPrepare {
 			v.err = parser.ErrSyntax.Gen("syntax error, unexpected '?'")
 		}
-	case *ast.PatternInExpr:
-		v.checkSameColumns(append(x.List, x.Expr)...)
 	case *ast.Limit:
 		if x.Count > math.MaxUint64-x.Offset {
 			x.Count = math.MaxUint64 - x.Offset
@@ -99,23 +80,6 @@ func (v *validator) Leave(in ast.Node) (out ast.Node, ok bool) {
 	}
 
 	return in, v.err == nil
-}
-
-// checkAllOneColumn checks that all expressions have one column.
-// Expression may have more than one column when it is a rowExpr or
-// a Subquery with more than one result fields.
-func (v *validator) checkAllOneColumn(exprs ...ast.ExprNode) {
-	for _, expr := range exprs {
-		switch x := expr.(type) {
-		case *ast.RowExpr:
-			v.err = ErrOneColumn
-		case *ast.SubqueryExpr:
-			if len(x.Query.GetResultFields()) != 1 {
-				v.err = ErrOneColumn
-			}
-		}
-	}
-	return
 }
 
 func checkAutoIncrementOp(colDef *ast.ColumnDef, num int) (bool, error) {
@@ -213,58 +177,6 @@ func (v *validator) checkAutoIncrement(stmt *ast.CreateTableStmt) {
 		mysql.TypeFloat, mysql.TypeDouble, mysql.TypeLonglong, mysql.TypeInt24:
 	default:
 		v.err = errors.Errorf("Incorrect column specifier for column '%s'", autoIncrementCol.Name.Name.O)
-	}
-}
-
-func (v *validator) checkBinaryOperation(x *ast.BinaryOperationExpr) {
-	// row constructor only supports comparison operation.
-	switch x.Op {
-	case opcode.LT, opcode.LE, opcode.GE, opcode.GT, opcode.EQ, opcode.NE, opcode.NullEQ:
-		v.checkSameColumns(x.L, x.R)
-	default:
-		v.checkAllOneColumn(x.L, x.R)
-	}
-}
-
-func columnCount(ex ast.ExprNode) int {
-	switch x := ex.(type) {
-	case *ast.RowExpr:
-		return len(x.Values)
-	case *ast.SubqueryExpr:
-		return len(x.Query.GetResultFields())
-	default:
-		return 1
-	}
-}
-
-func (v *validator) checkSameColumns(exprs ...ast.ExprNode) {
-	if len(exprs) == 0 {
-		return
-	}
-	count := columnCount(exprs[0])
-	for i := 1; i < len(exprs); i++ {
-		if columnCount(exprs[i]) != count {
-			v.err = ErrSameColumns
-			return
-		}
-	}
-}
-
-// checkFieldList checks if there is only one '*' and each field has only one column.
-func (v *validator) checkFieldList(x *ast.FieldList) {
-	var hasWildCard bool
-	for _, val := range x.Fields {
-		if val.WildCard != nil && val.WildCard.Table.L == "" {
-			if hasWildCard {
-				v.err = ErrMultiWildCard
-				return
-			}
-			hasWildCard = true
-		}
-		v.checkAllOneColumn(val.Expr)
-		if v.err != nil {
-			return
-		}
 	}
 }
 
