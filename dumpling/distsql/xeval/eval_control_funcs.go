a5965e8124c61091c0082a3ddfaf2fe64275b7d9
HuaiyuXu <391585975@qq.com>
2016-11-23 17:35:30 +0800

    [dumpling] distsql: push ifnull down (#2067)

--- dumpling/distsql/xeval/eval_control_funcs.go
+++ dumpling/distsql/xeval/eval_control_funcs.go
@@ -25,6 +25,8 @@ func (e *Evaluator) evalControlFuncs(expr *tipb.Expr) (d types.Datum, err error)
 		return e.evalCaseWhen(expr)
 	case tipb.ExprType_If:
 		return e.evalIf(expr)
+	case tipb.ExprType_IfNull:
+		return e.evalIfNull(expr)
 	}
 	return
 }
@@ -86,4 +88,15 @@ func (e *Evaluator) evalIf(expr *tipb.Expr) (d types.Datum, err error) {
 		d, err = e.Eval(expr.Children[2])
 	}
 	return d, errors.Trace(err)
+}
+
+func (e *Evaluator) evalIfNull(expr *tipb.Expr) (d types.Datum, err error) {
+	left, right, err := e.evalTwoChildren(expr)
+	if err != nil {
+		return d, errors.Trace(err)
+	}
+	if left.IsNull() {
+		return right, nil
+	}
+	return left, nil
 }
