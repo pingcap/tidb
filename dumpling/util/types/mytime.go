fbbb9781d096282fa1bdca5e22845fd152e79a71
tiancaiamao <tiancaiamao@gmail.com>
2016-12-06 10:59:10 +0800

    [dumpling] util/types: mysqlTime implements TimeInternal interface (#2142)

---
+++ dumpling/util/types/mytime.go
@@ -0,0 +1,84 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	gotime "time"
+)
+
+type mysqlTime struct {
+	year        uint16 // year <= 9999
+	month       uint8  // month <= 12
+	day         uint8  // day <= 31
+	hour        uint8  // hour <= 23
+	minute      uint8  // minute <= 59
+	second      uint8  // second <= 59
+	microsecond uint32
+}
+
+func (t mysqlTime) Year() int {
+	return int(t.year)
+}
+
+func (t mysqlTime) Month() int {
+	return int(t.month)
+}
+
+func (t mysqlTime) Day() int {
+	return int(t.day)
+}
+
+func (t mysqlTime) Hour() int {
+	return int(t.hour)
+}
+
+func (t mysqlTime) Minute() int {
+	return int(t.minute)
+}
+
+func (t mysqlTime) Second() int {
+	return int(t.second)
+}
+
+func (t mysqlTime) Microsecond() int {
+	return int(t.microsecond)
+}
+
+func (t mysqlTime) Weekday() gotime.Weekday {
+	return t.GoTime().Weekday()
+}
+
+func (t mysqlTime) YearDay() int {
+	return t.GoTime().YearDay()
+}
+
+func (t mysqlTime) ISOWeek() (int, int) {
+	return t.GoTime().ISOWeek()
+}
+
+func (t mysqlTime) GoTime() gotime.Time {
+	return gotime.Date(t.Year(), gotime.Month(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Microsecond()*1000, gotime.Local)
+}
+
+func newMysqlTime(year, month, day, hour, minute, second, microsecond int) mysqlTime {
+	return mysqlTime{
+		uint16(year),
+		uint8(month),
+		uint8(day),
+		uint8(hour),
+		uint8(minute),
+		uint8(second),
+		uint32(microsecond),
+	}
+}
