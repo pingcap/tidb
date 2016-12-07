576d465680042d8fd16cac62e8c0e52d8d8c2eac
disksing <i@disksing.com>
2016-12-07 13:31:11 +0800

    address comment.

--- store/tikv/region_request.go
+++ store/tikv/region_request.go
@@ -23,8 +23,21 @@ import (
 	"github.com/pingcap/kvproto/pkg/kvrpcpb"
 )
 
-// RegionRequestSender sends KV/Cop requests to tikv server.
-// It handles network errors and some region errors internally when it is safe to retry.
+// RegionRequestSender sends KV/Cop requests to tikv server. It handles network
+// errors and some region errors internally.
+//
+// Typically, a KV/Cop request is bind to a region, all keys that are involved
+// in the request should be located in the region.
+// The sending process begins with looking for the address of leader store's
+// address of the target region from cache, and the request is then sent to the
+// destination tikv server over TCP connection.
+// If region is updated, can be caused by leader transfer, region split, region
+// merge, or region balance, tikv server may not able to process request and
+// send back a RegionError.
+// RegionRequestSender takes care of errors that does not relevant to region
+// range, such as 'I/O timeout', 'NotLeader', and 'ServerIsBusy'. For other
+// errors, since region range have changed, the request may need to split, so we
+// simply return the error to caller.
 type RegionRequestSender struct {
 	bo          *Backoffer
 	regionCache *RegionCache
@@ -151,7 +164,6 @@ func (s *RegionRequestSender) onSendFail(regionID RegionVerID, ctx *kvrpcpb.Cont
 
 func (s *RegionRequestSender) onRegionError(region *Region, regionErr *errorpb.Error) (retry bool, err error) {
 	reportRegionError(regionErr)
-
 	if notLeader := regionErr.GetNotLeader(); notLeader != nil {
 		// Retry if error is `NotLeader`.
 		log.Warnf("tikv reports `NotLeader`: %s, ctx: %s, retry later", notLeader, region.GetContext())
@@ -179,5 +191,4 @@ func (s *RegionRequestSender) onRegionError(region *Region, regionErr *errorpb.E
 		s.regionCache.DropRegion(region.VerID())
 	}
 	return true, nil
-
 }
