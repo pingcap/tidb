d59ca6dd3db8714f69b369fd34ecc62a9c3e4250
zimuxia <zimu_xia@126.com>
2016-12-12 12:09:48 +0800

    [dumpling] *: Remove the package of bytes (#2221)
    * util: remove the package of bytes

--- dumpling/store/localstore/boltdb/boltdb_test.go
+++ dumpling/store/localstore/boltdb/boltdb_test.go
@@ -14,6 +14,7 @@
 package boltdb
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -190,4 +191,18 @@ func (s *testSuite) TestPrevSeek(c *C) {
 	c.Assert(err, NotNil)
 	c.Assert(k, IsNil)
 	c.Assert(v, IsNil)
+}
+
+func (s *testSuite) TestBytesClone(c *C) {
+	b := []byte("hello world")
+	shadowB := b
+	newB := cloneBytes(b)
+	c.Assert(b, DeepEquals, newB)
+	// Ugly hacks.Go doesn't allow compare two slice (except nil).
+	// For example:  b == newB <-- it's invalid.
+	// In this test, we must ensure CloneBytes method returns a new slice with
+	// the same value, so we need to check the new slice's address.
+	c.Assert(fmt.Sprintf("%p", b) != fmt.Sprintf("%p", newB), IsTrue)
+	// But the addresses are the same when it's a shadow copy.
+	c.Assert(fmt.Sprintf("%p", b), Equals, fmt.Sprintf("%p", shadowB))
 }
