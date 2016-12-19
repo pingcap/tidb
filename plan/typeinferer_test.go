f2161a0616e9dc9d1de71a4890163a104df558e4
Jason W <silentred3@gmail.com>
2016-12-19 15:34:22 +0800

    add test case for typeinferer

--- plan/typeinferer_test.go
+++ plan/typeinferer_test.go
@@ -153,6 +153,7 @@ func (ts *testTypeInferrerSuite) TestInferType(c *C) {
 		{"unhex('TiDB')", mysql.TypeVarString, "utf8"},
 		{"unhex(12)", mysql.TypeVarString, "utf8"},
 		{"DATE_FORMAT('2009-10-04 22:23:00', '%W %M %Y')", mysql.TypeVarString, "utf8"},
+		{"rpad('TiDB', 12, 'go')", mysql.TypeVarString, charset.CharsetUTF8},
 	}
 	for _, ca := range cases {
 		ctx := testKit.Se.(context.Context)
