abc19b08d5c1546f92a2610b70363fc41dcde4d4
tiancaiamao <tiancaiamao@gmail.com>
2016-12-20 11:14:23 +0800

    util/types: mysql time's week function (#2259)

--- util/types/mytime.go
+++ util/types/mytime.go
@@ -111,3 +111,108 @@ func newMysqlTime(year, month, day, hour, minute, second, microsecond int) mysql
 		uint32(microsecond),
 	}
 }
+
+// calcDaynr calculates days since 0000-00-00.
+func calcDaynr(year, month, day int) int {
+	if year == 0 && month == 0 {
+		return 0
+	}
+
+	delsum := 365*year + 31*(month-1) + day
+	if month <= 2 {
+		year--
+	} else {
+		delsum -= (month*4 + 23) / 10
+	}
+	temp := ((year/100 + 1) * 3) / 4
+	return delsum + year/4 - temp
+}
+
+// calcDaysInYear calculates days in one year, it works with 0 <= year <= 99.
+func calcDaysInYear(year int) int {
+	if (year&3) == 0 && (year%100 != 0 || (year%400 == 0 && (year != 0))) {
+		return 366
+	}
+	return 365
+}
+
+// calcWeekday calculates weekday from daynr, returns 0 for Monday, 1 for Tuesday ...
+func calcWeekday(daynr int, sundayFirstDayOfWeek bool) int {
+	daynr += 5
+	if sundayFirstDayOfWeek {
+		daynr++
+	}
+	return daynr % 7
+}
+
+type weekBehaviour uint
+
+const (
+	// If set, Sunday is first day of week, otherwise Monday is first day of week.
+	weekBehaviourMondayFirst weekBehaviour = 1 << iota
+	// If set, Week is in range 1-53, otherwise Week is in range 0-53.
+	// Note that this flag is only releveant if WEEK_JANUARY is not set.
+	weekBehaviourYear
+	// If not set, Weeks are numbered according to ISO 8601:1988.
+	// If set, the week that contains the first 'first-day-of-week' is week 1.
+	weekBehaviourFirstWeekday
+)
+
+func (v weekBehaviour) test(flag weekBehaviour) bool {
+	return (v & flag) != 0
+}
+
+func weekMode(mode int) weekBehaviour {
+	var weekFormat weekBehaviour
+	weekFormat = weekBehaviour(mode & 7)
+	if (weekFormat & weekBehaviourMondayFirst) == 0 {
+		weekFormat ^= weekBehaviourFirstWeekday
+	}
+	return weekFormat
+}
+
+// calcWeek calculates week and year for the time.
+func calcWeek(t *mysqlTime, wb weekBehaviour) (year int, week int) {
+	var days int
+	daynr := calcDaynr(int(t.year), int(t.month), int(t.day))
+	firstDaynr := calcDaynr(int(t.year), 1, 1)
+	mondayFirst := wb.test(weekBehaviourMondayFirst)
+	weekYear := wb.test(weekBehaviourYear)
+	firstWeekday := wb.test(weekBehaviourFirstWeekday)
+
+	weekday := calcWeekday(int(firstDaynr), !mondayFirst)
+
+	year = int(t.year)
+
+	if t.month == 1 && int(t.day) <= 7-weekday {
+		if !weekYear &&
+			((firstWeekday && weekday != 0) || (!firstWeekday && weekday >= 4)) {
+			week = 0
+			return
+		}
+		weekYear = true
+		(year)--
+		days = calcDaysInYear(year)
+		firstDaynr -= days
+		weekday = (weekday + 53*7 - days) % 7
+	}
+
+	if (firstWeekday && weekday != 0) ||
+		(!firstWeekday && weekday >= 4) {
+		days = daynr - (firstDaynr + 7 - weekday)
+	} else {
+		days = daynr - (firstDaynr - weekday)
+	}
+
+	if weekYear && days >= 52*7 {
+		weekday = (weekday + calcDaysInYear(year)) % 7
+		if (!firstWeekday && weekday < 4) ||
+			(firstWeekday && weekday == 0) {
+			year++
+			week = 1
+			return
+		}
+	}
+	week = days/7 + 1
+	return
+}
