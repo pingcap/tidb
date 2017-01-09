743a2cdc7290faa19c401df03c12389df77e00d3
Ewan Chou <coocood@gmail.com>
2017-01-10 00:01:18 +0800

    *: address comment

--- kv/memdb_buffer.go
+++ kv/memdb_buffer.go
@@ -27,8 +27,11 @@ import (
 
 // Those limits is enforced to make sure the transaction can be well handled by TiKV.
 var (
+	// The limit of single entry size (len(key) + len(value)).
 	EntrySizeLimit  = 8 * 1024 * 1024
+	// The limit of number of entries in the MemBuffer.
 	BufferLenLimit  = 100 * 1000
+	// The limit of the sum of all entry size.
 	BufferSizeLimit = 100 * 1024 * 1024
 )
 
