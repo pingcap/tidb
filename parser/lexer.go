edf85a5cf59c1322c90259141c6a11ee025bc71f
tiancaiamao <tiancaiamao@gmail.com>
2017-01-09 13:32:06 +0800

    address comment

--- parser/lexer.go
+++ parser/lexer.go
@@ -83,7 +83,7 @@ func (s *Scanner) Errorf(format string, a ...interface{}) {
 	if len(val) > 2048 {
 		val = val[:2048]
 	}
-	err := fmt.Errorf("line %d column %d near \"%s\"%s (len %d)", s.r.p.Line, s.r.p.Col, val, str, len(s.r.s))
+	err := fmt.Errorf("line %d column %d near \"%s\"%s (total length %d)", s.r.p.Line, s.r.p.Col, val, str, len(s.r.s))
 	s.errs = append(s.errs, err)
 }
 
