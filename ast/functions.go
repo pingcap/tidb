440b7653d4157975494c3f3651a60fd5a3c35a14
Han Fei <hanfei19910905@gmail.com>
2017-01-14 16:23:56 +0800

    *: fix a bug when aggregate function meet subquery. (#2452)

--- ast/functions.go
+++ ast/functions.go
@@ -303,11 +303,6 @@ type AggregateFuncExpr struct {
 	// For example, column c1 values are "1", "2", "2",  "sum(c1)" is "5",
 	// but "sum(distinct c1)" is "3".
 	Distinct bool
-
-	CurrentGroup []byte
-	// contextPerGroupMap is used to store aggregate evaluation context.
-	// Each entry for a group.
-	contextPerGroupMap map[string](*AggEvaluateContext)
 }
 
 // Accept implements Node Accept interface.
@@ -325,56 +320,6 @@ func (n *AggregateFuncExpr) Accept(v Visitor) (Node, bool) {
 		n.Args[i] = node.(ExprNode)
 	}
 	return v.Leave(n)
-}
-
-// AggregateFuncExtractor visits Expr tree.
-// It converts ColunmNameExpr to AggregateFuncExpr and collects AggregateFuncExpr.
-type AggregateFuncExtractor struct {
-	inAggregateFuncExpr bool
-	// AggFuncs is the collected AggregateFuncExprs.
-	AggFuncs   []*AggregateFuncExpr
-	extracting bool
-}
-
-// Enter implements Visitor interface.
-func (a *AggregateFuncExtractor) Enter(n Node) (node Node, skipChildren bool) {
-	switch n.(type) {
-	case *AggregateFuncExpr:
-		a.inAggregateFuncExpr = true
-	case *SelectStmt, *InsertStmt, *DeleteStmt, *UpdateStmt:
-		// Enter a new context, skip it.
-		// For example: select sum(c) + c + exists(select c from t) from t;
-		if a.extracting {
-			return n, true
-		}
-	}
-	a.extracting = true
-	return n, false
-}
-
-// Leave implements Visitor interface.
-func (a *AggregateFuncExtractor) Leave(n Node) (node Node, ok bool) {
-	switch v := n.(type) {
-	case *AggregateFuncExpr:
-		a.inAggregateFuncExpr = false
-		a.AggFuncs = append(a.AggFuncs, v)
-	case *ColumnNameExpr:
-		// compose new AggregateFuncExpr
-		if !a.inAggregateFuncExpr {
-			// For example: select sum(c) + c from t;
-			// The c in sum() should be evaluated for each row.
-			// The c after plus should be evaluated only once.
-			agg := &AggregateFuncExpr{
-				F:    AggFuncFirstRow,
-				Args: []ExprNode{v},
-			}
-			agg.SetFlag((v.GetFlag() | FlagHasAggregateFunc))
-			agg.SetType(v.GetType())
-			a.AggFuncs = append(a.AggFuncs, agg)
-			return agg, true
-		}
-	}
-	return n, true
 }
 
 // AggEvaluateContext is used to store intermediate result when calculating aggregate functions.
