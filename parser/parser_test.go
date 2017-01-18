1f040d58cc55d589fa9ad96d0ed833c20f437a4d
xia <zimu_xia@126.com>
2017-01-18 16:55:24 +0800

    *: support `alter table rename table`

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -293,6 +293,8 @@ func (s *testParserSuite) TestDMLStmt(c *C) {
 		{"ALTER TABLE t ENABLE KEYS", true},
 		{"ALTER TABLE t MODIFY COLUMN a varchar(255)", true},
 		{"ALTER TABLE t CHANGE COLUMN a b varchar(255)", true},
+		{"ALTER TABLE db.t RENAME to db1.t1", true},
+		{"ALTER TABLE t RENAME as t1", true},
 
 		// from join
 		{"SELECT * from t1, t2, t3", true},
