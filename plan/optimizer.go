bdd09dc9636ff0c0b32c72e07cd5eb33486d9943
hanfei <hanfei19910905@gmail.com>
2017-01-26 14:50:03 +0800

    plan: only apply will add cache plan.

--- plan/optimizer.go
+++ plan/optimizer.go
@@ -82,7 +82,7 @@ func doOptimize(flag uint64, logic LogicalPlan, ctx context.Context, allocator *
 		return nil, errors.Trace(ErrCartesianProductUnsupported)
 	}
 	logic.ResolveIndicesAndCorCols()
-	return physicalOptimize(logic, allocator)
+	return physicalOptimize(flag, logic, allocator)
 }
 
 func logicalOptimize(flag uint64, logic LogicalPlan, ctx context.Context, alloc *idAllocator) (LogicalPlan, error) {
@@ -102,14 +102,16 @@ func logicalOptimize(flag uint64, logic LogicalPlan, ctx context.Context, alloc 
 	return logic, errors.Trace(err)
 }
 
-func physicalOptimize(logic LogicalPlan, allocator *idAllocator) (PhysicalPlan, error) {
+func physicalOptimize(flag uint64, logic LogicalPlan, allocator *idAllocator) (PhysicalPlan, error) {
 	info, err := logic.convert2PhysicalPlan(&requiredProperty{})
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
 	pp := info.p
 	pp = EliminateProjection(pp)
-	addCachePlan(pp, allocator)
+	if flag&(flagDecorrelate) > 0 {
+		addCachePlan(pp, allocator)
+	}
 	log.Debugf("[PLAN] %s", ToString(pp))
 	return pp, nil
 }
