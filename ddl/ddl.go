4782648246465a1a4537e7e7d2cc7be397950a75
zimulala <zimu_xia@126.com>
2017-01-26 10:18:35 +0800

    ddl: add logs

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -1,4 +1,5 @@
 // Copyright 2013 The ql Authors. All rights reserved.
+
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSES/QL-LICENSE file.
 
@@ -168,6 +169,7 @@ func newDDL(store kv.Storage, infoHandle *infoschema.Handle, hook Callback, leas
 	d.start()
 
 	variable.RegisterStatistics(d)
+	log.Infof("start DDL:%s", d.uuid)
 
 	return d
 }
@@ -208,6 +210,7 @@ func (d *ddl) Stop() error {
 		// Background job's owner is me, clean it so other servers can complete it quickly.
 		return t.SetBgJobOwner(&model.Owner{})
 	})
+	log.Infof("stop DDL:%s", d.uuid)
 
 	return errors.Trace(err)
 }
@@ -244,6 +247,7 @@ func (d *ddl) close() {
 	close(d.quitCh)
 
 	d.wait.Wait()
+	log.Infof("close DDL:%s", d.uuid)
 }
 
 func (d *ddl) isClosed() bool {
