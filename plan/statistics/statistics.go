838833c3c9e3177524d8b50b4420218a5381a839
hanfei <hanfei19910905@gmail.com>
2017-01-26 15:34:14 +0800

    regress some change.

--- plan/statistics/statistics.go
+++ plan/statistics/statistics.go
@@ -33,11 +33,11 @@ const (
 	defaultBucketCount = 256
 
 	// When we haven't analyzed a table, we use pseudo statistics to estimate costs.
-	// It has row count 10000000, equal condition selects 1/1000 of total rows, less condition selects 1/30 of total rows,
+	// It has row count 10000000, equal condition selects 1/1000 of total rows, less condition selects 1/3 of total rows,
 	// between condition selects 1/40 of total rows.
 	pseudoRowCount    = 10000000
 	pseudoEqualRate   = 1000
-	pseudoLessRate    = 30
+	pseudoLessRate    = 3
 	pseudoBetweenRate = 40
 	pseudoTimestamp   = 1
 )
