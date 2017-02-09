e4c09dc8015aa95d9b427bac52deabf36934dcb7
xia <zimu_xia@126.com>
2017-02-09 13:08:19 +0800

    *: Support the statement of `drop default`

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -963,21 +963,25 @@ func (d *ddl) AlterColumn(ctx context.Context, ident ast.Ident, spec *ast.AlterT
 		return errBadField.GenByArgs(colName, ident.Name)
 	}
 
-	value, err := getDefaultValue(ctx, spec.NewColumn.Options[0], col.Tp, col.Decimal)
-	if err != nil {
-		return ErrColumnBadNull.Gen("invalid default value - %s", err)
-	}
-	col.DefaultValue = value
-	err = checkDefaultValue(col, true)
-	if err != nil {
-		return errors.Trace(err)
-	}
-	// Check column default value.
-	// TODO: Put this code to checkDefaultValue and fix issue 2600.
-	colInfo := col.ToInfo()
-	_, _, err = table.GetColDefaultValue(ctx, colInfo)
-	if err != nil {
-		return errors.Trace(err)
+	if len(spec.NewColumn.Options) == 0 {
+		col.DefaultValue = nil
+	} else {
+		value, err := getDefaultValue(ctx, spec.NewColumn.Options[0], col.Tp, col.Decimal)
+		if err != nil {
+			return ErrColumnBadNull.Gen("invalid default value - %s", err)
+		}
+		col.DefaultValue = value
+		err = checkDefaultValue(col, true)
+		if err != nil {
+			return errors.Trace(err)
+		}
+		// Check column default value.
+		// TODO: Put this code to checkDefaultValue and fix issue 2600.
+		colInfo := col.ToInfo()
+		_, _, err = table.GetColDefaultValue(ctx, colInfo)
+		if err != nil {
+			return errors.Trace(err)
+		}
 	}
 
 	job := &model.Job{
