850c0888fcd1f3a96bbcc06db85d787ae8129afd
Ewan Chou <coocood@gmail.com>
2017-02-16 13:03:07 +0800

    *: simplify test.

--- expression/builtin_encryption_test.go
+++ expression/builtin_encryption_test.go
@@ -24,66 +24,48 @@ import (
 	"github.com/pingcap/tidb/util/types"
 )
 
+
+var aesCases = []struct {
+	origin string
+	key    string
+	crypt  string
+}{
+	{"pingcap", "1234567890123456", "697BFE9B3F8C2F289DD82C88C7BC95C4"},
+	{"pingcap123", "1234567890123456", "CEC348F4EF5F84D3AA6C4FA184C65766"},
+	{"pingcap", "123456789012345678901234", "6F1589686860C8E8C7A40A78B25FF2C0"},
+	{"pingcap", "123", "996E0CA8688D7AD20819B90B273E01C6"},
+}
+
 func (s *testEvaluatorSuite) TestAESEncrypt(c *C) {
 	defer testleak.AfterTest(c)()
-
-	tests := []struct {
-		str    string
-		key    string
-		expect string
-	}{
-		{"pingcap", "1234567890123456", "697BFE9B3F8C2F289DD82C88C7BC95C4"},
-		{"pingcap123", "1234567890123456", "CEC348F4EF5F84D3AA6C4FA184C65766"},
-		{"pingcap", "123456789012345678901234", "6F1589686860C8E8C7A40A78B25FF2C0"},
-		{"pingcap", "123", "996E0CA8688D7AD20819B90B273E01C6"},
-	}
-	for _, test := range tests {
-		fc := funcs[ast.AesEncrypt]
-		str := types.NewStringDatum(test.str)
+	fc := funcs[ast.AesEncrypt]
+	for _, test := range aesCases {
+		str := types.NewStringDatum(test.origin)
 		key := types.NewStringDatum(test.key)
 		f, err := fc.getFunction(datumsToConstants([]types.Datum{str, key}), s.ctx)
 		crypt, err := f.eval(nil)
 		c.Assert(err, IsNil)
-		c.Assert(toHex(crypt), Equals, test.expect)
+		c.Assert(toHex(crypt), Equals, test.crypt)
 	}
-	// Test case for null input
-	fc := funcs[ast.AesEncrypt]
-	arg := types.NewStringDatum("str")
-	var argNull types.Datum
-	f, err := fc.getFunction(datumsToConstants([]types.Datum{arg, argNull}), s.ctx)
-	crypt, err := f.eval(nil)
-	c.Assert(err, IsNil)
-	c.Assert(crypt.IsNull(), IsTrue)
-
-	f, err = fc.getFunction(datumsToConstants([]types.Datum{argNull, arg}), s.ctx)
-	crypt, err = f.eval(nil)
-	c.Assert(err, IsNil)
-	c.Assert(crypt.IsNull(), IsTrue)
+	s.testNullInput(c, ast.AesDecrypt)
 }
 
 func (s *testEvaluatorSuite) TestAESDecrypt(c *C) {
 	defer testleak.AfterTest(c)()
-
 	fc := funcs[ast.AesDecrypt]
-	tests := []struct {
-		expect      string
-		key         string
-		hexCryptStr string
-	}{
-		{"pingcap", "1234567890123456", "697BFE9B3F8C2F289DD82C88C7BC95C4"},
-		{"pingcap123", "1234567890123456", "CEC348F4EF5F84D3AA6C4FA184C65766"},
-		{"pingcap", "123456789012345678901234", "6F1589686860C8E8C7A40A78B25FF2C0"},
-		{"pingcap", "123", "996E0CA8688D7AD20819B90B273E01C6"},
-	}
-	for _, test := range tests {
-		cryptStr := fromHex(test.hexCryptStr)
+	for _, test := range aesCases {
+		cryptStr := fromHex(test.crypt)
 		key := types.NewStringDatum(test.key)
 		f, err := fc.getFunction(datumsToConstants([]types.Datum{cryptStr, key}), s.ctx)
 		str, err := f.eval(nil)
 		c.Assert(err, IsNil)
-		c.Assert(str.GetString(), Equals, test.expect)
+		c.Assert(str.GetString(), Equals, test.origin)
 	}
-	// Test case for null input
+	s.testNullInput(c, ast.AesDecrypt)
+}
+
+func (s *testEvaluatorSuite) testNullInput(c *C, fnName string) {
+	fc := funcs[fnName]
 	arg := types.NewStringDatum("str")
 	var argNull types.Datum
 	f, err := fc.getFunction(datumsToConstants([]types.Datum{arg, argNull}), s.ctx)
