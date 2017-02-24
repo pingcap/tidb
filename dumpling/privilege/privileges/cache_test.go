a48e33252023e4193f1b6f3ef6f0b77ca642df26
tiancaiamao <tiancaiamao@gmail.com>
2017-02-24 13:04:59 +0800

    [dumpling] *: make TiDB more robust when synchronize mysql.user table (#2722)
    * load privilege table will ignore data that can't recognize
    * convert IP to domain for connection verification

--- dumpling/privilege/privileges/cache_test.go
+++ dumpling/privilege/privileges/cache_test.go
@@ -171,3 +171,72 @@ func (s *testCacheSuite) TestCaseInsensitive(c *C) {
 	c.Assert(p.RequestVerification("genius", "127.0.0.1", "TCTRAIN", "TCTRAINORDER", "", mysql.SelectPriv), IsTrue)
 	c.Assert(p.RequestVerification("genius", "127.0.0.1", "tctrain", "tctrainorder", "", mysql.SelectPriv), IsTrue)
 }
+
+func (s *testCacheSuite) TestAfterSyncMySQLUser(c *C) {
+	privileges.Enable = true
+	store, err := tidb.NewStore("memory://sync_mysql_user")
+	c.Assert(err, IsNil)
+	domain, err := tidb.BootstrapSession(store)
+	c.Assert(err, IsNil)
+	defer domain.Close()
+
+	se, err := tidb.CreateSession(store)
+	c.Assert(err, IsNil)
+	defer se.Close()
+
+	mustExec(c, se, "DROP TABLE mysql.user;")
+	mustExec(c, se, "USE mysql;")
+	mustExec(c, se, `CREATE TABLE user (
+  Host char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  User char(16) COLLATE utf8_bin NOT NULL DEFAULT '',
+  Password char(41) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+  Select_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Insert_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Update_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Delete_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Create_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Drop_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Reload_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Shutdown_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Process_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  File_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Grant_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  References_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Index_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Alter_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Show_db_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Super_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Create_tmp_table_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Lock_tables_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Execute_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Repl_slave_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Repl_client_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Create_view_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Show_view_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Create_routine_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Alter_routine_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Create_user_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Event_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Trigger_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  Create_tablespace_priv enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  ssl_type enum('','ANY','X509','SPECIFIED') CHARACTER SET utf8 NOT NULL DEFAULT '',
+  ssl_cipher blob NOT NULL,
+  x509_issuer blob NOT NULL,
+  x509_subject blob NOT NULL,
+  max_questions int(11) unsigned NOT NULL DEFAULT '0',
+  max_updates int(11) unsigned NOT NULL DEFAULT '0',
+  max_connections int(11) unsigned NOT NULL DEFAULT '0',
+  max_user_connections int(11) unsigned NOT NULL DEFAULT '0',
+  plugin char(64) COLLATE utf8_bin DEFAULT 'mysql_native_password',
+  authentication_string text COLLATE utf8_bin,
+  password_expired enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  PRIMARY KEY (Host,User)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Users and global privileges';`)
+	mustExec(c, se, `INSERT INTO user VALUES ('localhost','root','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0,'mysql_native_password','','N');
+`)
+	var p privileges.MySQLPrivilege
+	err = p.LoadUserTable(se)
+	c.Assert(err, IsNil)
+	// MySQL mysql.user table schema is not identical to TiDB, check it doesn't break privilege.
+	c.Assert(p.RequestVerification("root", "localhost", "test", "", "", mysql.SelectPriv), IsTrue)
+}
