bebe5df40d07e219b1583ee20a60f9e1d0add516
Yanzhe Chen <yanzhe.cn@gmail.com>
2017-02-27 15:26:26 +0800

    util/filesort: Add unit test for multi-worker case

--- util/filesort/filesort_test.go
+++ util/filesort/filesort_test.go
@@ -201,6 +201,55 @@ func (s *testFileSortSuite) TestMultipleFiles(c *C) {
 	}
 }
 
+func (s *testFileSortSuite) TestMultipleWorkers(c *C) {
+	defer testleak.AfterTest(c)()
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+
+	sc := new(variable.StatementContext)
+	keySize := r.Intn(10) + 1 // random int in range [1, 10]
+	valSize := r.Intn(20) + 1 // random int in range [1, 20]
+	bufSize := 40             // hold up to 40 items per file
+	byDesc := make([]bool, keySize)
+	for i := range byDesc {
+		byDesc[i] = (r.Intn(2) == 0)
+	}
+
+	var (
+		err    error
+		fs     *FileSorter
+		pkey   []types.Datum
+		key    []types.Datum
+		tmpDir string
+		ret    bool
+	)
+
+	tmpDir, err = ioutil.TempDir("", "util_filesort_test")
+	c.Assert(err, IsNil)
+
+	fsBuilder := new(Builder)
+	fs, err = fsBuilder.SetSC(sc).SetSchema(keySize, valSize).SetBuf(bufSize).SetWorkers(4).SetDesc(byDesc).SetDir(tmpDir).Build()
+	c.Assert(err, IsNil)
+
+	nRows := (r.Intn(bufSize) + 1) * (r.Intn(10) + 2)
+	for i := 1; i <= nRows; i++ {
+		err = fs.Input(nextRow(r, keySize, valSize))
+		c.Assert(err, IsNil)
+	}
+
+	pkey, _, _, err = fs.Output()
+	c.Assert(err, IsNil)
+	for i := 1; i < nRows; i++ {
+		key, _, _, err = fs.Output()
+		c.Assert(err, IsNil)
+		ret, err = lessThan(sc, key, pkey, byDesc)
+		c.Assert(err, IsNil)
+		c.Assert(ret, IsFalse)
+		pkey = key
+	}
+}
+
 func (s *testFileSortSuite) TestClose(c *C) {
 	defer testleak.AfterTest(c)()
 
