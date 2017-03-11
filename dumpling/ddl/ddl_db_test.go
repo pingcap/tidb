a80fab7fa49d98856e66542f78f344a4f33f547c
Lynn <zimu_xia@126.com>
2017-03-11 22:23:23 +0800

    [dumpling] ddl: don't support modifying the type definitions from Null to Not (#2810)

--- dumpling/ddl/ddl_db_test.go
+++ dumpling/ddl/ddl_db_test.go
@@ -751,7 +751,7 @@ func (s *testDBSuite) TestChangeColumn(c *C) {
 	rowStr := fmt.Sprintf("%v", nil)
 	s.tk.MustQuery("select aa from t3").Check(testkit.Rows("0", rowStr))
 	// for the following definitions: 'not null', 'null', 'default value' and 'comment'
-	s.mustExec(c, "alter table t3 change b b varchar(20) not null default 'c' comment 'my comment'")
+	s.mustExec(c, "alter table t3 change b b varchar(20) null default 'c' comment 'my comment'")
 	ctx := s.tk.Se.(context.Context)
 	is := sessionctx.GetDomain(ctx).InfoSchema()
 	tbl, err := is.TableByName(model.NewCIStr("test_db"), model.NewCIStr("t3"))
@@ -760,7 +760,7 @@ func (s *testDBSuite) TestChangeColumn(c *C) {
 	colB := tblInfo.Columns[1]
 	c.Assert(colB.Comment, Equals, "my comment")
 	hasNotNull := tmysql.HasNotNullFlag(colB.Flag)
-	c.Assert(hasNotNull, IsTrue)
+	c.Assert(hasNotNull, IsFalse)
 	s.mustExec(c, "insert into t3 set aa = 3")
 	rowStr = fmt.Sprintf("%v", []byte("a"))
 	rowStr1 := fmt.Sprintf("%v", []byte("b"))
@@ -768,7 +768,7 @@ func (s *testDBSuite) TestChangeColumn(c *C) {
 	s.tk.MustQuery("select b from t3").Check(testkit.Rows(rowStr, rowStr1, rowStr2))
 	// for timestamp
 	s.mustExec(c, "alter table t3 add column c timestamp not null")
-	s.mustExec(c, "alter table t3 change c c timestamp not null default '2017-02-11' comment 'col c comment' on update current_timestamp")
+	s.mustExec(c, "alter table t3 change c c timestamp null default '2017-02-11' comment 'col c comment' on update current_timestamp")
 	is = sessionctx.GetDomain(ctx).InfoSchema()
 	tbl, err = is.TableByName(model.NewCIStr("test_db"), model.NewCIStr("t3"))
 	c.Assert(err, IsNil)
@@ -776,7 +776,7 @@ func (s *testDBSuite) TestChangeColumn(c *C) {
 	colC := tblInfo.Columns[2]
 	c.Assert(colC.Comment, Equals, "col c comment")
 	hasNotNull = tmysql.HasNotNullFlag(colC.Flag)
-	c.Assert(hasNotNull, IsTrue)
+	c.Assert(hasNotNull, IsFalse)
 
 	// for failing tests
 	sql := "alter table t3 change aa a bigint default ''"
@@ -785,6 +785,8 @@ func (s *testDBSuite) TestChangeColumn(c *C) {
 	s.testErrorCode(c, sql, tmysql.ErrWrongDBName)
 	sql = "alter table t3 change t.a aa bigint"
 	s.testErrorCode(c, sql, tmysql.ErrWrongTableName)
+	sql = "alter table t3 change aa a bigint not null"
+	s.testErrorCode(c, sql, tmysql.ErrUnknown)
 }
 
 func (s *testDBSuite) TestAlterColumn(c *C) {
