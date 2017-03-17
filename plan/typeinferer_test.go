1f5e76bc9f3db53c55051954518d54bd514550f9
Rick Yu <cosmtrek@gmail.com>
2017-03-17 11:57:06 +0800

    builtin: add pi built-in function (#2836)

--- plan/typeinferer_test.go
+++ plan/typeinferer_test.go
@@ -133,6 +133,8 @@ func (ts *testTypeInferrerSuite) TestInferType(c *C) {
 		{"LOG2(3)", mysql.TypeDouble, charset.CharsetBin},
 		{"LOG10(3)", mysql.TypeDouble, charset.CharsetBin},
 		{"SQRT(3)", mysql.TypeDouble, charset.CharsetBin},
+		{"PI()", mysql.TypeDouble, charset.CharsetBin},
+		{"PI() + 0.000000000000000000", mysql.TypeDouble, charset.CharsetBin},
 		{"ACOS(1)", mysql.TypeDouble, charset.CharsetBin},
 		{"ASIN(1)", mysql.TypeDouble, charset.CharsetBin},
 		{"ATAN(1)", mysql.TypeDouble, charset.CharsetBin},
