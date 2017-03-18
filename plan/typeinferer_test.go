5968a2b358a30b5aefc7782cab3583c06d269748
ts25504 <ts25504@gmail.com>
2017-03-19 04:56:24 +0800

    builtin: add built-in function ord

--- plan/typeinferer_test.go
+++ plan/typeinferer_test.go
@@ -259,6 +259,7 @@ func (ts *testTypeInferrerSuite) TestInferType(c *C) {
 		{`ord(true)`, mysql.TypeLonglong, charset.CharsetBin},
 		{`ord(null)`, mysql.TypeLonglong, charset.CharsetBin},
 		{`is_ipv6('FE80::AAAA:0000:00C2:0002')`, mysql.TypeLonglong, charset.CharsetBin},
+		{`ord('2')`, mysql.TypeLonglong, charset.CharsetBin},
 	}
 	for _, ca := range cases {
 		ctx := testKit.Se.(context.Context)
