709da085b11bb109a30fb12ab096f70a395f620f
hiwjd <hiwjd0@gmail.com>
2017-03-25 17:00:56 +0800

    [dumpling] builtin: add insert built-in function (#2855)

--- dumpling/plan/typeinferer_test.go
+++ dumpling/plan/typeinferer_test.go
@@ -263,6 +263,7 @@ func (ts *testTypeInferrerSuite) TestInferType(c *C) {
 		{`exp(1)`, mysql.TypeDouble, charset.CharsetBin},
 		{`exp(1.23)`, mysql.TypeDouble, charset.CharsetBin},
 		{`exp('1.23')`, mysql.TypeDouble, charset.CharsetBin},
+		{`insert("Titanium", 3, 6, "DB")`, mysql.TypeVarString, charset.CharsetUTF8},
 		{`is_ipv6('FE80::AAAA:0000:00C2:0002')`, mysql.TypeLonglong, charset.CharsetBin},
 		{`ord('2')`, mysql.TypeLonglong, charset.CharsetBin},
 		{`ord(2)`, mysql.TypeLonglong, charset.CharsetBin},
