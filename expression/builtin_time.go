c06b2d45862f7fd7c749051c2be55700ee4a95d9
spongedc <duchuangucas@live.com>
2017-03-31 19:03:28 +0800

    builtin: add built-in function 'TO_DAYS'

--- expression/builtin_time.go
+++ expression/builtin_time.go
@@ -1999,7 +1999,26 @@ type builtinToDaysSig struct {
 
 // See https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html#function_to-days
 func (b *builtinToDaysSig) eval(row []types.Datum) (d types.Datum, err error) {
-	return d, errFunctionNotExists.GenByArgs("to_days")
+	args, err := b.evalArgs(row)
+	if err != nil {
+		return d, errors.Trace(err)
+	}
+	if args[0].IsNull() {
+		d.SetNull()
+		return d, nil
+	}
+	sc := b.ctx.GetSessionVars().StmtCtx
+	date, err := convertDatumToTime(sc, args[0])
+	if err != nil {
+		return d, errorOrWarning(err, b.ctx)
+	}
+	ret := types.TimestampDiff("DAY", types.ZeroDate, date)
+	if ret == 0 {
+		d.SetNull()
+	} else {
+		d.SetInt64(ret)
+	}
+	return d, nil
 }
 
 type toSecondsFunctionClass struct {
