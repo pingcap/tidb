14a16e658d7fa1a3210a6c29fcf204b189d1bfcf
Du Chuan <duchuangucas@live.com>
2017-04-07 12:35:00 +0800

    [dumpling] Builtin: add builtin function 'TO_DAYS' (#2983)

--- dumpling/plan/typeinferer.go
+++ dumpling/plan/typeinferer.go
@@ -358,7 +358,7 @@ func (v *typeInferrer) handleFuncCallExpr(x *ast.FuncCallExpr) {
 		tp = types.NewFieldType(mysql.TypeDatetime)
 	case "microsecond", "second", "minute", "hour", "day", "week", "month", "year",
 		"dayofweek", "dayofmonth", "dayofyear", "weekday", "weekofyear", "yearweek", "datediff",
-		"found_rows", "length", "extract", "locate", "unix_timestamp", "quarter", "is_ipv4":
+		"found_rows", "length", "extract", "locate", "unix_timestamp", "quarter", "is_ipv4", "to_days":
 		tp = types.NewFieldType(mysql.TypeLonglong)
 	case "now", "sysdate", "current_timestamp", "utc_timestamp":
 		tp = types.NewFieldType(mysql.TypeDatetime)
