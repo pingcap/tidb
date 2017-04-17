b20eb0355f8028958e3a637d4f854f7af83ffe09
tiancaiamao <tiancaiamao@gmail.com>
2017-04-17 17:17:07 +0800

    [dumpling] *: fix data race when defer cancel() is called (#3068)
    * *: fix data race when defer cancel() is called
    
    defer cancel() was introduced in this PR
    https://github.com/pingcap/tidb/pull/3010

--- dumpling/session.go
+++ dumpling/session.go
@@ -139,9 +139,9 @@ func (s *session) Cancel() {
 	s.cancelFunc()
 }
 
-// Canceled implements context.Context interface.
-func (s *session) Done() <-chan struct{} {
-	return s.goCtx.Done()
+// GoCtx returns the standard context.Context that bind with current transaction.
+func (s *session) GoCtx() goctx.Context {
+	return s.goCtx
 }
 
 func (s *session) cleanRetryInfo() {
