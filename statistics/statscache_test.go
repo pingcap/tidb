10e005f1e66b52cdff7c4c1305fa9a3d4764ab9e
Han Fei <hanfei19910905@gmail.com>
2017-04-17 21:13:18 +0800

    statistics: every histogram use its own version to update. (#3059)
    after we support delta updae in #3053 , we shouldn't load the whole histogram when we find the table version updated. Every histogram has it's own version.

--- statistics/statscache_test.go
+++ statistics/statscache_test.go
@@ -246,6 +246,38 @@ func (s *testStatsCacheSuite) TestDDL(c *C) {
 	c.Assert(statsTbl.Pseudo, IsFalse)
 }
 
+func (s *testStatsCacheSuite) TestLoadHist(c *C) {
+	store, do, err := newStoreWithBootstrap()
+	c.Assert(err, IsNil)
+	defer store.Close()
+	testKit := testkit.NewTestKit(c, store)
+	testKit.MustExec("use test")
+	testKit.MustExec("create table t (c1 int, c2 int)")
+	rowCount := 10
+	for i := 0; i < rowCount; i++ {
+		testKit.MustExec("insert into t values(1,2)")
+	}
+	testKit.MustExec("analyze table t")
+	h := do.StatsHandle()
+	is := do.InfoSchema()
+	tbl, err := is.TableByName(model.NewCIStr("test"), model.NewCIStr("t"))
+	c.Assert(err, IsNil)
+	tableInfo := tbl.Meta()
+	oldStatsTbl := h.GetTableStats(tableInfo)
+	for i := 0; i < rowCount; i++ {
+		testKit.MustExec("insert into t values(1,2)")
+	}
+	h.DumpStatsDeltaToKV()
+	h.Update(do.InfoSchema())
+	newStatsTbl := h.GetTableStats(tableInfo)
+	// The stats table is updated.
+	c.Assert(oldStatsTbl == newStatsTbl, IsFalse)
+	// The histograms is not updated.
+	for id, hist := range oldStatsTbl.Columns {
+		c.Assert(hist, Equals, newStatsTbl.Columns[id])
+	}
+}
+
 func newStoreWithBootstrap() (kv.Storage, *domain.Domain, error) {
 	store, err := tidb.NewStore(tidb.EngineGoLevelDBMemory)
 	if err != nil {
