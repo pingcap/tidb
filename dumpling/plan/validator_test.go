42b70e2afdfbb14c8d047b8a8786bca27d0f6dd9
Zejun Li <bobotu@users.noreply.github.com>
2017-04-20 01:31:05 +0800

    [dumpling] plan/validator:  return an error when alter auto_incerment (#3041) (#3072)

--- dumpling/plan/validator_test.go
+++ dumpling/plan/validator_test.go
@@ -71,6 +71,10 @@ func (s *testValidatorSuite) TestValidator(c *C) {
 			errors.New("[schema:1068]Multiple primary key defined")},
 		{"create table t(c1 int not null, c2 int not null, primary key(c1), primary key(c2))", true,
 			errors.New("[schema:1068]Multiple primary key defined")},
+		{"alter table t auto_increment=1", true, errors.New("[autoid:3]No support for setting auto_increment using alter_table")},
+		{"alter table t add column c int auto_increment key, auto_increment=10", true,
+			errors.New("[autoid:3]No support for setting auto_increment using alter_table")},
+		{"alter table t add column c int auto_increment key", true, nil},
 	}
 
 	store, err := tidb.NewStore(tidb.EngineGoLevelDBMemory)
