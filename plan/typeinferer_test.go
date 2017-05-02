73a7d32df349f4b3fad003c815ae2d246e7554c7
phasefei <phasefei@foxmail.com>
2017-05-02 19:27:39 +0800

    expression, plan: add implementation for function charset
    Implement function charset

--- plan/typeinferer_test.go
+++ plan/typeinferer_test.go
@@ -321,6 +321,7 @@ func (ts *testTypeInferrerSuite) TestInferType(c *C) {
 		{`bit_count(1)`, mysql.TypeLonglong, charset.CharsetBin, mysql.BinaryFlag},
 		{`time_to_sec("23:59:59")`, mysql.TypeLonglong, charset.CharsetBin, mysql.BinaryFlag},
 		{`inet6_aton('FE80::AAAA:0000:00C2:0002')`, mysql.TypeVarString, charset.CharsetUTF8, 0},
+		{"charset('abc')", mysql.TypeVarString, charset.CharsetUTF8, 0},
 	}
 	for _, tt := range tests {
 		ctx := testKit.Se.(context.Context)
