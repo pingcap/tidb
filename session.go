101887f711cb8873f46c2d74b2d752b1b213bec6
tiancaiamao <tiancaiamao@gmail.com>
2017-05-02 21:03:10 +0800

    tidb: fix version7 bootstrap

--- session.go
+++ session.go
@@ -926,7 +926,7 @@ func createSession(store kv.Storage) (*session, error) {
 
 const (
 	notBootstrapped         = 0
-	currentBootstrapVersion = 7
+	currentBootstrapVersion = 8
 )
 
 func getStoreBootstrapVersion(store kv.Storage) int64 {
