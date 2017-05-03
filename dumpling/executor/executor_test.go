ab315ba3964ebe032c54b07918eded427ce5f016
Zejun Li <bobotu@users.noreply.github.com>
2017-05-03 14:14:09 +0800

    [dumpling] *: make string in select result convert to bit correctly. (#3188)

--- dumpling/executor/executor_test.go
+++ dumpling/executor/executor_test.go
@@ -1323,3 +1323,32 @@ func (s *testSuite) TestSimpleDAG(c *C) {
 	tk.MustQuery("select a from t where c = 1 order by a limit 1").Check(testkit.Rows("1"))
 	tk.MustQuery("select count(*) from t where c = 1 ").Check(testkit.Rows("2"))
 }
+
+func (s *testSuite) TestConvertToBit(c *C) {
+	defer func() {
+		s.cleanEnv(c)
+		testleak.AfterTest(c)()
+	}()
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t, t1")
+	tk.MustExec("create table t (a bit(64))")
+	tk.MustExec("create table t1 (a varchar(2))")
+	tk.MustExec(`insert t1 value ('10')`)
+	tk.MustExec(`insert t select a from t1`)
+	tk.MustQuery("select a+0 from t").Check(testkit.Rows("12592"))
+
+	tk.MustExec("drop table if exists t, t1")
+	tk.MustExec("create table t (a bit(64))")
+	tk.MustExec("create table t1 (a binary(2))")
+	tk.MustExec(`insert t1 value ('10')`)
+	tk.MustExec(`insert t select a from t1`)
+	tk.MustQuery("select a+0 from t").Check(testkit.Rows("12592"))
+
+	tk.MustExec("drop table if exists t, t1")
+	tk.MustExec("create table t (a bit(64))")
+	tk.MustExec("create table t1 (a datetime)")
+	tk.MustExec(`insert t1 value ('09-01-01')`)
+	tk.MustExec(`insert t select a from t1`)
+	tk.MustQuery("select a+0 from t").Check(testkit.Rows("20090101000000"))
+}
