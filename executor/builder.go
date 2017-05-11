c1796ac66901867d403ec7daddad914c88364372
argregoryian@gmail.com <GregoryIan@users.noreply.github.com>
2017-05-11 11:24:57 +0800

    executor: address comment

--- executor/builder.go
+++ executor/builder.go
@@ -281,7 +281,7 @@ func (b *executorBuilder) buildLoadData(v *plan.LoadData) Executor {
 		return nil
 	}
 	insertVal := &InsertValues{ctx: b.ctx, Table: tbl, Columns: v.Columns}
-	tableCols := tbl.Cols()
+	tableCols := tbl.WritableCols()
 	columns, err := insertVal.getColumns(tableCols)
 	if err != nil {
 		b.err = errors.Trace(err)
