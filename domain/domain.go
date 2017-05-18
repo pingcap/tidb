4eb727262b71017d8beb48fe76b0b0b5b9b6fd34
zimulala <zimu_xia@126.com>
2017-05-18 20:49:15 +0800

    domain: update the interface of `NewDDL`

--- domain/domain.go
+++ domain/domain.go
@@ -378,7 +378,10 @@ func NewDomain(store kv.Storage, lease time.Duration) (d *Domain, err error) {
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	d.ddl = ddl.NewDDL(d.store, d.infoHandle, &ddlCallback{do: d}, lease)
+	ctx := goctx.Background()
+	callback := &ddlCallback{do: d}
+	// TODO: Use etcd client instead of nil.
+	d.ddl = ddl.NewDDL(ctx, nil, d.store, d.infoHandle, callback, lease)
 	if err = d.Reload(); err != nil {
 		return nil, errors.Trace(err)
 	}
