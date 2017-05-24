b77089f0c4598a42ac5d7adf22c9b5a6d8ea945a
qupeng <onlyqupeng@gmail.com>
2017-05-24 15:21:40 +0800

    [dumpling] add json_type function and compare json with primitive types. (#3307)

--- dumpling/util/types/json/json_test.go
+++ dumpling/util/types/json/json_test.go
@@ -14,7 +14,6 @@
 package json
 
 import (
-	"encoding/json"
 	"testing"
 
 	. "github.com/pingcap/check"
@@ -29,7 +28,7 @@ func TestT(t *testing.T) {
 }
 
 func (s *testJSONSuite) TestJSONSerde(c *C) {
-	var jstr1 = `{"a": [1, "2", {"aa": "bb"}, 4.0], "b": true}`
+	var jstr1 = `{"aaaaaaaaaaa": [1, "2", {"aa": "bb"}, 4.0], "bbbbbbbbbb": true, "ccccccccc": "d"}`
 	j1, err := ParseFromString(jstr1)
 	c.Assert(err, IsNil)
 
@@ -59,9 +58,9 @@ func (s *testJSONSuite) TestJSONSerde(c *C) {
 		t, err := Deserialize(data)
 		c.Assert(err, IsNil)
 
-		v1, _ := json.Marshal(t)
-		v2, _ := json.Marshal(s.Out)
-		c.Assert(string(v1), Equals, string(v2))
+		v1 := t.String()
+		v2 := s.Out.String()
+		c.Assert(v1, Equals, v2)
 	}
 }
 
@@ -73,4 +72,91 @@ func (s *testJSONSuite) TestParseFromString(c *C) {
 
 	var jstr2 = j1.String()
 	c.Assert(jstr2, Equals, `{"a":[1,"2",{"aa":"bb"},4,null],"b":true,"c":null}`)
+}
+
+func (s *testJSONSuite) TestJSONType(c *C) {
+	j1, err := ParseFromString(`{"a": "b"}`)
+	c.Assert(err, IsNil)
+
+	j2, err := ParseFromString(`["a", "b"]`)
+	c.Assert(err, IsNil)
+
+	j3, err := ParseFromString(`3`)
+	c.Assert(err, IsNil)
+
+	j4, err := ParseFromString(`3.0`)
+	c.Assert(err, IsNil)
+
+	j5, err := ParseFromString(`null`)
+	c.Assert(err, IsNil)
+
+	j6, err := ParseFromString(`true`)
+	c.Assert(err, IsNil)
+
+	var jList = []struct {
+		In  JSON
+		Out string
+	}{
+		{j1, "OBJECT"},
+		{j2, "ARRAY"},
+		{j3, "INTEGER"},
+		{j4, "DOUBLE"},
+		{j5, "NULL"},
+		{j6, "BOOLEAN"},
+	}
+
+	for _, j := range jList {
+		c.Assert(j.In.Type(), Equals, j.Out)
+	}
+}
+
+func (s *testJSONSuite) TestCompareJSON(c *C) {
+	var cmp int
+
+	// compare two JSON boolean.
+	jBool1, _ := ParseFromString(`true`)
+	jBool2, _ := ParseFromString(`false`)
+	cmp, _ = CompareJSON(jBool1, jBool2)
+	c.Assert(cmp > 0, IsTrue)
+
+	// compare two JSON ARRAY
+	jArray1, _ := ParseFromString(`["a", "c"]`)
+	jArray2, _ := ParseFromString(`["a", "b"]`)
+	cmp, _ = CompareJSON(jArray1, jArray2)
+	c.Assert(cmp > 0, IsTrue)
+
+	// compare two JSON OBJECT
+	jObject1, _ := ParseFromString(`{"a": "b"}`)
+	jObject2, _ := ParseFromString(`{"a": "c"}`)
+	cmp, _ = CompareJSON(jObject1, jObject2)
+	c.Assert(cmp != 0, IsTrue)
+
+	// compare two JSON string
+	jString1, _ := ParseFromString(`"hello"`)
+	jString2, _ := ParseFromString(`"hello, world"`)
+	cmp, _ = CompareJSON(jString1, jString2)
+	c.Assert(cmp < 0, IsTrue)
+
+	// compare two JSON integer
+	jInteger1, _ := ParseFromString(`3`)
+	jInteger2, _ := ParseFromString(`5`)
+	cmp, _ = CompareJSON(jInteger1, jInteger2)
+	c.Assert(cmp < 0, IsTrue)
+
+	jNull1, _ := ParseFromString(`null`)
+	jNull2, _ := ParseFromString(`null`)
+	cmp, _ = CompareJSON(jNull1, jNull2)
+	c.Assert(cmp == 0, IsTrue)
+
+	// compare two JSON with different types.
+	cmp, _ = CompareJSON(jBool1, jArray1)
+	c.Assert(cmp > 0, IsTrue)
+	cmp, _ = CompareJSON(jArray1, jObject1)
+	c.Assert(cmp > 0, IsTrue)
+	cmp, _ = CompareJSON(jObject1, jString1)
+	c.Assert(cmp > 0, IsTrue)
+	cmp, _ = CompareJSON(jString1, jInteger1)
+	c.Assert(cmp > 0, IsTrue)
+	cmp, _ = CompareJSON(jInteger1, jNull1)
+	c.Assert(cmp > 0, IsTrue)
 }
