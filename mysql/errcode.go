c4fb4d7d4aea3ee37bd14a0c73feb1f3b2df673c
qupeng <onlyqupeng@gmail.com>
2017-05-27 17:46:49 +0800

    fic errname.

--- mysql/errcode.go
+++ mysql/errcode.go
@@ -882,7 +882,7 @@ const (
 	ErrMustChangePasswordLogin                                      = 1862
 	ErrRowInWrongPartition                                          = 1863
 	ErrErrorLast                                                    = 1863
-	ErrNonDefaultValueForGeneratedColumn                            = 3105
+	ErrBadValueGeneratedColumn                                      = 3105
 	ErrDependentByGeneratedColumn                                   = 3108
 	ErrInvalidJSONText                                              = 3140
 	ErrInvalidJSONPath                                              = 3143
