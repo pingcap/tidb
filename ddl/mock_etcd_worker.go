a3316ca61ec096c07028ebf0e32204f1b00eb314
zimulala <zimu_xia@126.com>
2017-05-30 10:01:05 +0800

    ddl: pass golint

--- ddl/mock_etcd_worker.go
+++ ddl/mock_etcd_worker.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package mock is just for test only.
+// Package ddl is just for test only.
 package ddl
 
 import (
