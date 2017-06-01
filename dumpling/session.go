e1b89a014fc56cc00023c1b28dad999754f56145
louishust <hdchild@163.com>
2017-06-01 09:37:41 +0800

    [dumpling] privileges: Upgrade stored password format to Compatible with MySQL. (#3292)
    Password stored in mysql.user of TIDB is the format of SHA1(pwd),
    which is not compatible with MySQL.
    Different password format will cause user migrate data from MySQL
    login failed on TIDB.

--- dumpling/session.go
+++ dumpling/session.go
@@ -967,7 +967,7 @@ func createSession(store kv.Storage) (*session, error) {
 
 const (
 	notBootstrapped         = 0
-	currentBootstrapVersion = 11
+	currentBootstrapVersion = 12
 )
 
 func getStoreBootstrapVersion(store kv.Storage) int64 {
