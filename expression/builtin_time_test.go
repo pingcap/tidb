ea76444b5e8e388ff1a123a32b7f2cfa1eb49c88
真人 <weizhen.wang@duitang.com>
2017-06-16 21:22:13 +0800

    clean the empty line

--- expression/builtin_time_test.go
+++ expression/builtin_time_test.go
@@ -532,7 +532,6 @@ func (s *testEvaluatorSuite) TestAddTimeSig(c *C) {
 		{"235959", "00:00:01", "24:00:00"},
 		{"110:00:00", "1 02:00:00", "136:00:00"},
 		{"-110:00:00", "1 02:00:00", "-84:00:00"},
-		{"234", "1.123", "00:02:35.123000"},
 	}
 	for _, t := range tbl {
 		dur, err := types.ParseDuration(t.Input, getFsp(t.Input))
@@ -613,7 +612,6 @@ func (s *testEvaluatorSuite) TestSubTimeSig(c *C) {
 		result, _ := d.ToString()
 		c.Assert(result, Equals, t.expect)
 	}
-
 	tbll := []struct {
 		Input         int64
 		InputDuration int64
