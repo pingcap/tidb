d64259233b61bcee4c2dfc27d1d7d8fefcbb5065
Yiding Cui <winoros@gmail.com>
2017-06-22 14:37:37 +0800

    add calculation of column sampling

--- statistics/selectivity.go
+++ statistics/selectivity.go
@@ -27,8 +27,9 @@ import (
 // If one condition can't be calculated, we will assume that the selectivity of this condition is 0.8.
 const selectionFactor = 0.8
 
-// indexBlock is used when calculating selectivity.
-type indexBlock struct {
+// calcBlock is used when calculating selectivity.
+type calcBlock struct {
+	tp int
 	// the position that this index is in the index slice, if is -1, then this is primary key.
 	pos int
 	// the ith bit of `cover` will tell whether the ith expression is covered by this index.
@@ -41,14 +42,15 @@ type indexBlock struct {
 // The definition of selectivity is (row count after filter / row count before filter).
 // And exprs must be CNF now, in other words, `exprs[0] and exprs[1] and ... and exprs[len - 1]` should be held when you call this.
 // TODO: support expressions that the top layer is a DNF.
+// Currently the time complexity if o(n^2).
 func Selectivity(ctx context.Context, exprs []expression.Expression, keys []expression.KeyInfo,
-	keyIDs []int64, hasPk bool, lengths []int, statsTbl *Table) (float64, error) {
+	keyIDs []int64, hasPk bool, lengths [][]int, cols []*expression.Column, statsTbl *Table) (float64, error) {
 	// TODO: If len(exprs) is bigger than 64, we could use bitset structure to replace the uint64.
 	// This will simplify some code and speed up if we use this rather than a boolean slice.
 	if statsTbl.Pseudo || len(exprs) > 64 {
 		return selectionFactor, nil
 	}
-	var blocks []*indexBlock
+	var blocks []*calcBlock
 	sc := ctx.GetSessionVars().StmtCtx
 	idxBeginID := 0
 	if hasPk {
@@ -56,11 +58,12 @@ func Selectivity(ctx context.Context, exprs []expression.Expression, keys []expr
 		c := statsTbl.Columns[keyIDs[0]]
 		// pk should have histogram.
 		if c != nil && len(c.Buckets) > 0 {
-			covered, ranges, err := getMaskByColumn(sc, exprs, keys[0][0], ranger.IntRangeType)
+			covered, ranges, err := getMaskAndRanges(sc, exprs, ranger.IntRangeType, nil, keys[0][0])
 			if err != nil {
 				return 0, errors.Trace(err)
 			}
-			blocks = append(blocks, &indexBlock{
+			blocks = append(blocks, &calcBlock{
+				tp:     ranger.IntRangeType,
 				pos:    0,
 				cover:  covered,
 				ranges: ranges,
@@ -71,18 +74,36 @@ func Selectivity(ctx context.Context, exprs []expression.Expression, keys []expr
 		c := statsTbl.Indices[keyIDs[i]]
 		// This index should have histogram.
 		if c != nil && len(c.Buckets) > 0 {
-			covered, ranges, err := getMaskByIndex(sc, exprs, keys[i], lengths)
+			// covered, ranges, err := getMaskByIndex(sc, exprs, keys[i], lengths)
+			covered, ranges, err := getMaskAndRanges(sc, exprs, ranger.IndexRangeType, lengths[i], keys[i]...)
 			if err != nil {
 				return 0, errors.Trace(err)
 			}
-			blocks = append(blocks, &indexBlock{
-				pos:    0,
+			blocks = append(blocks, &calcBlock{
+				tp:     ranger.IndexRangeType,
+				pos:    i,
 				cover:  covered,
 				ranges: ranges,
 			})
 		}
 	}
-	blocks = getUsedIndicesByGreedy(blocks)
+	for i, col := range cols {
+		c := statsTbl.Columns[col.ID]
+		// This column should have histogram.
+		if c != nil && len(c.Buckets) > 0 {
+			covered, ranges, err := getMaskAndRanges(sc, exprs, ranger.ColumnRangeType, nil, col)
+			if err != nil {
+				return 0, errors.Trace(err)
+			}
+			blocks = append(blocks, &calcBlock{
+				tp: ranger.ColumnRangeType,
+				pos: i,
+				cover: covered,
+				ranges: ranges,
+			})
+		}
+	}
+	blocks = getUsedBlocksByGreedy(blocks)
 	ret := 1.0
 	// cut off the higher bit of mask
 	mask := uint64(math.MaxUint64) >> uint64(64-len(exprs))
@@ -104,39 +125,19 @@ func Selectivity(ctx context.Context, exprs []expression.Expression, keys []expr
 		}
 		ret *= rowCount / float64(statsTbl.Count)
 	}
-	return 0, nil
+	if mask > 0 {
+		ret *= selectionFactor
+	}
+	return ret, nil
 }
 
-func getMaskByColumn(sc *variable.StatementContext, exprs []expression.Expression, col *expression.Column,
-	rangeType int) (uint64, []types.Range, error) {
-	ranges, accessConds, _, err := ranger.BuildRange(sc, exprs, rangeType, []*expression.Column{col}, nil)
-	if err != nil {
-		return 0, nil, errors.Trace(err)
-	}
-	mask := uint64(0)
-	// There accessConds is a sub sequence of exprs. i.e. If exprs is `[ a > 1, b > 1, a < 10]`,
-	// accessConds could be `[ a > 1, b > 1 ]` or `[a > 1, a < 10]`, couldn't be `[ a < 10, a > 1 ]`
-	// So we can use the following o(n) algorithm.
-	i, j := 0, 0
-	for i < len(exprs) && j < len(accessConds) {
-		if exprs[i].Equal(accessConds[j], nil) {
-			mask |= 1 << uint64(i)
-			i++
-			j++
-		} else {
-			i++
-		}
-	}
-	return mask, ranges, nil
-}
-
-func getMaskByIndex(sc *variable.StatementContext, exprs []expression.Expression, idxCols []*expression.Column,
-	lengths []int) (uint64, []types.Range, error) {
+func getMaskAndRanges(sc *variable.StatementContext, exprs []expression.Expression, rangeType int,
+	lengths []int, cols ...*expression.Column) (uint64, []types.Range, error) {
 	exprsClone := make([]expression.Expression, 0, len(exprs))
 	for _, expr := range exprs {
 		exprsClone = append(exprsClone, expr.Clone())
 	}
-	ranges, accessConds, _, err := ranger.BuildRange(sc, exprsClone, ranger.IndexRangeType, idxCols, lengths)
+	ranges, accessConds, _, err := ranger.BuildRange(sc, exprsClone, rangeType, cols, lengths)
 	if err != nil {
 		return 0, nil, errors.Trace(err)
 	}
@@ -152,19 +153,19 @@ func getMaskByIndex(sc *variable.StatementContext, exprs []expression.Expression
 	return mask, ranges, nil
 }
 
-// getUsedIndicesByGreedy will select the indices and pk used for calculate selectivity by greedy algorithm.
-func getUsedIndicesByGreedy(blocks []*indexBlock) (newBlocks []*indexBlock) {
+// getUsedBlocksByGreedy will select the indices and pk used for calculate selectivity by greedy algorithm.
+func getUsedBlocksByGreedy(blocks []*calcBlock) (newBlocks []*calcBlock) {
 	mask := uint64(math.MaxUint64)
 	for {
 		// Choose the index that covers most.
 		bestID := -1
 		bestCount := 0
+		bestID, bestCount, bestTp := -1, 0, ranger.ColumnRangeType
 		for i, block := range blocks {
 			block.cover &= mask
 			bits := popcount(block.cover)
-			if bestCount < bits {
-				bestCount = bits
-				bestID = i
+			if (bestTp == ranger.ColumnRangeType && block.tp < ranger.ColumnRangeType) || bestCount < bits {
+				bestID, bestCount, bestTp = i, bits, block.tp
 			}
 		}
 		if bestCount == 0 {
