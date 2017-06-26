350c7d25ee4e7045c7e3e87ee0ca2fac46810d73
louishust <hdchild@163.com>
2017-06-26 22:07:08 +0800

    expression,executor:rewrite built-in func ascii using new expression evaluation architecture

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -791,6 +791,13 @@ func (s *testSuite) TestStringBuiltin(c *C) {
 	result := tk.MustQuery("select length(a), length(b), length(c), length(d), length(e), length(f), length(null) from t")
 	result.Check(testkit.Rows("1 3 19 8 6 2 <nil>"))
 
+	// for ascii
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a int, b double, c datetime, d time, e char(20), f bit(10))")
+	tk.MustExec(`insert into t values(1, 1.1, "2017-01-01 12:01:01", "12:01:01", "abcdef", 0b10101)`)
+	result = tk.MustQuery("select ascii(a), ascii(b), ascii(c), ascii(d), ascii(e), ascii(f), ascii(null) from t")
+	result.Check(testkit.Rows("49 49 50 49 97 0 <nil>"))
+
 	// for concat
 	tk.MustExec("drop table if exists t")
 	tk.MustExec("create table t(a int, b double, c datetime, d time, e char(20))")
