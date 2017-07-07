1524c021bcf09e0a4990e981a77c77acb14598c6
XuHuaiyu <391585975@qq.com>
2017-07-07 13:34:55 +0800

    fix ci

--- expression/builtin_math_test.go
+++ expression/builtin_math_test.go
@@ -21,7 +21,6 @@ import (
 	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb/ast"
 	"github.com/pingcap/tidb/mysql"
-	"github.com/pingcap/tidb/util/charset"
 	"github.com/pingcap/tidb/util/testleak"
 	"github.com/pingcap/tidb/util/testutil"
 	"github.com/pingcap/tidb/util/types"
@@ -555,12 +554,6 @@ func (s *testEvaluatorSuite) TestSin(c *C) {
 	for _, t := range cases {
 		f, err := newFunctionForTest(s.ctx, ast.Sin, primitiveValsToConstants([]interface{}{t.args})...)
 		c.Assert(err, IsNil)
-		tp := f.GetType()
-		c.Assert(tp.Tp, Equals, mysql.TypeDouble)
-		c.Assert(tp.Charset, Equals, charset.CharsetBin)
-		c.Assert(tp.Collate, Equals, charset.CollationBin)
-		c.Assert(tp.Flag, Equals, uint(mysql.BinaryFlag))
-		c.Assert(tp.Flen, Equals, 23)
 
 		d, err := f.Eval(nil)
 		if t.getErr {
@@ -597,12 +590,6 @@ func (s *testEvaluatorSuite) TestCos(c *C) {
 	for _, t := range cases {
 		f, err := newFunctionForTest(s.ctx, ast.Cos, primitiveValsToConstants([]interface{}{t.args})...)
 		c.Assert(err, IsNil)
-		tp := f.GetType()
-		c.Assert(tp.Tp, Equals, mysql.TypeDouble)
-		c.Assert(tp.Charset, Equals, charset.CharsetBin)
-		c.Assert(tp.Collate, Equals, charset.CollationBin)
-		c.Assert(tp.Flag, Equals, uint(mysql.BinaryFlag))
-		c.Assert(tp.Flen, Equals, 23)
 
 		d, err := f.Eval(nil)
 		if t.getErr {
@@ -713,12 +700,6 @@ func (s *testEvaluatorSuite) TestTan(c *C) {
 	for _, t := range cases {
 		f, err := newFunctionForTest(s.ctx, ast.Tan, primitiveValsToConstants([]interface{}{t.args})...)
 		c.Assert(err, IsNil)
-		tp := f.GetType()
-		c.Assert(tp.Tp, Equals, mysql.TypeDouble)
-		c.Assert(tp.Charset, Equals, charset.CharsetBin)
-		c.Assert(tp.Collate, Equals, charset.CollationBin)
-		c.Assert(tp.Flag, Equals, uint(mysql.BinaryFlag))
-		c.Assert(tp.Flen, Equals, 23)
 
 		d, err := f.Eval(nil)
 		if t.getErr {
