78f4cef3425f3233f765ccc66dc85f79d6ebcf1f
Wenxuan Shi <hi@breeswish.org>
2017-07-07 00:40:33 -0500

    plan: check database when parsing `show tables` and `show table status` (#3655)
    * Show errors when no database is selected

--- executor/show_test.go
+++ executor/show_test.go
@@ -18,6 +18,7 @@ import (
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb"
+	"github.com/pingcap/tidb/plan"
 	"github.com/pingcap/tidb/privilege/privileges"
 	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/util/testkit"
@@ -273,4 +274,12 @@ func (s *testSuite) TestShowStatsMeta(c *C) {
 	result = tk.MustQuery("show stats_meta where table_name = 't'")
 	c.Assert(len(result.Rows()), Equals, 1)
 	c.Assert(result.Rows()[0][1], Equals, "t")
+}
+
+func (s *testSuite) TestIssue3641(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	_, err := tk.Exec("show tables;")
+	c.Assert(err.Error(), Equals, plan.ErrNoDB.Error())
+	_, err = tk.Exec("show table status;")
+	c.Assert(err.Error(), Equals, plan.ErrNoDB.Error())
 }
