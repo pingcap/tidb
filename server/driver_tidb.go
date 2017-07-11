b609f8c97900062e536a34ab936d4cd0e8d5896b
tiancaiamao <tiancaiamao@gmail.com>
2017-07-11 17:09:33 +0800

    server: escape database name "use `xxx`" in OpenCtx

--- server/driver_tidb.go
+++ server/driver_tidb.go
@@ -130,7 +130,7 @@ func (qd *TiDBDriver) OpenCtx(connID uint64, capability uint32, collation uint8,
 	session.SetClientCapability(capability)
 	session.SetConnectionID(connID)
 	if dbname != "" {
-		_, err = session.Execute("use " + dbname)
+		_, err = session.Execute(fmt.Sprintf("use `%s`", dbname))
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
