26467afaa6107987adc3f1f9f1c73aee9d0ecf2f
zimulala <zimu_xia@126.com>
2017-07-19 13:51:10 +0800

    ddl: add a comment

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -147,6 +147,7 @@ type DDL interface {
 	OwnerManager() OwnerManager
 	// WorkerVars gets the session variables for DDL worker.
 	WorkerVars() *variable.SessionVars
+	// SetHook sets the hook. It's exported for testing.
 	SetHook(h Callback)
 }
 
@@ -452,6 +453,7 @@ func (d *ddl) callHookOnChanged(err error) error {
 	return errors.Trace(err)
 }
 
+// SetHook implements DDL.SetHook interface.
 func (d *ddl) SetHook(h Callback) {
 	d.hookMu.Lock()
 	defer d.hookMu.Unlock()
