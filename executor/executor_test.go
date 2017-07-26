3befdf3662d3cb74be070fc4145abf491b2923cf
XuHuaiyu <391585975@qq.com>
2017-07-26 17:29:04 +0800

    *: rewrite leftShift using new expression evaluation architecture

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -1029,6 +1029,10 @@ func (s *testSuite) TestOpBuiltin(c *C) {
 	// for logical and
 	result := tk.MustQuery("select 1 && 1, 1 && 0, 0 && 1, 0 && 0, 2 && -1, null && 1, '1a' && 'a'")
 	result.Check(testkit.Rows("1 0 0 0 1 <nil> 0"))
+
+	// for leftShift
+	result = tk.MustQuery("select 123 << 2, -123 << 2, null << 1")
+	result.Check(testkit.Rows("492 18446744073709551124 <nil>"))
 }
 
 func (s *testSuite) TestBuiltin(c *C) {
