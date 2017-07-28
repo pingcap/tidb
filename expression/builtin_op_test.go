024d8d73c01abfeaf7cd12b2827066f6f4c1e1bd
XuHuaiyu <391585975@qq.com>
2017-07-29 00:44:53 +0800

    expression, executor: 1. revert typeinfer of cast and binary ops 2. add TestUnaryNot

--- expression/builtin_op_test.go
+++ expression/builtin_op_test.go
@@ -425,3 +425,54 @@ func (s *testEvaluatorSuite) TestBitNeg(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(f.isDeterministic(), IsTrue)
 }
+
+func (s *testEvaluatorSuite) TestUnaryNot(c *C) {
+	defer testleak.AfterTest(c)()
+
+	sc := s.ctx.GetSessionVars().StmtCtx
+	origin := sc.IgnoreTruncate
+	defer func() {
+		sc.IgnoreTruncate = origin
+	}()
+	sc.IgnoreTruncate = true
+
+	cases := []struct {
+		args     []interface{}
+		expected int64
+		isNil    bool
+		getErr   bool
+	}{
+		{[]interface{}{1}, 0, false, false},
+		{[]interface{}{0}, 1, false, false},
+		{[]interface{}{123}, 0, false, false},
+		{[]interface{}{-123}, 0, false, false},
+		{[]interface{}{"123"}, 0, false, false},
+		{[]interface{}{nil}, 0, true, false},
+
+		{[]interface{}{errors.New("must error")}, 0, false, true},
+	}
+
+	for _, t := range cases {
+		f, err := newFunctionForTest(s.ctx, ast.UnaryNot, primitiveValsToConstants(t.args)...)
+		c.Assert(err, IsNil)
+		d, err := f.Eval(nil)
+		if t.getErr {
+			c.Assert(err, NotNil)
+		} else {
+			c.Assert(err, IsNil)
+			if t.isNil {
+				c.Assert(d.Kind(), Equals, types.KindNull)
+			} else {
+				c.Assert(d.GetInt64(), Equals, t.expected)
+			}
+		}
+	}
+
+	// Test incorrect parameter count.
+	_, err := newFunctionForTest(s.ctx, ast.UnaryNot, Zero, Zero)
+	c.Assert(err, NotNil)
+
+	f, err := funcs[ast.UnaryNot].getFunction([]Expression{Zero}, s.ctx)
+	c.Assert(err, IsNil)
+	c.Assert(f.isDeterministic(), IsTrue)
+}
