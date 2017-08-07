2ccb4ebccf72a3e655ae04090a603f46d12c17e5
yulai.li <yulai.li@speedycloud.cn>
2017-08-07 11:42:46 +0800

    Update command line parameter for store

--- tidb-server/main.go
+++ tidb-server/main.go
@@ -47,7 +47,7 @@ import (
 
 var (
 	version         = flag.Bool("V", false, "print version information and exit")
-	store           = flag.String("store", "goleveldb", "registered store name, [memory, goleveldb, boltdb, tikv, mocktikv]")
+	store           = flag.String("store", "goleveldb", "registered store name, [memory, goleveldb, boltdb, tikv, mocktikv, lmdb]")
 	storePath       = flag.String("path", "/tmp/tidb", "tidb storage path")
 	logLevel        = flag.String("L", "info", "log level: info, debug, warn, error, fatal")
 	host            = flag.String("host", "0.0.0.0", "tidb server host")
