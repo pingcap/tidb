cb5928a7c50b07bf7651673529fb819d177f2714
spongedc <duchuangucas@live.com>
2017-08-10 17:41:39 +0800

    set charset && binaryFlag if needed

--- expression/builtin_string.go
+++ expression/builtin_string.go
@@ -2236,7 +2236,7 @@ func (c *eltFunctionClass) getFunction(args []Expression, ctx context.Context) (
 	}
 	for _, arg := range args[1:] {
 		argType := arg.GetType()
-		//setBinFlagOrBinStr(argType, bf.tp)
+		setBinFlagOrBinStr(argType, bf.tp)
 		if argType.Flen > bf.tp.Flen {
 			bf.tp.Flen = argType.Flen
 		}
--- plan/typeinfer_test.go
+++ plan/typeinfer_test.go
@@ -164,6 +164,7 @@ func (s *testPlanSuite) createTestCase4StrFuncs() []typeInferTestCase {
 		{"tidb_version()", mysql.TypeVarString, charset.CharsetUTF8, 0, len(printer.GetTiDBInfo()), types.UnspecifiedLength},
 		{"password(c_char)", mysql.TypeVarString, charset.CharsetUTF8, 0, mysql.PWDHashLen + 1, types.UnspecifiedLength},
 		{"elt(c_int, c_char, c_char, c_char)", mysql.TypeVarString, charset.CharsetUTF8, 0, 20, types.UnspecifiedLength},
+		{"elt(c_int, c_char, c_char, c_binary)", mysql.TypeVarString, charset.CharsetBin, mysql.BinaryFlag, 20, types.UnspecifiedLength},
 
 		{"lpad('TiDB',   12,    'go'    )", mysql.TypeVarString, charset.CharsetUTF8, 0, 48, types.UnspecifiedLength},
 		{"lpad(c_binary, 12,    'go'    )", mysql.TypeVarString, charset.CharsetBin, mysql.BinaryFlag, 12, types.UnspecifiedLength},
