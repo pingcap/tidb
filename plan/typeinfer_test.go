bd623be836c9a8a79d8a17ee7de63be717296cd5
XuHuaiyu <391585975@qq.com>
2017-08-10 16:56:38 +0800

    fix ci

--- plan/typeinfer_test.go
+++ plan/typeinfer_test.go
@@ -368,7 +368,7 @@ func (s *testPlanSuite) createTestCase4ControlFuncs() []typeInferTestCase {
 		{"ifnull(c_int, c_int    )", mysql.TypeLong, charset.CharsetBin, mysql.BinaryFlag, 22, 0},
 		{"ifnull(c_int, c_decimal)", mysql.TypeNewDecimal, charset.CharsetBin, mysql.BinaryFlag, 17, 3},
 		{"if(c_int, c_decimal, c_int)", mysql.TypeNewDecimal, charset.CharsetBin, mysql.BinaryFlag, 15, 3},
-		{"if(c_int, c_char, c_int   )", mysql.TypeString, charset.CharsetUTF8, 0, 20, 0},
+		{"if(c_int, c_char, c_int   )", mysql.TypeString, charset.CharsetBin, 0, 20, 0},
 	}
 }
 
