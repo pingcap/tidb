ed107542cff5836da7521c15da15832d733622c0
Lynn <zimu_xia@126.com>
2017-08-11 20:45:31 +0800

    ddl: rename the interface (#4153)

--- ddl/mock.go
+++ ddl/mock.go
@@ -78,8 +78,8 @@ func (m *mockOwnerManager) GetOwnerID(ctx goctx.Context, key string) (string, er
 	return "", errors.New("no owner")
 }
 
-// CampaignOwners implements mockOwnerManager.CampaignOwners interface.
-func (m *mockOwnerManager) CampaignOwners(_ goctx.Context) error {
+// CampaignOwner implements mockOwnerManager.CampaignOwner interface.
+func (m *mockOwnerManager) CampaignOwner(_ goctx.Context) error {
 	m.SetOwner(true)
 	return nil
 }
