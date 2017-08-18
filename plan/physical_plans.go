01cec98cd2373bb56e1bc905c83aba8471e8607a
hanfei <hanfei19910905@gmail.com>
2017-08-18 20:40:19 +0800

    address comment

--- plan/physical_plans.go
+++ plan/physical_plans.go
@@ -1183,18 +1183,18 @@ func buildSchema(p PhysicalPlan) {
 
 // rebuildSchema rebuilds the schema for physical plans, because new planner may change indexjoin's schema.
 func rebuildSchema(p PhysicalPlan) bool {
-	need2Rebuild := false
+	needRebuild := false
 	for _, ch := range p.Children() {
-		need2Rebuild = need2Rebuild || rebuildSchema(ch.(PhysicalPlan))
+		needRebuild = needRebuild || rebuildSchema(ch.(PhysicalPlan))
 	}
-	if need2Rebuild {
+	if needRebuild {
 		buildSchema(p)
 	}
 	switch p.(type) {
 	case *PhysicalIndexJoin, *PhysicalHashJoin, *PhysicalMergeJoin:
-		need2Rebuild = true
+		needRebuild = true
 	case *Projection, *PhysicalAggregation:
-		need2Rebuild = false
+		needRebuild = false
 	}
-	return need2Rebuild
+	return needRebuild
 }
