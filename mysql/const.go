97e9dfe92ce8acb6fa42834097a504379c711567
Wenxuan Shi <hi@breeswish.org>
2017-08-21 16:10:04 +0800

    expression: Fix Flen for DATETIME, TIMESTAMP and TIME (#4264)

--- mysql/const.go
+++ mysql/const.go
@@ -211,7 +211,7 @@ const (
 	MaxDateWidth            = 10 // YYYY-MM-DD.
 	MaxDatetimeWidthNoFsp   = 19 // YYYY-MM-DD HH:MM:SS
 	MaxDatetimeWidthWithFsp = 26 // YYYY-MM-DD HH:MM:SS[.fraction]
-	MaxDurationWidthNoFsp   = 9  // HH:MM:SS
+	MaxDurationWidthNoFsp   = 10 // HH:MM:SS
 	MaxDurationWidthWithFsp = 15 // HH:MM:SS[.fraction]
 	MaxBlobWidth            = 16777216
 )
