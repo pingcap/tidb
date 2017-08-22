d1ff141e5e4f23cfc582d9dfdfee9870d405a959
disksing <i@disksing.com>
2017-08-22 20:02:08 +0800

    vendor: update pd.

--- _vendor/src/github.com/pingcap/pd/pd-client/client.go
+++ _vendor/src/github.com/pingcap/pd/pd-client/client.go
@@ -14,8 +14,6 @@
 package pd
 
 import (
-	"net"
-	"net/url"
 	"strings"
 	"sync"
 	"time"
@@ -63,6 +61,7 @@ type tsoRequest struct {
 
 const (
 	pdTimeout             = 3 * time.Second
+	updateLeaderTimeout   = time.Second // Use a shorter timeout to recover faster from network isolation.
 	maxMergeTSORequests   = 10000
 	maxInitClusterRetries = 100
 )
@@ -148,10 +147,10 @@ func (c *client) initClusterID() error {
 }
 
 func (c *client) updateLeader() error {
-	ctx, cancel := context.WithCancel(c.ctx)
-	defer cancel()
 	for _, u := range c.urls {
+		ctx, cancel := context.WithTimeout(c.ctx, updateLeaderTimeout)
 		members, err := c.getMembers(ctx, u)
+		cancel()
 		if err != nil || members.GetLeader() == nil || len(members.GetLeader().GetClientUrls()) == 0 {
 			continue
 		}
@@ -206,17 +205,7 @@ func (c *client) getOrCreateGRPCConn(addr string) (*grpc.ClientConn, error) {
 		return conn, nil
 	}
 
-	cc, err := grpc.Dial(addr, grpc.WithDialer(func(addr string, d time.Duration) (net.Conn, error) {
-		u, err := url.Parse(addr)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-		// For tests.
-		if u.Scheme == "unix" || u.Scheme == "unixs" {
-			return net.DialTimeout(u.Scheme, u.Host, d)
-		}
-		return net.DialTimeout("tcp", u.Host, d)
-	}), grpc.WithInsecure()) // TODO: Support HTTPS.
+	cc, err := grpc.Dial(strings.TrimPrefix(addr, "http://"), grpc.WithInsecure()) // TODO: Support HTTPS.
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
@@ -296,7 +285,7 @@ func (c *client) tsLoop() {
 
 		if stream == nil {
 			var ctx context.Context
-			ctx, cancel = context.WithCancel(c.ctx)
+			ctx, cancel = context.WithTimeout(c.ctx, pdTimeout)
 			stream, err = c.leaderClient().Tso(ctx)
 			if err != nil {
 				log.Errorf("[pd] create tso stream error: %v", err)
