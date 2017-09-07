27368123818890d8d7eddfe6f111ab45fec9646d
Jian Zhang <zjsariel@gmail.com>
2017-09-07 18:02:15 +0800

    expression: rewrite builtin function: EXPORT_SET (#4434)

--- expression/builtin_string_test.go
+++ expression/builtin_string_test.go
@@ -1685,6 +1685,8 @@ func (s *testEvaluatorSuite) TestExportSet(c *C) {
 	for _, t := range estd {
 		f, err := fc.getFunction(s.ctx, datumsToConstants(types.MakeDatums(t.argLst...)))
 		c.Assert(err, IsNil)
+		c.Assert(f, NotNil)
+		c.Assert(f.canBeFolded(), IsTrue)
 		exportSetRes, err := f.eval(nil)
 		res, err := exportSetRes.ToString()
 		c.Assert(err, IsNil)
