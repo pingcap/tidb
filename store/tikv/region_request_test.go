15e0ff07a9105fc54a01a058ccd442c6b5316b1f
atmzhou <zhouningnan@gmail.com>
2017-10-24 15:39:34 +0800

    pass compile

--- store/tikv/region_request_test.go
+++ store/tikv/region_request_test.go
@@ -211,6 +211,9 @@ func (s *mockTikvGrpcServer) RawScan(goctx.Context, *kvrpcpb.RawScanRequest) (*k
 func (s *mockTikvGrpcServer) Coprocessor(goctx.Context, *coprocessor.Request) (*coprocessor.Response, error) {
 	return nil, errors.New("unreachable")
 }
+func (s *mockTikvGrpcServer) CoprocessorStream(*coprocessor.Request, kvrpcpb.Tikv_CoprocessorStreamServer) error {
+	return errors.New("unreachable")
+}
 func (s *mockTikvGrpcServer) Raft(tikvpb.Tikv_RaftServer) error {
 	return errors.New("unreachable")
 }
