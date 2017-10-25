1f4fc3532077771cf78c52d496cb5296cb3c7ba0
HuaiyuXu <391585975@qq.com>
2017-10-25 00:55:20 -0500

    executor, plan: correct the schema type of ShowStmt (#4886)

--- executor/show_test.go
+++ executor/show_test.go
@@ -18,6 +18,7 @@ import (
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb"
+	"github.com/pingcap/tidb/mysql"
 	"github.com/pingcap/tidb/plan"
 	"github.com/pingcap/tidb/privilege/privileges"
 	"github.com/pingcap/tidb/sessionctx/variable"
@@ -451,4 +452,20 @@ func (s *testSuite) TestShow2(c *C) {
 
 	tk.MustExec("grant all on *.* to 'root'@'%'")
 	tk.MustQuery("show grants").Check(testkit.Rows("GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"))
+}
+
+func (s *testSuite) TestCollation(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+
+	rs, err := tk.Exec("show collation;")
+	c.Assert(err, IsNil)
+	fields, err := rs.Fields()
+	c.Assert(err, IsNil)
+	c.Assert(fields[0].Column.Tp, Equals, mysql.TypeVarchar)
+	c.Assert(fields[1].Column.Tp, Equals, mysql.TypeVarchar)
+	c.Assert(fields[2].Column.Tp, Equals, mysql.TypeLonglong)
+	c.Assert(fields[3].Column.Tp, Equals, mysql.TypeVarchar)
+	c.Assert(fields[4].Column.Tp, Equals, mysql.TypeVarchar)
+	c.Assert(fields[5].Column.Tp, Equals, mysql.TypeLonglong)
 }
