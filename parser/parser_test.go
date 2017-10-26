e40ad67efa9408230773a158fd9f4558580cc903
Darren Hoo <darren.hoo@gmail.com>
2017-10-26 22:52:13 +0800

    parser, executor: truncate info field for show processlist and show full processlist support (#4739)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1914,6 +1914,7 @@ func (s *testParserSuite) TestSessionManage(c *C) {
 		{"kill tidb connection 23123", true},
 		{"kill tidb query 23123", true},
 		{"show processlist", true},
+		{"show full processlist", true},
 	}
 	s.RunTest(c, table)
 }
