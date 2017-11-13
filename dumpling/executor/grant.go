3b9a14f9126a1f252bbfab04663d55ae047ab1f1
Ewan Chou <coocood@gmail.com>
2017-11-13 21:06:31 +0800

    [dumpling] executor: use baseExecutor for all Executors (#5087)
    Makes it easy to add new method on Executor interface.

--- dumpling/executor/grant.go
+++ dumpling/executor/grant.go
@@ -20,7 +20,6 @@ import (
 	"github.com/juju/errors"
 	"github.com/pingcap/tidb/ast"
 	"github.com/pingcap/tidb/context"
-	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/model"
 	"github.com/pingcap/tidb/mysql"
@@ -41,20 +40,16 @@ var (
 
 // GrantExec executes GrantStmt.
 type GrantExec struct {
+	baseExecutor
+
 	Privs      []*ast.PrivElem
 	ObjectType ast.ObjectTypeType
 	Level      *ast.GrantLevel
 	Users      []*ast.UserSpec
 	WithGrant  bool
 
-	ctx  context.Context
 	is   infoschema.InfoSchema
 	done bool
-}
-
-// Schema implements the Executor Schema interface.
-func (e *GrantExec) Schema() *expression.Schema {
-	return expression.NewSchema()
 }
 
 // Next implements Execution Next interface.
