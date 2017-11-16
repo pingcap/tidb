99117a8891ee283bc9ef94c6b10e3bb278f4756d
caixiaojian <mc02cxj@gmail.com>
2017-11-16 22:21:14 +0800

    types: add overflow truncate when convert str to float

--- types/convert.go
+++ types/convert.go
@@ -291,6 +291,17 @@ func StrToFloat(sc *variable.StatementContext, str string) (float64, error) {
 	validStr, err := getValidFloatPrefix(sc, str)
 	f, err1 := strconv.ParseFloat(validStr, 64)
 	if err1 != nil {
+		if err2, ok := err1.(*strconv.NumError); ok {
+			if err2.Err == strconv.ErrRange {
+				warnErr := ErrTruncatedWrongVal.GenByArgs("DOUBLE", str)
+				err1 = sc.HandleOverflow(err1, warnErr)
+				if math.IsInf(f, 1) {
+					f = math.MaxFloat64
+				} else if math.IsInf(f, -1) {
+					f = -math.MaxFloat64
+				}
+			}
+		}
 		return f, errors.Trace(err1)
 	}
 	return f, errors.Trace(err)
