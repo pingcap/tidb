0a9df7b5ee5adc289c794c45be359c62a9d364a6
Yiding Cui <winoros@gmail.com>
2017-11-16 16:23:33 +0800

    plan, ranger: support `not in` and correct the behavior. (#5057)

--- util/ranger/points.go
+++ util/ranger/points.go
@@ -284,19 +284,24 @@ func (r *builder) buildFromIsFalse(expr *expression.ScalarFunction, isNot int) [
 	return []point{startPoint, endPoint}
 }
 
-func (r *builder) newBuildFromIn(expr *expression.ScalarFunction) []point {
-	var rangePoints []point
+func (r *builder) buildFromIn(expr *expression.ScalarFunction) ([]point, bool) {
 	list := expr.GetArgs()[1:]
+	rangePoints := make([]point, 0, len(list)*2)
+	hasNull := false
 	for _, e := range list {
 		v, ok := e.(*expression.Constant)
 		if !ok {
 			r.err = ErrUnsupportedType.Gen("expr:%v is not constant", e)
-			return fullRange
+			return fullRange, hasNull
 		}
 		dt, err := v.Eval(nil)
 		if err != nil {
 			r.err = ErrUnsupportedType.Gen("expr:%v is not evaluated", e)
-			return fullRange
+			return fullRange, hasNull
+		}
+		if dt.IsNull() {
+			hasNull = true
+			continue
 		}
 		startPoint := point{value: types.NewDatum(dt.GetValue()), start: true}
 		endPoint := point{value: types.NewDatum(dt.GetValue())}
@@ -307,31 +312,21 @@ func (r *builder) newBuildFromIn(expr *expression.ScalarFunction) []point {
 	if sorter.err != nil {
 		r.err = sorter.err
 	}
-	// check duplicates
-	hasDuplicate := false
-	isStart := false
-	for _, v := range rangePoints {
-		if isStart == v.start {
-			hasDuplicate = true
-			break
+	// check and remove duplicates
+	curPos, frontPos := 0, 0
+	for frontPos < len(rangePoints) {
+		if rangePoints[curPos].start == rangePoints[frontPos].start {
+			frontPos++
+		} else {
+			curPos++
+			rangePoints[curPos] = rangePoints[frontPos]
+			frontPos++
 		}
-		isStart = v.start
 	}
-	if !hasDuplicate {
-		return rangePoints
+	if curPos > 0 {
+		curPos++
 	}
-	// remove duplicates
-	distinctRangePoints := make([]point, 0, len(rangePoints))
-	isStart = false
-	for i := 0; i < len(rangePoints); i++ {
-		current := rangePoints[i]
-		if isStart == current.start {
-			continue
-		}
-		distinctRangePoints = append(distinctRangePoints, current)
-		isStart = current.start
-	}
-	return distinctRangePoints
+	return rangePoints[:curPos], hasNull
 }
 
 func (r *builder) newBuildFromPatternLike(expr *expression.ScalarFunction) []point {
@@ -419,9 +414,21 @@ func (r *builder) buildFromNot(expr *expression.ScalarFunction) []point {
 	case ast.IsFalsity:
 		return r.buildFromIsFalse(expr, 1)
 	case ast.In:
-		// Pattern not in is not supported.
-		r.err = ErrUnsupportedType.Gen("NOT IN is not supported")
-		return fullRange
+		rangePoints, hasNull := r.buildFromIn(expr)
+		if hasNull {
+			return nil
+		}
+		retRangePoints := make([]point, 0, len(rangePoints)+2)
+		previousValue := types.Datum{}
+		for i := 0; i < len(rangePoints); i += 2 {
+			retRangePoints = append(retRangePoints, point{value: previousValue, start: true, excl: true})
+			retRangePoints = append(retRangePoints, point{value: rangePoints[i].value, excl: true})
+			previousValue = rangePoints[i].value
+		}
+		// Append the interval (last element, max value].
+		retRangePoints = append(retRangePoints, point{value: previousValue, start: true, excl: true})
+		retRangePoints = append(retRangePoints, point{value: types.MaxValueDatum()})
+		return retRangePoints
 	case ast.Like:
 		// Pattern not like is not supported.
 		r.err = ErrUnsupportedType.Gen("NOT LIKE is not supported.")
@@ -447,7 +454,8 @@ func (r *builder) buildFromScalarFunc(expr *expression.ScalarFunction) []point {
 	case ast.IsFalsity:
 		return r.buildFromIsFalse(expr, 0)
 	case ast.In:
-		return r.newBuildFromIn(expr)
+		retPoints, _ := r.buildFromIn(expr)
+		return retPoints
 	case ast.Like:
 		return r.newBuildFromPatternLike(expr)
 	case ast.IsNull:
