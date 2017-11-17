ee2168ed621b6a1b515a076ce109ceb961f0d96b
Drogon <jack.xsuperman@gmail.com>
2017-11-17 16:06:43 +0800

    Remove ineffectual assignment to joinedRow (#5133)

--- executor/join_result_generators.go
+++ executor/join_result_generators.go
@@ -169,8 +169,7 @@ func (outputer *leftOuterSemiJoinResultGenerator) emitMatchedInners(outer Row, i
 			return resultBuffer, false, errors.Trace(err)
 		}
 		if matched {
-			joinedRow = joinedRow[:len(outer)]
-			joinedRow = append(outer, types.NewDatum(true))
+			joinedRow = append(joinedRow[:len(outer)], types.NewDatum(true))
 			return append(resultBuffer, joinedRow), true, nil
 		}
 	}
@@ -209,8 +208,7 @@ func (outputer *antiLeftOuterSemiJoinResultGenerator) emitMatchedInners(outer Ro
 			return resultBuffer, false, errors.Trace(err)
 		}
 		if matched {
-			joinedRow = joinedRow[:len(outer)]
-			joinedRow = append(outer, types.NewDatum(false))
+			joinedRow = append(joinedRow[:len(outer)], types.NewDatum(false))
 			return append(resultBuffer, joinedRow), true, nil
 		}
 	}
