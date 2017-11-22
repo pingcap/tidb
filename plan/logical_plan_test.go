98d084bc27c7e5c9b5892bdddf4b2749869e1dbe
ceshihao <davidzheng23@gmail.com>
2017-11-22 10:09:07 +0800

    move ErrKeyDoesNotExits check ut to TestValidate

--- plan/logical_plan_test.go
+++ plan/logical_plan_test.go
@@ -660,28 +660,6 @@ func (s *testPlanSuite) TestPlanBuilder(c *C) {
 	}
 }
 
-func (s *testPlanSuite) TestPlanBuilderError(c *C) {
-	defer testleak.AfterTest(c)()
-	tests := []struct {
-		sql string
-		err string
-	}{
-		{
-			sql: "select * from t t1 use index(e)",
-			err: "[plan:1176]Key 'e' doesn't exist in table 't'",
-		},
-	}
-	for _, ca := range tests {
-		comment := Commentf("for %s", ca.sql)
-		stmt, err := s.ParseOneStmt(ca.sql, "", "")
-		c.Assert(err, IsNil, comment)
-
-		Preprocess(s.ctx, stmt, s.is, false)
-		_, err = BuildLogicalPlan(s.ctx, stmt, s.is)
-		c.Assert(err.Error(), Equals, ca.err)
-	}
-}
-
 func (s *testPlanSuite) TestJoinReOrder(c *C) {
 	defer testleak.AfterTest(c)()
 	tests := []struct {
@@ -1092,6 +1070,10 @@ func (s *testPlanSuite) TestValidate(c *C) {
 		//	sql: "(select a as b, b from t) union (select a, b from t) order by a",
 		//	err: ErrUnknownColumn,
 		//},
+		{
+			sql: "select * from t t1 use index(e)",
+			err: ErrKeyDoesNotExits,
+		},
 	}
 	for _, tt := range tests {
 		sql := tt.sql
