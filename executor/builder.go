aa0072cfe138dfbcef6de55f04453816bc9be5c9
winkyao <golangwink@gmail.com>
2017-12-11 16:30:56 +0800

    executor: DeleteExec suport NextChunk

--- executor/builder.go
+++ executor/builder.go
@@ -893,13 +893,15 @@ func (b *executorBuilder) buildDelete(v *plan.Delete) Executor {
 		b.err = errors.Trace(b.err)
 		return nil
 	}
-	return &DeleteExec{
+	deleteExec := &DeleteExec{
 		baseExecutor: newBaseExecutor(nil, b.ctx),
 		SelectExec:   selExec,
 		Tables:       v.Tables,
 		IsMultiTable: v.IsMultiTable,
 		tblID2Table:  tblID2table,
 	}
+	deleteExec.supportChk = true
+	return deleteExec
 }
 
 func (b *executorBuilder) buildAnalyzeIndexPushdown(task plan.AnalyzeIndexTask) *AnalyzeIndexExec {
