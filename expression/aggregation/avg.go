06ae67d6ab9ab40af0c68e6f093cb35ecfa94246
Du Chuan <duchuangucas@live.com>
2017-12-25 21:15:37 +0800

    expression: fix return type and decimal len for builtin aggregation function 'avg' and 'sum' (#5292)
    * expression: fix return type and decimal len for builtin aggregation function 'avg' and 'sum'
    
    * Code format refine
    
    * Code format refine
    
    * Fix frac for avg

--- expression/aggregation/avg.go
+++ expression/aggregation/avg.go
@@ -36,9 +36,24 @@ func (af *avgFunction) Clone() Aggregation {
 
 // GetType implements Aggregation interface.
 func (af *avgFunction) GetType() *types.FieldType {
-	ft := types.NewFieldType(mysql.TypeNewDecimal)
+	var ft *types.FieldType
+	if types.IsTypeFloat(af.Args[0].GetType().Tp) {
+		ft = types.NewFieldType(mysql.TypeDouble)
+		ft.Decimal = af.Args[0].GetType().Decimal
+	} else {
+		ft = types.NewFieldType(mysql.TypeNewDecimal)
+		if af.GetMode() == FinalMode {
+			ft.Decimal = af.Args[1].GetType().Decimal
+		} else {
+			ft.Decimal = af.Args[0].GetType().Decimal
+			if ft.Decimal < 0 {
+				ft.Decimal = 0
+			}
+			ft.Decimal += types.DivFracIncr
+		}
+	}
+	ft.Flen = mysql.MaxRealWidth
 	types.SetBinChsClnFlag(ft)
-	ft.Flen, ft.Decimal = mysql.MaxRealWidth, af.Args[0].GetType().Decimal
 	return ft
 }
 
@@ -97,8 +112,18 @@ func (af *avgFunction) GetResult(ctx *AggEvaluateContext) (d types.Datum) {
 	to := new(types.MyDecimal)
 	err := types.DecimalDiv(x, y, to, types.DivFracIncr)
 	terror.Log(errors.Trace(err))
-	err = to.Round(to, ctx.Value.Frac()+types.DivFracIncr, types.ModeHalfEven)
+	frac := af.GetType().Decimal
+	if frac == -1 {
+		frac = types.MaxFraction
+	}
+	err = to.Round(to, frac, types.ModeHalfEven)
 	terror.Log(errors.Trace(err))
+	if ctx.Value.Kind() == types.KindFloat64 {
+		f, err := to.ToFloat64()
+		terror.Log(errors.Trace(err))
+		d.SetFloat64(f)
+		return
+	}
 	d.SetMysqlDecimal(to)
 	return
 }
