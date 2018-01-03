54fa32bbd96b20dde7756000508057f083331d36
winkyao <golangwink@gmail.com>
2018-01-03 23:43:19 +0800

    fix ci

--- table/tables/tables.go
+++ table/tables/tables.go
@@ -33,6 +33,7 @@ import (
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util"
 	"github.com/pingcap/tidb/util/codec"
+	"github.com/pingcap/tipb/go-binlog"
 	log "github.com/sirupsen/logrus"
 )
 
