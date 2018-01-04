f88da91d8f00efc2fe1583b23c355395ba78aafa
zhexuany <yangzhexuan@pingcap.com>
2018-01-04 22:12:44 +0800

    update clean_vendor.sj

--- hack/clean_vendor.sh
+++ hack/clean_vendor.sh
@@ -1,3 +1,5 @@
 find vendor -type f -not -name "*.go" -not -name "LICENSE" -not -name "*.s" -not -name "PATENTS" | xargs -I {} rm {}
 # delete all test files
+find vendor -type f -name "*_generated.go" | xargs -I {} rm {}
 find vendor -type f -name "*_test.go" | xargs -I {} rm {}
+find vendor -type f -name "doc.go" | xargs -I {} rm {}
