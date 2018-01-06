c67e2cd30901affdd15ce428dc51f660f2d4968a
winkyao <82091309@qq.com>
2018-01-06 22:40:17 +0800

    parser, expression: support SEPARATOR in group_concat aggregate function (#5569)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1138,6 +1138,7 @@ func (s *testParserSuite) TestBuiltin(c *C) {
 		{`select count(distinct all c1) from t;`, false},
 		{`select count(distinctrow all c1) from t;`, false},
 		{`select group_concat(c2,c1) from t group by c1;`, true},
+		{`select group_concat(c2,c1 SEPARATOR ';') from t group by c1;`, true},
 		{`select group_concat(distinct c2,c1) from t group by c1;`, true},
 		{`select group_concat(distinctrow c2,c1) from t group by c1;`, true},
 
