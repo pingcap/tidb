fa513f1fdecb0de20928a8c4dfce79b83413a280
XuWentao <wtao.xu@gmail.com>
2018-01-11 19:22:02 +0800

    resolve only one region

--- store/tikv/gcworker/gc_worker.go
+++ store/tikv/gcworker/gc_worker.go
@@ -524,7 +524,7 @@ func resolveLocks(ctx goctx.Context, store tikv.Storage, bo *tikv.Backoffer, ide
 		default:
 		}
 
-		if len(task.endKey) != 0 && key.Cmp(task.endKey) > 0 {
+		if len(task.endKey) != 0 && key.Cmp(task.endKey) >= 0 {
 			break
 		}
 
