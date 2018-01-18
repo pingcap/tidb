c5c0714cdea270e5d0aa41130c21fd447f17de43
HuaiyuXu <391585975@qq.com>
2018-01-18 11:14:25 +0800

    executor, sessionctx, util: log a warning when memory usage of HashJoinExec exceed threshhold (#5658)

--- executor/join.go
+++ executor/join.go
@@ -29,6 +29,7 @@ import (
 	"github.com/pingcap/tidb/util/chunk"
 	"github.com/pingcap/tidb/util/codec"
 	"github.com/pingcap/tidb/util/mvmap"
+	log "github.com/sirupsen/logrus"
 	goctx "golang.org/x/net/context"
 )
 
@@ -76,6 +77,7 @@ type HashJoinExec struct {
 	joinChkResourceCh  []chan *chunk.Chunk
 	joinResultCh       chan *hashjoinWorkerResult
 	hashTableValBufs   [][][]byte
+	totalMemoryUsage   int64
 }
 
 // outerChkResource stores the result of the join outer fetch worker,
@@ -159,7 +161,6 @@ func (e *HashJoinExec) Open(goCtx goctx.Context) error {
 	e.closeCh = make(chan struct{})
 	e.finished.Store(false)
 	e.workerWaitGroup = sync.WaitGroup{}
-
 	e.resultCursor = 0
 	return nil
 }
@@ -331,6 +332,7 @@ func (e *HashJoinExec) fetchSelectedInnerRows(goCtx goctx.Context) (err error) {
 	innerExecChk := e.childrenResults[e.innerIdx]
 	selected := make([]bool, 0, chunk.InitialCapacity)
 	innerResult := chunk.NewList(e.innerExec.retTypes(), e.maxChunkSize)
+	memExceedThreshold, execMemThreshold := false, e.ctx.GetSessionVars().MemThreshold
 	for {
 		innerExecChk.Reset()
 		err = e.innerExec.NextChunk(goCtx, innerExecChk)
@@ -348,6 +350,11 @@ func (e *HashJoinExec) fetchSelectedInnerRows(goCtx goctx.Context) (err error) {
 			if selected[idx] {
 				innerResult.AppendRow(innerExecChk.GetRow(idx))
 			}
+		}
+		innerMemUsage := innerResult.MemoryUsage()
+		if !memExceedThreshold && innerMemUsage > execMemThreshold {
+			memExceedThreshold = true
+			log.Warnf(ErrMemExceedThreshold.GenByArgs("HashJoin", innerMemUsage, execMemThreshold).Error())
 		}
 	}
 	e.innerResult = innerResult
