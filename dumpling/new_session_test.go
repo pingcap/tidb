ce0bc16d68eedf28722d40bff2e7c1061fd96d36
tiancaiamao <tiancaiamao@gmail.com>
2018-01-21 23:49:29 -0600

    [dumpling] *: execute statement meets error should make no side effect (#5678)

--- dumpling/new_session_test.go
+++ dumpling/new_session_test.go
@@ -1805,3 +1805,32 @@ func (s *testSchemaSuite) TestIndexLookUpReaderChunk(c *C) {
 	c.Assert(count, Equals, 90)
 	rs.Close()
 }
+
+func (s *testSessionSuite) TestStatementErrorInTransaction(c *C) {
+	tk := testkit.NewTestKitWithInit(c, s.store)
+	tk.MustExec("create table statement_side_effect (c int primary key)")
+	tk.MustExec("begin")
+	tk.MustExec("insert into statement_side_effect values (1)")
+	_, err := tk.Exec("insert into statement_side_effect value (2),(3),(4),(1)")
+	c.Assert(err, NotNil)
+	// TODO: Fix here, dirty table should not be touched, too.
+	// tk.MustQuery(`select * from statement_side_effect`).Check(testkit.Rows("1"))
+	tk.MustExec("commit")
+	tk.MustQuery(`select * from statement_side_effect`).Check(testkit.Rows("1"))
+
+	tk.MustExec("drop table if exists test;")
+	tk.MustExec(`create table test (
+ 		  a int(11) DEFAULT NULL,
+ 		  b int(11) DEFAULT NULL
+ 	) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;`)
+	tk.MustExec("insert into test values (1, 2), (1, 2), (1, 1), (1, 1);")
+
+	tk.MustExec("start transaction;")
+	// In the transaction, statement error should not rollback the transaction.
+	_, err = tk.Exec("update tset set b=11 where a=1 and b=2;")
+	c.Assert(err, NotNil)
+	// Test for a bug that last line rollback and exit transaction, this line autocommit.
+	tk.MustExec("update test set b = 11 where a = 1 and b = 2;")
+	tk.MustExec("rollback")
+	tk.MustQuery("select * from test where a = 1 and b = 11").Check(testkit.Rows())
+}
