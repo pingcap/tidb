d6602343a00ba74e8d4c5b9dc8abc1b9a72484a0
Evgeniy Kulikov <im@kulikov.im>
2018-01-26 19:27:59 +0300

    - cleanup "AUTHORS", "AUTHORS.md", 'CONTRIBUTORS', 'NOTICE' - cleanup empty folders (and _vendor) - add cleanup commands to `hack/cleanup_vendor.sh`

--- hack/clean_vendor.sh
+++ hack/clean_vendor.sh
@@ -1,4 +1,8 @@
+dep ensure &>/dev/null
 find vendor \( -type f -or -type l \)  -not -name "*.go" -not -name "LICENSE" -not -name "*.s" -not -name "PATENTS" -not -name "*.h" -not -name "*.c" | xargs -I {} rm {}
 # delete all test files
 find vendor -type f -name "*_generated.go" | xargs -I {} rm {}
 find vendor -type f -name "*_test.go" | xargs -I {} rm {}
+find vendor -type f -name "AUTHORS" -name "AUTHORS.md" -iname 'CONTRIBUTORS' -iname 'NOTICE' | xargs -I {} rm {}
+find vendor -type d -name "_vendor" | xargs -I {} rm -rf {}
+find vendor -type d -empty | xargs -I {} rm -rf {}
