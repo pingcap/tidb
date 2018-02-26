b3ffa3cb589180dba018eccffb2273409b56a7a9
XuWentao <30992981+wentaoxu@users.noreply.github.com>
2018-02-26 16:33:29 +0800

    store/tikv: resolve locks in a batch (#5750)

--- store/tikv/lock_resolver.go
+++ store/tikv/lock_resolver.go
@@ -17,6 +17,7 @@ import (
 	"container/list"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/juju/errors"
 	"github.com/pingcap/kvproto/pkg/kvrpcpb"
@@ -146,6 +147,88 @@ func (lr *LockResolver) getResolved(txnID uint64) (TxnStatus, bool) {
 
 	s, ok := lr.mu.resolved[txnID]
 	return s, ok
+}
+
+// BatchResolveLocks resolve locks in a batch
+func (lr *LockResolver) BatchResolveLocks(bo *Backoffer, locks []*Lock, loc RegionVerID) (bool, error) {
+	if len(locks) == 0 {
+		return true, nil
+	}
+
+	metrics.TiKVLockResolverCounter.WithLabelValues("batch_resolve").Inc()
+
+	var expiredLocks []*Lock
+	for _, l := range locks {
+		if lr.store.GetOracle().IsExpired(l.TxnID, l.TTL) {
+			metrics.TiKVLockResolverCounter.WithLabelValues("expired").Inc()
+			expiredLocks = append(expiredLocks, l)
+		} else {
+			metrics.TiKVLockResolverCounter.WithLabelValues("not_expired").Inc()
+		}
+	}
+	if len(expiredLocks) != len(locks) {
+		log.Errorf("BatchResolveLocks: get %d Locks, but only %d are expired, maybe safe point is wrong!", len(locks), len(expiredLocks))
+		return false, nil
+	}
+
+	startTime := time.Now()
+	txnInfos := make(map[uint64]uint64)
+	for _, l := range expiredLocks {
+		if _, ok := txnInfos[l.TxnID]; ok {
+			continue
+		}
+
+		status, err := lr.getTxnStatus(bo, l.TxnID, l.Primary)
+		if err != nil {
+			return false, errors.Trace(err)
+		}
+		txnInfos[l.TxnID] = uint64(status)
+	}
+	log.Infof("BatchResolveLocks: it took %v to lookup %v txn status", time.Since(startTime), len(txnInfos))
+
+	var listTxnInfos []*kvrpcpb.TxnInfo
+	for txnID, status := range txnInfos {
+		listTxnInfos = append(listTxnInfos, &kvrpcpb.TxnInfo{
+			Txn:    txnID,
+			Status: status,
+		})
+	}
+
+	req := &tikvrpc.Request{
+		Type: tikvrpc.CmdResolveLock,
+		ResolveLock: &kvrpcpb.ResolveLockRequest{
+			TxnInfos: listTxnInfos,
+		},
+	}
+	startTime = time.Now()
+	resp, err := lr.store.SendReq(bo, req, loc, readTimeoutShort)
+	if err != nil {
+		return false, errors.Trace(err)
+	}
+
+	regionErr, err := resp.GetRegionError()
+	if err != nil {
+		return false, errors.Trace(err)
+	}
+
+	if regionErr != nil {
+		err = bo.Backoff(BoRegionMiss, errors.New(regionErr.String()))
+		if err != nil {
+			return false, errors.Trace(err)
+		}
+		return false, nil
+	}
+
+	cmdResp := resp.ResolveLock
+	if cmdResp == nil {
+		return false, errors.Trace(ErrBodyMissing)
+	}
+	if keyErr := cmdResp.GetError(); keyErr != nil {
+		return false, errors.Errorf("unexpected resolve err: %s", keyErr)
+	}
+
+	log.Infof("BatchResolveLocks: it took %v to resolve %v locks in a batch.", time.Since(startTime), len(expiredLocks))
+	return true, nil
 }
 
 // ResolveLocks tries to resolve Locks. The resolving process is in 3 steps:
