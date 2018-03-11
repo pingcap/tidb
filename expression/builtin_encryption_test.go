0dea1ef62e00a28e43961b081e7a3fe1a1bb2c2a
J Miles <j.milesy.nz@gmail.com>
2018-03-12 12:07:29 +1300

    Minor tidy up from PR comment

--- expression/builtin_encryption_test.go
+++ expression/builtin_encryption_test.go
@@ -360,10 +360,12 @@ func (s *testEvaluatorSuite) TestPassword(c *C) {
 		}
 
 		warnings := s.ctx.GetSessionVars().StmtCtx.GetWarnings()
-		lastWarn := warnings[len(warnings)-1]
 		if t.getWarn {
 			c.Assert(len(warnings), Equals, warnCount+1)
+
+			lastWarn := warnings[len(warnings)-1]
 			c.Assert(terror.ErrorEqual(errDeprecatedSyntaxNoReplacement, lastWarn), IsTrue)
+
 			warnCount = len(warnings)
 		} else {
 			c.Assert(len(warnings), Equals, warnCount)
