ffcec7e4600fc1c405d83041393ec3699756da9d
xiaojian cai <mc02cxj@gmail.com>
2018-03-21 10:59:23 +0800

    [dumpling] *: remove useless code  (#5343)

--- dumpling/plan/preprocess.go
+++ dumpling/plan/preprocess.go
@@ -218,11 +218,6 @@ func (p *preprocessor) checkDropDatabaseGrammar(stmt *ast.DropDatabaseStmt) {
 }
 
 func (p *preprocessor) checkCreateTableGrammar(stmt *ast.CreateTableStmt) {
-	if stmt.Table == nil {
-		p.err = ddl.ErrWrongTableName.GenByArgs("")
-		return
-	}
-
 	tName := stmt.Table.Name.String()
 	if isIncorrectName(tName) {
 		p.err = ddl.ErrWrongTableName.GenByArgs(tName)
@@ -265,10 +260,6 @@ func (p *preprocessor) checkCreateTableGrammar(stmt *ast.CreateTableStmt) {
 }
 
 func (p *preprocessor) checkDropTableGrammar(stmt *ast.DropTableStmt) {
-	if stmt.Tables == nil {
-		p.err = ddl.ErrWrongTableName.GenByArgs("")
-		return
-	}
 	for _, t := range stmt.Tables {
 		if isIncorrectName(t.Name.String()) {
 			p.err = ddl.ErrWrongTableName.GenByArgs(t.Name.String())
@@ -296,11 +287,6 @@ func (p *preprocessor) checkCreateIndexGrammar(stmt *ast.CreateIndexStmt) {
 }
 
 func (p *preprocessor) checkAlterTableGrammar(stmt *ast.AlterTableStmt) {
-	if stmt.Table == nil {
-		p.err = ddl.ErrWrongTableName.GenByArgs("")
-		return
-	}
-
 	tName := stmt.Table.Name.String()
 	if isIncorrectName(tName) {
 		p.err = ddl.ErrWrongTableName.GenByArgs(tName)
@@ -315,9 +301,8 @@ func (p *preprocessor) checkAlterTableGrammar(stmt *ast.AlterTableStmt) {
 				return
 			}
 		}
-		if len(spec.NewColumns) > 0 && spec.NewColumns[0] != nil {
-			if err := checkColumn(spec.NewColumns[0]); err != nil {
-				p.err = err
+		for _, colDef := range spec.NewColumns {
+			if p.err = checkColumn(colDef); p.err != nil {
 				return
 			}
 		}
@@ -341,14 +326,13 @@ func (p *preprocessor) checkAlterTableGrammar(stmt *ast.AlterTableStmt) {
 
 // checkDuplicateColumnName checks if index exists duplicated columns.
 func checkDuplicateColumnName(indexColNames []*ast.IndexColName) error {
-	for i := 0; i < len(indexColNames); i++ {
-		name1 := indexColNames[i].Column.Name
-		for j := i + 1; j < len(indexColNames); j++ {
-			name2 := indexColNames[j].Column.Name
-			if name1.L == name2.L {
-				return infoschema.ErrColumnExists.GenByArgs(name2)
-			}
+	colNames := make(map[string]struct{}, len(indexColNames))
+	for _, indexColName := range indexColNames {
+		name := indexColName.Column.Name
+		if _, ok := colNames[name.L]; ok {
+			return infoschema.ErrColumnExists.GenByArgs(name)
 		}
+		colNames[name.L] = struct{}{}
 	}
 	return nil
 }
