273e2f262e725340251c4e32be065f041c6b57e4
lwhile <lwhile521@gmail.com>
2018-03-22 12:21:28 +0800

    Remove Next function totally

--- executor/aggregate.go
+++ executor/aggregate.go
@@ -141,11 +141,6 @@ func (e *HashAggExec) execute(ctx context.Context) (err error) {
 	}
 }
 
-// Next implements the Executor Next interface.
-func (e *HashAggExec) Next(ctx context.Context) (Row, error) {
-	return nil, nil
-}
-
 func (e *HashAggExec) getGroupKey(row types.Row) ([]byte, error) {
 	vals := make([]types.Datum, 0, len(e.GroupByItems))
 	for _, item := range e.GroupByItems {
