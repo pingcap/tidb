5c25c9acce246e4cd2bfcd67412e959e0fff8e8d
zhexuany <yangzhexuan@pingcap.com>
2018-03-22 15:54:43 +0800

    remvoe useless print

--- expression/expr_to_pb.go
+++ expression/expr_to_pb.go
@@ -181,8 +181,6 @@ func (pc PbConverter) columnToPBExpr(column *Column) *tipb.Expr {
 	}
 
 	if pc.client.IsRequestTypeSupported(kv.ReqTypeDAG, kv.ReqSubTypeBasic) {
-		fmt.Printf("column index is %d in columnToPBExpr.\n", column.Index)
-		fmt.Printf("column fieldtype is %v\n", column.RetType)
 		return &tipb.Expr{
 			Tp:        tipb.ExprType_ColumnRef,
 			Val:       codec.EncodeInt(nil, int64(column.Index)),
