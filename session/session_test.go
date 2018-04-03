09610cee8e50555503bc4aa844df2cd417309dd5
Ewan Chou <coocood@gmail.com>
2018-04-03 19:56:54 +0800

    *: remove redundant test

--- session/session_test.go
+++ session/session_test.go
@@ -1977,11 +1977,6 @@ func (s *testSessionSuite) TestRollbackOnCompileError(c *C) {
 	c.Assert(recoverErr, IsTrue)
 }
 
-func (s *testSessionSuite) TestCastAsBinary(c *C) {
-	tk := testkit.NewTestKitWithInit(c, s.store)
-	tk.MustQuery("select hex(cast(0x10 as binary(2)))").Check(testkit.Rows("1000"))
-}
-
 func (s *testSessionSuite) TestDBUserNameLength(c *C) {
 	tk := testkit.NewTestKitWithInit(c, s.store)
 	tk.MustExec("create table if not exists t (a int)")
