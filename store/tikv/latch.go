666ed9807fb214a1ddced8ed245c3fb14ab66cd3
wuxuelian <AndreMouche@126.com>
2018-04-07 15:47:56 +0800

    tikv: implement local latches for transaction

---
+++ store/tikv/latch.go
@@ -0,0 +1,188 @@
+// Copyright 2018 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tikv
+
+import (
+	"fmt"
+	"hash/fnv"
+	"math"
+	"sort"
+
+	"sync"
+)
+
+// Latch stores a key's waiting transactions
+type Latch struct {
+	waiting      []uint64
+	lastCommitTs uint64
+	lock         sync.Mutex
+}
+
+// acquire tries to get current key's lock for the transaction with startTs
+// acquire is true when success
+// timeout is true when the startTs is already timeout
+// newWait is true when current transaction is new for the current latch
+func (l *Latch) acquire(startTs uint64) (acquire, timeout, newWait bool) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	timeout = startTs <= l.lastCommitTs
+	if timeout {
+		return
+	}
+	if len(l.waiting) == 0 || l.waiting[0] != startTs {
+		l.waiting = append(l.waiting, startTs)
+		newWait = true
+	}
+
+	acquire = l.waiting[0] == startTs
+	return
+}
+
+// release releases the transaction with startTs and commitTs from current latch.
+// isEmpty is true when the waiting queue is empty after release current transaction,
+// otherwise return the front transaction in queue.
+func (l *Latch) release(startTs uint64, commitTs uint64) (isEmpty bool, front uint64) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if startTs != l.waiting[0] {
+		panic(fmt.Sprintf("invalid front ts %v, latch:%+v", startTs, l))
+	}
+	// for rollback or timeout, the commitTs maybe zero
+	if commitTs > l.lastCommitTs {
+		l.lastCommitTs = commitTs
+	}
+	l.waiting = l.waiting[1:]
+	if len(l.waiting) == 0 {
+		// Should we reset the lastCommitTs since next key
+		// may different from current key?
+		// l.lastCommitTs = 0
+		isEmpty = true
+	} else {
+		front = l.waiting[0]
+		isEmpty = false
+	}
+	return
+}
+
+// try append startTs into latch, return false
+// when startTs is already timeout
+func (l *Latch) append(startTs uint64) bool {
+	if startTs <= l.lastCommitTs {
+		return false
+	}
+	l.waiting = append(l.waiting, startTs)
+	return true
+}
+
+// TLock is the locks' information required for a transaction
+type TLock struct {
+	// the slot IDs of the latches(keys) that a startTs must acquire before being able to processed
+	requiredSlots []int
+	/// The number of latches that the transaction has acquired.
+	acquiredCount int
+	/// The number of latches that the waiting queue contains current transaction
+	waitedCount int
+	// Current transaction's startTs
+	startTs uint64
+}
+
+// NewTLock creates a new lock
+func NewTLock(startTs uint64, requiredSlots []int) TLock {
+	return TLock{
+		requiredSlots: requiredSlots,
+		acquiredCount: 0,
+		waitedCount:   0,
+		startTs:       startTs,
+	}
+}
+
+//// Acquired Returns true if all the required latches(keys) have be acquired,
+//// false otherwise.
+//func (lock *TLock) Acquired() bool {
+//	return lock.acquiredCount == len(lock.requiredSlots)
+//}
+
+// Latches which are used for concurrency control
+// Each latch is indexed by a slit ID, hence the term latch and slot are used in interchangeable,
+// but conceptually a latch is a queue, and a slot is an index to the queue
+type Latches struct {
+	slots []Latch
+	size  int
+}
+
+// NewLatches the size will be rounded up to the power of 2
+func NewLatches(size int) Latches {
+	powerOfTwoSize := int(math.Pow(2, math.Ceil(math.Log2(float64(size)))))
+	slots := make([]Latch, powerOfTwoSize, powerOfTwoSize)
+	return Latches{
+		slots: slots,
+		size:  powerOfTwoSize,
+	}
+}
+
+func (latches *Latches) GenTLock(startTs uint64, keys [][]byte) TLock {
+	hashes := make(map[int]bool)
+	for id := 0; id < len(keys); id++ {
+		hashes[latches.hash(keys[id])] = true
+	}
+	slots := make([]int, 0, len(hashes))
+	for key, _ := range hashes {
+		slots = append(slots, key)
+	}
+	sort.Ints(slots)
+	return NewTLock(startTs, slots)
+}
+
+// return hash int for current key
+func (latches *Latches) hash(key []byte) int {
+	h := fnv.New32a()
+	h.Write(key)
+	return int(h.Sum32()) & (latches.size - 1)
+}
+
+// Acquire tries to acquire the lock for a transaction
+// It returns with timeout = true when the transaction is timeout(
+// when the lock.startTs is smaller than any key's last commitTs).
+// It returns with acquired = true when acquire success and the transaction
+// is ready to 2PC
+func (l *Latches) Acquire(lock *TLock) (acquired, timeout bool) {
+	var newWait bool
+	for lock.acquiredCount < len(lock.requiredSlots) {
+		slotID := lock.requiredSlots[lock.acquiredCount]
+		acquired, timeout, newWait = l.slots[slotID].acquire(lock.startTs)
+		if newWait {
+			lock.waitedCount += 1
+		}
+		if timeout || !acquired {
+			return
+		}
+		lock.acquiredCount += 1
+	}
+	return
+}
+
+// Releases all latches owned by the `lock` and returns the wakeup list.
+// Preconditions: the caller must ensure the transaction is at the front of the latches.
+func (l *Latches) Release(lock *TLock, commitTs uint64) (wakeupList []uint64) {
+	wakeupList = make([]uint64, 0, lock.waitedCount)
+	for id := 0; id < lock.waitedCount; id++ {
+		slotID := lock.requiredSlots[id]
+		isEmpty, front := l.slots[slotID].release(lock.startTs, commitTs)
+		if !isEmpty {
+			wakeupList = append(wakeupList, front)
+		}
+
+	}
+	return
+}
---
+++ store/tikv/latch_test.go
@@ -0,0 +1,168 @@
+// Copyright 2018 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tikv
+
+import (
+	. "github.com/pingcap/check"
+	"sync"
+	"sync/atomic"
+)
+
+var _ = Suite(&testLatchSuite{})
+var baseTso uint64
+
+type testLatchSuite struct {
+}
+
+func (s *testLatchSuite) SetUpSuite(c *C) {
+}
+
+func (s *testLatchSuite) TearDownSuite(c *C) {
+}
+
+func getTso() uint64 {
+	return atomic.AddUint64(&baseTso, uint64(1))
+}
+
+func (s *testLatchSuite) TestWakeUp(c *C) {
+	latches := NewLatches(256)
+	keysA := [][]byte{
+		[]byte("a"), []byte("a"), []byte("b"), []byte("c")}
+	startTsA := getTso()
+	lockA := latches.GenTLock(startTsA, keysA)
+
+	keysB := [][]byte{[]byte("d"), []byte("e"), []byte("a"), []byte("c")}
+	startTsB := getTso()
+	lockB := latches.GenTLock(startTsB, keysB)
+
+	//A acquire lock success
+	acquired, timeout := latches.Acquire(&lockA)
+	c.Assert(timeout, IsFalse)
+	c.Assert(acquired, IsTrue)
+
+	// B acquire lock failed
+	acquired, timeout = latches.Acquire(&lockB)
+	c.Assert(timeout, IsFalse)
+	c.Assert(acquired, IsFalse)
+
+	// A release lock, and get wakeup list
+	commitTSA := getTso()
+	wakeupList := latches.Release(&lockA, commitTSA)
+	c.Assert(wakeupList[0], Equals, startTsB)
+
+	// B acquire failed since startTSB has timeout for some keys
+	acquired, timeout = latches.Acquire(&lockB)
+	c.Assert(timeout, IsTrue)
+	c.Assert(acquired, IsFalse)
+
+	// B release lock since it received a timeout
+	wakeupList = latches.Release(&lockB, 0)
+	c.Assert(len(wakeupList), Equals, 0)
+
+	// B restart:get a new startTso
+	startTsB = getTso()
+	lockB = latches.GenTLock(startTsB, keysB)
+	acquired, timeout = latches.Acquire(&lockB)
+	c.Assert(acquired, IsTrue)
+	c.Assert(timeout, IsFalse)
+}
+
+type txn struct {
+	keys    [][]byte
+	startTs uint64
+	lock    TLock
+}
+
+func newTxn(keys [][]byte, startTs uint64, lock TLock) txn {
+	return txn{
+		keys:    keys,
+		startTs: startTs,
+		lock:    lock,
+	}
+}
+
+type txnStore struct {
+	txns    map[uint64]*txn
+	latches Latches
+	lock    sync.Mutex
+	wait    *sync.WaitGroup
+}
+
+func newTxnStore(wait *sync.WaitGroup) *txnStore {
+	txnsMap := make(map[uint64]*txn)
+	return &txnStore{
+		txns:    txnsMap,
+		latches: NewLatches(256),
+		lock:    sync.Mutex{},
+		wait:    wait,
+	}
+}
+
+func (store *txnStore) runTxn(startTs uint64) {
+	store.lock.Lock()
+	txn, ok := store.txns[startTs]
+	store.lock.Unlock()
+	if !ok {
+		panic(startTs)
+	}
+	acquired, timeout := store.latches.Acquire(&txn.lock)
+
+	if !timeout && !acquired {
+		return
+	}
+	commitTs := uint64(0)
+	if !timeout {
+		// DO 2pc
+		commitTs = getTso()
+	}
+	wakeupList := store.latches.Release(&txn.lock, commitTs)
+	for _, s := range wakeupList {
+		go store.runTxn(s)
+	}
+	store.lock.Lock()
+	delete(store.txns, startTs)
+	store.lock.Unlock()
+	if timeout {
+		// restart Txn
+		go store.newTxn(txn.keys)
+	} else {
+		store.wait.Done()
+	}
+}
+
+func (store *txnStore) newTxn(keys [][]byte) {
+	startTs := getTso()
+	lock := store.latches.GenTLock(startTs, keys)
+	t := newTxn(keys, startTs, lock)
+	store.lock.Lock()
+	defer store.lock.Unlock()
+	store.txns[t.startTs] = &t
+	go store.runTxn(t.startTs)
+}
+
+func (s *testLatchSuite) TestWithConcurrency(c *C) {
+	waitGroup := sync.WaitGroup{}
+	txn1 := [][]byte{
+		[]byte("a"), []byte("a"), []byte("b"), []byte("c")}
+	txn2 := [][]byte{
+		[]byte("a"), []byte("d"), []byte("e"), []byte("f")}
+	txn3 := [][]byte{
+		[]byte("e"), []byte("f"), []byte("g"), []byte("h")}
+	waitGroup.Add(3)
+	store := newTxnStore(&waitGroup)
+	go store.newTxn(txn1)
+	go store.newTxn(txn2)
+	go store.newTxn(txn3)
+	waitGroup.Wait()
+}
