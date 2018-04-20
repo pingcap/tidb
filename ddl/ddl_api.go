6af19eda5683347eefeec7af301e788d2a37095c
zhexuany <yangzhexuan@pingcap.com>
2018-04-20 10:17:54 +0800

    remove useless code

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -799,7 +799,6 @@ func (d *ddl) CreateTable(ctx sessionctx.Context, s *ast.CreateTableStmt) (err e
 		return errors.Trace(err)
 	}
 	if s.Partition != nil {
-		fmt.Printf("expr is %v", s.Partition.Expr)
 		pi := &model.PartitionInfo{
 			Type: s.Partition.Tp,
 		}
