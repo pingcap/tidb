376357a46525af81fb2fbdac9dd3abda17a792ae
Ewan Chou <coocood@gmail.com>
2018-04-23 19:54:45 +0800

    *: add backoff types

--- store/tikv/coprocessor.go
+++ store/tikv/coprocessor.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"io"
 	"sort"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -639,7 +640,8 @@ const (
 func (worker *copIteratorWorker) logTimeCopTask(costTime time.Duration, task *copTask, bo *Backoffer, resp *tikvrpc.Response) {
 	logStr := fmt.Sprintf("[TIME_COP_TASK] resp_time:%s txn_start_ts:%d region_id:%d store_addr:%s", costTime, worker.req.StartTs, task.region.id, task.storeAddr)
 	if bo.totalSleep > minLogBackoffTime {
-		logStr += fmt.Sprintf(" backoff_ms:%d", bo.totalSleep)
+		backoffTypes := strings.Replace(fmt.Sprintf("%v", bo.types), " ", ",", -1)
+		logStr += fmt.Sprintf(" backoff_ms:%d backoff_types:%s", bo.totalSleep, backoffTypes)
 	}
 	var detail *kvrpcpb.ExecDetails
 	if task.cmdType == tikvrpc.CmdCopStream {
