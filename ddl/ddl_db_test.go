54d6926740051414233a0e86f739438c08f70bbf
winkyao <golangwink@gmail.com>
2018-04-26 15:19:58 +0800

    ddl: support rename the same table name

--- ddl/ddl_db_test.go
+++ ddl/ddl_db_test.go
@@ -1533,6 +1533,7 @@ func (s *testDBSuite) testRenameTable(c *C, sql string) {
 	c.Assert(newTblInfo.Meta().ID, Equals, oldTblID)
 	s.tk.MustQuery("select * from t1").Check(testkit.Rows("1 1", "2 2"))
 	s.tk.MustExec("use test")
+
 	// Make sure t doesn't exist.
 	s.tk.MustExec("create table t (c1 int, c2 int)")
 	s.tk.MustExec("drop table t")
@@ -1552,12 +1553,19 @@ func (s *testDBSuite) testRenameTable(c *C, sql string) {
 	// for failure case
 	failSQL := fmt.Sprintf(sql, "test_not_exist.t", "test_not_exist.t")
 	s.testErrorCode(c, failSQL, tmysql.ErrFileNotFound)
+	failSQL = fmt.Sprintf(sql, "test.test_not_exist", "test.test_not_exist")
+	s.testErrorCode(c, failSQL, tmysql.ErrFileNotFound)
 	failSQL = fmt.Sprintf(sql, "test.t_not_exist", "test_not_exist.t")
 	s.testErrorCode(c, failSQL, tmysql.ErrFileNotFound)
 	failSQL = fmt.Sprintf(sql, "test1.t2", "test_not_exist.t")
 	s.testErrorCode(c, failSQL, tmysql.ErrErrorOnRename)
-	failSQL = fmt.Sprintf(sql, "test1.t2", "test1.t2")
-	s.testErrorCode(c, failSQL, tmysql.ErrTableExists)
+
+	// for the same table name
+	s.tk.MustExec("use test1")
+	s.tk.MustExec("create table if not exists t (c1 int, c2 int)")
+	s.tk.MustExec("create table if not exists t1 (c1 int, c2 int)")
+	s.tk.MustExec(fmt.Sprintf(sql, "test1.t", "test1.t"))
+	s.tk.MustExec(fmt.Sprintf(sql, "test1.t1", "test1.t1"))
 
 	s.tk.MustExec("drop database test1")
 }
