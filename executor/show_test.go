cd15501cf94b006a2e4f1811da2db6f42856de1d
tiancaiamao <tiancaiamao@gmail.com>
2018-04-28 07:13:23 -0500

    privilege: clean up privilege.Enable variable (#6427)

--- executor/show_test.go
+++ executor/show_test.go
@@ -24,7 +24,6 @@ import (
 	"github.com/pingcap/tidb/meta/autoid"
 	"github.com/pingcap/tidb/mysql"
 	"github.com/pingcap/tidb/plan"
-	"github.com/pingcap/tidb/privilege/privileges"
 	"github.com/pingcap/tidb/session"
 	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/util"
@@ -342,8 +341,6 @@ func (s *testSuite) TestShow(c *C) {
 }
 
 func (s *testSuite) TestShowVisibility(c *C) {
-	save := privileges.Enable
-	privileges.Enable = true
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("create database showdatabase")
 	tk.MustExec("use showdatabase")
@@ -381,7 +378,6 @@ func (s *testSuite) TestShowVisibility(c *C) {
 	rows := tk1.MustQuery("show databases").Rows()
 	c.Assert(len(rows), GreaterEqual, 2) // At least INFORMATION_SCHEMA and showdatabase
 
-	privileges.Enable = save
 	tk.MustExec(`drop user 'show'@'%'`)
 	tk.MustExec("drop database showdatabase")
 }
