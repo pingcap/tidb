f52dbc485d1e5ee73d0509ec25177732159cf294
tiancaiamao <tiancaiamao@gmail.com>
2018-05-10 14:37:49 +0800

    session: tiny update leak check for some test function

--- session/tidb_test.go
+++ session/tidb_test.go
@@ -16,7 +16,6 @@ package session
 import (
 	"fmt"
 	"os"
-	"runtime"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -140,8 +139,6 @@ func (s *testMainSuite) TestRetryDialPumpClient(c *C) {
 }
 
 func (s *testMainSuite) TestSysSessionPoolGoroutineLeak(c *C) {
-	c.Skip("make leak should check it")
-	// TODO: testleak package should be able to find this leak.
 	store, dom := newStoreWithBootstrap(c, s.dbName+"goroutine_leak")
 	defer dom.Close()
 	defer store.Close()
@@ -150,7 +147,6 @@ func (s *testMainSuite) TestSysSessionPoolGoroutineLeak(c *C) {
 
 	// Test an issue that sysSessionPool doesn't call session's Close, cause
 	// asyncGetTSWorker goroutine leak.
-	before := runtime.NumGoroutine()
 	count := 200
 	var wg sync.WaitGroup
 	wg.Add(count)
@@ -164,21 +160,9 @@ func (s *testMainSuite) TestSysSessionPoolGoroutineLeak(c *C) {
 	wg.Wait()
 	se.sysSessionPool().Close()
 	c.Assert(se.sysSessionPool().IsClosed(), Equals, true)
-	for i := 0; i < 300; i++ {
-		// After and before should be Equal, but this test may be disturbed by other factors.
-		// So I relax the strict check to make CI more stable.
-		after := runtime.NumGoroutine()
-		if after-before < 3 {
-			return
-		}
-		time.Sleep(20 * time.Millisecond)
-	}
-	after := runtime.NumGoroutine()
-	c.Assert(after-before, Less, 3)
 }
 
 func (s *testMainSuite) TestSchemaCheckerSimple(c *C) {
-	defer testleak.AfterTest(c)()
 	lease := 5 * time.Millisecond
 	validator := domain.NewSchemaValidator(lease)
 	checker := &schemaLeaseChecker{SchemaValidator: validator}
