61f64c2eb27d37a4e6911a44b62849136836f761
MyonKeminta <mk@mkmkm.me>
2018-05-15 10:49:57 +0800

    address comments

--- store/tikv/delete_range.go
+++ store/tikv/delete_range.go
@@ -36,7 +36,7 @@ type DeleteRangeTask struct {
 }
 
 // NewDeleteRangeTask creates a DeleteRangeTask. Deleting will not be performed right away.
-// WARNING: Currently this API may last some dirty keys in TiKV. Be careful using this API.
+// WARNING: Currently, this API may leave some waste key-value pairs uncleaned in TiKV. Be careful while using it.
 func NewDeleteRangeTask(ctx context.Context, store Storage, bo *Backoffer, startKey []byte, endKey []byte) *DeleteRangeTask {
 	return &DeleteRangeTask{
 		completedRegions: 0,
