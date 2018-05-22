d7ab671a16c2ae77e5cb8d916053f7430f00f122
winkyao <golangwink@gmail.com>
2018-05-22 21:47:33 +0800

    chunk: fix delete index in year type;

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -2777,3 +2777,12 @@ func (s *testSuite) TestCoprocessorStreamingWarning(c *C) {
 	result.Check(testkit.Rows())
 	tk.MustQuery("show warnings").Check(testutil.RowsWithSep("|", "Warning|1105|Division by 0"))
 }
+
+func (s *testSuite) TestYearTypeDeleteIndex(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a YEAR, PRIMARY KEY(a));")
+	tk.MustExec("delete from t;")
+	tk.MustExec("admin check table t")
+}
