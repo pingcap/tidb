7575bb8179b617de1dce1735079aa1b51bb632ab
Jian Zhang <zjsariel@gmail.com>
2018-05-28 17:07:50 +0800

    add test

---
+++ executor/insert_test.go
@@ -0,0 +1,40 @@
+package executor_test
+
+import (
+	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb/util/testkit"
+)
+
+func (s *testSuite) TestInsetOnDup(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a YEAR, PRIMARY KEY(a));")
+	tk.MustExec("insert into t set a = '2151';")
+	tk.MustExec("delete from t;")
+	tk.MustExec("admin check table t")
+
+	tk.MustExec(`drop table if exists t1, t2;`)
+	tk.MustExec(`create table t1(a1 bigint primary key, b1 bigint);`)
+	tk.MustExec(`create table t2(a2 bigint primary key, b2 bigint);`)
+	tk.MustExec(`insert into t1 values(1, 100);`)
+	tk.MustExec(`insert into t2 values(1, 200);`)
+
+	tk.MustExec(`insert into t1 select a2, b2 from t2 on duplicate key update b1 = a2;`)
+	tk.MustQuery(`select * from t1;`).Check(testkit.Rows("1 1"))
+
+	tk.MustExec(`insert into t1 select a2, b2 from t2 on duplicate key update b1 = b2;`)
+	tk.MustQuery(`select * from t1;`).Check(testkit.Rows("1 200"))
+
+	tk.MustExec(`insert into t1 select a2, b2 from t2 on duplicate key update a1 = a2;`)
+	tk.MustQuery(`select * from t1;`).Check(testkit.Rows("1 200"))
+
+	tk.MustExec(`insert into t1 select a2, b2 from t2 on duplicate key update b1 = 300;`)
+	tk.MustQuery(`select * from t1;`).Check(testkit.Rows("1 300"))
+
+	tk.MustExec(`insert into t1 values(1, 1) on duplicate key update b1 = 400;`)
+	tk.MustQuery(`select * from t1;`).Check(testkit.Rows("1 400"))
+
+	tk.MustExec(`insert into t1 select 1, 500 from t2 on duplicate key update b1 = 400;`)
+	tk.MustQuery(`select * from t1;`).Check(testkit.Rows("1 400"))
+}
