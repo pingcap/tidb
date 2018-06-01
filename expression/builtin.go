46da21b5c1cf177a357750b7d04c0ee8d61cc9b3
winkyao <golangwink@gmail.com>
2018-06-01 17:56:44 +0800

    *: support select tidb_is_ddl_owner, to figure out whether tidb instance is ddl owner (#6682)

--- expression/builtin.go
+++ expression/builtin.go
@@ -462,7 +462,8 @@ var funcs = map[string]functionClass{
 	ast.SessionUser:  &userFunctionClass{baseFunctionClass{ast.SessionUser, 0, 0}},
 	ast.SystemUser:   &userFunctionClass{baseFunctionClass{ast.SystemUser, 0, 0}},
 	// This function is used to show tidb-server version info.
-	ast.TiDBVersion: &tidbVersionFunctionClass{baseFunctionClass{ast.TiDBVersion, 0, 0}},
+	ast.TiDBVersion:    &tidbVersionFunctionClass{baseFunctionClass{ast.TiDBVersion, 0, 0}},
+	ast.TiDBIsDDLOwner: &tidbIsDDLOwnerFunctionClass{baseFunctionClass{ast.TiDBIsDDLOwner, 0, 0}},
 
 	// control functions
 	ast.If:     &ifFunctionClass{baseFunctionClass{ast.If, 3, 3}},
