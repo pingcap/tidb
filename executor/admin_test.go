0c5dec9ce763c8cd998b422737f735c4a0bca579
winkyao <golangwink@gmail.com>
2018-06-08 15:37:32 +0800

    expression, util: DecodeDecimal return real precision and frac (#6732)

--- executor/admin_test.go
+++ executor/admin_test.go
@@ -423,6 +423,12 @@ func (s *testSuite) TestAdminCheckTable(c *C) {
 	tk.MustExec(`insert into test set a='12:10:36';`)
 	tk.MustExec(`admin check table test`)
 
+	// Test decimal
+	tk.MustExec(`drop table if exists test`)
+	tk.MustExec("CREATE TABLE test (  a decimal, PRIMARY KEY (a));")
+	tk.MustExec("insert into test set a=10;")
+	tk.MustExec("admin check table test;")
+
 	// Test timestamp type check table.
 	tk.MustExec(`drop table if exists test`)
 	tk.MustExec(`create table test ( a  TIMESTAMP, primary key(a) );`)
