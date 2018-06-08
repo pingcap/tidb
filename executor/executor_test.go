976e8cd22136dcb62a92dce5ca680ae134e747b6
lysu <sulifx@gmail.com>
2018-06-08 19:01:13 +0800

    *: Fix select lock option for union statement (#6579) (#6786)

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -2776,3 +2776,35 @@ func (s *testSuite) TestYearTypeDeleteIndex(c *C) {
 	tk.MustExec("delete from t;")
 	tk.MustExec("admin check table t")
 }
+
+func (s *testSuite) TestForSelectScopeInUnion(c *C) {
+	// A union B for update, the "for update" option belongs to union statement, so
+	// it should works on both A and B.
+	tk1 := testkit.NewTestKit(c, s.store)
+	tk2 := testkit.NewTestKit(c, s.store)
+	tk1.MustExec("use test")
+	tk1.MustExec("drop table if exists t")
+	tk1.MustExec("create table t(a int)")
+	tk1.MustExec("insert into t values (1)")
+
+	tk1.MustExec("begin")
+	// 'For update' would act on the second select.
+	tk1.MustQuery("select 1 as a union select a from t for update")
+
+	tk2.MustExec("use test")
+	tk2.MustExec("update t set a = a + 1")
+
+	// As tk1 use select 'for update', it should dectect conflict and fail.
+	_, err := tk1.Exec("commit")
+	c.Assert(err, NotNil)
+
+	tk1.MustExec("begin")
+	// 'For update' would be ignored if 'order by' or 'limit' exists.
+	tk1.MustQuery("select 1 as a union select a from t limit 5 for update")
+	tk1.MustQuery("select 1 as a union select a from t order by a for update")
+
+	tk2.MustExec("update t set a = a + 1")
+
+	_, err = tk1.Exec("commit")
+	c.Assert(err, IsNil)
+}
--- parser/parser.y
+++ parser/parser.y
@@ -4543,6 +4543,9 @@ UnionStmt:
 		if $6 != nil {
 		    union.Limit = $6.(*ast.Limit)
 		}
+		if $5 == nil && $6 == nil {
+		    st.LockTp = $7.(ast.SelectLockType)
+		}
 		$$ = union
 	}
 |	UnionClauseList "UNION" UnionOpt SelectStmtFromDual SelectStmtLimit SelectLockOpt
@@ -4556,6 +4559,8 @@ UnionStmt:
 		union.SelectList.Selects = append(union.SelectList.Selects, st)
 		if $5 != nil {
 		    union.Limit = $5.(*ast.Limit)
+		} else {
+		    st.LockTp = $6.(ast.SelectLockType)
 		}
 		$$ = union
 	}
@@ -4574,6 +4579,9 @@ UnionStmt:
 		}
 		if $6 != nil {
 			union.Limit = $6.(*ast.Limit)
+		}
+		if $5 == nil && $6 == nil {
+			st.LockTp = $7.(ast.SelectLockType)
 		}
 		$$ = union
 	}
