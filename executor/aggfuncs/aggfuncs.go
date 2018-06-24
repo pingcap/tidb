d3a1ceda325454afbf2681b1ffdafa51996b0dc9
Jian Zhang <zjsariel@gmail.com>
2018-06-24 14:22:01 +0800

    only introduce the framework in this PR

--- executor/aggfuncs/aggfuncs.go
+++ executor/aggfuncs/aggfuncs.go
@@ -21,39 +21,46 @@ import (
 
 // All the AggFunc implementations are listed here for navigation.
 var (
-	// All the AggFunc implementations for "COUNT".
-	// All the AggFunc implementations for "SUM".
-	_ AggFunc = (*sum4Decimal)(nil)
-
-	// All the AggFunc implementations for "AVG".
-	_ AggFunc = (*avgDedup4Decimal)(nil)
-	_ AggFunc = (*avgOriginal4Decimal)(nil)
-	_ AggFunc = (*avgPartial4Decimal)(nil)
-
-	_ AggFunc = (*avgDedup4Float64)(nil)
-	_ AggFunc = (*avgOriginal4Float64)(nil)
-	_ AggFunc = (*avgPartial4Float64)(nil)
-
-	_ AggFunc = (*avgDedup4Float32)(nil)
-	_ AggFunc = (*avgOriginal4Float32)(nil)
-	_ AggFunc = (*avgPartial4Float32)(nil)
-
-	// All the AggFunc implementations for "FIRSTROW".
-	// All the AggFunc implementations for "MAX".
-	// All the AggFunc implementations for "MIN".
-	// All the AggFunc implementations for "GROUP_CONCAT".
-	// All the AggFunc implementations for "BIT_OR".
-	// All the AggFunc implementations for "BIT_XOR".
-	// All the AggFunc implementations for "BIT_AND".
+// All the AggFunc implementations for "COUNT" are listed here.
+// All the AggFunc implementations for "SUM" are listed here.
+// All the AggFunc implementations for "AVG" are listed here.
+// All the AggFunc implementations for "FIRSTROW" are listed here.
+// All the AggFunc implementations for "MAX" are listed here.
+// All the AggFunc implementations for "MIN" are listed here.
+// All the AggFunc implementations for "GROUP_CONCAT" are listed here.
+// All the AggFunc implementations for "BIT_OR" are listed here.
+// All the AggFunc implementations for "BIT_XOR" are listed here.
+// All the AggFunc implementations for "BIT_AND" are listed here.
 )
 
-// AggFunc is the interface to evaluate aggregate functions.
+// AggFunc is the interface to evaluate the aggregate functions.
 type AggFunc interface {
+	// AllocPartialResult allocates a specific data structure to store the
+	// partial result, initializes it, and converts it to a bype slice to return
+	// back. Aggregate operator implementations, no mater whether it's a hash or
+	// stream implementation, should hold this byte slice for further operations
+	// like: "ResetPartialResult", "UpdatePartialResult".
 	AllocPartialResult() []byte
+
+	// ResetPartialResult resets the partial result to the original state for a
+	// specific aggregate function. It converts the input byte slice to the
+	// specific data structure which stores the partial result and then reset
+	// every field to the proper original state.
 	ResetPartialResult(partialBytes []byte)
+
+	// UpdatePartialResult updates the specific partial result for an aggregate
+	// function using the input rows which all belonging to the same data group.
+	// It converts the input byte slice to the specific data structure which
+	// stores the partial result and then iterates on the input rows and update
+	// that partial result according to the functionality and the state of the
+	// aggregate function.
 	UpdatePartialResult(sctx sessionctx.Context, rowsInGroup []chunk.Row, partialBytes []byte) error
 
-	AppendPartialResult2Chunk(sctx sessionctx.Context, partialBytes []byte, chk *chunk.Chunk) error
+	// AppendFinalResult2Chunk finalizes the partial result and append the
+	// final result to the input chunk. Like other operations, it converts the
+	// input byte slice to the specific data structure which stores the partial
+	// result and then calculates the final result and append that final result
+	// to the chunk provided.
 	AppendFinalResult2Chunk(sctx sessionctx.Context, partialBytes []byte, chk *chunk.Chunk) error
 }
 
--- executor/aggfuncs/builder.go
+++ executor/aggfuncs/builder.go
@@ -20,7 +20,8 @@ import (
 	"github.com/pingcap/tidb/types"
 )
 
-// Build is used to build AggFunc according to the aggFuncDesc
+// Build is used to build a specific AggFunc implementation according to the
+// input aggFuncDesc
 func Build(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	switch aggFuncDesc.Name {
 	case ast.AggFuncCount:
@@ -47,125 +48,52 @@ func Build(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "COUNT".
 func buildCount(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "SUM".
 func buildSum(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
-	base := baseAggFunc{
-		input:  aggFuncDesc.Args,
-		output: output,
-	}
-
-	switch aggFuncDesc.Mode {
-	// Build sum functions which consume the original data and remove the
-	// duplicated input of the same group.
-	case aggregation.DedupMode:
-		return nil
-
-	// Build sum functions which consume the original data and update their
-	// partial results.
-	case aggregation.CompleteMode, aggregation.Partial1Mode:
-		switch aggFuncDesc.Args[0].GetType().Tp {
-		case mysql.TypeNewDecimal:
-			if aggFuncDesc.HasDistinct {
-				return &sum4Decimal{base, make(map[types.MyDecimal]bool)}
-			}
-			return &sum4Decimal{base, nil}
-		}
-
-	// Build sum functions which consume the partial result of other agg
-	// functions and update their partial results.
-	case aggregation.Partial2Mode, aggregation.FinalMode:
-		switch aggFuncDesc.Args[0].GetType().Tp {
-		case mysql.TypeNewDecimal:
-			return &sum4Decimal{base, nil}
-		}
-	}
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "AVG".
 func buildAvg(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
-	base := baseAggFunc{
-		input:  aggFuncDesc.Args,
-		output: output,
-	}
-
-	switch aggFuncDesc.Mode {
-	// Build avg functions which consume the original data and remove the
-	// duplicated input of the same group.
-	case aggregation.DedupMode:
-		return nil
-		// TODO: implement UpdatePartialResult for the following structs.
-		// switch aggFuncDesc.Args[0].GetType().Tp {
-		// case mysql.TypeDecimal:
-		// 	return &avgDedup4Decimal{baseAvgDecimal{base}}
-		// case mysql.TypeFloat:
-		// 	return &avgDedup4Float32{baseAvgFloat32{base}}
-		// case mysql.TypeDouble:
-		// 	return &avgDedup4Float64{baseAvgFloat64{base}}
-		// }
-
-	// Build avg functions which consume the original data and update their
-	// partial results.
-	case aggregation.CompleteMode, aggregation.Partial1Mode:
-		switch aggFuncDesc.Args[0].GetType().Tp {
-		case mysql.TypeNewDecimal:
-			if aggFuncDesc.HasDistinct {
-				return &avgOriginal4Decimal{baseAvgDecimal{base}, make(map[types.MyDecimal]bool)}
-			}
-			return &avgOriginal4Decimal{baseAvgDecimal{base}, nil}
-		case mysql.TypeFloat:
-			if aggFuncDesc.HasDistinct {
-				return &avgOriginal4Float32{baseAvgFloat32{base}, make(map[float32]bool)}
-			}
-			return &avgOriginal4Float32{baseAvgFloat32{base}, nil}
-		case mysql.TypeDouble:
-			if aggFuncDesc.HasDistinct {
-				return &avgOriginal4Float64{baseAvgFloat64{base}, make(map[float64]bool)}
-			}
-			return &avgOriginal4Float64{baseAvgFloat64{base}, nil}
-		}
-
-	// Build avg functions which consume the partial result of other agg
-	// functions and update their partial results.
-	case aggregation.Partial2Mode, aggregation.FinalMode:
-		switch aggFuncDesc.Args[1].GetType().Tp {
-		case mysql.TypeNewDecimal:
-			return &avgPartial4Decimal{baseAvgDecimal{base}}
-		case mysql.TypeFloat:
-			return &avgPartial4Float32{baseAvgFloat32{base}}
-		case mysql.TypeDouble:
-			return &avgPartial4Float64{baseAvgFloat64{base}}
-		}
-	}
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "FIRST_ROW".
 func buildFirstRow(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "MAX".
 func buildMax(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "MIN".
 func buildMin(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "GROUP_CONCAT".
 func buildGroupConcat(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "BIT_OR".
 func buildBitOr(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "BIT_XOR".
 func buildBitXor(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
 
+// buildCount builds the AggFunc implementation for function "BIT_AND".
 func buildBitAnd(aggFuncDesc *aggregation.AggFuncDesc, output []int) AggFunc {
 	return nil
 }
