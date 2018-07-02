bb721f5735d809053b60ee309f9765d0c9dacb71
tiancaiamao <tiancaiamao@gmail.com>
2018-07-02 15:26:35 +0800

    *: kill one's own connection doesn't require SUPER privilege
    `kill tidb connID`, if the user is the owner of that connection, there
    is no need to check the SUPER privilege.
    SessionManager interface is slightly modified.

--- util/processinfo.go
+++ util/processinfo.go
@@ -33,6 +33,6 @@ type ProcessInfo struct {
 // SessionManager is an interface for session manage. Show processlist and
 // kill statement rely on this interface.
 type SessionManager interface {
-	ShowProcessList() []ProcessInfo
+	ShowProcessList() map[uint64]ProcessInfo
 	Kill(connectionID uint64, query bool)
 }
