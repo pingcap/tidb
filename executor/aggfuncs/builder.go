5f59da364b9b4fb262621e827e3b1db0ac6bb9a0
crazycs <crazycs520@gmail.com>
2018-07-04 19:46:44 +0800

    aggfuncs: add comment

--- executor/aggfuncs/builder.go
+++ executor/aggfuncs/builder.go
@@ -120,6 +120,7 @@ func buildGroupConcat(aggFuncDesc *aggregation.AggFuncDesc, ordinal int) AggFunc
 
 // buildCount builds the AggFunc implementation for function "BIT_OR".
 func buildBitOr(aggFuncDesc *aggregation.AggFuncDesc, ordinal int) AggFunc {
+	// BIT_OR doesn't need to handle the distinct property.
 	base := baseAggFunc{
 		args:    aggFuncDesc.Args,
 		ordinal: ordinal,
--- executor/aggfuncs/func_bit_or.go
+++ executor/aggfuncs/func_bit_or.go
@@ -23,22 +23,20 @@ type bitOrUint64 struct {
 	baseAggFunc
 }
 
-type partialResult4BitFunc struct {
-	value uint64
-}
+type partialResult4BitFunc = uint64
 
 func (e *bitOrUint64) AllocPartialResult() PartialResult {
-	return PartialResult(&partialResult4BitFunc{})
+	return PartialResult(new(partialResult4BitFunc))
 }
 
 func (e *bitOrUint64) ResetPartialResult(pr PartialResult) {
 	p := (*partialResult4BitFunc)(pr)
-	p.value = 0
+	*p = 0
 }
 
 func (e *bitOrUint64) AppendFinalResult2Chunk(sctx sessionctx.Context, pr PartialResult, chk *chunk.Chunk) error {
 	p := (*partialResult4BitFunc)(pr)
-	chk.AppendUint64(e.ordinal, p.value)
+	chk.AppendUint64(e.ordinal, *p)
 	return nil
 }
 
@@ -49,11 +47,10 @@ func (e *bitOrUint64) UpdatePartialResult(sctx sessionctx.Context, rowsInGroup [
 		if err != nil {
 			return errors.Trace(err)
 		}
-
 		if isNull {
 			continue
 		}
-		p.value |= uint64(inputValue)
+		*p |= uint64(inputValue)
 	}
 	return nil
 }
