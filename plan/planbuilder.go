372a6fc921776819ca1df1832a383b055aeb82c3
Ewan Chou <coocood@gmail.com>
2018-07-09 13:55:36 +0800

    plan: refine explain result format (#7011)

--- plan/planbuilder.go
+++ plan/planbuilder.go
@@ -1232,7 +1232,7 @@ func (b *planBuilder) buildExplain(explain *ast.ExplainStmt) Plan {
 	p := &Explain{StmtPlan: pp}
 	switch strings.ToLower(explain.Format) {
 	case ast.ExplainFormatROW:
-		retFields := []string{"id", "task", "operator info", "count"}
+		retFields := []string{"id", "count", "task", "operator info"}
 		schema := expression.NewSchema(make([]*expression.Column, 0, len(retFields))...)
 		for _, fieldName := range retFields {
 			schema.Append(buildColumn("", fieldName, mysql.TypeString, mysql.MaxBlobWidth))
