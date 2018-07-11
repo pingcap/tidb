28403da061bc35045fe8c5b70fc245f6c845204a
XuHuaiyu <391585975@qq.com>
2018-07-11 17:48:21 +0800

    address comment

--- executor/aggfuncs/aggfuncs.go
+++ executor/aggfuncs/aggfuncs.go
@@ -41,8 +41,11 @@ var (
 	// All the AggFunc implementations for "BIT_OR" are listed here.
 	_ AggFunc = (*bitOrUint64)(nil)
 
-// All the AggFunc implementations for "BIT_XOR" are listed here.
-// All the AggFunc implementations for "BIT_AND" are listed here.
+	// All the AggFunc implementations for "BIT_XOR" are listed here.
+	_ AggFunc = (*bitXorUint64)(nil)
+
+	// All the AggFunc implementations for "BIT_AND" are listed here.
+	_ AggFunc = (*bitAndUint64)(nil)
 )
 
 // PartialResult represents data structure to store the partial result for the
