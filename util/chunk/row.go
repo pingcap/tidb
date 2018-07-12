1ad63deb55d905fe60e12683f43da7aa1f32c261
robi <sulifx@gmail.com>
2018-07-12 22:24:29 +0800

    *: make arrow no longer store fsp for duration

--- util/chunk/row.go
+++ util/chunk/row.go
@@ -20,6 +20,7 @@ import (
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/types/json"
 	"github.com/pingcap/tidb/util/hack"
+	"time"
 )
 
 var _ types.Row = Row{}
@@ -88,7 +89,8 @@ func (r Row) GetTime(colIdx int) types.Time {
 // GetDuration returns the Duration value with the colIdx.
 func (r Row) GetDuration(colIdx int) types.Duration {
 	col := r.c.columns[colIdx]
-	return *(*types.Duration)(unsafe.Pointer(&col.data[r.idx*16]))
+	dur := *(*int64)(unsafe.Pointer(&col.data[r.idx*8]))
+	return types.Duration{Duration: time.Duration(dur), Fsp: types.WaitFillFsp}
 }
 
 func (r Row) getNameValue(colIdx int) (string, uint64) {
@@ -175,7 +177,11 @@ func (r Row) GetDatum(colIdx int, tp *types.FieldType) types.Datum {
 		}
 	case mysql.TypeDuration:
 		if !r.IsNull(colIdx) {
-			d.SetMysqlDuration(r.GetDuration(colIdx))
+			duration := r.GetDuration(colIdx)
+			if duration.Fsp == types.WaitFillFsp {
+				duration.Fsp = tp.Decimal
+			}
+			d.SetMysqlDuration(duration)
 		}
 	case mysql.TypeNewDecimal:
 		if !r.IsNull(colIdx) {
