b179fc28b1de078c3ab9f1595a274499c6f4244c
Jian Zhang <zjsariel@gmail.com>
2018-07-12 11:03:43 +0800

    add test

--- util/chunk/codec.go
+++ util/chunk/codec.go
@@ -15,19 +15,24 @@ package chunk
 
 import (
 	"encoding/binary"
+	"fmt"
 	"reflect"
 	"unsafe"
 
+	"github.com/pingcap/tidb/mysql"
 	"github.com/pingcap/tidb/types"
 )
 
 type Codec struct {
-	// colTypes is only used when decode a Chunk.
+	// colTypes is only used when decoding a Chunk.
 	colTypes []*types.FieldType
+
+	// notNullBitmap is only used when decoding a Chunk.
+	notNullBitmap []byte
 }
 
 func NewCodec(colTypes []*types.FieldType) *Codec {
-	return &Codec{colTypes}
+	return &Codec{colTypes, nil}
 }
 
 func (c *Codec) Encode(chk *Chunk) []byte {
@@ -46,14 +51,14 @@ func (c *Codec) encodeFixLenColumn(buffer []byte, col *column) []byte {
 	startOffset := len(buffer)
 
 	// encode column
-	var lenBuffer [8]byte
+	var lenBuffer [4]byte
 	buffer = append(buffer, lenBuffer[:]...)
 	buffer = c.encodeColumn(buffer, lenBuffer[:], col)
 
 	// supplement the length info of this column in the front
 	numEncodedBytes := len(buffer) - startOffset
-	binary.LittleEndian.PutUint64(lenBuffer[:], uint64(numEncodedBytes))
-	copy(buffer[startOffset:], lenBuffer[:8])
+	binary.LittleEndian.PutUint32(lenBuffer[:], uint32(numEncodedBytes))
+	copy(buffer[startOffset:], lenBuffer[:4])
 
 	return buffer
 }
@@ -62,22 +67,20 @@ func (c *Codec) encodeVarLenColumn(buffer []byte, col *column) []byte {
 	startOffset := len(buffer)
 
 	// encode column
-	var lenBuffer [8]byte
+	var lenBuffer [4]byte
 	buffer = append(buffer, lenBuffer[:]...)
 	buffer = c.encodeColumn(buffer, lenBuffer[:], col)
 
 	// encode offsets
 	offsetBytes := c.i32SliceToBytes(col.offsets)
-	binary.LittleEndian.PutUint64(lenBuffer[:], uint64(len(offsetBytes)))
-	buffer = append(buffer, lenBuffer[:8]...)
+	binary.LittleEndian.PutUint32(lenBuffer[:], uint32(len(offsetBytes)))
+	buffer = append(buffer, lenBuffer[:4]...)
 	buffer = append(buffer, offsetBytes...)
-	numPaddingBytes := c.calcNumPaddingBytes(len(offsetBytes))
-	buffer = append(buffer, lenBuffer[:numPaddingBytes]...)
 
 	// supplement the length info of this column in the front
 	numEncodedBytes := len(buffer) - startOffset
-	binary.LittleEndian.PutUint64(lenBuffer[:], uint64(numEncodedBytes))
-	copy(buffer[startOffset:], lenBuffer[:8])
+	binary.LittleEndian.PutUint32(lenBuffer[:], uint32(numEncodedBytes))
+	copy(buffer[startOffset:], lenBuffer[:4])
 
 	return buffer
 }
@@ -91,19 +94,19 @@ func (c *Codec) encodeColumn(buffer, lenBuffer []byte, col *column) []byte {
 	binary.LittleEndian.PutUint32(lenBuffer, uint32(col.nullCount))
 	buffer = append(buffer, lenBuffer[:4]...)
 
+	fmt.Printf("encodeColumn: col.nullCount=%v, col.length=%v\n", col.nullCount, col.length)
+
 	// encode nullBitmap
-	binary.LittleEndian.PutUint64(lenBuffer, uint64(len(col.nullBitmap)))
-	buffer = append(buffer, lenBuffer[:8]...)
-	buffer = append(buffer, col.nullBitmap...)
-	numPaddingBytes := c.calcNumPaddingBytes(len(col.nullBitmap))
-	buffer = append(buffer, lenBuffer[:numPaddingBytes]...)
+	if col.nullCount > 0 {
+		binary.LittleEndian.PutUint32(lenBuffer, uint32(len(col.nullBitmap)))
+		buffer = append(buffer, lenBuffer[:4]...)
+		buffer = append(buffer, col.nullBitmap...)
+	}
 
 	// encode data
-	binary.LittleEndian.PutUint64(lenBuffer, uint64(len(col.data)))
-	buffer = append(buffer, lenBuffer[:8]...)
+	binary.LittleEndian.PutUint32(lenBuffer, uint32(len(col.data)))
+	buffer = append(buffer, lenBuffer[:4]...)
 	buffer = append(buffer, col.data...)
-	numPaddingBytes = c.calcNumPaddingBytes(len(col.data))
-	buffer = append(buffer, lenBuffer[:numPaddingBytes]...)
 
 	return buffer
 }
@@ -119,37 +122,29 @@ func (c *Codec) i32SliceToBytes(i32s []int32) (b []byte) {
 	return b
 }
 
-func (c *Codec) calcNumPaddingBytes(numAppendedBytes int) int {
-	numTailBytes := numAppendedBytes & 0x7
-	return (8 - numTailBytes) & 0x7
-}
-
 func (c *Codec) Decode(buffer []byte) *Chunk {
 	chk := &Chunk{}
-	for len(buffer) > 0 {
+	for ordinal := 0; len(buffer) > 0; ordinal++ {
 		col := &column{}
-		buffer = c.decodeColumn(buffer, col)
+		buffer = c.decodeColumn(buffer, col, ordinal)
 		chk.columns = append(chk.columns, col)
 	}
 	return chk
 }
 
-func (c *Codec) Decode2Chunk(buffer []byte) *Chunk {
-	chk := &Chunk{}
-	for len(buffer) > 0 {
-		col := &column{}
-		buffer = c.decodeColumn(buffer, col)
-		chk.columns = append(chk.columns, col)
+func (c *Codec) DecodeToChunk(buffer []byte, chk *Chunk) (remained []byte) {
+	for i := 0; i < len(chk.columns); i++ {
+		buffer = c.decodeColumn(buffer, chk.columns[i], i)
 	}
-	return chk
+	return buffer
 }
 
-func (c *Codec) decodeColumn(buffer []byte, col *column) []byte {
-	numEatenBytes := uint64(0)
+func (c *Codec) decodeColumn(buffer []byte, col *column, ordinal int) (remained []byte) {
+	numEatenBytes := uint32(0)
 
 	// decode number of the total bytes of a column.
-	numTotalBytes := binary.LittleEndian.Uint64(buffer[numEatenBytes:])
-	numEatenBytes += 8
+	numTotalBytes := binary.LittleEndian.Uint32(buffer[numEatenBytes:])
+	numEatenBytes += 4
 
 	// decode length.
 	col.length = int(binary.LittleEndian.Uint32(buffer[numEatenBytes:]))
@@ -160,27 +155,52 @@ func (c *Codec) decodeColumn(buffer []byte, col *column) []byte {
 	numEatenBytes += 4
 
 	// decode nullBitmap.
-	numBitmapBytes := uint64(binary.LittleEndian.Uint64(buffer[numEatenBytes:]))
-	numEatenBytes += 8
-	col.nullBitmap = buffer[numEatenBytes : numEatenBytes+numBitmapBytes]
-	numEatenBytes += numBitmapBytes + uint64(c.calcNumPaddingBytes(int(numBitmapBytes)))
+	fmt.Printf("decodeColumn: col.nullCount=%v, col.length=%v\n", col.nullCount, col.length)
+	if col.nullCount > 0 {
+		numBitmapBytes := uint32(binary.LittleEndian.Uint32(buffer[numEatenBytes:]))
+		numEatenBytes += 4
+		col.nullBitmap = append(col.nullBitmap[:0], buffer[numEatenBytes:numEatenBytes+numBitmapBytes]...)
+		numEatenBytes += numBitmapBytes
+	} else {
+		col.nullBitmap = append(col.nullBitmap[:0], c.allNotNull(col.length)...)
+	}
 
 	// decode data.
-	numDataBytes := uint64(binary.LittleEndian.Uint64(buffer[numEatenBytes:]))
-	numEatenBytes += 8
-	col.data = buffer[numEatenBytes : numEatenBytes+numDataBytes]
-	numEatenBytes += numDataBytes + uint64(c.calcNumPaddingBytes(int(numDataBytes)))
+	numDataBytes := uint32(binary.LittleEndian.Uint32(buffer[numEatenBytes:]))
+	numEatenBytes += 4
+	col.data = append(col.data[:0], buffer[numEatenBytes:numEatenBytes+numDataBytes]...)
+	numEatenBytes += numDataBytes
 
+	numFixedBytes := getFixedLen(c.colTypes[ordinal])
+	if numFixedBytes != -1 {
+		if cap(col.elemBuf) < numFixedBytes {
+			col.elemBuf = make([]byte, numFixedBytes)
+		}
+		return buffer[numTotalBytes:]
+	}
 	if numEatenBytes == numTotalBytes {
 		return buffer[numTotalBytes:]
 	}
 
 	// decode offsets.
-	numOffsetBytes := binary.LittleEndian.Uint64(buffer[numEatenBytes:])
-	numEatenBytes += 8
-	col.offsets = c.bytesToI32Slice(buffer[numEatenBytes : numEatenBytes+numOffsetBytes])
+	numOffsetBytes := binary.LittleEndian.Uint32(buffer[numEatenBytes:])
+	numEatenBytes += 4
+	col.offsets = append(col.offsets[:0], c.bytesToI32Slice(buffer[numEatenBytes:numEatenBytes+numOffsetBytes])...)
 
 	return buffer[numTotalBytes:]
+}
+
+func (c *Codec) allNotNull(numElements int) []byte {
+	numBytes := (numElements + 7) >> 3
+	if len(c.notNullBitmap) < numBytes {
+		newNotNullBitmap := make([]byte, numBytes)
+		copy(newNotNullBitmap, c.notNullBitmap)
+		for i := len(c.notNullBitmap); i < numBytes; i++ {
+			newNotNullBitmap[i] = byte(0xFF)
+		}
+		c.notNullBitmap = newNotNullBitmap
+	}
+	return c.notNullBitmap[:numBytes]
 }
 
 func (c *Codec) bytesToI32Slice(b []byte) (i32s []int32) {
@@ -192,4 +212,20 @@ func (c *Codec) bytesToI32Slice(b []byte) (i32s []int32) {
 	hdr.Cap = hdr.Len
 	hdr.Data = uintptr(unsafe.Pointer(&b[0]))
 	return i32s
+}
+
+func getFixedLen(colType *types.FieldType) int {
+	switch colType.Tp {
+	case mysql.TypeFloat:
+		return 4
+	case mysql.TypeTiny, mysql.TypeShort, mysql.TypeInt24, mysql.TypeLong,
+		mysql.TypeLonglong, mysql.TypeDouble, mysql.TypeYear:
+		return 8
+	case mysql.TypeDuration, mysql.TypeDate, mysql.TypeDatetime, mysql.TypeTimestamp:
+		return 16
+	case mysql.TypeNewDecimal:
+		return types.MyDecimalStructSize
+	default:
+		return -1
+	}
 }
