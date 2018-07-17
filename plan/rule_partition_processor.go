a076e4f389a2c6de1dbb8b1f340980bde8d6a7b5
tiancaiamao <tiancaiamao@gmail.com>
2018-07-17 13:42:44 +0800

    address comment

--- plan/rule_partition_processor.go
+++ plan/rule_partition_processor.go
@@ -105,10 +105,9 @@ func (s *partitionProcessor) prune(ds *DataSource) (LogicalPlan, error) {
 		newDataSource.baseLogicalPlan = newBaseLogicalPlan(ds.context(), TypeTableScan, &newDataSource)
 		newDataSource.isPartition = true
 		newDataSource.partitionID = pi.Definitions[i].ID
-		// Find a better way to handle here.
-		// There are so many place that refer to the old DataSource including the schema
-		// and the parent node's schema, it's hard to update all the references correctly.
-		// So just reset the plan ID here, the newDataSource pretend itself to be the old one.
+		// There are many expression nodes in the plan tree use the original datasource
+		// id as FromID. So we set the id of the newDataSource with the original one to
+		// avoid traversing the whole plan tree to update the references.
 		newDataSource.id = ds.id
 
 		children = append(children, &newDataSource)
