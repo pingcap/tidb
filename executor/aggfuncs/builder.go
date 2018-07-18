6b13c4309ce8223389bbc2ae06a62e5fae074747
HuaiyuXu <391585975@qq.com>
2018-07-18 09:37:09 +0800

    executor: support firstrow under new aggregation evaluation framework (#7057)

--- executor/aggfuncs/builder.go
+++ executor/aggfuncs/builder.go
@@ -143,6 +143,37 @@ func buildAvg(aggFuncDesc *aggregation.AggFuncDesc, ordinal int) AggFunc {
 
 // buildFirstRow builds the AggFunc implementation for function "FIRST_ROW".
 func buildFirstRow(aggFuncDesc *aggregation.AggFuncDesc, ordinal int) AggFunc {
+	base := baseAggFunc{
+		args:    aggFuncDesc.Args,
+		ordinal: ordinal,
+	}
+
+	evalType, fieldType := aggFuncDesc.RetTp.EvalType(), aggFuncDesc.RetTp
+	switch aggFuncDesc.Mode {
+	case aggregation.DedupMode:
+	default:
+		switch evalType {
+		case types.ETInt:
+			return &firstRow4Int{base}
+		case types.ETReal:
+			switch fieldType.Tp {
+			case mysql.TypeFloat:
+				return &firstRow4Float32{base}
+			case mysql.TypeDouble:
+				return &firstRow4Float64{base}
+			}
+		case types.ETDecimal:
+			return &firstRow4Decimal{base}
+		case types.ETDatetime, types.ETTimestamp:
+			return &firstRow4Time{base}
+		case types.ETDuration:
+			return &firstRow4Duration{base}
+		case types.ETString:
+			return &firstRow4String{base}
+		case types.ETJson:
+			return &firstRow4JSON{base}
+		}
+	}
 	return nil
 }
 
