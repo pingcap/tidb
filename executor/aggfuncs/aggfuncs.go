cbfdba45a4efd76438ff5a4120da582822a6e7a2
laidahe <laidahe2006@126.com>
2018-07-21 16:21:24 +0800

    executor: add sum aggfunc
    support  int / uint / decimal for sum

--- executor/aggfuncs/aggfuncs.go
+++ executor/aggfuncs/aggfuncs.go
@@ -32,8 +32,9 @@ var (
 	_ AggFunc = (*avgOriginal4Float64)(nil)
 	_ AggFunc = (*avgPartial4Float64)(nil)
 
-	_ AggFunc = (*sumPartial4Decimal)(nil)
-	_ AggFunc = (*sumOriginal4Decimal)(nil)
+	_ AggFunc = (*sumAggFunc4Int)(nil)
+	_ AggFunc = (*sumAggFunc4Uint)(nil)
+	_ AggFunc = (*sumAggFunc4Decimal)(nil)
 
 	// All the AggFunc implementations for "FIRSTROW" are listed here.
 	// All the AggFunc implementations for "MAX" are listed here.
