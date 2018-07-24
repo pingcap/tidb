c6839974430f3ff4f1e13d66ded14b3f3e2a7367
winkyao <golangwink@gmail.com>
2018-07-24 09:50:01 +0800

    only backfill not found key into batchVals map

--- ddl/index.go
+++ ddl/index.go
@@ -658,12 +658,12 @@ func (w *addIndexWorker) batchCheckUniqueKey(txn kv.Transaction, idxRecords []*i
 				}
 			}
 			idxRecords[i].skip = true
-		}
-
-		// The keys in w.batchCheckKeys also maybe duplicate,
-		// so we need to backfill the not found key into `batchVals` map.
-		if w.distinctCheckFlags[i] {
-			batchVals[string(key)] = tables.EncodeHandle(idxRecords[i].handle)
+		} else {
+			// The keys in w.batchCheckKeys also maybe duplicate,
+			// so we need to backfill the not found key into `batchVals` map.
+			if w.distinctCheckFlags[i] {
+				batchVals[string(key)] = tables.EncodeHandle(idxRecords[i].handle)
+			}
 		}
 	}
 	// Constrains is already checked.
