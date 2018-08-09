1e3f2b3e3e9afbe9346cae44dc4c95c31e443d95
winkyao <golangwink@gmail.com>
2018-08-09 17:37:02 +0800

    *: update import path from coreos/gofail to etcd-io/gofail to fix ci (#7335)

--- ddl/ddl_db_test.go
+++ ddl/ddl_db_test.go
@@ -23,7 +23,7 @@ import (
 	"sync"
 	"time"
 
-	gofail "github.com/coreos/gofail/runtime"
+	gofail "github.com/etcd-io/gofail/runtime"
 	"github.com/juju/errors"
 	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb/ddl"
