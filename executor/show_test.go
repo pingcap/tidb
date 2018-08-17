1fb887f209fe6fd9feee1ed4d42b3f36f48250de
tiancaiamao <tiancaiamao@gmail.com>
2018-08-17 13:36:19 +0800

    executor: drop partition information in 'show create table' (#7388)

--- executor/show_test.go
+++ executor/show_test.go
@@ -330,8 +330,7 @@ func (s *testSuite) TestShow(c *C) {
 	tk.MustQuery("show create table t").Check(testutil.RowsWithSep("|",
 		"t CREATE TABLE `t` (\n"+
 			"  `a` int(11) DEFAULT NULL\n"+
-			") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin"+"\nPARTITION BY RANGE ( `a` ) (\n  PARTITION p0 VALUES LESS THAN 10,\n  PARTITION p1 VALUES LESS THAN 20,\n  PARTITION p2 VALUES LESS THAN MAXVALUE\n)",
-	))
+			") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin"))
 }
 
 func (s *testSuite) TestShowVisibility(c *C) {
