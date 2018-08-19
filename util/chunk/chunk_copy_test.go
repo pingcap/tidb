33ae5a09904a3514765d76c0cf9b52d747347071
crazycs <crazycs520@gmail.com>
2018-08-19 22:42:30 +0800

    refine code

--- util/chunk/chunk_copy_test.go
+++ util/chunk/chunk_copy_test.go
@@ -21,7 +21,7 @@ func newChunkWithInitCap(cap int, elemLen ...int) *Chunk {
 }
 
 func getChunk() *Chunk {
-	chk := newChunkWithInitCap(1024, 8, 8, 0, 0)
+	chk := newChunkWithInitCap(numRows, 8, 8, 0, 0)
 	for i := 0; i < numRows; i++ {
 		//chk.AppendNull(0)
 		chk.AppendInt64(0, int64(i))
@@ -30,6 +30,15 @@ func getChunk() *Chunk {
 		chk.AppendBytes(3, []byte("01234567890zxcvbnmqwer"))
 	}
 	return chk
+}
+
+func prepareChks() (it1 Iterator, row Row, dst *Chunk) {
+	chk1 := getChunk()
+	row = chk1.GetRow(0)
+	it1 = NewIterator4Chunk(chk1)
+	it1.Begin()
+	dst = newChunkWithInitCap(numRows, 8, 8, 0, 0, 8, 8, 0, 0)
+	return it1, row, dst
 }
 
 func checkDstChk(t *testing.T, dst *Chunk) {
@@ -69,11 +78,7 @@ func checkDstChk(t *testing.T, dst *Chunk) {
 }
 
 func TestCopyFieldByField(t *testing.T) {
-	chk1 := getChunk()
-	row := chk1.GetRow(0)
-	it1 := NewIterator4Chunk(chk1)
-	it1.Begin()
-	dst := newChunkWithInitCap(1024, 8, 8, 0, 0, 8, 8, 0, 0)
+	it1, row, dst := prepareChks()
 
 	dst.Reset()
 	for lhs := it1.Begin(); lhs != it1.End(); lhs = it1.Next() {
@@ -84,11 +89,7 @@ func TestCopyFieldByField(t *testing.T) {
 }
 
 func TestCopyColumnByColumn(t *testing.T) {
-	chk1 := getChunk()
-	row := chk1.GetRow(0)
-	it1 := NewIterator4Chunk(chk1)
-
-	dst := newChunkWithInitCap(1024, 8, 8, 0, 0, 8, 8, 0, 0)
+	it1, row, dst := prepareChks()
 
 	dst.Reset()
 	for it1.Begin(); it1.Current() != it1.End(); {
@@ -99,12 +100,7 @@ func TestCopyColumnByColumn(t *testing.T) {
 
 func BenchmarkCopyFieldByField(b *testing.B) {
 	b.ReportAllocs()
-	chk1 := getChunk()
-	row := getChunk().GetRow(0)
-
-	it1 := NewIterator4Chunk(chk1)
-
-	dst := newChunkWithInitCap(1024, 8, 8, 0, 0, 8, 8, 0, 0)
+	it1, row, dst := prepareChks()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -118,18 +114,13 @@ func BenchmarkCopyFieldByField(b *testing.B) {
 
 func BenchmarkCopyColumnByColumn(b *testing.B) {
 	b.ReportAllocs()
-	chk1 := getChunk()
-	row := getChunk().GetRow(0)
-
-	it1 := NewIterator4Chunk(chk1)
-
-	dst := newChunkWithInitCap(1024, 8, 8, 0, 0, 8, 8, 0, 0)
+	it1, row, dst := prepareChks()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		dst.Reset()
 		for it1.Begin(); it1.Current() != it1.End(); {
-			dst.AppendRightMultiRows(it1, row, 128)
+			dst.AppendRightMultiRows(it1, row, 1024)
 		}
 	}
 }
