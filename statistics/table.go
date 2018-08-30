bc42577edb480ffceb3b729cdf0c22a5c0260531
Yiding Cui <winoros@gmail.com>
2018-08-30 15:27:47 +0800

    remove blank line.

--- statistics/table.go
+++ statistics/table.go
@@ -472,7 +472,6 @@ func getOrdinalOfRangeCond(sc *stmtctx.StatementContext, ran *ranger.Range) int 
 
 // GenerateHistCollFromColumnInfo generates a new HistColl whose ColID2IdxID and IdxID2ColIDs is built from the given parameter.
 func (coll *HistColl) GenerateHistCollFromColumnInfo(infos []*model.ColumnInfo, columns []*expression.Column) HistColl {
-
 	newColHistMap := make(map[int64]*Column)
 	colInfoID2UniqueID := make(map[int64]int64)
 	colNames2UniqueID := make(map[string]int64)
