d4e82a20e711409b33245158dbf51eeb2d7495b0
Shafreeck Sea <shafreeck@gmail.com>
2018-08-30 18:04:14 +0800

    Add test case for KeyOnly being false

--- store/tikv/scan_test.go
+++ store/tikv/scan_test.go
@@ -109,5 +109,24 @@ func (s *testScanSuite) TestSeek(c *C) {
 		}
 		scan.Next()
 		c.Assert(scan.Valid(), IsFalse)
+
+		// Restore KeyOnly to false
+		txn3.SetOption(kv.KeyOnly, false)
+		scan, err = txn3.Seek(encodeKey(s.prefix, ""))
+		c.Assert(err, IsNil)
+
+		for i := 0; i < rowNum; i++ {
+			k := scan.Key()
+			c.Assert([]byte(k), BytesEquals, encodeKey(s.prefix, s08d("key", i)))
+			v := scan.Value()
+			c.Assert(v, BytesEquals, valueBytes(i))
+			// Because newScan return first item without calling scan.Next() just like go-hbase,
+			// for-loop count will decrease 1.
+			if i < rowNum-1 {
+				scan.Next()
+			}
+		}
+		scan.Next()
+		c.Assert(scan.Valid(), IsFalse)
 	}
 }
