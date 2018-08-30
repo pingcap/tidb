0ccae95726edbf3a3dccd059979bfe4dc64c0283
zhexuany <yangzhexuan@pingcap.com>
2018-08-30 17:24:41 +0800

    remove extra imports from trace.go

--- plan/trace.go
+++ plan/trace.go
@@ -1,12 +1,5 @@
 package plan
 
-import (
-	"github.com/juju/errors"
-	"github.com/pingcap/tidb/ast"
-	"github.com/pingcap/tidb/expression"
-	"github.com/pingcap/tidb/mysql"
-)
-
 // Trace represents a trace plan.
 type Trace struct {
 	baseSchemaProducer
