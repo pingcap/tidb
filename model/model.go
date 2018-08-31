68e165b1e7b3e5fc98b293500db623c0774a316a
crazycs <crazycs520@gmail.com>
2018-08-31 10:43:02 +0800

    Fix bit default value bug (#7249)

--- model/model.go
+++ model/model.go
@@ -22,6 +22,7 @@ import (
 	"github.com/juju/errors"
 	"github.com/pingcap/tidb/mysql"
 	"github.com/pingcap/tidb/types"
+	"github.com/pingcap/tidb/util/hack"
 	"github.com/pingcap/tipb/go-tipb"
 )
 
@@ -69,6 +70,7 @@ type ColumnInfo struct {
 	Offset              int                 `json:"offset"`
 	OriginDefaultValue  interface{}         `json:"origin_default"`
 	DefaultValue        interface{}         `json:"default"`
+	DefaultValueBit     []byte              `json:"default_bit"`
 	GeneratedExprString string              `json:"generated_expr_string"`
 	GeneratedStored     bool                `json:"generated_stored"`
 	Dependences         map[string]struct{} `json:"dependences"`
@@ -86,6 +88,31 @@ func (c *ColumnInfo) Clone() *ColumnInfo {
 // IsGenerated returns true if the column is generated column.
 func (c *ColumnInfo) IsGenerated() bool {
 	return len(c.GeneratedExprString) != 0
+}
+
+// SetDefaultValue sets the default value.
+func (c *ColumnInfo) SetDefaultValue(value interface{}) error {
+	c.DefaultValue = value
+	if c.Tp == mysql.TypeBit {
+		// For mysql.TypeBit type, the default value storage format must be a string.
+		// Other value such as int must convert to string format first.
+		if v, ok := value.(string); ok {
+			c.DefaultValueBit = []byte(v)
+			return nil
+		}
+		return types.ErrInvalidDefault.GenByArgs(c.Name)
+	}
+	return nil
+}
+
+// GetDefaultValue gets the default value of the column.
+// Default value use to stored in DefaultValue field, but now,
+// bit type default value will store in DefaultValueBit for fix bit default value decode/encode bug.
+func (c *ColumnInfo) GetDefaultValue() interface{} {
+	if c.Tp == mysql.TypeBit && c.DefaultValueBit != nil {
+		return hack.String(c.DefaultValueBit)
+	}
+	return c.DefaultValue
 }
 
 // FindColumnInfo finds ColumnInfo in cols by name.
