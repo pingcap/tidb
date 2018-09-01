7b2cf0f619770935e6e59a049f33d6b6e530aa6c
Kenan Yao <cauchy1992@gmail.com>
2018-09-01 21:35:11 +0800

    convert empty range scan to TableDual

--- plan/find_best_task.go
+++ plan/find_best_task.go
@@ -252,6 +252,14 @@ func (ds *DataSource) findBestTask(prop *requiredProp) (t task, err error) {
 	t = invalidTask
 
 	for _, path := range ds.possibleAccessPaths {
+		// if we already know the range of the scan is empty, just return a TableDual
+		if len(path.ranges) == 0 {
+			dual := PhysicalTableDual{}.init(ds.ctx, ds.stats)
+			dual.SetSchema(ds.schema)
+			return &rootTask{
+				p: dual,
+			}, nil
+		}
 		if path.isTablePath {
 			tblTask, err := ds.convertToTableScan(prop, path)
 			if err != nil {
@@ -263,11 +271,10 @@ func (ds *DataSource) findBestTask(prop *requiredProp) (t task, err error) {
 			continue
 		}
 		// We will use index to generate physical plan if:
-		// this path's range is nil or
 		// this path's access cond is not nil or
 		// we have prop to match or
 		// this index is forced to choose.
-		if len(path.ranges) == 0 || len(path.accessConds) > 0 || len(prop.cols) > 0 || path.forced {
+		if len(path.accessConds) > 0 || len(prop.cols) > 0 || path.forced {
 			idxTask, err := ds.convertToIndexScan(prop, path)
 			if err != nil {
 				return nil, errors.Trace(err)
