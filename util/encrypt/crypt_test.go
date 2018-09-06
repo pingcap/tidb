0a20e3bdc7d4bb68786a3eabe8784d2ebc93d95e
Emmett <42971249+lanjingquan@users.noreply.github.com>
2018-09-06 20:59:43 +0800

    fix again

--- util/encrypt/crypt_test.go
+++ util/encrypt/crypt_test.go
@@ -41,7 +41,7 @@ func (s *testEncryptSuite) TestSQLDecode(c *C) {
 			continue
 		}
 		c.Assert(err, IsNil, Commentf("%v", t))
-		result := toHex([]type(crypted))
+		result := toHex([]byte(crypted))
 		c.Assert(result, Equals, t.expect, Commentf("%v", t))
 	}
 }
