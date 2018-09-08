761a3529d5eef0ed50d7ed60c8289e3226a363f9
Emmett <42971249+lanjingquan@users.noreply.github.com>
2018-09-08 08:32:24 +0800

    add some cases for NULL inputs and other conditions

--- expression/builtin_encryption_test.go
+++ expression/builtin_encryption_test.go
@@ -37,6 +37,13 @@ var cryptTests = []struct {
 	{"pingcap123", "123456789012345678901234", "7698723DC6DFE7724221"},
 	{"pingcap#%$%^", "*^%YTu1234567", "8634B9C55FF55E5B6328F449"},
 	{"pingcap", "", "4A77B524BD2C5C"},
+        {"分布式データベース","pass1234@#$%%^^&","80CADC8D328B3026D04FB285F36FED04BBCA0CC685BF78B1E687CE"},
+        {"分布式データベース","分布式7782734adgwy1242","0E24CFEF272EE32B6E0BFBDB89F29FB43B4B30DAA95C3F914444BC"},
+        {"pingcap","密匙","CE5C02A5010010"},
+        {"pingcap数据库","数据库passwd12345667","36D5F90D3834E30E396BE3226E3B4ED3"},
+        {7382.3456,"数据库passwd12345667","B6484047F783239225"},
+        {"数据库",nil,nil},
+        {nil,"数据库passwd12345667",nil},
 }
 
 func (s *testEvaluatorSuite) TestSQLDecode(c *C) {
