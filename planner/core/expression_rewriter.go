606d7061c0d2c64e5daa681cbaf19937236993b6
Kenan Yao <cauchy1992@gmail.com>
2018-09-13 12:31:48 +0800

    for uncorrelated subquery, evaluate not

--- planner/core/expression_rewriter.go
+++ planner/core/expression_rewriter.go
@@ -562,7 +562,7 @@ func (er *expressionRewriter) handleExistSubquery(v *ast.ExistsSubqueryExpr) (as
 			er.err = errors.Trace(err)
 			return v, true
 		}
-		if len(rows) > 0 {
+		if (len(rows) > 0 && !v.Not) || (len(rows) == 0 && v.Not) {
 			er.ctxStack = append(er.ctxStack, expression.One.Clone())
 		} else {
 			er.ctxStack = append(er.ctxStack, expression.Zero.Clone())
