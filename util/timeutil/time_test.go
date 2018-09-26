0ee6935a7e25bef8483ccb843a8c197abb06e783
zhexuany <yangzhexuan@pingcap.com>
2018-09-26 15:36:11 +0800

    fix get tz at mojave

--- util/timeutil/time_test.go
+++ util/timeutil/time_test.go
@@ -34,6 +34,7 @@ type testTimeSuite struct{}
 
 func (s *testTimeSuite) TestgetTZNameFromFileName(c *C) {
 	tz, err := inferTZNameFromFileName("/user/share/zoneinfo/Asia/Shanghai")
+
 	c.Assert(err, IsNil)
 	c.Assert(tz, Equals, "Asia/Shanghai")
 }
