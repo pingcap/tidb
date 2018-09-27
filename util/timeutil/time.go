f3672ec3d20a0e9f65276957650496662edb0e7a
zhexuany <yangzhexuan@pingcap.com>
2018-09-28 00:03:53 +0800

    add system back

--- util/timeutil/time.go
+++ util/timeutil/time.go
@@ -30,7 +30,7 @@ import (
 func init() {
 	// We need set systemTZ when it is in testing process.
 	if systemTZ == "" {
-		systemTZ = "Asia/Shanghai"
+		systemTZ = "System"
 	}
 	locCa = &locCache{}
 	locCa.locMap = make(map[string]*time.Location)
