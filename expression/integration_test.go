c80f8a71619ce8f92a2025cdb2135a46cdbb9dde
robi <sulifx@gmail.com>
2018-09-27 18:57:47 +0800

    add testcase to insert groupconcat truncate

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -2951,6 +2951,7 @@ func (s *testIntegrationSuite) TestAggregationBuiltinGroupConcat(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
 	tk.MustExec("create table t(a varchar(100))")
+	tk.MustExec("create table d(a varchar(100))")
 	tk.MustExec("insert into t values('hello'), ('hello')")
 	result := tk.MustQuery("select group_concat(a) from t")
 	result.Check(testkit.Rows("hello,hello"))
@@ -2958,6 +2959,13 @@ func (s *testIntegrationSuite) TestAggregationBuiltinGroupConcat(c *C) {
 	tk.MustExec("set @@group_concat_max_len=7")
 	result = tk.MustQuery("select group_concat(a) from t")
 	result.Check(testkit.Rows("hello,h"))
+	tk.MustQuery("show warnings").Check(testutil.RowsWithSep("|", "Warning 1260 Some row was cut by GROUPCONCAT()"))
+
+	_, err := tk.Exec("insert into d select group_concat(a) from t")
+	c.Assert(errors.Cause(err).(*terror.Error).Code(), Equals, terror.ErrCode(mysql.ErrCutValueGroupConcat))
+
+	tk.Exec("set sql_mode=''")
+	tk.MustExec("insert into d select group_concat(a) from t")
 }
 
 func (s *testIntegrationSuite) TestOtherBuiltin(c *C) {
