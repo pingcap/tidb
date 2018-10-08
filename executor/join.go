a81254d5b24b71f7138fa60632eb8b75e5bfec4c
xuhuaiyu <391585975@qq.com>
2018-10-08 20:45:39 +0800

    address comment

--- executor/join.go
+++ executor/join.go
@@ -73,12 +73,6 @@ type HashJoinExec struct {
 
 	memTracker *memory.Tracker // track memory usage.
 
-	// useRadixPartition indicates whether to use radix-based cluster algorithm
-	// to partition.
-	// Note: Only used for testing now, we'll check whether to use radix
-	// partition according to the the skew of inner data when radix partition is
-	// fully supported.
-	UseRadixPartition bool
 	// radixBits indicates the bit number using for radix partitioning. Inner
 	// relation will be split to 2^radixBits sub-relations before building the
 	// hash tables.
