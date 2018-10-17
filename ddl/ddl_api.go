4b4c443d14f47fabb0b7512f11663cd5ce131e45
crazycs <crazycs520@gmail.com>
2018-10-17 20:21:10 +0800

    ddl: add check when create table with foreign key. (#7885)
    * ddl: add check when create table with foreign key

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -727,6 +727,9 @@ func buildTableInfo(ctx sessionctx.Context, d *ddl, tableName model.CIStr, cols 
 			fk.RefTable = constr.Refer.Table.Name
 			fk.State = model.StatePublic
 			for _, key := range constr.Keys {
+				if table.FindCol(cols, key.Column.Name.O) == nil {
+					return nil, errKeyColumnDoesNotExits.GenWithStackByArgs(key.Column.Name)
+				}
 				fk.Cols = append(fk.Cols, key.Column.Name)
 			}
 			for _, key := range constr.Refer.IndexColNames {
@@ -749,7 +752,7 @@ func buildTableInfo(ctx sessionctx.Context, d *ddl, tableName model.CIStr, cols 
 			for _, key := range constr.Keys {
 				col = table.FindCol(cols, key.Column.Name.O)
 				if col == nil {
-					return nil, errKeyColumnDoesNotExits.GenWithStack("key column %s doesn't exist in table", key.Column.Name)
+					return nil, errKeyColumnDoesNotExits.GenWithStackByArgs(key.Column.Name)
 				}
 				// Virtual columns cannot be used in primary key.
 				if col.IsGenerated() && !col.GeneratedStored {
