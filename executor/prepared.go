e7b9c489bf30188090894a165dbcddac72e61e2f
Jun-Seok Heo <jun.seok.heo@samsung.com>
2018-10-19 14:13:31 +0900

    planner: fix the potential panic due to the PR #7684 when using a prepared plan cache

--- executor/prepared.go
+++ executor/prepared.go
@@ -159,6 +159,7 @@ func (e *PrepareExec) Next(ctx context.Context, chk *chunk.Chunk) error {
 		prepared.Params[i].SetDatum(types.NewIntDatum(0))
 	}
 	var p plannercore.Plan
+	vars.StmtCtx.UseCache = false
 	p, err = plannercore.BuildLogicalPlan(e.ctx, stmt, e.is)
 	if err != nil {
 		return errors.Trace(err)
