5abb424cf7c42e34a404454d661ed052de122dc7
tiancaiamao <tiancaiamao@gmail.com>
2018-10-21 13:21:26 +0800

    *: move `Statement` and `RecordSet` from ast to sqlexec package (#7970)

--- statistics/ddl.go
+++ statistics/ddl.go
@@ -16,7 +16,6 @@ package statistics
 import (
 	"fmt"
 
-	"github.com/pingcap/tidb/ast"
 	"github.com/pingcap/tidb/ddl/util"
 	"github.com/pingcap/tidb/model"
 	"github.com/pingcap/tidb/mysql"
@@ -119,7 +118,7 @@ func (h *Handle) insertColStats2KV(physicalID int64, colInfo *model.ColumnInfo) 
 	// If we didn't update anything by last SQL, it means the stats of this table does not exist.
 	if h.mu.ctx.GetSessionVars().StmtCtx.AffectedRows() > 0 {
 		// By this step we can get the count of this table, then we can sure the count and repeats of bucket.
-		var rs []ast.RecordSet
+		var rs []sqlexec.RecordSet
 		rs, err = exec.Execute(ctx, fmt.Sprintf("select count from mysql.stats_meta where table_id = %d", physicalID))
 		if len(rs) > 0 {
 			defer terror.Call(rs[0].Close)
