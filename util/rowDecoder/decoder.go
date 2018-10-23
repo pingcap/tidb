a221ae0c44c12f5f2f3dca9b2d658ae5786c20b6
crazycs <crazycs520@gmail.com>
2018-10-23 17:55:58 +0800

    admin: refine admin check decoder (#7862)

--- util/rowDecoder/decoder.go
+++ util/rowDecoder/decoder.go
@@ -96,9 +96,9 @@ func (rd RowDecoder) DecodeAndEvalRowWithMap(ctx sessionctx.Context, b []byte, d
 			return nil, errors.Trace(err)
 		}
 
-		if val.Kind() == types.KindMysqlTime {
+		if val.Kind() == types.KindMysqlTime && sysLoc != time.UTC {
 			t := val.GetMysqlTime()
-			if t.Type == mysql.TypeTimestamp && sysLoc != time.UTC {
+			if t.Type == mysql.TypeTimestamp {
 				err := t.ConvertTimeZone(sysLoc, time.UTC)
 				if err != nil {
 					return nil, errors.Trace(err)
