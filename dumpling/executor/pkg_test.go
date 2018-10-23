a4f8b7e67133086db2991d7fb4c3af58d6397e9a
parastorli <43286488+parastorli@users.noreply.github.com>
2018-10-23 14:28:45 +0800

    [dumpling] executor: let information_schema be the first database in ShowDatabases (#7938)

--- dumpling/executor/pkg_test.go
+++ dumpling/executor/pkg_test.go
@@ -110,3 +110,33 @@ func (s *pkgTestSuite) TestNestedLoopApply(c *C) {
 		}
 	}
 }
+
+func (s *pkgTestSuite) TestMoveInfoSchemaToFront(c *C) {
+	dbss := [][]string{
+		{},
+		{"A", "B", "C", "a", "b", "c"},
+		{"A", "B", "C", "INFORMATION_SCHEMA"},
+		{"A", "B", "INFORMATION_SCHEMA", "a"},
+		{"INFORMATION_SCHEMA"},
+		{"A", "B", "C", "INFORMATION_SCHEMA", "a", "b"},
+	}
+	wanted := [][]string{
+		{},
+		{"A", "B", "C", "a", "b", "c"},
+		{"INFORMATION_SCHEMA", "A", "B", "C"},
+		{"INFORMATION_SCHEMA", "A", "B", "a"},
+		{"INFORMATION_SCHEMA"},
+		{"INFORMATION_SCHEMA", "A", "B", "C", "a", "b"},
+	}
+
+	for _, dbs := range dbss {
+		moveInfoSchemaToFront(dbs)
+	}
+
+	for i, dbs := range wanted {
+		c.Check(len(dbss[i]), Equals, len(dbs))
+		for j, db := range dbs {
+			c.Check(dbss[i][j], Equals, db)
+		}
+	}
+}
