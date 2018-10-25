c39ae46b591238dd3b8e2761637add5888277f61
yu34po <yu34po123@163.com>
2018-10-25 15:28:40 +0800

    change extrahandler columns check to schema's columns

--- planner/core/logical_plans.go
+++ planner/core/logical_plans.go
@@ -343,16 +343,7 @@ func (ds *DataSource) deriveTablePathStats(path *accessPath) (bool, error) {
 	}
 	if pkCol == nil {
 		path.ranges = ranger.FullIntRange(false)
-		if len(ds.Columns) > 0 {
-			extraHandleCol := ds.Columns[len(ds.Columns)-1]
-			if extraHandleCol.ID == model.ExtraHandleID {
-				pkCol = expression.ColInfo2Col(ds.schema.Columns, extraHandleCol)
-			} else {
-				return false, nil
-			}
-		} else {
-			return false, nil
-		}
+		return false, nil
 	}
 
 	path.ranges = ranger.FullIntRange(mysql.HasUnsignedFlag(pkCol.RetType.Flag))
