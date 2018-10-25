6a348fdcddb7a1d7f545473bd8bb9dab04d66c10
Jun-Seok Heo <jun.seok.heo@samsung.com>
2018-10-25 14:53:58 +0900

    [dumpling] planner: fix a panic of a cached prepared statement with IndexScan (#8017)

--- dumpling/planner/core/prepare_test.go
+++ dumpling/planner/core/prepare_test.go
@@ -63,3 +63,29 @@ func (s *testPrepareSuite) TestPrepareCache(c *C) {
 	tk.MustQuery("execute stmt5").Check(testkit.Rows("1", "2", "2", "3", "4", "5"))
 	tk.MustQuery("execute stmt5").Check(testkit.Rows("1", "2", "2", "3", "4", "5"))
 }
+
+func (s *testPrepareSuite) TestPrepareCacheIndexScan(c *C) {
+	defer testleak.AfterTest(c)()
+	store, dom, err := newStoreWithBootstrap()
+	c.Assert(err, IsNil)
+	tk := testkit.NewTestKit(c, store)
+	orgEnable := core.PreparedPlanCacheEnabled()
+	orgCapacity := core.PreparedPlanCacheCapacity
+	defer func() {
+		dom.Close()
+		store.Close()
+		core.SetPreparedPlanCache(orgEnable)
+		core.PreparedPlanCacheCapacity = orgCapacity
+	}()
+	core.SetPreparedPlanCache(true)
+	core.PreparedPlanCacheCapacity = 100
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a int, b int, c int, primary key (a, b))")
+	tk.MustExec("insert into t values(1, 1, 2), (1, 2, 3), (1, 3, 3), (2, 1, 2), (2, 2, 3), (2, 3, 3)")
+	tk.MustExec(`prepare stmt1 from "select a, c from t where a = ? and c = ?"`)
+	tk.MustExec("set @a=1, @b=3")
+	// When executing one statement at the first time, we don't use cache, so we need to execute it at least twice to test the cache.
+	tk.MustQuery("execute stmt1 using @a, @b").Check(testkit.Rows("1 3", "1 3"))
+	tk.MustQuery("execute stmt1 using @a, @b").Check(testkit.Rows("1 3", "1 3"))
+}
