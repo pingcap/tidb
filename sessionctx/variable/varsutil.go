e13c2d01386b8cfe985f3f32dd0dc298fd93104f
iamzhoug37 <2541781827@qq.com>
2018-10-29 19:23:15 +0800

    add blank

--- sessionctx/variable/varsutil.go
+++ sessionctx/variable/varsutil.go
@@ -244,7 +244,7 @@ func ValidateSetSystemVar(vars *SessionVars, name string, value string) (string,
 		return checkUInt64SystemVar(name, value, 0, 2, vars)
 	case InnodbLockWaitTimeout:
 		return checkUInt64SystemVar(name, value, 1, 1073741824, vars)
-	//See "https://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_max_allowed_packet"
+	// See "https://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_max_allowed_packet"
 	case MaxAllowedPacket:
 		return checkUInt64SystemVar(name, value, 1024, 1073741824, vars)
 	case MaxConnections:
