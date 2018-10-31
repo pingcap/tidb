dcc5fc7b55d2b097f330ac54b464c5d823d0d4ae
Lynn <zimu_xia@126.com>
2018-10-31 15:42:41 +0800

    ddl, domain: make schema correct after canceling jobs (#7997) (#8057)

--- ddl/fail_db_test.go
+++ ddl/fail_db_test.go
@@ -15,14 +15,130 @@ package ddl_test
 
 import (
 	"fmt"
+	"time"
 
 	gofail "github.com/etcd-io/gofail/runtime"
 	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb/ddl"
 	"github.com/pingcap/tidb/domain"
+	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/model"
+	"github.com/pingcap/tidb/parser"
+	"github.com/pingcap/tidb/session"
+	"github.com/pingcap/tidb/store/mockstore"
 	"github.com/pingcap/tidb/util/testkit"
+	"github.com/pingcap/tidb/util/testleak"
 	"golang.org/x/net/context"
 )
+
+var _ = Suite(&testFailDBSuite{})
+
+type testFailDBSuite struct {
+	lease time.Duration
+	store kv.Storage
+	dom   *domain.Domain
+	se    session.Session
+	p     *parser.Parser
+}
+
+func (s *testFailDBSuite) SetUpSuite(c *C) {
+	testleak.BeforeTest()
+	s.lease = 200 * time.Millisecond
+	ddl.WaitTimeWhenErrorOccured = 1 * time.Microsecond
+	var err error
+	s.store, err = mockstore.NewMockTikvStore()
+	c.Assert(err, IsNil)
+	session.SetSchemaLease(s.lease)
+	s.dom, err = session.BootstrapSession(s.store)
+	c.Assert(err, IsNil)
+	s.se, err = session.CreateSession4Test(s.store)
+	c.Assert(err, IsNil)
+	s.p = parser.New()
+}
+
+func (s *testFailDBSuite) TearDownSuite(c *C) {
+	s.se.Execute(context.Background(), "drop database if exists test_db_state")
+	s.se.Close()
+	s.dom.Close()
+	s.store.Close()
+	testleak.AfterTest(c)()
+}
+
+// TestHalfwayCancelOperations tests the case that the schema is correct after the execution of operations are cancelled halfway.
+func (s *testFailDBSuite) TestHalfwayCancelOperations(c *C) {
+	gofail.Enable("github.com/pingcap/tidb/ddl/truncateTableErr", `return(true)`)
+	defer gofail.Disable("github.com/pingcap/tidb/ddl/truncateTableErr")
+
+	// test for truncating table
+	_, err := s.se.Execute(context.Background(), "create database cancel_job_db")
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "use cancel_job_db")
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "create table t(a int)")
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "insert into t values(1)")
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "truncate table t")
+	c.Assert(err, NotNil)
+	// Make sure that the table's data has not been deleted.
+	rs, err := s.se.Execute(context.Background(), "select count(*) from t")
+	c.Assert(err, IsNil)
+	chk := rs[0].NewChunk()
+	err = rs[0].Next(context.Background(), chk)
+	c.Assert(err, IsNil)
+	c.Assert(chk.NumRows() == 0, IsFalse)
+	row := chk.GetRow(0)
+	c.Assert(row.Len(), Equals, 1)
+	c.Assert(row.GetInt64(0), DeepEquals, int64(1))
+	c.Assert(rs[0].Close(), IsNil)
+	// Reload schema.
+	s.dom.ResetHandle(s.store)
+	err = s.dom.DDL().(ddl.DDLForTest).GetHook().OnChanged(nil)
+	c.Assert(err, IsNil)
+	s.se, err = session.CreateSession4Test(s.store)
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "use cancel_job_db")
+	c.Assert(err, IsNil)
+	// Test schema is correct.
+	_, err = s.se.Execute(context.Background(), "select * from t")
+	c.Assert(err, IsNil)
+
+	// test for renaming table
+	gofail.Enable("github.com/pingcap/tidb/ddl/errRenameTable", `return(true)`)
+	defer gofail.Disable("github.com/pingcap/tidb/ddl/errRenameTable")
+	_, err = s.se.Execute(context.Background(), "create table tx(a int)")
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "insert into tx values(1)")
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "rename table tx to ty")
+	c.Assert(err, NotNil)
+	// Make sure that the table's data has not been deleted.
+	rs, err = s.se.Execute(context.Background(), "select count(*) from tx")
+	c.Assert(err, IsNil)
+	chk = rs[0].NewChunk()
+	err = rs[0].Next(context.Background(), chk)
+	c.Assert(err, IsNil)
+	c.Assert(chk.NumRows() == 0, IsFalse)
+	row = chk.GetRow(0)
+	c.Assert(row.Len(), Equals, 1)
+	c.Assert(row.GetInt64(0), DeepEquals, int64(1))
+	c.Assert(rs[0].Close(), IsNil)
+	// Reload schema.
+	s.dom.ResetHandle(s.store)
+	err = s.dom.DDL().(ddl.DDLForTest).GetHook().OnChanged(nil)
+	c.Assert(err, IsNil)
+	s.se, err = session.CreateSession4Test(s.store)
+	c.Assert(err, IsNil)
+	_, err = s.se.Execute(context.Background(), "use cancel_job_db")
+	c.Assert(err, IsNil)
+	// Test schema is correct.
+	_, err = s.se.Execute(context.Background(), "select * from tx")
+	c.Assert(err, IsNil)
+
+	// clean up
+	_, err = s.se.Execute(context.Background(), "drop database cancel_job_db")
+	c.Assert(err, IsNil)
+}
 
 // TestInitializeOffsetAndState tests the case that the column's offset and state don't be initialized in the file of ddl_api.go when
 // doing the operation of 'modify column'.
