ec59d18b267b7bf69715597f6664fc3a7c1c7808
HuaiyuXu <391585975@qq.com>
2018-11-02 13:42:12 +0800

    planner: refine PhysicalProjection.ResolveIndices during neighbouring Proj (#8073)

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -1083,6 +1083,24 @@ func (s *testSuite) TestUnion(c *C) {
 	tk.MustQuery("select a, b from (select a, 0 as d, b from t union all select a, 0 as d, b from t) test;").Check(testkit.Rows("1 2", "1 2"))
 }
 
+func (s *testSuite) TestNeighbouringProj(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+
+	tk.MustExec("drop table if exists t1, t2")
+	tk.MustExec("create table t1(a int, b int)")
+	tk.MustExec("create table t2(a int, b int)")
+	tk.MustExec("insert into t1 value(1, 1), (2, 2)")
+	tk.MustExec("insert into t2 value(1, 1), (2, 2)")
+	tk.MustQuery("select sum(c) from (select t1.a as a, t1.a as c, length(t1.b) from t1  union select a, b, b from t2) t;").Check(testkit.Rows("5"))
+
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a bigint, b bigint, c bigint);")
+	tk.MustExec("insert into t values(1, 1, 1), (2, 2, 2), (3, 3, 3);")
+	rs := tk.MustQuery("select cast(count(a) as signed), a as another, a from t group by a order by cast(count(a) as signed), a limit 10;")
+	rs.Check(testkit.Rows("1 1 1", "1 2 2", "1 3 3"))
+}
+
 func (s *testSuite) TestIn(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
