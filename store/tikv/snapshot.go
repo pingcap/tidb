d6ee0908b02ce261bcbd468900dcee904d1eba4a
MyonKeminta <MyonKeminta@users.noreply.github.com>
2018-11-05 17:24:46 +0800

    Add end_key param to ticlient scanner
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- store/tikv/snapshot.go
+++ store/tikv/snapshot.go
@@ -280,7 +280,7 @@ func (s *tikvSnapshot) get(bo *Backoffer, k kv.Key) ([]byte, error) {
 
 // Seek return a list of key-value pair after `k`.
 func (s *tikvSnapshot) Seek(k kv.Key) (kv.Iterator, error) {
-	scanner, err := newScanner(s, k, scanBatchSize)
+	scanner, err := newScanner(s, k, nil, scanBatchSize)
 	return scanner, errors.Trace(err)
 }
 
