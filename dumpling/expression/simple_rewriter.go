f6d51154da7604f30c2ae6701a27c8a5ecdbbf46
Jun-Seok Heo <jun.seok.heo@samsung.com>
2018-11-05 15:24:48 +0900

    [dumpling] planner,executor,parser: support the prepared plan cache for insert/delete/update statements (#8107)

--- dumpling/expression/simple_rewriter.go
+++ dumpling/expression/simple_rewriter.go
@@ -150,9 +150,11 @@ func (sr *simpleRewriter) Leave(originInNode ast.Node) (retNode ast.Node, ok boo
 			sr.inToExpression(len(v.List), v.Not, &v.Type)
 		}
 	case *driver.ParamMarkerExpr:
-		tp := types.NewFieldType(mysql.TypeUnspecified)
-		types.DefaultParamTypeForValue(v.GetValue(), tp)
-		value := &Constant{Value: v.ValueExpr.Datum, RetType: tp}
+		var value Expression
+		value, sr.err = GetParamExpression(sr.ctx, v, sr.useCache())
+		if sr.err != nil {
+			return retNode, false
+		}
 		sr.push(value)
 	case *ast.RowExpr:
 		sr.rowToScalarFunc(v)
@@ -166,6 +168,10 @@ func (sr *simpleRewriter) Leave(originInNode ast.Node) (retNode ast.Node, ok boo
 		return retNode, false
 	}
 	return originInNode, true
+}
+
+func (sr *simpleRewriter) useCache() bool {
+	return sr.ctx.GetSessionVars().StmtCtx.UseCache
 }
 
 func (sr *simpleRewriter) binaryOpToExpression(v *ast.BinaryOperationExpr) {
