d9208338e40f8b835c9da1c0109ac096c8677c68
Jian Zhang <zjsariel@gmail.com>
2018-11-07 11:39:32 +0800

    refine implementation

--- util/chunk/pool.go
+++ util/chunk/pool.go
@@ -14,17 +14,15 @@
 package chunk
 
 import (
-	"container/list"
-	"math/rand"
 	"sync"
-	"time"
-	"unsafe"
 
 	"github.com/pingcap/tidb/types"
 )
 
 // Pool is the column pool.
 type Pool struct {
+	initCap int
+
 	varLenColPool   *colPool
 	fixLenColPool4  *colPool
 	fixLenColPool8  *colPool
@@ -33,35 +31,34 @@ type Pool struct {
 }
 
 // NewPool creates a new Pool.
-func NewPool(maxChunkSize int) *Pool {
-	numShards := 8
+func NewPool(initCap int) *Pool {
 	return &Pool{
-		varLenColPool:   newColPool(numShards, varElemLen),
-		fixLenColPool4:  newColPool(numShards, 4),
-		fixLenColPool8:  newColPool(numShards, 8),
-		fixLenColPool16: newColPool(numShards, 16),
-		fixLenColPool40: newColPool(numShards, 40),
+		initCap:         initCap,
+		varLenColPool:   newColPool(varElemLen, initCap),
+		fixLenColPool4:  newColPool(4, initCap),
+		fixLenColPool8:  newColPool(8, initCap),
+		fixLenColPool16: newColPool(16, initCap),
+		fixLenColPool40: newColPool(40, initCap),
 	}
 }
 
 // GetChunk gets a Chunk from the Pool.
-func (p *Pool) GetChunk(fields []*types.FieldType, cap int) *Chunk {
+func (p *Pool) GetChunk(fields []*types.FieldType) *Chunk {
 	chk := new(Chunk)
-	chk.columns = make([]*column, 0, len(fields))
-	chk.capacity = cap
-
-	for _, f := range fields {
+	chk.capacity = p.initCap
+	chk.columns = make([]*column, len(fields))
+	for i, f := range fields {
 		switch elemLen := getFixedLen(f); elemLen {
 		case varElemLen:
-			chk.columns = append(chk.columns, p.varLenColPool.get(cap))
+			chk.columns[i] = p.varLenColPool.Get().(*column)
 		case 4:
-			chk.columns = append(chk.columns, p.fixLenColPool4.get(cap))
+			chk.columns[i] = p.fixLenColPool4.Get().(*column)
 		case 8:
-			chk.columns = append(chk.columns, p.fixLenColPool8.get(cap))
+			chk.columns[i] = p.fixLenColPool8.Get().(*column)
 		case 16:
-			chk.columns = append(chk.columns, p.fixLenColPool16.get(cap))
+			chk.columns[i] = p.fixLenColPool16.Get().(*column)
 		case 40:
-			chk.columns = append(chk.columns, p.fixLenColPool40.get(cap))
+			chk.columns[i] = p.fixLenColPool40.Get().(*column)
 		}
 	}
 	return chk
@@ -72,92 +69,30 @@ func (p *Pool) PutChunk(fields []*types.FieldType, chk *Chunk) {
 	for i, f := range fields {
 		switch elemLen := getFixedLen(f); elemLen {
 		case varElemLen:
-			p.varLenColPool.put(chk.columns[i])
+			p.varLenColPool.Put(chk.columns[i])
 		case 4:
-			p.fixLenColPool4.put(chk.columns[i])
+			p.fixLenColPool4.Put(chk.columns[i])
 		case 8:
-			p.fixLenColPool8.put(chk.columns[i])
+			p.fixLenColPool8.Put(chk.columns[i])
 		case 16:
-			p.fixLenColPool16.put(chk.columns[i])
+			p.fixLenColPool16.Put(chk.columns[i])
 		case 40:
-			p.fixLenColPool40.put(chk.columns[i])
+			p.fixLenColPool40.Put(chk.columns[i])
 		}
 	}
+	chk.columns = nil // release the column references.
 }
 
 type colPool struct {
-	shards  []*colPoolShard
-	elemLen int
-	rander  *rand.Rand
+	sync.Pool
 }
 
-func newColPool(numShards int, elemLen int) *colPool {
-	cp := &colPool{
-		shards:  make([]*colPoolShard, numShards),
-		elemLen: elemLen,
-		rander:  rand.New(rand.NewSource(time.Now().Unix())),
+func newColPool(elemLen, initCap int) *colPool {
+	pool := &colPool{}
+	if elemLen != varElemLen {
+		pool.New = func() interface{} { return newFixedLenColumn(elemLen, initCap) }
+	} else {
+		pool.New = func() interface{} { return newVarLenColumn(initCap, nil) }
 	}
-	for i := 0; i < numShards; i++ {
-		cp.shards[i] = newColPoolShard()
-	}
-	return cp
-}
-
-func (cp *colPool) put(col *column) {
-	x := *(*uint64)(unsafe.Pointer(&col))
-	x = (x ^ (x >> 30)) * uint64(0xbf58476d1ce4e5b9)
-	x = (x ^ (x >> 27)) * uint64(0x94d049bb133111eb)
-	x = x ^ (x >> 31)
-	ordinal := x % uint64(len(cp.shards))
-	cp.shards[ordinal].put(col)
-}
-
-func (cp *colPool) get(cap int) *column {
-	ordinal := cp.rander.Int() % len(cp.shards)
-	col := cp.shards[ordinal].get()
-	if col != nil {
-		return col
-	}
-
-	if cp.elemLen == varElemLen {
-		return newVarLenColumn(cap, nil)
-	}
-	return newFixedLenColumn(cp.elemLen, cap)
-}
-
-func newColPoolShard() *colPoolShard {
-	return &colPoolShard{
-		cols:   list.New(),
-		exists: make(map[*column]struct{}),
-	}
-}
-
-type colPoolShard struct {
-	sync.Mutex
-	cols *list.List
-
-	exists map[*column]struct{}
-}
-
-func (ps *colPoolShard) put(col *column) {
-	ps.Lock()
-	defer ps.Unlock()
-
-	if _, ok := ps.exists[col]; !ok {
-		ps.cols.PushFront(col)
-		ps.exists[col] = struct{}{}
-	}
-}
-
-func (ps *colPoolShard) get() *column {
-	ps.Lock()
-	defer ps.Unlock()
-
-	if ps.cols.Len() > 0 {
-		head := ps.cols.Front()
-		col := ps.cols.Remove(head).(*column)
-		delete(ps.exists, col)
-		return col
-	}
-	return nil
+	return pool
 }
