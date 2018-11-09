5c1678dca1b7ddb50c54fa2b5b63b38482d3dcf6
winkyao <golangwink@gmail.com>
2018-11-09 11:45:15 +0800

    address comment

--- executor/show.go
+++ executor/show.go
@@ -618,7 +618,7 @@ func (e *ShowExec) fetchShowCreateTable() error {
 	buf.WriteString(") ENGINE=InnoDB")
 	charsetName := tb.Meta().Charset
 	if len(charsetName) == 0 {
-		charsetName = charset.CharsetUTF8MB4
+		charsetName = mysql.DefaultCharset
 	}
 	collate := tb.Meta().Collate
 	// Set default collate if collate is not specified.
