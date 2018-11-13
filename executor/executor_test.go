c6eec3070b794e4e4ef39fa7fe3a86a0b54a027c
Andrew <AndrewDi@users.noreply.github.com>
2018-11-13 17:03:34 +0800

    planner, executor: eliminate extra columns introduced by OrderBy upon Union (#8290)

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -1089,6 +1089,15 @@ func (s *testSuite) TestUnion(c *C) {
 	tk.MustExec("set @@tidb_max_chunk_size=2;")
 	tk.MustQuery("select count(*) from (select a as c, a as d from t1 union all select a, b from t1) t;").Check(testkit.Rows("12"))
 
+	// #issue 8189 and #issue 8199
+	tk.MustExec("drop table if exists t1")
+	tk.MustExec("drop table if exists t2")
+	tk.MustExec("CREATE TABLE t1 (a int not null, b char (10) not null)")
+	tk.MustExec("insert into t1 values(1,'a'),(2,'b'),(3,'c'),(3,'c')")
+	tk.MustExec("CREATE TABLE t2 (a int not null, b char (10) not null)")
+	tk.MustExec("insert into t2 values(1,'a'),(2,'b'),(3,'c'),(3,'c')")
+	tk.MustQuery("select a from t1 union select a from t1 order by (select a+1);").Check(testkit.Rows("1", "2", "3"))
+
 	// #issue 8201
 	for i := 0; i < 4; i++ {
 		tk.MustQuery("SELECT(SELECT 0 AS a FROM dual UNION SELECT 1 AS a FROM dual ORDER BY a ASC  LIMIT 1) AS dev").Check(testkit.Rows("0"))
