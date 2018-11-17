a929e182d51bc2b32e542ed7bad762963190c853
hao.hu <huhao0715@gmail.com>
2018-11-17 22:11:49 +0800

    support system variable wait_timeout. (#8245)

--- session/session.go
+++ session/session.go
@@ -1371,6 +1371,7 @@ const loadCommonGlobalVarsSQL = "select HIGH_PRIORITY * from mysql.global_variab
 	variable.MaxAllowedPacket + quoteCommaQuote +
 	variable.TimeZone + quoteCommaQuote +
 	variable.BlockEncryptionMode + quoteCommaQuote +
+	variable.WaitTimeout + quoteCommaQuote +
 	/* TiDB specific global variables: */
 	variable.TiDBSkipUTF8Check + quoteCommaQuote +
 	variable.TiDBIndexJoinBatchSize + quoteCommaQuote +
