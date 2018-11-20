a08c8a579ffd2cd245a74482adc6006c3d2f2839
lysu <sulifx@gmail.com>
2018-11-20 12:42:24 +0800

    [parser] parser: add driver import check & example (#44)

--- parser/yy_parser.go
+++ parser/yy_parser.go
@@ -76,6 +76,13 @@ type stmtTexter interface {
 
 // New returns a Parser object.
 func New() *Parser {
+	if ast.NewValueExpr == nil ||
+		ast.NewParamMarkerExpr == nil ||
+		ast.NewHexLiteral == nil ||
+		ast.NewBitLiteral == nil {
+		panic("no parser driver (forgotten import?) https://github.com/pingcap/parser/issues/43")
+	}
+
 	return &Parser{
 		cache: make([]yySymType, 200),
 	}
