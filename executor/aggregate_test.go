01ca08b8c2b36e35396daf60049d3b554a4d75e0
Morgan Tocker <tocker@gmail.com>
2018-11-21 20:35:59 -0700

    infoschema: Add missing i_s.files columns (#8387)

--- executor/aggregate_test.go
+++ executor/aggregate_test.go
@@ -240,7 +240,7 @@ func (s *testSuite) TestAggregation(c *C) {
 
 	result = tk.MustQuery("select count(*) from information_schema.columns")
 	// When adding new memory columns in information_schema, please update this variable.
-	columnCountOfAllInformationSchemaTables := "759"
+	columnCountOfAllInformationSchemaTables := "762"
 	result.Check(testkit.Rows(columnCountOfAllInformationSchemaTables))
 
 	tk.MustExec("drop table if exists t1")
