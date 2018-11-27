c96345877b76dd3c2975fa0e1d2abca16b7529be
Jun-Seok Heo <jun.seok.heo@samsung.com>
2018-11-27 09:54:07 +0900

    executor,planner: make `order by ?` performs correctly when prepare-cache is enabled

--- planner/core/expression_rewriter.go
+++ planner/core/expression_rewriter.go
@@ -756,7 +756,7 @@ func (er *expressionRewriter) Leave(originInNode ast.Node) (retNode ast.Node, ok
 		er.ctxStack = append(er.ctxStack, value)
 	case *driver.ParamMarkerExpr:
 		var value expression.Expression
-		value, er.err = expression.GetParamExpression(er.ctx, v, er.useCache())
+		value, er.err = expression.GetParamExpression(er.ctx, v)
 		if er.err != nil {
 			return retNode, false
 		}
@@ -941,10 +941,26 @@ func (er *expressionRewriter) isNullToExpression(v *ast.IsNullExpr) {
 }
 
 func (er *expressionRewriter) positionToScalarFunc(v *ast.PositionExpr) {
-	if v.N > 0 && v.N <= er.schema.Len() {
-		er.ctxStack = append(er.ctxStack, er.schema.Columns[v.N-1])
+	pos := v.N
+	str := strconv.Itoa(pos)
+	stkLen := len(er.ctxStack)
+	if v.P != nil {
+		val := er.ctxStack[stkLen-1]
+		intNum, isNull, err := expression.GetIntFromConstant(er.ctx, val)
+		er.ctxStack = er.ctxStack[:stkLen-1]
+		str = "?"
+		if err == nil {
+			if isNull {
+				return
+			}
+			pos = intNum
+		}
+		er.err = err
+	}
+	if er.err == nil && pos > 0 && pos <= er.schema.Len() {
+		er.ctxStack = append(er.ctxStack, er.schema.Columns[pos-1])
 	} else {
-		er.err = ErrUnknownColumn.GenWithStackByArgs(strconv.Itoa(v.N), clauseMsg[er.b.curClause])
+		er.err = ErrUnknownColumn.GenWithStackByArgs(str, clauseMsg[er.b.curClause])
 	}
 }
 
