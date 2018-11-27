f64d3791568fb693bcf3265eab1c5b0bb7270e44
caixiaojian <mc02cxj@gmail.com>
2018-11-27 13:21:28 +0800

    executor: fix show create table for hash partition

--- executor/show.go
+++ executor/show.go
@@ -644,33 +644,38 @@ func (e *ShowExec) fetchShowCreateTable() error {
 	partitionInfo := tb.Meta().Partition
 	if partitionInfo != nil {
 		// this if statement takes care of range columns case
-		if partitionInfo.Columns != nil && partitionInfo.Type == model.PartitionTypeRange {
-			buf.WriteString(fmt.Sprintf("\nPARTITION BY RANGE COLUMNS("))
-			for i, col := range partitionInfo.Columns {
-				buf.WriteString(col.L)
-				if i < len(partitionInfo.Columns)-1 {
-					buf.WriteString(",")
+		if partitionInfo.Type == model.PartitionTypeHash {
+			buf.WriteString(fmt.Sprintf("\nPARTITION BY HASH( %s )", partitionInfo.Expr))
+			buf.WriteString(fmt.Sprintf("\nPARTITIONS %d", partitionInfo.Num))
+		} else {
+			if partitionInfo.Columns != nil && partitionInfo.Type == model.PartitionTypeRange {
+				buf.WriteString(fmt.Sprintf("\nPARTITION BY RANGE COLUMNS("))
+				for i, col := range partitionInfo.Columns {
+					buf.WriteString(col.L)
+					if i < len(partitionInfo.Columns)-1 {
+						buf.WriteString(",")
+					}
+				}
+				buf.WriteString(") (\n")
+			} else {
+				buf.WriteString(fmt.Sprintf("\nPARTITION BY %s ( %s ) (\n", partitionInfo.Type.String(), partitionInfo.Expr))
+			}
+			for i, def := range partitionInfo.Definitions {
+				lessThans := ""
+				lessThans = strings.Join(def.LessThan, ",")
+
+				var parDef string
+				parDef = fmt.Sprintf("  PARTITION %s VALUES LESS THAN (%s)", def.Name, lessThans)
+				if i < len(partitionInfo.Definitions)-1 {
+					buf.WriteString(parDef)
+					buf.WriteString(",\n")
+				} else {
+					buf.WriteString(parDef)
+					buf.WriteString("\n")
 				}
 			}
-			buf.WriteString(") (\n")
-		} else {
-			buf.WriteString(fmt.Sprintf("\nPARTITION BY %s ( %s ) (\n", partitionInfo.Type.String(), partitionInfo.Expr))
+			buf.WriteString(")")
 		}
-		for i, def := range partitionInfo.Definitions {
-			lessThans := ""
-			lessThans = strings.Join(def.LessThan, ",")
-
-			var parDef string
-			parDef = fmt.Sprintf("  PARTITION %s VALUES LESS THAN (%s)", def.Name, lessThans)
-			if i < len(partitionInfo.Definitions)-1 {
-				buf.WriteString(parDef)
-				buf.WriteString(",\n")
-			} else {
-				buf.WriteString(parDef)
-				buf.WriteString("\n")
-			}
-		}
-		buf.WriteString(")")
 	}
 
 	if hasAutoIncID {
--- executor/show_test.go
+++ executor/show_test.go
@@ -18,7 +18,6 @@ import (
 	"strconv"
 	"time"
 
-	. "github.com/pingcap/check"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/parser/auth"
 	"github.com/pingcap/parser/model"
@@ -388,6 +387,15 @@ func (s *testSuite) TestShow(c *C) {
 			"  `c` char(1) DEFAULT NULL,\n"+
 			"  `d` int(11) DEFAULT NULL\n"+
 			") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin"+"\nPARTITION BY RANGE COLUMNS(a,d,c) (\n  PARTITION p0 VALUES LESS THAN (5,10,\"ggg\"),\n  PARTITION p1 VALUES LESS THAN (10,20,\"mmm\"),\n  PARTITION p2 VALUES LESS THAN (15,30,\"sss\"),\n  PARTITION p3 VALUES LESS THAN (50,MAXVALUE,MAXVALUE)\n)",
+	))
+
+	// Test hash partition
+	tk.MustExec(`drop table if exists t`)
+	tk.MustExec(`CREATE TABLE t (a int) PARTITION BY HASH(a) PARTITIONS 4`)
+	tk.MustQuery("show create table t").Check(testutil.RowsWithSep("|",
+		"t CREATE TABLE `t` (\n"+
+			"  `a` int(11) DEFAULT NULL\n"+
+			") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin"+"\nPARTITION BY HASH( `a` )\nPARTITIONS 4",
 	))
 
 	// Test show create table compression type.
