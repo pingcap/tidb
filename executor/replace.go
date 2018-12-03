b3df63be93b438faec778b3b4fb99ccf07b34a32
tiancaiamao <tiancaiamao@gmail.com>
2018-12-03 13:06:22 +0800

    address comment

--- executor/replace.go
+++ executor/replace.go
@@ -178,6 +178,10 @@ func (e *ReplaceExec) exec(ctx context.Context, newRows [][]types.Datum) error {
 
 // Next implements the Executor Next interface.
 func (e *ReplaceExec) Next(ctx context.Context, chk *chunk.Chunk) error {
+	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
+		span1 := span.Tracer().StartSpan("replace.Next", opentracing.ChildOf(span.Context()))
+		defer span1.Finish()
+	}
 	chk.Reset()
 	if len(e.children) > 0 && e.children[0] != nil {
 		return e.insertRowsFromSelect(ctx, e.exec)
