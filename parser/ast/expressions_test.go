8a205ccc0a68164d778bfcd1072444fd51a2d197
leoppro <zhaoyilin@pingcap.com>
2018-12-04 12:03:07 +0800

    [parser] *: support AST to SQL text (#56)
    * ast2sql demo
    
    * add SQLSentence
    
    * mock func Restore
    
    * change copyright
    
    * add comment
    
    * add test
    
    * change tidb dependentment
    
    * fix error comment
    
    * use strings.Builder
    
    * fix test
    
    * add error return value for Recoverable
    
    * add error return value for Recoverable
    
    * add error return value for Recoverable
    
    * impl Restore of UnaryOperationExpr
    
    * integrate utils
    
    * switch tidb dependency
    
    * add comment
    
    * add comment
    
    * update unit test
    
    * fix Restore of DatabaseOption
    
    * fix Restore of DatabaseOption
    
    * fix Restore of DatabaseOption
    
    * update unit test
    
    * use errorf

--- parser/ast/expressions_test.go
+++ parser/ast/expressions_test.go
@@ -14,7 +14,10 @@
 package ast_test
 
 import (
+	"strings"
+
 	. "github.com/pingcap/check"
+	"github.com/pingcap/parser"
 	. "github.com/pingcap/parser/ast"
 	_ "github.com/pingcap/tidb/types/parser_driver"
 )
@@ -99,5 +102,43 @@ func (tc *testExpressionsSuite) TestExpresionsVisitorCover(c *C) {
 		c.Check(ce.enterCnt, Equals, v.expectedEnterCnt)
 		c.Check(ce.leaveCnt, Equals, v.expectedLeaveCnt)
 		v.node.Accept(visitor1{})
+	}
+}
+
+type exprTestCase struct {
+	sourceSQL string
+	expectSQL string
+}
+
+func (tc *testExpressionsSuite) createTestCase4UnaryOperationExpr() []exprTestCase {
+	return []exprTestCase{
+		{"select ++1", "SELECT ++1"},
+		{"select --1", "SELECT --1"},
+		{"select -+1", "SELECT -+1"},
+		{"select -1", "SELECT -1"},
+	}
+}
+
+func (tc *testExpressionsSuite) TestExpresionsRestore(c *C) {
+	parser := parser.New()
+	var testNodes []exprTestCase
+	testNodes = append(testNodes, tc.createTestCase4UnaryOperationExpr()...)
+
+	for _, node := range testNodes {
+		stmt, err := parser.ParseOneStmt(node.sourceSQL, "", "")
+		comment := Commentf("source %#v", node)
+		c.Assert(err, IsNil, comment)
+		var sb strings.Builder
+		sb.WriteString("SELECT ")
+		err = stmt.(*SelectStmt).Fields.Fields[0].Expr.Restore(&sb)
+		c.Assert(err, IsNil, comment)
+		restoreSql := sb.String()
+		comment = Commentf("source %#v; restore %v", node, restoreSql)
+		c.Assert(restoreSql, Equals, node.expectSQL, comment)
+		stmt2, err := parser.ParseOneStmt(restoreSql, "", "")
+		c.Assert(err, IsNil, comment)
+		CleanNodeText(stmt)
+		CleanNodeText(stmt2)
+		c.Assert(stmt2, DeepEquals, stmt, comment)
 	}
 }
