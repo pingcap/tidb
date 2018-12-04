aa384ae55cf8a27aa22036ed71b87798f1aafee2
xuhuaiyu <391585975@qq.com>
2018-12-04 14:24:35 +0800

    address comment

--- expression/builtin_cast.go
+++ expression/builtin_cast.go
@@ -1174,7 +1174,7 @@ func (b *builtinCastStringAsTimeSig) evalTime(row chunk.Row) (res types.Time, is
 	}
 	sc := b.ctx.GetSessionVars().StmtCtx
 	res, err = types.ParseTime(sc, val, b.tp.Tp, b.tp.Decimal)
-	hasWarning := err != nil
+	hasErrOrWarning := err != nil
 	if err = handleInvalidTimeError(b.ctx, err); err != nil {
 		return res, true, err
 	}
@@ -1182,7 +1182,7 @@ func (b *builtinCastStringAsTimeSig) evalTime(row chunk.Row) (res types.Time, is
 		// Truncate hh:mm:ss part if the type is Date.
 		res.Time = types.FromDate(res.Time.Year(), res.Time.Month(), res.Time.Day(), 0, 0, 0, 0)
 	}
-	return res, hasWarning, err
+	return res, hasErrOrWarning, err
 }
 
 type builtinCastStringAsDurationSig struct {
