ac3556b6cd9e5ebd41e5a084eb71b71eb3324c0b
Kenan Yao <cauchy1992@gmail.com>
2018-12-05 20:48:52 +0800

    plan: implementation phase framework of cascades planner (#8449)

--- planner/cascades/optimize.go
+++ planner/cascades/optimize.go
@@ -15,9 +15,10 @@ package cascades
 
 import (
 	"container/list"
+	"math"
 
-	"github.com/pingcap/errors"
 	plannercore "github.com/pingcap/tidb/planner/core"
+	"github.com/pingcap/tidb/planner/property"
 	"github.com/pingcap/tidb/sessionctx"
 )
 
@@ -125,6 +126,110 @@ func findMoreEquiv(g *Group, elem *list.Element) (eraseCur bool, err error) {
 	return eraseCur, nil
 }
 
+// onPhaseImplementation starts implementating physical operators from given root Group.
 func onPhaseImplementation(sctx sessionctx.Context, g *Group) (plannercore.Plan, error) {
-	return nil, errors.New("the onPhaseImplementation() of the cascades planner is not implemented")
+	prop := &property.PhysicalProperty{
+		ExpectedCnt: math.MaxFloat64,
+	}
+	// TODO replace MaxFloat64 costLimit by variable from sctx, or other sources.
+	impl, err := implGroup(g, prop, math.MaxFloat64)
+	if err != nil {
+		return nil, err
+	}
+	if impl == nil {
+		return nil, plannercore.ErrInternal.GenWithStackByArgs("Can't find a proper physical plan for this query")
+	}
+	return impl.getPlan(), nil
+}
+
+// implGroup picks one implementation with lowest cost for a Group, which satisfies specified physical property.
+func implGroup(g *Group, reqPhysProp *property.PhysicalProperty, costLimit float64) (Implementation, error) {
+	groupImpl := g.getImpl(reqPhysProp)
+	if groupImpl != nil {
+		if groupImpl.getCost() <= costLimit {
+			return groupImpl, nil
+		}
+		return nil, nil
+	}
+	// Handle implementation rules for each equivalent GroupExpr.
+	var cumCost float64
+	var childCosts []float64
+	var childPlans []plannercore.PhysicalPlan
+	outCount := math.Min(g.prop.Stats.RowCount, reqPhysProp.ExpectedCnt)
+	for elem := g.equivalents.Front(); elem != nil; elem = elem.Next() {
+		curExpr := elem.Value.(*GroupExpr)
+		impls, err := implGroupExpr(curExpr, reqPhysProp)
+		if err != nil {
+			return nil, err
+		}
+		for _, impl := range impls {
+			cumCost = 0.0
+			childCosts = childCosts[:0]
+			childPlans = childPlans[:0]
+			for i, childGroup := range curExpr.children {
+				childImpl, err := implGroup(childGroup, impl.getPlan().GetChildReqProps(i), costLimit-cumCost)
+				if err != nil {
+					return nil, err
+				}
+				if childImpl == nil {
+					impl.setCost(math.MaxFloat64)
+					break
+				}
+				childCost := childImpl.getCost()
+				childCosts = append(childCosts, childCost)
+				cumCost += childCost
+				childPlans = append(childPlans, childImpl.getPlan())
+			}
+			if impl.getCost() == math.MaxFloat64 {
+				continue
+			}
+			cumCost = impl.calcCost(outCount, childCosts, curExpr.children...)
+			if cumCost > costLimit {
+				continue
+			}
+			if groupImpl == nil || groupImpl.getCost() > cumCost {
+				impl.getPlan().SetChildren(childPlans...)
+				groupImpl = impl
+				costLimit = cumCost
+			}
+		}
+	}
+	// Handle enforcer rules for required physical property.
+	for _, rule := range GetEnforcerRules(reqPhysProp) {
+		newReqPhysProp := rule.NewProperty(reqPhysProp)
+		enforceCost := rule.GetEnforceCost(outCount)
+		childImpl, err := implGroup(g, newReqPhysProp, costLimit-enforceCost)
+		if err != nil {
+			return nil, err
+		}
+		if childImpl == nil {
+			continue
+		}
+		impl := rule.OnEnforce(reqPhysProp, childImpl)
+		cumCost = enforceCost + childImpl.getCost()
+		impl.setCost(cumCost)
+		if groupImpl == nil || groupImpl.getCost() > cumCost {
+			groupImpl = impl
+			costLimit = cumCost
+		}
+	}
+	if groupImpl == nil || groupImpl.getCost() == math.MaxFloat64 {
+		return nil, nil
+	}
+	g.insertImpl(reqPhysProp, groupImpl)
+	return groupImpl, nil
+}
+
+func implGroupExpr(cur *GroupExpr, reqPhysProp *property.PhysicalProperty) (impls []Implementation, err error) {
+	for _, rule := range GetImplementationRules(cur.exprNode) {
+		if !rule.Match(cur, reqPhysProp) {
+			continue
+		}
+		impl, err := rule.OnImplement(cur, reqPhysProp)
+		if err != nil {
+			return nil, err
+		}
+		impls = append(impls, impl)
+	}
+	return impls, nil
 }
