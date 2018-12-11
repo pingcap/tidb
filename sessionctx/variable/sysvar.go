128e73686c6a3e5d2c83b5e65aad7776a656bb90
crazycs <crazycs520@gmail.com>
2018-12-11 19:39:50 +0800

    ddl: add ddl_reorg_batch_size variable to control ddl worker batch size and enlarge default batch size. (#8365) (#8614)

--- sessionctx/variable/sysvar.go
+++ sessionctx/variable/sysvar.go
@@ -665,6 +665,7 @@ var defaultSysVars = []*SysVar{
 	{ScopeSession, TiDBQueryLogMaxLen, strconv.Itoa(logutil.DefaultQueryLogMaxLen)},
 	{ScopeSession, TiDBConfig, ""},
 	{ScopeGlobal | ScopeSession, TiDBDDLReorgWorkerCount, strconv.Itoa(DefTiDBDDLReorgWorkerCount)},
+	{ScopeGlobal | ScopeSession, TiDBDDLReorgBatchSize, strconv.Itoa(DefTiDBDDLReorgBatchSize)},
 	{ScopeSession, TiDBDDLReorgPriority, "PRIORITY_LOW"},
 	{ScopeSession, TiDBForcePriority, mysql.Priority2Str[DefTiDBForcePriority]},
 }
