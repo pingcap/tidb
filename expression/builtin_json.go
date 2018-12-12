036ac546fbd7551c5cc358459a9171c7be8c78ae
xuhuaiyu <391585975@qq.com>
2018-12-12 14:27:23 +0800

    fix circil

--- expression/builtin_json.go
+++ expression/builtin_json.go
@@ -14,7 +14,7 @@
 package expression
 
 import (
-	"github.com/pingcap/errors"
+	"github.com/juju/errors"
 	"github.com/pingcap/tidb/ast"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/types"
