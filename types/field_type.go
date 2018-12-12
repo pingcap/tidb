ac918513c103b057c7537e048e5b99e22d071975
Jun-Seok Heo <jun.seok.heo@samsung.com>
2018-12-12 14:34:52 +0900

    clean up the codes

--- types/field_type.go
+++ types/field_type.go
@@ -127,10 +127,9 @@ func DefaultParamTypeForValue(value interface{}, tp *FieldType) {
 		tp.Flen = UnspecifiedLength
 		tp.Decimal = UnspecifiedLength
 	default:
-		flen := tp.Flen
 		DefaultTypeForValue(value, tp)
 		if hasVariantFieldLength(tp) {
-			tp.Flen = flen
+			tp.Flen = UnspecifiedLength
 		}
 		if tp.Tp == mysql.TypeUnspecified {
 			tp.Tp = mysql.TypeVarString
