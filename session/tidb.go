d964ae5be690b6aeb062ecdf17acce5cbec82019
tiancaiamao <tiancaiamao@gmail.com>
2018-12-14 01:53:22 +0800

    fix typo

--- session/tidb.go
+++ session/tidb.go
@@ -167,9 +167,9 @@ func runStmt(ctx context.Context, sctx sessionctx.Context, s sqlexec.Statement) 
 		}
 	}
 
-	// There are two known case the s.txn.fail is not nil:
+	// There are two known cases that the s.txn.fail is not nil:
 	// 1. active transaction fail, can't get start ts for example
-	// 2. transaction too large cause StmtCommit fail
+	// 2. transaction too large and StmtCommit fail
 	// On both cases, we can return error in advance.
 	if se.txn.fail != nil {
 		err = se.txn.fail
