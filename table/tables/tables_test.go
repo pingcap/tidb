ef95b166e169fe7785b28d5ecd9154d7a142e0ad
tiancaiamao <tiancaiamao@gmail.com>
2018-12-14 17:16:37 +0800

    session: add error log to catch bug stack in Commit

--- table/tables/tables_test.go
+++ table/tables/tables_test.go
@@ -284,6 +284,7 @@ func (ts *testSuite) TestUnsignedPK(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(len(row), Equals, 2)
 	c.Assert(row[0].Kind(), Equals, types.KindUint64)
+	ts.se.StmtCommit()
 	c.Assert(sctx.Txn(true).Commit(context.Background()), IsNil)
 }
 
