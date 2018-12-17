3dcd1f10098fe692d37419ed24614e11cbbec9f3
crazycs <crazycs520@gmail.com>
2018-12-17 15:51:08 +0800

    server: close all connection directly when terminate tidb (#8692)

--- server/conn.go
+++ server/conn.go
@@ -153,6 +153,10 @@ func (cc *clientConn) Close() error {
 	delete(cc.server.clients, cc.connectionID)
 	connections := len(cc.server.clients)
 	cc.server.rwlock.Unlock()
+	return closeConn(cc, connections)
+}
+
+func closeConn(cc *clientConn, connections int) error {
 	metrics.ConnGauge.Set(float64(connections))
 	err := cc.bufReadConn.Close()
 	terror.Log(errors.Trace(err))
@@ -160,6 +164,11 @@ func (cc *clientConn) Close() error {
 		return cc.ctx.Close()
 	}
 	return nil
+}
+
+func (cc *clientConn) closeWithoutLock() error {
+	delete(cc.server.clients, cc.connectionID)
+	return closeConn(cc, len(cc.server.clients))
 }
 
 // writeInitialHandshake sends server version, connection ID, server capability, collation, server status
