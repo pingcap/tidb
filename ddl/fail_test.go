b1d741867d964c24060f991c28b4059cf338842c
tiancaiamao <tiancaiamao@gmail.com>
2018-12-19 16:00:20 +0800

    *: replace github.com/etcd-io/gofail with the forked one (#8693)
    With the adoption of Go module, GOPATH is not need any more
    But github.com/etcd-io/gofail doesn't support packages outside the GOPATH
    To overcome the drawback, this commit replace it with our forked one

--- ddl/fail_test.go
+++ ddl/fail_test.go
@@ -14,8 +14,8 @@
 package ddl
 
 import (
-	gofail "github.com/etcd-io/gofail/runtime"
 	. "github.com/pingcap/check"
+	gofail "github.com/pingcap/gofail/runtime"
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/tidb/types"
