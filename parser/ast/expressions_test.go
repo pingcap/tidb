305068c56e20c400b66e00dec6e86e67b0dd3a0c
exialin <exialin37@gmail.com>
2018-12-24 11:34:58 +0800

    [parser] opcode: fix literal string of operator `EQ` (#106)

--- parser/ast/expressions_test.go
+++ parser/ast/expressions_test.go
@@ -192,6 +192,7 @@ func (tc *testExpressionsSuite) TestBinaryOperationExpr(c *C) {
 		{"3+5", "3+5"},
 		{"3-5", "3-5"},
 		{"a<>5", "`a`!=5"},
+		{"a=1", "`a`=1"},
 	}
 	extractNodeFunc := func(node Node) Node {
 		return node.(*SelectStmt).Fields.Fields[0].Expr
