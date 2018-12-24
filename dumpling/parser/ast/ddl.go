8941b6dad0d6ce81314c03fb633ec06ffbafc62d
chendapao <feloxx@163.com>
2018-12-24 12:07:09 +0800

    [dumpling] [parser] parser: implement Restore for OnDeleteOpt And OnUpdateOpt (#98)

--- dumpling/parser/ast/ddl.go
+++ dumpling/parser/ast/ddl.go
@@ -257,7 +257,11 @@ type OnDeleteOpt struct {
 
 // Restore implements Node interface.
 func (n *OnDeleteOpt) Restore(ctx *RestoreCtx) error {
-	return errors.New("Not implemented")
+	if n.ReferOpt != ReferOptionNoOption {
+		ctx.WriteKeyWord("ON DELETE ")
+		ctx.WriteKeyWord(n.ReferOpt.String())
+	}
+	return nil
 }
 
 // Accept implements Node Accept interface.
@@ -278,7 +282,11 @@ type OnUpdateOpt struct {
 
 // Restore implements Node interface.
 func (n *OnUpdateOpt) Restore(ctx *RestoreCtx) error {
-	return errors.New("Not implemented")
+	if n.ReferOpt != ReferOptionNoOption {
+		ctx.WriteKeyWord("ON UPDATE ")
+		ctx.WriteKeyWord(n.ReferOpt.String())
+	}
+	return nil
 }
 
 // Accept implements Node Accept interface.
