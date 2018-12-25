838d91b2e8120a4b529f8f348ad413d2726dd02d
imtbkcat <songlingyu@pingcap.com>
2018-12-25 12:34:04 +0800

    use testErrCode to test

--- ddl/db_integration_test.go
+++ ddl/db_integration_test.go
@@ -118,17 +118,11 @@ func (s *testIntegrationSuite) TestNoZeroDateMode(c *C) {
 	tk.MustExec("use test;")
 	tk.MustExec("set session sql_mode='STRICT_TRANS_TABLES,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION';")
 
-	_, err := tk.Exec("create table test_zero_date(agent_start_time date NOT NULL DEFAULT '0000-00-00')")
-	c.Assert(err, NotNil)
-	c.Assert(terror.ErrorEqual(err, ddl.ErrInvalidDefaultValue), IsTrue, Commentf("err %v", err))
+	s.testErrorCode(c, tk, "create table test_zero_date(agent_start_time date NOT NULL DEFAULT '0000-00-00')", mysql.ErrInvalidDefault)
+	s.testErrorCode(c, tk, "create table test_zero_date(agent_start_time datetime NOT NULL DEFAULT '0000-00-00 00:00:00')", mysql.ErrInvalidDefault)
+	s.testErrorCode(c, tk, "create table test_zero_date(agent_start_time timestamp NOT NULL DEFAULT '0000-00-00 00:00:00')", mysql.ErrInvalidDefault)
 
-	_, err = tk.Exec("create table test_zero_date(agent_start_time datetime NOT NULL DEFAULT '0000-00-00 00:00:00')")
-	c.Assert(err, NotNil)
-	c.Assert(terror.ErrorEqual(err, ddl.ErrInvalidDefaultValue), IsTrue, Commentf("err %v", err))
-
-	_, err = tk.Exec("create table test_zero_date(agent_start_time timestamp NOT NULL DEFAULT '0000-00-00 00:00:00')")
-	c.Assert(err, NotNil)
-	c.Assert(terror.ErrorEqual(err, ddl.ErrInvalidDefaultValue), IsTrue, Commentf("err %v", err))
+	tk.MustExec("set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
 }
 
 // for issue #3848
