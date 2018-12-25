797ada227af28624d2e347bc6c96b220ad7384e2
qw4990 <qw4990@163.com>
2018-12-25 21:21:12 +0800

    planner, executor: add post-process after physical plan optimazation and move buildProjBelowAgg from executor to planner

--- planner/core/optimizer.go
+++ planner/core/optimizer.go
@@ -14,15 +14,19 @@
 package core
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/parser/ast"
+	"github.com/pingcap/parser/model"
 	"github.com/pingcap/tidb/expression"
+	"github.com/pingcap/tidb/expression/aggregation"
 	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/planner/property"
 	"github.com/pingcap/tidb/privilege"
 	"github.com/pingcap/tidb/sessionctx"
+	"github.com/pingcap/tidb/types"
 )
 
 // OptimizeAstNode optimizes the query to a physical plan directly.
@@ -105,8 +109,163 @@ func DoOptimize(flag uint64, logic LogicalPlan) (PhysicalPlan, error) {
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	finalPlan := eliminatePhysicalProjection(physical)
+	finalPlan, err := postOptimize(physical)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
 	return finalPlan, nil
+}
+
+func postOptimize(plan PhysicalPlan) (PhysicalPlan, error) {
+	plan = eliminatePhysicalProjection(plan)
+
+	// build projection below aggregation
+	plan = buildProjBelowAgg(plan)
+	return plan, nil
+}
+
+// buildProjBelowAgg builds a ProjOperator below AggOperator.
+// If all the args of `aggFuncs`, and all the item of `groupByItems`
+// are columns or constants, we do not need to build the `proj`.
+func buildProjBelowAgg(plan PhysicalPlan) PhysicalPlan {
+	for i, child := range plan.Children() {
+		plan.Children()[i] = buildProjBelowAgg(child)
+	}
+
+	var aggFuncs []*aggregation.AggFuncDesc
+	var groupByItems []expression.Expression
+	if aggHash, ok := plan.(*PhysicalHashAgg); ok {
+		aggFuncs = aggHash.AggFuncs
+		groupByItems = aggHash.GroupByItems
+	} else if aggStream, ok := plan.(*PhysicalStreamAgg); ok {
+		aggFuncs = aggStream.AggFuncs
+		groupByItems = aggStream.GroupByItems
+	} else {
+		return plan
+	}
+
+	return doBuildProjBelowAgg(plan, aggFuncs, groupByItems)
+}
+
+func doBuildProjBelowAgg(aggPlan PhysicalPlan, aggFuncs []*aggregation.AggFuncDesc, groupByItems []expression.Expression) PhysicalPlan {
+	hasScalarFunc := false
+
+	// If the mode is FinalMode, we do not need to wrap cast upon the args,
+	// since the types of the args are already the expected.
+	if len(aggFuncs) > 0 && aggFuncs[0].Mode != aggregation.FinalMode {
+		wrapCastForAggArgs(aggPlan.context(), aggFuncs)
+	}
+
+	for i := 0; !hasScalarFunc && i < len(aggFuncs); i++ {
+		f := aggFuncs[i]
+		for _, arg := range f.Args {
+			_, isScalarFunc := arg.(*expression.ScalarFunction)
+			hasScalarFunc = hasScalarFunc || isScalarFunc
+		}
+	}
+	for i, isScalarFunc := 0, false; !hasScalarFunc && i < len(groupByItems); i++ {
+		_, isScalarFunc = groupByItems[i].(*expression.ScalarFunction)
+		hasScalarFunc = hasScalarFunc || isScalarFunc
+	}
+	if !hasScalarFunc {
+		return aggPlan
+	}
+
+	projSchemaCols := make([]*expression.Column, 0, len(aggFuncs)+len(groupByItems))
+	projExprs := make([]expression.Expression, 0, cap(projSchemaCols))
+	cursor := 0
+
+	for _, f := range aggFuncs {
+		for i, arg := range f.Args {
+			if _, isCnst := arg.(*expression.Constant); isCnst {
+				continue
+			}
+			projExprs = append(projExprs, arg)
+			newArg := &expression.Column{
+				RetType: arg.GetType(),
+				ColName: model.NewCIStr(fmt.Sprintf("%s_%d", f.Name, i)),
+				Index:   cursor,
+			}
+			projSchemaCols = append(projSchemaCols, newArg)
+			f.Args[i] = newArg
+			cursor++
+		}
+	}
+
+	for i, item := range groupByItems {
+		if _, isCnst := item.(*expression.Constant); isCnst {
+			continue
+		}
+		projExprs = append(projExprs, item)
+		newArg := &expression.Column{
+			RetType: item.GetType(),
+			ColName: model.NewCIStr(fmt.Sprintf("group_%d", i)),
+			Index:   cursor,
+		}
+		projSchemaCols = append(projSchemaCols, newArg)
+		groupByItems[i] = newArg
+		cursor++
+	}
+
+	child := aggPlan.Children()[0]
+	prop := aggPlan.GetChildReqProps(0).Clone()
+	proj := PhysicalProjection{
+		Exprs:                projExprs,
+		AvoidColumnEvaluator: false,
+	}.Init(aggPlan.context(), child.statsInfo().ScaleByExpectCnt(prop.ExpectedCnt), prop)
+	proj.SetSchema(expression.NewSchema(projSchemaCols...))
+	proj.SetChildren(child)
+
+	aggPlan.SetChildren(proj)
+	return aggPlan
+}
+
+// wrapCastForAggArgs wraps the args of an aggregate function with a cast function.
+func wrapCastForAggArgs(ctx sessionctx.Context, funcs []*aggregation.AggFuncDesc) {
+	for _, f := range funcs {
+		// We do not need to wrap cast upon these functions,
+		// since the EvalXXX method called by the arg is determined by the corresponding arg type.
+		if f.Name == ast.AggFuncCount || f.Name == ast.AggFuncMin || f.Name == ast.AggFuncMax || f.Name == ast.AggFuncFirstRow {
+			continue
+		}
+		var castFunc func(ctx sessionctx.Context, expr expression.Expression) expression.Expression
+		switch retTp := f.RetTp; retTp.EvalType() {
+		case types.ETInt:
+			castFunc = expression.WrapWithCastAsInt
+		case types.ETReal:
+			castFunc = expression.WrapWithCastAsReal
+		case types.ETString:
+			castFunc = expression.WrapWithCastAsString
+		case types.ETDecimal:
+			castFunc = expression.WrapWithCastAsDecimal
+		default:
+			panic("should never happen in executorBuilder.wrapCastForAggArgs")
+		}
+		for i := range f.Args {
+			f.Args[i] = castFunc(ctx, f.Args[i])
+			if f.Name != ast.AggFuncAvg && f.Name != ast.AggFuncSum {
+				continue
+			}
+			// After wrapping cast on the argument, flen etc. may not the same
+			// as the type of the aggregation function. The following part set
+			// the type of the argument exactly as the type of the aggregation
+			// function.
+			// Note: If the `Tp` of argument is the same as the `Tp` of the
+			// aggregation function, it will not wrap cast function on it
+			// internally. The reason of the special handling for `Column` is
+			// that the `RetType` of `Column` refers to the `infoschema`, so we
+			// need to set a new variable for it to avoid modifying the
+			// definition in `infoschema`.
+			if col, ok := f.Args[i].(*expression.Column); ok {
+				col.RetType = types.NewFieldType(col.RetType.Tp)
+			}
+			// originTp is used when the the `Tp` of column is TypeFloat32 while
+			// the type of the aggregation function is TypeFloat64.
+			originTp := f.Args[i].GetType().Tp
+			*(f.Args[i].GetType()) = *(f.RetTp)
+			f.Args[i].GetType().Tp = originTp
+		}
+	}
 }
 
 func logicalOptimize(flag uint64, logic LogicalPlan) (LogicalPlan, error) {
