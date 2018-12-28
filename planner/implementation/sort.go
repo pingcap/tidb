1a2a072b910b1e4a6626baeaf75c6c50b8cdf72a
Kenan Yao <cauchy1992@gmail.com>
2018-12-28 15:45:36 +0800

    plan/cascades: generate logical property for Group (#8833)
    Scheme info is generated when converting the initial logical
    plan tree to Group, so in exploration phase, we need to fill
    in the schema of newly built Group; stats info is generated in
    lazy style when it is needed during implementation phase.

--- planner/implementation/sort.go
+++ planner/implementation/sort.go
@@ -32,7 +32,7 @@ func NewSortImpl(sort *plannercore.PhysicalSort) *SortImpl {
 
 // CalcCost calculates the cost of the sort Implementation.
 func (impl *SortImpl) CalcCost(outCount float64, childCosts []float64, children ...*memo.Group) float64 {
-	cnt := math.Min(children[0].LogicalProperty.Stats.RowCount, impl.plan.GetChildReqProps(0).ExpectedCnt)
+	cnt := math.Min(children[0].Prop.Stats.RowCount, impl.plan.GetChildReqProps(0).ExpectedCnt)
 	sort := impl.plan.(*plannercore.PhysicalSort)
 	impl.cost = sort.GetCost(cnt) + childCosts[0]
 	return impl.cost
