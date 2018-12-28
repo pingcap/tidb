f65d3e5214061970eb6654615dc3f9db6aa5992b
qw4990 <qw4990@163.com>
2018-12-28 14:30:31 +0800

    planner, executor: add post-process after physical plan optimazation and move buildProjBelowAgg from executor to planner (#8828)

--- planner/core/optimizer.go
+++ planner/core/optimizer.go
@@ -109,19 +109,16 @@ func DoOptimize(flag uint64, logic LogicalPlan) (PhysicalPlan, error) {
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	finalPlan, err := postOptimize(physical)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
+	finalPlan := postOptimize(physical)
 	return finalPlan, nil
 }
 
-func postOptimize(plan PhysicalPlan) (PhysicalPlan, error) {
+func postOptimize(plan PhysicalPlan) PhysicalPlan {
 	plan = eliminatePhysicalProjection(plan)
 
 	// build projection below aggregation
 	plan = buildProjBelowAgg(plan)
-	return plan, nil
+	return plan
 }
 
 // buildProjBelowAgg builds a ProjOperator below AggOperator.
