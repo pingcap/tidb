9fedc6f929837f02a227a92c891645dc22c041f5
qw4990 <qw4990@163.com>
2019-01-02 15:00:13 +0800

    planner, executor: add post-process after physical plan optimazation and move buildProjBelowAgg from executor to planner (#8828)

--- planner/core/rule_build_proj_below_agg.go
+++ planner/core/rule_build_proj_below_agg.go
@@ -1,4 +1,4 @@
-// Copyright 2015 PingCAP, Inc.
+// Copyright 2018 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@ package core
 
 import (
 	"fmt"
+
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/tidb/expression"
