63070fcc1424d8bf8e700a033559cc7029efa7ac
tiancaiamao <tiancaiamao@gmail.com>
2019-01-04 16:41:47 +0800

    session: add a `doNotCommit` flag to transaction when `StmtCommit` fail (#8918) (#8924)

--- session/session_fail_test.go
+++ session/session_fail_test.go
@@ -20,15 +20,23 @@ import (
 )
 
 func (s *testSessionSuite) TestFailStatementCommit(c *C) {
-	defer gofail.Disable("github.com/pingcap/tidb/session/mockStmtCommitError")
 
 	tk := testkit.NewTestKitWithInit(c, s.store)
 	tk.MustExec("create table t (id int)")
 	tk.MustExec("begin")
 	tk.MustExec("insert into t values (1)")
+
 	gofail.Enable("github.com/pingcap/tidb/session/mockStmtCommitError", `return(true)`)
-	_, err := tk.Exec("insert into t values (2)")
+	_, err := tk.Exec("insert into t values (2),(3),(4),(5)")
 	c.Assert(err, NotNil)
-	tk.MustExec("commit")
-	tk.MustQuery(`select * from t`).Check(testkit.Rows("1"))
+
+	gofail.Disable("github.com/pingcap/tidb/session/mockStmtCommitError")
+
+	tk.MustQuery("select * from t").Check(testkit.Rows("1"))
+	tk.MustExec("insert into t values (3)")
+	tk.MustExec("insert into t values (4)")
+	_, err = tk.Exec("commit")
+	c.Assert(err, NotNil)
+
+	tk.MustQuery(`select * from t`).Check(testkit.Rows())
 }
