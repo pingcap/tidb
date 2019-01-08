fcf8d4d883e523c4cf0838cd64e5967af47d4aa6
Yiding Cui <winoros@gmail.com>
2019-01-08 19:32:26 +0800

    return error for `ResolveIndices`

--- planner/core/plan.go
+++ planner/core/plan.go
@@ -140,7 +140,7 @@ type PhysicalPlan interface {
 	SetChildren(...PhysicalPlan)
 
 	// ResolveIndices resolves the indices for columns. After doing this, the columns can evaluate the rows by their indices.
-	ResolveIndices()
+	ResolveIndices() error
 }
 
 type baseLogicalPlan struct {
