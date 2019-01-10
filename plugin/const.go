c9105ff5230e484d2711ec1aaa8c47a0c2872e00
lysu <sulifx@gmail.com>
2019-01-10 16:47:58 +0800

    plugin: plugin framework core

---
+++ plugin/const.go
@@ -0,0 +1,62 @@
+// Copyright 2018 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+// Kind presents the kind of plugin.
+type Kind uint8
+
+const (
+	// Audit indicates it is a Audit plugin.
+	Audit Kind = 1 + iota
+	// Authentication it plugin is a Authentication plugin.
+	Authentication
+)
+
+func (k Kind) String() (str string) {
+	switch k {
+	case Audit:
+		str = "Audit"
+	case Authentication:
+		str = "Authentication"
+	}
+	return
+}
+
+// State present the state of plugin.
+type State uint8
+
+const (
+	// Uninitialized indicates plugin is uninitialized.
+	Uninitialized State = iota
+	// Ready indicates plugin is ready to work.
+	Ready
+	// Dying indicates plugin will be close soon.
+	Dying
+	// Disable indicate plugin is disabled.
+	Disable
+)
+
+func (s State) String() (str string) {
+	switch s {
+	case Uninitialized:
+		str = "Uninitialized"
+	case Ready:
+		str = "Ready"
+	case Dying:
+		str = "Dying"
+	case Disable:
+		str = "Disable"
+	}
+	return
+}
---
+++ plugin/spi_test.go
@@ -0,0 +1,50 @@
+// Copyright 2018 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/pingcap/tidb/plugin"
+)
+
+func TestExportManifest(t *testing.T) {
+	callRecorder := struct {
+		OnInitCalled      bool
+		NotifyEventCalled bool
+	}{}
+	manifest := &plugin.AuditManifest{
+		Manifest: plugin.Manifest{
+			Kind:    plugin.Authentication,
+			Name:    "test audit",
+			Version: 1,
+			OnInit: func(ctx context.Context, manifest *plugin.Manifest) error {
+				callRecorder.OnInitCalled = true
+				return nil
+			},
+		},
+		NotifyEvent: func(ctx context.Context) error {
+			callRecorder.NotifyEventCalled = true
+			return nil
+		},
+	}
+	exported := plugin.ExportManifest(manifest)
+	exported.OnInit(context.Background(), exported)
+	audit := plugin.DeclareAuditManifest(exported)
+	audit.NotifyEvent(context.Background())
+	if !callRecorder.NotifyEventCalled || !callRecorder.OnInitCalled {
+		t.Fatalf("export test failure")
+	}
+}
