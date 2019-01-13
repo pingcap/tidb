64aa696e7961d25b7e5b03eb88844841e777cc77
AndrewDi <pandipd@outlook.com>
2019-01-13 17:01:36 +0800

    fix sqlmode=nil

--- executor/show_test.go
+++ executor/show_test.go
@@ -306,6 +306,7 @@ func (s *testSuite2) TestShowCreateTable(c *C) {
 	tk.MustExec("use test")
 	tk.MustExec("drop table if exists t1")
 	tk.MustExec("create table t1(a int,b int)")
+	tk.MustExec("drop view if exists v1")
 	tk.MustExec("create or replace view v1 as select * from t1")
 	tk.MustQuery("show create table v1").Check(testutil.RowsWithSep("|", "v1|CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v1` (`a`, `b`) AS select * from t1"))
 
