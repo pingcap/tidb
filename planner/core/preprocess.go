131121f38f0e808e7290876b96e515f8538cc09f
ciscoxll <ciscocciexl@gmail.com>
2019-01-14 21:02:55 +0800

    Fix CI

--- planner/core/preprocess.go
+++ planner/core/preprocess.go
@@ -20,7 +20,6 @@ import (
 	"github.com/pingcap/errors"
 	"github.com/pingcap/parser"
 	"github.com/pingcap/parser/ast"
-	"github.com/pingcap/parser/charset"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/parser/mysql"
 	"github.com/pingcap/tidb/ddl"
