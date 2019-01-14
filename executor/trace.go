9d2e90a2e7f297dc6cbc4953c3bf9903ec8a2b96
lysu <sulifx@gmail.com>
2019-01-14 13:13:07 +0800

    resolve conflict

--- executor/trace.go
+++ executor/trace.go
@@ -48,8 +48,8 @@ type TraceExec struct {
 }
 
 // Next executes real query and collects span later.
-func (e *TraceExec) Next(ctx context.Context, chk *chunk.Chunk) error {
-	chk.Reset()
+func (e *TraceExec) Next(ctx context.Context, req *chunk.RecordBatch) error {
+	req.Reset()
 	if e.exhausted {
 		return nil
 	}
@@ -92,7 +92,7 @@ func (e *TraceExec) Next(ctx context.Context, chk *chunk.Chunk) error {
 		}
 		trace := traces[0]
 		sortTraceByStartTime(trace)
-		dfsTree(trace, "", false, chk)
+		dfsTree(trace, "", false, req.Chunk)
 		e.exhausted = true
 		return nil
 	}
@@ -106,10 +106,10 @@ func (e *TraceExec) Next(ctx context.Context, chk *chunk.Chunk) error {
 	// Split json data into rows to avoid the max packet size limitation.
 	const maxRowLen = 4096
 	for len(data) > maxRowLen {
-		chk.AppendString(0, string(data[:maxRowLen]))
+		req.AppendString(0, string(data[:maxRowLen]))
 		data = data[maxRowLen:]
 	}
-	chk.AppendString(0, string(data))
+	req.AppendString(0, string(data))
 	e.exhausted = true
 	return nil
 }
@@ -119,7 +119,7 @@ func drainRecordSet(ctx context.Context, sctx sessionctx.Context, rs sqlexec.Rec
 	chk := rs.NewChunk()
 
 	for {
-		err := rs.Next(ctx, chk)
+		err := rs.Next(ctx, chunk.NewRecordBatch(chk))
 		if err != nil || chk.NumRows() == 0 {
 			return rows, errors.Trace(err)
 		}
