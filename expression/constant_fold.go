8af5f0cec765150d9425e4aba4639527c201bfa7
spongedc <duchuangucas@live.com>
2019-02-15 14:37:16 +0800

    remove unnecessary constant-folding rule

--- expression/constant_fold.go
+++ expression/constant_fold.go
@@ -24,9 +24,8 @@ var specialFoldHandler = map[string]func(*ScalarFunction) (Expression, bool){}
 
 func init() {
 	specialFoldHandler = map[string]func(*ScalarFunction) (Expression, bool){
-		ast.If:        ifFoldHandler,
-		ast.Ifnull:    ifNullFoldHandler,
-		ast.NameConst: nameConstFoldHandler,
+		ast.If:     ifFoldHandler,
+		ast.Ifnull: ifNullFoldHandler,
 	}
 }
 
@@ -34,10 +33,6 @@ func init() {
 func FoldConstant(expr Expression) Expression {
 	e, _ := foldConstant(expr)
 	return e
-}
-
-func nameConstFoldHandler(expr *ScalarFunction) (Expression, bool) {
-	return foldConstant(expr.GetArgs()[1])
 }
 
 func ifFoldHandler(expr *ScalarFunction) (Expression, bool) {
