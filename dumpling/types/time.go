72bf40c622db11977cc186809cd3910394561c51
kennytm <kennytm@gmail.com>
2019-03-01 17:36:11 +0800

    [dumpling] types: provide the default capacity for result of ParseDateFormat (#9516)
    Prevents calling `runtime.growslice` too many times in a tight loop when
    processing many datetime values.

--- dumpling/types/time.go
+++ dumpling/types/time.go
@@ -564,7 +564,11 @@ func ParseDateFormat(format string) []string {
 	format = strings.TrimSpace(format)
 
 	start := 0
-	var seps = make([]string, 0)
+	// Initialize `seps` with capacity of 6. The input `format` is typically
+	// a date time of the form "2006-01-02 15:04:05", which has 6 numeric parts
+	// (the fractional second part is usually removed by `splitDateTime`).
+	// Setting `seps`'s capacity to 6 avoids reallocation in this common case.
+	seps := make([]string, 0, 6)
 	for i := 0; i < len(format); i++ {
 		// Date format must start and end with number.
 		if i == 0 || i == len(format)-1 {
