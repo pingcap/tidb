e5bfef4fa625468b120c99e8b209db4710ae0b31
Xiaoguang Sun <sunxiaoguang@users.noreply.github.com>
2019-03-04 11:17:09 +0800

    [parser] *: Support ENGINE TokuDB and it's ROW_FORMAT (#222)

--- parser/terror/terror_test.go
+++ parser/terror/terror_test.go
@@ -15,6 +15,7 @@ package terror
 
 import (
 	"encoding/json"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -110,7 +111,13 @@ func (s *testTErrorSuite) TestTraceAndLocation(c *C) {
 	err := example()
 	stack := errors.ErrorStack(err)
 	lines := strings.Split(stack, "\n")
-	c.Assert(len(lines), Equals, 23)
+	var sysStack = 0
+	for _, line := range lines {
+		if strings.Contains(line, runtime.GOROOT()) {
+			sysStack++
+		}
+	}
+	c.Assert(len(lines)-(2*sysStack), Equals, 15)
 	var containTerr bool
 	for _, v := range lines {
 		if strings.Contains(v, "terror_test.go") {
