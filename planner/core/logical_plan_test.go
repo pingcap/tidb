d29f15020026df1f5247a584c9a1d9bb67f94676
Yiding Cui <winoros@gmail.com>
2019-03-07 15:33:53 +0800

    add test

--- planner/core/logical_plan_test.go
+++ planner/core/logical_plan_test.go
@@ -2195,6 +2195,10 @@ func (s *testPlanSuite) TestWindowFunction(c *C) {
 			sql:    "select row_number() over(rows between 1 preceding and 1 following) from t",
 			result: "TableReader(Table(t))->Window(row_number() over())->Projection",
 		},
+		{
+			sql:    "select avg(b), max(avg(b)) over(rows between 1 preceding and 1 following) max, min(avg(b)) over(rows between 1 preceding and 1 following) min from t group by c",
+			result: "IndexLookUp(Index(t.c_d_e)[[NULL,+inf]], Table(t))->Projection->StreamAgg->Window(max(sel_agg_4) over(rows between 1 preceding and 1 following))->Window(min(sel_agg_5) over(r     ows between 1 preceding and 1 following))->Projection",
+		},
 	}
 
 	s.Parser.EnableWindowFunc(true)
