e3d058ac16d229fc62b5a99e8766673c0ef3023d
wjhuang2016 <huangwenjun1997@gmail.com>
2019-03-11 17:10:52 +0800

    Improve readability

--- planner/core/rule_join_elimination.go
+++ planner/core/rule_join_elimination.go
@@ -80,11 +80,8 @@ func (o *outerJoinEliminator) isAggColsAllFromOuterTable(outerPlan LogicalPlan, 
 	for _, col := range aggCols {
 		columnName := &ast.ColumnName{Schema: col.DBName, Table: col.TblName, Name: col.ColName}
 		c, err := outerPlan.Schema().FindColumn(columnName)
-		if err != nil {
+		if err != nil || c == nil {
 			return false, err
-		}
-		if c == nil {
-			return false, nil
 		}
 	}
 	return true, nil
@@ -98,11 +95,8 @@ func (o *outerJoinEliminator) isParentColsAllFromOuterTable(outerPlan LogicalPla
 	for _, col := range parentSchema.Columns {
 		columnName := &ast.ColumnName{Schema: col.DBName, Table: col.TblName, Name: col.ColName}
 		c, err := outerPlan.Schema().FindColumn(columnName)
-		if err != nil {
+		if err != nil || c == nil {
 			return false, err
-		}
-		if c == nil {
-			return false, nil
 		}
 	}
 	return true, nil
