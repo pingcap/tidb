05afe8990215d80eb74886fe0120fe00023bea05
WangXiangUSTC <xiang13225080@163.com>
2019-03-11 14:09:37 +0800

    format code

--- sessionctx/variable/varsutil.go
+++ sessionctx/variable/varsutil.go
@@ -310,7 +310,7 @@ func ValidateSetSystemVar(vars *SessionVars, name string, value string) (string,
 		return value, err
 	case WarningCount, ErrorCount:
 		return value, ErrReadOnly.GenWithStackByArgs(name)
-		
+
 	case GeneralLog, TiDBGeneralLog, AvoidTemporalUpgrade, BigTables, CheckProxyUsers, LogBin, CoreFile, EndMakersInJSON, SQLLogBin, OfflineMode,
 		PseudoSlaveMode, LowPriorityUpdates, SkipNameResolve, ForeignKeyChecks, SQLSafeUpdates, TiDBConstraintCheckInPlace:
 		if strings.EqualFold(value, "ON") || value == "1" {
