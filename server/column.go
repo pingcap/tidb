948c242e2092485f2026b41b29d521ad341bb22e
wjhuang2016 <huangwenjun1997@gmail.com>
2019-03-15 00:36:40 +0800

    Fix select constant

--- server/column.go
+++ server/column.go
@@ -65,7 +65,13 @@ func dumpFlag(tp byte, flag uint16) uint16 {
 		return flag | uint16(mysql.SetFlag)
 	case mysql.TypeEnum:
 		return flag | uint16(mysql.EnumFlag)
+	case mysql.TypeNull:
+		return flag
+
 	default:
+		if mysql.HasBinaryFlag(uint(flag)) {
+			return flag | uint16(mysql.NotNullFlag)
+		}
 		return flag
 	}
 }
