ba26012231e2807c2a9a1cf350e891e3a7d70501
Yiding Cui <winoros@gmail.com>
2019-03-14 19:05:27 +0800

    expression: using pingcap/log and unify the log format (#9507)

--- expression/constant_fold.go
+++ expression/constant_fold.go
@@ -14,9 +14,12 @@
 package expression
 
 import (
+	"context"
+
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/tidb/util/chunk"
-	log "github.com/sirupsen/logrus"
+	"github.com/pingcap/tidb/util/logutil"
+	"go.uber.org/zap"
 )
 
 // specialFoldHandler stores functions for special UDF to constant fold
@@ -44,7 +47,7 @@ func ifFoldHandler(expr *ScalarFunction) (Expression, bool) {
 			// Failed to fold this expr to a constant, print the DEBUG log and
 			// return the original expression to let the error to be evaluated
 			// again, in that time, the error is returned to the client.
-			log.Debugf("fold constant %s: %s", expr.ExplainInfo(), err.Error())
+			logutil.Logger(context.Background()).Debug("fold expression to constant", zap.String("expression", expr.ExplainInfo()), zap.Error(err))
 			return expr, false
 		}
 		if !isNull0 && arg0 != 0 {
@@ -135,7 +138,7 @@ func foldConstant(expr Expression) (Expression, bool) {
 		}
 		value, err := x.Eval(chunk.Row{})
 		if err != nil {
-			log.Warnf("fold constant %s: %s", x.ExplainInfo(), err.Error())
+			logutil.Logger(context.Background()).Debug("fold expression to constant", zap.String("expression", x.ExplainInfo()), zap.Error(err))
 			return expr, isDeferredConst
 		}
 		if isDeferredConst {
@@ -146,7 +149,7 @@ func foldConstant(expr Expression) (Expression, bool) {
 		if x.DeferredExpr != nil {
 			value, err := x.DeferredExpr.Eval(chunk.Row{})
 			if err != nil {
-				log.Warnf("fold constant %s: %s", x.ExplainInfo(), err.Error())
+				logutil.Logger(context.Background()).Debug("fold expression to constant", zap.String("expression", x.ExplainInfo()), zap.Error(err))
 				return expr, true
 			}
 			return &Constant{Value: value, RetType: x.RetType, DeferredExpr: x.DeferredExpr}, true
