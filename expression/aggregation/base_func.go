37ad92cee618c1bf6a66fbe9e248496ae9c08524
Yiding Cui <winoros@gmail.com>
2019-03-15 16:01:56 +0800

    executor: add window function NTILE (#9682)

--- expression/aggregation/base_func.go
+++ expression/aggregation/base_func.go
@@ -100,6 +100,8 @@ func (a *baseFuncDesc) typeInfer(ctx sessionctx.Context) {
 		a.typeInfer4NumberFuncs()
 	case ast.WindowFuncCumeDist:
 		a.typeInfer4CumeDist()
+	case ast.WindowFuncNtile:
+		a.typeInfer4Ntile()
 	case ast.WindowFuncPercentRank:
 		a.typeInfer4PercentRank()
 	case ast.WindowFuncLead, ast.WindowFuncLag:
@@ -202,6 +204,13 @@ func (a *baseFuncDesc) typeInfer4NumberFuncs() {
 func (a *baseFuncDesc) typeInfer4CumeDist() {
 	a.RetTp = types.NewFieldType(mysql.TypeDouble)
 	a.RetTp.Flen, a.RetTp.Decimal = mysql.MaxRealWidth, mysql.NotFixedDec
+}
+
+func (a *baseFuncDesc) typeInfer4Ntile() {
+	a.RetTp = types.NewFieldType(mysql.TypeLonglong)
+	a.RetTp.Flen = 21
+	types.SetBinChsClnFlag(a.RetTp)
+	a.RetTp.Flag |= mysql.UnsignedFlag
 }
 
 func (a *baseFuncDesc) typeInfer4PercentRank() {
