7a3a007a3cd35018793a2e7c842b6e786bbc88d8
Keyi Xie <xiekeyi98@gmail.com>
2019-03-19 17:04:02 +0800

    adjust comment

--- types/time_test.go
+++ types/time_test.go
@@ -5,7 +5,8 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-// // Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
