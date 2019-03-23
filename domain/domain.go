ea2efbfb871123e060ca75b553c43fdd7873e183
Keyi Xie <xiekeyi98@gmail.com>
2019-03-23 11:08:11 +0800

    adress comment

--- domain/domain.go
+++ domain/domain.go
@@ -900,7 +900,7 @@ func (do *Domain) updateStatsWorker(ctx sessionctx.Context, owner owner.Manager)
 	if err != nil {
 		logutil.Logger(context.Background()).Debug("init stats info failed", zap.Error(err))
 	} else {
-		logutil.Logger(context.Background()).Info("init stats info time", zap.Duration("take time", time.Since(t)))
+		logutil.Logger(context.Background()).Info("init stats info time", zap.Duration("take_time", time.Since(t)))
 	}
 	defer func() {
 		do.SetStatsUpdating(false)
@@ -1004,7 +1004,7 @@ func recoverInDomain(funcName string, quit bool) {
 		return
 	}
 	buf := util.GetStack()
-	logutil.Logger(context.Background()).Error("recover in domain failed", zap.String("function name", funcName),
+	logutil.Logger(context.Background()).Error("recover in domain failed", zap.String("function_name", funcName),
 		zap.Any("error", r), zap.String("buffer", string(buf)))
 	metrics.PanicCounter.WithLabelValues(metrics.LabelDomain).Inc()
 	if quit {
