834da81312f8ac23f76970f8ad9ec802af865728
crazycs520 <crazycs520@gmail.com>
2019-03-26 19:06:16 +0800

    fix test and address comment

--- ddl/db_integration_test.go
+++ ddl/db_integration_test.go
@@ -591,7 +591,7 @@ func (s *testIntegrationSuite) TestChangingTableCharset(c *C) {
 	rs, err = tk.Exec("alter table t charset utf8mb4 collate utf8mb4_bin")
 	c.Assert(err, NotNil)
 
-	// Test change column charset when change table charset.
+	// Test change column charset when changing table charset.
 	tk.MustExec("drop table t;")
 	tk.MustExec("create table t(a varchar(10)) charset utf8")
 	tk.MustExec("alter table t convert to charset utf8mb4;")
@@ -1486,14 +1486,13 @@ func (s *testIntegrationSuite) TestIgnoreColumnUTF8Charset(c *C) {
 
 	// Test for alter table convert charset
 	config.GetGlobalConfig().TreatOldVersionUTF8AsUTF8MB4 = true
-	s.tk.MustExec("alter table t change column b b varchar(40) character set ascii") // reload schema.
+	s.tk.MustExec("alter table t drop column b") // reload schema.
 	s.tk.MustExec("alter table t convert to charset utf8mb4;")
 
 	config.GetGlobalConfig().TreatOldVersionUTF8AsUTF8MB4 = false
-	s.tk.MustExec("alter table t change column b b varchar(50) character set ascii") // reload schema.
-	// TODO: fix  this after PR 9790.
+	s.tk.MustExec("alter table t add column b varchar(50);") // reload schema.
 	s.tk.MustQuery("show create table t").Check(testkit.Rows("t CREATE TABLE `t` (\n" +
-		"  `a` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,\n" +
-		"  `b` varchar(50) CHARACTER SET ascii COLLATE ascii_bin DEFAULT NULL\n" +
+		"  `a` varchar(20) DEFAULT NULL,\n" +
+		"  `b` varchar(50) DEFAULT NULL\n" +
 		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin"))
 }
