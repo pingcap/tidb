f699a4dfef436ab926a813cc3b3df61be07d1774
lzmhhh123 <lzmhhh123@gmail.com>
2019-03-28 16:27:00 +0800

    fix ci

--- store/tikv/2pc_test.go
+++ store/tikv/2pc_test.go
@@ -43,7 +43,7 @@ func (s *testCommitterSuite) SetUpTest(c *C) {
 	client := mocktikv.NewRPCClient(s.cluster, mvccStore)
 	pdCli := &codecPDClient{mocktikv.NewPDClient(s.cluster)}
 	spkv := NewMockSafePointKV()
-	store, err := newTikvStore("mocktikv-store", pdCli, spkv, client, false)
+	store, err := newTikvStore("mocktikv-store", pdCli, spkv, client, nil, false)
 	c.Assert(err, IsNil)
 	store.EnableTxnLocalLatches(1024000)
 	s.store = store
