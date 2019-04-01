06295dcfec69d1f368169aad76784143fbd7d8ec
bb7133 <bb7133@gmail.com>
2019-04-01 23:03:40 +0800

    [parser] types: port 'HasCharset' function from MySQL (#270)

--- parser/types/field_type.go
+++ parser/types/field_type.go
@@ -319,3 +319,16 @@ func (ft *FieldType) StorageLength() int {
 		return VarStorageLen
 	}
 }
+
+// HasCharset indicates if a COLUMN has an associated charset. Returning false here prevents some information
+// statements(like `SHOW CREATE TABLE`) from attaching a CHARACTER SET clause to the column.
+func HasCharset(ft *FieldType) bool {
+	switch ft.Tp {
+	case mysql.TypeVarchar, mysql.TypeString, mysql.TypeVarString, mysql.TypeBlob,
+		mysql.TypeTinyBlob, mysql.TypeMediumBlob, mysql.TypeLongBlob:
+		return !mysql.HasBinaryFlag(ft.Flag)
+	case mysql.TypeEnum, mysql.TypeSet:
+		return true
+	}
+	return false
+}
