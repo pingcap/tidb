ab423614d946cd4f130f1e2a69dd26238b02decc
bb7133 <bb7133@gmail.com>
2019-04-06 16:50:48 +0800

    metrics: improve ut coverage to 100%

--- metrics/server_test.go
+++ metrics/server_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 PingCAP, Inc.
+// Copyright 2019 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,32 +14,12 @@
 package metrics
 
 import (
-	"testing"
-
 	. "github.com/pingcap/check"
 	"github.com/pingcap/errors"
+	"github.com/pingcap/parser/terror"
 )
 
-func TestT(t *testing.T) {
-	TestingT(t)
-}
-
-var _ = Suite(&testSuite{})
-
-type testSuite struct {
-}
-
-func (s *testSuite) TestMetrics(c *C) {
-	// Make sure it doesn't panic.
-	PanicCounter.WithLabelValues(LabelDomain).Inc()
-}
-
-func (s *testSuite) TestRegisterMetrics(c *C) {
-	// Make sure it doesn't panic.
-	RegisterMetrics()
-}
-
-func (s *testSuite) TestRetLabel(c *C) {
-	c.Assert(RetLabel(nil), Equals, opSucc)
-	c.Assert(RetLabel(errors.New("test error")), Equals, opFailed)
+func (s *testSuite) TestExecuteErrorToLabel(c *C) {
+	c.Assert(ExecuteErrorToLabel(errors.New("test")), Equals, `unknown`)
+	c.Assert(ExecuteErrorToLabel(terror.ErrResultUndetermined), Equals, `global:2`)
 }
