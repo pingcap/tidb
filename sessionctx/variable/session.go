73f1dd38a088abc85afc049e550545e1c23b8978
Zhang Jian <zjsariel@gmail.com>
2019-04-10 13:05:05 +0800

    Update sessionctx/variable/session.go
    Co-Authored-By: crazycs520 <crazycs520@gmail.com>

--- sessionctx/variable/session.go
+++ sessionctx/variable/session.go
@@ -919,7 +919,7 @@ func (s *SessionVars) SlowLogFormat(txnTS uint64, costTime time.Duration, execDe
 		firstComma := false
 		vStr := ""
 		for k, v := range statsInfos {
-			if v == 0 {
+			if v == statistics.PseudoVersion {
 				vStr = "pseudo"
 			} else {
 				vStr = strconv.FormatUint(v, 10)
