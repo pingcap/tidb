78b1c7b8a0c63b62d61fc2e8e7e55b9b08c79205
xuhuaiyu <391585975@qq.com>
2019-04-11 10:12:29 +0800

    executor: use EncodeBytes in countOriginDistinct

--- executor/aggregate_test.go
+++ executor/aggregate_test.go
@@ -694,3 +694,11 @@ func (s *testSuite1) TestAggJSON(c *C) {
 		`"hello"`,
 	))
 }
+
+func (s *testSuite1) TestIssue10099(c *C) {
+	tk := testkit.NewTestKitWithInit(c, s.store)
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a char(10), b char(10))")
+	tk.MustExec("insert into t values('1', '222'), ('12', '22')")
+	tk.MustQuery("select count(distinct a, b) from t").Check(testkit.Rows("2"))
+}
