fc48f8063c4ea1549ba5e6d5efe6631b0485f75d
Lingyu Song <songlingyu@pingcap.com>
2019-04-17 14:25:49 +0800

    [dumpling] privilege, executor: support `SET DEFAULT ROLE` (#9949)

--- dumpling/privilege/privilege.go
+++ dumpling/privilege/privilege.go
@@ -56,6 +56,12 @@ type Manager interface {
 	// ActiveRoles active roles for current session.
 	// The first illegal role will be returned.
 	ActiveRoles(ctx sessionctx.Context, roleList []*auth.RoleIdentity) (bool, string)
+
+	// FindEdge find if there is an edge between role and user.
+	FindEdge(ctx sessionctx.Context, role *auth.RoleIdentity, user *auth.UserIdentity) bool
+
+	// GetDefaultRoles returns all default roles for certain user.
+	GetDefaultRoles(user, host string) []*auth.RoleIdentity
 }
 
 const key keyType = 0
