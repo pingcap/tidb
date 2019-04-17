616bb83ee0a280a399bd879911131f4269b292a4
Lingyu Song <songlingyu@pingcap.com>
2019-04-17 14:25:49 +0800

    privilege, executor: support `SET DEFAULT ROLE` (#9949)

--- session/session.go
+++ session/session.go
@@ -1196,6 +1196,7 @@ func (s *session) Auth(user *auth.UserIdentity, authentication []byte, salt []by
 	user.AuthUsername, user.AuthHostname, success = pm.ConnectionVerification(user.Username, user.Hostname, authentication, salt)
 	if success {
 		s.sessionVars.User = user
+		s.sessionVars.ActiveRoles = pm.GetDefaultRoles(user.AuthUsername, user.AuthHostname)
 		return true
 	} else if user.Hostname == variable.DefHostname {
 		logutil.Logger(context.Background()).Error("user connection verification failed",
@@ -1213,6 +1214,7 @@ func (s *session) Auth(user *auth.UserIdentity, authentication []byte, salt []by
 				AuthUsername: u,
 				AuthHostname: h,
 			}
+			s.sessionVars.ActiveRoles = pm.GetDefaultRoles(u, h)
 			return true
 		}
 	}
