55dc12f786691bee5d5efd34fd14c2986490c5ef
Jiahao Huang <july2993@gmail.com>
2019-04-19 11:23:16 +0800

    *: Add support "show open tables" (#10166)
    TiDB has no concept like mysql's "table cache" and "open table"
    For simplicity, we just return an empty result with the same structure
    as MySQL's SHOW OPEN TABLES

--- executor/show.go
+++ executor/show.go
@@ -130,6 +130,8 @@ func (e *ShowExec) fetchAll() error {
 		return e.fetchShowStatus()
 	case ast.ShowTables:
 		return e.fetchShowTables()
+	case ast.ShowOpenTables:
+		return e.fetchShowOpenTables()
 	case ast.ShowTableStatus:
 		return e.fetchShowTableStatus()
 	case ast.ShowTriggers:
@@ -220,6 +222,12 @@ func (e *ShowExec) fetchShowProcessList() error {
 			pi.Mem,
 		})
 	}
+	return nil
+}
+
+func (e *ShowExec) fetchShowOpenTables() error {
+	// TiDB has no concept like mysql's "table cache" and "open table"
+	// For simplicity, we just return an empty result with the same structure as MySQL's SHOW OPEN TABLES
 	return nil
 }
 
