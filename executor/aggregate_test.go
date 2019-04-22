e4aee765872ae67770863d5a64d9259a08bd6695
HuaiyuXu <xuhuaiyu@pingcap.com>
2019-04-22 15:12:16 +0800

    executor, util: fix wrong behavior of group_concat(distinct) (#10108)

--- executor/aggregate_test.go
+++ executor/aggregate_test.go
@@ -694,3 +694,11 @@ func (s *testSuite1) TestAggJSON(c *C) {
 		`"hello"`,
 	))
 }
+
+func (s *testSuite1) TestIssue10098(c *C) {
+	tk := testkit.NewTestKitWithInit(c, s.store)
+	tk.MustExec(`drop table if exists t;`)
+	tk.MustExec("create table t(a char(10), b char(10))")
+	tk.MustExec("insert into t values('1', '222'), ('12', '22')")
+	tk.MustQuery("select group_concat(distinct a, b) from t").Check(testkit.Rows("1222,1222"))
+}
