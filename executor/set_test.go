f608c9b284b6db0b41eb92f2403d056d461d1f33
crazycs <crazycs520@gmail.com>
2019-04-24 21:56:24 +0800

    ddl: add table option pre_split_regions for pre-split region when create table with  shard_row_id_bits. (#10138)

--- executor/set_test.go
+++ executor/set_test.go
@@ -339,6 +339,13 @@ func (s *testSuite2) TestSetVar(c *C) {
 	tk.MustQuery("select @@global.read_only;").Check(testkit.Rows("1"))
 	_, err = tk.Exec("set global read_only = abc")
 	c.Assert(err, NotNil)
+
+	// test for tidb_wait_table_split_finish
+	tk.MustQuery(`select @@session.tidb_wait_table_split_finish;`).Check(testkit.Rows("0"))
+	tk.MustExec("set tidb_wait_table_split_finish = 1")
+	tk.MustQuery(`select @@session.tidb_wait_table_split_finish;`).Check(testkit.Rows("1"))
+	tk.MustExec("set tidb_wait_table_split_finish = 0")
+	tk.MustQuery(`select @@session.tidb_wait_table_split_finish;`).Check(testkit.Rows("0"))
 }
 
 func (s *testSuite2) TestSetCharset(c *C) {
--- store/tikv/store_test.go
+++ store/tikv/store_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/pingcap/errors"
 	pb "github.com/pingcap/kvproto/pkg/kvrpcpb"
 	"github.com/pingcap/kvproto/pkg/metapb"
+	"github.com/pingcap/kvproto/pkg/pdpb"
 	"github.com/pingcap/pd/client"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/store/mockoracle"
@@ -168,7 +169,7 @@ func (c *mockPDClient) GetStore(ctx context.Context, storeID uint64) (*metapb.St
 	return c.client.GetStore(ctx, storeID)
 }
 
-func (c *mockPDClient) GetAllStores(ctx context.Context) ([]*metapb.Store, error) {
+func (c *mockPDClient) GetAllStores(ctx context.Context, opts ...pd.GetStoreOption) ([]*metapb.Store, error) {
 	c.RLock()
 	defer c.Unlock()
 
@@ -183,6 +184,14 @@ func (c *mockPDClient) UpdateGCSafePoint(ctx context.Context, safePoint uint64) 
 }
 
 func (c *mockPDClient) Close() {}
+
+func (c *mockPDClient) ScatterRegion(ctx context.Context, regionID uint64) error {
+	return nil
+}
+
+func (c *mockPDClient) GetOperator(ctx context.Context, regionID uint64) (*pdpb.GetOperatorResponse, error) {
+	return &pdpb.GetOperatorResponse{Status: pdpb.OperatorStatus_SUCCESS}, nil
+}
 
 type checkRequestClient struct {
 	Client
