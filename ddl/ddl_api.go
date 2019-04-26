728809b262cf7bc2cb50968fee0466772d03b1f2
Calvin Weng <wenghao@pingcap.com>
2019-04-26 20:21:52 +0800

    Update ddl/ddl_api.go
    Co-Authored-By: crazycs520 <crazycs520@gmail.com>

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -1810,7 +1810,7 @@ func (d *ddl) RebaseAutoID(ctx sessionctx.Context, ident ast.Ident, newBase int6
 	// If newBase < autoIncID, we need to do a rebase before returning.
 	// Assume there are 2 TiDB servers: TiDB-A with allocator range of 0 ~ 30000; TiDB-B with allocator range of 30001 ~ 60000.
 	// if user send SQL `alter table t1 auto_increment = 100` to TiDB-B,
-	// and if TiDB-B just return here,
+	// and TiDB-B finds 100 < 30001 but returns without any handling,
 	// then TiDB-A may still allocate 99 for auto_increment column. This doesn't make sense for the user.
 	newBase = mathutil.MaxInt64(newBase, autoIncID)
 	job := &model.Job{
