e778a2dd0334293b36c77053e9fab08ad7f7d99d
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2019-04-28 18:46:07 +0800

    record how many keys txn involved in this region
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- store/tikv/2pc.go
+++ store/tikv/2pc.go
@@ -424,7 +424,7 @@ func (c *twoPhaseCommitter) prewriteSingleBatch(bo *Backoffer, batch batchKeys) 
 			PrimaryLock:  c.primary(),
 			StartVersion: c.startTS,
 			LockTtl:      c.lockTTL,
-			TxnSize: uint64(len(c.keys)),
+			TxnSize: uint64(len(batch.keys)),
 		},
 		Context: pb.Context{
 			Priority: c.priority,
