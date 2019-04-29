6a578a8b57199270cf7bf4b3bd864fd7f27a27b7
Song Guo <gs199704@gmail.com>
2019-04-29 12:15:59 +0800

    statistics: Support store CMSketch with Top N (#10255)

--- executor/analyze.go
+++ executor/analyze.go
@@ -322,11 +322,8 @@ func (e *AnalyzeIndexExec) buildStatsFromResult(result distsql.SelectResult, nee
 		if needCMS {
 			if resp.Cms == nil {
 				logutil.Logger(context.TODO()).Warn("nil CMS in response", zap.String("table", e.idxInfo.Table.O), zap.String("index", e.idxInfo.Name.O))
-			} else {
-				err := cms.MergeCMSketch(statistics.CMSketchFromProto(resp.Cms))
-				if err != nil {
-					return nil, nil, err
-				}
+			} else if err := cms.MergeCMSketch(statistics.CMSketchFromProto(resp.Cms)); err != nil {
+				return nil, nil, err
 			}
 		}
 	}
