430e3d759d137252ef1fcd11fd5c351093f0ce5f
Guo Song <gs199704@gmail.com>
2019-05-06 16:44:43 +0800

    add more tests

--- expression/builtin_time.go
+++ expression/builtin_time.go
@@ -2677,20 +2677,22 @@ func (du *baseDateArithmitical) getIntervalFromDecimal(ctx sessionctx.Context, a
 		interval = "00:" + interval
 	case "SECOND_MICROSECOND":
 		/* keep interval as original decimal */
-	case "SECOND", "MICROSECOND":
-		args[1] = WrapWithCastAsReal(ctx, args[1])
+	case "SECOND":
+		// Decimal's EvalString is like %f format.
 		interval, isNull, err = args[1].EvalString(ctx, row)
 		if isNull || err != nil {
 			return "", true, err
 		}
 	default:
-		// YEAR, QUARTER, MONTH, WEEK, DAY, HOUR, MINUTE
+		// YEAR, QUARTER, MONTH, WEEK, DAY, HOUR, MINUTE, MICROSECOND
 		args[1] = WrapWithCastAsInt(ctx, args[1])
 		interval, isNull, err = args[1].EvalString(ctx, row)
 		if isNull || err != nil {
 			return "", true, err
 		}
 	}
+
+	fmt.Printf("baseDateArithmitical.getIntervalFromDecimal interval %v\n", interval)
 
 	return interval, false, nil
 }
