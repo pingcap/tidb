4f980489db28169a07d955f26c32a542aa84f358
Guo Song <gs199704@gmail.com>
2019-05-11 10:19:17 +0800

    fix

--- types/time_test.go
+++ types/time_test.go
@@ -1369,7 +1369,7 @@ func (s *testTimeSuite) TestCheckMonthDay(c *C) {
 		if t.isValidDate {
 			c.Check(err, IsNil)
 		} else {
-			c.Check(types.ErrIncorrectDatetimeValue.Equal(err), IsTrue)
+			c.Check(types.ErrInvalidTimeFormat.Equal(err), IsTrue)
 		}
 	}
 }
