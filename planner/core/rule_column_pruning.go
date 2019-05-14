390cd1978df4624c54a5c8e0aef9d9495fac6de2
lysu <sulifx@gmail.com>
2019-05-14 17:43:42 +0800

    planner: keep unfoldable exprs when pruning for ORDER BY items (#10064) (#10442)

--- planner/core/rule_column_pruning.go
+++ planner/core/rule_column_pruning.go
@@ -120,6 +120,9 @@ func (ls *LogicalSort) PruneColumns(parentUsedCols []*expression.Column) {
 	for i := len(ls.ByItems) - 1; i >= 0; i-- {
 		cols := expression.ExtractColumns(ls.ByItems[i].Expr)
 		if len(cols) == 0 {
+			if !ls.ByItems[i].Expr.ConstItem() {
+				continue
+			}
 			ls.ByItems = append(ls.ByItems[:i], ls.ByItems[i+1:]...)
 		} else {
 			parentUsedCols = append(parentUsedCols, cols...)
