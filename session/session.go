52c9d03a5e146c6a04d933221b7265ec59cc3bdf
Song Guo <gs199704@gmail.com>
2019-05-22 21:10:46 +0800

    support view related privileges in mysql.tables_priv (#9894)

--- session/session.go
+++ session/session.go
@@ -1564,7 +1564,7 @@ func createSessionWithDomain(store kv.Storage, dom *domain.Domain) (*session, er
 
 const (
 	notBootstrapped         = 0
-	currentBootstrapVersion = 31
+	currentBootstrapVersion = 32
 )
 
 func getStoreBootstrapVersion(store kv.Storage) int64 {
