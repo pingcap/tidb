6a7fb328d4217c4804036346a58b91985282ab1f
xuhuaiyu <391585975@qq.com>
2019-05-23 19:46:54 +0800

    distsql: improve package distsql test code coverage to 85%

---
+++ distsql/select_result_test.go
@@ -0,0 +1,57 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package distsql
+
+import (
+	"fmt"
+	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb/sessionctx/stmtctx"
+	"github.com/pingcap/tidb/util/execdetails"
+	"github.com/pingcap/tidb/util/mock"
+	"github.com/pingcap/tipb/go-tipb"
+)
+
+func (s *testSuite) TestUpdateCopRuntimeStats(c *C) {
+	ctx := mock.NewContext()
+	ctx.GetSessionVars().StmtCtx = new(stmtctx.StatementContext)
+	sr := selectResult{ctx: ctx}
+	c.Assert(ctx.GetSessionVars().StmtCtx.RuntimeStatsColl, IsNil)
+	sr.updateCopRuntimeStats("a")
+
+	ctx.GetSessionVars().StmtCtx.RuntimeStatsColl = execdetails.NewRuntimeStatsColl()
+	t := uint64(1)
+	sr.selectResp = &tipb.SelectResponse{
+		ExecutionSummaries: []*tipb.ExecutorExecutionSummary{
+			{&t, &t, &t, []byte{}},
+		},
+	}
+	c.Assert(len(sr.selectResp.GetExecutionSummaries()) != len(sr.copPlanIDs), IsTrue)
+	sr.updateCopRuntimeStats("callee")
+
+	sr.copPlanIDs = []fmt.Stringer{copPlan{}}
+	c.Assert(ctx.GetSessionVars().StmtCtx.RuntimeStatsColl, NotNil)
+	c.Assert(len(sr.selectResp.GetExecutionSummaries()), Equals, len(sr.copPlanIDs))
+	sr.updateCopRuntimeStats("callee")
+	for _, execSum := range sr.selectResp.GetExecutionSummaries() {
+		c.Assert(*execSum.NumIterations, Equals, uint64(1))
+		c.Assert(*execSum.NumProducedRows, Equals, uint64(1))
+		c.Assert(*execSum.TimeProcessedNs, Equals, uint64(1))
+	}
+}
+
+type copPlan struct{}
+
+func (p copPlan) String() string {
+	return "copPlan"
+}
