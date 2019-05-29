6e9007c51e518209fde6aa27717cac7f18af7331
Ewan Chou <coocood@gmail.com>
2019-05-29 14:02:23 +0800

    store/tikv: remove canceled requests before sending

--- store/tikv/client.go
+++ store/tikv/client.go
@@ -336,6 +336,10 @@ type batchCommandsEntry struct {
 	err      error
 }
 
+func (b *batchCommandsEntry) isCanceled() bool {
+	return atomic.LoadInt32(&b.canceled) == 1
+}
+
 const idleTimeout = 3 * time.Minute
 
 // fetchAllPendingRequests fetches all pending requests from the channel.
@@ -476,6 +480,7 @@ func (a *connArray) batchSendLoop(cfg config.TiKVClient) {
 			bestBatchWaitSize += 1
 		}
 
+		length = removeCanceledRequests(&entries, &requests)
 		maxBatchID := atomic.AddUint64(&batchCommandsClient.idAlloc, uint64(length))
 		for i := 0; i < length; i++ {
 			requestID := uint64(i) + maxBatchID - uint64(length)
@@ -504,6 +509,23 @@ func (a *connArray) batchSendLoop(cfg config.TiKVClient) {
 			batchCommandsClient.failPendingRequests(err)
 		}
 	}
+}
+
+// removeCanceledRequests removes canceled requests before sending.
+func removeCanceledRequests(
+	entries *[]*batchCommandsEntry,
+	requests *[]*tikvpb.BatchCommandsRequest_Request) int {
+	validEntries := (*entries)[:0]
+	validRequets := (*requests)[:0]
+	for _, e := range *entries {
+		if !e.isCanceled() {
+			validEntries = append(validEntries, e)
+			validRequets = append(validRequets, e.req)
+		}
+	}
+	*entries = validEntries
+	*requests = validRequets
+	return len(*entries)
 }
 
 // rpcClient is RPC client struct.
