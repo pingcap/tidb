4d1ae02567a5c7dec5fe92fe0031041ea78ecf95
HuaiyuXu <xuhuaiyu@pingcap.com>
2019-06-05 20:38:11 +0800

    [dumpling] *: print an expensive log when a query exceeds time threshold (#10350)

--- dumpling/sessionctx/variable/sysvar.go
+++ dumpling/sessionctx/variable/sysvar.go
@@ -697,6 +697,7 @@ var defaultSysVars = []*SysVar{
 	{ScopeSession, TiDBSlowQueryFile, ""},
 	{ScopeSession, TiDBWaitTableSplitFinish, BoolToIntStr(DefTiDBWaitTableSplitFinish)},
 	{ScopeSession, TiDBLowResolutionTSO, "0"},
+	{ScopeSession, TiDBExpensiveQueryTimeThreshold, strconv.Itoa(DefTiDBExpensiveQueryTimeThreshold)},
 }
 
 // SynonymsSysVariables is synonyms of system variables.
