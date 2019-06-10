4e08ce0ff4b1b41b4f5c9cf0ad14941d7b1770ec
qw4990 <qw4990@163.com>
2019-06-10 13:58:56 +0800

    check result

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -304,7 +304,38 @@ func (s *testSuite) TestShow(c *C) {
 		"binary binary binary 1"))
 	c.Assert(len(tk.MustQuery("show master status").Rows()), Equals, 1)
 	tk.MustQuery("show create database test").Check(testkit.Rows("test CREATE DATABASE `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 */"))
-	tk.MustQuery("show privileges")
+	tk.MustQuery("show privileges").Check(testkit.Rows("Alter Tables To alter the table",
+		"Alter Tables To alter the table",
+		"Alter routine Functions,Procedures To alter or drop stored functions/procedures",
+		"Create Databases,Tables,Indexes To create new databases and tables",
+		"Create routine Databases To use CREATE FUNCTION/PROCEDURE",
+		"Create temporary tables Databases To use CREATE TEMPORARY TABLE",
+		"Create view Tables To create new views",
+		"Create user Server Admin To create new users",
+		"Delete Tables To delete existing rows",
+		"Drop Databases,Tables To drop databases, tables, and views",
+		"Event Server Admin To create, alter, drop and execute events",
+		"Execute Functions,Procedures To execute stored routines",
+		"File File access on server To read and write files on the server",
+		"Grant option Databases,Tables,Functions,Procedures To give to other users those privileges you possess",
+		"Index Tables To create or drop indexes",
+		"Insert Tables To insert data into tables",
+		"Lock tables Databases To use LOCK TABLES (together with SELECT privilege)",
+		"Process Server Admin To view the plain text of currently executing queries",
+		"Proxy Server Admin To make proxy user possible",
+		"References Databases,Tables To have references on tables",
+		"Reload Server Admin To reload or refresh tables, logs and privileges",
+		"Replication client Server Admin To ask where the slave or master servers are",
+		"Replication slave Server Admin To read binary log events from the master",
+		"Select Tables To retrieve rows from table",
+		"Show databases Server Admin To see all databases with SHOW DATABASES",
+		"Show view Tables To see views with SHOW CREATE VIEW",
+		"Shutdown Server Admin To shut down the server",
+		"Super Server Admin To use KILL thread, SET GLOBAL, CHANGE MASTER, etc.",
+		"Trigger Tables To use triggers",
+		"Create tablespace Server Admin To create/alter/drop tablespaces",
+		"Update Tables To update existing rows",
+		"Usage Server Admin No privileges - allow connect only"))
 	c.Assert(len(tk.MustQuery("show table status").Rows()), Equals, 1)
 }
 
