c5d7c3f27c9fe2de7425a18819022eabaa44ff0a
tiancaiamao <tiancaiamao@gmail.com>
2019-06-12 16:04:57 +0800

    ddl: fix 'create table like' for partitioned table

--- ddl/partition.go
+++ ddl/partition.go
@@ -102,7 +102,6 @@ func buildTablePartitionInfo(ctx sessionctx.Context, d *ddl, s *ast.CreateTableS
 		}
 	}
 
-	// TODO: generate multiple global ID for paritions, reduce the times of obtaining the global ID from the storage.
 	if s.Partition.Tp == model.PartitionTypeRange {
 		if err := buildRangePartitionDefinitions(ctx, d, s, pi); err != nil {
 			return nil, errors.Trace(err)
@@ -116,13 +115,13 @@ func buildTablePartitionInfo(ctx sessionctx.Context, d *ddl, s *ast.CreateTableS
 }
 
 func buildHashPartitionDefinitions(ctx sessionctx.Context, d *ddl, s *ast.CreateTableStmt, pi *model.PartitionInfo) error {
+	genIDs, err := d.genGlobalID(int(pi.Num))
+	if err != nil {
+		return errors.Trace(err)
+	}
 	defs := make([]model.PartitionDefinition, pi.Num)
 	for i := 0; i < len(defs); i++ {
-		pid, err := d.genGlobalID()
-		if err != nil {
-			return errors.Trace(err)
-		}
-		defs[i].ID = pid
+		defs[i].ID = genIDs[i]
 		if len(s.Partition.Definitions) == 0 {
 			defs[i].Name = model.NewCIStr(fmt.Sprintf("p%v", i))
 		} else {
@@ -136,15 +135,15 @@ func buildHashPartitionDefinitions(ctx sessionctx.Context, d *ddl, s *ast.Create
 }
 
 func buildRangePartitionDefinitions(ctx sessionctx.Context, d *ddl, s *ast.CreateTableStmt, pi *model.PartitionInfo) error {
-	for _, def := range s.Partition.Definitions {
-		pid, err := d.genGlobalID()
-		if err != nil {
-			return errors.Trace(err)
-		}
+	genIDs, err := d.genGlobalID(len(s.Partition.Definitions))
+	if err != nil {
+		return err
+	}
+	for ith, def := range s.Partition.Definitions {
 		comment, _ := def.Comment()
 		piDef := model.PartitionDefinition{
 			Name:    def.Name,
-			ID:      pid,
+			ID:      genIDs[ith],
 			Comment: comment,
 		}
 
