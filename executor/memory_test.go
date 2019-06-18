8b48d20e689f9b79c3e801ea518b0cd2e4ce0e7c
qw4990 <qw4990@163.com>
2019-06-18 13:00:14 +0800

    remove some unnecessary code

--- executor/memory_test.go
+++ executor/memory_test.go
@@ -15,52 +15,29 @@ package executor_test
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"runtime"
 
 	. "github.com/pingcap/check"
-	"github.com/pingcap/parser"
 	"github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/session"
 	"github.com/pingcap/tidb/store/mockstore"
-	"github.com/pingcap/tidb/store/mockstore/mocktikv"
-	"github.com/pingcap/tidb/util/mock"
 )
 
 var _ = SerialSuites(&testMemoryLeak{})
 
 type testMemoryLeak struct {
-	cluster   *mocktikv.Cluster
-	mvccStore mocktikv.MVCCStore
-	store     kv.Storage
-	domain    *domain.Domain
-	*parser.Parser
-	ctx *mock.Context
+	store  kv.Storage
+	domain *domain.Domain
 }
 
 func (s *testMemoryLeak) SetUpSuite(c *C) {
-	s.Parser = parser.New()
-	flag.Lookup("mockTikv")
-	useMockTikv := *mockTikv
-	if useMockTikv {
-		s.cluster = mocktikv.NewCluster()
-		mocktikv.BootstrapWithSingleStore(s.cluster)
-		s.mvccStore = mocktikv.MustNewMVCCStore()
-		store, err := mockstore.NewMockTikvStore(
-			mockstore.WithCluster(s.cluster),
-			mockstore.WithMVCCStore(s.mvccStore),
-		)
-		c.Assert(err, IsNil)
-		s.store = store
-		session.SetSchemaLease(0)
-		session.SetStatsLease(0)
-	}
-	d, err := session.BootstrapSession(s.store)
+	var err error
+	s.store, err = mockstore.NewMockTikvStore()
 	c.Assert(err, IsNil)
-	d.SetStatsUpdating(true)
-	s.domain = d
+	s.domain, err = session.BootstrapSession(s.store)
+	c.Assert(err, IsNil)
 }
 
 func (s *testMemoryLeak) TestPBMemoryLeak(c *C) {
