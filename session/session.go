d9a0f160553f7268b59062c4583b3fd4057a7b5b
HuaiyuXu <391585975@qq.com>
2019-06-19 13:47:41 +0800

    session: init expensiveQueryHandle immediately after domain be initiated (#10845)

--- session/session.go
+++ session/session.go
@@ -1448,6 +1448,7 @@ func BootstrapSession(store kv.Storage) (*domain.Domain, error) {
 
 	timeutil.SetSystemTZ(tz)
 	dom := domain.GetDomain(se)
+	dom.InitExpensiveQueryHandle()
 
 	if !config.GetGlobalConfig().Security.SkipGrantTable {
 		err = dom.LoadPrivilegeLoop(se)
@@ -1479,7 +1480,6 @@ func BootstrapSession(store kv.Storage) (*domain.Domain, error) {
 	if err != nil {
 		return nil, err
 	}
-	dom.InitExpensiveQueryHandle()
 	if raw, ok := store.(tikv.EtcdBackend); ok {
 		err = raw.StartGCWorker()
 		if err != nil {
