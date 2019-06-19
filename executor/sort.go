f6be085bc6f08e5365b9e571f4ef39de90b0278b
tiancaiamao <tiancaiamao@gmail.com>
2019-06-19 15:55:28 +0800

    executor: tiny refactor the Executor interface (#10846)
    Introduce a base() method to get the baseExecutor, so we don't need to add
    method from baseExecutor to the Executor interface any more
    
    Before:
    ```
    type Executor interface {
    	Open(context.Context) error
    	Next(ctx context.Context, req *chunk.RecordBatch) error
    	Close() error
    	Schema() *expression.Schema
    
    	retTypes() []*types.FieldType
    	newFirstChunk() *chunk.Chunk
    }
    ```
    
    After:
    ```
    type Executor interface {
            base() *baseExecutor
    	Open(context.Context) error
    	Next(ctx context.Context, req *chunk.RecordBatch) error
    	Close() error
    	Schema() *expression.Schema
    }
    ```

--- executor/sort.go
+++ executor/sort.go
@@ -105,12 +105,12 @@ func (e *SortExec) Next(ctx context.Context, req *chunk.RecordBatch) error {
 }
 
 func (e *SortExec) fetchRowChunks(ctx context.Context) error {
-	fields := e.retTypes()
+	fields := retTypes(e)
 	e.rowChunks = chunk.NewList(fields, e.initCap, e.maxChunkSize)
 	e.rowChunks.GetMemTracker().AttachTo(e.memTracker)
 	e.rowChunks.GetMemTracker().SetLabel(rowChunksLabel)
 	for {
-		chk := e.children[0].newFirstChunk()
+		chk := newFirstChunk(e.children[0])
 		err := e.children[0].Next(ctx, chunk.NewRecordBatch(chk))
 		if err != nil {
 			return err
@@ -275,11 +275,11 @@ func (e *TopNExec) Next(ctx context.Context, req *chunk.RecordBatch) error {
 
 func (e *TopNExec) loadChunksUntilTotalLimit(ctx context.Context) error {
 	e.chkHeap = &topNChunkHeap{e}
-	e.rowChunks = chunk.NewList(e.retTypes(), e.initCap, e.maxChunkSize)
+	e.rowChunks = chunk.NewList(retTypes(e), e.initCap, e.maxChunkSize)
 	e.rowChunks.GetMemTracker().AttachTo(e.memTracker)
 	e.rowChunks.GetMemTracker().SetLabel(rowChunksLabel)
 	for uint64(e.rowChunks.Len()) < e.totalLimit {
-		srcChk := e.children[0].newFirstChunk()
+		srcChk := newFirstChunk(e.children[0])
 		// adjust required rows by total limit
 		srcChk.SetRequiredRows(int(e.totalLimit-uint64(e.rowChunks.Len())), e.maxChunkSize)
 		err := e.children[0].Next(ctx, chunk.NewRecordBatch(srcChk))
@@ -305,7 +305,7 @@ func (e *TopNExec) executeTopN(ctx context.Context) error {
 		// The number of rows we loaded may exceeds total limit, remove greatest rows by Pop.
 		heap.Pop(e.chkHeap)
 	}
-	childRowChk := e.children[0].newFirstChunk()
+	childRowChk := newFirstChunk(e.children[0])
 	for {
 		err := e.children[0].Next(ctx, chunk.NewRecordBatch(childRowChk))
 		if err != nil {
@@ -349,7 +349,7 @@ func (e *TopNExec) processChildChk(childRowChk *chunk.Chunk) error {
 // but we want descending top N, then we will keep all data in memory.
 // But if data is distributed randomly, this function will be called log(n) times.
 func (e *TopNExec) doCompaction() error {
-	newRowChunks := chunk.NewList(e.retTypes(), e.initCap, e.maxChunkSize)
+	newRowChunks := chunk.NewList(retTypes(e), e.initCap, e.maxChunkSize)
 	newRowPtrs := make([]chunk.RowPtr, 0, e.rowChunks.Len())
 	for _, rowPtr := range e.rowPtrs {
 		newRowPtr := newRowChunks.AppendRow(e.rowChunks.GetRow(rowPtr))
