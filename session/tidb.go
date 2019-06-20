a10b571960f8a4c897cb1bb6503ee40d96ebbadd
qw4990 <qw4990@163.com>
2019-06-20 15:44:56 +0800

    remove RecordBatch

--- session/tidb.go
+++ session/tidb.go
@@ -271,7 +271,7 @@ func GetRows4Test(ctx context.Context, sctx sessionctx.Context, rs sqlexec.Recor
 	req := rs.NewFirstChunk()
 	for {
 		// Since we collect all the rows, we can not reuse the chunk.
-		iter := chunk.NewIterator4Chunk(req.Chunk)
+		iter := chunk.NewIterator4Chunk(req)
 
 		err := rs.Next(ctx, req)
 		if err != nil {
@@ -284,7 +284,7 @@ func GetRows4Test(ctx context.Context, sctx sessionctx.Context, rs sqlexec.Recor
 		for row := iter.Begin(); row != iter.End(); row = iter.Next() {
 			rows = append(rows, row)
 		}
-		req.Chunk = chunk.Renew(req.Chunk, sctx.GetSessionVars().MaxChunkSize)
+		req = chunk.Renew(req, sctx.GetSessionVars().MaxChunkSize)
 	}
 	return rows, nil
 }
