54d13c74e42ea7cd4c315740337035dd24657201
tiancaiamao <tiancaiamao@gmail.com>
2019-06-20 16:35:59 +0800

    executor: tiny clean up (#10875)
    code refactor for the tracing related code

--- executor/projection.go
+++ executor/projection.go
@@ -19,7 +19,6 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/opentracing/opentracing-go"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/sessionctx"
@@ -155,11 +154,6 @@ func (e *ProjectionExec) Open(ctx context.Context) error {
 //  +------------------------------+       +----------------------+
 //
 func (e *ProjectionExec) Next(ctx context.Context, req *chunk.RecordBatch) error {
-	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
-		span1 := span.Tracer().StartSpan("projection.Next", opentracing.ChildOf(span.Context()))
-		defer span1.Finish()
-	}
-
 	if e.runtimeStats != nil {
 		start := time.Now()
 		defer func() { e.runtimeStats.Record(time.Since(start), req.NumRows()) }()
