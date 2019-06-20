c30b649183cddefb01894cc4496c7b1d4505d6b0
crazycs520 <crazycs520@gmail.com>
2019-06-20 13:26:41 +0800

    *: add split table syntax to split table region (#10553)

--- executor/builder.go
+++ executor/builder.go
@@ -169,7 +169,7 @@ func (b *executorBuilder) build(p plannercore.Plan) Executor {
 	case *plannercore.PhysicalIndexLookUpReader:
 		return b.buildIndexLookUpReader(v)
 	case *plannercore.SplitRegion:
-		return b.buildSplitIndexRegion(v)
+		return b.buildSplitRegion(v)
 	default:
 		b.err = ErrUnknownPlan.GenWithStack("Unknown Plan %T", p)
 		return nil
@@ -1324,15 +1324,25 @@ func (b *executorBuilder) buildUnionAll(v *plannercore.PhysicalUnionAll) Executo
 	return e
 }
 
-func (b *executorBuilder) buildSplitIndexRegion(v *plannercore.SplitRegion) Executor {
+func (b *executorBuilder) buildSplitRegion(v *plannercore.SplitRegion) Executor {
 	base := newBaseExecutor(b.ctx, nil, v.ExplainID())
 	base.initCap = chunk.ZeroCapacity
-	return &SplitIndexRegionExec{
+	if v.IndexInfo != nil {
+		return &SplitIndexRegionExec{
+			baseExecutor: base,
+			tableInfo:    v.TableInfo,
+			indexInfo:    v.IndexInfo,
+			lower:        v.Lower,
+			upper:        v.Upper,
+			num:          v.Num,
+			valueLists:   v.ValueLists,
+		}
+	}
+	return &SplitTableRegionExec{
 		baseExecutor: base,
 		tableInfo:    v.TableInfo,
-		indexInfo:    v.IndexInfo,
-		lower:        v.Lower,
-		upper:        v.Upper,
+		lower:        v.Lower[0],
+		upper:        v.Upper[0],
 		num:          v.Num,
 		valueLists:   v.ValueLists,
 	}
