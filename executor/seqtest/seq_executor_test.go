f29b36ca1d0d9e8e0c82a27a7937c02074ddf68c
Tanner <tangenta@126.com>
2019-07-01 10:37:35 +0800

    *: fix malformed SQL statements in test (#10992)

--- executor/seqtest/seq_executor_test.go
+++ executor/seqtest/seq_executor_test.go
@@ -217,7 +217,7 @@ func (s *seqTestSuite) TestShow(c *C) {
 		"`c2` smallint unsigned default null," +
 		"`c3` mediumint unsigned default null," +
 		"`c4` int unsigned default null," +
-		"`c5` bigint unsigned default null);`"
+		"`c5` bigint unsigned default null);"
 
 	tk.MustExec(testSQL)
 	testSQL = "show create table t1"
