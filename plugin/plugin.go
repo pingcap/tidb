304f0d6421aa687c0e551c746628ce4bd7ae259d
lysu <sulifx@gmail.com>
2019-07-09 10:58:57 +0800

    address comment

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -458,17 +458,17 @@ func NotifyFlush(dom *domain.Domain, pluginName string) error {
 	return nil
 }
 
-// ChangeDisableFlagAndFlush changes plugin disable flag and notify other nodes to do same change..
+// ChangeDisableFlagAndFlush changes plugin disable flag and notify other nodes to do same change.
 func ChangeDisableFlagAndFlush(dom *domain.Domain, pluginName string, disable bool) error {
 	p := getByName(pluginName)
+	if p == nil || p.Manifest.flushWatcher == nil || p.State != Ready {
+		return errors.Errorf("plugin %s doesn't exists or unsupported flush or doesn't start with PD", pluginName)
+	}
 	disableInt := uint32(0)
 	if disable {
 		disableInt = 1
 	}
 	atomic.StoreUint32(&p.Disabled, disableInt)
-	if p == nil || p.Manifest.flushWatcher == nil || p.State != Ready {
-		return errors.Errorf("plugin %s doesn't exists or unsupported flush or doesn't start with PD", pluginName)
-	}
 	_, err := dom.GetEtcdClient().KV.Put(context.Background(), p.Manifest.flushWatcher.path, strconv.Itoa(int(disableInt)))
 	if err != nil {
 		return err
