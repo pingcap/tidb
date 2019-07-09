2b7f0e196b83c91f6668151c4ac26b7a11a8d2b4
kennytm <kennytm@gmail.com>
2019-07-09 11:11:13 +0800

    [parser] test: count coverage for all packages when unit testing (#377)

--- parser/test.sh
+++ parser/test.sh
@@ -1,7 +1,8 @@
 {
 	  mv go.mod1 go.mod
 	  mv go.sum1 go.sum
-	  GO111MODULE=on go test -race -covermode=atomic -coverprofile=coverage.txt ./...
+	  GO111MODULE=on go test -race ./... &&
+	  GO111MODULE=on go test -covermode=set -coverprofile=coverage.txt -coverpkg=./... ./...
 } || {
 	  mv go.mod go.mod1
 	  mv go.sum go.sum1
