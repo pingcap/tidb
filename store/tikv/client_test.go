8c5428efd55fcf3ab0840a3331e38782839a4c74
qupeng <qupeng@pingcap.com>
2019-07-12 11:44:50 +0800

    add case TestSendAfterClose

--- store/tikv/client_test.go
+++ store/tikv/client_test.go
@@ -14,11 +14,15 @@
 package tikv
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/kvproto/pkg/tikvpb"
 	"github.com/pingcap/tidb/config"
+	"github.com/pingcap/tidb/store/tikv/tikvrpc"
 )
 
 func TestT(t *testing.T) {
@@ -79,4 +83,32 @@ func (s *testClientSuite) TestRemoveCanceledRequests(c *C) {
 	c.Assert(len(requests), Equals, 2)
 	newEntryPtr := &entries[0]
 	c.Assert(entryPtr, Equals, newEntryPtr)
+}
+
+func (s *testClientSuite) TestSendAfterClose(c *C) {
+	server, port := startMockTikvService()
+	c.Assert(port > 0, IsTrue)
+
+	cfg := config.GetGlobalConfig().TiKVClient
+	cfg.GrpcConnectionCount = 1
+	rpcClient := newRPCClient(cfg, config.Security{})
+
+	addr := fmt.Sprintf("%s:%d", "127.0.0.1", port)
+	conn, err := rpcClient.getConnArray(addr)
+	c.Assert(err, IsNil)
+
+	conn.Close()
+
+	req := &tikvrpc.Request{
+		Type:  tikvrpc.CmdEmpty,
+		Empty: &tikvpb.BatchCommandsEmptyRequest{},
+	}
+	batchReq := req.ToBatchCommandsRequest()
+	c.Assert(batchReq, NotNil)
+
+	doRPCForBatchRequest(context.Background(), addr, conn.batchConn, batchReq, time.Second)
+	_, err = rpcClient.SendRequest(context.Background(), addr, req, time.Second)
+	c.Assert(err.Error() == "send to closed transport", IsTrue)
+
+	server.Stop()
 }
