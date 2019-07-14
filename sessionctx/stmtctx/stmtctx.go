c8ed781d5133b855199e604f59561f0cfa9f3e10
Maxwell <passagesfall@gmail.com>
2019-07-14 17:58:49 +0800

    executor: push-down LoadDataStmt in distsql (#11067)
    moving function `statementContextToFlags` to `StatementContext.PushDownFlags()`
    
    Co-authored-by: Lonng <heng@lonng.org>
    Co-authored-by: Foreyes <foreyes_1002@163.com>

--- sessionctx/stmtctx/stmtctx.go
+++ sessionctx/stmtctx/stmtctx.go
@@ -21,6 +21,7 @@ import (
 	"time"
 
 	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/model"
 	"github.com/pingcap/parser/mysql"
 	"github.com/pingcap/tidb/util/execdetails"
 	"github.com/pingcap/tidb/util/memory"
@@ -436,6 +437,39 @@ func (sc *StatementContext) ShouldIgnoreOverflowError() bool {
 		return true
 	}
 	return false
+}
+
+// PushDownFlags converts StatementContext to tipb.SelectRequest.Flags.
+func (sc *StatementContext) PushDownFlags() uint64 {
+	var flags uint64
+	if sc.InInsertStmt {
+		flags |= model.FlagInInsertStmt
+	} else if sc.InUpdateStmt || sc.InDeleteStmt {
+		flags |= model.FlagInUpdateOrDeleteStmt
+	} else if sc.InSelectStmt {
+		flags |= model.FlagInSelectStmt
+	}
+	if sc.IgnoreTruncate {
+		flags |= model.FlagIgnoreTruncate
+	} else if sc.TruncateAsWarning {
+		flags |= model.FlagTruncateAsWarning
+	}
+	if sc.OverflowAsWarning {
+		flags |= model.FlagOverflowAsWarning
+	}
+	if sc.IgnoreZeroInDate {
+		flags |= model.FlagIgnoreZeroInDate
+	}
+	if sc.DividedByZeroAsWarning {
+		flags |= model.FlagDividedByZeroAsWarning
+	}
+	if sc.PadCharToFullLength {
+		flags |= model.FlagPadCharToFullLength
+	}
+	if sc.InLoadDataStmt {
+		flags |= model.FlagInLoadDataStmt
+	}
+	return flags
 }
 
 // CopTasksDetails returns some useful information of cop-tasks during execution.
