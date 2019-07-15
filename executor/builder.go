7aad2e8f9bd6c0b1c6fb1f2736d4c7fde07b9f86
crazycs <crazycs520@gmail.com>
2019-07-15 19:14:33 +0800

    *: add show table regions syntax (#8719) (#10612) (#11238)

--- executor/builder.go
+++ executor/builder.go
@@ -516,6 +516,7 @@ func (b *executorBuilder) buildShow(v *plannercore.Show) Executor {
 		DBName:       model.NewCIStr(v.DBName),
 		Table:        v.Table,
 		Column:       v.Column,
+		IndexName:    v.IndexName,
 		User:         v.User,
 		Flag:         v.Flag,
 		Full:         v.Full,
