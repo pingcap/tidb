e2fde73a2eeef0ac59a9deed7dc7a8f6feaad2d1
amyangfei <amyangfei@gmail.com>
2019-07-16 10:09:45 +0800

    fix error check

--- util/mock/context.go
+++ util/mock/context.go
@@ -25,7 +25,6 @@ import (
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/owner"
 	"github.com/pingcap/tidb/sessionctx"
-	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util"
 	"github.com/pingcap/tidb/util/kvcache"
@@ -264,7 +263,6 @@ func NewContext() *Context {
 	sctx.sessionVars.MaxChunkSize = 32
 	sctx.sessionVars.StmtCtx.TimeZone = time.UTC
 	sctx.sessionVars.GlobalVarsAccessor = variable.NewMockGlobalAccessor()
-	sctx.sessionVars.SetSystemVar(variable.MaxAllowedPacket, "67108864")
 	return sctx
 }
 
