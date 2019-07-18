c3d195cab63a46ea3a931081c4758fc1e167d5ae
wjhuang2016 <huangwenjun1997@gmail.com>
2019-07-18 18:47:42 +0800

    planner: reject invalid conversion from like to =

--- planner/core/physical_plan_test.go
+++ planner/core/physical_plan_test.go
@@ -1170,11 +1170,10 @@ func (s *testPlanSuite) TestRefine(c *C) {
 			sql:  `select a from t where c_str like 123`,
 			best: "IndexReader(Index(t.c_d_e_str)[[\"123\",\"123\"]])->Projection",
 		},
-		// c is type int which will be added cast to specified type when building function signature,
-		// and rewrite predicate like to predicate '='  when exact match , index still can be used.
+		// c is type int which will be added cast to specified type when building function signature, no index can be used.
 		{
 			sql:  `select a from t where c like '1'`,
-			best: "IndexReader(Index(t.c_d_e)[[1,1]])->Projection",
+			best: "TableReader(Table(t))->Sel([like(cast(test.t.c), 1, 92)])->Projection",
 		},
 		{
 			sql:  `select a from t where c = 1.9 and d > 3`,
