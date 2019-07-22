3f0bb09e3e00772991dbb88e7b752ad3666b7135
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2019-07-22 11:20:32 +0800

    update some comments

--- util/chunk/chunk_test.go
+++ util/chunk/chunk_test.go
@@ -964,7 +964,7 @@ func (b *benchChunkGrowCase) String() string {
 	if b.reuse {
 		buff.WriteString("renew,")
 	} else {
-		buff.WriteString("Reset,")
+		buff.WriteString("reset,")
 	}
 	buff.WriteString("cntPerCall:" + strconv.Itoa(b.cntPerCall) + ",")
 	buff.WriteString("cap from:" + strconv.Itoa(b.initCap) + " to " + strconv.Itoa(b.maxCap) + ",")
