a650de8980e7fae487f7a6b042b54fbd93129808
Haibin Xie <lambdax.tyler@gmail.com>
2019-07-23 16:35:56 +0800

    [parser] parser: support more analyze options (#388)

--- parser/ast/stats.go
+++ parser/ast/stats.go
@@ -32,11 +32,36 @@ type AnalyzeTableStmt struct {
 	TableNames     []*TableName
 	PartitionNames []model.CIStr
 	IndexNames     []model.CIStr
-	MaxNumBuckets  uint64
+	AnalyzeOpts    []AnalyzeOpt
 
 	// IndexFlag is true when we only analyze indices for a table.
 	IndexFlag   bool
 	Incremental bool
+}
+
+// AnalyzeOptType is the type for analyze options.
+type AnalyzeOptionType int
+
+// Analyze option types.
+const (
+	AnalyzeOptNumBuckets = iota
+	AnalyzeOptNumTopN
+	AnalyzeOptCMSketchDepth
+	AnalyzeOptCMSketchWidth
+)
+
+// AnalyzeOptionString stores the string form of analyze options.
+var AnalyzeOptionString = map[AnalyzeOptionType]string{
+	AnalyzeOptNumBuckets:    "BUCKETS",
+	AnalyzeOptNumTopN:       "TOPN",
+	AnalyzeOptCMSketchWidth: "CMSKETCH WIDTH",
+	AnalyzeOptCMSketchDepth: "CMSKETCH DEPTH",
+}
+
+// AnalyzeOpt stores the analyze option type and value.
+type AnalyzeOpt struct {
+	Type  AnalyzeOptionType
+	Value uint64
 }
 
 // Restore implements Node interface.
@@ -74,10 +99,15 @@ func (n *AnalyzeTableStmt) Restore(ctx *RestoreCtx) error {
 		}
 		ctx.WriteName(index.O)
 	}
-	if n.MaxNumBuckets != 0 {
-		ctx.WriteKeyWord(" WITH ")
-		ctx.WritePlainf("%d", n.MaxNumBuckets)
-		ctx.WriteKeyWord(" BUCKETS")
+	if len(n.AnalyzeOpts) != 0 {
+		ctx.WriteKeyWord(" WITH")
+		for i, opt := range n.AnalyzeOpts {
+			if i != 0 {
+				ctx.WritePlain(",")
+			}
+			ctx.WritePlainf(" %d ", opt.Value)
+			ctx.WritePlain(AnalyzeOptionString[opt.Type])
+		}
 	}
 	return nil
 }
