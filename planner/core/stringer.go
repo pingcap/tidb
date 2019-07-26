0dd6760bc1e9669a8162d3c1680f585f0626b291
Kenan Yao <cauchy1992@gmail.com>
2019-07-26 15:35:10 +0800

    planner: support subquery in `SHOW` statement (#10942) (#11459)

--- planner/core/stringer.go
+++ planner/core/stringer.go
@@ -114,11 +114,7 @@ func toString(in Plan, strs []string, idxs []int) ([]string, []int) {
 	case *ShowDDL:
 		str = "ShowDDL"
 	case *Show:
-		if len(x.Conditions) == 0 {
-			str = "Show"
-		} else {
-			str = fmt.Sprintf("Show(%s)", x.Conditions)
-		}
+		str = "Show"
 	case *LogicalSort, *PhysicalSort:
 		str = "Sort"
 	case *LogicalJoin:
