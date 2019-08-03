4621738383dd6a31979fc8685c13aedf3604e110
kennytm <kennytm@gmail.com>
2019-08-03 16:27:11 +0800

    [parser] ast,parser: added several expression types to represent keyword arguments (#142)
    * ast,parser: added several expression types to represent keyword arguments
    
    Added TimeUnitExpr to represent time and timestamp units like SECOND in
    DATE_ADD(time, INTERVAL x SECOND).
    
    Added GetFormatSelectorExpr to represent the selectors like DATETIME in
    GET_FORMAT(DATETIME, locale).
    
    Added TrimDirectionExpr to represent the direction like BOTH in
    TRIM(BOTH 'x' FROM 'xyz').
    
    Changed FrameBound's Unit to use TimeUnitType directly.
    
    Previously these were all converted into a string or integer ValueExpr,
    which made Restore() implementation very ugly.
    
    * ast: change type of PartitionMethod.Unit from ValueExpr to TimeUnitType

--- parser/ast/ddl.go
+++ parser/ast/ddl.go
@@ -14,8 +14,6 @@
 package ast
 
 import (
-	"strings"
-
 	"github.com/pingcap/errors"
 	"github.com/pingcap/parser/auth"
 	. "github.com/pingcap/parser/format"
@@ -2448,7 +2446,7 @@ type PartitionMethod struct {
 	// RANGE COLUMNS and LIST COLUMNS types
 	ColumnNames []*ColumnName
 	// Unit is a time unit used as argument of SYSTEM_TIME type
-	Unit ValueExpr
+	Unit TimeUnitType
 	// Limit is a row count used as argument of the SYSTEM_TIME type
 	Limit uint64
 
@@ -2465,20 +2463,13 @@ func (n *PartitionMethod) Restore(ctx *RestoreCtx) error {
 
 	switch {
 	case n.Tp == model.PartitionTypeSystemTime:
-		if n.Expr != nil && n.Unit != nil {
+		if n.Expr != nil && n.Unit != TimeUnitInvalid {
 			ctx.WriteKeyWord(" INTERVAL ")
 			if err := n.Expr.Restore(ctx); err != nil {
 				return errors.Annotate(err, "An error occurred while restore PartitionMethod.Expr")
 			}
-
-			// Here the Unit string should not be quoted.
-			// TODO: This is a temporary workaround that should be changed once something like "Keyword Expression" is implemented.
-			var sb strings.Builder
-			if err := n.Unit.Restore(NewRestoreCtx(0, &sb)); err != nil {
-				return errors.Annotate(err, "An error occurred while restore PartitionMethod.Unit")
-			}
 			ctx.WritePlain(" ")
-			ctx.WriteKeyWord(sb.String())
+			ctx.WriteKeyWord(n.Unit.String())
 		}
 
 	case n.Expr != nil:
@@ -2527,13 +2518,6 @@ func (n *PartitionMethod) acceptInPlace(v Visitor) bool {
 			return false
 		}
 		n.ColumnNames[i] = newColName.(*ColumnName)
-	}
-	if n.Unit != nil {
-		unit, ok := n.Unit.Accept(v)
-		if !ok {
-			return false
-		}
-		n.Unit = unit.(ValueExpr)
 	}
 	return true
 }
