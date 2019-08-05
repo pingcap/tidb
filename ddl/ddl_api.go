390f99e799584492b951e7267249134de303dd9e
AilinKid <314806019@qq.com>
2019-08-05 09:57:05 +0800

    Make map with capacity

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -2109,7 +2109,7 @@ func (d *ddl) AddColumn(ctx sessionctx.Context, ti ast.Ident, spec *ast.AlterTab
 			if err = checkAutoIncrementRef(specNewColumn.Name.Name.L, dependColNames, t.Meta()); err != nil {
 				return errors.Trace(err)
 			}
-			duplicateColNames := make(map[string]struct{})
+			duplicateColNames := make(map[string]struct{}, len(dependColNames))
 			for k := range dependColNames {
 				duplicateColNames[k] = struct{}{}
 			}
