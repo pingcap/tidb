3aad6c7d5da035211c3b2e3ebea44e2050dbc71a
Feng Liyuan <darktemplar.f@gmail.com>
2019-08-06 11:38:31 +0800

    make script compatible

--- tools/check/check_testSuite.sh
+++ tools/check/check_testSuite.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 exitCode=0
-for testSuite in `find -name "*_test.go" | xargs grep -P "type test(.*)Suite" | awk '{print $2}'`; do
+for testSuite in `find . -name "*_test.go" -print0 | xargs -0 grep -P "type test(.*)Suite" | awk '{print $2}'`; do
 	# TODO: ugly regex
 	# TODO: check code comment
-	find -name "*_test.go" | xargs grep -P "_ = (check\.)?(Suite|SerialSuites)\((&?${testSuite}{|new\(${testSuite}\))" > /dev/null
-	[[ $? != 0 ]] && find -name "*_test.go" | xargs grep "func (s \*${testSuite}) Test" > /dev/null
-	[[ $? == 0 ]] && echo "${testSuite} is not enabled" && exitCode=1
+	if ! find . -name "*_test.go" -print0 | xargs -0 grep -P "_ = (check\.)?(Suite|SerialSuites)\((&?${testSuite}{|new\(${testSuite}\))" > /dev/null
+	then
+	  if find . -name "*_test.go" -print0 | xargs -0 grep "func (s \*${testSuite}) Test" > /dev/null
+    then
+      echo "${testSuite} is not enabled" && exitCode=1
+    fi
+  fi
 done
 exit ${exitCode}
