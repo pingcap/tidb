04ad5ba0e3e80606249368b7ca2189d65caacb03
Haibin Xie <lambdax.tyler@gmail.com>
2019-08-06 17:16:26 +0800

    *: support bindings for prepare statement

--- bindinfo/bind.go
+++ bindinfo/bind.go
@@ -15,155 +15,66 @@ package bindinfo
 
 import "github.com/pingcap/parser/ast"
 
-// BindHint will add hints for originStmt according to hintedStmt' hints.
-func BindHint(originStmt, hintedStmt ast.StmtNode) ast.StmtNode {
-	switch x := originStmt.(type) {
+// HintsSet contains all hints of a query.
+type HintsSet struct {
+	tableHints map[int64][]*ast.TableOptimizerHint
+	indexHints map[int64][]*ast.IndexHint
+}
+
+type hintCollector struct {
+	tableCounter int64
+	indexCounter int64
+	HintsSet
+}
+
+// CollectHint collects hints for a statement.
+func CollectHint(in ast.StmtNode) *HintsSet {
+	hc := hintCollector{HintsSet: HintsSet{tableHints: make(map[int64][]*ast.TableOptimizerHint), indexHints: make(map[int64][]*ast.IndexHint)}}
+	in.Accept(&hc)
+	return &hc.HintsSet
+}
+
+func (hc *hintCollector) Enter(in ast.Node) (ast.Node, bool) {
+	switch v := in.(type) {
 	case *ast.SelectStmt:
-		return selectBind(x, hintedStmt.(*ast.SelectStmt))
-	default:
-		return originStmt
+		hc.tableHints[hc.tableCounter] = v.TableHints
+		hc.tableCounter++
+	case *ast.TableName:
+		hc.indexHints[hc.indexCounter] = v.IndexHints
+		hc.indexCounter++
 	}
+	return in, false
 }
 
-func selectBind(originalNode, hintedNode *ast.SelectStmt) *ast.SelectStmt {
-	if hintedNode.TableHints != nil {
-		originalNode.TableHints = hintedNode.TableHints
-	}
-	if originalNode.From != nil {
-		originalNode.From.TableRefs = resultSetNodeBind(originalNode.From.TableRefs, hintedNode.From.TableRefs).(*ast.Join)
-	}
-	if originalNode.Where != nil {
-		originalNode.Where = exprBind(originalNode.Where, hintedNode.Where).(ast.ExprNode)
-	}
-
-	if originalNode.Having != nil {
-		originalNode.Having.Expr = exprBind(originalNode.Having.Expr, hintedNode.Having.Expr)
-	}
-
-	if originalNode.OrderBy != nil {
-		originalNode.OrderBy = orderByBind(originalNode.OrderBy, hintedNode.OrderBy)
-	}
-
-	if originalNode.Fields != nil {
-		origFields := originalNode.Fields.Fields
-		hintFields := hintedNode.Fields.Fields
-		for idx := range origFields {
-			origFields[idx].Expr = exprBind(origFields[idx].Expr, hintFields[idx].Expr)
-		}
-	}
-	return originalNode
+func (hc *hintCollector) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
 }
 
-func orderByBind(originalNode, hintedNode *ast.OrderByClause) *ast.OrderByClause {
-	for idx := 0; idx < len(originalNode.Items); idx++ {
-		originalNode.Items[idx].Expr = exprBind(originalNode.Items[idx].Expr, hintedNode.Items[idx].Expr)
-	}
-	return originalNode
+type hintBinder struct {
+	tableCounter int64
+	indexCounter int64
+	*HintsSet
 }
 
-func exprBind(originalNode, hintedNode ast.ExprNode) ast.ExprNode {
-	switch v := originalNode.(type) {
-	case *ast.SubqueryExpr:
-		if v.Query != nil {
-			v.Query = resultSetNodeBind(v.Query, hintedNode.(*ast.SubqueryExpr).Query)
-		}
-	case *ast.ExistsSubqueryExpr:
-		if v.Sel != nil {
-			v.Sel.(*ast.SubqueryExpr).Query = resultSetNodeBind(v.Sel.(*ast.SubqueryExpr).Query, hintedNode.(*ast.ExistsSubqueryExpr).Sel.(*ast.SubqueryExpr).Query)
-		}
-	case *ast.PatternInExpr:
-		if v.Sel != nil {
-			v.Sel.(*ast.SubqueryExpr).Query = resultSetNodeBind(v.Sel.(*ast.SubqueryExpr).Query, hintedNode.(*ast.PatternInExpr).Sel.(*ast.SubqueryExpr).Query)
-		}
-	case *ast.BinaryOperationExpr:
-		if v.L != nil {
-			v.L = exprBind(v.L, hintedNode.(*ast.BinaryOperationExpr).L)
-		}
-		if v.R != nil {
-			v.R = exprBind(v.R, hintedNode.(*ast.BinaryOperationExpr).R)
-		}
-	case *ast.IsNullExpr:
-		if v.Expr != nil {
-			v.Expr = exprBind(v.Expr, hintedNode.(*ast.IsNullExpr).Expr)
-		}
-	case *ast.IsTruthExpr:
-		if v.Expr != nil {
-			v.Expr = exprBind(v.Expr, hintedNode.(*ast.IsTruthExpr).Expr)
-		}
-	case *ast.PatternLikeExpr:
-		if v.Pattern != nil {
-			v.Pattern = exprBind(v.Pattern, hintedNode.(*ast.PatternLikeExpr).Pattern)
-		}
-	case *ast.CompareSubqueryExpr:
-		if v.L != nil {
-			v.L = exprBind(v.L, hintedNode.(*ast.CompareSubqueryExpr).L)
-		}
-		if v.R != nil {
-			v.R = exprBind(v.R, hintedNode.(*ast.CompareSubqueryExpr).R)
-		}
-	case *ast.BetweenExpr:
-		if v.Left != nil {
-			v.Left = exprBind(v.Left, hintedNode.(*ast.BetweenExpr).Left)
-		}
-		if v.Right != nil {
-			v.Right = exprBind(v.Right, hintedNode.(*ast.BetweenExpr).Right)
-		}
-	case *ast.UnaryOperationExpr:
-		if v.V != nil {
-			v.V = exprBind(v.V, hintedNode.(*ast.UnaryOperationExpr).V)
-		}
-	case *ast.CaseExpr:
-		if v.Value != nil {
-			v.Value = exprBind(v.Value, hintedNode.(*ast.CaseExpr).Value)
-		}
-		if v.ElseClause != nil {
-			v.ElseClause = exprBind(v.ElseClause, hintedNode.(*ast.CaseExpr).ElseClause)
-		}
-	}
-	return originalNode
-}
-
-func resultSetNodeBind(originalNode, hintedNode ast.ResultSetNode) ast.ResultSetNode {
-	switch x := originalNode.(type) {
-	case *ast.Join:
-		return joinBind(x, hintedNode.(*ast.Join))
-	case *ast.TableSource:
-		ts, _ := hintedNode.(*ast.TableSource)
-		switch v := x.Source.(type) {
-		case *ast.SelectStmt:
-			x.Source = selectBind(v, ts.Source.(*ast.SelectStmt))
-		case *ast.UnionStmt:
-			x.Source = unionSelectBind(v, hintedNode.(*ast.TableSource).Source.(*ast.UnionStmt))
-		case *ast.TableName:
-			x.Source.(*ast.TableName).IndexHints = ts.Source.(*ast.TableName).IndexHints
-		}
-		return x
+func (hc *hintBinder) Enter(in ast.Node) (ast.Node, bool) {
+	switch v := in.(type) {
 	case *ast.SelectStmt:
-		return selectBind(x, hintedNode.(*ast.SelectStmt))
-	case *ast.UnionStmt:
-		return unionSelectBind(x, hintedNode.(*ast.UnionStmt))
-	default:
-		return x
+		v.TableHints = hc.tableHints[hc.tableCounter]
+		hc.tableCounter++
+	case *ast.TableName:
+		v.IndexHints = hc.indexHints[hc.indexCounter]
+		hc.indexCounter++
 	}
+	return in, false
 }
 
-func joinBind(originalNode, hintedNode *ast.Join) *ast.Join {
-	if originalNode.Left != nil {
-		originalNode.Left = resultSetNodeBind(originalNode.Left, hintedNode.Left)
-	}
-
-	if hintedNode.Right != nil {
-		originalNode.Right = resultSetNodeBind(originalNode.Right, hintedNode.Right)
-	}
-
-	return originalNode
+func (hc *hintBinder) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
 }
 
-func unionSelectBind(originalNode, hintedNode *ast.UnionStmt) ast.ResultSetNode {
-	selects := originalNode.SelectList.Selects
-	for i := len(selects) - 1; i >= 0; i-- {
-		originalNode.SelectList.Selects[i] = selectBind(selects[i], hintedNode.SelectList.Selects[i])
-	}
-
-	return originalNode
+// BindHint will add hints for originStmt according to the hints in `bindMeta`.
+func BindHint(stmt ast.StmtNode, hintsSet *HintsSet) ast.StmtNode {
+	hb := hintBinder{HintsSet: hintsSet}
+	stmt.Accept(&hb)
+	return stmt
 }
--- planner/optimize.go
+++ planner/optimize.go
@@ -15,13 +15,20 @@ package planner
 
 import (
 	"context"
+	"strings"
 
+	"github.com/pingcap/parser"
 	"github.com/pingcap/parser/ast"
+	"github.com/pingcap/tidb/bindinfo"
+	"github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/infoschema"
+	"github.com/pingcap/tidb/metrics"
 	"github.com/pingcap/tidb/planner/cascades"
 	plannercore "github.com/pingcap/tidb/planner/core"
 	"github.com/pingcap/tidb/privilege"
 	"github.com/pingcap/tidb/sessionctx"
+	"github.com/pingcap/tidb/util/logutil"
+	"go.uber.org/zap"
 )
 
 // Optimize does optimization and creates a Plan.
@@ -32,6 +39,26 @@ func Optimize(ctx context.Context, sctx sessionctx.Context, node ast.Node, is in
 		return fp, nil
 	}
 
+	var oriHint *bindinfo.HintsSet
+	if stmtNode, ok := node.(ast.StmtNode); ok {
+		node, oriHint = addHint(sctx, stmtNode)
+	}
+	plan, err := optimize(ctx, sctx, node, is)
+	// Restore the original hint in case of prepare stmt.
+	if oriHint != nil {
+		node = bindinfo.BindHint(node.(ast.StmtNode), oriHint)
+		if err != nil {
+			handleInvalidBindRecord(ctx, sctx, node.(ast.StmtNode))
+		}
+	}
+	if err == nil || oriHint == nil {
+		return plan, err
+	}
+	// Reoptimize after restore the original hint.
+	return optimize(ctx, sctx, node, is)
+}
+
+func optimize(ctx context.Context, sctx sessionctx.Context, node ast.Node, is infoschema.InfoSchema) (plannercore.Plan, error) {
 	// build logical plan
 	sctx.GetSessionVars().PlanID = 0
 	sctx.GetSessionVars().PlanColumnID = 0
@@ -73,6 +100,111 @@ func Optimize(ctx context.Context, sctx sessionctx.Context, node ast.Node, is in
 		return cascades.FindBestPlan(sctx, logic)
 	}
 	return plannercore.DoOptimize(ctx, builder.GetOptFlag(), logic)
+}
+
+func addHint(ctx sessionctx.Context, stmtNode ast.StmtNode) (ast.StmtNode, *bindinfo.HintsSet) {
+	// When the domain is initializing, the bind will be nil.
+	if ctx.Value(bindinfo.SessionBindInfoKeyType) == nil {
+		return stmtNode, nil
+	}
+	switch x := stmtNode.(type) {
+	case *ast.ExplainStmt:
+		var oriHintSet *bindinfo.HintsSet
+		switch x.Stmt.(type) {
+		case *ast.SelectStmt:
+			normalizeExplainSQL := parser.Normalize(x.Text())
+			idx := strings.Index(normalizeExplainSQL, "select")
+			normalizeSQL := normalizeExplainSQL[idx:]
+			hash := parser.DigestHash(normalizeSQL)
+			x.Stmt, oriHintSet = addHintForSelect(hash, normalizeSQL, ctx, x.Stmt)
+		}
+		return x, oriHintSet
+	case *ast.SelectStmt:
+		normalizeSQL, hash := parser.NormalizeDigest(x.Text())
+		return addHintForSelect(hash, normalizeSQL, ctx, x)
+	default:
+		return stmtNode, nil
+	}
+}
+
+func addHintForSelect(hash, normdOrigSQL string, ctx sessionctx.Context, stmt ast.StmtNode) (ast.StmtNode, *bindinfo.HintsSet) {
+	sessionHandle := ctx.Value(bindinfo.SessionBindInfoKeyType).(*bindinfo.SessionHandle)
+	bindRecord := sessionHandle.GetBindRecord(normdOrigSQL, ctx.GetSessionVars().CurrentDB)
+	if bindRecord != nil {
+		if bindRecord.Status == bindinfo.Invalid {
+			return stmt, nil
+		}
+		if bindRecord.Status == bindinfo.Using {
+			metrics.BindUsageCounter.WithLabelValues(metrics.ScopeSession).Inc()
+			oriHint := bindinfo.CollectHint(stmt)
+			return bindinfo.BindHint(stmt, bindRecord.HintsSet), oriHint
+		}
+	}
+	globalHandle := domain.GetDomain(ctx).BindHandle()
+	bindRecord = globalHandle.GetBindRecord(hash, normdOrigSQL, ctx.GetSessionVars().CurrentDB)
+	if bindRecord == nil {
+		bindRecord = globalHandle.GetBindRecord(hash, normdOrigSQL, "")
+	}
+	if bindRecord != nil {
+		metrics.BindUsageCounter.WithLabelValues(metrics.ScopeGlobal).Inc()
+		oriHint := bindinfo.CollectHint(stmt)
+		return bindinfo.BindHint(stmt, bindRecord.HintsSet), oriHint
+	}
+	return stmt, nil
+}
+
+func handleInvalidBindRecord(ctx context.Context, sctx sessionctx.Context, stmtNode ast.StmtNode) {
+	var normdOrigSQL, hash string
+	switch x := stmtNode.(type) {
+	case *ast.ExplainStmt:
+		switch x.Stmt.(type) {
+		case *ast.SelectStmt:
+			normalizeExplainSQL := parser.Normalize(x.Text())
+			idx := strings.Index(normalizeExplainSQL, "select")
+			normdOrigSQL = normalizeExplainSQL[idx:]
+			hash = parser.DigestHash(normdOrigSQL)
+		default:
+			return
+		}
+	case *ast.SelectStmt:
+		normdOrigSQL, hash = parser.NormalizeDigest(x.Text())
+	default:
+		return
+	}
+	sessionHandle := sctx.Value(bindinfo.SessionBindInfoKeyType).(*bindinfo.SessionHandle)
+	bindMeta := sessionHandle.GetBindRecord(normdOrigSQL, sctx.GetSessionVars().CurrentDB)
+	if bindMeta != nil {
+		bindMeta.Status = bindinfo.Invalid
+		return
+	}
+
+	globalHandle := domain.GetDomain(sctx).BindHandle()
+	bindMeta = globalHandle.GetBindRecord(hash, normdOrigSQL, sctx.GetSessionVars().CurrentDB)
+	if bindMeta == nil {
+		bindMeta = globalHandle.GetBindRecord(hash, normdOrigSQL, "")
+	}
+	if bindMeta != nil {
+		record := &bindinfo.BindRecord{
+			OriginalSQL: bindMeta.OriginalSQL,
+			BindSQL:     bindMeta.BindSQL,
+			Db:          sctx.GetSessionVars().CurrentDB,
+			Charset:     bindMeta.Charset,
+			Collation:   bindMeta.Collation,
+			Status:      bindinfo.Invalid,
+		}
+
+		err := sessionHandle.AddBindRecord(record)
+		if err != nil {
+			logutil.Logger(ctx).Warn("handleInvalidBindRecord failed", zap.Error(err))
+		}
+
+		globalHandle := domain.GetDomain(sctx).BindHandle()
+		dropBindRecord := &bindinfo.BindRecord{
+			OriginalSQL: bindMeta.OriginalSQL,
+			Db:          bindMeta.Db,
+		}
+		globalHandle.AddDropInvalidBindTask(dropBindRecord)
+	}
 }
 
 func init() {
