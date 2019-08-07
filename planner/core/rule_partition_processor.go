eca09b1f2f2fdf7a200c31afa018d2b41bd60fd2
Zhuomin(Charming) Liu <lzmhhh123@gmail.com>
2019-08-07 19:10:07 +0800

    *: add opt_rule_blacklist in mysql tables. (#11096) (#11658)

--- planner/core/rule_partition_processor.go
+++ planner/core/rule_partition_processor.go
@@ -206,3 +206,7 @@ func (s *partitionProcessor) findByName(partitionNames []model.CIStr, partitionN
 	}
 	return false
 }
+
+func (*partitionProcessor) name() string {
+	return "partition_processor"
+}
