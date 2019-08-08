3f4ad82774121fb4c19eaac359f45d3e80212f28
lysu <sulifx@gmail.com>
2019-08-08 14:57:41 +0800

    *: support new 'log' format for trace

--- util/logutil/log.go
+++ util/logutil/log.go
@@ -24,6 +24,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/opentracing/opentracing-go"
+	tlog "github.com/opentracing/opentracing-go/log"
 	"github.com/pingcap/errors"
 	zaplog "github.com/pingcap/log"
 	log "github.com/sirupsen/logrus"
@@ -355,4 +357,27 @@ func WithKeyValue(ctx context.Context, key, value string) context.Context {
 		logger = zaplog.L()
 	}
 	return context.WithValue(ctx, ctxLogKey, logger.With(zap.String(key, value)))
+}
+
+const TraceEventKey = "event"
+
+// Event records event in current tracing span.
+func Event(ctx context.Context, event string) {
+	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
+		span.LogFields(tlog.String(TraceEventKey, event))
+	}
+}
+
+// Eventf records event in current tracing span with format support.
+func Eventf(ctx context.Context, format string, args ...interface{}) {
+	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
+		span.LogFields(tlog.String(TraceEventKey, fmt.Sprintf(format, args...)))
+	}
+}
+
+// SetTag sets tag kv-pair in current tracing span
+func SetTag(ctx context.Context, key string, value interface{}) {
+	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
+		span.SetTag(key, value)
+	}
 }
