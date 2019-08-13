86ad1a96c3a626dd69d8769cd5f8f7196c0c7a56
Lynn <zimu_xia@126.com>
2019-08-13 14:06:11 +0800

    *: speed up the operation of "admin check table" (#8572, #1156… (#11676)

--- util/testkit/testkit.go
+++ util/testkit/testkit.go
@@ -214,6 +214,15 @@ func (tk *TestKit) QueryToErr(sql string, args ...interface{}) error {
 	return resErr
 }
 
+// ExecToErr executes a sql statement and discard results.
+func (tk *TestKit) ExecToErr(sql string, args ...interface{}) error {
+	res, err := tk.Exec(sql, args...)
+	if res != nil {
+		tk.c.Assert(res.Close(), check.IsNil)
+	}
+	return err
+}
+
 // ResultSetToResult converts sqlexec.RecordSet to testkit.Result.
 // It is used to check results of execute statement in binary mode.
 func (tk *TestKit) ResultSetToResult(rs sqlexec.RecordSet, comment check.CommentInterface) *Result {
