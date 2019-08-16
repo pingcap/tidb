815fc3925d21e5e16357c7af0ea1e87e7ebebc2b
Mingcong Han <hanmingcong123@hotmail.com>
2019-08-16 11:07:59 +0800

    cherry pick #11749 to release-3.0
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- planner/core/physical_plan_test.go
+++ planner/core/physical_plan_test.go
@@ -221,6 +221,7 @@ func (s *testPlanSuite) TestDAGPlanBuilderBasePhysicalPlan(c *C) {
 		stmt, err := s.ParseOneStmt(tt, "", "")
 		c.Assert(err, IsNil, comment)
 
+<<<<<<< HEAD
 		core.Preprocess(se, stmt, s.is)
 		p, err := planner.Optimize(context.TODO(), se, stmt, s.is)
 		c.Assert(err, IsNil)
@@ -229,6 +230,401 @@ func (s *testPlanSuite) TestDAGPlanBuilderBasePhysicalPlan(c *C) {
 			output[i].Best = core.ToString(p)
 		})
 		c.Assert(core.ToString(p), Equals, output[i].Best, Commentf("for %s", tt))
+=======
+		err = se.NewTxn(context.Background())
+		c.Assert(err, IsNil)
+		// Make txn not read only.
+		txn, err := se.Txn(true)
+		c.Assert(err, IsNil)
+		txn.Set(kv.Key("AAA"), []byte("BBB"))
+		c.Assert(se.StmtCommit(), IsNil)
+		p, err := planner.Optimize(context.TODO(), se, stmt, s.is)
+		c.Assert(err, IsNil)
+		c.Assert(core.ToString(p), Equals, tt.best, Commentf("for %s", tt.sql))
+	}
+}
+
+func (s *testPlanSuite) TestDAGPlanBuilderAgg(c *C) {
+	defer testleak.AfterTest(c)()
+	store, dom, err := newStoreWithBootstrap()
+	c.Assert(err, IsNil)
+	defer func() {
+		dom.Close()
+		store.Close()
+	}()
+	se, err := session.CreateSession4Test(store)
+	c.Assert(err, IsNil)
+	se.Execute(context.Background(), "use test")
+	se.Execute(context.Background(), "set sql_mode='STRICT_TRANS_TABLES'") // disable only full group by
+	c.Assert(err, IsNil)
+	ctx := se.(sessionctx.Context)
+	sessionVars := ctx.GetSessionVars()
+	sessionVars.HashAggFinalConcurrency = 1
+	sessionVars.HashAggPartialConcurrency = 1
+
+	tests := []struct {
+		sql  string
+		best string
+	}{
+		// Test distinct.
+		{
+			sql:  "select distinct b from t",
+			best: "TableReader(Table(t)->HashAgg)->HashAgg",
+		},
+		{
+			sql:  "select count(*) from (select * from t order by b) t group by b",
+			best: "TableReader(Table(t))->Sort->StreamAgg",
+		},
+		{
+			sql:  "select count(*), x from (select b as bbb, a + 1 as x from (select * from t order by b) t) t group by bbb",
+			best: "TableReader(Table(t))->Sort->Projection->StreamAgg",
+		},
+		// Test agg + table.
+		{
+			sql:  "select sum(a), avg(b + c) from t group by d",
+			best: "TableReader(Table(t)->HashAgg)->HashAgg",
+		},
+		{
+			sql:  "select sum(distinct a), avg(b + c) from t group by d",
+			best: "TableReader(Table(t))->Projection->HashAgg",
+		},
+		//  Test group by (c + d)
+		{
+			sql:  "select sum(e), avg(e + c) from t where c = 1 group by (c + d)",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]]->HashAgg)->HashAgg",
+		},
+		// Test stream agg + index single.
+		{
+			sql:  "select sum(e), avg(e + c) from t where c = 1 group by c",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]]->StreamAgg)->StreamAgg",
+		},
+		// Test hash agg + index single.
+		{
+			sql:  "select sum(e), avg(e + c) from t where c = 1 group by e",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]]->HashAgg)->HashAgg",
+		},
+		// Test hash agg + index double.
+		{
+			sql:  "select sum(e), avg(b + c) from t where c = 1 and e = 1 group by d",
+			best: "IndexLookUp(Index(t.c_d_e)[[1,1]]->Sel([eq(test.t.e, 1)]), Table(t))->Projection->HashAgg",
+		},
+		// Test stream agg + index double.
+		{
+			sql:  "select sum(e), avg(b + c) from t where c = 1 and b = 1",
+			best: "IndexLookUp(Index(t.c_d_e)[[1,1]], Table(t)->Sel([eq(test.t.b, 1)]))->Projection->StreamAgg",
+		},
+		// Test hash agg + order.
+		{
+			sql:  "select sum(e) as k, avg(b + c) from t where c = 1 and b = 1 and e = 1 group by d order by k",
+			best: "IndexLookUp(Index(t.c_d_e)[[1,1]]->Sel([eq(test.t.e, 1)]), Table(t)->Sel([eq(test.t.b, 1)]))->Projection->Projection->StreamAgg->Sort",
+		},
+		// Test stream agg + order.
+		{
+			sql:  "select sum(e) as k, avg(b + c) from t where c = 1 and b = 1 and e = 1 group by c order by k",
+			best: "IndexLookUp(Index(t.c_d_e)[[1,1]]->Sel([eq(test.t.e, 1)]), Table(t)->Sel([eq(test.t.b, 1)]))->Projection->Projection->StreamAgg->Sort",
+		},
+		// Test agg can't push down.
+		{
+			sql:  "select sum(to_base64(e)) from t where c = 1",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]])->Projection->StreamAgg",
+		},
+		{
+			sql:  "select (select count(1) k from t s where s.a = t.a having k != 0) from t",
+			best: "MergeLeftOuterJoin{TableReader(Table(t))->TableReader(Table(t))->Projection}(test.t.a,test.s.a)->Projection->Projection",
+		},
+		// Test stream agg with multi group by columns.
+		{
+			sql:  "select sum(to_base64(e)) from t group by e,d,c order by c",
+			best: "IndexReader(Index(t.c_d_e)[[NULL,+inf]])->Projection->StreamAgg->Projection",
+		},
+		{
+			sql:  "select sum(e+1) from t group by e,d,c order by c",
+			best: "IndexReader(Index(t.c_d_e)[[NULL,+inf]]->StreamAgg)->StreamAgg->Projection",
+		},
+		{
+			sql:  "select sum(to_base64(e)) from t group by e,d,c order by c,e",
+			best: "IndexReader(Index(t.c_d_e)[[NULL,+inf]])->Projection->StreamAgg->Sort->Projection",
+		},
+		{
+			sql:  "select sum(e+1) from t group by e,d,c order by c,e",
+			best: "IndexReader(Index(t.c_d_e)[[NULL,+inf]]->StreamAgg)->StreamAgg->Sort->Projection",
+		},
+		// Test stream agg + limit or sort
+		{
+			sql:  "select count(*) from t group by g order by g limit 10",
+			best: "IndexReader(Index(t.g)[[NULL,+inf]])->StreamAgg->Limit->Projection",
+		},
+		{
+			sql:  "select count(*) from t group by g limit 10",
+			best: "IndexReader(Index(t.g)[[NULL,+inf]]->StreamAgg)->StreamAgg->Limit",
+		},
+		{
+			sql:  "select count(*) from t group by g order by g",
+			best: "IndexReader(Index(t.g)[[NULL,+inf]])->StreamAgg->Projection",
+		},
+		{
+			sql:  "select count(*) from t group by g order by g desc limit 1",
+			best: "IndexReader(Index(t.g)[[NULL,+inf]])->StreamAgg->Limit->Projection",
+		},
+		// Test hash agg + limit or sort
+		{
+			sql:  "select count(*) from t group by b order by b limit 10",
+			best: "TableReader(Table(t))->HashAgg->TopN([test.t.b],0,10)->Projection",
+		},
+		{
+			sql:  "select count(*) from t group by b order by b",
+			best: "TableReader(Table(t))->HashAgg->Sort->Projection",
+		},
+		{
+			sql:  "select count(*) from t group by b limit 10",
+			best: "TableReader(Table(t)->HashAgg)->HashAgg->Limit",
+		},
+		// Test merge join + stream agg
+		{
+			sql:  "select sum(a.g), sum(b.g) from t a join t b on a.g = b.g group by a.g",
+			best: "MergeInnerJoin{IndexReader(Index(t.g)[[NULL,+inf]])->IndexReader(Index(t.g)[[NULL,+inf]])}(test.a.g,test.b.g)->Projection->StreamAgg",
+		},
+		// Test index join + stream agg
+		{
+			sql:  "select /*+ tidb_inlj(a,b) */ sum(a.g), sum(b.g) from t a join t b on a.g = b.g and a.g > 60 group by a.g order by a.g limit 1",
+			best: "IndexJoin{IndexReader(Index(t.g)[(60,+inf]])->IndexReader(Index(t.g)[[NULL,+inf]]->Sel([gt(test.b.g, 60)]))}(test.a.g,test.b.g)->Projection->StreamAgg->Limit->Projection",
+		},
+		{
+			sql:  "select sum(a.g), sum(b.g) from t a join t b on a.g = b.g and a.a>5 group by a.g order by a.g limit 1",
+			best: "MergeInnerJoin{IndexReader(Index(t.g)[[NULL,+inf]]->Sel([gt(test.a.a, 5)]))->IndexReader(Index(t.g)[[NULL,+inf]])}(test.a.g,test.b.g)->Projection->StreamAgg->Limit->Projection",
+		},
+		{
+			sql:  "select sum(d) from t",
+			best: "TableReader(Table(t)->StreamAgg)->StreamAgg",
+		},
+	}
+	for _, tt := range tests {
+		comment := Commentf("for %s", tt.sql)
+		stmt, err := s.ParseOneStmt(tt.sql, "", "")
+		c.Assert(err, IsNil, comment)
+
+		p, err := planner.Optimize(context.TODO(), se, stmt, s.is)
+		c.Assert(err, IsNil)
+		c.Assert(core.ToString(p), Equals, tt.best, Commentf("for %s", tt.sql))
+	}
+}
+
+func (s *testPlanSuite) TestRefine(c *C) {
+	defer testleak.AfterTest(c)()
+	store, dom, err := newStoreWithBootstrap()
+	c.Assert(err, IsNil)
+	defer func() {
+		dom.Close()
+		store.Close()
+	}()
+	se, err := session.CreateSession4Test(store)
+	c.Assert(err, IsNil)
+	_, err = se.Execute(context.Background(), "use test")
+	c.Assert(err, IsNil)
+
+	tests := []struct {
+		sql  string
+		best string
+	}{
+		{
+			sql:  "select a from t where c is not null",
+			best: "IndexReader(Index(t.c_d_e)[[-inf,+inf]])->Projection",
+		},
+		{
+			sql:  "select a from t where c >= 4",
+			best: "IndexReader(Index(t.c_d_e)[[4,+inf]])->Projection",
+		},
+		{
+			sql:  "select a from t where c <= 4",
+			best: "IndexReader(Index(t.c_d_e)[[-inf,4]])->Projection",
+		},
+		{
+			sql:  "select a from t where c = 4 and d = 5 and e = 6",
+			best: "IndexReader(Index(t.c_d_e)[[4 5 6,4 5 6]])->Projection",
+		},
+		{
+			sql:  "select a from t where d = 4 and c = 5",
+			best: "IndexReader(Index(t.c_d_e)[[5 4,5 4]])->Projection",
+		},
+		{
+			sql:  "select a from t where c = 4 and e < 5",
+			best: "IndexReader(Index(t.c_d_e)[[4,4]]->Sel([lt(test.t.e, 5)]))->Projection",
+		},
+		{
+			sql:  "select a from t where c = 4 and d <= 5 and d > 3",
+			best: "IndexReader(Index(t.c_d_e)[(4 3,4 5]])->Projection",
+		},
+		{
+			sql:  "select a from t where d <= 5 and d > 3",
+			best: "TableReader(Table(t)->Sel([le(test.t.d, 5) gt(test.t.d, 3)]))->Projection",
+		},
+		{
+			sql:  "select a from t where c between 1 and 2",
+			best: "IndexReader(Index(t.c_d_e)[[1,2]])->Projection",
+		},
+		{
+			sql:  "select a from t where c not between 1 and 2",
+			best: "IndexReader(Index(t.c_d_e)[[-inf,1) (2,+inf]])->Projection",
+		},
+		{
+			sql:  "select a from t where c <= 5 and c >= 3 and d = 1",
+			best: "IndexReader(Index(t.c_d_e)[[3,5]]->Sel([eq(test.t.d, 1)]))->Projection",
+		},
+		{
+			sql:  "select a from t where c = 1 or c = 2 or c = 3",
+			best: "IndexReader(Index(t.c_d_e)[[1,3]])->Projection",
+		},
+		{
+			sql:  "select b from t where c = 1 or c = 2 or c = 3 or c = 4 or c = 5",
+			best: "IndexLookUp(Index(t.c_d_e)[[1,5]], Table(t))->Projection",
+		},
+		{
+			sql:  "select a from t where c = 5",
+			best: "IndexReader(Index(t.c_d_e)[[5,5]])->Projection",
+		},
+		{
+			sql:  "select a from t where c = 5 and b = 1",
+			best: "IndexLookUp(Index(t.c_d_e)[[5,5]], Table(t)->Sel([eq(test.t.b, 1)]))->Projection",
+		},
+		{
+			sql:  "select a from t where not a",
+			best: "TableReader(Table(t)->Sel([not(test.t.a)]))",
+		},
+		{
+			sql:  "select a from t where c in (1)",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]])->Projection",
+		},
+		{
+			sql:  "select a from t where c in ('1')",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]])->Projection",
+		},
+		{
+			sql:  "select a from t where c = 1.0",
+			best: "IndexReader(Index(t.c_d_e)[[1,1]])->Projection",
+		},
+		{
+			sql:  "select a from t where c in (1) and d > 3",
+			best: "IndexReader(Index(t.c_d_e)[(1 3,1 +inf]])->Projection",
+		},
+		{
+			sql:  "select a from t where c in (1, 2, 3) and (d > 3 and d < 4 or d > 5 and d < 6)",
+			best: "Dual->Projection",
+		},
+		{
+			sql:  "select a from t where c in (1, 2, 3) and (d > 2 and d < 4 or d > 5 and d < 7)",
+			best: "IndexReader(Index(t.c_d_e)[(1 2,1 4) (1 5,1 7) (2 2,2 4) (2 5,2 7) (3 2,3 4) (3 5,3 7)])->Projection",
+		},
+		{
+			sql:  "select a from t where c in (1, 2, 3)",
+			best: "IndexReader(Index(t.c_d_e)[[1,1] [2,2] [3,3]])->Projection",
+		},
+		{
+			sql:  "select a from t where c in (1, 2, 3) and d in (1,2) and e = 1",
+			best: "IndexReader(Index(t.c_d_e)[[1 1 1,1 1 1] [1 2 1,1 2 1] [2 1 1,2 1 1] [2 2 1,2 2 1] [3 1 1,3 1 1] [3 2 1,3 2 1]])->Projection",
+		},
+		{
+			sql:  "select a from t where d in (1, 2, 3)",
+			best: "TableReader(Table(t)->Sel([in(test.t.d, 1, 2, 3)]))->Projection",
+		},
+		{
+			sql:  "select a from t where c not in (1)",
+			best: "IndexReader(Index(t.c_d_e)[[-inf,1) (1,+inf]])->Projection",
+		},
+		// test like
+		{
+			sql:  "select a from t use index(c_d_e) where c != 1",
+			best: "IndexReader(Index(t.c_d_e)[[-inf,1) (1,+inf]])->Projection",
+		},
+		{
+			sql:  "select a from t where c_str like ''",
+			best: `IndexReader(Index(t.c_d_e_str)[["",""]])->Projection`,
+		},
+		{
+			sql:  "select a from t where c_str like 'abc'",
+			best: `IndexReader(Index(t.c_d_e_str)[["abc","abc"]])->Projection`,
+		},
+		{
+			sql:  "select a from t where c_str not like 'abc'",
+			best: `IndexReader(Index(t.c_d_e_str)[[-inf,"abc") ("abc",+inf]])->Projection`,
+		},
+		{
+			sql:  "select a from t where not (c_str like 'abc' or c_str like 'abd')",
+			best: `IndexReader(Index(t.c_d_e_str)[[-inf,"abc") ("abc","abd") ("abd",+inf]])->Projection`,
+		},
+		{
+			sql:  "select a from t where c_str like '_abc'",
+			best: "TableReader(Table(t)->Sel([like(test.t.c_str, _abc, 92)]))->Projection",
+		},
+		{
+			sql:  `select a from t where c_str like 'abc%'`,
+			best: `IndexReader(Index(t.c_d_e_str)[["abc","abd")])->Projection`,
+		},
+		{
+			sql:  "select a from t where c_str like 'abc_'",
+			best: `IndexReader(Index(t.c_d_e_str)[("abc","abd")]->Sel([like(test.t.c_str, abc_, 92)]))->Projection`,
+		},
+		{
+			sql:  "select a from t where c_str like 'abc%af'",
+			best: `IndexReader(Index(t.c_d_e_str)[["abc","abd")]->Sel([like(test.t.c_str, abc%af, 92)]))->Projection`,
+		},
+		{
+			sql:  `select a from t where c_str like 'abc\\_' escape ''`,
+			best: `IndexReader(Index(t.c_d_e_str)[["abc_","abc_"]])->Projection`,
+		},
+		{
+			sql:  `select a from t where c_str like 'abc\\_'`,
+			best: `IndexReader(Index(t.c_d_e_str)[["abc_","abc_"]])->Projection`,
+		},
+		{
+			sql:  `select a from t where c_str like 'abc\\\\_'`,
+			best: "IndexReader(Index(t.c_d_e_str)[(\"abc\\\",\"abc]\")]->Sel([like(test.t.c_str, abc\\\\_, 92)]))->Projection",
+		},
+		{
+			sql:  `select a from t where c_str like 'abc\\_%'`,
+			best: "IndexReader(Index(t.c_d_e_str)[[\"abc_\",\"abc`\")])->Projection",
+		},
+		{
+			sql:  `select a from t where c_str like 'abc=_%' escape '='`,
+			best: "IndexReader(Index(t.c_d_e_str)[[\"abc_\",\"abc`\")])->Projection",
+		},
+		{
+			sql:  `select a from t where c_str like 'abc\\__'`,
+			best: "IndexReader(Index(t.c_d_e_str)[(\"abc_\",\"abc`\")]->Sel([like(test.t.c_str, abc\\__, 92)]))->Projection",
+		},
+		{
+			// Check that 123 is converted to string '123'. index can be used.
+			sql:  `select a from t where c_str like 123`,
+			best: "IndexReader(Index(t.c_d_e_str)[[\"123\",\"123\"]])->Projection",
+		},
+		{
+			sql:  `select a from t where c = 1.9 and d > 3`,
+			best: "Dual",
+		},
+		{
+			sql:  `select a from t where c < 1.1`,
+			best: "IndexReader(Index(t.c_d_e)[[-inf,2)])->Projection",
+		},
+		{
+			sql:  `select a from t where c <= 1.9`,
+			best: "IndexReader(Index(t.c_d_e)[[-inf,1]])->Projection",
+		},
+		{
+			sql:  `select a from t where c >= 1.1`,
+			best: "IndexReader(Index(t.c_d_e)[[2,+inf]])->Projection",
+		},
+		{
+			sql:  `select a from t where c > 1.9`,
+			best: "IndexReader(Index(t.c_d_e)[(1,+inf]])->Projection",
+		},
+		{
+			sql:  `select a from t where c = 123456789098765432101234`,
+			best: "Dual",
+		},
+		{
+			sql:  `select a from t where c = 'hanfei'`,
+			best: "TableReader(Table(t))->Sel([eq(cast(test.t.c), cast(hanfei))])->Projection",
+		},
+>>>>>>> 103cbcd... planner: rewrite `in` as `eq` when the rhs list has only one item
 	}
 }
 
