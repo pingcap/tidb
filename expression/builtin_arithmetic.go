d77f9cfb6c06c622eeeedbb1b78ff65a324be62a
jackyan <y870414423@gmail.com>
2019-08-20 20:54:26 +0800

    expression: fix DIV with decimal type

--- expression/builtin_arithmetic.go
+++ expression/builtin_arithmetic.go
@@ -763,11 +763,24 @@ func (s *builtinArithmeticIntDivideDecimalSig) evalInt(row chunk.Row) (ret int64
 		return 0, true, errors.Trace(err)
 	}
 
+	isLHSUnsigned := mysql.HasUnsignedFlag(s.args[0].GetType().Flag)
+	isRHSUnsigned := mysql.HasUnsignedFlag(s.args[1].GetType().Flag)
+
 	ret, err = c.ToInt()
-	// err returned by ToInt may be ErrTruncated or ErrOverflow, only handle ErrOverflow, ignore ErrTruncated.
-	if err == types.ErrOverflow {
-		return 0, true, types.ErrOverflow.GenWithStackByArgs("BIGINT", fmt.Sprintf("(%s DIV %s)", s.args[0].String(), s.args[1].String()))
+	if isLHSUnsigned || isRHSUnsigned {
+		val, err := c.ToUint()
+		if err == types.ErrOverflow && ret != 0 {
+			// err returned by ToInt may be ErrTruncated or ErrOverflow, only handle ErrOverflow, ignore ErrTruncated.
+			return 0, true, types.ErrOverflow.GenWithStackByArgs("BIGINT UNSIGNED", fmt.Sprintf("(%s DIV %s)", s.args[0].String(), s.args[1].String()))
+		}
+		ret = int64(val)
+	} else {
+		// err returned by ToInt may be ErrTruncated or ErrOverflow, only handle ErrOverflow, ignore ErrTruncated.
+		if err == types.ErrOverflow {
+			return 0, true, types.ErrOverflow.GenWithStackByArgs("BIGINT", fmt.Sprintf("(%s DIV %s)", s.args[0].String(), s.args[1].String()))
+		}
 	}
+
 	return ret, false, nil
 }
 
