51dce2abd1ce3e098bcc5317f3e78ff4d57ca103
AilinKid <314806019@qq.com>
2019-08-24 15:10:30 +0800

    add comment

--- tidb-server/main.go
+++ tidb-server/main.go
@@ -346,7 +346,7 @@ func loadConfig() string {
 		}
 		terror.MustNil(err)
 	} else {
-		// configCheck should have the config file specified
+		// configCheck should have the config file specified.
 		if *configCheck {
 			fmt.Fprintln(os.Stderr, "config check failed", errors.New("no config file specified for config-check"))
 			os.Exit(1)
