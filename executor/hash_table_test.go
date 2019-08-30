f0e04d68d0e174ccd3251d50f94b4e363499ce11
Feng Liyuan <darktemplar.f@gmail.com>
2019-08-30 19:15:59 +0800

    licence

--- executor/hash_table_test.go
+++ executor/hash_table_test.go
@@ -1,3 +1,16 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package executor
 
 import (
@@ -8,7 +21,7 @@ import (
 func (s *pkgTestSuite) TestRowHashMap(c *C) {
 	m := newRowHashMap()
 	m.Put(1, chunk.RowPtr{ChkIdx: 1, RowIdx: 1})
-	c.Check(m.Get(1), DeepEquals, []chunk.RowPtr{{1, 1}})
+	c.Check(m.Get(1), DeepEquals, []chunk.RowPtr{{ChkIdx: 1, RowIdx: 1}})
 
 	rawData := map[uint64][]chunk.RowPtr{}
 	for i := uint64(0); i < 10; i++ {
