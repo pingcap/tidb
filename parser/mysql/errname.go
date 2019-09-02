b16b67a5b0fe9ed0fa855061fac289a60b23ffe9
Tanner <xietengjin@pingcap.com>
2019-09-02 11:07:20 +0800

    [parser] parser: add a mysql error message (#534)

--- parser/mysql/errname.go
+++ parser/mysql/errname.go
@@ -885,6 +885,7 @@ var MySQLErrName = map[uint16]string{
 	ErrDependentByGeneratedColumn:                            "Column '%s' has a generated column dependency.",
 	ErrGeneratedColumnFunctionIsNotAllowed:                   "Expression of generated column '%s' contains a disallowed function.",
 	ErrGeneratedColumnRefAutoInc:                             "Generated column '%s' cannot refer to auto-increment column.",
+	ErrInvalidFieldSize:                                      "Invalid size for column '%s'.",
 	ErrInvalidJSONData:                                       "Invalid JSON data provided to function %s: %s",
 	ErrInvalidJSONText:                                       "Invalid JSON text: %-.192s",
 	ErrInvalidJSONPath:                                       "Invalid JSON path expression %s.",
