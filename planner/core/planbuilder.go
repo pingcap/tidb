c64959be49edf0b6d0e33de7b5f4d786008f496c
francis0407 <hanmingcong123@hotmail.com>
2019-09-05 23:06:54 +0800

    address comment

--- planner/core/planbuilder.go
+++ planner/core/planbuilder.go
@@ -558,9 +558,11 @@ func (b *PlanBuilder) getPossibleAccessPaths(indexHints []*ast.IndexHint, tblInf
 		}
 
 		hasScanHint = true
+		
+		// It is syntactically valid to omit index_list for USE INDEX, which means “use no indexes.” 
+		// Omitting index_list for FORCE INDEX or IGNORE INDEX is a syntax error.
+		// See https://dev.mysql.com/doc/refman/8.0/en/index-hints.html.
 		if hint.IndexNames == nil && hint.HintType != ast.HintIgnore {
-			// If indexNames is nil, choose TablePath.
-			// For example: `select a from t use index()`.
 			if path := getTablePath(publicPaths); path != nil {
 				hasUseOrForce = true
 				path.forced = true
