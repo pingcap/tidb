87648ae7000ab999e45b6ead11993bab22fe3eba
Shuaipeng Yu <jackysp@gmail.com>
2019-09-05 19:06:39 +0800

    *: remove word error in warn, info, debug logs
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- store/tikv/2pc.go
+++ store/tikv/2pc.go
@@ -948,7 +948,7 @@ func (c *twoPhaseCommitter) execute(ctx context.Context) error {
 				zap.Uint64("txnStartTS", c.startTS))
 			return errors.Trace(err)
 		}
-		logutil.Logger(ctx).Debug("2PC succeed with error",
+		logutil.Logger(ctx).Debug("got some exceptions, but 2pc was still successful",
 			zap.Error(err),
 			zap.Uint64("txnStartTS", c.startTS))
 	}
