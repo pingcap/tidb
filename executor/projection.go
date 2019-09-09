5ede5f3135ebf0899ee3a8cd9877c8bfd2070abf
jackyan <y870414423@gmail.com>
2019-09-09 18:37:40 +0800

    Add concurrency information for projection

--- executor/projection.go
+++ executor/projection.go
@@ -256,6 +256,10 @@ func (e *ProjectionExec) Close() error {
 		}
 		e.outputCh = nil
 	}
+	if e.runtimeStats != nil {
+		rootStats := e.ctx.GetSessionVars().StmtCtx.RuntimeStatsColl.GetRootStats(e.baseExecutor.id.String())
+		rootStats.SetConcurrencyNum(e.numWorkers)
+	}
 	return e.baseExecutor.Close()
 }
 
