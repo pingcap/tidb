d82daf02f6fe6cf9902a213981735e41bc27f3a1
AilinKid <314806019@qq.com>
2019-09-10 16:43:33 +0800

    rebase maxUInt64 or maxInt64 ok

--- meta/autoid/autoid.go
+++ meta/autoid/autoid.go
@@ -113,10 +113,6 @@ func (alloc *allocator) NextGlobalAutoID(tableID int64) (int64, error) {
 }
 
 func (alloc *allocator) rebase4Unsigned(tableID int64, requiredBase uint64, allocIDs bool) error {
-	// Keep requiredBase valid.
-	if requiredBase == math.MaxUint64 {
-		return ErrAutoincReadFailed
-	}
 	// Satisfied by alloc.base, nothing to do.
 	if requiredBase <= uint64(alloc.base) {
 		return nil
@@ -163,10 +159,6 @@ func (alloc *allocator) rebase4Unsigned(tableID int64, requiredBase uint64, allo
 }
 
 func (alloc *allocator) rebase4Signed(tableID, requiredBase int64, allocIDs bool) error {
-	// Keep requiredBase valid.
-	if requiredBase == math.MaxInt64 {
-		return ErrAutoincReadFailed
-	}
 	// Satisfied by alloc.base, nothing to do.
 	if requiredBase <= alloc.base {
 		return nil
