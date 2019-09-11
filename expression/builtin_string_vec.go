4b6c38d783c632a3fa3f01295ceb813f32f8225d
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2019-09-11 10:40:44 +0800

    expression: implement vectorized evaluation for `builtinLowerStr` (#12013)

--- expression/builtin_string_vec.go
+++ expression/builtin_string_vec.go
@@ -1,12 +1,55 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package expression
 
 import (
 	"math"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/chunk"
 )
+
+func (b *builtinLowerSig) vecEvalString(input *chunk.Chunk, result *chunk.Column) error {
+	if err := b.args[0].VecEvalString(b.ctx, input, result); err != nil {
+		return err
+	}
+	if types.IsBinaryStr(b.args[0].GetType()) {
+		return nil
+	}
+
+Loop:
+	for i := 0; i < input.NumRows(); i++ {
+		str := result.GetBytes(i)
+		for _, c := range str {
+			if c >= utf8.RuneSelf {
+				continue Loop
+			}
+		}
+		for i := range str {
+			if str[i] >= 'A' && str[i] <= 'Z' {
+				str[i] += 'a' - 'A'
+			}
+		}
+	}
+	return nil
+}
+
+func (b *builtinLowerSig) vectorized() bool {
+	return true
+}
 
 func (b *builtinRepeatSig) vecEvalString(input *chunk.Chunk, result *chunk.Column) error {
 	n := input.NumRows()
