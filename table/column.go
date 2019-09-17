0912a2268ae40b5b2d967a314d2a16aa73a658de
tangenta <tangenta@126.com>
2019-09-17 17:15:41 +0800

    add comments

--- table/column.go
+++ table/column.go
@@ -363,6 +363,9 @@ func CheckNotNull(cols []*Column, row []types.Datum) error {
 
 // GetColOriginDefaultValue gets default value of the column from original default value.
 func GetColOriginDefaultValue(ctx sessionctx.Context, col *model.ColumnInfo) (types.Datum, error) {
+	// If the column type is BIT, both `OriginDefaultValue` and `DefaultValue` of ColumnInfo are corrupted, because
+	// after JSON marshaling and unmarshaling against the field with type `interface{}`, the content with actual type `[]byte` is changed.
+	// We need `DefaultValueBit` to restore OriginDefaultValue before reading it.
 	if col.OriginDefaultValue != nil && col.DefaultValueBit != nil && col.Tp == mysql.TypeBit {
 		col.OriginDefaultValue = col.DefaultValueBit
 	}
