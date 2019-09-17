a42c15a48c1bce87ef9754545dfa0cde625ecd72
crazycs520 <crazycs520@gmail.com>
2019-09-17 20:20:36 +0800

    ddl: fix drop index failed when index contain auto_increment column and more than 2 index contain auto_increment_column

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -3231,9 +3231,10 @@ func (d *ddl) DropIndex(ctx sessionctx.Context, ti ast.Ident, indexName model.CI
 		return ErrCantDropFieldOrKey.GenWithStack("index %s doesn't exist", indexName)
 	}
 
+	// Check for drop index on auto_increment column.
 	cols := t.Cols()
 	for _, idxCol := range indexInfo.Columns {
-		if mysql.HasAutoIncrementFlag(cols[idxCol.Offset].Flag) {
+		if mysql.HasAutoIncrementFlag(cols[idxCol.Offset].Flag) && countOfIndexContainColumn(t.Meta(), idxCol.Name.L) < 2 {
 			return autoid.ErrWrongAutoKey
 		}
 	}
@@ -3249,6 +3250,20 @@ func (d *ddl) DropIndex(ctx sessionctx.Context, ti ast.Ident, indexName model.CI
 	err = d.doDDLJob(ctx, job)
 	err = d.callHookOnChanged(err)
 	return errors.Trace(err)
+}
+
+func countOfIndexContainColumn(tblInfo *model.TableInfo, colName string) int {
+	count := 0
+	for _, idx := range tblInfo.Indices {
+		for _, c := range idx.Columns {
+			if c.Name.L != colName {
+				continue
+			}
+			count++
+			break
+		}
+	}
+	return count
 }
 
 func isDroppableColumn(tblInfo *model.TableInfo, colName model.CIStr) error {
