5384a0cc74b884f6a6b5181ac2d4b53503d9b85b
Zhihao Chen <1797079433@qq.com>
2019-09-18 20:32:09 +0800

    expression: implement vectorized evaluation for builtinSinSig (#12237)

--- expression/builtin_math_vec.go
+++ expression/builtin_math_vec.go
@@ -199,6 +199,24 @@ func (b *builtinCosSig) vectorized() bool {
 	return true
 }
 
+func (b *builtinSinSig) vecEvalReal(input *chunk.Chunk, result *chunk.Column) error {
+	if err := b.args[0].VecEvalReal(b.ctx, input, result); err != nil {
+		return err
+	}
+	f64s := result.Float64s()
+	for i := 0; i < len(f64s); i++ {
+		if result.IsNull(i) {
+			continue
+		}
+		f64s[i] = math.Sin(f64s[i])
+	}
+	return nil
+}
+
+func (b *builtinSinSig) vectorized() bool {
+	return true
+}
+
 func (b *builtinTanSig) vecEvalReal(input *chunk.Chunk, result *chunk.Column) error {
 	if err := b.args[0].VecEvalReal(b.ctx, input, result); err != nil {
 		return err
