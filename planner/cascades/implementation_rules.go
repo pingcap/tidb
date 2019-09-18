b32365d9d263b6eb974235d4e3a1de04c44cadc7
hanmc <hanmingcong123@hotmail.com>
2019-09-18 20:42:51 +0800

    revert change

--- planner/cascades/implementation_rules.go
+++ planner/cascades/implementation_rules.go
@@ -174,7 +174,7 @@ type ImplSelection struct {
 
 // Match implements ImplementationRule Match interface.
 func (r *ImplSelection) Match(expr *memo.GroupExpr, prop *property.PhysicalProperty) (matched bool) {
-	return prop.IsEmpty()
+	return true
 }
 
 // OnImplement implements ImplementationRule OnImplement interface.
