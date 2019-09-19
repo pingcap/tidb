85b6afa481afaa0dbd6c01a48493b4ed68859910
tsthght <781181214@qq.com>
2019-09-19 17:51:26 +0800

    expression: implement vectorized evaluation for

--- expression/builtin_math_vec.go
+++ expression/builtin_math_vec.go
@@ -365,3 +365,22 @@ func (b *builtinPowSig) vecEvalReal(input *chunk.Chunk, result *chunk.Column) er
 func (b *builtinPowSig) vectorized() bool {
 	return true
 }
+
+func (b *builtinFloorRealSig) vecEvalReal(input *chunk.Chunk, result *chunk.Column) error {
+	if err := b.args[0].VecEvalReal(b.ctx, input, result); err != nil {
+		return err
+	}
+	f64s := result.Float64s()
+	for i := 0; i < len(f64s); i++ {
+		if result.IsNull(i) {
+			continue
+		}
+		f64s[i] = math.Floor(f64s[i])
+	}
+	return nil
+
+}
+
+func (b *builtinFloorRealSig) vectorized() bool {
+	return true
+}
