c25ae506ee0c405d0425fac1c42f32d56751f552
tangwz <tangwz.com@gmail.com>
2019-09-22 14:51:18 +0800

    expression: implement vectorized evaluation for builtinAbsIntSig (#12103)

--- expression/builtin_math_vec_test.go
+++ expression/builtin_math_vec_test.go
@@ -60,6 +60,7 @@ var vecBuiltinMathCases = map[string][]vecExprBenchCase{
 	},
 	ast.Abs: {
 		{types.ETDecimal, []types.EvalType{types.ETDecimal}, nil},
+		{types.ETInt, []types.EvalType{types.ETInt}, nil},
 	},
 	ast.Round: {
 		{types.ETDecimal, []types.EvalType{types.ETDecimal}, nil},
