29f0da786c9f7a513bc244bef72c7aecf6b54826
imtbkcat <songlingyu@pingcap.com>
2019-09-23 19:50:52 +0800

    add server status for dispatch fail log

--- server/conn.go
+++ server/conn.go
@@ -160,12 +160,8 @@ type clientConn struct {
 
 func (cc *clientConn) String() string {
 	collationStr := mysql.Collations[cc.collation]
-	autoCommit := 0
-	if cc.ctx.GetSessionVars().IsAutocommit() {
-		autoCommit = 1
-	}
-	return fmt.Sprintf("id:%d, addr:%s status:%d, collation:%s, user:%s autocommit:%d",
-		cc.connectionID, cc.bufReadConn.RemoteAddr(), cc.ctx.Status(), collationStr, cc.user, autoCommit,
+	return fmt.Sprintf("id:%d, addr:%s status:%d, collation:%s, user:%s",
+		cc.connectionID, cc.bufReadConn.RemoteAddr(), cc.ctx.Status(), collationStr, cc.user,
 	)
 }
 
@@ -542,6 +538,20 @@ func (cc *clientConn) readOptionalSSLRequestAndHandshakeResponse(ctx context.Con
 	return err
 }
 
+func (cc *clientConn) SessionStatusToString() string {
+	status := cc.ctx.Status()
+	inTxn, autoCommit := 0, 0
+	if status & mysql.ServerStatusInTrans > 0 {
+		inTxn = 1
+	}
+	if status & mysql.ServerStatusAutocommit > 0 {
+		autoCommit = 1
+	}
+	return fmt.Sprintf("inTxn:%d, autocommit:%d",
+		inTxn, autoCommit,
+	)
+}
+
 func (cc *clientConn) openSessionAndDoAuth(authData []byte) error {
 	var tlsStatePtr *tls.ConnectionState
 	if cc.tlsConn != nil {
@@ -678,6 +688,7 @@ func (cc *clientConn) Run(ctx context.Context) {
 			logutil.Logger(ctx).Warn("command dispatched failed",
 				zap.String("connInfo", cc.String()),
 				zap.String("command", mysql.Command2Str[data[0]]),
+				zap.String("status", cc.SessionStatusToString()),
 				zap.String("sql", queryStrForLog(string(data[1:]))),
 				zap.String("err", errStrForLog(err)),
 			)
