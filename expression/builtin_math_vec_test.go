4ab4cd0813ac80b9d77bf15217407e349bcc272e
tsthght <tsthght@yeah.net>
2019-09-24 10:22:33 +0800

    fix bug

--- expression/builtin_math_vec_test.go
+++ expression/builtin_math_vec_test.go
@@ -80,7 +80,7 @@ var vecBuiltinMathCases = map[string][]vecExprBenchCase{
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal, types.ETReal}, geners: []dataGenerator{&rangeRealGener{0, 10, 0.5}, &rangeRealGener{0, 100, 0.5}}},
 	},
 	ast.Truncate: {
-		{types.ETReal, []types.EvalType{types.ETReal, types.ETInt}, []dataGenerator{nil, &rangeInt64Gener{-10, 10}}},
+		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal, types.ETInt}, geners: []dataGenerator{nil, &rangeInt64Gener{-10, 10}}},
 	},
 }
 
