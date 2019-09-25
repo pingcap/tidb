7c87d926ff86cc638722df8cef05cb00f1d07fc7
tsthght <tsthght@yeah.net>
2019-09-25 08:37:33 +0800

    gofmt

--- expression/builtin_math_vec_test.go
+++ expression/builtin_math_vec_test.go
@@ -24,6 +24,7 @@ import (
 var vecBuiltinMathCases = map[string][]vecExprBenchCase{
 	ast.Log: {
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal}},
+		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal, types.ETReal}, geners: []dataGenerator{nil, nil}},
 	},
 	ast.Log10: {
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal}},
@@ -80,8 +81,6 @@ var vecBuiltinMathCases = map[string][]vecExprBenchCase{
 	ast.Pow: {
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal, types.ETReal}, geners: []dataGenerator{&rangeRealGener{0, 10, 0.5}, &rangeRealGener{0, 100, 0.5}}},
 	},
-	ast.Log: {
-		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal, types.ETReal},geners: []dataGenerator{nil, nil}},
 	ast.Truncate: {
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal, types.ETInt}, geners: []dataGenerator{nil, &rangeInt64Gener{-10, 10}}},
 	},
