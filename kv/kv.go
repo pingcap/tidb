095bee9655520f8c26cff5fb6569909292906f07
Zhuomin(Charming) Liu <lzmhhh123@gmail.com>
2019-09-27 15:31:04 +0800

    executor: support table reader executor read from tiflash. (#12371)

--- kv/kv.go
+++ kv/kv.go
@@ -221,6 +221,16 @@ const (
 	ReqSubTypeAnalyzeCol = 10005
 )
 
+// StoreType represents the type of a store.
+type StoreType uint8
+
+const (
+	// TiKV means the type of a store is TiKV.
+	TiKV StoreType = iota
+	// TiFlash means the type of a store is TiFlash.
+	TiFlash
+)
+
 // Request represents a kv request.
 type Request struct {
 	// Tp is the request type.
@@ -252,6 +262,8 @@ type Request struct {
 	Streaming bool
 	// ReplicaRead is used for reading data from replicas, only follower is supported at this time.
 	ReplicaRead ReplicaReadType
+	// StoreType represents this request is sent to the which type of store.
+	StoreType StoreType
 }
 
 // ResultSubset represents a result subset from a single storage unit.
