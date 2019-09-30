92f930f9f03eb027240823fdb827f160a1b93453
b41sh <baishen2009@gmail.com>
2019-09-30 13:53:27 +0800

    fix

--- expression/builtin_time_vec.go
+++ expression/builtin_time_vec.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	   http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -261,10 +261,7 @@ func (b *builtinDayNameSig) vecEvalReal(input *chunk.Chunk, result *chunk.Column
 }
 
 func (b *builtinDayNameSig) vecEvalInt(input *chunk.Chunk, result *chunk.Column) error {
-	if err := b.vecEvalIndex(input, result); err != nil {
-		return err
-	}
-	return nil
+	return b.vecEvalIndex(input, result)
 }
 
 func (b *builtinWeekDaySig) vectorized() bool {
