a5998ff2e2607eb16f154428ffd9f9b1f2cad454
liuhao <github.haoliu@gmail.com>
2019-10-05 00:40:05 +0800

    fix expression/builtin_op_vec_test.go code fmt

--- expression/builtin_op_vec_test.go
+++ expression/builtin_op_vec_test.go
@@ -22,9 +22,9 @@ import (
 )
 
 var vecBuiltinOpCases = map[string][]vecExprBenchCase{
-	ast.IsTruth:   {},
+	ast.IsTruth: {},
 	ast.IsFalsity: {
-		{retEvalType: types.ETInt, childrenTypes:[]types.EvalType{types.ETInt}, geners: []dataGenerator{&defaultGener{0.2, types.ETInt}}},
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETInt}, geners: []dataGenerator{&defaultGener{0.2, types.ETInt}}},
 	},
 	ast.LogicOr: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETInt, types.ETInt}, geners: makeBinaryLogicOpDataGeners()},
