09fccc704be7bb6c30f6036f7bb9717d2fbf8b96
tangwz <tangwz.com@gmail.com>
2019-10-06 23:22:42 +0800

    expression: implement vectorized evaluation for builtinIntIsNullSig

--- expression/builtin_op_vec.go
+++ expression/builtin_op_vec.go
@@ -119,11 +119,31 @@ func (b *builtinUnaryMinusDecimalSig) vecEvalDecimal(input *chunk.Chunk, result 
 }
 
 func (b *builtinIntIsNullSig) vectorized() bool {
-	return false
+	return true
 }
 
 func (b *builtinIntIsNullSig) vecEvalInt(input *chunk.Chunk, result *chunk.Column) error {
-	return errors.Errorf("not implemented")
+	numRows := input.NumRows()
+	buf, err := b.bufAllocator.get(types.ETInt, numRows)
+	if err != nil {
+		return err
+	}
+	defer b.bufAllocator.put(buf)
+
+	if err := b.args[0].VecEvalReal(b.ctx, input, buf); err != nil {
+		return err
+	}
+
+	result.ResizeInt64(numRows, false)
+	i64s := result.Int64s()
+	for i := 0; i < numRows; i++ {
+		if buf.IsNull(i) {
+			i64s[i] = 1
+		} else {
+			i64s[i] = 0
+		}
+	}
+	return nil
 }
 
 func (b *builtinRealIsNullSig) vectorized() bool {
