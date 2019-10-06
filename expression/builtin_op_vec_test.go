2ac22afe86765ec970acc8686c77a3ac0f9265ed
tangwz <tangwz.com@gmail.com>
2019-10-07 00:56:28 +0800

    expression: implement vectorized evaluation for builtinDurationIsNullSig

--- expression/builtin_op_vec_test.go
+++ expression/builtin_op_vec_test.go
@@ -40,7 +40,9 @@ var vecBuiltinOpCases = map[string][]vecExprBenchCase{
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETInt}},
 	},
 	ast.UnaryMinus: {},
-	ast.IsNull:     {},
+	ast.IsNull: {
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDuration}},
+	},
 }
 
 // givenValsGener returns the items sequentially from the slice given at
