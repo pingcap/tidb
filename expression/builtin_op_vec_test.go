bcccddba567ce75095a4613b450ab2b6d3de74ba
liuhao2050 <github.haoliu@gmail.com>
2019-10-08 13:39:46 +0800

    fix code format

--- expression/builtin_op_vec_test.go
+++ expression/builtin_op_vec_test.go
@@ -26,8 +26,8 @@ var vecBuiltinOpCases = map[string][]vecExprBenchCase{
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDecimal}, geners: []dataGenerator{&defaultGener{0.2, types.ETDecimal}}},
 	},
 	ast.IsFalsity: {
-    {retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDecimal}},
-  },
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDecimal}},
+	},
 	ast.LogicOr: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETInt, types.ETInt}, geners: makeBinaryLogicOpDataGeners()},
 	},
