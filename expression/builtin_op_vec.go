64043ca1d034bbc41b370b13838780790ee0c280
liuhao2050 <github.haoliu@gmail.com>
2019-10-08 13:24:14 +0800

    fix

--- expression/builtin_op_vec.go
+++ expression/builtin_op_vec.go
@@ -286,9 +286,9 @@ func (b *builtinRealIsFalseSig) vecEvalInt(input *chunk.Chunk, result *chunk.Col
 
 	result.ResizeInt64(numRows, false)
 	i64s := result.Int64s()
-
+	bufI64s := buf.Int64s()
 	for i := 0; i < numRows; i++ {
-		if buf.IsNull(i) || buf.GetInt64(i) != 0 {
+		if buf.IsNull(i) || bufI64s[i] != 0 {
 			i64s[i] = 0
 		} else {
 			i64s[i] = 1
