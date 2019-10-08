9fecc61fea4b7c230de7ad1d67b383d2d063e688
Deardrops <passagesfall@gmail.com>
2019-10-08 11:47:24 +0800

    fixup CI

--- ddl/db_integration_test.go
+++ ddl/db_integration_test.go
@@ -2054,7 +2054,7 @@ func (s *testIntegrationSuite3) TestSqlFunctionsInGeneratedColumns(c *C) {
 	tk.MustGetErrCode("create table t (a int, b int as ((SELECT 1 FROM t1 UNION SELECT 1 FROM t1)))", mysql.ErrGeneratedColumnFunctionIsNotAllowed)
 	// 5. Variable
 	tk.MustExec("set @x = 1")
-	tk.MustGetErrCode("create table t (a int, b int as (@x)", mysql.ErrGeneratedColumnFunctionIsNotAllowed)
+	tk.MustGetErrCode("create table t (a int, b int as (@x))", mysql.ErrGeneratedColumnFunctionIsNotAllowed)
 	tk.MustGetErrCode("create table t (a int, b int as (@@max_connections))", mysql.ErrGeneratedColumnFunctionIsNotAllowed)
 	// 6. Aggregate function
 	tk.MustGetErrCode("create table t1 (a int, b int as (avg(a)));", mysql.ErrInvalidGroupFuncUse)
