624df649d1e652ae0e52ad56ab553cbbd674c408
Arenatlx <ailinsilence4@gmail.com>
2019-10-10 10:34:44 +0800

    dml : Multiple rows insert in a statement should have consecutive autoID if needed.  (#11876)

--- table/tables/tables.go
+++ table/tables/tables.go
@@ -904,13 +904,13 @@ func GetColDefaultValue(ctx sessionctx.Context, col *table.Column, defaultVals [
 }
 
 // AllocAutoIncrementValue implements table.Table AllocAutoIncrementValue interface.
-func (t *tableCommon) AllocAutoIncrementValue(ctx sessionctx.Context) (int64, error) {
-	return t.Allocator(ctx).Alloc(t.tableID)
+func (t *tableCommon) AllocAutoIncrementValue(ctx sessionctx.Context, n int) (int64, int64, error) {
+	return t.Allocator(ctx).Alloc(t.Meta().ID, uint64(n))
 }
 
 // AllocHandle implements table.Table AllocHandle interface.
 func (t *tableCommon) AllocHandle(ctx sessionctx.Context) (int64, error) {
-	rowID, err := t.Allocator(ctx).Alloc(t.tableID)
+	_, rowID, err := t.Allocator(ctx).Alloc(t.tableID, 1)
 	if err != nil {
 		return 0, errors.Trace(err)
 	}
