bdf1680021b29643f595802bc1644e0d755997bf
lysu <sulifx@gmail.com>
2019-10-11 22:54:04 +0800

    mocktikv: fix data race in mocktikv split (#12414)

--- store/mockstore/mocktikv/rpc.go
+++ store/mockstore/mocktikv/rpc.go
@@ -624,9 +624,7 @@ func (h *rpcHandler) handleSplitRegion(req *kvrpcpb.SplitRegionRequest) *kvrpcpb
 		}
 		newRegionID, newPeerIDs := h.cluster.AllocID(), h.cluster.AllocIDs(len(region.Peers))
 		newRegion := h.cluster.SplitRaw(region.GetId(), newRegionID, k, newPeerIDs, newPeerIDs[0])
-		// The mocktikv should return a deep copy of meta info to avoid data race
-		metaCloned := proto.Clone(newRegion.Meta)
-		resp.Regions = append(resp.Regions, metaCloned.(*metapb.Region))
+		resp.Regions = append(resp.Regions, newRegion)
 	}
 	return resp
 }
