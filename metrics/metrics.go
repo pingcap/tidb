48664bf24ab1101747e7ac54b5cd9cdfcb8b288e
tiancaiamao <tiancaiamao@gmail.com>
2019-10-11 21:36:04 +0800

    address comment

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -151,7 +151,6 @@ func RegisterMetrics() {
 	prometheus.MustRegister(TiKVRangeTaskStats)
 	prometheus.MustRegister(TiKVRangeTaskPushDuration)
 	prometheus.MustRegister(HandleSchemaValidate)
-	prometheus.MustRegister(TiKVTokenWaitDuration)
 	prometheus.MustRegister(TiKVTxnHeartBeatHistogram)
 	prometheus.MustRegister(GRPCConnTransientFailureCounter)
 }
