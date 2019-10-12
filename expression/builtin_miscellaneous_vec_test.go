943036865bd38a02d0c27483735b94e65e503cb1
tsthght <781181214@qq.com>
2019-10-12 13:21:10 +0800

    expression: implement vectorized evaluation for 'builtinRealAnyValueSig'

--- expression/builtin_miscellaneous_vec_test.go
+++ expression/builtin_miscellaneous_vec_test.go
@@ -39,6 +39,7 @@ var vecBuiltinMiscellaneousCases = map[string][]vecExprBenchCase{
 	ast.AnyValue: {
 		{retEvalType: types.ETDuration, childrenTypes: []types.EvalType{types.ETDuration}},
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETInt}},
+		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal}},
 	},
 	ast.NameConst: {
 		{retEvalType: types.ETDuration, childrenTypes: []types.EvalType{types.ETString, types.ETDuration}},
