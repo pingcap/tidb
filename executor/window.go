2ba3f4fd7a268189ebaa2c76aaca11bdf224e7b8
Feng Liyuan <darktemplar.f@gmail.com>
2019-10-12 20:22:10 +0800

    fix build

--- executor/window.go
+++ executor/window.go
@@ -167,9 +167,7 @@ func (e *WindowExec) fetchChildIfNecessary(ctx context.Context) (EOF bool, err e
 func (e *WindowExec) copyChk(src, dst *chunk.Chunk) error {
 	columns := e.Schema().Columns[:len(e.Schema().Columns)-e.numWindowFuncs]
 	for i, col := range columns {
-		if err := dst.MakeRefTo(i, src, col.Index); err != nil {
-			return err
-		}
+		dst.MakeRefTo(i, src, col.Index)
 	}
 	return nil
 }
