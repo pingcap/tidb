86b09e2c4ac713c0c3beecfbfcd575deefef0acf
tangwz <tangwz.com@gmail.com>
2019-10-14 22:14:26 +0800

    expression: implement vectorized evaluation for builtinCastJSONAsIntSig

--- expression/builtin_cast_vec_test.go
+++ expression/builtin_cast_vec_test.go
@@ -25,6 +25,7 @@ var vecBuiltinCastCases = map[string][]vecExprBenchCase{
 	ast.Cast: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETInt}},
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETReal}},
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETJson}},
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETInt}},
 		{retEvalType: types.ETDuration, childrenTypes: []types.EvalType{types.ETInt}, geners: []dataGenerator{new(randDurInt)}},
 		{retEvalType: types.ETReal, childrenTypes: []types.EvalType{types.ETReal}},
