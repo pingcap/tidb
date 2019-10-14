ae26269176367352951b57e437f2acd03edd22de
lzmhhh123 <lzmhhh123@gmail.com>
2019-10-14 19:13:50 +0800

    address comment

--- planner/core/exhaust_physical_plans.go
+++ planner/core/exhaust_physical_plans.go
@@ -593,7 +593,7 @@ func (p *LogicalJoin) buildIndexJoinInner2IndexScan(
 	// The index merge join's inner plan is different from index join, so we
 	// should construct another inner plan for it.
 	// Because we can't keep order for union scan, if there is a union scan in inner task,
-	// we can construct index merge join.
+	// we can't construct index merge join.
 	if us == nil {
 		innerTask2 := p.constructInnerIndexScanTask(ds, helper.chosenPath, helper.chosenRemained, outerJoinKeys, us, rangeInfo, true, !prop.IsEmpty() && prop.Items[0].Desc, avgInnerRowCnt)
 		joins = append(joins, p.constructIndexMergeJoin(prop, outerIdx, innerTask2, helper.chosenRanges, keyOff2IdxOff, helper.chosenPath, helper.lastColManager)...)
