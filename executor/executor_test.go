c923a74f41bd7046a3d62fb4da4a544dc2b79efb
tsthght <781181214@qq.com>
2019-10-15 13:51:17 +0800

    Remove the comment

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -1850,7 +1850,6 @@ func (s *testSuiteP1) TestGeneratedColumnRead(c *C) {
 	result = tk.MustQuery(`SELECT * FROM test_gc_read_m`)
 	result.Check(testkit.Rows(`1 2 4`, `2 3 6`))
 
-	// Test for issue #12233
 	tk.MustExec(`create table testjson( id int auto_increment not null primary key, j json )default charset=utf8 engine=innodb;`)
 	tk.MustExec(`insert into testjson set j='{"test":1}';`)
 	result = tk.MustQuery(`select id from testjson where json_extract(j, '$.test');`)
