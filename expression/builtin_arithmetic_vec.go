a7aaba7a9784c1d2da51aa3c9b37e291b5ddd86b
ZhaoQian888 <1900022850@pku.edu.cn>
2019-10-15 16:35:47 +0800

    delete useless line

--- expression/builtin_arithmetic_vec.go
+++ expression/builtin_arithmetic_vec.go
@@ -170,7 +170,6 @@ func (b *builtinArithmeticModRealSig) vecEvalReal(input *chunk.Chunk, result *ch
 	if err := b.args[0].VecEvalReal(b.ctx, input, result); err != nil {
 		return err
 	}
-	// result.MergeNulls(buf)
 	x := result.Float64s()
 	y := buf.Float64s()
 	for i := 0; i < n; i++ {
