7ee20ee225038f58937ecc08557f3c6aa3fbe62d
tsthght <781181214@qq.com>
2019-10-15 14:50:55 +0800

    simplify table schema of testcase

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -1850,10 +1850,10 @@ func (s *testSuiteP1) TestGeneratedColumnRead(c *C) {
 	result = tk.MustQuery(`SELECT * FROM test_gc_read_m`)
 	result.Check(testkit.Rows(`1 2 4`, `2 3 6`))
 
-	tk.MustExec(`create table testjson( id int auto_increment not null primary key, j json )default charset=utf8 engine=innodb;`)
-	tk.MustExec(`insert into testjson set j='{"test":1}';`)
-	result = tk.MustQuery(`select id from testjson where json_extract(j, '$.test');`)
-	result.Check(testkit.Rows(`1`))
+	tk.MustExec(`create table testjson(j json )`)
+	tk.MustExec(`insert into testjson set j='{"test":1}'`)
+	result = tk.MustQuery(`select j from testjson where json_extract(j, '$.test')`)
+	result.Check(testkit.Rows(`{"test":1}`))
 
 	// Test not null generated columns.
 	tk.MustExec(`CREATE TABLE test_gc_read_1(a int primary key, b int, c int as (a+b) not null, d int as (a*b) stored)`)
