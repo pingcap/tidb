f590aec8e1ce5424ebaa8285911c2c124cbe83a7
Feng Liyuan <darktemplar.f@gmail.com>
2019-10-16 18:33:21 +0800

    fix test

--- executor/aggregate.go
+++ executor/aggregate.go
@@ -207,7 +207,6 @@ func (e *HashAggExec) Close() error {
 		}
 		close(e.finalOutputCh)
 	}
-	e.executed = false
 	close(e.finishCh)
 	for _, ch := range e.partialOutputChs {
 		for range ch {
@@ -215,6 +214,7 @@ func (e *HashAggExec) Close() error {
 	}
 	for range e.finalOutputCh {
 	}
+	e.executed = false
 	return e.baseExecutor.Close()
 }
 
