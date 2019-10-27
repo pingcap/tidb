bb71711e8ac69cbf4e6338bd41f264e62f50a77f
shihongzhi <shi65881583@gmail.com>
2019-10-27 20:21:44 +0800

    implement vectorized evaluation for `builtinSubstringBinary2ArgsSig`

--- expression/builtin_string_vec_test.go
+++ expression/builtin_string_vec_test.go
@@ -52,10 +52,15 @@ var vecBuiltinStringCases = map[string][]vecExprBenchCase{
 	ast.ASCII: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETString}, geners: []dataGenerator{&defaultGener{0.2, types.ETString}}},
 	},
-	ast.Concat:         {},
-	ast.ConcatWS:       {},
-	ast.Convert:        {},
-	ast.Substring:      {},
+	ast.Concat:   {},
+	ast.ConcatWS: {},
+	ast.Convert:  {},
+	ast.Substring: {
+		{
+			retEvalType:   types.ETString,
+			childrenTypes: []types.EvalType{types.ETString, types.ETInt},
+		},
+	},
 	ast.SubstringIndex: {},
 	ast.Locate: {
 		{
