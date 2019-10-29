66f7697f76ce7d8b9c3075093335e9efafe19935
tiancaiamao <tiancaiamao@gmail.com>
2019-10-29 14:56:12 +0800

    address comment

--- store/tikv/2pc_test.go
+++ store/tikv/2pc_test.go
@@ -14,6 +14,7 @@
 package tikv
 
 import (
+	"bytes"
 	"context"
 	"math"
 	"math/rand"
@@ -506,6 +507,21 @@ func (s *testCommitterSuite) TestRejectCommitTS(c *C) {
 	time.Sleep(3 * time.Millisecond)
 	err = commiter.commitKeys(bo, commiter.keys)
 	c.Assert(err, IsNil)
+
+	// Use startTS+2 to read the data and get nothing.
+	// Use max.Uint64 to read the data and success.
+	// That means the final commitTS > startTS+2, it's not the one we provide.
+	// So we cover the rety commitTS logic.
+	txn1, err := s.store.BeginWithStartTS(commiter.startTS + 2)
+	c.Assert(err, IsNil)
+	_, err = txn1.Get(bo.ctx, []byte("x"))
+	c.Assert(kv.IsErrNotFound(err), IsTrue)
+
+	txn2, err := s.store.BeginWithStartTS(math.MaxUint64)
+	c.Assert(err, IsNil)
+	val, err := txn2.Get(bo.ctx, []byte("x"))
+	c.Assert(err, IsNil)
+	c.Assert(bytes.Equal(val, []byte("v")), IsTrue)
 }
 
 func (s *testCommitterSuite) TestPessimisticPrewriteRequest(c *C) {
