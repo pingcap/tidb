a5ccb5fce0e4fde068f9057e485ca62aa280369c
Eugene Kalinin <ekalinin@users.noreply.github.com>
2019-10-29 13:29:38 +0300

    [parser] parser: support FLUSH LOGS | HOSTS syntax (#589)

--- parser/ast/misc.go
+++ parser/ast/misc.go
@@ -572,6 +572,8 @@ const (
 	FlushPrivileges
 	FlushStatus
 	FlushTiDBPlugin
+	FlushHosts
+	FlushLogs
 )
 
 // FlushStmt is a statement to flush tables/privileges/optimizer costs and so on.
@@ -621,8 +623,12 @@ func (n *FlushStmt) Restore(ctx *RestoreCtx) error {
 			}
 			ctx.WritePlain(v)
 		}
+	case FlushHosts:
+		ctx.WriteKeyWord("HOSTS")
+	case FlushLogs:
+		ctx.WriteKeyWord("LOGS")
 	default:
-		return errors.New("Unsupported type of FlushTables")
+		return errors.New("Unsupported type of FlushStmt")
 	}
 	return nil
 }
