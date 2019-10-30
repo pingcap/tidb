38fd2388f6b5f61bfa9607e2ca540816f59a099b
Jack Yu <jackysp@gmail.com>
2019-10-30 11:37:06 +0800

    [parser] mysql: add some errors for TiDB (#584)

--- parser/mysql/errcode.go
+++ parser/mysql/errcode.go
@@ -947,6 +947,12 @@ const (
 	ErrUnsupportedReloadPlugin    = 8018
 	ErrUnsupportedReloadPluginVar = 8019
 	ErrTableLocked                = 8020
+	ErrNotExist                   = 8021
+	ErrTxnRetryable               = 8022
+	ErrCannotSetNilValue          = 8023
+	ErrInvalidTxn                 = 8024
+	ErrEntryTooLarge              = 8025
+	ErrNotImplemented             = 8026
 
 	// TiKV/PD errors.
 	ErrPDServerTimeout    = 9001
