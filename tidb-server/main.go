0049816ee0884e684c276e75b9969aac20b3f82e
AilinKid <314806019@qq.com>
2019-11-01 16:37:43 +0800

    add hot reload items

--- tidb-server/main.go
+++ tidb-server/main.go
@@ -387,7 +387,7 @@ func loadConfig() string {
 var hotReloadConfigItems = []string{"Performance.MaxProcs", "Performance.MaxMemory", "Performance.CrossJoin",
 	"Performance.FeedbackProbability", "Performance.QueryFeedbackLimit", "Performance.PseudoEstimateRatio",
 	"OOMUseTmpStorage", "OOMAction", "MemQuotaQuery", "StmtSummary.MaxStmtCount", "StmtSummary.MaxSQLLength", "Log.QueryLogMaxLen",
-	"TiKVClient.EnableChunkRPC"}
+	"TiKVClient.EnableChunkRPC", "StoreLimit"}
 
 func reloadConfig(nc, c *config.Config) {
 	// Just a part of config items need to be reload explicitly.
