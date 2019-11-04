2390d100ac4a533ce81798f1e675518ac8e0a820
CWang <icditwang@163.com>
2019-11-04 21:58:15 +0800

    add noted for BinaryFlag in test case

--- expression/builtin_string_vec_test.go
+++ expression/builtin_string_vec_test.go
@@ -176,6 +176,7 @@ var vecBuiltinStringCases = map[string][]vecExprBenchCase{
 	},
 	ast.Left: {
 		{retEvalType: types.ETString, childrenTypes: []types.EvalType{types.ETString, types.ETInt}},
+		// need to add BinaryFlag for the Binary func
 		{retEvalType: types.ETString, childrenTypes: []types.EvalType{types.ETString, types.ETInt},
 			childrenFieldTypes: []*types.FieldType{{Tp: mysql.TypeString, Flag: mysql.BinaryFlag, Collate: charset.CollationBin},
 				{Tp: mysql.TypeLonglong}},
