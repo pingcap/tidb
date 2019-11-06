3fd6796f050623d9ecfc72e43abd39844f7f90c6
Lonng <heng@lonng.org>
2019-11-06 11:50:27 +0800

    address comment
    Signed-off-by: Lonng <heng@lonng.org>

--- expression/builtin_arithmetic.go
+++ expression/builtin_arithmetic.go
@@ -422,14 +422,13 @@ func (s *builtinArithmeticMinusIntSig) evalInt(row chunk.Row) (val int64, isNull
 			return 0, true, types.ErrOverflow.GenWithStackByArgs("BIGINT UNSIGNED", fmt.Sprintf("(%s - %s)", s.args[0].String(), s.args[1].String()))
 		}
 	case !isLHSUnsigned && isRHSUnsigned:
-		if (a < 0) || (uint64(a) < uint64(b)) {
+		if a < 0 || uint64(a) < uint64(b) {
 			return 0, true, types.ErrOverflow.GenWithStackByArgs("BIGINT UNSIGNED", fmt.Sprintf("(%s - %s)", s.args[0].String(), s.args[1].String()))
 		}
 	case !isLHSUnsigned && !isRHSUnsigned:
-		// We need `(a >= 0 && b == math.MinInt64)` because of -(math.MinInt64) == math.MinInt64.
-		// If a<0 && b<=0, then no matter whether b==math.MinInt64, a-b will not overflow.
-		// If a<0 && b>0, then a-b<0, so we need `math.MinInt64<=a-b`, which means `-b >= math.MinInt64 -a`,
-		// no matte whether a==math.MinInt64.
+		// We need `(a >= 0 && b == math.MinInt64)` due to `-(math.MinInt64) == math.MinInt64`.
+		// If `a<0 && b<=0`: `a-b` will not overflow even though b==math.MinInt64.
+		// If `a<0 && b>0`: `a-b` will not overflow only if `math.MinInt64<=a-b` satisfied
 		if (a >= 0 && b == math.MinInt64) || (a > 0 && -b > math.MaxInt64-a) || (a < 0 && -b < math.MinInt64-a) {
 			return 0, true, types.ErrOverflow.GenWithStackByArgs("BIGINT", fmt.Sprintf("(%s - %s)", s.args[0].String(), s.args[1].String()))
 		}
