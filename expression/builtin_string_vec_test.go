452efd13a24029950c76d89b0ff536b8e5f7aaf5
mmyj <mayj.hello@gmail.com>
2019-11-07 22:56:47 +0800

    expression: implement vectorized evaluation for `builtinConcatSig`

--- expression/builtin_string_vec_test.go
+++ expression/builtin_string_vec_test.go
@@ -54,7 +54,9 @@ var vecBuiltinStringCases = map[string][]vecExprBenchCase{
 	ast.ASCII: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETString}, geners: []dataGenerator{&defaultGener{0.2, types.ETString}}},
 	},
-	ast.Concat:   {},
+	ast.Concat: {
+		{retEvalType: types.ETString, childrenTypes: []types.EvalType{types.ETString, types.ETString, types.ETString}},
+	},
 	ast.ConcatWS: {},
 	ast.Convert:  {},
 	ast.Substring: {
