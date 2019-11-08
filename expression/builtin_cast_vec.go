06260f38c473a20af5722ee9e9fa6c5ff8a6ace5
chengmyy <573136281@qq.com>
2019-11-08 16:19:47 +0800

    fix handle err

--- expression/builtin_cast_vec.go
+++ expression/builtin_cast_vec.go
@@ -1085,11 +1085,7 @@ func (b *builtinCastTimeAsTimeSig) vecEvalTime(input *chunk.Chunk, result *chunk
 		}
 		res, err = tm.RoundFrac(sc, int8(b.tp.Decimal))
 		if err != nil {
-			if err = handleInvalidTimeError(b.ctx, err); err != nil {
-				return err
-			}
-			result.SetNull(i, true)
-			continue
+			return err
 		}
 
 		if b.tp.Tp == mysql.TypeDate {
