fba46710aef97038e69325a4c619cf85ab132311
Haibin Xie <lambdax.tyler@gmail.com>
2019-11-08 19:14:53 +0800

    stats: fix panic when fast analyze on empty table (#13284)

--- executor/analyze.go
+++ executor/analyze.go
@@ -1089,7 +1089,9 @@ func (e *AnalyzeFastExec) runTasks() ([]*statistics.Histogram, []*statistics.CMS
 		sort.Slice(collector.Samples, func(i, j int) bool { return collector.Samples[i].RowID < collector.Samples[j].RowID })
 		collector.CalcTotalSize()
 		// Scale the total column size.
-		collector.TotalSize *= rowCount / int64(len(collector.Samples))
+		if len(collector.Samples) > 0 {
+			collector.TotalSize *= rowCount / int64(len(collector.Samples))
+		}
 		if i < hasPKInfo {
 			hists[i], cms[i], err = e.buildColumnStats(e.pkInfo.ID, e.collectors[i], &e.pkInfo.FieldType, rowCount)
 		} else if i < hasPKInfo+len(e.colsInfo) {
