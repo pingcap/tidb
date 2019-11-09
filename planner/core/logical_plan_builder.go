b2e3f97435dd21bbc464d7c87149fbd632c48ebd
Yiding Cui <winoros@gmail.com>
2019-11-09 10:51:12 +0800

    release-3.1: pick two commits about update with subquery (#13290)

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -2674,11 +2674,19 @@ func (b *PlanBuilder) buildUpdate(ctx context.Context, update *ast.UpdateStmt) (
 		b.visitInfo = appendVisitInfo(b.visitInfo, mysql.SelectPriv, dbName, t.Name.L, "", nil)
 	}
 
+	oldSchema := p.Schema().Clone()
 	if sel.Where != nil {
-		p, err = b.buildSelection(ctx, p, sel.Where, nil)
+		p, err = b.buildSelection(ctx, p, update.Where, nil)
 		if err != nil {
 			return nil, err
 		}
+	}
+	// TODO: expression rewriter should not change the output columns. We should cut the columns here.
+	if p.Schema().Len() != oldSchema.Len() {
+		proj := LogicalProjection{Exprs: expression.Column2Exprs(oldSchema.Columns)}.Init(b.ctx)
+		proj.SetSchema(oldSchema)
+		proj.SetChildren(p)
+		p = proj
 	}
 	if sel.OrderBy != nil {
 		p, err = b.buildSort(ctx, p, sel.OrderBy.Items, nil, nil)
