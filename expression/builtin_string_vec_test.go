861ed79095439d9fda8564f370ceed4503d1ae98
庄天翼 <zty0826@gmail.com>
2019-11-09 15:21:42 +0800

    expression: implement vectorized evaluation for `builtinOrdSig` (#13304)

--- expression/builtin_string_vec_test.go
+++ expression/builtin_string_vec_test.go
@@ -151,7 +151,9 @@ var vecBuiltinStringCases = map[string][]vecExprBenchCase{
 		{retEvalType: types.ETString, childrenTypes: []types.EvalType{types.ETInt}},
 		{retEvalType: types.ETString, childrenTypes: []types.EvalType{types.ETString}, geners: []dataGenerator{&numStrGener{rangeInt64Gener{-10, 10}}}},
 	},
-	ast.Ord:   {},
+	ast.Ord: {
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETString}},
+	},
 	ast.Quote: {},
 	ast.Bin: {
 		{retEvalType: types.ETString, childrenTypes: []types.EvalType{types.ETInt}},
