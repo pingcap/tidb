4280223bfff36e0673467c822cce429131a05c7c
Reminiscent <y870414423@gmail.com>
2019-11-11 19:00:56 +0800

    add some comments for ```GetGoDuration``` function

--- util/chunk/column.go
+++ util/chunk/column.go
@@ -538,7 +538,8 @@ func (c *Column) GetDuration(rowID int, fillFsp int) types.Duration {
 	return types.Duration{Duration: time.Duration(dur), Fsp: int8(fillFsp)}
 }
 
-// GetGoDuration returns the Duration in the specific row.
+// GetGoDuration returns the time.Duration in the specific row.
+// Different from the Row.GetDuration method, the argument Fsp is ignored, so the user should handle it outside.
 func (c *Column) GetGoDuration(rowID int) time.Duration {
 	dur := *(*int64)(unsafe.Pointer(&c.data[rowID*8]))
 	return time.Duration(dur)
