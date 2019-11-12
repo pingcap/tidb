3f2d9d8540d11add4a164274c71400d634b2b58a
Eugene Kalinin <ekalinin@users.noreply.github.com>
2019-11-12 14:47:36 +0300

    expression: implement vectorized evaluation for builtinNowWithArgSig (#13372)

--- expression/builtin_time_vec_test.go
+++ expression/builtin_time_vec_test.go
@@ -71,6 +71,9 @@ var vecBuiltinTimeCases = map[string][]vecExprBenchCase{
 	ast.MicroSecond: {},
 	ast.Now: {
 		{retEvalType: types.ETDatetime},
+		{retEvalType: types.ETDatetime, childrenTypes: []types.EvalType{types.ETInt},
+			geners: []dataGenerator{&rangeInt64Gener{0, 7}},
+		},
 	},
 	ast.DayOfWeek: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDatetime}},
