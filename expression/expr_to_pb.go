7071c71dd1ccd82f1167776741b235dad66e3642
Lonng <heng@lonng.org>
2019-11-12 15:31:12 +0800

    expression: panic in test mode if builtin function's PbCode unspecified (#13334)
    Signed-off-by: Lonng <heng@lonng.org>

--- expression/expr_to_pb.go
+++ expression/expr_to_pb.go
@@ -17,6 +17,7 @@ import (
 	"strings"
 	"sync/atomic"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/charset"
@@ -221,18 +222,21 @@ func (pc PbConverter) columnToPBExpr(column *Column) *tipb.Expr {
 }
 
 func (pc PbConverter) scalarFuncToPBExpr(expr *ScalarFunction) *tipb.Expr {
-	// check whether this function can be pushed.
+	// Check whether this function has ProtoBuf signature.
+	pbCode := expr.Function.PbCode()
+	if pbCode <= tipb.ScalarFuncSig_Unspecified {
+		failpoint.Inject("PanicIfPbCodeUnspecified", func() {
+			panic(errors.Errorf("unspecified PbCode: %T", expr.Function))
+		})
+		return nil
+	}
+
+	// Check whether this function can be pushed.
 	if !pc.canFuncBePushed(expr) {
 		return nil
 	}
 
-	// check whether this function has ProtoBuf signature.
-	pbCode := expr.Function.PbCode()
-	if pbCode < 0 {
-		return nil
-	}
-
-	// check whether all of its parameters can be pushed.
+	// Check whether all of its parameters can be pushed.
 	children := make([]*tipb.Expr, 0, len(expr.GetArgs()))
 	for _, arg := range expr.GetArgs() {
 		pbArg := pc.ExprToPB(arg)
@@ -252,7 +256,7 @@ func (pc PbConverter) scalarFuncToPBExpr(expr *ScalarFunction) *tipb.Expr {
 		implicitArgs = encoded
 	}
 
-	// construct expression ProtoBuf.
+	// Construct expression ProtoBuf.
 	return &tipb.Expr{
 		Tp:        tipb.ExprType_ScalarFunc,
 		Val:       implicitArgs,
