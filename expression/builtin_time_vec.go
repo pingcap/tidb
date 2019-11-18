0e782e71603495bb038416f2ab57b2c4363460fc
Lanearth <949930437@qq.com>
2019-11-18 20:57:20 +0800

    format the import package

--- expression/builtin_time_vec.go
+++ expression/builtin_time_vec.go
@@ -14,13 +14,13 @@
 package expression
 
 import (
-	"github.com/pingcap/tidb/sessionctx/variable"
 	"math"
 	"strconv"
 	"time"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/chunk"
 )
