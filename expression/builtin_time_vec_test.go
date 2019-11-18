a8e9fa3857ba07bd344f5bf8c381b923996d03ea
Lanearth <949930437@qq.com>
2019-11-18 12:04:54 +0800

    expression: implement vectorized evaluation for `builtinWeekWithModeSig` (#13402)

--- expression/builtin_time_vec_test.go
+++ expression/builtin_time_vec_test.go
@@ -142,6 +142,9 @@ var vecBuiltinTimeCases = map[string][]vecExprBenchCase{
 			},
 		},
 	},
+	ast.Week: {
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDatetime, types.ETInt}},
+	},
 	ast.Month: {
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDatetime}},
 	},
