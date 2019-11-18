4cd3cd6188b49017ecda6a0069ebb556dc13b199
imtbkcat <songlingyu@pingcap.com>
2019-11-18 14:24:08 +0800

    change error to log

--- session/session.go
+++ session/session.go
@@ -465,22 +465,25 @@ func (s *session) doCommitWithRetry(ctx context.Context) error {
 		// Don't retry in BatchInsert mode. As a counter-example, insert into t1 select * from t2,
 		// BatchInsert already commit the first batch 1000 rows, then it commit 1000-2000 and retry the statement,
 		// Finally t1 will have more data than t2, with no errors return to user!
-		if s.isTxnRetryableError(err) {
-			if !s.sessionVars.BatchInsert && commitRetryLimit > 0 && !isPessimistic {
-				logutil.Logger(ctx).Warn("sql",
-					zap.String("label", s.getSQLLabel()),
-					zap.Error(err),
-					zap.String("txn", s.txn.GoString()))
-				// Transactions will retry 2 ~ commitRetryLimit times.
-				// We make larger transactions retry less times to prevent cluster resource outage.
-				txnSizeRate := float64(txnSize) / float64(kv.TxnTotalSizeLimit)
-				maxRetryCount := commitRetryLimit - int64(float64(commitRetryLimit-1)*txnSizeRate)
-				err = s.retry(ctx, uint(maxRetryCount))
-			} else {
-				err = errors.Annotate(err, fmt.Sprintf("isBatchInsert:%v isPessimistic:%v commitRetryLimit: %v",
-					s.sessionVars.BatchInsert, isPessimistic, commitRetryLimit))
-			}
+		if s.isTxnRetryableError(err) && !s.sessionVars.BatchInsert && commitRetryLimit > 0 && !isPessimistic {
+			logutil.Logger(ctx).Warn("sql",
+				zap.String("label", s.getSQLLabel()),
+				zap.Error(err),
+				zap.String("txn", s.txn.GoString()))
+			// Transactions will retry 2 ~ commitRetryLimit times.
+			// We make larger transactions retry less times to prevent cluster resource outage.
+			txnSizeRate := float64(txnSize) / float64(kv.TxnTotalSizeLimit)
+			maxRetryCount := commitRetryLimit - int64(float64(commitRetryLimit-1)*txnSizeRate)
+			err = s.retry(ctx, uint(maxRetryCount))
+		} else {
+			logutil.Logger(ctx).Warn("can not retry txn",
+				zap.String("label", s.getSQLLabel()),
+				zap.Error(err),
+				zap.Bool("isBatchInsert", s.sessionVars.BatchInsert),
+				zap.Bool("isPessimistic", isPessimistic),
+				zap.Int64("commitRetryLimit", commitRetryLimit))
 		}
+
 	}
 	counter := s.sessionVars.TxnCtx.StatementCount
 	duration := time.Since(s.GetSessionVars().TxnCtx.CreateTime).Seconds()
