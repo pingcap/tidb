765b8c9b3146bf1ba088550c101533be67090204
zhongshanhao <572108581@qq.com>
2019-11-20 15:49:20 +0800

    expression: implement vectorized evaluation for builtinExtractDatetimeSig

--- expression/builtin_time_vec_test.go
+++ expression/builtin_time_vec_test.go
@@ -92,7 +92,11 @@ var vecBuiltinTimeCases = map[string][]vecExprBenchCase{
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDatetime}},
 	},
 	ast.MicroSecond: {
+<<<<<<< HEAD
 		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDuration}, geners: []dataGenerator{&rangeDurationGener{0.2}}},
+=======
+		{retEvalType: types.ETInt, childrenTypes: []types.EvalType{types.ETDuration}},
+>>>>>>> expression: implement vectorized evaluation for builtinExtractDatetimeSig
 	},
 	ast.Now: {
 		{retEvalType: types.ETDatetime},
