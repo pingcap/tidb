b08e881a8ff46b511e5c026a296c26482b0cae3e
AilinKid <314806019@qq.com>
2019-11-20 18:02:07 +0800

    sort insert

--- executor/insert_test.go
+++ executor/insert_test.go
@@ -768,5 +768,5 @@ func (s *testSuite3) TestJiraIssue5366(c *C) {
 	tk.MustExec(`use test`)
 	tk.MustExec(`create table bug (a varchar(100))`)
 	tk.MustExec(` insert into bug select  ifnull(JSON_UNQUOTE(JSON_EXTRACT('[{"amount":2000,"feeAmount":0,"merchantNo":"20190430140319679394","shareBizCode":"20160311162_SECOND"}]', '$[0].merchantNo')),'') merchant_no union SELECT '20180531557' merchant_no;`)
-	tk.MustQuery(`select * from bug`).Check(testkit.Rows("20190430140319679394", "20180531557"))
+	tk.MustQuery(`select * from bug`).Sort().Check(testkit.Rows("20180531557", "20190430140319679394"))
 }
