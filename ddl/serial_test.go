f509bdfb2bb67edc07a1f712e056677da4bb4a2f
Lynn <zimu_xia@126.com>
2019-11-21 16:13:31 +0800

    ddl: fix the error msg for drop PK

--- ddl/serial_test.go
+++ ddl/serial_test.go
@@ -91,6 +91,23 @@ func (s *testSerialSuite) TestPrimaryKey(c *C) {
 	c.Assert(ddl.ErrUnsupportedModifyPrimaryKey.Equal(err), IsTrue)
 	_, err = tk.Exec("alter table primary_key_test drop primary key")
 	c.Assert(err.Error(), Equals, "[ddl:8200]Unsupported drop primary key when alter-primary-key is false")
+
+	// Change the value of AlterPrimaryKey.
+	tk.MustExec("create table primary_key_test1 (a int, b varchar(10), primary key(a))")
+	tk.MustExec("create table primary_key_test2 (a int, b varchar(10), primary key(b))")
+	cfg := config.GetGlobalConfig()
+	newCfg := *cfg
+	orignalAlterPrimaryKey := newCfg.AlterPrimaryKey
+	newCfg.AlterPrimaryKey = true
+	config.StoreGlobalConfig(&newCfg)
+	defer func() {
+		newCfg.AlterPrimaryKey = orignalAlterPrimaryKey
+		config.StoreGlobalConfig(&newCfg)
+	}()
+
+	_, err = tk.Exec("alter table primary_key_test1 drop primary key")
+	c.Assert(err.Error(), Equals, "[ddl:8200]Unsupported drop primary key when the table's pkIsHandle is true")
+	tk.MustExec("alter table primary_key_test2 drop primary key")
 }
 
 func (s *testSerialSuite) TestMultiRegionGetTableEndHandle(c *C) {
