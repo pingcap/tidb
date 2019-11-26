441a4facfde1d3dfaaac157b5fe85830311fb12e
wshwsh12 <793703860@qq.com>
2019-11-26 15:45:08 +0800

    address comments

--- store/mockstore/mocktikv/cop_handler_dag.go
+++ store/mockstore/mocktikv/cop_handler_dag.go
@@ -576,7 +576,7 @@ func (h *rpcHandler) fillUpData4SelectResponse(selResp *tipb.SelectResponse, dag
 		h.encodeDefault(selResp, rows, dagReq.OutputOffsets)
 	case tipb.EncodeType_TypeChunk:
 		if dagReq.TidbSystemEndian != distsql.GetSystemEndian() {
-			return errors.Errorf("Mocktikv endian is must be same as TiDB system endian.")
+			return errors.Errorf("Mocktikv endian must be the same as TiDB system endian.")
 		}
 		colTypes := h.constructRespSchema(dagCtx)
 		loc := dagCtx.evalCtx.sc.TimeZone
