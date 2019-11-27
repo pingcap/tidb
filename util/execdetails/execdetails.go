897cbe85d0fced4090ba73b270b0981e3e513bf2
Reminiscent <y870414423@gmail.com>
2019-11-27 15:23:28 +0800

    fix typo

--- util/execdetails/execdetails.go
+++ util/execdetails/execdetails.go
@@ -423,7 +423,7 @@ func (e *RuntimeStats) SetRowNum(rowNum int64) {
 }
 
 // SetConcurrencyInfo sets the concurrency information.
-// When the num <= 0, it means the exector operator is not executed concurrently.
+// When the num <= 0, it means the exector operator is not executed parallel.
 func (e *RuntimeStats) SetConcurrencyInfo(name string, num int) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
