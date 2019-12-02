c2998d24042e3a4d99099cbc003f03b4c3501a14
Shuaipeng Yu <jackysp@gmail.com>
2019-12-02 17:03:43 +0800

    tiny change
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- executor/adapter.go
+++ executor/adapter.go
@@ -254,11 +254,6 @@ func (a *ExecStmt) IsReadOnly(vars *variable.SessionVars) bool {
 // RebuildPlan rebuilds current execute statement plan.
 // It returns the current information schema version that 'a' is using.
 func (a *ExecStmt) RebuildPlan(ctx context.Context) (int64, error) {
-	startTime := time.Now()
-	defer func() {
-		a.Ctx.GetSessionVars().DurationCompile = time.Since(startTime)
-	}()
-
 	is := GetInfoSchema(a.Ctx)
 	a.InfoSchema = is
 	if err := plannercore.Preprocess(a.Ctx, a.StmtNode, is, plannercore.InTxnRetry); err != nil {
