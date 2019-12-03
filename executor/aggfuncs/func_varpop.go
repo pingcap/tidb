cb94e59a098e2a171c2a6cf650b8739d52ef014f
张旭 <zhangxu@zhangxudeMacBook-Pro.local>
2019-12-03 20:01:38 +0800

    using new algorithm to calculate variance

--- executor/aggfuncs/func_varpop.go
+++ executor/aggfuncs/func_varpop.go
@@ -1,9 +1,9 @@
 package aggfuncs
 
 import (
-	"github.com/pingcap/errors"
-	"github.com/pingcap/tidb/sessionctx"
-	"github.com/pingcap/tidb/util/chunk"
+"github.com/pingcap/errors"
+"github.com/pingcap/tidb/sessionctx"
+"github.com/pingcap/tidb/util/chunk"
 )
 
 type varPop4Float64 struct {
@@ -13,7 +13,7 @@ type varPop4Float64 struct {
 type partialResult4Float64 struct {
 	count        int64
 	sum          float64
-	quadraticSum float64
+	variance	float64
 }
 
 func (e *varPop4Float64) AllocPartialResult() PartialResult {
@@ -32,9 +32,15 @@ func (e *varPop4Float64) AppendFinalResult2Chunk(sctx sessionctx.Context, pr Par
 		chk.AppendNull(e.ordinal)
 		return nil
 	}
-	varPop := p.quadraticSum/float64(p.count) - p.sum*p.sum/float64(p.count*p.count)
-	chk.AppendFloat64(e.ordinal, varPop)
+	varicance := p.variance / float64(p.count)
+	chk.AppendFloat64(e.ordinal, varicance)
 	return nil
+}
+
+func calculateIntermediate(count int64, sum float64, input float64, variance float64) float64 {
+	t := float64(count) * input - sum
+	variance += (t * t) / (float64(count * (count - 1)))
+	return variance
 }
 
 func (e *varPop4Float64) UpdatePartialResult(sctx sessionctx.Context, rowsInGroup []chunk.Row, pr PartialResult) error {
@@ -47,11 +53,22 @@ func (e *varPop4Float64) UpdatePartialResult(sctx sessionctx.Context, rowsInGrou
 		if isNull {
 			continue
 		}
-		p.sum += input
-		p.quadraticSum += input * input
 		p.count++
+		p.sum += input
+		if p.count > 1 {
+			p.variance = calculateIntermediate(p.count, p.sum, input, p.variance)
+		}
 	}
 	return nil
+}
+
+func calculateMerge(srcCount, dstCount int64, srcSum, dstSum, srcVariance, dstVariance float64) float64 {
+	srcCountFloat64 := float64(srcCount)
+	dstCountFloat64 := float64(dstCount)
+
+	t := (srcCountFloat64 / dstCountFloat64) * dstSum - srcSum
+	dstVariance += srcVariance + ((dstCountFloat64 / srcCountFloat64) / (dstCountFloat64 + srcCountFloat64)) * t * t
+	return dstVariance
 }
 
 func (e *varPop4Float64) MergePartialResult(sctx sessionctx.Context, src PartialResult, dst PartialResult) error {
@@ -59,8 +76,16 @@ func (e *varPop4Float64) MergePartialResult(sctx sessionctx.Context, src Partial
 	if p1.count == 0 {
 		return nil
 	}
-	p2.sum += p1.sum
-	p2.quadraticSum += p1.quadraticSum
-	p2.count += p1.count
+	if p2.count == 0 {
+		p2.count = p1.count
+		p2.sum = p1.sum
+		p2.variance = p1.variance
+		return nil
+	}
+	if p2.count != 0 && p1.count != 0 {
+		p2.variance = calculateMerge(p1.count, p2.count, p1.sum, p2.sum, p1.variance, p2.variance)
+		p2.count += p1.count
+		p2.sum += p1.sum
+	}
 	return nil
 }
