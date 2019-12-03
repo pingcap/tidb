1294a9405d2b7e402b51d2f2537141c9265eb891
knock-C <55902309+knock-C@users.noreply.github.com>
2019-12-03 22:47:45 +0800

    Merge branch 'master' of https://github.com/pingcap/tidb
    # Conflicts:
    #	expression/builtin_other_vec_test.go

--- expression/builtin_other_vec.go
+++ expression/builtin_other_vec.go
@@ -106,7 +106,7 @@ func (b *builtinBitCountSig) vecEvalInt(input *chunk.Chunk, result *chunk.Column
 		}
 		i64s[i] = i64s[i] - ((i64s[i] >> 1) & 0x5555555555555555)
 		i64s[i] = (i64s[i] & 0x3333333333333333) + ((i64s[i] >> 2) & 0x3333333333333333)
-		i64s[i] = (i64s[i]& 0x0f0f0f0f0f0f0f0f)+ ((i64s[i] >> 4) & 0x0f0f0f0f0f0f0f0f)
+		i64s[i] = (i64s[i] & 0x0f0f0f0f0f0f0f0f) + ((i64s[i] >> 4) & 0x0f0f0f0f0f0f0f0f)
 		i64s[i] = i64s[i] + (i64s[i] >> 8)
 		i64s[i] = i64s[i] + (i64s[i] >> 16)
 		i64s[i] = i64s[i] + (i64s[i] >> 32)
