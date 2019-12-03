e77b0b83c4366bd6fa0fe1409f215cd2a2239b2b
Arenatlx <ailinsilence4@gmail.com>
2019-12-03 13:50:26 +0800

    [dumpling] ddl : support ADMIN REPAIR TABLE to override bad tableInfo in meta & supply a REPAIR MODE for safely restart. (#12046)

--- dumpling/ddl/ddl_worker.go
+++ dumpling/ddl/ddl_worker.go
@@ -527,6 +527,8 @@ func (w *worker) runDDLJob(d *ddlCtx, t *meta.Meta, job *model.Job) (ver int64, 
 		ver, err = onDropSchema(t, job)
 	case model.ActionCreateTable:
 		ver, err = onCreateTable(d, t, job)
+	case model.ActionRepairTable:
+		ver, err = onRepairTable(d, t, job)
 	case model.ActionCreateView:
 		ver, err = onCreateView(d, t, job)
 	case model.ActionDropTable, model.ActionDropView:
