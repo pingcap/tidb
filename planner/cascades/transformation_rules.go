f841e40dc0dc3d287c0ed10d16a7eb27da940e28
t0350 <t0350.prog@gmail.com>
2019-12-04 16:45:06 +0800

    planner/cascades: add transformation rule SameProjectionElimiation

--- planner/cascades/transformation_rules.go
+++ planner/cascades/transformation_rules.go
@@ -60,6 +60,9 @@ var defaultTransformationMap = map[memo.Operand][]Transformation{
 	memo.OperandLimit: {
 		NewRuleTransformLimitToTopN(),
 	},
+	memo.OperandProjection: {
+		NewRuleSameProjectionElimination(),
+	},
 }
 
 type baseRule struct {
@@ -590,4 +593,44 @@ func (r *PushSelDownJoin) OnTransform(old *memo.ExprIter) (newExprs []*memo.Grou
 	newJoinExpr := memo.NewGroupExpr(join)
 	newJoinExpr.SetChildren(leftGroup, rightGroup)
 	return []*memo.GroupExpr{newJoinExpr}, true, false, nil
+}
+
+// SameProjectionElimination eliminate the projection.
+type SameProjectionElimination struct {
+	baseRule
+}
+
+// NewRuleSameProjectionElimination creates a new Transformation SameProjectionElimination.
+// The pattern of this rule is `Projection -> Any`.
+func NewRuleSameProjectionElimination() Transformation {
+	rule := &SameProjectionElimination{}
+	rule.pattern = memo.BuildPattern(
+		memo.OperandProjection,
+		memo.EngineTiDBOnly,
+		memo.NewPattern(memo.OperandAny, memo.EngineTiDBOnly),
+	)
+	return rule
+}
+
+// OnTransform implements Transformation interface.
+// This rule tries to eliminate the projection whose output columns are the same with its child.
+func (r *SameProjectionElimination) OnTransform(old *memo.ExprIter) (newExprs []*memo.GroupExpr, eraseOld bool, eraseAll bool, err error) {
+	child := old.Children[0]
+	if child.Group.Prop.Schema.Len() != old.GetExpr().Group.Prop.Schema.Len() {
+		return nil, false, false, nil
+	}
+
+	oldCols := old.GetExpr().Group.Prop.Schema.Columns
+	for i, col := range child.Group.Prop.Schema.Columns {
+		if !col.Equal(nil, oldCols[i]) {
+			return nil, false, false, nil
+		}
+	}
+
+	// Promote the children group's expression.
+	finalGroupExprs := make([]*memo.GroupExpr, 0, child.Group.Equivalents.Len())
+	for elem := child.Group.Equivalents.Front(); elem != nil; elem = elem.Next() {
+		finalGroupExprs = append(finalGroupExprs, elem.Value.(*memo.GroupExpr))
+	}
+	return finalGroupExprs, true, false, nil
 }
