8f09e816b2e07b0eeff754050be4beed08636204
Ewan Chou <coocood@gmail.com>
2019-12-05 19:39:49 +0800

    store/tikv: fix grpc backoff config (#13928)

--- store/tikv/client.go
+++ store/tikv/client.go
@@ -136,7 +136,10 @@ func (a *connArray) Init(addr string, security config.Security, idleNotify *uint
 			grpc.WithUnaryInterceptor(unaryInterceptor),
 			grpc.WithStreamInterceptor(streamInterceptor),
 			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(MaxRecvMsgSize)),
-			grpc.WithConnectParams(grpc.ConnectParams{Backoff: backoff.Config{BaseDelay: time.Second * 3}}),
+			grpc.WithConnectParams(grpc.ConnectParams{
+				Backoff:           backoff.Config{MaxDelay: time.Second * 3},
+				MinConnectTimeout: dialTimeout,
+			}),
 			grpc.WithKeepaliveParams(keepalive.ClientParameters{
 				Time:                time.Duration(keepAlive) * time.Second,
 				Timeout:             time.Duration(keepAliveTimeout) * time.Second,
