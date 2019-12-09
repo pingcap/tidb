d439a575661cac92cb1134bb6da719f92b29f700
djshow832 <873581766@qq.com>
2019-12-09 16:11:49 +0800

    infoschema, util: add table `events_statements_summary_by_digest_history` (#13813)

--- config/config_test.go
+++ config/config_test.go
@@ -190,8 +190,11 @@ max-batch-size=128
 region-cache-ttl=6000
 store-limit=0
 [stmt-summary]
+enable=true
 max-stmt-count=1000
 max-sql-length=1024
+refresh-interval=100
+history-size=100
 `)
 
 	c.Assert(err, IsNil)
@@ -217,8 +220,11 @@ max-sql-length=1024
 	c.Assert(conf.EnableTableLock, IsTrue)
 	c.Assert(conf.DelayCleanTableLock, Equals, uint64(5))
 	c.Assert(conf.SplitRegionMaxNum, Equals, uint64(10000))
+	c.Assert(conf.StmtSummary.Enable, Equals, true)
 	c.Assert(conf.StmtSummary.MaxStmtCount, Equals, uint(1000))
 	c.Assert(conf.StmtSummary.MaxSQLLength, Equals, uint(1024))
+	c.Assert(conf.StmtSummary.RefreshInterval, Equals, 100)
+	c.Assert(conf.StmtSummary.HistorySize, Equals, 100)
 	c.Assert(conf.EnableBatchDML, Equals, true)
 	c.Assert(conf.RepairMode, Equals, true)
 	c.Assert(f.Close(), IsNil)
