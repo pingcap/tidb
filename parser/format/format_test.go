4d44c189ccce436f1027a56a20cc9be121da6974
Tanner <tangenta@126.com>
2019-12-13 17:28:13 +0800

    [parser] *: remove testleak (#676)

--- parser/format/format_test.go
+++ parser/format/format_test.go
@@ -20,7 +20,6 @@ import (
 	"testing"
 
 	. "github.com/pingcap/check"
-	"github.com/pingcap/tidb/util/testleak"
 )
 
 func TestT(t *testing.T) {
@@ -43,7 +42,6 @@ func checkFormat(c *C, f Formatter, buf *bytes.Buffer, str, expect string) {
 }
 
 func (s *testFormatSuite) TestFormat(c *C) {
-	defer testleak.AfterTest(c)()
 	str := "abc%d%%e%i\nx\ny\n%uz\n"
 	buf := &bytes.Buffer{}
 	f := IndentFormatter(buf, "\t")
