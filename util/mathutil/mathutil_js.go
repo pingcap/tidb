f1b01eaef775e6b9c897257cf347238f42e14aa4
5kbpers <tangminghua@pingcap.com>
2019-12-13 11:21:15 +0800

    add comments
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- util/mathutil/mathutil_js.go
+++ util/mathutil/mathutil_js.go
@@ -1,102 +1,105 @@
 package mathutil
 
-const MaxUint = ^uint(0) 
-const MinUint = 0 
-const MaxInt = int(MaxUint >> 1) 
-const MinInt = -MaxInt - 1
+// The maximum number uint can record
+const (
+	MaxUint = ^uint(0)
+	MinUint = 0
+	MaxInt  = int(MaxUint >> 1)
+	MinInt  = -MaxInt - 1
+)
 
+// MaxUint64 returns the larger of a and b.
 func MaxUint64(x, y uint64) uint64 {
 	if x > y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MinUint64 returns the smaller of a and b.
 func MinUint64(x, y uint64) uint64 {
 	if x < y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MaxUint32 returns the larger of a and b.
 func MaxUint32(x, y uint32) uint32 {
 	if x > y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MinUint32 returns the smaller of a and b.
 func MinUint32(x, y uint32) uint32 {
 	if x < y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MaxInt64 returns the larger of a and b.
 func MaxInt64(x, y int64) int64 {
 	if x > y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MinInt64 returns the smaller of a and b.
 func MinInt64(x, y int64) int64 {
 	if x < y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MaxInt32 returns the larger of a and b.
 func MaxInt32(x, y int32) int32 {
 	if x > y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MinInt32 returns the smaller of a and b.
 func MinInt32(x, y int32) int32 {
 	if x < y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MaxInt8 returns the larger of a and b.
 func MaxInt8(x, y int8) int8 {
 	if x > y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// MinInt8 returns the smaller of a and b.
 func MinInt8(x, y int8) int8 {
 	if x < y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// Max returns the larger of a and b.
 func Max(x, y int) int {
 	if x > y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
 
+// Min returns the larger of a and b.
 func Min(x, y int) int {
 	if x < y {
 		return x
-	} else {
-		return y
 	}
+	return y
 }
---
+++ util/signal/signal_wasm.go
@@ -0,0 +1,5 @@
+package signal
+
+// SetupSignalHandler setup signal handler for TiDB Server
+func SetupSignalHandler(shutdownFunc func(bool)) {
+}
