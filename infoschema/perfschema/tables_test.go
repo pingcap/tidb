7c5b2173783b7272589bbacde390ef9785879ef3
djshow832 <873581766@qq.com>
2019-12-25 20:18:17 +0800

    add test case for plan digest

--- infoschema/perfschema/tables_test.go
+++ infoschema/perfschema/tables_test.go
@@ -101,6 +101,8 @@ func (s *testTableSuite) TestStmtSummaryTable(c *C) {
 	).Check(testkit.Rows("insert test test.t <nil> 4 0 0 0 0 0 2 2 1 1 1 /**/insert into t values(4, 'd') "))
 
 	// Test SELECT.
+	c.Assert(failpoint.Enable("github.com/pingcap/tidb/planner/core/mockPlanRowCount", "return(100)"), IsNil)
+	defer func() { c.Assert(failpoint.Disable("github.com/pingcap/tidb/planner/core/mockPlanRowCount"), IsNil) }()
 	tk.MustQuery("select * from t where a=2")
 	tk.MustQuery(`select stmt_type, schema_name, table_names, index_names, exec_count, cop_task_num, avg_total_keys, 
 		max_total_keys, avg_processed_keys, max_processed_keys, avg_write_keys, max_write_keys, avg_prewrite_regions, 
@@ -118,6 +120,18 @@ func (s *testTableSuite) TestStmtSummaryTable(c *C) {
 		from performance_schema.events_statements_summary_by_digest
 		order by exec_count desc limit 1`,
 	).Check(testkit.Rows("insert test test.t <nil> 4 0 0 0 0 0 2 2 1 1 1 /**/insert into t values(4, 'd') "))
+
+	// Test different plans with same digest.
+	c.Assert(failpoint.Enable("github.com/pingcap/tidb/planner/core/mockPlanRowCount", "return(1000)"), IsNil)
+	tk.MustQuery("select * from t where a=3")
+	tk.MustQuery(`select stmt_type, schema_name, table_names, index_names, exec_count, cop_task_num, avg_total_keys, 
+		max_total_keys, avg_processed_keys, max_processed_keys, avg_write_keys, max_write_keys, avg_prewrite_regions, 
+		max_prewrite_regions, avg_affected_rows, query_sample_text, plan 
+		from performance_schema.events_statements_summary_by_digest
+		where digest_text like 'select * from t%'`,
+	).Check(testkit.Rows("select test test.t t:k 2 2 0 0 0 0 0 0 0 0 0 select * from t where a=3 \tIndexLookUp_10\troot\t10\t\n" +
+		"\t├─IndexScan_8 \tcop \t10\ttable:t, index:a, range:[3,3], keep order:false, stats:pseudo\n" +
+		"\t└─TableScan_9 \tcop \t10\ttable:t, keep order:false, stats:pseudo"))
 
 	// Disable it again.
 	tk.MustExec("set global tidb_enable_stmt_summary = false")
