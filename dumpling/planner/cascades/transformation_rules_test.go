bed8e8b3890a28380f9014287f48883fa8e75d1c
gauss1314 <gauss1314@gmail.com>
2019-12-27 13:14:39 +0800

    [dumpling] planner/cascades: add transformation rule PushTopNDownTiKVSingleGather  (#14242)

--- dumpling/planner/cascades/transformation_rules_test.go
+++ dumpling/planner/cascades/transformation_rules_test.go
@@ -156,6 +156,7 @@ func (s *testTransformationRuleSuite) TestTopNRules(c *C) {
 		memo.OperandTopN: {
 			NewRulePushTopNDownProjection(),
 			NewRulePushTopNDownUnionAll(),
+			NewRulePushTopNDownTiKVSingleGather(),
 		},
 	})
 	var input []string
