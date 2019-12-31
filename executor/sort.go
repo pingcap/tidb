f19fb21dac78c159a19aa5cb990812c19dc07f5f
wshwsh12 <793703860@qq.com>
2019-12-31 11:00:32 +0800

    add comments

--- executor/sort.go
+++ executor/sort.go
@@ -360,6 +360,7 @@ func (e *SortExec) spillToDiskByRowPtr() (disk *chunk.ListInDisk, err error) {
 	return rowChunksInDisk, nil
 }
 
+// ActionSpill returns a memory.ActionOnExceed for spilling over to disk.
 func (e *SortExec) ActionSpill() memory.ActionOnExceed {
 	return &spillSortDiskAction{e: e}
 }
