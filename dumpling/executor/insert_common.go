b7778b5d32b5c09c7977a805430ef4a9d2b986ba
Qiannan <lvqiannan@gmail.com>
2019-12-31 17:19:59 +0800

    [dumpling] executor: resort fields for reducing the memory allocated (#14208)

--- dumpling/executor/insert_common.go
+++ dumpling/executor/insert_common.go
@@ -42,8 +42,6 @@ type InsertValues struct {
 	curBatchCnt    uint64
 	maxRowsInBatch uint64
 	lastInsertID   uint64
-	hasRefCols     bool
-	hasExtraHandle bool
 
 	SelectExec Executor
 
@@ -56,13 +54,16 @@ type InsertValues struct {
 
 	insertColumns []*table.Column
 
-	allAssignmentsAreConstant bool
-
 	// colDefaultVals is used to store casted default value.
 	// Because not every insert statement needs colDefaultVals, so we will init the buffer lazily.
 	colDefaultVals  []defaultVal
 	evalBuffer      chunk.MutRow
 	evalBufferTypes []*types.FieldType
+
+	allAssignmentsAreConstant bool
+
+	hasRefCols     bool
+	hasExtraHandle bool
 
 	// Fill the autoID lazily to datum. This is used for being compatible with JDBC using getGeneratedKeys().
 	// `insert|replace values` can guarantee consecutive autoID in a batch.
