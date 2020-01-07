8089aae372108f0fb511deb5cedf76746842d0c3
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-01-07 21:09:18 +0800

    fixCI

--- store/tikv/store_test.go
+++ store/tikv/store_test.go
@@ -95,6 +95,10 @@ type mockPDClient struct {
 	stop   bool
 }
 
+func (c *mockPDClient) ConfigClient() pd.ConfigClient {
+	return nil
+}
+
 func (c *mockPDClient) enable() {
 	c.Lock()
 	defer c.Unlock()
