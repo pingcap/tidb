3e953cee2a6cc6af289ece6322f335f803ecefda
Tanner <tangenta@126.com>
2020-01-09 00:38:20 +0800

    tests: add naughty strings integration test (#22)
    * tests: add naughty_strings integration test
    
    * .travis.yml, Makefile: enable integration test in CI
    
    * tests: remove 'color' option of 'diff'
    
    * tests/_utils: refine run_sql* scripts
    
    * tests: using environment variables to control DSN

---
+++ tests/naughty_strings/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eu
+
+run_sql "DROP DATABASE IF EXISTS naughty_strings"
+run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings-schema-create.sql"
+export DUMPLING_TEST_DATABASE="naughty_strings"
+run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.t-schema.sql"
+run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.t.sql"
+run_dumpling "naughty_strings"
+diff "$DUMPLING_BASE_NAME/data" "$DUMPLING_OUTPUT_DIR"
