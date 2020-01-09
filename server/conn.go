53a69eb5153895db835c02b5ebfdff2863eef9d4
lysu <sulifx@gmail.com>
2020-01-09 16:41:01 +0800

    server: refine pprof sql in prepare stmt

--- server/conn.go
+++ server/conn.go
@@ -816,10 +816,10 @@ func (cc *clientConn) dispatch(ctx context.Context, data []byte) error {
 	cmd := data[0]
 	data = data[1:]
 	if variable.EnablePProfSQLCPU.Load() {
-		lastSQL := getLastStmtInConn{cc}.PProfLabel()
-		if len(lastSQL) > 0 {
+		label := getLastStmtInConn{cc}.PProfLabel()
+		if len(label) > 0 {
 			defer pprof.SetGoroutineLabels(ctx)
-			ctx = pprof.WithLabels(ctx, pprof.Labels("sql", parser.Normalize(lastSQL)))
+			ctx = pprof.WithLabels(ctx, pprof.Labels("sql", label))
 			pprof.SetGoroutineLabels(ctx)
 		}
 	}
@@ -1564,6 +1564,7 @@ func (cc getLastStmtInConn) String() string {
 	}
 }
 
+// PProfLabel return sql label used to tag pprof.
 func (cc getLastStmtInConn) PProfLabel() string {
 	if len(cc.lastPacket) == 0 {
 		return ""
@@ -1579,7 +1580,7 @@ func (cc getLastStmtInConn) PProfLabel() string {
 	case mysql.ComStmtReset:
 		return "ResetStmt"
 	case mysql.ComQuery, mysql.ComStmtPrepare:
-		return queryStrForLog(string(hack.String(data)))
+		return parser.Normalize(queryStrForLog(string(hack.String(data))))
 	case mysql.ComStmtExecute, mysql.ComStmtFetch:
 		stmtID := binary.LittleEndian.Uint32(data[0:4])
 		return queryStrForLog(cc.preparedStmt2StringNoArgs(stmtID))
