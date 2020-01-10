3626264dd1b48d67e1b44acaff08b4af62cbdc4f
Haibin Xie <lambdax.tyler@gmail.com>
2020-01-10 16:46:20 +0800

    executor: add privilege check for show bindings

--- bindinfo/bind_test.go
+++ bindinfo/bind_test.go
@@ -22,6 +22,7 @@ import (
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/auth"
 	"github.com/pingcap/tidb/bindinfo"
 	"github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/kv"
@@ -681,4 +682,20 @@ func (s *testSuite) TestOutdatedInfoSchema(c *C) {
 	c.Assert(s.domain.BindHandle().Update(false), IsNil)
 	tk.MustExec("truncate table mysql.bind_info")
 	tk.MustExec("create global binding for select * from t using select * from t use index(idx)")
+}
+
+func (s *testSuite) TestPrivileges(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	s.cleanBindingEnv(tk)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a int, b int, index idx(a))")
+	tk.MustExec("create global binding for select * from t using select * from t use index(idx)")
+	c.Assert(tk.Se.Auth(&auth.UserIdentity{Username: "root", Hostname: "%"}, nil, nil), IsTrue)
+	rows := tk.MustQuery("show global bindings").Rows()
+	c.Assert(len(rows), Equals, 1)
+	tk.MustExec("create user test@'%'")
+	c.Assert(tk.Se.Auth(&auth.UserIdentity{Username: "test", Hostname: "%"}, nil, nil), IsTrue)
+	rows = tk.MustQuery("show global bindings").Rows()
+	c.Assert(len(rows), Equals, 0)
 }
