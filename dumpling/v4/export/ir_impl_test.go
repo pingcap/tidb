a882f1e04f01ece3809734426f3df63df91951ab
Tanner <tangenta@126.com>
2020-01-11 17:12:26 +0800

    [dumpling] *: add integration test for dumping in given file size (#26)
    * *: add integration test for dumping in given file size
    
    * log: use zap.Error instead of zap.String('err', xx)
    
    * *: fix style problems

--- dumpling/v4/export/ir_impl_test.go
+++ dumpling/v4/export/ir_impl_test.go
@@ -1,6 +1,8 @@
 package export
 
 import (
+	"strings"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	. "github.com/pingcap/check"
 )
@@ -10,15 +12,25 @@ var _ = Suite(&testIRImplSuite{})
 type testIRImplSuite struct{}
 
 type simpleRowReceiver struct {
-	data string
+	data []string
 }
 
-func (s *simpleRowReceiver) BindAddress(arg []interface{}) {
-	arg[0] = &s.data
+func newSimpleRowReceiver(length int) *simpleRowReceiver {
+	return &simpleRowReceiver{data: make([]string, length)}
+}
+
+func (s *simpleRowReceiver) BindAddress(args []interface{}) {
+	for i := range args {
+		args[i] = &s.data[i]
+	}
 }
 
 func (s *simpleRowReceiver) ReportSize() uint64 {
-	panic("not implement")
+	var sum uint64
+	for _, datum := range s.data {
+		sum += uint64(len(datum))
+	}
+	return sum
 }
 
 func (s *testIRImplSuite) TestRowIter(c *C) {
@@ -35,18 +47,49 @@ func (s *testIRImplSuite) TestRowIter(c *C) {
 	c.Assert(err, IsNil)
 
 	iter := newRowIter(rows, 1)
-	for i := 0; i < 100; i += 1 {
+	for i := 0; i < 100; i++ {
 		c.Assert(iter.HasNext(), IsTrue)
 	}
-	res := &simpleRowReceiver{}
+	res := newSimpleRowReceiver(1)
 	c.Assert(iter.Next(res), IsNil)
-	c.Assert(res.data, Equals, "1")
+	c.Assert(res.data, DeepEquals, []string{"1"})
 	c.Assert(iter.HasNext(), IsTrue)
 	c.Assert(iter.HasNext(), IsTrue)
 	c.Assert(iter.Next(res), IsNil)
-	c.Assert(res.data, Equals, "2")
+	c.Assert(res.data, DeepEquals, []string{"2"})
 	c.Assert(iter.HasNext(), IsTrue)
 	c.Assert(iter.Next(res), IsNil)
-	c.Assert(res.data, Equals, "3")
+	c.Assert(res.data, DeepEquals, []string{"3"})
 	c.Assert(iter.HasNext(), IsFalse)
+}
+
+func (s *testIRImplSuite) TestSizedRowIter(c *C) {
+	db, mock, err := sqlmock.New()
+	c.Assert(err, IsNil)
+	defer db.Close()
+
+	twentyBytes := strings.Repeat("x", 20)
+	thirtyBytes := strings.Repeat("x", 30)
+	expectedRows := mock.NewRows([]string{"a", "b"})
+	for i := 0; i < 10; i++ {
+		expectedRows.AddRow(twentyBytes, thirtyBytes)
+	}
+	mock.ExpectQuery("SELECT a, b FROM t").WillReturnRows(expectedRows)
+	rows, err := db.Query("SELECT a, b FROM t")
+	c.Assert(err, IsNil)
+
+	sizedRowIter := sizedRowIter{
+		rowIter:   newRowIter(rows, 2),
+		sizeLimit: 200,
+	}
+	res := newSimpleRowReceiver(2)
+	for i := 0; i < 200/50; i++ {
+		c.Assert(sizedRowIter.HasNext(), IsTrue)
+		err := sizedRowIter.Next(res)
+		c.Assert(err, IsNil)
+	}
+	c.Assert(sizedRowIter.HasNext(), IsFalse)
+	c.Assert(sizedRowIter.HasNext(), IsFalse)
+	rows.Close()
+	c.Assert(sizedRowIter.Next(res), NotNil)
 }
