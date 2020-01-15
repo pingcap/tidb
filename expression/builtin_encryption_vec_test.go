51b0ff58260c64aebfcad8429e7c6a45cce4747f
glorv <glorvs@163.com>
2020-01-15 22:54:52 +0800

    move encryption to suite1

--- expression/builtin_encryption_vec_test.go
+++ expression/builtin_encryption_vec_test.go
@@ -74,7 +74,7 @@ var vecBuiltinEncryptionCases = map[string][]vecExprBenchCase{
 	},
 }
 
-func (s *testEvaluatorSuite) TestVectorizedBuiltinEncryptionFunc(c *C) {
+func (s *testEvaluatorSuite1) TestVectorizedBuiltinEncryptionFunc(c *C) {
 	testVectorizedBuiltinFunc(c, vecBuiltinEncryptionCases)
 }
 
