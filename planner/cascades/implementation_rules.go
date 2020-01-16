ca88a26ce8a5d14a65b6404546a4f65c66619c0e
Reminiscent <y870414423@gmail.com>
2020-01-16 11:49:02 +0800

    address comments

--- planner/cascades/implementation_rules.go
+++ planner/cascades/implementation_rules.go
@@ -475,6 +475,7 @@ func (r *ImplMergeJoin) Match(expr *memo.GroupExpr, prop *property.PhysicalPrope
 // OnImplement implements ImplementationRule OnImplement interface.
 func (r *ImplMergeJoin) OnImplement(expr *memo.GroupExpr, reqProp *property.PhysicalProperty) ([]memo.Implementation, error) {
 	join := expr.ExprNode.(*plannercore.LogicalJoin)
+	join.SetSchema(expr.Schema())
 	physicalMergeJoins := join.GetMergeJoin(reqProp)
 	mergeJoinImpls := make([]memo.Implementation, 0, len(physicalMergeJoins))
 	for _, physicalPlan := range physicalMergeJoins {
