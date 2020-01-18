5527c582e0ae92470bca88d4ccfeff9e14871da5
Chengpeng Yan <41809508+Reminiscent@users.noreply.github.com>
2020-01-18 22:16:50 +0800

    planner/cascades: add ImplementationRule for MergeJoin (#14450)

--- planner/implementation/join.go
+++ planner/implementation/join.go
@@ -45,3 +45,31 @@ func (impl *HashJoinImpl) AttachChildren(children ...memo.Implementation) memo.I
 func NewHashJoinImpl(hashJoin *plannercore.PhysicalHashJoin) *HashJoinImpl {
 	return &HashJoinImpl{baseImpl{plan: hashJoin}}
 }
+
+// MergeJoinImpl is the implementation for PhysicalMergeJoin.
+type MergeJoinImpl struct {
+	baseImpl
+}
+
+// CalcCost implements Implementation CalcCost interface.
+func (impl *MergeJoinImpl) CalcCost(outCount float64, children ...memo.Implementation) float64 {
+	mergeJoin := impl.plan.(*plannercore.PhysicalMergeJoin)
+	// The children here are only used to calculate the cost.
+	mergeJoin.SetChildren(children[0].GetPlan(), children[1].GetPlan())
+	selfCost := mergeJoin.GetCost(children[0].GetPlan().StatsCount(), children[1].GetPlan().StatsCount())
+	impl.cost = selfCost + children[0].GetCost() + children[1].GetCost()
+	return impl.cost
+}
+
+// AttachChildren implements Implementation AttachChildren interface.
+func (impl *MergeJoinImpl) AttachChildren(children ...memo.Implementation) memo.Implementation {
+	mergeJoin := impl.plan.(*plannercore.PhysicalMergeJoin)
+	mergeJoin.SetChildren(children[0].GetPlan(), children[1].GetPlan())
+	mergeJoin.SetSchema(plannercore.BuildPhysicalJoinSchema(mergeJoin.JoinType, mergeJoin))
+	return impl
+}
+
+// NewMergeJoinImpl creates a new MergeJoinImpl.
+func NewMergeJoinImpl(mergeJoin *plannercore.PhysicalMergeJoin) *MergeJoinImpl {
+	return &MergeJoinImpl{baseImpl{plan: mergeJoin}}
+}
