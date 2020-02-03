865161f294849a72f92c7d62a5d0efbf241293b8
mmyj <mayj.hello@gmail.com>
2020-02-03 22:50:36 +0800

    executor: Improve the performance of `WindowExec` by using sliding window #14294

--- executor/window.go
+++ executor/window.go
@@ -293,7 +293,7 @@ func (p *rowFrameWindowProcessor) appendResult2Chunk(ctx sessionctx.Context, row
 	numRows := uint64(len(rows))
 	var (
 		err                      error
-		initializedSlidingWindow bool
+		initializedSlidingWindow = false
 		lastStart                uint64
 		lastEnd                  uint64
 		shiftStart               uint64
