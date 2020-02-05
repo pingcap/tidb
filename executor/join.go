b46554309425dd4edbf8a72cf88036f1613918a0
Feng Liyuan <darktemplar.f@gmail.com>
2020-02-05 13:37:23 +0800

    executor: Count the number of hash table collisions and build elapse during Hash Join. (#14423)

--- executor/join.go
+++ executor/join.go
@@ -136,6 +136,7 @@ func (e *HashJoinExec) Close() error {
 	if e.runtimeStats != nil {
 		concurrency := cap(e.joiners)
 		e.runtimeStats.SetConcurrencyInfo("Concurrency", concurrency)
+		e.runtimeStats.SetAdditionalInfo(e.rowContainer.stat.String())
 	}
 	err := e.baseExecutor.Close()
 	return err
