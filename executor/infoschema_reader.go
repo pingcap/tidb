dd3ed4fa3b54e966e7ee969fd09583e072aeaea9
guoni <guoni96@icloud.com>
2020-02-21 10:48:05 +0800

    fmt import

--- executor/infoschema_reader.go
+++ executor/infoschema_reader.go
@@ -15,6 +15,9 @@ package executor
 
 import (
 	"context"
+	"sort"
+	"strings"
+
 	"github.com/cznic/mathutil"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/parser/mysql"
@@ -23,8 +26,6 @@ import (
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/chunk"
-	"sort"
-	"strings"
 )
 
 type memtableRetriever struct {
