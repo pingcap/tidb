dcdfd60c519d7303dcfffe0a90f82715bde8af1d
Feng Liyuan <darktemplar.f@gmail.com>
2020-02-24 16:22:36 +0800

    planner: move flagPrunColumnsAgain to the last (#14791)

--- planner/core/planbuilder.go
+++ planner/core/planbuilder.go
@@ -389,7 +389,8 @@ func NewPlanBuilder(sctx sessionctx.Context, is infoschema.InfoSchema, processor
 func (b *PlanBuilder) Build(ctx context.Context, node ast.Node) (Plan, error) {
 	b.optFlag = flagPrunColumns
 	defer func() {
-		if b.optFlag&flagPredicatePushDown > 0 {
+		// if there is something after flagPrunColumns, do flagPrunColumnsAgain
+		if b.optFlag&flagPrunColumns > 0 && b.optFlag-flagPrunColumns > flagPrunColumns {
 			b.optFlag |= flagPrunColumnsAgain
 		}
 	}()
