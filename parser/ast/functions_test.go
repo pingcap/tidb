46600d4798e2184ffcd666bd33963538c595f85b
bb7133 <bb7133@gmail.com>
2020-02-24 15:39:21 +0800

    [parser] parser, ast: add `SetCollatorExpr` (#749)
    This ExprNode is equivalent to the `Item_func_set_collation` in MySQL,
    it is used to specify the collation for the result of another
    expression.
    
    Co-authored-by: tangenta <tangenta@126.com>

--- parser/ast/functions_test.go
+++ parser/ast/functions_test.go
@@ -87,8 +87,9 @@ func (ts *testFunctionsSuite) TestFuncCallExprRestore(c *C) {
 		{"weight_string(a)", "WEIGHT_STRING(`a`)"},
 		{"Weight_stRing(test.a)", "WEIGHT_STRING(`test`.`a`)"},
 		{"weight_string('a')", "WEIGHT_STRING('a')"},
-		// TODO(bb7133): collate for literal values cannot be restored.
-		//{"weight_string(_utf8 'a' collate utf8_general_ci)", "WEIGHT_STRING(_UTF8'a' COLLATE utf8_general_ci)"},
+		// Expressions with collations of different charsets will lead to an error in MySQL, but the error check should be done in TiDB, so it's valid here.
+		{"weight_string('a' collate utf8_general_ci collate utf8mb4_general_ci)", "WEIGHT_STRING('a' COLLATE utf8_general_ci COLLATE utf8mb4_general_ci)"},
+		{"weight_string(_utf8 'a' collate utf8_general_ci)", "WEIGHT_STRING(_UTF8'a' COLLATE utf8_general_ci)"},
 		{"weight_string(_utf8 'a')", "WEIGHT_STRING(_UTF8'a')"},
 		{"weight_string(a as char(5))", "WEIGHT_STRING(`a` AS CHAR(5))"},
 		{"weight_string(a as character(5))", "WEIGHT_STRING(`a` AS CHAR(5))"},
