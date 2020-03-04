ef1faab2ce57487b9e1afcc664832cacda693309
Shuaipeng Yu <jackysp@gmail.com>
2020-03-04 13:00:01 +0800

    *: fix read committed hanged
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- session/session.go
+++ session/session.go
@@ -2025,14 +2025,15 @@ func (s *session) PrepareTxnCtx(ctx context.Context) {
 	}
 }
 
-// PrepareTSFuture uses to try to get txn future.
+// PrepareTSFuture uses to try to get ts future.
 func (s *session) PrepareTSFuture(ctx context.Context) {
+	// Prepare the transaction future if the transaction is invalid (at the beginning of the transaction).
 	if !s.txn.validOrPending() {
 		txnFuture := s.getTxnFuture(ctx)
 		s.txn.changeInvalidToPending(txnFuture)
-		s.GetSessionVars().TxnCtx.SetStmtFuture(txnFuture.future)
 		return
 	}
+	// Prepare the statement future if the transaction is valid in RC transactions.
 	if s.txn.Valid() && s.GetSessionVars().IsPessimisticReadConsistency() {
 		s.GetSessionVars().TxnCtx.SetStmtFuture(s.getTxnFuture(ctx).future)
 	}
