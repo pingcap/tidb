ca344a25e22d4102c87a0c5d213e344117371829
Feng Liyuan <darktemplar.f@gmail.com>
2020-03-05 18:59:13 +0800

    update comment

--- util/chunk/codec_test.go
+++ util/chunk/codec_test.go
@@ -30,8 +30,8 @@ type testCodecSuite struct{}
 
 func (s *testCodecSuite) TestCodec(c *check.C) {
 	if runtime.Version() >= "go1.14" {
-		// TODO: fix it
-		c.Skip("cannot pass checkptr, TODO to fix it")
+		// TODO: fix https://github.com/pingcap/tidb/issues/15154
+		c.Skip("cannot pass checkptr, TODO to fix https://github.com/pingcap/tidb/issues/15154")
 	}
 	numCols := 6
 	numRows := 10
