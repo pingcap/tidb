352faaa6f33011d73432deb812c9ba5d205b9143
kennytm <kennytm@gmail.com>
2020-03-06 12:45:52 +0800

    tests: some improvements to the integration tests (#37)
    * go.sum: go insists on adding another entry :\
    
    * tests: run multi-value insert using a single statement
    
    * tests: place the MySQL password into config file to suppress warning
    
    See: https://stackoverflow.com/q/20751352
    
    * .travis.yml: disable testing outside of master and release-* branches
    
    * tests: fix test cases on MySQL 5.7 & 8
    
    Co-authored-by: Ian <ArGregoryIan@gmail.com>

--- tests/naughty_strings/run.sh
+++ tests/naughty_strings/run.sh
@@ -8,4 +8,5 @@ export DUMPLING_TEST_DATABASE="naughty_strings"
 run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.t-schema.sql"
 run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.t.sql"
 run_dumpling
-diff "$DUMPLING_BASE_NAME/data" "$DUMPLING_OUTPUT_DIR"
+# FIXME should compare the schemas too, but they differ too much among MySQL versions.
+diff "$DUMPLING_BASE_NAME/data/naughty_strings.t.sql" "$DUMPLING_OUTPUT_DIR/naughty_strings.t.sql"
