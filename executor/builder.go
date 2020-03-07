3cc17ecf8d8ed9373db665921b18fb7db1386867
zhaox1n <zhao.x1n@foxmail.com>
2020-03-07 22:01:25 +0800

    Migrate DataForTable and DateForPartition

--- executor/builder.go
+++ executor/builder.go
@@ -1410,6 +1410,7 @@ func (b *executorBuilder) buildMemTable(v *plannercore.PhysicalMemTable) Executo
 		case strings.ToLower(infoschema.TableSchemata),
 			strings.ToLower(infoschema.TableTiDBIndexes),
 			strings.ToLower(infoschema.TableViews),
+			strings.ToLower(infoschema.TableTables),
 			strings.ToLower(infoschema.TableEngines),
 			strings.ToLower(infoschema.TableCollations),
 			strings.ToLower(infoschema.TableCharacterSets),
