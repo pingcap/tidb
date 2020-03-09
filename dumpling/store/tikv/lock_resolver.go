ee788a4f1fd96caece23db1399778a01dd83dc68
lysu <sulifx@gmail.com>
2020-03-09 21:22:16 +0800

    [dumpling] upgrade pd-client to support pd client tls reload (#15071)

--- dumpling/store/tikv/lock_resolver.go
+++ dumpling/store/tikv/lock_resolver.go
@@ -25,7 +25,7 @@ import (
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/kvproto/pkg/kvrpcpb"
-	pd "github.com/pingcap/pd/client"
+	pd "github.com/pingcap/pd/v4/client"
 	"github.com/pingcap/tidb/config"
 	"github.com/pingcap/tidb/metrics"
 	"github.com/pingcap/tidb/store/tikv/tikvrpc"
