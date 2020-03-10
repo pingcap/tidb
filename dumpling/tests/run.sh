08bb8e16d3579a818351556ab99922d54ea18d69
3pointer <luancheng@pingcap.com>
2020-03-10 15:40:23 +0800

    [dumpling] tests: add e2e test (#34)
    * tests: add e2e test
    
    * fix ci
    
    * Update tests/_utils/check_sync_diff
    
    Co-Authored-By: kennytm <kennytm@gmail.com>
    
    * fix ci
    
    * fix ci
    
    * remove pd-server
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * debug ci
    
    * debug ci
    
    * fix ci
    
    * leave TODO
    
    * Update tests/_utils/check_sync_diff
    
    Co-Authored-By: Ian <ArGregoryIan@gmail.com>
    
    Co-authored-by: kennytm <kennytm@gmail.com>
    Co-authored-by: Ian <ArGregoryIan@gmail.com>

--- dumpling/tests/run.sh
+++ dumpling/tests/run.sh
@@ -20,29 +20,16 @@ set -eu
 
 mkdir -p "$DUMPLING_TEST_DIR"
 PATH="tests/_utils:$PATH"
+. "tests/_utils/run_services"
 
-cat > "$DUMPLING_TEST_DIR/mysql.cnf" <<EOF
-[client]
-user = ${DUMPLING_TEST_USER}
-host = ${DUMPLING_TEST_HOST}
-port = ${DUMPLING_TEST_PORT}
-password = ${DUMPLING_TEST_PASSWORD}
-default-character-set = utf8mb4
-EOF
 
-test_connection() {
-    i=0
-    while ! run_sql 'select 0 limit 0' > /dev/null; do
-        i=$((i+1))
-        if [ "$i" -gt 5 ]; then
-            echo 'Failed to ping MySQL Server'
-            exit 1
-        fi
-        sleep 3
-    done
-}
+file_should_exist bin/tidb-server
+file_should_exist bin/tidb-lightning
+file_should_exist bin/dumpling
+file_should_exist bin/sync_diff_inspector
 
-test_connection
+trap stop_services EXIT
+start_services
 
 for script in tests/*/run.sh; do
     echo "****************** Running test $script..."
@@ -52,11 +39,12 @@ for script in tests/*/run.sh; do
     DUMPLING_OUTPUT_DIR="$DUMPLING_TEST_DIR"/sql_res."$TEST_NAME"
     export DUMPLING_OUTPUT_DIR
 
-    echo "Cleaning up test output dir: $DUMPLING_OUTPUT_DIR"
-    rm "$DUMPLING_OUTPUT_DIR" -rf
-
     PATH="tests/_utils:$PATH" \
     sh "$script"
+
+    echo "Cleaning up test output dir: $DUMPLING_OUTPUT_DIR"
+    rm -rf "$DUMPLING_OUTPUT_DIR"
+
 done
 
 echo "Passed integration tests."
