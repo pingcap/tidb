583194822152713be4b6ec5ca9619f864fed5d88
Evan Zhou <coocood@gmail.com>
2020-03-13 13:49:41 +0800

    add more test

--- planner/core/point_get_plan_test.go
+++ planner/core/point_get_plan_test.go
@@ -393,13 +393,19 @@ func (s *testPointGetSuite) TestBatchPointGetPartition(c *C) {
 	tk.MustQuery("explain select * from t where a in (1, 2, 3, 4)").Check(testkit.Rows(
 		"Batch_Point_Get_1 4.00 root table:t, handle:[1 2 3 4], keep order:false, desc:false",
 	))
+	tk.MustQuery("select * from t where a in (1, 2, 3, 4)").Check(testkit.Rows("1 1", "2 2", "3 3", "4 4"))
+
 	tk.MustQuery("explain update t set b = b + 1 where a in (1, 2, 3, 4)").Check(testkit.Rows(
 		"Update_2 N/A root N/A]\n[└─Batch_Point_Get_1 4.00 root table:t, handle:[1 2 3 4], keep order:false, desc:false",
 	))
+	tk.MustExec("update t set b = b + 1 where a in (1, 2, 3, 4)")
+	tk.MustQuery("select * from t where a in (1, 2, 3, 4)").Check(testkit.Rows("1 2", "2 3", "3 4", "4 5"))
+
 	tk.MustQuery("explain delete from t where a in (1, 2, 3, 4)").Check(testkit.Rows(
 		"Delete_2 N/A root N/A]\n[└─Batch_Point_Get_1 4.00 root table:t, handle:[1 2 3 4], keep order:false, desc:false",
 	))
-	tk.MustQuery("select * from t where a in (1, 2, 3, 4)").Check(testkit.Rows("1 1", "2 2", "3 3", "4 4"))
+	tk.MustExec("delete from t where a in (1, 2, 3, 4)")
+	tk.MustQuery("select * from t where a in (1, 2, 3, 4)").Check(testkit.Rows())
 
 	tk.MustExec("drop table t")
 	tk.MustExec("create table t(a int, b int, c int, primary key (a, b)) PARTITION BY HASH(a) PARTITIONS 4")
@@ -407,12 +413,19 @@ func (s *testPointGetSuite) TestBatchPointGetPartition(c *C) {
 	tk.MustQuery("explain select * from t where (a, b) in ((1, 1), (2, 2), (3, 3), (4, 4))").Check(testkit.Rows(
 		"Batch_Point_Get_1 4.00 root table:t, index:a b, keep order:false, desc:false",
 	))
+	tk.MustQuery("select * from t where (a, b) in ((1, 1), (2, 2), (3, 3), (4, 4))").
+		Check(testkit.Rows("1 1 1", "2 2 2", "3 3 3", "4 4 4"))
+
 	tk.MustQuery("explain update t set c = c + 1 where (a,b) in ((1,1),(2,2),(3,3),(4,4))").Check(testkit.Rows(
 		"Update_2 N/A root N/A]\n[└─Batch_Point_Get_1 4.00 root table:t, index:a b, keep order:false, desc:false",
 	))
+	tk.MustExec("update t set c = c + 1 where (a,b) in ((1,1),(2,2),(3,3),(4,4))")
+	tk.MustQuery("select * from t where (a, b) in ((1, 1), (2, 2), (3, 3), (4, 4))").Sort().
+		Check(testkit.Rows("1 1 2", "2 2 3", "3 3 4", "4 4 5"))
+
 	tk.MustQuery("explain delete from t where (a,b) in ((1,1),(2,2),(3,3),(4,4))").Check(testkit.Rows(
 		"Delete_2 N/A root N/A]\n[└─Batch_Point_Get_1 4.00 root table:t, index:a b, keep order:false, desc:false",
 	))
-	tk.MustQuery("select * from t where (a, b) in ((1, 1), (2, 2), (3, 3), (4, 4))").
-		Check(testkit.Rows("1 1 1", "2 2 2", "3 3 3", "4 4 4"))
+	tk.MustExec("delete from t where (a,b) in ((1,1),(2,2),(3,3),(4,4))")
+	tk.MustQuery("select * from t where (a, b) in ((1, 1), (2, 2), (3, 3), (4, 4))").Check(testkit.Rows())
 }
