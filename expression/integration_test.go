fa141724cc6771972d695d7ca745788866265cee
wshwsh12 <793703860@qq.com>
2020-03-13 11:23:26 +0800

    fix

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -5345,15 +5345,15 @@ func (s *testIntegrationSuite) TestValuesForBinaryLiteral(c *C) {
 	// See issue #15310
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test;")
-	tk.MustExec("create table t(id int primary key auto_increment, a bit(1));")
-	tk.MustExec("insert into t values(1,1);")
-	err := tk.ExecToErr("insert into t values(1,1) on duplicate key update id = values(id),a = values(a);")
+	tk.MustExec("create table testValuesBinary(id int primary key auto_increment, a bit(1));")
+	tk.MustExec("insert into testValuesBinary values(1,1);")
+	err := tk.ExecToErr("insert into testValuesBinary values(1,1) on duplicate key update id = values(id),a = values(a);")
 	c.Assert(err, IsNil)
-	tk.MustQuery("select a=0 from t;").Check(testkit.Rows("0"))
-	err = tk.ExecToErr("insert into t values(1,0) on duplicate key update id = values(id),a = values(a);")
+	tk.MustQuery("select a=0 from testValuesBinary;").Check(testkit.Rows("0"))
+	err = tk.ExecToErr("insert into testValuesBinary values(1,0) on duplicate key update id = values(id),a = values(a);")
 	c.Assert(err, IsNil)
-	tk.MustQuery("select a=0 from t;").Check(testkit.Rows("1"))
-	tk.MustExec("drop table t;")
+	tk.MustQuery("select a=0 from testValuesBinary;").Check(testkit.Rows("1"))
+	tk.MustExec("drop table testValuesBinary;")
 }
 
 func (s *testIntegrationSuite) TestIssue14159(c *C) {
