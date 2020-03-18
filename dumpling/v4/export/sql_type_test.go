a069b0912b414d40de3fa9f1b71275922bc8b62d
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-03-18 19:01:10 +0800

    [dumpling] optimize dumpling's effiency (#50)
    * initial commit
    
    * fix
    
    * fix2
    
    * fix effiency bug
    
    * make greater chan
    
    * avoid malloc
    
    * remove batch
    
    * use new method
    
    * try sync.Pool
    
    * fix bug
    
    * refine escape
    
    * fix
    
    * switch to bytes.buffer
    
    * add pipe
    
    * fix
    
    * change usage
    
    * test
    
    * add variable to struct
    
    * use global variable
    
    * add UT
    
    * update go.mod go.sum
    
    * refine code
    
    * refine code again
    
    * add escape integration tests
    
    * fix bug
    
    * make pipe bigger
    
    * fix
    
    * fix ut
    
    * fix primary
    
    * fix bug
    
    * fix
    
    * fix
    
    * use byte stream
    
    * tmp
    
    * async scan and next
    
    * remove async scan
    
    * remove bytes stream
    
    * refine code
    
    * refine log
    
    * remove debug log
    
    * remove DAO
    
    * add wait group
    
    * use sql.RawBytes
    
    * use bytes
    
    * Revert "use bytes"
    
    This reverts commit fb79bf5bb000989e7347bba024fce7f2346234be.
    
    * test
    
    * refine code
    
    * use rawbytes for SQLTypeBytes
    
    * use byte stream
    
    * refine code
    
    * use bytes.buffer as buffer and write to os.file direcly

--- dumpling/v4/export/sql_type_test.go
+++ dumpling/v4/export/sql_type_test.go
@@ -1,15 +1,23 @@
 package export
 
-import . "github.com/pingcap/check"
+import (
+	"bytes"
+
+	. "github.com/pingcap/check"
+)
 
 var _ = Suite(&testSqlByteSuite{})
 
 type testSqlByteSuite struct{}
 
 func (s *testSqlByteSuite) TestEscape(c *C) {
-	str := `MWQeWw""'\rNmtGxzGp`
+	var bf bytes.Buffer
+	str := []byte(`MWQeWw""'\rNmtGxzGp`)
 	expectStrBackslash := `MWQeWw\"\"\'\\rNmtGxzGp`
 	expectStrWithoutBackslash := `MWQeWw""''\rNmtGxzGp`
-	c.Assert(escape(str, true), Equals, expectStrBackslash)
-	c.Assert(escape(str, false), Equals, expectStrWithoutBackslash)
+	escape(str, &bf, true)
+	c.Assert(bf.String(), Equals, expectStrBackslash)
+	bf.Reset()
+	escape(str, &bf, false)
+	c.Assert(bf.String(), Equals, expectStrWithoutBackslash)
 }
