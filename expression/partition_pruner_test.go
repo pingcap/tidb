dc1152c9914dda828c5347e3636545677039a6eb
Lingyu Song <songlingyu@pingcap.com>
2020-03-25 10:38:06 +0800

    plan, expression: add date function support for hash partition (#15068) (#15618)

--- expression/partition_pruner_test.go
+++ expression/partition_pruner_test.go
@@ -66,6 +66,9 @@ func (s *testSuite2) TestHashPartitionPruner(c *C) {
 	tk.MustExec("drop table if exists t1, t2;")
 	tk.MustExec("create table t2(id int, a int, b int, primary key(id, a)) partition by hash(id + a) partitions 10;")
 	tk.MustExec("create table t1(id int primary key, a int, b int) partition by hash(id) partitions 10;")
+	tk.MustExec("create table t3(id int, a int, b int, primary key(id, a)) partition by hash(id) partitions 10;")
+	tk.MustExec("create table t4(d datetime, a int, b int, primary key(d, a)) partition by hash(year(d)) partitions 10;")
+	tk.MustExec("create table t5(d date, a int, b int, primary key(d, a)) partition by hash(month(d)) partitions 10;")
 
 	var input []string
 	var output []struct {
