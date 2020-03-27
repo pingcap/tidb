bf95bf72f05c83c5568cc14262380b33e3d7cb97
Reminiscent <y870414423@gmail.com>
2020-03-27 16:25:48 +0800

    address comment

--- executor/aggregate_test.go
+++ executor/aggregate_test.go
@@ -854,6 +854,8 @@ func (s *testSuiteAgg) TestIssue15690(c *C) {
 	tk.MustExec(`insert into t values(null),(null);`)
 	tk.MustExec(`insert into t values(0),(2),(2),(4),(8);`)
 	tk.MustQuery(`select /*+ stream_agg() */ distinct * from t;`).Check(testkit.Rows("<nil>", "0", "2", "4", "8"))
+	tk.MustQuery("show warnings;")
+	c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(0))
 
 	// check for FLOAT type
 	tk.MustExec(`drop table if exists t;`)
@@ -861,6 +863,8 @@ func (s *testSuiteAgg) TestIssue15690(c *C) {
 	tk.MustExec(`insert into t values(null),(null),(null),(null);`)
 	tk.MustExec(`insert into t values(1.1),(1.1);`)
 	tk.MustQuery(`select /*+ stream_agg() */ distinct * from t;`).Check(testkit.Rows("<nil>", "1.1"))
+	tk.MustQuery("show warnings;")
+	c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(0))
 
 	// check for DECIMAL type
 	tk.MustExec(`drop table if exists t;`)
@@ -868,6 +872,8 @@ func (s *testSuiteAgg) TestIssue15690(c *C) {
 	tk.MustExec(`insert into t values(null),(null),(null);`)
 	tk.MustExec(`insert into t values(1.1),(2.2),(2.2);`)
 	tk.MustQuery(`select /*+ stream_agg() */ distinct * from t;`).Check(testkit.Rows("<nil>", "1.1", "2.2"))
+	tk.MustQuery("show warnings;")
+	c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(0))
 
 	// check for DATETIME type
 	tk.MustExec(`drop table if exists t;`)
@@ -875,12 +881,16 @@ func (s *testSuiteAgg) TestIssue15690(c *C) {
 	tk.MustExec(`insert into t values(null);`)
 	tk.MustExec(`insert into t values("2019-03-20 21:50:00"),("2019-03-20 21:50:01"), ("2019-03-20 21:50:00");`)
 	tk.MustQuery(`select /*+ stream_agg() */ distinct * from t;`).Check(testkit.Rows("<nil>", "2019-03-20 21:50:00", "2019-03-20 21:50:01"))
+	tk.MustQuery("show warnings;")
+	c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(0))
 
 	// check for JSON type
 	tk.MustExec(`drop table if exists t;`)
 	tk.MustExec(`create table t(a json);`)
 	tk.MustExec(`insert into t values(null),(null),(null),(null);`)
 	tk.MustQuery(`select /*+ stream_agg() */ distinct * from t;`).Check(testkit.Rows("<nil>"))
+	tk.MustQuery("show warnings;")
+	c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(0))
 
 	// check for char type
 	tk.MustExec(`drop table if exists t;`)
@@ -888,4 +898,6 @@ func (s *testSuiteAgg) TestIssue15690(c *C) {
 	tk.MustExec(`insert into t values(null),(null),(null),(null);`)
 	tk.MustExec(`insert into t values('a'),('b');`)
 	tk.MustQuery(`select /*+ stream_agg() */ distinct * from t;`).Check(testkit.Rows("<nil>", "a", "b"))
+	tk.MustQuery("show warnings;")
+	c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(0))
 }
