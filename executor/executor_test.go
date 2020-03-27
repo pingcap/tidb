e7180da80b95e63e8c3be818aceca4189c4c0c30
Song Gao <2695690803@qq.com>
2020-03-28 01:54:22 +0800

    fix unit test

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -4503,9 +4503,11 @@ func (s *testSuite) TestOOMPanicAction(c *C) {
 	s.domain.ExpensiveQueryHandle().SetSessionManager(sm)
 	orgAction := config.GetGlobalConfig().OOMAction
 	setOOMAction(config.OOMActionCancel)
+	orgOOMUseTmpStorage := config.GetGlobalConfig().OOMUseTmpStorage
 	setOOMUseTmpStorage(false)
 	defer func() {
 		setOOMAction(orgAction)
+		setOOMUseTmpStorage(orgOOMUseTmpStorage)
 	}()
 	tk.MustExec("set @@tidb_mem_quota_query=1;")
 	err := tk.QueryToErr("select sum(b) from t group by a;")
