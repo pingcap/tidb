35eff1b3e6b811ac4ace5283816e2342eb49b89d
Zhang Jian <zjsariel@gmail.com>
2020-03-28 21:11:28 +0800

    bindinfo, session: mute internal logs during capture bindings

--- session/session.go
+++ session/session.go
@@ -1102,9 +1102,12 @@ func (s *session) execute(ctx context.Context, sql string) (recordSets []sqlexec
 	stmtNodes, warns, err := s.ParseSQL(ctx, sql, charsetInfo, collation)
 	if err != nil {
 		s.rollbackOnError(ctx)
-		logutil.Logger(ctx).Warn("parse SQL failed",
-			zap.Error(err),
-			zap.String("SQL", sql))
+
+		// Only print log message when this SQL is from the user.
+		// Mute the warning for internal SQLs.
+		if !s.sessionVars.InRestrictedSQL {
+			logutil.Logger(ctx).Warn("parse SQL failed", zap.Error(err), zap.String("SQL", sql))
+		}
 		return nil, util.SyntaxError(err)
 	}
 	durParse := time.Since(parseStartTime)
@@ -1130,9 +1133,12 @@ func (s *session) execute(ctx context.Context, sql string) (recordSets []sqlexec
 		stmt, err := compiler.Compile(ctx, stmtNode)
 		if err != nil {
 			s.rollbackOnError(ctx)
-			logutil.Logger(ctx).Warn("compile SQL failed",
-				zap.Error(err),
-				zap.String("SQL", sql))
+
+			// Only print log message when this SQL is from the user.
+			// Mute the warning for internal SQLs.
+			if !s.sessionVars.InRestrictedSQL {
+				logutil.Logger(ctx).Warn("compile SQL failed", zap.Error(err), zap.String("SQL", sql))
+			}
 			return nil, err
 		}
 		durCompile := time.Since(s.sessionVars.StartTime)
