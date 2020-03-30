179b9de851568a47e585b4f3727a0b10a8c6656a
wshwsh12 <793703860@qq.com>
2020-03-30 16:59:43 +0800

    address comments

--- util/codec/codec.go
+++ util/codec/codec.go
@@ -310,7 +310,8 @@ func encodeHashChunkRowIdx(sc *stmtctx.StatementContext, row chunk.Row, tp *type
 	case mysql.TypeFloat:
 		flag = floatFlag
 		f := float64(row.GetFloat32(idx))
-		// For negative zero
+		// For negative zero. In memory, 0 is [0, 0, 0, 0, 0, 0, 0, 0] and -0 is [0, 0, 0, 0, 0, 0, 0, 128].
+		// It makes -0's hash val different from 0's.
 		if f == 0 {
 			f = 0
 		}
@@ -318,7 +319,8 @@ func encodeHashChunkRowIdx(sc *stmtctx.StatementContext, row chunk.Row, tp *type
 	case mysql.TypeDouble:
 		flag = floatFlag
 		f := row.GetFloat64(idx)
-		// For negative zero
+		// For negative zero. In memory, 0 is [0, 0, 0, 0, 0, 0, 0, 0] and -0 is [0, 0, 0, 0, 0, 0, 0, 128].
+		// It makes -0's hash val different from 0's.
 		if f == 0 {
 			f = 0
 		}
@@ -426,7 +428,8 @@ func HashChunkSelected(sc *stmtctx.StatementContext, h []hash.Hash64, chk *chunk
 			} else {
 				buf[0] = floatFlag
 				d := float64(f)
-				// For negative zero
+				// For negative zero. In memory, 0 is [0, 0, 0, 0, 0, 0, 0, 0] and -0 is [0, 0, 0, 0, 0, 0, 0, 128].
+				// It makes -0's hash val different from 0's.
 				if d == 0 {
 					d = 0
 				}
@@ -449,7 +452,8 @@ func HashChunkSelected(sc *stmtctx.StatementContext, h []hash.Hash64, chk *chunk
 				isNull[i] = true
 			} else {
 				buf[0] = floatFlag
-				// For negative zero
+				// For negative zero. In memory, 0 is [0, 0, 0, 0, 0, 0, 0, 0] and -0 is [0, 0, 0, 0, 0, 0, 0, 128].
+				// It makes -0's hash val different from 0's.
 				if f == 0 {
 					f = 0
 				}
