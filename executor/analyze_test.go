3a55cf767b1ffd8df464df6c2f207f3ea9f00575
Zhuomin(Charming) Liu <lzmhhh123@gmail.com>
2020-04-01 16:39:29 +0800

    cherry pick #15845 to release-3.0
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- executor/analyze_test.go
+++ executor/analyze_test.go
@@ -308,6 +308,7 @@ func (s *testSuite1) TestIssue15751(c *C) {
 	tk.MustExec("ANALYZE TABLE t0")
 }
 
+<<<<<<< HEAD
 func (s *testSuite1) TestIssue15752(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
@@ -319,6 +320,8 @@ func (s *testSuite1) TestIssue15752(c *C) {
 	tk.MustExec("ANALYZE TABLE t0 INDEX i0")
 }
 
+=======
+>>>>>>> 312891b... statistics: fix panic when merge topn (#15845)
 func (s *testSuite1) TestAnalyzeIncremental(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
