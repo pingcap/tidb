1a5ba3939c9863ee4f21cf8a28da7893fa3466d1
Song Gao <2695690803@qq.com>
2020-04-02 22:49:05 +0800

    remove useless code

--- util/sys/storage/sys_posix.go
+++ util/sys/storage/sys_posix.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build darwin
+// +build linux darwin
 
 package storage
 
