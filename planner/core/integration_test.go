dcda1e7cba4b1f2f20cd45904138e945ebd87f04
wshwsh12 <793703860@qq.com>
2020-04-02 15:13:19 +0800

    fix cherry-pick

--- planner/core/integration_test.go
+++ planner/core/integration_test.go
@@ -788,16 +788,6 @@ func (s *testIntegrationSuite) TestHintWithoutTableWarning(c *C) {
 		}
 	}
 }
-<<<<<<< HEAD
-=======
-
-func (s *testIntegrationSuite) TestIssue15858(c *C) {
-	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t")
-	tk.MustExec("create table t(a int primary key)")
-	tk.MustExec("select * from t t1, (select a from t order by a+1) t2 where t1.a = t2.a")
-}
 
 func (s *testIntegrationSuite) TestIssue15846(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
@@ -824,4 +814,3 @@ func (s *testIntegrationSuite) TestIssue15846(c *C) {
 	tk.MustExec("INSERT INTO t0(t0) VALUES (NULL), (NULL);")
 	tk.MustQuery("SELECT t1.c0 FROM t1 LEFT JOIN t0 ON 1;").Check(testkit.Rows("0", "0"))
 }
->>>>>>> aa7ef9c... planner: fix wrong outer join elimination when join key is empty (#15894)
