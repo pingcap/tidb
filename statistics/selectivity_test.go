1ceb0c6c53c53c7a2f07b7d0d5cbf1d3e7a72423
Kenan Yao <cauchy1992@gmail.com>
2020-04-03 15:50:09 +0800

    statistics: fix repetitive selectivity accounting and stabilify the result (#15536)

--- statistics/selectivity_test.go
+++ statistics/selectivity_test.go
@@ -560,3 +560,21 @@ func (s *testStatsSuite) TestColumnIndexNullEstimation(c *C) {
 		"  └─TableScan_5 5.00 cop[tikv] table:t, range:[-inf,+inf], keep order:false",
 	))
 }
+
+func (s *testStatsSuite) TestSelectivityGreedyAlgo(c *C) {
+	nodes := make([]*statistics.StatsNode, 3)
+	nodes[0] = statistics.MockStatsNode(1, 3, 2)
+	nodes[1] = statistics.MockStatsNode(2, 5, 2)
+	nodes[2] = statistics.MockStatsNode(3, 9, 2)
+
+	// Sets should not overlap on mask, so only nodes[0] is chosen.
+	usedSets := statistics.GetUsableSetsByGreedy(nodes)
+	c.Assert(len(usedSets), Equals, 1)
+	c.Assert(usedSets[0].ID, Equals, int64(1))
+
+	nodes[0], nodes[1] = nodes[1], nodes[0]
+	// Sets chosen should be stable, so the returned node is still the one with ID 1.
+	usedSets = statistics.GetUsableSetsByGreedy(nodes)
+	c.Assert(len(usedSets), Equals, 1)
+	c.Assert(usedSets[0].ID, Equals, int64(1))
+}
