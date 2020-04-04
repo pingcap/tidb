43c2a357e91a8cace22968440eff42a0ba056830
HuaiyuXu <391585975@qq.com>
2020-04-04 15:22:11 +0800

    planner: fix redundant brackets cause full group by check fail… (#16012)

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -1754,7 +1754,7 @@ func (b *PlanBuilder) checkOnlyFullGroupByWithGroupClause(p LogicalPlan, sel *as
 		if field.Auxiliary {
 			continue
 		}
-		checkExprInGroupBy(p, field.Expr, offset, ErrExprInSelect, gbyCols, gbyExprs, notInGbyCols)
+		checkExprInGroupBy(p, getInnerFromParenthesesAndUnaryPlus(field.Expr), offset, ErrExprInSelect, gbyCols, gbyExprs, notInGbyCols)
 	}
 
 	if sel.OrderBy != nil {
