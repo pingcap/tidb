d3097057cb2bfdc1eebad51fb2034bbf4fb179f3
wshwsh12 <793703860@qq.com>
2020-04-09 11:40:58 +0800

    fix cherry-pick

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -4928,6 +4928,7 @@ func (s *testIntegrationSuite) TestIssue15790(c *C) {
 	tk.MustQuery("SELECT * FROM t0 WHERE -10000000000000000000 | t0.c0 UNION SELECT * FROM t0;").Check(testkit.Rows("0"))
 	tk.MustQuery("SELECT * FROM t0 WHERE -10000000000000000000 | t0.c0 UNION all SELECT * FROM t0;").Check(testkit.Rows("0", "0"))
 	tk.MustExec("drop table t0;")
+}
 
 func (s *testIntegrationSuite) TestCacheRefineArgs(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
