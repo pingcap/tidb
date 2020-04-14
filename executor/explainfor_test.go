7543757ea4073e960130da9e63640f48f38004ee
mizuki <xuxiangad@foxmail.com>
2020-04-14 15:40:43 +0800

    add test case for skip_inspection

--- executor/explainfor_test.go
+++ executor/explainfor_test.go
@@ -125,6 +125,8 @@ func (s *testSuite) TestExplainMetricTable(c *C) {
 
 func (s *testSuite) TestInspectionResultTable(c *C) {
 	tk := testkit.NewTestKitWithInit(c, s.store)
+	tk.MustQuery("desc select * from information_schema.inspection_result where rule = 'ddl' and rule = 'config'").Check(testkit.Rows(
+		`MemTableScan_5 10000.00 root table:INSPECTION_RESULT skip_inspection: true`))
 	tk.MustQuery("desc select * from information_schema.inspection_result where rule in ('ddl', 'config')").Check(testkit.Rows(
 		`MemTableScan_5 10000.00 root table:INSPECTION_RESULT rules:["config","ddl"],items:[]`))
 	tk.MustQuery("desc select * from information_schema.inspection_result where item in ('ddl.lease', 'raftstore.threadpool')").Check(testkit.Rows(
