e48b883fa9d0e1361728d17ded67ea0cff903b5f
lawyerphx <panhexiang@pingcap.com>
2020-04-16 02:38:10 -0700

    update common_plans.go

--- planner/core/common_plans.go
+++ planner/core/common_plans.go
@@ -351,16 +351,10 @@ func (e *Execute) getPhysicalPlan(ctx context.Context, sctx sessionctx.Context, 
 	isRange := e.isRangePartition(p)
 	_, isTableDual := p.(*PhysicalTableDual)
 	if !isTableDual && prepared.UseCache && !isRange {
-		err = e.setFoundInPlanCache(sctx, true)
-		if err != nil {
-			return err
-		}
 		cached := NewPSTMTPlanCacheValue(p, names, stmtCtx.TblInfo2UnionScan)
 		preparedStmt.NormalizedPlan, preparedStmt.PlanDigest = NormalizePlan(p)
 		stmtCtx.SetPlanDigest(preparedStmt.NormalizedPlan, preparedStmt.PlanDigest)
 		sctx.PreparedPlanCache().Put(cacheKey, cached)
-	} else {
-		err = e.setFoundInPlanCache(sctx, false)
 	}
 	return err
 }
