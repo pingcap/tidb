37d83d28cfce5f4382384b10e0d10433deaa2c25
Andrew <AndrewDi@users.noreply.github.com>
2020-04-17 15:52:17 +0800

    consistency: enhance auto resolve consistency (#56)
    * enhance aoto resolve consistency
    
    * fix test
    
    * fix test
    
    * fix tiny error
    
    * Update v4/export/sql.go
    
    Co-Authored-By: kennytm <kennytm@gmail.com>
    
    * Update v4/export/sql.go
    
    Co-Authored-By: kennytm <kennytm@gmail.com>
    
    * fix test
    
    Co-authored-by: kennytm <kennytm@gmail.com>

--- v4/export/sql.go
+++ v4/export/sql.go
@@ -284,6 +284,18 @@ func SetTiDBSnapshot(db *sql.DB, snapshot string) error {
 	return withStack(err)
 }
 
+func CheckTiDBWithTiKV(db *sql.DB) (bool, error) {
+	var count int
+	handleOneRow := func(rows *sql.Rows) error {
+		return rows.Scan(&count)
+	}
+	err := simpleQuery(db, "SELECT COUNT(1) as c FROM MYSQL.TiDB WHERE VARIABLE_NAME='tikv_gc_safe_point'", handleOneRow)
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
+
 func buildSelectField(db *sql.DB, dbName, tableName string) (string, error) {
 	query := `SELECT COLUMN_NAME,EXTRA FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=? AND TABLE_NAME=?;`
 	rows, err := db.Query(query, dbName, tableName)
