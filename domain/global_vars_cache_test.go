7605abd210c7ce7e975fdbecdf5ba7024991b6ae
cfzjywxk <lsswxr@163.com>
2020-04-17 21:38:40 +0800

    server: use single flight to load global variables (#16489)

--- domain/global_vars_cache_test.go
+++ domain/global_vars_cache_test.go
@@ -14,6 +14,8 @@
 package domain
 
 import (
+	"sync"
+	"sync/atomic"
 	"time"
 
 	. "github.com/pingcap/check"
@@ -97,6 +99,90 @@ func getResultField(colName string, id, offset int) *ast.ResultField {
 		TableAsName: model.NewCIStr("tbl"),
 		DBName:      model.NewCIStr("test"),
 	}
+}
+
+func (gvcSuite *testGVCSuite) TestConcurrentOneFlight(c *C) {
+	defer testleak.AfterTest(c)()
+	testleak.BeforeTest()
+
+	store, err := mockstore.NewMockTikvStore()
+	c.Assert(err, IsNil)
+	defer store.Close()
+	ddlLease := 50 * time.Millisecond
+	dom := NewDomain(store, ddlLease, 0, mockFactory)
+	err = dom.Init(ddlLease, sysMockFactory)
+	c.Assert(err, IsNil)
+	defer dom.Close()
+
+	// Get empty global vars cache.
+	gvc := dom.GetGlobalVarsCache()
+	succ, rows, fields := gvc.Get()
+	c.Assert(succ, IsFalse)
+	c.Assert(rows, IsNil)
+	c.Assert(fields, IsNil)
+
+	// Get a variable from global vars cache.
+	rf := getResultField("c", 1, 0)
+	rf1 := getResultField("c1", 2, 1)
+	ft := &types.FieldType{
+		Tp:      mysql.TypeString,
+		Charset: charset.CharsetBin,
+		Collate: charset.CollationBin,
+	}
+	ft1 := &types.FieldType{
+		Tp:      mysql.TypeString,
+		Charset: charset.CharsetBin,
+		Collate: charset.CollationBin,
+	}
+	ck := chunk.NewChunkWithCapacity([]*types.FieldType{ft, ft1}, 1024)
+	ck.AppendString(0, "variable1")
+	ck.AppendString(1, "value1")
+	row := ck.GetRow(0)
+	gvc.Update([]chunk.Row{row}, []*ast.ResultField{rf, rf1})
+
+	ckLow := chunk.NewChunkWithCapacity([]*types.FieldType{ft, ft1}, 1024)
+	val := "fromStorage"
+	val1 := "fromStorage1"
+	ckLow.AppendString(0, val)
+	ckLow.AppendString(1, val1)
+
+	// Let cache become invalid, and try concurrent load
+	counter := int32(0)
+	waitRet := new(sync.WaitGroup)
+	waitRet.Add(1)
+	gvc.lastModify = time.Now().Add(time.Duration(-10) * time.Second)
+	loadFunc := func() ([]chunk.Row, []*ast.ResultField, error) {
+		atomic.AddInt32(&counter, 1)
+		waitRet.Wait()
+		return []chunk.Row{ckLow.GetRow(0)}, []*ast.ResultField{rf, rf1}, nil
+	}
+	wg := new(sync.WaitGroup)
+	worker := 100
+	resArray := make([]loadResult, worker)
+	for i := 0; i < worker; i++ {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			resRow, resField, _ := gvc.LoadGlobalVariables(loadFunc)
+			resArray[idx].rows = resRow
+			resArray[idx].fields = resField
+		}(i)
+	}
+	waitRet.Done()
+	wg.Wait()
+	succ, rows, fields = gvc.Get()
+	c.Assert(counter, Equals, int32(1))
+	c.Assert(resArray[0].rows[0].GetString(0), Equals, val)
+	c.Assert(resArray[0].rows[0].GetString(1), Equals, val1)
+	for i := 0; i < worker; i++ {
+		c.Assert(resArray[0].rows[0], Equals, resArray[i].rows[0])
+		c.Assert(resArray[i].rows[0].GetString(0), Equals, val)
+		c.Assert(resArray[i].rows[0].GetString(1), Equals, val1)
+	}
+	// Validate cache
+	c.Assert(succ, IsTrue)
+	c.Assert(rows[0], Equals, resArray[0].rows[0])
+	c.Assert(fields, DeepEquals, []*ast.ResultField{rf, rf1})
 }
 
 func (gvcSuite *testGVCSuite) TestCheckEnableStmtSummary(c *C) {
