32976b830d26c3861ee745b84098a70a9eaf6771
AilinKid <314806019@qq.com>
2020-04-23 14:27:34 +0800

    shut down auto_rand switch

--- config/config.go
+++ config/config.go
@@ -655,7 +655,7 @@ var defaultConf = Config{
 		Engines: []string{"tikv", "tiflash", "tidb"},
 	},
 	Experimental: Experimental{
-		AllowAutoRandom:       true,
+		AllowAutoRandom:       false,
 		AllowsExpressionIndex: false,
 	},
 	EnableCollectExecutionInfo: false,
