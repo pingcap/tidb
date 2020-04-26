93eb844963b6c9e7f305d8fa0c430737b5673c57
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-04-26 23:17:20 +0800

    [dumpling] executor: do not quote numbers in JSON data when using `set config` #16829 (#16835)

--- dumpling/planner/core/planbuilder.go
+++ dumpling/planner/core/planbuilder.go
@@ -527,7 +527,6 @@ func (b *PlanBuilder) Build(ctx context.Context, node ast.Node) (Plan, error) {
 func (b *PlanBuilder) buildSetConfig(ctx context.Context, v *ast.SetConfigStmt) (Plan, error) {
 	mockTablePlan := LogicalTableDual{}.Init(b.ctx, b.getSelectOffset())
 	expr, _, err := b.rewrite(ctx, v.Value, mockTablePlan, nil, true)
-	expr = expression.WrapWithCastAsString(b.ctx, expr)
 	return &SetConfig{Name: v.Name, Type: v.Type, Instance: v.Instance, Value: expr}, err
 }
 
