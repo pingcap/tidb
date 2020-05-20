4c516a5c06faa5775ca2c3ae224894708d7fc41d
kennytm <kennytm@gmail.com>
2020-05-20 15:41:37 +0800

    Added --version, --logfile and --logfmt flags (pingcap/dumpling#72)
    * cmd: replace cobra by pflag
    
    we don't need subcommand parsing, let's choose a less heavyweight library
    
    * cmd: added the --version/-V flag
    
    * log: support configurating log file and format

--- dumpling/cmd/dumpling/main.go
+++ dumpling/cmd/dumpling/main.go
@@ -14,6 +14,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	_ "net/http/pprof"
 	"os"
@@ -21,7 +22,7 @@ import (
 
 	"github.com/pingcap/dumpling/v4/cli"
 	"github.com/pingcap/dumpling/v4/export"
-	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var (
@@ -35,6 +36,8 @@ var (
 	fileSize      uint64
 	statementSize uint64
 	logLevel      string
+	logFile       string
+	logFormat     string
 	consistency   string
 	snapshot      string
 	noViews       bool
@@ -49,15 +52,6 @@ var (
 	sql           string
 
 	escapeBackslash bool
-
-	rootCmd = &cobra.Command{
-		Use:   "dumpling",
-		Short: "A tool to dump MySQL/TiDB data",
-		Long:  `Dumpling is a CLI tool that helps you dump MySQL/TiDB data`,
-		Run: func(cmd *cobra.Command, args []string) {
-			run()
-		},
-	}
 )
 
 var defaultOutputDir = timestampDirName()
@@ -66,34 +60,48 @@ func timestampDirName() string {
 	return fmt.Sprintf("./export-%s", time.Now().Format(time.RFC3339))
 }
 
-func init() {
-	rootCmd.PersistentFlags().StringVarP(&database, "database", "B", "", "Database to dump")
-	rootCmd.PersistentFlags().StringVarP(&host, "host", "H", "127.0.0.1", "The host to connect to")
-	rootCmd.PersistentFlags().StringVarP(&user, "user", "u", "root", "Username with privileges to run the dump")
-	rootCmd.PersistentFlags().IntVarP(&port, "port", "P", 4000, "TCP/IP port to connect to")
-	rootCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "User password")
-	rootCmd.PersistentFlags().IntVarP(&threads, "threads", "t", 4, "Number of goroutines to use, default 4")
-	rootCmd.PersistentFlags().Uint64VarP(&fileSize, "filesize", "F", export.UnspecifiedSize, "The approximate size of output file")
-	rootCmd.PersistentFlags().Uint64VarP(&statementSize, "statement-size", "S", export.UnspecifiedSize, "Attempted size of INSERT statement in bytes")
-	rootCmd.PersistentFlags().StringVarP(&outputDir, "output", "o", defaultOutputDir, "Output directory")
-	rootCmd.PersistentFlags().StringVar(&logLevel, "loglevel", "info", "Log level: {debug|info|warn|error|dpanic|panic|fatal}")
-	rootCmd.PersistentFlags().StringVar(&consistency, "consistency", "auto", "Consistency level during dumping: {auto|none|flush|lock|snapshot}")
-	rootCmd.PersistentFlags().StringVar(&snapshot, "snapshot", "", "Snapshot position. Valid only when consistency=snapshot")
-	rootCmd.PersistentFlags().BoolVarP(&noViews, "no-views", "W", true, "Do not dump views")
-	rootCmd.PersistentFlags().StringVar(&statusAddr, "status-addr", ":8281", "dumpling API server and pprof addr")
-	rootCmd.PersistentFlags().Uint64VarP(&rows, "rows", "r", export.UnspecifiedSize, "Split table into chunks of this many rows, default unlimited")
-	rootCmd.PersistentFlags().StringVar(&where, "where", "", "Dump only selected records")
-	rootCmd.PersistentFlags().BoolVar(&escapeBackslash, "escape-backslash", true, "use backslash to escape quotation marks")
-	rootCmd.PersistentFlags().StringVar(&fileType, "filetype", "sql", "The type of export file (sql/csv)")
-	rootCmd.PersistentFlags().BoolVar(&noHeader, "no-header", false, "whether not to dump CSV table header")
-	rootCmd.PersistentFlags().BoolVarP(&noSchemas, "no-schemas", "m", false, "Do not dump table schemas with the data")
-	rootCmd.PersistentFlags().BoolVarP(&noData, "no-data", "d", false, "Do not dump table data")
-	rootCmd.PersistentFlags().StringVar(&csvNullValue, "csv-null-value", "\\N", "The null value used when export to csv")
-	rootCmd.PersistentFlags().StringVarP(&sql, "sql", "s", "", "Dump data with given sql")
-}
+func main() {
+	pflag.Usage = func() {
+		fmt.Fprint(os.Stderr, "Dumpling is a CLI tool that helps you dump MySQL/TiDB data\n\nUsage:\n  dumpling [flags]\n\nFlags:\n")
+		pflag.PrintDefaults()
+	}
+	pflag.ErrHelp = errors.New("")
 
-func run() {
+	pflag.StringVarP(&database, "database", "B", "", "Database to dump")
+	pflag.StringVarP(&host, "host", "H", "127.0.0.1", "The host to connect to")
+	pflag.StringVarP(&user, "user", "u", "root", "Username with privileges to run the dump")
+	pflag.IntVarP(&port, "port", "P", 4000, "TCP/IP port to connect to")
+	pflag.StringVarP(&password, "password", "p", "", "User password")
+	pflag.IntVarP(&threads, "threads", "t", 4, "Number of goroutines to use, default 4")
+	pflag.Uint64VarP(&fileSize, "filesize", "F", export.UnspecifiedSize, "The approximate size of output file")
+	pflag.Uint64VarP(&statementSize, "statement-size", "S", export.UnspecifiedSize, "Attempted size of INSERT statement in bytes")
+	pflag.StringVarP(&outputDir, "output", "o", defaultOutputDir, "Output directory")
+	pflag.StringVar(&logLevel, "loglevel", "info", "Log level: {debug|info|warn|error|dpanic|panic|fatal}")
+	pflag.StringVarP(&logFile, "logfile", "L", "", "Log file `path`, leave empty to write to console")
+	pflag.StringVar(&logFormat, "logfmt", "text", "Log `format`: {text|json}")
+	pflag.StringVar(&consistency, "consistency", "auto", "Consistency level during dumping: {auto|none|flush|lock|snapshot}")
+	pflag.StringVar(&snapshot, "snapshot", "", "Snapshot position. Valid only when consistency=snapshot")
+	pflag.BoolVarP(&noViews, "no-views", "W", true, "Do not dump views")
+	pflag.StringVar(&statusAddr, "status-addr", ":8281", "dumpling API server and pprof addr")
+	pflag.Uint64VarP(&rows, "rows", "r", export.UnspecifiedSize, "Split table into chunks of this many rows, default unlimited")
+	pflag.StringVar(&where, "where", "", "Dump only selected records")
+	pflag.BoolVar(&escapeBackslash, "escape-backslash", true, "use backslash to escape quotation marks")
+	pflag.StringVar(&fileType, "filetype", "sql", "The type of export file (sql/csv)")
+	pflag.BoolVar(&noHeader, "no-header", false, "whether not to dump CSV table header")
+	pflag.BoolVarP(&noSchemas, "no-schemas", "m", false, "Do not dump table schemas with the data")
+	pflag.BoolVarP(&noData, "no-data", "d", false, "Do not dump table data")
+	pflag.StringVar(&csvNullValue, "csv-null-value", "\\N", "The null value used when export to csv")
+	pflag.StringVarP(&sql, "sql", "s", "", "Dump data with given sql")
+
+	printVersion := pflag.BoolP("version", "V", false, "Print Dumpling version")
+
+	pflag.Parse()
+
 	println(cli.LongVersion())
+
+	if *printVersion {
+		return
+	}
 
 	conf := export.DefaultConfig()
 	conf.Database = database
@@ -112,6 +120,8 @@ func run() {
 	conf.Where = where
 	conf.EscapeBackslash = escapeBackslash
 	conf.LogLevel = logLevel
+	conf.LogFile = logFile
+	conf.LogFormat = logFormat
 	conf.FileType = fileType
 	conf.NoHeader = noHeader
 	conf.NoSchemas = noSchemas
@@ -124,9 +134,4 @@ func run() {
 		fmt.Printf("dump failed: %s\n", err.Error())
 		os.Exit(1)
 	}
-	return
-}
-
-func main() {
-	rootCmd.Execute()
 }
