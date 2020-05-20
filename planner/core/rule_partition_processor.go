94e814a3822ea09cfcbb9cb320469d9b8cb6c1d2
tiancaiamao <tiancaiamao@gmail.com>
2020-05-20 14:49:54 +0800

    address comment

--- planner/core/rule_partition_processor.go
+++ planner/core/rule_partition_processor.go
@@ -480,9 +480,6 @@ func partitionRangeForOrExpr(sctx sessionctx.Context, expr1, expr2 expression.Ex
 
 func partitionRangeForInExpr(sctx sessionctx.Context, args []expression.Expression,
 	pruner *rangePruner) partitionRangeOR {
-	if len(args) <= 1 {
-		return pruner.fullRange()
-	}
 	col, ok := args[0].(*expression.Column)
 	if !ok || col.ID != pruner.col.ID {
 		return pruner.fullRange()
@@ -495,10 +492,19 @@ func partitionRangeForInExpr(sctx sessionctx.Context, args []expression.Expressi
 		if !ok {
 			return pruner.fullRange()
 		}
+		switch constExpr.Value.Kind() {
+		case types.KindInt64, types.KindUint64:
+		case types.KindNull:
+			result = append(result, partitionRange{0, 1})
+			continue
+		default:
+			return pruner.fullRange()
+		}
 		val, err := constExpr.Value.ToInt64(sctx.GetSessionVars().StmtCtx)
 		if err != nil {
 			return pruner.fullRange()
 		}
+
 		start, end := pruneUseBinarySearch(pruner.lessThan, dataForPrune{op: ast.EQ, c: val}, unsigned)
 		result = append(result, partitionRange{start, end})
 	}
