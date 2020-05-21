87db4af54f9c8096f4cae4aff4fc341c270212d7
Chengpeng Yan <41809508+Reminiscent@users.noreply.github.com>
2020-05-21 19:26:00 +0800

    cherry pick #16248 to release-4.0
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- executor/index_lookup_merge_join.go
+++ executor/index_lookup_merge_join.go
@@ -26,6 +26,11 @@ import (
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/chunk"
+<<<<<<< HEAD
+=======
+	"github.com/pingcap/tidb/util/execdetails"
+	"github.com/pingcap/tidb/util/logutil"
+>>>>>>> 0d95b09... executor: Remove unnecessary information in explain analyze output (#16248)
 	"github.com/pingcap/tidb/util/memory"
 	"github.com/pingcap/tidb/util/ranger"
 	"github.com/pingcap/tidb/util/stringutil"
@@ -686,7 +691,7 @@ func (e *IndexLookUpMergeJoin) Close() error {
 	e.memTracker = nil
 	if e.runtimeStats != nil {
 		concurrency := cap(e.resultCh)
-		e.runtimeStats.SetConcurrencyInfo("Concurrency", concurrency)
+		e.runtimeStats.SetConcurrencyInfo(execdetails.NewConcurrencyInfo("Concurrency", concurrency))
 	}
 	return e.baseExecutor.Close()
 }
