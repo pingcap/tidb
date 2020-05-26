3987aa104c6feb5e3da2fdfca71778c9a638cfd7
lawyerphx <panhexiang@pingcap.com>
2020-05-25 23:00:51 -0700

    address comment.

--- session/bootstrap.go
+++ session/bootstrap.go
@@ -240,7 +240,7 @@ const (
 		update_time timestamp(3) NOT NULL,
 		charset text NOT NULL,
 		collation text NOT NULL,
-		create_way varchar(20) NOT NULL default 'unknow',
+		create_way varchar(20) NOT NULL default 'unknown',
 		INDEX sql_index(original_sql(1024),default_db(1024)) COMMENT "accelerate the speed when add global binding query",
 		INDEX time_index(update_time) COMMENT "accelerate the speed when querying with last update time"
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;`
@@ -1068,7 +1068,7 @@ func upgradeToVer47(s Session, ver int64) {
 	if ver >= version47 {
 		return
 	}
-	doReentrantDDL(s, "ALTER TABLE mysql.bind_info ADD COLUMN `create_way` varchar(20) NOT NULL", infoschema.ErrColumnExists)
+	doReentrantDDL(s, "ALTER TABLE mysql.bind_info ADD COLUMN `create_way` varchar(20) NOT NULL default 'unknown'", infoschema.ErrColumnExists)
 }
 
 // updateBootstrapVer updates bootstrap version variable in mysql.TiDB table.
