6e074d4eb3c01886b3da7164d8d327e1daed6158
Shuaipeng Yu <jackysp@gmail.com>
2020-05-27 13:45:08 +0800

    add more
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- metrics/server.go
+++ metrics/server.go
@@ -128,7 +128,7 @@ var (
 			Subsystem: "server",
 			Name:      "get_token_duration_seconds",
 			Help:      "Duration (us) for getting token, it should be small until concurrency limit is reached.",
-			Buckets:   prometheus.ExponentialBuckets(1, 2, 26), // 1us ~ 33s
+			Buckets:   prometheus.ExponentialBuckets(1, 2, 30), // 1us ~ 528s
 		})
 
 	TotalQueryProcHistogram = prometheus.NewHistogram(
@@ -137,7 +137,7 @@ var (
 			Subsystem: "server",
 			Name:      "slow_query_process_duration_seconds",
 			Help:      "Bucketed histogram of processing time (s) of of slow queries.",
-			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 22), // 1ms ~ 2048s
+			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 28), // 1ms ~ 1.5days
 		})
 	TotalCopProcHistogram = prometheus.NewHistogram(
 		prometheus.HistogramOpts{
@@ -145,7 +145,7 @@ var (
 			Subsystem: "server",
 			Name:      "slow_query_cop_duration_seconds",
 			Help:      "Bucketed histogram of all cop processing time (s) of of slow queries.",
-			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 22), // 1ms ~ 2048s
+			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 28), // 1ms ~ 1.5days
 		})
 	TotalCopWaitHistogram = prometheus.NewHistogram(
 		prometheus.HistogramOpts{
@@ -153,7 +153,7 @@ var (
 			Subsystem: "server",
 			Name:      "slow_query_wait_duration_seconds",
 			Help:      "Bucketed histogram of all cop waiting time (s) of of slow queries.",
-			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 22), // 1ms ~ 2048s
+			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 28), // 1ms ~ 1.5days
 		})
 )
 
