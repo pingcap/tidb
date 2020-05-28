97b8998ee262dee5416b604c62dfdfbbd797ab3f
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-05-28 10:53:25 +0800

    fix the issue that max_execution_time hint doesn't work if it exceed the expensive-slow-log-threshold

--- util/expensivequery/expensivequery.go
+++ util/expensivequery/expensivequery.go
@@ -59,16 +59,22 @@ func (eqh *Handle) Run() {
 		case <-ticker.C:
 			processInfo := sm.ShowProcessList()
 			for _, info := range processInfo {
-				if len(info.Info) == 0 || info.ExceedExpensiveTimeThresh {
+				if len(info.Info) == 0 {
 					continue
 				}
+				// kill the query whose run time exceeds its max execution time
 				costTime := time.Since(info.Time)
+				if info.MaxExecutionTime > 0 && costTime > time.Duration(info.MaxExecutionTime)*time.Microsecond {
+					sm.Kill(info.ID, true)
+				}
+
+				// log expensive query
+				if info.ExceedExpensiveTimeThresh {
+					continue
+				}
 				if costTime >= time.Second*time.Duration(threshold) && log.GetLevel() <= zapcore.WarnLevel {
 					logExpensiveQuery(costTime, info)
 					info.ExceedExpensiveTimeThresh = true
-
-				} else if info.MaxExecutionTime > 0 && costTime > time.Duration(info.MaxExecutionTime)*time.Millisecond {
-					sm.Kill(info.ID, true)
 				}
 			}
 			threshold = atomic.LoadUint64(&variable.ExpensiveQueryTimeThreshold)
