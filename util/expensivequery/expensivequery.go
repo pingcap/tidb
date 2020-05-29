c32a3a7067a3a4258028daf766de1d54cfc398ee
mantuliu <240951888@qq.com>
2020-05-29 19:07:34 +0800

    cherry pick #17359 to release-3.1
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- util/expensivequery/expensivequery.go
+++ util/expensivequery/expensivequery.go
@@ -62,15 +62,20 @@ func (eqh *Handle) Run() {
 		case <-ticker.C:
 			processInfo := sm.ShowProcessList()
 			for _, info := range processInfo {
+<<<<<<< HEAD
 				if info.Info == nil || info.ExceedExpensiveTimeThresh {
+=======
+				if len(info.Info) == 0 {
+>>>>>>> 7096916... expensivequery: fix the issue that max_exec_time hint doesn't work if it exceeds the expensive-threshold  (#17359)
 					continue
 				}
 				costTime := time.Since(info.Time)
-				if costTime >= time.Second*time.Duration(threshold) && log.GetLevel() <= zapcore.WarnLevel {
+				if !info.ExceedExpensiveTimeThresh && costTime >= time.Second*time.Duration(threshold) && log.GetLevel() <= zapcore.WarnLevel {
 					logExpensiveQuery(costTime, info)
 					info.ExceedExpensiveTimeThresh = true
+				}
 
-				} else if info.MaxExecutionTime > 0 && costTime > time.Duration(info.MaxExecutionTime)*time.Millisecond {
+				if info.MaxExecutionTime > 0 && costTime > time.Duration(info.MaxExecutionTime)*time.Millisecond {
 					sm.Kill(info.ID, true)
 				}
 			}
