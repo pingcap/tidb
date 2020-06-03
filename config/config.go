2940cda89ac4076e3273d47f2dc6bdccd05da598
crazycs <crazycs520@gmail.com>
2020-06-03 11:23:52 +0800

    open collect runtime information to bench
    Signed-off-by: crazycs <crazycs520@gmail.com>

--- config/config.go
+++ config/config.go
@@ -683,7 +683,7 @@ var defaultConf = Config{
 		AllowAutoRandom:       false,
 		AllowsExpressionIndex: false,
 	},
-	EnableCollectExecutionInfo: false,
+	EnableCollectExecutionInfo: true,
 }
 
 var (
