8c14c0481a36850da15c6f652f083b9961914618
crazycs <crazycs520@gmail.com>
2020-06-03 11:39:02 +0800

    fix test
    Signed-off-by: crazycs <crazycs520@gmail.com>

--- ddl/ddl_worker.go
+++ ddl/ddl_worker.go
@@ -462,7 +462,7 @@ func (w *worker) handleDDLJobQueue(d *ddlCtx) error {
 				// Some DDL jobs (such as adding indexes) may need to update the table info and the schema version,
 				// then shouldn't discard the KV modification.
 				// Otherwise, we should discard the KV modification when running job.
-				txn.Discard()
+				txn.Reset()
 			}
 			err = w.updateDDLJob(t, job, runJobErr != nil)
 			if err = w.handleUpdateJobError(t, job, err); err != nil {
