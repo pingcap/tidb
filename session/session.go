b7181f7b44c2003fb58eba149641f56bd5e3213d
Yiding Cui <winoros@gmail.com>
2020-06-19 15:18:40 +0800

    planner: save explain result of plan in session fo ... (#17687) (#18124)

--- session/session.go
+++ session/session.go
@@ -1017,6 +1017,7 @@ func (s *session) SetProcessInfo(sql string, t time.Time, command byte, maxExecu
 		DB:               s.sessionVars.CurrentDB,
 		Command:          command,
 		Plan:             s.currentPlan,
+		PlanExplainRows:  plannercore.GetExplainRowsForPlan(s.currentPlan),
 		Time:             t,
 		State:            s.Status(),
 		Info:             sql,
