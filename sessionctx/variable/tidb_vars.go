aff39f1456f944e63edde72f118b0255a12164e4
Han Fei <hanfei19910905@gmail.com>
2020-06-24 10:48:12 +0800

    cherry pick #18190 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -460,7 +460,7 @@ const (
 	DefTiDBHashJoinConcurrency         = 5
 	DefTiDBProjectionConcurrency       = 4
 	DefTiDBOptimizerSelectivityLevel   = 0
-	DefTiDBAllowBatchCop               = 0
+	DefTiDBAllowBatchCop               = 1
 	DefTiDBTxnMode                     = ""
 	DefTiDBRowFormatV1                 = 1
 	DefTiDBRowFormatV2                 = 2
