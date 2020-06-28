33fb289d986a4c75bf9825f264d2f1542a86a680
wjhuang2016 <huangwenjun1997@gmail.com>
2020-06-28 11:37:19 +0800

    fix
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- ddl/db_change_test.go
+++ ddl/db_change_test.go
@@ -1324,7 +1324,7 @@ func (s *testStateChangeSuite) TestParallelDDLBeforeRunDDLJob(c *C) {
 		se.SetConnectionID(firstConnID)
 		_, err1 := se.Execute(context.Background(), "alter table test_table drop column c2")
 		c.Assert(err1, IsNil)
-		// Sleep a while to make sure the connection 1 break out the first loop
+		// Sleep a while to make sure the connection 1 break out the first loop.
 		time.Sleep(100 * time.Millisecond)
 		atomic.StoreInt32(&sessionCnt, finishedCnt)
 	}()
