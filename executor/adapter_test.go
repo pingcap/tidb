17b5bc7b1d8b4e0f2bcb3232c68d92b55b68ef7b
mmyj <mayj.hello@gmail.com>
2020-06-29 11:19:28 +0800

    fix

--- executor/adapter_test.go
+++ executor/adapter_test.go
@@ -55,18 +55,21 @@ func (s *testSuiteP2) TestFormatSQL(c *C) {
 	preparedSQL := executor.FormatSQL("select ?, ?, ?, ?, ?, ?, ?;", preparedParams)()
 	c.Check(preparedSQL, Equals, "select 1, 2, \"hello, 世界\", [1, 2, 3], {}, {\"a\": \"9223372036854775809\"}, 2011-11-10 11:11:11.111111;")
 
-	cfg := config.NewConfig()
-	cfg.Log.QueryLogMaxLen = 10
-	config.StoreGlobalConfig(cfg)
-	preparedSQL = executor.FormatSQL("select ?, ?, ?, ?, ?, ?, ?;", preparedParams)()
-	c.Check(preparedSQL, Equals, "\"select 1, \"(len:27)")
-
-	config.StoreGlobalConfig(config.NewConfig())
 	preparedParams = variable.PreparedParams{
 		types.NewIntDatum(1),
 	}
 	preparedSQL = executor.FormatSQL("select count(*), ?;", preparedParams)()
 	c.Check(preparedSQL, Equals, "select count(*), 1;")
+
+	cfg := config.GetGlobalConfig()
+	originQueryLogMaxLen := cfg.Log.QueryLogMaxLen
+	cfg.Log.QueryLogMaxLen = 10
+	config.StoreGlobalConfig(cfg)
+	preparedSQL = executor.FormatSQL("select ?, ?, ?, ?, ?, ?, ?;", preparedParams)()
+	c.Check(preparedSQL, Equals, "\"select 1, \"(len:27)")
+
+	cfg.Log.QueryLogMaxLen = originQueryLogMaxLen
+	config.StoreGlobalConfig(cfg)
 }
 
 // mustParseTimeIntoDatum is similar to ParseTime but panic if any error occurs.
