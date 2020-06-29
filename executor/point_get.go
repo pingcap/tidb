9f6abc184971ec3c7b5c1b4383a81e1bd1035485
tiancaiamao <tiancaiamao@gmail.com>
2020-06-29 16:36:49 +0800

    address comment

--- executor/point_get.go
+++ executor/point_get.go
@@ -112,6 +112,21 @@ func (e *PointGetExecutor) buildVirtualColumnInfo() {
 
 // Open implements the Executor interface.
 func (e *PointGetExecutor) Open(context.Context) error {
+	return nil
+}
+
+// Close implements the Executor interface.
+func (e *PointGetExecutor) Close() error {
+	return nil
+}
+
+// Next implements the Executor interface.
+func (e *PointGetExecutor) Next(ctx context.Context, req *chunk.Chunk) error {
+	req.Reset()
+	if e.done {
+		return nil
+	}
+	e.done = true
 	txnCtx := e.ctx.GetSessionVars().TxnCtx
 	snapshotTS := e.startTS
 	if e.lock {
@@ -135,23 +150,7 @@ func (e *PointGetExecutor) Open(context.Context) error {
 		e.snapshot.SetOption(kv.ReplicaRead, kv.ReplicaReadFollower)
 	}
 	e.snapshot.SetOption(kv.TaskID, e.ctx.GetSessionVars().StmtCtx.TaskID)
-	return nil
-}
-
-// Close implements the Executor interface.
-func (e *PointGetExecutor) Close() error {
-	return nil
-}
-
-// Next implements the Executor interface.
-func (e *PointGetExecutor) Next(ctx context.Context, req *chunk.Chunk) error {
-	req.Reset()
-	if e.done {
-		return nil
-	}
-	e.done = true
 	var tblID int64
-	var err error
 	if e.partInfo != nil {
 		tblID = e.partInfo.ID
 	} else {
