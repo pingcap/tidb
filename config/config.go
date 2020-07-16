47d9dd0041183b9fe73d326e8d95c7628fad01f1
tangenta <tangenta@126.com>
2020-07-16 12:21:59 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -708,25 +708,14 @@ func StoreGlobalConfig(config *Config) {
 }
 
 var deprecatedConfig = map[string]struct{}{
-<<<<<<< HEAD
-	"pessimistic-txn.ttl":        {},
-	"log.file.log-rotate":        {},
-	"log.log-slow-query":         {},
-	"txn-local-latches":          {},
-	"txn-local-latches.enabled":  {},
-	"txn-local-latches.capacity": {},
-	"max-txn-time-use":           {},
-=======
 	"pessimistic-txn.ttl":            {},
 	"log.file.log-rotate":            {},
 	"log.log-slow-query":             {},
 	"txn-local-latches":              {},
 	"txn-local-latches.enabled":      {},
 	"txn-local-latches.capacity":     {},
-	"performance.max-memory":         {},
 	"max-txn-time-use":               {},
 	"experimental.allow-auto-random": {},
->>>>>>> 38621d2... config: deprecate allow-auto-random option (#18618)
 }
 
 func isAllDeprecatedConfigItems(items []string) bool {
