f35f65083bf9c5a334308ade94ca14fbb9ba5ae6
tangenta <tangenta@126.com>
2020-07-17 16:16:08 +0800

    refine check

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -3563,7 +3563,7 @@ func checkColumnWithIndexConstraint(tbInfo *model.TableInfo, originalCol, newCol
 func checkAutoRandom(tableInfo *model.TableInfo, originCol *table.Column, specNewColumn *ast.ColumnDef) (uint64, error) {
 	// Disallow add/drop actions on auto_random.
 	var oldRandBits uint64
-	if tableInfo.GetPkName().L == originCol.Name.L {
+	if tableInfo.PKIsHandle && (tableInfo.GetPkName().L == originCol.Name.L) {
 		oldRandBits = tableInfo.AutoRandomBits
 	}
 	newRandBits, err := extractAutoRandomBitsFromColDef(specNewColumn)
