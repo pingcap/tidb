3dd2e6a378c599f67f93cd4c15b181e3aa7277d2
jixiaodong <xiaodong.ji@outlook.com>
2020-07-21 12:30:59 +0800

    remove memTracker from StmtCommit

--- session/txn.go
+++ session/txn.go
@@ -510,27 +510,19 @@ func (s *session) HasDirtyContent(tid int64) bool {
 }
 
 // StmtCommit implements the sessionctx.Context interface.
-func (s *session) StmtCommit(memTracker *memory.Tracker) (err error) {
+func (s *session) StmtCommit(memTracker *memory.Tracker) error {
 	defer func() {
-		// If StmtCommit is called in batch mode, we need to clear the txn size
-		// in memTracker to avoid double-counting. If it's not batch mode, this
-		// work has no effect because that no more data will be appended into
-		// s.txn.
-		if r := recover(); r != nil {
-			err = errors.Errorf("%v", r)
-			s.txn.doNotCommit = err
-			return
-		}
 		s.txn.cleanup()
 	}()
+
 	st := &s.txn
-	txnSize := st.Transaction.Size()
 	bufLen := st.stmtBufLen()
 
 	if _, err := st.flushStmtBuf(); err != nil {
 		return err
 	}
 
+	var err error
 	failpoint.Inject("mockStmtCommitError", func(val failpoint.Value) {
 		if val.(bool) && bufLen > 3 {
 			err = errors.New("mock stmt commit error")
@@ -539,9 +531,6 @@ func (s *session) StmtCommit(memTracker *memory.Tracker) (err error) {
 	if err != nil {
 		st.doNotCommit = err
 		return err
-	}
-	if memTracker != nil {
-		memTracker.Consume(int64(st.Transaction.Size() - txnSize))
 	}
 
 	// Need to flush binlog.
