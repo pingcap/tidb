c1caa8ba2d96a86d79fefa0fb4de0d9090b7ca62
cfzjywxk <lsswxr@163.com>
2020-07-24 16:30:03 +0800

    test:stablize test case prewrite secondaries (#18751)
    * stablize test case prewrite secondaries
    
    * try to fix race
    
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- store/tikv/2pc_test.go
+++ store/tikv/2pc_test.go
@@ -47,6 +47,7 @@ var _ = SerialSuites(&testCommitterSuite{})
 func (s *testCommitterSuite) SetUpSuite(c *C) {
 	atomic.StoreUint64(&ManagedLockTTL, 3000) // 3s
 	s.OneByOneSuite.SetUpSuite(c)
+	CommitMaxBackoff = 1000
 }
 
 func (s *testCommitterSuite) SetUpTest(c *C) {
@@ -77,7 +78,6 @@ func (s *testCommitterSuite) SetUpTest(c *C) {
 	// c.Assert(err, IsNil)
 
 	s.store = store
-	CommitMaxBackoff = 1000
 }
 
 func (s *testCommitterSuite) TearDownSuite(c *C) {
@@ -1137,7 +1137,7 @@ func (s *testCommitterSuite) TestResolveMixed(c *C) {
 
 // TestSecondaryKeys tests that when async commit is enabled, each prewrite message includes an
 // accurate list of secondary keys.
-func (s *testCommitterSuite) TestPrewiteSecondaryKeys(c *C) {
+func (s *testCommitterSuite) TestPrewriteSecondaryKeys(c *C) {
 	defer config.RestoreFunc()()
 	config.UpdateGlobal(func(conf *config.Config) {
 		conf.TiKVClient.EnableAsyncCommit = true
@@ -1169,9 +1169,11 @@ func (s *testCommitterSuite) TestPrewiteSecondaryKeys(c *C) {
 	ctx := context.Background()
 	// TODO remove this when minCommitTS is returned from mockStore prewrite response.
 	committer.minCommitTS = committer.startTS + 10
+	committer.testingKnobs.noFallBack = true
 	err = committer.execute(ctx)
 	c.Assert(err, IsNil)
-	c.Assert(mock.seenPrimaryReq > 0 && mock.seenSecondaryReq > 0, IsTrue)
+	c.Assert(mock.seenPrimaryReq > 0, IsTrue)
+	c.Assert(mock.seenSecondaryReq > 0, IsTrue)
 }
 
 func (s *testCommitterSuite) TestAsyncCommit(c *C) {
