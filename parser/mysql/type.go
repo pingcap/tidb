63da8aaba01f46cbc8fb6b142fda6c714467c879
wangggong <wangruichao2014@xiaochuankeji.cn>
2020-07-27 16:37:37 +0800

    [parser] fix: remove TypeDecimal (#944)
    Signed-off-by: Wang Ruichao <wangruichao2014@xiaochuankeji.cn>

--- parser/mysql/type.go
+++ parser/mysql/type.go
@@ -15,17 +15,17 @@ package mysql
 
 // MySQL type information.
 const (
-	TypeDecimal   byte = 0
-	TypeTiny      byte = 1
-	TypeShort     byte = 2
-	TypeLong      byte = 3
-	TypeFloat     byte = 4
-	TypeDouble    byte = 5
-	TypeNull      byte = 6
-	TypeTimestamp byte = 7
-	TypeLonglong  byte = 8
-	TypeInt24     byte = 9
-	TypeDate      byte = 10
+	TypeUnspecified byte = 0
+	TypeTiny        byte = 1
+	TypeShort       byte = 2
+	TypeLong        byte = 3
+	TypeFloat       byte = 4
+	TypeDouble      byte = 5
+	TypeNull        byte = 6
+	TypeTimestamp   byte = 7
+	TypeLonglong    byte = 8
+	TypeInt24       byte = 9
+	TypeDate        byte = 10
 	/* TypeDuration original name was TypeTime, renamed to TypeDuration to resolve the conflict with Go type Time.*/
 	TypeDuration byte = 11
 	TypeDatetime byte = 12
@@ -46,9 +46,6 @@ const (
 	TypeString     byte = 0xfe
 	TypeGeometry   byte = 0xff
 )
-
-// TypeUnspecified is an uninitialized type. TypeDecimal is not used in MySQL.
-const TypeUnspecified = TypeDecimal
 
 // Flag information.
 const (
