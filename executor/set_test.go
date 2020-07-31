d684ed9e23152f816fedc967cfcaf8ec796de5e6
Kolbe Kegel <kolbe@pingcap.com>
2020-07-31 01:23:14 -0700

    cherry pick #18896 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/set_test.go
+++ executor/set_test.go
@@ -915,6 +915,9 @@ func (s *testSuite5) TestValidateSetVar(c *C) {
 	tk.MustExec("SET SESSION tidb_skip_isolation_level_check = 0")
 	_, err = tk.Exec("set @@tx_isolation='SERIALIZABLE'")
 	c.Assert(terror.ErrorEqual(err, variable.ErrUnsupportedIsolationLevel), IsTrue, Commentf("err %v", err))
+
+	tk.MustExec("set global allow_auto_random_explicit_insert=on;")
+	tk.MustQuery("select @@global.allow_auto_random_explicit_insert;").Check(testkit.Rows("1"))
 }
 
 func (s *testSuite5) TestSelectGlobalVar(c *C) {
