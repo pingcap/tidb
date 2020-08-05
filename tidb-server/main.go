998a8a600002c4d313cd71c73cb3cfe109907b9d
Zejun Li <lizejun@pingcap.com>
2020-08-05 19:15:54 +0800

    metrics: monitoring GOMAXPROCS (#18987)
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- tidb-server/main.go
+++ tidb-server/main.go
@@ -233,6 +233,7 @@ func setCPUAffinity() {
 		exit()
 	}
 	runtime.GOMAXPROCS(len(cpu))
+	metrics.MaxProcs.Set(float64(runtime.GOMAXPROCS(0)))
 }
 
 func registerStores() {
@@ -376,6 +377,7 @@ func reloadConfig(nc, c *config.Config) {
 	}
 	if nc.Performance.MaxProcs != c.Performance.MaxProcs {
 		runtime.GOMAXPROCS(int(nc.Performance.MaxProcs))
+		metrics.MaxProcs.Set(float64(runtime.GOMAXPROCS(0)))
 	}
 	if nc.TiKVClient.StoreLimit != c.TiKVClient.StoreLimit {
 		storeutil.StoreLimit.Store(nc.TiKVClient.StoreLimit)
@@ -508,6 +510,7 @@ func setGlobalVars() {
 	// We should respect to user's settings in config file.
 	// The default value of MaxProcs is 0, runtime.GOMAXPROCS(0) is no-op.
 	runtime.GOMAXPROCS(int(cfg.Performance.MaxProcs))
+	metrics.MaxProcs.Set(float64(runtime.GOMAXPROCS(0)))
 
 	ddlLeaseDuration := parseDuration(cfg.Lease)
 	session.SetSchemaLease(ddlLeaseDuration)
