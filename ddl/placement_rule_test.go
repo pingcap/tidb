b00af20ed11138f61ad929b9c7c58e98ab59c1e7
xhe <xw897002528@gmail.com>
2020-08-10 17:35:10 +0800

    ddl: improve tests

--- ddl/placement_rule_test.go
+++ ddl/placement_rule_test.go
@@ -90,7 +90,18 @@ add placement policy
 
 	_, err = tk.Exec(`alter table t1 alter partition p0
 add placement policy
-	constraints='{"+   zone   =   sh  ": 1, "- zone = bj": 2}'
+	constraints='["+   zone   =   sh  "]'
+	role=leader
+	replicas=3,
+add placement policy
+	constraints='{"+zone=sh,+zone=bj":1,"+zone=sh,+zone=bj":1}'
+	role=leader
+	replicas=3`)
+	c.Assert(err, ErrorMatches, ".*pd unavailable.*")
+
+	_, err = tk.Exec(`alter table t1 alter partition p0
+add placement policy
+	constraints='{"+   zone   =   sh  ": 1, "- zone = bj,+zone=sh": 2}'
 	role=leader
 	replicas=3,
 alter placement policy
