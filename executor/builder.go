3948c7ba7b5d4a081abf2162b791ab67fe650882
cfzjywxk <lsswxr@163.com>
2020-08-11 15:22:53 +0800

    fix apply with union scan and point batch point get (#19113)
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- executor/builder.go
+++ executor/builder.go
@@ -906,6 +906,7 @@ func (b *executorBuilder) buildUnionScanFromReader(reader Executor, v *plannerco
 	us.mutableRow = chunk.MutRowFromTypes(retTypes(us))
 
 	// If the push-downed condition contains virtual column, we may build a selection upon reader
+	originReader := reader
 	if sel, ok := reader.(*SelectionExec); ok {
 		reader = sel.children[0]
 	}
@@ -959,7 +960,7 @@ func (b *executorBuilder) buildUnionScanFromReader(reader Executor, v *plannerco
 		us.virtualColumnIndex = buildVirtualColumnIndex(us.Schema(), us.columns)
 	default:
 		// The mem table will not be written by sql directly, so we can omit the union scan to avoid err reporting.
-		return reader
+		return originReader
 	}
 	return us
 }
