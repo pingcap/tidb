5b98b74e57196b734eed2f90c15486a431e2d654
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-08-12 22:30:48 +0800

    [dumpling] close connection after export (#130)
    * close connection after export

--- dumpling/v4/export/connectionsPool.go
+++ dumpling/v4/export/connectionsPool.go
@@ -6,25 +6,40 @@ import (
 )
 
 type connectionsPool struct {
-	conns chan *sql.Conn
+	conns        chan *sql.Conn
+	createdConns []*sql.Conn
 }
 
 func newConnectionsPool(ctx context.Context, n int, pool *sql.DB) (*connectionsPool, error) {
 	connectPool := &connectionsPool{
-		conns: make(chan *sql.Conn, n),
+		conns:        make(chan *sql.Conn, n),
+		createdConns: make([]*sql.Conn, 0, n),
 	}
 	for i := 0; i < n; i++ {
 		conn, err := createConnWithConsistency(ctx, pool)
 		if err != nil {
-			return nil, err
+			connectPool.Close()
+			return connectPool, err
 		}
 		connectPool.releaseConn(conn)
+		connectPool.createdConns = append(connectPool.createdConns, conn)
 	}
 	return connectPool, nil
 }
 
 func (r *connectionsPool) getConn() *sql.Conn {
 	return <-r.conns
+}
+
+func (r *connectionsPool) Close() error {
+	var err error
+	for _, conn := range r.createdConns {
+		err2 := conn.Close()
+		if err2 != nil {
+			err = err2
+		}
+	}
+	return err
 }
 
 func (r *connectionsPool) releaseConn(conn *sql.Conn) {
