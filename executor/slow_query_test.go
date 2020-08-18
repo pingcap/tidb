488509e69189e3a0b5252bca2e42a2a3566c0303
jyz0309 <45495947@qq.com>
2020-08-18 17:06:57 +0800

    update failpoint test

--- executor/slow_query_test.go
+++ executor/slow_query_test.go
@@ -23,6 +23,7 @@ import (
 	"time"
 
 	. "github.com/pingcap/check"
+	"github.com/pingcap/failpoint"
 	"github.com/pingcap/parser/terror"
 	plannercore "github.com/pingcap/tidb/planner/core"
 	"github.com/pingcap/tidb/sessionctx"
@@ -51,6 +52,41 @@ func parseSlowLog(sctx sessionctx.Context, reader *bufio.Reader) ([][]types.Datu
 	terror.Log(retriever.initialize(sctx))
 	rows, err := parseLog(retriever, sctx, reader)
 	return rows, err
+}
+func (s *testExecSuite) TestPanic(c *C) {
+	slowLogStr :=
+		`# Time: 2019-04-28T15:24:04.309074+08:00
+# Txn_start_ts: 405888132465033227
+# User@Host: root[root] @ localhost [127.0.0.1]
+# Query_time: 0.216905
+# Cop_time: 0.38 Process_time: 0.021 Request_count: 1 Total_keys: 637 Processed_keys: 436
+# Is_internal: true
+# Digest: 42a1c8aae6f133e934d4bf0147491709a8812ea05ff8819ec522780fe657b772
+# Stats: t1:1,t2:2
+# Cop_proc_avg: 0.1 Cop_proc_p90: 0.2 Cop_proc_max: 0.03 Cop_proc_addr: 127.0.0.1:20160
+# Cop_wait_avg: 0.05 Cop_wait_p90: 0.6 Cop_wait_max: 0.8 Cop_wait_addr: 0.0.0.0:20160
+# Mem_max: 70724
+# Disk_max: 65536
+# Plan_from_cache: true
+# Succ: false
+# Plan_digest: 60e9378c746d9a2be1c791047e008967cf252eb6de9167ad3aa6098fa2d523f4
+# Prev_stmt: update t set i = 1;
+use test;
+select * from t;`
+	c.Assert(failpoint.Enable("github.com/pingcap/tidb/ddl/errorMockPanic", `return(true)`), IsNil)
+	defer func() {
+		c.Assert(failpoint.Disable("github.com/pingcap/tidb/ddl/errorMockPanic"), IsNil)
+	}()
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	c.Assert(err, IsNil)
+	retriever := &slowQueryRetriever{}
+	reader := bufio.NewReader(bytes.NewBufferString(slowLogStr))
+	sctx := mock.NewContext()
+	sctx.GetSessionVars().TimeZone = loc
+	rows, err := parseLog(retriever, sctx, reader)
+	c.Assert(rows, IsNil)
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "panic test")
 }
 
 func (s *testExecSuite) TestParseSlowLogFile(c *C) {
