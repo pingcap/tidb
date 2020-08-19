df4794fc193545679890d6d6c8e0aeca5ea8edd7
Nullnotnil <nullnotnil1@gmail.com>
2020-08-19 08:59:08 -0600

    executor, server: reduce log spam

--- server/server.go
+++ server/server.go
@@ -385,10 +385,10 @@ func (s *Server) onConn(conn *clientConn) {
 		return
 	}
 
-	logutil.Logger(ctx).Info("new connection", zap.String("remoteAddr", conn.bufReadConn.RemoteAddr().String()))
+	logutil.Logger(ctx).Debug("new connection", zap.String("remoteAddr", conn.bufReadConn.RemoteAddr().String()))
 
 	defer func() {
-		logutil.Logger(ctx).Info("connection closed")
+		logutil.Logger(ctx).Debug("connection closed")
 	}()
 	s.rwlock.Lock()
 	s.clients[conn.connectionID] = conn
