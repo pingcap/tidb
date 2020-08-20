f269c5a42c09bf20caafba1983d22ba088f2fe24
Lynn <zimu_xia@126.com>
2020-08-20 17:17:04 +0800

    *: update tests

--- planner/core/partition_pruning_test.go
+++ planner/core/partition_pruning_test.go
@@ -142,7 +142,7 @@ func prepareTestCtx(c *C, createTable string, partitionExpr string) *testCtx {
 	sctx := mock.NewContext()
 	tblInfo, err := ddl.BuildTableInfoFromAST(stmt.(*ast.CreateTableStmt))
 	c.Assert(err, IsNil)
-	columns, names, err := expression.ColumnInfos2ColumnsAndNames(sctx, model.NewCIStr("t"), tblInfo.Name, tblInfo.Columns, tblInfo)
+	columns, names, err := expression.ColumnInfos2ColumnsAndNames(sctx, model.NewCIStr("t"), tblInfo.Name, tblInfo, false)
 	c.Assert(err, IsNil)
 	schema := expression.NewSchema(columns...)
 
