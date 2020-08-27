ded946d9243aa842bf01974297166e2a55735701
lzmhhh123 <lzmhhh123@gmail.com>
2020-08-27 12:04:35 +0800

    executor: partition table reader below apply should reset in Open

--- executor/partition_table_test.go
+++ executor/partition_table_test.go
@@ -102,3 +102,46 @@ func (s *partitionTableSuite) TestDAGTableID(c *C) {
 	// Check table ID is changed to partition ID for each partition.
 	c.Assert(m, HasLen, 4)
 }
+
+func (s *partitionTableSuite) TestPartitionReaderUnderApply(c *C) {
+	tk := testkit.NewTestKitWithInit(c, s.store)
+	tk.MustExec("use test")
+
+	// For issue 19458.
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(c_int int)")
+	tk.MustExec("insert into t values(1), (2), (3), (4), (5), (6), (7), (8), (9)")
+	tk.MustExec("DROP TABLE IF EXISTS `t1`")
+	tk.MustExec(`CREATE TABLE t1 (
+		  c_int int NOT NULL,
+		  c_str varchar(40) NOT NULL,
+		  c_datetime datetime NOT NULL,
+		  c_timestamp timestamp NULL DEFAULT NULL,
+		  c_double double DEFAULT NULL,
+		  c_decimal decimal(12,6) DEFAULT NULL,
+		  PRIMARY KEY (c_int,c_str,c_datetime)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+		 PARTITION BY RANGE (c_int)
+		(PARTITION p0 VALUES LESS THAN (2) ENGINE = InnoDB,
+		 PARTITION p1 VALUES LESS THAN (4) ENGINE = InnoDB,
+		 PARTITION p2 VALUES LESS THAN (6) ENGINE = InnoDB,
+		 PARTITION p3 VALUES LESS THAN (8) ENGINE = InnoDB,
+		 PARTITION p4 VALUES LESS THAN (10) ENGINE = InnoDB,
+		 PARTITION p5 VALUES LESS THAN (20) ENGINE = InnoDB,
+		 PARTITION p6 VALUES LESS THAN (50) ENGINE = InnoDB,
+		 PARTITION p7 VALUES LESS THAN (1000000000) ENGINE = InnoDB)`)
+	tk.MustExec("INSERT INTO `t1` VALUES (19,'nifty feistel','2020-02-28 04:01:28','2020-02-04 06:11:57',32.430079,1.284000),(20,'objective snyder','2020-04-15 17:55:04','2020-05-30 22:04:13',37.690874,9.372000)")
+	tk.MustExec("begin")
+	tk.MustExec("insert into t1 values (22, 'wizardly saha', '2020-05-03 16:35:22', '2020-05-03 02:18:42', 96.534810, 0.088)")
+	tk.MustQuery("select c_int from t where (select min(t1.c_int) from t1 where t1.c_int > t.c_int) > (select count(*) from t1 where t1.c_int > t.c_int) order by c_int").Check(testkit.Rows(
+		"1", "2", "3", "4", "5", "6", "7", "8", "9"))
+	tk.MustExec("rollback")
+
+	// For issue 19450.
+	tk.MustExec("drop table if exists t1, t2")
+	tk.MustExec("create table t1  (c_int int, c_str varchar(40), c_decimal decimal(12, 6), primary key (c_int))")
+	tk.MustExec("create table t2  (c_int int, c_str varchar(40), c_decimal decimal(12, 6), primary key (c_int)) partition by hash (c_int) partitions 4")
+	tk.MustExec("insert into t1 values (1, 'romantic robinson', 4.436), (2, 'stoic chaplygin', 9.826), (3, 'vibrant shamir', 6.300), (4, 'hungry wilson', 4.900), (5, 'naughty swartz', 9.524)")
+	tk.MustExec("insert into t2 select * from t1")
+	tk.MustExec("select * from t1 where c_decimal in (select c_decimal from t2 where t1.c_int = t2.c_int or t1.c_int = t2.c_int and t1.c_str > t2.c_str)")
+}
