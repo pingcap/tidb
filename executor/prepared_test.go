ad8fe77ceb06e585946258a76be8764f7727eeb4
ichn-hu <zfhu16@gmail.com>
2020-09-03 18:57:01 +0800

    use skip test

--- executor/prepared_test.go
+++ executor/prepared_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/pingcap/tidb/domain"
 	plannercore "github.com/pingcap/tidb/planner/core"
 	"github.com/pingcap/tidb/util"
+	"github.com/pingcap/tidb/util/israce"
 	"github.com/pingcap/tidb/util/testkit"
 	"github.com/pingcap/tidb/util/testleak"
 )
@@ -157,37 +158,40 @@ func (s *testSuite9) TestPlanCacheOnPointGet(c *C) {
 	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
 
 	// NOTE: the following test case causes data race in release-4.0, in order to surpass the CI check for later PRs
-	//       it is temporarily commented out, we will add it back once the race condition is resolved
+	//       it is temporarily skipped when race detection is enabled, we will add it back once the race condition is resolved
+	if israce.RaceEnabled {
+		c.Skip("skip race test")
+	}
 
 	// case 2:
-	//tk.MustExec(`drop table if exists ta, tb`)
-	//tk.MustExec(`create table ta (a varchar(10) primary key, b int not null)`)
-	//tk.MustExec(`insert ta values ('a', 1), ('b', 2)`)
-	//tk.MustExec(`create table tb (b int primary key, c int)`)
-	//tk.MustExec(`insert tb values (1, 1), (2, 2)`)
-	//tk.MustExec(`prepare stmt1 from "select * from ta, tb where ta.b = tb.b and ta.a = ?"`)
-	//tk.MustExec(`set @v1 = 'a', @v2 = 'b'`)
-	//tk.MustQuery(`execute stmt1 using @v1`).Check(testkit.Rows("a 1 1 1"))
-	//tk.MustQuery(`execute stmt1 using @v2`).Check(testkit.Rows("b 2 2 2"))
-	//tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
-	//tk.MustQuery(`execute stmt1 using @v2`).Check(testkit.Rows("b 2 2 2"))
-	//tkProcess = tk.Se.ShowProcess()
-	//ps = []*util.ProcessInfo{tkProcess}
-	//tk.Se.SetSessionManager(&mockSessionManager1{PS: ps})
-	//rows = tk.MustQuery(fmt.Sprintf("explain for connection %d", tkProcess.ID)).Rows()
-	//c.Assert(strings.Index(rows[1][0].(string), `Point_Get`), Equals, 6)
+	tk.MustExec(`drop table if exists ta, tb`)
+	tk.MustExec(`create table ta (a varchar(10) primary key, b int not null)`)
+	tk.MustExec(`insert ta values ('a', 1), ('b', 2)`)
+	tk.MustExec(`create table tb (b int primary key, c int)`)
+	tk.MustExec(`insert tb values (1, 1), (2, 2)`)
+	tk.MustExec(`prepare stmt1 from "select * from ta, tb where ta.b = tb.b and ta.a = ?"`)
+	tk.MustExec(`set @v1 = 'a', @v2 = 'b'`)
+	tk.MustQuery(`execute stmt1 using @v1`).Check(testkit.Rows("a 1 1 1"))
+	tk.MustQuery(`execute stmt1 using @v2`).Check(testkit.Rows("b 2 2 2"))
+	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
+	tk.MustQuery(`execute stmt1 using @v2`).Check(testkit.Rows("b 2 2 2"))
+	tkProcess = tk.Se.ShowProcess()
+	ps = []*util.ProcessInfo{tkProcess}
+	tk.Se.SetSessionManager(&mockSessionManager1{PS: ps})
+	rows = tk.MustQuery(fmt.Sprintf("explain for connection %d", tkProcess.ID)).Rows()
+	c.Assert(strings.Index(rows[1][0].(string), `Point_Get`), Equals, 6)
 
 	// case 3:
-	//tk.MustExec(`drop table if exists ta, tb`)
-	//tk.MustExec(`create table ta (a varchar(10), b varchar(10), c int, primary key (a, b))`)
-	//tk.MustExec(`insert ta values ('a', 'a', 1), ('b', 'b', 2), ('c', 'c', 3)`)
-	//tk.MustExec(`create table tb (b int primary key, c int)`)
-	//tk.MustExec(`insert tb values (1, 1), (2, 2), (3,3)`)
-	//tk.MustExec(`prepare stmt1 from "select * from ta, tb where ta.c = tb.b and ta.a = ? and ta.b = ?"`)
-	//tk.MustExec(`set @v1 = 'a', @v2 = 'b', @v3 = 'c'`)
-	//tk.MustQuery(`execute stmt1 using @v1, @v1`).Check(testkit.Rows("a a 1 1 1"))
-	//tk.MustQuery(`execute stmt1 using @v2, @v2`).Check(testkit.Rows("b b 2 2 2"))
-	//tk.MustExec(`prepare stmt2 from "select * from ta, tb where ta.c = tb.b and (ta.a, ta.b) in ((?, ?), (?, ?))"`)
-	//tk.MustQuery(`execute stmt2 using @v1, @v1, @v2, @v2`).Check(testkit.Rows("a a 1 1 1", "b b 2 2 2"))
-	//tk.MustQuery(`execute stmt2 using @v2, @v2, @v3, @v3`).Check(testkit.Rows("b b 2 2 2", "c c 3 3 3"))
+	tk.MustExec(`drop table if exists ta, tb`)
+	tk.MustExec(`create table ta (a varchar(10), b varchar(10), c int, primary key (a, b))`)
+	tk.MustExec(`insert ta values ('a', 'a', 1), ('b', 'b', 2), ('c', 'c', 3)`)
+	tk.MustExec(`create table tb (b int primary key, c int)`)
+	tk.MustExec(`insert tb values (1, 1), (2, 2), (3,3)`)
+	tk.MustExec(`prepare stmt1 from "select * from ta, tb where ta.c = tb.b and ta.a = ? and ta.b = ?"`)
+	tk.MustExec(`set @v1 = 'a', @v2 = 'b', @v3 = 'c'`)
+	tk.MustQuery(`execute stmt1 using @v1, @v1`).Check(testkit.Rows("a a 1 1 1"))
+	tk.MustQuery(`execute stmt1 using @v2, @v2`).Check(testkit.Rows("b b 2 2 2"))
+	tk.MustExec(`prepare stmt2 from "select * from ta, tb where ta.c = tb.b and (ta.a, ta.b) in ((?, ?), (?, ?))"`)
+	tk.MustQuery(`execute stmt2 using @v1, @v1, @v2, @v2`).Check(testkit.Rows("a a 1 1 1", "b b 2 2 2"))
+	tk.MustQuery(`execute stmt2 using @v2, @v2, @v3, @v3`).Check(testkit.Rows("b b 2 2 2", "c c 3 3 3"))
 }
