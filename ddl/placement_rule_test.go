bcd65724ab9055a9f9a6e11ccb299da5709186b0
xhe <xw897002528@gmail.com>
2020-09-04 14:54:05 +0800

    ddl: alter rules when truncating partitions (#19505)

--- ddl/placement_rule_test.go
+++ ddl/placement_rule_test.go
@@ -16,12 +16,20 @@ package ddl
 import (
 	. "github.com/pingcap/check"
 	"github.com/pingcap/parser/ast"
+	"github.com/pingcap/parser/model"
 	"github.com/pingcap/tidb/ddl/placement"
 )
 
 var _ = Suite(&testPlacementSuite{})
 
 type testPlacementSuite struct {
+}
+
+func (s *testPlacementSuite) compareRuleOp(n, o *placement.RuleOp) bool {
+	ok1, _ := DeepEquals.Check([]interface{}{n.Action, o.Action}, nil)
+	ok2, _ := DeepEquals.Check([]interface{}{n.DeleteByIDPrefix, o.DeleteByIDPrefix}, nil)
+	ok3, _ := DeepEquals.Check([]interface{}{n.Rule, o.Rule}, nil)
+	return ok1 && ok2 && ok3
 }
 
 func (s *testPlacementSuite) TestPlacementBuild(c *C) {
@@ -299,25 +307,119 @@ func (s *testPlacementSuite) TestPlacementBuild(c *C) {
 			for i := range t.output {
 				found := false
 				for j := range out {
-					ok1, _ := DeepEquals.Check([]interface{}{out[j].Action, t.output[i].Action}, nil)
-					ok2, _ := DeepEquals.Check([]interface{}{out[j].DeleteByIDPrefix, t.output[i].DeleteByIDPrefix}, nil)
-					ok3, _ := DeepEquals.Check([]interface{}{out[j].Rule, t.output[i].Rule}, nil)
-					if ok1 && ok2 && ok3 {
+					if s.compareRuleOp(out[j], t.output[i]) {
 						found = true
 						break
 					}
 				}
 				if !found {
 					c.Logf("test %d, %d-th output", k, i)
-					c.Logf("\texcept %+v - %+v\n\tbut got", t.output[i], t.output[i].Rule)
+					c.Logf("\texcept %+v\n\tbut got", t.output[i])
 					for j := range out {
-						c.Logf("\t%+v - %+v", out[j], out[j].Rule)
+						c.Logf("\t%+v", out[j])
 					}
 					c.Fail()
 				}
 			}
 		} else {
 			c.Assert(err.Error(), ErrorMatches, t.err)
+		}
+	}
+}
+
+func (s *testPlacementSuite) TestPlacementBuildDrop(c *C) {
+	tests := []struct {
+		input  []int64
+		output []*placement.RuleOp
+	}{
+		{
+			input: []int64{2},
+			output: []*placement.RuleOp{
+				{
+					Action:           placement.RuleOpDel,
+					DeleteByIDPrefix: true,
+					Rule: &placement.Rule{
+						GroupID: placement.RuleDefaultGroupID,
+						ID:      "0_t0_p2",
+					},
+				},
+			},
+		},
+		{
+			input: []int64{1, 2},
+			output: []*placement.RuleOp{
+				{
+					Action:           placement.RuleOpDel,
+					DeleteByIDPrefix: true,
+					Rule: &placement.Rule{
+						GroupID: placement.RuleDefaultGroupID,
+						ID:      "0_t0_p1",
+					},
+				},
+				{
+					Action:           placement.RuleOpDel,
+					DeleteByIDPrefix: true,
+					Rule: &placement.Rule{
+						GroupID: placement.RuleDefaultGroupID,
+						ID:      "0_t0_p2",
+					},
+				},
+			},
+		},
+	}
+	for _, t := range tests {
+		out := buildPlacementDropRules(0, 0, t.input)
+		c.Assert(len(out), Equals, len(t.output))
+		for i := range t.output {
+			c.Assert(s.compareRuleOp(out[i], t.output[i]), IsTrue, Commentf("except: %+v, obtained: %+v", t.output[i], out[i]))
+		}
+	}
+}
+
+func (s *testPlacementSuite) TestPlacementBuildTruncate(c *C) {
+	rules := []*placement.RuleOp{
+		{Rule: &placement.Rule{ID: "0_t0_p1"}},
+		{Rule: &placement.Rule{ID: "0_t0_p94"}},
+		{Rule: &placement.Rule{ID: "0_t0_p48"}},
+	}
+
+	tests := []struct {
+		input  []int64
+		output []*placement.RuleOp
+	}{
+		{
+			input: []int64{1},
+			output: []*placement.RuleOp{
+				{Action: placement.RuleOpDel, Rule: &placement.Rule{GroupID: placement.RuleDefaultGroupID, ID: "0_t0_p1"}},
+				{Action: placement.RuleOpAdd, Rule: &placement.Rule{ID: "0_t0_p2__0_0"}},
+			},
+		},
+		{
+			input: []int64{94, 48},
+			output: []*placement.RuleOp{
+				{Action: placement.RuleOpDel, Rule: &placement.Rule{GroupID: placement.RuleDefaultGroupID, ID: "0_t0_p94"}},
+				{Action: placement.RuleOpAdd, Rule: &placement.Rule{ID: "0_t0_p95__0_0"}},
+				{Action: placement.RuleOpDel, Rule: &placement.Rule{GroupID: placement.RuleDefaultGroupID, ID: "0_t0_p48"}},
+				{Action: placement.RuleOpAdd, Rule: &placement.Rule{ID: "0_t0_p49__0_1"}},
+			},
+		},
+	}
+	for _, t := range tests {
+		copyRules := make([]*placement.RuleOp, len(rules))
+		for _, rule := range rules {
+			copyRules = append(copyRules, rule.Clone())
+		}
+
+		newPartitions := make([]model.PartitionDefinition, 0, len(t.input))
+		for _, j := range t.input {
+			newPartitions = append(newPartitions, model.PartitionDefinition{ID: j + 1})
+		}
+
+		out := buildPlacementTruncateRules(rules, 0, 0, 0, t.input, newPartitions)
+
+		c.Assert(len(out), Equals, len(t.output))
+		for i := range t.output {
+			c.Assert(s.compareRuleOp(out[i], t.output[i]), IsTrue, Commentf("except: %+v, obtained: %+v", t.output[i], out[i]))
 		}
 	}
 }
