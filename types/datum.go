908e832d3e59c953e4895e793ccf864f5984227c
Patrick <dreamlike.sky@foxmail.com>
2020-09-05 12:42:47 +0800

    Revert "throw ErrTrunctedData error"
    This reverts commit f690a3e0eb3853df105ed812e3885b4f72906a8d.

--- types/datum.go
+++ types/datum.go
@@ -1397,9 +1397,10 @@ func (d *Datum) convertToMysqlEnum(sc *stmtctx.StatementContext, target *FieldTy
 	default:
 		var uintDatum Datum
 		uintDatum, err = d.convertToUint(sc, target)
-		if err == nil {
-			e, err = ParseEnumValue(target.Elems, uintDatum.GetUint64())
+		if err != nil {
+			return ret, errors.Trace(err)
 		}
+		e, err = ParseEnumValue(target.Elems, uintDatum.GetUint64())
 	}
 	if err != nil {
 		err = errors.Wrap(ErrTruncated, "convert to MySQL enum failed: "+err.Error())
@@ -1420,9 +1421,10 @@ func (d *Datum) convertToMysqlSet(sc *stmtctx.StatementContext, target *FieldTyp
 	default:
 		var uintDatum Datum
 		uintDatum, err = d.convertToUint(sc, target)
-		if err == nil {
-			s, err = ParseSetValue(target.Elems, uintDatum.GetUint64())
+		if err != nil {
+			return ret, errors.Trace(err)
 		}
+		s, err = ParseSetValue(target.Elems, uintDatum.GetUint64())
 	}
 	if err != nil {
 		err = errors.Wrap(ErrTruncated, "convert to MySQL set failed: "+err.Error())
