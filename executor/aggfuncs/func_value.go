1345a16ad2a8b31f365a488270fdb115d99a9210
hetianfeng <hetianfeng@zhihu.com>
2020-09-05 11:00:25 +0800

    format executor/aggfuncs/func_value.go

--- executor/aggfuncs/func_value.go
+++ executor/aggfuncs/func_value.go
@@ -14,13 +14,14 @@
 package aggfuncs
 
 import (
+	"unsafe"
+
 	"github.com/pingcap/parser/mysql"
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/types/json"
 	"github.com/pingcap/tidb/util/chunk"
-	"unsafe"
 )
 
 const (
