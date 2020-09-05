074bd4b250c84d18a8c121e0cfc433c7da5fe8de
Null not nil <nullnotnil1@gmail.com>
2020-09-05 16:05:37 -0600

    sessionctx, executor: tidb_dml_batch_size global

--- sessionctx/variable/sysvar.go
+++ sessionctx/variable/sysvar.go
@@ -644,7 +644,7 @@ var defaultSysVars = []*SysVar{
 	{ScopeSession, TiDBBatchInsert, BoolToIntStr(DefBatchInsert)},
 	{ScopeSession, TiDBBatchDelete, BoolToIntStr(DefBatchDelete)},
 	{ScopeSession, TiDBBatchCommit, BoolToIntStr(DefBatchCommit)},
-	{ScopeSession, TiDBDMLBatchSize, strconv.Itoa(DefDMLBatchSize)},
+	{ScopeGlobal | ScopeSession, TiDBDMLBatchSize, strconv.Itoa(DefDMLBatchSize)},
 	{ScopeSession, TiDBCurrentTS, strconv.Itoa(DefCurretTS)},
 	{ScopeGlobal | ScopeSession, TiDBMaxChunkSize, strconv.Itoa(DefMaxChunkSize)},
 	{ScopeGlobal | ScopeSession, TiDBAllowBatchCop, strconv.Itoa(DefTiDBAllowBatchCop)},
