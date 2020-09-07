d5defa04d9c8ba981fafdfcff92d1889f2187b6a
Zhuomin(Charming) Liu <lzmhhh123@gmail.com>
2020-09-07 13:23:39 +0800

    cherry pick #19622 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- infoschema/tables_test.go
+++ infoschema/tables_test.go
@@ -302,6 +302,7 @@ func (s *testTableSuite) TestInfoschemaFieldValue(c *C) {
 			"  `TIME` int(7) NOT NULL DEFAULT 0,\n" +
 			"  `STATE` varchar(7) DEFAULT NULL,\n" +
 			"  `INFO` longtext DEFAULT NULL,\n" +
+			"  `DIGEST` varchar(64) DEFAULT '',\n" +
 			"  `MEM` bigint(21) unsigned DEFAULT NULL,\n" +
 			"  `TxnStart` varchar(64) NOT NULL DEFAULT ''\n" +
 			") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin"))
@@ -457,6 +458,7 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 		Host:    "localhost",
 		DB:      "information_schema",
 		Command: byte(1),
+		Digest:  "abc1",
 		State:   1,
 		Info:    "do something",
 		StmtCtx: tk.Se.GetSessionVars().StmtCtx,
@@ -467,6 +469,7 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 		Host:    "localhost",
 		DB:      "test",
 		Command: byte(2),
+		Digest:  "abc2",
 		State:   2,
 		Info:    strings.Repeat("x", 101),
 		StmtCtx: tk.Se.GetSessionVars().StmtCtx,
@@ -474,8 +477,13 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 	tk.Se.SetSessionManager(sm)
 	tk.MustQuery("select * from information_schema.PROCESSLIST order by ID;").Sort().Check(
 		testkit.Rows(
+<<<<<<< HEAD
 			fmt.Sprintf("1 user-1 localhost information_schema Quit 9223372036 1 %s 0 ", "do something"),
 			fmt.Sprintf("2 user-2 localhost test Init DB 9223372036 2 %s 0 ", strings.Repeat("x", 101)),
+=======
+			fmt.Sprintf("1 user-1 localhost information_schema Quit 9223372036 %s %s abc1 0 ", "in transaction", "do something"),
+			fmt.Sprintf("2 user-2 localhost test Init DB 9223372036 %s %s abc2 0 ", "autocommit", strings.Repeat("x", 101)),
+>>>>>>> 4f9b350... infoschema: add digest for process list (#19622)
 		))
 	tk.MustQuery("SHOW PROCESSLIST;").Sort().Check(
 		testkit.Rows(
@@ -495,6 +503,7 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 		Host:    "localhost",
 		DB:      "information_schema",
 		Command: byte(1),
+		Digest:  "abc1",
 		State:   1,
 		StmtCtx: tk.Se.GetSessionVars().StmtCtx,
 	}
@@ -503,6 +512,7 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 		User:          "user-2",
 		Host:          "localhost",
 		Command:       byte(2),
+		Digest:        "abc2",
 		State:         2,
 		Info:          strings.Repeat("x", 101),
 		StmtCtx:       tk.Se.GetSessionVars().StmtCtx,
@@ -512,8 +522,13 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 	tk.Se.GetSessionVars().TimeZone = time.UTC
 	tk.MustQuery("select * from information_schema.PROCESSLIST order by ID;").Check(
 		testkit.Rows(
+<<<<<<< HEAD
 			fmt.Sprintf("1 user-1 localhost information_schema Quit 9223372036 1 %s 0 ", "<nil>"),
 			fmt.Sprintf("2 user-2 localhost <nil> Init DB 9223372036 2 %s 0 07-29 03:26:05.158(410090409861578752)", strings.Repeat("x", 101)),
+=======
+			fmt.Sprintf("1 user-1 localhost information_schema Quit 9223372036 %s %s abc1 0 ", "in transaction", "<nil>"),
+			fmt.Sprintf("2 user-2 localhost <nil> Init DB 9223372036 %s %s abc2 0 07-29 03:26:05.158(410090409861578752)", "autocommit", strings.Repeat("x", 101)),
+>>>>>>> 4f9b350... infoschema: add digest for process list (#19622)
 		))
 	tk.MustQuery("SHOW PROCESSLIST;").Sort().Check(
 		testkit.Rows(
@@ -527,11 +542,19 @@ func (s *testTableSuite) TestSomeTables(c *C) {
 		))
 	tk.MustQuery("select * from information_schema.PROCESSLIST where db is null;").Check(
 		testkit.Rows(
+<<<<<<< HEAD
 			fmt.Sprintf("2 user-2 localhost <nil> Init DB 9223372036 2 %s 0 07-29 03:26:05.158(410090409861578752)", strings.Repeat("x", 101)),
 		))
 	tk.MustQuery("select * from information_schema.PROCESSLIST where Info is null;").Check(
 		testkit.Rows(
 			fmt.Sprintf("1 user-1 localhost information_schema Quit 9223372036 1 %s 0 ", "<nil>"),
+=======
+			fmt.Sprintf("2 user-2 localhost <nil> Init DB 9223372036 %s %s abc2 0 07-29 03:26:05.158(410090409861578752)", "autocommit", strings.Repeat("x", 101)),
+		))
+	tk.MustQuery("select * from information_schema.PROCESSLIST where Info is null;").Check(
+		testkit.Rows(
+			fmt.Sprintf("1 user-1 localhost information_schema Quit 9223372036 %s %s abc1 0 ", "in transaction", "<nil>"),
+>>>>>>> 4f9b350... infoschema: add digest for process list (#19622)
 		))
 }
 
@@ -789,7 +812,11 @@ func (s *testClusterTableSuite) TestSelectClusterTable(c *C) {
 		tk.MustQuery("select count(*) from `CLUSTER_SLOW_QUERY`").Check(testkit.Rows("1"))
 		tk.MustQuery("select count(*) from `CLUSTER_SLOW_QUERY` where time='2019-02-12 19:33:56.571953'").Check(testkit.Rows("1"))
 		tk.MustQuery("select count(*) from `CLUSTER_PROCESSLIST`").Check(testkit.Rows("1"))
+<<<<<<< HEAD
 		tk.MustQuery("select * from `CLUSTER_PROCESSLIST`").Check(testkit.Rows(":10080 1 root 127.0.0.1 <nil> Query 9223372036 0 <nil> 0 "))
+=======
+		tk.MustQuery("select * from `CLUSTER_PROCESSLIST`").Check(testkit.Rows(fmt.Sprintf(":10080 1 root 127.0.0.1 <nil> Query 9223372036 %s <nil>  0 ", "")))
+>>>>>>> 4f9b350... infoschema: add digest for process list (#19622)
 		tk.MustQuery("select query_time, conn_id from `CLUSTER_SLOW_QUERY` order by time limit 1").Check(testkit.Rows("4.895492 6"))
 		tk.MustQuery("select count(*) from `CLUSTER_SLOW_QUERY` group by digest").Check(testkit.Rows("1"))
 		tk.MustQuery("select digest, count(*) from `CLUSTER_SLOW_QUERY` group by digest").Check(testkit.Rows("42a1c8aae6f133e934d4bf0147491709a8812ea05ff8819ec522780fe657b772 1"))
@@ -839,7 +866,11 @@ select * from t3;
 	tk.MustQuery("select count(*) from `CLUSTER_SLOW_QUERY`").Check(testkit.Rows("4"))
 	tk.MustQuery("select count(*) from `SLOW_QUERY`").Check(testkit.Rows("4"))
 	tk.MustQuery("select count(*) from `CLUSTER_PROCESSLIST`").Check(testkit.Rows("1"))
+<<<<<<< HEAD
 	tk.MustQuery("select * from `CLUSTER_PROCESSLIST`").Check(testkit.Rows(":10080 1 root 127.0.0.1 <nil> Query 9223372036 0 <nil> 0 "))
+=======
+	tk.MustQuery("select * from `CLUSTER_PROCESSLIST`").Check(testkit.Rows(fmt.Sprintf(":10080 1 root 127.0.0.1 <nil> Query 9223372036 %s <nil>  0 ", "")))
+>>>>>>> 4f9b350... infoschema: add digest for process list (#19622)
 	tk.MustExec("create user user1")
 	tk.MustExec("create user user2")
 	user1 := testkit.NewTestKit(c, s.store)
