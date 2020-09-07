7c3edca2191a6e84417e279c780bacde5954400c
Arenatlx <ailinsilence4@gmail.com>
2020-09-07 17:11:02 +0800

    Update ddl/db_test.go
    Co-authored-by: djshow832 <zhangming@pingcap.com>

--- ddl/db_test.go
+++ ddl/db_test.go
@@ -762,7 +762,7 @@ func (s *testDBSuite5) TestParallelDropSchemaAndDropTable(c *C) {
 	wg.Wait()
 	c.Assert(done, IsTrue)
 	c.Assert(checkErr, NotNil)
-	// There are two possible assert result is because that:
+	// There are two possible assert result because:
 	// 1: If drop-database is finished before drop-table being put into the ddl job queue, it will return unknown table error directly in previous check .
 	// 2: If drop-table has passed the previous check and been put into the ddl job queue, then drop-database finished, it will return schema change error.
 	assertRes := checkErr.Error() == "[domain:8028]Information schema is changed during the execution of the"+
