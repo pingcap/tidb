ffa459fd3cf3f25631824669d83101ecd98799ff
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-09-08 15:00:42 +0800

    fix CI

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -6820,27 +6820,6 @@ func (s *testIntegrationSuite) TestIssue18850(c *C) {
 	tk.MustQuery("select /*+ HASH_JOIN(t, t1) */ * from t join t1 on t.b = t1.b1;").Check(testkit.Rows("1 A 1 A"))
 }
 
-<<<<<<< HEAD
-=======
-func (s *testIntegrationSerialSuite) TestNullValueRange(c *C) {
-	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t")
-	tk.MustExec("create table t(a int, b int, index(a))")
-	tk.MustExec("insert into t values (null, 0), (null, 1), (10, 11), (10, 12)")
-	tk.MustQuery("select * from t use index(a) where a is null order by b").Check(testkit.Rows("<nil> 0", "<nil> 1"))
-	tk.MustQuery("select * from t use index(a) where a<=>null order by b").Check(testkit.Rows("<nil> 0", "<nil> 1"))
-	tk.MustQuery("select * from t use index(a) where a<=>10 order by b").Check(testkit.Rows("10 11", "10 12"))
-
-	tk.MustExec("drop table if exists t1")
-	tk.MustExec("create table t1(a int, b int, c int, unique key(a, b, c))")
-	tk.MustExec("insert into t1 values (1, null, 1), (1, null, 2), (1, null, 3), (1, null, 4)")
-	tk.MustExec("insert into t1 values (1, 1, 1), (1, 2, 2), (1, 3, 33), (1, 4, 44)")
-	tk.MustQuery("select c from t1 where a=1 and b<=>null and c>2 order by c").Check(testkit.Rows("3", "4"))
-	tk.MustQuery("select c from t1 where a=1 and b is null and c>2 order by c").Check(testkit.Rows("3", "4"))
-	tk.MustQuery("select c from t1 where a=1 and b is not null and c>2 order by c").Check(testkit.Rows("33", "44"))
-}
-
 func (s *testIntegrationSerialSuite) TestIssue18652(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
@@ -6850,7 +6829,6 @@ func (s *testIntegrationSerialSuite) TestIssue18652(c *C) {
 	tk.MustQuery("SELECT * FROM t1 WHERE ( LOG( `col_smallint_key_signed`, -8297584758403770424 ) ) DIV 1").Check(testkit.Rows())
 }
 
->>>>>>> 63998c1... expression: fix NaN for vec expression (#19482)
 func (s *testIntegrationSerialSuite) TestIssue18662(c *C) {
 	collate.SetNewCollationEnabledForTest(true)
 	defer collate.SetNewCollationEnabledForTest(false)
