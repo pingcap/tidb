da8670d4de9d3c555732e616a5a4c51bdb009764
dongyan <dybest@icloud.com>
2020-09-10 11:55:19 +0800

    add benchmark & test

--- executor/aggfuncs/func_group_concat_test.go
+++ executor/aggfuncs/func_group_concat_test.go
@@ -15,6 +15,7 @@ package aggfuncs_test
 
 import (
 	"fmt"
+	"testing"
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/parser/ast"
@@ -43,5 +44,18 @@ func (s *testSuite) TestGroupConcat(c *C) {
 		test2 = buildMultiArgsAggTester(ast.AggFuncGroupConcat, []byte{mysql.TypeString, mysql.TypeString}, mysql.TypeString, 5, nil, "44 33 22 11 00"[:i])
 		test2.orderBy = true
 		s.testMultiArgsAggFunc(c, test2)
+	}
+}
+
+func BenchmarkGroupConcat(b *testing.B) {
+	s := testSuite{}
+	s.SetUpSuite(nil)
+
+	rowNum := 50000
+	tests := []aggTest{
+		buildAggTester(ast.AggFuncGroupConcat, mysql.TypeString, rowNum, nil, 2.0),
+	}
+	for _, test := range tests {
+		s.benchmarkAggFunc(b, test)
 	}
 }
