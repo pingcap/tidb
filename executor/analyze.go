8e82aec5fce65e26560b515a9d571517981a8a8c
rebelice <yangrebelice@gmail.com>
2020-09-11 15:19:59 +0800

    handle conflict

--- executor/analyze.go
+++ executor/analyze.go
@@ -337,14 +337,10 @@ func (e *AnalyzeIndexExec) buildStatsFromResult(result distsql.SelectResult, nee
 			}
 		}
 	}
-<<<<<<< HEAD
 	err := hist.ExtractTopN(cms, len(e.idxInfo.Columns), defaultNumTopN)
-=======
-	err := hist.ExtractTopN(cms, len(e.idxInfo.Columns), uint32(e.opts[ast.AnalyzeOptNumTopN]))
 	if needCMS && cms != nil {
 		cms.CalcDefaultValForAnalyze(uint64(hist.NDV))
 	}
->>>>>>> 0859e75... statistics: add default value of CMSketch for `Analyze` (#19455)
 	return hist, cms, err
 }
 
