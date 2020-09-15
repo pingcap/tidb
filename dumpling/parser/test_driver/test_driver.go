12bf269a888692d06a3ba874a901cf9ff2155a74
wangggong <793160615@qq.com>
2020-09-15 15:55:43 +0800

    [dumpling] [parser] fix: add "UNDERSCORE_CHARSET hexLit/bitLit" in Literal (#926)
    Signed-off-by: wangggong <793160615@qq.com>
    
    Co-authored-by: Wang Ruichao <wangruichao2014@xiaochuankeji.cn>

--- dumpling/parser/test_driver/test_driver.go
+++ dumpling/parser/test_driver/test_driver.go
@@ -21,6 +21,7 @@ import (
 	"strconv"
 
 	"github.com/pingcap/parser/ast"
+	"github.com/pingcap/parser/charset"
 	"github.com/pingcap/parser/format"
 	"github.com/pingcap/parser/mysql"
 )
@@ -87,6 +88,11 @@ func (n *ValueExpr) Restore(ctx *format.RestoreCtx) error {
 	case KindMysqlDecimal:
 		ctx.WritePlain(n.GetMysqlDecimal().String())
 	case KindBinaryLiteral:
+		if n.Type.Charset != "" && n.Type.Charset != mysql.DefaultCharset &&
+			n.Type.Charset != charset.CharsetBin {
+			ctx.WritePlain("_")
+			ctx.WriteKeyWord(n.Type.Charset + " ")
+		}
 		if n.Type.Flag&mysql.UnsignedFlag != 0 {
 			ctx.WritePlainf("x'%x'", n.GetBytes())
 		} else {
