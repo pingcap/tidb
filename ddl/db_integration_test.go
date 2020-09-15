1590b404c54af86cd69ac2912cb72fb98a2c05a5
xuhuaiyu <391585975@qq.com>
2020-09-15 14:38:52 +0800

    fix ci

--- ddl/db_integration_test.go
+++ ddl/db_integration_test.go
@@ -288,19 +288,19 @@ func (s *testIntegrationSuite2) TestIssue19229(c *C) {
 	tk.MustExec("CREATE TABLE enumt (type enum('a', 'b') );")
 	_, err := tk.Exec("insert into enumt values('xxx');")
 	terr := errors.Cause(err).(*terror.Error)
-	c.Assert(terr.Code(), Equals, errors.ErrCode(errno.WarnDataTruncated))
+	c.Assert(terr.Equal(types.ErrTruncated), IsTrue)
 	_, err = tk.Exec("insert into enumt values(-1);")
 	terr = errors.Cause(err).(*terror.Error)
-	c.Assert(terr.Code(), Equals, errors.ErrCode(errno.WarnDataTruncated))
+	c.Assert(terr.Equal(types.ErrTruncated), IsTrue)
 	tk.MustExec("drop table enumt")
 
 	tk.MustExec("CREATE TABLE sett (type set('a', 'b') );")
 	_, err = tk.Exec("insert into sett values('xxx');")
 	terr = errors.Cause(err).(*terror.Error)
-	c.Assert(terr.Code(), Equals, errors.ErrCode(errno.WarnDataTruncated))
+	c.Assert(terr.Equal(types.ErrTruncated), IsTrue)
 	_, err = tk.Exec("insert into sett values(-1);")
 	terr = errors.Cause(err).(*terror.Error)
-	c.Assert(terr.Code(), Equals, errors.ErrCode(errno.WarnDataTruncated))
+	c.Assert(terr.Equal(types.ErrTruncated), IsTrue)
 	tk.MustExec("drop table sett")
 }
 
