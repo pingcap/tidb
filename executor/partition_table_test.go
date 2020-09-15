9642c6982d83c28fe223f6fbd34afe812af2ce22
tiancaiamao <tiancaiamao@gmail.com>
2020-09-15 16:24:02 +0800

    executor: improve test coverage for partition table code

--- executor/partition_table_test.go
+++ executor/partition_table_test.go
@@ -160,3 +160,18 @@ func (s *partitionTableSuite) TestPartitionReaderUnderApply(c *C) {
 		"4 hungry wilson 4.900000",
 		"5 naughty swartz 9.524000"))
 }
+
+func (s *partitionTableSuite) TestImproveCoverage(c *C) {
+	tk := testkit.NewTestKitWithInit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec(`create table coverage_rr (
+pk1 varchar(35) NOT NULL,
+pk2 int NOT NULL,
+c int,
+PRIMARY KEY (pk1,pk2)) partition by hash(pk2) partitions 4;`)
+	tk.MustExec("create table coverage_dt (pk1 varchar(35), pk2 int)")
+	tk.MustExec("insert into coverage_rr values ('ios', 3, 2),('android', 4, 7),('linux',5,1)")
+	tk.MustExec("insert into coverage_dt values ('apple',3),('ios',3),('linux',5)")
+	tk.MustQuery("select /*+ INL_JOIN(dt, rr) */ * from coverage_dt dt join coverage_rr rr on (dt.pk1 = rr.pk1 and dt.pk2 = rr.pk2);").Check(testkit.Rows("ios 3 ios 3 2", "linux 5 linux 5 1"))
+	tk.MustQuery("select /*+ INL_MERGE_JOIN(dt, rr) */ * from coverage_dt dt join coverage_rr rr on (dt.pk1 = rr.pk1 and dt.pk2 = rr.pk2);").Check(testkit.Rows("ios 3 ios 3 2", "linux 5 linux 5 1"))
+}
