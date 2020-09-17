c9bcdf81f32c579d1e8834e509402c1508c86912
MyonKeminta <MyonKeminta@users.noreply.github.com>
2020-09-17 13:34:43 +0800

    Rename and add tests
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- config/config.go
+++ config/config.go
@@ -491,8 +491,8 @@ type TiKVClient struct {
 	// CommitTimeout is the max time which command 'commit' will wait.
 	CommitTimeout string `toml:"commit-timeout" json:"commit-timeout"`
 	// EnableAsyncCommit enables async commit for all transactions.
-	EnableAsyncCommit       bool `toml:"enable-async-commit" json:"enable-async-commit"`
-	AsyncCommitTxnSizeLimit uint `toml:"async-commit-txn-size-limit" json:"async-commit-txn-size-limit"`
+	EnableAsyncCommit    bool `toml:"enable-async-commit" json:"enable-async-commit"`
+	AsyncCommitKeysLimit uint `toml:"async-commit-keys-limit" json:"async-commit-keys-limit"`
 
 	// MaxBatchSize is the max batch size when calling batch commands API.
 	MaxBatchSize uint `toml:"max-batch-size" json:"max-batch-size"`
@@ -681,12 +681,12 @@ var defaultConf = Config{
 		Reporter: OpenTracingReporter{},
 	},
 	TiKVClient: TiKVClient{
-		GrpcConnectionCount:     4,
-		GrpcKeepAliveTime:       10,
-		GrpcKeepAliveTimeout:    3,
-		CommitTimeout:           "41s",
-		EnableAsyncCommit:       false,
-		AsyncCommitTxnSizeLimit: 256,
+		GrpcConnectionCount:  4,
+		GrpcKeepAliveTime:    10,
+		GrpcKeepAliveTimeout: 3,
+		CommitTimeout:        "41s",
+		EnableAsyncCommit:    false,
+		AsyncCommitKeysLimit: 256,
 
 		MaxBatchSize:      128,
 		OverloadThreshold: 200,
