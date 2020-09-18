eaa47ca741334f811ed054cebf1b4faef7b8ddf9
xhe <xw897002528@gmail.com>
2020-09-18 11:39:43 +0800

    ddl: fix test
    Signed-off-by: xhe <xw897002528@gmail.com>

--- domain/infosync/info.go
+++ domain/infosync/info.go
@@ -277,6 +277,10 @@ func doRequest(ctx context.Context, addrs []string, route, method string, body i
 		var url string
 		if strings.HasPrefix(addr, "http://") {
 			url = fmt.Sprintf("%s%s", addr, route)
+		} else if len(addr) > 0 && (addr[0] < '0' || addr[0] > '9') {
+			// expect an HTTP addr or an IP addr
+			err = errors.New(`"placement rules feature is disabled"`)
+			continue
 		} else {
 			url = fmt.Sprintf("http://%s%s", addr, route)
 		}
