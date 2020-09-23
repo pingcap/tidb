28e8ced0f8280f8d63b730a1808a9df8792460a0
xiaodong-ji <xiaodong.ji@outlook.com>
2020-09-23 15:52:30 +0800

    cherry pick #18871 to release-3.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- types/datum.go
+++ types/datum.go
@@ -1266,7 +1266,7 @@ func (d *Datum) convertToMysqlYear(sc *stmtctx.StatementContext, target *FieldTy
 	}
 	y, err = AdjustYear(y, adjust)
 	if err != nil {
-		_, err = invalidConv(d, target.Tp)
+		err = ErrOverflow.GenWithStackByArgs("DECIMAL", fmt.Sprintf("(%d, %d)", target.Flen, target.Decimal))
 	}
 	ret.SetInt64(y)
 	return ret, err
