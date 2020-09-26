3763d2a7f0462f226ce32cad3f2bfea8dbaeb3bd
b41sh <baishen2009@gmail.com>
2020-09-26 12:12:09 +0800

    fix

--- planner/core/preprocess.go
+++ planner/core/preprocess.go
@@ -154,15 +154,15 @@ func (p *preprocessor) Enter(in ast.Node) (out ast.Node, skipChildren bool) {
 	case *ast.Join:
 		p.checkNonUniqTableAlias(node)
 	case *ast.CreateBindingStmt:
-		EraseLastSemicolon(node.OriginSel)
-		EraseLastSemicolon(node.HintedSel)
-		p.checkBindGrammar(node.OriginSel, node.HintedSel)
+		EraseLastSemicolon(node.OriginNode)
+		EraseLastSemicolon(node.HintedNode)
+		p.checkBindGrammar(node.OriginNode, node.HintedNode)
 		return in, true
 	case *ast.DropBindingStmt:
-		EraseLastSemicolon(node.OriginSel)
-		if node.HintedSel != nil {
-			EraseLastSemicolon(node.HintedSel)
-			p.checkBindGrammar(node.OriginSel, node.HintedSel)
+		EraseLastSemicolon(node.OriginNode)
+		if node.HintedNode != nil {
+			EraseLastSemicolon(node.HintedNode)
+			p.checkBindGrammar(node.OriginNode, node.HintedNode)
 		}
 		return in, true
 	case *ast.RecoverTableStmt, *ast.FlashBackTableStmt:
