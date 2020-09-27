a67084d1be74addcc2c06847e78b75a109227757
欧冰 <bing.ou@outlook.com>
2020-09-27 10:24:03 +0800

    add method: DropIndexes

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -2194,6 +2194,8 @@ func (d *ddl) AlterTable(ctx sessionctx.Context, ident ast.Ident, specs []*ast.A
 				err = d.AddColumns(ctx, ident, validSpecs)
 			case ast.AlterTableDropColumn:
 				err = d.DropColumns(ctx, ident, validSpecs)
+			case ast.AlterTableDropIndexes:
+				err = d.DropIndexes(ctx, ident, validSpecs)
 			default:
 				return errRunMultiSchemaChanges
 			}
@@ -2236,6 +2238,7 @@ func (d *ddl) AlterTable(ctx sessionctx.Context, ident ast.Ident, specs []*ast.A
 			err = d.DropIndex(ctx, ident, model.NewCIStr(spec.Name), spec.IfExists)
 		case ast.AlterTableDropPrimaryKey:
 			err = d.DropIndex(ctx, ident, model.NewCIStr(mysql.PrimaryKeyName), spec.IfExists)
+		
 		case ast.AlterTableRenameIndex:
 			err = d.RenameIndex(ctx, ident, spec)
 		case ast.AlterTableDropPartition:
@@ -4771,6 +4774,93 @@ func (d *ddl) DropIndex(ctx sessionctx.Context, ti ast.Ident, indexName model.CI
 	err = d.doDDLJob(ctx, job)
 	// index not exists, but if_exists flags is true, so we ignore this error.
 	if ErrCantDropFieldOrKey.Equal(err) && ifExists {
+		ctx.GetSessionVars().StmtCtx.AppendNote(err)
+		return nil
+	}
+	err = d.callHookOnChanged(err)
+	return errors.Trace(err)
+}
+
+func (d *ddl) DropIndexes(ctx sessionctx.Context, ti ast.Ident, specs []*ast.AlterTableSpec) error {
+	is := d.infoHandle.Get()
+	schema, ok := is.SchemaByName(ti.Schema)
+	if !ok {
+		return errors.Trace(infoschema.ErrDatabaseNotExists)
+	}
+	t, err := is.TableByName(ti.Schema, ti.Name)
+	if err != nil {
+		return errors.Trace(infoschema.ErrTableNotExists.GenWithStackByArgs(ti.Schema, ti.Name))
+	}
+
+	allIndexIsExists := true
+	args := make([]interface{}, len(specs), len(specs))
+	for _, spec := range specs {
+		var indexName model.CIStr
+		switch spec.Tp {
+		case ast.AlterTableDropIndex:
+			indexName = model.NewCIStr(spec.Name)
+		case ast.AlterTableDropPrimaryKey:
+			indexName = model.NewCIStr(mysql.PrimaryKeyName)
+		default:
+			return nil
+		}
+
+		indexInfo := t.Meta().FindIndexByName(indexName.L)
+		var isPK bool
+		if indexName.L == strings.ToLower(mysql.PrimaryKeyName) &&
+			// Before we fixed #14243, there might be a general index named `primary` but not a primary key.
+			(indexInfo == nil || indexInfo.Primary) {
+			isPK = true
+		}
+		if isPK {
+			if !config.GetGlobalConfig().AlterPrimaryKey {
+				return ErrUnsupportedModifyPrimaryKey.GenWithStack("Unsupported drop primary key when alter-primary-key is false")
+			}
+			// If the table's PKIsHandle is true, we can't find the index from the table. So we check the value of PKIsHandle.
+			if indexInfo == nil && !t.Meta().PKIsHandle {
+				return ErrCantDropFieldOrKey.GenWithStack("Can't DROP 'PRIMARY'; check that column/key exists")
+			}
+			if t.Meta().PKIsHandle {
+				return ErrUnsupportedModifyPrimaryKey.GenWithStack("Unsupported drop primary key when the table's pkIsHandle is true")
+			}
+			if t.Meta().IsCommonHandle {
+				return ErrUnsupportedModifyPrimaryKey.GenWithStack("Unsupported drop primary key when the table is using clustered index")
+			}
+		}
+		if indexInfo == nil {
+			err = ErrCantDropFieldOrKey.GenWithStack("index %s doesn't exist", indexName)
+			if spec.IfExists {
+				ctx.GetSessionVars().StmtCtx.AppendNote(err)
+				continue
+			}
+			return err
+		}
+
+		// Check for drop index on auto_increment column.
+		err = checkDropIndexOnAutoIncrementColumn(t.Meta(), indexInfo)
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		args = append(args, indexName)
+
+		if !spec.IfExists {
+			allIndexIsExists = false
+		}
+	}
+
+	job := &model.Job{
+		SchemaID:   schema.ID,
+		TableID:    t.Meta().ID,
+		SchemaName: schema.Name.L,
+		Type:       model.ActionDropIndexes,
+		BinlogInfo: &model.HistoryInfo{},
+		Args:       args,
+	}
+
+	err = d.doDDLJob(ctx, job)
+	// index not exists, but if_exists flags is true, so we ignore this error.
+	if ErrCantDropFieldOrKey.Equal(err) && allIndexIsExists {
 		ctx.GetSessionVars().StmtCtx.AppendNote(err)
 		return nil
 	}
