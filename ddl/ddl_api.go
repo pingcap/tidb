848920a768744d8668ba1c35ac0e81a81338638f
crazycs <crazycs520@gmail.com>
2020-09-30 14:04:33 +0800

    support add/drop list partition
    Signed-off-by: crazycs <crazycs520@gmail.com>

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -2805,7 +2805,12 @@ func (d *ddl) AddTablePartitions(ctx sessionctx.Context, ident ast.Ident, spec *
 	tmp := *partInfo
 	tmp.Definitions = append(pi.Definitions, tmp.Definitions...)
 	meta.Partition = &tmp
-	err = checkPartitionByRange(ctx, meta, nil)
+	switch pi.Type {
+	case model.PartitionTypeRange:
+		err = checkPartitionByRange(ctx, meta, nil)
+	case model.PartitionTypeList:
+		err = checkPartitionByList(ctx, meta, nil)
+	}
 	meta.Partition = pi
 	if err != nil {
 		if ErrSameNamePartition.Equal(err) && spec.IfNotExists {
@@ -4924,11 +4929,12 @@ func validateCommentLength(vars *variable.SessionVars, indexName string, indexOp
 }
 
 func buildPartitionInfo(ctx sessionctx.Context, meta *model.TableInfo, d *ddl, spec *ast.AlterTableSpec) (*model.PartitionInfo, error) {
-	if meta.Partition.Type == model.PartitionTypeRange {
+	switch meta.Partition.Type {
+	case model.PartitionTypeRange, model.PartitionTypeList:
 		if len(spec.PartDefinitions) == 0 {
 			return nil, ast.ErrPartitionsMustBeDefined.GenWithStackByArgs(meta.Partition.Type)
 		}
-	} else {
+	default:
 		// we don't support ADD PARTITION for all other partition types yet.
 		return nil, errors.Trace(ErrUnsupportedAddPartition)
 	}
@@ -4951,13 +4957,6 @@ func buildPartitionInfo(ctx sessionctx.Context, meta *model.TableInfo, d *ddl, s
 		if err := checkTooLongTable(def.Name); err != nil {
 			return nil, err
 		}
-		// For RANGE partition only VALUES LESS THAN should be possible.
-		clause := def.Clause.(*ast.PartitionDefinitionClauseLessThan)
-		if len(part.Columns) > 0 {
-			if err := checkColumnsTypeAndValuesMatch(ctx, meta, clause.Exprs); err != nil {
-				return nil, err
-			}
-		}
 
 		comment, _ := def.Comment()
 		piDef := model.PartitionDefinition{
@@ -4965,16 +4964,58 @@ func buildPartitionInfo(ctx sessionctx.Context, meta *model.TableInfo, d *ddl, s
 			ID:      genIDs[ith],
 			Comment: comment,
 		}
-
-		buf := new(bytes.Buffer)
-		for _, expr := range clause.Exprs {
-			expr.Format(buf)
-			piDef.LessThan = append(piDef.LessThan, buf.String())
-			buf.Reset()
+		if meta.Partition.Type == model.PartitionTypeRange {
+			err = buildRangePartitionInfo(ctx, meta, part, def, &piDef)
+		} else {
+			err = buildListPartitionInfo(ctx, meta, part, def, &piDef)
+		}
+		if err != nil {
+			return nil, err
 		}
 		part.Definitions = append(part.Definitions, piDef)
 	}
 	return part, nil
+}
+
+func buildRangePartitionInfo(ctx sessionctx.Context, meta *model.TableInfo, part *model.PartitionInfo, def *ast.PartitionDefinition, piDef *model.PartitionDefinition) error {
+	// For RANGE partition only VALUES LESS THAN should be possible.
+	clause := def.Clause.(*ast.PartitionDefinitionClauseLessThan)
+	if len(part.Columns) > 0 {
+		if err := checkColumnsTypeAndValuesMatch(ctx, meta, clause.Exprs); err != nil {
+			return err
+		}
+	}
+	buf := new(bytes.Buffer)
+	for _, expr := range clause.Exprs {
+		expr.Format(buf)
+		piDef.LessThan = append(piDef.LessThan, buf.String())
+		buf.Reset()
+	}
+	return nil
+}
+
+func buildListPartitionInfo(ctx sessionctx.Context, meta *model.TableInfo, part *model.PartitionInfo, def *ast.PartitionDefinition, piDef *model.PartitionDefinition) error {
+	// For List partition only VALUES IN should be possible.
+	clause := def.Clause.(*ast.PartitionDefinitionClauseIn)
+	if len(part.Columns) > 0 {
+		for _, vs := range clause.Values {
+			if err := checkColumnsTypeAndValuesMatch(ctx, meta, vs); err != nil {
+				return err
+			}
+		}
+	}
+	buf := new(bytes.Buffer)
+	for _, vs := range clause.Values {
+		inValue := make([]string, 0, len(vs))
+		for i := range vs {
+			buf.Reset()
+			vs[i].Format(buf)
+			inValue = append(inValue, buf.String())
+		}
+		piDef.InValues = append(piDef.InValues, inValue)
+		buf.Reset()
+	}
+	return nil
 }
 
 func checkColumnsTypeAndValuesMatch(ctx sessionctx.Context, meta *model.TableInfo, exprs []ast.ExprNode) error {
