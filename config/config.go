5fe40de69621cb1e16ef2f872636a5d9e355f312
miamiaoxyz <miamiaoxyz@163.com>
2020-10-10 15:13:45 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -52,7 +52,7 @@ const (
 	// DefMaxOfMaxIndexLength is the maximum index length(in bytes) for TiDB v3.0.7 and previous version.
 	DefMaxOfMaxIndexLength = 3072 * 4
 	// DefMinQuotaStatistics is the minimum statistic memory quota(in bytes).
-	DefMinQuotaStatistics = 1 << 30
+	DefMinQuotaStatistics = 32 << 30
 	// DefPort is the default port of TiDB
 	DefPort = 4000
 	// DefStatusPort is the default status port of TiDB
