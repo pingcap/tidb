02616aabfb2d1c50b8b9fecdb616ff05b25ae5b8
William0423 <5408919@qq.com>
2020-10-10 20:56:00 +0800

    executor: refactor TestMemJsonObjectagg() in func_json_objectagg_test.go

--- executor/aggfuncs/func_json_objectagg_test.go
+++ executor/aggfuncs/func_json_objectagg_test.go
@@ -113,10 +113,7 @@ func (s *testSuite) TestMemJsonObjectagg(c *C) {
 			argCombines = append(argCombines, argTypes)
 		}
 	}
-
-	var tests []multiArgsAggMemTest
 	numRows := 5
-
 	for k := 0; k < len(argCombines); k++ {
 		entries := make(map[string]interface{})
 
@@ -143,14 +140,12 @@ func (s *testSuite) TestMemJsonObjectagg(c *C) {
 			}
 		}
 
-		aggTest1 := buildMultiArgsAggMemTester(ast.AggFuncJsonObjectAgg, argTypes, mysql.TypeJSON, numRows, aggfuncs.DefPartialResult4JsonObjectAgg, defaultMultiArgsMemDeltaGens, false)
-		tests = append(tests, aggTest1)
-		aggTest2 := buildMultiArgsAggMemTester(ast.AggFuncJsonObjectAgg, argTypes, mysql.TypeJSON, numRows, aggfuncs.DefPartialResult4JsonObjectAgg, defaultMultiArgsMemDeltaGens, true)
-		tests = append(tests, aggTest2)
-
-	}
-
-	for _, test := range tests {
-		s.testMultiArgsAggMemFunc(c, test)
+		tests := []multiArgsAggMemTest{
+			buildMultiArgsAggMemTester(ast.AggFuncJsonObjectAgg, argTypes, mysql.TypeJSON, numRows, aggfuncs.DefPartialResult4JsonObjectAgg, defaultMultiArgsMemDeltaGens, true),
+			buildMultiArgsAggMemTester(ast.AggFuncJsonObjectAgg, argTypes, mysql.TypeJSON, numRows, aggfuncs.DefPartialResult4JsonObjectAgg, defaultMultiArgsMemDeltaGens, false),
+		}
+		for _, test := range tests {
+			s.testMultiArgsAggMemFunc(c, test)
+		}
 	}
 }
