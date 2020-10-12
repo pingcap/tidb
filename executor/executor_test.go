b99f203f6f1cb4f98565c29bf50e82da06cba6a2
jixiaodong <xiaodong.ji@outlook.com>
2020-10-12 21:34:47 +0800

    fix unit test

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -4480,8 +4480,8 @@ func (s *testSuiteP2) TestStrToDateBuiltin(c *C) {
 
 func (s *testSuiteP2) TestStrToDateBuiltinWithWarnings(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec(`SELECT STR_TO_DATE('0000-1-01', '%Y-%m-%d');`)
-	tk.MustQuery("show warnings").Check(testutil.RowsWithSep("|", "Warning|1411|Incorrect datetime value: '0000-1-01' for function str_to_date"))
+	tk.MustExec(`SELECT STR_TO_DATE('0000-1-01', '%Y-%m-%d')`)
+	tk.MustQuery("show warnings").Check(testkit.Rows("Warning 1411 Incorrect datetime value: '0000-1-01' for function str_to_date"))
 }
 
 func (s *testSuiteP2) TestReadPartitionedTable(c *C) {
