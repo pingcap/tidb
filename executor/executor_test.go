a3b9087fe61eac1bb2b3ed5ff57f26a01451c550
jyz0309 <45495947@qq.com>
2020-10-14 13:30:26 +0800

    add test
    Signed-off-by: jyz0309 <45495947@qq.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -6395,6 +6395,10 @@ func (s *testSerialSuite1) TestIndexlookupRuntimeStats(c *C) {
 	c.Assert(len(rows), Equals, 3)
 	explain := fmt.Sprintf("%v", rows[0])
 	c.Assert(explain, Matches, ".*time:.*loops:.*index_task:.*table_task:{num.*concurrency.*time.*}.*")
+	indexExplain := fmt.Sprintf("%v", rows[1])
+	tableExplain := fmt.Sprintf("%v", rows[2])
+	c.Assert(indexExplain, Matches, ".*time:.*loops:.*cop_task:.*")
+	c.Assert(tableExplain, Matches, ".*time:.*loops:.*cop_task:.*")
 }
 
 func (s *testSuite) TestCollectDMLRuntimeStats(c *C) {
