1967a075e6dd6c776c7aa10fb8d5630a9272a84f
crazycs <crazycs520@gmail.com>
2020-10-19 14:51:08 +0800

    Update executor/slow_query.go
    Co-authored-by: Arenatlx <ailinsilence4@gmail.com>

--- executor/slow_query.go
+++ executor/slow_query.go
@@ -48,7 +48,7 @@ import (
 	"go.uber.org/zap"
 )
 
-// ParseSlowLogBatchSize is the batch size of slow-log lines for worker to parse. it exports for testing.
+// ParseSlowLogBatchSize is the batch size of slow-log lines for a worker to parse, exported for testing.
 var ParseSlowLogBatchSize = 64
 
 //slowQueryRetriever is used to read slow log data.
