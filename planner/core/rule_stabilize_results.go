5401c4495b18f7bf0b681624f9eebad4a1538956
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-10-30 18:53:41 +0800

    WIP

--- planner/core/rule_stabilize_results.go
+++ planner/core/rule_stabilize_results.go
@@ -2,78 +2,43 @@ package core
 
 import (
 	"context"
-	"github.com/pingcap/tidb/expression"
+
 	"github.com/pingcap/tidb/planner/util"
 )
 
 type resultsStabilizer struct {
 }
 
-/*
-	1. find the first Sort in the plan, return the original plan if there is no Sort;
-	2. extract handleCols from DataSources and inject them into the Sort;
-	3. create an new Projection upon the Sort;
-*/
 func (rs *resultsStabilizer) optimize(ctx context.Context, lp LogicalPlan) (LogicalPlan, error) {
-	return rs.injectHandleColsIntoSort(lp)
-}
-
-func (rs *resultsStabilizer) injectHandleColsIntoSort(lp LogicalPlan) (LogicalPlan, error) {
-	switch x := lp.(type) {
-	case *LogicalSort:
-		originSchema := lp.Schema().Clone()
-		originCols := make([]expression.Expression, 0, len(originSchema.Columns))
-		for _, col := range originSchema.Columns {
-			originCols = append(originCols, col)
+	ok, err := rs.stabilizeSort(lp)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		byItems := make([]*util.ByItems, 0, len(lp.Schema().Columns))
+		for _, col := range lp.Schema().Columns {
+			byItems = append(byItems, &util.ByItems{Expr: col})
 		}
-		handleCols, err := rs.extraHandleCols(lp)
-		if err != nil {
-			return nil, err
-		}
-		for _, hc := range handleCols {
-			x.ByItems = append(x.ByItems, &util.ByItems{Expr: hc})
-		}
-		proj := LogicalProjection{Exprs: originCols}.Init(x.ctx, x.SelectBlockOffset())
-		proj.SetSchema(originSchema)
-		proj.SetChildren(x)
-		return proj, nil
-	case *LogicalSelection, *LogicalProjection:
-		newChild, err := rs.injectHandleColsIntoSort(lp.Children()[0])
-		if err != nil {
-			return nil, err
-		}
-		lp.SetChild(0, newChild)
+		sort := LogicalSort{
+			ByItems: byItems,
+		}.Init(lp.SCtx(), lp.SelectBlockOffset())
+		sort.SetChildren(lp)
+		lp = sort
 	}
 	return lp, nil
 }
 
-func (rs *resultsStabilizer) extraHandleCols(lp LogicalPlan) ([]*expression.Column, error) {
-	handleCols := make([]*expression.Column, 0, 2)
+func (rs *resultsStabilizer) stabilizeSort(lp LogicalPlan) (bool, error) {
 	switch x := lp.(type) {
-	case *DataSource:
-		handleCols = append(handleCols, x.handleCol)
-	default:
-		for _, child := range lp.Children() {
-			cols, err := rs.extraHandleCols(child)
-			if err != nil {
-				return nil, err
-			}
-			handleCols = append(handleCols, cols...)
+	case *LogicalSort:
+		for _, col := range x.Schema().Columns {
+			x.ByItems = append(x.ByItems, &util.ByItems{Expr: col})
 		}
-		for _, hc := range handleCols {
-			notExist := true
-			for _, col := range lp.Schema().Columns {
-				if col.Equal(nil, hc) {
-					notExist = false
-					break
-				}
-			}
-			if notExist {
-				lp.Schema().Columns = append(lp.Schema().Columns, hc)
-			}
-		}
+		return true, nil
+	case *LogicalSelection, *LogicalProjection:
+		return rs.stabilizeSort(lp.Children()[0])
 	}
-	return handleCols, nil
+	return false, nil
 }
 
 func (rs *resultsStabilizer) name() string {
