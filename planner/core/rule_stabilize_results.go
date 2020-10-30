c98826c5d2aaac916cdddb254b0df5c58c2ad9d1
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-10-30 21:37:18 +0800

    WIP

--- planner/core/rule_stabilize_results.go
+++ planner/core/rule_stabilize_results.go
@@ -10,25 +10,14 @@ type resultsStabilizer struct {
 }
 
 func (rs *resultsStabilizer) optimize(ctx context.Context, lp LogicalPlan) (LogicalPlan, error) {
-	ok, err := rs.stabilizeSort(lp)
-	if err != nil {
-		return nil, err
-	}
-	if !ok {
-		byItems := make([]*util.ByItems, 0, len(lp.Schema().Columns))
-		for _, col := range lp.Schema().Columns {
-			byItems = append(byItems, &util.ByItems{Expr: col})
-		}
-		sort := LogicalSort{
-			ByItems: byItems,
-		}.Init(lp.SCtx(), lp.SelectBlockOffset())
-		sort.SetChildren(lp)
-		lp = sort
+	stable := rs.stabilizeSort(lp)
+	if !stable {
+		lp = rs.injectSort(lp)
 	}
 	return lp, nil
 }
 
-func (rs *resultsStabilizer) stabilizeSort(lp LogicalPlan) (bool, error) {
+func (rs *resultsStabilizer) stabilizeSort(lp LogicalPlan) bool {
 	switch x := lp.(type) {
 	case *LogicalSort:
 		for _, col := range x.Schema().Columns {
@@ -43,11 +32,30 @@ func (rs *resultsStabilizer) stabilizeSort(lp LogicalPlan) (bool, error) {
 				x.ByItems = append(x.ByItems, &util.ByItems{Expr: col})
 			}
 		}
-		return true, nil
+		return true
 	case *LogicalSelection, *LogicalProjection:
 		return rs.stabilizeSort(lp.Children()[0])
 	}
-	return false, nil
+	return false
+}
+
+func (rs *resultsStabilizer) injectSort(lp LogicalPlan) LogicalPlan {
+	switch lp.(type) {
+	case *LogicalSelection, *LogicalProjection:
+		newChild := rs.injectSort(lp.Children()[0])
+		lp.SetChildren(newChild)
+		return lp
+	default:
+		byItems := make([]*util.ByItems, 0, len(lp.Schema().Columns))
+		for _, col := range lp.Schema().Columns {
+			byItems = append(byItems, &util.ByItems{Expr: col})
+		}
+		sort := LogicalSort{
+			ByItems: byItems,
+		}.Init(lp.SCtx(), lp.SelectBlockOffset())
+		sort.SetChildren(lp)
+		return sort
+	}
 }
 
 func (rs *resultsStabilizer) name() string {
