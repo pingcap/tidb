0ebab5297920744043fecdbf85c48d21465354f4
jwxiong <xiongjiwei1996@outlook.com>
2020-11-03 13:39:36 +0800

    pinyin order imp

--- util/collate/pinyin_tidb_as_cs.go
+++ util/collate/pinyin_tidb_as_cs.go
@@ -19,15 +19,61 @@ type zhPinyinTiDBASCSCollator struct {
 
 // Collator interface, no implements now.
 func (py *zhPinyinTiDBASCSCollator) Compare(a, b string) int {
-	panic("implement me")
+	a = truncateTailingSpace(a)
+	b = truncateTailingSpace(b)
+
+	ar, br := rune(0), rune(0)
+	ai, bi := 0, 0
+
+	for ai < len(a) && bi < len(b) {
+		ar, ai = decodeRune(a, ai)
+		br, bi = decodeRune(b, bi)
+
+		cmp := int(convertRunePinyin(ar)) - int(convertRunePinyin(br))
+		if cmp != 0 {
+			return sign(cmp)
+		}
+	}
+
+	return sign((len(a) - ai) - (len(b) - bi))
 }
 
 // Collator interface, no implements now.
 func (py *zhPinyinTiDBASCSCollator) Key(str string) []byte {
-	panic("implement me")
+	str = truncateTailingSpace(str)
+	buf := make([]byte, 0, len(str))
+	r, i := rune(0), 0
+
+	for i < len(str) {
+		r, i = decodeRune(str, i)
+		k := convertRunePinyin(r)
+
+		switch {
+		case k < 0xFF:
+			buf = append(buf, byte(k))
+		case k < 0xFFFF:
+			buf = append(buf, byte(k>>8), byte(k))
+		default:
+			buf = append(buf, byte(k>>24), byte(k>>16), byte(k>>8), byte(k))
+		}
+	}
+
+	return buf
 }
 
 // Collator interface, no implements now.
 func (py *zhPinyinTiDBASCSCollator) Pattern() WildcardPattern {
 	panic("implement me")
+}
+
+func convertRunePinyin(r rune) uint32 {
+	if r <= 0xFFFF {
+		return zhPinyinTiDBASCSBMP[r]
+	}
+
+	if c, ok := zhPinyinTiDBASCSNoBMP[r]; ok {
+		return c
+	}
+
+	return 0xFF000000 + uint32(r) + 0x1E248
 }
