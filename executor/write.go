5d8e79bc875178416807c9724160e2a4e4800b45
alfredlu <wslzj40@gmail.com>
2020-11-04 11:21:49 +0800

    nitpick

--- executor/write.go
+++ executor/write.go
@@ -95,7 +95,7 @@ func updateRecord(ctx context.Context, sctx sessionctx.Context, h kv.Handle, old
 	for i, col := range t.Cols() {
 		collation := newData[i].Collation()
 		// We should use binary collation to compare datum, otherwise the result will be incorrect.
-		newData[i].SetCollation(charset.CharsetBin)
+		newData[i].SetCollation(charset.CollationBin)
 		cmp, err := newData[i].CompareDatum(sc, &oldData[i])
 		newData[i].SetCollation(collation)
 		if err != nil {
