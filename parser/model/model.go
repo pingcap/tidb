000cb7018f0b014541c5ceb50d80a247cc0cfa64
crazycs <crazycs520@gmail.com>
2020-11-04 15:11:16 +0800

    [parser] *: tiny optimize for huge partition table (#1072)
    * *: tiny optimize for huge partition table
    
    Signed-off-by: crazycs520 <crazycs520@gmail.com>
    
    * Update model/model.go
    
    Co-authored-by: kennytm <kennytm@gmail.com>
    
    Co-authored-by: kennytm <kennytm@gmail.com>

--- parser/model/model.go
+++ parser/model/model.go
@@ -748,9 +748,12 @@ type PartitionInfo struct {
 
 // GetNameByID gets the partition name by ID.
 func (pi *PartitionInfo) GetNameByID(id int64) string {
-	for _, def := range pi.Definitions {
-		if id == def.ID {
-			return def.Name.L
+	definitions := pi.Definitions
+	// do not convert this loop to `for _, def := range definitions`.
+	// see https://github.com/pingcap/parser/pull/1072 for the benchmark.
+	for i := range definitions {
+		if id == definitions[i].ID {
+			return definitions[i].Name.L
 		}
 	}
 	return ""
@@ -776,8 +779,9 @@ func (ci *PartitionDefinition) Clone() PartitionDefinition {
 // FindPartitionDefinitionByName finds PartitionDefinition by name.
 func (t *TableInfo) FindPartitionDefinitionByName(partitionDefinitionName string) *PartitionDefinition {
 	lowConstrName := strings.ToLower(partitionDefinitionName)
-	for i, pd := range t.Partition.Definitions {
-		if pd.Name.L == lowConstrName {
+	definitions := t.Partition.Definitions
+	for i := range definitions {
+		if definitions[i].Name.L == lowConstrName {
 			return &t.Partition.Definitions[i]
 		}
 	}
