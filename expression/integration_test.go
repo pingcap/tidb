89260ac793e4d1effce79374f81cebbce7e1b9f3
pengdaqian <xsagre@qq.com>
2020-11-16 01:23:26 +0800

    fix test error

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -2315,7 +2315,6 @@ func (s *testIntegrationSuite2) TestBuiltin(c *C) {
 	defer s.cleanEnv(c)
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
-	ctx := context.Background()
 
 	// for is true && is false
 	tk.MustExec("drop table if exists t")
@@ -2754,13 +2753,7 @@ func (s *testIntegrationSuite2) TestBuiltin(c *C) {
 	c.Assert(err, NotNil)
 
 	// test case decimal precision less than the scale.
-	rs, err := tk.Exec("select cast(12.1 as decimal(3, 4));")
-	c.Assert(err, IsNil)
-	_, err = session.GetRows4Test(ctx, tk.Se, rs)
-	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "[types:1427]For float(M,D), double(M,D) or decimal(M,D), M must be >= D (column '').")
-	c.Assert(rs.Close(), IsNil)
-
+	tk.MustGetErrMsg("select cast(12.1 as decimal(3, 4));", "[types:1427]For float(M,D), double(M,D) or decimal(M,D), M must be >= D (column '').")
 	// test unhex and hex
 	result = tk.MustQuery("select unhex('4D7953514C')")
 	result.Check(testkit.Rows("MySQL"))
