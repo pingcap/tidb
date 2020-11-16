0689ad0ef5180c5353f190a7f38d00cabae9864d
ichn-hu <zfhu16@gmail.com>
2020-11-16 17:09:05 +0800

    address comment

--- types/datum.go
+++ types/datum.go
@@ -985,7 +985,7 @@ func ProduceStrWithSpecifiedTp(s string, tp *FieldType, sc *stmtctx.StatementCon
 
 		if len(overflowed) != 0 {
 			trimed := strings.TrimRight(overflowed, " \t\n\r")
-			if len(trimed) == 0 && ((tp.Tp == mysql.TypeVarchar && !IsBinaryStr(tp)) || (tp.Tp == mysql.TypeString && !IsBinaryStr(tp))) {
+			if len(trimed) == 0 && !IsBinaryStr (tp) && IsTypeChar(tp.Tp) {
 				if tp.Tp == mysql.TypeVarchar {
 					sc.AppendWarning(ErrTruncated.GenWithStack("Data truncated, field len %d, data len %d", flen, characterLen))
 				}
