64e9c63b654f0b8ea86f9ebda2db8cf84569de91
Yilin Chen <sticnarf@gmail.com>
2020-11-17 16:10:55 +0800

    fix bugs
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- session/pessimistic_test.go
+++ session/pessimistic_test.go
@@ -2021,9 +2021,8 @@ func (s *testPessimisticSuite) TestAsyncCommitWithSchemaChange(c *C) {
 		tk2.MustExec("alter table tk add index k2(c2)")
 	}()
 	c.Assert(failpoint.Enable("github.com/pingcap/tidb/store/tikv/beforePrewrite", "1*sleep(1200)"), IsNil)
-	// should fail if prewrite takes too long
-	err := tk.ExecToErr("commit")
-	c.Assert(err, ErrorMatches, ".*commit TS \\d+ is too large")
+	// should fallback to traditional 2PC if prewrite takes too long
+	tk.MustExec("commit")
 	c.Assert(failpoint.Disable("github.com/pingcap/tidb/store/tikv/beforePrewrite"), IsNil)
 	tk3.MustExec("admin check table tk")
 }
