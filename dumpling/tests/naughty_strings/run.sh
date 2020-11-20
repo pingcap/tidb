8db5ca43c91f37b4b352e1875c414252e0462b7b
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-11-20 03:46:34 -0600

    [dumpling] Add more log and compress option (#202)
    * add percentage log
    * add summary
    * redirect log to dumpling's logger
    * add --compress option and relative integration test
    * add prometheus metrics in dumpling

--- dumpling/tests/naughty_strings/run.sh
+++ dumpling/tests/naughty_strings/run.sh
@@ -10,6 +10,14 @@ run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.t.sql"
 run_dumpling --escape-backslash=false
 # FIXME should compare the schemas too, but they differ too much among MySQL versions.
 diff "$DUMPLING_BASE_NAME/expect/naughty_strings.t.sql" "$DUMPLING_OUTPUT_DIR/naughty_strings.t.000000000.sql"
+
+# run with compress option
+rm "$DUMPLING_OUTPUT_DIR/naughty_strings.t.000000000.sql"
+run_dumpling --escape-backslash=false --compress "gzip"
+file_should_exist "$DUMPLING_OUTPUT_DIR/naughty_strings.t.000000000.sql.gz"
+gzip "$DUMPLING_OUTPUT_DIR/naughty_strings.t.000000000.sql.gz" -d
+diff "$DUMPLING_BASE_NAME/expect/naughty_strings.t.sql" "$DUMPLING_OUTPUT_DIR/naughty_strings.t.000000000.sql"
+
 run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.escape-schema.sql"
 run_sql_file "$DUMPLING_BASE_NAME/data/naughty_strings.escape.sql"
 run_dumpling --escape-backslash=true
