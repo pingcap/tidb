6bb3d2c3e2ec3ef312d4fc8695b5bb98bc57ed8b
Ryan Leung <rleungx@gmail.com>
2020-11-24 16:02:35 +0800

    omit the 404 error when /config/placement-rule is not existed
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- domain/infosync/info.go
+++ domain/infosync/info.go
@@ -330,11 +330,10 @@ func doRequest(ctx context.Context, addrs []string, route, method string, body i
 		}
 
 		res, err = util2.InternalHTTPClient().Do(req)
-
-		if strings.Contains(route, "placement-rule") && res.StatusCode == http.StatusNotFound {
-			return nil, nil
-		}
 		if err == nil {
+			if strings.Contains(route, "placement-rule") && res.StatusCode == http.StatusNotFound {
+				return nil, nil
+			}
 			bodyBytes, err := ioutil.ReadAll(res.Body)
 			if err != nil {
 				return nil, err
