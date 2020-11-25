a557aa089bcad4221f7ffdf614490166cc03083b
iosmanthus <myosmanthustree@gmail.com>
2020-11-25 14:47:29 +0800

    fix IsTypeInteger redeclared
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- types/etc.go
+++ types/etc.go
@@ -83,15 +83,6 @@ func IsTypeNumeric(tp byte) bool {
 	return false
 }
 
-// IsTypeInteger returns if tp is an integer type
-func IsTypeInteger(tp byte) bool {
-	switch tp {
-	case mysql.TypeTiny, mysql.TypeShort, mysql.TypeInt24, mysql.TypeLong, mysql.TypeLonglong, mysql.TypeYear:
-		return true
-	}
-	return false
-}
-
 // IsTemporalWithDate returns a boolean indicating
 // whether the tp is time type with date.
 func IsTemporalWithDate(tp byte) bool {
