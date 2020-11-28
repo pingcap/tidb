0ba309c092aeb23f0e0058237adb9e45f2d1bc42
AI-Login-is-already-taken <956308585@qq.com>
2020-11-28 11:41:34 +0800

    fix test

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -7784,7 +7784,7 @@ func (s *testIntegrationSerialSuite) TestIssue20128(c *C) {
 	tk.MustExec("insert into t (b)values(1);")
 	tk.MustQuery("select * from t order by a asc;").Check(
 		testkit.Rows(
-			"nil -11623",
+			"nil 1",
 			"b'0' 0",
 			"b'1' 1",
 			"b'1111111111111111111111111111111111111111111111111010110101001100' -11623",
