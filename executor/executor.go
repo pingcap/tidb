f4c9531d6fb3076e723b91c93050f211b4abf3b0
crazycs520 <crazycs520@gmail.com>
2020-12-01 22:02:41 +0800

    address comment
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- executor/executor.go
+++ executor/executor.go
@@ -1640,7 +1640,10 @@ func ResetContextOfStmt(ctx sessionctx.Context, s ast.StmtNode) (err error) {
 		sc.AllowInvalidDate = vars.SQLMode.HasAllowInvalidDatesMode()
 		sc.IgnoreZeroInDate = !vars.SQLMode.HasNoZeroInDateMode() || !vars.SQLMode.HasNoZeroDateMode() || !vars.StrictSQLMode || stmt.IgnoreErr || sc.AllowInvalidDate
 		sc.Priority = stmt.Priority
-	case *ast.CreateTableStmt, *ast.AlterTableStmt:
+	case *ast.CreateTableStmt:
+		// Make sure the sql_mode is strict when checking column default value.
+		sc.InCreateTableStmt = true
+	case *ast.AlterTableStmt:
 		// Make sure the sql_mode is strict when checking column default value.
 	case *ast.LoadDataStmt:
 		sc.DupKeyAsWarning = true
