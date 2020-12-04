7e9df57e412048a02b754720613b0c5eb597d0d6
Han Fei <hanfei19910905@gmail.com>
2020-12-04 13:52:10 +0800

    address comments

--- planner/core/fragment.go
+++ planner/core/fragment.go
@@ -71,9 +71,9 @@ func (e *mppTaskGenerator) generateMPPTasksForFragment(f *Fragment) (tasks []*kv
 		}
 	}
 	if f.TableScan != nil {
-		tasks, err = e.constructSinglePhysicalTable(context.Background(), f.TableScan.Table.ID, f.TableScan.Ranges)
+		tasks, err = e.constructMPPTasksImpl(context.Background(), f.TableScan.Table.ID, f.TableScan.Ranges)
 	} else {
-		tasks, err = e.constructSinglePhysicalTable(context.Background(), -1, nil)
+		tasks, err = e.constructMPPTasksImpl(context.Background(), -1, nil)
 	}
 	if err != nil {
 		return nil, errors.Trace(err)
@@ -92,7 +92,7 @@ func (e *mppTaskGenerator) generateMPPTasksForFragment(f *Fragment) (tasks []*kv
 }
 
 // single physical table means a table without partitions or a single partition in a partition table.
-func (e *mppTaskGenerator) constructSinglePhysicalTable(ctx context.Context, tableID int64, ranges []*ranger.Range) ([]*kv.MPPTask, error) {
+func (e *mppTaskGenerator) constructMPPTasksImpl(ctx context.Context, tableID int64, ranges []*ranger.Range) ([]*kv.MPPTask, error) {
 	var kvRanges []kv.KeyRange
 	if tableID != -1 {
 		kvRanges = distsql.TableRangesToKVRanges(tableID, ranges, nil)
