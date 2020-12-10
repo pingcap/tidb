f7cb565668287ada4ffec367b5f263e8b35becd4
lzmhhh123 <lzmhhh123@gmail.com>
2020-12-10 11:03:20 +0800

    fix conflicts
    Signed-off-by: lzmhhh123 <lzmhhh123@gmail.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -6383,8 +6383,6 @@ func (s *testSuite) TestIssue20305(c *C) {
 	tk.MustQuery("SELECT * FROM `t3` where y <= a").Check(testkit.Rows("2155 2156"))
 }
 
-<<<<<<< HEAD
-=======
 func (s *testSuite) TestIssue13953(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
@@ -6395,30 +6393,6 @@ func (s *testSuite) TestIssue13953(c *C) {
 		testkit.Rows("1 1"))
 }
 
-func (s *testSuite) TestZeroDateTimeCompatibility(c *C) {
-	SQLs := []string{
-		`select YEAR(0000-00-00), YEAR("0000-00-00")`,
-		`select MONTH(0000-00-00), MONTH("0000-00-00")`,
-		`select DAYOFWEEK(0000-00-00), DAYOFWEEK("0000-00-00")`,
-		`select DAYOFMONTH(0000-00-00), DAYOFMONTH("0000-00-00")`,
-		`select DAYOFYEAR(0000-00-00), DAYOFYEAR("0000-00-00")`,
-		`select QUARTER(0000-00-00), QUARTER("0000-00-00")`,
-		`select EXTRACT(DAY FROM 0000-00-00), EXTRACT(DAY FROM "0000-00-00")`,
-		`select EXTRACT(MONTH FROM 0000-00-00), EXTRACT(MONTH FROM "0000-00-00")`,
-		`select EXTRACT(YEAR FROM 0000-00-00), EXTRACT(YEAR FROM "0000-00-00")`,
-		`select EXTRACT(WEEK FROM 0000-00-00), EXTRACT(WEEK FROM "0000-00-00")`,
-		`select EXTRACT(QUARTER FROM 0000-00-00), EXTRACT(QUARTER FROM "0000-00-00")`,
-	}
-	tk := testkit.NewTestKit(c, s.store)
-
-	for _, t := range SQLs {
-		fmt.Println(t)
-		tk.MustQuery(t).Check(testkit.Rows("0 <nil>"))
-		c.Assert(tk.Se.GetSessionVars().StmtCtx.WarningCount(), Equals, uint16(1))
-	}
-}
-
->>>>>>> 8dd5b0a0c... planner: do not propagate column eq with different column types (#21495)
 func (s *testSuite) TestOOMActionPriority(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
