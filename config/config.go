797349fc84370d60d353d389b00e37e80367866e
Shuaipeng Yu <jackysp@gmail.com>
2020-12-11 14:54:42 +0800

    *: make TestPointGetReadLock stable
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- config/config.go
+++ config/config.go
@@ -653,7 +653,7 @@ var defaultConf = Config{
 	IndexLimit:                   64,
 	AlterPrimaryKey:              false,
 	TreatOldVersionUTF8AsUTF8MB4: true,
-	EnableTableLock:              false,
+	EnableTableLock:              true,
 	DelayCleanTableLock:          0,
 	SplitRegionMaxNum:            1000,
 	RepairMode:                   false,
