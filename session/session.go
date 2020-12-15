4757ecb3d881e0ed969f45415f35ab089cee06a9
Arenatlx <ailinsilence4@gmail.com>
2020-12-15 17:00:15 +0800

    add switch for point get cache (#21669)

--- session/session.go
+++ session/session.go
@@ -2284,6 +2284,7 @@ var builtinGlobalVariable = []string{
 	variable.TiDBShardAllocateStep,
 	variable.TiDBEnableChangeColumnType,
 	variable.TiDBEnableChangeMultiSchema,
+	variable.TiDBEnablePointGetCache,
 	variable.TiDBEnableAmendPessimisticTxn,
 	variable.TiDBMemQuotaApplyCache,
 	variable.TiDBEnableParallelApply,
