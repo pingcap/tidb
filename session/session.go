1333f4be09fc13f89c2cddb044ab827fc655bee5
Kenan Yao <cauchy1992@gmail.com>
2020-12-18 03:51:35 +0800

    bindinfo: sync concurrent ops on mysql.bind_info from multiple tidb instances (#21629)

--- session/session.go
+++ session/session.go
@@ -2133,7 +2133,7 @@ func CreateSessionWithDomain(store kv.Storage, dom *domain.Domain) (*session, er
 
 const (
 	notBootstrapped         = 0
-	currentBootstrapVersion = version56
+	currentBootstrapVersion = version57
 )
 
 func getStoreBootstrapVersion(store kv.Storage) int64 {
