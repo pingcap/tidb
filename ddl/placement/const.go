3aa608523aa9c327431c553e1d388977cdfa0a73
xhe <xw897002528@gmail.com>
2020-12-23 18:53:42 +0800

    ddl: address comment
    Signed-off-by: xhe <xw897002528@gmail.com>

--- ddl/placement/const.go
+++ ddl/placement/const.go
@@ -13,11 +13,12 @@
 
 package placement
 
-// BundleIDPrefix is the bundle prefix of all rule bundles from TiDB_DDL statements.
-const BundleIDPrefix = "TiDB_DDL_"
-
-// PDBundleID is the bundle name of pd, the default bundle for all regions.
-const PDBundleID = "pd"
+const (
+	// BundleIDPrefix is the bundle prefix of all rule bundles from TiDB_DDL statements.
+	BundleIDPrefix = "TiDB_DDL_"
+	// PDBundleID is the bundle name of pd, the default bundle for all regions.
+	PDBundleID = "pd"
+)
 
 const (
 	// RuleIndexDefault is the default index for a rule, check Rule.Index.
