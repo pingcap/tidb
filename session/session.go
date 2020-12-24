7746fe9d191f5ea6e368e2d22173020ab31ddf79
lance6716 <lance6716@gmail.com>
2020-12-24 16:36:47 +0800

    session: fix didn't change current version in #21856

--- session/session.go
+++ session/session.go
@@ -2134,7 +2134,7 @@ func CreateSessionWithDomain(store kv.Storage, dom *domain.Domain) (*session, er
 
 const (
 	notBootstrapped         = 0
-	currentBootstrapVersion = version57
+	currentBootstrapVersion = version58
 )
 
 func getStoreBootstrapVersion(store kv.Storage) int64 {
