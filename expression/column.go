6166b1f3d8b59ef338276fc970d083492f1707ab
tjk <jianke.tang@sciencespo.fr>
2021-01-04 22:51:17 +0800

    remove superfluous else indentation

--- expression/column.go
+++ expression/column.go
@@ -252,18 +252,16 @@ func (col *Column) VecEvalReal(ctx sessionctx.Context, input *chunk.Chunk, resul
 			for i, j := range sel {
 				if src.IsNull(j) {
 					continue
-				} else {
-					f64s[i] = float64(f32s[j])
 				}
+				f64s[i] = float64(f32s[j])
 			}
 			return nil
 		}
 		for i := range f32s {
 			if src.IsNull(i) {
 				continue
-			} else {
-				f64s[i] = float64(f32s[i])
 			}
+			f64s[i] = float64(f32s[i])
 		}
 		return nil
 	}
