80e735d1acd2c0dfd4ccf52d4a27edd4312eb0c5
wshwsh12 <793703860@qq.com>
2021-01-06 10:47:45 +0800

    resolve conflict

--- session/session_test.go
+++ session/session_test.go
@@ -3830,23 +3830,6 @@ func (s *testSessionSerialSuite) TestDefaultWeekFormat(c *C) {
 	tk2 := testkit.NewTestKitWithInit(c, s.store)
 	tk2.MustQuery("select week('2020-02-02'), @@default_week_format, week('2020-02-02');").Check(testkit.Rows("6 4 6"))
 }
-<<<<<<< HEAD
-=======
-
-func (s *testSessionSerialSuite) TestIssue21944(c *C) {
-	tk1 := testkit.NewTestKitWithInit(c, s.store)
-	_, err := tk1.Exec("set @@tidb_current_ts=1;")
-	c.Assert(err.Error(), Equals, "[variable:1238]Variable 'tidb_current_ts' is a read only variable")
-}
-
-func (s *testSessionSerialSuite) TestIssue21943(c *C) {
-	tk := testkit.NewTestKitWithInit(c, s.store)
-	_, err := tk.Exec("set @@last_plan_from_binding='123';")
-	c.Assert(err.Error(), Equals, "[variable:1238]Variable 'last_plan_from_binding' is a read only variable")
-
-	_, err = tk.Exec("set @@last_plan_from_cache='123';")
-	c.Assert(err.Error(), Equals, "[variable:1238]Variable 'last_plan_from_cache' is a read only variable")
-}
 
 func (s *testSessionSerialSuite) TestProcessInfoIssue22068(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
@@ -3865,4 +3848,3 @@ func (s *testSessionSerialSuite) TestProcessInfoIssue22068(c *C) {
 	c.Assert(pi.Plan, IsNil)
 	wg.Wait()
 }
->>>>>>> 57cd69473... session: set process info before building plan (#22101)
