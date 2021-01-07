67b2867b2e9a97e9a560b950d78d74346a766dc8
jwxiong <xiongjiwei1996@outlook.com>
2021-01-07 13:42:03 +0800

    fix

--- planner/core/rule_predicate_push_down.go
+++ planner/core/rule_predicate_push_down.go
@@ -341,6 +341,9 @@ func simplifyOuterJoin(p *LogicalJoin, predicates []expression.Expression) {
 // If it is a conjunction containing a null-rejected condition as a conjunct.
 // If it is a disjunction of null-rejected conditions.
 func isNullRejected(ctx sessionctx.Context, schema *expression.Schema, expr expression.Expression) bool {
+	if expression.ContainMutableConst(ctx, []expression.Expression{expr}) {
+		return false
+	}
 	expr = expression.PushDownNot(ctx, expr)
 	sc := ctx.GetSessionVars().StmtCtx
 	sc.InNullRejectCheck = true
