b181a507b9feb35f61465b3dbea56beb42e17bfe
Han Fei <hanfei19910905@gmail.com>
2021-01-22 13:55:26 +0800

    address comments

--- store/mockstore/unistore/cophandler/mpp_exec.go
+++ store/mockstore/unistore/cophandler/mpp_exec.go
@@ -1,3 +1,16 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cophandler
 
 import (
@@ -292,10 +305,8 @@ func (e *joinExec) buildHashTable() error {
 			return nil
 		}
 		rows := chk.NumRows()
-		i := 0
-		for i < rows {
+		for i := 0; i < rows; i++ {
 			row := chk.GetRow(i)
-			i++
 			keyCol := row.GetDatum(e.buildKey.Index, e.buildChild.getFieldTypes()[e.buildKey.Index])
 			key, err := keyCol.ToString()
 			if err != nil {
