a22d1aea5640aa8d3cfe6bc3328868c09e582a96
lzmhhh123 <lzmhhh123@gmail.com>
2021-01-26 16:18:56 +0800

    fix conflicts
    Signed-off-by: lzmhhh123 <lzmhhh123@gmail.com>

--- executor/executor.go
+++ executor/executor.go
@@ -1710,21 +1710,6 @@ func ResetContextOfStmt(ctx sessionctx.Context, s ast.StmtNode) (err error) {
 	return
 }
 
-<<<<<<< HEAD
-=======
-// ResetUpdateStmtCtx resets statement context for UpdateStmt.
-func ResetUpdateStmtCtx(sc *stmtctx.StatementContext, stmt *ast.UpdateStmt, vars *variable.SessionVars) {
-	sc.InUpdateStmt = true
-	sc.DupKeyAsWarning = stmt.IgnoreErr
-	sc.BadNullAsWarning = !vars.StrictSQLMode || stmt.IgnoreErr
-	sc.TruncateAsWarning = !vars.StrictSQLMode || stmt.IgnoreErr
-	sc.DividedByZeroAsWarning = !vars.StrictSQLMode || stmt.IgnoreErr
-	sc.AllowInvalidDate = vars.SQLMode.HasAllowInvalidDatesMode()
-	sc.IgnoreZeroInDate = !vars.SQLMode.HasNoZeroInDateMode() || !vars.SQLMode.HasNoZeroDateMode() || !vars.StrictSQLMode || stmt.IgnoreErr || sc.AllowInvalidDate
-	sc.Priority = stmt.Priority
-}
-
->>>>>>> afb2ab95f... table: fix zero date in different sqlmode (#20206)
 // FillVirtualColumnValue will calculate the virtual column value by evaluating generated
 // expression using rows from a chunk, and then fill this value into the chunk
 func FillVirtualColumnValue(virtualRetTypes []*types.FieldType, virtualColumnIndex []int,
