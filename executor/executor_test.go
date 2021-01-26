63e08600cad6b42cd9af6ce1bb9754486f8a6554
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-01-26 19:17:35 +0800

    expression: handle hybrid field types for where clause (#21724) (#21810)
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -6541,6 +6541,18 @@ func (s *testSuite) TestTxnRetry(c *C) {
 	tk.MustQuery("select * from t").Check(testkit.Rows("10"))
 }
 
+func (s *testSuite) Test12201(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists e")
+	tk.MustExec("create table e (e enum('a', 'b'))")
+	tk.MustExec("insert into e values ('a'), ('b')")
+	tk.MustQuery("select * from e where case 1 when 0 then e end").Check(testkit.Rows())
+	tk.MustQuery("select * from e where case 1 when 1 then e end").Check(testkit.Rows("a", "b"))
+	tk.MustQuery("select * from e where case e when 1 then e end").Check(testkit.Rows("a"))
+	tk.MustQuery("select * from e where case 1 when e then e end").Check(testkit.Rows("a"))
+}
+
 func (s *testSuite) TestIssue22201(c *C) {
 	tk := testkit.NewTestKitWithInit(c, s.store)
 	tk.MustQuery("SELECT HEX(WEIGHT_STRING('ab' AS BINARY(1000000000000000000)));").Check(testkit.Rows("<nil>"))
