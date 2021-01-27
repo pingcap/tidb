cffe865658cd9cf1ae3342594a768d31b7330aea
jyz0309 <45495947@qq.com>
2021-01-27 16:18:35 +0800

    add test
    Signed-off-by: jyz0309 <45495947@qq.com>

--- types/convert_test.go
+++ types/convert_test.go
@@ -244,10 +244,10 @@ func (s *testTypeConvertSuite) TestConvertType(c *C) {
 	c.Assert(terror.ErrorEqual(err, ErrOverflow), IsTrue, Commentf("err %v", err))
 	c.Assert(v.(*MyDecimal).String(), Equals, "-9999.9999")
 	v, err = Convert("1,999.00", ft)
-	c.Assert(err, IsNil, Commentf(errors.ErrorStack(err)))
+	c.Assert(terror.ErrorEqual(err, ErrBadNumber), IsTrue, Commentf("err %v", err))
 	c.Assert(v.(*MyDecimal).String(), Equals, "1999.0000")
 	v, err = Convert("1,999,999.00", ft)
-	c.Assert(terror.ErrorEqual(err, ErrOverflow), IsTrue, Commentf("err %v", err))
+	c.Assert(terror.ErrorEqual(err, ErrBadNumber), IsTrue, Commentf("err %v", err))
 	c.Assert(v.(*MyDecimal).String(), Equals, "9999.9999")
 
 	// Test Datum.ToDecimal with bad number.
