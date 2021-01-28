1280374ce652e5228b3de15bfc53dd7c7d10cbac
wshwsh12 <793703860@qq.com>
2021-01-28 15:01:22 +0800

    add benchmark and test

--- executor/benchmark_test.go
+++ executor/benchmark_test.go
@@ -17,7 +17,6 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
-	"github.com/pingcap/tidb/executor/aggfuncs"
 	"io/ioutil"
 	"math/rand"
 	"os"
@@ -27,11 +26,11 @@ import (
 	"sync"
 	"testing"
 	"time"
-	"unsafe"
 
 	"github.com/pingcap/log"
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/tidb/executor/aggfuncs"
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/expression/aggregation"
 	"github.com/pingcap/tidb/planner/core"
@@ -2079,78 +2078,7 @@ func BenchmarkAggPartialResultMapperMemoryUsage(b *testing.B) {
 				for num := 0; num < c.rowNum; num++ {
 					aggMap[strconv.Itoa(num)] = tempSlice
 				}
-				if getB(aggMap) != c.expectedB {
-					fmt.Println(getB(aggMap))
-					panic("B is different from estimated B in aggPartialResultMapper")
-				}
-				if getGrowing(aggMap) != c.expectedGrowing {
-					panic("Growing is different from estimated growing in aggPartialResultMapper")
-				}
 			}
 		})
 	}
-}
-
-// A header for a Go map.
-type hmap struct {
-	// Note: the format of the hmap is also encoded in cmd/compile/internal/gc/reflect.go.
-	// Make sure this stays in sync with the compiler's definition.
-	count     int // # live cells == size of map.  Must be first (used by len() builtin)
-	flags     uint8
-	B         uint8  // log_2 of # of buckets (can hold up to loadFactor * 2^B items)
-	noverflow uint16 // approximate number of overflow buckets; see incrnoverflow for details
-	hash0     uint32 // hash seed
-
-	buckets    unsafe.Pointer // array of 2^B Buckets. may be nil if count==0.
-	oldbuckets unsafe.Pointer // previous bucket array of half the size, non-nil only when growing
-	nevacuate  uintptr        // progress counter for evacuation (buckets less than this have been evacuated)
-
-	extra *mapextra // optional fields
-}
-
-// mapextra holds fields that are not present on all maps.
-type mapextra struct {
-	// If both key and elem do not contain pointers and are inline, then we mark bucket
-	// type as containing no pointers. This avoids scanning such maps.
-	// However, bmap.overflow is a pointer. In order to keep overflow buckets
-	// alive, we store pointers to all overflow buckets in hmap.extra.overflow and hmap.extra.oldoverflow.
-	// overflow and oldoverflow are only used if key and elem do not contain pointers.
-	// overflow contains overflow buckets for hmap.buckets.
-	// oldoverflow contains overflow buckets for hmap.oldbuckets.
-	// The indirection allows to store a pointer to the slice in hiter.
-	overflow    *[]*bmap
-	oldoverflow *[]*bmap
-
-	// nextOverflow holds a pointer to a free overflow bucket.
-	nextOverflow *bmap
-}
-
-const (
-	bucketCntBits = 3
-	bucketCnt     = 1 << bucketCntBits
-)
-
-// A bucket for a Go map.
-type bmap struct {
-	// tophash generally contains the top byte of the hash value
-	// for each key in this bucket. If tophash[0] < minTopHash,
-	// tophash[0] is a bucket evacuation state instead.
-	tophash [bucketCnt]uint8
-	// Followed by bucketCnt keys and then bucketCnt elems.
-	// NOTE: packing all the keys together and then all the elems together makes the
-	// code a bit more complicated than alternating key/elem/key/elem/... but it allows
-	// us to eliminate padding which would be needed for, e.g., map[int64]int8.
-	// Followed by an overflow pointer.
-}
-
-func getB(m aggPartialResultMapper) int {
-	point := (**hmap)(unsafe.Pointer(&m))
-	value := *point
-	return int(value.B)
-}
-
-func getGrowing(m aggPartialResultMapper) bool {
-	point := (**hmap)(unsafe.Pointer(&m))
-	value := *point
-	return value.oldbuckets != nil
 }
--- executor/executor_pkg_test.go
+++ executor/executor_pkg_test.go
@@ -16,7 +16,10 @@ package executor
 import (
 	"context"
 	"crypto/tls"
+	"runtime"
+	"strconv"
 	"time"
+	"unsafe"
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/failpoint"
@@ -24,6 +27,7 @@ import (
 	"github.com/pingcap/parser/auth"
 	"github.com/pingcap/parser/mysql"
 	"github.com/pingcap/tidb/config"
+	"github.com/pingcap/tidb/executor/aggfuncs"
 	"github.com/pingcap/tidb/expression"
 	plannerutil "github.com/pingcap/tidb/planner/util"
 	"github.com/pingcap/tidb/sessionctx/variable"
@@ -445,4 +449,132 @@ func (s *pkgTestSuite) TestSlowQueryRuntimeStats(c *C) {
 	c.Assert(stats.String(), Equals, stats.Clone().String())
 	stats.Merge(stats.Clone())
 	c.Assert(stats.String(), Equals, "initialize: 2ms, read_file: 2s, parse_log: {time:200ms, concurrency:15}, total_file: 4, read_file: 4, read_size: 2 GB")
+}
+
+func (s *pkgTestSuite) TestAggPartialResultMapperB(c *C) {
+	if runtime.Version() < `go1.15` {
+		c.Skip("Unsupported version")
+	}
+	type testCase struct {
+		rowNum          int
+		expectedB       int
+		expectedGrowing bool
+	}
+	cases := []testCase{
+		{
+			rowNum:          0,
+			expectedB:       0,
+			expectedGrowing: false,
+		},
+		{
+			rowNum:          100,
+			expectedB:       4,
+			expectedGrowing: false,
+		},
+		{
+			rowNum:          10000,
+			expectedB:       11,
+			expectedGrowing: false,
+		},
+		{
+			rowNum:          1000000,
+			expectedB:       18,
+			expectedGrowing: false,
+		},
+		{
+			rowNum:          851968, // 6.5 * (1 << 17)
+			expectedB:       17,
+			expectedGrowing: false,
+		},
+		{
+			rowNum:          851969, // 6.5 * (1 << 17) + 1
+			expectedB:       18,
+			expectedGrowing: true,
+		},
+		{
+			rowNum:          425984, // 6.5 * (1 << 16)
+			expectedB:       16,
+			expectedGrowing: false,
+		},
+		{
+			rowNum:          425985, // 6.5 * (1 << 16) + 1
+			expectedB:       17,
+			expectedGrowing: true,
+		},
+	}
+
+	for _, tc := range cases {
+		aggMap := make(aggPartialResultMapper)
+		tempSlice := make([]aggfuncs.PartialResult, 10)
+		for num := 0; num < tc.rowNum; num++ {
+			aggMap[strconv.Itoa(num)] = tempSlice
+		}
+
+		c.Assert(getB(aggMap), Equals, tc.expectedB)
+		c.Assert(getGrowing(aggMap), Equals, tc.expectedGrowing)
+	}
+}
+
+// A header for a Go map.
+type hmap struct {
+	// Note: the format of the hmap is also encoded in cmd/compile/internal/gc/reflect.go.
+	// Make sure this stays in sync with the compiler's definition.
+	count     int // # live cells == size of map.  Must be first (used by len() builtin)
+	flags     uint8
+	B         uint8  // log_2 of # of buckets (can hold up to loadFactor * 2^B items)
+	noverflow uint16 // approximate number of overflow buckets; see incrnoverflow for details
+	hash0     uint32 // hash seed
+
+	buckets    unsafe.Pointer // array of 2^B Buckets. may be nil if count==0.
+	oldbuckets unsafe.Pointer // previous bucket array of half the size, non-nil only when growing
+	nevacuate  uintptr        // progress counter for evacuation (buckets less than this have been evacuated)
+
+	extra *mapextra // optional fields
+}
+
+// mapextra holds fields that are not present on all maps.
+type mapextra struct {
+	// If both key and elem do not contain pointers and are inline, then we mark bucket
+	// type as containing no pointers. This avoids scanning such maps.
+	// However, bmap.overflow is a pointer. In order to keep overflow buckets
+	// alive, we store pointers to all overflow buckets in hmap.extra.overflow and hmap.extra.oldoverflow.
+	// overflow and oldoverflow are only used if key and elem do not contain pointers.
+	// overflow contains overflow buckets for hmap.buckets.
+	// oldoverflow contains overflow buckets for hmap.oldbuckets.
+	// The indirection allows to store a pointer to the slice in hiter.
+	overflow    *[]*bmap
+	oldoverflow *[]*bmap
+
+	// nextOverflow holds a pointer to a free overflow bucket.
+	nextOverflow *bmap
+}
+
+const (
+	bucketCntBits = 3
+	bucketCnt     = 1 << bucketCntBits
+)
+
+// A bucket for a Go map.
+type bmap struct {
+	// tophash generally contains the top byte of the hash value
+	// for each key in this bucket. If tophash[0] < minTopHash,
+	// tophash[0] is a bucket evacuation state instead.
+	tophash [bucketCnt]uint8
+	// Followed by bucketCnt keys and then bucketCnt elems.
+	// NOTE: packing all the keys together and then all the elems together makes the
+	// code a bit more complicated than alternating key/elem/key/elem/... but it allows
+	// us to eliminate padding which would be needed for, e.g., map[int64]int8.
+	// Followed by an overflow pointer.
+}
+
+func getB(m aggPartialResultMapper) int {
+	point := (**hmap)(unsafe.Pointer(&m))
+	value := *point
+	return int(value.B)
+}
+
+func getGrowing(m aggPartialResultMapper) bool {
+	point := (**hmap)(unsafe.Pointer(&m))
+	value := *point
+	return value.oldbuckets != nil
 }
