6eb1ee4ddece96fa0d6bbe56621aae13481e3854
Reminiscent <y870414423@gmail.com>
2021-02-01 10:25:59 +0800

    update test results

--- util/ranger/ranger_test.go
+++ util/ranger/ranger_test.go
@@ -1444,6 +1444,7 @@ func (s *testRangerSuite) TestIndexRangeForBit(c *C) {
 	testKit := testkit.NewTestKit(c, store)
 	testKit.MustExec("use test;")
 	testKit.MustExec("set @@tidb_partition_prune_mode = 'static-only';")
+	testKit.MustExec("set @@tidb_executor_concurrency = 1;")
 	testKit.MustExec("drop table if exists t;")
 	testKit.MustExec("CREATE TABLE `t` (" +
 		"a bit(1) DEFAULT NULL," +
