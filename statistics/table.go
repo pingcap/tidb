2d717c12b3b4cc3f681690bb9cf858b4bbfacda0
Chengpeng Yan <41809508+Reminiscent@users.noreply.github.com>
2021-02-05 11:42:41 +0800

    statistics: merge the partition-level stats to global-level stats (#22667)

--- statistics/table.go
+++ statistics/table.go
@@ -197,6 +197,16 @@ func (t *Table) ColumnByName(colName string) *Column {
 	return nil
 }
 
+// GetStatsInfo returns their statistics according to the ID of the column or index, including histogram, CMSketch and TopN.
+func (t *Table) GetStatsInfo(ID int64, isIndex bool) (*Histogram, *CMSketch, *TopN) {
+	if isIndex {
+		idxStatsInfo := t.Indices[ID]
+		return idxStatsInfo.Histogram.Copy(), idxStatsInfo.CMSketch.Copy(), idxStatsInfo.TopN.Copy()
+	}
+	colStatsInfo := t.Columns[ID]
+	return colStatsInfo.Histogram.Copy(), colStatsInfo.CMSketch.Copy(), colStatsInfo.TopN.Copy()
+}
+
 type tableColumnID struct {
 	TableID  int64
 	ColumnID int64
