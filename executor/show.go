dc3a3e74aef45c72c6e1b5e5a9d1f945bd98bf19
xhe <xw897002528@gmail.com>
2021-02-23 15:36:48 +0800

    *: fix
    Signed-off-by: xhe <xw897002528@gmail.com>

--- executor/show.go
+++ executor/show.go
@@ -408,7 +408,7 @@ func (e *ShowExec) fetchShowTableStatus() error {
                data_free, auto_increment, create_time, update_time, check_time,
                table_collation, IFNULL(checksum,''), create_options, table_comment
                FROM information_schema.tables
-	       WHERE table_schema=%? ORDER BY table_name`, e.DBName)
+	       WHERE table_schema=%? ORDER BY table_name`, e.DBName.O)
 
 	rows, _, err := e.ctx.(sqlexec.RestrictedSQLExecutor).ExecRestrictedSQLWithSnapshot(e.ctx, sql)
 
