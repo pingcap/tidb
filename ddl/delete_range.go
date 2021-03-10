6b6f29c34f6736f5a426ebcd35bc183c03f4dd06
ailinkid <314806019@qq.com>
2021-03-10 09:54:56 +0800

    .
    Signed-off-by: ailinkid <314806019@qq.com>

--- ddl/delete_range.go
+++ ddl/delete_range.go
@@ -346,29 +346,6 @@ func insertJobIntoDeleteRangeTable(ctx sessionctx.Context, job *model.Job) error
 	return nil
 }
 
-<<<<<<< HEAD
-=======
-func doBatchDeleteIndiceRange(s sqlexec.SQLExecutor, jobID, tableID int64, indexIDs []int64, ts uint64) error {
-	logutil.BgLogger().Info("[ddl] batch insert into delete-range indices", zap.Int64("jobID", jobID), zap.Int64s("elementIDs", indexIDs))
-	paramsList := make([]interface{}, 0, len(indexIDs)*5)
-	var buf strings.Builder
-	buf.WriteString(insertDeleteRangeSQLPrefix)
-	for i, indexID := range indexIDs {
-		startKey := tablecodec.EncodeTableIndexPrefix(tableID, indexID)
-		endKey := tablecodec.EncodeTableIndexPrefix(tableID, indexID+1)
-		startKeyEncoded := hex.EncodeToString(startKey)
-		endKeyEncoded := hex.EncodeToString(endKey)
-		buf.WriteString(insertDeleteRangeSQLValue)
-		if i != len(indexIDs)-1 {
-			buf.WriteString(",")
-		}
-		paramsList = append(paramsList, jobID, indexID, startKeyEncoded, endKeyEncoded, ts)
-	}
-	_, err := s.ExecuteInternal(context.Background(), buf.String(), paramsList...)
-	return errors.Trace(err)
-}
-
->>>>>>> dedaabb80... ddl: migrate part of ddl package code from Execute/ExecRestricted to safe API (2) (#22729)
 func doInsert(s sqlexec.SQLExecutor, jobID int64, elementID int64, startKey, endKey kv.Key, ts uint64) error {
 	logutil.BgLogger().Info("[ddl] insert into delete-range table", zap.Int64("jobID", jobID), zap.Int64("elementID", elementID))
 	startKeyEncoded := hex.EncodeToString(startKey)
