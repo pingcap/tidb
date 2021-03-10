c502def354a04a05262f04d74879f18bc2dbd740
Nayuta Yanagisawa <nayuta.yanagisawa@hey.com>
2021-03-10 22:42:11 +0900

    [dumpling] [parser] parser: regard NULL as literal when normalizing query (#1166)
    * parser: regard NULL as literal when normalizing queries
    
    Currently, Normalize() converts "SELECT NULL" to "select null", while NULL is
    regarded as a literal in MySQL, as described in the documentation.
    https://dev.mysql.com/doc/refman/8.0/en/literals.html
    It seems natural that Normalize() would convert "SELECT NULL" to "select ?"
    from the consistency viewpoint.
    
    * Handle case of 'select `null`'
    
    * Fix quotation
    
    Co-authored-by: Arenatlx <ailinsilence4@gmail.com>
    Co-authored-by: kennytm <kennytm@gmail.com>

--- dumpling/parser/digester_test.go
+++ dumpling/parser/digester_test.go
@@ -30,6 +30,9 @@ func (s *testSQLDigestSuite) TestNormalize(c *C) {
 	}{
 		{"select _utf8mb4'123'", "select (_charset) ?"},
 		{"SELECT 1", "select ?"},
+		{"select null", "select ?"},
+		{"select \\N", "select ?"},
+		{"SELECT `null`", "select `null`"},
 		{"select * from b where id = 1", "select * from `b` where `id` = ?"},
 		{"select 1 from b where id in (1, 3, '3', 1, 2, 3, 4)", "select ? from `b` where `id` in ( ... )"},
 		{"select 1 from b where id in (1, a, 4)", "select ? from `b` where `id` in ( ? , `a` , ? )"},
