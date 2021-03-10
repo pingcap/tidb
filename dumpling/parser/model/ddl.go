983f694c038adce043bc963b0fe0fea601ac805e
欧冰 <32380374+ou-bing@users.noreply.github.com>
2021-03-10 18:09:14 +0800

    [dumpling] [parser] add ActionType: ActionDropIndexes (#1186)

--- dumpling/parser/model/ddl.go
+++ dumpling/parser/model/ddl.go
@@ -78,6 +78,7 @@ const (
 	ActionAlterCheckConstraint          ActionType = 45
 	ActionAlterTableAlterPartition      ActionType = 46
 	ActionRenameTables                  ActionType = 47
+	ActionDropIndexes                   ActionType = 48
 )
 
 const (
@@ -133,6 +134,7 @@ var actionMap = map[ActionType]string{
 	ActionDropCheckConstraint:           "drop check constraint",
 	ActionAlterCheckConstraint:          "alter check constraint",
 	ActionAlterTableAlterPartition:      "alter partition",
+	ActionDropIndexes:                   "drop multi-indexes",
 }
 
 // String return current ddl action in string
