6accc1f0c5ad70d19f4632558f62a7f09b8b2194
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-03-11 14:14:58 +0800

    [dumpling] statistics: add a test case which builds global-stats on different versions of partition-stats (#23231)

--- dumpling/statistics/handle/handle_test.go
+++ dumpling/statistics/handle/handle_test.go
@@ -1326,18 +1326,30 @@ partition by range (a) (
 	c.Assert(s.do.StatsHandle().DumpStatsDeltaToKV(handle.DumpAll), IsNil)
 
 	tk.MustExec("set @@tidb_partition_prune_mode='static'")
-	tk.MustExec("analyze table t")
-	c.Assert(len(tk.MustQuery("show stats_meta").Rows()), Equals, 2) // p0 + p1
+	tk.MustExec("set @@tidb_analyze_version=1")
+	tk.MustExec("analyze table t") // both p0 and p1 are in ver1
+	c.Assert(len(tk.MustQuery("show stats_meta").Rows()), Equals, 2)
 
 	tk.MustExec("set @@tidb_partition_prune_mode='dynamic'")
 	tk.MustExec("set @@tidb_analyze_version=1")
-	err := tk.ExecToErr("analyze table t")
+	err := tk.ExecToErr("analyze table t") // try to build global-stats on ver1
 	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "[stats]: global statistics for partitioned tables only available in statistics version2, please set tidb_analyze_version to 2")
+	c.Assert(err.Error(), Equals, "[stats]: some partition level statistics are not in statistics version 2, please set tidb_analyze_version to 2 and analyze the this table")
 
+	tk.MustExec("set @@tidb_partition_prune_mode='dynamic'")
 	tk.MustExec("set @@tidb_analyze_version=2")
-	tk.MustExec("analyze table t")
-	c.Assert(len(tk.MustQuery("show stats_meta").Rows()), Equals, 3) // p0 + p1 + global
+	err = tk.ExecToErr("analyze table t partition p1") // only analyze p1 to let it in ver2 while p0 is in ver1
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "[stats]: some partition level statistics are not in statistics version 2, please set tidb_analyze_version to 2 and analyze the this table")
+
+	tk.MustExec("analyze table t") // both p0 and p1 are in ver2
+	c.Assert(len(tk.MustQuery("show stats_meta").Rows()), Equals, 3)
+
+	tk.MustExec("alter table t add partition (partition p2 values less than (30))")
+	tk.MustExec("insert t values (13), (14)")
+	c.Assert(s.do.StatsHandle().DumpStatsDeltaToKV(handle.DumpAll), IsNil)
+	tk.MustExec("analyze table t partition p2")                      // it will success since p0 and p1 are both in ver2
+	c.Assert(len(tk.MustQuery("show stats_meta").Rows()), Equals, 4) // p0, p1, p2 and global
 }
 
 func (s *testStatsSuite) TestExtendedStatsDefaultSwitch(c *C) {
@@ -1585,7 +1597,7 @@ func (s *testStatsSuite) TestPartitionPruneModeSessionVariable(c *C) {
 	tk2.MustExec("set @@tidb_partition_prune_mode = '" + string(variable.Static) + "'")
 	tk2.MustExec(`set @@tidb_analyze_version=2`)
 
-	tk1.MustExec(`create table t (a int, key(a)) partition by range(a) 
+	tk1.MustExec(`create table t (a int, key(a)) partition by range(a)
 					(partition p0 values less than (10),
 					partition p1 values less than (22))`)
 
