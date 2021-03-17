bce34f66516d8bedb27af85dad257bac6790af04
Kenan Yao <cauchy1992@gmail.com>
2021-03-17 16:52:55 +0800

    cherry pick #23238 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/find_best_task.go
+++ planner/core/find_best_task.go
@@ -528,7 +528,7 @@ func (ds *DataSource) skylinePruning(prop *property.PhysicalProperty) []*candida
 			continue
 		}
 		// if we already know the range of the scan is empty, just return a TableDual
-		if len(path.Ranges) == 0 && !ds.ctx.GetSessionVars().StmtCtx.UseCache {
+		if len(path.Ranges) == 0 {
 			return []*candidatePath{{path: path}}
 		}
 		if path.StoreType != kv.TiFlash && prop.IsFlashProp() {
