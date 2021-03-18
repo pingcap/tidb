d370409faa0ddc3899859e34070e1ef236bde878
YIXIAO SHI <aieruishi@gmail.com>
2021-03-18 19:37:36 +0800

    store/tikv: remove tidb/metrics from store/tikv (#23302)

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -46,9 +46,12 @@ const (
 	opSucc   = "ok"
 	opFailed = "err"
 
+	TiDB         = "tidb"
 	LabelScope   = "scope"
 	ScopeGlobal  = "global"
 	ScopeSession = "session"
+	Server       = "server"
+	TiKVClient   = "tikvclient"
 )
 
 // RetLabel returns "ok" when err == nil and "err" when err != nil.
@@ -139,7 +142,6 @@ func RegisterMetrics() {
 	prometheus.MustRegister(TotalCopProcHistogram)
 	prometheus.MustRegister(TotalCopWaitHistogram)
 	prometheus.MustRegister(HandleSchemaValidate)
-	prometheus.MustRegister(GRPCConnTransientFailureCounter)
 	prometheus.MustRegister(MaxProcs)
 	prometheus.MustRegister(GOGC)
 	prometheus.MustRegister(ConnIdleDurationHistogram)
@@ -147,6 +149,7 @@ func RegisterMetrics() {
 	prometheus.MustRegister(TokenGauge)
 	prometheus.MustRegister(ConfigStatus)
 
-	tikvmetrics.InitMetrics("tidb", "tikvclient")
+	tikvmetrics.InitMetrics(TiDB, TiKVClient)
 	tikvmetrics.RegisterMetrics()
+	tikvmetrics.TiKVPanicCounter = PanicCounter // reset tidb metrics for tikv metrics
 }
