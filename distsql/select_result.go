ae979d493d60c786b9a4e6204e5739acce536583
wshwsh12 <793703860@qq.com>
2021-03-26 17:16:59 +0800

    fmt

--- distsql/select_result.go
+++ distsql/select_result.go
@@ -17,8 +17,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/pingcap/tidb/util/logutil"
-	"go.uber.org/zap"
 	"sort"
 	"strconv"
 	"sync/atomic"
@@ -39,8 +37,10 @@ import (
 	"github.com/pingcap/tidb/util/codec"
 	"github.com/pingcap/tidb/util/dbterror"
 	"github.com/pingcap/tidb/util/execdetails"
+	"github.com/pingcap/tidb/util/logutil"
 	"github.com/pingcap/tidb/util/memory"
 	"github.com/pingcap/tipb/go-tipb"
+	"go.uber.org/zap"
 )
 
 var (
