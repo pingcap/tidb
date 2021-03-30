4cc90f021f16f82125a5100eb969c64861333f31
Chengpeng Yan <41809508+Reminiscent@users.noreply.github.com>
2021-03-30 18:03:24 +0800

    cherry pick #23651 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/rule_partition_processor.go
+++ planner/core/rule_partition_processor.go
@@ -266,6 +266,10 @@ func (or partitionRangeOR) union(x partitionRangeOR) partitionRangeOR {
 }
 
 func (or partitionRangeOR) simplify() partitionRangeOR {
+	// if the length of the `or` is zero. We should return early.
+	if len(or) == 0 {
+		return or
+	}
 	// Make the ranges order by start.
 	sort.Sort(or)
 	sorted := or
