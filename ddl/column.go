9a10ef732da6da52c71f58407851a9f211bca817
tangenta <tangenta@126.com>
2021-04-02 09:25:22 +0800

    make fmt

--- ddl/column.go
+++ ddl/column.go
@@ -797,7 +797,7 @@ func migrateAutoIDToAutoRandID(d *ddlCtx, t *meta.Meta, dbInfo *model.DBInfo, tb
 		if err != nil {
 			return errors.Trace(err)
 		}
-		err = alloc.Rebase(tblInfo.ID, newBase - 1, false)
+		err = alloc.Rebase(tblInfo.ID, newBase-1, false)
 		if err != nil {
 			return errors.Trace(err)
 		}
