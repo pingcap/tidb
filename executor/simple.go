fc4e4061227611533f632249812ee02cecadb6c2
lihaowei <haoweili35@gmail.com>
2021-04-06 10:46:33 +0800

    fix issue #23649
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- executor/simple.go
+++ executor/simple.go
@@ -973,7 +973,7 @@ func (e *SimpleExec) executeGrantRole(s *ast.GrantRoleStmt) error {
 			return err
 		}
 		if !exists {
-			return ErrCannotUser.GenWithStackByArgs("GRANT ROLE", role.String())
+			return ErrGrantRole.GenWithStackByArgs(role.String())
 		}
 	}
 	for _, user := range s.Users {
