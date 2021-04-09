6d2827c73238d001b6635a7dbd24cd5ba05d31f5
yisaer <disxiaofei@163.com>
2021-04-09 15:51:00 +0800

    fix error

--- store/mockstore/unistore/tikv/server.go
+++ store/mockstore/unistore/tikv/server.go
@@ -15,6 +15,7 @@ package tikv
 
 import (
 	"context"
+	"github.com/pingcap/kvproto/pkg/coprocessor_v2"
 	"io"
 	"sync/atomic"
 	"time"
@@ -1018,6 +1019,21 @@ func (svr *Server) VerDeleteRange(context.Context, *kvrpcpb.VerDeleteRangeReques
 // CheckLeader implements implements the tikvpb.TikvServer interface.
 func (svr *Server) CheckLeader(context.Context, *kvrpcpb.CheckLeaderRequest) (*kvrpcpb.CheckLeaderResponse, error) {
 	panic("unimplemented")
+}
+
+// RawCompareAndSwap implements the tikvpb.TikvServer interface.
+func (svr *Server) RawCompareAndSwap(context.Context, *kvrpcpb.RawCASRequest) (*kvrpcpb.RawCASResponse, error) {
+	panic("implement me")
+}
+
+// CoprocessorV2 implements the tikvpb.TikvServer interface.
+func (svr *Server) CoprocessorV2(context.Context, *coprocessor_v2.RawCoprocessorRequest) (*coprocessor_v2.RawCoprocessorResponse, error) {
+	panic("implement me")
+}
+
+// GetStoreSafeTS implements the tikvpb.TikvServer interface.
+func (svr *Server) GetStoreSafeTS(context.Context, *kvrpcpb.StoreSafeTSRequest) (*kvrpcpb.StoreSafeTSResponse, error) {
+	return &kvrpcpb.StoreSafeTSResponse{}, nil
 }
 
 func convertToKeyError(err error) *kvrpcpb.KeyError {
