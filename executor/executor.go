7e416fc82cc847b87fd7e4a2e8f8eec718beffec
ken <ikenchina@gmail.com>
2021-04-12 16:37:11 +0800

    executor: checking chunk is full precedes filtering

--- executor/executor.go
+++ executor/executor.go
@@ -1250,12 +1250,14 @@ func (e *SelectionExec) Next(ctx context.Context, req *chunk.Chunk) error {
 
 	for {
 		for ; e.inputRow != e.inputIter.End(); e.inputRow = e.inputIter.Next() {
-			if !e.selected[e.inputRow.Idx()] {
-				continue
-			}
 			if req.IsFull() {
 				return nil
 			}
+
+			if !e.selected[e.inputRow.Idx()] {
+				continue
+			}
+
 			req.AppendRow(e.inputRow)
 		}
 		mSize := e.childResult.MemoryUsage()
