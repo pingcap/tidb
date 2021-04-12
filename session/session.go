7d9485a73132720ff1fa1b8aeca137c59844beca
rebelice <yangrebelice@gmail.com>
2021-04-12 17:29:12 +0800

    fix unit test

--- session/session.go
+++ session/session.go
@@ -2092,9 +2092,9 @@ func CreateSessionWithOpt(store kv.Storage, opt *Opt) (Session, error) {
 	// which periodically updates stats using the collected data.
 	if do.StatsHandle() != nil && do.StatsUpdating() {
 		s.statsCollector = do.StatsHandle().NewSessionStatsCollector()
-//		if GetIndexUsageSyncLease() > 0 {
+		if GetIndexUsageSyncLease() > 0 {
 			s.idxUsageCollector = do.StatsHandle().NewSessionIndexUsageCollector()
-//		}
+		}
 	}
 
 	return s, nil
