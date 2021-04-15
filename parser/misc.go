9ea01fd2440665601cf458df56367c5314beb590
wjHuang <huangwenjun1997@gmail.com>
2021-04-15 16:19:31 +0800

    [parser] support CTE clause (#1207)

--- parser/misc.go
+++ parser/misc.go
@@ -549,6 +549,7 @@ var tokenMap = map[string]int{
 	"REBUILD":                  rebuild,
 	"RECENT":                   recent,
 	"RECOVER":                  recover,
+	"RECURSIVE":                recursive,
 	"REDUNDANT":                redundant,
 	"REFERENCES":               references,
 	"REGEXP":                   regexpKwd,
