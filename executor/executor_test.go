f5cac305b0c62ddc050a0384da2df85c7fab9acf
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-04-16 22:25:51 +0800

    planner: donot prune all columns for Projection (#24024) (#24093)

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -6892,12 +6892,12 @@ func (s *testSuiteP2) TestApplyCache(c *C) {
 	tk.MustExec("insert into t values (1),(1),(1),(1),(1),(1),(1),(1),(1);")
 	tk.MustExec("analyze table t;")
 	result := tk.MustQuery("explain analyze SELECT count(1) FROM (SELECT (SELECT min(a) FROM t as t2 WHERE t2.a > t1.a) AS a from t as t1) t;")
-	c.Assert(result.Rows()[1][0], Equals, "└─Apply_39")
+	c.Assert(result.Rows()[2][0], Equals, "  └─Apply_41")
 	var (
 		ind  int
 		flag bool
 	)
-	value := (result.Rows()[1][5]).(string)
+	value := (result.Rows()[2][5]).(string)
 	for ind = 0; ind < len(value)-5; ind++ {
 		if value[ind:ind+5] == "cache" {
 			flag = true
@@ -6912,9 +6912,9 @@ func (s *testSuiteP2) TestApplyCache(c *C) {
 	tk.MustExec("insert into t values (1),(2),(3),(4),(5),(6),(7),(8),(9);")
 	tk.MustExec("analyze table t;")
 	result = tk.MustQuery("explain analyze SELECT count(1) FROM (SELECT (SELECT min(a) FROM t as t2 WHERE t2.a > t1.a) AS a from t as t1) t;")
-	c.Assert(result.Rows()[1][0], Equals, "└─Apply_39")
+	c.Assert(result.Rows()[2][0], Equals, "  └─Apply_41")
 	flag = false
-	value = (result.Rows()[1][5]).(string)
+	value = (result.Rows()[2][5]).(string)
 	for ind = 0; ind < len(value)-5; ind++ {
 		if value[ind:ind+5] == "cache" {
 			flag = true
