9dccc3523a2419ac01e8fcb7dc0e2445df8ea2df
disksing <i@disksing.com>
2021-04-19 00:13:32 +0800

    fix warning
    Signed-off-by: disksing <i@disksing.com>

--- store/mockstore/mocktikv/puppet.go
+++ store/mockstore/mocktikv/puppet.go
@@ -17,13 +17,29 @@ import "github.com/pingcap/tidb/store/tikv/mockstore/mocktikv"
 
 // Type alias to keep br working. Remove it after BR updates.
 type (
+	// MVCCStore is a mvcc key-value storage.
 	MVCCStore = mocktikv.MVCCStore
-	Cluster   = mocktikv.Cluster
+	// Cluster simulates a TiKV cluster. It focuses on management and the change of
+	// meta data. A Cluster mainly includes following 3 kinds of meta data:
+	// 1) Region: A Region is a fragment of TiKV's data whose range is [start, end).
+	//    The data of a Region is duplicated to multiple Peers and distributed in
+	//    multiple Stores.
+	// 2) Peer: A Peer is a replica of a Region's data. All peers of a Region form
+	//    a group, each group elects a Leader to provide services.
+	// 3) Store: A Store is a storage/service node. Try to think it as a TiKV server
+	//    process. Only the store with request's Region's leader Peer could respond
+	//    to client's request.
+	Cluster = mocktikv.Cluster
 )
 
 // Variable alias to keep br working. Remove it after BR updates.
 var (
+	// MustNewMVCCStore is used for testing, use NewMVCCLevelDB instead.
 	MustNewMVCCStore = mocktikv.MustNewMVCCStore
-	NewCluster       = mocktikv.NewCluster
-	NewPDClient      = mocktikv.NewPDClient
+	// NewCluster creates an empty cluster. It needs to be bootstrapped before
+	// providing service.
+	NewCluster = mocktikv.NewCluster
+	// NewPDClient creates a mock pd.Client that uses local timestamp and meta data
+	// from a Cluster.
+	NewPDClient = mocktikv.NewPDClient
 )
