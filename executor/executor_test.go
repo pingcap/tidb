7cb48bdd890477035585c3c7996de35400c00f21
tangenta <tangenta@126.com>
2021-04-21 20:23:59 +0800

    import package errno

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -42,6 +42,7 @@ import (
 	"github.com/pingcap/tidb/ddl"
 	"github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/domain/infosync"
+	"github.com/pingcap/tidb/errno"
 	"github.com/pingcap/tidb/executor"
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/infoschema"
