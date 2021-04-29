fc1f0cd372cef63cc3c0d8f62cf63435f96bf092
disksing <i@disksing.com>
2021-04-29 18:53:40 +0800

    store/tikv: remove use of ReplicaRead transaction option in store/tikv
    Signed-off-by: disksing <i@disksing.com>

--- store/driver/txn/snapshot.go
+++ store/driver/txn/snapshot.go
@@ -73,6 +73,8 @@ func (s *tikvSnapshot) SetOption(opt int, val interface{}) {
 		s.KVSnapshot.SetNotFillCache(val.(bool))
 	case tikvstore.SnapshotTS:
 		s.KVSnapshot.SetSnapshotTS(val.(uint64))
+	case tikvstore.ReplicaRead:
+		s.KVSnapshot.SetReplicaRead(val.(tikvstore.ReplicaReadType))
 	default:
 		s.KVSnapshot.SetOption(opt, val)
 	}
