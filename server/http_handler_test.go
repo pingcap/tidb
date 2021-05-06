5952b8de41502af8d1d2622a7dfcf44ca915e4a8
tangenta <tangenta@126.com>
2021-05-06 20:30:52 +0800

    server: add MVCC API for secondary index with common handle

--- server/http_handler_test.go
+++ server/http_handler_test.go
@@ -552,7 +552,7 @@ partition by range (a)
 	err = txn2.Commit()
 	c.Assert(err, IsNil)
 	dbt.mustExec("drop table if exists t")
-	dbt.mustExec("create table t (a double, b varchar(20), c int, primary key(a,b) clustered)")
+	dbt.mustExec("create table t (a double, b varchar(20), c int, primary key(a,b) clustered, key idx(c))")
 	dbt.mustExec("insert into t values(1.1,'111',1),(2.2,'222',2)")
 }
 
@@ -953,6 +953,10 @@ func (ts *HTTPHandlerTestSuite) TestGetIndexMVCC(c *C) {
 	decodeKeyMvcc(resp.Body, c, true)
 
 	resp, err = ts.fetchStatus("/mvcc/index/tidb/test/idx2/3?a=4&b=")
+	c.Assert(err, IsNil)
+	decodeKeyMvcc(resp.Body, c, true)
+
+	resp, err = ts.fetchStatus("/mvcc/index/tidb/t/idx?a=1.1&b=111&c=1")
 	c.Assert(err, IsNil)
 	decodeKeyMvcc(resp.Body, c, true)
 
