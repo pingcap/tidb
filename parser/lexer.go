64cc6f7d5599c2af389a1eb45ed20825fa23f968
xiongjiwei <xiongjiwei1996@outlook.com>
2021-05-08 15:10:14 +0800

    [parser] lexer: fix lexer get wrong token (#1222)

--- parser/lexer.go
+++ parser/lexer.go
@@ -171,6 +171,7 @@ func (s *Scanner) Lex(v *yySymType) int {
 		tok == stringLit &&
 		s.r.s[v.offset] == '"' {
 		tok = identifier
+		s.identifierDot = false
 	}
 
 	if tok == pipes && !(s.sqlMode.HasPipesAsConcatMode()) {
