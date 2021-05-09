74676c57146c45cbafe6c9f50e52803e0d7c77a5
lihaowei <haoweili35@gmail.com>
2021-05-09 10:21:42 +0800

    change table name

--- executor/admin_test.go
+++ executor/admin_test.go
@@ -79,11 +79,11 @@ func (s *testSuite5) TestAdminCheckIndex(c *C) {
 func (s *testSuite5) TestAdminCheckIndexInTemporaryMode(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
-	tk.MustExec("drop table if exists admin_test;")
-	tk.MustExec("create global temporary table admin_test (c1 int, c2 int, c3 int default 1, primary key (c1), index (c1), unique key(c2)) ON COMMIT DELETE ROWS;")
-	tk.MustExec("insert admin_test (c1, c2) values (1, 1), (2, 2), (3, 3);")
-	tk.MustGetErrCode("admin check table admin_test;", mysql.ErrAdminCheckTable)
-	tk.MustExec("drop table if exists admin_test;")
+	tk.MustExec("drop table if exists temporary_admin_test;")
+	tk.MustExec("create global temporary table temporary_admin_test (c1 int, c2 int, c3 int default 1, primary key (c1), index (c1), unique key(c2)) ON COMMIT DELETE ROWS;")
+	tk.MustExec("insert temporary_admin_test (c1, c2) values (1, 1), (2, 2), (3, 3);")
+	tk.MustGetErrCode("admin check table temporary_admin_test;", mysql.ErrAdminCheckTable)
+	tk.MustExec("drop table if exists temporary_admin_test;")
 }
 
 func (s *testSuite5) TestAdminRecoverIndex(c *C) {
