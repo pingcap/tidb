9fcb66f579164cf09670fce0c5b175b503dd27a6
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-05-12 15:49:40 +0800

    store/tikv: fix wrong unit of TiKVTokenWaitDuration (#23720) (#23775)

--- store/tikv/2pc.go
+++ store/tikv/2pc.go
@@ -1926,6 +1926,6 @@ func (batchExe *batchExecutor) process(batches []batchMutations) error {
 		}
 	}
 	close(exitCh)
-	metrics.TiKVTokenWaitDuration.Observe(batchExe.tokenWaitDuration.Seconds())
+	metrics.TiKVTokenWaitDuration.Observe(float64(batchExe.tokenWaitDuration.Nanoseconds()))
 	return err
 }
