dad15384228e0726beb46bb5bed5a5b7ddcabecc
Zhi Qi <30543181+LittleFall@users.noreply.github.com>
2021-05-12 18:05:40 +0800

    planner: support set tidb_allow_mpp to `2` or `ENFORCE` to enforce use mpp mode. (#24516)

--- planner/core/task.go
+++ planner/core/task.go
@@ -2007,10 +2007,16 @@ func (t *mppTask) convertToRootTaskImpl(ctx sessionctx.Context) *rootTask {
 		StoreType: kv.TiFlash,
 	}.Init(ctx, t.p.SelectBlockOffset())
 	p.stats = t.p.statsInfo()
-	return &rootTask{
-		p:   p,
-		cst: t.cst / p.ctx.GetSessionVars().CopTiFlashConcurrencyFactor,
+
+	p.cost = t.cst / p.ctx.GetSessionVars().CopTiFlashConcurrencyFactor
+	if p.ctx.GetSessionVars().IsMPPEnforced() {
+		p.cost = 0
 	}
+	rt := &rootTask{
+		p:   p,
+		cst: p.cost,
+	}
+	return rt
 }
 
 func (t *mppTask) needEnforce(prop *property.PhysicalProperty) bool {
