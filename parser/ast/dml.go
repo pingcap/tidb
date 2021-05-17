2025bc4d3e88236e1f9b22e51b56901377e7b7b3
ShuNing <nolouch@gmail.com>
2021-05-17 14:03:38 +0800

    [parser] ast: make AsOf node be accepted (#1226)
    * ast: make AsOf node be accepted
    
    Signed-off-by: nolouch <nolouch@gmail.com>

--- parser/ast/dml.go
+++ parser/ast/dml.go
@@ -418,6 +418,13 @@ func (n *TableName) Accept(v Visitor) (Node, bool) {
 		}
 		n.TableSample = newTs.(*TableSample)
 	}
+	if n.AsOf != nil {
+		newNode, skipChildren := n.AsOf.Accept(v)
+		if skipChildren {
+			return v.Leave(n)
+		}
+		n.AsOf = newNode.(*AsOfClause)
+	}
 	return v.Leave(n)
 }
 
