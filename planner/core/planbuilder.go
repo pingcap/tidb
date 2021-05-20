1e05961c814ee230becbe544314783ed7cb7180f
wjhuang2016 <huangwenjun1997@gmail.com>
2021-05-20 16:27:04 +0800

    clean
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- planner/core/planbuilder.go
+++ planner/core/planbuilder.go
@@ -441,9 +441,6 @@ type PlanBuilder struct {
 	outerSchemas []*expression.Schema
 	outerNames   [][]*types.FieldName
 	outerCTEs    []*cteInfo
-	cteID2Plan   map[int]LogicalPlan
-	// outerRecursiveCheck
-	outerRecursiveCheck []bool
 	// colMapper stores the column that must be pre-resolved.
 	colMapper map[*ast.ColumnNameExpr]int
 	// visitInfo is used for privilege check.
@@ -613,8 +610,6 @@ func NewPlanBuilder(sctx sessionctx.Context, is infoschema.InfoSchema, processor
 		ctx:                 sctx,
 		is:                  is,
 		outerCTEs:           make([]*cteInfo, 0),
-		cteID2Plan:          make(map[int]LogicalPlan),
-		outerRecursiveCheck: make([]bool, 0),
 		colMapper:           make(map[*ast.ColumnNameExpr]int),
 		handleHelper:        &handleColHelper{id2HandleMapStack: make([]map[int64][]HandleCols, 0)},
 		hintProcessor:       processor,
