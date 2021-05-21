2585e8b0750cc0119dc9d27e86e6b51c1c254a12
lihaowei <haoweili35@gmail.com>
2021-05-21 20:41:55 +0800

    fmt
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- ddl/error.go
+++ ddl/error.go
@@ -279,7 +279,7 @@ var (
 	ErrPartitionNoTemporary = dbterror.ClassDDL.NewStd(mysql.ErrPartitionNoTemporary)
 
 	ErrOptOnTemporaryTable = dbterror.ClassDDL.NewStd(mysql.ErrOptOnTemporaryTable)
-  
+
 	errUnsupportedOnCommitPreserve = dbterror.ClassDDL.NewStdErr(mysql.ErrUnsupportedDDLOperation, parser_mysql.Message("TiDB doesn't support ON COMMIT PRESERVE ROWS for now", nil))
 	errUnsupportedEngineTemporary  = dbterror.ClassDDL.NewStdErr(mysql.ErrUnsupportedDDLOperation, parser_mysql.Message("TiDB doesn't support this kind of engine for temporary table", nil))
 )
