b53b32ac41052837a7314ed6a7537f09d9990cc3
JmPotato <ghzpotato@gmail.com>
2021-05-24 16:41:00 +0800

    Fix a typo
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- planner/core/common_plans.go
+++ planner/core/common_plans.go
@@ -724,7 +724,7 @@ type Simple struct {
 	//   Used for `global kill`. See https://github.com/pingcap/tidb/blob/master/docs/design/2020-06-01-global-kill.md.
 	IsFromRemote bool
 
-	// StaleTxnStartTS the StartTS that is used to build a staleness transaction by 'START TRANSACTION READ ONLY' statement.
+	// StaleTxnStartTS is the StartTS that is used to build a staleness transaction by 'START TRANSACTION READ ONLY' statement.
 	StaleTxnStartTS uint64
 }
 
