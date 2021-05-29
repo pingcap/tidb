d283fa41f98e8b45224440868bec29f3c4b3eca5
wshwsh12 <793703860@qq.com>
2021-05-29 13:54:39 +0800

    empty commit

--- executor/aggfuncs/func_max_min.go
+++ executor/aggfuncs/func_max_min.go
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under  the License.
 
 package aggfuncs
 
