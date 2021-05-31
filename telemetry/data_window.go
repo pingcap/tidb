1c333931520c88906674fd05c3ce5c535e1c22fd
yinweiling <2442001477@qq.com>
2021-05-31 16:41:14 +0800

    add SQL_3.1.1&3.1.3

--- telemetry/data_window.go
+++ telemetry/data_window.go
@@ -14,10 +14,20 @@
 package telemetry
 
 import (
+	"context"
+	"math"
 	"sync"
 	"time"
 
+	"github.com/pingcap/tidb/domain/infosync"
+
+	// "github.com/pingcap/tidb/types"
+	"github.com/pingcap/errors"
+	"github.com/prometheus/client_golang/api"
+	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	pmodel "github.com/prometheus/common/model"
 	"go.uber.org/atomic"
+	// "go.uber.org/zap"
 )
 
 var (
@@ -51,6 +61,7 @@ const (
 
 	maxSubWindowLength         = int(ReportInterval / SubWindowSize) // TODO: Ceiling?
 	maxSubWindowLengthInWindow = int(WindowSize / SubWindowSize)     // TODO: Ceiling?
+	promReadTimeout            = time.Second * 10
 )
 
 type windowData struct {
@@ -58,6 +69,26 @@ type windowData struct {
 	ExecuteCount   uint64             `json:"executeCount"`
 	TiFlashUsage   tiFlashUsageData   `json:"tiFlashUsage"`
 	CoprCacheUsage coprCacheUsageData `json:"coprCacheUsage"`
+	SQLUsage       sqlUsageData       `json:"SQLUsage"`
+}
+
+type sqlType struct {
+	Use      int64 `json:"Use"`
+	Show     int64 `json:"Show"`
+	Begin    int64 `json:"Begin"`
+	Commit   int64 `json:"Commit"`
+	Rollback int64 `json:"Rollback"`
+	Insert   int64 `json:"Insert"`
+	Replace  int64 `json:"Replace"`
+	Delete   int64 `json:"Delete"`
+	Update   int64 `json:"Update"`
+	Select   int64 `json:"Select"`
+	Other    int64 `json:"Other"`
+}
+
+type sqlUsageData struct {
+	SQLTotal int64   `json:"total"`
+	SQLType  sqlType `json:"type"`
 }
 
 type coprCacheUsageData struct {
@@ -80,7 +111,128 @@ var (
 	subWindowsLock    = sync.RWMutex{}
 )
 
+func getSQLSum(sqlTypeDeta *sqlType) int64 {
+	result := sqlTypeDeta.Use
+	result += sqlTypeDeta.Show
+	result += sqlTypeDeta.Begin
+	result += sqlTypeDeta.Commit
+	result += sqlTypeDeta.Rollback
+	result += sqlTypeDeta.Insert
+	result += sqlTypeDeta.Replace
+	result += sqlTypeDeta.Delete
+	result += sqlTypeDeta.Update
+	result += sqlTypeDeta.Select
+	result += sqlTypeDeta.Other
+	return result
+}
+
+func readSQLMetric(timepoint time.Time, SQLresult sqlUsageData) (sqlUsageData, error) {
+	ctx := context.TODO()
+	quantile := 0.99
+
+	result, err := querySQLMetric(ctx, timepoint, quantile)
+	if err != nil {
+		if err1, ok := err.(*promv1.Error); ok {
+			return SQLresult, errors.Errorf("query metric error, msg: %v, detail: %v", err1.Msg, err1.Detail)
+		}
+		return SQLresult, errors.Errorf("query metric error: %v", err.Error())
+	}
+
+	dataAnylis(result, &SQLresult)
+	return SQLresult, nil
+
+}
+
+func querySQLMetric(ctx context.Context, queryTime time.Time, quantile float64) (result pmodel.Value, err error) {
+	// Add retry to avoid network error.
+	var prometheusAddr string
+	for i := 0; i < 5; i++ {
+		//TODO: the prometheus will be Integrated into the PD, then we need to query the prometheus in PD directly, which need change the quire API
+		prometheusAddr, err = infosync.GetPrometheusAddr()
+		if err == nil || err == infosync.ErrPrometheusAddrIsNotSet {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	if err != nil {
+		return nil, err
+	}
+	promClient, err := api.NewClient(api.Config{
+		Address: prometheusAddr,
+	})
+	if err != nil {
+		return nil, err
+	}
+	promQLAPI := promv1.NewAPI(promClient)
+	ctx, cancel := context.WithTimeout(ctx, promReadTimeout)
+	defer cancel()
+	promQL := "sum(tidb_executor_statement_total{}) by (instance,type)"
+	// Add retry to avoid network error.
+	for i := 0; i < 5; i++ {
+		result, _, err = promQLAPI.Query(ctx, promQL, queryTime)
+		if err == nil {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return result, err
+}
+
+func dataAnylis(promResult pmodel.Value, SQLresult *sqlUsageData) {
+	switch promResult.Type() {
+	case pmodel.ValVector:
+		matrix := promResult.(pmodel.Vector)
+		for _, m := range matrix {
+			v := m.Value
+			genRecord(m.Metric, v, SQLresult)
+		}
+	}
+}
+
+func genRecord(metric pmodel.Metric, pair pmodel.SampleValue, SQLresult *sqlUsageData) {
+	v := ""
+	if metric != nil {
+		v = string(metric[pmodel.LabelName("type")])
+	}
+	if len(v) == 0 {
+		v = "total"
+	}
+	var record int64
+	if math.IsNaN(float64(pair)) {
+		record = 0
+	} else {
+		record = int64(float64(pair))
+	}
+	switch v {
+	case "total":
+		SQLresult.SQLTotal = record
+	case "Use":
+		SQLresult.SQLType.Use = record
+	case "Show":
+		SQLresult.SQLType.Show = record
+	case "Begin":
+		SQLresult.SQLType.Begin = record
+	case "Commit":
+		SQLresult.SQLType.Commit = record
+	case "Rollback":
+		SQLresult.SQLType.Rollback = record
+	case "Insert":
+		SQLresult.SQLType.Insert = record
+	case "Replace":
+		SQLresult.SQLType.Replace = record
+	case "Delete":
+		SQLresult.SQLType.Delete = record
+	case "Update":
+		SQLresult.SQLType.Update = record
+	case "Select":
+		SQLresult.SQLType.Select = record
+	default:
+		SQLresult.SQLType.Other = record
+	}
+}
+
 // RotateSubWindow rotates the telemetry sub window.
+// ctx sessionctx.Context
 func RotateSubWindow() {
 	thisSubWindow := windowData{
 		BeginAt:      time.Now(),
@@ -98,7 +250,13 @@ func RotateSubWindow() {
 			GTE80:  CurrentCoprCacheHitRatioGTE80Count.Swap(0),
 			GTE100: CurrentCoprCacheHitRatioGTE100Count.Swap(0),
 		},
+		SQLUsage: sqlUsageData{
+			SQLTotal: 0,
+		},
 	}
+	thisSubWindow.SQLUsage, _ = readSQLMetric(time.Now(), thisSubWindow.SQLUsage)
+	thisSubWindow.SQLUsage.SQLTotal = getSQLSum(&thisSubWindow.SQLUsage.SQLType)
+
 	subWindowsLock.Lock()
 	rotatedSubWindows = append(rotatedSubWindows, &thisSubWindow)
 	if len(rotatedSubWindows) > maxSubWindowLength {
@@ -117,6 +275,12 @@ func getWindowData() []*windowData {
 	i := 0
 	for i < len(rotatedSubWindows) {
 		thisWindow := *rotatedSubWindows[i]
+		var startWindow windowData
+		if i == 0 {
+			startWindow = thisWindow
+		} else {
+			startWindow = *rotatedSubWindows[i-1]
+		}
 		aggregatedSubWindows := 1
 		// Aggregate later sub windows
 		i++
@@ -131,6 +295,18 @@ func getWindowData() []*windowData {
 			thisWindow.CoprCacheUsage.GTE40 += rotatedSubWindows[i].CoprCacheUsage.GTE40
 			thisWindow.CoprCacheUsage.GTE80 += rotatedSubWindows[i].CoprCacheUsage.GTE80
 			thisWindow.CoprCacheUsage.GTE100 += rotatedSubWindows[i].CoprCacheUsage.GTE100
+			thisWindow.SQLUsage.SQLTotal = rotatedSubWindows[i].SQLUsage.SQLTotal - startWindow.SQLUsage.SQLTotal
+			thisWindow.SQLUsage.SQLType.Use = rotatedSubWindows[i].SQLUsage.SQLType.Use - startWindow.SQLUsage.SQLType.Use
+			thisWindow.SQLUsage.SQLType.Show = rotatedSubWindows[i].SQLUsage.SQLType.Show - startWindow.SQLUsage.SQLType.Show
+			thisWindow.SQLUsage.SQLType.Begin = rotatedSubWindows[i].SQLUsage.SQLType.Begin - startWindow.SQLUsage.SQLType.Begin
+			thisWindow.SQLUsage.SQLType.Commit = rotatedSubWindows[i].SQLUsage.SQLType.Commit - startWindow.SQLUsage.SQLType.Commit
+			thisWindow.SQLUsage.SQLType.Rollback = rotatedSubWindows[i].SQLUsage.SQLType.Rollback - startWindow.SQLUsage.SQLType.Rollback
+			thisWindow.SQLUsage.SQLType.Insert = rotatedSubWindows[i].SQLUsage.SQLType.Insert - startWindow.SQLUsage.SQLType.Insert
+			thisWindow.SQLUsage.SQLType.Replace = rotatedSubWindows[i].SQLUsage.SQLType.Replace - startWindow.SQLUsage.SQLType.Replace
+			thisWindow.SQLUsage.SQLType.Delete = rotatedSubWindows[i].SQLUsage.SQLType.Delete - startWindow.SQLUsage.SQLType.Delete
+			thisWindow.SQLUsage.SQLType.Update = rotatedSubWindows[i].SQLUsage.SQLType.Update - startWindow.SQLUsage.SQLType.Update
+			thisWindow.SQLUsage.SQLType.Select = rotatedSubWindows[i].SQLUsage.SQLType.Select - startWindow.SQLUsage.SQLType.Select
+			thisWindow.SQLUsage.SQLType.Other = rotatedSubWindows[i].SQLUsage.SQLType.Other - startWindow.SQLUsage.SQLType.Other
 			aggregatedSubWindows++
 			i++
 		}
