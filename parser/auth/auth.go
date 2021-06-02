389b10d46160d4adaa188eeb0f3bad843c36f0b1
Daniël van Eeden <github@myname.nl>
2021-06-02 05:06:10 +0200

    [parser] auth: add the caching sha2 algorithm for authentication (#1232)
    This allows validating passwords against the `authentication_string`
    data that MySQL stores for caching_sha2 passwords.
    
    Related:
    - https://github.com/pingcap/tidb/issues/9411

--- parser/auth/auth.go
+++ parser/auth/auth.go
@@ -14,14 +14,9 @@
 package auth
 
 import (
-	"bytes"
-	"crypto/sha1"
-	"encoding/hex"
 	"fmt"
 
-	"github.com/pingcap/errors"
 	"github.com/pingcap/parser/format"
-	"github.com/pingcap/parser/terror"
 )
 
 // UserIdentity represents username and hostname.
@@ -78,65 +73,4 @@ func (role *RoleIdentity) Restore(ctx *format.RestoreCtx) error {
 func (role *RoleIdentity) String() string {
 	// TODO: Escape username and hostname.
 	return fmt.Sprintf("`%s`@`%s`", role.Username, role.Hostname)
-}
-
-// CheckScrambledPassword check scrambled password received from client.
-// The new authentication is performed in following manner:
-//   SERVER:  public_seed=create_random_string()
-//            send(public_seed)
-//   CLIENT:  recv(public_seed)
-//            hash_stage1=sha1("password")
-//            hash_stage2=sha1(hash_stage1)
-//            reply=xor(hash_stage1, sha1(public_seed,hash_stage2)
-//            // this three steps are done in scramble()
-//            send(reply)
-//   SERVER:  recv(reply)
-//            hash_stage1=xor(reply, sha1(public_seed,hash_stage2))
-//            candidate_hash2=sha1(hash_stage1)
-//            check(candidate_hash2==hash_stage2)
-//            // this three steps are done in check_scramble()
-func CheckScrambledPassword(salt, hpwd, auth []byte) bool {
-	crypt := sha1.New()
-	_, err := crypt.Write(salt)
-	terror.Log(errors.Trace(err))
-	_, err = crypt.Write(hpwd)
-	terror.Log(errors.Trace(err))
-	hash := crypt.Sum(nil)
-	// token = scrambleHash XOR stage1Hash
-	if len(auth) != len(hash) {
-		return false
-	}
-	for i := range hash {
-		hash[i] ^= auth[i]
-	}
-
-	return bytes.Equal(hpwd, Sha1Hash(hash))
-}
-
-// Sha1Hash is an util function to calculate sha1 hash.
-func Sha1Hash(bs []byte) []byte {
-	crypt := sha1.New()
-	_, err := crypt.Write(bs)
-	terror.Log(errors.Trace(err))
-	return crypt.Sum(nil)
-}
-
-// EncodePassword converts plaintext password to hashed hex string.
-func EncodePassword(pwd string) string {
-	if len(pwd) == 0 {
-		return ""
-	}
-	hash1 := Sha1Hash([]byte(pwd))
-	hash2 := Sha1Hash(hash1)
-
-	return fmt.Sprintf("*%X", hash2)
-}
-
-// DecodePassword converts hex string password without prefix '*' to byte array.
-func DecodePassword(pwd string) ([]byte, error) {
-	x, err := hex.DecodeString(pwd[1:])
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	return x, nil
 }
---
+++ parser/auth/mysql_native_password_test.go
@@ -0,0 +1,45 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	. "github.com/pingcap/check"
+)
+
+func (s *testAuthSuite) TestEncodePassword(c *C) {
+	pwd := "123"
+	c.Assert(EncodePassword(pwd), Equals, "*23AE809DDACAF96AF0FD78ED04B6A265E05AA257")
+}
+
+func (s *testAuthSuite) TestDecodePassword(c *C) {
+	x, err := DecodePassword(EncodePassword("123"))
+	c.Assert(err, IsNil)
+	c.Assert(x, DeepEquals, Sha1Hash(Sha1Hash([]byte("123"))))
+}
+
+func (s *testAuthSuite) TestCheckScramble(c *C) {
+	pwd := "abc"
+	salt := []byte{85, 92, 45, 22, 58, 79, 107, 6, 122, 125, 58, 80, 12, 90, 103, 32, 90, 10, 74, 82}
+	auth := []byte{24, 180, 183, 225, 166, 6, 81, 102, 70, 248, 199, 143, 91, 204, 169, 9, 161, 171, 203, 33}
+	encodepwd := EncodePassword(pwd)
+	hpwd, err := DecodePassword(encodepwd)
+	c.Assert(err, IsNil)
+
+	res := CheckScrambledPassword(salt, hpwd, auth)
+	c.Assert(res, IsTrue)
+
+	// Do not panic for invalid input.
+	res = CheckScrambledPassword(salt, hpwd, []byte("xxyyzz"))
+	c.Assert(res, IsFalse)
+}
