5e42ac4090386b1b36f1c2127a1cd2f7255130da
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-06-04 15:10:28 +0800

    [dumpling] planner: enable the dynamic mode of partition tables by default (#25143)

--- dumpling/sessionctx/variable/tidb_vars.go
+++ dumpling/sessionctx/variable/tidb_vars.go
@@ -691,7 +691,7 @@ const (
 	DefTiDBEnableTelemetry             = true
 	DefTiDBEnableParallelApply         = false
 	DefTiDBEnableAmendPessimisticTxn   = false
-	DefTiDBPartitionPruneMode          = "static"
+	DefTiDBPartitionPruneMode          = "dynamic"
 	DefTiDBEnableRateLimitAction       = true
 	DefTiDBEnableAsyncCommit           = false
 	DefTiDBEnable1PC                   = false
