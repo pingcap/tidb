cabc0cbc4417c4cb89bdeb0c80ec6b1256fe4c0a
Han Fei <hanfei19910905@gmail.com>
2021-06-08 15:44:28 +0800

    cherry pick #25210 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/task.go
+++ planner/core/task.go
@@ -1739,7 +1739,7 @@ func (p *PhysicalStreamAgg) attach2Task(tasks ...task) task {
 
 // GetCost computes cost of stream aggregation considering CPU/memory.
 func (p *PhysicalStreamAgg) GetCost(inputRows float64, isRoot bool) float64 {
-	aggFuncFactor := p.getAggFuncCostFactor()
+	aggFuncFactor := p.getAggFuncCostFactor(false)
 	var cpuCost float64
 	sessVars := p.ctx.GetSessionVars()
 	if isRoot {
@@ -1786,7 +1786,12 @@ func (p *PhysicalHashAgg) attach2TaskForMpp(tasks ...task) task {
 		if proj != nil {
 			attachPlan2Task(proj, mpp)
 		}
+<<<<<<< HEAD
 		mpp.addCost(p.GetCost(inputRows, false))
+=======
+		mpp.addCost(p.GetCost(inputRows, false, true))
+		p.cost = mpp.cost()
+>>>>>>> a7f3c4d8b... planner/core: change agg cost factor (#25210)
 		return mpp
 	case Mpp2Phase:
 		proj := p.convertAvgForMPP()
@@ -1817,18 +1822,38 @@ func (p *PhysicalHashAgg) attach2TaskForMpp(tasks ...task) task {
 			attachPlan2Task(proj, newMpp)
 		}
 		// TODO: how to set 2-phase cost?
+<<<<<<< HEAD
 		newMpp.addCost(p.GetCost(inputRows, false))
+=======
+		newMpp.addCost(p.GetCost(inputRows, false, true))
+		finalAgg.SetCost(mpp.cost())
+		if proj != nil {
+			proj.SetCost(mpp.cost())
+		}
+>>>>>>> a7f3c4d8b... planner/core: change agg cost factor (#25210)
 		return newMpp
 	case MppTiDB:
 		partialAgg, finalAgg := p.newPartialAggregate(kv.TiFlash, false)
 		if partialAgg != nil {
 			attachPlan2Task(partialAgg, mpp)
 		}
+<<<<<<< HEAD
 		mpp.addCost(p.GetCost(inputRows, false))
 		t = mpp.convertToRootTask(p.ctx)
 		inputRows = t.count()
 		attachPlan2Task(finalAgg, t)
 		t.addCost(p.GetCost(inputRows, true))
+=======
+		mpp.addCost(p.GetCost(inputRows, false, true))
+		if partialAgg != nil {
+			partialAgg.SetCost(mpp.cost())
+		}
+		t = mpp.convertToRootTask(p.ctx)
+		inputRows = t.count()
+		attachPlan2Task(finalAgg, t)
+		t.addCost(p.GetCost(inputRows, true, false))
+		finalAgg.SetCost(t.cost())
+>>>>>>> a7f3c4d8b... planner/core: change agg cost factor (#25210)
 		return t
 	default:
 		return invalidTask
@@ -1858,7 +1883,7 @@ func (p *PhysicalHashAgg) attach2Task(tasks ...task) task {
 					partialAgg.SetChildren(cop.indexPlan)
 					cop.indexPlan = partialAgg
 				}
-				cop.addCost(p.GetCost(inputRows, false))
+				cop.addCost(p.GetCost(inputRows, false, false))
 			}
 			// In `newPartialAggregate`, we are using stats of final aggregation as stats
 			// of `partialAgg`, so the network cost of transferring result rows of `partialAgg`
@@ -1891,15 +1916,20 @@ func (p *PhysicalHashAgg) attach2Task(tasks ...task) task {
 	// hash aggregation, it would cause under-estimation as the reason mentioned in comment above.
 	// To make it simple, we also treat 2-phase parallel hash aggregation in TiDB layer as
 	// 1-phase when computing cost.
+<<<<<<< HEAD
 	t.addCost(p.GetCost(inputRows, true))
+=======
+	t.addCost(p.GetCost(inputRows, true, false))
+	p.cost = t.cost()
+>>>>>>> a7f3c4d8b... planner/core: change agg cost factor (#25210)
 	return t
 }
 
 // GetCost computes the cost of hash aggregation considering CPU/memory.
-func (p *PhysicalHashAgg) GetCost(inputRows float64, isRoot bool) float64 {
+func (p *PhysicalHashAgg) GetCost(inputRows float64, isRoot bool, isMPP bool) float64 {
 	cardinality := p.statsInfo().RowCount
 	numDistinctFunc := p.numDistinctFunc()
-	aggFuncFactor := p.getAggFuncCostFactor()
+	aggFuncFactor := p.getAggFuncCostFactor(isMPP)
 	var cpuCost float64
 	sessVars := p.ctx.GetSessionVars()
 	if isRoot {
