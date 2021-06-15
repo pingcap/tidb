a11e4509b391249e9506d0f4fef168dea291fa72
JingShang Lu <jingshanglu1@gmail.com>
2021-06-15 16:36:37 +0800

    cherry pick #25357 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/sample_test.go
+++ executor/sample_test.go
@@ -119,6 +119,35 @@ func (s *testTableSampleSuite) TestTableSampleMultiRegions(c *C) {
 	tk.MustExec("drop table t2;")
 }
 
+<<<<<<< HEAD
+=======
+func (s *testTableSampleSuite) TestTableSampleNoSplitTable(c *C) {
+	tk := s.initSampleTest(c)
+	atomic.StoreUint32(&ddl.EnableSplitTableRegion, 0)
+	tk.MustExec("drop table if exists t1;")
+	tk.MustExec("drop table if exists t2;")
+	tk.MustExec("create table t1 (id int primary key);")
+	tk.MustExec("create table t2 (id int primary key);")
+	tk.MustExec("insert into t2 values(1);")
+	rows := tk.MustQuery("select * from t1 tablesample regions();").Rows()
+	rows2 := tk.MustQuery("select * from t2 tablesample regions();").Rows()
+	c.Assert(len(rows), Equals, 0)
+	c.Assert(len(rows2), Equals, 1)
+}
+
+func (s *testTableSampleSuite) TestTableSamplePlan(c *C) {
+	tk := s.initSampleTest(c)
+	tk.MustExec("drop table if exists t;")
+	tk.MustExec("create table t (a bigint, b int default 10);")
+	tk.MustExec("split table t between (0) and (100000) regions 4;")
+	tk.MustExec("insert into t(a) values (1), (2), (3);")
+	rows := tk.MustQuery("explain analyze select a from t tablesample regions();").Rows()
+	c.Assert(len(rows), Equals, 2)
+	tableSample := fmt.Sprintf("%v", rows[1])
+	c.Assert(tableSample, Matches, ".*TableSample.*")
+}
+
+>>>>>>> c8772c4bc... executor: fix incorrect rows returned by `TABLESAMPLE`  (#25357)
 func (s *testTableSampleSuite) TestTableSampleSchema(c *C) {
 	tk := s.initSampleTest(c)
 	tk.Se.GetSessionVars().EnableClusteredIndex = variable.ClusteredIndexDefModeOn
