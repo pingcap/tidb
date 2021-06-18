ca9914fc31f7bd2bc06bd63731baccfc24941a33
Arenatlx <ailinsilence4@gmail.com>
2021-06-18 16:15:57 +0800

    Update types/datum.go
    Co-authored-by: Lynn <zimu_xia@126.com>

--- types/datum.go
+++ types/datum.go
@@ -1129,7 +1129,7 @@ func (d *Datum) convertToMysqlTimestamp(sc *stmtctx.StatementContext, target *Fi
 	switch d.k {
 	case KindMysqlTime:
 		// `select timestamp(cast("1000-01-02 23:59:59" as date)); ` casts usage will succeed.
-		// alter datetime("1000-01-02 23:59:59") to timestamp will error.
+		// Alter datetime("1000-01-02 23:59:59") to timestamp will error.
 		if sc.IsDDLJobInQueue {
 			t, err = d.GetMysqlTime().Convert(sc, target.Tp)
 			if err != nil {
