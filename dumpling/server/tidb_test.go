61b6a04c07d5d5fd533abc7094efd425119f8743
crazycs <crazycs520@gmail.com>
2021-06-23 15:31:22 +0800

    [dumpling] *: fix wrong resource tag of transaction commit statement (#25616)

--- dumpling/server/tidb_test.go
+++ dumpling/server/tidb_test.go
@@ -1380,6 +1380,15 @@ func (ts *tidbTestTopSQLSuite) TestTopSQLCPUProfile(c *C) {
 		}
 	}
 
+	// Test case 4: transaction commit
+	ctx4, cancel4 := context.WithCancel(context.Background())
+	defer cancel4()
+	go ts.loopExec(ctx4, c, func(db *sql.DB) {
+		db.Exec("begin")
+		db.Exec("insert into t () values (),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),()")
+		db.Exec("commit")
+	})
+
 	// Check result of test case 1.
 	for _, ca := range cases1 {
 		checkFn(ca.sql, ca.planRegexp)
@@ -1397,6 +1406,9 @@ func (ts *tidbTestTopSQLSuite) TestTopSQLCPUProfile(c *C) {
 		checkFn(ca.prepare, ca.planRegexp)
 		ca.cancel()
 	}
+
+	// Check result of test case 4.
+	checkFn("commit", "")
 }
 
 func (ts *tidbTestTopSQLSuite) TestTopSQLAgent(c *C) {
