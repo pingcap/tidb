45c51d997996eb23e316930fcb1893b7a5947503
jianzhiyao <jianzhiyao020@qq.com>
2021-06-24 19:48:36 +0800

    t

--- server/conn.go
+++ server/conn.go
@@ -1850,6 +1850,7 @@ func (cc *clientConn) writeColumnInfo(columns []*ColumnInfo, serverStatus uint16
 // serverStatus, a flag bit represents server information
 // The first return value indicates whether error occurs at the first call of ResultSet.Next.
 func (cc *clientConn) writeChunks(ctx context.Context, rs ResultSet, binary bool, serverStatus uint16) (bool, error) {
+	h := cc.alloc.AllocWithLen(4, 1024)
 	req := rs.NewChunk()
 	gotColumnInfo := false
 	firstNext := true
@@ -1896,11 +1897,13 @@ func (cc *clientConn) writeChunks(ctx context.Context, rs ResultSet, binary bool
 		sendRows := make([][]byte, rowCount)
 		nullBitmapOff, numBytes4Null := 5, (colCount+7+2)/8
 		for i := 0; i < rowCount; i++ {
+			sendRows[i] = make([]byte, 4, 1024)
+			copy(sendRows[i], h)
 			if binary {
-				sendRows[i] = cc.alloc.AllocWithLen(4+1+numBytes4Null, 1024+1+numBytes4Null)
-				sendRows[i][4] = mysql.OKHeader
-			} else {
-				sendRows[i] = cc.alloc.AllocWithLen(4, 1024)
+				sendRows[i] = append(sendRows[i], mysql.OKHeader)
+				for j := 0; j < numBytes4Null; j++ {
+					sendRows[i] = append(sendRows[i], 0)
+				}
 			}
 		}
 		columns := rs.Columns()
