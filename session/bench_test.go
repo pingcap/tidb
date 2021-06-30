9016566938eb5e194d20d31a320ad55046400133
tiancaiamao <tiancaiamao@gmail.com>
2021-06-30 10:30:42 +0800

    Makefile,session: collect the benchmark data and generate json output file

--- session/bench_test.go
+++ session/bench_test.go
@@ -15,8 +15,13 @@ package session
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"math/rand"
+	"os"
+	"reflect"
+	"runtime"
 	"strconv"
 	"strings"
 	"testing"
@@ -1583,4 +1588,108 @@ func BenchmarkHashPartitionPruningMultiSelect(b *testing.B) {
 		}
 	}
 	b.StopTimer()
+}
+
+type BenchOutput struct {
+	Date   string
+	Commit string
+	Result []BenchResult
+}
+
+type BenchResult struct {
+	Name        string
+	NsPerOp     int64
+	AllocsPerOp int64
+	BytesPerOp  int64
+}
+
+func benchmarkResultToJson(name string, r testing.BenchmarkResult) BenchResult {
+	return BenchResult{
+		Name:        name,
+		NsPerOp:     r.NsPerOp(),
+		AllocsPerOp: r.AllocsPerOp(),
+		BytesPerOp:  r.AllocedBytesPerOp(),
+	}
+}
+
+func callerName(f func(b *testing.B)) string {
+	fullName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+	idx := strings.LastIndexByte(fullName, '.')
+	if idx > 0 && idx+1 < len(fullName) {
+		return fullName[idx+1:]
+	}
+	return fullName
+}
+
+var (
+	date       = flag.String("date", "", " commit date")
+	commitHash = flag.String("commit", "0ec8f2d9f", "brief git commit hash")
+	outfile    = flag.String("outfile", "bench-daily.json", "specify the output file")
+)
+
+// TestBenchDaily collects the daily benchmark test result and generates a json output file.
+// The format of the json output is described by the BenchOutput.
+// Used by this command in the Makefile
+// 	make bench-daily TO=xxx.json
+func TestBenchDaily(t *testing.T) {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+
+	if date == "" {
+		// Don't run unless 'date' is specified.
+		// Avoiding slow down the CI.
+		return
+	}
+
+	tests := []func(b *testing.B){
+		BenchmarkBasic,
+		BenchmarkTableScan,
+		BenchmarkTableLookup,
+		BenchmarkExplainTableLookup,
+		BenchmarkStringIndexScan,
+		BenchmarkExplainStringIndexScan,
+		BenchmarkStringIndexLookup,
+		BenchmarkIntegerIndexScan,
+		BenchmarkIntegerIndexLookup,
+		BenchmarkDecimalIndexScan,
+		BenchmarkDecimalIndexLookup,
+		BenchmarkInsertWithIndex,
+		BenchmarkInsertNoIndex,
+		BenchmarkSort,
+		BenchmarkJoin,
+		BenchmarkJoinLimit,
+		BenchmarkPartitionPruning,
+		BenchmarkRangeColumnPartitionPruning,
+		BenchmarkHashPartitionPruningPointSelect,
+		BenchmarkHashPartitionPruningMultiSelect,
+	}
+
+	res := make([]BenchResult, 0, len(tests))
+	for _, t := range tests {
+		name := callerName(t)
+		r1 := testing.Benchmark(t)
+		r2 := benchmarkResultToJson(name, r1)
+		res = append(res, r2)
+	}
+
+	if *outfile == "" {
+		*outfile = fmt.Sprintf("%s_%s.json", *date, *commitHash)
+	}
+	out, err := os.Create(*outfile)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	output := BenchOutput{
+		Date:   *date,
+		Commit: *commitHash,
+		Result: res,
+	}
+	enc := json.NewEncoder(out)
+	err = enc.Encode(output)
+	if err != nil {
+		panic(err)
+	}
 }
