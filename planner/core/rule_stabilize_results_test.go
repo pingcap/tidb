e352671a369792b24f36c60b0c217a761247e54f
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-07-06 19:48:33 +0800

    wip

--- planner/core/rule_stabilize_results_test.go
+++ planner/core/rule_stabilize_results_test.go
@@ -186,7 +186,6 @@ func (s *testRuleStabilizeResults) TestStableResultModeOnPartitionTable(c *C) {
 					partition p1 values less than (200),
 					partition p2 values less than (300),
 					partition p3 values less than (400))`)
-	tk.MustQuery("select @@tidb_partition_prune_mode").Check(testkit.Rows("static"))
 	s.runTestData(c, tk, "TestStableResultModeOnPartitionTable")
 }
 
