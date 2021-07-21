6ca1644f2173e88e3170385cfc5ce823b23c6a99
Han Fei <hanfei19910905@gmail.com>
2021-07-21 19:01:55 +0800

    hot fix

--- store/copr/mpp.go
+++ store/copr/mpp.go
@@ -152,7 +152,10 @@ func (m *mppIterator) run(ctx context.Context) {
 		m.mu.Unlock()
 		m.wg.Add(1)
 		bo := backoff.NewBackoffer(ctx, copNextMaxBackoff)
-		go m.handleDispatchReq(ctx, bo, task)
+		go func () {
+			m.handleDispatchReq(ctx, bo, task)
+			m.wg.Done()
+		}()
 	}
 	m.wg.Wait()
 	close(m.respChan)
@@ -176,9 +179,6 @@ func (m *mppIterator) sendToRespCh(resp *mppResponse) (exit bool) {
 // - dispatch all tasks at once, and connect tasks at second.
 // - dispatch tasks and establish connection at the same time.
 func (m *mppIterator) handleDispatchReq(ctx context.Context, bo *Backoffer, req *kv.MPPDispatchRequest) {
-	defer func() {
-		m.wg.Done()
-	}()
 	var regionInfos []*coprocessor.RegionInfo
 	originalTask, ok := req.Meta.(*batchCopTask)
 	if ok {
