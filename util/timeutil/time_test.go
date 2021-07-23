f0c1eaa3e73b8e703e06908d0f6a92951f73c9bd
top628LJ <928073183@qq.com>
2021-07-23 12:21:26 +0800

    util/timeutil: migrate test-infra to testify
    Signed-off-by: top628LJ <928073183@qq.com>

--- util/timeutil/time_test.go
+++ util/timeutil/time_test.go
@@ -23,51 +23,49 @@ import (
 	"strings"
 	"testing"
 
-	. "github.com/pingcap/check"
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Suite(&testTimeSuite{})
+func TestGetTZNameFromFileName(t *testing.T) {
+	t.Parallel()
 
-func TestT(t *testing.T) {
-	TestingT(t)
-}
-
-type testTimeSuite struct{}
-
-func (s *testTimeSuite) TestgetTZNameFromFileName(c *C) {
 	tz, err := inferTZNameFromFileName("/usr/share/zoneinfo/Asia/Shanghai")
 
-	c.Assert(err, IsNil)
-	c.Assert(tz, Equals, "Asia/Shanghai")
+	assert.Nil(t, err)
+	assert.Equal(t, "Asia/Shanghai", tz)
 
 	tz, err = inferTZNameFromFileName("/usr/share/zoneinfo.default/Asia/Shanghai")
 
-	c.Assert(err, IsNil)
-	c.Assert(tz, Equals, "Asia/Shanghai")
+	assert.Nil(t, err)
+	assert.Equal(t, "Asia/Shanghai", tz)
 }
 
-func (s *testTimeSuite) TestLocal(c *C) {
+func TestLocal(t *testing.T) {
+	t.Parallel()
+
 	os.Setenv("TZ", "Asia/Shanghai")
 	systemTZ.Store(InferSystemTZ())
 	loc := SystemLocation()
-	c.Assert(systemTZ.Load(), Equals, "Asia/Shanghai")
-	c.Assert(loc.String(), Equals, "Asia/Shanghai")
+	assert.Equal(t, "Asia/Shanghai", systemTZ.Load())
+	assert.Equal(t, "Asia/Shanghai", loc.String())
 
 	os.Setenv("TZ", "UTC")
 	// reset systemTZ
 	systemTZ.Store(InferSystemTZ())
 	loc = SystemLocation()
-	c.Assert(loc.String(), Equals, "UTC")
+	assert.Equal(t, "UTC", loc.String())
 
 	os.Setenv("TZ", "")
 	// reset systemTZ
 	systemTZ.Store(InferSystemTZ())
 	loc = SystemLocation()
-	c.Assert(loc.String(), Equals, "UTC")
+	assert.Equal(t, "UTC", loc.String())
 	os.Unsetenv("TZ")
 }
 
-func (s *testTimeSuite) TestInferOneStepLinkForPath(c *C) {
+func TestInferOneStepLinkForPath(t *testing.T) {
+	t.Parallel()
+
 	os.Remove(filepath.Join(os.TempDir(), "testlink1"))
 	os.Remove(filepath.Join(os.TempDir(), "testlink2"))
 	os.Remove(filepath.Join(os.TempDir(), "testlink3"))
@@ -75,15 +73,15 @@ func (s *testTimeSuite) TestInferOneStepLinkForPath(c *C) {
 	var err error
 	var link1 *os.File
 	link1, err = os.Create(filepath.Join(os.TempDir(), "testlink1"))
-	c.Assert(err, IsNil)
+	assert.Nil(t, err)
 	err = os.Symlink(link1.Name(), filepath.Join(os.TempDir(), "testlink2"))
-	c.Assert(err, IsNil)
+	assert.Nil(t, err)
 	err = os.Symlink(filepath.Join(os.TempDir(), "testlink2"), filepath.Join(os.TempDir(), "testlink3"))
-	c.Assert(err, IsNil)
+	assert.Nil(t, err)
 	link2, err = inferOneStepLinkForPath(filepath.Join(os.TempDir(), "testlink3"))
-	c.Assert(err, IsNil)
-	c.Assert(link2, Equals, filepath.Join(os.TempDir(), "testlink2"))
+	assert.Nil(t, err)
+	assert.Equal(t, filepath.Join(os.TempDir(), "testlink2"), link2)
 	link3, err = filepath.EvalSymlinks(filepath.Join(os.TempDir(), "testlink3"))
-	c.Assert(err, IsNil)
-	c.Assert(strings.Index(link3, link1.Name()), Not(Equals), -1)
+	assert.Nil(t, err)
+	assert.NotEqual(t, -1, strings.Index(link3, link1.Name()))
 }
