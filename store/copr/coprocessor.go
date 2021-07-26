41038c1ee8d9b3c6b3ac05b4fd7502ebdb7142f6
disksing <i@disksing.com>
2021-07-26 16:13:06 +0800

    store/copr: use lite resolve lock for coprocessor
    Signed-off-by: disksing <i@disksing.com>

--- store/copr/coprocessor.go
+++ store/copr/coprocessor.go
@@ -405,7 +405,7 @@ func (it *copIterator) open(ctx context.Context, enabledRateLimitAction bool) {
 			respChan:        it.respChan,
 			finishCh:        it.finishCh,
 			vars:            it.vars,
-			kvclient:        tikv.NewClientHelper(it.store.store, &it.resolvedLocks, false),
+			kvclient:        tikv.NewClientHelper(it.store.store, &it.resolvedLocks, true),
 			memTracker:      it.memTracker,
 			replicaReadSeed: it.replicaReadSeed,
 			actionOnExceed:  it.actionOnExceed,
