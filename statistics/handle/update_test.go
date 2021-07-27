bd7fd1bf769d045d9292024ae24283cc9d470be7
time-and-fate <25057648+time-and-fate@users.noreply.github.com>
2021-07-27 17:41:42 +0800

    fix test

--- statistics/handle/update_test.go
+++ statistics/handle/update_test.go
@@ -1815,8 +1815,8 @@ func (s *testStatsSuite) TestAbnormalIndexFeedback(c *C) {
 			sql: "select * from t where a = 2 and b > 10",
 			hist: "column:2 ndv:20 totColSize:20\n" +
 				"num: 5 lower_bound: -9223372036854775808 upper_bound: 7 repeats: 0 ndv: 0\n" +
-				"num: 4 lower_bound: 7 upper_bound: 14 repeats: 0 ndv: 0\n" +
-				"num: 5 lower_bound: 14 upper_bound: 9223372036854775807 repeats: 0 ndv: 0",
+				"num: 6 lower_bound: 7 upper_bound: 14 repeats: 0 ndv: 0\n" +
+				"num: 8 lower_bound: 14 upper_bound: 9223372036854775807 repeats: 0 ndv: 0",
 			rangeID: tblInfo.Columns[1].ID,
 			idxID:   tblInfo.Indices[0].ID,
 			eqCount: 3,
