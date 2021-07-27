b57945c38345e75d9249460c91dd9fbd0b7338bd
tison <wander4096@gmail.com>
2021-07-27 20:32:49 +0800

    parallel topn_slow_query_test
    Signed-off-by: tison <wander4096@gmail.com>

--- domain/topn_slow_query_test.go
+++ domain/topn_slow_query_test.go
@@ -17,10 +17,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPush(t *testing.T) {
+	t.Parallel()
+
 	slowQuery := newTopNSlowQueries(10, 0, 10)
 	// Insert data into the heap.
 	slowQuery.Append(&SlowQueryInfo{Duration: 300 * time.Millisecond})
@@ -33,40 +35,42 @@ func TestPush(t *testing.T) {
 	slowQuery.Append(&SlowQueryInfo{Duration: 1000 * time.Millisecond})
 	slowQuery.Append(&SlowQueryInfo{Duration: 1100 * time.Millisecond})
 	slowQuery.Append(&SlowQueryInfo{Duration: 1200 * time.Millisecond})
-	assert.Equal(t, 300*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 300*time.Millisecond, slowQuery.user.data[0].Duration)
 	checkHeap(&slowQuery.user, t)
 
 	// Update all data in the heap.
 	slowQuery.Append(&SlowQueryInfo{Duration: 1300 * time.Millisecond})
-	assert.Equal(t, 400*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 400*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1400 * time.Millisecond})
-	assert.Equal(t, 500*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 500*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1500 * time.Millisecond})
-	assert.Equal(t, 600*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 600*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1500 * time.Millisecond})
-	assert.Equal(t, 700*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 700*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1600 * time.Millisecond})
-	assert.Equal(t, 800*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 800*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1700 * time.Millisecond})
-	assert.Equal(t, 900*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 900*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1800 * time.Millisecond})
-	assert.Equal(t, 1000*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 1000*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 1900 * time.Millisecond})
-	assert.Equal(t, 1100*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 1100*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 2000 * time.Millisecond})
-	assert.Equal(t, 1200*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 1200*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 2100 * time.Millisecond})
-	assert.Equal(t, 1300*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 1300*time.Millisecond, slowQuery.user.data[0].Duration)
 	checkHeap(&slowQuery.user, t)
 
 	// Data smaller than heap top will not be inserted.
 	slowQuery.Append(&SlowQueryInfo{Duration: 1200 * time.Millisecond})
-	assert.Equal(t, 1300*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 1300*time.Millisecond, slowQuery.user.data[0].Duration)
 	slowQuery.Append(&SlowQueryInfo{Duration: 666 * time.Millisecond})
-	assert.Equal(t, 1300*time.Millisecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 1300*time.Millisecond, slowQuery.user.data[0].Duration)
 }
 
 func TestRemoveExpired(t *testing.T) {
+	t.Parallel()
+
 	now := time.Now()
 	slowQuery := newTopNSlowQueries(6, 3*time.Second, 10)
 
@@ -75,41 +79,43 @@ func TestRemoveExpired(t *testing.T) {
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(2 * time.Second), Duration: 4})
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(3 * time.Second), Duration: 3})
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(4 * time.Second), Duration: 2})
-	assert.Equal(t, 2*time.Nanosecond, slowQuery.user.data[0].Duration)
+	require.Equal(t, 2*time.Nanosecond, slowQuery.user.data[0].Duration)
 
 	slowQuery.RemoveExpired(now.Add(5 * time.Second))
-	assert.Len(t, slowQuery.user.data, 2)
-	assert.Equal(t, 2*time.Nanosecond, slowQuery.user.data[0].Duration)
+	require.Len(t, slowQuery.user.data, 2)
+	require.Equal(t, 2*time.Nanosecond, slowQuery.user.data[0].Duration)
 
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(3 * time.Second), Duration: 3})
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(4 * time.Second), Duration: 2})
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(5 * time.Second), Duration: 1})
 	slowQuery.Append(&SlowQueryInfo{Start: now.Add(6 * time.Second), Duration: 0})
-	assert.Len(t, slowQuery.user.data, 6)
-	assert.Equal(t, 0*time.Nanosecond, slowQuery.user.data[0].Duration)
+	require.Len(t, slowQuery.user.data, 6)
+	require.Equal(t, 0*time.Nanosecond, slowQuery.user.data[0].Duration)
 
 	slowQuery.RemoveExpired(now.Add(6 * time.Second))
-	assert.Len(t, slowQuery.user.data, 4)
-	assert.Equal(t, 0*time.Nanosecond, slowQuery.user.data[0].Duration)
+	require.Len(t, slowQuery.user.data, 4)
+	require.Equal(t, 0*time.Nanosecond, slowQuery.user.data[0].Duration)
 }
 
 func TestQueue(t *testing.T) {
+	t.Parallel()
+
 	q := newTopNSlowQueries(10, time.Minute, 5)
 	q.Append(&SlowQueryInfo{SQL: "aaa"})
 	q.Append(&SlowQueryInfo{SQL: "bbb"})
 	q.Append(&SlowQueryInfo{SQL: "ccc"})
 
 	query := q.recent.Query(1)
-	assert.Equal(t, "ccc", query[0].SQL)
+	require.Equal(t, "ccc", query[0].SQL)
 
 	query = q.recent.Query(2)
-	assert.Equal(t, "ccc", query[0].SQL)
-	assert.Equal(t, "bbb", query[1].SQL)
+	require.Equal(t, "ccc", query[0].SQL)
+	require.Equal(t, "bbb", query[1].SQL)
 
 	query = q.recent.Query(6)
-	assert.Equal(t, "ccc", query[0].SQL)
-	assert.Equal(t, "bbb", query[1].SQL)
-	assert.Equal(t, "aaa", query[2].SQL)
+	require.Equal(t, "ccc", query[0].SQL)
+	require.Equal(t, "bbb", query[1].SQL)
+	require.Equal(t, "aaa", query[2].SQL)
 
 	q.Append(&SlowQueryInfo{SQL: "ddd"})
 	q.Append(&SlowQueryInfo{SQL: "eee"})
@@ -117,16 +123,16 @@ func TestQueue(t *testing.T) {
 	q.Append(&SlowQueryInfo{SQL: "ggg"})
 
 	query = q.recent.Query(3)
-	assert.Equal(t, "ggg", query[0].SQL)
-	assert.Equal(t, "fff", query[1].SQL)
-	assert.Equal(t, "eee", query[2].SQL)
+	require.Equal(t, "ggg", query[0].SQL)
+	require.Equal(t, "fff", query[1].SQL)
+	require.Equal(t, "eee", query[2].SQL)
 
 	query = q.recent.Query(6)
-	assert.Equal(t, "ggg", query[0].SQL)
-	assert.Equal(t, "fff", query[1].SQL)
-	assert.Equal(t, "eee", query[2].SQL)
-	assert.Equal(t, "ddd", query[3].SQL)
-	assert.Equal(t, "ccc", query[4].SQL)
+	require.Equal(t, "ggg", query[0].SQL)
+	require.Equal(t, "fff", query[1].SQL)
+	require.Equal(t, "eee", query[2].SQL)
+	require.Equal(t, "ddd", query[3].SQL)
+	require.Equal(t, "ccc", query[4].SQL)
 }
 
 func checkHeap(q *slowQueryHeap, t *testing.T) {
@@ -134,10 +140,10 @@ func checkHeap(q *slowQueryHeap, t *testing.T) {
 		left := 2*i + 1
 		right := 2*i + 2
 		if left < len(q.data) {
-			assert.LessOrEqual(t, q.data[i].Duration, q.data[left].Duration)
+			require.LessOrEqual(t, q.data[i].Duration, q.data[left].Duration)
 		}
 		if right < len(q.data) {
-			assert.LessOrEqual(t, q.data[i].Duration, q.data[right].Duration)
+			require.LessOrEqual(t, q.data[i].Duration, q.data[right].Duration)
 		}
 	}
 }
