613725f221a21cf29ff889cb36cc0d7f65d7f086
wshwsh12 <793703860@qq.com>
2021-07-27 20:35:48 +0800

    address comments

--- expression/builtin_control.go
+++ expression/builtin_control.go
@@ -190,6 +190,8 @@ func (c *caseWhenFunctionClass) getFunction(ctx sessionctx.Context, args []Expre
 		if fieldTp.Collate == "binary" {
 			fieldTp.Charset, fieldTp.Collate = charset.GetDefaultCharsetAndCollate()
 		}
+	} else {
+		fieldTp.Charset, fieldTp.Collate = "binary", "binary"
 	}
 	if isBinaryFlag {
 		fieldTp.Flag |= mysql.BinaryFlag
