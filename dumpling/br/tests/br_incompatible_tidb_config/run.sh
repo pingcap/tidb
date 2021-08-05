51be19b7015610a86f494eda416df3e70074a73b
3pointer <luancheng@pingcap.com>
2021-08-06 00:17:12 +0800

    [dumpling] *: merge BR into TiDB (#26655)

---
+++ dumpling/br/tests/br_incompatible_tidb_config/run.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+#
+# Copyright 2020 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eux
+
+cur=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $cur/../_utils/run_services
+
+DB="$TEST_NAME"
+
+# prepare database
+echo "Restart cluster with max-index-length=12288"
+start_services --tidb-cfg $cur/config/tidb-max-index-length.toml
+
+run_sql "drop schema if exists $DB;"
+run_sql "create schema $DB;"
+
+# test alter pk issue https://github.com/pingcap/br/issues/215
+TABLE="t1"
+INCREMENTAL_TABLE="t1inc"
+
+run_sql "create table $DB.$TABLE (a int primary key nonclustered, b int unique);"
+run_sql "insert into $DB.$TABLE values (42, 42);"
+
+# backup
+run_br --pd $PD_ADDR backup db --db "$DB" -s "local://$TEST_DIR/$DB$TABLE"
+
+run_sql "create table $DB.$INCREMENTAL_TABLE (a int primary key nonclustered, b int unique);"
+run_sql "insert into $DB.$INCREMENTAL_TABLE values (42, 42);"
+
+# drop pk
+run_sql "alter table $DB.$INCREMENTAL_TABLE drop primary key"
+run_sql "drop table $DB.$INCREMENTAL_TABLE"
+run_sql "create table $DB.$INCREMENTAL_TABLE like $DB.$TABLE"
+run_sql "insert into $DB.$INCREMENTAL_TABLE values (42, 42);"
+
+# incremental backup
+run_br --pd $PD_ADDR backup db --db "$DB" -s "local://$TEST_DIR/$DB$INCREMENTAL_TABLE"
+
+# restore
+run_sql "drop schema $DB;"
+
+run_br --pd $PD_ADDR restore db --db "$DB" -s "local://$TEST_DIR/$DB$TABLE"
+
+run_br --pd $PD_ADDR restore db --db "$DB" -s "local://$TEST_DIR/$DB$INCREMENTAL_TABLE"
+
+run_sql "drop schema $DB;"
+run_sql "create schema $DB;"
+
+# test max-index-length issue https://github.com/pingcap/br/issues/217
+TABLE="t2"
+run_sql "create table $DB.$TABLE (a varchar(3072) primary key);"
+run_sql "insert into $DB.$TABLE values ('42');"
+
+# backup
+run_br --pd $PD_ADDR backup db --db "$DB" -s "local://$TEST_DIR/$DB$TABLE"
+
+# restore
+run_sql "drop schema $DB;"
+run_br --pd $PD_ADDR restore db --db "$DB" -s "local://$TEST_DIR/$DB$TABLE"
+
+run_sql "drop schema $DB;"
+
+# test auto random issue https://github.com/pingcap/br/issues/228
+TABLE="t3"
+INCREMENTAL_TABLE="t3inc"
+run_sql "create schema $DB;"
+run_sql "create table $DB.$TABLE (a bigint(11) NOT NULL /*T!30100 AUTO_RANDOM(5) */, PRIMARY KEY (a) clustered)"
+run_sql "insert into $DB.$TABLE values ('42');"
+
+# Full backup
+run_br --pd $PD_ADDR backup db --db "$DB" -s "local://$TEST_DIR/$DB$TABLE"
+
+run_sql "create table $DB.$INCREMENTAL_TABLE (a bigint(11) NOT NULL /*T!30100 AUTO_RANDOM(5) */, PRIMARY KEY (a) clustered)"
+run_sql "insert into $DB.$INCREMENTAL_TABLE values ('42');"
+
+# incremental backup test for execute DDL
+last_backup_ts=$(run_br validate decode --field="end-version" -s "local://$TEST_DIR/$DB$TABLE" | grep -oE "^[0-9]+")
+run_br --pd $PD_ADDR backup db --db "$DB" -s "local://$TEST_DIR/$DB$INCREMENTAL_TABLE" --lastbackupts $last_backup_ts
+
+run_sql "drop schema $DB;"
+
+# full restore
+run_br --pd $PD_ADDR restore db --db "$DB" -s "local://$TEST_DIR/$DB$TABLE"
+# incremental restore
+run_br --pd $PD_ADDR restore db --db "$DB" -s "local://$TEST_DIR/$DB$INCREMENTAL_TABLE"
+
+run_sql "drop schema $DB;"
+
+# test auto random issue https://github.com/pingcap/br/issues/241
+TABLE="t4"
+run_sql "create schema $DB;"
+run_sql "create table $DB.$TABLE(a bigint key clustered auto_random(5));"
+run_sql "insert into $DB.$TABLE values (),(),(),(),();"
+
+# Table backup
+run_br --pd $PD_ADDR backup table --db "$DB" --table "$TABLE" -s "local://$TEST_DIR/$DB$TABLE"
+run_sql "drop schema $DB;"
+
+# Table restore, restore normally without Duplicate entry
+run_br --pd $PD_ADDR restore table --db "$DB" --table "$TABLE" -s "local://$TEST_DIR/$DB$TABLE"
+
+# run insert after restore
+run_sql "insert into $DB.$TABLE values (),(),(),(),();"
+
+row_count=$(run_sql "select a & b'0000011111111111111111111111111' from $DB.$TABLE;" | grep -v "a" | grep -v "-" | sort -u | wc -l)
+if [ "$row_count" -ne "10" ];then
+    echo "TEST: [$TEST_NAME] failed!, because auto_random didn't rebase"
+    exit 1
+fi
+
+echo "Restart service with normal"
+start_services
---
+++ dumpling/br/tests/br_z_gc_safepoint/run.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+#
+# Copyright 2019 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Test whether BR fails fast when backup ts exceeds GC safe point.
+# It is call br_*z*_gc_safepoint, because it brings lots of write and
+# slows down other tests to changing GC safe point. Adding a z prefix to run
+# the test last.
+
+set -eux
+
+DB="$TEST_NAME"
+TABLE="usertable"
+
+MAX_UINT64=9223372036854775807
+
+run_sql "CREATE DATABASE $DB;"
+
+go-ycsb load mysql -P tests/$TEST_NAME/workload -p mysql.host=$TIDB_IP -p mysql.port=$TIDB_PORT -p mysql.user=root -p mysql.db=$DB
+
+# Update GC safepoint to now + 5s after 10s seconds.
+sleep 10 && bin/gc -pd $PD_ADDR \
+    --ca "$TEST_DIR/certs/ca.pem" \
+    --cert "$TEST_DIR/certs/br.pem" \
+    --key "$TEST_DIR/certs/br.key" \
+    -gc-offset "5s" -update-service true &
+
+# total bytes is 1136000
+# Set ratelimit to 40960 bytes/second, it will finish within 25s,
+# so it won't trigger exceed GC safe point error. even It use updateServiceGCSafePoint to update GC safePoint.
+backup_gc_fail=0
+echo "backup start (won't fail)..."
+run_br --pd $PD_ADDR backup table -s "local://$TEST_DIR/$DB/1" --db $DB -t $TABLE --ratelimit 40960 --ratelimit-unit 1 || backup_gc_fail=1
+
+if [ "$backup_gc_fail" -ne "0" ];then
+    echo "TEST: [$TEST_NAME] test check backup ts failed!"
+    exit 1
+fi
+
+# set safePoint otherwise the default safePoint is zero
+bin/gc -pd $PD_ADDR \
+    --ca "$TEST_DIR/certs/ca.pem" \
+    --cert "$TEST_DIR/certs/br.pem" \
+    --key "$TEST_DIR/certs/br.key" \
+    -gc-offset "1s"
+
+backup_gc_fail=0
+echo "incremental backup start (expect fail)..."
+run_br --pd $PD_ADDR backup table -s "local://$TEST_DIR/$DB/2" --db $DB -t $TABLE --lastbackupts 1 --ratelimit 1 --ratelimit-unit 1 || backup_gc_fail=1
+
+if [ "$backup_gc_fail" -ne "1" ];then
+    echo "TEST: [$TEST_NAME] test check last backup ts failed!"
+    exit 1
+fi
+
+backup_gc_fail=0
+echo "incremental backup with max_uint64 start (expect fail)..."
+run_br --pd $PD_ADDR backup table -s "local://$TEST_DIR/$DB/3" --db $DB -t $TABLE --lastbackupts $MAX_UINT64 --ratelimit 1 --ratelimit-unit 1 || backup_gc_fail=1
+
+if [ "$backup_gc_fail" -ne "1" ];then
+    echo "TEST: [$TEST_NAME] test check max backup ts failed!"
+    exit 1
+fi
+
+run_sql "DROP DATABASE $DB;"
