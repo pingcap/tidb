b7804fe3e701fa156c5807868fc4b56a282ce671
Meng Xin <tregoldmeng@gmail.com>
2021-08-05 15:35:13 +0800

    [dumpling] expression: support date function pushed down to tiflash (#26640)

--- dumpling/expression/expression.go
+++ dumpling/expression/expression.go
@@ -1015,7 +1015,7 @@ func scalarExprSupportedByFlash(function *ScalarFunction) bool {
 		ast.Plus, ast.Minus, ast.Div, ast.Mul, ast.Abs, ast.Mod,
 		ast.If, ast.Ifnull, ast.Case,
 		ast.Concat, ast.ConcatWS,
-		ast.Year, ast.Month, ast.Day,
+		ast.Date, ast.Year, ast.Month, ast.Day,
 		ast.DateDiff, ast.TimestampDiff, ast.DateFormat, ast.FromUnixTime,
 		ast.Sqrt, ast.Log, ast.Log2, ast.Log10, ast.Ln, ast.Exp, ast.Pow, ast.Sign,
 		ast.Radians, ast.Degrees, ast.Conv, ast.CRC32,
