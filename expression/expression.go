238e38796191f133dc57949daa76bc73dc06880b
Meng Xin <tregoldmeng@gmail.com>
2021-08-05 15:35:13 +0800

    cherry pick #26640 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/expression.go
+++ expression/expression.go
@@ -1002,7 +1002,7 @@ func scalarExprSupportedByFlash(function *ScalarFunction) bool {
 		ast.Plus, ast.Minus, ast.Div, ast.Mul, ast.Abs, /*ast.Mod,*/
 		ast.If, ast.Ifnull, ast.Case,
 		ast.Concat, ast.ConcatWS,
-		ast.Year, ast.Month, ast.Day,
+		ast.Date, ast.Year, ast.Month, ast.Day,
 		ast.DateDiff, ast.TimestampDiff, ast.DateFormat, ast.FromUnixTime,
 		ast.JSONLength:
 		return true
