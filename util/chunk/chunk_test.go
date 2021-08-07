6434d993aa7f6dfa48f6054c5f9bb4b3e57dc5ef
xxchan <37948597+xxchan@users.noreply.github.com>
2021-08-07 11:28:02 +0800

    simplify TestT

--- util/chunk/chunk_test.go
+++ util/chunk/chunk_test.go
@@ -36,12 +36,6 @@ import (
 )
 
 func TestT(t *testing.T) {
-	path, _ := os.MkdirTemp("", "oom-use-tmp-storage")
-	config.UpdateGlobal(func(conf *config.Config) {
-		conf.TempStoragePath = path
-	})
-	_ = os.RemoveAll(path) // clean the uncleared temp file during the last run.
-	_ = os.MkdirAll(path, 0755)
 	check.TestingT(t)
 }
 
