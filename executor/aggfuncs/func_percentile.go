7f62aa0eea4a28e54a3bf681e3e016527fa91b11
lysu <sulifx@gmail.com>
2021-08-09 15:43:29 +0800

    div first then multiply

--- executor/aggfuncs/func_percentile.go
+++ executor/aggfuncs/func_percentile.go
@@ -39,7 +39,7 @@ var (
 
 func percentile(data sort.Interface, percent int) int {
 	// Ordinal rank k = Ceil(P / 100 * N)
-	k := int(math.Ceil(float64(data.Len()*percent) / 100))
+	k := int(math.Ceil(float64(data.Len()) * (float64(percent) / 100)))
 	return selection.Select(data, k)
 }
 
