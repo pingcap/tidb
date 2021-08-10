709348dc67e4a8345dda02630dfddfdf4bcbd33e
wjHuang <huangwenjun1997@gmail.com>
2021-08-10 20:39:15 +0800

    cherry pick #27055 to release-5.2
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- config/config.go
+++ config/config.go
@@ -709,21 +709,21 @@ func StoreGlobalConfig(config *Config) {
 }
 
 var deprecatedConfig = map[string]struct{}{
-	"pessimistic-txn.ttl":            {},
-	"pessimistic-txn.enable":         {},
-	"log.file.log-rotate":            {},
-	"log.log-slow-query":             {},
-	"txn-local-latches":              {},
-	"txn-local-latches.enabled":      {},
-	"txn-local-latches.capacity":     {},
-	"performance.max-memory":         {},
-	"max-txn-time-use":               {},
-	"experimental.allow-auto-random": {},
-	"enable-redact-log":              {}, // use variable tidb_redact_log instead
-	"tikv-client.copr-cache.enable":  {},
-	"alter-primary-key":              {}, // use NONCLUSTERED keyword instead
-	"enable-streaming":               {},
-	"allow-expression-index":         {},
+	"pessimistic-txn.ttl":                 {},
+	"pessimistic-txn.enable":              {},
+	"log.file.log-rotate":                 {},
+	"log.log-slow-query":                  {},
+	"txn-local-latches":                   {},
+	"txn-local-latches.enabled":           {},
+	"txn-local-latches.capacity":          {},
+	"performance.max-memory":              {},
+	"max-txn-time-use":                    {},
+	"experimental.allow-auto-random":      {},
+	"enable-redact-log":                   {}, // use variable tidb_redact_log instead
+	"tikv-client.copr-cache.enable":       {},
+	"alter-primary-key":                   {}, // use NONCLUSTERED keyword instead
+	"enable-streaming":                    {},
+	"experimental.allow-expression-index": {},
 }
 
 func isAllDeprecatedConfigItems(items []string) bool {
