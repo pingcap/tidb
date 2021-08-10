207ac0d11706dbffd280539053ae48aac41bb041
Chao Wang <cclcwangchao@hotmail.com>
2021-08-10 18:09:11 +0800

    executor: Add `SHOW PLACEMENT LABELS` statement

---
+++ executor/show_placement.go
@@ -0,0 +1,124 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package executor
+
+import (
+	"context"
+	gjson "encoding/json"
+	"sort"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/infoschema"
+	"github.com/pingcap/tidb/store/helper"
+	"github.com/pingcap/tidb/types/json"
+	"github.com/pingcap/tidb/util/sqlexec"
+)
+
+type showPlacementLabelsResultBuilder struct {
+	labelKey2values map[string]interface{}
+}
+
+func (b *showPlacementLabelsResultBuilder) AppendStoreLabels(bj json.BinaryJSON) error {
+	if b.labelKey2values == nil {
+		b.labelKey2values = make(map[string]interface{})
+	}
+
+	data, err := bj.MarshalJSON()
+	if string(data) == "null" {
+		return nil
+	}
+
+	if bj.TypeCode != json.TypeCodeArray {
+		return errors.New("only array or null type is allowed")
+	}
+
+	labels := make([]*helper.StoreLabel, 0, bj.GetElemCount())
+	err = gjson.Unmarshal(data, &labels)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	for _, label := range labels {
+		if values, ok := b.labelKey2values[label.Key]; ok {
+			values.(map[string]interface{})[label.Value] = true
+		} else {
+			b.labelKey2values[label.Key] = map[string]interface{}{label.Value: true}
+		}
+	}
+
+	return nil
+}
+
+func (b *showPlacementLabelsResultBuilder) BuildRows() ([][]interface{}, error) {
+	rows := make([][]interface{}, 0, len(b.labelKey2values))
+	for _, key := range b.sortMapKeys(b.labelKey2values) {
+		values := b.sortMapKeys(b.labelKey2values[key].(map[string]interface{}))
+		d, err := gjson.Marshal(values)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+
+		valuesJSON := json.BinaryJSON{}
+		err = valuesJSON.UnmarshalJSON(d)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+
+		rows = append(rows, []interface{}{key, valuesJSON})
+	}
+
+	return rows, nil
+}
+
+func (b *showPlacementLabelsResultBuilder) sortMapKeys(m map[string]interface{}) []string {
+	sorted := make([]string, 0, len(m))
+	for key := range m {
+		sorted = append(sorted, key)
+	}
+
+	sort.Strings(sorted)
+	return sorted
+}
+
+func (e *ShowExec) fetchShowPlacementLabels(ctx context.Context) error {
+	exec := e.ctx.(sqlexec.RestrictedSQLExecutor)
+	stmt, err := exec.ParseWithParams(ctx, "SELECT DISTINCT LABEL FROM %n.%n", "INFORMATION_SCHEMA", infoschema.TableTiKVStoreStatus)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	rows, _, err := exec.ExecRestrictedStmt(ctx, stmt)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	b := &showPlacementLabelsResultBuilder{}
+	for _, row := range rows {
+		bj := row.GetJSON(0)
+		if err := b.AppendStoreLabels(bj); err != nil {
+			return err
+		}
+	}
+
+	result, err := b.BuildRows()
+	if err != nil {
+		return err
+	}
+
+	for _, row := range result {
+		e.appendRow(row)
+	}
+
+	return nil
+}
---
+++ executor/show_placement_test.go
@@ -0,0 +1,84 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package executor
+
+import (
+	gjson "encoding/json"
+
+	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb/store/helper"
+	"github.com/pingcap/tidb/types/json"
+)
+
+var _ = SerialSuites(&testShowPlacementSuit{})
+
+type testShowPlacementSuit struct {
+}
+
+func (s *testPartitionSuite) TestShowPlacementLabelsBuilder(c *C) {
+	cases := []struct {
+		stores  [][]*helper.StoreLabel
+		expects [][]interface{}
+	}{
+		{
+			stores:  nil,
+			expects: nil,
+		},
+		{
+			stores: [][]*helper.StoreLabel{
+				{{"zone", "z1"}, {"rack", "r3"}, {"host", "h1"}},
+				{{"zone", "z1"}, {"rack", "r1"}, {"host", "h2"}},
+				{{"zone", "z1"}, {"rack", "r2"}, {"host", "h2"}},
+				{{"zone", "z2"}, {"rack", "r1"}, {"host", "h2"}},
+				nil,
+				{{"k1", "v1"}},
+			},
+			expects: [][]interface{}{
+				{"host", []string{"h1", "h2"}},
+				{"k1", []string{"v1"}},
+				{"rack", []string{"r1", "r2", "r3"}},
+				{"zone", []string{"z1", "z2"}},
+			},
+		},
+	}
+
+	b := &showPlacementLabelsResultBuilder{}
+	toBinaryJson := func(obj interface{}) (bj json.BinaryJSON) {
+		d, err := gjson.Marshal(obj)
+		c.Assert(err, IsNil)
+		err = bj.UnmarshalJSON(d)
+		c.Assert(err, IsNil)
+		return
+	}
+
+	for _, ca := range cases {
+		for _, store := range ca.stores {
+			bj := toBinaryJson(store)
+			err := b.AppendStoreLabels(bj)
+			c.Assert(err, IsNil)
+		}
+
+		rows, err := b.BuildRows()
+		c.Assert(err, IsNil)
+		c.Assert(len(rows), Equals, len(ca.expects))
+		for idx, expect := range ca.expects {
+			row := rows[idx]
+			bj := toBinaryJson(expect[1])
+
+			c.Assert(row[0].(string), Equals, expect[0].(string))
+			c.Assert(row[1].(json.BinaryJSON).TypeCode, Equals, bj.TypeCode)
+			c.Assert(row[1].(json.BinaryJSON).Value, BytesEquals, bj.Value)
+		}
+	}
+}
