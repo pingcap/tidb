e28c3bb59b672fbcb129b880b412eb3f95da9214
Morgan Tocker <tocker@gmail.com>
2021-08-11 16:47:27 -0600

    plugin: improve example

--- plugin/conn_ip_example/conn_ip_example.go
+++ plugin/conn_ip_example/conn_ip_example.go
@@ -76,9 +76,15 @@ func OnInit(ctx context.Context, manifest *plugin.Manifest) error {
 	return nil
 }
 
+// OnFlush implements TiDB plugin's OnFlush SPI.
+func OnFlush(ctx context.Context, manifest *plugin.Manifest) error {
+	fmt.Println("## conn_ip_example OnFlush called ##")
+	return nil
+}
+
 // OnShutdown implements TiDB plugin's OnShutdown SPI.
 func OnShutdown(ctx context.Context, manifest *plugin.Manifest) error {
-	fmt.Println("## conn_ip_examples OnShutdown called ##")
+	fmt.Println("## conn_ip_example OnShutdown called ##")
 	fmt.Printf("---- context: %s\n", ctx)
 	fmt.Printf("---- read cfg in shutdown [key: conn_ip_example_key, value: %s]\n", variable.GetSysVar("conn_ip_example_key").Value)
 	atomic.SwapInt32(&connection, 0)
@@ -88,9 +94,14 @@ func OnShutdown(ctx context.Context, manifest *plugin.Manifest) error {
 // OnGeneralEvent implements TiDB Audit plugin's OnGeneralEvent SPI.
 func OnGeneralEvent(ctx context.Context, sctx *variable.SessionVars, event plugin.GeneralEvent, cmd string) {
 	fmt.Println("## conn_ip_example OnGeneralEvent called ##")
-	fmt.Printf("---- new connection by %s\n", ctx.Value("ip"))
 	if sctx != nil {
 		fmt.Printf("---- session status: %d\n", sctx.Status)
+		digest, _ := sctx.StmtCtx.SQLDigest()
+		fmt.Printf("---- statement sql: %s, digest: %s\n", sctx.StmtCtx.OriginalSQL, digest)
+		if len(sctx.StmtCtx.Tables) > 0 {
+			fmt.Printf("---- statement tables: %#v\n", sctx.StmtCtx.Tables)
+		}
+		fmt.Printf("---- executed by user: %#v\n", sctx.User)
 	}
 	switch event {
 	case plugin.Log:
@@ -116,6 +127,7 @@ func OnConnectionEvent(ctx context.Context, event plugin.ConnectionEvent, info *
 	fmt.Println("## conn_ip_example onConnectionEvent called ##")
 	fmt.Printf("---- conenct event: %s, reason: [%s]\n", event, reason)
 	fmt.Printf("---- connection host: %s\n", info.Host)
+	fmt.Printf("---- connection details: %s@%s/%s type: %s\n", info.User, info.Host, info.DB, info.ConnectionType)
 	atomic.AddInt32(&connection, 1)
 	return nil
 }
