bea32eafa8f071336c1b25b64646340b30daf30c
yedamo <logindaveye@gmail.com>
2021-08-12 20:51:47 +0800

    make ci happy

--- server/main_test.go
+++ server/main_test.go
@@ -24,8 +24,12 @@ func TestMain(m *testing.M) {
 	testbridge.WorkaroundGoCheckFlags()
 
 	opts := []goleak.Option{
+		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
+		goleak.IgnoreTopFunction("net/http.(*persistConn).readLoop"),
+		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/pkg/logutil.(*MergeLogger).outputLoop"),
+		goleak.IgnoreTopFunction("github.com/go-sql-driver/mysql.(*mysqlConn).startWatcher.func1"),
 	}
 
 	goleak.VerifyTestMain(m, opts...)
