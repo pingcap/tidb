cd5499e9943682121f8d80a5b85d0ba42f093ad4
Zhi Qi <30543181+LittleFall@users.noreply.github.com>
2021-08-13 16:57:58 +0800

    planner: Fix the problem that `PlanBuilder.buildWindowFunctions` may change sub operator's schema. (#27176)

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -5189,7 +5189,7 @@ func (b *PlanBuilder) buildProjectionForWindow(ctx context.Context, p LogicalPla
 		p = np
 		switch newArg.(type) {
 		case *expression.Column, *expression.Constant:
-			newArgList = append(newArgList, newArg)
+			newArgList = append(newArgList, newArg.Clone())
 			continue
 		}
 		proj.Exprs = append(proj.Exprs, newArg)
@@ -5221,7 +5221,7 @@ func (b *PlanBuilder) buildArgs4WindowFunc(ctx context.Context, p LogicalPlan, a
 		p = np
 		switch newArg.(type) {
 		case *expression.Column, *expression.Constant:
-			newArgList = append(newArgList, newArg)
+			newArgList = append(newArgList, newArg.Clone())
 			continue
 		}
 		col := &expression.Column{
