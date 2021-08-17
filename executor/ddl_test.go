9ea368cd9c0e10f96f97605021399d99b371ad60
lzmhhh123 <lzmhhh123@gmail.com>
2021-08-17 11:48:25 +0800

    fix test
    Signed-off-by: lzmhhh123 <lzmhhh123@gmail.com>

--- executor/ddl_test.go
+++ executor/ddl_test.go
@@ -343,7 +343,7 @@ func (s *testSuite6) TestIssue24771(c *C) {
 						bj_code int
 					);`)
 	tk.MustExec(`drop view if exists v_st_2;`)
-	tk.MustExec(`create or replace view v_st_2 as
+	tk.MustExec(`create definer='root'@'localhost' view v_st_2 as
 		select st.st_name,bj.bj_name,zy.zy_name
 		from (
 			select bj_code,
@@ -354,7 +354,8 @@ func (s *testSuite6) TestIssue24771(c *C) {
 		) as bj
 		left join zy_tab as zy on zy.zy_code = bj.zy_code
 		left join st_tab as st on bj.bj_code = st.bj_code;`)
-	tk.MustQuery(`select * from v_st_2;`)
+	tk.MustQuery(`show create view v_st_2`)
+	tk.MustQuery(`select * from v_st_2`)
 }
 
 func (s testSuite6) TestTruncateSequence(c *C) {
