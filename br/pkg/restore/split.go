7b4d1dff1900a0a305e0b5987ac9bd41f51c1f1f
joccau <zhaozhi_gao@126.com>
2021-08-18 15:49:07 +0800

    Delete repeated code

--- br/pkg/restore/split.go
+++ br/pkg/restore/split.go
@@ -98,14 +98,6 @@ func (rs *RegionSplitter) Split(
 			maxKey = rule.GetNewKeyPrefix()
 		}
 	}
-	for _, rule := range rewriteRules.Data {
-		if bytes.Compare(minKey, rule.GetNewKeyPrefix()) > 0 {
-			minKey = rule.GetNewKeyPrefix()
-		}
-		if bytes.Compare(maxKey, rule.GetNewKeyPrefix()) < 0 {
-			maxKey = rule.GetNewKeyPrefix()
-		}
-	}
 	interval := SplitRetryInterval
 	scatterRegions := make([]*RegionInfo, 0)
 SplitRegions:
