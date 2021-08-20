3335955542a2285b4f0a6b48a80af6c732c8c658
UBarney <UBarney@users.noreply.github.com>
2021-08-20 13:12:01 +0800

    cherry pick #27122 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -9167,3 +9167,9 @@ func (s *testIntegrationSuite) TestRefineArgNullValues(c *C) {
 		"<nil>",
 	))
 }
+
+func (s *testIntegrationSuite) TestIssue26977(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	result := tk.MustQuery("select a + 1 as f from (select cast(0xfffffffffffffff0 as unsigned) as a union select cast(1 as unsigned)) t having f != 2;")
+	result.Check(testkit.Rows("18446744073709551601"))
+}
