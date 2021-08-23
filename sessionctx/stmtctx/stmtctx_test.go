7f7b6ecb0709baec2def76041cfc61715b4fa60c
Hangjie Mo <mohangjie1995@gmail.com>
2021-08-23 16:02:40 +0800

    sessionctx/stmtctx: migrate test-infra to testify for sessionctx/stmtctx #27280

--- sessionctx/stmtctx/stmtctx_test.go
+++ sessionctx/stmtctx/stmtctx_test.go
@@ -19,21 +19,14 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/util/execdetails"
+	"github.com/stretchr/testify/require"
 	"github.com/tikv/client-go/v2/util"
 )
 
-func TestT(t *testing.T) {
-	TestingT(t)
-}
-
-type stmtctxSuit struct{}
-
-var _ = Suite(&stmtctxSuit{})
-
-func (s *stmtctxSuit) TestCopTasksDetails(c *C) {
+func TestCopTasksDetails(t *testing.T) {
+	t.Parallel()
 	ctx := new(stmtctx.StatementContext)
 	backoffs := []string{"tikvRPC", "pdRPC", "regionMiss"}
 	for i := 0; i < 100; i++ {
@@ -53,28 +46,29 @@ func (s *stmtctxSuit) TestCopTasksDetails(c *C) {
 		ctx.MergeExecDetails(d, nil)
 	}
 	d := ctx.CopTasksDetails()
-	c.Assert(d.NumCopTasks, Equals, 100)
-	c.Assert(d.AvgProcessTime, Equals, time.Second*101/2)
-	c.Assert(d.P90ProcessTime, Equals, time.Second*91)
-	c.Assert(d.MaxProcessTime, Equals, time.Second*100)
-	c.Assert(d.MaxProcessAddress, Equals, "100")
-	c.Assert(d.AvgWaitTime, Equals, time.Millisecond*101/2)
-	c.Assert(d.P90WaitTime, Equals, time.Millisecond*91)
-	c.Assert(d.MaxWaitTime, Equals, time.Millisecond*100)
-	c.Assert(d.MaxWaitAddress, Equals, "100")
+	require.Equal(t, 100, d.NumCopTasks)
+	require.Equal(t, time.Second*101/2, d.AvgProcessTime)
+	require.Equal(t, time.Second*91, d.P90ProcessTime)
+	require.Equal(t, time.Second*100, d.MaxProcessTime)
+	require.Equal(t, "100", d.MaxProcessAddress)
+	require.Equal(t, time.Millisecond*101/2, d.AvgWaitTime)
+	require.Equal(t, time.Millisecond*91, d.P90WaitTime)
+	require.Equal(t, time.Millisecond*100, d.MaxWaitTime)
+	require.Equal(t, "100", d.MaxWaitAddress)
 	fields := d.ToZapFields()
-	c.Assert(len(fields), Equals, 9)
+	require.Equal(t, 9, len(fields))
 	for _, backoff := range backoffs {
-		c.Assert(d.MaxBackoffAddress[backoff], Equals, "100")
-		c.Assert(d.MaxBackoffTime[backoff], Equals, 100*time.Millisecond*100)
-		c.Assert(d.P90BackoffTime[backoff], Equals, time.Millisecond*100*91)
-		c.Assert(d.AvgBackoffTime[backoff], Equals, time.Millisecond*100*101/2)
-		c.Assert(d.TotBackoffTimes[backoff], Equals, 101*50)
-		c.Assert(d.TotBackoffTime[backoff], Equals, 101*50*100*time.Millisecond)
+		require.Equal(t, "100", d.MaxBackoffAddress[backoff])
+		require.Equal(t, 100*time.Millisecond*100, d.MaxBackoffTime[backoff])
+		require.Equal(t, time.Millisecond*100*91, d.P90BackoffTime[backoff])
+		require.Equal(t, time.Millisecond*100*101/2, d.AvgBackoffTime[backoff])
+		require.Equal(t, 101*50, d.TotBackoffTimes[backoff])
+		require.Equal(t, 101*50*100*time.Millisecond, d.TotBackoffTime[backoff])
 	}
 }
 
-func (s *stmtctxSuit) TestStatementContextPushDownFLags(c *C) {
+func TestStatementContextPushDownFLags(t *testing.T) {
+	t.Parallel()
 	testCases := []struct {
 		in  *stmtctx.StatementContext
 		out uint64
@@ -95,6 +89,6 @@ func (s *stmtctxSuit) TestStatementContextPushDownFLags(c *C) {
 	}
 	for _, tt := range testCases {
 		got := tt.in.PushDownFlags()
-		c.Assert(got, Equals, tt.out, Commentf("get %v, want %v", got, tt.out))
+		require.Equal(t, tt.out, got)
 	}
 }
