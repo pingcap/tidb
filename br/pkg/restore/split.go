db1c0b286a0ff31b0dcff5ca04c8eed95466143b
Yu Juncen <yujuncen@pingcap.com>
2021-08-30 13:57:55 +0800

    restore: fix some comment

--- br/pkg/restore/split.go
+++ br/pkg/restore/split.go
@@ -262,8 +262,7 @@ func (rs *RegionSplitter) splitAndScatterRegions(
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	// After we never split at the rewrite rule (i.e. begin of tables / indices),
-	// there would be some regions be scattered twice, e.g.:
+	// There would be some regions be scattered twice, e.g.:
 	// |--1-|--2-+----|-3--|
 	//      |    +(t1)|
 	//      +(t1_r4)  |
@@ -271,7 +270,7 @@ func (rs *RegionSplitter) splitAndScatterRegions(
 	// When spliting at `t1_r4`, we would scatter region 1, 2.
 	// When spliting at `t2_r42`, we would scatter region 2, 3.
 	// Because we don't split at t1 anymore.
-	// The trick here is a pinky promise: never scatter regions you didn't imported any data.
+	// The trick here is a pinky promise: never scatter regions you haven't imported any data.
 	// In this scenario, it is the last region after spliting (applying to >= 5.0).
 	if bytes.Equal(newRegions[len(newRegions)-1].Region.StartKey, keys[len(keys)-1]) {
 		newRegions = newRegions[:len(newRegions)-1]
