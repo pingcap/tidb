1ff9808e9bb159f0b2ec8f0271923d3bf7ed38e1
dragonly <liyilongko@gmail.com>
2021-08-30 17:05:31 +0800

    remove unused benchmark code

--- util/logutil/general_log_logger.go
+++ util/logutil/general_log_logger.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-	"unicode/utf8"
 
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
@@ -35,125 +34,11 @@ type GeneralLogEntry struct {
 	query             string
 }
 
-const _hex = "0123456789abcdef"
-
 var fnGetQueryPool = sync.Pool{New: func() interface{} {
 	ret := strings.Builder{}
 	ret.Grow(128)
 	return &ret
 }}
-
-func (e *GeneralLogEntry) writeToBufferDirect(buf *buffer.Buffer) {
-	e.buf = buf
-	e.buf.AppendString("[GENERAL_LOG] [conn=")
-	e.buf.AppendUint(e.ConnID)
-	e.buf.AppendString("] [user=")
-	e.safeAddStringWithQuote(e.user)
-	e.buf.AppendString("] [schemaVersion=")
-	e.buf.AppendInt(e.schemaMetaVersion)
-	e.buf.AppendString("] [txnStartTS=")
-	e.buf.AppendUint(e.TxnStartTS)
-	e.buf.AppendString("] [forUpdateTS=")
-	e.buf.AppendUint(e.TxnForUpdateTS)
-	e.buf.AppendString("] [isReadConsistency=")
-	e.buf.AppendBool(e.IsReadConsistency)
-	e.buf.AppendString("] [current_db=")
-	e.buf.AppendString(e.CurrentDB)
-	e.buf.AppendString("] [txn_mode=")
-	e.buf.AppendString(e.TxnMode)
-	e.buf.AppendString("] [sql=")
-	fnGetQueryBuf := fnGetQueryPool.Get().(*strings.Builder)
-	e.safeAddStringWithQuote(e.query)
-	fnGetQueryBuf.Reset()
-	fnGetQueryPool.Put(fnGetQueryBuf)
-	e.buf.AppendString("]")
-}
-
-// adapted from pingcap/log.textEncoder
-func (e *GeneralLogEntry) safeAddStringWithQuote(s string) {
-	if !needDoubleQuotes(s) {
-		e.safeAddString(s)
-		return
-	}
-	e.buf.AppendByte('"')
-	e.safeAddString(s)
-	e.buf.AppendByte('"')
-}
-
-// adapted from pingcap/log.textEncoder
-func (e *GeneralLogEntry) safeAddString(s string) {
-	for i := 0; i < len(s); {
-		if e.tryAddRuneSelf(s[i]) {
-			i++
-			continue
-		}
-		r, size := utf8.DecodeRuneInString(s[i:])
-		if e.tryAddRuneError(r, size) {
-			i++
-			continue
-		}
-		e.buf.AppendString(s[i : i+size])
-		i += size
-	}
-}
-
-// adapted from pingcap/log.textEncoder
-func (e *GeneralLogEntry) tryAddRuneSelf(b byte) bool {
-	if b >= utf8.RuneSelf {
-		return false
-	}
-	if 0x20 <= b && b != '\\' && b != '"' {
-		e.buf.AppendByte(b)
-		return true
-	}
-	switch b {
-	case '\\', '"':
-		e.buf.AppendByte('\\')
-		e.buf.AppendByte(b)
-	case '\n':
-		e.buf.AppendByte('\\')
-		e.buf.AppendByte('n')
-	case '\r':
-		e.buf.AppendByte('\\')
-		e.buf.AppendByte('r')
-	case '\t':
-		e.buf.AppendByte('\\')
-		e.buf.AppendByte('t')
-
-	default:
-		// Encode bytes < 0x20, except for the escape sequences above.
-		e.buf.AppendString(`\u00`)
-		e.buf.AppendByte(_hex[b>>4])
-		e.buf.AppendByte(_hex[b&0xF])
-	}
-	return true
-}
-
-// adapted from pingcap/log.textEncoder
-func (e *GeneralLogEntry) tryAddRuneError(r rune, size int) bool {
-	if r == utf8.RuneError && size == 1 {
-		e.buf.AppendString(`\ufffd`)
-		return true
-	}
-	return false
-}
-
-// copied from pingcap/log.textEncoder
-// See [log-fileds](https://github.com/tikv/rfcs/blob/master/text/0018-unified-log-format.md#log-fields-section).
-func needDoubleQuotes(s string) bool {
-	for i := 0; i < len(s); {
-		b := s[i]
-		if b <= 0x20 {
-			return true
-		}
-		switch b {
-		case '\\', '"', '[', ']', '=':
-			return true
-		}
-		i++
-	}
-	return false
-}
 
 type GeneralLog struct {
 	bufPool         buffer.Pool
