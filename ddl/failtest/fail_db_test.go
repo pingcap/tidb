a0c7edf4f6738ea058b9b5467ccf1285afb59d15
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-09-01 21:02:20 +0530

    remove TestGenGlobalIDFail as its migrated
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- ddl/failtest/fail_db_test.go
+++ ddl/failtest/fail_db_test.go
@@ -386,58 +386,6 @@ func (s *testFailDBSuite) TestFailSchemaSyncer(c *C) {
 	c.Assert(err, IsNil)
 }
 
-// MIGRATING NOW
-// TODO: type C seems to come from "github.com/pingcap/check"
-func (s *testFailDBSuite) TestGenGlobalIDFail(c *C) {
-	defer func() {
-		// TODO: c, Assert and isNil seem to come from "github.com/pingcap/check"
-		c.Assert(failpoint.Disable("github.com/pingcap/tidb/ddl/mockGenGlobalIDFail"), IsNil)
-	}()
-	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec("create database if not exists gen_global_id_fail")
-	tk.MustExec("use gen_global_id_fail")
-
-	sql1 := "create table t1(a bigint PRIMARY KEY, b int)"
-	sql2 := `create table t2(a bigint PRIMARY KEY, b int) partition by range (a) (
-			      partition p0 values less than (3440),
-			      partition p1 values less than (61440),
-			      partition p2 values less than (122880),
-			      partition p3 values less than maxvalue)`
-	sql3 := `truncate table t1`
-	sql4 := `truncate table t2`
-
-	testcases := []struct {
-		sql     string
-		table   string
-		mockErr bool
-	}{
-		{sql1, "t1", true},
-		{sql2, "t2", true},
-		{sql1, "t1", false},
-		{sql2, "t2", false},
-		{sql3, "t1", true},
-		{sql4, "t2", true},
-		{sql3, "t1", false},
-		{sql4, "t2", false},
-	}
-
-	for idx, test := range testcases {
-		if test.mockErr {
-			c.Assert(failpoint.Enable("github.com/pingcap/tidb/ddl/mockGenGlobalIDFail", `return(true)`), IsNil)
-			_, err := tk.Exec(test.sql)
-			// TODO: c, Assert and NotNil and Commentf seem to come from "github.com/pingcap/check"
-			c.Assert(err, NotNil, Commentf("the %dth test case '%s' fail", idx, test.sql))
-		} else {
-			c.Assert(failpoint.Enable("github.com/pingcap/tidb/ddl/mockGenGlobalIDFail", `return(false)`), IsNil)
-			tk.MustExec(test.sql)
-			tk.MustExec(fmt.Sprintf("insert into %s values (%d, 42)", test.table, rand.Intn(65536)))
-			tk.MustExec(fmt.Sprintf("admin check table %s", test.table))
-		}
-	}
-	tk.MustExec("admin check table t1")
-	tk.MustExec("admin check table t2")
-}
-
 func (s *testFailDBSuite) MyTestGenGlobalIDFail(t *testing.T) {
 	defer func() {
 		require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/ddl/mockGenGlobalIDFail"))
