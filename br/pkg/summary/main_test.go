1be2f5e9623627a09b9f3d22a01e34d82fa6ec2a
hermit <702189255@qq.com>
2021-09-20 21:38:49 +0800

    refactor: migrate test-infra to testify

---
+++ br/pkg/summary/main_test.go
@@ -0,0 +1,13 @@
+package summary
+
+import (
+	"testing"
+
+	"github.com/pingcap/tidb/util/testbridge"
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	testbridge.WorkaroundGoCheckFlags()
+	goleak.VerifyTestMain(m)
+}
