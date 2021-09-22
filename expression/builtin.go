aec3705d00cfec17121d65731d844ee83f9b44e9
wjhuang2016 <huangwenjun1997@gmail.com>
2021-09-22 15:29:34 +0800

    refine comment
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- expression/builtin.go
+++ expression/builtin.go
@@ -927,7 +927,7 @@ func (b *baseBuiltinFunc) setDecimalAndFlenForDatetime(fsp int) {
 	b.tp.Decimal = fsp
 	b.tp.Flen = mysql.MaxDatetimeWidthNoFsp + fsp
 	if fsp > 0 {
-		// For `.`
+		// Add the length for `.`.
 		b.tp.Flen++
 	}
 }
@@ -942,7 +942,7 @@ func (b *baseBuiltinFunc) setDecimalAndFlenForTime(fsp int) {
 	b.tp.Decimal = fsp
 	b.tp.Flen = mysql.MaxDurationWidthNoFsp + fsp
 	if fsp > 0 {
-		// For `.`
+		// Add the length for `.`.
 		b.tp.Flen++
 	}
 }
