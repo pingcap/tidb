af3f7699b4ad92b466e0f8fe485638ddbfc0fa97
wuxiaoju <wuxiaoju@pingcap.com>
2021-09-26 15:17:47 +0800

    fix error handling for failed ut, related to issues/27434

--- util/codec/codec.go
+++ util/codec/codec.go
@@ -898,6 +898,9 @@ func CutOne(b []byte) (data []byte, remain []byte, err error) {
 	if err != nil {
 		return nil, nil, errors.Trace(err)
 	}
+	if l < 0 {
+		return nil, nil, errors.New("Fail to peek the first encoded value")
+	}
 	return b[:l], b[l:], nil
 }
 
@@ -918,6 +921,9 @@ func SetRawValues(data []byte, values []types.Datum) error {
 		l, err := peek(data)
 		if err != nil {
 			return errors.Trace(err)
+		}
+		if l < 0 {
+			return errors.New("Fail to peek the first encoded value")
 		}
 		values[i].SetRaw(data[:l:l])
 		data = data[l:]
