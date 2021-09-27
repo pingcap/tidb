d1f7a36b2d879cd4efd2037d0b159a0274cbb704
tiancaiamao <tiancaiamao@gmail.com>
2021-09-27 20:53:39 +0800

    tidb-server: prevent the misuse of the terror API

--- tidb-server/main.go
+++ tidb-server/main.go
@@ -191,6 +191,11 @@ func main() {
 	storage, dom := createStoreAndDomain()
 	svr := createServer(storage, dom)
 
+	// Register error API is not thread-safe, the caller MUST NOT register errors after initialization.
+	// To prevent misuse, set a flag to indicate that register new error will panic immediately.
+	// For regression of issue like https://github.com/pingcap/tidb/issues/28190
+	terror.RegisterFinish()
+
 	exited := make(chan struct{})
 	signal.SetupSignalHandler(func(graceful bool) {
 		svr.Close()
