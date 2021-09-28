e5001f83bfd667317383c94272bd9cbb47df56bb
kennytm <kennytm@gmail.com>
2021-09-29 02:47:20 +0800

    lightning: let TableKVDecoder decode handle/row to index keys

--- br/pkg/lightning/backend/kv/kv2sql.go
+++ br/pkg/lightning/backend/kv/kv2sql.go
@@ -31,6 +31,7 @@ type TableKVDecoder struct {
 	se  *session
 	// tableName is the unique table name in the form "`db`.`tbl`".
 	tableName string
+	genCols   []genCol
 }
 
 func (t *TableKVDecoder) Name() string {
@@ -67,6 +68,48 @@ func (t *TableKVDecoder) DecodeRawRowDataAsStr(h kv.Handle, value []byte) (res s
 	return fmt.Sprintf("/* ERROR: %s */", err)
 }
 
+// IterRawIndexKeys generates the raw index keys corresponding to the raw row,
+// and then iterate them using `fn`. The input buffer will be reused.
+func (t *TableKVDecoder) IterRawIndexKeys(h kv.Handle, rawRow []byte, fn func([]byte) error) error {
+	row, _, err := t.DecodeRawRowData(h, rawRow)
+	if err != nil {
+		return err
+	}
+	if len(t.genCols) > 0 {
+		for i, col := range t.tbl.Cols() {
+			if col.IsGenerated() {
+				row[i] = types.GetMinValue(&col.FieldType)
+			}
+		}
+		if err, _ := evaluateGeneratedColumns(t.se, row, t.tbl.Cols(), t.genCols); err != nil {
+			return err
+		}
+	}
+
+	indices := t.tbl.Indices()
+
+	var buffer []types.Datum
+	var indexBuffer []byte
+	for _, index := range indices {
+		indexValues, err := index.FetchValues(row, buffer)
+		if err != nil {
+			return err
+		}
+		indexKey, _, err := index.GenIndexKey(t.se.vars.StmtCtx, indexValues, h, indexBuffer)
+		if err != nil {
+			return err
+		}
+		if err := fn(indexKey); err != nil {
+			return err
+		}
+		if len(indexKey) > len(indexBuffer) {
+			indexBuffer = indexKey
+		}
+	}
+
+	return nil
+}
+
 func NewTableKVDecoder(tbl table.Table, tableName string, options *SessionOptions) (*TableKVDecoder, error) {
 	metric.KvEncoderCounter.WithLabelValues("open").Inc()
 	se := newSession(options)
@@ -75,9 +118,15 @@ func NewTableKVDecoder(tbl table.Table, tableName string, options *SessionOption
 	recordCtx := tables.NewCommonAddRecordCtx(len(cols))
 	tables.SetAddRecordCtx(se, recordCtx)
 
+	genCols, err := collectGeneratedColumns(se, tbl.Meta(), cols)
+	if err != nil {
+		return nil, err
+	}
+
 	return &TableKVDecoder{
 		tbl:       tbl,
 		se:        se,
 		tableName: tableName,
+		genCols:   genCols,
 	}, nil
 }
