b50caf1dca9c7757eb341dc7e948e29ce4aa3bb6
Reminiscent <y870414423@gmail.com>
2021-09-29 17:29:59 +0800

    planner: disable tiflash plan caching

--- planner/core/optimizer.go
+++ planner/core/optimizer.go
@@ -199,7 +199,35 @@ func postOptimize(sctx sessionctx.Context, plan PhysicalPlan) PhysicalPlan {
 	mergeContinuousSelections(plan)
 	plan = eliminateUnionScanAndLock(sctx, plan)
 	plan = enableParallelApply(sctx, plan)
+	checkPlanCacheable4PlanCache(sctx, plan)
 	return plan
+}
+
+// checkPlanCacheable4PlanCache used to check whether a plan can be cached.
+// Plans that meet the following characteristics cannot be cached:
+// 1. Use the TiFlash engine.
+// Todo: make more careful check here.
+func checkPlanCacheable4PlanCache(sctx sessionctx.Context, plan PhysicalPlan) {
+	if sctx.GetSessionVars().StmtCtx.UseCache && useTiFlash(plan) {
+		sctx.GetSessionVars().StmtCtx.MaybeOverOptimized4PlanCache = true
+	}
+}
+
+// useTiFlash used to check whether the plan use the TiFlash engine.
+func useTiFlash(p PhysicalPlan) bool {
+	if len(p.Children()) > 0 {
+		for _, plan := range p.Children() {
+			return useTiFlash(plan)
+		}
+	}
+	switch x := p.(type) {
+	case *PhysicalTableReader:
+		switch x.StoreType {
+		case kv.TiFlash:
+			return true
+		}
+	}
+	return false
 }
 
 func enableParallelApply(sctx sessionctx.Context, plan PhysicalPlan) PhysicalPlan {
