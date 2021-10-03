6af814f9aa3c9ab3d48fe7fddf49edb177bfb8f3
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-10-03 13:23:57 +0530

    planner/core: migrate TestListPartitionAgg to testify (#28413)
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- planner/core/integration_partition_test.go
+++ planner/core/integration_partition_test.go
@@ -290,12 +290,15 @@ func TestListPartitionOrderLimit(t *testing.T) {
 	}
 }
 
-func (s *testIntegrationPartitionSerialSuite) TestListPartitionAgg(c *C) {
+func TestListPartitionAgg(t *testing.T) {
 	if israce.RaceEnabled {
-		c.Skip("skip race test")
+		t.Skip("skip race test")
 	}
 
-	tk := testkit.NewTestKitWithInit(c, s.store)
+	store, dom := SetUpTest(t)
+	defer TearDownTest(t, store, dom)
+
+	tk := newtestkit.NewTestKit(t, store)
 	tk.MustExec("create database list_partition_agg")
 	tk.MustExec("use list_partition_agg")
 	tk.MustExec("drop table if exists tlist")
