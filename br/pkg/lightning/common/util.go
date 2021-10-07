afb61f3fd03e2b66fcdac8ef7d7a86c2d478dc68
fengou1 <feng.ou@pingcap.com>
2021-10-07 14:10:00 +0800

    refactoring retryable

--- br/pkg/lightning/common/util.go
+++ br/pkg/lightning/common/util.go
@@ -30,7 +30,6 @@ import (
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/br/pkg/lightning/log"
 	"github.com/pingcap/tidb/br/pkg/utils"
-	"github.com/pingcap/tidb/parser/model"
 	"go.uber.org/zap"
 )
 
