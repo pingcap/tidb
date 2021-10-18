b27a40e410b65f054870826031de3def9ef66fb7
3pointer <luancheng@pingcap.com>
2021-10-18 10:25:45 +0800

    Update br/pkg/lightning/restore/check_info.go
    Co-authored-by: kennytm <kennytm@gmail.com>

--- br/pkg/lightning/restore/check_info.go
+++ br/pkg/lightning/restore/check_info.go
@@ -31,7 +31,6 @@ import (
 	"github.com/pingcap/tidb/br/pkg/lightning/backend/kv"
 	"github.com/pingcap/tidb/br/pkg/lightning/checkpoints"
 	"github.com/pingcap/tidb/br/pkg/lightning/common"
-	md "github.com/pingcap/tidb/br/pkg/lightning/mydump"
 	"github.com/pingcap/tidb/br/pkg/lightning/config"
 	"github.com/pingcap/tidb/br/pkg/lightning/log"
 	"github.com/pingcap/tidb/br/pkg/lightning/mydump"
