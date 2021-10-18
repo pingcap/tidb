50459483cad09b12f8ca79a545dddf1c2dd4b52a
sylzd <liuzidong@xiaomi.com>
2021-10-18 14:42:36 +0800

    forbit policy named default

--- ddl/placement_policy_test.go
+++ ddl/placement_policy_test.go
@@ -199,6 +199,7 @@ func (s *testDBSuite6) TestResetSchemaPlacement(c *C) {
 	tk.MustExec("drop database if exists TestResetPlacementDB;")
 	tk.MustExec("create placement policy `TestReset` followers=4;")
 	tk.MustGetErrCode("create placement policy `default` followers=4;", mysql.ErrReservedSyntax)
+	tk.MustGetErrCode("create placement policy default followers=4;", mysql.ErrParse)
 
 	tk.MustExec("create database TestResetPlacementDB placement policy `TestReset`;")
 	tk.MustExec("use TestResetPlacementDB")
