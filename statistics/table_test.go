c14dbe124fd94c459ca56deb2829635fff5d9ebe
xuyifan <675434007@qq.com>
2021-10-21 16:53:36 +0800

    add test

--- statistics/table_test.go
+++ statistics/table_test.go
@@ -12,13 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package statistics
+package statistics_test
 
 import (
-	"github.com/pingcap/tidb/session"
+	"github.com/pingcap/tidb/statistics"
 	"testing"
 
+	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/parser/mysql"
+	"github.com/pingcap/tidb/session"
+	"github.com/pingcap/tidb/testkit"
 	"github.com/pingcap/tidb/types"
 	"github.com/stretchr/testify/require"
 )
@@ -29,24 +32,44 @@ func TestEstimateTypeWidth(t *testing.T) {
 	var colType *types.FieldType
 
 	colType = &types.FieldType{Tp: mysql.TypeLonglong}
-	require.Equal(t, 8, EstimateTypeWidthForChunk(colType)) // fixed-witch type
+	require.Equal(t, 8, statistics.EstimateTypeWidthForChunk(colType)) // fixed-witch type
 
 	colType = &types.FieldType{Tp: mysql.TypeString, Flen: 31}
-	require.Equal(t, 31, EstimateTypeWidthForChunk(colType)) // colLen <= 32
+	require.Equal(t, 31, statistics.EstimateTypeWidthForChunk(colType)) // colLen <= 32
 
 	colType = &types.FieldType{Tp: mysql.TypeString, Flen: 999}
-	require.Equal(t, 515, EstimateTypeWidthForChunk(colType)) // colLen < 1000
+	require.Equal(t, 515, statistics.EstimateTypeWidthForChunk(colType)) // colLen < 1000
 
 	colType = &types.FieldType{Tp: mysql.TypeString, Flen: 2000}
-	require.Equal(t, 516, EstimateTypeWidthForChunk(colType)) // colLen < 1000
+	require.Equal(t, 516, statistics.EstimateTypeWidthForChunk(colType)) // colLen < 1000
 
 	colType = &types.FieldType{Tp: mysql.TypeString}
-	require.Equal(t, 32, EstimateTypeWidthForChunk(colType)) // value after guessing
+	require.Equal(t, 32, statistics.EstimateTypeWidthForChunk(colType)) // value after guessing
 }
 
 func TestGetAvgRowSizeWithoutColStats(t *testing.T) {
 	t.Parallel()
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
+	se, err := session.CreateSession4Test(store)
+	require.NoError(t, err)
+	coll := &statistics.HistColl{}
+	col := &expression.Column{}
+	se.GetSessionVars().EnableChunkRPC = true
 
-	session.CreateSession4Test(nil)
+	col.RetType = &types.FieldType{Tp: mysql.TypeString, Flen: 999}
+	// 515 + 1/8
+	require.Equal(t, float64(515.125), coll.GetAvgRowSize(se, []*expression.Column{col}, false, false))
 
+	col.RetType = &types.FieldType{Tp: mysql.TypeString}
+	// 32 + 1/8
+	require.Equal(t, float64(32.125), coll.GetAvgRowSize(se, []*expression.Column{col}, false, false))
+
+	col.RetType = &types.FieldType{Tp: mysql.TypeFloat}
+	// 4 + 1/8
+	require.Equal(t, float64(4.125), coll.GetAvgRowSize(se, []*expression.Column{col}, false, false))
+
+	se.GetSessionVars().EnableChunkRPC = false
+	// 8 + 1
+	require.Equal(t, float64(9), coll.GetAvgRowSize(se, []*expression.Column{col}, false, false))
 }
