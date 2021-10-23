a2203fe59e643d2e818bde7c21ab692ab50e0a17
Daniël van Eeden <git@myname.nl>
2021-10-23 07:52:46 +0200

    cherry pick #28528 to release-5.2
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/util.go
+++ expression/util.go
@@ -18,7 +18,6 @@ import (
 	"math"
 	"strconv"
 	"strings"
-	"time"
 	"unicode"
 
 	"github.com/pingcap/errors"
@@ -360,8 +359,8 @@ func SubstituteCorCol2Constant(expr Expression) (Expression, error) {
 }
 
 // timeZone2Duration converts timezone whose format should satisfy the regular condition
-// `(^(+|-)(0?[0-9]|1[0-2]):[0-5]?\d$)|(^+13:00$)` to time.Duration.
-func timeZone2Duration(tz string) time.Duration {
+// `(^(+|-)(0?[0-9]|1[0-2]):[0-5]?\d$)|(^+13:00$)` to int for use by time.FixedZone().
+func timeZone2int(tz string) int {
 	sign := 1
 	if strings.HasPrefix(tz, "-") {
 		sign = -1
@@ -372,7 +371,7 @@ func timeZone2Duration(tz string) time.Duration {
 	terror.Log(err)
 	m, err := strconv.Atoi(tz[i+1:])
 	terror.Log(err)
-	return time.Duration(sign) * (time.Duration(h)*time.Hour + time.Duration(m)*time.Minute)
+	return sign * ((h * 3600) + (m * 60))
 }
 
 var logicalOps = map[string]struct{}{
