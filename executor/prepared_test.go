b1fedd8dfcf78badf14f4ac27a51c2f2f359d542
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-10-25 16:33:09 +0800

    fixup

--- executor/prepared_test.go
+++ executor/prepared_test.go
@@ -406,7 +406,7 @@ func (s *testPrepareSuite) TestPlanCacheWithDifferentVariableTypes(c *C) {
 	}
 }
 
-func (s *testPrepareSuite) TestPlanCacheXXX(c *C) {
+func (s *testPrepareSuite) TestPlanCacheResults(c *C) {
 	store, dom, err := newStoreWithBootstrap()
 	c.Assert(err, IsNil)
 	tk := testkit.NewTestKit(c, store)
