6c456b27c94c9a27f4de210773d386be55027ae5
Reminiscent <y870414423@gmail.com>
2021-10-27 11:56:56 +0800

    add more test cases 4 Invisible Index

--- executor/explainfor_test.go
+++ executor/explainfor_test.go
@@ -1240,3 +1240,36 @@ func (s *testPrepareSerialSuite) TestSelectView4PlanCache(c *C) {
 
 	tk.MustExec("drop view v;")
 }
+
+func (s *testPrepareSerialSuite) TestInvisibleIndex4PlanCache(c *C) {
+	tk := testkit.NewTestKitWithInit(c, s.store)
+
+	orgEnable := core.PreparedPlanCacheEnabled()
+	defer func() {
+		core.SetPreparedPlanCache(orgEnable)
+	}()
+	core.SetPreparedPlanCache(true)
+
+	var err error
+	tk.Se, err = session.CreateSession4TestWithOpt(s.store, &session.Opt{
+		PreparedPlanCache: kvcache.NewSimpleLRUCache(100, 0.1, math.MaxUint64),
+	})
+	c.Assert(err, IsNil)
+
+	tk.MustExec("use test")
+	tk.MustExec("set @@tidb_enable_collect_execution_info=0;")
+	tk.MustExec("drop table if exists t;")
+	tk.MustExec("CREATE TABLE t(c1 INT, index idx_c(c1));")
+
+	tk.MustExec("prepare stmt from 'select * from t use index(idx_c) where c1 > 1;';")
+	tk.MustQuery("execute stmt;").Check(testkit.Rows())
+	tk.MustQuery("execute stmt;").Check(testkit.Rows())
+	tk.MustQuery("select @@last_plan_from_cache;").Check(testkit.Rows("1"))
+
+	tk.MustExec("ALTER TABLE t ALTER INDEX idx_c INVISIBLE;")
+	err = tk.ExecToErr("select * from t use index(idx_c) where c1 > 1;")
+	c.Assert(err.Error(), Equals, "[planner:1176]Key 'idx_c' doesn't exist in table 't'")
+
+	err = tk.ExecToErr("execute stmt;")
+	c.Assert(err.Error(), Equals, "[planner:1176]Key 'idx_c' doesn't exist in table 't'")
+}
