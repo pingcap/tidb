4b4a09372d41c066edf53a04c5425a8127152114
tison <wander4096@gmail.com>
2021-10-28 00:10:14 +0800

    parser: migrate test-infra to testify for terror_test.go
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ parser/terror/terror_serial_test.go
@@ -0,0 +1,46 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package terror
+
+import (
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/pingcap/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTraceAndLocation(t *testing.T) {
+	err := example()
+	stack := errors.ErrorStack(err)
+	lines := strings.Split(stack, "\n")
+	goroot := strings.ReplaceAll(runtime.GOROOT(), string(os.PathSeparator), "/")
+	var sysStack = 0
+	for _, line := range lines {
+		if strings.Contains(line, goroot) {
+			sysStack++
+		}
+	}
+	require.Equalf(t, 15, len(lines)-(2*sysStack), "stack =\n%s", stack)
+	var containTerr bool
+	for _, v := range lines {
+		if strings.Contains(v, "terror_test.go") {
+			containTerr = true
+			break
+		}
+	}
+	require.True(t, containTerr)
+}
