6297c589e0016460dbeb810d23d425be06dff881
Meng Xin <tregoldmeng@gmail.com>
2021-10-29 12:06:50 +0800

    expression : support sysdate pushed down to tiflash (#29063)

--- expression/expression.go
+++ expression/expression.go
@@ -1121,6 +1121,8 @@ func scalarExprSupportedByFlash(function *ScalarFunction) bool {
 		}
 	case ast.Upper, ast.Ucase, ast.Lower, ast.Lcase:
 		return true
+	case ast.Sysdate:
+		return true
 	}
 	return false
 }
