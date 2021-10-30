1e842e87625e60beab615fad255d4aac66885721
wshwsh12 <793703860@qq.com>
2021-10-30 18:31:08 +0800

    support

--- expression/expression.go
+++ expression/expression.go
@@ -1043,7 +1043,8 @@ func scalarExprSupportedByFlash(function *ScalarFunction) bool {
 		ast.JSONLength,
 		ast.InetNtoa, ast.InetAton, ast.Inet6Ntoa, ast.Inet6Aton,
 		ast.Coalesce, ast.ASCII, ast.Length, ast.Trim, ast.Position, ast.Format,
-		ast.LTrim, ast.RTrim:
+		ast.LTrim, ast.RTrim,
+		ast.Hour, ast.Minute, ast.Second, ast.MicroSecond:
 		switch function.Function.PbCode() {
 		case tipb.ScalarFuncSig_InDuration,
 			tipb.ScalarFuncSig_CoalesceDuration,
