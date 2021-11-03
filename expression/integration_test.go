56b90951f7fde15ab7c89358ba6679e54f3baf8f
Hangjie Mo <mohangjie1995@gmail.com>
2021-11-03 11:37:01 +0800

    cherry pick #29310 to release-5.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -9905,6 +9905,7 @@ func (s *testIntegrationSuite) TestIssue28643(c *C) {
 	tk.MustQuery("select hour(a) from t;").Check(testkit.Rows("838", "838"))
 }
 
+<<<<<<< HEAD
 func (s *testIntegrationSuite) TestIssue27831(c *C) {
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
@@ -9917,4 +9918,17 @@ func (s *testIntegrationSuite) TestIssue27831(c *C) {
 	tk.MustExec("create table t(a enum(\"a\", \"b\"), b enum(\"a\", \"b\"), c bool, d int, index idx(d))")
 	tk.MustExec("insert into t values(\"a\", \"a\", 1, 1);")
 	tk.MustQuery("select /*+ inl_hash_join(t1) */  * from t t1 right join t t2 on t1.a=t2.b and t1.a= t2.c and t1.d=t2.d;").Check(testkit.Rows("a a 1 1 a a 1 1"))
+=======
+func (s *testIntegrationSuite) TestIssue29244(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a time(4));")
+	tk.MustExec("insert into t values(\"-700:10:10.123456111\");")
+	tk.MustExec("insert into t values(\"700:10:10.123456111\");")
+	tk.MustExec("set tidb_enable_vectorized_expression = on;")
+	tk.MustQuery("select microsecond(a) from t;").Check(testkit.Rows("123500", "123500"))
+	tk.MustExec("set tidb_enable_vectorized_expression = off;")
+	tk.MustQuery("select microsecond(a) from t;").Check(testkit.Rows("123500", "123500"))
+>>>>>>> a95927f50... expression: Fix wrong result of microsecond function in vectorized (#29310)
 }
