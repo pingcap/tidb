d7e42543c22368b60a0a6fb726a717430a428647
Mattias Jonsson <mattias.jonsson@pingcap.com>
2021-11-04 12:21:59 +0100

    executor: index out of range for lookUpContents (#27138)

--- executor/builder.go
+++ executor/builder.go
@@ -3662,7 +3662,7 @@ func (builder *dataReaderBuilder) buildTableReaderForIndexJoin(ctx context.Conte
 			return nil, err
 		}
 		var kvRanges []kv.KeyRange
-		if keyColumnsIncludeAllPartitionColumns(lookUpContents[0].keyCols, pe) {
+		if len(lookUpContents) > 0 && keyColumnsIncludeAllPartitionColumns(lookUpContents[0].keyCols, pe) {
 			// In this case we can use dynamic partition pruning.
 			locateKey := make([]types.Datum, e.Schema().Len())
 			kvRanges = make([]kv.KeyRange, 0, len(lookUpContents))
@@ -3717,7 +3717,7 @@ func (builder *dataReaderBuilder) buildTableReaderForIndexJoin(ctx context.Conte
 		return nil, err
 	}
 	var kvRanges []kv.KeyRange
-	if keyColumnsIncludeAllPartitionColumns(lookUpContents[0].keyCols, pe) {
+	if len(lookUpContents) > 0 && keyColumnsIncludeAllPartitionColumns(lookUpContents[0].keyCols, pe) {
 		locateKey := make([]types.Datum, e.Schema().Len())
 		kvRanges = make([]kv.KeyRange, 0, len(lookUpContents))
 		for _, content := range lookUpContents {
