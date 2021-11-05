a9687017b686c08cc59b5ec18bad59d7f0e05b8a
Shenghui Wu <793703860@qq.com>
2021-11-05 16:29:03 +0800

    cherry pick #29375 to release-5.2
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/index_lookup_join.go
+++ executor/index_lookup_join.go
@@ -85,10 +85,11 @@ type IndexLookUpJoin struct {
 }
 
 type outerCtx struct {
-	rowTypes []*types.FieldType
-	keyCols  []int
-	hashCols []int
-	filter   expression.CNFExprs
+	rowTypes  []*types.FieldType
+	keyCols   []int
+	hashTypes []*types.FieldType
+	hashCols  []int
+	filter    expression.CNFExprs
 }
 
 type innerCtx struct {
@@ -96,6 +97,7 @@ type innerCtx struct {
 	rowTypes      []*types.FieldType
 	keyCols       []int
 	keyColIDs     []int64 // the original ID in its table, used by dynamic partition pruning
+	hashTypes     []*types.FieldType
 	hashCols      []int
 	colLens       []int
 	hasPrefixCol  bool
