46046c94d47505f35f292e86997b21da4285cf55
Shenghui Wu <793703860@qq.com>
2021-11-05 15:27:06 +0800

    cherry pick #29454 to release-5.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/constant_fold.go
+++ expression/constant_fold.go
@@ -140,7 +140,7 @@ func caseWhenHandler(expr *ScalarFunction) (Expression, bool) {
 			foldedExpr.GetType().Decimal = expr.GetType().Decimal
 			return foldedExpr, isDeferredConst
 		}
-		return BuildCastFunction(expr.GetCtx(), foldedExpr, foldedExpr.GetType()), isDeferredConst
+		return foldedExpr, isDeferredConst
 	}
 	return expr, isDeferredConst
 }
