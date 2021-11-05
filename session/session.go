1daea76ddfe467bf1fc50d2797c931528fda2637
Zh1Cheung <zh1cheunglq@gmail.com>
2021-11-05 16:34:07 +0800

    optimize the logic of code branch

--- session/session.go
+++ session/session.go
@@ -1522,8 +1522,7 @@ func (s *session) ExecuteStmt(ctx context.Context, stmtNode ast.StmtNode) (sqlex
 	}
 
 	s.PrepareTxnCtx(ctx)
-	err := s.loadCommonGlobalVariablesIfNeeded()
-	if err != nil {
+	if err := s.loadCommonGlobalVariablesIfNeeded(); err != nil {
 		return nil, err
 	}
 
