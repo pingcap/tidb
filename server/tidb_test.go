db99c53ab9aff9f300305a80d921138bfd030649
yedamo <logindaveye@gmail.com>
2021-11-07 12:20:44 +0800

    server: migrate `TestNullFlag`

--- server/tidb_test.go
+++ server/tidb_test.go
@@ -1495,68 +1495,72 @@ func (ts *tidbTestSuite) TestSumAvg(c *C) {
 	ts.runTestSumAvg(c)
 }
 
-func (ts *tidbTestSuite) TestNullFlag(c *C) {
+func TestNullFlag(t *testing.T) {
+	t.Parallel()
+	ts, cleanup := createTiDBTest(t)
+	defer cleanup()
+
 	qctx, err := ts.tidbdrv.OpenCtx(uint64(0), 0, uint8(tmysql.DefaultCollationID), "test", nil)
-	c.Assert(err, IsNil)
+	require.NoError(t, err)
 
 	ctx := context.Background()
 	{
 		// issue #9689
 		rs, err := Execute(ctx, qctx, "select 1")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		cols := rs.Columns()
-		c.Assert(len(cols), Equals, 1)
+		require.Len(t, cols, 1)
 		expectFlag := uint16(tmysql.NotNullFlag | tmysql.BinaryFlag)
-		c.Assert(dumpFlag(cols[0].Type, cols[0].Flag), Equals, expectFlag)
+		require.Equal(t, expectFlag, dumpFlag(cols[0].Type, cols[0].Flag))
 	}
 
 	{
 		// issue #19025
 		rs, err := Execute(ctx, qctx, "select convert('{}', JSON)")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		cols := rs.Columns()
-		c.Assert(len(cols), Equals, 1)
+		require.Len(t, cols, 1)
 		expectFlag := uint16(tmysql.BinaryFlag)
-		c.Assert(dumpFlag(cols[0].Type, cols[0].Flag), Equals, expectFlag)
+		require.Equal(t, expectFlag, dumpFlag(cols[0].Type, cols[0].Flag))
 	}
 
 	{
 		// issue #18488
 		_, err := Execute(ctx, qctx, "use test")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		_, err = Execute(ctx, qctx, "CREATE TABLE `test` (`iD` bigint(20) NOT NULL, `INT_TEST` int(11) DEFAULT NULL);")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		rs, err := Execute(ctx, qctx, `SELECT id + int_test as res FROM test  GROUP BY res ORDER BY res;`)
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		cols := rs.Columns()
-		c.Assert(len(cols), Equals, 1)
+		require.Len(t, cols, 1)
 		expectFlag := uint16(tmysql.BinaryFlag)
-		c.Assert(dumpFlag(cols[0].Type, cols[0].Flag), Equals, expectFlag)
+		require.Equal(t, expectFlag, dumpFlag(cols[0].Type, cols[0].Flag))
 	}
 	{
 
 		rs, err := Execute(ctx, qctx, "select if(1, null, 1) ;")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		cols := rs.Columns()
-		c.Assert(len(cols), Equals, 1)
+		require.Len(t, cols, 1)
 		expectFlag := uint16(tmysql.BinaryFlag)
-		c.Assert(dumpFlag(cols[0].Type, cols[0].Flag), Equals, expectFlag)
+		require.Equal(t, expectFlag, dumpFlag(cols[0].Type, cols[0].Flag))
 	}
 	{
 		rs, err := Execute(ctx, qctx, "select CASE 1 WHEN 2 THEN 1 END ;")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		cols := rs.Columns()
-		c.Assert(len(cols), Equals, 1)
+		require.Len(t, cols, 1)
 		expectFlag := uint16(tmysql.BinaryFlag)
-		c.Assert(dumpFlag(cols[0].Type, cols[0].Flag), Equals, expectFlag)
+		require.Equal(t, expectFlag, dumpFlag(cols[0].Type, cols[0].Flag))
 	}
 	{
 		rs, err := Execute(ctx, qctx, "select NULL;")
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 		cols := rs.Columns()
-		c.Assert(len(cols), Equals, 1)
+		require.Len(t, cols, 1)
 		expectFlag := uint16(tmysql.BinaryFlag)
-		c.Assert(dumpFlag(cols[0].Type, cols[0].Flag), Equals, expectFlag)
+		require.Equal(t, expectFlag, dumpFlag(cols[0].Type, cols[0].Flag))
 	}
 }
 
