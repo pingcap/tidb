458627a93cd4aa5c965de2b685f653e37fbea6f0
asuka <312856403@qq.com>
2021-11-08 15:31:12 +0800

    executor: migrate test-infra to testify for concurrent_map_test.go

--- executor/concurrent_map_test.go
+++ executor/concurrent_map_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 PingCAP, Inc.
+// Copyright 2021 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,19 +16,20 @@ package executor
 
 import (
 	"sync"
+	"testing"
 
-	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb/util/chunk"
+	"github.com/stretchr/testify/require"
 )
 
 // TestConcurrentMap first inserts 1000 entries, then checks them
-func (cm *pkgTestSuite) TestConcurrentMap(c *C) {
+func TestConcurrentMap(t *testing.T) {
 	m := newConcurrentMap()
 	const iterations = 1000
 	const mod = 111
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	// Using go routines insert 1000 entires into the map.
+	// Using go routines insert 1000 entries into the map.
 	go func() {
 		defer wg.Done()
 		for i := 0; i < iterations/2; i++ {
@@ -50,17 +51,17 @@ func (cm *pkgTestSuite) TestConcurrentMap(c *C) {
 	for i := 0; i < iterations; i++ {
 		found := false
 		for en, ok := m.Get(uint64(i % mod)); en != nil; en = en.next {
-			c.Assert(ok, IsTrue)
+			require.True(t, ok)
 			if en.ptr.RowIdx == uint32(i) && en.ptr.ChkIdx == uint32(i) {
 				found = true
 			}
 		}
-		c.Assert(found, IsTrue)
+		require.True(t, found)
 	}
 	// test some unexpected cases
 	_, ok := m.Get(uint64(mod))
-	c.Assert(ok, IsFalse)
+	require.False(t, ok)
 
 	_, ok = m.Get(uint64(mod + 1))
-	c.Assert(ok, IsFalse)
+	require.False(t, ok)
 }
