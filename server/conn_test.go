8972f2fc74b8bf03346d22f50a42e660447414fe
tiancaiamao <tiancaiamao@gmail.com>
2021-11-09 14:31:04 +0800

    *: revert "reduce object allocation by reuse chunk column" #29064 (#29580)

--- server/conn_test.go
+++ server/conn_test.go
@@ -37,7 +37,6 @@ import (
 	"github.com/pingcap/tidb/table"
 	"github.com/pingcap/tidb/testkit"
 	"github.com/pingcap/tidb/util/arena"
-	"github.com/pingcap/tidb/util/chunk"
 	"github.com/stretchr/testify/require"
 	"github.com/tikv/client-go/v2/testutils"
 )
@@ -505,7 +504,6 @@ func testDispatch(t *testing.T, inputs []dispatchInput, capability uint32) {
 		collation:  mysql.DefaultCollationID,
 		peerHost:   "localhost",
 		alloc:      arena.NewAllocator(512),
-		chunkAlloc: chunk.NewAllocator(),
 		ctx:        tc,
 		capability: capability,
 	}
@@ -583,9 +581,8 @@ func TestConnExecutionTimeout(t *testing.T) {
 		server: &Server{
 			capability: defaultCapability,
 		},
-		ctx:        tc,
-		alloc:      arena.NewAllocator(32 * 1024),
-		chunkAlloc: chunk.NewAllocator(),
+		ctx:   tc,
+		alloc: arena.NewAllocator(32 * 1024),
 	}
 	srv := &Server{
 		clients: map[uint64]*clientConn{
@@ -693,8 +690,7 @@ func TestPrefetchPointKeys(t *testing.T) {
 	defer clean()
 
 	cc := &clientConn{
-		alloc:      arena.NewAllocator(1024),
-		chunkAlloc: chunk.NewAllocator(),
+		alloc: arena.NewAllocator(1024),
 		pkt: &packetIO{
 			bufWriter: bufio.NewWriter(bytes.NewBuffer(nil)),
 		},
@@ -766,8 +762,7 @@ func TestTiFlashFallback(t *testing.T) {
 	defer clean()
 
 	cc := &clientConn{
-		alloc:      arena.NewAllocator(1024),
-		chunkAlloc: chunk.NewAllocator(),
+		alloc: arena.NewAllocator(1024),
 		pkt: &packetIO{
 			bufWriter: bufio.NewWriter(bytes.NewBuffer(nil)),
 		},
@@ -877,8 +872,7 @@ func TestShowErrors(t *testing.T) {
 	store, clean := testkit.CreateMockStore(t)
 	defer clean()
 	cc := &clientConn{
-		alloc:      arena.NewAllocator(1024),
-		chunkAlloc: chunk.NewAllocator(),
+		alloc: arena.NewAllocator(1024),
 		pkt: &packetIO{
 			bufWriter: bufio.NewWriter(bytes.NewBuffer(nil)),
 		},
@@ -915,7 +909,6 @@ func TestHandleAuthPlugin(t *testing.T) {
 	cc := &clientConn{
 		connectionID: 1,
 		alloc:        arena.NewAllocator(1024),
-		chunkAlloc:   chunk.NewAllocator(),
 		pkt: &packetIO{
 			bufWriter: bufio.NewWriter(bytes.NewBuffer(nil)),
 		},
