4eccf15eba8690b9308b30b970471326db2691d4
Shenghui Wu <793703860@qq.com>
2021-11-09 10:34:49 +0800

    fix

--- expression/builtin_cast.go
+++ expression/builtin_cast.go
@@ -1836,7 +1836,7 @@ func BuildCastFunction(ctx sessionctx.Context, expr Expression, tp *types.FieldT
 	case types.ETJson:
 		fc = &castAsJSONFunctionClass{baseFunctionClass{ast.Cast, 1, 1}, tp}
 	case types.ETString:
-		if tp.Flen == types.UnspecifiedLength || tp.Flen == types.ErrorLength {
+		if tp.Flen == types.UnspecifiedLength {
 			tp.Flen = expr.GetType().Flen
 		}
 		fc = &castAsStringFunctionClass{baseFunctionClass{ast.Cast, 1, 1}, tp}
