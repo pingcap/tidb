0f2f4f0a76607a63688b2cf63c0b804633bbf571
youjiali1995 <zlwgx1023@gmail.com>
2021-11-15 17:25:31 +0800

    unistore: get/batch get/scan support read-through-lock close #29881
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- br/pkg/lightning/restore/restore_test.go
+++ br/pkg/lightning/restore/restore_test.go
@@ -1778,7 +1778,7 @@ func (s *tableRestoreSuite) TestCheckClusterRegion(c *C) {
 	testCases := []testCase{
 		{
 			stores: api.StoresInfo{Stores: []*api.StoreInfo{
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 1}}, Status: &api.StoreStatus{RegionCount: 200}},
+				{Store: &api.MetaStore{StoreID: 1}, Status: &api.StoreStatus{RegionCount: 200}},
 			}},
 			emptyRegions: api.RegionsInfo{
 				Regions: append([]api.RegionInfo(nil), makeRegions(100, 1)...),
@@ -1789,9 +1789,9 @@ func (s *tableRestoreSuite) TestCheckClusterRegion(c *C) {
 		},
 		{
 			stores: api.StoresInfo{Stores: []*api.StoreInfo{
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 1}}, Status: &api.StoreStatus{RegionCount: 2000}},
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 2}}, Status: &api.StoreStatus{RegionCount: 3100}},
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 3}}, Status: &api.StoreStatus{RegionCount: 2500}},
+				{Store: &api.MetaStore{StoreID: 1}, Status: &api.StoreStatus{RegionCount: 2000}},
+				{Store: &api.MetaStore{StoreID: 2}, Status: &api.StoreStatus{RegionCount: 3100}},
+				{Store: &api.MetaStore{StoreID: 3}, Status: &api.StoreStatus{RegionCount: 2500}},
 			}},
 			emptyRegions: api.RegionsInfo{
 				Regions: append(append(append([]api.RegionInfo(nil),
@@ -1809,9 +1809,9 @@ func (s *tableRestoreSuite) TestCheckClusterRegion(c *C) {
 		},
 		{
 			stores: api.StoresInfo{Stores: []*api.StoreInfo{
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 1}}, Status: &api.StoreStatus{RegionCount: 1200}},
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 2}}, Status: &api.StoreStatus{RegionCount: 3000}},
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 3}}, Status: &api.StoreStatus{RegionCount: 2500}},
+				{Store: &api.MetaStore{StoreID: 1}, Status: &api.StoreStatus{RegionCount: 1200}},
+				{Store: &api.MetaStore{StoreID: 2}, Status: &api.StoreStatus{RegionCount: 3000}},
+				{Store: &api.MetaStore{StoreID: 3}, Status: &api.StoreStatus{RegionCount: 2500}},
 			}},
 			expectMsgs:     []string{".*Region distribution is unbalanced.*but we expect it must not be less than 0.5.*"},
 			expectResult:   false,
@@ -1819,9 +1819,9 @@ func (s *tableRestoreSuite) TestCheckClusterRegion(c *C) {
 		},
 		{
 			stores: api.StoresInfo{Stores: []*api.StoreInfo{
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 1}}, Status: &api.StoreStatus{RegionCount: 0}},
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 2}}, Status: &api.StoreStatus{RegionCount: 2800}},
-				{Store: &api.MetaStore{Store: &metapb.Store{Id: 3}}, Status: &api.StoreStatus{RegionCount: 2500}},
+				{Store: &api.MetaStore{StoreID: 1}, Status: &api.StoreStatus{RegionCount: 0}},
+				{Store: &api.MetaStore{StoreID: 2}, Status: &api.StoreStatus{RegionCount: 2800}},
+				{Store: &api.MetaStore{StoreID: 3}, Status: &api.StoreStatus{RegionCount: 2500}},
 			}},
 			expectMsgs:     []string{".*Region distribution is unbalanced.*but we expect it must not be less than 0.5.*"},
 			expectResult:   false,
