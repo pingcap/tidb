3f9d61f16d45e471d541667e87e1f555adda8340
sylzd <liuzidong@xiaomi.com>
2021-11-16 17:06:24 +0800

    fix check

--- dumpling/export/writer_test.go
+++ dumpling/export/writer_test.go
@@ -36,7 +36,7 @@ func TestWriteDatabaseMeta(t *testing.T) {
 
 	bytes, err := ioutil.ReadFile(p)
 	require.NoError(t, err)
-	require.Equal(t, "/*!40101 SET NAMES binary*/;\nCREATE DATABASE `test`;\n", string(bytes))
+	require.Equal(t, "/*!40101 SET NAMES binary*/;\n/*T![placement] SET PLACEMENT_CHECKS = 0*/;\nCREATE DATABASE `test`;\n", string(bytes))
 }
 
 func TestWritePolicyMeta(t *testing.T) {
@@ -79,7 +79,7 @@ func TestWriteTableMeta(t *testing.T) {
 	require.NoError(t, err)
 	bytes, err := ioutil.ReadFile(p)
 	require.NoError(t, err)
-	require.Equal(t, "/*!40101 SET NAMES binary*/;\nCREATE TABLE t (a INT);\n", string(bytes))
+	require.Equal(t, "/*!40101 SET NAMES binary*/;\n/*T![placement] SET PLACEMENT_CHECKS = 0*/;\nCREATE TABLE t (a INT);\n", string(bytes))
 }
 
 func TestWriteViewMeta(t *testing.T) {
@@ -92,7 +92,7 @@ func TestWriteViewMeta(t *testing.T) {
 	writer, clean := createTestWriter(config, t)
 	defer clean()
 
-	specCmt := "/*!40101 SET NAMES binary*/;\n"
+	specCmt := "/*!40101 SET NAMES binary*/;\n/*T![placement] SET PLACEMENT_CHECKS = 0*/;\n"
 	createTableSQL := "CREATE TABLE `v`(\n`a` int\n)ENGINE=MyISAM;\n"
 	createViewSQL := "DROP TABLE IF EXISTS `v`;\nDROP VIEW IF EXISTS `v`;\nSET @PREV_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;\nSET @PREV_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;\nSET @PREV_COLLATION_CONNECTION=@@COLLATION_CONNECTION;\nSET character_set_client = utf8;\nSET character_set_results = utf8;\nSET collation_connection = utf8_general_ci;\nCREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v` (`a`) AS SELECT `t`.`a` AS `a` FROM `test`.`t`;\nSET character_set_client = @PREV_CHARACTER_SET_CLIENT;\nSET character_set_results = @PREV_CHARACTER_SET_RESULTS;\nSET collation_connection = @PREV_COLLATION_CONNECTION;\n"
 	err := writer.WriteViewMeta("test", "v", createTableSQL, createViewSQL)
