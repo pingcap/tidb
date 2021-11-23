851cf901d979b5ae443fb8a826d35af3717a12f4
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-11-23 16:24:54 +0800

    fixup

--- planner/optimize.go
+++ planner/optimize.go
@@ -117,16 +117,6 @@ func matchSQLBinding(stmtNode ast.StmtNode, sctx sessionctx.Context) (bindRecord
 func Optimize(ctx context.Context, sctx sessionctx.Context, node ast.Node, is infoschema.InfoSchema) (plannercore.Plan, types.NameSlice, error) {
 	sessVars := sctx.GetSessionVars()
 
-	var bindRecord *bindinfo.BindRecord
-	var scope string
-	var useBinding bool
-	stmtNode, isStmtNode := node.(ast.StmtNode)
-	if isStmtNode {
-		bindRecord, scope, useBinding = matchSQLBinding(stmtNode, sctx)
-		// add the extra Limit after matching the bind record
-		node = plannercore.TryAddExtraLimit(sctx, stmtNode)
-	}
-
 	// Because for write stmt, TiFlash has a different results when lock the data in point get plan. We ban the TiFlash
 	// engine in not read only stmt.
 	if _, isolationReadContainTiFlash := sessVars.IsolationReadEngines[kv.TiFlash]; isolationReadContainTiFlash && !IsReadOnly(node, sessVars) {
@@ -166,6 +156,16 @@ func Optimize(ctx context.Context, sctx sessionctx.Context, node ast.Node, is in
 		}
 	}
 	sctx.PrepareTSFuture(ctx)
+
+	var bindRecord *bindinfo.BindRecord
+	var scope string
+	var useBinding bool
+	stmtNode, isStmtNode := node.(ast.StmtNode)
+	if isStmtNode {
+		bindRecord, scope, useBinding = matchSQLBinding(stmtNode, sctx)
+		// add the extra Limit after matching the bind record
+		node = plannercore.TryAddExtraLimit(sctx, stmtNode)
+	}
 
 	var names types.NameSlice
 	var bestPlan, bestPlanFromBind plannercore.Plan
