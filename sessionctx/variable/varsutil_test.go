ae7e122b97596a5c5c59f8663787a2bbea85db6f
Yujie Xia <xiayjchn@gmail.com>
2021-11-24 14:09:10 +0800

    fix fail test

--- sessionctx/variable/varsutil_test.go
+++ sessionctx/variable/varsutil_test.go
@@ -493,7 +493,7 @@ func TestVarsutil(t *testing.T) {
 	err = SetSessionSystemVar(v, TiDBAnalyzeVersion, "4")
 	require.NoError(t, err) // converts to max value
 	warn := v.StmtCtx.GetWarnings()[0]
-	require.Regexp(t, "Truncated incorrect tidb_analyze_version value$", warn.Err.Error())
+	require.Regexp(t, "Truncated incorrect tidb_analyze_version value", warn.Err.Error())
 }
 
 func TestValidate(t *testing.T) {
