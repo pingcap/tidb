0e1e491e8e2ea92bdf51917a9b6ac0a16c0cdc59
3pointer <luancheng@pingcap.com>
2021-11-24 10:58:16 +0800

    update default value

--- br/pkg/task/common.go
+++ br/pkg/task/common.go
@@ -188,7 +188,9 @@ func DefineCommonFlags(flags *pflag.FlagSet) {
 
 	// Default concurrency is different for backup and restore.
 	// Leave it 0 and let them adjust the value.
-	flags.Uint32(flagConcurrency, 0, "The size of thread pool on br that executes the task, the default value of backup is 4, (restore is 128)")
+	flags.Uint32(flagConcurrency, 0, "The size of thread pool on br that executes the task," +
+		"because of different task level of the backup and restore. " +
+		"the default value should be different. if don't set. the default value of backup will adjust to 4, (restore is 128)")
 
 	flags.Uint64(flagRateLimitUnit, units.MiB, "The unit of rate limit")
 	_ = flags.MarkHidden(flagRateLimitUnit)
