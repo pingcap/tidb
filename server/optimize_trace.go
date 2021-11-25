0644763dc962a9b5cbd5c18f943879b179efd5f6
yisaer <disxiaofei@163.com>
2021-11-25 15:47:35 +0800

    support download file
    Signed-off-by: yisaer <disxiaofei@163.com>

---
+++ server/optimize_trace.go
@@ -0,0 +1,61 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/domain"
+	"github.com/pingcap/tidb/parser/terror"
+)
+
+type OptimizeTraceHandler struct {
+}
+
+func (s *Server) newOptimizeTraceHandler() *OptimizeTraceHandler {
+	return &OptimizeTraceHandler{}
+}
+
+func (prh OptimizeTraceHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/zip")
+	w.Header().Set("Content-Disposition", "attachment; filename=\"optimize_trace.zip\"")
+
+	params := mux.Vars(req)
+
+	name := params[pFileName]
+	path := filepath.Join(domain.GetOptimizerTraceDirName(), name)
+	file, err := os.Open(path)
+	if err != nil {
+		writeError(w, err)
+	} else {
+		_, err := io.Copy(w, file)
+		if err != nil {
+			terror.Log(errors.Trace(err))
+		}
+	}
+	err = file.Close()
+	if err != nil {
+		terror.Log(errors.Trace(err))
+	}
+	err = os.Remove(path)
+	if err != nil {
+		terror.Log(errors.Trace(err))
+	}
+}
