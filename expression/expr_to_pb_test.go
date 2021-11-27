499fd3091377755fd44ff1bbacf5e8bc6f2beff7
guo-shaoge <shaoge1994@163.com>
2021-11-28 01:58:30 +0800

    trivial chg
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- expression/expr_to_pb_test.go
+++ expression/expr_to_pb_test.go
@@ -957,7 +957,7 @@ func (s *testEvaluatorSuite) TestExprOnlyPushDownToFlash(c *C) {
 
 	pushed, remained = PushDownExprs(sc, exprs, client, kv.TiKV)
 	c.Assert(len(pushed), Equals, 1)
-	c.Assert(len(remained), Equals, len(exprs) - 1)
+	c.Assert(len(remained), Equals, len(exprs)-1)
 }
 
 func (s *testEvaluatorSuite) TestExprOnlyPushDownToTiKV(c *C) {
