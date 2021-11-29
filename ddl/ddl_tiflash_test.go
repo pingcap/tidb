cf45cd0713e85738a77903c21cbe6d7c2df8a736
CalvinNeo <calvinneo1995@gmail.com>
2021-11-29 19:47:37 +0800

    enable
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- ddl/ddl_tiflash_test.go
+++ ddl/ddl_tiflash_test.go
@@ -287,33 +287,33 @@ func CheckTableAvailable(dom *domain.Domain, c *C, count uint64, labels []string
 	c.Assert(replica.LocationLabels, DeepEquals, labels)
 }
 
-//// Truncate table shall not block.
-//func (s *tiflashDDLTestSuite) TestTiFlashTruncateTable(c *C) {
-//	tk := testkit.NewTestKit(c, s.store)
-//	tk.MustExec("use test")
-//
-//	tk.MustExec("drop table if exists ddltiflash")
-//	tk.MustExec("create table ddltiflash(z int not null) partition by range (z) (partition p0 values less than (10), partition p1 values less than (20))")
-//	tk.MustExec("alter table ddltiflash set tiflash replica 1")
-//
-//	time.Sleep(ddl.PollTiFlashInterval * 3)
-//	// Should get schema right now
-//
-//	tk.MustExec("truncate table ddltiflash")
-//	time.Sleep(ddl.PollTiFlashInterval * 3)
-//	CheckTableAvailable(s.dom, c, 1, []string{})
-//
-//	time.Sleep(ddl.PollTiFlashInterval * 3)
-//	tk.MustExec("drop table if exists ddltiflash")
-//	tk.MustExec("create table ddltiflash(z int)")
-//	tk.MustExec("alter table ddltiflash set tiflash replica 1")
-//	time.Sleep(ddl.PollTiFlashInterval * 3)
-//	// Should get schema right now
-//
-//	tk.MustExec("truncate table ddltiflash")
-//	time.Sleep(ddl.PollTiFlashInterval * 3)
-//	CheckTableAvailable(s.dom, c, 1, []string{})
-//}
+// Truncate table shall not block.
+func (s *tiflashDDLTestSuite) TestTiFlashTruncateTable(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+
+	tk.MustExec("drop table if exists ddltiflash")
+	tk.MustExec("create table ddltiflash(z int not null) partition by range (z) (partition p0 values less than (10), partition p1 values less than (20))")
+	tk.MustExec("alter table ddltiflash set tiflash replica 1")
+
+	time.Sleep(ddl.PollTiFlashInterval * 3)
+	// Should get schema right now
+
+	tk.MustExec("truncate table ddltiflash")
+	time.Sleep(ddl.PollTiFlashInterval * 3)
+	CheckTableAvailable(s.dom, c, 1, []string{})
+
+	time.Sleep(ddl.PollTiFlashInterval * 3)
+	tk.MustExec("drop table if exists ddltiflash")
+	tk.MustExec("create table ddltiflash(z int)")
+	tk.MustExec("alter table ddltiflash set tiflash replica 1")
+	time.Sleep(ddl.PollTiFlashInterval * 3)
+	// Should get schema right now
+
+	tk.MustExec("truncate table ddltiflash")
+	time.Sleep(ddl.PollTiFlashInterval * 3)
+	CheckTableAvailable(s.dom, c, 1, []string{})
+}
 
 // TiFlash Table shall be eventually available, even with lots of small table created.
 func (s *tiflashDDLTestSuite) TestTiFlashMassiveReplicaAvailable(c *C) {
