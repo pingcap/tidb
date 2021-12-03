999fec3eb35631725d7aebd3a015f70bd9d3123f
HuaiyuXu <391585975@qq.com>
2021-12-03 15:33:54 +0800

    cherry pick #30265 to release-5.3
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- util/chunk/mutrow_test.go
+++ util/chunk/mutrow_test.go
@@ -90,6 +90,29 @@ func TestMutRow(t *testing.T) {
 	require.Equal(t, mutRow.c.columns[0].data, chk.columns[0].data)
 }
 
+func TestIssue29947(t *testing.T) {
+	t.Parallel()
+
+	mutRow := MutRowFromTypes(allTypes)
+	nilDatum := types.NewDatum(nil)
+
+	dataBefore := make([][]byte, 0, len(mutRow.c.columns))
+	elemBufBefore := make([][]byte, 0, len(mutRow.c.columns))
+	for _, col := range mutRow.c.columns {
+		dataBefore = append(dataBefore, col.data)
+		elemBufBefore = append(elemBufBefore, col.elemBuf)
+	}
+	for i, col := range mutRow.c.columns {
+		mutRow.SetDatum(i, nilDatum)
+		require.Equal(t, col.IsNull(0), true)
+		for _, off := range col.offsets {
+			require.Equal(t, off, int64(0))
+		}
+		require.Equal(t, col.data, dataBefore[i])
+		require.Equal(t, col.elemBuf, elemBufBefore[i])
+	}
+}
+
 func BenchmarkMutRowSetRow(b *testing.B) {
 	b.ReportAllocs()
 	rowChk := newChunk(8, 0)
