bcdee32a2d32a1b0376cfa2d506c905e4b77a94b
山岚 <36239017+YuJuncen@users.noreply.github.com>
2021-12-07 17:52:19 +0800

    BR: add stream backup meta client (#30088)
    * stream: initial commit of stream backup
    
    * br/stream: basic logic of stream backup metadata
    
    * br/stream: add prefix scanner
    
    * br/stream: use types from kv package
    
    * br/stream: unexport prefix scanner(remove it to utils package and export it in need)
    
    * br/stream: move kvproto to br-stream branch, added a playground
    
    * br/stream: added some new functions for playground
    
    * br/stream: added a builder for task
    
    * br/stream: remove playground, use math.MaxUInt64
    
    * br: replace kvproto with br-stream
    
    * br/stream: update error code
    
    * br/stream: fix tests
    
    * br/stream: added copyright

--- br/pkg/errors/errors.go
+++ br/pkg/errors/errors.go
@@ -54,6 +54,9 @@ var (
 	ErrRestoreRTsConstrain = errors.Normalize("resolved ts constrain violation", errors.RFCCodeText("BR:Restore:ErrRestoreResolvedTsConstrain"))
 
 	ErrPiTRInvalidCDCLogFormat = errors.Normalize("invalid cdc log format", errors.RFCCodeText("BR:PiTR:ErrPiTRInvalidCDCLogFormat"))
+	ErrPiTRTaskNotFound        = errors.Normalize("task not found", errors.RFCCodeText("BR:PiTR:ErrTaskNotFound"))
+	ErrPiTRInvalidTaskInfo     = errors.Normalize("task info is invalid", errors.RFCCodeText("BR:PiTR:ErrInvalidTaskInfo"))
+	ErrPiTRMalformedMetadata   = errors.Normalize("malformed metadata", errors.RFCCodeText("BR:PiTR:ErrMalformedMetadata"))
 
 	ErrStorageUnknown           = errors.Normalize("unknown external storage error", errors.RFCCodeText("BR:ExternalStorage:ErrStorageUnknown"))
 	ErrStorageInvalidConfig     = errors.Normalize("invalid external storage config", errors.RFCCodeText("BR:ExternalStorage:ErrStorageInvalidConfig"))
