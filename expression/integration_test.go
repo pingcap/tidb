c87bc71e881cd46eedb84f3b94c12dbf1be63962
lvlongxiang <lvlongxiang1994@gmail.com>
2021-12-07 13:23:14 +0800

    rebase

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -10681,3 +10681,30 @@ func (s *testIntegrationSuite) TestIssue30326(c *C) {
 	c.Assert(err, NotNil)
 	c.Assert(err.Error(), Equals, "[executor:1242]Subquery returns more than 1 row")
 }
+
+func (s *testIntegrationSuite) TestIssue29708(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test;")
+	tk.MustExec("drop table if exists t1;")
+	tk.MustExec("CREATE TABLE t1 (a text)character set utf8 ;")
+	_, err := tk.Exec("INSERT INTO t1 VALUES  (REPEAT(0125,200000000));")
+	c.Assert(err, NotNil)
+	tk.MustQuery("select * from t1").Check(nil)
+
+	// test vectorized build-in function
+	tk.MustExec("insert into t1 (a) values ('a'),('b');")
+	_, err = tk.Exec("insert into t1 select REPEAT(a,200000000) from t1;")
+	c.Assert(err, NotNil)
+	tk.MustQuery("select a from t1 order by a;").Check([][]interface{}{
+		{"a"},
+		{"b"},
+	})
+
+	// test cast
+	_, err = tk.Exec(`insert into t1 values  (cast("a" as binary(4294967295)));`)
+	c.Assert(err, NotNil)
+	tk.MustQuery("select a from t1 order by a;").Check([][]interface{}{
+		{"a"},
+		{"b"},
+	})
+}
