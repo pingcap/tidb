f9bcc52816ecd80e893f87bef0e0d23f95879b38
wjHuang <huangwenjun1997@gmail.com>
2021-12-13 23:30:19 +0800

    Update points.go

--- util/ranger/points.go
+++ util/ranger/points.go
@@ -568,6 +568,9 @@ func (r *builder) buildFromIn(expr *expression.ScalarFunction) ([]*point, bool) 
 			hasNull = true
 			continue
 		}
+		if dt.Kind() == types.KindString || dt.Kind() == types.KindBinaryLiteral {
+			dt.SetString(dt.GetString(), colCollate)
+		}
 		if expr.GetArgs()[0].GetType().Tp == mysql.TypeEnum {
 			switch dt.Kind() {
 			case types.KindString, types.KindBytes, types.KindBinaryLiteral:
