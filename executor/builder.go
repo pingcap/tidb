eb7fa690169da5367afbf24992a2f3010954697c
fzhedu <fzhedu@gmail.com>
2021-12-16 14:30:26 +0800

    add tests

--- executor/builder.go
+++ executor/builder.go
@@ -4192,7 +4192,7 @@ func (b *executorBuilder) buildWindow(v *plannercore.PhysicalWindow) Executor {
 	partialResults := make([]aggfuncs.PartialResult, 0, len(v.WindowFuncDescs))
 	resultColIdx := v.Schema().Len() - len(v.WindowFuncDescs)
 	for _, desc := range v.WindowFuncDescs {
-		aggDesc:= aggregation.NewAggFuncDescForWindowFunc(desc,false)
+		aggDesc := aggregation.NewAggFuncDescForWindowFunc(desc, false)
 		agg := aggfuncs.BuildWindowFunctions(b.ctx, aggDesc, resultColIdx, orderByCols)
 		windowFuncs = append(windowFuncs, agg)
 		partialResult, _ := agg.AllocPartialResult()
