367aeadc87cd1f2bb37c6e4169e035ae6fe1291e
you06 <you1474600@gmail.com>
2021-12-16 20:00:35 +0800

    add test
    Signed-off-by: you06 <you1474600@gmail.com>

--- util/paging/paging_test.go
+++ util/paging/paging_test.go
@@ -9,12 +9,14 @@ import (
 func TestGrowPagingSize(t *testing.T) {
 	t.Parallel()
 
+	require.Equal(t, GrowPagingSize(MinPagingSize), MinPagingSize*pagingSizeGrow)
 	require.Equal(t, GrowPagingSize(MaxPagingSize), MaxPagingSize)
 	require.Equal(t, GrowPagingSize(MaxPagingSize/pagingSizeGrow+1), MaxPagingSize)
 }
 
 func TestCalculateSeekCnt(t *testing.T) {
 	t.Parallel()
+
 	require.InDelta(t, CalculateSeekCnt(0), 0, 0.1)
 	require.InDelta(t, CalculateSeekCnt(1), 1, 0.1)
 	require.InDelta(t, CalculateSeekCnt(MinPagingSize), 1, 0.1)
