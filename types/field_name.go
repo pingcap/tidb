e139c348010e1b9f35318e294796463b3cb7bbd3
time-and-fate <25057648+time-and-fate@users.noreply.github.com>
2021-12-17 15:53:00 +0800

    resolve conflicts

--- types/field_name.go
+++ types/field_name.go
@@ -29,16 +29,8 @@ type FieldName struct {
 	ColName     model.CIStr
 
 	Hidden bool
-<<<<<<< HEAD
-=======
-
-	// NotExplicitUsable is used for mark whether a column can be explicit used in SQL.
-	// update stmt can write `writeable` column implicitly but cannot use non-public columns explicit.
-	// e.g. update t set a = 10 where b = 10; which `b` is in `writeOnly` state
-	NotExplicitUsable bool
 
 	Redundant bool
->>>>>>> ead31402b... *: resolve select fields properly for coalesced columns of natural join (#25094)
 }
 
 const emptyName = "EMPTY_NAME"
