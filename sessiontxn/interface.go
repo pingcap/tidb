ecacf28456c0cdd96a8470e381747b7dfb3ed147
Chao Wang <cclcwangchao@hotmail.com>
2021-12-20 16:43:01 +0800

    move failpoint content to a single file

--- sessiontxn/interface.go
+++ sessiontxn/interface.go
@@ -15,63 +15,9 @@
 package sessiontxn
 
 import (
-	"fmt"
-
 	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/sessionctx"
-	"github.com/pingcap/tidb/util/stringutil"
 )
-
-// AssertRecordsKey is used to save failPoint invoke records
-// Only for test
-var AssertRecordsKey stringutil.StringerStr = "assertTxnManagerRecords"
-
-// AssertTxnInfoSchemaKey is used to set the expected infoschema that should be check in failPoint
-// Only for test
-var AssertTxnInfoSchemaKey stringutil.StringerStr = "assertTxnInfoSchemaKey"
-
-// AssertTxnInfoSchemaAfterRetryKey is used to set the expected infoschema that should be check in failPoint after retry
-// Only for test
-var AssertTxnInfoSchemaAfterRetryKey stringutil.StringerStr = "assertTxnInfoSchemaAfterRetryKey"
-
-// RecordAssert is used only for test
-func RecordAssert(sctx sessionctx.Context, name string, value interface{}) {
-	records, ok := sctx.Value(AssertRecordsKey).(map[string]interface{})
-	if !ok {
-		records = make(map[string]interface{})
-		sctx.SetValue(AssertRecordsKey, records)
-	}
-	records[name] = value
-}
-
-// AssertTxnManagerInfoSchema is used only for test
-func AssertTxnManagerInfoSchema(sctx sessionctx.Context, is interface{}) {
-	assertVersion := func(expected interface{}) {
-		if expected == nil {
-			return
-		}
-
-		expectVer := expected.(infoschema.InfoSchema).SchemaMetaVersion()
-		gotVer := GetTxnManager(sctx).GetTxnInfoSchema().SchemaMetaVersion()
-		if gotVer != expectVer {
-			panic(fmt.Sprintf("Txn schema version not match, expect:%d, got:%d", expectVer, gotVer))
-		}
-	}
-
-	if localTables := sctx.GetSessionVars().LocalTemporaryTables; localTables != nil {
-		got, ok := GetTxnManager(sctx).GetTxnInfoSchema().(*infoschema.TemporaryTableAttachedInfoSchema)
-		if !ok {
-			panic("Expected to be a TemporaryTableAttachedInfoSchema")
-		}
-
-		if got.LocalTemporaryTables != localTables {
-			panic("Local tables should be the same with the one in session")
-		}
-	}
-
-	assertVersion(is)
-	assertVersion(sctx.Value(AssertTxnInfoSchemaKey))
-}
 
 // TxnContextProvider provides txn context
 type TxnContextProvider interface {
