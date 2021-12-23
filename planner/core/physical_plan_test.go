6a76ccea0b92260f41ad693130387873b5511ba9
time-and-fate <25057648+time-and-fate@users.noreply.github.com>
2021-12-23 16:43:42 +0800

    add test

--- planner/core/physical_plan_test.go
+++ planner/core/physical_plan_test.go
@@ -2016,3 +2016,26 @@ func (s *testPlanSuite) TestIssue28316(c *C) {
 		tk.MustQuery("explain format='brief' " + ts).Check(testkit.Rows(output[i].Plan...))
 	}
 }
+
+func (s *testPlanSuite) TestIssue30965(c *C) {
+	store, dom, err := newStoreWithBootstrap()
+	c.Assert(err, IsNil)
+	defer func() {
+		dom.Close()
+		store.Close()
+	}()
+	tk := testkit.NewTestKit(c, store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t30965")
+	tk.MustExec("CREATE TABLE `t30965` ( `a` int(11) DEFAULT NULL, `b` int(11) DEFAULT NULL, `c` int(11) DEFAULT NULL, `d` int(11) GENERATED ALWAYS AS (`a` + 1) VIRTUAL, KEY `ib` (`b`));")
+	tk.MustExec("insert into t30965 (a,b,c) value(3,4,5);")
+	tk.MustQuery("select count(*) from t30965 where d = 2 and b = 4 and a = 3 and c = 5;").Check(testkit.Rows("0"))
+	tk.MustQuery("explain select count(*) from t30965 where d = 2 and b = 4 and a = 3 and c = 5;").Check(
+		testkit.Rows(
+			"StreamAgg_10 1.00 root  funcs:count(1)->Column#6",
+			"└─Selection_28 0.00 root  eq(test.t30965.d, 2)",
+			"  └─IndexLookUp_27 0.00 root  ",
+			"    ├─IndexRangeScan_24(Build) 10.00 cop[tikv] table:t30965, index:ib(b) range:[4,4], keep order:false, stats:pseudo",
+			"    └─Selection_26(Probe) 0.00 cop[tikv]  eq(test.t30965.a, 3), eq(test.t30965.c, 5)",
+			"      └─TableRowIDScan_25 10.00 cop[tikv] table:t30965 keep order:false, stats:pseudo"))
+}
