7cf31bd9ae49dd88a75ba4ade8e325da192098f3
tangenta <tangenta@126.com>
2021-12-28 22:03:30 +0800

    fix integration test TestChangeFromBitToStringInvalidUtf8ErrMsg

--- types/datum.go
+++ types/datum.go
@@ -1027,17 +1027,13 @@ func (d *Datum) convertToString(sc *stmtctx.StatementContext, target *FieldType)
 	case KindBinaryLiteral:
 		s, err = d.GetBinaryStringDecoded(sc, target.Charset)
 	case KindMysqlBit:
-		// issue #25037
-		// bit to binary/varbinary. should consider transferring to uint first.
-		if target.Tp == mysql.TypeString || (target.Tp == mysql.TypeVarchar && target.Collate == charset.CollationBin) {
-			val, err := d.GetBinaryLiteral().ToInt(sc)
-			if err != nil {
-				s = d.GetBinaryLiteral().ToString()
-			} else {
-				s = strconv.FormatUint(val, 10)
-			}
-		} else {
+		// https://github.com/pingcap/tidb/issues/25037.
+		// Consider converting to uint first.
+		val, err := d.GetBinaryLiteral().ToInt(sc)
+		if err != nil {
 			s = d.GetBinaryLiteral().ToString()
+		} else {
+			s = strconv.FormatUint(val, 10)
 		}
 	case KindMysqlJSON:
 		s = d.GetMysqlJSON().String()
