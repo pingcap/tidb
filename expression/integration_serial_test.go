fe09dea90a618ffd970b7d270fbc08f1c961a177
lzmhhh123 <lzmhhh123@gmail.com>
2021-12-29 11:59:05 +0800

    fix
    Signed-off-by: lzmhhh123 <lzmhhh123@gmail.com>

--- expression/integration_serial_test.go
+++ expression/integration_serial_test.go
@@ -2044,7 +2044,7 @@ func TestTimeBuiltin(t *testing.T) {
 	result.Check(testkit.Rows("<nil> <nil> <nil> <nil> <nil>"))
 	result = tk.MustQuery(`select quarter("0000-00-00"), quarter("0000-00-00 00:00:00");`)
 	result.Check(testkit.Rows("0 0"))
-	tk.MustQuery("show warnings").Check(testkit.Rows(""))
+	tk.MustQuery("show warnings").Check(testkit.Rows())
 	result = tk.MustQuery(`select quarter(0), quarter(0.0), quarter(0e1), quarter(0.00);`)
 	result.Check(testkit.Rows("0 0 0 0"))
 	tk.MustQuery("show warnings").Check(testkit.Rows())
