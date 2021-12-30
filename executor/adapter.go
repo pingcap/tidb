51775cc9ec2b2d5c9982ec7526c812dd7cd7c2b2
tiancaiamao <tiancaiamao@gmail.com>
2021-12-30 10:07:55 +0800

    address comment

--- executor/adapter.go
+++ executor/adapter.go
@@ -503,6 +503,10 @@ func isNoResultPlan(p plannercore.Plan) bool {
 	if p.Schema().Len() == 0 {
 		return true
 	}
+
+	// Currently this is only for the "DO" statement. Take "DO 1, @a=2;" as an example:
+	// the Projection has two expressions and two columns in the schema, but we should
+	// not return the result of the two expressions.
 	switch raw := p.(type) {
 	case *plannercore.LogicalProjection:
 		if raw.CalculateNoDelay {
