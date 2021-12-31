dab280a0015b1b5892c057eb72096058f33d07bd
tangenta <tangenta@126.com>
2021-12-31 16:52:32 +0800

    parser: decode the binary literal value in enum or set

--- ddl/integration_test.go
+++ ddl/integration_test.go
@@ -59,3 +59,22 @@ func TestDefaultValueIsBinaryString(t *testing.T) {
 	tk.MustGetErrMsg("create table t (a blob default 0xE4BDA0E5A5BD81);",
 		"[ddl:1101]BLOB/TEXT/JSON column 'a' can't have a default value")
 }
+
+// https://github.com/pingcap/tidb/issues/30740.
+func TestDefaultValueInEnum(t *testing.T) {
+	collate.SetCharsetFeatEnabledForTest(true)
+	defer collate.SetCharsetFeatEnabledForTest(false)
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test;")
+	// The value 0x91 should not cause panic.
+	tk.MustExec("create table t(a enum('a', 0x91) charset gbk);")
+	tk.MustExec("insert into t values (1), (2);")                 // Use 1-base index to locate the value.
+	tk.MustQuery("select a from t;").Check(testkit.Rows("a", "")) // 0x91 is truncate.
+
+	tk.MustExec("drop table t;")
+	tk.MustGetErrMsg("create table t(a set('a', 0x91, '') charset gbk);",
+		"[types:1291]Column 'a' has duplicated value '' in SET")
+
+}
