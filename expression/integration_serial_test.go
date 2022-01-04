473012b1b89466e4b40761941ad9ac6f8267455b
wjhuang2016 <huangwenjun1997@gmail.com>
2022-01-04 14:30:45 +0800

    fix test
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- expression/integration_serial_test.go
+++ expression/integration_serial_test.go
@@ -180,11 +180,6 @@ func TestCollationBasic(t *testing.T) {
 	tk.MustExec("create table t(a char(10))")
 	tk.MustExec("insert into t values ('a')")
 	tk.MustQuery("select * from t where a in ('b' collate utf8mb4_general_ci, 'A', 3)").Check(testkit.Rows("a"))
-	// These test cases may not the same as MySQL, but it's more reasonable.
-	tk.MustQuery("select ('a', 'a') in (('A' collate utf8mb4_general_ci, 'A' collate utf8mb4_general_ci));").Check(testkit.Rows("1"))
-	tk.MustQuery("select ('a', 'a') in (('A' collate utf8mb4_general_ci, 'A' collate utf8mb4_bin));").Check(testkit.Rows("0"))
-	tk.MustQuery("select ('a', 'a') in (('A' collate utf8mb4_general_ci, 'A' collate utf8mb4_general_ci), ('b', 'b'));").Check(testkit.Rows("1"))
-	tk.MustQuery("select ('a', 'a') in (('A' collate utf8mb4_general_ci, 'A' collate utf8mb4_bin), ('b', 'b'));").Check(testkit.Rows("0"))
 }
 
 func TestWeightString(t *testing.T) {
