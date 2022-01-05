453bb7f0a0c4b569ac8aaf6f5aaec76b4867d1d6
Zhuomin Liu <lzmhhh123@gmail.com>
2022-01-05 16:22:36 +0800

    cherry pick #31078 to release-5.4
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/integration_serial_test.go
+++ expression/integration_serial_test.go
@@ -2043,10 +2043,8 @@ func TestTimeBuiltin(t *testing.T) {
 	result = tk.MustQuery(`select quarter("2012-14-20"), quarter("aa"), quarter(null), quarter(11), quarter(12.99);`)
 	result.Check(testkit.Rows("<nil> <nil> <nil> <nil> <nil>"))
 	result = tk.MustQuery(`select quarter("0000-00-00"), quarter("0000-00-00 00:00:00");`)
-	result.Check(testkit.Rows("<nil> <nil>"))
-	tk.MustQuery("show warnings").Check(testutil.RowsWithSep("|",
-		"Warning|1292|Incorrect datetime value: '0000-00-00 00:00:00.000000'",
-		"Warning|1292|Incorrect datetime value: '0000-00-00 00:00:00.000000'"))
+	result.Check(testkit.Rows("0 0"))
+	tk.MustQuery("show warnings").Check(testkit.Rows())
 	result = tk.MustQuery(`select quarter(0), quarter(0.0), quarter(0e1), quarter(0.00);`)
 	result.Check(testkit.Rows("0 0 0 0"))
 	tk.MustQuery("show warnings").Check(testkit.Rows())
