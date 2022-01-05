7f70fb27a92303e4e02ac36dbc00f1b0d9712e7a
glorv <glorvs@163.com>
2022-01-05 17:33:07 +0800

    add integration test for gbk

--- br/tests/lightning_new_collation/run.sh
+++ br/tests/lightning_new_collation/run.sh
@@ -46,6 +46,15 @@ for BACKEND in local importer tidb; do
   check_contains "id: 1"
   check_contains "v2: 啊啊"
 
+  run_lightning --backend $BACKEND -d "tests/$TEST_NAME/data-gbk" --config "tests/$TEST_NAME/gbk.toml"
+
+  run_sql 'SELECT count(*) from nc.gbk_source;'
+  check_contains "count(*): 3"
+
+  run_sql 'SELECT id, v from nc.gbk_source order by v limit 1;'
+  check_contains "id: 1"
+  check_contains "v2: 啊啊"
+
 done
 
 # restart with original config
