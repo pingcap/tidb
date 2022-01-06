e74d5750e1e926213fcb05dc0f6a29cf4911b2a8
CalvinNeo <calvinneo1995@gmail.com>
2022-01-06 15:12:40 +0800

    fix
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- ddl/ddl_tiflash_api.go
+++ ddl/ddl_tiflash_api.go
@@ -1,8 +1,8 @@
-// Copyright 2016 PingCAP, Inc.
+// Copyright 2022 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the LiceMakeNewRulense at
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -15,6 +15,7 @@
 // Copyright 2013 The ql Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSES/QL-LICENSE file.
+
 
 package ddl
 
