a1ec384bfb503dd368250b35ca8b163e4b97293a
mornyx <mornyx.z@gmail.com>
2022-01-06 12:48:01 +0800

    Fix lint; Add license
    Signed-off-by: mornyx <mornyx.z@gmail.com>

--- util/topsql/reporter/single_target_test.go
+++ util/topsql/reporter/single_target_test.go
@@ -1,3 +1,17 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package reporter
 
 import (
@@ -60,5 +74,6 @@ func TestSingleTargetDataSink(t *testing.T) {
 	assert.True(t, exist)
 	assert.Equal(t, sqlMeta.NormalizedSql, "SQL-1")
 	normalizedPlan, exist := server.GetPlanMetaByDigestBlocking([]byte("P1"), 5*time.Second)
+	assert.True(t, exist)
 	assert.Equal(t, normalizedPlan, "PLAN-1")
 }
