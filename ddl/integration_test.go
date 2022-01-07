10a922dfa58f8df8edf39c24bf1ad8fcb5a661b3
tangenta <tangenta@126.com>
2022-01-07 11:22:37 +0800

    parser: use utf-8 charset to decode enum binary string literal (#31336)
    close pingcap/tidb#30740

--- ddl/integration_test.go
+++ ddl/integration_test.go
@@ -73,10 +73,14 @@ func TestDefaultValueInEnum(t *testing.T) {
 	tk.MustExec("insert into t values (1), (2);")                 // Use 1-base index to locate the value.
 	tk.MustQuery("select a from t;").Check(testkit.Rows("a", "")) // 0x91 is truncate.
 	tk.MustExec("drop table t;")
+	tk.MustExec("create table t (a enum('a', 0x91)) charset gbk;") // Test for table charset.
+	tk.MustExec("insert into t values (1), (2);")
+	tk.MustQuery("select a from t;").Check(testkit.Rows("a", ""))
+	tk.MustExec("drop table t;")
 	tk.MustGetErrMsg("create table t(a set('a', 0x91, '') charset gbk);",
 		"[types:1291]Column 'a' has duplicated value '' in SET")
-	// Test valid gbk string value in enum.
-	tk.MustExec("create table t (a enum('a', 0xC4E3BAC3) charset gbk);")
+	// Test valid utf-8 string value in enum. Note that the binary literal only can be decoded to utf-8.
+	tk.MustExec("create table t (a enum('a', 0xE4BDA0E5A5BD) charset gbk);")
 	tk.MustExec("insert into t values (1), (2);")
 	tk.MustQuery("select a from t;").Check(testkit.Rows("a", "你好"))
 }
