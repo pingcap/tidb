bc5978a0c12e4f8bb79fc971d2a96a567da62a1f
3pointer <luancheng@pingcap.com>
2022-01-11 20:31:47 +0800

    public RunInGoTest for mock store

--- server/server.go
+++ server/server.go
@@ -73,7 +73,7 @@ var (
 	serverPID   int
 	osUser      string
 	osVersion   string
-	runInGoTest bool
+	RunInGoTest bool
 )
 
 func init() {
@@ -235,7 +235,7 @@ func NewServer(cfg *config.Config, driver IDriver) (*Server, error) {
 		s.capability |= mysql.ClientSSL
 	}
 
-	if s.cfg.Host != "" && (s.cfg.Port != 0 || runInGoTest) {
+	if s.cfg.Host != "" && (s.cfg.Port != 0 || RunInGoTest) {
 		addr := fmt.Sprintf("%s:%d", s.cfg.Host, s.cfg.Port)
 		tcpProto := "tcp"
 		if s.cfg.EnableTCP4Only {
@@ -245,7 +245,7 @@ func NewServer(cfg *config.Config, driver IDriver) (*Server, error) {
 			return nil, errors.Trace(err)
 		}
 		logutil.BgLogger().Info("server is running MySQL protocol", zap.String("addr", addr))
-		if runInGoTest && s.cfg.Port == 0 {
+		if RunInGoTest && s.cfg.Port == 0 {
 			s.cfg.Port = uint(s.listener.Addr().(*net.TCPAddr).Port)
 		}
 	}
