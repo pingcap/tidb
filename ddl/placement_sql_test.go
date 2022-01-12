c04ea51cacb635165731f2895d8168b56a9461b1
王超 <cclcwangchao@hotmail.com>
2022-01-12 21:43:42 +0800

    *: Add some tests for CreateTable/SchemaWithInfo for placement (#31602)
    close pingcap/tidb#31455

--- ddl/placement_sql_test.go
+++ ddl/placement_sql_test.go
@@ -657,6 +657,53 @@ func (s *testDBSuite6) TestPlacementMode(c *C) {
 		" PARTITION `p1` VALUES LESS THAN (1000),\n" +
 		" PARTITION `p2` VALUES LESS THAN (10000),\n" +
 		" PARTITION `p3` VALUES LESS THAN (100000))"))
+
+	// create tableWithInfo in ignore mode
+	tk.MustExec("drop table if exists t2")
+	tbl, err := s.getClonedTable("test", "t1")
+	c.Assert(err, IsNil)
+	c.Assert(tbl.PlacementPolicyRef, NotNil)
+	tbl.Name = model.NewCIStr("t2")
+	err = s.dom.DDL().CreateTableWithInfo(tk.Se, model.NewCIStr("test"), tbl, ddl.OnExistError)
+	c.Assert(err, IsNil)
+	tk.MustQuery("show create table t2").Check(testkit.Rows("t2 CREATE TABLE `t2` (\n" +
+		"  `id` int(11) DEFAULT NULL\n" +
+		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='aaa'"))
+
+	// createTableWithInfo in ignore mode (policy not exists)
+	tk.MustExec("drop table if exists t2")
+	tbl, err = s.getClonedTable("test", "t1")
+	c.Assert(err, IsNil)
+	c.Assert(tbl.PlacementPolicyRef, NotNil)
+	tbl.Name = model.NewCIStr("t2")
+	tbl.PlacementPolicyRef.Name = model.NewCIStr("pxx")
+	err = s.dom.DDL().CreateTableWithInfo(tk.Se, model.NewCIStr("test"), tbl, ddl.OnExistError)
+	c.Assert(err, IsNil)
+	tk.MustQuery("show create table t2").Check(testkit.Rows("t2 CREATE TABLE `t2` (\n" +
+		"  `id` int(11) DEFAULT NULL\n" +
+		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='aaa'"))
+
+	// createSchemaWithInfo in ignore mode
+	tk.MustExec("drop database if exists db2")
+	db1, ok := s.getClonedDatabase("db1")
+	c.Assert(ok, IsTrue)
+	c.Assert(db1.PlacementPolicyRef, NotNil)
+	db1.Name = model.NewCIStr("db2")
+	err = s.dom.DDL().CreateSchemaWithInfo(tk.Se, db1, ddl.OnExistError)
+	c.Assert(err, IsNil)
+	tk.MustQuery("show create database db2").Check(testkit.Rows("db2 CREATE DATABASE `db2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */"))
+
+	// createSchemaWithInfo in ignore mode (policy not exists)
+	tk.MustExec("drop database if exists db2")
+	db1, ok = s.getClonedDatabase("db1")
+	c.Assert(ok, IsTrue)
+	c.Assert(db1.PlacementPolicyRef, NotNil)
+	db1.Name = model.NewCIStr("db2")
+	db1.PlacementPolicyRef.Name = model.NewCIStr("pxx")
+	err = s.dom.DDL().CreateSchemaWithInfo(tk.Se, db1, ddl.OnExistError)
+	c.Assert(err, IsNil)
+	tk.MustQuery("show create database db2").Check(testkit.Rows("db2 CREATE DATABASE `db2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */"))
+
 }
 
 func (s *testDBSuite6) TestPlacementTiflashCheck(c *C) {
