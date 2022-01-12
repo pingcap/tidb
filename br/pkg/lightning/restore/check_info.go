5e7dae91e321c85de390693b94d9a3e8b8d08711
disksing <i@disksing.com>
2022-01-12 17:17:20 +0800

    store: avoid importing pd/server
    fix #31616
    
    Signed-off-by: disksing <i@disksing.com>

--- br/pkg/lightning/restore/check_info.go
+++ br/pkg/lightning/restore/check_info.go
@@ -41,9 +41,8 @@ import (
 	"github.com/pingcap/tidb/br/pkg/version"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/parser/mysql"
+	"github.com/pingcap/tidb/store/pdtypes"
 	"github.com/pingcap/tidb/table/tables"
-	"github.com/tikv/pd/server/api"
-	pdconfig "github.com/tikv/pd/server/config"
 
 	"go.uber.org/zap"
 	"modernc.org/mathutil"
@@ -72,7 +71,7 @@ func (rc *Controller) isSourceInLocal() bool {
 }
 
 func (rc *Controller) getReplicaCount(ctx context.Context) (uint64, error) {
-	result := &pdconfig.ReplicationConfig{}
+	result := &pdtypes.ReplicationConfig{}
 	err := rc.tls.WithHost(rc.cfg.TiDB.PdAddr).GetJSON(ctx, pdReplicate, &result)
 	if err != nil {
 		return 0, errors.Trace(err)
@@ -81,7 +80,7 @@ func (rc *Controller) getReplicaCount(ctx context.Context) (uint64, error) {
 }
 
 func (rc *Controller) getClusterAvail(ctx context.Context) (uint64, error) {
-	result := &api.StoresInfo{}
+	result := &pdtypes.StoresInfo{}
 	if err := rc.tls.WithHost(rc.cfg.TiDB.PdAddr).GetJSON(ctx, pdStores, result); err != nil {
 		return 0, errors.Trace(err)
 	}
@@ -183,7 +182,7 @@ func (rc *Controller) checkEmptyRegion(ctx context.Context) error {
 	defer func() {
 		rc.checkTemplate.Collect(Critical, passed, message)
 	}()
-	storeInfo := &api.StoresInfo{}
+	storeInfo := &pdtypes.StoresInfo{}
 	err := rc.tls.WithHost(rc.cfg.TiDB.PdAddr).GetJSON(ctx, pdStores, storeInfo)
 	if err != nil {
 		return errors.Trace(err)
@@ -192,12 +191,12 @@ func (rc *Controller) checkEmptyRegion(ctx context.Context) error {
 		return nil
 	}
 
-	var result api.RegionsInfo
+	var result pdtypes.RegionsInfo
 	if err := rc.tls.WithHost(rc.cfg.TiDB.PdAddr).GetJSON(ctx, pdEmptyRegions, &result); err != nil {
 		return errors.Trace(err)
 	}
 	regions := make(map[uint64]int)
-	stores := make(map[uint64]*api.StoreInfo)
+	stores := make(map[uint64]*pdtypes.StoreInfo)
 	for _, region := range result.Regions {
 		for _, peer := range region.Peers {
 			regions[peer.StoreId]++
@@ -260,12 +259,12 @@ func (rc *Controller) checkRegionDistribution(ctx context.Context) error {
 		rc.checkTemplate.Collect(Critical, passed, message)
 	}()
 
-	result := &api.StoresInfo{}
+	result := &pdtypes.StoresInfo{}
 	err := rc.tls.WithHost(rc.cfg.TiDB.PdAddr).GetJSON(ctx, pdStores, result)
 	if err != nil {
 		return errors.Trace(err)
 	}
-	stores := make([]*api.StoreInfo, 0, len(result.Stores))
+	stores := make([]*pdtypes.StoreInfo, 0, len(result.Stores))
 	for _, store := range result.Stores {
 		if store.Store.State != metapb.StoreState_Up {
 			continue
