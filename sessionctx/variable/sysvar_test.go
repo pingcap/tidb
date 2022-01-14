6ac5ecf8a570b1e631b2d78ae39d2367a0685284
cbcwestwolf <1004626265@qq.com>
2022-01-14 11:56:45 +0800

    Change the order of parameter in the require.Equal

--- sessionctx/variable/sysvar_test.go
+++ sessionctx/variable/sysvar_test.go
@@ -851,11 +851,11 @@ func TestNetBufferLength(t *testing.T) {
 
 	val, err := netBufferLength.Validate(vars, "1", ScopeGlobal)
 	require.NoError(t, err)
-	require.Equal(t, val, "1024") // converts it to min value
+	require.Equal(t, "1024", val) // converts it to min value
 	val, err = netBufferLength.Validate(vars, "10485760", ScopeGlobal)
 	require.NoError(t, err)
-	require.Equal(t, val, "1048576") // converts it to max value
+	require.Equal(t, "1048576", val) // converts it to max value
 	val, err = netBufferLength.Validate(vars, "524288", ScopeGlobal)
 	require.NoError(t, err)
-	require.Equal(t, val, "524288") // unchanged
+	require.Equal(t, "524288", val) // unchanged
 }
