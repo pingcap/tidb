52b5352474da253ca1ce01e4d893cb15a30aeeb4
mornyx <mornyx.z@gmail.com>
2022-01-14 19:54:57 +0800

    Add maxStmtStatsSize
    Signed-off-by: mornyx <mornyx.z@gmail.com>

--- util/topsql/stmtstats/aggregator.go
+++ util/topsql/stmtstats/aggregator.go
@@ -23,6 +23,8 @@ import (
 	"go.uber.org/atomic"
 )
 
+const maxStmtStatsSize = 1000000
+
 // globalAggregator is global *aggregator.
 var globalAggregator = newAggregator()
 
@@ -32,6 +34,7 @@ var globalAggregator = newAggregator()
 type aggregator struct {
 	ctx        context.Context
 	cancel     context.CancelFunc
+	statsLen   atomic.Uint32
 	statsSet   sync.Map // map[*StatementStats]struct{}
 	collectors sync.Map // map[Collector]struct{}
 	running    *atomic.Bool
@@ -86,6 +89,10 @@ func (m *aggregator) aggregate() {
 // register binds StatementStats to aggregator.
 // register is thread-safe.
 func (m *aggregator) register(stats *StatementStats) {
+	if m.statsLen.Load() > maxStmtStatsSize {
+		return
+	}
+	m.statsLen.Inc()
 	m.statsSet.Store(stats, struct{}{})
 }
 
@@ -93,6 +100,7 @@ func (m *aggregator) register(stats *StatementStats) {
 // unregister is thread-safe.
 func (m *aggregator) unregister(stats *StatementStats) {
 	m.statsSet.Delete(stats)
+	m.statsLen.Dec()
 }
 
 // registerCollector binds a Collector to aggregator.
