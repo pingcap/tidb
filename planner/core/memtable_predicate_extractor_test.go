c28908791b8b053d7dd7314615d8ebc6f323d279
Weizhen Wang <wangweizhen@pingcap.com>
2022-01-20 20:41:45 +0800

    *: add pushdown for ShowStmt and implement for show columns (#31742)
    close pingcap/tidb#29910

--- planner/core/memtable_predicate_extractor_test.go
+++ planner/core/memtable_predicate_extractor_test.go
@@ -23,12 +23,15 @@ import (
 	. "github.com/pingcap/check"
 	"github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/errno"
 	"github.com/pingcap/tidb/parser"
 	plannercore "github.com/pingcap/tidb/planner/core"
 	"github.com/pingcap/tidb/session"
 	"github.com/pingcap/tidb/store/mockstore"
 	"github.com/pingcap/tidb/util/hint"
 	"github.com/pingcap/tidb/util/set"
+	"github.com/pingcap/tidb/util/testutil"
+	"github.com/stretchr/testify/require"
 )
 
 var _ = Suite(&extractorSuite{})
@@ -1420,3 +1423,281 @@ func (s *extractorSuite) TestTiDBHotRegionsHistoryTableExtractor(c *C) {
 		}
 	}
 }
+<<<<<<< HEAD
+=======
+
+func TestTikvRegionPeersExtractor(t *testing.T) {
+	store, dom, clean := testkit.CreateMockStoreAndDomain(t)
+	defer clean()
+
+	se, err := session.CreateSession4Test(store)
+	require.NoError(t, err)
+
+	var cases = []struct {
+		sql                 string
+		regionIDs, storeIDs []uint64
+		skipRequest         bool
+	}{
+		// Test `region_id`, `store_id` columns.
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id=100",
+			regionIDs: []uint64{100},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where 100=region_id",
+			regionIDs: []uint64{100},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where 100=region_id or region_id=101",
+			regionIDs: []uint64{100, 101},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where 100=region_id or region_id=101 or region_id=102 or 103 = region_id",
+			regionIDs: []uint64{100, 101, 102, 103},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where (region_id=100 or region_id=101) and (store_id=200 or store_id=201)",
+			regionIDs: []uint64{100, 101},
+			storeIDs:  []uint64{200, 201},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id in (100, 101)",
+			regionIDs: []uint64{100, 101},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id in (100, 101) and store_id=200",
+			regionIDs: []uint64{100, 101},
+			storeIDs:  []uint64{200},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id in (100, 101) and store_id in (200, 201)",
+			regionIDs: []uint64{100, 101},
+			storeIDs:  []uint64{200, 201},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id=100 and store_id in (200, 201)",
+			regionIDs: []uint64{100},
+			storeIDs:  []uint64{200, 201},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id=100 and store_id=200",
+			regionIDs: []uint64{100},
+			storeIDs:  []uint64{200},
+		},
+		{
+			sql:         "select * from information_schema.tikv_region_peers where region_id=100 and region_id=101",
+			skipRequest: true,
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id=100 and region_id in (100,101)",
+			regionIDs: []uint64{100},
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id=100 and region_id in (100,101) and store_id=200 and store_id in (200,201)",
+			regionIDs: []uint64{100},
+			storeIDs:  []uint64{200},
+		},
+		{
+			sql:         "select * from information_schema.tikv_region_peers where region_id=100 and region_id in (101,102)",
+			skipRequest: true,
+		},
+		{
+			sql:         "select * from information_schema.tikv_region_peers where region_id=100 and region_id in (101,102) and store_id=200 and store_id in (200,201)",
+			skipRequest: true,
+		},
+		{
+			sql:         "select * from information_schema.tikv_region_peers where region_id=100 and region_id in (100,101) and store_id=200 and store_id in (201,202)",
+			skipRequest: true,
+		},
+		{
+			sql: `select * from information_schema.tikv_region_peers
+								where region_id=100 and region_id in (100,101)
+								and store_id=200 and store_id in (201,202)`,
+			skipRequest: true,
+		},
+		{
+			sql:       "select * from information_schema.tikv_region_peers where region_id in (100,101) and region_id in (101,102)",
+			regionIDs: []uint64{101},
+		},
+		{
+			sql: `select * from information_schema.tikv_region_peers
+							where region_id in (100,101)
+							and region_id in (101,102)
+							and store_id in (200,201)
+							and store_id in (201,202)`,
+			regionIDs: []uint64{101},
+			storeIDs:  []uint64{201},
+		},
+		{
+			sql: `select * from information_schema.tikv_region_peers
+							where region_id in (100,101)
+							and region_id in (100,102)
+							and region_id in (102,103)
+							and region_id in (103,104)`,
+			skipRequest: true,
+		},
+		// Test columns that is not extracted by TikvRegionPeersExtractor
+		{
+			sql: `select * from information_schema.tikv_region_peers
+							where peer_id=100
+							and is_learner=0
+							and is_leader=1
+							and status='NORMAL'
+							and down_seconds=1000`,
+		},
+	}
+	parser := parser.New()
+	for _, ca := range cases {
+		logicalMemTable := getLogicalMemTable(t, dom, se, parser, ca.sql)
+		require.NotNil(t, logicalMemTable.Extractor)
+
+		tikvRegionPeersExtractor := logicalMemTable.Extractor.(*plannercore.TikvRegionPeersExtractor)
+		if len(ca.regionIDs) > 0 {
+			require.EqualValues(t, ca.regionIDs, tikvRegionPeersExtractor.RegionIDs, "SQL: %v", ca.sql)
+		}
+		if len(ca.storeIDs) > 0 {
+			require.EqualValues(t, ca.storeIDs, tikvRegionPeersExtractor.StoreIDs, "SQL: %v", ca.sql)
+		}
+	}
+}
+
+func TestColumns(t *testing.T) {
+	store, dom, clean := testkit.CreateMockStoreAndDomain(t)
+	defer clean()
+
+	se, err := session.CreateSession4Test(store)
+	require.NoError(t, err)
+
+	var cases = []struct {
+		sql                string
+		columnName         set.StringSet
+		tableSchema        set.StringSet
+		tableName          set.StringSet
+		columnNamePattern  []string
+		tableSchemaPattern []string
+		tableNamePattern   []string
+		skipRequest        bool
+	}{
+		{
+			sql:        `select * from INFORMATION_SCHEMA.COLUMNS where column_name='T';`,
+			columnName: set.NewStringSet("t"),
+		},
+		{
+			sql:         `select * from INFORMATION_SCHEMA.COLUMNS where table_schema='TEST';`,
+			tableSchema: set.NewStringSet("test"),
+		},
+		{
+			sql:       `select * from INFORMATION_SCHEMA.COLUMNS where table_name='TEST';`,
+			tableName: set.NewStringSet("test"),
+		},
+		{
+			sql:        "select * from information_schema.COLUMNS where table_name in ('TEST','t') and column_name in ('A','b')",
+			columnName: set.NewStringSet("a", "b"),
+			tableName:  set.NewStringSet("test", "t"),
+		},
+		{
+			sql:       `select * from information_schema.COLUMNS where table_name='a' and table_name in ('a', 'B');`,
+			tableName: set.NewStringSet("a"),
+		},
+		{
+			sql:         `select * from information_schema.COLUMNS where table_name='a' and table_name='B';`,
+			skipRequest: true,
+		},
+		{
+			sql:              `select * from information_schema.COLUMNS where table_name like 'T%';`,
+			tableNamePattern: []string{"(?i)T.*"},
+		},
+		{
+			sql:               `select * from information_schema.COLUMNS where column_name like 'T%';`,
+			columnNamePattern: []string{"(?i)T.*"},
+		},
+		{
+			sql:               `select * from information_schema.COLUMNS where column_name like 'i%';`,
+			columnNamePattern: []string{"(?i)i.*"},
+		},
+		{
+			sql:               `select * from information_schema.COLUMNS where column_name like 'abc%' or column_name like "def%";`,
+			columnNamePattern: []string{"(?i)abc.*|def.*"},
+		},
+		{
+			sql:               `select * from information_schema.COLUMNS where column_name like 'abc%' and column_name like "%def";`,
+			columnNamePattern: []string{"(?i)abc.*", "(?i).*def"},
+		},
+	}
+	parser := parser.New()
+	for _, ca := range cases {
+		logicalMemTable := getLogicalMemTable(t, dom, se, parser, ca.sql)
+		require.NotNil(t, logicalMemTable.Extractor)
+
+		columnsTableExtractor := logicalMemTable.Extractor.(*plannercore.ColumnsTableExtractor)
+		require.Equal(t, ca.skipRequest, columnsTableExtractor.SkipRequest, "SQL: %v", ca.sql)
+
+		require.Equal(t, ca.columnName.Count(), columnsTableExtractor.ColumnName.Count())
+		if ca.columnName.Count() > 0 && columnsTableExtractor.ColumnName.Count() > 0 {
+			require.EqualValues(t, ca.columnName, columnsTableExtractor.ColumnName, "SQL: %v", ca.sql)
+		}
+
+		require.Equal(t, ca.tableSchema.Count(), columnsTableExtractor.TableSchema.Count())
+		if ca.tableSchema.Count() > 0 && columnsTableExtractor.TableSchema.Count() > 0 {
+			require.EqualValues(t, ca.tableSchema, columnsTableExtractor.TableSchema, "SQL: %v", ca.sql)
+		}
+		require.Equal(t, ca.tableName.Count(), columnsTableExtractor.TableName.Count())
+		if ca.tableName.Count() > 0 && columnsTableExtractor.TableName.Count() > 0 {
+			require.EqualValues(t, ca.tableName, columnsTableExtractor.TableName, "SQL: %v", ca.sql)
+		}
+		require.Equal(t, len(ca.tableNamePattern), len(columnsTableExtractor.TableNamePatterns))
+		if len(ca.tableNamePattern) > 0 && len(columnsTableExtractor.TableNamePatterns) > 0 {
+			require.EqualValues(t, ca.tableNamePattern, columnsTableExtractor.TableNamePatterns, "SQL: %v", ca.sql)
+		}
+		require.Equal(t, len(ca.columnNamePattern), len(columnsTableExtractor.ColumnNamePatterns))
+		if len(ca.columnNamePattern) > 0 && len(columnsTableExtractor.ColumnNamePatterns) > 0 {
+			require.EqualValues(t, ca.columnNamePattern, columnsTableExtractor.ColumnNamePatterns, "SQL: %v", ca.sql)
+		}
+		require.Equal(t, len(ca.tableSchemaPattern), len(columnsTableExtractor.TableSchemaPatterns))
+		if len(ca.tableSchemaPattern) > 0 && len(columnsTableExtractor.TableSchemaPatterns) > 0 {
+			require.EqualValues(t, ca.tableSchemaPattern, columnsTableExtractor.TableSchemaPatterns, "SQL: %v", ca.sql)
+		}
+	}
+}
+
+func TestPredicateQuery(t *testing.T) {
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
+
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("create table t(id int, abclmn int);")
+	tk.MustQuery("select TABLE_NAME from information_schema.columns where table_schema = 'test' and column_name like 'i%'").Check(testkit.Rows("t"))
+	tk.MustQuery("select TABLE_NAME from information_schema.columns where table_schema = 'TEST' and column_name like 'I%'").Check(testkit.Rows("t"))
+	tk.MustQuery("select TABLE_NAME from information_schema.columns where table_schema = 'TEST' and column_name like 'ID'").Check(testkit.Rows("t"))
+	tk.MustQuery("select TABLE_NAME from information_schema.columns where table_schema = 'TEST' and column_name like 'id'").Check(testkit.Rows("t"))
+	tk.MustQuery("select column_name from information_schema.columns where table_schema = 'TEST' and (column_name like 'I%' or column_name like '%D')").Check(testkit.Rows("id"))
+	tk.MustQuery("select column_name from information_schema.columns where table_schema = 'TEST' and (column_name like 'abc%' and column_name like '%lmn')").Check(testkit.Rows("abclmn"))
+	tk.MustQuery("describe t").Check(testkit.Rows("id int(11) YES  <nil> ", "abclmn int(11) YES  <nil> "))
+	tk.MustQuery("describe t id").Check(testkit.Rows("id int(11) YES  <nil> "))
+	tk.MustQuery("describe t ID").Check(testkit.Rows("id int(11) YES  <nil> "))
+	tk.MustGetErrCode("describe t 'I%'", errno.ErrParse)
+	tk.MustGetErrCode("describe t I%", errno.ErrParse)
+
+	tk.MustQuery("show columns from t like 'abclmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns from t like 'ABCLMN'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns from t like 'abc%'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns from t like 'ABC%'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns from t like '%lmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns from t like '%LMN'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns in t like '%lmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns in t like '%LMN'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show fields in t like '%lmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show fields in t like '%LMN'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+
+	tk.MustQuery("show columns from t where field like '%lmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns from t where field = 'abclmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show columns in t where field = 'abclmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show fields from t where field = 'abclmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("show fields in t where field = 'abclmn'").Check(testutil.RowsWithSep(",", "abclmn,int(11),YES,,<nil>,"))
+	tk.MustQuery("explain t").Check(testkit.Rows("id int(11) YES  <nil> ", "abclmn int(11) YES  <nil> "))
+
+	tk.MustGetErrCode("show columns from t like id", errno.ErrBadField)
+	tk.MustGetErrCode("show columns from t like `id`", errno.ErrBadField)
+}
+>>>>>>> ad1c5b508 (*: add pushdown for ShowStmt and implement for show columns (#31742))
---
+++ planner/core/stringer_test.go
@@ -0,0 +1,72 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/pingcap/tidb/parser"
+	"github.com/pingcap/tidb/planner/core"
+	"github.com/pingcap/tidb/testkit"
+	"github.com/pingcap/tidb/util/hint"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPlanStringer(t *testing.T) {
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
+	tk := testkit.NewTestKit(t, store)
+
+	tk.MustExec("use test")
+	tk.MustExec("create table t(a int, b int, c int, index idx(a))")
+	tests := []struct {
+		sql  string
+		plan string
+	}{
+		{
+			sql:  "show columns from t like 'a'",
+			plan: "Show(field:[a])",
+		},
+		{
+			sql:  "show columns from t like 'a%'",
+			plan: "Show(field_pattern:[(?i)a.*])",
+		},
+		{
+			sql:  "show columns from t where field = 'a'",
+			plan: "Show->Sel([eq(Column#13, a)])->Projection",
+		},
+		{
+			sql:  "desc t",
+			plan: "Show",
+		},
+		{
+			sql:  "desc t a",
+			plan: "Show(field:[a])",
+		},
+	}
+	parser := parser.New()
+	for _, tt := range tests {
+		stmt, err := parser.ParseOneStmt(tt.sql, "", "")
+		require.NoError(t, err, "for %s", tt.sql)
+		ret := &core.PreprocessorReturn{}
+		builder, _ := core.NewPlanBuilder().Init(tk.Session(), ret.InfoSchema, &hint.BlockHintProcessor{})
+		p, err := builder.Build(context.TODO(), stmt)
+		require.NoError(t, err, "for %s", tt.sql)
+		p, err = core.LogicalOptimize(context.TODO(), builder.GetOptFlag(), p.(core.LogicalPlan))
+		require.NoError(t, err, "for %s", tt.sql)
+		require.Equal(t, tt.plan, core.ToString(p))
+	}
+}
