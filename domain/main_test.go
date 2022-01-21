bb0b370a341272dae929fa256ce5dbb416875281
Zhi Qi <qizhi@pingcap.com>
2022-01-21 21:08:22 +0800

    remove

--- domain/main_test.go
+++ domain/main_test.go
@@ -17,7 +17,6 @@ package domain_test
 import (
 	"testing"
 
-	. "github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/util/testbridge"
 	"go.uber.org/goleak"
 )
@@ -34,7 +33,7 @@ func TestMain(m *testing.M) {
 // TestDomainSerial handles tests in serial
 func TestDomainSerial(t *testing.T) {
 	// these tests should run in serial for failpoint is global
-	t.Run("info", SubTestInfo)
-	t.Run("domain", SubTestDomain)
-	t.Run("domainSession", SubTestDomainSession)
+	//t.Run("info", SubTestInfo)
+	//t.Run("domain", SubTestDomain)
+	//t.Run("domainSession", SubTestDomainSession)
 }
