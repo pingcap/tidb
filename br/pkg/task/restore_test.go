43d40feae3af6481ec3acd501ba01b0fe00684fe
fengou1 <feng.ou@pingcap.com>
2022-01-23 17:39:30 +0800

    refactor code, create a new func for configuring the restore client

--- br/pkg/task/restore_test.go
+++ br/pkg/task/restore_test.go
@@ -3,11 +3,19 @@
 package task
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/pingcap/tidb/br/pkg/restore"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/keepalive"
 )
+
+var defaultKeepaliveCfg = keepalive.ClientParameters{
+	Time:    3 * time.Second,
+	Timeout: 10 * time.Second,
+}
 
 func TestRestoreConfigAdjust(t *testing.T) {
 	cfg := &RestoreConfig{}
@@ -17,4 +25,25 @@ func TestRestoreConfigAdjust(t *testing.T) {
 	require.Equal(t, defaultSwitchInterval, cfg.Config.SwitchModeInterval)
 	require.Equal(t, restore.DefaultMergeRegionKeyCount, cfg.MergeSmallRegionKeyCount)
 	require.Equal(t, restore.DefaultMergeRegionSizeBytes, cfg.MergeSmallRegionSizeBytes)
+}
+
+func TestconfigureRestoreClient(t *testing.T) {
+	cfg := Config{
+		Concurrency: 1024,
+	}
+	restoreComCfg := RestoreCommonConfig{
+		Online: true,
+	}
+	restoreCfg := &RestoreConfig{
+		Config:              cfg,
+		RestoreCommonConfig: restoreComCfg,
+		DdlBatchSize:        128,
+	}
+	client := &restore.Client{}
+
+	ctx := context.Background()
+	err := configureRestoreClient(ctx, client, restoreCfg)
+	require.NoError(t, err)
+	require.Equal(t, client.GetBatchDdlSize(), 128)
+	require.True(t, true, client.IsOnline())
 }
