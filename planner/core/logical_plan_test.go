2b48a279d24573859d33b53391f63d247341b2ec
yibin <huyibin@pingcap.com>
2022-01-24 17:06:11 +0800

    cherry pick #31400 to release-5.2
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/logical_plan_test.go
+++ planner/core/logical_plan_test.go
@@ -92,6 +92,44 @@ func (s *testPlanSuite) TestPredicatePushDown(c *C) {
 	}
 }
 
+<<<<<<< HEAD
+=======
+// Issue: 31399
+func (s *testPlanSuite) TestImplicitCastNotNullFlag(c *C) {
+	defer testleak.AfterTest(c)()
+	ctx := context.Background()
+	ca := "select count(*) from t3 group by a having bit_and(b) > 1;"
+	comment := Commentf("for %s", ca)
+	stmt, err := s.ParseOneStmt(ca, "", "")
+	c.Assert(err, IsNil, comment)
+	p, _, err := BuildLogicalPlanForTest(ctx, s.ctx, stmt, s.is)
+	c.Assert(err, IsNil)
+	p, err = logicalOptimize(context.TODO(), flagPredicatePushDown|flagJoinReOrder|flagPrunColumns|flagEliminateProjection, p.(LogicalPlan))
+	c.Assert(err, IsNil)
+	// AggFuncs[0] is count; AggFuncs[1] is bit_and, args[0] is return type of the implicit cast
+	castNotNullFlag := (p.(*LogicalProjection).children[0].(*LogicalSelection).children[0].(*LogicalAggregation).AggFuncs[1].Args[0].GetType().Flag) & mysql.NotNullFlag
+	var nullableFlag uint = 0
+	c.Assert(castNotNullFlag, Equals, nullableFlag)
+}
+
+func (s *testPlanSuite) TestEliminateProjectionUnderUnion(c *C) {
+	defer testleak.AfterTest(c)()
+	ctx := context.Background()
+	ca := "Select a from t3 join ( (select 127 as IDD from t3) union all (select 1 as IDD from t3) ) u on t3.b = u.IDD;"
+	comment := Commentf("for %s", ca)
+	stmt, err := s.ParseOneStmt(ca, "", "")
+	c.Assert(err, IsNil, comment)
+	p, _, err := BuildLogicalPlanForTest(ctx, s.ctx, stmt, s.is)
+	c.Assert(err, IsNil)
+	p, err = logicalOptimize(context.TODO(), flagPredicatePushDown|flagJoinReOrder|flagPrunColumns|flagEliminateProjection, p.(LogicalPlan))
+	c.Assert(err, IsNil)
+	// after folding constants, the null flag should keep the same with the old one's (i.e., the schema's).
+	schemaNullFlag := p.(*LogicalProjection).children[0].(*LogicalJoin).children[1].Children()[1].(*LogicalProjection).schema.Columns[0].RetType.Flag & mysql.NotNullFlag
+	exprNullFlag := p.(*LogicalProjection).children[0].(*LogicalJoin).children[1].Children()[1].(*LogicalProjection).Exprs[0].GetType().Flag & mysql.NotNullFlag
+	c.Assert(schemaNullFlag, Equals, exprNullFlag)
+}
+
+>>>>>>> 9ad0fa341... expression: pass the arg's nullable type to target type for cast function (#31400)
 func (s *testPlanSuite) TestJoinPredicatePushDown(c *C) {
 	defer testleak.AfterTest(c)()
 	var (
