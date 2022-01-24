dfdc89a1e77e40ffceffa4b4ede2cc0e92a6301f
yisaer <disxiaofei@163.com>
2022-01-24 14:01:03 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- planner/core/plan_test.go
+++ planner/core/plan_test.go
@@ -679,11 +679,12 @@ func TestCopPaging(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		tk.MustQuery("explain format='brief' select * from t force index(i) where mod(id, 2) > 0 and id <= 1024 and c1 >= 0 and c1 <= 1024 and c2 in (2, 4, 6, 8) order by c1 limit 960").Check(kit.Rows(
 			"Limit 3.20 root  offset:0, count:960",
-			"└─IndexLookUp 3.20 root  paging:true",
-			"  ├─Selection(Build) 819.20 cop[tikv]  gt(mod(test.t.id, 2), 0), le(test.t.id, 1024)",
-			"  │ └─IndexRangeScan 1024.00 cop[tikv] table:t, index:i(c1) range:[0,1024], keep order:true",
-			"  └─Selection(Probe) 3.20 cop[tikv]  in(test.t.c2, 2, 4, 6, 8)",
-			"    └─TableRowIDScan 819.20 cop[tikv] table:t keep order:false"))
+			"└─Selection 2.56 root  gt(mod(test.t.id, 2), 0)",
+			"  └─IndexLookUp 3.20 root  paging:true",
+			"    ├─Selection(Build) 819.20 cop[tikv]  le(test.t.id, 1024)",
+			"    │ └─IndexRangeScan 1024.00 cop[tikv] table:t, index:i(c1) range:[0,1024], keep order:true",
+			"    └─Selection(Probe) 3.20 cop[tikv]  in(test.t.c2, 2, 4, 6, 8)",
+			"      └─TableRowIDScan 819.20 cop[tikv] table:t keep order:false"))
 	}
 
 	// limit 961 exceeds the threshold, it should not go paging
@@ -701,10 +702,11 @@ func TestCopPaging(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		tk.MustQuery("explain format='brief' select * from t force index(i) where mod(id, 2) > 0 and id <= 1024 and c1 >= 0 and c1 <= 1024 and c2 in (2, 4, 6, 8) order by c1 limit 961").Check(kit.Rows(
 			"Limit 3.20 root  offset:0, count:961",
-			"└─IndexLookUp 3.20 root  ",
-			"  ├─Selection(Build) 819.20 cop[tikv]  gt(mod(test.t.id, 2), 0), le(test.t.id, 1024)",
-			"  │ └─IndexRangeScan 1024.00 cop[tikv] table:t, index:i(c1) range:[0,1024], keep order:true",
-			"  └─Selection(Probe) 3.20 cop[tikv]  in(test.t.c2, 2, 4, 6, 8)",
-			"    └─TableRowIDScan 819.20 cop[tikv] table:t keep order:false"))
+			"└─Selection 2.56 root  gt(mod(test.t.id, 2), 0)",
+			"  └─IndexLookUp 3.20 root  ",
+			"    ├─Selection(Build) 819.20 cop[tikv]  le(test.t.id, 1024)",
+			"    │ └─IndexRangeScan 1024.00 cop[tikv] table:t, index:i(c1) range:[0,1024], keep order:true",
+			"    └─Selection(Probe) 3.20 cop[tikv]  in(test.t.c2, 2, 4, 6, 8)",
+			"      └─TableRowIDScan 819.20 cop[tikv] table:t keep order:false"))
 	}
 }
