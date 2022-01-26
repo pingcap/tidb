374cbd8fa5338dc9f8f456f580cfd5f65fdb463b
Weizhen Wang <wangweizhen@pingcap.com>
2022-01-26 12:22:38 +0800

    ddl: unstable test in the TestDomain
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- ddl/ddl_tiflash_api.go
+++ ddl/ddl_tiflash_api.go
@@ -411,8 +411,14 @@ func HandlePlacementRuleRoutine(ctx sessionctx.Context, d *ddl, tableList []TiFl
 }
 
 func (d *ddl) PollTiFlashRoutine() {
+	defer d.wg.Done()
 	pollTiflashContext := NewTiFlashManagementContext()
 	for {
+		select {
+		case <-d.ctx.Done():
+			return
+		case <-time.After(PollTiFlashInterval):
+		}
 		if d.IsTiFlashPollEnabled() {
 			if d.sessPool == nil {
 				logutil.BgLogger().Error("failed to get sessionPool for pollTiFlashReplicaStatus")
@@ -436,11 +442,6 @@ func (d *ddl) PollTiFlashRoutine() {
 				}
 				logutil.BgLogger().Error("failed to get session for pollTiFlashReplicaStatus", zap.Error(err))
 			}
-		}
-		select {
-		case <-d.ctx.Done():
-			return
-		case <-time.After(PollTiFlashInterval):
 		}
 	}
 }
