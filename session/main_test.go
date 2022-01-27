c81b6f48da14b2f47e48166a655dbf571d750c4f
disksing <i@disksing.com>
2022-01-27 16:32:50 +0800

    session: cleanup goleak ignore list
    close #32023
    
    Signed-off-by: disksing <i@disksing.com>

--- session/main_test.go
+++ session/main_test.go
@@ -54,24 +54,10 @@ func TestMain(m *testing.M) {
 	})
 	tikv.EnableFailpoints()
 	opts := []goleak.Option{
-		// TODO: figure the reason and shorten this list
-		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/internal/retry.newBackoffFn.func1"),
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/pkg/logutil.(*MergeLogger).outputLoop"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
-		goleak.IgnoreTopFunction("google.golang.org/grpc.(*addrConn).resetTransport"),
-		goleak.IgnoreTopFunction("google.golang.org/grpc.(*ccBalancerWrapper).watcher"),
-		goleak.IgnoreTopFunction("google.golang.org/grpc/internal/transport.(*controlBuffer).get"),
-		goleak.IgnoreTopFunction("google.golang.org/grpc/internal/transport.(*http2Client).keepalive"),
-		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
-		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
 	}
-	callback := func(i int) int {
-		// wait for MVCCLevelDB to close, MVCCLevelDB will be closed in one second
-		time.Sleep(time.Second)
-		testDataMap.GenerateOutputIfNeeded()
-		return i
-	}
-	goleak.VerifyTestMain(testmain.WrapTestingM(m, callback), opts...)
+	goleak.VerifyTestMain(m, opts...)
 }
 
 func GetClusteredIndexSuiteData() testdata.TestData {
