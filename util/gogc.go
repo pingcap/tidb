38d5b96cfa146b7601b33886fd144c13ae450388
Weizhen Wang <wangweizhen@pingcap.com>
2022-02-03 00:08:16 +0800

    metrics: add metrics for golang gc
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- util/gogc.go
+++ util/gogc.go
@@ -18,20 +18,19 @@ import (
 	"os"
 	"runtime/debug"
 	"strconv"
-	"sync/atomic"
 	"time"
 
 	"github.com/pingcap/tidb/metrics"
+	atomicutil "go.uber.org/atomic"
 )
 
-var gogcValue int64
+var gogcValue atomicutil.Int64 = *atomicutil.NewInt64(100)
 
 func init() {
-	gogcValue = 100
 	if val, err := strconv.Atoi(os.Getenv("GOGC")); err == nil {
-		gogcValue = int64(val)
+		gogcValue.Store(int64(val))
 	}
-	metrics.GOGC.Set(float64(gogcValue))
+	metrics.GOGC.Set(float64(gogcValue.Load()))
 }
 
 // SetGOGC update GOGC and related metrics.
@@ -41,12 +40,12 @@ func SetGOGC(val int) {
 	}
 	debug.SetGCPercent(val)
 	metrics.GOGC.Set(float64(val))
-	atomic.StoreInt64(&gogcValue, int64(val))
+	gogcValue.Store(int64(val))
 }
 
 // GetGOGC returns the current value of GOGC.
 func GetGOGC() int {
-	return int(atomic.LoadInt64(&gogcValue))
+	return int(gogcValue.Load())
 }
 
 type GOGCStatSampler struct {
