fc84b87148a7a66c01e8788f68efe24165f77dd4
disksing <i@disksing.com>
2022-02-10 11:25:16 +0800

    address comments
    Signed-off-by: disksing <i@disksing.com>

--- store/pdtypes/statistics.go
+++ store/pdtypes/statistics.go
@@ -1,4 +1,4 @@
-// Copyright 2021 PingCAP, Inc.
+// Copyright 2022 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
