d27a7181bd47cf67f16d517b5c3f1b7b7b95e5ba
Jack Yu <jackysp@gmail.com>
2022-02-15 14:51:27 +0800

    remove grafonnet-lib
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- metrics/grafana/generate_json.sh
+++ metrics/grafana/generate_json.sh
@@ -16,6 +16,7 @@
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 git clone https://github.com/grafana/grafonnet-lib.git
 
-JSONNET_PATH=grafonnet-lib \
-  jsonnet tidb_summary.jsonnet > tidb_summary.json
+export JSONNET_PATH=grafonnet-lib
+jsonnet tidb_summary.jsonnet > tidb_summary.json
+rm -rf $JSONNET_PATH
 
