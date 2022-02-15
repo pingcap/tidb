e34e9e40e15b31c0eb36ab423539cd8384988b54
Ziqian Qin <ekexium@gmail.com>
2022-02-15 14:31:38 +0800

    cherry pick #32308 to release-5.4
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -9830,3 +9830,17 @@ func (s *testSerialSuite) TestIssue30971(c *C) {
 		c.Assert(fields, HasLen, test.fields)
 	}
 }
+
+func (s *testSerialSuite) TestEncodingSet(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("CREATE TABLE `enum-set` (`set` SET(" +
+		"'x00','x01','x02','x03','x04','x05','x06','x07','x08','x09','x10','x11','x12','x13','x14','x15'," +
+		"'x16','x17','x18','x19','x20','x21','x22','x23','x24','x25','x26','x27','x28','x29','x30','x31'," +
+		"'x32','x33','x34','x35','x36','x37','x38','x39','x40','x41','x42','x43','x44','x45','x46','x47'," +
+		"'x48','x49','x50','x51','x52','x53','x54','x55','x56','x57','x58','x59','x60','x61','x62','x63'" +
+		")NOT NULL PRIMARY KEY)")
+	tk.MustExec("INSERT INTO `enum-set` VALUES\n(\"x00,x59\");")
+	tk.MustQuery("select `set` from `enum-set` use index(PRIMARY)").Check(testkit.Rows("x00,x59"))
+	tk.MustExec("admin check table `enum-set`")
+}
