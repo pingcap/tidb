0ae8a31bd0029d100baae634098b9f418d93048b
Reminiscent <y870414423@gmail.com>
2022-02-15 14:14:11 +0800

    fix ut

--- planner/core/prepare_test.go
+++ planner/core/prepare_test.go
@@ -1619,10 +1619,11 @@ func (s *testPlanSerialSuite) TestParamMarker4FastPlan(c *C) {
 
 	tk.MustQuery(`execute stmt using @a4`).Check(testkit.Rows("1"))
 	tk.MustQuery(`execute stmt using @a4`).Check(testkit.Rows("1"))
-	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
+	// Because of the restrictions in function refineArgs.
+	// If we use string <cmp> int, the result plan can not be cached.
+	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("0"))
 	tk.MustQuery(`execute stmt using @a1`).Check(testkit.Rows())
-	// Use the cached PointGet plan
-	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
+	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("0"))
 
 }
 
