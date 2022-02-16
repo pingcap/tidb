174fa77225467c1ac7260a79ebdc61ab9a3cf9b2
Reminiscent <y870414423@gmail.com>
2022-02-16 15:45:12 +0800

    revert the change

--- planner/core/prepare_test.go
+++ planner/core/prepare_test.go
@@ -1942,11 +1942,8 @@ func (s *testPrepareSerialSuite) TestIssue28246(c *C) {
 	tk.MustQuery("execute stmt using @a").Check(testkit.Rows("<nil>"))
 	tk.MustQuery("execute stmt using @b").Check(testkit.Rows("9223372036854775807"))
 	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
-
-	tk.MustExec(`prepare stmt from 'select/*+ hash_agg() */ count(distinct col1) from PK_TCOLLATION10197 where col1 > ?;';`)
-	tk.MustQuery("execute stmt using @b").Check(testkit.Rows("0"))
-
-	tk.MustQuery("select/*+ hash_agg() */ count(distinct col1) from PK_TCOLLATION10197 where col1 > '龺';").Check(testkit.Rows("0"))
+	tk.MustQuery("execute stmt using @a").Check(testkit.Rows("<nil>"))
+	tk.MustQuery("select @@last_plan_from_cache").Check(testkit.Rows("1"))
 }
 
 func (s *testPlanSerialSuite) TestPartitionTable(c *C) {
