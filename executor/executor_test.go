d0daf4d2ff9efbb86917b96cfb76feceb79eb470
yisaer <disxiaofei@163.com>
2022-02-22 11:42:53 +0800

    fix conflict
    Signed-off-by: yisaer <disxiaofei@163.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -19,18 +19,20 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/pingcap/tidb/util/testleak"
 	"math"
 	"math/rand"
 	"net"
 	"os"
 	"path/filepath"
+	"reflect"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
-
 	"github.com/golang/protobuf/proto"
 	. "github.com/pingcap/check"
 	"github.com/pingcap/errors"
@@ -9384,213 +9386,6 @@ func (s *testSerialSuite) TestIssue28650(c *C) {
 		}()
 	}
 }
-<<<<<<< HEAD
-=======
-
-func (s *testSerialSuite) TestIssue30289(c *C) {
-	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec("use test")
-	fpName := "github.com/pingcap/tidb/executor/issue30289"
-	c.Assert(failpoint.Enable(fpName, `return(true)`), IsNil)
-	defer func() {
-		c.Assert(failpoint.Disable(fpName), IsNil)
-	}()
-	tk.MustExec("drop table if exists t")
-	tk.MustExec("create table t(a int)")
-	err := tk.QueryToErr("select /*+ hash_join(t1) */ * from t t1 join t t2 on t1.a=t2.a")
-	c.Assert(err.Error(), Matches, "issue30289 build return error")
-}
-
-func (s *testSerialSuite) TestIssue29498(c *C) {
-	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec("use test")
-	tk.MustExec("DROP TABLE IF EXISTS t1;")
-	tk.MustExec("CREATE TABLE t1 (t3 TIME(3), d DATE, t TIME);")
-	tk.MustExec("INSERT INTO t1 VALUES ('00:00:00.567', '2002-01-01', '00:00:02');")
-
-	res := tk.MustQuery("SELECT CONCAT(IFNULL(t3, d)) AS col1 FROM t1;")
-	row := res.Rows()[0][0].(string)
-	c.Assert(len(row), Equals, mysql.MaxDatetimeWidthNoFsp+3+1)
-	c.Assert(row[len(row)-12:], Equals, "00:00:00.567")
-
-	res = tk.MustQuery("SELECT IFNULL(t3, d) AS col1 FROM t1;")
-	row = res.Rows()[0][0].(string)
-	c.Assert(len(row), Equals, mysql.MaxDatetimeWidthNoFsp+3+1)
-	c.Assert(row[len(row)-12:], Equals, "00:00:00.567")
-
-	res = tk.MustQuery("SELECT CONCAT(IFNULL(t, d)) AS col1 FROM t1;")
-	row = res.Rows()[0][0].(string)
-	c.Assert(len(row), Equals, mysql.MaxDatetimeWidthNoFsp)
-	c.Assert(row[len(row)-8:], Equals, "00:00:02")
-
-	res = tk.MustQuery("SELECT IFNULL(t, d) AS col1 FROM t1;")
-	row = res.Rows()[0][0].(string)
-	c.Assert(len(row), Equals, mysql.MaxDatetimeWidthNoFsp)
-	c.Assert(row[len(row)-8:], Equals, "00:00:02")
-
-	res = tk.MustQuery("SELECT CONCAT(xx) FROM (SELECT t3 AS xx FROM t1 UNION SELECT d FROM t1) x ORDER BY -xx LIMIT 1;")
-	row = res.Rows()[0][0].(string)
-	c.Assert(len(row), Equals, mysql.MaxDatetimeWidthNoFsp+3+1)
-	c.Assert(row[len(row)-12:], Equals, "00:00:00.567")
-
-	res = tk.MustQuery("SELECT CONCAT(CASE WHEN d IS NOT NULL THEN t3 ELSE d END) AS col1 FROM t1;")
-	row = res.Rows()[0][0].(string)
-	c.Assert(len(row), Equals, mysql.MaxDatetimeWidthNoFsp+3+1)
-	c.Assert(row[len(row)-12:], Equals, "00:00:00.567")
-}
-
-// Test invoke Close without invoking Open before for each operators.
-func (s *testSerialSuite) TestUnreasonablyClose(c *C) {
-	defer testleak.AfterTest(c)()
-
-	is := infoschema.MockInfoSchema([]*model.TableInfo{plannercore.MockSignedTable(), plannercore.MockUnsignedTable()})
-	se, err := session.CreateSession4Test(s.store)
-	c.Assert(err, IsNil)
-	_, err = se.Execute(context.Background(), "use test")
-	c.Assert(err, IsNil)
-	// To enable the shuffleExec operator.
-	_, err = se.Execute(context.Background(), "set @@tidb_merge_join_concurrency=4")
-	c.Assert(err, IsNil)
-
-	var opsNeedsCovered = []plannercore.PhysicalPlan{
-		&plannercore.PhysicalHashJoin{},
-		&plannercore.PhysicalMergeJoin{},
-		&plannercore.PhysicalIndexJoin{},
-		&plannercore.PhysicalIndexHashJoin{},
-		&plannercore.PhysicalTableReader{},
-		&plannercore.PhysicalIndexReader{},
-		&plannercore.PhysicalIndexLookUpReader{},
-		&plannercore.PhysicalIndexMergeReader{},
-		&plannercore.PhysicalApply{},
-		&plannercore.PhysicalHashAgg{},
-		&plannercore.PhysicalStreamAgg{},
-		&plannercore.PhysicalLimit{},
-		&plannercore.PhysicalSort{},
-		&plannercore.PhysicalTopN{},
-		&plannercore.PhysicalCTE{},
-		&plannercore.PhysicalCTETable{},
-		&plannercore.PhysicalMaxOneRow{},
-		&plannercore.PhysicalProjection{},
-		&plannercore.PhysicalSelection{},
-		&plannercore.PhysicalTableDual{},
-		&plannercore.PhysicalWindow{},
-		&plannercore.PhysicalShuffle{},
-		&plannercore.PhysicalUnionAll{},
-	}
-	executorBuilder := executor.NewMockExecutorBuilderForTest(se, is, nil, math.MaxUint64, false, "global")
-
-	var opsNeedsCoveredMask uint64 = 1<<len(opsNeedsCovered) - 1
-	opsAlreadyCoveredMask := uint64(0)
-	for i, tc := range []string{
-		"select /*+ hash_join(t1)*/ * from t t1 join t t2 on t1.a = t2.a",
-		"select /*+ merge_join(t1)*/ * from t t1 join t t2 on t1.f = t2.f",
-		"select t.f from t use index(f)",
-		"select /*+ inl_join(t1) */ * from t t1 join t t2 on t1.f=t2.f",
-		"select /*+ inl_hash_join(t1) */ * from t t1 join t t2 on t1.f=t2.f",
-		"SELECT count(1) FROM (SELECT (SELECT min(a) FROM t as t2 WHERE t2.a > t1.a) AS a from t as t1) t",
-		"select /*+ hash_agg() */ count(f) from t group by a",
-		"select /*+ stream_agg() */ count(f) from t group by a",
-		"select * from t order by a, f",
-		"select * from t order by a, f limit 1",
-		"select * from t limit 1",
-		"select (select t1.a from t t1 where t1.a > t2.a) as a from t t2;",
-		"select a + 1 from t",
-		"select count(*) a from t having a > 1",
-		"select * from t where a = 1.1",
-		"with recursive cte1(c1) as (select 1 union select c1 + 1 from cte1 limit 5 offset 0) select * from cte1",
-		"select /*+use_index_merge(t, c_d_e, f)*/ * from t where c < 1 or f > 2",
-		"select sum(f) over (partition by f) from t",
-		"select /*+ merge_join(t1)*/ * from t t1 join t t2 on t1.d = t2.d",
-		"select a from t union all select a from t",
-	} {
-		comment := Commentf("case:%v sql:%s", i, tc)
-		c.Assert(err, IsNil, comment)
-		stmt, err := s.ParseOneStmt(tc, "", "")
-		c.Assert(err, IsNil, comment)
-
-		err = se.NewTxn(context.Background())
-		c.Assert(err, IsNil, comment)
-		p, _, err := planner.Optimize(context.TODO(), se, stmt, is)
-		c.Assert(err, IsNil, comment)
-		// This for loop level traverses the plan tree to get which operators are covered.
-		for child := []plannercore.PhysicalPlan{p.(plannercore.PhysicalPlan)}; len(child) != 0; {
-			newChild := make([]plannercore.PhysicalPlan, 0, len(child))
-			for _, ch := range child {
-				found := false
-				for k, t := range opsNeedsCovered {
-					if reflect.TypeOf(t) == reflect.TypeOf(ch) {
-						opsAlreadyCoveredMask |= 1 << k
-						found = true
-						break
-					}
-				}
-				c.Assert(found, IsTrue, Commentf("case: %v sql: %s operator %v is not registered in opsNeedsCoveredMask", i, tc, reflect.TypeOf(ch)))
-				switch x := ch.(type) {
-				case *plannercore.PhysicalCTE:
-					newChild = append(newChild, x.RecurPlan)
-					newChild = append(newChild, x.SeedPlan)
-					continue
-				case *plannercore.PhysicalShuffle:
-					newChild = append(newChild, x.DataSources...)
-					newChild = append(newChild, x.Tails...)
-					continue
-				}
-				newChild = append(newChild, ch.Children()...)
-			}
-			child = newChild
-		}
-
-		e := executorBuilder.Build(p)
-
-		func() {
-			defer func() {
-				r := recover()
-				buf := make([]byte, 4096)
-				stackSize := runtime.Stack(buf, false)
-				buf = buf[:stackSize]
-				c.Assert(r, IsNil, Commentf("case: %v\n sql: %s\n error stack: %v", i, tc, string(buf)))
-			}()
-			c.Assert(e.Close(), IsNil, comment)
-		}()
-	}
-	// The following code is used to make sure all the operators registered
-	// in opsNeedsCoveredMask are covered.
-	commentBuf := strings.Builder{}
-	if opsAlreadyCoveredMask != opsNeedsCoveredMask {
-		for i := range opsNeedsCovered {
-			if opsAlreadyCoveredMask&(1<<i) != 1<<i {
-				commentBuf.WriteString(fmt.Sprintf(" %v", reflect.TypeOf(opsNeedsCovered[i])))
-			}
-		}
-	}
-	c.Assert(opsAlreadyCoveredMask, Equals, opsNeedsCoveredMask, Commentf("these operators are not covered %s", commentBuf.String()))
-}
-
-func (s *testSerialSuite) TestIssue30971(c *C) {
-	tk := testkit.NewTestKit(c, s.store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t1, t2")
-	tk.MustExec("create table t1 (id int);")
-	tk.MustExec("create table t2 (id int, c int);")
-
-	testCases := []struct {
-		sql    string
-		fields int
-	}{
-		// Fix a bug that the column length field returned to client is incorrect using MySQL prepare protocol.
-		{"select * from t1 union select 1 from t1", 1},
-		{"select c from t2 union select * from t1", 1},
-		{"select * from t1", 1},
-		{"select * from t2 where c in (select * from t1)", 2},
-		{"insert into t1 values (?)", 0},
-		{"update t1 set id = ?", 0},
-	}
-	for _, test := range testCases {
-		_, _, fields, err := tk.Se.PrepareStmt(test.sql)
-		c.Assert(err, IsNil)
-		c.Assert(fields, HasLen, test.fields)
-	}
-}
 
 // Details at https://github.com/pingcap/tidb/issues/31038
 func (s *testSerialSuite) TestFix31038(c *C) {
@@ -9606,4 +9401,3 @@ func (s *testSerialSuite) TestFix31038(c *C) {
 	tk.MustQuery("select * from t123;")
 	failpoint.Disable("github.com/pingcap/tidb/store/copr/disable-collect-execution")
 }
->>>>>>> 2bbeebd0d... store: forbid collecting info if enable-collect-execution-info disabled (#31282)
