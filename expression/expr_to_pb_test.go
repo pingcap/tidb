91aa3a6c7f261049d651f0e915a640a1a418cccf
yibin <huyibin@pingcap.com>
2022-02-24 13:53:43 +0800

    Add push-down unit test for dayname/monthname

--- expression/expr_to_pb_test.go
+++ expression/expr_to_pb_test.go
@@ -1015,6 +1015,16 @@ func TestExprPushDownToFlash(t *testing.T) {
 	require.NoError(t, err)
 	exprs = append(exprs, function)
 
+	// DayName: supported
+	function, err = NewFunction(mock.NewContext(), ast.DayName, types.NewFieldType(mysql.TypeString), datetimeColumn)
+	require.NoError(t, err)
+	exprs = append(exprs, function)
+
+	// MonthName: supported
+	function, err = NewFunction(mock.NewContext(), ast.MonthName, types.NewFieldType(mysql.TypeString), datetimeColumn)
+	require.NoError(t, err)
+	exprs = append(exprs, function)
+
 	pushed, remained = PushDownExprs(sc, exprs, client, kv.TiFlash)
 	require.Len(t, pushed, len(exprs))
 	require.Len(t, remained, 0)
