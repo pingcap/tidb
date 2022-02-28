d7babe54935808b7e7b7f48824a2cfd3caaef6c8
tison <wander4096@gmail.com>
2022-02-28 17:04:59 +0800

    trigger optimizer init
    Signed-off-by: tison <wander4096@gmail.com>

--- util/keydecoder/keydecoder_test.go
+++ util/keydecoder/keydecoder_test.go
@@ -12,20 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package keydecoder_test
+package keydecoder
 
 import (
 	"testing"
 
 	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/parser/model"
+	_ "github.com/pingcap/tidb/planner/core"
 	"github.com/pingcap/tidb/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/table"
 	"github.com/pingcap/tidb/table/tables"
 	"github.com/pingcap/tidb/testkit/testutil"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/codec"
-	. "github.com/pingcap/tidb/util/keydecoder"
 	"github.com/stretchr/testify/assert"
 )
 
