bc9b9ed75467adc267dc192c94e9d570f86c43e9
D3Hunter <jujj603@gmail.com>
2022-03-03 19:01:46 +0800

    fix it

--- br/tests/lightning_checkonly/run.sh
+++ br/tests/lightning_checkonly/run.sh
@@ -24,43 +24,43 @@ run_lightning -d "tests/$TEST_NAME/data_sql" --check-only > $out_file_name 2>&1 
 check_contains "flag needs an argument: -check-only"
 run_lightning -d "tests/$TEST_NAME/data_sql" --check-only xxx > $out_file_name 2>&1 || true
 check_contains "invalid check-only config"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=2,1000 > $out_file_name 2>&1 || true
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 2,1000 > $out_file_name 2>&1 || true
 check_contains "rate should be in range (0, 1]"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=-1,1000 > $out_file_name 2>&1 || true
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only -1,1000 > $out_file_name 2>&1 || true
 check_contains "rate should be in range (0, 1]"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=,1000 > $out_file_name 2>&1 || true
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only ,1000 > $out_file_name 2>&1 || true
 check_contains "invalid check-only config"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1,-1000 > $out_file_name 2>&1 || true
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1,-1000 > $out_file_name 2>&1 || true
 check_contains "rows should be positive or -1"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1, > $out_file_name 2>&1 || true
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1, > $out_file_name 2>&1 || true
 check_contains "invalid check-only config"
 
-echo ">>> check-only=normal"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only normal > $out_file_name
+echo ">>> check-only=default"
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only default > $out_file_name
 check_contains "tidb lightning exit successfully"
 run_sql "$check_db_sql"
 check_contains "s: 0"
 
-echo ">>> check-only=normal & check-requirements=true"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-requirements=true --check-only normal > $out_file_name
+echo ">>> check-only=default & check-requirements=true"
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-requirements=true --check-only default > $out_file_name
 check_contains "tidb lightning exit successfully"
 run_sql "$check_db_sql"
 check_contains "s: 0"
 
 echo ">>> check-only sample for sql files"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1,1 > $out_file_name
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1,1 > $out_file_name
 check_contains "All checks have been passed"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1,2 > $out_file_name
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1,2 > $out_file_name
 check_contains "Total sample of 2 rows of data checked, 1 errors found."
 check_contains "Some checks failed, please check the log for more information."
 check_contains "Log file location: /tmp/backup_restore_test/lightning.log"
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1,3 > $out_file_name
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1,3 > $out_file_name
 check_contains "Total sample of 3 rows of data checked, 2 errors found."
 check_contains "Some checks failed, please check the log for more information."
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1,-1 > $out_file_name
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1,-1 > $out_file_name
 check_contains "Total sample of 3 rows of data checked, 2 errors found."
 check_contains "Some checks failed, please check the log for more information."
-run_lightning -d "tests/$TEST_NAME/data_sql" --check-only sample=1,100 > $out_file_name
+run_lightning -d "tests/$TEST_NAME/data_sql" --check-only 1,100 > $out_file_name
 check_contains "Total sample of 3 rows of data checked, 2 errors found."
 check_contains "Some checks failed, please check the log for more information."
 run_sql "$check_db_sql"
@@ -68,16 +68,16 @@ check_contains "s: 0"
 
 echo ">>> check-only sample for csv files"
 # check using binary charset
-run_lightning -d "tests/$TEST_NAME/data_csv" --check-only sample=1,2 > $out_file_name
+run_lightning -d "tests/$TEST_NAME/data_csv" --check-only 1,2 > $out_file_name
 check_contains "All checks have been passed"
 # check using gbk charset
-run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only sample=1,1 > $out_file_name
+run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only 1,1 > $out_file_name
 check_contains "All checks have been passed"
-run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only sample=1,2 > $out_file_name
+run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only 1,2 > $out_file_name
 check_contains "Total sample of 2 rows of data checked, 1 errors found."
 check_contains "Some checks failed, please check the log for more information."
 check_contains "Log file location: /tmp/backup_restore_test/lightning.log"
-run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only sample=1,3 > $out_file_name
+run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only 1,3 > $out_file_name
 check_contains "Total sample of 3 rows of data checked, 2 errors found."
 check_contains "Some checks failed, please check the log for more information."
 run_sql "$check_db_sql"
@@ -86,7 +86,7 @@ check_contains "s: 0"
 echo ">>> check-only sample for csv files and table schema already exists in tidb"
 run_sql "create database test_check;"
 run_sql "create table test_check.tbl1(id varchar(100));"
-run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only sample=1,3 > $out_file_name
+run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_csv" --check-only 1,3 > $out_file_name
 check_contains "Total sample of 3 rows of data checked, 2 errors found."
 check_contains "Some checks failed, please check the log for more information."
 run_sql "select count(1) s from test_check.tbl1"
@@ -95,9 +95,9 @@ run_sql "drop database test_check"
 
 echo ">>> check-only sample for multiple csv files"
 # default sample rate=0.01, so at most 1 file will be checked
-run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_multiple_csv" --check-only sample > $out_file_name
+run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_multiple_csv" --check-only default > $out_file_name
 check_contains "Total sample of 3 rows of data checked"
-run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_multiple_csv" --check-only sample=1,1000 > $out_file_name
+run_lightning --config "tests/$TEST_NAME/config_gbk.toml" -d "tests/$TEST_NAME/data_multiple_csv" --check-only 1,1000 > $out_file_name
 check_contains "Total sample of 6 rows of data checked"
 run_sql "$check_db_sql"
 check_contains "s: 0"
