14a361ac31cbd3579d791e04cbb047d2dc029584
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-05 23:29:40 +0800

    fix lint
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- ddl/job_table.go
+++ ddl/job_table.go
@@ -350,7 +350,7 @@ func (d *ddl) doGeneralDDLJobWorker(job *model.Job) {
 			}
 			d.generalDDLWorkerPool.put(wk)
 		}()
-		//wk.handleDDLJobWaitSchemaSynced(d.ddlCtx, job)
+		wk.handleDDLJobWaitSchemaSynced(d.ddlCtx, job)
 		if err := wk.HandleDDLJob(d.ddlCtx, job, d.ddlJobCh); err != nil {
 			log.Error("[ddl] handle General DDL job failed", zap.Error(err))
 		}
@@ -387,7 +387,7 @@ func (d *ddl) doReorgDDLJobWorker(job *model.Job) {
 			}
 			d.reorgWorkerPool.put(wk)
 		}()
-		//wk.handleDDLJobWaitSchemaSynced(d.ddlCtx, job)
+		wk.handleDDLJobWaitSchemaSynced(d.ddlCtx, job)
 		if err := wk.HandleDDLJob(d.ddlCtx, job, d.ddlJobCh); err != nil {
 			log.Error("[ddl] handle Reorg DDL job failed", zap.Error(err))
 		}
