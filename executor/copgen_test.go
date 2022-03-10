dc14c9bf9f9e753e8fe162eab95f2fd76e850daf
ichn-hu <zfhu16@gmail.com>
2022-03-10 18:16:48 +0800

    add copr-test

--- executor/copgen_test.go
+++ executor/copgen_test.go
@@ -1,6 +1,15 @@
 package executor_test
 
 import (
+	"github.com/pingcap/tidb/parser"
+	"github.com/pingcap/tidb/parser/ast"
+	"github.com/pingcap/tidb/parser/model"
+	"github.com/pingcap/tidb/parser/mysql"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/pingcap/tidb/domain"
@@ -53,4 +62,65 @@ func TestProjectionPushdown(t *testing.T) {
 	tk.MustExec("set @@tidb_opt_projection_push_down=1")
 	tk.MustQuery("select DATEDIFF(d, '2020-01-01') from t")
 	require.NoError(t, testGen.Dump("/tmp/datediff_data.json"))
+}
+
+func parseSQLText(data string) (res []ast.StmtNode, warns []error, err error) {
+	p := parser.New()
+	p.EnableWindowFunc(true)
+	// TODO, is there any problem in sqlMode?
+	p.SetSQLMode(mysql.ModeNone)
+	// FIXME, should change the collation and charset according to user's sql
+	statements, warns, err := p.Parse(data, "utf8mb4", "utf8mb4_bin")
+	return statements, warns, err
+}
+
+func TestCoprTest(t *testing.T) {
+	store, dom, clean, testGen := testkit.CreateMockStoreAndDomainWithTestGen(t)
+	defer clean()
+
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	// read file as data
+	data, err := ioutil.ReadFile("../../copr-test/push-down-test/prepare/0_data.sql")
+	require.NoError(t, err)
+	tk.MustExec(string(data))
+	//tk.MustQuery("show tables").Check(testkit.Rows(""))
+	//tk.MustQuery("desc table1000_int_autoinc").Check(testkit.Rows(""))
+	// add all tables to test
+	db, ok := dom.InfoSchema().SchemaByName(model.NewCIStr("test"))
+	require.True(t, ok)
+	for _, tblInfo := range db.Tables {
+		err = testGen.AddTable("test", tblInfo.Name.L)
+		require.NoError(t, err)
+	}
+
+	require.NoError(t, testGen.Prepare())
+	tk.MustExec("set @@sql_mode=''")
+	tk.MustExec("set @@tidb_opt_projection_push_down=1")
+	dir := "../../copr-test/push-down-test/sql"
+	var files []string
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(info.Name(), ".sql") || strings.HasSuffix(info.Name(), "10_agg.sql") {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+	require.NoError(t, err)
+	for _, file := range files {
+		data, err := ioutil.ReadFile(file)
+		require.NoError(t, err)
+		statements, warns, err := parseSQLText(string(data))
+		if warns != nil {
+			log.Printf("Parse warning: %v\n", warns)
+		}
+		require.NoError(t, err)
+		for _, stmt := range statements {
+			tk.MustExec(stmt.Text())
+		}
+	}
+	require.NoError(t, testGen.Dump("/tmp/copr-test.json"))
 }
