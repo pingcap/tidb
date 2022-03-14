ccbbe9a7e6ce0906853eb0d6fea2fc335051f17e
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-15 00:02:19 +0800

    fix
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- domain/domain.go
+++ domain/domain.go
@@ -789,7 +789,7 @@ func (do *Domain) Init(ddlLease time.Duration, sysExecutorFactory func(*Domain) 
 	var capacity int
 	if variable.AllowConcurrencyDDL.Load() {
 		// reorgWorker + generalWorker + sessForAddDDL + PollTiFlashRoutine + limitDDLJobs +
-		capacity = (10+1)*4 + 1 + 1 + 1
+		capacity = (10+1)*5 + 1 + 1 + 1
 	} else {
 		capacity = 2
 	}
