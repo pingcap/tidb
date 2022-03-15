ee0a038edee83f8da736c7e659ff069a099d4d76
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-16 00:56:30 +0800

    fix TestTableStorageStats
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- executor/infoschema_cluster_table_test.go
+++ executor/infoschema_cluster_table_test.go
@@ -33,6 +33,7 @@ import (
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/server"
 	"github.com/pingcap/tidb/session/txninfo"
+	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/store/helper"
 	"github.com/pingcap/tidb/testkit"
 	"github.com/pingcap/tidb/util"
@@ -319,7 +320,11 @@ func (s *infosSchemaClusterTableSuite) TestTableStorageStats() {
 		"test 2",
 	))
 	rows := tk.MustQuery("select TABLE_NAME from information_schema.TABLE_STORAGE_STATS where TABLE_SCHEMA = 'mysql';").Rows()
-	s.Require().Len(rows, 30)
+	result := 30
+	if variable.AllowConcurrencyDDL.Load() {
+		result = result + 2 // tidb_ddl_job and tidb_ddl_reorg
+	}
+	s.Require().Len(rows, result)
 
 	// More tests about the privileges.
 	tk.MustExec("create user 'testuser'@'localhost'")
