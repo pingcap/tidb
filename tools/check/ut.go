89f740da8d28bfec9498737c514e66cef38b2413
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-15 09:29:28 +0800

    try
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- tools/check/ut.go
+++ tools/check/ut.go
@@ -733,7 +733,7 @@ func (n *numa) testCommand(pkg string, fn string, old bool) *exec.Cmd {
 		args = append(args, "-test.coverprofile", tmpFile)
 	}
 	args = append(args, "-test.cpu", "1")
-	args = append(args, []string{"-test.timeout", "3m"}...)
+	args = append(args, []string{"-test.timeout", "4m"}...)
 	if old {
 		// session.test -test.run '^TestT$' -check.f testTxnStateSerialSuite.TestTxnInfoWithPSProtoco
 		args = append(args, "-test.run", "^TestT$", "-check.f", fn)
