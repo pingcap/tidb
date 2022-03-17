6987b40d693e748aed77c2e03d2dd2922bb59e97
docsir <zhanghaodong@pingcap.com>
2022-03-17 15:55:52 +0800

    add IT for no-schemas and no-sequences

--- dumpling/tests/sequences/run.sh
+++ dumpling/tests/sequences/run.sh
@@ -13,6 +13,18 @@ export DUMPLING_TEST_DATABASE="sequences"
 run_sql "create sequence s start with 1 minvalue 1 maxvalue 9223372036854775806 increment by 1 cache 1000 nocycle ENGINE=InnoDB;"
 run_sql "select nextval(s);"
 
+# test --no-sequences
+run_dumpling --no-sequences
+file_not_exist "$DUMPLING_OUTPUT_DIR/sequences.s-schema-sequence.sql"
+
+rm -rf $DUMPLING_OUTPUT_DIR
 run_dumpling --no-sequences=false
 diff "$DUMPLING_BASE_NAME/data/sequences.s-schema-sequence-expect.sql" "$DUMPLING_OUTPUT_DIR/sequences.s-schema-sequence.sql"
+file_not_exist "$DUMPLING_OUTPUT_DIR/sequences.s.000000000.sql"
+
+# test --no-schemas
+rm -rf $DUMPLING_OUTPUT_DIR
+run_dumpling --no-schemas
+file_not_exist "$DUMPLING_OUTPUT_DIR/sequences-schema-create.sql"
+file_not_exist "$DUMPLING_OUTPUT_DIR/sequences.s-schema-sequence.sql"
 file_not_exist "$DUMPLING_OUTPUT_DIR/sequences.s.000000000.sql"
