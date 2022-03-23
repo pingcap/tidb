74ca71c1d5b329b0b7dca6e9b435c565a9553e4e
yisaer <disxiaofei@163.com>
2022-03-23 18:16:01 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- executor/stale_txn_test.go
+++ executor/stale_txn_test.go
@@ -1302,12 +1302,12 @@ func TestPlanCacheWithStaleReadByBinaryProto(t *testing.T) {
 	tk.MustExec("use test")
 	tk.MustExec("create table t1 (id int primary key, v int)")
 	tk.MustExec("insert into t1 values(1, 10)")
-	time.Sleep(time.Second)
 	se := tk.Session()
-	now := time.Now()
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Millisecond * 100)
+	tk.MustExec("set @a=now(6)")
+	time.Sleep(time.Second)
 	tk.MustExec("update t1 set v=100 where id=1")
-	stmtID1, _, _, err := se.PrepareStmt(fmt.Sprintf("select * from t1 as of timestamp '%v' where id=1", now.Format("2006-1-2 15:04:05")))
+	stmtID1, _, _, err := se.PrepareStmt("select * from t1 as of timestamp @a where id=1")
 	require.NoError(t, err)
 
 	for i := 0; i < 3; i++ {
