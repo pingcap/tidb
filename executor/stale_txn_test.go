ae5669ed0f1f2958b12c8b929f59421d81ea7c5f
yisaer <disxiaofei@163.com>
2022-03-23 17:22:43 +0800

    Revert "fix unstable test"
    This reverts commit b809c3c978d716a5588e07c22219794d048f5191.

--- executor/stale_txn_test.go
+++ executor/stale_txn_test.go
@@ -1303,15 +1303,22 @@ func TestPlanCacheWithStaleReadByBinaryProto(t *testing.T) {
 	tk.MustExec("create table t1 (id int primary key, v int)")
 	tk.MustExec("insert into t1 values(1, 10)")
 	se := tk.Session()
-	time.Sleep(time.Second * 3)
+	tk.MustExec("set @a=now(6)")
+	time.Sleep(time.Millisecond * 5)
 	tk.MustExec("update t1 set v=100 where id=1")
 
-	stmtID1, _, _, err := se.PrepareStmt("select * from t1 as of timestamp NOW() - INTERVAL 2 SECOND where id=1")
+	stmtID1, _, _, err := se.PrepareStmt("select * from t1 as of timestamp @a where id=1")
 	require.NoError(t, err)
 
-	for i := 0; i < 3; i++ {
-		rs, err := se.ExecutePreparedStmt(context.TODO(), stmtID1, nil)
-		require.NoError(t, err)
-		tk.ResultSetToResult(rs, fmt.Sprintf("%v", rs)).Check(testkit.Rows("1 10"))
-	}
+	rs, err := se.ExecutePreparedStmt(context.TODO(), stmtID1, nil)
+	require.NoError(t, err)
+	tk.ResultSetToResult(rs, fmt.Sprintf("%v", rs)).Check(testkit.Rows("1 10"))
+
+	rs, err = se.ExecutePreparedStmt(context.TODO(), stmtID1, nil)
+	require.NoError(t, err)
+	tk.ResultSetToResult(rs, fmt.Sprintf("%v", rs)).Check(testkit.Rows("1 10"))
+
+	rs, err = se.ExecutePreparedStmt(context.TODO(), stmtID1, nil)
+	require.NoError(t, err)
+	tk.ResultSetToResult(rs, fmt.Sprintf("%v", rs)).Check(testkit.Rows("1 10"))
 }
