cbb81d4f15aafbe896ccfd686b74a4042063cbc9
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-23 19:24:20 +0800

    fix test build
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- ddl/ddl_test.go
+++ ddl/ddl_test.go
@@ -453,7 +453,7 @@ func TestNotifyDDLJob(t *testing.T) {
 	// Ensure that the notification is not handled in workers `start` function.
 	d.cancel()
 	for _, worker := range d.workers {
-		worker.close()
+		worker.Close()
 	}
 
 	job := &model.Job{
@@ -496,7 +496,7 @@ func TestNotifyDDLJob(t *testing.T) {
 	// Ensure that the notification is not handled by worker's "start".
 	d1.cancel()
 	for _, worker := range d1.workers {
-		worker.close()
+		worker.Close()
 	}
 	d1.ownerManager.RetireOwner()
 	d1.asyncNotifyWorker(job)
