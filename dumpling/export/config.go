5a1760475759c0598873ac8e03127eb6b75f570f
docsir <73268456+docsir@users.noreply.github.com>
2022-03-24 12:34:33 +0800

    dumpling: support dumping sequences (#32570)
    close pingcap/tidb#32538

--- dumpling/export/config.go
+++ dumpling/export/config.go
@@ -45,6 +45,7 @@ const (
 	flagConsistency              = "consistency"
 	flagSnapshot                 = "snapshot"
 	flagNoViews                  = "no-views"
+	flagNoSequences              = "no-sequences"
 	flagSortByPk                 = "order-by-primary-key"
 	flagStatusAddr               = "status-addr"
 	flagRows                     = "rows"
@@ -84,6 +85,7 @@ type Config struct {
 	AllowCleartextPasswords  bool
 	SortByPk                 bool
 	NoViews                  bool
+	NoSequences              bool
 	NoHeader                 bool
 	NoSchemas                bool
 	NoData                   bool
@@ -166,6 +168,7 @@ func DefaultConfig() *Config {
 		Snapshot:            "",
 		Consistency:         consistencyTypeAuto,
 		NoViews:             true,
+		NoSequences:         true,
 		Rows:                UnspecifiedSize,
 		Where:               "",
 		FileType:            "",
@@ -231,6 +234,7 @@ func (conf *Config) DefineFlags(flags *pflag.FlagSet) {
 	flags.String(flagConsistency, consistencyTypeAuto, "Consistency level during dumping: {auto|none|flush|lock|snapshot}")
 	flags.String(flagSnapshot, "", "Snapshot position (uint64 or MySQL style string timestamp). Valid only when consistency=snapshot")
 	flags.BoolP(flagNoViews, "W", true, "Do not dump views")
+	flags.Bool(flagNoSequences, true, "Do not dump sequences")
 	flags.Bool(flagSortByPk, true, "Sort dump results by primary key through order by sql")
 	flags.String(flagStatusAddr, ":8281", "dumpling API server and pprof addr")
 	flags.Uint64P(flagRows, "r", UnspecifiedSize, "If specified, dumpling will split table into chunks and concurrently dump them to different files to improve efficiency. For TiDB v3.0+, specify this will make dumpling split table with each file one TiDB region(no matter how many rows is).\n"+
@@ -325,6 +329,10 @@ func (conf *Config) ParseFromFlags(flags *pflag.FlagSet) error {
 		return errors.Trace(err)
 	}
 	conf.NoViews, err = flags.GetBool(flagNoViews)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	conf.NoSequences, err = flags.GetBool(flagNoSequences)
 	if err != nil {
 		return errors.Trace(err)
 	}
