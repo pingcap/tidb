9e950cea258aa1475012a12d10588a13fc30dc0e
WangLe1321 <wangle1321@163.com>
2022-03-25 11:56:26 +0800

    chore: wrap blacklist config code around

--- br/pkg/restore/config.go
+++ br/pkg/restore/config.go
@@ -2,4 +2,9 @@ package restore
 
 import "github.com/pingcap/tidb/parser/model"
 
-var incrementalRestoreActionBlacklist = []model.ActionType{model.ActionSetTiFlashReplica, model.ActionUpdateTiFlashReplicaStatus, model.ActionLockTable, model.ActionUnlockTable}
+var incrementalRestoreActionBlacklist = []model.ActionType{
+	model.ActionSetTiFlashReplica,
+	model.ActionUpdateTiFlashReplicaStatus,
+	model.ActionLockTable,
+	model.ActionUnlockTable,
+}
