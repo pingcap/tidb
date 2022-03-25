8f11f0f3fc4d649fa7f2aadfd5bde7afdf5cae39
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-03-25 18:28:34 +0800

    cherry pick #33449 to release-6.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- config/config.go
+++ config/config.go
@@ -782,6 +782,7 @@ func StoreGlobalConfig(config *Config) {
 }
 
 var deprecatedConfig = map[string]struct{}{
+<<<<<<< HEAD
 	"pessimistic-txn.ttl":              {},
 	"pessimistic-txn.enable":           {},
 	"log.file.log-rotate":              {},
@@ -797,6 +798,31 @@ var deprecatedConfig = map[string]struct{}{
 	"alter-primary-key":                {}, // use NONCLUSTERED keyword instead
 	"enable-streaming":                 {},
 	"performance.mem-profile-interval": {},
+=======
+	"pessimistic-txn.ttl":                {},
+	"pessimistic-txn.enable":             {},
+	"log.file.log-rotate":                {},
+	"log.log-slow-query":                 {},
+	"txn-local-latches":                  {},
+	"txn-local-latches.enabled":          {},
+	"txn-local-latches.capacity":         {},
+	"performance.max-memory":             {},
+	"max-txn-time-use":                   {},
+	"experimental.allow-auto-random":     {},
+	"enable-redact-log":                  {}, // use variable tidb_redact_log instead
+	"tikv-client.copr-cache.enable":      {},
+	"alter-primary-key":                  {}, // use NONCLUSTERED keyword instead
+	"enable-streaming":                   {},
+	"performance.mem-profile-interval":   {},
+	"lower-case-table-names":             {},
+	"stmt-summary":                       {},
+	"stmt-summary.enable":                {},
+	"stmt-summary.enable-internal-query": {},
+	"stmt-summary.max-stmt-count":        {},
+	"stmt-summary.max-sql-length":        {},
+	"stmt-summary.refresh-interval":      {},
+	"stmt-summary.history-size":          {},
+>>>>>>> 17408b432... config: be compatible with deprecated stmt-summary config items (#33449)
 }
 
 func isAllDeprecatedConfigItems(items []string) bool {
