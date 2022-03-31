5610bea53e2ffef50d76c6c2cbde4825f931abd2
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-03-31 16:51:07 +0800

    fixup

--- planner/core/task.go
+++ planner/core/task.go
@@ -980,7 +980,7 @@ func buildIndexLookUpTask(ctx sessionctx.Context, t *copTask) *rootTask {
 	p.PartitionInfo = t.partitionInfo
 	setTableScanToTableRowIDScan(p.tablePlan)
 	p.stats = t.tablePlan.statsInfo()
-	newTask.cst = p.GetCost()
+	newTask.cst += p.GetCost()
 	p.cost = newTask.cst
 
 	// Do not inject the extra Projection even if t.needExtraProj is set, or the schema between the phase-1 agg and
