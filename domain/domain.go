b7d3cd74ee5319bceac0c75e9e1b45d817d80da8
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-31 22:08:45 +0800

    domain: use util.WaitGroupWrapper insteal of sync.WaitGroup
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- domain/domain.go
+++ domain/domain.go
@@ -1322,8 +1322,7 @@ func (do *Domain) StartLoadStatsSubWorkers(ctxList []sessionctx.Context) {
 	statsHandle := do.StatsHandle()
 	for i, ctx := range ctxList {
 		statsHandle.StatsLoad.SubCtxs[i] = ctx
-		do.wg.Add(1)
-		go statsHandle.SubLoadWorker(ctx, do.exit, &do.wg)
+		do.wg.Run(func() { statsHandle.SubLoadWorker(ctx, do.exit) })
 	}
 }
 
