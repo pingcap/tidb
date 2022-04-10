5433a242de279b60272dcd570610a86e581aee09
Weizhen Wang <wangweizhen@pingcap.com>
2022-04-10 23:29:15 +0800

    util: migrate importer and watcher from tidb-tools to tidb
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ util/importer/config.go
@@ -0,0 +1,44 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package importer
+
+import (
+	"fmt"
+
+	"github.com/pingcap/tidb-tools/pkg/dbutil"
+)
+
+// Config is the configuration.
+type Config struct {
+	TableSQL string `toml:"table-sql" json:"table-sql"`
+
+	IndexSQL string `toml:"index-sql" json:"index-sql"`
+
+	LogLevel string `toml:"log-level" json:"log-level"`
+
+	WorkerCount int `toml:"worker-count" json:"worker-count"`
+
+	JobCount int `toml:"job-count" json:"job-count"`
+
+	Batch int `toml:"batch" json:"batch"`
+
+	DBCfg dbutil.DBConfig `toml:"db" json:"db"`
+}
+
+func (c *Config) String() string {
+	if c == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("Config(%+v)", *c)
+}
