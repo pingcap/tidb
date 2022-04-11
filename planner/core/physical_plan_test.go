1d218999a1c44546c18a9550eaf05a99fea77134
Zhou Kunqin <25057648+time-and-fate@users.noreply.github.com>
2022-04-11 15:35:36 +0800

    resolve conflict

--- planner/core/physical_plan_test.go
+++ planner/core/physical_plan_test.go
@@ -1790,67 +1790,6 @@ func (s *testPlanSuite) TestPossibleProperties(c *C) {
 		"1 58.0000",
 	))
 }
-<<<<<<< HEAD
-=======
-
-func (s *testPlanSuite) TestSelectionPartialPushDown(c *C) {
-	var (
-		input  []string
-		output []struct {
-			SQL  string
-			Plan []string
-		}
-	)
-	s.testData.GetTestCases(c, &input, &output)
-	store, dom, err := newStoreWithBootstrap()
-	c.Assert(err, IsNil)
-	defer func() {
-		dom.Close()
-		store.Close()
-	}()
-	tk := testkit.NewTestKit(c, store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t1, t2")
-	tk.MustExec("create table t1(a int, b int as (a+1) virtual)")
-	tk.MustExec("create table t2(a int, b int as (a+1) virtual, c int, key idx_a(a))")
-
-	for i, ts := range input {
-		s.testData.OnRecord(func() {
-			output[i].SQL = ts
-			output[i].Plan = s.testData.ConvertRowsToStrings(tk.MustQuery("explain format='brief'" + ts).Rows())
-		})
-		tk.MustQuery("explain format='brief' " + ts).Check(testkit.Rows(output[i].Plan...))
-	}
-}
-
-func (s *testPlanSuite) TestIssue28316(c *C) {
-	var (
-		input  []string
-		output []struct {
-			SQL  string
-			Plan []string
-		}
-	)
-	s.testData.GetTestCases(c, &input, &output)
-	store, dom, err := newStoreWithBootstrap()
-	c.Assert(err, IsNil)
-	defer func() {
-		dom.Close()
-		store.Close()
-	}()
-	tk := testkit.NewTestKit(c, store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t")
-	tk.MustExec("create table t(a int)")
-
-	for i, ts := range input {
-		s.testData.OnRecord(func() {
-			output[i].SQL = ts
-			output[i].Plan = s.testData.ConvertRowsToStrings(tk.MustQuery("explain format='brief'" + ts).Rows())
-		})
-		tk.MustQuery("explain format='brief' " + ts).Check(testkit.Rows(output[i].Plan...))
-	}
-}
 
 func (s *testPlanSuite) TestIssue30965(c *C) {
 	store, dom, err := newStoreWithBootstrap()
@@ -1874,4 +1813,3 @@ func (s *testPlanSuite) TestIssue30965(c *C) {
 			"    └─Selection(Probe) 0.00 cop[tikv]  eq(test.t30965.a, 3), eq(test.t30965.c, 5)",
 			"      └─TableRowIDScan 10.00 cop[tikv] table:t30965 keep order:false, stats:pseudo"))
 }
->>>>>>> e24493f41... planner: fix `AccessPath.TableFilters` got modified unexpectedly (#30966)
