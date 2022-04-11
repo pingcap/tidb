102c98762d10cf61c510f20a4ab4d6c76fab4544
crazycs520 <crazycs520@gmail.com>
2022-04-11 14:06:52 +0800

    address comment
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- util/topsql/collector/main_test.go
+++ util/topsql/collector/main_test.go
@@ -66,16 +66,23 @@ func TestPProfCPUProfile(t *testing.T) {
 	// Test disable then re-enable.
 	topsqlstate.DisableTopSQL()
 	time.Sleep(interval * 2)
+	dataChLen := len(mc.dataCh)
+	deltaLen := 0
 	topsqlstate.EnableTopSQL()
 	for i := 0; i < 10; i++ {
 		t1 := time.Now()
 		data = <-mc.dataCh
 		require.True(t, time.Since(t1) < interval*4)
 		if len(data) > 0 {
-			break
+			deltaLen++
+			if deltaLen > dataChLen {
+				// Here we can ensure that we receive new data after "re-enable".
+				break
+			}
 		}
 	}
 	require.True(t, len(data) > 0)
+	require.True(t, deltaLen > dataChLen)
 	require.Equal(t, []byte("sql_digest value"), data[0].SQLDigest)
 }
 
