6ce903055c37b66bdde784b9f76e00ca0c638da2
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2022-04-12 22:10:35 +0800

    executor: make sure buildHashTable worker exits before IndexHashJoin finish (#31334) (#31418)
    close pingcap/tidb#31062

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -9010,6 +9010,7 @@ func (s *testSuite) TestCTEWithIndexLookupJoinDeadLock(c *C) {
 }
 
 func (s *testSerialSuite) TestIssue28650(c *C) {
+	defer testleak.AfterTest(c)()
 	tk := testkit.NewTestKit(c, s.store)
 	tk.MustExec("use test")
 	tk.MustExec("drop table if exists t1, t2;")
