2f8fc1e663a04431c84ee1a43d70303e775927e0
crazycs520 <crazycs520@gmail.com>
2022-04-14 01:17:11 +0800

    add memory tracker for slow query
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- executor/slow_query.go
+++ executor/slow_query.go
@@ -66,9 +66,11 @@ type slowQueryRetriever struct {
 	checker               *slowLogChecker
 	columnValueFactoryMap map[string]slowQueryColumnValueFactory
 
-	taskList   chan slowLogTask
-	stats      *slowQueryRuntimeStats
-	memTracker *memory.Tracker
+	taskList      chan slowLogTask
+	stats         *slowQueryRuntimeStats
+	memTracker    *memory.Tracker
+	lastFetchSize int64
+	cancel        context.CancelFunc
 }
 
 func (e *slowQueryRetriever) retrieve(ctx context.Context, sctx sessionctx.Context) ([][]types.Datum, error) {
@@ -77,6 +79,7 @@ func (e *slowQueryRetriever) retrieve(ctx context.Context, sctx sessionctx.Conte
 		if err != nil {
 			return nil, err
 		}
+		ctx, e.cancel = context.WithCancel(ctx)
 		e.initializeAsyncParsing(ctx, sctx)
 	}
 	return e.dataForSlowLog(ctx, sctx)
@@ -142,6 +145,9 @@ func (e *slowQueryRetriever) close() error {
 			logutil.BgLogger().Error("close slow log file failed.", zap.Error(err))
 		}
 	}
+	if e.cancel != nil {
+		e.cancel()
+	}
 	return nil
 }
 
@@ -198,6 +204,8 @@ func (e *slowQueryRetriever) dataForSlowLog(ctx context.Context, sctx sessionctx
 		task slowLogTask
 		ok   bool
 	)
+	e.memConsume(-e.lastFetchSize)
+	e.lastFetchSize = 0
 	for {
 		select {
 		case task, ok = <-e.taskList:
@@ -215,6 +223,7 @@ func (e *slowQueryRetriever) dataForSlowLog(ctx context.Context, sctx sessionctx
 		if len(rows) == 0 {
 			continue
 		}
+		e.lastFetchSize = calculateDatumsSize(rows)
 		return rows, nil
 	}
 }
@@ -439,7 +448,7 @@ func (e *slowQueryRetriever) parseSlowLog(ctx context.Context, sctx sessionctx.C
 				return
 			case e.taskList <- t:
 			}
-			e.sendParsedSlowLogCh(ctx, t, parsedSlowLog{nil, err})
+			e.sendParsedSlowLogCh(t, parsedSlowLog{nil, err})
 		}
 		if len(logs) == 0 || len(logs[0]) == 0 {
 			break
@@ -467,7 +476,7 @@ func (e *slowQueryRetriever) parseSlowLog(ctx context.Context, sctx sessionctx.C
 			}
 			wg.Run(func() {
 				result, err := e.parseLog(ctx, sctx, log, start)
-				e.sendParsedSlowLogCh(ctx, t, parsedSlowLog{result, err})
+				e.sendParsedSlowLogCh(t, parsedSlowLog{result, err})
 				<-ch
 			})
 			offset.offset = e.fileLine
@@ -482,10 +491,10 @@ func (e *slowQueryRetriever) parseSlowLog(ctx context.Context, sctx sessionctx.C
 	wg.Wait()
 }
 
-func (e *slowQueryRetriever) sendParsedSlowLogCh(ctx context.Context, t slowLogTask, re parsedSlowLog) {
+func (e *slowQueryRetriever) sendParsedSlowLogCh(t slowLogTask, re parsedSlowLog) {
 	select {
 	case t.resultCh <- re:
-	case <-ctx.Done():
+	default:
 		return
 	}
 }
@@ -532,6 +541,9 @@ func splitByColon(line string) (fields []string, values []string) {
 
 func (e *slowQueryRetriever) parseLog(ctx context.Context, sctx sessionctx.Context, log []string, offset offset) (data [][]types.Datum, err error) {
 	start := time.Now()
+	logSize := calculateLogSize(log)
+	e.memConsume(logSize)
+	defer e.memConsume(-logSize)
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("%s", r)
@@ -620,6 +632,8 @@ func (e *slowQueryRetriever) parseLog(ctx context.Context, sctx sessionctx.Conte
 				// Get the sql string, and mark the start flag to false.
 				_ = e.setColumnValue(sctx, row, tz, variable.SlowLogQuerySQLStr, string(hack.Slice(line)), e.checker, fileLine)
 				e.setDefaultValue(row)
+				e.memConsume(calculateRowSize(row))
+				//fmt.Printf("slow query mem consume %v------\n", e.memTracker.BytesConsumed())
 				data = append(data, row)
 				startFlag = false
 			} else {
@@ -774,7 +788,9 @@ func parsePlan(planString string) string {
 		return planString
 	}
 	planString = planString[len(variable.SlowLogPlanPrefix) : len(planString)-len(variable.SlowLogPlanSuffix)]
+	//fmt.Printf("start to decode plan, size: %v --\n", len(planString))
 	decodePlanString, err := plancodec.DecodePlan(planString)
+	//fmt.Printf("finish decode plan, size: %v, last size: %v--\n", len(planString), len(decodePlanString))
 	if err == nil {
 		planString = decodePlanString
 	} else {
@@ -1087,6 +1103,31 @@ func readLastLines(ctx context.Context, file *os.File, endCursor int64) ([]strin
 func (e *slowQueryRetriever) initializeAsyncParsing(ctx context.Context, sctx sessionctx.Context) {
 	e.taskList = make(chan slowLogTask, 1)
 	go e.parseDataForSlowLog(ctx, sctx)
+}
+
+func calculateLogSize(log []string) int64 {
+	size := 0
+	for _, line := range log {
+		size += len(line)
+	}
+	return int64(size)
+}
+
+func calculateDatumsSize(rows [][]types.Datum) int64 {
+	size := int64(0)
+	for _, row := range rows {
+		size += calculateRowSize(row)
+	}
+	return size
+}
+
+func calculateRowSize(row []types.Datum) int64 {
+	size := 0
+	for _, col := range row {
+		raw := col.GetRaw()
+		size += len(raw)
+	}
+	return int64(size)
 }
 
 func (e *slowQueryRetriever) memConsume(bytes int64) {
