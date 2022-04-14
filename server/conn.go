77601b209982e5242449fdc760f370e1e2f1a2a0
knull-cn <hu__haifeng@163.com>
2022-04-14 17:51:12 +0800

    fix by review

--- server/conn.go
+++ server/conn.go
@@ -2341,7 +2341,8 @@ func (cc *clientConn) handleChangeUser(ctx context.Context, data []byte) error {
 	if err := cc.ctx.Close(); err != nil {
 		logutil.Logger(ctx).Debug("close old context failed", zap.Error(err))
 	}
-	// new openSession here, openSessionAndDoAuth will not run again because ctx was not nil.
+	// session was closed by `ctx.Close` and should `openSession` explicitly to renew session.
+	// `openSession` won't run again in `openSessionAndDoAuth` because ctx is not nil.
 	err := cc.openSession()
 	if err != nil {
 		return err
