2a5bbf9ddbde1d7e73cd94a3673db67b87cff1c5
ekexium <ekexium@gmail.com>
2022-04-15 18:12:18 +0800

    fix test
    Signed-off-by: ekexium <ekexium@gmail.com>

--- session/nontransactional_test.go
+++ session/nontransactional_test.go
@@ -87,7 +87,7 @@ func TestNonTransactionalDeleteErrorMessage(t *testing.T) {
 	defer failpoint.Disable("github.com/pingcap/tidb/session/splitDeleteError")
 	rows := tk.MustQuery("split on a limit 3 delete from t").Rows()
 	require.Equal(t, 1, len(rows))
-	require.Equal(t, rows[0][2].(string), "Early return: error occurred in the first job: injected split delete error")
+	require.Equal(t, rows[0][2].(string), "Early return: error occurred in the first job. All jobs are canceled: injected split delete error")
 }
 
 func TestNonTransactionalDeleteSplitOnTiDBRowID(t *testing.T) {
