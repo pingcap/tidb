97ed6fc77f47256818420f529c5c0282ad72ef03
tangenta <tangenta@126.com>
2022-04-18 22:48:03 +0800

    cherry pick #33991 to release-5.3
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- parser/ast/dml.go
+++ parser/ast/dml.go
@@ -14,8 +14,6 @@
 package ast
 
 import (
-	"strings"
-
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/parser/auth"
 	"github.com/pingcap/tidb/parser/format"
@@ -286,14 +284,7 @@ func (n *TableName) restoreName(ctx *format.RestoreCtx) {
 		ctx.WritePlain(".")
 	} else if ctx.DefaultDB != "" {
 		// Try CTE, for a CTE table name, we shouldn't write the database name.
-		ok := false
-		for _, name := range ctx.CTENames {
-			if strings.EqualFold(name, n.Name.String()) {
-				ok = true
-				break
-			}
-		}
-		if !ok {
+		if !ctx.IsCTETableName(n.Name.L) {
 			ctx.WriteName(ctx.DefaultDB)
 			ctx.WritePlain(".")
 		}
@@ -1117,7 +1108,7 @@ func (n *WithClause) Restore(ctx *format.RestoreCtx) error {
 		if n.IsRecursive {
 			// If the CTE is recursive, we should make it visible for the CTE's query.
 			// Otherwise, we should put it to stack after building the CTE's query.
-			ctx.CTENames = append(ctx.CTENames, cte.Name.L)
+			ctx.RecordCTEName(cte.Name.L)
 		}
 		if len(cte.ColNameList) > 0 {
 			ctx.WritePlain(" (")
@@ -1135,7 +1126,7 @@ func (n *WithClause) Restore(ctx *format.RestoreCtx) error {
 			return err
 		}
 		if !n.IsRecursive {
-			ctx.CTENames = append(ctx.CTENames, cte.Name.L)
+			ctx.RecordCTEName(cte.Name.L)
 		}
 	}
 	ctx.WritePlain(" ")
@@ -1161,10 +1152,7 @@ func (n *WithClause) Accept(v Visitor) (Node, bool) {
 // Restore implements Node interface.
 func (n *SelectStmt) Restore(ctx *format.RestoreCtx) error {
 	if n.WithBeforeBraces {
-		l := len(ctx.CTENames)
-		defer func() {
-			ctx.CTENames = ctx.CTENames[:l]
-		}()
+		defer ctx.RestoreCTEFunc()()
 		err := n.With.Restore(ctx)
 		if err != nil {
 			return err
@@ -1512,10 +1500,7 @@ type SetOprSelectList struct {
 // Restore implements Node interface.
 func (n *SetOprSelectList) Restore(ctx *format.RestoreCtx) error {
 	if n.With != nil {
-		l := len(ctx.CTENames)
-		defer func() {
-			ctx.CTENames = ctx.CTENames[:l]
-		}()
+		defer ctx.RestoreCTEFunc()()
 		if err := n.With.Restore(ctx); err != nil {
 			return errors.Annotate(err, "An error occurred while restore SetOprSelectList.With")
 		}
@@ -1616,10 +1601,7 @@ func (*SetOprStmt) resultSet() {}
 // Restore implements Node interface.
 func (n *SetOprStmt) Restore(ctx *format.RestoreCtx) error {
 	if n.With != nil {
-		l := len(ctx.CTENames)
-		defer func() {
-			ctx.CTENames = ctx.CTENames[:l]
-		}()
+		defer ctx.RestoreCTEFunc()()
 		if err := n.With.Restore(ctx); err != nil {
 			return errors.Annotate(err, "An error occurred while restore UnionStmt.With")
 		}
@@ -2201,10 +2183,7 @@ type DeleteStmt struct {
 // Restore implements Node interface.
 func (n *DeleteStmt) Restore(ctx *format.RestoreCtx) error {
 	if n.With != nil {
-		l := len(ctx.CTENames)
-		defer func() {
-			ctx.CTENames = ctx.CTENames[:l]
-		}()
+		defer ctx.RestoreCTEFunc()()
 		err := n.With.Restore(ctx)
 		if err != nil {
 			return err
@@ -2365,10 +2344,7 @@ type UpdateStmt struct {
 // Restore implements Node interface.
 func (n *UpdateStmt) Restore(ctx *format.RestoreCtx) error {
 	if n.With != nil {
-		l := len(ctx.CTENames)
-		defer func() {
-			ctx.CTENames = ctx.CTENames[:l]
-		}()
+		defer ctx.RestoreCTEFunc()()
 		err := n.With.Restore(ctx)
 		if err != nil {
 			return err
