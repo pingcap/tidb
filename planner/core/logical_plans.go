5a8bafcf4a854892ff184150543e83e2eb662de1
xuyifan <675434007@qq.com>
2022-04-18 18:28:07 +0800

    remove unrelated code

--- planner/core/logical_plans.go
+++ planner/core/logical_plans.go
@@ -707,13 +707,6 @@ func (la *LogicalAggregation) GetGroupByCols() []*expression.Column {
 		if col, ok := item.(*expression.Column); ok {
 			groupByCols = append(groupByCols, col)
 		}
-		if function, ok := item.(*expression.ScalarFunction); ok {
-			if len(function.GetArgs()) == 1 {
-				if col, ok := function.GetArgs()[0].(*expression.Column); ok {
-					groupByCols = append(groupByCols, col)
-				}
-			}
-		}
 	}
 	return groupByCols
 }
