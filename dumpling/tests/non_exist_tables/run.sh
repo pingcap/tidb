55174407b26bb855673ebba89d137f4e6f93787b
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2022-04-19 15:09:25 +0800

    fix integration test

--- dumpling/tests/non_exist_tables/run.sh
+++ dumpling/tests/non_exist_tables/run.sh
@@ -17,10 +17,10 @@ run_sql "insert into \`$DB_NAME\`.\`$TABLE_NAME1\` values (1), (2);"
 
 # Test for specifying --tables-list with non-existing tables, should report an error
 set +e
-rm -rf "$DUMPLING_TEST_DIR"
-run_dumpling --consistency=lock --tables-list "$DB_NAME.$TABLE_NAME2" -L ${DUMPLING_TEST_DIR}/dumpling.log
+rm -rf "$DUMPLING_OUTPUT_DIR"
+run_dumpling --consistency=lock --tables-list "$DB_NAME.$TABLE_NAME2" -L ${DUMPLING_OUTPUT_DIR}/dumpling.log
 set -e
 
-actual=$(grep -w "Error 1146: Table 'non_exist_tables.t2' doesn't exist" ${DUMPLING_TEST_DIR}/dumpling.log|wc -l)
+actual=$(grep -w "Error 1146: Table 'non_exist_tables.t2' doesn't exist" ${DUMPLING_OUTPUT_DIR}/dumpling.log|wc -l)
 echo "expected 1 return error when specifying --tables-list with non-existing tables, actual ${actual}"
 [ $actual = 1 ]
