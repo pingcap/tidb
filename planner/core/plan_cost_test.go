412ccdc88720500a63181ebf7c23ba6a94e43568
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-04-21 11:17:45 +0800

    fixup

--- planner/core/plan_cost_test.go
+++ planner/core/plan_cost_test.go
@@ -16,7 +16,6 @@ package core_test
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/pingcap/tidb/testkit"
@@ -50,15 +49,6 @@ func TestNewCostInterfaceTiKV(t *testing.T) {
 
 	tk.MustExec("use test")
 	tk.MustExec(`create table t (a int primary key, b int, c int, d int, key b(b), key cd(c, d))`)
-	var vals []string
-	for i := 0; i < 1500; i++ {
-		vals = append(vals, fmt.Sprintf(`(%v, %v, %v, %v)`, i, i, i, i))
-		if len(vals) >= 100 {
-			tk.MustExec(fmt.Sprintf(`insert into t values %v`, strings.Join(vals, ", ")))
-			vals = vals[:0]
-		}
-	}
-	tk.MustExec(`analyze table t`)
 	tk.MustExec(`set @@session.tidb_stats_load_sync_wait=2000`)
 
 	queries := []string{
