f95082a5b3f2871eb35680e0dc1be7c9e59b239d
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-04-27 21:25:21 +0800

    fixup

--- planner/core/plan_cost.go
+++ planner/core/plan_cost.go
@@ -460,12 +460,18 @@ func (p *PhysicalTopN) GetPlanCost(taskType property.TaskType) (float64, error) 
 
 // GetPlanCost calculates the cost of the plan if it has not been calculated yet and returns the cost.
 func (p *BatchPointGetPlan) GetPlanCost(taskType property.TaskType) (float64, error) {
-	return 0, nil
+	if p.accessCols == nil {
+		return 0, nil // generated in FastPlan Optimization
+	}
+	return p.GetCost(), nil // generated in Physical Plan Optimization
 }
 
 // GetPlanCost calculates the cost of the plan if it has not been calculated yet and returns the cost.
 func (p *PointGetPlan) GetPlanCost(taskType property.TaskType) (float64, error) {
-	return 0, nil
+	if p.accessCols == nil {
+		return 0, nil // generated in FastPlan Optimization
+	}
+	return p.GetCost(), nil // generated in Physical Plan Optimization
 }
 
 // GetPlanCost calculates the cost of the plan if it has not been calculated yet and returns the cost.
