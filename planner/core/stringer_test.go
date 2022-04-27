2f98d2111a9a45b6033d406ec432cf13e8d87067
Weizhen Wang <wangweizhen@pingcap.com>
2022-04-27 18:35:06 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- planner/core/stringer_test.go
+++ planner/core/stringer_test.go
@@ -42,11 +42,7 @@ func TestPlanStringer(t *testing.T) {
 		},
 		{
 			sql:  "show columns from t like 'a%'",
-<<<<<<< HEAD
-			plan: "Show(field_pattern:[(?i)a.*])",
-=======
 			plan: "Show(field_pattern:[a%])",
->>>>>>> b4ebd44127 (cherry pick #32490 to release-5.4)
 		},
 		{
 			sql:  "show columns from t where field = 'a'",
@@ -70,19 +66,11 @@ func TestPlanStringer(t *testing.T) {
 		},
 		{
 			sql:  "show tables in test like 't%'",
-<<<<<<< HEAD
-			plan: "Show(table_pattern:[(?i)t.*])",
-		},
-		{
-			sql:  "show tables in test like '%T%'",
-			plan: "Show(table_pattern:[(?i).*T.*])",
-=======
 			plan: "Show(table_pattern:[t%])",
 		},
 		{
 			sql:  "show tables in test like '%T%'",
 			plan: "Show(table_pattern:[%t%])",
->>>>>>> b4ebd44127 (cherry pick #32490 to release-5.4)
 		},
 	}
 	parser := parser.New()
