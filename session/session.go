d95ba8c956063da59483884f9f19f46526629021
wjhuang2016 <huangwenjun1997@gmail.com>
2022-04-28 16:22:37 +0800

    fix
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- session/session.go
+++ session/session.go
@@ -2839,7 +2839,9 @@ func BootstrapSession(store kv.Storage) (*domain.Domain, error) {
 		return nil, err
 	}
 
-	if store.Name() == "TiKV" {
+	if dom.GetEtcdClient() != nil {
+		// We only want telemetry data in production-like clusters. When TiDB is deployed over other engines,
+		// for example, unistore engine (used for local tests), we just skip it. Its etcd client is nil.
 		dom.TelemetryReportLoop(ses[5])
 		dom.TelemetryRotateSubWindowLoop(ses[5])
 	}
