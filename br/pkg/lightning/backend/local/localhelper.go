c0e39e400f1f9e72313d6438289feb2ead83bb9a
D3Hunter <jujj603@gmail.com>
2022-04-29 14:30:52 +0800

    lightning: fix epoch-not-match not retryable (#34179)
    close pingcap/tidb#34172

--- br/pkg/lightning/backend/local/localhelper.go
+++ br/pkg/lightning/backend/local/localhelper.go
@@ -452,7 +452,7 @@ func (local *local) waitForScatterRegion(ctx context.Context, regionInfo *split.
 			return
 		}
 		if err != nil {
-			if !utils.IsRetryableError(err) {
+			if !common.IsRetryableError(err) {
 				log.L().Warn("wait for scatter region encountered non-retryable error", logutil.Region(regionInfo.Region), zap.Error(err))
 				return
 			}
