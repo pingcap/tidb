656074aef07cca431e68b833c98790ffebf83bef
Shenghui Wu <793703860@qq.com>
2022-05-09 15:16:33 +0800

    cherry pick #34418 to release-5.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/index_lookup_join.go
+++ executor/index_lookup_join.go
@@ -310,6 +310,10 @@ func (e *IndexLookUpJoin) getFinishedTask(ctx context.Context) (*lookUpJoinTask,
 		return task, nil
 	}
 
+	// The previous task has been processed, so release the occupied memory
+	if task != nil {
+		task.memTracker.Detach()
+	}
 	select {
 	case task = <-e.resultCh:
 	case <-ctx.Done():
@@ -556,7 +560,7 @@ func (iw *innerWorker) constructLookupContent(task *lookUpJoinTask) ([]*indexJoi
 				return nil, err
 			}
 			if rowIdx == 0 {
-				iw.lookup.memTracker.Consume(types.EstimatedMemUsage(dLookUpKey, numRows))
+				iw.memTracker.Consume(types.EstimatedMemUsage(dLookUpKey, numRows))
 			}
 			if dHashKey == nil {
 				// Append null to make looUpKeys the same length as outer Result.
