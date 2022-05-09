ce307853982a85097445a7be2b9409397ff4e5e2
Zhou Kunqin <25057648+time-and-fate@users.noreply.github.com>
2022-05-09 12:14:33 +0800

    cherry pick #34449 to release-5.4
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/integration_test.go
+++ planner/core/integration_test.go
@@ -5305,3 +5305,18 @@ func (s *testIntegrationSuite) TestDNFCondSelectivityWithConst(c *C) {
 		"[  └─TableFullScan_5 129.00 cop[tikv] table:t1 keep order:false"))
 	testKit.MustExec("drop table if exists t1")
 }
+
+func TestIssue31609(t *testing.T) {
+	store, _, clean := testkit.CreateMockStoreAndDomain(t)
+	defer clean()
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+
+	tk.MustQuery("explain select rank() over (partition by table_name) from information_schema.tables").Check(testkit.Rows(
+		"Projection_7 10000.00 root  Column#27",
+		"└─Shuffle_11 10000.00 root  execution info: concurrency:5, data sources:[MemTableScan_9]",
+		"  └─Window_8 10000.00 root  rank()->Column#27 over(partition by Column#3)",
+		"    └─Sort_10 10000.00 root  Column#3",
+		"      └─MemTableScan_9 10000.00 root table:TABLES ",
+	))
+}
