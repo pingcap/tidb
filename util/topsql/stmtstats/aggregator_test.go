29972644421ec1579f4c84c7413800a2d2077a72
mornyx <mornyx.z@gmail.com>
2022-05-11 12:10:36 +0800

    Fix mem leak in stmtstats
    Signed-off-by: mornyx <mornyx.z@gmail.com>

--- util/topsql/stmtstats/aggregator_test.go
+++ util/topsql/stmtstats/aggregator_test.go
@@ -63,7 +63,7 @@ func Test_aggregator_register_collect(t *testing.T) {
 	a.registerCollector(newMockCollector(func(data StatementStatsMap) {
 		total.Merge(data)
 	}))
-	a.aggregate(true)
+	a.aggregate()
 	assert.NotEmpty(t, total)
 	assert.Equal(t, uint64(1), total[SQLPlanDigest{SQLDigest: "SQL-1"}].ExecCount)
 	assert.Equal(t, uint64(time.Millisecond.Nanoseconds()), total[SQLPlanDigest{SQLDigest: "SQL-1"}].SumDurationNs)
