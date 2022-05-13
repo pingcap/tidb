26c8332dae02a4a6be2b410b245c5087474ba164
Morgan Tocker <tocker@gmail.com>
2022-05-13 07:10:47 -0600

    fix bad merge

--- session/nontransactional.go
+++ session/nontransactional.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
+	"github.com/pingcap/tidb/config"
 	"github.com/pingcap/tidb/domain"
 	"github.com/pingcap/tidb/errno"
 	"github.com/pingcap/tidb/executor"
