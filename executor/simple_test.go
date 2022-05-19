b2890b41735c1173d0b2360fb69ce7bb0c2badaa
Xiaoju Wu <wuxiaoju@pingcap.com>
2022-05-19 18:22:39 +0800

    cherry pick #34820 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/simple_test.go
+++ executor/simple_test.go
@@ -651,6 +651,12 @@ partition by range (a) (
 
 	tk.MustExec("drop stats t partition p0, p1, global")
 	checkPartitionStats("global")
+
+	tk.MustExec("analyze table test_drop_gstats")
+	checkPartitionStats("global", "p0", "p1", "global")
+
+	tk.MustExec("drop stats test_drop_gstats")
+	checkPartitionStats()
 }
 
 func (s *testSuite3) TestDropStats(c *C) {
