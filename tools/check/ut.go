524f18e400244655cf9477797e02dd99d2291a27
Weizhen Wang <wangweizhen@pingcap.com>
2022-05-23 18:40:53 +0800

    ut: add timeout for race
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- tools/check/ut.go
+++ tools/check/ut.go
@@ -808,6 +808,8 @@ func (n *numa) testCommand(pkg string, fn string) *exec.Cmd {
 	if !race {
 		// Don't set timeout for race because it takes a longer when race is enabled.
 		args = append(args, []string{"-test.timeout", "2m"}...)
+	} else {
+		args = append(args, []string{"-test.timeout", "20m"}...)
 	}
 	// session.test -test.run TestClusteredPrefixColum
 	args = append(args, "-test.run", fn)
