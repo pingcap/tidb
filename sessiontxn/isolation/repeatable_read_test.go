a3557d103e961f30dd1f424e2c7ad32a3ca4a43a
SpadeA-Tang <u6748471@anu.edu.au>
2022-05-30 17:54:53 +0800

    init
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

---
+++ sessiontxn/isolation/repeatable_read_test.go
@@ -0,0 +1,22 @@
+package isolation_test
+
+import (
+	"github.com/pingcap/tidb/sessiontxn"
+	"github.com/pingcap/tidb/sessiontxn/isolation"
+	"github.com/pingcap/tidb/testkit"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRepeatableReadProvider(t *testing.T) {
+	store, _, clean := testkit.CreateMockStoreAndDomain(t)
+	defer clean()
+}
+
+func initializeRepeatableReadProvider(t *testing.T, tk *testkit.TestKit, txnMode string) *isolation.RepeatableReadTxnContextProvider {
+	tk.MustExec("set @@tx_isolation = 'REPEATABLE-READ")
+	tk.MustExec("begin pessimistic")
+	provider := sessiontxn.GetTxnManager(tk.Session()).GetContextProvider()
+	require.IsType(t, &isolation.RepeatableReadTxnContextProvider{}, provider)
+	return provider.(*isolation.RepeatableReadTxnContextProvider)
+}
