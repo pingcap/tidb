e6baea46cf463c356f716c209256d2e0fcf8b606
littlelittlehorse <ymkzpx@outlook.com>
2022-05-31 10:54:15 +0800

    update

--- table/tables/partition_test.go
+++ table/tables/partition_test.go
@@ -736,7 +736,10 @@ func TestPruneModeWarningInfo(t *testing.T) {
 
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("set @@tidb_partition_prune_mode = 'static'")
+	tk.MustQuery("show warnings").Check(testkit.Rows())
 	tk.MustExec("set session tidb_partition_prune_mode = 'dynamic'")
 	tk.MustQuery("show warnings").Sort().Check(testkit.Rows("Warning 1105 Please analyze all partition tables again for consistency between partition and global stats",
 		"Warning 1105 Please avoid setting partition prune mode to dynamic at session level and set partition prune mode to dynamic at global level"))
+	tk.MustExec("set global tidb_partition_prune_mode = 'dynamic'")
+	tk.MustQuery("show warnings").Check(testkit.Rows("Warning 1105 Please analyze all partition tables again for consistency between partition and global stats"))
 }
