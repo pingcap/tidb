125481200ea5b316a59fb3914b10fb2a5b6fd8ad
SpadeA-Tang <u6748471@anu.edu.au>
2022-05-31 20:08:23 +0800

    update
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- sessiontxn/isolation/repeatable_read.go
+++ sessiontxn/isolation/repeatable_read.go
@@ -14,13 +14,174 @@
 
 package isolation
 
-import "github.com/pingcap/tidb/sessionctx"
+import (
+	"context"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/parser/ast"
+	"github.com/pingcap/tidb/parser/terror"
+	"github.com/pingcap/tidb/sessionctx"
+	"github.com/pingcap/tidb/sessionctx/variable"
+	"github.com/pingcap/tidb/sessiontxn"
+	"github.com/pingcap/tidb/util/logutil"
+	tikverr "github.com/tikv/client-go/v2/error"
+	"go.uber.org/zap"
+)
 
-type RepeatableReadTxnContextProvider struct {
-	baseTxnContextProvider
+type updateStmtState struct {
+	updateTS          uint64
+	updateForUpdateTS func() error
+	onNextRetryOrStmt func() error
 }
 
-// NewRepeatableReadTxnContextProvider returns a new RepeatableReadTxnContextProvider
-func NewRepeatableReadTxnContextProvider(sctx sessionctx.Context) *RepeatableReadTxnContextProvider {
-	panic("Unimplemented")
+type PessimisticRRTxnContextProvider struct {
+	baseTxnContextProvider
+	updateStmtState
+}
+
+func (p *PessimisticRRTxnContextProvider) getForUpdateTs() (ts uint64, err error) {
+	if p.updateTS != 0 {
+		return p.updateTS, nil
+	}
+
+	if err := p.updateForUpdateTs(); err != nil {
+		return 0, err
+	}
+
+	return p.updateTS, nil
+}
+
+func (p *PessimisticRRTxnContextProvider) updateForUpdateTs() error {
+	var txn kv.Transaction
+	var err error
+
+	if txn, err = p.activeTxn(); err != nil {
+		return err
+	}
+
+	txnCxt := p.sctx.GetSessionVars().TxnCtx
+	futureTS := sessiontxn.NewOracleFuture(p.ctx, p.sctx, txnCxt.TxnScope)
+
+	var ts uint64
+	if ts, err = futureTS.Wait(); err != nil {
+		return err
+	}
+
+	txnCxt.SetForUpdateTS(ts)
+	txn.SetOption(kv.SnapshotTS, ts)
+
+	p.updateTS = ts
+
+	return nil
+}
+
+// NewPessimisticRRTxnContextProvider returns a new PessimisticRRTxnContextProvider
+func NewPessimisticRRTxnContextProvider(sctx sessionctx.Context, causalConsistencyOnly bool) *PessimisticRRTxnContextProvider {
+	provider := &PessimisticRRTxnContextProvider{
+		baseTxnContextProvider: baseTxnContextProvider{
+			sctx:                  sctx,
+			causalConsistencyOnly: causalConsistencyOnly,
+			onInitializeTxnCtx: func(txnCtx *variable.TransactionContext) {
+				txnCtx.IsPessimistic = true
+				txnCtx.Isolation = ast.RepeatableRead
+			},
+			onTxnActive: func(txn kv.Transaction) {
+				txn.SetOption(kv.Pessimistic, true)
+			},
+		},
+	}
+
+	provider.getStmtReadTSFunc = provider.getTxnStartTS
+	provider.getStmtForUpdateTSFunc = provider.getForUpdateTs
+	provider.updateForUpdateTS = provider.updateForUpdateTs
+
+	return provider
+}
+
+// OnStmtStart is the hook that should be called when a new statement started
+func (p *PessimisticRRTxnContextProvider) OnStmtStart(ctx context.Context) error {
+	p.ctx = ctx
+	p.infoSchema = sessiontxn.GetTxnManager(p.sctx).GetTxnInfoSchema()
+	return nil
+}
+
+// OnStmtErrorForNextAction is the hook that should be called when a new statement get an error
+func (p *PessimisticRRTxnContextProvider) OnStmtErrorForNextAction(point sessiontxn.StmtErrorHandlePoint, err error) (sessiontxn.StmtErrorAction, error) {
+	switch point {
+	case sessiontxn.StmtErrAfterPessimisticLock:
+		return p.handleAfterPessimisticLockError(err)
+	default:
+		return sessiontxn.NoIdea()
+	}
+}
+
+// Advise is used to give advice to provider
+func (p *PessimisticRRTxnContextProvider) Advise(tp sessiontxn.AdviceType) error {
+	switch tp {
+	case sessiontxn.AdviceWarmUp:
+		return p.warmUp()
+	default:
+		return p.baseTxnContextProvider.Advise(tp)
+	}
+}
+
+func (p *PessimisticRRTxnContextProvider) warmUp() error {
+	if p.isTidbSnapshotEnabled() {
+		return nil
+	}
+
+	if err := p.prepareTxn(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// handleAfterQueryError will be called when the handle point is `StmtErrAfterQuery`.
+// At this point the query will be retried from the beginning.
+func (p *PessimisticRRTxnContextProvider) handleAfterQueryError(queryErr error) (sessiontxn.StmtErrorAction, error) {
+	sessVars := p.sctx.GetSessionVars()
+	if errors.ErrorEqual(queryErr, kv.ErrWriteConflict) {
+		logutil.Logger(p.ctx).Info("Pessimistic repeatable read failed, retry it",
+			zap.String("sql", sessVars.StmtCtx.OriginalSQL))
+		return sessiontxn.RetryReady()
+	}
+
+	return sessiontxn.NoIdea()
+}
+
+func (p *PessimisticRRTxnContextProvider) handleAfterPessimisticLockError(lockErr error) (sessiontxn.StmtErrorAction, error) {
+	sessVars := p.sctx.GetSessionVars()
+	txnCtx := sessVars.TxnCtx
+
+	if deadlock, ok := errors.Cause(lockErr).(*tikverr.ErrDeadlock); ok {
+		if !deadlock.IsRetryable {
+			return sessiontxn.ErrorAction(lockErr)
+		}
+
+		logutil.Logger(p.ctx).Info("single statement deadlock, retry statement",
+			zap.Uint64("txn", txnCtx.StartTS),
+			zap.Uint64("lockTS", deadlock.LockTs),
+			zap.Stringer("lockKey", kv.Key(deadlock.LockKey)),
+			zap.Uint64("deadlockKeyHash", deadlock.DeadlockKeyHash))
+	} else if terror.ErrorEqual(kv.ErrWriteConflict, lockErr) {
+		errStr := lockErr.Error()
+		forUpdateTS := txnCtx.GetForUpdateTS()
+
+		logutil.Logger(p.ctx).Debug("pessimistic write conflict, retry statement",
+			zap.Uint64("txn", txnCtx.StartTS),
+			zap.Uint64("forUpdateTS", forUpdateTS),
+			zap.String("err", errStr))
+	} else {
+		if err := p.updateForUpdateTS(); err != nil {
+			logutil.Logger(p.ctx).Warn("UpdateForUpdateTS failed", zap.Error(err))
+		}
+
+		return sessiontxn.ErrorAction(lockErr)
+	}
+
+	if err := p.updateForUpdateTS(); err != nil {
+		return sessiontxn.ErrorAction(lockErr)
+	}
+
+	return sessiontxn.RetryReady()
 }
--- sessiontxn/isolation/repeatable_read_test.go
+++ sessiontxn/isolation/repeatable_read_test.go
@@ -1,25 +1,144 @@
 package isolation_test
 
 import (
+	"context"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/kvproto/pkg/kvrpcpb"
+	"github.com/pingcap/tidb/executor"
+	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/sessiontxn"
 	"github.com/pingcap/tidb/sessiontxn/isolation"
 	"github.com/pingcap/tidb/testkit"
 	"github.com/stretchr/testify/require"
+	tikverr "github.com/tikv/client-go/v2/error"
 	"testing"
 )
+
+func newDeadLockError(isRetryable bool) error {
+	return &tikverr.ErrDeadlock{
+		Deadlock:    &kvrpcpb.Deadlock{},
+		IsRetryable: isRetryable,
+	}
+}
+
+func TestPessimisticRRErrorHandle(t *testing.T) {
+	store, _, clean := testkit.CreateMockStoreAndDomain(t)
+	defer clean()
+	tk := testkit.NewTestKit(t, store)
+	se := tk.Session()
+
+	var lockErr error
+
+	compareTS := getOracleTS(t, se)
+	lockErr = kv.ErrWriteConflict
+	provider := initializeRepeatableReadProvider(t, tk, "PESSIMISTIC")
+	nextAction, err := provider.OnStmtErrorForNextAction(sessiontxn.StmtErrAfterPessimisticLock, lockErr)
+	require.NoError(t, err)
+	require.Equal(t, sessiontxn.StmtActionRetryReady, nextAction)
+	ts, err := provider.GetStmtForUpdateTS()
+	require.NoError(t, err)
+	// StmtActionRetryReady means we will update the forUpdateTS, so it should be larger than the compareTS
+	require.Greater(t, ts, compareTS)
+
+	// Update compareTS for the next comparison
+	compareTS = getOracleTS(t, se)
+	lockErr = newDeadLockError(false)
+	nextAction, err = provider.OnStmtErrorForNextAction(sessiontxn.StmtErrAfterPessimisticLock, lockErr)
+	require.Equal(t, lockErr, err)
+	require.Equal(t, sessiontxn.StmtActionError, nextAction)
+	ts, err = provider.GetStmtForUpdateTS()
+	require.NoError(t, err)
+	// StmtActionError means we will not update the forUpdateTS, so it should be less than the compareTS
+	require.Greater(t, compareTS, ts)
+
+	lockErr = newDeadLockError(true)
+	nextAction, err = provider.OnStmtErrorForNextAction(sessiontxn.StmtErrAfterPessimisticLock, lockErr)
+	require.NoError(t, err)
+	require.Equal(t, sessiontxn.StmtActionRetryReady, nextAction)
+	ts, err = provider.GetStmtForUpdateTS()
+	require.NoError(t, err)
+	// StmtActionRetryReady means we will update the forUpdateTS, so it should be larger than the compareTS
+	require.Greater(t, ts, compareTS)
+
+	compareTS = getOracleTS(t, se)
+	// StmtErrAfterLock: other errors should only update forUpdateTS but not retry
+	lockErr = errors.New("other error")
+	nextAction, err = provider.OnStmtErrorForNextAction(sessiontxn.StmtErrAfterPessimisticLock, lockErr)
+	require.Equal(t, lockErr, err)
+	require.Equal(t, sessiontxn.StmtActionError, nextAction)
+	ts, err = provider.GetStmtForUpdateTS()
+	require.NoError(t, err)
+	require.Greater(t, ts, compareTS)
+
+	compareTS = getOracleTS(t, se)
+	// StmtErrAfterQuery: always not retry and not update forUpdateTS
+	lockErr = kv.ErrWriteConflict
+	nextAction, err = provider.OnStmtErrorForNextAction(sessiontxn.StmtErrAfterQuery, lockErr)
+	require.Equal(t, sessiontxn.StmtActionNoIdea, nextAction)
+	require.Nil(t, err)
+	ts, err = provider.GetStmtForUpdateTS()
+	require.NoError(t, err)
+	require.Greater(t, compareTS, ts)
+
+}
 
 func TestRepeatableReadProvider(t *testing.T) {
 	store, _, clean := testkit.CreateMockStoreAndDomain(t)
 	defer clean()
 	tk := testkit.NewTestKit(t, store)
-	initializeRepeatableReadProvider(t, tk, "PESSIMISTIC")
+	se := tk.Session()
+	provider := initializeRepeatableReadProvider(t, tk, "PESSIMISTIC")
 
+	stmts, _, err := parser.New().Parse("select * from t", "", "")
+	require.NoError(t, err)
+	readOnlyStmt := stmts[0]
+
+	stmts, _, err = parser.New().Parse("select * from t for update", "", "")
+	require.NoError(t, err)
+	forUpdateStmt := stmts[0]
+
+	compareTS := getOracleTS(t, se)
+	// The read ts should be less than the compareTS
+	require.NoError(t, executor.ResetContextOfStmt(se, readOnlyStmt))
+	require.NoError(t, provider.OnStmtStart(context.TODO()))
+	ts, err := provider.GetStmtReadTS()
+	require.NoError(t, err)
+	require.Greater(t, compareTS, ts)
+
+	// The read ts should also be less than the compareTS in a new statement
+	require.NoError(t, executor.ResetContextOfStmt(se, readOnlyStmt))
+	require.NoError(t, provider.OnStmtStart(context.TODO()))
+	ts, err = provider.GetStmtReadTS()
+	require.NoError(t, err)
+	require.Greater(t, compareTS, ts)
+
+	// The read ts should still be less than the compareTS in a retry statement
+	require.NoError(t, executor.ResetContextOfStmt(se, readOnlyStmt))
+	require.NoError(t, provider.OnStmtRetry(context.TODO()))
+	ts, err = provider.GetStmtReadTS()
+	require.NoError(t, err)
+	require.Greater(t, compareTS, ts)
+
+	// The for update read ts should be larger than the compareTS
+	require.NoError(t, executor.ResetContextOfStmt(se, forUpdateStmt))
+	require.NoError(t, provider.OnStmtStart(context.TODO()))
+	ts, err = provider.GetStmtForUpdateTS()
+	require.NoError(t, err)
+	require.Greater(t, ts, compareTS)
+
+	// But the read ts is still less than the compareTS
+	require.NoError(t, executor.ResetContextOfStmt(se, readOnlyStmt))
+	require.NoError(t, provider.OnStmtStart(context.TODO()))
+	ts, err = provider.GetStmtReadTS()
+	require.NoError(t, err)
+	require.Greater(t, compareTS, ts)
 }
 
-func initializeRepeatableReadProvider(t *testing.T, tk *testkit.TestKit, txnMode string) *isolation.RepeatableReadTxnContextProvider {
-	tk.MustExec("set @@tx_isolation='REPEATABLE-READ'")
+func initializeRepeatableReadProvider(t *testing.T, tk *testkit.TestKit, txnMode string) *isolation.PessimisticRRTxnContextProvider {
+	tk.MustExec("set @@tx_isolation = 'REPEATABLE-READ'")
 	tk.MustExec("begin pessimistic")
 	provider := sessiontxn.GetTxnManager(tk.Session()).GetContextProvider()
-	require.IsType(t, &isolation.RepeatableReadTxnContextProvider{}, provider)
-	return provider.(*isolation.RepeatableReadTxnContextProvider)
+	require.IsType(t, &isolation.PessimisticRRTxnContextProvider{}, provider)
+	return provider.(*isolation.PessimisticRRTxnContextProvider)
 }
