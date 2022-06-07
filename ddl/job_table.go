821f36a796f82bba8c9a7a56cdfaa3d1a9e669cb
xiongjiwei <xiongjiwei1996@outlook.com>
2022-06-07 18:55:56 +0800

    add ddl manager to handle ddl job
    find a runnable ddl job
    ship the job to the worker
    
    it adds a function HandleDDLJob which will do the ddl job, and it is a replacer of HandleDDLJobQueue. The last thing of this commit is adding schemaVersionManager to update the schema version in a new txn, it prevents the txn conflict with the schema version key.

--- ddl/job_table.go
+++ ddl/job_table.go
@@ -14,6 +14,55 @@
 
 package ddl
 
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/parser/model"
+	"github.com/pingcap/tidb/util/logutil"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.uber.org/zap"
+)
+
+var (
+	addingDDLJobGeneral = "/tidb/ddl/add_ddl_job_general"
+	addingDDLJobReorg   = "/tidb/ddl/add_ddl_job_reorg"
+)
+
+func (d *ddl) insertRunningDDLJobMap(id int64) {
+	d.runningJobs.Lock()
+	defer d.runningJobs.Unlock()
+	d.runningJobs.runningJobMap[id] = struct{}{}
+}
+
+func (d *ddl) deleteRunningDDLJobMap(id int64) {
+	d.runningJobs.Lock()
+	defer d.runningJobs.Unlock()
+	delete(d.runningJobs.runningJobMap, id)
+}
+
+func (d *ddl) getRunningDDLJobIDs() string {
+	d.resetRunningIDs()
+	d.runningJobs.RLock()
+	d.runningJobs.RUnlock()
+	for id := range d.runningJobs.runningJobMap {
+		d.runningOrBlockedIDs = append(d.runningOrBlockedIDs, strconv.Itoa(int(id)))
+	}
+	return strings.Join(d.runningOrBlockedIDs, ",")
+}
+
+func (d *ddl) resetRunningIDs() {
+	d.runningOrBlockedIDs = d.runningOrBlockedIDs[:1]
+}
+
+const (
+	getJob = "select job_meta from mysql.tidb_ddl_job where job_id in (select min(job_id) from mysql.tidb_ddl_job group by schema_id, table_id) and %s reorg and job_id not in (%s)"
+)
+
 type jobType int
 
 func (t jobType) String() string {
@@ -30,3 +79,142 @@ const (
 	general jobType = iota
 	reorg           = iota
 )
+
+func (d *ddl) getJob(sess *session, tp jobType, filter func(*model.Job) (bool, error)) (*model.Job, error) {
+	not := "not"
+	label := "get_job_general"
+	if tp == reorg {
+		not = ""
+		label = "get_job_reorg"
+	}
+	sql := fmt.Sprintf(getJob, not, d.getRunningDDLJobIDs())
+	rows, err := sess.execute(context.Background(), sql, label)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	for _, row := range rows {
+		jobBinary := row.GetBytes(0)
+		runJob := model.Job{}
+		err := runJob.Decode(jobBinary)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		b, err := filter(&runJob)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		if b {
+			return &runJob, nil
+		}
+	}
+	return nil, nil
+}
+
+func (d *ddl) getGeneralJob(sess *session) (*model.Job, error) {
+	return d.getJob(sess, general, func(job *model.Job) (bool, error) {
+		if job.Type == model.ActionDropSchema {
+			sql := fmt.Sprintf("select * from mysql.tidb_ddl_job where schema_id = %d and job_id < %d limit 1", job.SchemaID, job.ID)
+			return d.checkJobIsRunnable(sess, sql)
+		}
+		return true, nil
+	})
+}
+
+func (d *ddl) checkJobIsRunnable(sess *session, sql string) (bool, error) {
+	rows, err := sess.execute(context.Background(), sql, "check_runnable")
+	return len(rows) == 0, err
+}
+
+func (d *ddl) getReorgJob(sess *session) (*model.Job, error) {
+	return d.getJob(sess, reorg, func(job *model.Job) (bool, error) {
+		sql := fmt.Sprintf("select * from mysql.tidb_ddl_job where schema_id = %d and is_drop_schema and job_id < %d limit 1", job.SchemaID, job.ID)
+		return d.checkJobIsRunnable(sess, sql)
+	})
+}
+
+func (d *ddl) startDispatchLoop() {
+	se, err := d.sessPool.get()
+	if err != nil {
+		logutil.BgLogger().Fatal("dispatch loop get session failed, it should not happen, please try restart TiDB", zap.Error(err))
+	}
+	defer d.sessPool.put(se)
+	sess := newSession(se)
+	var notifyDDLJobByEtcdChGeneral clientv3.WatchChan
+	var notifyDDLJobByEtcdChReorg clientv3.WatchChan
+	if d.etcdCli != nil {
+		notifyDDLJobByEtcdChGeneral = d.etcdCli.Watch(context.Background(), addingDDLJobGeneral)
+		notifyDDLJobByEtcdChReorg = d.etcdCli.Watch(context.Background(), addingDDLJobReorg)
+	}
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	for {
+		if isChanClosed(d.ctx.Done()) {
+			return
+		}
+		if !d.isOwner() {
+			d.once.Store(true)
+			time.Sleep(time.Second)
+			continue
+		}
+		select {
+		case <-d.ddlJobCh:
+		case <-ticker.C:
+		case _, ok := <-notifyDDLJobByEtcdChGeneral:
+			if !ok {
+				logutil.BgLogger().Warn("[ddl] start worker watch channel closed", zap.String("watch key", addingDDLJobGeneral))
+				notifyDDLJobByEtcdChGeneral = d.etcdCli.Watch(context.Background(), addingDDLJobGeneral)
+				time.Sleep(time.Duration(1) * time.Second)
+				continue
+			}
+		case _, ok := <-notifyDDLJobByEtcdChReorg:
+			if !ok {
+				logutil.BgLogger().Warn("[ddl] start worker watch channel closed", zap.String("watch key", addingDDLJobReorg))
+				notifyDDLJobByEtcdChReorg = d.etcdCli.Watch(context.Background(), addingDDLJobReorg)
+				time.Sleep(time.Duration(1) * time.Second)
+				continue
+			}
+		}
+		d.runDDLJob(sess, d.generalDDLWorkerPool, d.getGeneralJob)
+		d.runDDLJob(sess, d.reorgWorkerPool, d.getReorgJob)
+	}
+}
+
+func (d *ddl) runDDLJob(sess *session, pool *workerPool, getJob func(*session) (*model.Job, error)) {
+	wk, err := pool.get()
+	if err != nil || wk == nil {
+		logutil.BgLogger().Debug(fmt.Sprintf("[ddl] no %v worker available now", pool.tp()), zap.Error(err))
+		return
+	}
+
+	job, err := getJob(sess)
+	if job == nil || err != nil {
+		if err != nil {
+			logutil.BgLogger().Warn("[ddl] get job met error", zap.Error(err))
+		}
+		pool.put(wk)
+		return
+	}
+
+	d.doDDLJob(wk, pool, job)
+}
+
+func (d *ddl) doDDLJob(wk *worker, pool *workerPool, job *model.Job) {
+	injectFailPointForGetJob(job)
+	d.insertRunningDDLJobMap(job.ID)
+	d.wg.Run(func() {
+		defer func() {
+			pool.put(wk)
+			d.deleteRunningDDLJobMap(job.ID)
+			asyncNotify(d.ddlJobCh)
+		}()
+		// we should wait 2 * d.lease time to guarantee all TiDB server have finished the schema change.
+		// see waitSchemaSynced for more details.
+		if !d.isSynced(job) || d.once.Load() {
+			wk.waitSchemaSynced(d.ddlCtx, job, 2*d.lease)
+			d.once.Store(false)
+		}
+		if err := wk.HandleDDLJob(d.ddlCtx, job); err != nil {
+			logutil.BgLogger().Info("[ddl] handle ddl job failed", zap.Error(err), zap.String("job", job.String()))
+		}
+	})
+}
