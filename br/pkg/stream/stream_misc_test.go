1c48f92843b67fc5bf41832d1adefd22820c58cb
Zak Zhao <57036248+joccau@users.noreply.github.com>
2022-06-13 10:12:15 +0800

    adapt new checkpoint model (#35304)
    Signed-off-by: Yu Juncen <yujuncen@pingcap.com>
    
    Co-authored-by: Yu Juncen <yujuncen@pingcap.com>

--- br/pkg/stream/stream_misc_test.go
+++ br/pkg/stream/stream_misc_test.go
@@ -15,18 +15,27 @@ func TestGetCheckpointOfTask(t *testing.T) {
 		Info: backuppb.StreamBackupTaskInfo{
 			StartTs: 8,
 		},
-		Progress: map[uint64]uint64{
-			1: 10,
-			2: 8,
-			6: 12,
+		Checkpoints: []stream.Checkpoint{
+			{
+				ID: 1,
+				TS: 10,
+			},
+			{
+				ID: 2,
+				TS: 8,
+			},
+			{
+				ID: 6,
+				TS: 12,
+			},
 		},
 	}
 
-	require.Equal(t, task.GetCheckpoint(), uint64(8), "progress = %v", task.Progress)
-	task.Progress[2] = 18
+	require.Equal(t, task.GetMinStoreCheckpoint().TS, uint64(8), "progress = %v", task.Checkpoints)
+	task.Checkpoints[1].TS = 18
 
-	require.Equal(t, task.GetCheckpoint(), uint64(10))
-	task.Progress[1] = 14
+	require.Equal(t, task.GetMinStoreCheckpoint().TS, uint64(10))
+	task.Checkpoints[0].TS = 14
 
-	require.Equal(t, task.GetCheckpoint(), uint64(12), "progress = %v", task.Progress)
+	require.Equal(t, task.GetMinStoreCheckpoint().TS, uint64(12), "progress = %v", task.Checkpoints)
 }
