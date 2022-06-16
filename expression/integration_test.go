ea70ff1af09082132229ba26891c2eeb2577be6c
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2022-06-16 16:12:34 +0800

    expression: fix append to result twice when vecEval compress('') (#33523) (#33555)
    close pingcap/tidb#33397

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -10560,3 +10560,13 @@ func (s *testIntegrationSuite) TestIssue30326(c *C) {
 	c.Assert(err, NotNil)
 	c.Assert(err.Error(), Equals, "[executor:1242]Subquery returns more than 1 row")
 }
+
+func (s *testIntegrationSuite) TestIssue33397(c *C) {
+	tk := testkit.NewTestKit(c, s.store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t;")
+	tk.MustExec("create table t(a varchar(32));")
+	tk.MustExec("insert into t values(''), ('');")
+	tk.MustExec("set @@tidb_enable_vectorized_expression = true;")
+	tk.MustQuery("select compress(a) from t").Check(testkit.Rows("", ""))
+}
