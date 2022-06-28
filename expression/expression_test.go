9fe875adb76502f54c36d854592d109a45a38fca
Zhi Qi <30543181+LittleFall@users.noreply.github.com>
2022-06-28 14:02:39 +0800

    cherry pick #35759 to release-5.4
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/expression_test.go
+++ expression/expression_test.go
@@ -79,6 +79,30 @@ func TestEvaluateExprWithNullAndParameters(t *testing.T) {
 	require.True(t, isScalarFunc) // the expression with parameters is not evaluated
 }
 
+func TestEvaluateExprWithNullNoChangeRetType(t *testing.T) {
+	ctx := createContext(t)
+	tblInfo := newTestTableBuilder("").add("col_str", mysql.TypeString, 0).build()
+	schema := tableInfoToSchemaForTest(tblInfo)
+
+	castStrAsJSON := BuildCastFunction(ctx, schema.Columns[0], types.NewFieldType(mysql.TypeJSON))
+	jsonConstant := &Constant{Value: types.NewDatum("123"), RetType: types.NewFieldType(mysql.TypeJSON)}
+
+	// initially has ParseToJSONFlag
+	flagInCast := castStrAsJSON.(*ScalarFunction).RetType.GetFlag()
+	require.True(t, mysql.HasParseToJSONFlag(flagInCast))
+
+	// cast's ParseToJSONFlag removed by `DisableParseJSONFlag4Expr`
+	eq, err := newFunctionForTest(ctx, ast.EQ, jsonConstant, castStrAsJSON)
+	require.NoError(t, err)
+	flagInCast = eq.(*ScalarFunction).GetArgs()[1].(*ScalarFunction).RetType.GetFlag()
+	require.False(t, mysql.HasParseToJSONFlag(flagInCast))
+
+	// after EvaluateExprWithNull, this flag should be still false
+	EvaluateExprWithNull(ctx, schema, eq)
+	flagInCast = eq.(*ScalarFunction).GetArgs()[1].(*ScalarFunction).RetType.GetFlag()
+	require.False(t, mysql.HasParseToJSONFlag(flagInCast))
+}
+
 func TestConstant(t *testing.T) {
 	ctx := createContext(t)
 	sc := &stmtctx.StatementContext{TimeZone: time.Local}
