332adef60f2aa69913007d69464f04aa4124573d
Yiding Cui <winoros@gmail.com>
2022-06-29 20:34:26 +0800

    fix typo

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -3630,7 +3630,7 @@ func (b *PlanBuilder) pushTableHints(hints []*ast.TableOptimizerHint, currentLev
 			leadingHintCnt++
 		case HintSemiJoinRewrite:
 			if !b.checkSemiJoinHint {
-				b.ctx.GetSessionVars().StmtCtx.AppendWarning(ErrInternal.GenWithStack("The SEMI_JOIN_REWRITE hint is not used correctly, maybe it's not in a subquery or the subquery is not EXSITS clause."))
+				b.ctx.GetSessionVars().StmtCtx.AppendWarning(ErrInternal.GenWithStack("The SEMI_JOIN_REWRITE hint is not used correctly, maybe it's not in a subquery or the subquery is not EXISTS clause."))
 				continue
 			}
 			b.hasValidSemiJoinHint = true
