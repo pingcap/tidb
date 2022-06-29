74edb634767ec1ebd4cfbd6828cbe2cda38bb10a
Reminiscent <y870414423@gmail.com>
2022-06-29 16:52:27 +0800

    address comments

--- planner/core/exhaust_physical_plans.go
+++ planner/core/exhaust_physical_plans.go
@@ -395,11 +395,9 @@ func (p *LogicalJoin) getHashJoins(prop *property.PhysicalProperty) ([]PhysicalP
 		}
 	case InnerJoin:
 		if useLeftToBuild {
-			joins = append(joins, p.getHashJoin(prop, 1, true))
-			joins = append(joins, p.getHashJoin(prop, 0, false))
-		} else if useRightToBuild {
 			joins = append(joins, p.getHashJoin(prop, 1, false))
-			joins = append(joins, p.getHashJoin(prop, 0, true))
+		} else if useRightToBuild {
+			joins = append(joins, p.getHashJoin(prop, 0, false))
 		} else {
 			joins = append(joins, p.getHashJoin(prop, 1, false))
 			joins = append(joins, p.getHashJoin(prop, 0, false))
