a5da2415b156514b3939cd9acec232760eb904b0
wjhuang2016 <huangwenjun1997@gmail.com>
2022-06-30 16:43:00 +0800

    fix sequnce as default
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- ddl/column.go
+++ ddl/column.go
@@ -1774,6 +1774,7 @@ func updateColumnDefaultValue(d *ddlCtx, t *meta.Meta, job *model.Job, newCol *m
 	// The newCol's offset may be the value of the old schema version, so we can't use newCol directly.
 	oldCol.DefaultValue = newCol.DefaultValue
 	oldCol.DefaultValueBit = newCol.DefaultValueBit
+	oldCol.DefaultIsExpr = newCol.DefaultIsExpr
 	if mysql.HasNoDefaultValueFlag(newCol.GetFlag()) {
 		oldCol.AddFlag(mysql.NoDefaultValueFlag)
 	} else {
