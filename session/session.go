b65b7cfb5689cb42923ffedcd430de4027a827e1
Xiaoguang Sun <sunxiaoguang@gmail.com>
2022-07-03 10:35:44 +0800

    Initialize telemetry asynchronously
    Telemetry initialization might take 30 seconds when the route from
    cluster and service endpoint is slow. It can be made asynchronously
    without sacrificing anything.
    
    Signed-off-by: Xiaoguang Sun <sunxiaoguang@gmail.com>

--- session/session.go
+++ session/session.go
@@ -2880,10 +2880,12 @@ func BootstrapSession(store kv.Storage) (*domain.Domain, error) {
 	}
 
 	if dom.GetEtcdClient() != nil {
-		// We only want telemetry data in production-like clusters. When TiDB is deployed over other engines,
-		// for example, unistore engine (used for local tests), we just skip it. Its etcd client is nil.
-		dom.TelemetryReportLoop(ses[5])
-		dom.TelemetryRotateSubWindowLoop(ses[5])
+		go func() {
+			// We only want telemetry data in production-like clusters. When TiDB is deployed over other engines,
+			// for example, unistore engine (used for local tests), we just skip it. Its etcd client is nil.
+			dom.TelemetryReportLoop(ses[5])
+			dom.TelemetryRotateSubWindowLoop(ses[5])
+		}()
 	}
 
 	// A sub context for update table stats, and other contexts for concurrent stats loading.
