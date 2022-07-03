3dd85f1d5215e2d3af2273a52f731a4545967c12
YangKeao <yangkeao@chunibyo.icu>
2022-07-03 21:27:00 +0800

    implement hexInt pushdown
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- expression/expression.go
+++ expression/expression.go
@@ -1156,10 +1156,7 @@ func scalarExprSupportedByFlash(function *ScalarFunction) bool {
 	case ast.IsTruthWithNull, ast.IsTruthWithoutNull, ast.IsFalsity:
 		return true
 	case ast.Hex:
-		switch function.Function.PbCode() {
-		case tipb.ScalarFuncSig_HexStrArg:
-			return true
-		}
+		return true
 	}
 	return false
 }
