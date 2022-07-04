2bd5160fcc7a09d55665c542cd7e5c89815ba3a4
SpadeA-Tang <u6748471@anu.edu.au>
2022-07-04 14:12:20 +0800

    update
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- sessiontxn/interface.go
+++ sessiontxn/interface.go
@@ -151,10 +151,10 @@ type TxnManager interface {
 	GetStmtForUpdateTS() (uint64, error)
 	// GetContextProvider returns the current TxnContextProvider
 	GetContextProvider() TxnContextProvider
-	// GetReadSnapshot get snapshot with read ts
-	GetReadSnapshot() (kv.Snapshot, error)
-	// GetForUpdateSnapshot get snapshot with for update ts
-	GetForUpdateSnapshot() (kv.Snapshot, error)
+	// GetSnapshotWithStmtReadTS get snapshot with read ts
+	GetSnapshotWithStmtReadTS() (kv.Snapshot, error)
+	// GetSnapshotWithStmtForUpdateTS get snapshot with for update ts
+	GetSnapshotWithStmtForUpdateTS() (kv.Snapshot, error)
 
 	// EnterNewTxn enters a new transaction.
 	EnterNewTxn(ctx context.Context, req *EnterNewTxnRequest) error
