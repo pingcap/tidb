153391334f35cd9f239a39253f7383340aa49e02
yisaer <disxiaofei@163.com>
2022-07-06 13:54:31 +0800

    add test
    Signed-off-by: yisaer <disxiaofei@163.com>

--- executor/analyze_test.go
+++ executor/analyze_test.go
@@ -16,6 +16,8 @@ package executor_test
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"sync/atomic"
 	"testing"
 
@@ -323,7 +325,16 @@ func TestAnalyzePartitionTableForFloat(t *testing.T) {
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("set @@tidb_partition_prune_mode='dynamic'")
 	tk.MustExec("use test")
-	tk.MustExec("CREATE TABLE t1 ( id int, num float(9,8) DEFAULT NULL, PRIMARY KEY (id)) PARTITION BY RANGE (id) (PARTITION p0 VALUES LESS THAN MAXVALUE)")
-	tk.MustExec("INSERT INTO t1 (id,num) VALUES (6404,0.44816685),(6532,0.7986926);")
+	tk.MustExec("CREATE TABLE t1 ( id bigint(20) unsigned NOT NULL AUTO_INCREMENT, num float(9,8) DEFAULT NULL, PRIMARY KEY (id)  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin PARTITION BY HASH (id) PARTITIONS 128;")
+	// To reproduce the error we meet in https://github.com/pingcap/tidb/issues/35910, we should use the data provided in this issue
+	b, err := ioutil.ReadFile("testdata/analyze_test_data.sql")
+	require.NoError(t, err)
+	sqls := strings.Split(string(b), ";")
+	for _, sql := range sqls {
+		if len(sql) < 1 {
+			continue
+		}
+		tk.MustExec(sql)
+	}
 	tk.MustExec("analyze table t1")
 }
