781568c760802cd9b7a3465743af43d0769fda2d
Weizhen Wang <wangweizhen@pingcap.com>
2022-07-09 11:14:37 +0800

    build: add blackimport linter to avoid using banned libraries
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ build/linter/blackimport/analysis.go
@@ -0,0 +1,42 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package blackimport
+
+import (
+	"strconv"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "blackimport",
+	Doc:  "avoid using banned libraries",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, f := range pass.Files {
+		for _, imp := range f.Imports {
+			path, err := strconv.Unquote(imp.Path.Value)
+			if err != nil {
+				return nil, err
+			}
+			if path == "github.com/pingcap/check" {
+				pass.Reportf(imp.Pos(), "It has been replaced by github.com/stretchr/testify. https://github.com/pingcap/tidb/issues/26022")
+			}
+		}
+	}
+	return nil, nil
+}
---
+++ build/linter/blackimport/testdata/go.mod
@@ -0,0 +1,18 @@
+module testdata
+
+go 1.18
+
+require github.com/pingcap/check v0.0.0-20211026125417-57bd13f7b5f0
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/pingcap/log v0.0.0-20191012051959-b742a5d432e9 // indirect
+	go.uber.org/atomic v1.5.0 // indirect
+	go.uber.org/multierr v1.4.0 // indirect
+	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
+	go.uber.org/zap v1.12.0 // indirect
+	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
+	golang.org/x/tools v0.0.0-20191107010934-f79515f33823 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	honnef.co/go/tools v0.0.1-2019.2.3 // indirect
+)
