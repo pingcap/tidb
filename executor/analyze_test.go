c5c16285c7ad794dc2cf409b8d07abc8147192be
Song Gao <disxiaofei@163.com>
2022-07-11 18:57:05 +0800

    cherry pick #35978 to release-5.4
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/analyze_test.go
+++ executor/analyze_test.go
@@ -17,9 +17,14 @@ package executor_test
 import (
 	"context"
 	"fmt"
+<<<<<<< HEAD
 	"strconv"
 	"strings"
 	"sync"
+=======
+	"io/ioutil"
+	"strings"
+>>>>>>> d10d25457... util: let TypeFloat should be decoded as Float32 in Chunk (#35978)
 	"sync/atomic"
 	"testing"
 	"time"
@@ -2583,4 +2588,24 @@ func TestAnalyzeColumnsErrorAndWarning(t *testing.T) {
 			))
 		}(val)
 	}
+}
+
+func TestAnalyzePartitionTableForFloat(t *testing.T) {
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("set @@tidb_partition_prune_mode='dynamic'")
+	tk.MustExec("use test")
+	tk.MustExec("CREATE TABLE t1 ( id bigint(20) unsigned NOT NULL AUTO_INCREMENT, num float(9,8) DEFAULT NULL, PRIMARY KEY (id)  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin PARTITION BY HASH (id) PARTITIONS 128;")
+	// To reproduce the error we meet in https://github.com/pingcap/tidb/issues/35910, we should use the data provided in this issue
+	b, err := ioutil.ReadFile("testdata/analyze_test_data.sql")
+	require.NoError(t, err)
+	sqls := strings.Split(string(b), ";")
+	for _, sql := range sqls {
+		if len(sql) < 1 {
+			continue
+		}
+		tk.MustExec(sql)
+	}
+	tk.MustExec("analyze table t1")
 }
