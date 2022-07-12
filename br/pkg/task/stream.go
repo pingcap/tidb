a25a47ad5ceabaf008b280b0a58454a36d7ab71b
Jianjun Liao <36503113+Leavrth@users.noreply.github.com>
2022-07-12 14:33:56 +0800

    br: deal with mddljob when pitr restores (#35976)
    * deal with droptable mddljob
    
    * base code
    
    * directly map indexID
    
    * fix dropschema mddljob
    
    * delete unused variable `meetup`
    
    * fix new TableID
    
    * add unit_test
    
    * fmt
    
    * insert gc_delete_range later
    
    * add unit_test
    
    * fix unit test
    
    * skip the mddljob that cannot be decoded
    
    * add some comments
    
    * add some comments
    
    * skip the non-existent db/table/partition mddljob
    
    * add comment
    
    * error when meet ddl exchange-table-partition

--- br/pkg/task/stream.go
+++ br/pkg/task/stream.go
@@ -1114,6 +1114,7 @@ func restoreStream(
 
 	pm := g.StartProgress(ctx, "Restore Meta Files", int64(len(ddlFiles)), !cfg.LogProgress)
 	if err = withProgress(pm, func(p glue.Progress) error {
+		client.RunGCRowsLoader(ctx)
 		return client.RestoreMetaKVFiles(ctx, ddlFiles, schemasReplace, p.Inc)
 	}); err != nil {
 		return errors.Annotate(err, "failed to restore meta files")
@@ -1141,6 +1142,11 @@ func restoreStream(
 	if err = client.SaveSchemas(ctx, schemasReplace, logMinTS, cfg.RestoreTS); err != nil {
 		return errors.Trace(err)
 	}
+
+	if err = client.InsertGCRows(ctx); err != nil {
+		return errors.Annotate(err, "failed to insert rows into gc_delete_range")
+	}
+
 	return nil
 }
 
