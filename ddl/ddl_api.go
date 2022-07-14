5f522f166d3dff6943a332e6127343f387735595
littlelittlehorse <ymkzpx@outlook.com>
2022-07-14 10:45:53 +0800

    fix

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -3981,7 +3981,6 @@ func (d *ddl) ExchangeTablePartition(ctx sessionctx.Context, ident ast.Ident, sp
 	if err != nil {
 		return errors.Trace(err)
 	}
-	ctx.GetSessionVars().StmtCtx.AppendWarning(fmt.Errorf("after the exchange, please analyze related table of the exchange to update statistics"))
 	err = d.callHookOnChanged(job, err)
 	return errors.Trace(err)
 }
