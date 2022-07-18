ea90fff81e3cda7cda11b8fa835b8bb6ec19c7d1
Leavrth <jianjun.liao@outlook.com>
2022-07-18 21:16:19 +0800

    remove the limit to using storage

--- br/pkg/task/stream.go
+++ br/pkg/task/stream.go
@@ -307,10 +307,6 @@ func NewStreamMgr(ctx context.Context, cfg *StreamConfig, g glue.Glue, isStreamS
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
-		if backend.GetS3() == nil && backend.GetLocal() == nil {
-			return nil, errors.Annotate(berrors.ErrStorageInvalidConfig,
-				"Only support s3/local storage currently.")
-		}
 
 		opts := storage.ExternalStorageOptions{
 			NoCredentials:   cfg.NoCreds,
