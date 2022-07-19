eefa70b8df5af21121bf55e3c5cd0aecf6caf3e9
Zhigao Tong <tongzhigao@pingcap.com>
2022-07-19 16:43:07 +0800

    cherry pick #36258 to release-5.3
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -9484,3 +9484,26 @@ func (s *testSerialSuite) TestEncodingSet(c *C) {
 	tk.MustQuery("select `set` from `enum-set` use index(PRIMARY)").Check(testkit.Rows("x00,x59"))
 	tk.MustExec("admin check table `enum-set`")
 }
+
+func TestBinaryStrNumericOperator(t *testing.T) {
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
+
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+
+	// Test normal warnings.
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t(a varbinary(10))")
+	tk.MustExec("insert into t values ('123.12')")
+	tk.MustQuery("select 1+a from t").Check(testkit.Rows(
+		"124.12"))
+	tk.MustQuery("select a-1 from t").Check(testkit.Rows(
+		"122.12"))
+	tk.MustQuery("select -10*a from t").Check(testkit.Rows(
+		"-1231.2"))
+	tk.MustQuery("select a/-2 from t").Check(testkit.Rows(
+		"-61.56"))
+	// there should be no warning.
+	tk.MustQuery("show warnings").Check(testkit.Rows())
+}
