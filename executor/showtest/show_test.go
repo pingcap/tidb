f98f6175188dc116113379841997189abff65fe6
djshow832 <873581766@qq.com>
2022-07-19 16:58:03 +0800

    fix one test result

--- executor/showtest/show_test.go
+++ executor/showtest/show_test.go
@@ -1738,7 +1738,7 @@ func TestShowTemporaryTable(t *testing.T) {
 		"  `i` int(11) NOT NULL AUTO_INCREMENT,\n" +
 		"  `j` int(11) DEFAULT NULL,\n" +
 		"  PRIMARY KEY (`i`) /*T![clustered_index] CLUSTERED */\n" +
-		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=2"
+		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=3"
 	tk.MustQuery("show create table t7").Check(testkit.Rows("t7 " + expect))
 }
 
