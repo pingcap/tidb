3377cc1fc99bfd5eb99d28151a08c30124976c00
time-and-fate <25057648+time-and-fate@users.noreply.github.com>
2022-07-19 13:47:53 +0800

    update comments

--- sessionctx/variable/session.go
+++ sessionctx/variable/session.go
@@ -2713,7 +2713,7 @@ func (s *SessionVars) EnableEvalTopNEstimationForStrMatch() bool {
 }
 
 // GetStrMatchDefaultSelectivity means the default selectivity for like and regexp.
-// Note that 0 is a special value, which means the default selectivity is 0.1 and TopN assisted estimation is enabled.
+// Note: 0 is a special value, which means the default selectivity is 0.1 and TopN assisted estimation is enabled.
 func (s *SessionVars) GetStrMatchDefaultSelectivity() float64 {
 	if s.DefaultStrMatchSelectivity == 0 {
 		return 0.1
@@ -2722,6 +2722,10 @@ func (s *SessionVars) GetStrMatchDefaultSelectivity() float64 {
 }
 
 // GetNegateStrMatchDefaultSelectivity means the default selectivity for not like and not regexp.
+// Note:
+//     0 is a special value, which means the default selectivity is 0.9 and TopN assisted estimation is enabled.
+//     0.8 (the default value) is also a special value. For backward compatability, when the variable is set to 0.8, we
+//   keep the default selectivity of like/regexp and not like/regexp all 0.8.
 func (s *SessionVars) GetNegateStrMatchDefaultSelectivity() float64 {
 	if s.DefaultStrMatchSelectivity == 0 {
 		return 0.9
