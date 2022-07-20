937c309ce19fd2b21abca403f55c218886ba08d1
Mattias Jonsson <mjonss@gmail.com>
2022-07-20 11:16:15 +0200

    Fixed a typo

--- ddl/partition.go
+++ ddl/partition.go
@@ -771,7 +771,7 @@ func generatePartitionDefinitionsFromInterval(ctx sessionctx.Context, partOption
 	switch partOptions.Interval.IntervalExpr.TimeUnit {
 	case ast.TimeUnitInvalid, ast.TimeUnitYear, ast.TimeUnitQuarter, ast.TimeUnitMonth, ast.TimeUnitWeek, ast.TimeUnitDay, ast.TimeUnitHour, ast.TimeUnitDayMinute, ast.TimeUnitSecond:
 	default:
-		return dbterror.ErrGeneralUnsupportedDDL.GenWithStackByArgs("INTERVAL partitioning, only supports YEAR, QUARTER, MONTH, WEEK, DAY, HOUR, MINUTE and SECONDS as time unit")
+		return dbterror.ErrGeneralUnsupportedDDL.GenWithStackByArgs("INTERVAL partitioning, only supports YEAR, QUARTER, MONTH, WEEK, DAY, HOUR, MINUTE and SECOND as time unit")
 	}
 	first := ast.PartitionDefinitionClauseLessThan{
 		Exprs: []ast.ExprNode{*partOptions.Interval.FirstRangeEnd},
