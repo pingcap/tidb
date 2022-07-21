6c704f756fb5b15a6aed10754850af365ac5ed84
MoCuishle28 <32541204+MoCuishle28@users.noreply.github.com>
2022-07-21 20:01:09 +0800

    log-backup: remove the timezone from log-date (#36369)
    close pingcap/tidb#36370

--- br/pkg/stream/stream_status.go
+++ br/pkg/stream/stream_status.go
@@ -119,9 +119,9 @@ func (p *printByTable) AddTask(task TaskStatus) {
 	table := p.console.CreateTable()
 	table.Add("name", task.Info.Name)
 	table.Add("status", task.colorfulStatusString())
-	table.Add("start", fmt.Sprint(oracle.GetTimeFromTS(task.Info.StartTs)))
+	table.Add("start", fmt.Sprint(FormatDate(oracle.GetTimeFromTS(task.Info.StartTs))))
 	if task.Info.EndTs > 0 {
-		table.Add("end", fmt.Sprint(oracle.GetTimeFromTS(task.Info.EndTs)))
+		table.Add("end", fmt.Sprint(FormatDate(oracle.GetTimeFromTS(task.Info.EndTs))))
 	}
 	s := storage.FormatBackendURL(task.Info.GetStorage())
 	table.Add("storage", s.String())
@@ -135,7 +135,7 @@ func (p *printByTable) AddTask(task TaskStatus) {
 		if gap > 10*time.Minute {
 			gapColor = color.New(color.FgRed)
 		}
-		info := fmt.Sprintf("%s; gap=%s", pTime, gapColor.Sprint(gap))
+		info := fmt.Sprintf("%s; gap=%s", FormatDate(pTime), gapColor.Sprint(gap))
 		return info
 	}
 	table.Add("checkpoint[global]", formatTS(task.globalCheckpoint))
