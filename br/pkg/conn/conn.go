e019170d2446d003fa7ab24f0504b0dedc981716
3pointer <luancheng@pingcap.com>
2022-07-22 16:12:44 +0800

    update

--- br/pkg/conn/conn.go
+++ br/pkg/conn/conn.go
@@ -245,9 +245,10 @@ func NewMgr(
 		// when tidb < 6.2 we need set EnableConcurrentDDL false to make ddl works.
 		// we will keep this check until 7.0, which allow the breaking changes.
 		// NOTE: must call it after domain created!
+		// FIXME: remove this check in v7.0
 		err = version.CheckClusterVersion(ctx, controller.GetPDClient(), version.CheckVersionForDDL)
 		if err != nil {
-			return nil, errors.Annotate(err, "")
+			return nil, errors.Annotate(err, "unable to check cluster version for ddl")
 		}
 	}
 
