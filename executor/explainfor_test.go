6135e5a663752ce6151117210b8301c00e431714
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2022-07-25 15:17:11 +0800

    test case
    add test case

--- executor/explainfor_test.go
+++ executor/explainfor_test.go
@@ -1200,6 +1200,12 @@ func TestIgnorePlanCacheWithPrepare(t *testing.T) {
 	tk.MustQuery("execute stmt;").Check(testkit.Rows())
 	tk.MustQuery("select @@last_plan_from_binding;").Check(testkit.Rows("1"))
 
+	tk.MustExec("create binding for select * from t using select /*+ use_index(t, idx_a) */ * from t;")
+	tk.MustQuery("execute stmt;").Check(testkit.Rows())
+	tk.MustQuery("select @@last_plan_from_cache;").Check(testkit.Rows("1"))
+	tk.MustQuery("execute stmt;").Check(testkit.Rows())
+	tk.MustQuery("select @@last_plan_from_binding;").Check(testkit.Rows("1"))
+
 	// test straight_join
 	tk.MustExec("prepare stmt_join from 'select * from t, r where r.a = t.a;';")
 	tk.MustExec("create binding for select * from t, r where r.a = t.a using select /*+ straight_join() */* from t, r where r.a = t.a;")
@@ -1212,6 +1218,12 @@ func TestIgnorePlanCacheWithPrepare(t *testing.T) {
 	tk.MustExec("create binding for select * from t, r where r.a = t.a using select /*+ ignore_plan_cache() */* from t, r where r.a = t.a;")
 	tk.MustQuery("execute stmt_join;").Check(testkit.Rows())
 	tk.MustQuery("select @@last_plan_from_cache;").Check(testkit.Rows("0"))
+	tk.MustQuery("execute stmt_join;").Check(testkit.Rows())
+	tk.MustQuery("select @@last_plan_from_binding;").Check(testkit.Rows("1"))
+
+	tk.MustExec("create binding for select * from t, r where r.a = t.a using select /*+ straight_join() */* from t, r where r.a = t.a;")
+	tk.MustQuery("execute stmt_join;").Check(testkit.Rows())
+	tk.MustQuery("select @@last_plan_from_cache;").Check(testkit.Rows("1"))
 	tk.MustQuery("execute stmt_join;").Check(testkit.Rows())
 	tk.MustQuery("select @@last_plan_from_binding;").Check(testkit.Rows("1"))
 
