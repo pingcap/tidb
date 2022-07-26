e440c5a01ee49c3c124bde19ff110859981efea9
wjhuang2016 <huangwenjun1997@gmail.com>
2022-07-26 17:21:36 +0800

    fix
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- infoschema/tables_test.go
+++ infoschema/tables_test.go
@@ -1579,7 +1579,7 @@ func TestVariablesInfo(t *testing.T) {
 	tk.MustQuery(`SELECT * FROM variables_info WHERE variable_name = 'innodb_compression_level'`).Check(testkit.Rows("innodb_compression_level GLOBAL 6 8 <nil> <nil> <nil> YES"))
 
 	// enum
-	tk.MustQuery(`SELECT * FROM variables_info WHERE variable_name = 'tidb_txn_mode'`).Check(testkit.Rows("tidb_txn_mode SESSION,GLOBAL   <nil> <nil> pessimistic,optimistic NO"))
+	tk.MustQuery(`SELECT * FROM variables_info WHERE variable_name = 'tidb_txn_mode'`).Check(testkit.Rows("tidb_txn_mode SESSION,GLOBAL  pessimistic <nil> <nil> pessimistic,optimistic NO"))
 
 	// noop
 	tk.MustQuery(`SELECT * FROM variables_info WHERE variable_name = 'max_connections' AND is_noop='NO'`).Check(testkit.Rows("max_connections INSTANCE 0 0 0 100000 <nil> NO"))
