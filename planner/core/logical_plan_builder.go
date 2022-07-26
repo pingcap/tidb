af41a74d7c0b7e637f2e1b9e2d7794444ac4d177
yisaer <disxiaofei@163.com>
2022-07-26 14:51:46 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -5179,20 +5179,12 @@ func CheckUpdateList(assignFlags []int, updt *Update, newTblID2Table map[int64]t
 		}
 
 		for i, col := range tbl.WritableCols() {
-<<<<<<< HEAD
-			if flags[i] >= 0 && col.State != model.StatePublic {
-=======
 			// schema may be changed between building plan and building executor
 			// If i >= len(flags), it means the target table has been added columns, then we directly skip the check
 			if i >= len(flags) {
 				continue
 			}
-			if flags[i] < 0 {
-				continue
-			}
-
-			if col.State != model.StatePublic {
->>>>>>> c76888143... executor: fix panic during update stmt (#36230)
+			if flags[i] >= 0 && col.State != model.StatePublic {
 				return ErrUnknownColumn.GenWithStackByArgs(col.Name, clauseMsg[fieldList])
 			}
 			if flags[i] >= 0 {
