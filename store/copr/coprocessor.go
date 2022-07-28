ffae13072a0aae333ed589f7dcc222247c64652f
tiancaiamao <tiancaiamao@gmail.com>
2022-07-28 10:18:10 +0800

    address comment

--- store/copr/coprocessor.go
+++ store/copr/coprocessor.go
@@ -52,6 +52,7 @@ import (
 	"github.com/tikv/client-go/v2/txnkv/txnsnapshot"
 	"github.com/tikv/client-go/v2/util"
 	"go.uber.org/zap"
+	"golang.org/x/exp/slices"
 )
 
 var coprCacheCounterEvict = tidbmetrics.DistSQLCoprCacheCounter.WithLabelValues("evict")
@@ -71,7 +72,15 @@ type CopClient struct {
 
 // Send builds the request and gets the coprocessor iterator response.
 func (c *CopClient) Send(ctx context.Context, req *kv.Request, variables interface{}, option *kv.ClientSendOption) kv.Response {
-	if config.CheckTableBeforeDrop && !checkKeyRangeSorted(req.KeyRanges) {
+	if config.CheckTableBeforeDrop && !slices.IsSortedFunc(req.KeyRanges, func(i, j kv.KeyRange) bool {
+		if i.EndKey.Cmp(j.StartKey) > 0 {
+			return false
+		}
+		if i.StartKey.Cmp(i.EndKey) > 0 {
+			return false
+		}
+		return true
+	}) {
 		// In checking mode
 		logutil.BgLogger().Fatal("distsql request key range not sorted!")
 	}
@@ -1365,22 +1374,4 @@ func isolationLevelToPB(level kv.IsoLevel) kvrpcpb.IsolationLevel {
 	default:
 		return kvrpcpb.IsolationLevel_SI
 	}
-}
-
-func checkKeyRangeSorted(keyRanges []kv.KeyRange) bool {
-	if len(keyRanges) == 0 {
-		return true
-	}
-	lastKey := keyRanges[0].EndKey
-	for i := 1; i < len(keyRanges); i++ {
-		r := keyRanges[i]
-		if r.StartKey.Cmp(r.EndKey) >= 0 {
-			return false
-		}
-		if r.StartKey.Cmp(lastKey) < 0 {
-			return false
-		}
-		lastKey = r.EndKey
-	}
-	return true
 }
