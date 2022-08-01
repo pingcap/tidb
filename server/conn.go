390198176846f919d9c6e6b35c7b6d0b3f032bb7
Daniël van Eeden <git@myname.nl>
2022-08-01 08:49:50 +0200

    Fix linter issue about unchecked error (take 2)

--- server/conn.go
+++ server/conn.go
@@ -2175,10 +2175,10 @@ func (cc *clientConn) handleFieldList(ctx context.Context, sql string) (err erro
 	if cc.capability&mysql.ClientDeprecateEOF > 0 {
 		err = cc.writeOkWith(ctx, cc.ctx.LastMessage(), cc.ctx.AffectedRows(),
 			cc.ctx.LastInsertID(), cc.ctx.Status(), cc.ctx.WarningCount(), true)
-		cc.pkt.flush()
 		if err != nil {
 			return err
 		}
+		cc.pkt.flush()
 		return nil
 	}
 	if err := cc.writeEOF(0); err != nil {
