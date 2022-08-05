5dadae472d9d16c5ce5ad6c653ba9182fc182a4d
Weizhen Wang <wangweizhen@pingcap.com>
2022-08-05 14:03:50 +0800

    fix test
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- br/pkg/lightning/config/bytesize_test.go
+++ br/pkg/lightning/config/bytesize_test.go
@@ -89,11 +89,11 @@ func TestByteSizeTOMLDecode(t *testing.T) {
 		},
 		{
 			input: "x = ['100000']",
-			err:   "toml: line 1 (last key \"x\"): incompatible types:.*",
+			err:   "toml: .* incompatible types:.*",
 		},
 		{
 			input: "x = { size = '100000' }",
-			err:   "toml: line 1 (last key \"x\"): incompatible types:.*",
+			err:   "toml: .* incompatible types:.*",
 		},
 	}
 
