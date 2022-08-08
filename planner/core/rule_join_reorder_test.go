b8e431bec4d6d4037c55ea4e6a72016cb40e1137
Yiding Cui <winoros@gmail.com>
2022-08-08 18:15:35 +0800

    fix merge conflict

--- planner/core/rule_join_reorder_test.go
+++ planner/core/rule_join_reorder_test.go
@@ -329,29 +329,10 @@ func TestJoinOrderHint4Subquery(t *testing.T) {
 
 	runJoinReorderTestData(t, tk, "TestJoinOrderHint4Subquery")
 }
-<<<<<<< HEAD
-=======
-
-func TestLeadingJoinHint4OuterJoin(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t, t1, t2, t3, t4, t5, t6, t7, t8;")
-	tk.MustExec("create table t(a int, b int, key(a));")
-	tk.MustExec("create table t1(a int, b int, key(a));")
-	tk.MustExec("create table t2(a int, b int, key(a));")
-	tk.MustExec("create table t3(a int, b int, key(a));")
-	tk.MustExec("create table t4(a int, b int, key(a));")
-	tk.MustExec("create table t5(a int, b int, key(a));")
-	tk.MustExec("create table t6(a int, b int, key(a));")
-	tk.MustExec("create table t7(a int, b int, key(a));")
-	tk.MustExec("create table t8(a int, b int, key(a));")
-	runJoinReorderTestData(t, tk, "TestLeadingJoinHint4OuterJoin")
-}
 
 func TestOuterJoinWIthEqCondCrossInnerJoin(t *testing.T) {
-	store := testkit.CreateMockStore(t)
+	store, clean := testkit.CreateMockStore(t)
+	defer clean()
 
 	tk := testkit.NewTestKit(t, store)
 
@@ -400,4 +381,3 @@ FROM   t3 tt
 		"      └─TableFullScan 10000.00 cop[tikv] table:t1 keep order:false, stats:pseudo",
 	))
 }
->>>>>>> 1e040684e... planner: fix the wrong join reorder produced by some right ouer join (#36936)
