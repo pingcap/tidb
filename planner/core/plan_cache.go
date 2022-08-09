5fe32080267e1bc825adc6d56ef563a9a95cef5b
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2022-08-09 12:11:57 +0800

    Update plan_cache.go

--- planner/core/plan_cache.go
+++ planner/core/plan_cache.go
@@ -106,7 +106,11 @@ func parseParamTypes(sctx sessionctx.Context, params []expression.Expression) (p
 		userVar, ok := sctx.GetSessionVars().UserVars.Vars[name]
 		var tp *ptypes.FieldType
 		if v, ok1 := userVar.(*expression.Constant); ok && ok1 {
-			tp = v.RetType
+			if v.RetType == nil {
+				types.DefaultParamTypeForValue(v.Value.GetValue(), tp)
+			} else {
+				tp = v.RetType
+			}
 		} else {
 			tp = types.NewFieldType(mysql.TypeNull)
 		}
