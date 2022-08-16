8ddd94ecc2252225be505d7d65886e139d65e03d
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2022-08-16 12:27:49 +0800

    minvalue = 0

--- sessionctx/variable/sysvar.go
+++ sessionctx/variable/sysvar.go
@@ -800,7 +800,7 @@ var defaultSysVars = []*SysVar{
 		s.EnableGeneralPlanCache = TiDBOptOn(val)
 		return nil
 	}},
-	{Scope: ScopeGlobal | ScopeSession, Name: TiDBGeneralPlanCacheSize, Value: strconv.FormatUint(uint64(DefTiDBGeneralPlanCacheSize), 10), Type: TypeUnsigned, MinValue: 0, MaxValue: 100000, SetSession: func(s *SessionVars, val string) error {
+	{Scope: ScopeGlobal | ScopeSession, Name: TiDBGeneralPlanCacheSize, Value: strconv.FormatUint(uint64(DefTiDBGeneralPlanCacheSize), 10), Type: TypeUnsigned, MinValue: 1, MaxValue: 100000, SetSession: func(s *SessionVars, val string) error {
 		uVal, err := strconv.ParseUint(val, 10, 64)
 		if err == nil {
 			s.GeneralPlanCacheSize = uVal
