bee697e75bb80b0de77634cacfd3319341742472
xhe <xw897002528@gmail.com>
2022-08-16 13:04:44 +0800

    again
    Signed-off-by: xhe <xw897002528@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 mkdir -p bin
 echo $TERM
 bazel $1 build $2 \
+	--color=yes \
 	//cmd/importer:importer //tidb-server:tidb-server //tidb-server:tidb-server-check --//build:with_nogo_flag=true
 if [ "$TERM" = "" ]; then
 	export TERM="xterm-256color"
