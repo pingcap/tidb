fff886a9c19f38c81eadc438f444037df3c81d3e
qw4990 <zhangyuanjia@pingcap.com>
2022-08-16 17:56:34 +0800

    fixup

--- util/mock/context.go
+++ util/mock/context.go
@@ -247,7 +247,7 @@ func (*Context) SetGlobalSysVar(_ sessionctx.Context, name string, value string)
 	return nil
 }
 
-// PreparedPlanCache implements the sessionctx.Context interface.
+// GetPlanCache implements the sessionctx.Context interface.
 func (c *Context) GetPlanCache(_ bool) *kvcache.SimpleLRUCache {
 	return c.pcache
 }
