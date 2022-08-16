086bcf706db8bb7bdd83193301508b7b461bc796
Weizhen Wang <wangweizhen@pingcap.com>
2022-08-16 12:09:17 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- session/session.go
+++ session/session.go
@@ -1226,7 +1226,7 @@ func createSessionFunc(store kv.Storage) pools.Factory {
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
-		err = se.sessionVars.SetSystemVar(variable.TiDBEnableWindowFunction, "1")
+		err = se.sessionVars.SetSystemVar(variable.TiDBEnableWindowFunction, variable.BoolToOnOff(variable.DefEnableWindowFunction))
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
