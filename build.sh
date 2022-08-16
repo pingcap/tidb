19e3a68b77b2946836f687e23c22193b1d186478
xhe <xw897002528@gmail.com>
2022-08-16 13:37:22 +0800

    again
    Signed-off-by: xhe <xw897002528@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+
+if [ -t 0 ] ; then
+	echo "is a tty"
+else
+	echo "not a tty"
+fi
+
 COLOR=""
 mkdir -p bin
 echo $TERM
 if [ "$TERM" = "" ]; then
 	COLOR="--color=yes"
 fi
-bazel $1 build $2 \
+bazel $1 build $2 $COLOR \
 	//cmd/importer:importer //tidb-server:tidb-server //tidb-server:tidb-server-check --//build:with_nogo_flag=true
 if [ "$TERM" = "" ]; then
 	export TERM="xterm-256color"
@@ -17,5 +24,5 @@ echo $TERM
 if [ "$TERM" = "" ]; then
 	COLOR="--color=yes"
 fi
-bazel $1 build $2 \
+bazel $1 build $2 $COLOR \
 	//cmd/importer:importer //tidb-server:tidb-server //tidb-server:tidb-server-check --//build:with_nogo_flag=true
