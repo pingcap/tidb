9837405cf55b03a22d6ef67171568e5fd0577883
Reminiscent <y870414423@gmail.com>
2022-08-16 13:50:16 +0800

    Revert "Merge the PointGet short path to general execute path"
    This reverts commit 762f07e64c217faffbdec094ed7163b3f9bf1246.

--- session/session.go
+++ session/session.go
@@ -2011,9 +2011,11 @@ func (s *session) ExecuteStmt(ctx context.Context, stmtNode ast.StmtNode) (sqlex
 	logStmt(stmt, s)
 
 	var recordSet sqlexec.RecordSet
-	recordSet, err = runStmt(ctx, s, stmt)
 	if stmt.PsStmt != nil { // point plan short path
+		recordSet, err = stmt.PointGet(ctx)
 		s.txn.changeToInvalid()
+	} else {
+		recordSet, err = runStmt(ctx, s, stmt)
 	}
 	if err != nil {
 		if !errIsNoisy(err) {
@@ -2339,13 +2341,13 @@ func (s *session) preparedStmtExec(ctx context.Context, execStmt *ast.ExecuteStm
 	sessionExecuteCompileDurationGeneral.Observe(time.Since(s.sessionVars.StartTime).Seconds())
 	logGeneralQuery(stmt, s, true)
 
-	var recordSet sqlexec.RecordSet
-	recordSet, err = runStmt(ctx, s, stmt)
 	if stmt.PsStmt != nil { // point plan short path
+		resultSet, err := stmt.PointGet(ctx)
 		s.txn.changeToInvalid()
+		return resultSet, err
 	}
 
-	return recordSet, err
+	return runStmt(ctx, s, stmt)
 }
 
 // ExecutePreparedStmt executes a prepared statement.
