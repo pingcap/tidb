33b1da1f1d480c4b6c4369e4e82bf3d46dd46813
xhe <xw897002528@gmail.com>
2022-08-16 13:22:49 +0800

    again
    Signed-off-by: xhe <xw897002528@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+COLOR=""
 mkdir -p bin
 echo $TERM
+if [ "$TERM" = "" ]; then
+	COLOR="--color=yes"
+fi
 bazel $1 build $2 \
-	--color=yes \
 	//cmd/importer:importer //tidb-server:tidb-server //tidb-server:tidb-server-check --//build:with_nogo_flag=true
 if [ "$TERM" = "" ]; then
 	export TERM="xterm-256color"
@@ -11,5 +14,8 @@ else
 	export TERM=""
 fi
 echo $TERM
+if [ "$TERM" = "" ]; then
+	COLOR="--color=yes"
+fi
 bazel $1 build $2 \
 	//cmd/importer:importer //tidb-server:tidb-server //tidb-server:tidb-server-check --//build:with_nogo_flag=true
