3d17fdd4247dcfc04104103bdb37c28525b0126e
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2022-08-22 15:38:53 +0800

    lru for plan cache

---
+++ planner/core/plan_cache_lru.go
@@ -0,0 +1,192 @@
+// Package core Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package core
+
+import (
+	"container/list"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/util/kvcache"
+)
+
+// CacheEntry wraps Key and Value. It's the value of list.Element.
+type CacheEntry struct {
+	PlanKey   kvcache.Key
+	PlanValue kvcache.Value
+}
+
+// LRUPlanCache is a simple least recently used cache, not thread-safe, JUST use for plan cache.
+type LRUPlanCache struct {
+	capacity uint
+	size     uint
+	// buckets replace the map in general LRU
+	buckets map[string][]*list.Element
+
+	// choose set rules to get one element from bucket.
+	choose func([]*list.Element, interface{}) (*list.Element, int, bool)
+
+	// onEvict function will be called if any eviction happened
+	onEvict func(kvcache.Key, kvcache.Value)
+	cache   *list.List
+}
+
+// NewLRUPlanCache creates a PCLRUCache object, whose capacity is "capacity".
+// NOTE: "capacity" should be a positive value.
+func NewLRUPlanCache(capacity uint) *LRUPlanCache {
+	if capacity < 1 {
+		panic("capacity of LRU Cache should be at least 1.")
+	}
+	return &LRUPlanCache{
+		capacity: capacity,
+		size:     0,
+		buckets:  make(map[string][]*list.Element),
+		onEvict:  nil,
+		cache:    list.New(),
+	}
+}
+
+// SetOnEvict set the function called on each eviction.
+func (l *LRUPlanCache) SetOnEvict(onEvict func(kvcache.Key, kvcache.Value)) {
+	l.onEvict = onEvict
+}
+
+// SetChoose set the function called on each eviction.
+func (l *LRUPlanCache) SetChoose(choose func([]*list.Element, interface{}) (*list.Element, int, bool)) {
+	l.choose = choose
+}
+
+// Get tries to find the corresponding value according to the given key.
+func (l *LRUPlanCache) Get(key kvcache.Key, paramTypes interface{}) (value kvcache.Value, ok bool) {
+	bucket, bucketExist := l.buckets[string(key.Hash())]
+	if bucketExist {
+		if element, _, exist := l.choose(bucket, paramTypes); exist {
+			l.cache.MoveToFront(element)
+			return element.Value.(*CacheEntry).PlanValue, true
+		}
+	}
+	return nil, false
+}
+
+// Put puts the (key, value) pair into the LRU Cache.
+func (l *LRUPlanCache) Put(key kvcache.Key, value kvcache.Value, paramTypes interface{}) {
+	hash := string(key.Hash())
+	bucket, bucketExist := l.buckets[hash]
+	// in the cache
+	if bucketExist {
+		if element, _, exist := l.choose(bucket, paramTypes); exist {
+			element.Value.(*CacheEntry).PlanValue = value
+			l.cache.MoveToFront(element)
+			return
+		}
+	}
+	// not in the cache
+	newCacheEntry := &CacheEntry{
+		PlanKey:   key,
+		PlanValue: value,
+	}
+	element := l.cache.PushFront(newCacheEntry)
+	bucket = append(bucket, element)
+	l.buckets[hash] = bucket
+	l.size++
+	if l.size > l.capacity {
+		l.RemoveOldest()
+	}
+}
+
+// Delete deletes the multi-values from the LRU Cache.
+func (l *LRUPlanCache) Delete(key kvcache.Key) {
+	hash := string(key.Hash())
+	bucket, bucketExist := l.buckets[hash]
+	if bucketExist {
+		for _, element := range bucket {
+			l.cache.Remove(element)
+			l.size--
+		}
+		l.buckets[hash] = nil
+	}
+}
+
+// DeleteAll deletes all elements from the LRU Cache.
+func (l *LRUPlanCache) DeleteAll() {
+	for lru := l.cache.Back(); lru != nil; lru = l.cache.Back() {
+		l.cache.Remove(lru)
+		l.size--
+	}
+	l.buckets = make(map[string][]*list.Element)
+}
+
+// Size gets the current cache size.
+func (l *LRUPlanCache) Size() int {
+	return int(l.size)
+}
+
+// Values return all values in cache.
+func (l *LRUPlanCache) Values() []kvcache.Value {
+	values := make([]kvcache.Value, 0, l.cache.Len())
+	for ele := l.cache.Front(); ele != nil; ele = ele.Next() {
+		value := ele.Value.(*CacheEntry).PlanValue
+		values = append(values, value)
+	}
+	return values
+}
+
+// Keys return all keys in cache.
+func (l *LRUPlanCache) Keys() []kvcache.Key {
+	keys := make([]kvcache.Key, 0, len(l.buckets))
+	for _, bucket := range l.buckets {
+		keys = append(keys, bucket[0].Value.(*CacheEntry).PlanKey)
+	}
+	return keys
+}
+
+// SetCapacity sets capacity of the cache.
+func (l *LRUPlanCache) SetCapacity(capacity uint) error {
+	if capacity < 1 {
+		return errors.New("capacity of lru cache should be at least 1")
+	}
+	l.capacity = capacity
+	for l.size > l.capacity {
+		l.RemoveOldest()
+	}
+	return nil
+}
+
+// RemoveOldest removes the oldest element from the cache.
+func (l *LRUPlanCache) RemoveOldest() (key kvcache.Key, value kvcache.Value, ok bool) {
+	if l.size > 0 {
+		lru := l.cache.Back()
+		if l.onEvict != nil {
+			l.onEvict(lru.Value.(*CacheEntry).PlanKey, lru.Value.(*CacheEntry).PlanValue)
+		}
+
+		l.cache.Remove(lru)
+		l.removeFromBucket(lru)
+		l.size--
+		return lru.Value.(*CacheEntry).PlanKey, lru.Value.(*CacheEntry).PlanValue, true
+	}
+	return nil, nil, false
+}
+
+// removeFromBucket remove element from bucket
+func (l *LRUPlanCache) removeFromBucket(element *list.Element) {
+	hash := string(element.Value.(*CacheEntry).PlanKey.Hash())
+	bucket := l.buckets[hash]
+	for i, ele := range bucket {
+		if ele == element {
+			bucket = append(bucket[:i], bucket[i+1:]...)
+			l.buckets[hash] = bucket
+			return
+		}
+	}
+}
---
+++ planner/core/plan_cache_lru_test.go
@@ -0,0 +1,336 @@
+// Package core Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package core
+
+import (
+	"container/list"
+	"github.com/pingcap/tidb/util/kvcache"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type mockCacheKey struct {
+	hash []byte
+	key  int64
+}
+
+func (mk *mockCacheKey) Hash() []byte {
+	if mk.hash != nil {
+		return mk.hash
+	}
+	mk.hash = make([]byte, 8)
+	for i := uint(0); i < 8; i++ {
+		mk.hash[i] = byte((mk.key >> ((i - 1) * 8)) & 0xff)
+	}
+	return mk.hash
+}
+
+func newMockHashKey(key int64) *mockCacheKey {
+	return &mockCacheKey{
+		key: key,
+	}
+}
+
+type fakePlan struct {
+	a int64
+	s []string
+}
+
+func pickFromBucket(bucket []*list.Element, itB interface{}) (*list.Element, int, bool) {
+	for i, element := range bucket {
+		itemsA := element.Value.(*CacheEntry).PlanValue.(*fakePlan).s
+		itemsB := itB.([]string)
+		flag := true
+		for j := 0; j < len(itemsA); j++ {
+			if itemsA[j] != itemsB[j] {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			return element, i, true
+		}
+	}
+	return nil, -1, false
+}
+
+func TestLRUPCPut(t *testing.T) {
+	pcLRU := NewLRUPlanCache(3)
+	pcLRU.SetChoose(pickFromBucket)
+	require.Equal(t, uint(3), pcLRU.capacity)
+
+	keys := make([]*mockCacheKey, 5)
+	vals := make([]*fakePlan, 5)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}, {"d", "3"}, {"e", "4"}}
+	maxMemDroppedKv := make(map[kvcache.Key]kvcache.Value)
+
+	// test onEvict function
+	pcLRU.SetOnEvict(func(key kvcache.Key, value kvcache.Value) {
+		maxMemDroppedKv[key] = value
+	})
+
+	// one key multi value
+	for i := 0; i < 5; i++ {
+		keys[i] = newMockHashKey(1)
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		pcLRU.Put(keys[i], vals[i], pTypes[i])
+	}
+	require.Equal(t, pcLRU.size, pcLRU.capacity)
+	require.Equal(t, uint(3), pcLRU.size)
+
+	// test for non-existent elements
+	require.Len(t, maxMemDroppedKv, 2)
+	for i := 0; i < 2; i++ {
+		bucket, exist := pcLRU.buckets[string(keys[i].Hash())]
+		require.True(t, exist)
+		for _, element := range bucket {
+			require.NotEqual(t, vals[i], element.Value.(*CacheEntry).PlanValue)
+		}
+		require.Equal(t, vals[i], maxMemDroppedKv[keys[i]])
+	}
+
+	// test for existent elements
+	root := pcLRU.cache.Front()
+	require.NotNil(t, root)
+	for i := 4; i >= 2; i-- {
+		entry, ok := root.Value.(*CacheEntry)
+		require.True(t, ok)
+		require.NotNil(t, entry)
+
+		// test key
+		key := entry.PlanKey
+		require.NotNil(t, key)
+		require.Equal(t, keys[i], key)
+
+		bucket, exist := pcLRU.buckets[string(keys[i].Hash())]
+		require.True(t, exist)
+		element, _, exist := pcLRU.choose(bucket, pTypes[i])
+		require.NotNil(t, element)
+		require.True(t, exist)
+		require.Equal(t, root, element)
+
+		// test value
+		value, ok := entry.PlanValue.(*fakePlan)
+		require.True(t, ok)
+		require.Equal(t, vals[i], value)
+
+		root = root.Next()
+	}
+
+	// test for end of double-linked list
+	require.Nil(t, root)
+}
+
+func TestLRUPCGet(t *testing.T) {
+	lru := NewLRUPlanCache(3)
+	lru.SetChoose(pickFromBucket)
+
+	keys := make([]*mockCacheKey, 5)
+	vals := make([]*fakePlan, 5)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}, {"d", "3"}, {"e", "4"}}
+
+	// 5 bucket
+	for i := 0; i < 5; i++ {
+		keys[i] = newMockHashKey(int64(i))
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		lru.Put(keys[i], vals[i], pTypes[i])
+	}
+
+	// test for non-existent elements
+	for i := 0; i < 2; i++ {
+		value, exists := lru.Get(keys[i], pTypes[i])
+		require.False(t, exists)
+		require.Nil(t, value)
+	}
+
+	for i := 2; i < 5; i++ {
+		value, exists := lru.Get(keys[i], pTypes[i])
+		require.True(t, exists)
+		require.NotNil(t, value)
+		require.Equal(t, vals[i], value)
+		require.Equal(t, uint(3), lru.size)
+		require.Equal(t, uint(3), lru.capacity)
+
+		root := lru.cache.Front()
+		require.NotNil(t, root)
+
+		entry, ok := root.Value.(*CacheEntry)
+		require.True(t, ok)
+		require.Equal(t, keys[i], entry.PlanKey)
+
+		value, ok = entry.PlanValue.(*fakePlan)
+		require.True(t, ok)
+		require.Equal(t, vals[i], value)
+	}
+}
+
+func TestLRUPCGet2(t *testing.T) {
+	lru := NewLRUPlanCache(3)
+	lru.SetChoose(pickFromBucket)
+
+	keys := make([]*mockCacheKey, 5)
+	vals := make([]*fakePlan, 5)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}, {"d", "3"}, {"e", "4"}}
+
+	// 5 bucket
+	for i := 0; i < 5; i++ {
+		keys[i] = newMockHashKey(int64(i % 3))
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		lru.Put(keys[i], vals[i], pTypes[i])
+	}
+
+	// test for non-existent elements
+	for i := 0; i < 2; i++ {
+		value, exists := lru.Get(keys[i], pTypes[i])
+		require.False(t, exists)
+		require.Nil(t, value)
+	}
+
+	for i := 2; i < 5; i++ {
+		value, exists := lru.Get(keys[i], pTypes[i])
+		require.True(t, exists)
+		require.NotNil(t, value)
+		require.Equal(t, vals[i], value)
+		require.Equal(t, uint(3), lru.size)
+		require.Equal(t, uint(3), lru.capacity)
+
+		root := lru.cache.Front()
+		require.NotNil(t, root)
+
+		entry, ok := root.Value.(*CacheEntry)
+		require.True(t, ok)
+		require.Equal(t, keys[i], entry.PlanKey)
+
+		value, ok = entry.PlanValue.(*fakePlan)
+		require.True(t, ok)
+		require.Equal(t, vals[i], value)
+	}
+}
+
+func TestLRUPCDelete(t *testing.T) {
+	lru := NewLRUPlanCache(3)
+	lru.SetChoose(pickFromBucket)
+
+	keys := make([]*mockCacheKey, 3)
+	vals := make([]*fakePlan, 3)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}}
+
+	for i := 0; i < 3; i++ {
+		keys[i] = newMockHashKey(int64(i))
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		lru.Put(keys[i], vals[i], pTypes[i])
+	}
+	require.Equal(t, 3, int(lru.size))
+
+	lru.Delete(keys[1])
+	value, exists := lru.Get(keys[1], pTypes[1])
+	require.False(t, exists)
+	require.Nil(t, value)
+	require.Equal(t, 2, int(lru.size))
+
+	_, exists = lru.Get(keys[0], pTypes[0])
+	require.True(t, exists)
+
+	_, exists = lru.Get(keys[2], pTypes[2])
+	require.True(t, exists)
+}
+
+func TestLRUPCDeleteAll(t *testing.T) {
+	lru := NewLRUPlanCache(3)
+	lru.SetChoose(pickFromBucket)
+
+	keys := make([]*mockCacheKey, 3)
+	vals := make([]*fakePlan, 3)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}}
+
+	for i := 0; i < 3; i++ {
+		keys[i] = newMockHashKey(int64(i))
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		lru.Put(keys[i], vals[i], pTypes[i])
+	}
+	require.Equal(t, 3, int(lru.size))
+
+	lru.DeleteAll()
+
+	for i := 0; i < 3; i++ {
+		value, exists := lru.Get(keys[i], pTypes[i])
+		require.False(t, exists)
+		require.Nil(t, value)
+		require.Equal(t, 0, int(lru.size))
+	}
+}
+
+func TestLRUPCKeys(t *testing.T) {
+	lru := NewLRUPlanCache(5)
+	lru.SetChoose(pickFromBucket)
+
+	keys := make([]*mockCacheKey, 5)
+	vals := make([]*fakePlan, 5)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}, {"d", "3"}, {"e", "4"}}
+
+	for i := 0; i < 5; i++ {
+		keys[i] = newMockHashKey(int64(i))
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		lru.Put(keys[i], vals[i], pTypes[i])
+	}
+
+	ks := lru.Keys()
+	require.Equal(t, 5, len(ks))
+	for i := 0; i < 5; i++ {
+		require.Contains(t, ks, keys[i])
+	}
+}
+
+func TestLRUPCValues(t *testing.T) {
+	lru := NewLRUPlanCache(5)
+	lru.SetChoose(pickFromBucket)
+
+	keys := make([]*mockCacheKey, 5)
+	vals := make([]*fakePlan, 5)
+	pTypes := [][]string{{"a", "0"}, {"b", "1"}, {"c", "2"}, {"d", "3"}, {"e", "4"}}
+
+	for i := 0; i < 5; i++ {
+		keys[i] = newMockHashKey(int64(i))
+		vals[i] = &fakePlan{
+			a: int64(i),
+			s: pTypes[i],
+		}
+		lru.Put(keys[i], vals[i], pTypes[i])
+	}
+
+	values := lru.Values()
+	require.Equal(t, 5, len(values))
+	for i := 0; i < 5; i++ {
+		require.Equal(t, vals[4-i], values[i])
+	}
+}
