0a6d7230eca67b653c628a1410b58b9a3466d59e
Chao Wang <cclcwangchao@hotmail.com>
2022-08-22 11:14:03 +0800

    lint

--- ddl/placement_sql_test.go
+++ ddl/placement_sql_test.go
@@ -18,14 +18,12 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/pingcap/tidb/domain/infosync"
-
-	"github.com/pingcap/tidb/infoschema"
-
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/ddl"
 	"github.com/pingcap/tidb/domain"
+	"github.com/pingcap/tidb/domain/infosync"
 	mysql "github.com/pingcap/tidb/errno"
+	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/testkit"
