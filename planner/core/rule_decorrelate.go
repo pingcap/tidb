ecdf6c9cab3b5c4a0b0f8b444fe6bec2db628253
Arenatlx <314806019@qq.com>
2022-08-24 18:02:21 +0800

    cherry pick #37117 to release-6.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/rule_decorrelate.go
+++ planner/core/rule_decorrelate.go
@@ -169,10 +169,28 @@ func (s *decorrelateSolver) optimize(ctx context.Context, p LogicalPlan, opt *lo
 				// TODO: Actually, it can be optimized. We need to first push the projection down to the selection. And then the APPLY can be decorrelated.
 				goto NoOptimize
 			}
+
+			// step1: substitute the all the schema with new expressions (including correlated column maybe, but it doesn't affect the collation infer inside)
+			// eg: projection: constant("guo") --> column8, once upper layer substitution failed here, the lower layer behind
+			// projection can't supply column8 anymore.
+			//
+			//	upper OP (depend on column8)   --> projection(constant "guo" --> column8)  --> lower layer OP
+			//	          |                                                       ^
+			//	          +-------------------------------------------------------+
+			//
+			//	upper OP (depend on column8)   --> lower layer OP
+			//	          |                             ^
+			//	          +-----------------------------+      // Fail: lower layer can't supply column8 anymore.
+			hasFail := apply.columnSubstituteAll(proj.Schema(), proj.Exprs)
+			if hasFail {
+				goto NoOptimize
+			}
+			// step2: when it can be substituted all, we then just do the de-correlation (apply conditions included).
 			for i, expr := range proj.Exprs {
 				proj.Exprs[i] = expr.Decorrelate(outerPlan.Schema())
 			}
-			apply.columnSubstitute(proj.Schema(), proj.Exprs)
+			apply.decorrelate(outerPlan.Schema())
+
 			innerPlan = proj.children[0]
 			apply.SetChildren(outerPlan, innerPlan)
 			if apply.JoinType != SemiJoin && apply.JoinType != LeftOuterSemiJoin && apply.JoinType != AntiSemiJoin && apply.JoinType != AntiLeftOuterSemiJoin {
