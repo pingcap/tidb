31e253bf4a48f517261dbb0a26cd112f0f4e5b8e
wuxiaoju <wuxiaoju@pingcap.com>
2022-08-28 11:06:33 +0800

    fix

--- executor/adapter.go
+++ executor/adapter.go
@@ -395,17 +395,19 @@ func (a *ExecStmt) Exec(ctx context.Context) (_ sqlexec.RecordSet, err error) {
 		oriScan := sctx.GetSessionVars().DistSQLScanConcurrency()
 		oriIndex := sctx.GetSessionVars().IndexSerialScanConcurrency()
 		oriIso, _ := sctx.GetSessionVars().GetSystemVar(variable.TxnIsolation)
-		autoConcurrency, err1 := sctx.GetSessionVars().GetSessionOrGlobalSystemVar(variable.TiDBAutoBuildStatsConcurrency)
-		terror.Log(err1)
-		if err1 == nil {
+		autoConcurrency, ok1 := sctx.GetSessionVars().GetSystemVar(variable.TiDBAutoBuildStatsConcurrency)
+		if ok1 {
 			terror.Log(sctx.GetSessionVars().SetSystemVar(variable.TiDBBuildStatsConcurrency, autoConcurrency))
+		} else {
+			logutil.BgLogger().Error("fail to get val for ", zap.String("key", variable.TiDBAutoBuildStatsConcurrency))
 		}
-		sVal, err2 := sctx.GetSessionVars().GetSessionOrGlobalSystemVar(variable.TiDBSysProcScanConcurrency)
-		terror.Log(err2)
-		if err2 == nil {
+		sVal, ok2 := sctx.GetSessionVars().GetSystemVar(variable.TiDBSysProcScanConcurrency)
+		if ok2 {
 			concurrency, err3 := strconv.ParseInt(sVal, 10, 64)
 			terror.Log(err3)
 			sctx.GetSessionVars().SetDistSQLScanConcurrency(int(concurrency))
+		} else {
+			logutil.BgLogger().Error("fail to get val for ", zap.String("key", variable.TiDBSysProcScanConcurrency))
 		}
 		sctx.GetSessionVars().SetIndexSerialScanConcurrency(1)
 		terror.Log(sctx.GetSessionVars().SetSystemVar(variable.TxnIsolation, ast.ReadCommitted))
