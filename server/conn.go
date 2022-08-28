9db5b4de43204f3cd9f4bbabe749bc3bc14dfd6d
cbcwestwolf <1004626265@qq.com>
2022-08-28 16:20:43 +0800

    Fix

--- server/conn.go
+++ server/conn.go
@@ -753,17 +753,16 @@ func (cc *clientConn) authSha(ctx context.Context) ([]byte, error) {
 
 // authSM3 implements the sm3_password specific part of the protocol.
 func (cc *clientConn) authSM3(ctx context.Context) ([]byte, error) {
-
 	const (
-		SM3Command       = 1
-		RequestRsaPubKey = 2 // Not supported yet, only TLS is supported as secure channel.
-		FastAuthOk       = 3
-		FastAuthFail     = 4
+		sm3Command       = 1
+		requestRsaPubKey = 2 // Not supported yet, only TLS is supported as secure channel.
+		fastAuthOk       = 3
+		fastAuthFail     = 4
 	)
 
 	// Currently we always send a "FastAuthFail" as the cached part of the protocol isn't implemented yet.
 	// This triggers the client to send the full response.
-	err := cc.writePacket([]byte{0, 0, 0, 0, SM3Command, FastAuthFail})
+	err := cc.writePacket([]byte{0, 0, 0, 0, sm3Command, fastAuthFail})
 	if err != nil {
 		logutil.Logger(ctx).Error("authSM3 packet write failed", zap.Error(err))
 		return nil, err
