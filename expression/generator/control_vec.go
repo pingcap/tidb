cd0020f91c9c5632eab634475281b5efb96336f8
HuaiyuXu <391585975@qq.com>
2022-08-29 12:18:22 +0800

    cherry pick #37417 to release-6.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- expression/generator/control_vec.go
+++ expression/generator/control_vec.go
@@ -230,6 +230,7 @@ var builtinIfNullVec = template.Must(template.New("builtinIfNullVec").Parse(`
 func (b *builtinIfNull{{ .TypeName }}Sig) fallbackEval{{ .TypeName }}(input *chunk.Chunk, result *chunk.Column) error {
 	n := input.NumRows()
 	{{- if .Fixed }}
+	result.Resize{{ .TypeNameInColumn }}(n, false)
 	x := result.{{ .TypeNameInColumn }}s()
 	for i := 0; i < n; i++ {
 		res, isNull, err := b.eval{{ .TypeName }}(input.GetRow(i))
@@ -345,6 +346,7 @@ var builtinIfVec = template.Must(template.New("builtinIfVec").Parse(`
 func (b *builtinIf{{ .TypeName }}Sig) fallbackEval{{ .TypeName }}(input *chunk.Chunk, result *chunk.Column) error {
 	n := input.NumRows()
 	{{- if .Fixed }}
+	result.Resize{{ .TypeNameInColumn }}(n, false)
 	x := result.{{ .TypeNameInColumn }}s()
 	for i := 0; i < n; i++ {
 		res, isNull, err := b.eval{{ .TypeName }}(input.GetRow(i))
