d5b8312cfec8a365c6dda0695c9cef0031affe76
qw4990 <zhangyuanjia@pingcap.com>
2022-09-02 10:44:40 +0800

    fixup

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -1021,7 +1021,7 @@ const (
 	DefTiDBEnableFastReorg                         = false
 	DefTiDBDDLDiskQuota                            = 100 * 1024 * 1024 * 1024 // 100GB
 	DefExecutorConcurrency                         = 5
-	DefTiDBEnableGeneralPlanCache                  = false
+	DefTiDBEnableGeneralPlanCache                  = true
 	DefTiDBGeneralPlanCacheSize                    = 100
 	// MaxDDLReorgBatchSize is exported for testing.
 	MaxDDLReorgBatchSize                     int32  = 10240
