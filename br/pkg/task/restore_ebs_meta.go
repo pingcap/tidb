64319d737aea0cbd2c7823f1c034c7175e1638c5
3pointer <luancheng@pingcap.com>
2022-09-02 14:39:05 +0800

    address comment

--- br/pkg/task/restore_ebs_meta.go
+++ br/pkg/task/restore_ebs_meta.go
@@ -173,25 +173,8 @@ func (h *restoreEBSMetaHelper) preRestore(ctx context.Context) error {
 	}
 	h.metaInfo = metaInfo
 
-	// stop scheduler before recover data
-	log.Info("starting to remove some PD schedulers")
-	restoreFunc, e := h.pdc.RemoveAllPDSchedulers(ctx)
-	if e != nil {
-		return errors.Trace(err)
-	}
-	defer func() {
-		if ctx.Err() != nil {
-			log.Warn("context canceled, doing clean work with background context")
-			ctx = context.Background()
-		}
-		if restoreE := restoreFunc(ctx); restoreE != nil {
-			log.Warn("failed to restore removed schedulers, you may need to restore them manually", zap.Error(restoreE))
-		}
-	}()
-
 	// todo: check whether target cluster is compatible with the backup
 	// but cluster hasn't bootstrapped, we cannot get cluster version from pd now.
-
 	return nil
 }
 
@@ -227,6 +210,22 @@ func (h *restoreEBSMetaHelper) restore() error {
 	if err = h.preRestore(ctx); err != nil {
 		return errors.Trace(err)
 	}
+
+	// stop scheduler before recover data
+	log.Info("starting to remove some PD schedulers")
+	restoreFunc, e := h.pdc.RemoveAllPDSchedulers(ctx)
+	if e != nil {
+		return errors.Trace(err)
+	}
+	defer func() {
+		if ctx.Err() != nil {
+			log.Warn("context canceled, doing clean work with background context")
+			ctx = context.Background()
+		}
+		if restoreE := restoreFunc(ctx); restoreE != nil {
+			log.Warn("failed to restore removed schedulers, you may need to restore them manually", zap.Error(restoreE))
+		}
+	}()
 
 	storeCount := h.metaInfo.GetStoreCount()
 	progress := h.g.StartProgress(ctx, h.cmdName, int64(storeCount), !h.cfg.LogProgress)
