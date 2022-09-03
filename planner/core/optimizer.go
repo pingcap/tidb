7ca3152b710fbc46a045e49ac5911dd21a95c263
AilinKid <314806019@qq.com>
2022-09-03 18:43:49 +0800

    refine code, don't left it affect test plan
    Signed-off-by: AilinKid <314806019@qq.com>

--- planner/core/optimizer.go
+++ planner/core/optimizer.go
@@ -375,9 +375,7 @@ func mergeContinuousSelections(p PhysicalPlan) {
 func postOptimize(sctx sessionctx.Context, flag uint64, plan PhysicalPlan) PhysicalPlan {
 	// some cases from update optimize will require avoiding projection elimination.
 	// see comments ahead of call of DoOptimize in function of buildUpdate().
-	if !(flag&flagEliminateProjection == 0 && sctx.GetSessionVars().StmtCtx.InUpdateStmt) {
-		plan = eliminatePhysicalProjection(plan)
-	}
+	plan = eliminatePhysicalProjection(plan)
 	plan = InjectExtraProjection(plan)
 	mergeContinuousSelections(plan)
 	plan = eliminateUnionScanAndLock(sctx, plan)
