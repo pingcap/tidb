dfe16ef7c9345feb189ec58b3d9fa02b440f9b31
xiongjiwei <xiongjiwei1996@outlook.com>
2022-09-08 18:11:34 +0800

    add json test for dumpling and lightning
    Signed-off-by: xiongjiwei <xiongjiwei1996@outlook.com>

---
+++ dumpling/tests/json_column/data.sql
@@ -0,0 +1,158 @@
+use json_database;
+
+create table t(id int primary key auto_increment,j json,j1 json as (json_extract(j, '$.a')),j2 json as (json_extract(j1, '$.b')),a int as (j ->> '$.a'),b char(255) as (j ->> '$.b'),c char(255) as (j2 ->> '$[0].a'));
+
+insert into t(j)values ('{"a":1, "b": {"a":1}}');
+insert into t(j)values ('{"a":1, "b": [1,2,3]}');
+insert into t(j)values ('[1,2]');
+insert into t(j)values ('true');
+insert into t(j)values ('false');
+insert into t(j)values (null);
+insert into t(j)values ('"string"');
+insert into t(j)values ('1');
+insert into t(j)values ('3.14');
+insert into t(j)values ('{"a":1, "b": {"a":1}, "c": "string"}');
+insert into t(j)values ('{
+  "a": 1,
+  "b": {
+    "correct": "official",
+    "among": "shade",
+    "provide": "state",
+    "rocky": 237984910,
+    "truth": [
+      false,
+      -1791701750,
+      2096731479,
+      true,
+      "atomic",
+      true
+    ],
+    "weather": 1056506954.57938
+  }
+}');
+
+insert into t(j)values ('[
+  -2110666701,
+  true,
+  {
+    "a": true,
+    "b": {
+      "pay": true,
+      "label": true,
+      "wore": -406186071,
+      "smile": true,
+      "exchange": -1014990079.4359617,
+      "hung": 2042967716.35775
+    },
+    "word": -54511405,
+    "tall": -724260188,
+    "light": "mud",
+    "kept": -1796997460.1243253
+  }
+]');
+
+insert into t (j) values ('{
+  "clock": "serious",
+  "neighborhood": {
+    "bright": {
+      "can": "news",
+      "four": "least",
+      "church": "fought",
+      "breakfast": 1559828244,
+      "fairly": "know",
+      "failed": "general"
+    },
+    "package": "little",
+    "cap": false,
+    "wire": -1407286819.3892212,
+    "sail": 238820588,
+    "sound": "refer"
+  },
+  "a": "1",
+  "b": false,
+  "public": "boy",
+  "settlers": 463147044.95204353
+}');
+
+insert into t (j) values ('{
+  "a": [
+    {
+      "if": true,
+      "receive": -1243715977,
+      "account": "prevent",
+      "belt": "like",
+      "so": 1413386810,
+      "clock": "audience"
+    },
+    -698026765,
+    2043745339,
+    796099282,
+    "wait",
+    false
+  ],
+  "depend": -605591161,
+  "organization": -146926736.93140602,
+  "a": "3",
+  "country": false,
+  "close": -10999699.591890812
+}');
+
+insert into t(j) values ('{
+  "seldom": "each",
+  "catch": 1214910538,
+  "b": [
+    2075813989,
+    -937439267,
+    922514137,
+    {
+      "war": 427469005,
+      "women": -1531675093,
+      "win": true,
+      "traffic": -1572011015.863554,
+      "who": true,
+      "establish": false
+    },
+    -649939334.6679158,
+    2136689538.1209803
+  ],
+  "push": "pot",
+  "a": 0,
+  "dance": "piano"
+}');
+
+insert into t (j)
+values ('[
+  1718710205.3502188,
+  {
+    "wing": -130388407.01911354,
+    "people": true,
+    "mile": 334993717.4304991
+  },
+  false
+]');
+
+insert into t (j) values ('[true, true, true]');
+insert into t (j) values ('[false, false, false]');
+
+insert into t (j) values ('{
+  "b": [
+    {
+      "a": 1,
+      "b": 2
+    },
+    {
+      "a": 3,
+      "b": 4
+    }
+  ]
+}');
+
+insert into t (j) values ('{
+  "badly": "due",
+  "arrangement": [
+    false,
+    850510874.2187676,
+    425258001.5952878
+  ],
+  "a": 1
+}');
---
+++ dumpling/tests/json_column/run.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Copyright 2020 PingCAP, Inc. Licensed under Apache-2.0.
+
+set -eu
+cur=$(cd `dirname $0`; pwd)
+
+DB_NAME="json_database"
+TABLE_NAME="t"
+
+# drop database on tidb
+export DUMPLING_TEST_PORT=4000
+run_sql "drop database if exists \`$DB_NAME\`;"
+
+# drop database on mysql
+export DUMPLING_TEST_PORT=3306
+run_sql "drop database if exists \`$DB_NAME\`;"
+
+# build data on mysql
+run_sql "create database \`$DB_NAME\` DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;"
+run_sql "create table \`$DB_NAME\`.\`$TABLE_NAME\`(id int primary key auto_increment,j json,j1 json as (json_extract(j, '$.a')),j2 json as (json_extract(j1, '$.b')),a int as (j ->> '$.a'),b char(255) as (j ->> '$.b'),c char(255) as (j2 ->> '\$[0].a'));"
+
+# insert 100 records
+run_sql_file "$cur/data.sql"
+
+# make sure the estimated count is accurate
+run_sql "analyze table \`$DB_NAME\`.\`$TABLE_NAME\`"
+
+# dumping
+export DUMPLING_TEST_DATABASE=$DB_NAME
+run_dumpling --rows 10 --loglevel debug
+
+cat "$cur/conf/lightning.toml"
+# use lightning import data to tidb
+run_lightning $cur/conf/lightning.toml
+
+# check mysql and tidb data
+check_sync_diff $cur/conf/diff_config.toml
+
+# drop database on tidb
+export DUMPLING_TEST_PORT=4000
+run_sql "drop database if exists \`$DB_NAME\`;"
+
+cat "$cur/conf/lightning.toml"
+# use lightning import data to tidb
+run_lightning $cur/conf/lightning.toml
+
+# check mysql and tidb data
+check_sync_diff $cur/conf/diff_config.toml
