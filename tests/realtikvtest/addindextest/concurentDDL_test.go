fa8fc5b208f71b78a738c5ef4dd6d502877d8e0c
Benjamin2037 <bear.c@pingcap.com>
2022-09-14 14:21:04 +0800

    edit import sequence

--- tests/realtikvtest/addindextest/concurentDDL_test.go
+++ tests/realtikvtest/addindextest/concurentDDL_test.go
@@ -15,8 +15,9 @@
 package addindextest
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func initConcurrentDDLTest(t *testing.T, colIIDs [][]int, colJIDs [][]int, tType testType) *suiteContext {
