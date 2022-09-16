5cd4a7af09c6b8b7febf69fa30580c5b5600a0a3
tangenta <tangenta@126.com>
2022-09-16 16:00:51 +0800

    remove unnecessary delay

--- ddl/reorg.go
+++ ddl/reorg.go
@@ -254,11 +254,9 @@ func (w *worker) runReorgJob(rh *reorgHandler, reorgInfo *reorgInfo, tblInfo *mo
 		}
 
 		updateBackfillProgress(w, reorgInfo, tblInfo, 0)
-		if job.ReorgMeta.ReorgTp != model.ReorgTypeLitMerge || reorgInfo.mergingTmpIdx {
-			if err1 := rh.RemoveDDLReorgHandle(job, reorgInfo.elements); err1 != nil {
-				logutil.BgLogger().Warn("[ddl] run reorg job done, removeDDLReorgHandle failed", zap.Error(err1))
-				return errors.Trace(err1)
-			}
+		if err1 := rh.RemoveDDLReorgHandle(job, reorgInfo.elements); err1 != nil {
+			logutil.BgLogger().Warn("[ddl] run reorg job done, removeDDLReorgHandle failed", zap.Error(err1))
+			return errors.Trace(err1)
 		}
 	case <-w.ctx.Done():
 		logutil.BgLogger().Info("[ddl] run reorg job quit")
