b5ed6740df266b909fcae78df782fce41bd29808
hehechen <awd123456sss@gmail.com>
2022-09-19 15:31:02 +0800

    ddl: fix collect TiFlash sync status bug (#37923)
    close pingcap/tidb#37862

--- store/helper/helper.go
+++ store/helper/helper.go
@@ -1214,7 +1214,7 @@ func ComputeTiFlashStatus(reader *bufio.Reader, regionReplica *map[int64]int) er
 			continue
 		}
 		realN++
-		r, err := strconv.ParseInt(s, 10, 32)
+		r, err := strconv.ParseInt(s, 10, 64)
 		if err != nil {
 			return errors.Trace(err)
 		}
