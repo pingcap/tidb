8199dab83d49dec4fb201f20c7aa1f8d282fe763
tiancaiamao <tiancaiamao@gmail.com>
2022-09-21 18:19:38 +0800

    resolve conflict

--- executor/table_reader.go
+++ executor/table_reader.go
@@ -180,22 +180,7 @@ func (e *TableReaderExecutor) Open(ctx context.Context) error {
 	// Treat temporary table as dummy table, avoid sending distsql request to TiKV.
 	// Calculate the kv ranges here, UnionScan rely on this kv ranges.
 	// cached table and temporary table are similar
-<<<<<<< HEAD
 	if e.table.Meta() != nil && e.table.Meta().TempTableType != model.TempTableNone {
-=======
-	if e.dummy {
-		if e.desc && len(secondPartRanges) != 0 {
-			// TiKV support reverse scan and the `resultHandler` process the range order.
-			// While in UnionScan, it doesn't use reverse scan and reverse the final result rows manually.
-			// So things are differ, we need to reverse the kv range here.
-			// TODO: If we refactor UnionScan to use reverse scan, update the code here.
-			// [9734095886065816708 9734095886065816709] | [1 3] [65535 9734095886065816707] => before the following change
-			// [1 3] [65535 9734095886065816707] | [9734095886065816708 9734095886065816709] => ranges part reverse here
-			// [1 3  65535 9734095886065816707 9734095886065816708 9734095886065816709] => scan (normal order) in UnionScan
-			// [9734095886065816709 9734095886065816708 9734095886065816707 65535 3  1] => rows reverse in UnionScan
-			firstPartRanges, secondPartRanges = secondPartRanges, firstPartRanges
-		}
->>>>>>> eb002463f... *: fix bug that UnionScan can't keep order caused wrong result (#33218)
 		kvReq, err := e.buildKVReq(ctx, firstPartRanges)
 		if err != nil {
 			return err
