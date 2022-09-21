68915eb7ca7e1805274455904a98ad89e6f34cbf
cfzjywxk <lsswxrxr@163.com>
2022-09-21 20:45:03 +0800

    cherry pick #38044 to release-5.4
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- planner/core/integration_test.go
+++ planner/core/integration_test.go
@@ -5403,3 +5403,24 @@ func (s *testIntegrationSuite) TestIssue33175(c *C) {
 		tk.MustExec("drop table t")
 	}
 }
+
+// TestExplainAnalyzeDMLCommit covers the issue #37373.
+func TestExplainAnalyzeDMLCommit(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists t")
+	tk.MustExec("create table t (c1 int key, c2 int);")
+	tk.MustExec("insert into t values (1, 1)")
+
+	err := failpoint.Enable("github.com/pingcap/tidb/session/mockSleepBeforeTxnCommit", "return(500)")
+	require.NoError(t, err)
+	defer func() {
+		_ = failpoint.Disable("github.com/pingcap/tidb/session/mockSleepBeforeTxnCommit")
+	}()
+	// The commit is paused by the failpoint, after the fix the explain statement
+	// execution should proceed after the commit finishes.
+	_, err = tk.Exec("explain analyze delete from t;")
+	require.NoError(t, err)
+	tk.MustQuery("select * from t").Check(testkit.Rows())
+}
