21d3d0a7649ebcf8634c263df5b4ae6bf2f25b44
guo-shaoge <shaoge1994@163.com>
2022-09-21 20:25:04 +0800

    fix config fmt
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- config/config.go
+++ config/config.go
@@ -276,7 +276,7 @@ type Config struct {
 	Plugin                     Plugin     `toml:"plugin" json:"plugin"`
 	MaxServerConnections       uint32     `toml:"max-server-connections" json:"max-server-connections"`
 	RunDDL                     bool       `toml:"run-ddl" json:"run-ddl"`
-	DisaggregatedTiFlash       bool       `toml:"disaggregated-tiflash" json: "disaggregated-tiflash"`
+	DisaggregatedTiFlash       bool       `toml:"disaggregated-tiflash" json:"disaggregated-tiflash"`
 }
 
 // UpdateTempStoragePath is to update the `TempStoragePath` if port/statusPort was changed
