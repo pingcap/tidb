13377ab82792ed0fa52086feb84be133608ee704
Zak Zhao <57036248+joccau@users.noreply.github.com>
2022-09-22 11:15:02 +0800

    cherry pick #38029 to release-6.3
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- telemetry/data_feature_usage.go
+++ telemetry/data_feature_usage.go
@@ -353,7 +353,7 @@ func getGlobalKillUsageInfo() bool {
 }
 
 func getLogBackupUsageInfo(ctx sessionctx.Context) bool {
-	return utils.CheckLogBackupEnabled(ctx) && utils.CheckLogBackupTaskExist()
+	return utils.IsLogBackupInUse(ctx)
 }
 
 func getCostModelVer2UsageInfo(ctx sessionctx.Context) bool {
