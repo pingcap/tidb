9c78bf503b6e0b8737692362d4b3e01aeda9380e
qw4990 <zhangyuanjia@pingcap.com>
2022-09-28 22:04:18 +0800

    fixup

--- planner/core/plan_cost.go
+++ planner/core/plan_cost.go
@@ -1174,6 +1174,9 @@ func (p *PhysicalTopN) GetPlanCost(taskType property.TaskType, option *PlanCostO
 	if p.planCostInit && !hasCostFlag(costFlag, CostFlagRecalculate) {
 		return p.planCost, nil
 	}
+	if p.ctx.GetSessionVars().CostModelVersion == modelVer2 {
+		return p.getPlanCostVer2(taskType, option)
+	}
 	childCost, err := p.children[0].GetPlanCost(taskType, option)
 	if err != nil {
 		return 0, err
