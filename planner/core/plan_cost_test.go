9787dd5e60bf3733bcdcc692344bb5663817bbf2
qw4990 <zhangyuanjia@pingcap.com>
2022-09-28 15:16:22 +0800

    fixup

--- planner/core/plan_cost_test.go
+++ planner/core/plan_cost_test.go
@@ -15,7 +15,6 @@
 package core_test
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -122,7 +121,6 @@ func TestScanOnSmallTable(t *testing.T) {
 	rs := tk.MustQuery("explain select * from t").Rows()
 	useTiKVScan := false
 	for _, r := range rs {
-		fmt.Println(">>> ", r)
 		op := r[0].(string)
 		task := r[2].(string)
 		if strings.Contains(op, "Scan") && strings.Contains(task, "tikv") {
