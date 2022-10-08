191aa1b5766217b35651107ed72d3268a5bc1d96
lance6716 <lance6716@gmail.com>
2022-10-08 19:34:34 +0800

    add another test
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- br/tests/lightning_character_sets/run.sh
+++ br/tests/lightning_character_sets/run.sh
@@ -76,7 +76,7 @@ run_sql 'SELECT sum(`唯一键`) AS s FROM charsets.mixed'
 check_contains 's: 5291'
 
 # test about unsupported charset in UTF-8 encoding dump files
-
+# test local backend
 run_lightning --config "tests/$TEST_NAME/greek.toml" -d "tests/$TEST_NAME/greek" 2>&1 | grep -q "Unknown character set: 'greek'"
 run_sql 'DROP DATABASE IF EXISTS charsets;'
 run_sql 'CREATE DATABASE charsets;'
@@ -84,4 +84,8 @@ run_sql 'CREATE TABLE charsets.greek (c VARCHAR(20) PRIMARY KEY);'
 run_lightning --config "tests/$TEST_NAME/greek.toml" -d "tests/$TEST_NAME/greek"
 run_sql "SELECT count(*) FROM charsets.greek WHERE c = 'α';"
 check_contains 'count(*): 1'
-# TODO: test tidb backend
+# test tidb backend
+run_sql 'TRUNCATE TABLE charsets.greek;'
+run_lightning --config "tests/$TEST_NAME/greek.toml" -d "tests/$TEST_NAME/greek" --backend tidb
+run_sql "SELECT count(*) FROM charsets.greek WHERE c = 'α';"
+check_contains 'count(*): 1'
