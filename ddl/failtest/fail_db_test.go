4b119dd65f1d81b3c9aae6de41991090d9c84669
Mattias Jonsson <mjonss@users.noreply.github.com>
2022-10-10 14:47:24 +0200

    Update fail_db_test.go
    According to review suggestion

--- ddl/failtest/fail_db_test.go
+++ ddl/failtest/fail_db_test.go
@@ -493,8 +493,6 @@ func TestModifyColumn(t *testing.T) {
 	tk.MustExec("admin check table t")
 
 	// Test unsupported statements.
-	tk.MustExec("create table t1(a int) partition by hash (a) partitions 2")
-	tk.MustExec("alter table t1 modify column a mediumint")
 	tk.MustExec("create table t2(id int, a int, b int generated always as (abs(a)) virtual, c int generated always as (a+1) stored)")
 	tk.MustGetErrMsg("alter table t2 modify column b mediumint", "[ddl:8200]Unsupported modify column: newCol IsGenerated false, oldCol IsGenerated true")
 	tk.MustGetErrMsg("alter table t2 modify column c mediumint", "[ddl:8200]Unsupported modify column: newCol IsGenerated false, oldCol IsGenerated true")
