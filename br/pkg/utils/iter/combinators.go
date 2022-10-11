a15832435b5259cec194d9682bf7eba93d5e781e
Yu Juncen <yujuncen@pingcap.com>
2022-10-11 11:46:47 +0800

    reorg file structure
    Signed-off-by: Yu Juncen <yujuncen@pingcap.com>

--- br/pkg/utils/iter/combinators.go
+++ br/pkg/utils/iter/combinators.go
@@ -1,64 +1,14 @@
+// Copyright 2022 PingCAP, Inc. Licensed under Apache-2.0.
+
 package iter
 
 import (
 	"context"
 
 	"github.com/pingcap/tidb/br/pkg/utils"
-	"golang.org/x/sync/errgroup"
 )
 
-type chunkMappingCfg struct {
-	chunkSize uint
-	quota     *utils.WorkerPool
-}
-
-type chunkMapping[T, R any] struct {
-	chunkMappingCfg
-	inner  TryNextor[T]
-	mapper func(context.Context, T) (R, error)
-
-	buffer fromSlice[R]
-}
-
-func (m *chunkMapping[T, R]) fillChunk(ctx context.Context) IterResult[fromSlice[R]] {
-	eg, cx := errgroup.WithContext(ctx)
-	s := CollectMany(ctx, m.inner, m.chunkSize)
-	if s.FinishedOrError() {
-		return DoneBy[fromSlice[R]](s)
-	}
-	r := make([]R, len(s.Item))
-	for i := 0; i < len(s.Item); i++ {
-		i := i
-		m.quota.ApplyOnErrorGroup(eg, func() error {
-			var err error
-			r[i], err = m.mapper(cx, s.Item[i])
-			return err
-		})
-	}
-	if err := eg.Wait(); err != nil {
-		return Throw[fromSlice[R]](err)
-	}
-	if len(r) > 0 {
-		return Emit(fromSlice[R](r))
-	}
-	return Done[fromSlice[R]]()
-}
-
-func (m *chunkMapping[T, R]) TryNext(ctx context.Context) IterResult[R] {
-	r := m.buffer.TryNext(ctx)
-	if !r.FinishedOrError() {
-		return Emit(r.Item)
-	}
-
-	r2 := m.fillChunk(ctx)
-	if !r2.FinishedOrError() {
-		m.buffer = r2.Item
-		return m.TryNext(ctx)
-	}
-
-	return DoneBy[R](r2)
-}
-
+// TransformConfig is the config for the combinator "transform".
 type TransformConfig func(*chunkMappingCfg)
 
 func WithConcurrency(n uint) TransformConfig {
@@ -97,24 +47,8 @@ func Transform[T, R any](it TryNextor[T], with func(context.Context, T) (R, erro
 	return r
 }
 
-type filter[T any] struct {
-	inner       TryNextor[T]
-	filterOutIf func(T) bool
-}
-
-func (f filter[T]) TryNext(ctx context.Context) IterResult[T] {
-	r := f.inner.TryNext(ctx)
-	if r.Err != nil || r.Finished {
-		return r
-	}
-
-	if f.filterOutIf(r.Item) {
-		return f.TryNext(ctx)
-	}
-
-	return r
-}
-
+// FilterOut returns an iterator that yields all elements the original iterator
+// generated and DOESN'T satisfies the predicate.
 func FilterOut[T any](it TryNextor[T], f func(T) bool) TryNextor[T] {
 	return filter[T]{
 		inner:       it,
@@ -122,20 +56,7 @@ func FilterOut[T any](it TryNextor[T], f func(T) bool) TryNextor[T] {
 	}
 }
 
-type take[T any] struct {
-	n     uint
-	inner TryNextor[T]
-}
-
-func (t *take[T]) TryNext(ctx context.Context) IterResult[T] {
-	if t.n == 0 {
-		return Done[T]()
-	}
-
-	t.n--
-	return t.inner.TryNext(ctx)
-}
-
+// TakeFirst takes the first n elements of the iterator.
 func TakeFirst[T any](inner TryNextor[T], n uint) TryNextor[T] {
 	return &take[T]{
 		n:     n,
@@ -143,45 +64,8 @@ func TakeFirst[T any](inner TryNextor[T], n uint) TryNextor[T] {
 	}
 }
 
-type join[T any] struct {
-	inner TryNextor[TryNextor[T]]
-
-	current TryNextor[T]
-}
-
-type pureMap[T, R any] struct {
-	inner TryNextor[T]
-
-	mapper func(T) R
-}
-
-func (p pureMap[T, R]) TryNext(ctx context.Context) IterResult[R] {
-	r := p.inner.TryNext(ctx)
-
-	if r.FinishedOrError() {
-		return DoneBy[R](r)
-	}
-	return Emit(p.mapper(r.Item))
-}
-
-func (j *join[T]) TryNext(ctx context.Context) IterResult[T] {
-	r := j.current.TryNext(ctx)
-	if r.Err != nil {
-		j.inner = empty[TryNextor[T]]{}
-		return r
-	}
-	if !r.Finished {
-		return r
-	}
-
-	nr := j.inner.TryNext(ctx)
-	if nr.FinishedOrError() {
-		return DoneBy[T](nr)
-	}
-	j.current = nr.Item
-	return j.TryNext(ctx)
-}
-
+// FlapMap applies the mapper over every elements the origin iterator generates,
+// then flatten them.
 func FlatMap[T, R any](it TryNextor[T], mapper func(T) TryNextor[R]) TryNextor[R] {
 	return &join[R]{
 		inner: pureMap[T, TryNextor[R]]{
@@ -192,6 +76,7 @@ func FlatMap[T, R any](it TryNextor[T], mapper func(T) TryNextor[R]) TryNextor[R
 	}
 }
 
+// Map applies the mapper over every elements the origin iterator yields.
 func Map[T, R any](it TryNextor[T], mapper func(T) R) TryNextor[R] {
 	return pureMap[T, R]{
 		inner:  it,
@@ -199,6 +84,8 @@ func Map[T, R any](it TryNextor[T], mapper func(T) R) TryNextor[R] {
 	}
 }
 
+// ConcatAll concatenates all elements yields by the iterators.
+// In another word, it 'chains' all the input iterators.
 func ConcatAll[T any](items ...TryNextor[T]) TryNextor[T] {
 	return &join[T]{
 		inner:   FromSlice(items),
