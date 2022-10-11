e2df44da24473edbb0dc1bbcc142c366a8291866
joccau <zak.zhao@pingcap.com>
2022-10-11 17:41:20 +0800

    retain 30min TTL safePoint when resume/stop task
    Signed-off-by: joccau <zak.zhao@pingcap.com>

--- br/pkg/task/stream.go
+++ br/pkg/task/stream.go
@@ -635,7 +635,7 @@ func RunStreamStop(
 	if err := streamMgr.setGCSafePoint(ctx,
 		utils.BRServiceSafePoint{
 			ID:       buildPauseSafePointName(ti.Info.Name),
-			TTL:      0,
+			TTL:      utils.DefaultStreamStartSafePointTTL,
 			BackupTS: 0,
 		},
 	); err != nil {
@@ -762,7 +762,7 @@ func RunStreamResume(
 	if err := streamMgr.setGCSafePoint(ctx,
 		utils.BRServiceSafePoint{
 			ID:       buildPauseSafePointName(ti.Info.Name),
-			TTL:      0,
+			TTL:      utils.DefaultStreamStartSafePointTTL,
 			BackupTS: globalCheckPointTS,
 		},
 	); err != nil {
