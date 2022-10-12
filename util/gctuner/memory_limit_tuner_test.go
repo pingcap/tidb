62a2e519bf91e3be0b59289608e3a4d75ddaf7d6
Shenghui Wu <793703860@qq.com>
2022-10-12 17:51:51 +0800

    util: fix data race for memory limit tuner test (#38419)

--- util/gctuner/memory_limit_tuner_test.go
+++ util/gctuner/memory_limit_tuner_test.go
@@ -63,7 +63,7 @@ func TestGlobalMemoryTuner(t *testing.T) {
 		time.Sleep(1 * time.Second) // If test.count > 1, wait tuning finished.
 		require.True(t, GlobalMemoryLimitTuner.isTuning.Load())
 		require.False(t, GlobalMemoryLimitTuner.waitingReset.Load())
-		require.Equal(t, GlobalMemoryLimitTuner.nextGCTriggeredByMemoryLimit, false)
+		require.Equal(t, GlobalMemoryLimitTuner.nextGCTriggeredByMemoryLimit.Load(), false)
 	}()
 
 	allocator := &mockAllocator{}
