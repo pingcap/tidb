7f4dbdb1c7aa0fce57ba5fc5a4543a9e4836ffbb
Jason Mo <mohangjie1995@gmail.com>
2022-10-21 17:27:19 +0800

    executor: fix getting the more result when select * from TIKV_REGION_STATUS where table_id=xx

--- executor/infoschema_reader.go
+++ executor/infoschema_reader.go
@@ -1694,6 +1694,7 @@ func keyColumnUsageInTable(schema *model.DBInfo, table *model.TableInfo) [][]typ
 }
 
 func (e *memtableRetriever) setDataForTiKVRegionStatus(ctx sessionctx.Context) (err error) {
+	var extractorTableIDs []int64
 	tikvStore, ok := ctx.GetStore().(helper.Storage)
 	if !ok {
 		return errors.New("Information about TiKV region status can be gotten only when the storage is TiKV")
@@ -1708,10 +1709,11 @@ func (e *memtableRetriever) setDataForTiKVRegionStatus(ctx sessionctx.Context) (
 	if e.extractor != nil {
 		extractor, ok := e.extractor.(*plannercore.TiKVRegionStatusExtractor)
 		if ok && len(extractor.GetTablesID()) > 0 {
-			for _, tableID := range extractor.GetTablesID() {
+			extractorTableIDs = extractor.GetTablesID()
+			for _, tableID := range extractorTableIDs {
 				regionsInfo, err := e.getRegionsInfoForTable(tikvHelper, is, tableID)
 				if err != nil {
-					return err
+					continue
 				}
 				allRegionsInfo = allRegionsInfo.Merge(regionsInfo)
 			}
@@ -1726,12 +1728,20 @@ func (e *memtableRetriever) setDataForTiKVRegionStatus(ctx sessionctx.Context) (
 	}
 	tableInfos := tikvHelper.GetRegionsTableInfo(allRegionsInfo, is.AllSchemas())
 	for i := range allRegionsInfo.Regions {
-		tableList := tableInfos[allRegionsInfo.Regions[i].ID]
-		if len(tableList) == 0 {
+		regionTableList := tableInfos[allRegionsInfo.Regions[i].ID]
+		if len(regionTableList) == 0 {
 			e.setNewTiKVRegionStatusCol(&allRegionsInfo.Regions[i], nil)
 		}
-		for j := range tableList {
-			e.setNewTiKVRegionStatusCol(&allRegionsInfo.Regions[i], &tableList[j])
+		for j := range regionTableList {
+			if len(extractorTableIDs) == 0 {
+				e.setNewTiKVRegionStatusCol(&allRegionsInfo.Regions[i], &regionTableList[j])
+			}
+			for _, tableID := range extractorTableIDs {
+				if tableID == regionTableList[j].Table.ID {
+					e.setNewTiKVRegionStatusCol(&allRegionsInfo.Regions[i], &regionTableList[j])
+					break
+				}
+			}
 		}
 	}
 	return nil
