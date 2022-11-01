854bf18661c32a0bb28e8342fe0f8168763913af
yibin <huyibin@pingcap.com>
2022-11-01 11:16:58 +0800

    Fix compile issue
    Signed-off-by: yibin <huyibin@pingcap.com>

--- planner/core/optimizer.go
+++ planner/core/optimizer.go
@@ -496,7 +496,7 @@ func prunePhysicalColumnsInternal(sctx sessionctx.Context, plan PhysicalPlan) er
 		leftCols, rightCols := x.extractUsedCols(schemaColumns)
 		matchPattern := false
 		for i := 0; i <= 1; i++ {
-			matchPattern = false  // HashJoin <- ExchangeReceiver <- ExchangeSender
+			matchPattern = false // HashJoin <- ExchangeReceiver <- ExchangeSender
 			var exchangeSender *PhysicalExchangeSender
 			if receiver, ok := x.children[i].(*PhysicalExchangeReceiver); ok {
 				exchangeSender, matchPattern = receiver.children[0].(*PhysicalExchangeSender)
