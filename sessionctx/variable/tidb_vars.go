874d75d19cb0bd1d21ff4babd892f37649c6befb
wuxiaoju <wuxiaoju@pingcap.com>
2022-11-02 20:31:29 +0800

    enable stats load by default

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -1009,7 +1009,7 @@ const (
 	DefTiDBTableCacheLease                         = 3 // 3s
 	DefTiDBPersistAnalyzeOptions                   = true
 	DefTiDBEnableColumnTracking                    = false
-	DefTiDBStatsLoadSyncWait                       = 0
+	DefTiDBStatsLoadSyncWait                       = 100
 	DefTiDBStatsLoadPseudoTimeout                  = true
 	DefSysdateIsNow                                = false
 	DefTiDBEnableMutationChecker                   = false
