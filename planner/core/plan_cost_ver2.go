1373e345df14348845f92ce75bf7e10b7db5ed75
qw4990 <zhangyuanjia@pingcap.com>
2022-11-07 11:31:54 +0800

    fixup

--- planner/core/plan_cost_ver2.go
+++ planner/core/plan_cost_ver2.go
@@ -16,6 +16,7 @@ package core
 
 import (
 	"fmt"
+	"github.com/pingcap/tidb/util/paging"
 	"math"
 	"strings"
 
@@ -256,6 +257,13 @@ func (p *PhysicalIndexLookUpReader) getPlanCostVer2(taskType property.TaskType, 
 	doubleReadCost := sumCostVer2(doubleReadCPUCost, doubleReadRequestCost)
 
 	p.planCostVer2 = sumCostVer2(indexSideCost, divCostVer2(sumCostVer2(tableSideCost, doubleReadCost), doubleReadConcurrency))
+
+	if p.ctx.GetSessionVars().EnablePaging && p.expectedCnt > 0 && p.expectedCnt <= paging.Threshold {
+		// if the expectCnt is below the paging threshold, using paging API
+		p.Paging = true                                   // TODO: move this operation from cost model to physical optimization
+		p.planCostVer2 = mulCostVer2(p.planCostVer2, 0.6) // TODO: remove this empirical value 0.6
+	}
+
 	p.planCostInit = true
 	return p.planCostVer2.label(p), nil
 }
