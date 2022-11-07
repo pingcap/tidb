64c7b0197c624a2168f7748c0fcebe0c684436ab
qw4990 <zhangyuanjia@pingcap.com>
2022-11-07 11:53:18 +0800

    fixup

--- planner/core/plan_cost_ver2.go
+++ planner/core/plan_cost_ver2.go
@@ -16,7 +16,6 @@ package core
 
 import (
 	"fmt"
-	"github.com/pingcap/tidb/util/paging"
 	"math"
 	"strings"
 
@@ -27,6 +26,7 @@ import (
 	"github.com/pingcap/tidb/planner/property"
 	"github.com/pingcap/tidb/planner/util"
 	"github.com/pingcap/tidb/sessionctx/variable"
+	"github.com/pingcap/tidb/util/paging"
 	"github.com/pingcap/tipb/go-tipb"
 )
 
