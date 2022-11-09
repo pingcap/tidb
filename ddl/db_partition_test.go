1e2ac197b2495394ebaf66465a11a4f00dfeca8e
Mattias Jonsson <mjonss@gmail.com>
2022-11-08 16:23:50 -0800

    Fix panic if partition column was renamed (#38932)

--- ddl/db_partition_test.go
+++ ddl/db_partition_test.go
@@ -4672,8 +4672,20 @@ func TestAlterModifyColumnOnPartitionedTableRename(t *testing.T) {
 	schemaName := "modColPartRename"
 	tk.MustExec("create database " + schemaName)
 	tk.MustExec("use " + schemaName)
+	tk.MustExec(`create table t (a int, b char) partition by range (a) (partition p0 values less than (10))`)
+	tk.MustContainErrMsg(`alter table t change a c int`, "[planner:1054]Unknown column 'a' in 'expression'")
+	tk.MustExec(`drop table t`)
 	tk.MustExec(`create table t (a char, b char) partition by range columns (a) (partition p0 values less than ('z'))`)
-	tk.MustExec(`alter table t change a c char`)
+	tk.MustContainErrMsg(`alter table t change a c char`, "[ddl:8200]New column does not match partition definitions: [ddl:1567]partition column name cannot be found")
+	tk.MustExec(`drop table t`)
+	tk.MustExec(`create table t (a int, b char) partition by list (a) (partition p0 values in (10))`)
+	tk.MustContainErrMsg(`alter table t change a c int`, "[planner:1054]Unknown column 'a' in 'expression'")
+	tk.MustExec(`drop table t`)
+	tk.MustExec(`create table t (a char, b char) partition by list columns (a) (partition p0 values in ('z'))`)
+	tk.MustContainErrMsg(`alter table t change a c char`, "[ddl:8200]New column does not match partition definitions: [ddl:1567]partition column name cannot be found")
+	tk.MustExec(`drop table t`)
+	tk.MustExec(`create table t (a int, b char) partition by hash (a) partitions 3`)
+	tk.MustContainErrMsg(`alter table t change a c int`, "[planner:1054]Unknown column 'a' in 'expression'")
 }
 
 func TestAlterModifyColumnOnPartitionedTableFail(t *testing.T) {
