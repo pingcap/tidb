8c8d6806f7d000243a290993e9962bd6c0e6a2b2
wjhuang2016 <huangwenjun1997@gmail.com>
2022-11-09 18:28:50 +0800

    fix
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -1195,33 +1195,13 @@ func (d *ddl) SwitchConcurrentDDL(toConcurrentDDL bool) error {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
-
-	var err error
-	defer func() {
-		if err != nil && !toConcurrentDDL {
-			sql := fmt.Sprintf("UPDATE HIGH_PRIORITY %[1]s.%[2]s SET VARIABLE_VALUE = %[4]d WHERE VARIABLE_NAME = '%[3]s'",
-				mysql.SystemDB, mysql.GlobalVariablesTable, variable.TiDBEnableMDL, 0)
-			sess, err := d.sessPool.get()
-			if err != nil {
-				logutil.BgLogger().Warn("[ddl] get session failed", zap.Error(err))
-				return
-			}
-			defer d.sessPool.put(sess)
-			se := newSession(sess)
-			_, err = se.execute(ctx, sql, "disableMDL")
-			if err != nil {
-				logutil.BgLogger().Warn("[ddl] disable MDL failed", zap.Error(err))
-			}
-		}
-	}()
-
-	defer cancel()
 	d.waiting.Store(true)
 	defer d.waiting.Store(false)
 	if err := d.wait4Switch(ctx); err != nil {
 		return err
 	}
 
+	var err error
 	if toConcurrentDDL {
 		err = d.MoveJobFromQueue2Table(false)
 	} else {
@@ -1236,13 +1216,31 @@ func (d *ddl) SwitchConcurrentDDL(toConcurrentDDL bool) error {
 
 // SwitchMDL enables MDL or disable DDL.
 func (d *ddl) SwitchMDL(enable bool) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	// Disable MDL for test.
+	if enable && !variable.DefTiDBEnableConcurrentDDL {
+		sql := fmt.Sprintf("UPDATE HIGH_PRIORITY %[1]s.%[2]s SET VARIABLE_VALUE = %[4]d WHERE VARIABLE_NAME = '%[3]s'",
+			mysql.SystemDB, mysql.GlobalVariablesTable, variable.TiDBEnableMDL, 0)
+		sess, err := d.sessPool.get()
+		if err != nil {
+			logutil.BgLogger().Warn("[ddl] get session failed", zap.Error(err))
+			return nil
+		}
+		defer d.sessPool.put(sess)
+		se := newSession(sess)
+		_, err = se.execute(ctx, sql, "disableMDL")
+		if err != nil {
+			logutil.BgLogger().Warn("[ddl] disable MDL failed", zap.Error(err))
+		}
+		return nil
+	}
+
 	isEnableBefore := variable.EnableMDL.Load()
 	if isEnableBefore == enable {
 		return nil
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-	defer cancel()
 
 	// Check if there is any DDL running.
 	// This check can not cover every corner cases, so users need to guarantee that there is no DDL running by themselves.
