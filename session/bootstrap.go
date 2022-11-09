e0190ebe34b2df40683550374fc2acfbbc38a02f
yisaer <disxiaofei@163.com>
2022-11-09 11:07:20 +0800

    fix comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- session/bootstrap.go
+++ session/bootstrap.go
@@ -654,7 +654,7 @@ const (
 	version99 = 99
 	// version100 converts server-memory-quota to a sysvar
 	version100 = 100
-	// version101 add a table
+	// version101 add mysql.plan_replayer_status table
 	version101 = 101
 )
 
