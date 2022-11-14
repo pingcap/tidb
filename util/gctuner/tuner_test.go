c0d59d3dfa3386372e354f9e59f302284eb8a475
Weizhen Wang <wangweizhen@pingcap.com>
2022-11-14 12:57:14 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- util/gctuner/tuner_test.go
+++ util/gctuner/tuner_test.go
@@ -37,7 +37,7 @@ func TestTuner(t *testing.T) {
 	runtime.GC()
 	for i := 0; i < 100; i++ {
 		runtime.GC()
-		require.Equal(t, MaxGCPercent, tn.getGCPercent())
+		require.Equal(t, maxGCPercent.Load(), tn.getGCPercent())
 	}
 
 	// 1/4 threshold
@@ -62,7 +62,7 @@ func TestTuner(t *testing.T) {
 	runtime.GC()
 	for i := 0; i < 100; i++ {
 		runtime.GC()
-		require.Equal(t, MinGCPercent, tn.getGCPercent())
+		require.Equal(t, minGCPercent.Load(), tn.getGCPercent())
 	}
 
 	// out of threshold
@@ -70,7 +70,7 @@ func TestTuner(t *testing.T) {
 	runtime.GC()
 	for i := 0; i < 100; i++ {
 		runtime.GC()
-		require.Equal(t, MinGCPercent, tn.getGCPercent())
+		require.Equal(t, minGCPercent.Load(), tn.getGCPercent())
 	}
 }
 
@@ -81,13 +81,13 @@ func TestCalcGCPercent(t *testing.T) {
 	require.Equal(t, defaultGCPercent, calcGCPercent(0, 1))
 	require.Equal(t, defaultGCPercent, calcGCPercent(1, 0))
 
-	require.Equal(t, MaxGCPercent, calcGCPercent(1, 3*gb))
-	require.Equal(t, MaxGCPercent, calcGCPercent(gb/10, 4*gb))
-	require.Equal(t, MaxGCPercent, calcGCPercent(gb/2, 4*gb))
+	require.Equal(t, maxGCPercent.Load(), calcGCPercent(1, 3*gb))
+	require.Equal(t, maxGCPercent.Load(), calcGCPercent(gb/10, 4*gb))
+	require.Equal(t, maxGCPercent.Load(), calcGCPercent(gb/2, 4*gb))
 	require.Equal(t, uint32(300), calcGCPercent(1*gb, 4*gb))
 	require.Equal(t, uint32(166), calcGCPercent(1.5*gb, 4*gb))
 	require.Equal(t, uint32(100), calcGCPercent(2*gb, 4*gb))
 	require.Equal(t, uint32(100), calcGCPercent(3*gb, 4*gb))
-	require.Equal(t, MinGCPercent, calcGCPercent(4*gb, 4*gb))
-	require.Equal(t, MinGCPercent, calcGCPercent(5*gb, 4*gb))
+	require.Equal(t, minGCPercent.Load(), calcGCPercent(4*gb, 4*gb))
+	require.Equal(t, minGCPercent.Load(), calcGCPercent(5*gb, 4*gb))
 }
