ea2c0bc53c050997b103d7eceb7dbf22fea1b622
qw4990 <zhangyuanjia@pingcap.com>
2022-11-15 10:40:24 +0800

    add docs

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -1079,7 +1079,7 @@ const (
 	DefTiDBForeignKeyChecks                      = false
 	DefTiDBAnalyzePartitionConcurrency           = 1
 	DefTiDBOptRangeMaxSize                       = 64 * int64(size.MB) // 64 MB
-	DefTiDBCostModelVer                          = 2
+	DefTiDBCostModelVer                          = 1
 	DefTiDBServerMemoryLimitSessMinSize          = 128 << 20
 	DefTiDBMergePartitionStatsConcurrency        = 1
 	DefTiDBServerMemoryLimitGCTrigger            = 0.7
