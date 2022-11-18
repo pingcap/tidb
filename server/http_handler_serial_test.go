86da639fa8341a68be37c193da79c860984ceb80
aidan liu <aidan.liu@pingcap.com>
2022-11-18 14:02:52 +0800

    modified:   server/http_handler_serial_test.go

--- server/http_handler_serial_test.go
+++ server/http_handler_serial_test.go
@@ -316,8 +316,8 @@ func TestTiFlashReplica(t *testing.T) {
 	body, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
 	require.NoError(t, resp.Body.Close())
-
 	require.Equal(t, "[schema:1146]Table which ID = 184 does not exist.", string(body))
+
 	tbl, err := ts.domain.InfoSchema().TableByName(model.NewCIStr("tidb"), model.NewCIStr("test"))
 	require.NoError(t, err)
 	req := fmt.Sprintf(`{"id":%d,"region_count":3,"flash_region_count":3}`, tbl.Meta().ID)
