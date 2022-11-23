ea53416ee578fb00933ae734dd4c0851e937228e
xuhuaiyu <391585975@qq.com>
2022-11-23 17:13:37 +0800

    executor: introduce build worker

--- executor/benchmark_test.go
+++ executor/benchmark_test.go
@@ -906,13 +906,13 @@ func prepare4HashJoin(testCase *hashJoinTestCase, innerExec, outerExec Executor)
 		joinSchema.Append(cols1...)
 	}
 
-	joinKeys := make([]*expression.Column, 0, len(testCase.keyIdx))
+	joinKeysColIdx := make([]int, 0, len(testCase.keyIdx))
 	for _, keyIdx := range testCase.keyIdx {
-		joinKeys = append(joinKeys, cols0[keyIdx])
+		joinKeysColIdx = append(joinKeysColIdx, keyIdx)
 	}
-	probeKeys := make([]*expression.Column, 0, len(testCase.keyIdx))
+	probeKeysColIdx := make([]int, 0, len(testCase.keyIdx))
 	for _, keyIdx := range testCase.keyIdx {
-		probeKeys = append(probeKeys, cols1[keyIdx])
+		probeKeysColIdx = append(probeKeysColIdx, keyIdx)
 	}
 	e := &HashJoinExec{
 		baseExecutor: newBaseExecutor(testCase.ctx, joinSchema, 5, innerExec, outerExec),
@@ -921,19 +921,21 @@ func prepare4HashJoin(testCase *hashJoinTestCase, innerExec, outerExec Executor)
 			isOuterJoin:     false,
 			useOuterToBuild: testCase.useOuterToBuild,
 			concurrency:     uint(testCase.concurrency),
+			probeTypes:      retTypes(outerExec),
+			buildTypes:      retTypes(innerExec),
 		},
 		probeSideTupleFetcher: &probeSideTupleFetcher{
 			probeSideExec: outerExec,
 		},
-		probeWorkers:      make([]probeWorker, testCase.concurrency),
-		buildKeys:         joinKeys,
-		probeKeys:         probeKeys,
-		buildSideExec:     innerExec,
-		buildSideEstCount: float64(testCase.rows),
+		probeWorkers: make([]*probeWorker, testCase.concurrency),
+		buildWorker: &buildWorker{
+			buildKeyColIdx: joinKeysColIdx,
+			buildSideExec:  innerExec,
+		},
 	}
 
 	childrenUsedSchema := markChildrenUsedCols(e.Schema(), e.children[0].Schema(), e.children[1].Schema())
-	defaultValues := make([]types.Datum, e.buildSideExec.Schema().Len())
+	defaultValues := make([]types.Datum, e.buildWorker.buildSideExec.Schema().Len())
 	lhsTypes, rhsTypes := retTypes(innerExec), retTypes(outerExec)
 	for i := uint(0); i < e.concurrency; i++ {
 		e.probeWorkers[i].workerID = i
@@ -941,6 +943,7 @@ func prepare4HashJoin(testCase *hashJoinTestCase, innerExec, outerExec Executor)
 		e.probeWorkers[i].hashJoinCtx = e.hashJoinCtx
 		e.probeWorkers[i].joiner = newJoiner(testCase.ctx, e.joinType, true, defaultValues,
 			nil, lhsTypes, rhsTypes, childrenUsedSchema, false)
+		e.probeWorkers[i].probeKeyColIdx = probeKeysColIdx
 	}
 	memLimit := int64(-1)
 	if testCase.disk {
