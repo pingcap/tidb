09a085ecffe8b3eaa32964b26d80a0fd5286142e
crazycs520 <crazycs520@gmail.com>
2022-11-28 17:14:46 +0800

    add check

--- br/tests/br_foreign_key/run.sh
+++ br/tests/br_foreign_key/run.sh
@@ -22,7 +22,7 @@ run_sql "set @@global.foreign_key_checks=1;"
 run_sql "set @@foreign_key_checks=1;"
 run_sql "create schema $DB;"
 run_sql "create table $DB.t1 (id int key);"
-run_sql "create table $DB.t2 (id int key, a int, b int, foreign key fk_1 (a) references t1(id) ON UPDATE SET NULL, foreign key fk_2 (b) references t1(id) ON DELETE CASCADE);"
+run_sql "create table $DB.t2 (id int key, a int, b int, foreign key fk_1 (a) references t1(id) ON UPDATE SET NULL ON DELETE SET NULL, foreign key fk_2 (b) references t1(id) ON DELETE CASCADE ON UPDATE CASCADE);"
 run_sql "insert into $DB.t1 values (1), (2), (3);"
 run_sql "insert into $DB.t2 values (1, 1, 1), (2, 2, 2), (3, 3, 3);"
 run_sql "update $DB.t1 set id=id+10 where id in (1, 3);"
@@ -38,7 +38,10 @@ run_br restore db --db $DB -s "local://$TEST_DIR/$DB" --pd $PD_ADDR
 
 set -x
 
-run_sql "select count(*) from $DB.cache_1;"
-check_contains 'count(*): 3'
+run_sql "select count(*) from $DB.t1;"
+check_contains 'count(*): 2'
+
+run_sql "select count(*) from $DB.t2;"
+check_contains 'count(*): 2'
 
 run_sql "drop schema $DB"
