02241503eee7a5834fffe32333562dc5caaeb793
HuaiyuXu <391585975@qq.com>
2022-11-28 17:02:01 +0800

    This is an automated cherry-pick of #39368
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- sessionctx/variable/session.go
+++ sessionctx/variable/session.go
@@ -579,6 +579,11 @@ type SessionVars struct {
 	// StmtCtx holds variables for current executing statement.
 	StmtCtx *stmtctx.StatementContext
 
+	// RefCountOfStmtCtx indicates the reference count of StmtCtx. When the
+	// StmtCtx is accessed by other sessions, e.g. oom-alarm-handler/expensive-query-handler, add one first.
+	// Note: this variable should be accessed and updated by atomic operations.
+	RefCountOfStmtCtx stmtctx.ReferenceCount
+
 	// AllowAggPushDown can be set to false to forbid aggregation push down.
 	AllowAggPushDown bool
 
@@ -1006,9 +1011,23 @@ type SessionVars struct {
 
 // InitStatementContext initializes a StatementContext, the object is reused to reduce allocation.
 func (s *SessionVars) InitStatementContext() *stmtctx.StatementContext {
+<<<<<<< HEAD
 	s.cached.curr = (s.cached.curr + 1) % 2
 	s.cached.data[s.cached.curr] = stmtctx.StatementContext{}
 	return &s.cached.data[s.cached.curr]
+=======
+	sc := &s.cachedStmtCtx[0]
+	if sc == s.StmtCtx {
+		sc = &s.cachedStmtCtx[1]
+	}
+	if s.RefCountOfStmtCtx.TryFreeze() {
+		*sc = stmtctx.StatementContext{}
+		s.RefCountOfStmtCtx.UnFreeze()
+	} else {
+		sc = &stmtctx.StatementContext{}
+	}
+	return sc
+>>>>>>> f8a6bde954 (*: add a reference count for StmtCtx (#39368))
 }
 
 // AllocMPPTaskID allocates task id for mpp tasks. It will reset the task id if the query's
