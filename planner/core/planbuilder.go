b26251d5daf5757a734d00b354aa21cb2d5947cb
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2022-12-01 22:48:08 +0800

    commit

--- planner/core/planbuilder.go
+++ planner/core/planbuilder.go
@@ -1079,8 +1079,9 @@ func (b *PlanBuilder) buildCreateBindPlanFromPlanDigest(v *ast.CreateBindingStmt
 		Db:           utilparser.GetDefaultDB(originNode, bindableStmt.Schema),
 		Charset:      bindableStmt.Charset,
 		Collation:    bindableStmt.Collation,
-		SQLDigest:    sqlDigestWithDB.String(),
 		Source:       bindinfo.History,
+		SQLDigest:    sqlDigestWithDB.String(),
+		PlanDigest:   v.PlanDigest,
 	}
 	b.visitInfo = appendVisitInfo(b.visitInfo, mysql.SuperPriv, "", "", "", nil)
 	return p, nil
