365ceb7cb7d5c032f21c688e5c161f5465db58cf
Weizhen Wang <wangweizhen@pingcap.com>
2022-12-06 11:27:11 +0800

    *: create resource manage to schedule by static
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ resourcemanage/scheduler/gradient2_scheduler.go
@@ -0,0 +1,76 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduler
+
+import (
+	"time"
+
+	"github.com/pingcap/tidb/resourcemanage/util"
+	"github.com/pingcap/tidb/util/mathutil"
+)
+
+const (
+	defaultSmoothing      float64 = 0.2
+	defaultMinConcurrency int     = 2
+
+	defaultOverloadConcurrencyDelta int = 2 // minimum concurrency = user setting concurrency - delta
+)
+
+// Gradient2Scheduler is a scheduler that uses the gradient of the queue length
+type Gradient2Scheduler struct {
+	smoothing      float64
+	estimatedLimit int16
+}
+
+// NewGradient2Scheduler is to create a new Gradient2Scheduler
+func NewGradient2Scheduler() *Gradient2Scheduler {
+	return &Gradient2Scheduler{
+		smoothing:      defaultSmoothing,
+		estimatedLimit: 1,
+	}
+}
+
+// Tune is to tune the concurrency of the component
+func (b *Gradient2Scheduler) Tune(c util.Component, p util.GorotinuePool) Command {
+	newLimit := b.tune(c, p)
+	if newLimit > float64(p.Running()) {
+		return Overclock
+	} else if newLimit < float64(p.Running()) {
+		return Downclock
+	}
+	return Hold
+}
+
+func (b *Gradient2Scheduler) tune(_ util.Component, p util.GorotinuePool) float64 {
+	if time.Since(p.LastTunerTs()) < minCPUSchedulerInterval {
+		return float64(p.Running())
+	}
+
+	if p.InFlight() < int64(p.Cap())/2 {
+		return float64(p.Running())
+	}
+
+	if (p.LongRTT() / float64(p.ShortRTT())) > 2 {
+		p.UpdateLongRTT(func(old float64) float64 {
+			return old * 0.9
+		})
+	}
+
+	gradient := mathutil.Max(0.5, mathutil.Min(1.0, p.LongRTT()/float64(p.ShortRTT())))
+	newLimit := float64(p.Running())*gradient + float64(p.GetQueueSize())
+	newLimit = float64(p.Running())*(1-b.smoothing) + newLimit*b.smoothing
+	newLimit = mathutil.Max(float64(defaultMinConcurrency), mathutil.Min(float64(defaultOverloadConcurrencyDelta+p.Cap()), newLimit))
+	return newLimit
+}
---
+++ resourcemanage/scheduler/gradient2_scheduler_test.go
@@ -0,0 +1,115 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduler
+
+import (
+	"testing"
+	"time"
+
+	"github.com/pingcap/tidb/resourcemanage/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGradient2Scheduler(t *testing.T) {
+	scheduler := NewGradient2Scheduler()
+	pool := util.NewFakeGPool(10)
+	rms := NewFakeResourceManage()
+	rms.RegisterPool(pool)
+	rms.Register(scheduler)
+	testcases := []struct {
+		Name      string
+		InFlight  int64
+		Capa      int
+		LongRTT   float64
+		ShortRTT  uint64
+		Queuesize int64
+		Running   int
+		Delta     time.Duration
+		Expected  Command
+	}{
+
+		{
+			Name:     "init",
+			Expected: Hold,
+		},
+		{
+			Name:     "p.InFlight() < int64(p.Cap())/2 is hold",
+			Capa:     100,
+			LongRTT:  20,
+			ShortRTT: 30,
+			Delta:    -10 * time.Second,
+			Expected: Hold,
+		},
+		{
+			Name:     "p.InFlight() > int64(p.Cap())/2 is hold",
+			InFlight: 2,
+			Capa:     100,
+			LongRTT:  20,
+			ShortRTT: 30,
+			Running:  2,
+			Delta:    -10 * time.Second,
+			Expected: Hold,
+		},
+		{
+			Name:      "basic Downclock and LongRTT / ShortRTT < 2",
+			InFlight:  132,
+			Capa:      100,
+			LongRTT:   30,
+			ShortRTT:  70,
+			Queuesize: 30,
+			Running:   102,
+			Delta:     -10 * time.Second,
+			Expected:  Downclock,
+		},
+		{
+			Name:      "near max concurrency，LongRTT / ShortRTT > 2",
+			InFlight:  132,
+			Capa:      100,
+			LongRTT:   70,
+			ShortRTT:  30,
+			Queuesize: 30,
+			Running:   102,
+			Delta:     -10 * time.Second,
+			Expected:  Hold,
+		},
+		{
+			Name:      "basic overlock, ShortRTT > LongRTT and LongRTT / ShortRTT < 2",
+			InFlight:  60,
+			Capa:      100,
+			LongRTT:   30,
+			ShortRTT:  80,
+			Queuesize: 50,
+			Running:   10,
+			Delta:     -10 * time.Second,
+			Expected:  Overclock,
+		},
+		{
+			Name:      "basic overlock, ShortRTT < LongRTT and LongRTT / ShortRTT > 2",
+			InFlight:  60,
+			Capa:      100,
+			LongRTT:   70,
+			ShortRTT:  30,
+			Queuesize: 40,
+			Running:   20,
+			Delta:     -10 * time.Second,
+			Expected:  Overclock,
+		},
+	}
+	for _, tc := range testcases {
+		pool.OnSample(0, tc.InFlight, 0, 0, tc.Capa, tc.LongRTT, tc.ShortRTT, tc.Queuesize, tc.Running)
+		pool.ImportLastTunerTs(time.Now().Add(tc.Delta))
+		require.Equal(t, tc.Expected, rms.Next(), tc.Name)
+	}
+}
