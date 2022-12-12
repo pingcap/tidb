708d1ec4c29100f58ee82263d180994c6cdfbef5
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2022-12-12 15:56:36 +0800

    Update tables_test.go

--- infoschema/tables_test.go
+++ infoschema/tables_test.go
@@ -1678,7 +1678,7 @@ func TestAddFieldsForBinding(t *testing.T) {
 	tk.MustExec("create table t(a int, key(a))")
 	tk.MustExec("select /*+ ignore_index(t, a)*/ * from t where a = 1")
 	planDigest := "4e3159169cc63c14b139a4e7d72eae1759875c9a9581f94bb2079aae961189cb"
-	rows := tk.MustQuery(fmt.Sprintf("select stmt_type, prepared, sample_user, schema_name, query_sample_text, charset, collation, plan_hint "+
+	rows := tk.MustQuery(fmt.Sprintf("select stmt_type, prepared, sample_user, schema_name, query_sample_text, charset, collation, plan_hint, digest_text "+
 		"from information_schema.cluster_statements_summary where plan_digest = '%s'", planDigest)).Rows()
 
 	require.Equal(t, rows[0][0], "Select")
@@ -1689,5 +1689,6 @@ func TestAddFieldsForBinding(t *testing.T) {
 	require.Equal(t, rows[0][5], "utf8mb4")
 	require.Equal(t, rows[0][6], "utf8mb4_bin")
 	require.Equal(t, rows[0][7], "use_index(@`sel_1` `test`.`t` ), ignore_index(`t` `a`)")
+	require.Equal(t, rows[0][8], "select * from `t` where `a` = ?")
 	fmt.Println(planDigest)
 }
