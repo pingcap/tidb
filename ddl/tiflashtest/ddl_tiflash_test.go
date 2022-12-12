7596439c863bf3888c3e2573ad52b3809cd2cf20
Weizhen Wang <wangweizhen@pingcap.com>
2022-12-12 14:34:04 +0800

    *: stable tiflash test in ddl
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- ddl/tiflashtest/ddl_tiflash_test.go
+++ ddl/tiflashtest/ddl_tiflash_test.go
@@ -16,7 +16,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSES/QL-LICENSE file.
 
-package ddl_test
+package tiflashtest
 
 import (
 	"context"
