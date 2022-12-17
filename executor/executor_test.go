36aafc9cdfa024bf833a11f9439263daa83aa0e4
Weizhen Wang <wangweizhen@pingcap.com>
2022-12-17 20:57:19 +0800

    executor: close recordset again
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -4623,13 +4623,10 @@ func TestUnion2(t *testing.T) {
 	terr = errors.Cause(err).(*terror.Error)
 	require.Equal(t, errors.ErrCode(mysql.ErrWrongUsage), terr.Code())
 
-	_, err = tk.Exec("(select a from t order by a) union all select a from t limit 1 union all select a from t limit 1")
-	require.Truef(t, terror.ErrorEqual(err, plannercore.ErrWrongUsage), "err %v", err)
+	tk.MustGetDBError("(select a from t order by a) union all select a from t limit 1 union all select a from t limit 1", plannercore.ErrWrongUsage)
 
-	_, err = tk.Exec("(select a from t limit 1) union all select a from t limit 1")
-	require.NoError(t, err)
-	_, err = tk.Exec("(select a from t order by a) union all select a from t order by a")
-	require.NoError(t, err)
+	tk.MustExec("(select a from t limit 1) union all select a from t limit 1")
+	tk.MustExec("(select a from t order by a) union all select a from t order by a")
 
 	tk.MustExec("drop table if exists t")
 	tk.MustExec("create table t(a int)")
@@ -4700,8 +4697,8 @@ func TestUnion2(t *testing.T) {
 	tk.MustExec("insert into t2 values(3,'c'),(4,'d'),(5,'f'),(6,'e')")
 	tk.MustExec("analyze table t1")
 	tk.MustExec("analyze table t2")
-	_, err = tk.Exec("(select a,b from t1 limit 2) union all (select a,b from t2 order by a limit 1) order by t1.b")
-	require.Equal(t, "[planner:1250]Table 't1' from one of the SELECTs cannot be used in global ORDER clause", err.Error())
+	tk.MustGetErrMsg("(select a,b from t1 limit 2) union all (select a,b from t2 order by a limit 1) order by t1.b",
+		"[planner:1250]Table 't1' from one of the SELECTs cannot be used in global ORDER clause")
 
 	// #issue 9900
 	tk.MustExec("drop table if exists t")
