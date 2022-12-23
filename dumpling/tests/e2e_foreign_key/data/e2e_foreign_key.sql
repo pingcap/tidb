a6abf1b63f853593da1c6c1459b8e193d9dac23c
crazycs520 <crazycs520@gmail.com>
2022-12-23 15:39:57 +0800

    dumpling: add foreign key e2e test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

---
+++ dumpling/tests/e2e_foreign_key/data/e2e_foreign_key.sql
@@ -0,0 +1,6 @@
+create table parent (id int key);
+create table child  (id int key, pid int, constraint fk_1 foreign key (pid) references parent(id));
+insert into parent values (1),(2),(3),(4),(5),(6),(7),(8),(9),(10);
+insert into child values (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10);
+set foreign_key_checks=0;
+insert into child values (100,100);
---
+++ dumpling/tests/e2e_foreign_key/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright 2020 PingCAP, Inc. Licensed under Apache-2.0.
+
+set -eu
+cur=$(cd `dirname $0`; pwd)
+
+DB_NAME="e2e_foreign_key"
+
+# drop database on tidb
+export DUMPLING_TEST_PORT=4000
+run_sql "drop database if exists $DB_NAME;"
+
+# drop database on mysql
+export DUMPLING_TEST_PORT=3306
+run_sql "drop database if exists $DB_NAME;"
+
+# build data on mysql
+run_sql "create database $DB_NAME DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;"
+run_sql_file "$DUMPLING_BASE_NAME/data/e2e_foreign_key.sql"
+
+# dumping
+export DUMPLING_TEST_DATABASE=$DB_NAME
+run_dumpling
+
+cat "$cur/conf/lightning.toml"
+# use lightning import data to tidb
+run_lightning $cur/conf/lightning.toml
+
+# check mysql and tidb data
+check_sync_diff $cur/conf/diff_config.toml
