3e2ca84d0c20c34e5de718269686b5eb849f7cb0
Mattias Jonsson <mjonss@gmail.com>
2022-12-26 09:54:13 +0000

    Removed cleaning of tidb_ddl_reorg when ddl is completed (CI testing)

--- ddl/reorg.go
+++ ddl/reorg.go
@@ -255,10 +255,13 @@ func (w *worker) runReorgJob(rh *reorgHandler, reorgInfo *reorgInfo, tblInfo *mo
 		}
 
 		updateBackfillProgress(w, reorgInfo, tblInfo, 0)
-		if err1 := rh.RemoveDDLReorgHandle(job, reorgInfo.elements); err1 != nil {
-			logutil.BgLogger().Warn("[ddl] run reorg job done, removeDDLReorgHandle failed", zap.Error(err1))
-			return errors.Trace(err1)
-		}
+		/*
+			if err1 := rh.RemoveDDLReorgHandle(job, reorgInfo.elements); err1 != nil {
+				logutil.BgLogger().Warn("[ddl] run reorg job done, removeDDLReorgHandle failed", zap.Error(err1))
+				return errors.Trace(err1)
+			}
+
+		*/
 	case <-w.ctx.Done():
 		logutil.BgLogger().Info("[ddl] run reorg job quit")
 		d.removeReorgCtx(job)
