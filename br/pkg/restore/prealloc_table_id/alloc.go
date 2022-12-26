4a21cb8f25cc20ecc63fc2cf05c05525d359cbc5
hillium <yujuncen@pingcap.com>
2022-12-26 18:54:12 +0800

    added test
    Signed-off-by: hillium <yujuncen@pingcap.com>

--- br/pkg/restore/prealloc_table_id/alloc.go
+++ br/pkg/restore/prealloc_table_id/alloc.go
@@ -7,6 +7,7 @@ import (
 	"math"
 
 	"github.com/pingcap/tidb/br/pkg/metautil"
+	"github.com/pingcap/tidb/parser/model"
 )
 
 const (
@@ -93,4 +94,18 @@ func (p *PreallocIDs) Alloc(m Allocator) error {
 // Prealloced checks whether a table ID has been successfully allocated.
 func (p *PreallocIDs) Prealloced(tid int64) bool {
 	return p.allocedFrom <= tid && tid < p.end
+}
+
+func (p *PreallocIDs) PreallocedFor(ti *model.TableInfo) bool {
+	if !p.Prealloced(ti.ID) {
+		return false
+	}
+	if ti.Partition != nil && ti.Partition.Definitions != nil {
+		for _, part := range ti.Partition.Definitions {
+			if !p.Prealloced(part.ID) {
+				return false
+			}
+		}
+	}
+	return true
 }
