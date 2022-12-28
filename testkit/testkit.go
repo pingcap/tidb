7908ee807d3a6a8933f64fe41040761e952c3191
Mattias Jonsson <mjonss@gmail.com>
2022-12-28 14:11:07 +0000

    Linting

--- testkit/testkit.go
+++ testkit/testkit.go
@@ -543,13 +543,13 @@ func DebugDumpOnTimeout(wg *sync.WaitGroup, c chan struct{}, d time.Duration) {
 	select {
 	case <-time.After(d):
 		log.Print("Injected timeout, dumping all goroutines:")
-		pprof.Lookup("goroutine").WriteTo(os.Stdout, 2)
+		_ = pprof.Lookup("goroutine").WriteTo(os.Stdout, 2)
 		log.Print("dumping all stack traces led to possible block:")
-		pprof.Lookup("block").WriteTo(os.Stdout, 2)
+		_ = pprof.Lookup("block").WriteTo(os.Stdout, 2)
 		log.Print("dumping all stack traces holding mutexes:")
-		pprof.Lookup("mutex").WriteTo(os.Stdout, 2)
+		_ = pprof.Lookup("mutex").WriteTo(os.Stdout, 2)
 		log.Print("dumping all stack traces led to creation of new OS threads:")
-		pprof.Lookup("threadcreate").WriteTo(os.Stdout, 2)
+		_ = pprof.Lookup("threadcreate").WriteTo(os.Stdout, 2)
 		panic("Injected timeout")
 	case <-c:
 		// Test finished
