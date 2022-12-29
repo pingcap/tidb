9ace6451a6b1c624c76a162c03aa4574f3e2d101
tangenta <tangenta@126.com>
2022-12-29 15:13:09 +0800

    fix linter

--- ddl/index.go
+++ ddl/index.go
@@ -872,7 +872,7 @@ func doReorgWorkForCreateIndex(w *worker, d *ddlCtx, t *meta.Meta, job *model.Jo
 		}
 		indexInfo.BackfillState = model.BackfillStateInapplicable // Prevent double-write on this index.
 		ver, err = updateVersionAndTableInfo(d, t, job, tbl.Meta(), true)
-		return true, ver, nil
+		return true, ver, err
 	default:
 		return false, 0, dbterror.ErrInvalidDDLState.GenWithStackByArgs("backfill", indexInfo.BackfillState)
 	}
