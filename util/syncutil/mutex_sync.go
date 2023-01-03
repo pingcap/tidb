68b57cb64bad81a8e857be7bb41bf494b6fea02f
Weizhen Wang <wangweizhen@pingcap.com>
2023-01-03 23:00:29 +0800

    use go-deadlock to find deadlock
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ util/syncutil/mutex_sync.go
@@ -0,0 +1,40 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build !deadlock
+
+package syncutil
+
+import "sync"
+
+// A Mutex is a mutual exclusion lock.
+type Mutex struct {
+	sync.Mutex
+}
+
+// AssertHeld may panic if the mutex is not locked (but it is not required to
+// do so). Functions which require that their callers hold a particular lock
+// may use this to enforce this requirement more directly than relying on the
+// race detector.
+//
+// Note that we do not require the lock to be held by any particular thread,
+// just that some thread holds the lock. This is both more efficient and allows
+// for rare cases where a mutex is locked in one thread and used in another.
+func (m *Mutex) AssertHeld() {
+}
+
+// An RWMutex is a reader/writer mutual exclusion lock.
+type RWMutex struct {
+	sync.RWMutex
+}
