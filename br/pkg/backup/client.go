112c804bdb0da9d0f617758ddd24f5826a85f35e
nolouch <nolouch@gmail.com>
2023-01-06 17:14:04 +0800

    allow br backup
    Signed-off-by: nolouch <nolouch@gmail.com>

--- br/pkg/backup/client.go
+++ br/pkg/backup/client.go
@@ -680,10 +680,6 @@ func skipUnsupportedDDLJob(job *model.Job) bool {
 		model.ActionAlterTableAttributes,
 		model.ActionAlterTablePartitionAttributes:
 		return true
-	case model.ActionCreateResourceGroup,
-		model.ActionAlterResourceGroup,
-		model.ActionDropResourceGroup:
-		return true
 	default:
 		return false
 	}
