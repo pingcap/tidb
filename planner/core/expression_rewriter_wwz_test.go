9b163a99650c2ce454241deea28e1e166602d95c
Weizhen Wang <wangweizhen@pingcap.com>
2023-01-10 16:41:32 +0800

    update nogo
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ planner/core/expression_rewriter_wwz_test.go
@@ -0,0 +1,16 @@
+package core_test
+
+import (
+	"testing"
+
+	"github.com/pingcap/tidb/testkit"
+)
+
+func TestFuck(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists test")
+	tk.MustExec("create table test(id varchar(32))")
+	tk.MustQuery("select table_name from information_schema.tables where lower(table_name) = 'TEST';").Check(testkit.Rows("test"))
+}
