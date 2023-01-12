dd45dea93ceab890d815691b76477b243fec82aa
Tong Zhigao <tongzhigao@pingcap.com>
2023-01-12 21:16:58 +0800

    fix ut

--- planner/core/optimizer_test.go
+++ planner/core/optimizer_test.go
@@ -19,7 +19,6 @@ import (
 	"testing"
 
 	"github.com/pingcap/tidb/expression"
-	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/planner/property"
