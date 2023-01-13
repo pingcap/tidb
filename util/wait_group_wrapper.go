943ae03413050293332e0abdb6a0a9c26d02e8e6
yisaer <disxiaofei@163.com>
2023-01-13 11:56:07 +0800

    support

--- util/wait_group_wrapper.go
+++ util/wait_group_wrapper.go
@@ -16,7 +16,88 @@ package util
 
 import (
 	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/pingcap/tidb/util/logutil"
+	"go.uber.org/zap"
 )
+
+// WaitGroupEnhancedWrapper wrapper wg
+type WaitGroupEnhancedWrapper struct {
+	sync.WaitGroup
+	exited          *atomic.Bool
+	source          string
+	registerProcess sync.Map
+}
+
+// NewWaitGroupEnhancedWrapper returns WaitGroupEnhancedWrapper
+func NewWaitGroupEnhancedWrapper(source string, exited *atomic.Bool) *WaitGroupEnhancedWrapper {
+	wgew := &WaitGroupEnhancedWrapper{
+		exited:          exited,
+		source:          source,
+		registerProcess: sync.Map{},
+	}
+	return wgew
+}
+
+func (w *WaitGroupEnhancedWrapper) checkUnExitedProcess() {
+	logutil.BgLogger().Info("waitGroupWrapper ready to check unexited process", zap.String("source", w.source))
+	ticker := time.NewTimer(10 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			if !w.exited.Load() {
+				continue
+			}
+			unexitedProcess := make([]string, 0)
+			w.registerProcess.Range(func(key, value any) bool {
+				unexitedProcess = append(unexitedProcess, key.(string))
+				return true
+			})
+			if len(unexitedProcess) > 0 {
+				logutil.BgLogger().Warn("background process unexited while received exited signal",
+					zap.Strings("process", unexitedProcess),
+					zap.String("source", w.source))
+			} else {
+				logutil.BgLogger().Info("waitGroupWrapper finish checking unexited process", zap.String("source", w.source))
+				return
+			}
+		}
+	}
+}
+
+func (w *WaitGroupEnhancedWrapper) Run(exec func(), label string) {
+	w.onStart(label)
+	w.Add(1)
+	go func() {
+		defer w.onExit(label)
+		exec()
+	}()
+}
+
+func (w *WaitGroupEnhancedWrapper) onStart(label string) {
+	_, ok := w.registerProcess.Load(label)
+	if ok {
+		logutil.BgLogger().Panic("WaitGroupEnhancedWrapper received duplicated source process",
+			zap.String("source", w.source),
+			zap.String("label", label))
+	}
+	w.registerProcess.Store(label, struct{}{})
+	w.Add(1)
+	logutil.BgLogger().Info("background process started",
+		zap.String("source", w.source),
+		zap.String("process", label))
+}
+
+func (w *WaitGroupEnhancedWrapper) onExit(label string) {
+	w.registerProcess.Delete(label)
+	w.Done()
+	logutil.BgLogger().Info("background process exited",
+		zap.String("source", w.source),
+		zap.String("process", label))
+}
 
 // WaitGroupWrapper is a wrapper for sync.WaitGroup
 type WaitGroupWrapper struct {
