8924c3f647064c3f9994cecfaa5f739393cbfe05
Weizhen Wang <wangweizhen@pingcap.com>
2023-01-14 14:43:49 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- util/grunning/disabled.go
+++ util/grunning/disabled.go
@@ -1,12 +1,16 @@
-// Copyright 2022 The Cockroach Authors.
+// Copyright 2022 PingCAP, Inc.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // See grunning.Supported() for an explanation behind this build tag.
 //
