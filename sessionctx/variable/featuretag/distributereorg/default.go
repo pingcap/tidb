edf840ac25cd2a78b9b0059be7f7dfad52f46e26
Weizhen Wang <wangweizhen@pingcap.com>
2023-01-17 10:49:55 +0800

    add distributereorg feature flag
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- sessionctx/variable/featuretag/distributereorg/default.go
+++ sessionctx/variable/featuretag/distributereorg/default.go
@@ -12,10 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !featuretag
+//go:build !distributereorg
 
 package distributereorg
 
 // TiDBEnableDistributeReorg is a feature tag
-// TODO: Before merge this PR, change this value to false.
-const TiDBEnableDistributeReorg bool = true
+const TiDBEnableDistributeReorg bool = false
