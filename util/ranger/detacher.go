83382cf1c95367d81e03d6d79ea884c3268436cd
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2023-01-18 15:56:19 +0800

    fix

--- util/ranger/detacher.go
+++ util/ranger/detacher.go
@@ -582,7 +582,8 @@ func ExtractEqAndInCondition(sctx sessionctx.Context, conditions []expression.Ex
 		if len(points[offset]) == 0 { // Early termination if false expression found
 			if expression.MaybeOverOptimized4PlanCache(sctx, conditions) {
 				// `a>@x and a<@y` --> `invalid-range if @x>=@y`
-				sctx.GetSessionVars().StmtCtx.SetSkipPlanCache(errors.Errorf("skip plan-cache: some parameters may be overwritten"))
+				sctx.GetSessionVars().StmtCtx.SkipPlanCache = true
+				sctx.GetSessionVars().StmtCtx.AppendWarning(errors.Errorf("skip plan-cache: some parameters may be overwritten"))
 			}
 			return nil, nil, nil, nil, true
 		}
@@ -606,7 +607,8 @@ func ExtractEqAndInCondition(sctx sessionctx.Context, conditions []expression.Ex
 		} else if len(points[i]) == 0 { // Early termination if false expression found
 			if expression.MaybeOverOptimized4PlanCache(sctx, conditions) {
 				// `a>@x and a<@y` --> `invalid-range if @x>=@y`
-				sctx.GetSessionVars().StmtCtx.SetSkipPlanCache(errors.Errorf("skip plan-cache: some parameters may be overwritten"))
+				sctx.GetSessionVars().StmtCtx.SkipPlanCache = true
+				sctx.GetSessionVars().StmtCtx.AppendWarning(errors.Errorf("skip plan-cache: some parameters may be overwritten"))
 			}
 			return nil, nil, nil, nil, true
 		} else {
