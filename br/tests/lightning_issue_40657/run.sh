de9a865793c3b661f87ca9801d8bbb8a53e10bf6
Yujie Xia <xiayjchn@gmail.com>
2023-01-18 18:10:17 +0800

    Update br/tests/lightning_issue_40657/run.sh

--- br/tests/lightning_issue_40657/run.sh
+++ br/tests/lightning_issue_40657/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2020 PingCAP, Inc.
+# Copyright 2023 PingCAP, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
