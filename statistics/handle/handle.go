8f6464ddfe8635c79d6c842e76a39f518267890e
yisaer <disxiaofei@163.com>
2023-01-29 15:31:00 +0800

    fix

--- statistics/handle/handle.go
+++ statistics/handle/handle.go
@@ -502,7 +502,7 @@ func NewHandle(ctx, initStatsCtx sessionctx.Context, lease time.Duration, pool s
 	err := handle.RefreshVars()
 	if err != nil {
 		return nil, err
-
+	}
 	return handle, nil
 }
 
