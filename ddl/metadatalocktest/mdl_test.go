b40db3d8f45f5bf0f8f8130ff99c45fbd6a83cf1
wjHuang <huangwenjun1997@gmail.com>
2023-02-01 16:47:12 +0800

    Update mdl_test.go

--- ddl/metadatalocktest/mdl_test.go
+++ ddl/metadatalocktest/mdl_test.go
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package metadatalocktest
-
 //go:build !featuretag
+
+package metadatalocktest
 
 import (
 	"fmt"
