a5292b8069ea829f83ad99cc99c6c55716e079f4
JmPotato <ghzpotato@gmail.com>
2023-02-06 15:33:57 +0800

    session, metrics: refine the resource control metrics (#41045)
    ref pingcap/tidb#38825

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -196,6 +196,7 @@ func RegisterMetrics() {
 	prometheus.MustRegister(LoadTableCacheDurationHistogram)
 	prometheus.MustRegister(NonTransactionalDMLCount)
 	prometheus.MustRegister(PessimisticDMLDurationByAttempt)
+	prometheus.MustRegister(ResourceGroupQueryTotalCounter)
 	prometheus.MustRegister(MemoryUsage)
 	prometheus.MustRegister(StatsCacheLRUCounter)
 	prometheus.MustRegister(StatsCacheLRUGauge)
