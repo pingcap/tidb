91658567eff22f335b7c8724af98b47fc2e6cea7
Zhigao Tong <tongzhigao@pingcap.com>
2023-02-07 11:09:58 +0800

    executor: disable paging for small limit (#41120)
    close pingcap/tidb#40741

--- distsql/request_builder.go
+++ distsql/request_builder.go
@@ -159,6 +159,7 @@ func (builder *RequestBuilder) SetDAGRequest(dag *tipb.DAGRequest) *RequestBuild
 		if limit != nil && limit.Limit < estimatedRegionRowCount {
 			builder.Request.Concurrency = 1
 		}
+		builder.Request.LimitSize = limit.GetLimit()
 	}
 	return builder
 }
