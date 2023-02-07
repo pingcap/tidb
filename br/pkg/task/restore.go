406522a46c9dfb0acd7a19075de8f3e58a7dcb56
Leavrth <jianjun.liao@outlook.com>
2023-02-07 16:31:16 +0800

    add more info in error
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- br/pkg/task/restore.go
+++ br/pkg/task/restore.go
@@ -475,7 +475,7 @@ func IsStreamRestore(cmdName string) bool {
 // RunRestore starts a restore task inside the current goroutine.
 func RunRestore(c context.Context, g glue.Glue, cmdName string, cfg *RestoreConfig) error {
 	if err := checkTaskExists(c, cfg); err != nil {
-		return errors.Trace(err)
+		return errors.Annotate(err, "failed to check task exits")
 	}
 
 	if IsStreamRestore(cmdName) {
