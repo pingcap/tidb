8aecf0af0ece13acdb6780e1b6fcd15db8495760
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2023-02-08 16:36:36 +0800

    fix test

--- br/tests/lightning_duplicate_resolution_incremental/data/dup_resolve_detect.ta-schema.sql
+++ br/tests/lightning_duplicate_resolution_incremental/data/dup_resolve_detect.ta-schema.sql
@@ -1,5 +1,5 @@
 create table ta (
-    id  int not null primary key nonclustered,
+    id varchar(11) not null primary key nonclustered, -- use varchar here to make sure _tidb_rowid will be generated
     name varchar(20) not null,
     size bigint not null,
     unique key uni_name(name)
