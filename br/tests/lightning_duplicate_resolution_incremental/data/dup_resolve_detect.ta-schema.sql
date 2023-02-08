69234af3b1c2b39ac7855b72c46c65c9386f87d8
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2023-02-08 16:20:52 +0800

    fix data

--- br/tests/lightning_duplicate_resolution_incremental/data/dup_resolve_detect.ta-schema.sql
+++ br/tests/lightning_duplicate_resolution_incremental/data/dup_resolve_detect.ta-schema.sql
@@ -1,5 +1,5 @@
 create table ta (
-    id  int not null primary key clustered,
+    id  int not null primary key nonclustered,
     name varchar(20) not null,
     size bigint not null,
     unique key uni_name(name)
