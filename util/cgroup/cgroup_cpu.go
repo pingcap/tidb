cf3e65bd7fc67e0dfbe785e84f60dda5ae51b369
Weizhen Wang <wangweizhen@pingcap.com>
2023-02-13 18:09:55 +0800

    fix

--- util/cgroup/cgroup_cpu.go
+++ util/cgroup/cgroup_cpu.go
@@ -21,6 +21,8 @@ import (
 	"github.com/pingcap/errors"
 )
 
+var noCPUControllerDetected = errors.New("no cpu controller detected")
+
 // Helper function for getCgroupCPU. Root is always "/", except in tests.
 func getCgroupCPU(root string) (CPUUsage, error) {
 	path, err := detectControlPath(filepath.Join(root, procPathCGroup), "cpu,cpuacct")
@@ -30,7 +32,7 @@ func getCgroupCPU(root string) (CPUUsage, error) {
 
 	// No CPU controller detected
 	if path == "" {
-		return CPUUsage{}, errors.New("no cpu controller detected")
+		return CPUUsage{}, noCPUControllerDetected
 	}
 
 	mount, ver, err := getCgroupDetails(filepath.Join(root, procPathMountInfo), path, "cpu,cpuacct")
