fd76460f0c847581c1e319baf25a5fb73504ca6e
Weizhen Wang <wangweizhen@pingcap.com>
2023-02-14 13:34:47 +0800

    *: update bazel
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- session/temporarytabletest/temporary_table_test.go
+++ session/temporarytabletest/temporary_table_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package temporarytable_test
+package temporarytabletest
 
 import (
 	"fmt"
