dfb38de3945aec688c7e5a36cc49b0cbf6af2312
Weizhen Wang <wangweizhen@pingcap.com>
2023-02-14 11:58:09 +0800

    *: update bazel
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- session/nontransactional_test/nontransactional_test.go
+++ session/nontransactional_test/nontransactional_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package session_test
+package nontransactional_test
 
 import (
 	"fmt"
