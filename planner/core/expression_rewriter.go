e5c78bff52cb5457d790d0d90f9c9e1187c11c40
qw4990 <zhangyuanjia@pingcap.com>
2023-02-15 15:30:11 +0800

    fixup

--- planner/core/expression_rewriter.go
+++ planner/core/expression_rewriter.go
@@ -1553,12 +1553,7 @@ func (er *expressionRewriter) inToExpression(lLen int, not bool, tp *types.Field
 					if c.GetType().EvalType() == types.ETInt {
 						continue // no need to refine it
 					}
-<<<<<<< HEAD
 					er.sctx.GetSessionVars().StmtCtx.SetSkipPlanCache(errors.Errorf("skip plan-cache: '%v' may be converted to INT", c.String()))
-=======
-				} else if !er.sctx.GetSessionVars().StmtCtx.UseCache {
-					// We should remove the mutable constant for correctness, because its value may be changed.
->>>>>>> ffaf2ac9eee (planner: remove the unnecessary skip-plan-cache flag in StmtCtx (#40235))
 					expression.RemoveMutableConst(er.sctx, []expression.Expression{c})
 				}
 				args[i], isExceptional = expression.RefineComparedConstant(er.sctx, *leftFt, c, opcode.EQ)
