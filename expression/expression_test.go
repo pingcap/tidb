98734efb12b94d4017f6ace03e2ce378b1946f0b
qw4990 <zhangyuanjia@pingcap.com>
2023-02-15 16:21:53 +0800

    fixup

--- expression/expression_test.go
+++ expression/expression_test.go
@@ -77,7 +77,7 @@ func TestEvaluateExprWithNullAndParameters(t *testing.T) {
 	res = EvaluateExprWithNull(ctx, schema, ltWithParam)
 	_, isConst := res.(*Constant)
 	require.True(t, isConst) // this expression is evaluated and skip-plan cache flag is set.
-	require.True(t, ctx.GetSessionVars().StmtCtx.UseCache)
+	require.False(t, ctx.GetSessionVars().StmtCtx.UseCache)
 }
 
 func TestEvaluateExprWithNullNoChangeRetType(t *testing.T) {
