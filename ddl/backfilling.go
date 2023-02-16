956b399527fbeba966ce3dc7b6ef049b98964ce0
tangenta <tangenta@126.com>
2023-02-16 10:47:05 +0800

    fix unit test

--- ddl/backfilling.go
+++ ddl/backfilling.go
@@ -1057,7 +1057,7 @@ func (dc *ddlCtx) writePhysicalTableRecord(sessPool *sessionPool, t table.Physic
 	}
 
 	for {
-		if startKey.Cmp(endKey) > 0 {
+		if startKey.Cmp(endKey) >= 0 {
 			break
 		}
 		kvRanges, err := splitTableRanges(t, reorgInfo.d.store, startKey, endKey, backfillTaskChanSize)
