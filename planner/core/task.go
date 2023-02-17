e2833ce6cd5a67414da125df1777019cf822a5ca
Yiding Cui <winoros@gmail.com>
2023-02-17 02:08:13 +0000

    fix check

--- planner/core/task.go
+++ planner/core/task.go
@@ -1003,7 +1003,13 @@ func (p *PhysicalTopN) canPushDownToTiKV(copTask *copTask) bool {
 	if len(copTask.rootTaskConds) != 0 {
 		return false
 	}
-	if p.containVirtualColumn(copTask.plan().Schema().Columns) {
+	if !copTask.indexPlanFinished && len(copTask.idxMergePartPlans) > 0 {
+		for _, partialPlan := range copTask.idxMergePartPlans {
+			if p.containVirtualColumn(partialPlan.Schema().Columns) {
+				return false
+			}
+		}
+	} else if p.containVirtualColumn(copTask.plan().Schema().Columns) {
 		return false
 	}
 	return true
@@ -1954,7 +1960,7 @@ func (p *PhysicalStreamAgg) attach2Task(tasks ...task) task {
 		//  1. double read, since the data of second read is ordered by handle instead of index. The `extraHandleCol` is added
 		//     if the double read needs to keep order. So we just use it to decided
 		//     whether the following plan is double read with order reserved.
-		//  2. the case that there's filters should be calcualted on TiDB side.
+		//  2. the case that there's filters should be calculated on TiDB side.
 		//  3. the case of index merge
 		if (cop.indexPlan != nil && cop.tablePlan != nil && cop.keepOrder) || len(cop.rootTaskConds) > 0 || len(cop.idxMergePartPlans) > 0 {
 			t = cop.convertToRootTask(p.ctx)
