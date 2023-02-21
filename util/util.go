1a221f0e315de84c17f10714159bc89f98713541
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2023-02-21 11:50:55 +0800

    Update util.go

--- util/util.go
+++ util/util.go
@@ -323,6 +323,4 @@ type PlanCacheMatchOpts struct {
 	// limitOffsetAndCount stores all the offset and key parameters extract from limit statement
 	// only used for cache and pick plan with parameters in limit
 	LimitOffsetAndCount []uint64
-	// HasSubQuery indicates whether this query has sub-query
-	HasSubQuery bool
 }
