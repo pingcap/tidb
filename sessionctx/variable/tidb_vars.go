73d82e330b02a39f74073d98daefbadd7deab9b9
Allen Zhong <zhongbenli@pingcap.com>
2023-02-21 20:05:06 +0800

    telemetry: set switch default to false (#41336) (#41542)
    ref pingcap/tidb#41335

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -836,7 +836,7 @@ const (
 	DefTiDBRestrictedReadOnly                    = false
 	DefTiDBSuperReadOnly                         = false
 	DefTiDBShardAllocateStep                     = math.MaxInt64
-	DefTiDBEnableTelemetry                       = true
+	DefTiDBEnableTelemetry                       = false
 	DefTiDBEnableParallelApply                   = false
 	DefTiDBEnableAmendPessimisticTxn             = false
 	DefTiDBPartitionPruneMode                    = "static"
