ef15ed3edf188d5306fcf83112f00862bfb16b5d
xuyifan <675434007@qq.com>
2023-02-22 15:27:38 +0800

    don't merge consecutive ranges when building ranges for cnf item

--- util/ranger/ranger_test.go
+++ util/ranger/ranger_test.go
@@ -1000,6 +1000,33 @@ func TestCompIndexMultiColDNF2(t *testing.T) {
 	}
 }
 
+func TestCompIndexMultiColDNF3(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+
+	testKit := testkit.NewTestKit(t, store)
+	testKit.MustExec("use test")
+	testKit.MustExec("drop table if exists t")
+	testKit.MustExec("create table t(a varchar(100), b int, c int, d int, index idx(a, b, c))")
+	testKit.MustExec("insert into t values ('t',1,1,1),('t',1,3,3),('t',2,1,3),('t',2,3,1),('w',0,3,3),('z',0,1,1)")
+
+	var input []string
+	var output []struct {
+		SQL    string
+		Plan   []string
+		Result []string
+	}
+	rangerSuiteData.LoadTestCases(t, &input, &output)
+	for i, tt := range input {
+		testdata.OnRecord(func() {
+			output[i].SQL = tt
+			output[i].Plan = testdata.ConvertRowsToStrings(testKit.MustQuery("explain " + tt).Rows())
+			output[i].Result = testdata.ConvertRowsToStrings(testKit.MustQuery(tt).Rows())
+		})
+		testKit.MustQuery("explain " + tt).Check(testkit.Rows(output[i].Plan...))
+		testKit.MustQuery(tt).Check(testkit.Rows(output[i].Result...))
+	}
+}
+
 func TestPrefixIndexMultiColDNF(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 
@@ -2564,4 +2591,8 @@ create table t(
 		got := fmt.Sprintf("%v", res.Ranges)
 		require.Equal(t, tt.resultStr, got, fmt.Sprintf("different for expr %s", tt.exprStr))
 	}
+}
+
+func TestConsiderDNFPath(t *testing.T) {
+
 }
