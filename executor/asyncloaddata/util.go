e319519aac332599c727c7b096a50a26c20721af
lance6716 <lance6716@gmail.com>
2023-02-27 13:50:54 +0800

    --wip-- [skip ci]

--- executor/asyncloaddata/util.go
+++ executor/asyncloaddata/util.go
@@ -16,6 +16,7 @@ package asyncloaddata
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/util/sqlexec"
@@ -28,21 +29,22 @@ const (
        job_id bigint(64) NOT NULL AUTO_INCREMENT,
        expected_status ENUM('running', 'paused', 'canceled') NOT NULL DEFAULT 'running',
        create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-       start_time TIMESTAMP,
-       update_time TIMESTAMP,
-       end_time TIMESTAMP,
+       start_time TIMESTAMP NULL DEFAULT NULL,
+       update_time TIMESTAMP NULL DEFAULT NULL,
+       end_time TIMESTAMP NULL DEFAULT NULL,
        data_source TEXT NOT NULL,
        table_schema VARCHAR(64) NOT NULL,
        table_name VARCHAR(64) NOT NULL,
        import_mode VARCHAR(64) NOT NULL,
        create_user VARCHAR(32) NOT NULL,
-       progress TEXT,
-       result_message TEXT,
-       error_message TEXT,
+       progress TEXT DEFAULT NULL,
+       result_message TEXT DEFAULT NULL,
+       error_message TEXT DEFAULT NULL,
        PRIMARY KEY (job_id),
-       KEY (created_user));`
+       KEY (create_user));`
 )
 
+// CreateLoadDataJob creates a load data job.
 func CreateLoadDataJob(
 	ctx context.Context,
 	conn sqlexec.SQLExecutor,
@@ -62,7 +64,7 @@ func CreateLoadDataJob(
 	if err != nil {
 		return 0, err
 	}
-	rows, err := sqlexec.DrainRecordSet(ctx, rs, 8)
+	rows, err := sqlexec.DrainRecordSet(ctx, rs, 1)
 	if err != nil {
 		return 0, err
 	}
@@ -70,4 +72,159 @@ func CreateLoadDataJob(
 		return 0, errors.Errorf("unexpected result length: %d", len(rows))
 	}
 	return rows[0].GetInt64(0), nil
+}
+
+// StartJob starts a load data job. A job can only be started once.
+func StartJob(
+	ctx context.Context,
+	conn sqlexec.SQLExecutor,
+	jobID int64,
+) error {
+	const updateSQL = `UPDATE mysql.load_data_jobs
+		SET start_time = CURRENT_TIMESTAMP
+		WHERE job_id = %? AND start_time IS NULL;`
+	_, err := conn.ExecuteInternal(ctx, updateSQL, jobID)
+	return err
+}
+
+// HeartBeatInSec is the interval of heartbeat.
+var HeartBeatInSec = 5
+
+// UpdateJobProgress updates the progress of a load data job. It should be called
+// periodically as heartbeat.
+func UpdateJobProgress(
+	ctx context.Context,
+	conn sqlexec.SQLExecutor,
+	jobID int64,
+	progress string,
+) (bool, error) {
+	// let TiDB handle heartbeat check for concurrent SQL
+	const updateSQL = `UPDATE mysql.load_data_jobs
+		SET progress = %?, update_time = CURRENT_TIMESTAMP
+		WHERE job_id = %?
+			AND (
+		    	update_time >= DATE_SUB(CURRENT_TIMESTAMP, INTERVAL %? SECOND)
+				OR update_time IS NULL);`
+	// we tolerate 2 times of failure/timeout when updating heartbeat
+	rs, err := conn.ExecuteInternal(ctx, updateSQL, progress, jobID, HeartBeatInSec*3)
+	if err != nil {
+		return false, err
+	}
+	c := rs.NewChunk(nil)
+	err = rs.Next(ctx, c)
+	if err != nil {
+		return false, err
+	}
+	return c.NumRows() == 1, nil
+}
+
+// FinishJob finishes a load data job. A job can only be started once.
+func FinishJob(
+	ctx context.Context,
+	conn sqlexec.SQLExecutor,
+	jobID int64,
+	result string,
+) error {
+	const updateSQL = `UPDATE mysql.load_data_jobs
+		SET end_time = CURRENT_TIMESTAMP, result_message = %?
+		WHERE job_id = %? AND result_message IS NULL AND error_message IS NULL;`
+	_, err := conn.ExecuteInternal(ctx, updateSQL, result, jobID)
+	return err
+}
+
+// FailJob fails a load data job. A job can only be started once.
+func FailJob(
+	ctx context.Context,
+	conn sqlexec.SQLExecutor,
+	jobID int64,
+	result string,
+) error {
+	const updateSQL = `UPDATE mysql.load_data_jobs
+		SET end_time = CURRENT_TIMESTAMP, error_message = %?
+		WHERE job_id = %? AND result_message IS NULL AND error_message IS NULL;`
+	_, err := conn.ExecuteInternal(ctx, updateSQL, result, jobID)
+	return err
+}
+
+type JobExpectedStatus int
+
+const (
+	JobExpectedRunning JobExpectedStatus = iota
+	JobExpectedPaused
+	JobExpectedCanceled
+)
+
+// UpdateJobExpectedStatus updates the expected status of a load data job.
+func UpdateJobExpectedStatus(
+	ctx context.Context,
+	conn sqlexec.SQLExecutor,
+	jobID int64,
+	status JobExpectedStatus,
+) error {
+	const (
+		toRunning = `UPDATE mysql.load_data_jobs
+			SET expected_status = %?
+			WHERE job_id = %? AND expected_status = 'paused';`
+		toPaused = `UPDATE mysql.load_data_jobs
+			SET expected_status = %?
+			WHERE job_id = %? AND expected_status = 'running';`
+		toCanceled = `UPDATE mysql.load_data_jobs
+			SET expected_status = %?
+			WHERE job_id = %? AND expected_status != 'canceled';`
+	)
+
+	var sql string
+	switch status {
+	case JobExpectedRunning:
+		sql = toRunning
+	case JobExpectedPaused:
+		sql = toPaused
+	case JobExpectedCanceled:
+		sql = toCanceled
+	}
+	_, err := conn.ExecuteInternal(ctx, sql, jobID)
+	return err
+}
+
+type JobStatus int
+
+const (
+	JobFailed JobStatus = iota
+	JobCanceled
+	JobPaused
+	JobFinished
+	JobPending
+	JobRunning
+)
+
+// GetJobStatus gets the status of a load data job. The returned error means
+// something wrong when querying the database. Other bussiness logic errors are
+// returned as JobFailed with message.
+func GetJobStatus(
+	ctx context.Context,
+	conn sqlexec.SQLExecutor,
+	jobID int64,
+) (JobStatus, string, error) {
+	const sql = `SELECT
+		expected_status,
+		update_time >= DATE_SUB(CURRENT_TIMESTAMP, INTERVAL %? SECOND) AS is_alive,
+		end_time,
+		result_message,
+		error_message,
+		start_time
+		FROM mysql.load_data_jobs
+		WHERE job_id = %?;`
+	rs, err := conn.ExecuteInternal(ctx, sql, HeartBeatInSec*3, jobID)
+	if err != nil {
+		return JobFailed, "", err
+	}
+	rows, err := sqlexec.DrainRecordSet(ctx, rs, 1)
+	if err != nil {
+		return JobFailed, "", err
+	}
+	if len(rows) != 1 {
+		return JobFailed, fmt.Sprintf("job %d not found", jobID), nil
+	}
+	row := rows[0]
+
 }
