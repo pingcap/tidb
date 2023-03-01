515b5231849b3741ddd937883bb2330ca47795aa
tangenta <tangenta@126.com>
2023-03-01 11:34:29 +0800

    provide a preallocated slice to EncodeInt

--- br/pkg/lightning/backend/kv/sql2kv.go
+++ br/pkg/lightning/backend/kv/sql2kv.go
@@ -30,7 +30,6 @@ import (
 	"github.com/pingcap/tidb/br/pkg/logutil"
 	"github.com/pingcap/tidb/br/pkg/redact"
 	"github.com/pingcap/tidb/expression"
-	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/meta/autoid"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/parser/mysql" //nolint: goimports
@@ -41,6 +40,7 @@ import (
 	"github.com/pingcap/tidb/tablecodec"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/chunk"
+	"github.com/pingcap/tidb/util/codec"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"golang.org/x/exp/slices"
@@ -434,7 +434,8 @@ func (kvcodec *tableKVEncoder) Encode(
 	}
 	kvPairs := kvcodec.se.takeKvPairs()
 	for i := 0; i < len(kvPairs.pairs); i++ {
-		kvPairs.pairs[i].RowID = kv.IntHandle(rowID).Encoded()
+		var encoded [8]byte
+		kvPairs.pairs[i].RowID = codec.EncodeInt(encoded[:0], rowID)
 	}
 	kvcodec.recordCache = record[:0]
 	return kvPairs, nil
