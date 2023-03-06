111cb49e94e0e955c4201943d26e4a0c0c0b0388
yisaer <disxiaofei@163.com>
2023-03-06 16:52:03 +0800

    fix

--- sessionctx/variable/session.go
+++ sessionctx/variable/session.go
@@ -1055,7 +1055,7 @@ type SessionVars struct {
 	// When it is false, ANALYZE reads the latest data.
 	// When it is true, ANALYZE reads data on the snapshot at the beginning of ANALYZE.
 	EnableAnalyzeSnapshot bool
-	
+
 	// EnableINLJoinInnerMultiPattern indicates whether enable multi pattern for index join inner side
 	EnableINLJoinInnerMultiPattern bool
 }
