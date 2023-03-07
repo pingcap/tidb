2dc6d89e5d0bc2e6eff7e0209083d013aee87e50
Yujie Xia <xiayjchn@gmail.com>
2023-03-07 12:29:56 +0800

    add benchmark

--- br/pkg/lightning/backend/local/compress_test.go
+++ br/pkg/lightning/backend/local/compress_test.go
@@ -8,9 +8,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 )
 
-func TestCompressor(t *testing.T) {
+func TestGzipCompressor(t *testing.T) {
 	compressor := &gzipCompressor{}
 	require.Equal(t, "gzip", compressor.Type())
 
@@ -32,7 +33,7 @@ func TestCompressor(t *testing.T) {
 	require.Equal(t, input, uncompressed)
 }
 
-func TestDecompressor(t *testing.T) {
+func TestGzipDecompressor(t *testing.T) {
 	decompressor := &gzipDecompressor{}
 	require.Equal(t, "gzip", decompressor.Type())
 
@@ -50,4 +51,51 @@ func TestDecompressor(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, input, uncompressed)
+}
+
+func BenchmarkGzipCompressor(b *testing.B) {
+	benchCompressor(b, &gzipCompressor{})
+}
+
+func BenchmarkGrpcGzipCompressor(b *testing.B) {
+	benchCompressor(b, grpc.NewGZIPCompressor())
+}
+
+func benchCompressor(b *testing.B, compressor grpc.Compressor) {
+	input := make([]byte, 1<<20)
+	_, err := rand.Read(input)
+	require.NoError(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf := &bytes.Buffer{}
+		err = compressor.Do(buf, input)
+		require.NoError(b, err)
+	}
+}
+
+func BenchmarkGzipDecompressor(b *testing.B) {
+	benchDecompressor(b, &gzipDecompressor{})
+}
+
+func BenchmarkGrpcGzipDecompressor(b *testing.B) {
+	benchDecompressor(b, grpc.NewGZIPDecompressor())
+}
+
+func benchDecompressor(b *testing.B, decompressor grpc.Decompressor) {
+	input := make([]byte, 1<<20)
+	_, err := rand.Read(input)
+	require.NoError(b, err)
+
+	buf := &bytes.Buffer{}
+	z := gzip.NewWriter(buf)
+	_, err = z.Write(input)
+	require.NoError(b, err)
+	require.NoError(b, z.Close())
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := decompressor.Do(bytes.NewReader(buf.Bytes()))
+		require.NoError(b, err)
+	}
 }
