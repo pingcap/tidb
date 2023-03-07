efd31a29ea591ac55eddd5f11c0a955adef0412b
Yujie Xia <xiayjchn@gmail.com>
2023-03-07 11:35:49 +0800

    fix panic

--- br/pkg/lightning/backend/local/compress.go
+++ br/pkg/lightning/backend/local/compress.go
@@ -12,24 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/*
- *
- * Copyright 2014 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package local
 
 import (
@@ -45,13 +27,17 @@ var (
 	_ grpc.Decompressor = (*gzipDecompressor)(nil)
 )
 
-type gzipCompressor struct {
-	pool sync.Pool
+type gzipCompressor struct{}
+
+var gzipWriterPool = sync.Pool{
+	New: func() any {
+		return gzip.NewWriter(io.Discard)
+	},
 }
 
 func (c *gzipCompressor) Do(w io.Writer, p []byte) error {
-	z := c.pool.Get().(*gzip.Writer)
-	defer c.pool.Put(z)
+	z := gzipWriterPool.Get().(*gzip.Writer)
+	defer gzipWriterPool.Put(z)
 	z.Reset(w)
 	if _, err := z.Write(p); err != nil {
 		return err
@@ -63,30 +49,24 @@ func (c *gzipCompressor) Type() string {
 	return "gzip"
 }
 
-type gzipDecompressor struct {
-	pool sync.Pool
+type gzipDecompressor struct{}
+
+var gzipReaderPool = sync.Pool{
+	New: func() any {
+		return &gzip.Reader{}
+	},
 }
 
 func (d *gzipDecompressor) Do(r io.Reader) ([]byte, error) {
-	var z *gzip.Reader
-	switch maybeZ := d.pool.Get().(type) {
-	case nil:
-		newZ, err := gzip.NewReader(r)
-		if err != nil {
-			return nil, err
-		}
-		z = newZ
-	case *gzip.Reader:
-		z = maybeZ
-		if err := z.Reset(r); err != nil {
-			d.pool.Put(z)
-			return nil, err
-		}
+	z := gzipReaderPool.Get().(*gzip.Reader)
+	if err := z.Reset(r); err != nil {
+		gzipReaderPool.Put(z)
+		return nil, err
 	}
 
 	defer func() {
 		z.Close()
-		d.pool.Put(z)
+		gzipReaderPool.Put(z)
 	}()
 	return io.ReadAll(z)
 }
