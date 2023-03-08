eb368cd462469f7434e6da503648257acfae4749
xiongjiwei <xiongjiwei1996@outlook.com>
2023-03-08 13:36:37 +0800

    update

--- br/tests/lightning_multi_valued_index/run.sh
+++ br/tests/lightning_multi_valued_index/run.sh
@@ -20,9 +20,9 @@ check_result() {
     run_sql "ADMIN CHECK TABLE mv.$1"
     run_sql "SELECT count(*) FROM mv.$1"
     check_contains 'count(*): 100'
-    run_sql "SELECT -22162723 MEMBER OF (j->'.number') as r FROM mv.$1 WHERE pk = -918151511"
+    run_sql "SELECT -22162723 MEMBER OF (j->'$.number') as r FROM mv.$1 WHERE pk = -918151511"
     check_contains 'r: 1'
-    run_sql "SELECT 'kjGNP2fd3X20K9HtIbaBw3VL7Ze58eiysenzLBij4jDFzlnO0Y3qfheDrGmo4OM' MEMBER OF (j->'.string') as r FROM mv.$1 WHERE pk = 6"
+    run_sql "SELECT 'kjGNP2fd3X20K9HtIbaBw3VL7Ze58eiysenzLBij4jDFzlnO0Y3qfheDrGmo4OM' MEMBER OF (j->'$.string') as r FROM mv.$1 WHERE pk = 6"
     check_contains 'r: 1'
 }
 
