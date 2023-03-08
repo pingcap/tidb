f9dd1334625491666d6719ddc388a441301b3edd
xiongjiwei <xiongjiwei1996@outlook.com>
2023-03-08 15:17:32 +0800

    update

--- br/tests/br_mv_index/run.sh
+++ br/tests/br_mv_index/run.sh
@@ -24,7 +24,7 @@ echo "load data..."
 # create database
 run_sql "CREATE DATABASE IF NOT EXISTS $DB;"
 # create table
-run_sql "CREATE TABLE IF NOT EXISTS ${DB}.${TABLE} (pk, primary key auto_increment, j json);"
+run_sql "CREATE TABLE IF NOT EXISTS ${DB}.${TABLE} (pk bigint primary key auto_increment, j json);"
 # insert records
 for i in $(seq $ROW_COUNT); do
     run_sql "INSERT INTO ${DB}.${TABLE}(j) VALUES ('{\"number\": [$i, $(($i+1)), $(($i+2))], \"string\": [\"${i}a\", \"$(($i+1))b\", \"$(($i+2))c\"]}');"
@@ -32,10 +32,17 @@ done
 
 # full backup
 echo "backup full start..."
-run_sql "CREATE INDEX idx_c1 ON ${DB}.${TABLE}((cast(j->>'$.number' as signed array))))"
-run_sql "CREATE INDEX idx_c1 ON ${DB}.${TABLE}((cast(j->>'$.string' as char(64) array))))"
+run_sql "CREATE INDEX idx_c1 ON ${DB}.${TABLE}((cast(j->'$.number' as signed array)))"
 run_br --pd $PD_ADDR backup full -s "local://$TEST_DIR/$DB/full"
 wait
+
+# run ddls
+echo "run ddls..."
+run_sql "CREATE INDEX idx_c1 ON ${DB}.${TABLE}((cast(j->'$.string' as char(64) array)))"
+# incremental backup
+echo "incremental backup start..."
+last_backup_ts=$(run_br validate decode --field="end-version" -s "local://$TEST_DIR/$DB/full" | grep -oE "^[0-9]+")
+run_br --pd $PD_ADDR backup table -s "local://$TEST_DIR/$DB/inc" --db $DB -t $TABLE --lastbackupts $last_backup_ts
 
 run_sql "DROP DATABASE $DB;"
 # full restore
@@ -47,5 +54,21 @@ if [ "${row_count_full}" != "${ROW_COUNT}" ];then
     echo "TEST: [$TEST_NAME] full restore fail on database $DB"
     exit 1
 fi
+
+# incremental restore
+echo "incremental restore start..."
+run_br restore table --db $DB --table $TABLE -s "local://$TEST_DIR/$DB/inc" --pd $PD_ADDR
+row_count_inc=$(run_sql "SELECT COUNT(*) FROM $DB.$TABLE;" | awk '/COUNT/{print $2}')
+# check full restore
+if [ "${row_count_inc}" != "${ROW_COUNT}" ];then
+    echo "TEST: [$TEST_NAME] incremental restore fail on database $DB"
+    exit 1
+fi
+
+run_sql "EXPLAIN SELECT * FROM t WHERE 1 MEMBER OF (j->'$.number')"
+check_contains 'IndexMerge'
+run_sql "EXPLAIN SELECT * FROM t WHERE 'a' MEMBER OF (j->'$.string')"
+check_contains 'IndexMerge'
+
 
 run_sql "DROP DATABASE $DB;"
