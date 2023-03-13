074a58e42b178e47fce57f1a66ff82aa775cdd1f
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2023-03-13 21:49:28 +0800

    Update common_plans.go

--- planner/core/common_plans.go
+++ planner/core/common_plans.go
@@ -1014,8 +1014,7 @@ type Simple struct {
 }
 
 // PhysicalSimpleWrapper is a wrapper of `Simple` to implement physical plan interface.
-//
-//	Used for simple statements executing in coprocessor.
+// Used for simple statements executing in coprocessor.
 type PhysicalSimpleWrapper struct {
 	basePhysicalPlan
 	Inner Simple
