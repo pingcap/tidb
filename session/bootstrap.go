2b4bec1b91512f21219e20282682a8c516561dfd
BornChanger <dawn_catcher@126.com>
2023-03-14 13:13:24 +0800

    *: bump current strap version
    Signed-off-by: BornChanger <dawn_catcher@126.com>

--- session/bootstrap.go
+++ session/bootstrap.go
@@ -834,7 +834,7 @@ const (
 
 // currentBootstrapVersion is defined as a variable, so we can modify its value for testing.
 // please make sure this is the largest version
-var currentBootstrapVersion int64 = version136
+var currentBootstrapVersion int64 = version137
 
 // DDL owner key's expired time is ManagerSessionTTL seconds, we should wait the time and give more time to have a chance to finish it.
 var internalSQLTimeout = owner.ManagerSessionTTL + 15
