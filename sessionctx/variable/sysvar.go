471c08401763ad042349571f3ca5e221c72b1e39
Zhou Kunqin <25057648+time-and-fate@users.noreply.github.com>
2023-03-16 01:52:41 +0800

    planner, sessionctx: add variable to control index selection for "WHERE b ORDER BY a LIMIT n" (#42116)
    close pingcap/tidb#42060

--- sessionctx/variable/sysvar.go
+++ sessionctx/variable/sysvar.go
@@ -2416,6 +2416,11 @@ var defaultSysVars = []*SysVar{
 	}, GetGlobal: func(ctx context.Context, vars *SessionVars) (string, error) {
 		return strconv.Itoa(int(TTLRunningTasks.Load())), nil
 	}},
+	{Scope: ScopeGlobal | ScopeSession, Name: TiDBOptOrderingIdxSelThresh, Value: strconv.FormatFloat(DefTiDBOptOrderingIdxSelThresh, 'f', -1, 64), Type: TypeFloat, MinValue: 0, MaxValue: 1,
+		SetSession: func(s *SessionVars, val string) error {
+			s.OptOrderingIdxSelThresh = tidbOptFloat64(val, DefTiDBOptOrderingIdxSelThresh)
+			return nil
+		}},
 }
 
 func setTiFlashComputeDispatchPolicy(s *SessionVars, val string) error {
