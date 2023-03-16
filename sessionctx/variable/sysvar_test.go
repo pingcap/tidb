b0c3d04f35ab6357327508e75483794ab19807ce
xufei <xufeixw@mail.ustc.edu.cn>
2023-03-16 16:30:54 +0800

    Revert "for test"
    This reverts commit ace5ff48fc85bb306659a97a37927906039b1b94.

--- sessionctx/variable/sysvar_test.go
+++ sessionctx/variable/sysvar_test.go
@@ -1165,9 +1165,9 @@ func TestTiDBEnableResourceControl(t *testing.T) {
 	// setup the hooks for test
 	// NOTE: the default system variable is true but the switch is false
 	// It is initialized at the first call of `rebuildSysVarCache`
-	enable := atomic.Bool{}
-	EnableGlobalResourceControlFunc = func() { enable.Store(true) }
-	DisableGlobalResourceControlFunc = func() { enable.Store(false) }
+	enable := false
+	EnableGlobalResourceControlFunc = func() { enable = true }
+	DisableGlobalResourceControlFunc = func() { enable = false }
 	setGlobalResourceControlFunc := func(enable bool) {
 		if enable {
 			EnableGlobalResourceControlFunc()
@@ -1185,7 +1185,7 @@ func TestTiDBEnableResourceControl(t *testing.T) {
 
 	// Default true
 	require.Equal(t, resourceControlEnabled.Value, On)
-	require.Equal(t, false, enable.Load())
+	require.Equal(t, enable, false)
 
 	// Set to On(init at start)
 	err := mock.SetGlobalSysVar(context.Background(), TiDBEnableResourceControl, On)
@@ -1193,7 +1193,7 @@ func TestTiDBEnableResourceControl(t *testing.T) {
 	val, err1 := mock.GetGlobalSysVar(TiDBEnableResourceControl)
 	require.NoError(t, err1)
 	require.Equal(t, On, val)
-	require.Equal(t, true, enable.Load())
+	require.Equal(t, enable, true)
 
 	// Set to Off
 	err = mock.SetGlobalSysVar(context.Background(), TiDBEnableResourceControl, Off)
@@ -1201,7 +1201,7 @@ func TestTiDBEnableResourceControl(t *testing.T) {
 	val, err1 = mock.GetGlobalSysVar(TiDBEnableResourceControl)
 	require.NoError(t, err1)
 	require.Equal(t, Off, val)
-	require.Equal(t, false, enable.Load())
+	require.Equal(t, enable, false)
 
 	// Set to On again
 	err = mock.SetGlobalSysVar(context.Background(), TiDBEnableResourceControl, On)
@@ -1209,5 +1209,5 @@ func TestTiDBEnableResourceControl(t *testing.T) {
 	val, err1 = mock.GetGlobalSysVar(TiDBEnableResourceControl)
 	require.NoError(t, err1)
 	require.Equal(t, On, val)
-	require.Equal(t, true, enable.Load())
+	require.Equal(t, enable, true)
 }
