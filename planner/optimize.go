23bc1dcce27e15e6d2570d09f44fa45ad6ded8dd
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2023-03-21 15:45:06 +0800

    planner: add more test cases for non-prep plan cache (#42412)
    ref pingcap/tidb#36598

--- planner/optimize.go
+++ planner/optimize.go
@@ -106,7 +106,13 @@ func getPlanFromNonPreparedPlanCache(ctx context.Context, sctx sessionctx.Contex
 		}
 	}()
 	val := sctx.GetSessionVars().GetNonPreparedPlanCacheStmt(paramSQL)
+	paramExprs := core.Params2Expressions(params)
+
 	if val == nil {
+		// GeneratePlanCacheStmtWithAST may evaluate these parameters so set their values into SCtx in advance.
+		if err := core.SetParameterValuesIntoSCtx(sctx, nil, paramExprs); err != nil {
+			return nil, nil, false, err
+		}
 		cachedStmt, _, _, err := core.GeneratePlanCacheStmtWithAST(ctx, sctx, false, paramSQL, stmt)
 		if err != nil {
 			return nil, nil, false, err
@@ -116,7 +122,6 @@ func getPlanFromNonPreparedPlanCache(ctx context.Context, sctx sessionctx.Contex
 	}
 	cachedStmt := val.(*core.PlanCacheStmt)
 
-	paramExprs := core.Params2Expressions(params)
 	cachedPlan, names, err := core.GetPlanFromSessionPlanCache(ctx, sctx, true, is, cachedStmt, paramExprs)
 	if err != nil {
 		return nil, nil, false, err
