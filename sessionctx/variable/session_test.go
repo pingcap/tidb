1d822666ac68e1d46d841344cd977007c9f26bc0
YangKeao <yangkeao@chunibyo.icu>
2023-03-23 08:08:44 -0400

    session, com_stmt: fetch all rows during EXECUTE command (#42473, hotfix) (#42516)

--- sessionctx/variable/session_test.go
+++ sessionctx/variable/session_test.go
@@ -15,6 +15,8 @@
 package variable_test
 
 import (
+	"context"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -25,10 +27,12 @@ import (
 	"github.com/pingcap/tidb/parser/auth"
 	"github.com/pingcap/tidb/parser/mysql"
 	plannercore "github.com/pingcap/tidb/planner/core"
+	"github.com/pingcap/tidb/sessionctx/sessionstates"
 	"github.com/pingcap/tidb/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/testkit"
 	"github.com/pingcap/tidb/types"
+	util2 "github.com/pingcap/tidb/util"
 	"github.com/pingcap/tidb/util/chunk"
 	"github.com/pingcap/tidb/util/execdetails"
 	"github.com/pingcap/tidb/util/mock"
@@ -487,56 +491,34 @@ func TestGetReuseChunk(t *testing.T) {
 	require.Nil(t, sessVars.ChunkPool.Alloc)
 }
 
-func TestPretectedTSList(t *testing.T) {
-	lst := &variable.NewSessionVars(nil).ProtectedTSList
-
-	// empty set
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(1))
-	require.Equal(t, 0, lst.Size())
-
-	// hold 1
-	unhold1 := lst.HoldTS(1)
-	require.Equal(t, uint64(1), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(1))
-
-	// hold 2 twice
-	unhold2a := lst.HoldTS(2)
-	unhold2b := lst.HoldTS(2)
-	require.Equal(t, uint64(1), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(2), lst.GetMinProtectedTS(1))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(2))
-	require.Equal(t, 2, lst.Size())
-
-	// unhold 2a
-	unhold2a()
-	require.Equal(t, uint64(1), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(2), lst.GetMinProtectedTS(1))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(2))
-	require.Equal(t, 2, lst.Size())
-	// unhold 2a again
-	unhold2a()
-	require.Equal(t, uint64(1), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(2), lst.GetMinProtectedTS(1))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(2))
-	require.Equal(t, 2, lst.Size())
-
-	// unhold 1
-	unhold1()
-	require.Equal(t, uint64(2), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(2), lst.GetMinProtectedTS(1))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(2))
-	require.Equal(t, 1, lst.Size())
-
-	// unhold 2b
-	unhold2b()
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(1))
-	require.Equal(t, 0, lst.Size())
-
-	// unhold 2b again
-	unhold2b()
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(0))
-	require.Equal(t, uint64(0), lst.GetMinProtectedTS(1))
-	require.Equal(t, 0, lst.Size())
+func TestUserVarConcurrently(t *testing.T) {
+	sv := variable.NewSessionVars(nil)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	var wg util2.WaitGroupWrapper
+	wg.Run(func() {
+		for i := 0; ; i++ {
+			select {
+			case <-time.After(time.Millisecond):
+				name := strconv.Itoa(i)
+				sv.SetUserVarVal(name, types.Datum{})
+				sv.GetUserVarVal(name)
+			case <-ctx.Done():
+				return
+			}
+		}
+	})
+	wg.Run(func() {
+		for {
+			select {
+			case <-time.After(time.Millisecond):
+				var states sessionstates.SessionStates
+				require.NoError(t, sv.EncodeSessionStates(ctx, &states))
+				require.NoError(t, sv.DecodeSessionStates(ctx, &states))
+			case <-ctx.Done():
+				return
+			}
+		}
+	})
+	wg.Wait()
+	cancel()
 }
