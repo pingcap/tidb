a7ae30672955db881b5656ac5b323d38c6a49244
CbcWestwolf <1004626265@qq.com>
2023-03-24 16:56:42 +0800

    This is an automated cherry-pick of #42367
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -4049,6 +4049,9 @@ func (d *ddl) ReorganizePartitions(ctx sessionctx.Context, ident ast.Ident, spec
 	// No preSplitAndScatter here, it will be done by the worker in onReorganizePartition instead.
 	err = d.DoDDLJob(ctx, job)
 	err = d.callHookOnChanged(job, err)
+	if err == nil {
+		ctx.GetSessionVars().StmtCtx.AppendWarning(errors.New("The statistics of related partitions will be outdated after reorganizing partitions. Please use 'ANALYZE TABLE' statement if you want to update it now"))
+	}
 	return errors.Trace(err)
 }
 
