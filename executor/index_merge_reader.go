0c8d54f434db18cfa3ce9ec33e6cb40df95764c7
yibin <huyibin@pingcap.com>
2023-03-27 16:50:44 +0800

    executor: Lower the log level from info to debug for index merge reader (#42597)
    close pingcap/tidb#41949

--- executor/index_merge_reader.go
+++ executor/index_merge_reader.go
@@ -789,7 +789,7 @@ func handleWorkerPanic(ctx context.Context, finished <-chan struct{}, ch chan<- 
 			defer close(ch)
 		}
 		if r == nil {
-			logutil.BgLogger().Info("worker finish without panic", zap.Any("worker", worker))
+			logutil.BgLogger().Debug("worker finish without panic", zap.Any("worker", worker))
 			return
 		}
 
@@ -1341,7 +1341,7 @@ func (w *indexMergeTableScanWorker) pickAndExecTask(ctx context.Context, task **
 func (w *indexMergeTableScanWorker) handleTableScanWorkerPanic(ctx context.Context, finished <-chan struct{}, task **indexMergeTableTask, worker string) func(r interface{}) {
 	return func(r interface{}) {
 		if r == nil {
-			logutil.BgLogger().Info("worker finish without panic", zap.Any("worker", worker))
+			logutil.BgLogger().Debug("worker finish without panic", zap.Any("worker", worker))
 			return
 		}
 
