65de0e3c43592829c6703dfee3d618b1a3347065
D3Hunter <jujj603@gmail.com>
2023-03-28 18:59:46 +0800

    fix kvstore import cycle

--- executor/importer/import.go
+++ executor/importer/import.go
@@ -30,6 +30,7 @@ import (
 	litlog "github.com/pingcap/tidb/br/pkg/lightning/log"
 	"github.com/pingcap/tidb/br/pkg/lightning/mydump"
 	"github.com/pingcap/tidb/br/pkg/storage"
+	tidbkv "github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/parser/terror"
@@ -42,6 +43,7 @@ import (
 	"github.com/pingcap/tidb/util/intest"
 	"github.com/pingcap/tidb/util/logutil"
 	"github.com/pingcap/tidb/util/stringutil"
+	kvconfig "github.com/tikv/client-go/v2/config"
 	"go.uber.org/zap"
 )
 
@@ -102,6 +104,8 @@ var (
 	// LoadDataReadBlockSize is exposed for test.
 	LoadDataReadBlockSize = int64(config.ReadBlockSize)
 )
+
+var GetKVStore func(path string, tls kvconfig.Security) (tidbkv.Storage, error)
 
 // FieldMapping indicates the relationship between input field and table column or user variable
 type FieldMapping struct {
