0cd8c4c318cfaa8b2f7965c225b3527f834dc6b5
Xiaoguang Sun <sunxiaoguang@users.noreply.github.com>
2023-03-30 17:40:55 +0800

    session: Do not run telemetry loops when it's disabled in config (#40156) (#41150)
    close pingcap/tidb#40155

--- session/session.go
+++ session/session.go
@@ -2927,8 +2927,12 @@ func BootstrapSession(store kv.Storage) (*domain.Domain, error) {
 	if dom.GetEtcdClient() != nil {
 		// We only want telemetry data in production-like clusters. When TiDB is deployed over other engines,
 		// for example, unistore engine (used for local tests), we just skip it. Its etcd client is nil.
-		dom.TelemetryReportLoop(ses[5])
-		dom.TelemetryRotateSubWindowLoop(ses[5])
+		if config.GetGlobalConfig().EnableTelemetry {
+			// There is no way to turn telemetry on with global variable `tidb_enable_telemetry`
+			// when it is disabled in config. See IsTelemetryEnabled function in telemetry/telemetry.go
+			dom.TelemetryReportLoop(ses[5])
+			dom.TelemetryRotateSubWindowLoop(ses[5])
+		}
 	}
 
 	// A sub context for update table stats, and other contexts for concurrent stats loading.
