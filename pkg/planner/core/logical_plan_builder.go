cb9be301563cbe9c30f2d9ed319693e403f76fea
cbcwestwolf <1004626265@qq.com>
2023-04-03 23:41:53 +0800

    planner: fix nil pointer at expression.(*CorrelatedColumn).Eval

--- pkg/planner/core/logical_plan_builder.go
+++ pkg/planner/core/logical_plan_builder.go
@@ -434,6 +434,7 @@ func (b *PlanBuilder) buildAggregation(ctx context.Context, p LogicalPlan, aggFu
 			if _, ok := correlatedAggMap[aggFunc]; ok {
 				b.correlatedAggMapper[aggFunc] = &expression.CorrelatedColumn{
 					Column: column,
+					Data:   new(types.Datum),
 				}
 			}
 		}
