e742bdf77994b4e76102f74c5a20100477e56fd5
tangenta <tangenta@126.com>
2023-04-04 16:47:01 +0800

    fix build

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -7276,11 +7276,11 @@ func (d *ddl) CleanDeadTableLock(unlockTables []model.TableLockTpInfo, se model.
 		Args:       []interface{}{arg},
 	}
 
-	ctx, err := d.sessPool.get()
+	ctx, err := d.sessPool.Get()
 	if err != nil {
 		return err
 	}
-	defer d.sessPool.put(ctx)
+	defer d.sessPool.Put(ctx)
 	err = d.DoDDLJob(ctx, job)
 	err = d.callHookOnChanged(job, err)
 	return errors.Trace(err)
