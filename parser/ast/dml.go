a25a107dc47194c05296ff8e47f57023a8fffeba
Aidan <97376271+keeplearning20221@users.noreply.github.com>
2023-04-04 09:08:08 +0800

    Update parser/ast/dml.go
    Co-authored-by: CbcWestwolf <1004626265@qq.com>

--- parser/ast/dml.go
+++ parser/ast/dml.go
@@ -2868,7 +2868,7 @@ type ShowStmt struct {
 	Tp                ShowStmtType // Databases/Tables/Columns/....
 	DBName            string
 	Table             *TableName  // Used for showing columns.
-	Procedure         *TableName  // Procedure is not save as table and naming method is consistent with the table name
+	Procedure         *TableName  // Procedure is not saved as table and the naming method is consistent with the table name
 	Partition         model.CIStr // Used for showing partition.
 	Column            *ColumnName // Used for `desc table column`.
 	IndexName         model.CIStr
