f3fa1e61aadd0ffea16eaec2427dded0723e04b8
qw4990 <zhangyuanjia@pingcap.com>
2023-04-07 22:43:28 +0800

    fixup

--- planner/core/casetest/physical_plan_test.go
+++ planner/core/casetest/physical_plan_test.go
@@ -245,6 +245,9 @@ func TestDAGPlanBuilderBasePhysicalPlan(t *testing.T) {
 	require.NoError(t, err)
 	_, err = se.Execute(context.Background(), "use test")
 	require.NoError(t, err)
+	_, err = se.Execute(context.Background(), "set @@tidb_enable_non_prepared_plan_cache=0")
+	require.NoError(t, err)
+
 
 	var input []string
 	var output []struct {
