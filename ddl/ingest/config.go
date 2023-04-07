cb96bbd870170cbc8f9d559734aa549813abacd0
lance6716 <lance6716@gmail.com>
2023-04-07 17:04:58 +0800

    ddl: set lightning cron config to real behaviour (#42855)
    ref pingcap/tidb#35983

--- ddl/ingest/config.go
+++ ddl/ingest/config.go
@@ -64,6 +64,8 @@ func genConfig(memRoot MemRoot, jobID int64, unique bool) (*Config, error) {
 	cfg.Security.CAPath = tidbCfg.Security.ClusterSSLCA
 	cfg.Security.CertPath = tidbCfg.Security.ClusterSSLCert
 	cfg.Security.KeyPath = tidbCfg.Security.ClusterSSLKey
+	// in DDL scenario, we don't switch import mode
+	cfg.Cron.SwitchMode = lightning.Duration{Duration: 0}
 
 	c := &Config{
 		Lightning:    cfg,
