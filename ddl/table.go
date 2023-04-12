add816bb22546f49c82080775ecfa328d1921e27
Mattias Jonsson <mjonss@users.noreply.github.com>
2023-04-12 18:09:01 +0200

    ddl: Fix issue with truncate table and partitions and tiflash (#42957)
    close pingcap/tidb#42940

--- ddl/table.go
+++ ddl/table.go
@@ -687,7 +687,8 @@ func onTruncateTable(d *ddlCtx, t *meta.Meta, job *model.Job) (ver int64, _ erro
 	tableID := job.TableID
 	var newTableID int64
 	var fkCheck bool
-	err := job.DecodeArgs(&newTableID, &fkCheck)
+	var newPartitionIDs []int64
+	err := job.DecodeArgs(&newTableID, &fkCheck, &newPartitionIDs)
 	if err != nil {
 		job.State = model.JobStateCancelled
 		return ver, errors.Trace(err)
@@ -733,7 +734,7 @@ func onTruncateTable(d *ddlCtx, t *meta.Meta, job *model.Job) (ver int64, _ erro
 	if tblInfo.GetPartitionInfo() != nil {
 		oldPartitionIDs = getPartitionIDs(tblInfo)
 		// We use the new partition ID because all the old data is encoded with the old partition ID, it can not be accessed anymore.
-		err = truncateTableByReassignPartitionIDs(t, tblInfo)
+		err = truncateTableByReassignPartitionIDs(t, tblInfo, newPartitionIDs)
 		if err != nil {
 			return ver, errors.Trace(err)
 		}
