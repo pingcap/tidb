8c9663941fc633bc57edfcfb3edc5da8533ace57
wjHuang <huangwenjun1997@gmail.com>
2023-04-12 21:57:02 +0800

    ddl, test: integrate add index with the distribute framework (#42977)
    close pingcap/tidb#42974

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -39,6 +39,8 @@ import (
 	sess "github.com/pingcap/tidb/ddl/internal/session"
 	"github.com/pingcap/tidb/ddl/syncer"
 	"github.com/pingcap/tidb/ddl/util"
+	"github.com/pingcap/tidb/disttask/framework/dispatcher"
+	"github.com/pingcap/tidb/disttask/framework/proto"
 	"github.com/pingcap/tidb/disttask/framework/scheduler"
 	"github.com/pingcap/tidb/domain/infosync"
 	"github.com/pingcap/tidb/infoschema"
@@ -686,6 +688,13 @@ func newDDL(ctx context.Context, options ...Option) *ddl {
 		func(taskMeta []byte, step int64) (scheduler.Scheduler, error) {
 			return NewBackfillSchedulerHandle(taskMeta, d)
 		})
+
+	dispatcher.RegisterTaskFlowHandle(BackfillTaskType, NewLitBackfillFlowHandle(d))
+	scheduler.RegisterSubtaskExectorConstructor(BackfillTaskType, func(minimalTask proto.MinimalTask, step int64) (scheduler.SubtaskExecutor, error) {
+		return &BackFillSubtaskExecutor{
+			Task: minimalTask,
+		}, nil
+	})
 
 	// Register functions for enable/disable ddl when changing system variable `tidb_enable_ddl`.
 	variable.EnableDDL = d.EnableDDL
