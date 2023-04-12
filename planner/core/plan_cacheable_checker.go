bb05f742ed4bbf08d3d68b025e9e0ba3540fb850
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2023-04-12 17:09:02 +0800

    planner: skip caching too large plan for plan cache (#42976)
    close pingcap/tidb#42975

--- planner/core/plan_cacheable_checker.go
+++ planner/core/plan_cacheable_checker.go
@@ -29,6 +29,7 @@ import (
 	driver "github.com/pingcap/tidb/types/parser_driver"
 	"github.com/pingcap/tidb/util/filter"
 	"github.com/pingcap/tidb/util/logutil"
+	"github.com/pingcap/tidb/util/size"
 	"go.uber.org/zap"
 )
 
@@ -51,9 +52,10 @@ func CacheableWithCtx(sctx sessionctx.Context, node ast.Node, is infoschema.Info
 		return false, "not a SELECT/UPDATE/INSERT/DELETE/SET statement"
 	}
 	checker := cacheableChecker{
-		sctx:      sctx,
-		cacheable: true,
-		schema:    is,
+		sctx:         sctx,
+		cacheable:    true,
+		schema:       is,
+		sumInListLen: 0,
 	}
 	node.Accept(&checker)
 	return checker.cacheable, checker.reason
@@ -65,6 +67,8 @@ type cacheableChecker struct {
 	cacheable bool
 	schema    infoschema.InfoSchema
 	reason    string // reason why cannot use plan-cache
+
+	sumInListLen int // the accumulated number of elements in all in-lists
 }
 
 // Enter implements Visitor interface.
@@ -114,7 +118,13 @@ func (checker *cacheableChecker) Enter(in ast.Node) (out ast.Node, skipChildren 
 				return in, true
 			}
 		}
-
+	case *ast.PatternInExpr:
+		checker.sumInListLen += len(node.List)
+		if checker.sumInListLen > 100 { // to save memory
+			checker.cacheable = false
+			checker.reason = "too many values in in-list (more than 100)"
+			return in, true
+		}
 	case *ast.VariableExpr:
 		checker.cacheable = false
 		checker.reason = "query has user-defined variables is un-cacheable"
@@ -593,6 +603,9 @@ func isPlanCacheable(sctx sessionctx.Context, p Plan, paramNum, limitParamNum in
 	}
 	if hasSubQuery && !sctx.GetSessionVars().EnablePlanCacheForSubquery {
 		return false, "the switch 'tidb_enable_plan_cache_for_subquery' is off"
+	}
+	if uint64(pp.MemoryUsage()) > 2*size.MB { // to save memory
+		return false, "plan is too large(>2MB)"
 	}
 	return isPhysicalPlanCacheable(sctx, pp, paramNum, limitParamNum, false)
 }
