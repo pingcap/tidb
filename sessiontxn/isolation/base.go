a9e8055e1046d7fd2991a589ea491c8dd1d1e68c
Weizhen Wang <wangweizhen@pingcap.com>
2023-04-12 10:37:01 +0800

    *: fix data race in the baseTxnContextProvider.ActivateTxn (#42960)
    close pingcap/tidb#42092

--- sessiontxn/isolation/base.go
+++ sessiontxn/isolation/base.go
@@ -288,7 +288,9 @@ func (p *baseTxnContextProvider) ActivateTxn() (kv.Transaction, error) {
 	}
 
 	sessVars := p.sctx.GetSessionVars()
+	sessVars.TxnCtxMu.Lock()
 	sessVars.TxnCtx.StartTS = txn.StartTS()
+	sessVars.TxnCtxMu.Unlock()
 	if sessVars.MemDBFootprint != nil {
 		sessVars.MemDBFootprint.Detach()
 	}
