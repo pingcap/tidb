263f51b884722e3e03470a981f5038bfdfccc023
wjhuang2016 <huangwenjun1997@gmail.com>
2023-04-13 17:30:31 +0800

    done
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- disttask/framework/scheduler/interface_mock.go
+++ disttask/framework/scheduler/interface_mock.go
@@ -64,7 +64,7 @@ func (t *MockTaskTable) GetSubtaskInStates(instanceID string, taskID int64, stat
 }
 
 // UpdateSubtaskStateAndError implements SubtaskTable.UpdateSubtaskState.
-func (t *MockTaskTable) UpdateSubtaskStateAndError(id int64, state string, err string) error {
+func (t *MockTaskTable) UpdateSubtaskStateAndError(id int64, state string, _ string) error {
 	args := t.Called(id, state)
 	return args.Error(0)
 }
