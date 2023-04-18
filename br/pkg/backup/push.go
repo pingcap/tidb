324780526140d6b495414227f08f0e6745c6d3d5
hillium <yujuncen@pingcap.com>
2023-04-18 10:39:44 +0800

    move `CheckStoreLiveness` to utils
    Signed-off-by: hillium <yujuncen@pingcap.com>

--- br/pkg/backup/push.go
+++ br/pkg/backup/push.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/pingcap/errors"
@@ -21,11 +20,6 @@ import (
 	"github.com/pingcap/tidb/br/pkg/rtree"
 	"github.com/pingcap/tidb/br/pkg/utils"
 	"go.uber.org/zap"
-)
-
-const (
-	// from PD: https://github.com/tikv/pd/blob/c40e319f50822678cda71ae62ee2fd70a9cac010/pkg/core/store.go#L523
-	storeDisconnectionDuration = 20 * time.Second
 )
 
 // pushDown wraps a backup task.
@@ -57,20 +51,6 @@ func newPushDown(mgr ClientMgr, capacity int) *pushDown {
 	}
 }
 
-func checkStoreLiveness(s *metapb.Store) error {
-	if s.State != metapb.StoreState_Up {
-		return errors.Annotatef(berrors.ErrKVStorage, "the store state isn't up, it is %s", s.State)
-	}
-	// If the field isn't present (the default value), skip this check.
-	if s.GetLastHeartbeat() > 0 {
-		lastHeartBeat := time.Unix(0, s.GetLastHeartbeat())
-		if sinceLastHB := time.Since(lastHeartBeat); sinceLastHB > storeDisconnectionDuration {
-			return errors.Annotatef(berrors.ErrKVStorage, "the store last heartbeat is too long ago, it is %s", sinceLastHB)
-		}
-	}
-	return nil
-}
-
 // FullBackup make a full backup of a tikv cluster.
 func (push *pushDown) pushBackup(
 	ctx context.Context,
@@ -97,7 +77,7 @@ func (push *pushDown) pushBackup(
 		store := s
 		storeID := s.GetId()
 		lctx := logutil.ContextWithField(ctx, zap.Uint64("store-id", storeID))
-		if err := checkStoreLiveness(s); err != nil {
+		if err := utils.CheckStoreLiveness(s); err != nil {
 			logutil.CL(lctx).Warn("skip store", logutil.ShortError(err))
 			continue
 		}
