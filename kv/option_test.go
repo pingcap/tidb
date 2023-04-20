69166aa01b91a8c200cfc1c88d099a1c4025d61d
hi-rustin <rustin.liu@gmail.com>
2023-04-20 16:33:00 +0800

    ddl: add more tests

--- kv/option_test.go
+++ kv/option_test.go
@@ -12,6 +12,7 @@ func TestSetCdcWriteSource(t *testing.T) {
 		cdcWriteSource         uint64
 		expectedSet            bool
 		expectedCdcWriteSource uint64
+		expectedError          string
 	}{
 		{
 			name:                   "cdc write source is set",
@@ -25,10 +26,19 @@ func TestSetCdcWriteSource(t *testing.T) {
 			expectedSet:            false,
 			expectedCdcWriteSource: 0,
 		},
+		{
+			name:           "cdc write source is not valid",
+			cdcWriteSource: 16,
+			expectedError:  ".*out of TiCDC write source range.*",
+		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			var txnOption uint64
 			err := SetCdcWriteSource(&txnOption, tc.cdcWriteSource)
+			if tc.expectedError != "" {
+				require.Regexp(t, tc.expectedError, err.Error())
+				return
+			}
 			require.NoError(t, err)
 			require.Equal(t, tc.expectedSet, isCdcWriteSourceSet(txnOption))
 			require.Equal(t, tc.expectedCdcWriteSource, getCdcWriteSource(txnOption))
@@ -43,6 +53,7 @@ func TestSetLossyDDLReorgSource(t *testing.T) {
 		lossyDDLReorgSource         uint64
 		expectedSet                 bool
 		expectedLossyDDLReorgSource uint64
+		expectedError               string
 	}{
 		{
 			name:                        "lossy ddl reorg source is set",
@@ -58,9 +69,19 @@ func TestSetLossyDDLReorgSource(t *testing.T) {
 			expectedSet:                 false,
 			expectedLossyDDLReorgSource: 0,
 		},
+		{
+			name:                "lossy ddl reorg source is not valid",
+			currentSource:       12, // SetCdcWriteSource
+			lossyDDLReorgSource: 16,
+			expectedError:       ".*out of lossy DDL reorg source range.*",
+		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			err := SetLossyDDLReorgSource(&tc.currentSource, tc.lossyDDLReorgSource)
+			if tc.expectedError != "" {
+				require.Regexp(t, tc.expectedError, err.Error())
+				return
+			}
 			require.NoError(t, err)
 			require.Equal(t, tc.expectedSet, isLossyDDLReorgSourceSet(tc.currentSource))
 			require.Equal(t, tc.expectedLossyDDLReorgSource, getLossyDDLReorgSource(tc.currentSource))
