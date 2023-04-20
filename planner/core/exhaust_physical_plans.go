ddfbed66e6163f28af7f02f0be03187b8c67956a
yibin <huyibin@pingcap.com>
2023-04-20 13:41:27 +0800

    Update logic and tests
    Signed-off-by: yibin <huyibin@pingcap.com>

--- planner/core/exhaust_physical_plans.go
+++ planner/core/exhaust_physical_plans.go
@@ -2398,14 +2398,13 @@ func (p *LogicalJoin) tryToGetMppHashJoin(prop *property.PhysicalProperty, useBC
 			preferredBuildIndex = 1
 		}
 	} else if p.JoinType.IsSemiJoin() {
-		if !p.isNAAJ() && len(p.EqualConditions) > 0 && (p.JoinType == SemiJoin || p.JoinType == AntiSemiJoin) {
+		if !useBCJ && !p.isNAAJ() && len(p.EqualConditions) > 0 && (p.JoinType == SemiJoin || p.JoinType == AntiSemiJoin) {
 			// TiFlash only supports Non-null_aware non-cross semi/anti_semi join to use both sides as build side
 			preferredBuildIndex = 1
 			// MPPOuterJoinFixedBuildSide default value is false
 			// use MPPOuterJoinFixedBuildSide here as a way to disable using left table as build side
 			if !p.ctx.GetSessionVars().MPPOuterJoinFixedBuildSide && p.children[1].statsInfo().Count() > p.children[0].statsInfo().Count() {
 				preferredBuildIndex = 0
-				useBCJ = false
 			}
 		} else {
 			preferredBuildIndex = 1
