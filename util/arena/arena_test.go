52ac46a784551ff230d1df0fa12bc5d2f354f8f1
Yujie Xia <xiayjchn@gmail.com>
2023-04-21 17:29:31 +0800

    util/extsort: introduce external sorter

--- util/arena/arena_test.go
+++ util/arena/arena_test.go
@@ -24,6 +24,7 @@ const (
 	arenaCap       = 1000
 	allocCapSmall  = 10
 	allocCapMedium = 20
+	allocCapLarge  = 980
 	allocCapOut    = 1024
 )
 
@@ -48,6 +49,10 @@ func TestSimpleArenaAllocator(t *testing.T) {
 	assert.Equal(t, allocCapSmall+allocCapMedium+allocCapSmall, arena.off)
 	assert.Len(t, slice, 2)
 	assert.Equal(t, allocCapSmall, cap(slice))
+
+	slice = arena.AllocWithLen(2, allocCapLarge)
+	assert.Equal(t, allocCapLarge, arena.off)
+	assert.Len(t, slice, 2)
 
 	arena.Reset()
 	assert.Zero(t, arena.off)
---
+++ util/extsort/external_sorter.go
@@ -0,0 +1,81 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package extsort
+
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrSorted    = errors.New("already sorted")
+	ErrNotSorted = errors.New("not sorted")
+)
+
+// ExternalSorter is an interface for external sorting.
+type ExternalSorter interface {
+	// NewWriter creates a new writer for writing key/value pairs before sorting.
+	// It should be called before calling Sort().
+	NewWriter(ctx context.Context) (Writer, error)
+	// Sort sorts the key/value pairs written by the writer.
+	// It should be called after all open writers are closed.
+	Sort(ctx context.Context) error
+	// NewIterator creates a new iterator for iterating over the key/value pairs after sorting.
+	// It should be called after calling Sort().
+	NewIterator(ctx context.Context) (Iterator, error)
+	// Close releases running resources held by the external sorter.
+	Close() error
+	// CloseAndCleanup closes the external sorter and cleans up all resources created by the sorter.
+	CloseAndCleanup() error
+}
+
+// Writer is an interface for writing key/value pairs to the external sorter.
+type Writer interface {
+	// Put adds a key/value pair to the writer.
+	Put(key, value []byte) error
+	// Size returns the total size of buffered key/value pairs.
+	Size() int64
+	// Flush flushes all buffered key/value pairs to the external sorter.
+	// the writer can be reused after calling Flush().
+	Flush() error
+	// Close flushes remaining buffered key/value pairs to the external sorter,
+	// and releases all resources held by the writer.
+	Close() error
+}
+
+// Iterator is an interface for iterating over the key/value pairs after sorting.
+type Iterator interface {
+	// Seek moves the iterator to the first key/value pair whose key is greater
+	// than or equal to the given key.
+	Seek(key []byte) bool
+	// First moves the iterator to the first key/value pair.
+	First() bool
+	// Next moves the iterator to the next key/value pair.
+	Next() bool
+	// Last moves the iterator to the last key/value pair.
+	Last() bool
+	// Valid returns true if the iterator is positioned at a valid key/value pair.
+	Valid() bool
+	// Error returns the error, if any, that was encountered during iteration.
+	Error() error
+	// UnsafeKey returns the key of the current key/value pair, without copying.
+	// The memory is only valid until the next call to change the iterator.
+	UnsafeKey() []byte
+	// UnsafeValue returns the value of the current key/value pair, without copying.
+	// The memory is only valid until the next call to change the iterator.
+	UnsafeValue() []byte
+	// Close releases all resources held by the iterator.
+	Close() error
+}
