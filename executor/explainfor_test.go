69125ad39b0f3ea6e835569941e86149604819b7
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2023-05-09 10:36:58 +0800

    planner: enable constant propagation for plan cache parameters (#43009)
    close pingcap/tidb#43520, close pingcap/tidb#43522

--- executor/explainfor_test.go
+++ executor/explainfor_test.go
@@ -462,12 +462,11 @@ func TestPointGetUserVarPlanCache(t *testing.T) {
 	ps := []*util.ProcessInfo{tkProcess}
 	tk.Session().SetSessionManager(&testkit.MockSessionManager{PS: ps})
 	tk.MustQuery(fmt.Sprintf("explain for connection %d", tkProcess.ID)).Check(testkit.Rows( // can use idx_a
-		`Projection_9 1.00 root  test.t1.a, test.t1.b, test.t2.a, test.t2.b`,
-		`└─MergeJoin_10 1.00 root  inner join, left key:test.t2.a, right key:test.t1.a`,
-		`  ├─TableReader_41(Build) 10.00 root  data:TableRangeScan_40`,
-		`  │ └─TableRangeScan_40 10.00 cop[tikv] table:t1 range:[1,1], keep order:true, stats:pseudo`,
-		`  └─Selection_39(Probe) 0.80 root  not(isnull(test.t2.a))`,
-		`    └─Point_Get_38 1.00 root table:t2, index:idx_a(a) `))
+		`Projection_9 10.00 root  test.t1.a, test.t1.b, test.t2.a, test.t2.b`,
+		`└─HashJoin_11 10.00 root  CARTESIAN inner join`,
+		`  ├─Point_Get_12(Build) 1.00 root table:t2, index:idx_a(a) `, // use idx_a
+		`  └─TableReader_14(Probe) 10.00 root  data:TableRangeScan_13`,
+		`    └─TableRangeScan_13 10.00 cop[tikv] table:t1 range:[1,1], keep order:false, stats:pseudo`))
 
 	tk.MustExec("set @a=2")
 	tk.MustQuery("execute stmt using @a").Check(testkit.Rows(
@@ -477,12 +476,11 @@ func TestPointGetUserVarPlanCache(t *testing.T) {
 	ps = []*util.ProcessInfo{tkProcess}
 	tk.Session().SetSessionManager(&testkit.MockSessionManager{PS: ps})
 	tk.MustQuery(fmt.Sprintf("explain for connection %d", tkProcess.ID)).Check(testkit.Rows( // can use idx_a
-		`Projection_9 1.00 root  test.t1.a, test.t1.b, test.t2.a, test.t2.b`,
-		`└─MergeJoin_10 1.00 root  inner join, left key:test.t2.a, right key:test.t1.a`,
-		`  ├─TableReader_41(Build) 10.00 root  data:TableRangeScan_40`,
-		`  │ └─TableRangeScan_40 10.00 cop[tikv] table:t1 range:[2,2], keep order:true, stats:pseudo`,
-		`  └─Selection_39(Probe) 0.80 root  not(isnull(test.t2.a))`,
-		`    └─Point_Get_38 1.00 root table:t2, index:idx_a(a) `))
+		`Projection_9 10.00 root  test.t1.a, test.t1.b, test.t2.a, test.t2.b`,
+		`└─HashJoin_11 10.00 root  CARTESIAN inner join`,
+		`  ├─Point_Get_12(Build) 1.00 root table:t2, index:idx_a(a) `,
+		`  └─TableReader_14(Probe) 10.00 root  data:TableRangeScan_13`,
+		`    └─TableRangeScan_13 10.00 cop[tikv] table:t1 range:[2,2], keep order:false, stats:pseudo`))
 	tk.MustQuery("execute stmt using @a").Check(testkit.Rows(
 		"2 4 2 2",
 	))
