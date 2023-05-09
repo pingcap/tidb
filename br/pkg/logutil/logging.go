3bd470be05d7f6a891fd5507095647a8b5f453b9
Leavrth <jianjun.liao@outlook.com>
2023-05-09 13:38:33 +0800

    fix unit test
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- br/pkg/logutil/logging.go
+++ br/pkg/logutil/logging.go
@@ -63,6 +63,7 @@ func (file zapFileMarshaler) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 
 type zapFilesMarshaler []*backuppb.File
 
+// MarshalLogObjectForFiles is an internal util function to zap something having `Files` field.
 func MarshalLogObjectForFiles(files []*backuppb.File, encoder zapcore.ObjectEncoder) error {
 	return zapFilesMarshaler(files).MarshalLogObject(encoder)
 }
