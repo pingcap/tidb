b255562ee0dafbf6b8557d3911626d3a1493b205
Lynn <zimu_xia@126.com>
2023-05-10 22:33:40 +0800

    .: add skip check mBootstrapKey when WithMockUpgrade is true

--- session/session.go
+++ session/session.go
@@ -3650,7 +3650,7 @@ func getStoreBootstrapVersion(store kv.Storage) int64 {
 		storeBootstrapped[store.UUID()] = true
 	}
 
-	return ver
+	return modifyBootstrapVersionForTest(store, ver)
 }
 
 func finishBootstrap(store kv.Storage) {
