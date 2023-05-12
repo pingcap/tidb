0b8baaea25571685079644aa08906a38d6a62f6f
guo-shaoge <shaoge1994@163.com>
2023-05-12 16:30:49 +0800

    fix err
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- executor/cte.go
+++ executor/cte.go
@@ -236,7 +236,7 @@ func (e *CTEExec) computeSeedPart(ctx context.Context) (err error) {
 			err = errors.Errorf("%v", r)
 		}
 	}()
-	failpoint.Inject("testCTEPanic", nil)
+	failpoint.Inject("testCTESeedPanic", nil)
 	e.curIter = 0
 	e.iterInTbl.SetIter(e.curIter)
 	chks := make([]*chunk.Chunk, 0, 10)
@@ -275,6 +275,7 @@ func (e *CTEExec) computeRecursivePart(ctx context.Context) (err error) {
 			err = errors.Errorf("%v", r)
 		}
 	}()
+	failpoint.Inject("testCTERecursivePanic", nil)
 	if e.recursiveExec == nil || e.iterInTbl.NumChunks() == 0 {
 		return
 	}
