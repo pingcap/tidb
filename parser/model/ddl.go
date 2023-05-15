0b2745ffd1d697f14dde4cd4be4984e5d7f33dc3
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-05-15 20:23:17 +0800

    ddl: wait schema change before rename table job is done (#43341) (#43456)
    close pingcap/tidb#43338

--- parser/model/ddl.go
+++ parser/model/ddl.go
@@ -836,7 +836,7 @@ func (job *Job) IsRollbackable() bool {
 		ActionDropForeignKey, ActionDropTablePartition:
 		return job.SchemaState == StatePublic
 	case ActionRebaseAutoID, ActionShardRowID,
-		ActionTruncateTable, ActionAddForeignKey, ActionRenameTable,
+		ActionTruncateTable, ActionAddForeignKey, ActionRenameTable, ActionRenameTables,
 		ActionModifyTableCharsetAndCollate, ActionTruncateTablePartition,
 		ActionModifySchemaCharsetAndCollate, ActionRepairTable,
 		ActionModifyTableAutoIdCache, ActionModifySchemaDefaultPlacement:
