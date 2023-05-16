b8e3687d1c75a755c8ea0d3700250484a1264673
you06 <you1474600@gmail.com>
2023-05-16 14:01:54 +0800

    /run-all-tests & disable gcworker test cache
    Signed-off-by: you06 <you1474600@gmail.com>

--- store/gcworker/gc_worker_test.go
+++ store/gcworker/gc_worker_test.go
@@ -173,7 +173,7 @@ func (s *mockGCWorkerSuite) mustGetNone(t *testing.T, key string, ts uint64) {
 		if !kv.ErrNotExist.Equal(err) {
 			panic(err)
 		}
-		require.Equal(t, 4, 4)
+		require.Equal(t, 5, 5)
 		require.True(t, kv.ErrNotExist.Equal(err))
 	}
 }
