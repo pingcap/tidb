69c7651fced8bd30ede5b4b34dc00f52d99b8723
D3Hunter <jujj603@gmail.com>
2023-05-18 12:03:35 +0800

    lightning: refactor switch mode for reuse (#43909)
    ref pingcap/tidb#42930

--- br/pkg/lightning/importer/import.go
+++ br/pkg/lightning/importer/import.go
@@ -31,7 +31,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
-	sstpb "github.com/pingcap/kvproto/pkg/import_sstpb"
 	berrors "github.com/pingcap/tidb/br/pkg/errors"
 	"github.com/pingcap/tidb/br/pkg/lightning/backend"
 	"github.com/pingcap/tidb/br/pkg/lightning/backend/encode"
@@ -231,6 +230,7 @@ type Controller struct {
 	preInfoGetter       PreImportInfoGetter
 	precheckItemBuilder *PrecheckItemBuilder
 	encBuilder          encode.EncodingBuilder
+	tikvModeSwitcher    *local.TiKVModeSwitcher
 
 	keyspaceName string
 }
@@ -458,6 +458,7 @@ func NewImportControllerWithPauser(
 		preInfoGetter:       preInfoGetter,
 		precheckItemBuilder: preCheckBuilder,
 		encBuilder:          encodingBuilder,
+		tikvModeSwitcher:    local.NewTiKVModeSwitcher(tls, cfg.TiDB.PdAddr, log.FromContext(ctx).Logger),
 
 		keyspaceName: p.KeyspaceName,
 	}
@@ -1171,12 +1172,12 @@ func (rc *Controller) buildRunPeriodicActionAndCancelFunc(ctx context.Context, s
 
 	var switchModeChan <-chan time.Time
 	// tidb backend don't need to switch tikv to import mode
-	if rc.cfg.TikvImporter.Backend != config.BackendTiDB && rc.cfg.Cron.SwitchMode.Duration > 0 {
+	if isLocalBackend(rc.cfg) && rc.cfg.Cron.SwitchMode.Duration > 0 {
 		switchModeTicker := time.NewTicker(rc.cfg.Cron.SwitchMode.Duration)
 		cancelFuncs = append(cancelFuncs, func(bool) { switchModeTicker.Stop() })
 		cancelFuncs = append(cancelFuncs, func(do bool) {
 			if do {
-				rc.switchToNormalMode(ctx)
+				rc.tikvModeSwitcher.ToNormalMode(ctx)
 			}
 		})
 		switchModeChan = switchModeTicker.C
@@ -1196,8 +1197,8 @@ func (rc *Controller) buildRunPeriodicActionAndCancelFunc(ctx context.Context, s
 					f()
 				}
 			}()
-			if rc.cfg.Cron.SwitchMode.Duration > 0 {
-				rc.switchToImportMode(ctx)
+			if rc.cfg.Cron.SwitchMode.Duration > 0 && isLocalBackend(rc.cfg) {
+				rc.tikvModeSwitcher.ToImportMode(ctx)
 			}
 			start := time.Now()
 			for {
@@ -1211,7 +1212,7 @@ func (rc *Controller) buildRunPeriodicActionAndCancelFunc(ctx context.Context, s
 
 				case <-switchModeChan:
 					// periodically switch to import mode, as requested by TiKV 3.0
-					rc.switchToImportMode(ctx)
+					rc.tikvModeSwitcher.ToImportMode(ctx)
 
 				case <-logProgressChan:
 					metrics, ok := metric.FromContext(ctx)
@@ -1916,45 +1917,6 @@ func (rc *Controller) doCompact(ctx context.Context, level int32) error {
 		tikv.StoreStateDisconnected,
 		func(c context.Context, store *tikv.Store) error {
 			return tikv.Compact(c, tls, store.Address, level)
-		},
-	)
-}
-
-func (rc *Controller) switchToImportMode(ctx context.Context) {
-	rc.switchTiKVMode(ctx, sstpb.SwitchMode_Import)
-}
-
-func (rc *Controller) switchToNormalMode(ctx context.Context) {
-	rc.switchTiKVMode(ctx, sstpb.SwitchMode_Normal)
-}
-
-func (rc *Controller) switchTiKVMode(ctx context.Context, mode sstpb.SwitchMode) {
-	// // tidb backend don't need to switch tikv to import mode
-	if isTiDBBackend(rc.cfg) {
-		return
-	}
-
-	log.FromContext(ctx).Info("switch import mode", zap.Stringer("mode", mode))
-
-	// It is fine if we miss some stores which did not switch to Import mode,
-	// since we're running it periodically, so we exclude disconnected stores.
-	// But it is essential all stores be switched back to Normal mode to allow
-	// normal operation.
-	var minState tikv.StoreState
-	if mode == sstpb.SwitchMode_Import {
-		minState = tikv.StoreStateOffline
-	} else {
-		minState = tikv.StoreStateDisconnected
-	}
-	tls := rc.tls.WithHost(rc.cfg.TiDB.PdAddr)
-	// we ignore switch mode failure since it is not fatal.
-	// no need log the error, it is done in kv.SwitchMode already.
-	_ = tikv.ForAllStores(
-		ctx,
-		tls,
-		minState,
-		func(c context.Context, store *tikv.Store) error {
-			return tikv.SwitchMode(c, tls, store.Address, mode)
 		},
 	)
 }
