180d80e7c66d4ebc653dcaa840838d1f0563c39c
Shenghui Wu <793703860@qq.com>
2023-05-18 18:43:43 +0800

    planner, expression: fix wrong fieldtype after aggregationPushDownSolver (#43906)
    close pingcap/tidb#43805

--- expression/integration_test.go
+++ expression/integration_test.go
@@ -10599,3 +10599,16 @@ func (s *testIntegrationSuite) TestIssue33397(c *C) {
 	tk.MustExec("set @@tidb_enable_vectorized_expression = true;")
 	tk.MustQuery("select compress(a) from t").Check(testkit.Rows("", ""))
 }
+
+func TestIfFunctionWithNull(t *testing.T) {
+	// issue 43805
+	store := testkit.CreateMockStore(t)
+
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists ordres;")
+	tk.MustExec("CREATE TABLE orders (id bigint(20) unsigned NOT NULL ,account_id bigint(20) unsigned NOT NULL DEFAULT '0' ,loan bigint(20) unsigned NOT NULL DEFAULT '0' ,stage_num int(20) unsigned NOT NULL DEFAULT '0' ,apply_time bigint(20) unsigned NOT NULL DEFAULT '0' ,PRIMARY KEY (id) /*T![clustered_index] CLUSTERED */,KEY idx_orders_account_id (account_id),KEY idx_orders_apply_time (apply_time));")
+	tk.MustExec("insert into orders values (20, 210802010000721168, 20000 , 2 , 1682484268727), (22, 210802010000721168, 35100 , 4 , 1650885615002);")
+	tk.MustQuery("select min(if(apply_to_now_days <= 30,loan,null)) as min, max(if(apply_to_now_days <= 720,loan,null)) as max from (select loan, datediff(from_unixtime(unix_timestamp() + 18000), from_unixtime(apply_time/1000 + 18000)) as apply_to_now_days from orders) t1;").Sort().Check(
+		testkit.Rows("20000 35100"))
+}
