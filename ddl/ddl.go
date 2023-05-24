c1fc2752a964858c0a8ecea42e980e8aacdc1b35
wjhuang2016 <huangwenjun1997@gmail.com>
2023-05-24 17:29:05 +0800

    refine
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -1474,11 +1474,6 @@ func pauseRunningJob(sess *sess.Session, job *model.Job,
 
 	job.State = model.JobStatePausing
 	job.AdminOperator = byWho
-
-	if job.RawArgs == nil {
-		return nil
-	}
-
 	return nil
 }
 
