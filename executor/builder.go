de6cba32df542cadf0dfd97ed1145a8b4d1c6ce9
Hangjie Mo <mohangjie1995@gmail.com>
2023-05-31 11:30:42 +0800

    This is an automated cherry-pick of #44283
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- executor/builder.go
+++ executor/builder.go
@@ -3843,20 +3843,30 @@ func buildIndexReq(ctx sessionctx.Context, columns []*model.IndexColumn, handleL
 	}
 
 	indexReq.OutputOffsets = []uint32{}
+<<<<<<< HEAD
 	if len(plans[0].(*plannercore.PhysicalIndexScan).ByItems) != 0 {
 		idxScan := plans[0].(*plannercore.PhysicalIndexScan)
 		tblInfo := idxScan.Table
+=======
+	idxScan := plans[0].(*plannercore.PhysicalIndexScan)
+	if len(idxScan.ByItems) != 0 {
+		schema := idxScan.Schema()
+>>>>>>> 3a489e4fb38 (executor: found column by idxScan.schema instead of idxScan.Table (#44283))
 		for _, item := range idxScan.ByItems {
 			c, ok := item.Expr.(*expression.Column)
 			if !ok {
 				return nil, errors.Errorf("Not support non-column in orderBy pushed down")
 			}
-			column := model.FindColumnInfoByID(tblInfo.Columns, c.ID)
-			for i, idxColumn := range columns {
-				if idxColumn.Name.L == column.Name.L {
+			find := false
+			for i, schemaColumn := range schema.Columns {
+				if schemaColumn.ID == c.ID {
 					indexReq.OutputOffsets = append(indexReq.OutputOffsets, uint32(i))
+					find = true
 					break
 				}
+			}
+			if !find {
+				return nil, errors.Errorf("Not found order by related columns in indexScan.schema")
 			}
 		}
 	}
