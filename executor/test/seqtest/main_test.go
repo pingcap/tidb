0627e34e147048e027b1e93fd8a8365204def621
Yongbo Jiang <cabinfeveroier@gmail.com>
2023-05-31 19:16:42 +0800

    *: Introduce runaway statement in resource group (#43843)
    ref pingcap/tidb#43691

--- executor/test/seqtest/main_test.go
+++ executor/test/seqtest/main_test.go
@@ -29,7 +29,7 @@ func TestMain(m *testing.M) {
 		conf.TiKVClient.AsyncCommit.AllowedClockDrift = 0
 	})
 	opts := []goleak.Option{
-		goleak.IgnoreTopFunction("github.com/golang/glog.(*loggingT).flushDaemon"),
+		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
 		goleak.IgnoreTopFunction("github.com/pingcap/tidb/executor.readProjection[...]"),
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
