cf696ae5d569654c8e27e5f19ffe9d9d9fe4acb1
Yifan Xu <30385241+xuyifangreeneyes@users.noreply.github.com>
2023-06-01 17:44:42 +0800

    statistics: fix unnecessary index fmsketch loading (#44330)
    close pingcap/tidb#42052

--- statistics/handle/handle.go
+++ statistics/handle/handle.go
@@ -1250,7 +1250,7 @@ func (h *Handle) fmSketchFromStorage(reader *statsReader, tblID int64, isIndex, 
 	return statistics.DecodeFMSketch(rows[0].GetBytes(0))
 }
 
-func (h *Handle) indexStatsFromStorage(reader *statsReader, row chunk.Row, table *statistics.Table, tableInfo *model.TableInfo) error {
+func (h *Handle) indexStatsFromStorage(reader *statsReader, row chunk.Row, table *statistics.Table, tableInfo *model.TableInfo, loadAll bool) error {
 	histID := row.GetInt64(2)
 	distinct := row.GetInt64(3)
 	histVer := row.GetUint64(4)
@@ -1278,9 +1278,14 @@ func (h *Handle) indexStatsFromStorage(reader *statsReader, row chunk.Row, table
 			if err != nil {
 				return errors.Trace(err)
 			}
-			fmSketch, err := h.fmSketchFromStorage(reader, table.PhysicalID, 1, histID)
-			if err != nil {
-				return errors.Trace(err)
+			var fmSketch *statistics.FMSketch
+			if loadAll {
+				// FMSketch is only used when merging partition stats into global stats. When merging partition stats into global stats,
+				// we load all the statistics, i.e., loadAll is true.
+				fmSketch, err = h.fmSketchFromStorage(reader, table.PhysicalID, 1, histID)
+				if err != nil {
+					return errors.Trace(err)
+				}
 			}
 			idx = &statistics.Index{
 				Histogram:  *hg,
@@ -1477,7 +1482,7 @@ func (h *Handle) TableStatsFromStorage(tableInfo *model.TableInfo, physicalID in
 	}
 	for _, row := range rows {
 		if row.GetInt64(1) > 0 {
-			err = h.indexStatsFromStorage(reader, row, table, tableInfo)
+			err = h.indexStatsFromStorage(reader, row, table, tableInfo, loadAll)
 		} else {
 			err = h.columnStatsFromStorage(reader, row, table, tableInfo, loadAll)
 		}
