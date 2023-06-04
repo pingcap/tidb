92e1ff134cf290a2793799b1a3c78f1c12ee0c0b
Weizhen Wang <wangweizhen@pingcap.com>
2023-06-04 18:22:44 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- statistics/handle/handle.go
+++ statistics/handle/handle.go
@@ -1005,7 +1005,7 @@ func (h *Handle) GetPartitionStats(tblInfo *model.TableInfo, pid int64, opts ...
 	if !ok {
 		tbl = statistics.PseudoTable(tblInfo)
 		tbl.PhysicalID = pid
-		if tblInfo.GetPartitionInfo() == nil && h.statsCacheLen() < 258 {
+		if tblInfo.GetPartitionInfo() == nil || h.statsCacheLen() < 64 {
 			h.updateStatsCache(statsCache.update([]*statistics.Table{tbl}, nil, statsCache.version))
 		}
 		return tbl
