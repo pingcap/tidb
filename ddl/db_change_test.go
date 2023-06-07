ff513df0ada6975d6fa6abd84bf921baaa80b2c9
xhe <xw897002528@gmail.com>
2023-06-07 14:32:39 +0800

    fix test
    Signed-off-by: xhe <xw897002528@gmail.com>

--- ddl/db_change_test.go
+++ ddl/db_change_test.go
@@ -652,7 +652,7 @@ func TestDeleteOnly(t *testing.T) {
 
 	sqls := make([]sqlWithErr, 5)
 	sqls[0] = sqlWithErr{"insert t set c1 = 'c1_insert', c3 = '2018-02-12', c4 = 1",
-		errors.Errorf("[ddl:1054]Unknown column 'c1' in 'field list'")}
+		errors.Errorf("[planner:1054]Unknown column 'c1' in 'field list'")}
 	sqls[1] = sqlWithErr{"update t set c1 = 'c1_insert', c3 = '2018-02-12', c4 = 1",
 		errors.Errorf("[planner:1054]Unknown column 'c1' in 'field list'")}
 	sqls[2] = sqlWithErr{"delete from t where c1='a'",
