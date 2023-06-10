c52f78d1aa52a573de90ad1f262db78fa919d16b
Weizhen Wang <wangweizhen@pingcap.com>
2023-06-10 15:34:36 +0800

    executor: fix data race at the TestShowStatsExtended
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- executor/show_stats_test.go
+++ executor/show_stats_test.go
@@ -280,8 +280,10 @@ func TestShowStatsExtended(t *testing.T) {
 	store, dom := testkit.CreateMockStoreAndDomain(t)
 
 	tk := testkit.NewTestKit(t, store)
-	dom.StatsHandle().Clear()
+	tk.MustExec("set @@global.tidb_ttl_job_enable=0")
+
 	tk.MustExec("use test")
+	dom.StatsHandle().Clear()
 	tk.MustExec("drop table if exists t")
 	tk.MustExec("create table t (a int, b int, c int)")
 	tk.MustExec("insert into t values(1,1,3),(2,2,2),(3,3,1)")
