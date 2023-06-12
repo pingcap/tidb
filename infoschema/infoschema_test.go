c6348e2d5b8c8fe51b46fb51f60a7c8468eb1465
tangenta <tangenta@126.com>
2023-06-12 20:49:53 +0800

    resolve conflicts

--- infoschema/infoschema_test.go
+++ infoschema/infoschema_test.go
@@ -813,16 +813,6 @@ func TestIndexComment(t *testing.T) {
 	tk.MustExec("create table t1 (c1 VARCHAR(10) NOT NULL COMMENT 'Abcdefghijabcd', c2 INTEGER COMMENT 'aBcdefghijab',c3 INTEGER COMMENT '01234567890', c4 INTEGER, c5 INTEGER, c6 INTEGER, c7 INTEGER, c8 VARCHAR(100), c9 CHAR(50), c10 DATETIME, c11 DATETIME, c12 DATETIME,c13 DATETIME, INDEX i1 (c1) COMMENT 'i1 comment',INDEX i2(c2) ) COMMENT='ABCDEFGHIJabc';")
 	tk.MustQuery("SELECT index_comment,char_length(index_comment),COLUMN_NAME FROM information_schema.statistics WHERE table_name='t1' ORDER BY index_comment;").Check(testkit.Rows(" 0 c2", "i1 comment 10 c1"))
 }
-<<<<<<< HEAD
-=======
-
-func TestIssue42400(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustQuery("show create table information_schema.ddl_jobs").CheckContain("`QUERY` text")
-	tk.MustQuery("select length(query) from information_schema.ddl_jobs;") // No error
-}
 
 func TestInfoSchemaRenameTable(t *testing.T) {
 	store := testkit.CreateMockStore(t)
@@ -843,4 +833,3 @@ func TestInfoSchemaRenameTable(t *testing.T) {
 	tk.MustQuery("SELECT count(*) FROM information_schema.TABLES WHERE (TABLE_SCHEMA = 'mysql') AND (TABLE_NAME = 't3');").
 		Check(testkit.Rows("1"))
 }
->>>>>>> 5d05714eae5 (infoschema: update raw args to make it drop correct table in `RENAME TABLE` (#44585))
