4b2393e8b3bfe524c8c10c8685020164515d667d
D3Hunter <jujj603@gmail.com>
2023-06-12 23:50:10 +0800

    change

--- disttask/importinto/subtask_executor.go
+++ disttask/importinto/subtask_executor.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package loaddata
+package importinto
 
 import (
 	"context"
@@ -30,7 +30,7 @@ import (
 // TestSyncChan is used to test.
 var TestSyncChan = make(chan struct{})
 
-// ImportMinimalTaskExecutor is a subtask executor for load data.
+// ImportMinimalTaskExecutor is a minimal task executor for IMPORT INTO.
 type ImportMinimalTaskExecutor struct {
 	task *MinimalTaskMeta
 }
