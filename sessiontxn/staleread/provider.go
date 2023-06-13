7c81cfdf42c014d51494a0a60988acce26a63df6
glorv <glorvs@163.com>
2023-06-13 13:13:20 +0800

    add rg name for staleness txn provider

--- sessiontxn/staleread/provider.go
+++ sessiontxn/staleread/provider.go
@@ -114,6 +114,7 @@ func (p *StalenessTxnContextProvider) activateStaleTxn() error {
 	txn.SetVars(sessVars.KVVars)
 	txn.SetOption(kv.IsStalenessReadOnly, true)
 	txn.SetOption(kv.TxnScope, txnScope)
+	txn.SetOption(kv.ResourceGroupName, p.sctx.GetSessionVars().ResourceGroupName)
 	internal.SetTxnAssertionLevel(txn, sessVars.AssertionLevel)
 	is, err := GetSessionSnapshotInfoSchema(p.sctx, p.ts)
 	if err != nil {
