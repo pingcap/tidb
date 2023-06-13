af1dcd7a7ba3d0dbeeb36430c3548c075d054105
MyonKeminta <MyonKeminta@users.noreply.github.com>
2023-06-13 12:05:30 +0800

    update check-bazel-prepare.sh to show more friendly update suggestion
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- tools/check/check-bazel-prepare.sh
+++ tools/check/check-bazel-prepare.sh
@@ -25,8 +25,10 @@ make bazel_ci_prepare
 after_checksum=`find . -type f \( -name *.bazel -o -name *.bzl \) -exec md5sum {} \;| sort -k 2`
 if [ "$before_checksum" != "$after_checksum" ]
 then
-  echo "Please run \`make bazel_prepare\` to update \`.bazel\` files"
+  echo "Please run \`make bazel_prepare\` to update \`.bazel\` files, or just apply the following git diff:"
+  echo
   git diff
+  echo -e "\nChecksum diff:"
   diff <(echo "$before_checksum") <(echo "$after_checksum")
   exit 1
 fi
