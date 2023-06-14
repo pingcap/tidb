4ad2ee5abfdf6040891f92968ba4b8d7a403e224
Connor1996 <zbk602423539@gmail.com>
2023-06-13 22:00:13 -0700

    skip error
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- domain/domain.go
+++ domain/domain.go
@@ -1245,7 +1245,9 @@ func (do *Domain) SetOnClose(onClose func()) {
 
 func (do *Domain) initResourceGroupsController(ctx context.Context, pdClient pd.Client) error {
 	if pdClient == nil {
-		return errors.New("cannot setup up resource controller, should use tikv storage")
+		logutil.BgLogger().Warn("cannot setup up resource controller, should use tikv storage")
+		// return nil as unistore doesn't support it
+		return nil
 	}
 
 	control, err := rmclient.NewResourceGroupController(ctx, do.ServerID(), pdClient, nil, rmclient.WithMaxWaitDuration(time.Second*30))
