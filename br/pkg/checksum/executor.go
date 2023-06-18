a45f87389eff24fb64427eb795aa7caed9251f9b
D3Hunter <jujj603@gmail.com>
2023-06-18 13:53:43 +0800

    change

--- br/pkg/checksum/executor.go
+++ br/pkg/checksum/executor.go
@@ -12,6 +12,7 @@ import (
 	"github.com/pingcap/tidb/br/pkg/metautil"
 	"github.com/pingcap/tidb/br/pkg/utils"
 	"github.com/pingcap/tidb/distsql"
+	"github.com/pingcap/tidb/domain/resourcegroup"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/sessionctx/variable"
@@ -213,6 +214,7 @@ func buildTableRequest(
 		SetStartTS(startTS).
 		SetChecksumRequest(checksum).
 		SetConcurrency(int(concurrency)).
+		SetResourceGroupName(resourcegroup.DefaultResourceGroupName).
 		Build()
 }
 
