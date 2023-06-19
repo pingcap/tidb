e11cbf2dd1abbfd723edf86276bdb44751dab2ea
Arenatlx <314806019@qq.com>
2023-06-19 20:08:41 +0800

    planner: enhance projection explain-info when simple column projected again with a new unique-id (#44520)
    close pingcap/tidb#44521

--- planner/core/integration_test.go
+++ planner/core/integration_test.go
@@ -4022,7 +4022,7 @@ func TestIssue31609(t *testing.T) {
 	tk.MustExec("use test")
 
 	tk.MustQuery("explain select rank() over (partition by table_name) from information_schema.tables").Check(testkit.Rows(
-		"Projection_7 10000.00 root  Column#27",
+		"Projection_7 10000.00 root  Column#27->Column#28",
 		"└─Shuffle_11 10000.00 root  execution info: concurrency:5, data sources:[MemTableScan_9]",
 		"  └─Window_8 10000.00 root  rank()->Column#27 over(partition by Column#3)",
 		"    └─Sort_10 10000.00 root  Column#3",
