54d6548e4b5372b792c6109e7796bd7dc7b8a1f7
xhe <xw897002528@gmail.com>
2023-06-20 15:28:35 +0800

    address comments
    Signed-off-by: xhe <xw897002528@gmail.com>

--- expression/builtin_cast_test.go
+++ expression/builtin_cast_test.go
@@ -1403,6 +1403,11 @@ func TestWrapWithCastAsString(t *testing.T) {
 			false,
 			"-127",
 		},
+		{
+			&Constant{RetType: types.NewFieldTypeWithCollation(mysql.TypeShort, charset.CollationBin, 1), Value: types.NewIntDatum(-127)},
+			false,
+			"-127",
+		},
 	}
 	for _, c := range cases {
 		expr := BuildCastFunction(ctx, c.expr, types.NewFieldType(mysql.TypeVarString))
