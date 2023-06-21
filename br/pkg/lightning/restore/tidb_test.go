d19162eb15b18f49763b60f56e4b3932577a0f6d
GMHDBJD <35025882+GMHDBJD@users.noreply.github.com>
2023-06-21 21:41:42 +0800

    This is an automated cherry-pick of #44803
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- br/pkg/lightning/restore/tidb_test.go
+++ br/pkg/lightning/restore/tidb_test.go
@@ -460,6 +460,7 @@ func TestObtainRowFormatVersionSucceed(t *testing.T) {
 
 	sysVars := ObtainImportantVariables(ctx, s.tiGlue.GetSQLExecutor(), true)
 	require.Equal(t, map[string]string{
+		"tidb_backoff_weight":     "6",
 		"tidb_row_format_version": "2",
 		"max_allowed_packet":      "1073741824",
 		"div_precision_increment": "10",
@@ -487,6 +488,7 @@ func TestObtainRowFormatVersionFailure(t *testing.T) {
 
 	sysVars := ObtainImportantVariables(ctx, s.tiGlue.GetSQLExecutor(), true)
 	require.Equal(t, map[string]string{
+		"tidb_backoff_weight":     "6",
 		"tidb_row_format_version": "1",
 		"max_allowed_packet":      "67108864",
 		"div_precision_increment": "4",
