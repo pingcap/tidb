f9bbd4b18edd2df63c63d284d7a2be3bd525c70c
Weizhen Wang <wangweizhen@pingcap.com>
2023-06-21 15:24:27 +0800

    update

--- statistics/handle/internal/cache/inner.go
+++ statistics/handle/internal/cache/inner.go
@@ -18,18 +18,29 @@ import "github.com/pingcap/tidb/statistics"
 
 // StatsCacheInner is the interface to manage the statsCache, it can be implemented by map, lru cache or other structures.
 type StatsCacheInner interface {
+	// GetByQuery retrieves cache triggered by a query. Usually used in LRU.
 	GetByQuery(int64) (*statistics.Table, bool)
+	// Get gets the cache.
 	Get(int64) (*statistics.Table, bool)
+	// PutByQuery puts a cache triggered by a query. Usually used in LRU.
 	PutByQuery(int64, *statistics.Table)
+	// Put puts a cache.
 	Put(int64, *statistics.Table)
+	// Del deletes a cache.
 	Del(int64)
+	// Cost returns the memory usage of the cache.
 	Cost() int64
+	// Keys returns the keys of the cache.
 	Keys() []int64
+	// Values returns the values of the cache.
 	Values() []*statistics.Table
 	Map() map[int64]*statistics.Table
+	// Len returns the length of the cache.
 	Len() int
 	FreshMemUsage()
+	// Copy returns a copy of the cache
 	Copy() StatsCacheInner
+	// SetCapacity sets the capacity of the cache
 	SetCapacity(int64)
 	// Front returns the front element's owner tableID, only used for test
 	Front() int64
