7d3eb7c3363ca740f2c712f2e1082c92abac118d
Leavrth <jianjun.liao@outlook.com>
2023-06-26 22:19:00 +0800

    fix unit test
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- br/pkg/storage/s3_test.go
+++ br/pkg/storage/s3_test.go
@@ -289,7 +289,6 @@ func TestS3Storage(t *testing.T) {
 
 	require.NoError(t, os.Setenv("AWS_ACCESS_KEY_ID", "ab"))
 	require.NoError(t, os.Setenv("AWS_SECRET_ACCESS_KEY", "cd"))
-	require.NoError(t, os.Setenv("AWS_SESSION_TOKEN", "ef"))
 	s := createGetBucketRegionServer("us-west-2", 200, true)
 	defer s.Close()
 
@@ -412,7 +411,15 @@ func TestS3Storage(t *testing.T) {
 }
 
 func TestS3URI(t *testing.T) {
-	backend, err := ParseBackend("s3://bucket/prefix/", nil)
+	accessKey := "ab"
+	secretAccessKey := "cd"
+	options := &BackendOptions{
+		S3: S3BackendOptions{
+			AccessKey:       accessKey,
+			SecretAccessKey: secretAccessKey,
+		},
+	}
+	backend, err := ParseBackend("s3://bucket/prefix/", options)
 	require.NoError(t, err)
 	storage, err := New(context.Background(), backend, &ExternalStorageOptions{})
 	require.NoError(t, err)
