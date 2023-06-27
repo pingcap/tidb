16783ca3688d0d00724f6e54efd7d6986caf03e3
dhysum <dhysum@gmail.com>
2023-06-27 13:23:54 +0800

    Reduce change

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -550,12 +550,11 @@ func (dc *ddlCtx) removeReorgCtx(jobID int64) {
 func (dc *ddlCtx) notifyReorgWorkerJobStateChange(job *model.Job) {
 	rc := dc.getReorgCtx(job.ID)
 	if rc == nil {
-		logutil.BgLogger().Warn("cannot find reorgCtx", zap.Int64("job ID", job.ID))
+		logutil.BgLogger().Warn("cannot find reorgCtx", zap.Int64("jobID", job.ID))
 		return
 	}
 	logutil.BgLogger().Info("[ddl] notify reorg worker the job's state",
-		zap.Int64("Job ID", job.ID), zap.String("Job State", job.State.String()),
-		zap.String("Schema State", job.SchemaState.String()))
+		zap.Int64("jobID", job.ID), zap.Int32("jobState", int32(job.State)), zap.Int("jobState", int(job.SchemaState)))
 	rc.notifyJobState(job.State)
 }
 
