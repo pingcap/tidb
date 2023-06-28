0954b82a153b656a66951425b173ee56fe7739e0
Jianjun Liao <36503113+Leavrth@users.noreply.github.com>
2023-06-28 17:52:09 +0800

    br: cleanup `AvaliablePartitionIDs` for tiflash when restore (#45016)
    close pingcap/tidb#45013

--- br/pkg/restore/client.go
+++ br/pkg/restore/client.go
@@ -2190,6 +2190,7 @@ func (rc *Client) PreCheckTableTiFlashReplica(
 			// we should not set available to true. because we cannot guarantee the raft log lag of tiflash when restore finished.
 			// just let tiflash ticker set it by checking lag of all related regions.
 			table.Info.TiFlashReplica.Available = false
+			table.Info.TiFlashReplica.AvailablePartitionIDs = nil
 			if recorder != nil {
 				recorder.AddTable(table.Info.ID, *table.Info.TiFlashReplica)
 				log.Info("record tiflash replica for table, to reset it by ddl later",
