e7a6a4f2ed7af9bded1b430c3a36a9d3079cfa83
wjHuang <huangwenjun1997@gmail.com>
2023-06-30 14:43:02 +0800

    Update table/column.go
    Co-authored-by: tangenta <tangenta@126.com>

--- table/column.go
+++ table/column.go
@@ -47,7 +47,7 @@ type Column struct {
 	*model.ColumnInfo
 	// If this column is a generated column, the expression will be stored here.
 	GeneratedExpr ast.ExprNode
-	// GetGeneratedExpr gets the generated column exprNode. If we want to rewrite the node, please use this function to avoid data race.
+	// GetGeneratedExpr gets the generated column exprNode. If you want to rewrite the node, please use this function to avoid data race.
 	GetGeneratedExpr func() ast.ExprNode
 	// If this column has default expr value, this expression will be stored here.
 	DefaultExpr ast.ExprNode
