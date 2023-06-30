4fdae0b332a9fd3d62985c651e87d5c8eb81bee5
yibin <huyibin@pingcap.com>
2023-06-30 14:54:41 +0800

    executor, distsql: enhance mpp cooridnator to handle execption and execution summaries better (#44697)
    ref pingcap/tidb#44032

--- server/server.go
+++ server/server.go
@@ -41,6 +41,7 @@ import (
 	"os/user"
 	"reflect"
 	"strconv"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -149,6 +150,20 @@ type Server struct {
 	authTokenCancelFunc context.CancelFunc
 	wg                  sync.WaitGroup
 	printMDLLogTime     time.Time
+}
+
+// GetStatusServerAddr gets statusServer address for MppCoordinatorManager usage
+func (s *Server) GetStatusServerAddr() (on bool, addr string) {
+	if !s.cfg.Status.ReportStatus {
+		return false, ""
+	}
+	if strings.Contains(s.statusAddr, config.DefStatusHost) {
+		if len(s.cfg.AdvertiseAddress) != 0 {
+			return true, strings.ReplaceAll(s.statusAddr, config.DefStatusHost, s.cfg.AdvertiseAddress)
+		}
+		return false, ""
+	}
+	return true, s.statusAddr
 }
 
 // ConnectionCount gets current connection count.
