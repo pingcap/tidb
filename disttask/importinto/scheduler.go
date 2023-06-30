e94e5b2051a09d23c5ead28f77a6769e34c31289
D3Hunter <jujj603@gmail.com>
2023-06-30 13:38:11 +0800

    revert import into: fix some subtask keeps running on cancel when triggered disk quota (#45074)
    ref pingcap/tidb#45048

--- disttask/importinto/scheduler.go
+++ disttask/importinto/scheduler.go
@@ -42,7 +42,9 @@ type importStepScheduler struct {
 	sharedVars    sync.Map
 	logger        *zap.Logger
 
-	wg sync.WaitGroup
+	importCtx    context.Context
+	importCancel context.CancelFunc
+	wg           sync.WaitGroup
 }
 
 func (s *importStepScheduler) InitSubtaskExecEnv(ctx context.Context) error {
@@ -76,10 +78,11 @@ func (s *importStepScheduler) InitSubtaskExecEnv(ctx context.Context) error {
 	}
 	s.tableImporter = tableImporter
 
+	s.importCtx, s.importCancel = context.WithCancel(context.Background())
 	s.wg.Add(1)
 	go func() {
 		defer s.wg.Done()
-		s.tableImporter.CheckDiskQuota(ctx)
+		s.tableImporter.CheckDiskQuota(s.importCtx)
 	}()
 	return nil
 }
@@ -176,6 +179,7 @@ func (s *importStepScheduler) OnSubtaskFinished(ctx context.Context, subtaskMeta
 
 func (s *importStepScheduler) CleanupSubtaskExecEnv(_ context.Context) (err error) {
 	s.logger.Info("cleanup subtask env")
+	s.importCancel()
 	s.wg.Wait()
 	return s.tableImporter.Close()
 }
