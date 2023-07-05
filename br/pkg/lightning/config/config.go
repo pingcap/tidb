ee7f4fc3a5d5322c734a5d0d927c06b5a0af529f
lance6716 <lance6716@gmail.com>
2023-07-05 15:38:14 +0800

    lightning: enable pre-deduplication and rename the recording table (#45122)
    ref pingcap/tidb#41629

--- br/pkg/lightning/config/config.go
+++ br/pkg/lightning/config/config.go
@@ -1205,8 +1205,6 @@ func (cfg *Config) AdjustCommon() (bool, error) {
 		}
 		cfg.TikvImporter.DuplicateResolution = DupeResAlgNone
 	case BackendLocal:
-		// force turn off pre-dedup for local backend
-		cfg.TikvImporter.OnDuplicate = ""
 		if cfg.TikvImporter.RegionSplitBatchSize <= 0 {
 			return mustHaveInternalConnections, common.ErrInvalidConfig.GenWithStack("`tikv-importer.region-split-batch-size` got %d, should be larger than 0", cfg.TikvImporter.RegionSplitBatchSize)
 		}
