26d75135266a5c8864278b35a0ce5469d4f88771
lance6716 <lance6716@gmail.com>
2023-07-07 21:03:18 +0800

    --wip-- [skip ci]

--- dumpling/tests/no_table_and_db_name/run.sh
+++ dumpling/tests/no_table_and_db_name/run.sh
@@ -27,8 +27,8 @@ run_sql "set global tidb_general_log=1;"
 # dumping with file size = 233 bytes, actually 10 rows
 run_dumpling -F 233B --filetype csv --sql "select * from $TEST_NAME.t"
 run_sql "set global tidb_general_log=0;"
-assert [ $( grep "GENERAL_LOG" $DUMPLING_TEST_DIR/tidb.log | grep --ignore-case "SHOW FULL TABLES" | wc -l ) -eq 0 ]
 assert [ $( grep "GENERAL_LOG" $DUMPLING_TEST_DIR/tidb.log | grep --ignore-case "INFORMATION_SCHEMA.TABLES" | wc -l ) -eq 0 ]
+assert [ $( grep "GENERAL_LOG" $DUMPLING_TEST_DIR/tidb.log | grep --ignore-case "SHOW FULL TABLES" | wc -l ) -eq 0 ]
 assert [ $( grep "GENERAL_LOG" $DUMPLING_TEST_DIR/tidb.log | grep --ignore-case "SHOW TABLE STATUS" | wc -l ) -eq 0 ]
 
 assert [ $( ls -lh $DUMPLING_OUTPUT_DIR | grep -e ".csv$" | wc -l ) -eq 10 ]
