f61521b878bc450a640f7e5a2a7da81013445fb7
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2023-07-07 20:35:11 +0800

    planner: simplify `StatsCachePointer` (#45238)
    ref pingcap/tidb#44461

--- statistics/handle/cache/statscache.go
+++ statistics/handle/cache/statscache.go
@@ -16,60 +16,27 @@ package cache
 
 import (
 	"sync/atomic"
-
-	"github.com/pingcap/tidb/util/memory"
-	"github.com/pingcap/tidb/util/syncutil"
 )
 
-// StatsCache is used to cache the stats of a table.
-type StatsCache struct {
-	cache      atomic.Pointer[StatsCacheWrapper]
-	memTracker *memory.Tracker
-	mu         syncutil.Mutex
+// StatsCachePointer is used to cache the stats of a table.
+type StatsCachePointer struct {
+	atomic.Pointer[StatsCacheWrapper]
 }
 
-// NewStatsCache creates a new StatsCache.
-func NewStatsCache() *StatsCache {
+// NewStatsCachePointer creates a new StatsCache.
+func NewStatsCachePointer() *StatsCachePointer {
 	newCache := NewStatsCacheWrapper()
-	result := StatsCache{
-		memTracker: memory.NewTracker(memory.LabelForStatsCache, -1),
-	}
-	result.cache.Store(newCache)
+	result := StatsCachePointer{}
+	result.Store(newCache)
 	return &result
 }
 
-// Clear removes all cached stats from the cache.
-func (s *StatsCache) Clear() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	newCache := NewStatsCacheWrapper()
-	s.cache.Store(newCache)
-	s.memTracker = memory.NewTracker(memory.LabelForStatsCache, -1)
-}
-
 // Load loads the cached stats from the cache.
-func (s *StatsCache) Load() *StatsCacheWrapper {
-	return s.cache.Load()
+func (s *StatsCachePointer) Load() *StatsCacheWrapper {
+	return s.Pointer.Load()
 }
 
-// Version returns the version of the cached stats.
-func (s *StatsCache) Version() uint64 {
-	return s.Load().Version()
-}
-
-// GetMemConsumed returns the memory usage of the cache.
-func (s *StatsCache) GetMemConsumed() int64 {
-	return s.memTracker.BytesConsumed()
-}
-
-// UpdateCache updates the cache with the new cache.
-func (s *StatsCache) UpdateCache(newCache StatsCacheWrapper) (updated bool, newCost int64) {
-	s.mu.Lock()
-	oldCache := s.cache.Load()
-	newCost = newCache.Cost()
-	s.memTracker.Consume(newCost - oldCache.Cost())
-	s.cache.Store(&newCache)
-	updated = true
-	s.mu.Unlock()
-	return updated, newCost
+// Replace replaces the cache with the new cache.
+func (s *StatsCachePointer) Replace(newCache *StatsCacheWrapper) {
+	s.Store(newCache)
 }
