619243b88cc7930af67c38636bf613edf784f450
qw4990 <zhangyuanjia@pingcap.com>
2023-07-13 12:29:40 +0800

    fix ci

--- sessiontxn/txn_context_test.go
+++ sessiontxn/txn_context_test.go
@@ -660,7 +660,7 @@ func TestTxnContextForStaleReadInPrepare(t *testing.T) {
 	})
 
 	// plan cache for stmtID3
-	doWithCheckPath(t, se, []string{"assertTxnManagerInCachedPlanExec", "assertTxnManagerInShortPointGetPlan"}, func() {
+	doWithCheckPath(t, se, []string{"assertTxnManagerInShortPointGetPlan"}, func() {
 		rs, err := se.ExecutePreparedStmt(context.TODO(), stmtID3, nil)
 		require.NoError(t, err)
 		tk.ResultSetToResult(rs, fmt.Sprintf("%v", rs)).Check(testkit.Rows("1 10"))
