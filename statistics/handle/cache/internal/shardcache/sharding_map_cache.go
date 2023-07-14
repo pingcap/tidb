e9f8d591af61e39b099c6f6be799d19bccb2065e
Weizhen Wang <wangweizhen@pingcap.com>
2023-07-14 23:35:12 +0800

    statcache: use shard map to replace mapcache
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ statistics/handle/cache/internal/shardcache/sharding_map_cache.go
@@ -0,0 +1,141 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shardcache
+
+import (
+	"github.com/pingcap/tidb/statistics"
+	"github.com/pingcap/tidb/statistics/handle/cache/internal"
+)
+
+const tablesCacheShardCnt = 256
+
+// MapCache is a cache based on map.
+type MapCache struct {
+	tables               [tablesCacheShardCnt]*tableCache
+	memUsage             int64
+	maxTableStatsVersion uint64
+}
+
+// NewMapCache creates a new map cache.
+func NewMapCache() *MapCache {
+	var tables [tablesCacheShardCnt]*tableCache
+	for v := range tables {
+		tables[v] = newTableCache()
+	}
+	return &MapCache{
+		tables:   tables,
+		memUsage: 0,
+	}
+}
+
+// Get implements statsCacheInner
+func (m *MapCache) Get(k int64, _ bool) (*statistics.Table, bool) {
+	table := m.tables[k%tablesCacheShardCnt]
+	return table.Get(k)
+}
+
+// Put implements statsCacheInner
+func (m *MapCache) Put(k int64, v *statistics.Table, _ bool) {
+	table := m.tables[k%tablesCacheShardCnt]
+	delta := table.Put(k, v)
+	m.memUsage += delta
+}
+
+// Del implements statsCacheInner
+func (m *MapCache) Del(k int64) {
+	table := m.tables[k%tablesCacheShardCnt]
+	m.memUsage -= table.Delete(k)
+}
+
+// Cost implements statsCacheInner
+func (m *MapCache) Cost() int64 {
+	return m.memUsage
+}
+
+// Keys implements statsCacheInner
+func (m *MapCache) Keys() []int64 {
+	ks := make([]int64, 0, len(m.tables))
+	for _, table := range m.tables {
+		table.Iterate(func(k int64, _ *statistics.Table) {
+			ks = append(ks, k)
+		})
+	}
+	return ks
+}
+
+// Values implements statsCacheInner
+func (m *MapCache) Values() []*statistics.Table {
+	vs := make([]*statistics.Table, 0, len(m.tables))
+	for _, table := range m.tables {
+		table.Iterate(func(_ int64, v *statistics.Table) {
+			vs = append(vs, v)
+		})
+	}
+	return vs
+}
+
+// Map implements statsCacheInner
+func (m *MapCache) Map() map[int64]*statistics.Table {
+	t := make(map[int64]*statistics.Table, len(m.tables))
+	for _, table := range m.tables {
+		table.Iterate(func(k int64, v *statistics.Table) {
+			t[k] = v
+		})
+	}
+	return t
+}
+
+// Len implements statsCacheInner
+func (m *MapCache) Len() int {
+	var s int
+	for idx := range m.tables {
+		s += m.tables[idx].Len()
+	}
+	return s
+}
+
+// FreshMemUsage implements statsCacheInner
+func (m *MapCache) FreshMemUsage() {
+	for _, table := range m.tables {
+		m.memUsage += table.FreshMemUsage()
+	}
+}
+
+// Copy implements statsCacheInner
+func (m *MapCache) Copy() internal.StatsCacheInner {
+	var tables [tablesCacheShardCnt]*tableCache
+	for v := range tables {
+		tables[v] = m.tables[v]
+	}
+	newM := &MapCache{
+		tables:               tables,
+		memUsage:             m.memUsage,
+		maxTableStatsVersion: m.maxTableStatsVersion,
+	}
+	return newM
+}
+
+// SetCapacity implements statsCacheInner
+func (*MapCache) SetCapacity(int64) {}
+
+// Front implements statsCacheInner
+func (*MapCache) Front() int64 {
+	return 0
+}
+
+// Version implements StatsCacheInner
+func (m *MapCache) Version() uint64 {
+	return m.maxTableStatsVersion
+}
---
+++ statistics/handle/cache/internal/shardcache/table_cache.go
@@ -0,0 +1,114 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shardcache
+
+import (
+	"sync"
+
+	"github.com/pingcap/tidb/statistics"
+)
+
+type cacheItem struct {
+	value *statistics.Table
+	key   int64
+	cost  int64
+}
+
+func (c cacheItem) copy() cacheItem {
+	return cacheItem{
+		key:   c.key,
+		value: c.value,
+		cost:  c.cost,
+	}
+}
+
+type tableCache struct {
+	mu   sync.RWMutex
+	Item map[int64]cacheItem
+}
+
+func newTableCache() *tableCache {
+	item := make(map[int64]cacheItem)
+	result := &tableCache{
+		Item: item,
+	}
+	return result
+}
+
+func (t *tableCache) Get(k int64) (*statistics.Table, bool) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	value, ok := t.Item[k]
+	return value.value, ok
+}
+
+func (t *tableCache) Put(k int64, v *statistics.Table) int64 {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	item, ok := t.Item[k]
+	if ok {
+		oldCost := item.cost
+		newCost := v.MemoryUsage().TotalMemUsage
+		item.value = v
+		item.cost = newCost
+		t.Item[k] = item
+		return newCost - oldCost
+	}
+	cost := v.MemoryUsage().TotalMemUsage
+	item = cacheItem{
+		key:   k,
+		value: v,
+		cost:  cost,
+	}
+	t.Item[k] = item
+	return cost
+}
+
+func (t *tableCache) Delete(k int64) int64 {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	item, ok := t.Item[k]
+	if !ok {
+		return 0
+	}
+	delete(t.Item, k)
+	return item.cost
+}
+
+func (t *tableCache) Len() int {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return len(t.Item)
+}
+
+func (t *tableCache) Iterate(f func(k int64, v *statistics.Table)) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	for k, v := range t.Item {
+		f(k, v.value)
+	}
+}
+
+func (t *tableCache) FreshMemUsage() int64 {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	delta := int64(0)
+	for _, v := range t.Item {
+		oldCost := v.cost
+		newCost := v.value.MemoryUsage().TotalMemUsage
+		delta = delta + (newCost - oldCost)
+	}
+	return delta
+}
