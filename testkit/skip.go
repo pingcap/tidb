f4a49d62f00dabafd4833582cad49506375202eb
Weizhen Wang <wangweizhen@pingcap.com>
2023-07-17 19:01:28 +0800

    ut: support test.short for ut
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- testkit/skip.go
+++ testkit/skip.go
@@ -3,7 +3,7 @@ package testkit
 import "testing"
 
 // SkipUnderShort skips the test if the -short flag is set.
-func SkipUnderShort(t testing.T, args ...interface{}) {
+func SkipUnderShort(t *testing.T, args ...interface{}) {
 	t.Helper()
 	if testing.Short() {
 		t.Skip(append([]interface{}{"disabled under -short"}, args...))
