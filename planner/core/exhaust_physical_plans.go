9964c9b20ce807b41781e38a1cba86dbf6366f9d
qw4990 <zhangyuanjia@pingcap.com>
2023-07-25 22:06:57 +0800

    fixup

--- planner/core/exhaust_physical_plans.go
+++ planner/core/exhaust_physical_plans.go
@@ -233,12 +233,11 @@ func (p *LogicalJoin) GetMergeJoin(prop *property.PhysicalProperty, schema *expr
 	}
 
 	if p.preferJoinType&preferNoMergeJoin > 0 {
-		if p.preferJoinType&preferMergeJoin > 0 {
-			p.SCtx().GetSessionVars().StmtCtx.AppendWarning(ErrInternal.GenWithStack(
-				"Some MERGE_JOIN and NO_MERGE_JOIN hints conflict, NO_MERGE_JOIN is ignored"))
-		} else {
+		if p.preferJoinType&preferMergeJoin == 0 {
 			return nil
 		}
+		p.SCtx().GetSessionVars().StmtCtx.AppendWarning(ErrInternal.GenWithStack(
+			"Some MERGE_JOIN and NO_MERGE_JOIN hints conflict, NO_MERGE_JOIN is ignored"))
 	}
 
 	// If TiDB_SMJ hint is existed, it should consider enforce merge join,
