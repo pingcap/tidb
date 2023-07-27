c0021437cbb2f24f56904e755b7c4e566a191d7a
lyzx2001 <rep_laureline@hotmail.com>
2023-07-27 16:33:39 +0800

    lightning: save NEXT_GLOBAL_ROW_ID as uint64 instead of int64

---
+++ br/tests/lightning_auto_columns/data/lightning_auto_cols.t_bigint.sql
@@ -0,0 +1,8 @@
+INSERT INTO t_bigint (id, c) VALUES
+(1, 'normal_pk_01');
+INSERT INTO t_bigint VALUES
+(NULL, 'null_pk_02');
+INSERT INTO t_bigint (id, c) VALUES
+(10942694589135710585, 'test0');
+INSERT INTO t_bigint (id, c) VALUES
+(10942694589135710586, 'test1');
