27228900dd1e6fb5a4152814d507ab88ebb66e54
ywqzzy <592838129@qq.com>
2023-07-28 16:59:50 +0800

    u

--- disttask/framework/storage/task_table.go
+++ disttask/framework/storage/task_table.go
@@ -383,7 +383,7 @@ func (stm *TaskManager) GetSubtaskInStatesCnt(taskID int64, states ...interface{
 func (stm *TaskManager) CollectSubTaskError(taskID int64) ([]error, error) {
 	rs, err := stm.executeSQLWithNewSession(
 		stm.ctx,
-		"select error from mysql.tidb_background_subtask where task_key = %? AND state = %??",
+		"select error from mysql.tidb_background_subtask where task_key = %? AND state = %?",
 		taskID, proto.TaskStateFailed)
 	if err != nil {
 		return nil, err
