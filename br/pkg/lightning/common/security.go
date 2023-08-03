2c261d8042135b104b6aaf0a94014cc03d1fc22b
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2023-08-03 16:14:39 +0800

    lightning: fix lightning TLS.WithHost to access the correct host (#45749)
    close pingcap/tidb#45747

--- br/pkg/lightning/common/security.go
+++ br/pkg/lightning/common/security.go
@@ -89,8 +89,15 @@ func NewTLSFromMockServer(server *httptest.Server) *TLS {
 	}
 }
 
+// GetMockTLSUrl returns tls's host for mock test
+func GetMockTLSUrl(tls *TLS) string {
+	return tls.url
+}
+
 // WithHost creates a new TLS instance with the host replaced.
 func (tc *TLS) WithHost(host string) *TLS {
+	host = strings.TrimPrefix(host, "http://")
+	host = strings.TrimPrefix(host, "https://")
 	var url string
 	host = strings.TrimPrefix(host, "http://")
 	host = strings.TrimPrefix(host, "https://")
