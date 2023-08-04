9c2b4ffb04ca7f45354632ac8de2d7e24ddde032
Weizhen Wang <wangweizhen@pingcap.com>
2023-08-05 00:38:42 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- session/sessiontest/session_test.go
+++ session/sessiontest/session_test.go
@@ -30,12 +30,10 @@ import (
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/parser/ast"
-	"github.com/pingcap/tidb/parser/auth"
 	"github.com/pingcap/tidb/parser/format"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/parser/terror"
-	"github.com/pingcap/tidb/privilege/privileges"
 	"github.com/pingcap/tidb/session"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/store/mockstore"
@@ -349,27 +347,6 @@ func TestDatabase(t *testing.T) {
 	tk.MustExec("drop schema if exists xxx")
 }
 
-func TestSkipWithGrant(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk := testkit.NewTestKit(t, store)
-
-	save2 := privileges.SkipWithGrant
-
-	privileges.SkipWithGrant = false
-	require.Error(t, tk.Session().Auth(&auth.UserIdentity{Username: "user_not_exist"}, []byte("yyy"), []byte("zzz"), nil))
-
-	privileges.SkipWithGrant = true
-	require.NoError(t, tk.Session().Auth(&auth.UserIdentity{Username: "xxx", Hostname: `%`}, []byte("yyy"), []byte("zzz"), nil))
-	require.NoError(t, tk.Session().Auth(&auth.UserIdentity{Username: "root", Hostname: `%`}, []byte(""), []byte(""), nil))
-	tk.MustExec("use test")
-	tk.MustExec("create table t (id int)")
-	tk.MustExec("create role r_1")
-	tk.MustExec("grant r_1 to root")
-	tk.MustExec("set role all")
-	tk.MustExec("show grants for root")
-	privileges.SkipWithGrant = save2
-}
-
 func TestParseWithParams(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 	tk := testkit.NewTestKit(t, store)
@@ -563,45 +540,6 @@ func TestStmtHints(t *testing.T) {
 	require.Equal(t, kv.ReplicaReadFollower, tk.Session().GetSessionVars().GetReplicaRead())
 }
 
-func TestGrantViewRelated(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tkRoot := testkit.NewTestKit(t, store)
-	tkUser := testkit.NewTestKit(t, store)
-	tkRoot.MustExec("use test")
-	tkUser.MustExec("use test")
-
-	tkRoot.Session().Auth(&auth.UserIdentity{Username: "root", Hostname: "localhost", CurrentUser: true, AuthUsername: "root", AuthHostname: "%"}, nil, []byte("012345678901234567890"), nil)
-
-	tkRoot.MustExec("create table if not exists t (a int)")
-	tkRoot.MustExec("create view v_version29 as select * from t")
-	tkRoot.MustExec("create user 'u_version29'@'%'")
-	tkRoot.MustExec("grant select on t to u_version29@'%'")
-
-	tkUser.Session().Auth(&auth.UserIdentity{Username: "u_version29", Hostname: "localhost", CurrentUser: true, AuthUsername: "u_version29", AuthHostname: "%"}, nil, []byte("012345678901234567890"), nil)
-
-	tkUser.MustQuery("select current_user();").Check(testkit.Rows("u_version29@%"))
-	require.Error(t, tkUser.ExecToErr("select * from test.v_version29;"))
-	tkUser.MustQuery("select current_user();").Check(testkit.Rows("u_version29@%"))
-	require.Error(t, tkUser.ExecToErr("create view v_version29_c as select * from t;"))
-
-	tkRoot.MustExec(`grant show view, select on v_version29 to 'u_version29'@'%'`)
-	tkRoot.MustQuery("select table_priv from mysql.tables_priv where host='%' and db='test' and user='u_version29' and table_name='v_version29'").Check(testkit.Rows("Select,Show View"))
-
-	tkUser.MustQuery("select current_user();").Check(testkit.Rows("u_version29@%"))
-	tkUser.MustQuery("show create view v_version29;")
-	require.Error(t, tkUser.ExecToErr("create view v_version29_c as select * from v_version29;"))
-
-	tkRoot.MustExec("create view v_version29_c as select * from v_version29;")
-	tkRoot.MustExec(`grant create view on v_version29_c to 'u_version29'@'%'`) // Can't grant privilege on a non-exist table/view.
-	tkRoot.MustQuery("select table_priv from mysql.tables_priv where host='%' and db='test' and user='u_version29' and table_name='v_version29_c'").Check(testkit.Rows("Create View"))
-	tkRoot.MustExec("drop view v_version29_c")
-
-	tkRoot.MustExec(`grant select on v_version29 to 'u_version29'@'%'`)
-	tkUser.MustQuery("select current_user();").Check(testkit.Rows("u_version29@%"))
-	tkUser.MustExec("create view v_version29_c as select * from v_version29;")
-}
-
 func TestLoadClientInteractive(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 
@@ -609,88 +547,6 @@ func TestLoadClientInteractive(t *testing.T) {
 	tk.RefreshSession()
 	tk.Session().GetSessionVars().ClientCapability = tk.Session().GetSessionVars().ClientCapability | mysql.ClientInteractive
 	tk.MustQuery("select @@wait_timeout").Check(testkit.Rows("28800"))
-}
-
-func TestUpdatePrivilege(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t1, t2;")
-	tk.MustExec("create table t1 (id int);")
-	tk.MustExec("create table t2 (id int);")
-	tk.MustExec("insert into t1 values (1);")
-	tk.MustExec("insert into t2 values (2);")
-	tk.MustExec("create user xxx;")
-	tk.MustExec("grant all on test.t1 to xxx;")
-	tk.MustExec("grant select on test.t2 to xxx;")
-
-	tk1 := testkit.NewTestKit(t, store)
-	tk1.MustExec("use test")
-	require.NoError(t, tk1.Session().Auth(&auth.UserIdentity{Username: "xxx", Hostname: "localhost"}, []byte(""), []byte(""), nil))
-
-	tk1.MustMatchErrMsg("update t2 set id = 666 where id = 1;", "privilege check.*")
-
-	// Cover a bug that t1 and t2 both require update privilege.
-	// In fact, the privlege check for t1 should be update, and for t2 should be select.
-	tk1.MustExec("update t1,t2 set t1.id = t2.id;")
-
-	// Fix issue 8911
-	tk.MustExec("create database weperk")
-	tk.MustExec("use weperk")
-	tk.MustExec("create table tb_wehub_server (id int, active_count int, used_count int)")
-	tk.MustExec("create user 'weperk'")
-	tk.MustExec("grant all privileges on weperk.* to 'weperk'@'%'")
-	require.NoError(t, tk1.Session().Auth(&auth.UserIdentity{Username: "weperk", Hostname: "%"}, []byte(""), []byte(""), nil))
-	tk1.MustExec("use weperk")
-	tk1.MustExec("update tb_wehub_server a set a.active_count=a.active_count+1,a.used_count=a.used_count+1 where id=1")
-
-	tk.MustExec("create database service")
-	tk.MustExec("create database report")
-	tk.MustExec(`CREATE TABLE service.t1 (
-  id int(11) DEFAULT NULL,
-  a bigint(20) NOT NULL,
-  b text DEFAULT NULL,
-  PRIMARY KEY (a)
-)`)
-	tk.MustExec(`CREATE TABLE report.t2 (
-  a bigint(20) DEFAULT NULL,
-  c bigint(20) NOT NULL
-)`)
-	tk.MustExec("grant all privileges on service.* to weperk")
-	tk.MustExec("grant all privileges on report.* to weperk")
-	tk1.Session().GetSessionVars().CurrentDB = ""
-	tk1.MustExec(`update service.t1 s,
-report.t2 t
-set s.a = t.a
-WHERE
-s.a = t.a
-and t.c >=  1 and t.c <= 10000
-and s.b !='xx';`)
-
-	// Fix issue 10028
-	tk.MustExec("create database ap")
-	tk.MustExec("create database tp")
-	tk.MustExec("grant all privileges on ap.* to xxx")
-	tk.MustExec("grant select on tp.* to xxx")
-	tk.MustExec("create table tp.record( id int,name varchar(128),age int)")
-	tk.MustExec("insert into tp.record (id,name,age) values (1,'john',18),(2,'lary',19),(3,'lily',18)")
-	tk.MustExec("create table ap.record( id int,name varchar(128),age int)")
-	tk.MustExec("insert into ap.record(id) values(1)")
-	require.NoError(t, tk1.Session().Auth(&auth.UserIdentity{Username: "xxx", Hostname: "localhost"}, []byte(""), []byte(""), nil))
-	tk1.MustExec("update ap.record t inner join tp.record tt on t.id=tt.id  set t.name=tt.name")
-}
-
-func TestDBUserNameLength(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("create table if not exists t (a int)")
-	// Test username length can be longer than 16.
-	tk.MustExec(`CREATE USER 'abcddfjakldfjaldddds'@'%' identified by ''`)
-	tk.MustExec(`grant all privileges on test.* to 'abcddfjakldfjaldddds'@'%'`)
-	tk.MustExec(`grant all privileges on test.t to 'abcddfjakldfjaldddds'@'%'`)
 }
 
 func TestHostLengthMax(t *testing.T) {
@@ -1723,14 +1579,6 @@ func TestSession(t *testing.T) {
 	tk.Session().Close()
 }
 
-func TestSessionAuth(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	require.Error(t, tk.Session().Auth(&auth.UserIdentity{Username: "Any not exist username with zero password!", Hostname: "anyhost"}, []byte(""), []byte(""), nil))
-}
-
 func TestLastInsertID(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 
@@ -1802,31 +1650,6 @@ func TestBinaryReadOnly(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 2, session.GetHistory(tk.Session()).Count())
 	tk.MustExec("commit")
-}
-
-func TestIndexMergeRuntimeStats(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("set @@tidb_enable_index_merge = 1")
-	tk.MustExec("create table t1(id int primary key, a int, b int, c int, d int)")
-	tk.MustExec("create index t1a on t1(a)")
-	tk.MustExec("create index t1b on t1(b)")
-	tk.MustExec("insert into t1 values(1,1,1,1,1),(2,2,2,2,2),(3,3,3,3,3),(4,4,4,4,4),(5,5,5,5,5)")
-	rows := tk.MustQuery("explain analyze select /*+ use_index_merge(t1, primary, t1a) */ * from t1 where id < 2 or a > 4;").Rows()
-	require.Len(t, rows, 4)
-	explain := fmt.Sprintf("%v", rows[0])
-	pattern := ".*time:.*loops:.*index_task:{fetch_handle:.*, merge:.*}.*table_task:{num.*concurrency.*fetch_row.*wait_time.*}.*"
-	require.Regexp(t, pattern, explain)
-	tableRangeExplain := fmt.Sprintf("%v", rows[1])
-	indexExplain := fmt.Sprintf("%v", rows[2])
-	tableExplain := fmt.Sprintf("%v", rows[3])
-	require.Regexp(t, ".*time:.*loops:.*cop_task:.*", tableRangeExplain)
-	require.Regexp(t, ".*time:.*loops:.*cop_task:.*", indexExplain)
-	require.Regexp(t, ".*time:.*loops:.*cop_task:.*", tableExplain)
-	tk.MustExec("set @@tidb_enable_collect_execution_info=0;")
-	tk.MustQuery("select /*+ use_index_merge(t1, primary, t1a) */ * from t1 where id < 2 or a > 4 order by a").Check(testkit.Rows("1 1 1 1 1", "5 5 5 5 5"))
 }
 
 func TestHandleAssertionFailureForPartitionedTable(t *testing.T) {
