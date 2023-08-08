63d25003541d0b715fb1cf5936ec5749d12ed6b6
jiyfhust <jiyfhust@163.com>
2023-08-08 19:13:39 +0800

    fix

--- expression/builtin_miscellaneous_vec.go
+++ expression/builtin_miscellaneous_vec.go
@@ -359,13 +359,19 @@ func doSleep(secs float64, sessVars *variable.SessionVars) (isKilled bool) {
 	for {
 		select {
 		case <-ticker.C:
-			// Regular kill or Killed because of max execution time.
-			if atomic.LoadUint32(&sessVars.Killed) == 1 || atomic.LoadUint32(&sessVars.Killed) == 2 {
-				if len(sessVars.StmtCtx.TableIDs) == 0 {
-					atomic.StoreUint32(&sessVars.Killed, 0)
+			// MySQL 8.0 sleep: https://dev.mysql.com/doc/refman/8.0/en/miscellaneous-functions.html#function_sleep
+			if len(sessVars.StmtCtx.TableIDs) == 0 {
+				// Regular kill or Killed because of max execution time
+				if atomic.CompareAndSwapUint32(&sessVars.Killed, 1, 0) || atomic.CompareAndSwapUint32(&sessVars.Killed, 2, 0) {
+					timer.Stop()
+					return true
 				}
-				timer.Stop()
-				return true
+			} else {
+				// Regular kill or Killed because of max execution time.
+				if atomic.LoadUint32(&sessVars.Killed) == 1 || atomic.LoadUint32(&sessVars.Killed) == 2 {
+					timer.Stop()
+					return true
+				}
 			}
 		case <-timer.C:
 			return false
