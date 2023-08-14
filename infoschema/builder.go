c5652a241d94bdb6722f81010488c9cc41c68b71
Weizhen Wang <wangweizhen@pingcap.com>
2023-08-14 22:18:18 +0800

    *: use std/slices to replace exp/slices

--- infoschema/builder.go
+++ infoschema/builder.go
@@ -15,8 +15,10 @@
 package infoschema
 
 import (
+	"cmp"
 	"context"
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/ngaut/pools"
@@ -36,7 +38,6 @@ import (
 	"github.com/pingcap/tidb/util/mathutil"
 	"github.com/pingcap/tidb/util/sqlexec"
 	"go.uber.org/zap"
-	"golang.org/x/exp/slices"
 )
 
 type policyGetter struct {
@@ -819,8 +820,8 @@ func (b *Builder) applyCreateTable(m *meta.Meta, dbInfo *model.DBInfo, tableID i
 	bucketIdx := tableBucketIdx(tableID)
 	sortedTbls := b.is.sortedTablesBuckets[bucketIdx]
 	sortedTbls = append(sortedTbls, tbl)
-	slices.SortFunc(sortedTbls, func(i, j table.Table) bool {
-		return i.Meta().ID < j.Meta().ID
+	slices.SortFunc(sortedTbls, func(i, j table.Table) int {
+		return cmp.Compare(i.Meta().ID, j.Meta().ID)
 	})
 	b.is.sortedTablesBuckets[bucketIdx] = sortedTbls
 
@@ -1027,8 +1028,8 @@ func (b *Builder) InitWithDBInfos(dbInfos []*model.DBInfo, policies []*model.Pol
 
 	// Sort all tables by `ID`
 	for _, v := range info.sortedTablesBuckets {
-		slices.SortFunc(v, func(a, b table.Table) bool {
-			return a.Meta().ID < b.Meta().ID
+		slices.SortFunc(v, func(a, b table.Table) int {
+			return cmp.Compare(a.Meta().ID, b.Meta().ID)
 		})
 	}
 	return b, nil
