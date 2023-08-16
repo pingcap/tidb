1fe3836de945853e13fd3a97e635a732ad07fd06
Jianjun Liao <36503113+Leavrth@users.noreply.github.com>
2023-08-16 13:51:30 +0800

    This is an automated cherry-pick of #46040
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- br/pkg/storage/azblob.go
+++ br/pkg/storage/azblob.go
@@ -107,25 +107,59 @@ type sharedKeyClientBuilder struct {
 	cred        *azblob.SharedKeyCredential
 	accountName string
 	serviceURL  string
+
+	clientOptions *azblob.ClientOptions
 }
 
+<<<<<<< HEAD
 func (b *sharedKeyClientBuilder) GetServiceClient() (azblob.ServiceClient, error) {
 	return azblob.NewServiceClientWithSharedKey(b.serviceURL, b.cred, getDefaultClientOptions())
+=======
+func (b *sharedKeyClientBuilder) GetServiceClient() (*azblob.Client, error) {
+	return azblob.NewClientWithSharedKeyCredential(b.serviceURL, b.cred, b.clientOptions)
+>>>>>>> 88225787f3c (br: configure the httpclient for external storage (#46040))
 }
 
 func (b *sharedKeyClientBuilder) GetAccountName() string {
 	return b.accountName
 }
 
+<<<<<<< HEAD
+=======
+// use SAS to access azure blob storage
+type sasClientBuilder struct {
+	accountName string
+	// Example of serviceURL: https://<account>.blob.core.windows.net/?<sas token>
+	serviceURL string
+
+	clientOptions *azblob.ClientOptions
+}
+
+func (b *sasClientBuilder) GetServiceClient() (*azblob.Client, error) {
+	return azblob.NewClientWithNoCredential(b.serviceURL, b.clientOptions)
+}
+
+func (b *sasClientBuilder) GetAccountName() string {
+	return b.accountName
+}
+
+>>>>>>> 88225787f3c (br: configure the httpclient for external storage (#46040))
 // use token to access azure blob storage
 type tokenClientBuilder struct {
 	cred        *azidentity.ClientSecretCredential
 	accountName string
 	serviceURL  string
+
+	clientOptions *azblob.ClientOptions
 }
 
+<<<<<<< HEAD
 func (b *tokenClientBuilder) GetServiceClient() (azblob.ServiceClient, error) {
 	return azblob.NewServiceClient(b.serviceURL, b.cred, getDefaultClientOptions())
+=======
+func (b *tokenClientBuilder) GetServiceClient() (*azblob.Client, error) {
+	return azblob.NewClient(b.serviceURL, b.cred, b.clientOptions)
+>>>>>>> 88225787f3c (br: configure the httpclient for external storage (#46040))
 }
 
 func (b *tokenClientBuilder) GetAccountName() string {
@@ -144,6 +178,31 @@ func getAzureServiceClientBuilder(options *backuppb.AzureBlobStorage, opts *Exte
 		return nil, errors.New("bucket(container) cannot be empty to access azure blob storage")
 	}
 
+<<<<<<< HEAD
+=======
+	clientOptions := getDefaultClientOptions()
+	if opts != nil && opts.HTTPClient != nil {
+		clientOptions.Transport = opts.HTTPClient
+	}
+
+	if len(options.AccountName) > 0 && len(options.AccessSig) > 0 {
+		serviceURL := options.Endpoint
+		if len(serviceURL) == 0 {
+			if strings.HasPrefix(options.AccessSig, "?") {
+				serviceURL = fmt.Sprintf("https://%s.blob.core.windows.net/%s", options.AccountName, options.AccessSig)
+			} else {
+				serviceURL = fmt.Sprintf("https://%s.blob.core.windows.net/?%s", options.AccountName, options.AccessSig)
+			}
+		}
+		return &sasClientBuilder{
+			options.AccountName,
+			serviceURL,
+
+			clientOptions,
+		}, nil
+	}
+
+>>>>>>> 88225787f3c (br: configure the httpclient for external storage (#46040))
 	if len(options.AccountName) > 0 && len(options.SharedKey) > 0 {
 		serviceURL := options.Endpoint
 		if len(serviceURL) == 0 {
@@ -157,6 +216,8 @@ func getAzureServiceClientBuilder(options *backuppb.AzureBlobStorage, opts *Exte
 			cred,
 			options.AccountName,
 			serviceURL,
+
+			clientOptions,
 		}, nil
 	}
 
@@ -185,6 +246,8 @@ func getAzureServiceClientBuilder(options *backuppb.AzureBlobStorage, opts *Exte
 				cred,
 				accountName,
 				serviceURL,
+
+				clientOptions,
 			}, nil
 		}
 		log.Warn("Failed to get azure token credential but environment variables exist, try to use shared key.", zap.String("tenantId", tenantID), zap.String("clientId", clientID), zap.String("clientSecret", "?"))
@@ -212,6 +275,8 @@ func getAzureServiceClientBuilder(options *backuppb.AzureBlobStorage, opts *Exte
 		cred,
 		accountName,
 		serviceURL,
+
+		clientOptions,
 	}, nil
 }
 
