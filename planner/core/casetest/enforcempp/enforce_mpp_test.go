c4025fa4e522b3c56f1349affef55eef3f28b485
Arenatlx <314806019@qq.com>
2023-08-16 15:02:01 +0800

    planner: fix index heuristic rule will prune out hint preferred tiflash path (#46102)
    close pingcap/tidb#40146

--- planner/core/casetest/enforcempp/enforce_mpp_test.go
+++ planner/core/casetest/enforcempp/enforce_mpp_test.go
@@ -39,6 +39,7 @@ func TestEnforceMPP(t *testing.T) {
 	tk.MustExec("drop table if exists t")
 	tk.MustExec("create table t(a int, b int)")
 	tk.MustExec("create index idx on t(a)")
+	tk.MustExec("CREATE TABLE `s` (\n  `a` int(11) DEFAULT NULL,\n  `b` int(11) DEFAULT NULL,\n  `c` int(11) DEFAULT NULL,\n  `d` int(11) DEFAULT NULL,\n  UNIQUE KEY `a` (`a`),\n  KEY `ii` (`a`,`b`)\n)")
 
 	// Default RPC encoding may cause statistics explain result differ and then the test unstable.
 	tk.MustExec("set @@tidb_enable_chunk_rpc = on")
@@ -52,6 +53,12 @@ func TestEnforceMPP(t *testing.T) {
 	require.True(t, exists)
 	for _, tblInfo := range db.Tables {
 		if tblInfo.Name.L == "t" {
+			tblInfo.TiFlashReplica = &model.TiFlashReplicaInfo{
+				Count:     1,
+				Available: true,
+			}
+		}
+		if tblInfo.Name.L == "s" {
 			tblInfo.TiFlashReplica = &model.TiFlashReplicaInfo{
 				Count:     1,
 				Available: true,
