aae73d8a0befd6744ed63771805f5ac68dd353fd
husharp <jinhao.hu@pingcap.com>
2023-08-17 17:25:18 +0800

    fix panic when failed to create pdclient
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- store/driver/tikv_driver.go
+++ store/driver/tikv_driver.go
@@ -156,11 +156,11 @@ func (d TiKVDriver) OpenWithOptions(path string, options ...Option) (resStore kv
 		),
 		pd.WithCustomTimeoutOption(time.Duration(d.pdConfig.PDServerTimeout)*time.Second),
 		pd.WithForwardingOption(config.GetGlobalConfig().EnableForwarding))
-	pdCli = util.InterceptedPDClient{Client: pdCli}
 
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
+	pdCli = util.InterceptedPDClient{Client: pdCli}
 
 	// FIXME: uuid will be a very long and ugly string, simplify it.
 	uuid := fmt.Sprintf("tikv-%v", pdCli.GetClusterID(context.TODO()))
