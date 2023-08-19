31be55688f05ad35ebe7eb2df2d0b0206ce2f0d9
Weizhen Wang <wangweizhen@pingcap.com>
2023-08-19 11:18:42 +0800

    executor: enable stats LFU cache
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- executor/main_test.go
+++ executor/main_test.go
@@ -50,6 +50,7 @@ func TestMain(m *testing.M) {
 		conf.TiKVClient.AsyncCommit.SafeWindow = 0
 		conf.TiKVClient.AsyncCommit.AllowedClockDrift = 0
 		conf.Experimental.AllowsExpressionIndex = true
+		conf.Performance.EnableStatsCacheMemQuota = true
 	})
 	tikv.EnableFailpoints()
 	variable.StatsCacheMemQuota.Store(5000)
