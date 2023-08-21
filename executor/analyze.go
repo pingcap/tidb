ee64474c94853892281aab3f4b1a6a89d8931f49
hi-rustin <rustin.liu@gmail.com>
2023-08-21 18:17:40 +0800

    planner: fix wrong resultsCh len

--- executor/analyze.go
+++ executor/analyze.go
@@ -109,7 +109,7 @@ func (e *AnalyzeExec) Next(ctx context.Context, _ *chunk.Chunk) error {
 
 	// Start workers with channel to collect results.
 	taskCh := make(chan *analyzeTask, concurrency)
-	resultsCh := make(chan *statistics.AnalyzeResults, concurrency)
+	resultsCh := make(chan *statistics.AnalyzeResults, len(tasks))
 	for i := 0; i < concurrency; i++ {
 		e.wg.Run(func() { e.analyzeWorker(taskCh, resultsCh) })
 	}
