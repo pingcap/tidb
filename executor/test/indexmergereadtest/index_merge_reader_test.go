10c32ce91d87f4c978c0185821233d24c2f67bcf
Hangjie Mo <mohangjie1995@gmail.com>
2023-08-21 15:18:33 +0800

    executor: fix retChk = nil in index_merge_reader (#46111)
    close pingcap/tidb#46005

--- executor/test/indexmergereadtest/index_merge_reader_test.go
+++ executor/test/indexmergereadtest/index_merge_reader_test.go
@@ -1263,3 +1263,16 @@ func TestIndexMergeKeepOrderDirtyRead(t *testing.T) {
 	tk.MustQuery(querySQL).Check(testkit.Rows("1 2 4", "1 1 1"))
 	tk.MustExec("rollback")
 }
+
+func TestIssues46005(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("set tidb_index_lookup_size = 1024")
+	tk.MustExec("create table t(a int, b int, c int, index idx1(a, c), index idx2(b, c))")
+	for i := 0; i < 1500; i++ {
+		tk.MustExec(fmt.Sprintf("insert into t(a,b,c) values (1, 1, %d)", i))
+	}
+
+	tk.MustQuery("select /*+ USE_INDEX_MERGE(t, idx1, idx2) */ * from t where a = 1 or b = 1 order by c limit 1025")
+}
