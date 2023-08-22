ed431e5a30c1ed60426ce7890f5e8f60b7a720c8
Hangjie Mo <mohangjie1995@gmail.com>
2023-08-22 22:36:34 +0800

    parser: fix parser identifier with dot (#46314)
    close pingcap/tidb#35031

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1058,6 +1058,7 @@ AAAAAAAAAAAA5gm5Mg==
 
 		{"select `t`.`1a`.1 from t;", true, "SELECT `t`.`1a`.`1` FROM `t`"},
 		{"select * from 1db.1table;", true, "SELECT * FROM `1db`.`1table`"},
+		{"select * from t where t. status = 1;", true, "SELECT * FROM `t` WHERE `t`.`status`=1"},
 
 		// for show placement
 		{"SHOW PLACEMENT", true, "SHOW PLACEMENT"},
