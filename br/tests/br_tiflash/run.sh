957b87d60a54465f24873528317ba0efdece8dd6
3pointer <luancheng@pingcap.com>
2023-08-22 13:10:57 +0800

    add partition test

--- br/tests/br_tiflash/run.sh
+++ br/tests/br_tiflash/run.sh
@@ -23,16 +23,30 @@ run_sql "CREATE DATABASE $DB"
 
 run_sql "CREATE TABLE $DB.kv(k varchar(256) primary key, v int)"
 
+CREATE TABLE $DB.partition_kv(
+    id BIGINT,
+    data INT,
+    PRIMARY KEY(id) CLUSTERED
+) PARTITION BY RANGE(id) (
+    PARTITION p0 VALUES LESS THAN (200),
+    PARTITION p1 VALUES LESS THAN (400),
+    PARTITION p2 VALUES LESS THAN (600)
+);
+
 stmt="INSERT INTO $DB.kv(k, v) VALUES ('1-record', 1)"
+parition_stmt="INSERT INTO $DB.partition_kv(k, v) VALUES (1, 1)"
 for i in $(seq 2 $RECORD_COUNT); do
     stmt="$stmt,('$i-record', $i)"
+    parition_stmt="$parition_stmt,($i, $i)"
 done
 run_sql "$stmt"
+run_sql "$parition_stmt"
 
 if ! run_sql "ALTER TABLE $DB.kv SET TIFLASH REPLICA 1"; then
   # 10s should be enough for tiflash-proxy get started
   sleep 10
   run_sql "ALTER TABLE $DB.kv SET TIFLASH REPLICA 1"
+  run_sql "ALTER TABLE $DB.partition_kv SET TIFLASH REPLICA 1"
 fi
 
 
