df269615f9a38f5918154b506abe8e374a2bf263
Jack Yu <jackysp@gmail.com>
2023-08-22 17:01:53 +0800

    planner: fix potential panic when generating hints in statement summary
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -547,7 +547,7 @@ func (b *PlanBuilder) buildResultSetNode(ctx context.Context, node ast.ResultSet
 			}
 		}
 		// `TableName` is not a select block, so we do not need to handle it.
-		if !isTableName && b.ctx.GetSessionVars().PlannerSelectBlockAsName.Load() != nil {
+		if !isTableName && len(*(b.ctx.GetSessionVars().PlannerSelectBlockAsName.Load())) > 0 {
 			plannerSelectBlockAsName := *(b.ctx.GetSessionVars().PlannerSelectBlockAsName.Load())
 			plannerSelectBlockAsName[p.SelectBlockOffset()] = ast.HintTable{DBName: p.OutputNames()[0].DBName, TableName: p.OutputNames()[0].TblName}
 		}
