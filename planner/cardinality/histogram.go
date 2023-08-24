663e0b94f9c51f5c8f84f25ebd8c5d4e06b1ab3f
qw4990 <zhangyuanjia@pingcap.com>
2023-08-24 12:23:21 +0800

    fixup

--- planner/cardinality/histogram.go
+++ planner/cardinality/histogram.go
@@ -1,3 +1,17 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cardinality
 
 import (
@@ -103,7 +117,6 @@ type countByRangeFunc = func(sessionctx.Context, int64, []*ranger.Range) (float6
 
 // newHistogramBySelectivity fulfills the content of new histogram by the given selectivity result.
 // TODO: remove this function after removing docs.pingcap.com/tidb/stable/system-variables#tidb_optimizer_selectivity_level.
-//	Also, there're redundant calculation with Selectivity(). We need to reduce it too.
 func newHistogramBySelectivity(sctx sessionctx.Context, histID int64, oldHist, newHist *statistics.Histogram, ranges []*ranger.Range, cntByRangeFunc countByRangeFunc) error {
 	cntPerVal := int64(oldHist.AvgCountPerNotNullValue(int64(oldHist.TotalRowCount())))
 	var totCnt int64
