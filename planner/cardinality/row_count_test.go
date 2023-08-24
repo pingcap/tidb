6a09f74cb391c62e620cfc8004a87185af4d8325
qw4990 <zhangyuanjia@pingcap.com>
2023-08-24 16:32:19 +0800

    fixup

--- planner/cardinality/row_count_test.go
+++ planner/cardinality/row_count_test.go
@@ -15,14 +15,14 @@
 package cardinality
 
 import (
-	"testing"
-
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/statistics"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/mock"
 	"github.com/stretchr/testify/require"
+	"math"
+	"testing"
 )
 
 func TestPseudoTable(t *testing.T) {
@@ -53,4 +53,107 @@ func TestPseudoTable(t *testing.T) {
 	tbl = statistics.PseudoTable(ti)
 	// We added a hidden column. The pseudo table still only have one column.
 	require.Equal(t, len(tbl.Columns), 1)
+}
+
+func TestEnumRangeValues(t *testing.T) {
+	tests := []struct {
+		low         types.Datum
+		high        types.Datum
+		lowExclude  bool
+		highExclude bool
+		res         string
+	}{
+		{
+			low:         types.NewIntDatum(0),
+			high:        types.NewIntDatum(5),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(0, 1, 2, 3, 4)",
+		},
+		{
+			low:         types.NewIntDatum(math.MinInt64),
+			high:        types.NewIntDatum(math.MaxInt64),
+			lowExclude:  false,
+			highExclude: false,
+			res:         "",
+		},
+		{
+			low:         types.NewUintDatum(0),
+			high:        types.NewUintDatum(5),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(0, 1, 2, 3, 4)",
+		},
+		{
+			low:         types.NewDurationDatum(getDuration("0:00:00")),
+			high:        types.NewDurationDatum(getDuration("0:00:05")),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(00:00:00, 00:00:01, 00:00:02, 00:00:03, 00:00:04)",
+		},
+		{
+			low:         types.NewDurationDatum(getDuration("0:00:00")),
+			high:        types.NewDurationDatum(getDuration("0:00:05")),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(00:00:00, 00:00:01, 00:00:02, 00:00:03, 00:00:04)",
+		},
+		{
+			low:         types.NewTimeDatum(getTime(2017, 1, 1, mysql.TypeDate)),
+			high:        types.NewTimeDatum(getTime(2017, 1, 5, mysql.TypeDate)),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(2017-01-01, 2017-01-02, 2017-01-03, 2017-01-04)",
+		},
+		{
+			low:         types.NewTimeDatum(getTimeStamp(0, 0, 0, mysql.TypeTimestamp)),
+			high:        types.NewTimeDatum(getTimeStamp(0, 0, 5, mysql.TypeTimestamp)),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(2017-01-01 00:00:00, 2017-01-01 00:00:01, 2017-01-01 00:00:02, 2017-01-01 00:00:03, 2017-01-01 00:00:04)",
+		},
+		{
+			low:         types.NewTimeDatum(getTimeStamp(0, 0, 0, mysql.TypeDatetime)),
+			high:        types.NewTimeDatum(getTimeStamp(0, 0, 5, mysql.TypeDatetime)),
+			lowExclude:  false,
+			highExclude: true,
+			res:         "(2017-01-01 00:00:00, 2017-01-01 00:00:01, 2017-01-01 00:00:02, 2017-01-01 00:00:03, 2017-01-01 00:00:04)",
+		},
+		// fix issue 11610
+		{
+			low:         types.NewIntDatum(math.MinInt64),
+			high:        types.NewIntDatum(0),
+			lowExclude:  false,
+			highExclude: false,
+			res:         "",
+		},
+		{
+			low:         types.NewTimeDatum(getTime(2017, 1, 1, mysql.TypeDate)),
+			high:        types.NewTimeDatum(getTime(2017, 1, 1, mysql.TypeDate)),
+			lowExclude:  true,
+			highExclude: true,
+			res:         "",
+		},
+	}
+	for _, test := range tests {
+		vals := enumRangeValues(test.low, test.high, test.lowExclude, test.highExclude)
+		str, err := types.DatumsToString(vals, true)
+		require.NoError(t, err)
+		require.Equal(t, test.res, str)
+	}
+}
+
+func getDuration(value string) types.Duration {
+	dur, _, _ := types.ParseDuration(nil, value, 0)
+	return dur
+}
+
+func getTime(year, month, day int, timeType byte) types.Time {
+	ret := types.NewTime(types.FromDate(year, month, day, 0, 0, 0, 0), timeType, types.DefaultFsp)
+	return ret
+}
+
+func getTimeStamp(hour, min, sec int, timeType byte) types.Time {
+	ret := types.NewTime(types.FromDate(2017, 1, 1, hour, min, sec, 0), timeType, 0)
+	return ret
 }
--- statistics/scalar_test.go
+++ statistics/scalar_test.go
@@ -15,7 +15,6 @@
 package statistics
 
 import (
-	"math"
 	"testing"
 
 	"github.com/pingcap/tidb/parser/mysql"
@@ -41,11 +40,6 @@ func getDuration(value string) types.Duration {
 
 func getTime(year, month, day int, timeType byte) types.Time {
 	ret := types.NewTime(types.FromDate(year, month, day, 0, 0, 0, 0), timeType, types.DefaultFsp)
-	return ret
-}
-
-func getTimeStamp(hour, min, sec int, timeType byte) types.Time {
-	ret := types.NewTime(types.FromDate(2017, 1, 1, hour, min, sec, 0), timeType, 0)
 	return ret
 }
 
@@ -173,93 +167,5 @@ func TestCalcFraction(t *testing.T) {
 		hg.PreCalculateScalar()
 		fraction := hg.calcFraction(0, &test.value)
 		require.InDelta(t, test.fraction, fraction, eps)
-	}
-}
-
-func TestEnumRangeValues(t *testing.T) {
-	tests := []struct {
-		low         types.Datum
-		high        types.Datum
-		lowExclude  bool
-		highExclude bool
-		res         string
-	}{
-		{
-			low:         types.NewIntDatum(0),
-			high:        types.NewIntDatum(5),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(0, 1, 2, 3, 4)",
-		},
-		{
-			low:         types.NewIntDatum(math.MinInt64),
-			high:        types.NewIntDatum(math.MaxInt64),
-			lowExclude:  false,
-			highExclude: false,
-			res:         "",
-		},
-		{
-			low:         types.NewUintDatum(0),
-			high:        types.NewUintDatum(5),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(0, 1, 2, 3, 4)",
-		},
-		{
-			low:         types.NewDurationDatum(getDuration("0:00:00")),
-			high:        types.NewDurationDatum(getDuration("0:00:05")),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(00:00:00, 00:00:01, 00:00:02, 00:00:03, 00:00:04)",
-		},
-		{
-			low:         types.NewDurationDatum(getDuration("0:00:00")),
-			high:        types.NewDurationDatum(getDuration("0:00:05")),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(00:00:00, 00:00:01, 00:00:02, 00:00:03, 00:00:04)",
-		},
-		{
-			low:         types.NewTimeDatum(getTime(2017, 1, 1, mysql.TypeDate)),
-			high:        types.NewTimeDatum(getTime(2017, 1, 5, mysql.TypeDate)),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(2017-01-01, 2017-01-02, 2017-01-03, 2017-01-04)",
-		},
-		{
-			low:         types.NewTimeDatum(getTimeStamp(0, 0, 0, mysql.TypeTimestamp)),
-			high:        types.NewTimeDatum(getTimeStamp(0, 0, 5, mysql.TypeTimestamp)),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(2017-01-01 00:00:00, 2017-01-01 00:00:01, 2017-01-01 00:00:02, 2017-01-01 00:00:03, 2017-01-01 00:00:04)",
-		},
-		{
-			low:         types.NewTimeDatum(getTimeStamp(0, 0, 0, mysql.TypeDatetime)),
-			high:        types.NewTimeDatum(getTimeStamp(0, 0, 5, mysql.TypeDatetime)),
-			lowExclude:  false,
-			highExclude: true,
-			res:         "(2017-01-01 00:00:00, 2017-01-01 00:00:01, 2017-01-01 00:00:02, 2017-01-01 00:00:03, 2017-01-01 00:00:04)",
-		},
-		// fix issue 11610
-		{
-			low:         types.NewIntDatum(math.MinInt64),
-			high:        types.NewIntDatum(0),
-			lowExclude:  false,
-			highExclude: false,
-			res:         "",
-		},
-		{
-			low:         types.NewTimeDatum(getTime(2017, 1, 1, mysql.TypeDate)),
-			high:        types.NewTimeDatum(getTime(2017, 1, 1, mysql.TypeDate)),
-			lowExclude:  true,
-			highExclude: true,
-			res:         "",
-		},
-	}
-	for _, test := range tests {
-		vals := enumRangeValues(test.low, test.high, test.lowExclude, test.highExclude)
-		str, err := types.DatumsToString(vals, true)
-		require.NoError(t, err)
-		require.Equal(t, test.res, str)
 	}
 }
