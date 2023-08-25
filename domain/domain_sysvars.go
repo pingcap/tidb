85875fc22205bbf885fd1075847ddcbe190826dd
Weizhen Wang <wangweizhen@pingcap.com>
2023-08-25 17:24:43 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- domain/domain_sysvars.go
+++ domain/domain_sysvars.go
@@ -20,7 +20,9 @@ import (
 	"time"
 
 	"github.com/pingcap/tidb/sessionctx/variable"
+	"github.com/pingcap/tidb/util/logutil"
 	pd "github.com/tikv/pd/client"
+	"go.uber.org/zap"
 )
 
 // initDomainSysVars() is called when a domain is initialized.
@@ -44,6 +46,7 @@ func (do *Domain) initDomainSysVars() {
 // setStatsCacheCapacity sets statsCache cap
 func (do *Domain) setStatsCacheCapacity(c int64) {
 	do.StatsHandle().SetStatsCacheCapacity(c)
+	logutil.BgLogger().Info("update stats cache capacity successfully", zap.Int64("capacity", c))
 }
 
 func (do *Domain) setPDClientDynamicOption(name, sVal string) {
