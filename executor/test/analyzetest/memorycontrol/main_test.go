685bcaaefc446464b5f198c721795ecf45be814b
Weizhen Wang <wangweizhen@pingcap.com>
2023-08-25 15:53:05 +0800

    *: enable lfu for analyze (#46399)
    ref pingcap/tidb#46158

--- executor/test/analyzetest/memorycontrol/main_test.go
+++ executor/test/analyzetest/memorycontrol/main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2022 PingCAP, Inc.
+// Copyright 2023 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,12 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package analyzetest
+package memorycontrol
 
 import (
 	"testing"
 
 	"github.com/pingcap/tidb/config"
+	"github.com/pingcap/tidb/sessionctx/variable"
 	"go.uber.org/goleak"
 )
 
@@ -25,6 +26,7 @@ func TestMain(m *testing.M) {
 	config.UpdateGlobal(func(conf *config.Config) {
 		conf.Performance.EnableStatsCacheMemQuota = true
 	})
+	variable.StatsCacheMemQuota.Store(1000000)
 	opts := []goleak.Option{
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
