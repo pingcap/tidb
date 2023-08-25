40bed95fa8bd9831d5f5cfc5457fe721cc259c25
Lynn <zimu_xia@126.com>
2023-08-25 17:45:36 +0800

    *: update the HTTP API about "upgrade" and fix a minor doc issue (#46425)
    close pingcap/tidb#46424

--- server/http_status.go
+++ server/http_status.go
@@ -240,7 +240,7 @@ func (s *Server) startHTTPServer() {
 	router.Handle("/tiflash/replica-deprecated", flashReplicaHandler{tikvHandlerTool})
 
 	// HTTP path for upgrade operations.
-	router.Handle("/upgrade", handler.NewClusterUpgradeHandler(tikvHandlerTool.Store.(kv.Storage))).Name("upgrade operations")
+	router.Handle("/upgrade/{op}", handler.NewClusterUpgradeHandler(tikvHandlerTool.Store.(kv.Storage))).Name("upgrade operations")
 
 	if s.cfg.Store == "tikv" {
 		// HTTP path for tikv.
