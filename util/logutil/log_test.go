f4c1f09a6f299f2348a3de1075fb7a72b966514a
Chao Wang <cclcwangchao@hotmail.com>
2023-08-28 14:28:53 +0800

    ddl: add `conn` and `session_alias` entry in ddl worker log

--- util/logutil/log_test.go
+++ util/logutil/log_test.go
@@ -84,6 +84,22 @@ func TestZapLoggerWithKeys(t *testing.T) {
 
 	err = InitLogger(conf)
 	require.NoError(t, err)
+	newLogger := LoggerWithTraceInfo(log.L(), &model.TraceInfo{ConnectionID: 456, SessionAlias: "alias789"})
+	ctx1 = context.WithValue(context.Background(), CtxLogKey, newLogger)
+	testZapLogger(ctx1, t, fileCfg.Filename, zapLogWithTraceInfoPattern)
+	err = os.Remove(fileCfg.Filename)
+	require.NoError(t, err)
+
+	err = InitLogger(conf)
+	require.NoError(t, err)
+	newLogger = LoggerWithTraceInfo(log.L(), nil)
+	ctx1 = context.WithValue(context.Background(), CtxLogKey, newLogger)
+	testZapLogger(ctx1, t, fileCfg.Filename, zapLogWithoutCheckKeyPattern)
+	err = os.Remove(fileCfg.Filename)
+	require.NoError(t, err)
+
+	err = InitLogger(conf)
+	require.NoError(t, err)
 	key := "ctxKey"
 	val := "ctxValue"
 	ctx1 = WithKeyValue(context.Background(), key, val)
