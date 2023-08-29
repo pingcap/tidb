94165f2502642f792bb9591b97e328a6d59ae456
ywqzzy <592838129@qq.com>
2023-08-29 14:54:12 +0800

    refine

--- ddl/disttask_flow.go
+++ ddl/disttask_flow.go
@@ -18,8 +18,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"sort"
-
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/disttask/framework/dispatcher"
 	"github.com/pingcap/tidb/disttask/framework/proto"
@@ -30,6 +28,7 @@ import (
 	"github.com/pingcap/tidb/store/helper"
 	"github.com/pingcap/tidb/table"
 	"github.com/tikv/client-go/v2/tikv"
+	"sort"
 )
 
 type litBackfillFlowHandle struct {
@@ -49,32 +48,29 @@ func (*litBackfillFlowHandle) OnTicker(_ context.Context, _ *proto.Task) {
 }
 
 // ProcessNormalFlow processes the normal flow.
-func (h *litBackfillFlowHandle) ProcessNormalFlow(_ context.Context, _ dispatcher.TaskHandle, gTask *proto.Task) (metas [][]byte, err error) {
+func (h *litBackfillFlowHandle) ProcessNormalFlow(ctx context.Context, _ dispatcher.TaskHandle, gTask *proto.Task) (metas [][]byte, err error) {
 	var globalTaskMeta BackfillGlobalMeta
 	if err = json.Unmarshal(gTask.Meta, &globalTaskMeta); err != nil {
 		return nil, err
 	}
-
 	d, ok := h.d.(*ddl)
 	if !ok {
 		return nil, errors.New("The getDDL result should be the type of *ddl")
 	}
-
 	job := &globalTaskMeta.Job
-	var tblInfo *model.TableInfo
-	err = kv.RunInNewTxn(d.ctx, d.store, true, func(ctx context.Context, txn kv.Transaction) error {
-		tblInfo, err = meta.NewMeta(txn).GetTable(job.SchemaID, job.TableID)
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-
+	tblInfo, err := getTblInfo(d, job)
 	var subTaskMetas [][]byte
 	if tblInfo.Partition == nil {
 		switch gTask.Step {
+		case proto.StepInit:
+			subtaskMeta, err := generateNonPartitionPlan(d, tblInfo, job)
+			if err != nil {
+				return nil, err
+			}
+			gTask.Step = proto.StepOne
+			return subtaskMeta, nil
 		case proto.StepOne:
-			serverNodes, err := dispatcher.GenerateSchedulerNodes(d.ctx)
+			serverNodes, err := dispatcher.GenerateSchedulerNodes(ctx)
 			if err != nil {
 				return nil, err
 			}
@@ -92,83 +88,20 @@ func (h *litBackfillFlowHandle) ProcessNormalFlow(_ context.Context, _ dispatche
 		case proto.StepTwo:
 			return nil, nil
 		default:
-		}
-		tbl, err := getTable(d.store, job.SchemaID, tblInfo)
-		if err != nil {
-			return nil, err
-		}
-		ver, err := getValidCurrentVersion(d.store)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-		startKey, endKey, err := getTableRange(d.jobContext(job.ID), d.ddlCtx, tbl.(table.PhysicalTable), ver.Ver, job.Priority)
-		if startKey == nil && endKey == nil {
-			// Empty table.
-			gTask.Step = proto.StepOne
 			return nil, nil
-		}
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-		regionCache := d.store.(helper.Storage).GetRegionCache()
-		recordRegionMetas, err := regionCache.LoadRegionsInKeyRange(tikv.NewBackofferWithVars(context.Background(), 20000, nil), startKey, endKey)
-		if err != nil {
-			return nil, err
-		}
-
-		subTaskMetas = make([][]byte, 0, 100)
-		regionBatch := 20
-		sort.Slice(recordRegionMetas, func(i, j int) bool {
-			return bytes.Compare(recordRegionMetas[i].StartKey(), recordRegionMetas[j].StartKey()) < 0
-		})
-		for i := 0; i < len(recordRegionMetas); i += regionBatch {
-			end := i + regionBatch
-			if end > len(recordRegionMetas) {
-				end = len(recordRegionMetas)
-			}
-			batch := recordRegionMetas[i:end]
-			subTaskMeta := &BackfillSubTaskMeta{StartKey: batch[0].StartKey(), EndKey: batch[len(batch)-1].EndKey()}
-			if i == 0 {
-				subTaskMeta.StartKey = startKey
-			}
-			if end == len(recordRegionMetas) {
-				subTaskMeta.EndKey = endKey
-			}
-			metaBytes, err := json.Marshal(subTaskMeta)
-			if err != nil {
-				return nil, err
-			}
-			subTaskMetas = append(subTaskMetas, metaBytes)
 		}
 	} else {
-		if gTask.State != proto.TaskStatePending {
-			// This flow for partition table has only one step, finish task when it is not pending
+		if gTask.Step != proto.StepInit {
+			// This flow for partition table has only one step
 			return nil, nil
 		}
-
-		defs := tblInfo.Partition.Definitions
-		physicalIDs := make([]int64, len(defs))
-		for i := range defs {
-			physicalIDs[i] = defs[i].ID
+		subTaskMetas, err := generatePartitionPlan(tblInfo)
+		if err != nil {
+			return nil, err
 		}
-
-		subTaskMetas = make([][]byte, 0, len(physicalIDs))
-		for _, physicalID := range physicalIDs {
-			subTaskMeta := &BackfillSubTaskMeta{
-				PhysicalTableID: physicalID,
-			}
-
-			metaBytes, err := json.Marshal(subTaskMeta)
-			if err != nil {
-				return nil, err
-			}
-
-			subTaskMetas = append(subTaskMetas, metaBytes)
-		}
+		gTask.Step = proto.StepOne
+		return subTaskMetas, nil
 	}
-
-	gTask.Step = proto.StepOne
-	return subTaskMetas, nil
 }
 
 func (*litBackfillFlowHandle) ProcessErrFlow(_ context.Context, _ dispatcher.TaskHandle, task *proto.Task, receiveErr []error) (meta []byte, err error) {
@@ -186,4 +119,89 @@ func (*litBackfillFlowHandle) GetEligibleInstances(ctx context.Context, _ *proto
 // IsRetryableErr implements TaskFlowHandle.IsRetryableErr interface.
 func (*litBackfillFlowHandle) IsRetryableErr(error) bool {
 	return true
+}
+
+func getTblInfo(d *ddl, job *model.Job) (tblInfo *model.TableInfo, err error) {
+	err = kv.RunInNewTxn(d.ctx, d.store, true, func(ctx context.Context, txn kv.Transaction) error {
+		tblInfo, err = meta.NewMeta(txn).GetTable(job.SchemaID, job.TableID)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return tblInfo, nil
+}
+
+func generatePartitionPlan(tblInfo *model.TableInfo) (metas [][]byte, err error) {
+	defs := tblInfo.Partition.Definitions
+	physicalIDs := make([]int64, len(defs))
+	for i := range defs {
+		physicalIDs[i] = defs[i].ID
+	}
+
+	subTaskMetas := make([][]byte, 0, len(physicalIDs))
+	for _, physicalID := range physicalIDs {
+		subTaskMeta := &BackfillSubTaskMeta{
+			PhysicalTableID: physicalID,
+		}
+
+		metaBytes, err := json.Marshal(subTaskMeta)
+		if err != nil {
+			return nil, err
+		}
+
+		subTaskMetas = append(subTaskMetas, metaBytes)
+	}
+	return subTaskMetas, nil
+}
+
+func generateNonPartitionPlan(d *ddl, tblInfo *model.TableInfo, job *model.Job) (metas [][]byte, err error) {
+	tbl, err := getTable(d.store, job.SchemaID, tblInfo)
+	if err != nil {
+		return nil, err
+	}
+	ver, err := getValidCurrentVersion(d.store)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	startKey, endKey, err := getTableRange(d.jobContext(job.ID), d.ddlCtx, tbl.(table.PhysicalTable), ver.Ver, job.Priority)
+	if startKey == nil && endKey == nil {
+		// Empty table.
+		return nil, nil
+	}
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	regionCache := d.store.(helper.Storage).GetRegionCache()
+	recordRegionMetas, err := regionCache.LoadRegionsInKeyRange(tikv.NewBackofferWithVars(context.Background(), 20000, nil), startKey, endKey)
+	if err != nil {
+		return nil, err
+	}
+
+	subTaskMetas := make([][]byte, 0, 100)
+	regionBatch := 20
+	sort.Slice(recordRegionMetas, func(i, j int) bool {
+		return bytes.Compare(recordRegionMetas[i].StartKey(), recordRegionMetas[j].StartKey()) < 0
+	})
+	for i := 0; i < len(recordRegionMetas); i += regionBatch {
+		end := i + regionBatch
+		if end > len(recordRegionMetas) {
+			end = len(recordRegionMetas)
+		}
+		batch := recordRegionMetas[i:end]
+		subTaskMeta := &BackfillSubTaskMeta{StartKey: batch[0].StartKey(), EndKey: batch[len(batch)-1].EndKey()}
+		if i == 0 {
+			subTaskMeta.StartKey = startKey
+		}
+		if end == len(recordRegionMetas) {
+			subTaskMeta.EndKey = endKey
+		}
+		metaBytes, err := json.Marshal(subTaskMeta)
+		if err != nil {
+			return nil, err
+		}
+		subTaskMetas = append(subTaskMetas, metaBytes)
+	}
+	return subTaskMetas, nil
 }
