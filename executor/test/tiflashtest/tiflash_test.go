5227de56fe4eddfbfcef5e07b2918ebff82a8464
Arenatlx <314806019@qq.com>
2023-08-30 16:51:30 +0800

    planner: fix wrong result when pushing Agg down through Union in MPP plans (#46310)

--- executor/test/tiflashtest/tiflash_test.go
+++ executor/test/tiflashtest/tiflash_test.go
@@ -1236,6 +1236,37 @@ func TestAggPushDownCountStar(t *testing.T) {
 	tk.MustQuery("select count(*) from c, o where c.c_id=o.c_id").Check(testkit.Rows("5"))
 }
 
+func TestAggPushDownUnionAndMPP(t *testing.T) {
+	store := testkit.CreateMockStore(t, withMockTiFlash(2))
+	tk := testkit.NewTestKit(t, store)
+
+	tk.MustExec("use test")
+	tk.MustExec("create table t (a int, b int)")
+	tk.MustExec("alter table t set tiflash replica 1")
+	tk.MustExec("insert into t values (1, 1);")
+	tk.MustExec("insert into t values (1, 1);")
+	tk.MustExec("insert into t values (1, 1);")
+	tk.MustExec("insert into t values (1, 1);")
+	tk.MustExec("insert into t values (1, 1);")
+	tk.MustExec("set @@tidb_allow_mpp=1;")
+	tk.MustExec("set @@tidb_enforce_mpp=1;")
+	tk.MustExec("set @@tidb_opt_agg_push_down=1")
+
+	tk.MustExec("create table c(c_id int)")
+	tk.MustExec("create table o(o_id int, c_id int)")
+	tk.MustExec("insert into c values(1),(1),(1),(1)")
+	tk.MustExec("insert into o values(1,1),(1,1),(1,2)")
+	tk.MustExec("alter table c set tiflash replica 1")
+	tk.MustExec("alter table o set tiflash replica 1")
+
+	tk.MustQuery("select a, count(*) from (select a, b from t " +
+		"union all " +
+		"select a, b from t" +
+		") t group by a order by a limit 10;").Check(testkit.Rows("1 10"))
+
+	tk.MustQuery("select o.o_id, count(*) from c, o where c.c_id=o.o_id group by o.o_id").Check(testkit.Rows("1 12"))
+}
+
 func TestGroupStreamAggOnTiFlash(t *testing.T) {
 	store := testkit.CreateMockStore(t, withMockTiFlash(2))
 	tk := testkit.NewTestKit(t, store)
