754acf95dd0570c35899e3057c22c876e84b367a
tangenta <tangenta@126.com>
2023-08-30 12:26:15 +0800

    Revert "fix partition table not import"
    This reverts commit 66479e7d8e438452bfad8b4a8f48016aa36193aa.

--- ddl/disttask_flow.go
+++ ddl/disttask_flow.go
@@ -71,29 +71,28 @@ func (h *litBackfillFlowHandle) ProcessNormalFlow(_ context.Context, _ dispatche
 	}
 
 	var subTaskMetas [][]byte
-	switch gTask.Step {
-	case proto.StepOne:
-		serverNodes, err := dispatcher.GenerateSchedulerNodes(d.ctx)
-		if err != nil {
-			return nil, err
-		}
-		subTaskMetas = make([][]byte, 0, len(serverNodes))
-		dummyMeta := &BackfillSubTaskMeta{}
-		metaBytes, err := json.Marshal(dummyMeta)
-		if err != nil {
-			return nil, err
-		}
-		for range serverNodes {
-			subTaskMetas = append(subTaskMetas, metaBytes)
-		}
-		gTask.Step = proto.StepTwo
-		return subTaskMetas, nil
-	case proto.StepTwo:
-		return nil, nil
-	}
-
-	// proto.StepInit
 	if tblInfo.Partition == nil {
+		switch gTask.Step {
+		case proto.StepOne:
+			serverNodes, err := dispatcher.GenerateSchedulerNodes(d.ctx)
+			if err != nil {
+				return nil, err
+			}
+			subTaskMetas = make([][]byte, 0, len(serverNodes))
+			dummyMeta := &BackfillSubTaskMeta{}
+			metaBytes, err := json.Marshal(dummyMeta)
+			if err != nil {
+				return nil, err
+			}
+			for range serverNodes {
+				subTaskMetas = append(subTaskMetas, metaBytes)
+			}
+			gTask.Step = proto.StepTwo
+			return subTaskMetas, nil
+		case proto.StepTwo:
+			return nil, nil
+		default:
+		}
 		tbl, err := getTable(d.store, job.SchemaID, tblInfo)
 		if err != nil {
 			return nil, err
