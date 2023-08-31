26c809fa87e8d2e12afba4a1a6f91f74f93f26f0
crazycs520 <crazycs520@gmail.com>
2023-08-31 16:52:55 +0800

    domain: enlarge infoCache capacity to avoid cache miss and add metric in grafana
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- domain/domain.go
+++ domain/domain.go
@@ -1046,7 +1046,7 @@ func NewDomain(store kv.Storage, ddlLease time.Duration, statsLease time.Duratio
 		exit:                make(chan struct{}),
 		sysSessionPool:      newSessionPool(capacity, factory),
 		statsLease:          statsLease,
-		infoCache:           infoschema.NewCache(16),
+		infoCache:           infoschema.NewCache(64),
 		slowQuery:           newTopNSlowQueries(config.GetGlobalConfig().InMemSlowQueryTopNNum, time.Hour*24*7, config.GetGlobalConfig().InMemSlowQueryRecentNum),
 		indexUsageSyncLease: idxUsageSyncLease,
 		dumpFileGcChecker:   &dumpFileGcChecker{gcLease: dumpFileGcLease, paths: []string{replayer.GetPlanReplayerDirName(), GetOptimizerTraceDirName(), GetExtractTaskDirName()}},
