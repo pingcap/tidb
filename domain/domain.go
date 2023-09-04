e2a6f47dfe889db8bcd3ffefa6462dba82b74673
crazycs520 <crazycs520@gmail.com>
2023-09-04 10:53:42 +0800

    fix
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- domain/domain.go
+++ domain/domain.go
@@ -1053,7 +1053,7 @@ func NewDomain(store kv.Storage, ddlLease time.Duration, statsLease time.Duratio
 		exit:                make(chan struct{}),
 		sysSessionPool:      newSessionPool(capacity, factory),
 		statsLease:          statsLease,
-		infoCache:           infoschema.NewCache(64),
+		infoCache:           infoschema.NewCache(16),
 		slowQuery:           newTopNSlowQueries(config.GetGlobalConfig().InMemSlowQueryTopNNum, time.Hour*24*7, config.GetGlobalConfig().InMemSlowQueryRecentNum),
 		indexUsageSyncLease: idxUsageSyncLease,
 		dumpFileGcChecker:   &dumpFileGcChecker{gcLease: dumpFileGcLease, paths: []string{replayer.GetPlanReplayerDirName(), GetOptimizerTraceDirName(), GetExtractTaskDirName()}},
