53afad41517ee2ade46a408107a1fea311ec08ad
Arenatlx <314806019@qq.com>
2023-09-04 17:30:57 +0800

    This is an automated cherry-pick of #46450
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- planner/core/rule_generate_column_substitute.go
+++ planner/core/rule_generate_column_substitute.go
@@ -74,32 +74,83 @@ func collectGenerateColumn(lp LogicalPlan, exprToColumn ExprColumnMap) {
 	}
 }
 
+<<<<<<< HEAD
 func tryToSubstituteExpr(expr *expression.Expression, sctx sessionctx.Context, candidateExpr expression.Expression, tp types.EvalType, schema *expression.Schema, col *expression.Column) {
 	if (*expr).Equal(sctx, candidateExpr) && candidateExpr.GetType().EvalType() == tp &&
 		schema.ColumnIndex(col) != -1 {
 		*expr = col
+=======
+func tryToSubstituteExpr(expr *expression.Expression, lp LogicalPlan, candidateExpr expression.Expression, tp types.EvalType, schema *expression.Schema, col *expression.Column, opt *logicalOptimizeOp) bool {
+	changed := false
+	if (*expr).Equal(lp.SCtx(), candidateExpr) && candidateExpr.GetType().EvalType() == tp &&
+		schema.ColumnIndex(col) != -1 {
+		*expr = col
+		appendSubstituteColumnStep(lp, candidateExpr, col, opt)
+		changed = true
+>>>>>>> 41c16424ac0 (planner: don't recompute the hashcode when generated column substitution doesn't happen (#46450))
 	}
+	return changed
 }
 
+<<<<<<< HEAD
 func substituteExpression(cond expression.Expression, sctx *stmtctx.StatementContext, sessionCtx sessionctx.Context, exprToColumn ExprColumnMap, schema *expression.Schema) {
+=======
+func appendSubstituteColumnStep(lp LogicalPlan, candidateExpr expression.Expression, col *expression.Column, opt *logicalOptimizeOp) {
+	reason := func() string { return "" }
+	action := func() string {
+		buffer := bytes.NewBufferString("expression:")
+		buffer.WriteString(candidateExpr.String())
+		buffer.WriteString(" substituted by")
+		buffer.WriteString(" column:")
+		buffer.WriteString(col.String())
+		return buffer.String()
+	}
+	opt.appendStepToCurrent(lp.ID(), lp.TP(), reason, action)
+}
+
+// SubstituteExpression is Exported for bench
+func SubstituteExpression(cond expression.Expression, lp LogicalPlan, exprToColumn ExprColumnMap, schema *expression.Schema, opt *logicalOptimizeOp) bool {
+	return substituteExpression(cond, lp, exprToColumn, schema, opt)
+}
+
+func substituteExpression(cond expression.Expression, lp LogicalPlan, exprToColumn ExprColumnMap, schema *expression.Schema, opt *logicalOptimizeOp) bool {
+>>>>>>> 41c16424ac0 (planner: don't recompute the hashcode when generated column substitution doesn't happen (#46450))
 	sf, ok := cond.(*expression.ScalarFunction)
 	if !ok {
-		return
+		return false
 	}
+<<<<<<< HEAD
+=======
+	sctx := lp.SCtx().GetSessionVars().StmtCtx
+	changed := false
+	collectChanged := func(partial bool) {
+		if partial && !changed {
+			changed = true
+		}
+	}
+>>>>>>> 41c16424ac0 (planner: don't recompute the hashcode when generated column substitution doesn't happen (#46450))
 	defer func() {
 		// If the argument is not changed, hash code doesn't need to recount again.
-		// But we always do it to keep the code simple and stupid.
-		expression.ReHashCode(sf, sctx)
+		if changed {
+			expression.ReHashCode(sf, sctx)
+		}
 	}()
 	var expr *expression.Expression
 	var tp types.EvalType
 	switch sf.FuncName.L {
 	case ast.EQ, ast.LT, ast.LE, ast.GT, ast.GE:
 		for candidateExpr, column := range exprToColumn {
+<<<<<<< HEAD
 			tryToSubstituteExpr(&sf.GetArgs()[1], sessionCtx, candidateExpr, sf.GetArgs()[0].GetType().EvalType(), schema, column)
 		}
 		for candidateExpr, column := range exprToColumn {
 			tryToSubstituteExpr(&sf.GetArgs()[0], sessionCtx, candidateExpr, sf.GetArgs()[1].GetType().EvalType(), schema, column)
+=======
+			collectChanged(tryToSubstituteExpr(&sf.GetArgs()[1], lp, candidateExpr, sf.GetArgs()[0].GetType().EvalType(), schema, column, opt))
+		}
+		for candidateExpr, column := range exprToColumn {
+			collectChanged(tryToSubstituteExpr(&sf.GetArgs()[0], lp, candidateExpr, sf.GetArgs()[1].GetType().EvalType(), schema, column, opt))
+>>>>>>> 41c16424ac0 (planner: don't recompute the hashcode when generated column substitution doesn't happen (#46450))
 		}
 	case ast.In:
 		expr = &sf.GetArgs()[0]
@@ -115,13 +166,18 @@ func substituteExpression(cond expression.Expression, sctx *stmtctx.StatementCon
 		}
 		if canSubstitute {
 			for candidateExpr, column := range exprToColumn {
+<<<<<<< HEAD
 				tryToSubstituteExpr(expr, sessionCtx, candidateExpr, tp, schema, column)
+=======
+				collectChanged(tryToSubstituteExpr(expr, lp, candidateExpr, tp, schema, column, opt))
+>>>>>>> 41c16424ac0 (planner: don't recompute the hashcode when generated column substitution doesn't happen (#46450))
 			}
 		}
 	case ast.Like:
 		expr = &sf.GetArgs()[0]
 		tp = sf.GetArgs()[1].GetType().EvalType()
 		for candidateExpr, column := range exprToColumn {
+<<<<<<< HEAD
 			tryToSubstituteExpr(expr, sessionCtx, candidateExpr, tp, schema, column)
 		}
 	case ast.LogicOr, ast.LogicAnd:
@@ -129,7 +185,17 @@ func substituteExpression(cond expression.Expression, sctx *stmtctx.StatementCon
 		substituteExpression(sf.GetArgs()[1], sctx, sessionCtx, exprToColumn, schema)
 	case ast.UnaryNot:
 		substituteExpression(sf.GetArgs()[0], sctx, sessionCtx, exprToColumn, schema)
+=======
+			collectChanged(tryToSubstituteExpr(expr, lp, candidateExpr, tp, schema, column, opt))
+		}
+	case ast.LogicOr, ast.LogicAnd:
+		collectChanged(substituteExpression(sf.GetArgs()[0], lp, exprToColumn, schema, opt))
+		collectChanged(substituteExpression(sf.GetArgs()[1], lp, exprToColumn, schema, opt))
+	case ast.UnaryNot:
+		collectChanged(substituteExpression(sf.GetArgs()[0], lp, exprToColumn, schema, opt))
+>>>>>>> 41c16424ac0 (planner: don't recompute the hashcode when generated column substitution doesn't happen (#46450))
 	}
+	return changed
 }
 
 func (gc *gcSubstituter) substitute(ctx context.Context, lp LogicalPlan, exprToColumn ExprColumnMap) LogicalPlan {
