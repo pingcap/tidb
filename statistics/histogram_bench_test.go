9c7a79c9b5f4fef8e1d7dc6c4d94f1de62625d28
Weizhen Wang <wangweizhen@pingcap.com>
2023-09-10 11:00:30 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- statistics/histogram_bench_test.go
+++ statistics/histogram_bench_test.go
@@ -28,19 +28,7 @@ import (
 
 const histogramLen = 100
 const popedTopNLen = 100
-
-var magicLower = [histogramLen]int64{
-	0, 100, 200, 300, 400, 500, 600, 700, 800, 900,
-	1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900,
-	2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900,
-	3000, 3100, 3200, 3300, 3400, 3500, 3600, 3700, 3800, 3900,
-	4000, 4100, 4200, 4300, 4400, 4500, 4600, 4700, 4800, 4900,
-	5000, 5100, 5200, 5300, 5400, 5500, 5600, 5700, 5800, 5900,
-	6000, 6100, 6200, 6300, 6400, 6500, 6600, 6700, 6800, 6900,
-	7000, 7100, 7200, 7300, 7400, 7500, 7600, 7700, 7800, 7900,
-	8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8800, 8900,
-	9000, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900,
-}
+const expBucketNumber = 100
 
 func genBucket4TestData(length int) []*bucket4Test {
 	result := make([]*bucket4Test, 0, length)
@@ -54,7 +42,7 @@ func genBucket4TestData(length int) []*bucket4Test {
 		if n == length-1 {
 			upper = 10000
 		} else {
-			upper = lower + (rand.Int63n(magicLower[n+1] - lower))
+			upper = lower + (rand.Int63n(int64(100*(n+1)) - lower))
 		}
 		result = append(result, &bucket4Test{
 			lower:  lower,
@@ -89,7 +77,6 @@ func benchmarkMergePartitionHist2GlobalHist(b *testing.B, partition int) {
 		hist := genHist4Bench(b, buckets, histogramLen)
 		hists = append(hists, hist)
 	}
-	const expBucketNumber = 100
 	poped := make([]TopNMeta, 0, popedTopNLen)
 	for n := 0; n < popedTopNLen; n++ {
 		b, _ := codec.EncodeKey(sc, nil, types.NewIntDatum(rand.Int63n(10000)))
@@ -115,5 +102,4 @@ func BenchmarkMergePartitionHist2GlobalHist(b *testing.B) {
 			}
 		})
 	}
-
 }
