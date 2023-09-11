aece9d4873ff864a39e5ed1dc8ba22155f226064
yibin <huyibin@pingcap.com>
2023-09-11 17:28:34 +0800

    Address build issue
    Signed-off-by: yibin <huyibin@pingcap.com>

--- expression/constant.go
+++ expression/constant.go
@@ -450,7 +450,7 @@ func (c *Constant) HashCode(sc *stmtctx.StatementContext) []byte {
 		c.hashcode = codec.EncodeInt(c.hashcode, int64(c.ParamMarker.order))
 		return c.hashcode
 	}
-	
+
 	_, err := c.Eval(chunk.Row{})
 	if err != nil {
 		terror.Log(err)
