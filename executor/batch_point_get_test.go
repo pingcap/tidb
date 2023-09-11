5367ce98ec1dad7877b9cd7e9b4bf6099647965b
jiyfhust <jiyfhust@163.com>
2023-09-11 14:21:45 +0800

    fix

--- executor/batch_point_get_test.go
+++ executor/batch_point_get_test.go
@@ -379,8 +379,12 @@ func TestBatchPointGetIssue46779(t *testing.T) {
 	tk.MustExec("drop table if exists t1")
 	tk.MustExec("CREATE TABLE t1 (id int, c varchar(128), primary key (id)) PARTITION BY HASH (id) PARTITIONS 3;")
 	tk.MustExec(`insert into t1 values (1, "a"), (11, "b"), (21, "c")`)
-	tk.MustQuery("select * from t1 where id in (1, 1, 11)").Sort().Check(testkit.Rows("1 a", "11 b"))
-	tk.MustQuery("select * from t1 where id in (1, 11, 11, 21)").Sort().Check(testkit.Rows("1 a", "11 b", "21 c"))
+	query := "select * from t1 where id in (1, 1, 11)"
+	require.True(t, tk.HasPlan(query, "Batch_Point_Get")) // check if BatchPointGet is used
+	tk.MustQuery(query).Sort().Check(testkit.Rows("1 a", "11 b"))
+	query = "select * from t1 where id in (1, 11, 11, 21)"
+	require.True(t, tk.HasPlan(query, "Batch_Point_Get")) // check if BatchPointGet is used
+	tk.MustQuery(query).Sort().Check(testkit.Rows("1 a", "11 b", "21 c"))
 
 	tk.MustExec("drop table if exists t2")
 	tk.MustExec(`CREATE TABLE t2 (id int, c varchar(128), primary key (id)) partition by range (id)(
@@ -388,6 +392,10 @@ func TestBatchPointGetIssue46779(t *testing.T) {
 		partition p1 values less than (20), 
 		partition p2 values less than (30));`)
 	tk.MustExec(`insert into t2 values (1, "a"), (11, "b"), (21, "c")`)
-	tk.MustQuery("select * from t2 where id in (1, 1, 11)").Sort().Check(testkit.Rows("1 a", "11 b"))
-	tk.MustQuery("select * from t2 where id in (1, 11, 11, 21)").Sort().Check(testkit.Rows("1 a", "11 b", "21 c"))
+	query = "select * from t2 where id in (1, 1, 11)"
+	require.True(t, tk.HasPlan(query, "Batch_Point_Get")) // check if BatchPointGet is used
+	tk.MustQuery(query).Sort().Check(testkit.Rows("1 a", "11 b"))
+	require.True(t, tk.HasPlan(query, "Batch_Point_Get")) // check if BatchPointGet is used
+	query = "select * from t2 where id in (1, 11, 11, 21)"
+	tk.MustQuery(query).Sort().Check(testkit.Rows("1 a", "11 b", "21 c"))
 }
