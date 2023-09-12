cc0ae8471189d8516685da74d84ffc04c5e22552
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2023-09-12 22:01:41 +0800

    variable: fix empty cloud storage uri (#46901)
    ref pingcap/tidb#45719

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -1530,8 +1530,6 @@ var (
 	GetExternalTimestamp func(ctx context.Context) (uint64, error)
 	// SetGlobalResourceControl is the func registered by domain to set cluster resource control.
 	SetGlobalResourceControl atomic.Pointer[func(bool)]
-	// RedactCloudStorageURI redacts the cloud storage URI.
-	RedactCloudStorageURI func(uri string) (string, error)
 	// ValidateCloudStorageURI validates the cloud storage URI.
 	ValidateCloudStorageURI func(ctx context.Context, uri string) error
 )
