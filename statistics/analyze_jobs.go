e775375e4ab9cf81eb46b81c1f729949701cdf88
Rustin Liu <rustin.liu@gmail.com>
2023-09-12 17:58:56 +0800

    statistics: use RWMutex in AnalyzeProgress (#46895)

--- statistics/analyze_jobs.go
+++ statistics/analyze_jobs.go
@@ -16,7 +16,27 @@ package statistics
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
+)
+
+const (
+	// AnalyzePending means the analyze job is pending
+	AnalyzePending = "pending"
+	// AnalyzeRunning means the analyze job is running
+	AnalyzeRunning = "running"
+	// AnalyzeFinished means the analyze job has finished
+	AnalyzeFinished = "finished"
+	// AnalyzeFailed means the analyze job has failed
+	AnalyzeFailed = "failed"
+)
+
+const (
+	// maxDelta is the threshold of delta count. If the delta count reaches this threshold, it will be dumped into
+	// mysql.analyze_jobs.
+	maxDelta int64 = 10000000
+	// dumpTimeInterval is the time interval of dumping delta count into mysql.analyze_jobs.
+	dumpTimeInterval = 5 * time.Second
 )
 
 // AnalyzeJob is used to represent the status of one analyze job.
@@ -36,58 +56,46 @@ type AnalyzeJob struct {
 // AnalyzeProgress represents the process of one analyze job.
 type AnalyzeProgress struct {
 	// lastDumpTime is the last time mysql.analyze_jobs.processed_rows is updated.
-	lastDumpTime time.Time
+	lastDumpTime   time.Time
+	lastDumpTimeMu sync.RWMutex
+
 	// deltaCount is the newly processed rows after the last time mysql.analyze_jobs.processed_rows is updated.
-	deltaCount int64
-	sync.Mutex
+	deltaCount atomic.Int64
 }
 
 // Update adds rowCount to the delta count. If the updated delta count reaches threshold, it returns the delta count for
-// dumping it into mysql.analyze_jobs and resets the delta count to 0. Otherwise it returns 0.
-func (p *AnalyzeProgress) Update(rowCount int64) (dumpCount int64) {
-	p.Lock()
-	defer p.Unlock()
-	p.deltaCount += rowCount
+// dumping it into mysql.analyze_jobs and resets the delta count to 0. Otherwise, it returns 0.
+func (p *AnalyzeProgress) Update(rowCount int64) int64 {
+	dumpCount := int64(0)
+	newCount := p.deltaCount.Add(rowCount)
+
 	t := time.Now()
-	const maxDelta int64 = 10000000
-	const dumpTimeInterval = 5 * time.Second
-	if p.deltaCount > maxDelta && t.Sub(p.lastDumpTime) > dumpTimeInterval {
-		dumpCount = p.deltaCount
-		p.deltaCount = 0
+	p.lastDumpTimeMu.Lock()
+	if newCount > maxDelta && t.Sub(p.lastDumpTime) > dumpTimeInterval {
+		dumpCount = newCount
+		p.deltaCount.Store(0)
 		p.lastDumpTime = t
-		return
 	}
-	return
+	p.lastDumpTimeMu.Unlock()
+
+	return dumpCount
 }
 
 // GetDeltaCount returns the delta count which hasn't been dumped into mysql.analyze_jobs.
 func (p *AnalyzeProgress) GetDeltaCount() int64 {
-	p.Lock()
-	defer p.Unlock()
-	return p.deltaCount
+	return p.deltaCount.Load()
 }
 
 // SetLastDumpTime sets the last dump time.
 func (p *AnalyzeProgress) SetLastDumpTime(t time.Time) {
-	p.Lock()
-	defer p.Unlock()
+	p.lastDumpTimeMu.Lock()
+	defer p.lastDumpTimeMu.Unlock()
 	p.lastDumpTime = t
 }
 
 // GetLastDumpTime returns the last dump time.
 func (p *AnalyzeProgress) GetLastDumpTime() time.Time {
-	p.Lock()
-	defer p.Unlock()
+	p.lastDumpTimeMu.RLock()
+	defer p.lastDumpTimeMu.RUnlock()
 	return p.lastDumpTime
 }
-
-const (
-	// AnalyzePending means the analyze job is pending
-	AnalyzePending = "pending"
-	// AnalyzeRunning means the analyze job is running
-	AnalyzeRunning = "running"
-	// AnalyzeFinished means the analyze job has finished
-	AnalyzeFinished = "finished"
-	// AnalyzeFailed means the analyze job has failed
-	AnalyzeFailed = "failed"
-)
