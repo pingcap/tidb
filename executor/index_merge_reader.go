8b3c8fb6a385a303f2f0325098d5e5565cbf6270
AilinKid <314806019@qq.com>
2023-09-13 14:21:06 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

--- executor/index_merge_reader.go
+++ executor/index_merge_reader.go
@@ -1483,7 +1483,7 @@ func (w *intersectionProcessWorker) doIntersectionPerPartition(ctx context.Conte
 // goto Loop start:
 //
 // encapsulate all the recorded handles (already in index order) as index merge table tasks, sending them out.
-func (_ *indexMergeProcessWorker) fetchLoopIntersectionWithOrderBy(_ context.Context, _ <-chan *indexMergeTableTask,
+func (*indexMergeProcessWorker) fetchLoopIntersectionWithOrderBy(_ context.Context, _ <-chan *indexMergeTableTask,
 	_ chan<- *indexMergeTableTask, _ chan<- *indexMergeTableTask, _ <-chan struct{}) {
 	// todo: pushed sort property with partial index plan and limit.
 }
