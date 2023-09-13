263b85a9fbec177e5ec6c67f5625fc47e30471d8
Jason Mo <mohangjie1995@gmail.com>
2023-09-13 19:35:52 +0800

    update

--- planner/core/integration_test.go
+++ planner/core/integration_test.go
@@ -957,10 +957,10 @@ func TestIndexMergePartialScansTiDBRowID(t *testing.T) {
 		expected  []string
 	}{
 		/* TODO: fix it or delete it
-			{
-				// 3 index scans
-				"c < 10 or c < 11 or c > 50", []string{"1", "10", "100"},
-			},
+		{
+			// 3 index scans
+			"c < 10 or c < 11 or c > 50", []string{"1", "10", "100"},
+		},
 		*/
 		{
 			// 2 index scans
