edaf4013c2ebd87dafb26a72fa50043fdf4f0b0a
you06 <you1474600@gmail.com>
2023-09-13 13:57:25 +0800

    fix flaky goleak test by ignoring the correct goroutine function
    Signed-off-by: you06 <you1474600@gmail.com>

--- tests/realtikvtest/testkit.go
+++ tests/realtikvtest/testkit.go
@@ -75,7 +75,7 @@ func RunTestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("google.golang.org/grpc/internal/transport.(*http2Client).keepalive"),
 		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
 		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
-		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/txnkv/transaction.keepAlive"),
+		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/txnkv/transaction.(*ttlManager).run"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
 	}
 	callback := func(i int) int {
