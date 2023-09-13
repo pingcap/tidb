996f7c0953d21e7b35b318864489863e4ddd4f1c
you06 <you1474600@gmail.com>
2023-09-13 14:14:36 +0800

    add backoff function to goleak ignore
    Signed-off-by: you06 <you1474600@gmail.com>

--- tests/realtikvtest/testkit.go
+++ tests/realtikvtest/testkit.go
@@ -75,7 +75,9 @@ func RunTestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("google.golang.org/grpc/internal/transport.(*http2Client).keepalive"),
 		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
 		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
-		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/txnkv/transaction.(*ttlManager).run"),
+		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/txnkv/transaction.keepAlive"),
+		// backoff function will lead to sleep, so there is a high probability of goroutine leak while it's doing backoff.
+		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/internal/retry.(*Config).createBackoffFn.newBackoffFn.func2"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
 	}
 	callback := func(i int) int {
