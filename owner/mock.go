0fd342c22a267c3bdb8e9037932296b9d4f509a2
Hangjie Mo <mohangjie1995@gmail.com>
2023-09-14 13:22:40 +0800

    owner, util: use `timeutil.Sleep()` instead of `sleepContext` (#46964)

--- owner/mock.go
+++ owner/mock.go
@@ -25,6 +25,7 @@ import (
 	"github.com/pingcap/tidb/ddl/util"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/util/logutil"
+	"github.com/pingcap/tidb/util/timeutil"
 	"go.uber.org/zap"
 )
 
@@ -123,13 +124,6 @@ func (*mockManager) SetOwnerOpValue(_ context.Context, op OpType) error {
 	return nil
 }
 
-func sleepContext(ctx context.Context, delay time.Duration) {
-	select {
-	case <-ctx.Done():
-	case <-time.After(delay):
-	}
-}
-
 // CampaignOwner implements Manager.CampaignOwner interface.
 func (m *mockManager) CampaignOwner(_ ...int) error {
 	m.wg.Add(1)
@@ -149,10 +143,12 @@ func (m *mockManager) CampaignOwner(_ ...int) error {
 				return
 			case <-m.resignDone:
 				m.RetireOwner()
-				sleepContext(m.ctx, 1*time.Second) // Give a chance to the other owner managers to get owner.
+				//nolint: errcheck
+				timeutil.Sleep(m.ctx, 1*time.Second) // Give a chance to the other owner managers to get owner.
 			default:
 				m.toBeOwner()
-				sleepContext(m.ctx, 1*time.Second) // Speed up domain.Close()
+				//nolint: errcheck
+				timeutil.Sleep(m.ctx, 1*time.Second) // Speed up domain.Close()
 				logutil.BgLogger().Debug("owner manager tick", zap.String("category", "ddl"), zap.String("ID", m.id),
 					zap.String("ownerKey", m.key), zap.String("currentOwner", util.MockGlobalStateEntry.OwnerKey(m.storeID, m.key).GetOwner()))
 			}
