6c07f271ae9fb8c109ce51a70907d118c8e870e5
Weizhen Wang <wangweizhen@pingcap.com>
2023-09-15 11:42:09 +0800

    statistics: use slices.BinarySearchFunc to simple TopN.FindTopN (#46996)
    ref pingcap/tidb#45933

--- statistics/cmsketch.go
+++ statistics/cmsketch.go
@@ -639,13 +639,8 @@ func (c *TopN) FindTopN(d []byte) int {
 	if c == nil {
 		return -1
 	}
-	match := false
-	idx := sort.Search(len(c.TopN), func(i int) bool {
-		cmpRst := bytes.Compare(c.TopN[i].Encoded, d)
-		if cmpRst == 0 {
-			match = true
-		}
-		return cmpRst >= 0
+	idx, match := slices.BinarySearchFunc(c.TopN, d, func(a TopNMeta, b []byte) int {
+		return bytes.Compare(a.Encoded, b)
 	})
 	if !match {
 		return -1
