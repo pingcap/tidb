19ec0e4f91ec1fe03b4bf04bb26d419c81384c44
lyzx2001 <rep_laureline@hotmail.com>
2023-09-19 12:06:02 +0800

    update createConflictErrorTable

--- br/pkg/lightning/errormanager/errormanager.go
+++ br/pkg/lightning/errormanager/errormanager.go
@@ -94,7 +94,7 @@ const (
 			raw_value   mediumblob NOT NULL COMMENT 'the value of the conflicted key',
 			raw_handle  mediumblob NOT NULL COMMENT 'the data handle derived from the conflicted key or value',
 			raw_row     mediumblob NOT NULL COMMENT 'the data retrieved from the handle',
-			KEY (task_id, table_name),
+			INDEX (task_id, table_name),
 			INDEX (index_name),
 			INDEX (table_name, index_name)
 		);
