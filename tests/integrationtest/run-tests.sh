492753ff005954a1e0a473536af9c993e75f6499
Hangjie Mo <mohangjie1995@gmail.com>
2023-09-20 00:58:42 +0800

    tests: `integrationtest` support report warnings (#47010)
    ref pingcap/tidb#45961

--- tests/integrationtest/run-tests.sh
+++ tests/integrationtest/run-tests.sh
@@ -88,7 +88,7 @@ function build_mysql_tester()
 {
     echo "building mysql-tester binary: $mysql_tester"
     rm -rf $mysql_tester
-    GOBIN=$PWD go install github.com/defined2014/mysql-tester/src@1da6225968e19999d28be4d06c357b9378df8b78
+    GOBIN=$PWD go install github.com/pingcap/mysql-tester/src@6891b636aa6ba666e426238f9c74deb502b846d2
     mv src mysql_tester
 }
 
