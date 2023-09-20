b27526c25f169575b2d73f56ac1ecdd5cb14cb1f
Zhihong Yu <yuzhihong@gmail.com>
2023-09-20 01:00:13 -0700

    br: change useConcurrentReaderCurrent to bool type (#46924)
    close pingcap/tidb#46923

--- br/pkg/lightning/backend/external/byte_reader.go
+++ br/pkg/lightning/backend/external/byte_reader.go
@@ -39,7 +39,7 @@ type byteReader struct {
 
 	retPointers []*[]byte
 
-	useConcurrentReaderCurrent atomic.Bool
+	useConcurrentReaderCurrent bool
 	useConcurrentReader        atomic.Bool
 
 	currFileOffset int64
@@ -109,13 +109,13 @@ func (r *byteReader) switchToConcurrentReaderImpl() error {
 	r.conReader.currentFileOffset = currOffset
 	r.conReader.bufferReadOffset = 0
 
-	r.useConcurrentReaderCurrent.Store(true)
 	r.conReader.buffer = make([]byte, r.conReader.concurrency*r.conReader.readBufferSize)
+	r.useConcurrentReaderCurrent = true
 	return nil
 }
 
 func (r *byteReader) switchToNormalReaderImpl() error {
-	r.useConcurrentReaderCurrent.Store(false)
+	r.useConcurrentReaderCurrent = false
 	r.currFileOffset = r.conReader.currentFileOffset
 	r.conReader.buffer = nil
 	_, err := r.storageReader.Seek(r.currFileOffset, io.SeekStart)
@@ -176,7 +176,7 @@ func (r *byteReader) cloneSlices() {
 }
 
 func (r *byteReader) next(n int) []byte {
-	if r.useConcurrentReaderCurrent.Load() {
+	if r.useConcurrentReaderCurrent {
 		return r.conReader.next(n)
 	}
 	end := mathutil.Min(r.bufOffset+n, len(r.buf))
@@ -187,7 +187,7 @@ func (r *byteReader) next(n int) []byte {
 
 func (r *byteReader) reload() error {
 	to := r.useConcurrentReader.Load()
-	now := r.useConcurrentReaderCurrent.Load()
+	now := r.useConcurrentReaderCurrent
 	if to != now {
 		logutil.Logger(r.ctx).Info("switch reader mode", zap.Bool("use concurrent mode", to))
 		if to {
