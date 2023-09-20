8aad40eb5838a1169576e7d606cc78c098d5dbcc
Xiaoguang Sun <sunxiaoguang@users.noreply.github.com>
2023-09-20 10:34:42 +0800

    This is an automated cherry-pick of #46976
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- privilege/privileges/cache.go
+++ privilege/privileges/cache.go
@@ -1151,8 +1151,13 @@ func (p *MySQLPrivilege) DBIsVisible(user, host, db string) bool {
 	return false
 }
 
+<<<<<<< HEAD
 func (p *MySQLPrivilege) showGrants(user, host string, roles []*auth.RoleIdentity) []string {
 	var gs []string
+=======
+func (p *MySQLPrivilege) showGrants(ctx sessionctx.Context, user, host string, roles []*auth.RoleIdentity) []string {
+	var gs []string //nolint: prealloc
+>>>>>>> 6271d1c80b2 (executor: Escape object in show grants output (#46976))
 	var sortFromIdx int
 	var hasGlobalGrant = false
 	// Some privileges may granted from role inheritance.
@@ -1232,7 +1237,10 @@ func (p *MySQLPrivilege) showGrants(user, host string, roles []*auth.RoleIdentit
 			}
 		}
 	}
+
+	sqlMode := ctx.GetSessionVars().SQLMode
 	for dbName, priv := range dbPrivTable {
+		dbName = stringutil.Escape(dbName, sqlMode)
 		g := dbPrivToString(priv)
 		if len(g) > 0 {
 			var s string
@@ -1255,7 +1263,7 @@ func (p *MySQLPrivilege) showGrants(user, host string, roles []*auth.RoleIdentit
 	sortFromIdx = len(gs)
 	tablePrivTable := make(map[string]mysql.PrivilegeType)
 	for _, record := range p.TablesPriv {
-		recordKey := record.DB + "." + record.TableName
+		recordKey := stringutil.Escape(record.DB, sqlMode) + "." + stringutil.Escape(record.TableName, sqlMode)
 		if user == record.User && host == record.Host {
 			if _, ok := dbPrivTable[record.DB]; ok {
 				tablePrivTable[recordKey] |= record.TablePriv
@@ -1297,10 +1305,16 @@ func (p *MySQLPrivilege) showGrants(user, host string, roles []*auth.RoleIdentit
 	// A map of "DB.Table" => Priv(col1, col2 ...)
 	sortFromIdx = len(gs)
 	columnPrivTable := make(map[string]privOnColumns)
+<<<<<<< HEAD
 	for _, record := range p.ColumnsPriv {
 		if !collectColumnGrant(&record, user, host, columnPrivTable) {
+=======
+	for i := range p.ColumnsPriv {
+		record := p.ColumnsPriv[i]
+		if !collectColumnGrant(&record, user, host, columnPrivTable, sqlMode) {
+>>>>>>> 6271d1c80b2 (executor: Escape object in show grants output (#46976))
 			for _, r := range allRoles {
-				collectColumnGrant(&record, r.Username, r.Hostname, columnPrivTable)
+				collectColumnGrant(&record, r.Username, r.Hostname, columnPrivTable, sqlMode)
 			}
 		}
 	}
@@ -1411,9 +1425,10 @@ func privOnColumnsToString(p privOnColumns) string {
 	return buf.String()
 }
 
-func collectColumnGrant(record *columnsPrivRecord, user, host string, columnPrivTable map[string]privOnColumns) bool {
+func collectColumnGrant(record *columnsPrivRecord, user, host string, columnPrivTable map[string]privOnColumns, sqlMode mysql.SQLMode) bool {
 	if record.baseRecord.match(user, host) {
-		recordKey := record.DB + "." + record.TableName
+		recordKey := stringutil.Escape(record.DB, sqlMode) + "." + stringutil.Escape(record.TableName, sqlMode)
+
 		privColumns, ok := columnPrivTable[recordKey]
 		if !ok {
 			privColumns = make(map[mysql.PrivilegeType]columnStrs)
