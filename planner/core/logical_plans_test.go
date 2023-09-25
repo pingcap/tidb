dab0276fa99cc8b1d865c3815e10f194eb12802e
AilinKid <314806019@qq.com>
2023-09-25 13:54:19 +0800

    sort imports
    Signed-off-by: AilinKid <314806019@qq.com>

--- planner/core/logical_plans_test.go
+++ planner/core/logical_plans_test.go
@@ -16,17 +16,17 @@ package core
 
 import (
 	"fmt"
-	"github.com/pingcap/tidb/infoschema"
-	"github.com/pingcap/tidb/parser"
-	"github.com/pingcap/tidb/sessionctx"
 	"testing"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/expression"
+	"github.com/pingcap/tidb/infoschema"
+	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/planner/util"
+	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/types"
 	"github.com/stretchr/testify/require"
 )
