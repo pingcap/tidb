4b46715fdb16fcbac10800a55bcbfb6f5274c32e
3pointer <luancheng@pingcap.com>
2023-09-27 16:33:17 +0800

    br: fix unstable integration tests (#47156)

--- br/pkg/task/backup.go
+++ br/pkg/task/backup.go
@@ -678,6 +678,22 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 			}
 		}()
 	}
+
+	failpoint.Inject("s3-outage-during-writing-file", func(v failpoint.Value) {
+		log.Info("failpoint s3-outage-during-writing-file injected, " +
+			"process will sleep for 5s and notify the shell to kill s3 service.")
+		if sigFile, ok := v.(string); ok {
+			file, err := os.Create(sigFile)
+			if err != nil {
+				log.Warn("failed to create file for notifying, skipping notify", zap.Error(err))
+			}
+			if file != nil {
+				file.Close()
+			}
+		}
+		time.Sleep(5 * time.Second)
+	})
+
 	metawriter.StartWriteMetasAsync(ctx, metautil.AppendDataFile)
 	err = client.BackupRanges(ctx, ranges, req, uint(cfg.Concurrency), cfg.ReplicaReadLabel, metawriter, progressCallBack)
 	if err != nil {
@@ -734,20 +750,6 @@ func RunBackup(c context.Context, g glue.Glue, cmdName string, cfg *BackupConfig
 	g.Record(summary.BackupDataSize, archiveSize)
 	//backup from tidb will fetch a general Size issue https://github.com/pingcap/tidb/issues/27247
 	g.Record("Size", archiveSize)
-	failpoint.Inject("s3-outage-during-writing-file", func(v failpoint.Value) {
-		log.Info("failpoint s3-outage-during-writing-file injected, " +
-			"process will sleep for 3s and notify the shell to kill s3 service.")
-		if sigFile, ok := v.(string); ok {
-			file, err := os.Create(sigFile)
-			if err != nil {
-				log.Warn("failed to create file for notifying, skipping notify", zap.Error(err))
-			}
-			if file != nil {
-				file.Close()
-			}
-		}
-		time.Sleep(3 * time.Second)
-	})
 	// Set task summary to success status.
 	summary.SetSuccessStatus(true)
 	return nil
