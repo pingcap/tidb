850edd877d605a6895d670977744c412ebf263cc
Jason Mo <mohangjie1995@gmail.com>
2023-09-27 16:47:24 +0800

    update variable_test

--- session/test/variable/variable_test.go
+++ session/test/variable/variable_test.go
@@ -22,7 +22,6 @@ import (
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/config"
 	"github.com/pingcap/tidb/kv"
-	"github.com/pingcap/tidb/parser/terror"
 	"github.com/pingcap/tidb/session"
 	"github.com/pingcap/tidb/sessionctx/variable"
 	"github.com/pingcap/tidb/store/copr"
@@ -55,70 +54,6 @@ func TestForbidSettingBothTSVariable(t *testing.T) {
 	tk.MustGetErrMsg("set @@tidb_snapshot = '2007-01-01 15:04:05.999999'", "tidb_read_staleness should be clear before setting tidb_snapshot")
 	tk.MustExec("set @@tidb_read_staleness = ''")
 	tk.MustExec("set @@tidb_snapshot = '2007-01-01 15:04:05.999999'")
-}
-
-func TestTiDBReadStaleness(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("set @@tidb_read_staleness='-5'")
-	tk.MustExec("set @@tidb_read_staleness='-100'")
-	err := tk.ExecToErr("set @@tidb_read_staleness='-5s'")
-	require.Error(t, err)
-	err = tk.ExecToErr("set @@tidb_read_staleness='foo'")
-	require.Error(t, err)
-	tk.MustExec("set @@tidb_read_staleness=''")
-	tk.MustExec("set @@tidb_read_staleness='0'")
-}
-
-// TestSetGroupConcatMaxLen is for issue #7034
-func TestSetGroupConcatMaxLen(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-
-	// Normal case
-	tk.MustExec("set global group_concat_max_len = 100")
-	tk.MustExec("set @@session.group_concat_max_len = 50")
-	result := tk.MustQuery("show global variables  where variable_name='group_concat_max_len';")
-	result.Check(testkit.Rows("group_concat_max_len 100"))
-
-	result = tk.MustQuery("show session variables  where variable_name='group_concat_max_len';")
-	result.Check(testkit.Rows("group_concat_max_len 50"))
-
-	result = tk.MustQuery("select @@group_concat_max_len;")
-	result.Check(testkit.Rows("50"))
-
-	result = tk.MustQuery("select @@global.group_concat_max_len;")
-	result.Check(testkit.Rows("100"))
-
-	result = tk.MustQuery("select @@session.group_concat_max_len;")
-	result.Check(testkit.Rows("50"))
-
-	tk.MustExec("set @@group_concat_max_len = 1024")
-
-	result = tk.MustQuery("select @@group_concat_max_len;")
-	result.Check(testkit.Rows("1024"))
-
-	result = tk.MustQuery("select @@global.group_concat_max_len;")
-	result.Check(testkit.Rows("100"))
-
-	result = tk.MustQuery("select @@session.group_concat_max_len;")
-	result.Check(testkit.Rows("1024"))
-
-	// Test value out of range
-	tk.MustExec("set @@group_concat_max_len=1")
-	tk.MustQuery("show warnings").Check(testkit.RowsWithSep("|", "Warning|1292|Truncated incorrect group_concat_max_len value: '1'"))
-	result = tk.MustQuery("select @@group_concat_max_len;")
-	result.Check(testkit.Rows("4"))
-
-	_, err := tk.Exec("set @@group_concat_max_len = 18446744073709551616")
-	require.True(t, terror.ErrorEqual(err, variable.ErrWrongTypeForVar), fmt.Sprintf("err %v", err))
-
-	// Test illegal type
-	_, err = tk.Exec("set @@group_concat_max_len='hello'")
-	require.True(t, terror.ErrorEqual(err, variable.ErrWrongTypeForVar), fmt.Sprintf("err %v", err))
 }
 
 func TestCoprocessorOOMAction(t *testing.T) {
@@ -269,37 +204,6 @@ func TestStatementCountLimit(t *testing.T) {
 	require.Error(t, err)
 }
 
-// TestDefaultWeekFormat checks for issue #21510.
-func TestDefaultWeekFormat(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk1 := testkit.NewTestKit(t, store)
-	tk1.MustExec("use test")
-	tk1.MustExec("set @@global.default_week_format = 4;")
-	defer tk1.MustExec("set @@global.default_week_format = default;")
-
-	tk2 := testkit.NewTestKit(t, store)
-	tk2.MustQuery("select week('2020-02-02'), @@default_week_format, week('2020-02-02');").Check(testkit.Rows("6 4 6"))
-}
-
-func TestIssue21944(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk1 := testkit.NewTestKit(t, store)
-	tk1.MustExec("use test")
-	_, err := tk1.Exec("set @@tidb_current_ts=1;")
-	require.Equal(t, "[variable:1238]Variable 'tidb_current_ts' is a read only variable", err.Error())
-}
-
-func TestIssue21943(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	_, err := tk.Exec("set @@last_plan_from_binding='123';")
-	require.Equal(t, "[variable:1238]Variable 'last_plan_from_binding' is a read only variable", err.Error())
-
-	_, err = tk.Exec("set @@last_plan_from_cache='123';")
-	require.Equal(t, "[variable:1238]Variable 'last_plan_from_cache' is a read only variable", err.Error())
-}
-
 func TestCorrectScopeError(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 	tk := testkit.NewTestKit(t, store)
@@ -337,63 +241,6 @@ func TestCorrectScopeError(t *testing.T) {
 	variable.UnregisterSysVar("sv_global")
 	variable.UnregisterSysVar("sv_session")
 	variable.UnregisterSysVar("sv_both")
-}
-
-func TestGlobalVarCollationServer(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("set @@global.collation_server=utf8mb4_general_ci")
-	tk.MustQuery("show global variables like 'collation_server'").Check(testkit.Rows("collation_server utf8mb4_general_ci"))
-	tk = testkit.NewTestKit(t, store)
-	tk.MustQuery("show global variables like 'collation_server'").Check(testkit.Rows("collation_server utf8mb4_general_ci"))
-	tk.MustQuery("show variables like 'collation_server'").Check(testkit.Rows("collation_server utf8mb4_general_ci"))
-}
-
-func TestMemoryUsageAlarmVariable(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_ratio=1")
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_ratio").Check(testkit.Rows("1"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_ratio=0")
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_ratio").Check(testkit.Rows("0"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_ratio=0.7")
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_ratio").Check(testkit.Rows("0.7"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_ratio=1.1")
-	tk.MustQuery("SHOW WARNINGS").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_memory_usage_alarm_ratio value: '1.1'"))
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_ratio").Check(testkit.Rows("1"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_ratio=-1")
-	tk.MustQuery("SHOW WARNINGS").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_memory_usage_alarm_ratio value: '-1'"))
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_ratio").Check(testkit.Rows("0"))
-	require.Error(t, tk.ExecToErr("set @@session.tidb_memory_usage_alarm_ratio=0.8"))
-
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_keep_record_num=1")
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_keep_record_num").Check(testkit.Rows("1"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_keep_record_num=100")
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_keep_record_num").Check(testkit.Rows("100"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_keep_record_num=0")
-	tk.MustQuery("SHOW WARNINGS").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_memory_usage_alarm_keep_record_num value: '0'"))
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_keep_record_num").Check(testkit.Rows("1"))
-	tk.MustExec("set @@global.tidb_memory_usage_alarm_keep_record_num=10001")
-	tk.MustQuery("SHOW WARNINGS").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_memory_usage_alarm_keep_record_num value: '10001'"))
-	tk.MustQuery("select @@global.tidb_memory_usage_alarm_keep_record_num").Check(testkit.Rows("10000"))
-}
-
-func TestSelectLockInShare(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("DROP TABLE IF EXISTS t_sel_in_share")
-	tk.MustExec("CREATE TABLE t_sel_in_share (id int DEFAULT NULL)")
-	tk.MustExec("insert into t_sel_in_share values (11)")
-	require.Error(t, tk.ExecToErr("select * from t_sel_in_share lock in share mode"))
-	tk.MustExec("set @@tidb_enable_noop_functions = 1")
-	tk.MustQuery("select * from t_sel_in_share lock in share mode").Check(testkit.Rows("11"))
-	tk.MustExec("DROP TABLE t_sel_in_share")
 }
 
 func TestReadDMLBatchSize(t *testing.T) {
@@ -515,63 +362,6 @@ func TestIsolationRead(t *testing.T) {
 	require.False(t, hasTiKV)
 }
 
-func TestEnablePartition(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("set tidb_enable_table_partition=off")
-	tk.MustQuery("show variables like 'tidb_enable_table_partition'").Check(testkit.Rows("tidb_enable_table_partition OFF"))
-
-	tk.MustExec("set global tidb_enable_table_partition = on")
-
-	tk.MustQuery("show variables like 'tidb_enable_table_partition'").Check(testkit.Rows("tidb_enable_table_partition OFF"))
-	tk.MustQuery("show global variables like 'tidb_enable_table_partition'").Check(testkit.Rows("tidb_enable_table_partition ON"))
-
-	tk.MustExec("set tidb_enable_list_partition=off")
-	tk.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition OFF"))
-	tk.MustExec("set global tidb_enable_list_partition=on")
-	tk.MustQuery("show global variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-	tk.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition OFF"))
-
-	tk.MustExec("set tidb_enable_list_partition=1")
-	tk.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-
-	tk.MustExec("set tidb_enable_list_partition=on")
-	tk.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-
-	tk.MustQuery("show global variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-	tk.MustExec("set global tidb_enable_list_partition=off")
-	tk.MustQuery("show global variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition OFF"))
-	tk.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-	tk.MustExec("set tidb_enable_list_partition=off")
-	tk.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition OFF"))
-
-	tk.MustExec("set global tidb_enable_list_partition=on")
-	tk.MustQuery("show global variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-
-	tk1 := testkit.NewTestKit(t, store)
-	tk1.MustExec("use test")
-	tk1.MustQuery("show variables like 'tidb_enable_table_partition'").Check(testkit.Rows("tidb_enable_table_partition ON"))
-	tk1.MustQuery("show variables like 'tidb_enable_list_partition'").Check(testkit.Rows("tidb_enable_list_partition ON"))
-}
-
-func TestIgnoreForeignKey(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("set @@foreign_key_checks=0")
-	sqlText := `CREATE TABLE address (
-		id bigint(20) NOT NULL AUTO_INCREMENT,
-		user_id bigint(20) NOT NULL,
-		PRIMARY KEY (id),
-		CONSTRAINT FK_7rod8a71yep5vxasb0ms3osbg FOREIGN KEY (user_id) REFERENCES waimaiqa.user (id),
-		INDEX FK_7rod8a71yep5vxasb0ms3osbg (user_id) comment ''
-		) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ROW_FORMAT=COMPACT COMMENT='' CHECKSUM=0 DELAY_KEY_WRITE=0;`
-	tk.MustExec(sqlText)
-}
-
 func TestIndexMergeRuntimeStats(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 
@@ -595,54 +385,4 @@ func TestIndexMergeRuntimeStats(t *testing.T) {
 	require.Regexp(t, ".*time:.*loops:.*cop_task:.*", tableExplain)
 	tk.MustExec("set @@tidb_enable_collect_execution_info=0;")
 	tk.MustQuery("select /*+ use_index_merge(t1, primary, t1a) */ * from t1 where id < 2 or a > 4 order by a").Check(testkit.Rows("1 1 1 1 1", "5 5 5 5 5"))
-}
-
-func TestSysdateIsNow(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustQuery("show variables like '%tidb_sysdate_is_now%'").Check(testkit.Rows("tidb_sysdate_is_now OFF"))
-	require.False(t, tk.Session().GetSessionVars().SysdateIsNow)
-	tk.MustExec("set @@tidb_sysdate_is_now=true")
-	tk.MustQuery("show variables like '%tidb_sysdate_is_now%'").Check(testkit.Rows("tidb_sysdate_is_now ON"))
-	require.True(t, tk.Session().GetSessionVars().SysdateIsNow)
-}
-
-func TestSessionAlias(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows(""))
-	// normal set
-	tk.MustExec("set @@tidb_session_alias='alias123'")
-	tk.MustQuery("show warnings").Check(testkit.Rows())
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows("alias123"))
-	tk.MustQuery("show warnings").Check(testkit.Rows())
-	// chinese value
-	tk.MustExec("set @@tidb_session_alias='中文'")
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows("中文"))
-	tk.MustQuery("show warnings").Check(testkit.Rows())
-	// set a long value
-	val := "0123456789012345678901234567890123456789012345678901234567890123456789"
-	tk.MustExec("set @@tidb_session_alias=?", val)
-	tk.MustQuery("show warnings").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_session_alias value: '0123456789012345678901234567890123456789012345678901234567890123456789'"))
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows(val[:64]))
-	// set a long value with unicode
-	val = "中文测试1中文测试2中文测试3中文测试4中文测试5中文测试6中文测试7中文测试8中文测试9中文测试0中文测试a中文测试b中文测试c"
-	tk.MustExec("set @@tidb_session_alias=?", val)
-	tk.MustQuery("show warnings").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_session_alias value: '中文测试1中文测试2中文测试3中文测试4中文测试5中文测试6中文测试7中文测试8中文测试9中文测试0中文测试a中文测试b中文测试c'"))
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows("中文测试1中文测试2中文测试3中文测试4中文测试5中文测试6中文测试7中文测试8中文测试9中文测试0中文测试a中文测试b中文测试"))
-	// end with space
-	tk.MustExec("set @@tidb_session_alias='abc  '")
-	tk.MustQuery("show warnings").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_session_alias value: 'abc  '"))
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows("abc"))
-	// end with space after truncate
-	tk.MustExec("set @@tidb_session_alias='abc                                                                    1'")
-	tk.MustQuery("show warnings").Check(testkit.Rows("Warning 1292 Truncated incorrect tidb_session_alias value: 'abc                                                                    1'"))
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows("abc"))
-	// reset to empty
-	tk.MustExec("set @@tidb_session_alias=''")
-	tk.MustQuery("show warnings").Check(testkit.Rows())
-	tk.MustQuery("select @@tidb_session_alias").Check(testkit.Rows(""))
 }
