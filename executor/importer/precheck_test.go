27a0351ca2d7c569b63c4eb91865fd32c34658c7
3pointer <luancheng@pingcap.com>
2023-09-28 18:18:21 +0800

    This is an automated cherry-pick of #47322
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- executor/importer/precheck_test.go
+++ executor/importer/precheck_test.go
@@ -25,6 +25,130 @@ func TestPreCheckCollector(t *testing.T) {
 	c := newPreCheckCollector()
 	require.True(t, c.success())
 
+<<<<<<< HEAD
 	c.fail(precheck.CheckTargetTableEmpty, "target table is not empty")
 	require.False(t, c.success())
+=======
+func createMockETCD(t *testing.T) (string, *embed.Etcd) {
+	cfg := embed.NewConfig()
+	cfg.Dir = t.TempDir()
+	// rand port in [20000, 60000)
+	randPort := int(rand.Int31n(40000)) + 20000
+	clientAddr := fmt.Sprintf(addrFmt, randPort)
+	lcurl, _ := url.Parse(clientAddr)
+	cfg.LCUrls, cfg.ACUrls = []url.URL{*lcurl}, []url.URL{*lcurl}
+	lpurl, _ := url.Parse(fmt.Sprintf(addrFmt, randPort+1))
+	cfg.LPUrls, cfg.APUrls = []url.URL{*lpurl}, []url.URL{*lpurl}
+	cfg.InitialCluster = "default=" + lpurl.String()
+	cfg.Logger = "zap"
+	embedEtcd, err := embed.StartEtcd(cfg)
+	require.NoError(t, err)
+
+	select {
+	case <-embedEtcd.Server.ReadyNotify():
+	case <-time.After(5 * time.Second):
+		embedEtcd.Server.Stop() // trigger a shutdown
+		require.False(t, true, "server took too long to start")
+	}
+	return clientAddr, embedEtcd
+}
+
+func TestCheckRequirements(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	ctx := util.WithInternalSourceType(context.Background(), kv.InternalImportInto)
+	conn := tk.Session().(sqlexec.SQLExecutor)
+
+	_, err := conn.Execute(ctx, "create table test.t(id int primary key)")
+	require.NoError(t, err)
+	is := tk.Session().GetDomainInfoSchema().(infoschema.InfoSchema)
+	tableObj, err := is.TableByName(model.NewCIStr("test"), model.NewCIStr("t"))
+	require.NoError(t, err)
+
+	c := &importer.LoadDataController{
+		Plan: &importer.Plan{
+			DBName: "test",
+		},
+		Table: tableObj,
+	}
+	require.ErrorIs(t, c.CheckRequirements(ctx, conn), exeerrors.ErrLoadDataPreCheckFailed)
+
+	// now checkTotalFileSize pass, and try next pre-check item
+	c.TotalFileSize = 1
+	// non-empty table
+	_, err = conn.Execute(ctx, "insert into test.t values(1)")
+	require.NoError(t, err)
+	require.ErrorIs(t, c.CheckRequirements(ctx, conn), exeerrors.ErrLoadDataPreCheckFailed)
+	// table not exists
+	_, err = conn.Execute(ctx, "drop table if exists test.t")
+	require.NoError(t, err)
+	require.ErrorContains(t, c.CheckRequirements(ctx, conn), "doesn't exist")
+
+	// create table again, now checkTableEmpty pass
+	_, err = conn.Execute(ctx, "create table test.t(id int primary key)")
+	require.NoError(t, err)
+
+	clientAddr, embedEtcd := createMockETCD(t)
+	require.NotNil(t, embedEtcd)
+	t.Cleanup(func() {
+		embedEtcd.Close()
+	})
+	backup := importer.GetEtcdClient
+	importer.GetEtcdClient = func() (*etcd.Client, error) {
+		etcdCli, err := clientv3.New(clientv3.Config{
+			Endpoints: []string{clientAddr},
+		})
+		require.NoError(t, err)
+		return etcd.NewClient(etcdCli, ""), nil
+	}
+	t.Cleanup(func() {
+		importer.GetEtcdClient = backup
+	})
+	// mock a PiTR task
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{clientAddr},
+	})
+	t.Cleanup(func() {
+		require.NoError(t, etcdCli.Close())
+	})
+	require.NoError(t, err)
+	pitrKey := streamhelper.PrefixOfTask() + "test"
+	_, err = etcdCli.Put(ctx, pitrKey, "")
+	require.NoError(t, err)
+	err = c.CheckRequirements(ctx, conn)
+	require.ErrorIs(t, err, exeerrors.ErrLoadDataPreCheckFailed)
+	require.ErrorContains(t, err, "found PiTR log streaming")
+
+	// remove PiTR task, and mock a CDC task
+	_, err = etcdCli.Delete(ctx, pitrKey)
+	require.NoError(t, err)
+	// example: /tidb/cdc/<clusterID>/<namespace>/changefeed/info/<changefeedID>
+	cdcKey := utils.CDCPrefix + "testcluster/test_ns/changefeed/info/test_cf"
+	_, err = etcdCli.Put(ctx, cdcKey, `{"state":"normal"}`)
+	require.NoError(t, err)
+	err = c.CheckRequirements(ctx, conn)
+	require.ErrorIs(t, err, exeerrors.ErrLoadDataPreCheckFailed)
+	require.ErrorContains(t, err, "found CDC changefeed")
+
+	// remove CDC task, pass
+	_, err = etcdCli.Delete(ctx, cdcKey)
+	require.NoError(t, err)
+	require.NoError(t, c.CheckRequirements(ctx, conn))
+
+	// with global sort
+	c.Plan.CloudStorageURI = ":"
+	require.ErrorIs(t, c.CheckRequirements(ctx, conn), exeerrors.ErrLoadDataInvalidURI)
+	c.Plan.CloudStorageURI = "sdsdsdsd://sdsdsdsd"
+	require.ErrorIs(t, c.CheckRequirements(ctx, conn), exeerrors.ErrLoadDataInvalidURI)
+	c.Plan.CloudStorageURI = "local:///tmp"
+	require.ErrorContains(t, c.CheckRequirements(ctx, conn), "unsupported cloud storage uri scheme: local")
+	// this mock cannot mock credential check, so we just skip it.
+	backend := s3mem.New()
+	faker := gofakes3.New(backend)
+	ts := httptest.NewServer(faker.Server())
+	defer ts.Close()
+	require.NoError(t, backend.CreateBucket("test-bucket"))
+	c.Plan.CloudStorageURI = fmt.Sprintf("s3://test-bucket/path?region=us-east-1&endpoint=%s&access-key=xxxxxx&secret-access-key=xxxxxx", ts.URL)
+	require.NoError(t, c.CheckRequirements(ctx, conn))
+>>>>>>> 95378e5bede (br: check the correct changefeed info when restore/import data (#47322))
 }
