7318249a5e395cd8b1bc1ef4d625fafd52d68a1b
Weizhen Wang <wangweizhen@pingcap.com>
2023-10-06 23:01:02 +0800

    u

--- statistics/handle/globalstats/global_stats.go
+++ statistics/handle/globalstats/global_stats.go
@@ -140,7 +140,8 @@ func MergePartitionStats2GlobalStats(
 			var err1 error
 			partitionStats, err1 = loadTablePartitionStatsFn(tableInfo, &def)
 			if err1 != nil {
-				if skipMissingPartitionStats && types.ErrPartitionStatsMissing.Equal(err1) {
+				if skipMissingPartitionStats && (types.ErrPartitionStatsMissing.Equal(err1) ||
+					types.ErrPartitionStatsMissing.Equal(err1)) {
 					globalStats.MissingPartitionStats = append(globalStats.MissingPartitionStats, fmt.Sprintf("partition `%s`", def.Name.L))
 					continue
 				}
