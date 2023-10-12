b9e9ffc4eecba5491178b35aed6b9f52a6024646
ywqzzy <592838129@qq.com>
2023-10-12 13:33:24 +0800

    fix

--- disttask/framework/dispatcher/dispatcher.go
+++ disttask/framework/dispatcher/dispatcher.go
@@ -387,7 +387,7 @@ func (d *BaseDispatcher) replaceDeadNodesIfAny() error {
 				replaceNodes[nodeID] = disttaskutil.GenerateExecID(n.IP, n.Port)
 			}
 		}
-		logutil.Logger(d.logCtx).Info("reschedule subtasks to other nodes")
+		logutil.Logger(d.logCtx).Info("reschedule subtasks to other nodes", zap.Int("node-cnt", len(replaceNodes)))
 		if err := d.taskMgr.UpdateFailedSchedulerIDs(d.Task.ID, replaceNodes); err != nil {
 			return err
 		}
