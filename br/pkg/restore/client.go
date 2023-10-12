9055706fd86fc92d8580e3aeda17ec5dafdab737
Leavrth <jianjun.liao@outlook.com>
2023-10-13 00:04:22 +0800

    add some annotations
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- br/pkg/restore/client.go
+++ br/pkg/restore/client.go
@@ -1757,7 +1757,7 @@ func (rc *Client) execChecksum(
 func (rc *Client) GoUpdateMetaAndLoadStats(ctx context.Context, inCh <-chan *CreatedTable, errCh chan<- error) chan *CreatedTable {
 	log.Info("Start to update meta then load stats")
 	outCh := DefaultOutputTableChan()
-	workers := utils.NewWorkerPool(1, "UpdateStats")
+	workers := utils.NewWorkerPool(8, "UpdateStats")
 	go concurrentHandleTablesCh(ctx, inCh, outCh, errCh, workers, func(c context.Context, tbl *CreatedTable) error {
 		oldTable := tbl.OldTable
 		// Not need to return err when failed because of update analysis-meta
@@ -1780,6 +1780,7 @@ func (rc *Client) GoUpdateMetaAndLoadStats(ctx context.Context, inCh <-chan *Cre
 				zap.Int64("new id", tbl.Table.ID),
 			)
 			start := time.Now()
+			// NOTICE: skip updating cache after load stats from json
 			if err := rc.statsHandler.LoadStatsFromJSON(ctx, rc.dom.InfoSchema(), oldTable.Stats, 0, true); err != nil {
 				log.Error("analyze table failed", zap.Any("table", oldTable.Stats), zap.Error(err))
 			}
