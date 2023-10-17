da27fab16237580950d37951723a86764bdf79cb
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2023-10-17 11:00:28 +0800

    planner: move more methods from StatsHandle to its sub-packages (#47679)
    ref pingcap/tidb#46905

--- pkg/statistics/handle/bootstrap.go
+++ pkg/statistics/handle/bootstrap.go
@@ -70,7 +70,7 @@ func (h *Handle) initStatsMeta(is infoschema.InfoSchema) (util.StatsCache, error
 		return nil, errors.Trace(err)
 	}
 	defer terror.Call(rc.Close)
-	tables, err := cache.NewStatsCacheImpl(h, h.TableStatsFromStorage)
+	tables, err := cache.NewStatsCacheImpl(h)
 	if err != nil {
 		return nil, err
 	}
