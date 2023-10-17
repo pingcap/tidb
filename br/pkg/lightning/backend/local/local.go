210841889961600dc9ec04c9e7c5a4fc3706a533
lance6716 <lance6716@gmail.com>
2023-10-17 11:38:51 +0800

    add test
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- br/pkg/lightning/backend/local/local.go
+++ br/pkg/lightning/backend/local/local.go
@@ -1431,7 +1431,6 @@ func (local *Backend) executeJob(
 			// if it's retryable error, we retry from scanning region
 			log.FromContext(ctx).Warn("meet retryable error when writing to TiKV",
 				log.ShortError(err), zap.Stringer("job stage", job.stage))
-			job.convertStageTo(needRescan)
 			job.lastRetryableErr = err
 			return nil
 		}
