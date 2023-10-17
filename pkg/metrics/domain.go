ead261e3bd0196af622c2e10f5cf411e7c5e0d31
Jack Yu <jackysp@gmail.com>
2023-10-17 14:10:38 -0700

    milliseconds better
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- pkg/metrics/domain.go
+++ pkg/metrics/domain.go
@@ -59,7 +59,7 @@ func InitDomainMetrics() {
 			Namespace: "tidb",
 			Subsystem: "domain",
 			Name:      "lease_expire_time",
-			Help:      "When the last time the lease is expired, it is in microseconds",
+			Help:      "When the last time the lease is expired, it is in milliseconds",
 		})
 
 	LoadSchemaCounter = NewCounterVec(
