acbf1184089901e05259b82449ce2699b19c1df5
lance6716 <lance6716@gmail.com>
2023-10-17 15:21:41 +0800

    add debug message
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- br/pkg/lightning/backend/local/local.go
+++ br/pkg/lightning/backend/local/local.go
@@ -1365,6 +1365,10 @@ func (local *Backend) startWorker(
 			}
 
 			if err != nil {
+				log.FromContext(ctx).Error("execute job failed",
+					log.ShortError(err),
+					zap.Bool("lance test", ctx.Err() == nil),
+				)
 				return err
 			}
 		}
