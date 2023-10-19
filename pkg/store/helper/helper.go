2cb271d8e2fe3264187d69d5470c31ca9485782a
crazycs520 <crazycs520@gmail.com>
2023-10-19 20:51:53 +0800

    fix test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/store/helper/helper.go
+++ pkg/store/helper/helper.go
@@ -98,7 +98,7 @@ func NewHelper(store Storage) *Helper {
 
 // GetMvccByEncodedKey get the MVCC value by the specific encoded key.
 func (h *Helper) GetMvccByEncodedKey(encodedKey kv.Key) (*kvrpcpb.MvccGetByKeyResponse, error) {
-	bo := tikv.NewBackofferWithVars(context.Background(), 500, nil)
+	bo := tikv.NewBackofferWithVars(context.Background(), 5000, nil)
 	for {
 		keyLocation, err := h.RegionCache.LocateKey(bo, encodedKey)
 		if err != nil {
