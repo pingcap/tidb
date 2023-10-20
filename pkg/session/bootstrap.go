21789dc358d10cc25947df897942bbf74b992d15
Weizhen Wang <wangweizhen@pingcap.com>
2023-10-20 14:51:58 +0800

    *
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/session/bootstrap.go
+++ pkg/session/bootstrap.go
@@ -2840,6 +2840,7 @@ func upgradeToVer177(s Session, ver int64) {
 	}
 	// ignore error when upgrading from v7.4 to higher version.
 	doReentrantDDL(s, CreateDistFrameworkMeta, infoschema.ErrTableExists)
+	s.GetSessionVars().GlobalVarsAccessor.SetGlobalSysVar(context.Background(), variable.TiDBEnableAsyncMergeGlobalStats, variable.Off)
 }
 
 func writeOOMAction(s Session) {
