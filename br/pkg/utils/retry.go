766e0c62c5e6354bfdec7bc207e6916716d3a30b
3pointer <luancheng@pingcap.com>
2023-10-20 16:39:30 +0800

    br: retry import sst files on invalidPart error (#47584) (#47858)
    ref pingcap/tidb#47656

--- br/pkg/utils/retry.go
+++ br/pkg/utils/retry.go
@@ -29,6 +29,7 @@ var retryableServerError = []string{
 	"internalerror",
 	"not read from or written to within the timeout period",
 	"<code>requesttimeout</code>",
+	"<code>invalidpart</code>",
 }
 
 // RetryableFunc presents a retryable operation.
