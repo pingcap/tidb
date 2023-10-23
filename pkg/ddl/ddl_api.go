8d12886faf07a42bc18d9a9195f18f0e45a531b3
Lynn <zimu_xia@126.com>
2023-10-23 13:42:25 +0800

    ddl, parser: fix the job version issue

--- pkg/ddl/ddl_api.go
+++ pkg/ddl/ddl_api.go
@@ -8992,5 +8992,6 @@ func NewDDLReorgMeta(ctx sessionctx.Context) *model.DDLReorgMeta {
 		WarningsCount:     make(map[errors.ErrorID]int64),
 		Location:          &model.TimeZoneLocation{Name: tzName, Offset: tzOffset},
 		ResourceGroupName: ctx.GetSessionVars().ResourceGroupName,
+		Version:           model.CurrentReorgMetaVersion,
 	}
 }
