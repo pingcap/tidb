971d6a903c3a0eeb1cc83bf99b2e6e349345575b
lance6716 <lance6716@gmail.com>
2023-10-25 14:28:46 +0800

    lightning: every HTTP retry should use its own request
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- br/pkg/pdutil/pd.go
+++ br/pkg/pdutil/pd.go
@@ -163,13 +163,13 @@ func pdRequestWithCode(
 		return 0, nil, errors.Trace(err)
 	}
 	reqURL := fmt.Sprintf("%s/%s", u, prefix)
-	req, err := http.NewRequestWithContext(ctx, method, reqURL, body)
-	if err != nil {
-		return 0, nil, errors.Trace(err)
-	}
 	var resp *http.Response
 	count := 0
 	for {
+		req, err := http.NewRequestWithContext(ctx, method, reqURL, body)
+		if err != nil {
+			return 0, nil, errors.Trace(err)
+		}
 		resp, err = cli.Do(req) //nolint:bodyclose
 		count++
 		failpoint.Inject("InjectClosed", func(v failpoint.Value) {
