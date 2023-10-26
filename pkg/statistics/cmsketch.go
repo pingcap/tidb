44a714ee0ff822ec74d01d55fe2e7b2ad8e03fd8
hi-rustin <rustin.liu@gmail.com>
2023-10-26 15:55:51 +0800

    statistics: better benchmark tests for merge topN
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- pkg/statistics/cmsketch.go
+++ pkg/statistics/cmsketch.go
@@ -828,14 +828,12 @@ func MergeTopN(topNs []*TopN, n uint32) (*TopN, []TopNMeta) {
 
 // CheckEmptyTopNs checks whether all TopNs are empty.
 func CheckEmptyTopNs(topNs []*TopN) bool {
-	count := uint64(0)
 	for _, topN := range topNs {
-		count += topN.TotalCount()
-		if count != 0 {
+		if topN.TotalCount() != 0 {
 			return false
 		}
 	}
-	return count == 0
+	return true
 }
 
 // SortTopnMeta sort topnMeta
