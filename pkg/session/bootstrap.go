cf5d3865bed460da93dda56256dabce840c0e15a
tangenta <tangenta@126.com>
2023-10-27 12:31:20 +0800

    bootstrap: modify mysql.global_variables COLUMN  to varchar(16383)

--- pkg/session/bootstrap.go
+++ pkg/session/bootstrap.go
@@ -1020,11 +1020,15 @@ const (
 	// version 178
 	//   write mDDLTableVersion into `mysql.tidb` table
 	version178 = 178
+
+	// vresion 179
+	//   enlarge `VARIABLE_VALUE` of `mysql.global_variables` from `varchar(1024)` to `varchar(16383)`.
+	version179 = 179
 )
 
 // currentBootstrapVersion is defined as a variable, so we can modify its value for testing.
 // please make sure this is the largest version
-var currentBootstrapVersion int64 = version178
+var currentBootstrapVersion int64 = version179
 
 // DDL owner key's expired time is ManagerSessionTTL seconds, we should wait the time and give more time to have a chance to finish it.
 var internalSQLTimeout = owner.ManagerSessionTTL + 15
@@ -1178,6 +1182,7 @@ var (
 		upgradeToVer176,
 		upgradeToVer177,
 		upgradeToVer178,
+		upgradeToVer179,
 	}
 )
 
@@ -2877,6 +2882,13 @@ func upgradeToVer178(s Session, ver int64) {
 		return
 	}
 	writeDDLTableVersion(s)
+}
+
+func upgradeToVer179(s Session, ver int64) {
+	if ver >= version179 {
+		return
+	}
+	doReentrantDDL(s, "ALTER TABLE mysql.global_variables MODIFY COLUMN `VARIABLE_VALUE` varchar(16383)")
 }
 
 func writeOOMAction(s Session) {
