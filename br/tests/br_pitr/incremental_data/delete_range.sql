df8d7c9bbf08c65763447bf894cdbc3303cc1f4c
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-10-31 15:20:07 +0800

    br: add integration test for pitr (#47740) (#47906)
    ref pingcap/tidb#47738

---
+++ br/tests/br_pitr/incremental_data/delete_range.sql
@@ -0,0 +1,30 @@
+-- 1. Drop Schema
+drop database db_to_be_dropped;
+-- 2. Drop/Truncate Table
+drop table table_to_be_dropped_or_truncated.t0_dropped;
+drop table table_to_be_dropped_or_truncated.t1_dropped;
+truncate table table_to_be_dropped_or_truncated.t0_truncated;
+truncate table table_to_be_dropped_or_truncated.t1_truncated;
+-- 3. Drop/Truncate/Reorganize Table Partition
+alter table partition_to_be_dropped_or_truncated.t1_dropped drop partition p0; 
+alter table partition_to_be_dropped_or_truncated.t1_truncated truncate partition p0;
+alter table partition_to_be_dropped_or_truncated.t1_truncated reorganize partition p2 INTO (PARTITION p2 VALUES LESS THAN (20), PARTITION p3 VALUES LESS THAN MAXVALUE);
+-- 4. Drop Table Index/PrimaryKey or Add Table Index/Primary
+alter table index_or_primarykey_to_be_dropped.t0 drop index k1;
+alter table index_or_primarykey_to_be_dropped.t1 drop index k1;
+alter table index_or_primarykey_to_be_dropped.t0 drop primary key;
+alter table index_or_primarykey_to_be_dropped.t1 drop primary key;
+create index k1 on index_or_primarykey_to_be_dropped.t0 (name);
+create index k1 on index_or_primarykey_to_be_dropped.t1 (name);
+alter table index_or_primarykey_to_be_dropped.t0 add primary key (id);
+alter table index_or_primarykey_to_be_dropped.t1 add primary key (id);
+-- 5. Drop Table Indexes
+alter table indexes_to_be_dropped.t0 drop index k1, drop index k2;
+alter table indexes_to_be_dropped.t1 drop index k1, drop index k2;
+-- 6. Drop Table Column/Columns
+alter table column_s_to_be_dropped.t0_column drop column name;
+alter table column_s_to_be_dropped.t1_column drop column name;
+alter table column_s_to_be_dropped.t0_columns drop column name, drop column c;
+alter table column_s_to_be_dropped.t1_columns drop column name, drop column c;
+-- 7. Modify Table Column
+alter table column_to_be_modified.t0 modify column name varchar(25);
