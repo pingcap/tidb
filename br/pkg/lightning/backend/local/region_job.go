1e38de610f9d5d2b6a9bf89531b00927c27ccfa0
tangenta <tangenta@126.com>
2023-11-01 12:17:05 +0800

    add import failure test

--- br/pkg/lightning/backend/local/region_job.go
+++ br/pkg/lightning/backend/local/region_job.go
@@ -303,6 +303,11 @@ func (local *Backend) doWrite(ctx context.Context, j *regionJob) error {
 			return annotateErr(err, peer, "when open write stream")
 		}
 
+		failpoint.Inject("mockWritePeerErr", func() {
+			err = errors.Errorf("mock write peer error")
+			failpoint.Return(annotateErr(err, peer))
+		})
+
 		// Bind uuid for this write request
 		if err = wstream.Send(req); err != nil {
 			return annotateErr(err, peer, "when send meta")
