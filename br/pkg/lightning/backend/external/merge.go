6fc1ae011e321cc3e54f6732361a75c6f9d60c93
wjHuang <huangwenjun1997@gmail.com>
2023-11-02 12:24:37 +0800

    external: merge overlapping files concurrently (#48073)
    close pingcap/tidb#48072

--- br/pkg/lightning/backend/external/merge.go
+++ br/pkg/lightning/backend/external/merge.go
@@ -3,13 +3,63 @@ package external
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/pingcap/tidb/br/pkg/storage"
+	"github.com/pingcap/tidb/pkg/util/logutil"
+	"github.com/pingcap/tidb/pkg/util/memory"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 // MergeOverlappingFiles reads from given files whose key range may overlap
 // and writes to new sorted, nonoverlapping files.
-func MergeOverlappingFiles(
-	ctx context.Context,
+func MergeOverlappingFiles(ctx context.Context, paths []string, store storage.ExternalStorage, readBufferSize int,
+	newFilePrefix string, blockSize int, writeBatchCount uint64, propSizeDist uint64, propKeysDist uint64,
+	onClose OnCloseFunc, concurrency int, checkHotspot bool) error {
+	var dataFilesSlice [][]string
+	batchCount := 1
+	if len(paths) > concurrency {
+		batchCount = len(paths) / concurrency
+	}
+	for i := 0; i < len(paths); i += batchCount {
+		end := i + batchCount
+		if end > len(paths) {
+			end = len(paths)
+		}
+		dataFilesSlice = append(dataFilesSlice, paths[i:end])
+	}
+
+	memTotal, err := memory.MemTotal()
+	if err != nil {
+		return err
+	}
+	memSize := (memTotal / 2) / uint64(len(dataFilesSlice))
+
+	var eg errgroup.Group
+	for _, files := range dataFilesSlice {
+		files := files
+		eg.Go(func() error {
+			return mergeOverlappingFilesImpl(
+				ctx,
+				files,
+				store,
+				readBufferSize,
+				newFilePrefix,
+				uuid.New().String(),
+				memSize,
+				blockSize,
+				writeBatchCount,
+				propSizeDist,
+				propKeysDist,
+				onClose,
+				checkHotspot,
+			)
+		})
+	}
+	return eg.Wait()
+}
+
+func mergeOverlappingFilesImpl(ctx context.Context,
 	paths []string,
 	store storage.ExternalStorage,
 	readBufferSize int,
@@ -28,20 +78,24 @@ func MergeOverlappingFiles(
 	if err != nil {
 		return err
 	}
-	defer iter.Close()
+	defer func() {
+		err := iter.Close()
+		if err != nil {
+			logutil.Logger(ctx).Warn("close iterator failed", zap.Error(err))
+		}
+	}()
 
 	writer := NewWriterBuilder().
 		SetMemorySizeLimit(memSizeLimit).
 		SetBlockSize(blockSize).
-		SetWriterBatchCount(writeBatchCount).
-		SetPropKeysDistance(propKeysDist).
-		SetPropSizeDistance(propSizeDist).
 		SetOnCloseFunc(onClose).
+		SetWriterBatchCount(writeBatchCount).
+		SetPropSizeDistance(propSizeDist).
+		SetPropKeysDistance(propKeysDist).
 		Build(store, newFilePrefix, writerID)
 
 	// currently use same goroutine to do read and write. The main advantage is
 	// there's no KV copy and iter can reuse the buffer.
-
 	for iter.Next() {
 		err = writer.WriteRow(ctx, iter.Key(), iter.Value(), nil)
 		if err != nil {
