8833d09f752cdb3304baa6865db66afe532d299f
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-11-02 17:33:39 +0800

    pdutil/backend: enlarge max retry time and fix nested retriable error (#48210) (#48228)
    close pingcap/tidb#46950

--- br/pkg/pdutil/pd.go
+++ br/pkg/pdutil/pd.go
@@ -52,8 +52,7 @@ const (
 	pauseTimeout         = 5 * time.Minute
 
 	// pd request retry time when connection fail
-	pdRequestRetryTime = 10
-
+	pdRequestRetryTime = 120
 	// set max-pending-peer-count to a large value to avoid scatter region failed.
 	maxPendingPeerUnlimited uint64 = math.MaxInt32
 )
@@ -169,6 +168,7 @@ func pdRequestWithCode(
 		resp *http.Response
 	)
 	count := 0
+	// the total retry duration: 120*1 = 2min
 	for {
 		req, err = http.NewRequestWithContext(ctx, method, reqURL, body)
 		if err != nil {
