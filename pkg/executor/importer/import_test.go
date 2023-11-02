7077f044e91d84fee15cd71e7f0efcad3faf280b
Weizhen Wang <wangweizhen@pingcap.com>
2023-11-02 15:50:53 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/executor/importer/import_test.go
+++ pkg/executor/importer/import_test.go
@@ -369,7 +369,8 @@ func TestSupportedSuffixForServerDisk(t *testing.T) {
 	// without permission to parent dir
 	c.Path = path.Join(tempDir, "no-perm", "no-perm.csv")
 	err = c.InitDataFiles(ctx)
-	require.ErrorIs(t, err, exeerrors.ErrLoadDataCantRead, err)
+	c.logger.Info("the error", zap.Error(err))
+	require.ErrorIs(t, err, exeerrors.ErrLoadDataCantRead)
 	require.ErrorContains(t, err, "permission denied")
 	// file not exists
 	c.Path = path.Join(tempDir, "not-exists.csv")
