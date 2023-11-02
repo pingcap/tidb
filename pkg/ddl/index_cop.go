11924f1ffc52d4b0759afcd0f6e91aacce986ec7
wjhuang2016 <huangwenjun1997@gmail.com>
2023-11-02 15:28:40 +0800

    try
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- pkg/ddl/index_cop.go
+++ pkg/ddl/index_cop.go
@@ -384,7 +384,7 @@ func buildHandle(pkDts []types.Datum, tblInfo *model.TableInfo, pkInfo *model.In
 	}
 	//return kv.IntHandle(pkDts[0].GetInt64()), nil
 	addr := unsafe.Pointer(uintptr(unsafe.Pointer(&bufHandle)) + 8)
-	**(**int64)(unsafe.Pointer(&addr)) = pkDts[0].GetInt64()
+	**(**int64)(addr) = pkDts[0].GetInt64()
 	//ih := bufHandle.(kv.IntHandle)
 	//ih.SetInt(int(pkDts[0].GetInt64()))
 	return bufHandle, nil
