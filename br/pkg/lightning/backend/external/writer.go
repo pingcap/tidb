25cfc94e6c5100a292e9c730a0f01e96a83d0f70
tangenta <tangenta@126.com>
2023-11-02 18:38:03 +0800

    remove sorty to fix data race

--- br/pkg/lightning/backend/external/writer.go
+++ br/pkg/lightning/backend/external/writer.go
@@ -20,11 +20,11 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"time"
 
 	"github.com/docker/go-units"
-	"github.com/jfcg/sorty/v2"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/br/pkg/lightning/backend"
 	"github.com/pingcap/tidb/br/pkg/lightning/backend/encode"
@@ -439,16 +439,8 @@ func (w *Writer) flushKVs(ctx context.Context, fromClose bool) (err error) {
 	}()
 
 	sortStart := time.Now()
-	sorty.MaxGor = 1
-	sorty.Sort(len(w.kvLocations), func(i, j, r, s int) bool {
-		posi, posj := w.kvLocations[i], w.kvLocations[j]
-		if bytes.Compare(w.getKeyByLoc(posi), w.getKeyByLoc(posj)) < 0 {
-			if r != s {
-				w.kvLocations[r], w.kvLocations[s] = w.kvLocations[s], w.kvLocations[r]
-			}
-			return true
-		}
-		return false
+	slices.SortFunc(w.kvLocations, func(i, j kvLocation) int {
+		return bytes.Compare(w.getKeyByLoc(i), w.getKeyByLoc(j))
 	})
 	sortDuration = time.Since(sortStart)
 
