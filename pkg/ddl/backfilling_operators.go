370cd1bd660bc51b25b3014a9159127eed70cfbd
GMHDBJD <35025882+GMHDBJD@users.noreply.github.com>
2023-11-03 14:00:36 +0800

    Update pkg/ddl/backfilling_operators.go
    Co-authored-by: tangenta <tangenta@126.com>

--- pkg/ddl/backfilling_operators.go
+++ pkg/ddl/backfilling_operators.go
@@ -208,7 +208,7 @@ func NewWriteIndexToExternalStoragePipeline(
 		return nil, err
 	}
 	memAvaliable := memTotal - memUsed
-	logutil.BgLogger().Info("memory info", zap.Uint64("total", memTotal), zap.Uint64("used", memUsed), zap.Uint64("avaliable", memAvaliable))
+	logutil.BgLogger().Info("build operators that write index to cloud storage", zap.Uint64("memory total", memTotal), zap.Uint64("memory used", memUsed), zap.Uint64("memory available", memAvaliable))
 	memSize := (memAvaliable / 2) / uint64(writerCnt) / uint64(len(indexes))
 
 	srcOp := NewTableScanTaskSource(ctx, store, tbl, startKey, endKey)
