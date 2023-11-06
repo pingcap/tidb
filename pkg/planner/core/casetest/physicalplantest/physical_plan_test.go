283d1a03712d153a07fdada7e627639840e5cefd
Jinlong Liu <50897894+King-Dylan@users.noreply.github.com>
2023-11-06 11:05:48 +0800

    Update
    Co-authored-by: Elsa <111482174+elsa0520@users.noreply.github.com>

--- pkg/planner/core/casetest/physicalplantest/physical_plan_test.go
+++ pkg/planner/core/casetest/physicalplantest/physical_plan_test.go
@@ -166,7 +166,7 @@ func TestAggEliminator(t *testing.T) {
 		require.Equal(t, output[i].Best, core.ToString(p), fmt.Sprintf("input: %s", tt))
 	}
 }
-
+// Fix Issue #45822 
 func TestRuleColumnPruningLogicalApply(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 	tk := testkit.NewTestKit(t, store)
