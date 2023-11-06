a30bc5c8288b290ed5737179e4e7307a53d58592
Rustin Liu <rustin.liu@gmail.com>
2023-11-06 21:56:12 +0800

    executor: better comments (#48311)

--- pkg/executor/compiler.go
+++ pkg/executor/compiler.go
@@ -61,8 +61,11 @@ func (c *Compiler) Compile(ctx context.Context, stmtNode ast.StmtNode) (_ *ExecS
 
 	c.Ctx.GetSessionVars().StmtCtx.IsReadOnly = plannercore.IsReadOnly(stmtNode, c.Ctx.GetSessionVars())
 
+	// Do preprocess and validate.
 	ret := &plannercore.PreprocessorReturn{}
-	err = plannercore.Preprocess(ctx, c.Ctx,
+	err = plannercore.Preprocess(
+		ctx,
+		c.Ctx,
 		stmtNode,
 		plannercore.WithPreprocessorReturn(ret),
 		plannercore.InitTxnContextProvider,
@@ -85,18 +88,19 @@ func (c *Compiler) Compile(ctx context.Context, stmtNode ast.StmtNode) (_ *ExecS
 	stmtCtx := sessVars.StmtCtx
 	// handle the execute statement
 	var (
-		pointPlanShortPathOK bool
-		preparedObj          *plannercore.PlanCacheStmt
+		pointGetPlanShortPathOK bool
+		preparedObj             *plannercore.PlanCacheStmt
 	)
 
 	if execStmt, ok := stmtNode.(*ast.ExecuteStmt); ok {
 		if preparedObj, err = plannercore.GetPreparedStmt(execStmt, sessVars); err != nil {
 			return nil, err
 		}
-		if pointPlanShortPathOK, err = plannercore.IsPointPlanShortPathOK(c.Ctx, is, preparedObj); err != nil {
+		if pointGetPlanShortPathOK, err = plannercore.IsPointGetPlanShortPathOK(c.Ctx, is, preparedObj); err != nil {
 			return nil, err
 		}
 	}
+	// Build the final physical plan.
 	finalPlan, names, err := planner.Optimize(ctx, c.Ctx, stmtNode, is)
 	if err != nil {
 		return nil, err
@@ -127,7 +131,8 @@ func (c *Compiler) Compile(ctx context.Context, stmtNode ast.StmtNode) (_ *ExecS
 		OutputNames:   names,
 		Ti:            &TelemetryInfo{},
 	}
-	if pointPlanShortPathOK {
+	// Use cached plan if possible.
+	if pointGetPlanShortPathOK {
 		if ep, ok := stmt.Plan.(*plannercore.Execute); ok {
 			if pointPlan, ok := ep.Plan.(*plannercore.PointGetPlan); ok {
 				stmtCtx.SetPlan(stmt.Plan)
@@ -140,9 +145,12 @@ func (c *Compiler) Compile(ctx context.Context, stmtNode ast.StmtNode) (_ *ExecS
 			}
 		}
 	}
-	if err = sessiontxn.OptimizeWithPlanAndThenWarmUp(c.Ctx, stmt.Plan); err != nil {
+
+	// Perform optimization and initialization related to the transaction level.
+	if err = sessiontxn.AdviseOptimizeWithPlanAndThenWarmUp(c.Ctx, stmt.Plan); err != nil {
 		return nil, err
 	}
+
 	return stmt, nil
 }
 
