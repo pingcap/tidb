e9c2583abe2386e189a7fa723452bef001998ed3
elsa0520 <elsa0520@163.com>
2023-11-07 17:05:35 +0800

    the cte indirectly contains agg also cannot be inline in recursive part

--- pkg/planner/core/planbuilder.go
+++ pkg/planner/core/planbuilder.go
@@ -494,7 +494,7 @@ type cteInfo struct {
 	isInline bool
 	// forceInlineByHintOrVar will be true when CTE is hint by merge() or session variable "tidb_opt_force_inline_cte=true"
 	forceInlineByHintOrVar bool
-	// If CTE contain aggregation or window function in query
+	// If CTE contain aggregation or window function in query (Indirect references to other cte containing agg or window in the query are also counted.)
 	containAggOrWindow bool
 	// Compute in preprocess phase. Record how many consumers the current CTE has
 	consumerCount int
