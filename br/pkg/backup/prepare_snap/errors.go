e34b09633f433f6d4a29ca6fe88b91cac00fb904
hillium <yujuncen@pingcap.com>
2023-11-08 20:31:21 +0800

    added client for preparing snapshot
    Signed-off-by: hillium <yujuncen@pingcap.com>

---
+++ br/pkg/backup/prepare_snap/errors.go
@@ -0,0 +1,21 @@
+package preparesnap
+
+import (
+	"github.com/pingcap/errors"
+	"github.com/pingcap/kvproto/pkg/errorpb"
+)
+
+func convertErr(err *errorpb.Error) error {
+	if err == nil {
+		return nil
+	}
+	return errors.New(err.Message)
+}
+
+func errLeaseExpired() error {
+	return errors.New("the lease has expired")
+}
+
+func unsupported() error {
+	return errors.New("the lease has expired")
+}
