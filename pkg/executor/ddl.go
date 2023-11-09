7f20f6a2b4cfcb2c36ce69b7e2e0867c3643b08a
BornChanger <dawn_catcher@126.com>
2023-11-09 22:36:26 +0800

    *: code format
    Signed-off-by: BornChanger <dawn_catcher@126.com>

--- pkg/executor/ddl.go
+++ pkg/executor/ddl.go
@@ -540,16 +540,18 @@ func (e *DDLExec) getRecoverTableByTableName(tableName *ast.TableName) (*model.J
 }
 
 func (e *DDLExec) executeFlashBackCluster(s *ast.FlashBackToTimestampStmt) error {
+	// Check `TO TSO` clause
 	if s.FlashbackTSO > 0 {
 		return domain.GetDomain(e.Ctx()).DDL().FlashbackCluster(e.Ctx(), s.FlashbackTSO)
-	} else {
-		flashbackTS, err := staleread.CalculateAsOfTsExpr(context.Background(), e.Ctx(), s.FlashbackTS)
-		if err != nil {
-			return err
-		}
-
-		return domain.GetDomain(e.Ctx()).DDL().FlashbackCluster(e.Ctx(), flashbackTS)
 	}
+
+	// Check `TO TIMESTAMP` clause
+	flashbackTS, err := staleread.CalculateAsOfTsExpr(context.Background(), e.Ctx(), s.FlashbackTS)
+	if err != nil {
+		return err
+	}
+
+	return domain.GetDomain(e.Ctx()).DDL().FlashbackCluster(e.Ctx(), flashbackTS)
 }
 
 func (e *DDLExec) executeFlashbackTable(s *ast.FlashBackTableStmt) error {
