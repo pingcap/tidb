fe0a6d0fda05a2b76768dcd6803c17a814dde1d4
Mattias Jonsson <mjonss@gmail.com>
2023-11-16 15:19:31 +0100

    Added more placement policy tests, including for remove partitioning

--- pkg/ddl/tests/partition/placement_test.go
+++ pkg/ddl/tests/partition/placement_test.go
@@ -35,31 +35,6 @@ func TestPartitionByWithPlacement(t *testing.T) {
 	tk1.MustExec(`create placement policy pp2 followers=2`)
 	tk1.MustExec(`create placement policy pp3 followers=3`)
 
-	/*
-		alterChan := make(chan error)
-		waitFor := func(col int, tableName, s string) {
-			for {
-				select {
-				case alterErr := <-alterChan:
-					require.Fail(t, "Alter completed unexpectedly", "With error %v", alterErr)
-				default:
-					// Alter still running
-				}
-				tk := testkit.NewTestKit(t, store)
-				tk.MustExec(`use test`)
-				//sql := `admin show ddl jobs where db_name = '` + strings.ToLower(dbName) + `' and table_name = '` + tableName + `'`
-				sql := `admin show ddl jobs where db_name = '` + strings.ToLower(dbName) + `' and table_name = '` + tableName + `' and job_type = 'alter table partition by' and state = 'running'`
-				res := tk.MustQuery(sql).Rows()
-				if len(res) == 1 && res[0][col] == s {
-					break
-				}
-				time.Sleep(10 * time.Millisecond)
-			}
-			// Allow reload of schema before continuing
-			time.Sleep(10 * time.Millisecond)
-		}
-	*/
-
 	type testCase struct {
 		schemaPP           string
 		createTableExtra   string
@@ -68,58 +43,61 @@ func TestPartitionByWithPlacement(t *testing.T) {
 		beforePartitionsPP []string
 		afterTablePP       string
 		afterPartitionsPP  []string
+		errStr             string
 	}
 
 	cases := []testCase{
-		/*
-			{
-				"",
-				"",
-				`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
-				"",
-				nil,
-				"",
-				nil,
-			},
-			{
-				" placement policy pp1",
-				"",
-				`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
-				"pp1",
-				nil,
-				"pp1",
-				nil,
-			},
-			{
-				" placement policy pp1",
-				" placement policy pp2",
-				`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
-				"pp2",
-				nil,
-				"pp2",
-				nil,
-			},
-			{
-				"",
-				" placement policy pp1",
-				`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
-				"pp1",
-				nil,
-				"pp1",
-				nil,
-			},
-			{
-				" placement policy pp1",
-				" placement policy pp2 partition by hash(a) partitions 3",
-				`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
-				"pp2",
-				nil,
-				"pp2",
-				nil,
-			},
-
-		*/
 		{
+			"",
+			"",
+			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
+			"",
+			nil,
+			"",
+			nil,
+			"",
+		},
+		{ // 1
+			" placement policy pp1",
+			"",
+			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
+			"pp1",
+			nil,
+			"pp1",
+			nil,
+			"",
+		},
+		{ // 2
+			" placement policy pp1",
+			" placement policy pp2",
+			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
+			"pp2",
+			nil,
+			"pp2",
+			nil,
+			"",
+		},
+		{ // 3
+			"",
+			" placement policy pp1",
+			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
+			"pp1",
+			nil,
+			"pp1",
+			nil,
+			"",
+		}, // 4
+		{
+			" placement policy pp1",
+			" placement policy pp2 partition by hash(a) partitions 3",
+			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
+			"pp2",
+			nil,
+			"pp2",
+			nil,
+			"",
+		},
+		{ // 5
 			" placement policy pp1",
 			" partition by hash(a) (partition p0, partition p1 placement policy pp3, partition p2)",
 			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288) placement policy pp3, partition pMax values less than (maxvalue))`,
@@ -127,8 +105,9 @@ func TestPartitionByWithPlacement(t *testing.T) {
 			[]string{"", "pp3", ""},
 			"pp1",
 			[]string{"", "", "pp3", ""},
+			"",
 		},
-		{
+		{ // 6
 			" placement policy pp1",
 			" placement policy pp2 partition by hash(a) (partition p0, partition p1 placement policy pp3, partition p2)",
 			`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`,
@@ -136,22 +115,67 @@ func TestPartitionByWithPlacement(t *testing.T) {
 			[]string{"", "pp3", ""},
 			"pp2",
 			nil,
+			"",
 		},
-	}
-	/*
-		"",
+		{ // 7
 			" placement policy pp2",
 			" placement policy pp3 partition by hash(a) (partition p0, partition p1 placement policy pp1, partition p2 placement policy pp2)",
-			" partition by range(a) (partition p0 values less than (100), partition p1 values less than (200) placement policy pp1, partition p2 values less than (maxvalue) placement policy pp2)",
-	*/
+			"alter table t placement policy pp1 partition by range(a) (partition p0 values less than (100) placement policy pp1, partition p1 values less than (200) placement policy pp2, partition p2 values less than (maxvalue))",
+			"pp3",
+			[]string{"", "pp1", "pp2"},
+			"",
+			nil,
+			"[ddl:8200]Unsupported multi schema change for alter table placement",
+		},
+		{ // 8
+			" placement policy pp2",
+			" placement policy pp3 partition by hash(a) (partition p0, partition p1 placement policy pp1, partition p2 placement policy pp2)",
+			"alter table t partition by range(a) (partition p0 values less than (100) placement policy pp1, partition p1 values less than (200) placement policy pp2, partition p2 values less than (maxvalue))",
+			"pp3",
+			[]string{"", "pp1", "pp2"},
+			"pp3",
+			[]string{"pp1", "pp2", ""},
+			"",
+		},
+		{ // 9
+			" placement policy pp2",
+			" placement policy pp3 partition by hash(a) (partition p0, partition p1 placement policy pp1, partition p2 placement policy pp2)",
+			"alter table t placement policy pp1",
+			"pp3",
+			[]string{"", "pp1", "pp2"},
+			"pp1",
+			[]string{"", "pp1", "pp2"},
+			"",
+		},
+		{ // 10
+			" placement policy pp2",
+			" placement policy pp3 partition by hash(a) (partition p0, partition p1 placement policy pp1, partition p2 placement policy pp2)",
+			"alter table t placement policy pp1 remove partitioning",
+			"pp3",
+			[]string{"", "pp1", "pp2"},
+			"pp1",
+			[]string{"", "pp1", "pp2"},
+			"[ddl:8200]Unsupported multi schema change for alter table placement",
+		},
+		{ // 11
+			" placement policy pp2",
+			" placement policy pp3 partition by hash(a) (partition p0, partition p1 placement policy pp1, partition p2 placement policy pp2)",
+			"alter table t remove partitioning",
+			"pp3",
+			[]string{"", "pp1", "pp2"},
+			"pp3",
+			nil,
+			"",
+		},
+	}
 
-	for testNr, testCase := range cases {
+	for testNr, currTest := range cases {
 		tk1.MustExec(`use test`)
 		tk1.MustExec(`drop schema if exists ` + dbName)
-		tk1.MustExec(`create schema ` + dbName + testCase.schemaPP)
+		tk1.MustExec(`create schema ` + dbName + currTest.schemaPP)
 		tk1.MustExec(`use ` + dbName)
 		tk1.MustExec(`drop table if exists t`)
-		tk1.MustExec(`create table t (a int not null auto_increment primary key, b varchar(255))` + testCase.createTableExtra)
+		tk1.MustExec(`create table t (a int not null auto_increment primary key, b varchar(255))` + currTest.createTableExtra)
 		tk1.MustExec(`insert into t (b) values ("a"),("b"),("c"),("d"),("e"),("f"),("g"),("h"),("i"),("j")`)
 		tk1.MustExec(`insert into t select null, concat("from: ", t.a, " - ", t2.a, " - ", t3.a) from t, t t2, t t3 LIMIT 313`)
 
@@ -159,58 +183,48 @@ func TestPartitionByWithPlacement(t *testing.T) {
 		tk2.MustExec(`use ` + dbName)
 		tk3 := testkit.NewTestKit(t, store)
 		tk3.MustExec(`use ` + dbName)
-		//tk2.MustExec(`BEGIN`)
-		//tk2.MustExec(`insert into t (b) values ("StateNone")`)
 		tk2.MustExec(`insert into t (b) values ("before")`)
 
 		tbl := external.GetTableByName(t, tk2, dbName, "t")
-		if testCase.afterTablePP != "" {
-			require.Equal(t, tbl.Meta().PlacementPolicyRef.Name.O, testCase.afterTablePP)
+		if currTest.beforeTablePP != "" {
+			require.Equal(t, currTest.beforeTablePP, tbl.Meta().PlacementPolicyRef.Name.O, "Wrong table Placement Policy in currTest %d before alter", testNr)
 		} else {
 			require.Nil(t, tbl.Meta().PlacementPolicyRef)
 		}
 
-		for i, partPP := range testCase.beforePartitionsPP {
+		for i, partPP := range currTest.beforePartitionsPP {
 			def := tbl.Meta().Partition.Definitions[i]
 			ppRef := def.PlacementPolicyRef
 			if partPP != "" {
-				require.Equal(t, ppRef.Name.O, partPP)
+				require.Equal(t, partPP, ppRef.Name.O, "currTest %d before alter, partition %s", testNr, def.Name.O)
 			} else {
-				require.Nil(t, ppRef, "testCase %d before alter, expected nil for partition %s", testNr, def.Name.O)
+				require.Nil(t, ppRef, "currTest %d before alter, expected nil for partition %s", testNr, def.Name.O)
 			}
 		}
 
-		tk1.MustExec(testCase.alterSQL)
+		if currTest.errStr != "" {
+			tk1.MustContainErrMsg(currTest.alterSQL, currTest.errStr)
+			continue
+		} else {
+			tk1.MustExec(currTest.alterSQL)
+		}
 
-		/*
-			go func() {
-				alterChan <- tk1.ExecToErr(`alter table t partition by range (a) (partition p0 values less than (88), partition p1 values less than (222), partition p2 values less than (288), partition pMax values less than (maxvalue))`)
-			}()
-			//waitFor(11, "t", "running")
-			waitFor(4, "t", "delete only")
-
-		*/
-
-		//tk3.MustExec(`BEGIN`)
-		//tk3.MustExec(`insert into t (b) values ("StateDeleteOnly")`)
 		tk3.MustExec(`insert into t (b) values ("after")`)
 
-		//tk2.MustExec(`COMMIT`)
-		//tk3.MustExec(`COMMIT`)
-
 		tbl = external.GetTableByName(t, tk3, dbName, "t")
-		if testCase.afterTablePP != "" {
-			require.Equal(t, tbl.Meta().PlacementPolicyRef.Name.O, testCase.afterTablePP)
+		if currTest.afterTablePP != "" {
+			require.Equal(t, currTest.afterTablePP, tbl.Meta().PlacementPolicyRef.Name.O, "Wrong table Placement Policy in currTest %d after alter", testNr)
 		} else {
 			require.Nil(t, tbl.Meta().PlacementPolicyRef)
 		}
 
-		for i, partPP := range testCase.afterPartitionsPP {
+		for i, partPP := range currTest.afterPartitionsPP {
+			def := tbl.Meta().Partition.Definitions[i]
 			ppRef := tbl.Meta().Partition.Definitions[i].PlacementPolicyRef
 			if partPP != "" {
-				require.Equal(t, ppRef.Name.O, partPP)
+				require.Equal(t, partPP, ppRef.Name.O, "currTest %d after alter, partition %s", testNr, def.Name.O)
 			} else {
-				require.Nil(t, ppRef)
+				require.Nil(t, ppRef, "currTest %d after alter, expected nil for partition %s", testNr, def.Name.O)
 			}
 		}
 	}
