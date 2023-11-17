33ac0fc1b3479a7e492d8ed8e609729054c885c9
BornChanger <97348524+BornChanger@users.noreply.github.com>
2023-11-17 20:17:18 +0800

    This is an automated cherry-pick of #48493
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- executor/ddl.go
+++ executor/ddl.go
@@ -531,7 +531,17 @@ func (e *DDLExec) getRecoverTableByTableName(tableName *ast.TableName) (*model.J
 }
 
 func (e *DDLExec) executeFlashBackCluster(s *ast.FlashBackToTimestampStmt) error {
+<<<<<<< HEAD:executor/ddl.go
 	flashbackTS, err := staleread.CalculateAsOfTsExpr(context.Background(), e.ctx, s.FlashbackTS)
+=======
+	// Check `TO TSO` clause
+	if s.FlashbackTSO > 0 {
+		return domain.GetDomain(e.Ctx()).DDL().FlashbackCluster(e.Ctx(), s.FlashbackTSO)
+	}
+
+	// Check `TO TIMESTAMP` clause
+	flashbackTS, err := staleread.CalculateAsOfTsExpr(context.Background(), e.Ctx(), s.FlashbackTS)
+>>>>>>> 6b3df662ca5 (flashback: support to flashback to tso syntax (#48493)):pkg/executor/ddl.go
 	if err != nil {
 		return err
 	}
