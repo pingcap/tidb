5c11979514645411336a6d81147b2abbeedb30cb
tiancaiamao <tiancaiamao@gmail.com>
2023-11-17 16:28:44 +0800

    Revert "server,session: make sure ResultSet.Close() errors return to the client (#48447) (#48486)"
    This reverts commit 87107b5082b731fce1183ab0bacef08bb1d92c6f.

--- pkg/server/conn.go
+++ pkg/server/conn.go
@@ -2050,7 +2050,11 @@ func (cc *clientConn) handleStmt(ctx context.Context, stmt ast.StmtNode, warns [
 			execStmt.(*executor.ExecStmt).FinishExecuteStmt(0, err, false)
 		}
 	}
-	return false, err
+	if err != nil {
+		return false, err
+	}
+
+	return false, nil
 }
 
 func (cc *clientConn) handleFileTransInConn(ctx context.Context, status uint16) (bool, error) {
@@ -2277,9 +2281,6 @@ func (cc *clientConn) writeChunks(ctx context.Context, rs resultset.ResultSet, b
 		if stmtDetail != nil {
 			stmtDetail.WriteSQLRespDuration += time.Since(start)
 		}
-	}
-	if err := rs.Close(); err != nil {
-		return false, err
 	}
 
 	if stmtDetail != nil {
