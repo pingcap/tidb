8ccc8a758bc3754b355897b98dc9f8cf6d586b1e
Arenatlx <314806019@qq.com>
2023-11-20 08:53:42 +0800

    planner: fix potential modification of shared immutable aggDesc (#48671)
    close pingcap/tidb#48643

--- planner/core/physical_plans.go
+++ planner/core/physical_plans.go
@@ -1778,10 +1778,20 @@ func (p *PhysicalHashAgg) MemoryUsage() (sum int64) {
 
 // NewPhysicalHashAgg creates a new PhysicalHashAgg from a LogicalAggregation.
 func NewPhysicalHashAgg(la *LogicalAggregation, newStats *property.StatsInfo, prop *property.PhysicalProperty) *PhysicalHashAgg {
+	newGbyItems := make([]expression.Expression, len(la.GroupByItems))
+	copy(newGbyItems, la.GroupByItems)
+	newAggFuncs := make([]*aggregation.AggFuncDesc, len(la.AggFuncs))
+	// There's some places that rewrites the aggFunc in-place.
+	// I clone it first.
+	// It needs a well refactor to make sure that the physical optimize should not change the things of logical plan.
+	// It's bad for cascades
+	for i, aggFunc := range la.AggFuncs {
+		newAggFuncs[i] = aggFunc.Clone()
+	}
 	agg := basePhysicalAgg{
-		GroupByItems: la.GroupByItems,
-		AggFuncs:     la.AggFuncs,
-	}.initForHash(la.ctx, newStats, la.blockOffset, prop)
+		GroupByItems: newGbyItems,
+		AggFuncs:     newAggFuncs,
+	}.initForHash(la.SCtx(), newStats, la.SelectBlockOffset(), prop)
 	return agg
 }
 
