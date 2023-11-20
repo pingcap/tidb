acf223e2f9017c9314cd83a882576948aecd69ff
BornChanger <dawn_catcher@126.com>
2023-11-21 00:03:52 +0800

    *: address comments
    Signed-off-by: BornChanger <dawn_catcher@126.com>

--- br/pkg/aws/ebs.go
+++ br/pkg/aws/ebs.go
@@ -366,22 +366,21 @@ func (e *EC2Session) waitDataFSREnabled(snapShotIDs []*string, targetAZ string) 
 	startIdx := 0
 	retryCount := 0
 	for startIdx < len(snapShotIDs) {
-		creditBalance, err := e.getFSRCreditBalance(snapShotIDs[startIdx], targetAZ)
-		if err != nil {
-			return errors.Trace(err)
-		}
-
-		if creditBalance >= 1.0 {
+		creditBalance, _ := e.getFSRCreditBalance(snapShotIDs[startIdx], targetAZ)
+		if creditBalance != nil && *creditBalance >= 1.0 {
 			startIdx++
 			retryCount = 0
 		} else {
-			if creditBalance == 0 {
+			if creditBalance == nil {
+				// For invalid calling, retry 3 times
 				if retryCount >= 3 {
 					return errors.Errorf("cloudwatch metrics for %s operation failed after retrying", *snapShotIDs[startIdx])
 				}
 				retryCount++
 			}
-			time.Sleep(1 * time.Minute)
+			// Retry for both invalid calling and not enough fsr credit
+			// Cloudwatch by default flushes every 5 seconds. So, 20 seconds wait should be enough
+			time.Sleep(20 * time.Second)
 		}
 	}
 
@@ -438,7 +437,7 @@ func (e *EC2Session) waitDataFSREnabled(snapShotIDs []*string, targetAZ string) 
 }
 
 // getFSRCreditBalance is used to get maximum fsr credit balance of snapshot for last 5 minutes
-func (e *EC2Session) getFSRCreditBalance(snapshotID *string, targetAZ string) (float64, error) {
+func (e *EC2Session) getFSRCreditBalance(snapshotID *string, targetAZ string) (*float64, error) {
 	// Set the time range to query for metrics
 	startTime := time.Now().Add(-5 * time.Minute)
 	endTime := time.Now()
@@ -468,17 +467,18 @@ func (e *EC2Session) getFSRCreditBalance(snapshotID *string, targetAZ string) (f
 	// Call cloudwatchClient API to retrieve the FastSnapshotRestoreCreditsBalance metric data
 	resp, err := e.cloudwatchClient.GetMetricStatisticsWithContext(context.Background(), input)
 	if err != nil {
-		return 0, errors.Trace(err)
+		log.Error("GetMetricStatisticsWithContext failed", zap.Error(err))
+		return nil, errors.Trace(err)
 	}
 
 	// parse the response
 	if len(resp.Datapoints) == 0 {
 		log.Warn("No result for metric FastSnapshotRestoreCreditsBalance returned", zap.Stringp("snapshot", snapshotID))
-		return 0, nil
+		return nil, nil
 	}
 	result := resp.Datapoints[0]
 	log.Info("credit balance", zap.Stringp("snapshot", snapshotID), zap.Float64p("credit", result.Maximum))
-	return *result.Maximum, nil
+	return result.Maximum, nil
 }
 
 // DisableDataFSR disables FSR for data volume snapshots
