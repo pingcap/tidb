8ff82ca919473a58547c0eeef5633027cc7221ab
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-11-21 21:14:41 +0800

    ddl: init internal session with reorg info for dist tasks (#48737) (#48747)
    close pingcap/tidb#48724

--- tests/realtikvtest/addindextest1/disttask_test.go
+++ tests/realtikvtest/addindextest1/disttask_test.go
@@ -192,3 +192,21 @@ func TestAddIndexDistPauseAndResume(t *testing.T) {
 
 	tk.MustExec(`set global tidb_enable_dist_task=0;`)
 }
+
+func TestAddIndexForCurrentTimestampColumn(t *testing.T) {
+	store := realtikvtest.CreateMockStoreAndSetup(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("drop database if exists addindexlit;")
+	tk.MustExec("create database addindexlit;")
+	tk.MustExec("use addindexlit;")
+	t.Cleanup(func() {
+		tk.MustExec("set global tidb_enable_dist_task = off;")
+	})
+	tk.MustExec(`set global tidb_ddl_enable_fast_reorg=on;`)
+	tk.MustExec("set global tidb_enable_dist_task = on;")
+
+	tk.MustExec("create table t (a timestamp default current_timestamp);")
+	tk.MustExec("insert into t values ();")
+	tk.MustExec("alter table t add index idx(a);")
+	tk.MustExec("admin check table t;")
+}
