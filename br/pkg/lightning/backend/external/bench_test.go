5375d41fb9f83fe59d97cae39393ca6809857bf8
lance6716 <lance6716@gmail.com>
2023-11-22 22:45:07 +0800

    revert some change

--- br/pkg/lightning/backend/external/bench_test.go
+++ br/pkg/lightning/backend/external/bench_test.go
@@ -315,7 +315,7 @@ func createEvenlyDistributedFiles(
 	for i := 0; i < fileCount; i++ {
 		builder := NewWriterBuilder().
 			SetBlockSize(10 * 1024 * 1024).
-			SetMemorySizeLimit(uint64(float64(fileSize) * 1.2))
+			SetMemorySizeLimit(uint64(float64(fileSize) * 1.1))
 		writer := builder.Build(
 			store,
 			"evenly_distributed",
