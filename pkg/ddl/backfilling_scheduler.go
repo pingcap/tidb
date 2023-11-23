2c10ce8573aaa3bc5ede7d014bdb4268fb51cc3d
Benjamin2037 <benjamin2037@sina.com>
2023-11-23 10:01:46 +0800

    Update backfilling_scheduler.go
    fix code conflict

--- pkg/ddl/backfilling_scheduler.go
+++ pkg/ddl/backfilling_scheduler.go
@@ -150,20 +150,11 @@ func initSessCtx(
 	tzLocation *model.TimeZoneLocation,
 	resGroupName string,
 ) error {
-<<<<<<< HEAD
-	// Unify the TimeZone settings in newContext.
-	if sessCtx.GetSessionVars().StmtCtx.TimeZone() == nil {
-		tz := *time.UTC
-		sessCtx.GetSessionVars().StmtCtx.SetTimeZone(&tz)
-	}
-	sessCtx.GetSessionVars().StmtCtx.IsDDLJobInQueue = true
-=======
 	// Correct the initial timezone.
 	tz := *time.UTC
 	sessCtx.GetSessionVars().TimeZone = &tz
 	sessCtx.GetSessionVars().StmtCtx.SetTimeZone(&tz)
 
->>>>>>> 1cf28267277 (ddl: fix add wrong timezone during adding index (#48805))
 	// Set the row encode format version.
 	rowFormat := variable.GetDDLReorgRowFormat()
 	sessCtx.GetSessionVars().RowEncoder.Enable = rowFormat != variable.DefTiDBRowFormatV1
