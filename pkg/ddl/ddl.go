5ec953d6226bf7b76f11434408ae39cbb06e1a07
tiancaiamao <tiancaiamao@gmail.com>
2023-11-24 19:25:14 +0800

    This is an automated cherry-pick of #48870
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/ddl/ddl.go
+++ pkg/ddl/ddl.go
@@ -47,6 +47,7 @@ import (
 	"github.com/pingcap/tidb/pkg/infoschema"
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/meta"
+	"github.com/pingcap/tidb/pkg/meta/autoid"
 	"github.com/pingcap/tidb/pkg/metrics"
 	"github.com/pingcap/tidb/pkg/owner"
 	"github.com/pingcap/tidb/pkg/parser/ast"
@@ -355,8 +356,12 @@ type ddlCtx struct {
 	statsHandle  *handle.Handle
 	tableLockCkr util.DeadTableLockChecker
 	etcdCli      *clientv3.Client
+<<<<<<< HEAD
 	// backfillJobCh gets notification if any backfill jobs coming.
 	backfillJobCh chan struct{}
+=======
+	autoidCli    *autoid.ClientDiscover
+>>>>>>> 8eb191303ac (*: fix grpc client leak bug for AUTO_ID_CACHE=1 tables (#48870))
 
 	*waitSchemaSyncedController
 	*schemaVersionManager
@@ -672,6 +677,7 @@ func newDDL(ctx context.Context, options ...Option) *ddl {
 		infoCache:                  opt.InfoCache,
 		tableLockCkr:               deadLockCkr,
 		etcdCli:                    opt.EtcdCli,
+		autoidCli:                  opt.AutoIDClient,
 		schemaVersionManager:       newSchemaVersionManager(),
 		waitSchemaSyncedController: newWaitSchemaSyncedController(),
 		runningJobIDs:              make([]string, 0, jobRecordCapacity),
