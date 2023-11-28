570f7349aeb8066d2350246f4b513256ddae151c
tangenta <tangenta@126.com>
2023-11-28 12:27:36 +0800

    init version of distributed lock

---
+++ pkg/meta/distributed_lock.go
@@ -0,0 +1,257 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package meta
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/pkg/parser/model"
+	"github.com/pingcap/tidb/pkg/util/intest"
+	"github.com/pingcap/tidb/pkg/util/logutil"
+	"go.uber.org/zap"
+)
+
+type dataStore interface {
+	begin() (startTS uint64, err error)
+	rollback()
+	commit() error
+
+	put(key, value []byte) error
+	get(key []byte) ([]byte, error)
+	del(key []byte) error
+}
+
+type lockContent struct {
+	StartTS    uint64 `json:"start_ts"`
+	InstanceID string `json:"instance_id"`
+}
+
+func newLockContent(startTS uint64, instanceID string) lockContent {
+	return lockContent{
+		StartTS:    startTS,
+		InstanceID: instanceID,
+	}
+}
+
+func (c lockContent) encode() ([]byte, error) {
+	return json.Marshal(c)
+}
+
+func (c *lockContent) decode(data []byte) error {
+	return json.Unmarshal(data, c)
+}
+
+type DistributedLock struct {
+	ctx        context.Context
+	cancel     context.CancelFunc
+	store      dataStore
+	instanceID string
+
+	lockName []byte
+	backoff  time.Duration
+	lease    time.Duration
+}
+
+func NewDistributedLock(ctx context.Context, store dataStore, instanceID string, lockName string) *DistributedLock {
+	ctx = logutil.WithCategory(ctx, "distributed-lock")
+	ctx, cancel := context.WithCancel(ctx)
+	return &DistributedLock{
+		ctx:        ctx,
+		cancel:     cancel,
+		store:      store,
+		instanceID: instanceID,
+
+		lockName: []byte(lockName),
+		backoff:  3 * time.Second,
+		lease:    30 * time.Second,
+	}
+}
+
+func (d *DistributedLock) SetBackoff(backoff time.Duration) *DistributedLock {
+	d.backoff = backoff
+	return d
+}
+
+func (d *DistributedLock) SetLease(lease time.Duration) *DistributedLock {
+	d.lease = lease
+	return d
+}
+
+func (d *DistributedLock) renewLockLoop() {
+	ticker := time.NewTicker(d.lease / 3)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-d.ctx.Done():
+			return
+		case <-ticker.C:
+			err := d.renew()
+			if err != nil {
+				logutil.BgLogger().Warn("renew lock failed", zap.Error(err))
+				return
+			}
+		}
+	}
+}
+
+func (d *DistributedLock) renew() error {
+	return d.getLockInTxnWithRetry(d.store,
+		func(lc *lockContent, startTS uint64) (bool, error) {
+			if lc == nil || lc.InstanceID != d.instanceID {
+				logutil.Logger(d.ctx).Warn("lock is unexpectedly removed", zap.String("instanceID", d.instanceID))
+				return false, nil
+			}
+
+			// Lock is exist, update it.
+			lc.StartTS = startTS
+			data, err := lc.encode()
+			if err != nil {
+				return false, errors.Trace(err)
+			}
+			err = d.store.put(d.lockName, data)
+			if err != nil {
+				return true, errors.Trace(err)
+			}
+			return false, nil
+		})
+}
+
+func (d *DistributedLock) Lock() error {
+	return d.getLockInTxnWithRetry(d.store,
+		func(lc *lockContent, startTS uint64) (bool, error) {
+			// Lock is not exist or expired, create or overwrite it.
+			if lc == nil || d.lockExpired(lc, startTS) {
+				lc := newLockContent(startTS, d.instanceID)
+				data, err := lc.encode()
+				if err != nil {
+					return false, errors.Trace(err)
+				}
+				err = d.store.put(d.lockName, data)
+				if err != nil {
+					return true, errors.Trace(err)
+				}
+				if !intest.InTest {
+					go d.renewLockLoop()
+				}
+				return false, nil
+			}
+			// Lock is valid, wait for a while.
+			return true, nil
+		})
+}
+
+func (d *DistributedLock) lockExpired(lc *lockContent, currentStartTS uint64) bool {
+	lockStartTime := model.TSConvert2Time(lc.StartTS)
+	currentTime := model.TSConvert2Time(currentStartTS)
+	expired := currentTime.Sub(lockStartTime) > d.lease
+	if expired {
+		logutil.Logger(d.ctx).Warn("lock is expired",
+			zap.Time("lockStartTime", lockStartTime),
+			zap.Time("currentTime", currentTime),
+			zap.Duration("lease", d.lease))
+	}
+	return expired
+}
+
+func (d *DistributedLock) Unlock() error {
+	defer d.cancel()
+	return d.getLockInTxnWithRetry(d.store,
+		func(lc *lockContent, startTS uint64) (bool, error) {
+			if lc == nil || lc.InstanceID != d.instanceID {
+				logutil.Logger(d.ctx).Warn("lock is unexpectedly removed", zap.String("instanceID", d.instanceID))
+				return false, nil
+			}
+
+			// Lock is exist, remove it.
+			err := d.store.del(d.lockName)
+			if err != nil {
+				return true, errors.Trace(err)
+			}
+			return false, nil
+		})
+}
+
+func (d *DistributedLock) getLockInTxnWithRetry(store dataStore,
+	fn func(lc *lockContent, startTS uint64) (retry bool, err error)) error {
+	for {
+		if contextIsDone(d.ctx) {
+			return d.ctx.Err()
+		}
+
+		var noRetry bool
+		err := runInTxn(d.store, func(startTS uint64) error {
+			data, err := d.store.get(d.lockName)
+			if err != nil {
+				return errors.Trace(err)
+			}
+
+			lc, err := decodeLockContent(data)
+			if err != nil {
+				noRetry = true
+				return errors.Trace(err)
+			}
+
+			needRetry, err := fn(lc, startTS)
+			noRetry = !needRetry
+			return err
+		})
+		if err != nil {
+			logutil.Logger(d.ctx).Warn("lock encounter error",
+				zap.Error(err), zap.Bool("retry", !noRetry))
+		}
+		if noRetry {
+			return err
+		}
+		time.Sleep(d.backoff)
+	}
+}
+
+func contextIsDone(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+func runInTxn(store dataStore, fn func(startTS uint64) error) error {
+	startTS, err := store.begin()
+	if err != nil {
+		return err
+	}
+	err = fn(startTS)
+	if err != nil {
+		store.rollback()
+		return err
+	}
+	return store.commit()
+}
+
+func decodeLockContent(data []byte) (*lockContent, error) {
+	if len(data) == 0 {
+		return nil, nil
+	}
+	var lc lockContent
+	err := lc.decode(data)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return &lc, nil
+}
