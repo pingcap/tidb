f91f64e67c40b90260eef4959b1ba964a21a2fc2
Jason Mo <mohangjie1995@gmail.com>
2023-12-06 13:46:02 +0800

    update planner/core/casetest/rule/rule_join_reorder, move 2 test cases

--- pkg/planner/core/casetest/rule/rule_join_reorder_test.go
+++ pkg/planner/core/casetest/rule/rule_join_reorder_test.go
@@ -17,7 +17,6 @@ package rule
 import (
 	"testing"
 
-	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/domain"
 	"github.com/pingcap/tidb/pkg/parser/model"
 	"github.com/pingcap/tidb/pkg/testkit"
@@ -44,19 +43,6 @@ func runJoinReorderTestData(t *testing.T, tk *testkit.TestKit, name string) {
 		tk.MustQuery("explain format = 'brief' " + input[i]).Check(testkit.Rows(output[i].Plan...))
 		tk.MustQuery("show warnings").Check(testkit.Rows(output[i].Warning...))
 	}
-}
-
-// test the global/session variable tidb_opt_enable_hash_join being set to no
-func TestOptEnableHashJoin(t *testing.T) {
-	store := testkit.CreateMockStore(t)
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("set tidb_opt_enable_hash_join=off")
-	tk.MustExec("create table t1(a int, b int, key(a));")
-	tk.MustExec("create table t2(a int, b int, key(a));")
-	tk.MustExec("create table t3(a int, b int, key(a));")
-	tk.MustExec("create table t4(a int, b int, key(a));")
-	runJoinReorderTestData(t, tk, "TestOptEnableHashJoin")
 }
 
 func TestJoinOrderHint4TiFlash(t *testing.T) {
@@ -90,25 +76,4 @@ func TestJoinOrderHint4TiFlash(t *testing.T) {
 
 	tk.MustExec("set @@tidb_allow_mpp=1; set @@tidb_enforce_mpp=1;")
 	runJoinReorderTestData(t, tk, "TestJoinOrderHint4TiFlash")
-}
-
-func TestJoinOrderHint4DynamicPartitionTable(t *testing.T) {
-	failpoint.Enable("github.com/pingcap/tidb/pkg/planner/core/forceDynamicPrune", `return(true)`)
-	defer failpoint.Disable("github.com/pingcap/tidb/pkg/planner/core/forceDynamicPrune")
-	store := testkit.CreateMockStore(t)
-
-	tk := testkit.NewTestKit(t, store)
-	tk.MustExec("use test")
-	tk.MustExec("drop table if exists t, t1, t2, t3;")
-	tk.MustExec(`create table t(a int, b int) partition by hash(a) partitions 3`)
-	tk.MustExec(`create table t1(a int, b int) partition by hash(a) partitions 4`)
-	tk.MustExec(`create table t2(a int, b int) partition by hash(a) partitions 5`)
-	tk.MustExec(`create table t3(a int, b int) partition by hash(b) partitions 3`)
-	tk.MustExec(`create table t4(a int, b int) partition by hash(a) partitions 4`)
-	tk.MustExec(`create table t5(a int, b int) partition by hash(a) partitions 5`)
-	tk.MustExec(`create table t6(a int, b int) partition by hash(b) partitions 3`)
-
-	tk.MustExec(`set @@tidb_partition_prune_mode="dynamic"`)
-	tk.MustExec("set @@tidb_enable_outer_join_reorder=true")
-	runJoinReorderTestData(t, tk, "TestJoinOrderHint4DynamicPartitionTable")
 }
