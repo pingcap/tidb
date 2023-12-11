51b2aff2e1c62d619bc0f51f7b39ff711476fcce
lyzx2001 <rep_laureline@hotmail.com>
2023-12-11 10:47:46 +0800

    add getChunkCompressedSizeForParquet

--- br/pkg/lightning/importer/table_import.go
+++ br/pkg/lightning/importer/table_import.go
@@ -1190,6 +1190,7 @@ func getChunkCompressedSizeForParquet(
 	}
 	parser, err := mydump.NewParquetParser(ctx, store, reader, chunk.FileMeta.Path)
 	if err != nil {
+		_ = parser.Close()
 		return 0, errors.Trace(err)
 	}
 	//nolint: errcheck
