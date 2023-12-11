4f2fc9dba8cb55cc3e981435654e22befb5a0042
Weizhen Wang <wangweizhen@pingcap.com>
2023-12-11 12:56:23 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/histogram.go
+++ pkg/statistics/histogram.go
@@ -1384,7 +1384,6 @@ func mergePartitionBuckets(sc *stmtctx.StatementContext, buckets []*bucket4Mergi
 		if compare == 0 {
 			res.Repeat += buckets[i].Repeat
 		}
-
 		if i != len(buckets)-1 {
 			tmp, err := mergeBucketNDV(sc, buckets[i], &right)
 			if err != nil {
