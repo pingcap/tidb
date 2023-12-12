a6f5627abd7e31ea4a4bbbd634ac0ace61ccc420
Arenatlx <314806019@qq.com>
2023-12-12 22:37:50 +0800

    This is an automated cherry-pick of #49338
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- planner/core/enforce_mpp_test.go
+++ planner/core/enforce_mpp_test.go
@@ -95,7 +95,12 @@ func TestEnforceMPP(t *testing.T) {
 	tk.MustExec("drop table if exists t")
 	tk.MustExec("create table t(a int, b int)")
 	tk.MustExec("create index idx on t(a)")
+<<<<<<< HEAD:planner/core/enforce_mpp_test.go
 	tk.MustExec("CREATE TABLE `s` (\n `a` int(11) DEFAULT NULL,\n `b` int(11) DEFAULT NULL,\n `c` int(11) DEFAULT NULL,\n `d` int(11) DEFAULT NULL,\n UNIQUE KEY `a` (`a`),\n KEY `ii` (`a`,`b`)\n)")
+=======
+	tk.MustExec("CREATE TABLE `s` (\n  `a` int(11) DEFAULT NULL,\n  `b` int(11) DEFAULT NULL,\n  `c` int(11) DEFAULT NULL,\n  `d` int(11) DEFAULT NULL,\n  UNIQUE KEY `a` (`a`),\n  KEY `ii` (`a`,`b`)\n)")
+	tk.MustExec("create table t3(id int, sala char(10), name char(100), primary key(id, sala)) partition by list columns (sala)(partition p1 values in('a'));")
+>>>>>>> 7c580823248 (planner: fix tiflash path will be identified as table dual (#49338)):pkg/planner/core/casetest/enforcempp/enforce_mpp_test.go
 
 	// Default RPC encoding may cause statistics explain result differ and then the test unstable.
 	tk.MustExec("set @@tidb_enable_chunk_rpc = on")
@@ -113,6 +118,12 @@ func TestEnforceMPP(t *testing.T) {
 			}
 		}
 		if tblInfo.Name.L == "s" {
+			tblInfo.TiFlashReplica = &model.TiFlashReplicaInfo{
+				Count:     1,
+				Available: true,
+			}
+		}
+		if tblInfo.Name.L == "t3" {
 			tblInfo.TiFlashReplica = &model.TiFlashReplicaInfo{
 				Count:     1,
 				Available: true,
