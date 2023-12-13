e18b468447794e9b7013d630169026f91d522d0c
lyzx2001 <rep_laureline@hotmail.com>
2023-12-13 14:26:16 +0800

    update

--- br/pkg/lightning/restore/table_restore.go
+++ br/pkg/lightning/restore/table_restore.go
@@ -588,8 +588,9 @@ func (tr *TableRestore) restoreEngine(
 
 		if chunk.FileMeta.Type == mydump.SourceTypeParquet {
 			// TODO: use the compressed size of the chunk to conduct memory control
-			_, err = getChunkCompressedSizeForParquet(ctx, chunk, rc.store)
-			return nil, errors.Trace(err)
+			if _, err = getChunkCompressedSizeForParquet(ctx, chunk, rc.store); err != nil {
+				return nil, errors.Trace(err)
+			}
 		}
 
 		restoreWorker := rc.regionWorkers.Apply()
