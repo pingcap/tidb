4b92d1d5bc1efd7f374085c0f9c479584a3b005a
ywqzzy <592838129@qq.com>
2023-12-13 13:18:30 +0800

    fix hang

--- pkg/disttask/framework/framework_test.go
+++ pkg/disttask/framework/framework_test.go
@@ -15,6 +15,7 @@
 package framework_test
 
 import (
+	"runtime"
 	"testing"
 	"time"
 
@@ -41,6 +42,13 @@ func TestFrameworkBasic(t *testing.T) {
 	distContext.SetOwner(1)
 	time.Sleep(2 * time.Second) // make sure owner changed
 	testutil.DispatchTaskAndCheckSuccess(ctx, t, "key5", testContext, nil)
+
+	val := runtime.GOMAXPROCS(1)
+	defer func() {
+		runtime.GOMAXPROCS(val)
+	}()
+
+	testutil.DispatchTaskAndCheckSuccess(ctx, t, "key6", testContext, nil)
 	distContext.Close()
 }
 
