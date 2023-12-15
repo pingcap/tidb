b57f66224ec96fa273c30e9b3e4f767c861e4fca
tiancaiamao <tiancaiamao@gmail.com>
2023-12-15 12:15:21 +0800

    executor: set OverflowAsWarning for insert statement in non-strict sql mode (#49383) (#49484)
    close pingcap/tidb#49369

--- executor/issuetest/executor_issue_test.go
+++ executor/issuetest/executor_issue_test.go
@@ -1461,3 +1461,19 @@ func TestIssue42662(t *testing.T) {
 	require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/executor/issue42662_1"))
 	require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/util/servermemorylimit/issue42662_2"))
 }
+
+func TestIssue49369(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("drop table if exists issue49369;")
+	tk.MustExec("CREATE TABLE `issue49369` (\n" +
+		"`x` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL\n" +
+		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;")
+	err := tk.ExecToErr("insert into issue49369   select round(cast('88888899999999999888888888888888888888888888888888888.11111111111111111111' as decimal(18,12)) * cast('88888899999999999888888888888888888888888888888888888.11111111111111111111' as decimal(42,18)) );")
+	require.EqualError(t, err, "[types:1690]DECIMAL value is out of range in '(18, 12)'")
+	tk.MustExec("set @@sql_mode = ''")
+	tk.MustExec("insert into issue49369   select round(cast('88888899999999999888888888888888888888888888888888888.11111111111111111111' as decimal(18,12)) * cast('88888899999999999888888888888888888888888888888888888.11111111111111111111' as decimal(42,18)) );")
+	tk.MustQuery("select * from issue49369").Check(testkit.Rows("999999999999999999000000000000"))
+	tk.MustExec("set @@sql_mode = default")
+}
