0b1d3647d158dcd9c0e0c03a3cf138c1c4f1329d
D3Hunter <jujj603@gmail.com>
2023-12-18 14:23:02 +0800

    lint

--- pkg/executor/import_into.go
+++ pkg/executor/import_into.go
@@ -274,6 +274,7 @@ func (e *ImportIntoExec) importFromSelect(ctx context.Context) error {
 	if err2 != nil {
 		return err2
 	}
+	//nolint: errcheck
 	defer ti.Close()
 	selectedRowCh := make(chan importer.QueryRow)
 	ti.SetSelectedRowCh(selectedRowCh)
