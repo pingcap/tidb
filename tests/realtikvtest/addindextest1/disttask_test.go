6840773b6f0d7705f3ca6c5998ded52f91e6d342
tangenta <tangenta@126.com>
2023-12-18 21:37:09 +0800

    dump on timeout

--- tests/realtikvtest/addindextest1/disttask_test.go
+++ tests/realtikvtest/addindextest1/disttask_test.go
@@ -17,7 +17,9 @@ package addindextest
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/config"
@@ -46,6 +48,15 @@ func init() {
 }
 
 func TestAddIndexDistBasic(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	dumpChan := make(chan struct{})
+	defer func() {
+		close(dumpChan)
+		wg.Wait()
+	}()
+	go testkit.DebugDumpOnTimeout(&wg, dumpChan, 7*time.Minute)
+
 	// mock that we only have 1 cpu, add-index task can be scheduled as usual
 	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/util/cpu/mockNumCpu", `return(1)`))
 	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/disttask/framework/storage/testSetLastTaskID", `return(true)`))
