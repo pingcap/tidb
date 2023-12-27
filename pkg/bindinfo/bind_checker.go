1f062026cf244ddb69a30c48e54f22d24b2abd1d
Weizhen Wang <wangweizhen@pingcap.com>
2023-12-27 16:22:04 +0800

    bindinfo: get all table name from ast
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ pkg/bindinfo/bind_checker.go
@@ -0,0 +1,67 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bindinfo
+
+import (
+	"sync"
+
+	"github.com/pingcap/tidb/pkg/parser/ast"
+)
+
+// GetAllTableName gets all table names from ast.Node.
+func GetAllTableName(in ast.Node) []*ast.TableName {
+	collector := collectTableNamePool.Get().(*collectTableName)
+	defer collector.DestroyAndPutToPool()
+	in.Accept(collector)
+	return collector.GetResult()
+}
+
+var collectTableNamePool = sync.Pool{
+	New: func() any {
+		return newCollectTableName()
+	},
+}
+
+type collectTableName struct {
+	tableNames []*ast.TableName
+}
+
+func newCollectTableName() *collectTableName {
+	return &collectTableName{
+		tableNames: make([]*ast.TableName, 0, 4),
+	}
+}
+
+// Enter implements Visitor interface.
+func (c *collectTableName) Enter(in ast.Node) (out ast.Node, skipChildren bool) {
+	if node, ok := in.(*ast.TableName); ok {
+		c.tableNames = append(c.tableNames, node)
+	}
+	return in, false
+}
+
+// Leave implements Visitor interface.
+func (c *collectTableName) Leave(in ast.Node) (out ast.Node, ok bool) {
+	return in, true
+}
+
+func (c *collectTableName) GetResult() []*ast.TableName {
+	return c.tableNames
+}
+
+func (c *collectTableName) DestroyAndPutToPool() {
+	clear(c.tableNames)
+	collectTableNamePool.Put(c)
+}
