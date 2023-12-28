767ec62a054c84afd26d45901ed30fb037281c9e
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-12-28 11:21:29 +0800

    brie: support batch ddl for sql restore (#49089) (#49851)
    close pingcap/tidb#48301

--- executor/insert_common.go
+++ executor/insert_common.go
@@ -1339,6 +1339,12 @@ func (e *InsertValues) addRecordWithAutoIDHint(ctx context.Context, row []types.
 	return nil
 }
 
+// CreateSession will be assigned by session package.
+var CreateSession func(ctx sessionctx.Context) (sessionctx.Context, error)
+
+// CloseSession will be assigned by session package.
+var CloseSession func(ctx sessionctx.Context)
+
 // InsertRuntimeStat record the stat about insert and check
 type InsertRuntimeStat struct {
 	*execdetails.BasicRuntimeStats
