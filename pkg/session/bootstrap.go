4f170b9b310f29322654483ba40cdcb38dc51aee
Hangjie Mo <mohangjie1995@gmail.com>
2023-12-29 15:10:05 +0800

    Update bootstrap.go

--- pkg/session/bootstrap.go
+++ pkg/session/bootstrap.go
@@ -1049,7 +1049,7 @@ const (
 
 // currentBootstrapVersion is defined as a variable, so we can modify its value for testing.
 // please make sure this is the largest version
-var currentBootstrapVersion int64 = version181
+var currentBootstrapVersion int64 = version182
 
 // DDL owner key's expired time is ManagerSessionTTL seconds, we should wait the time and give more time to have a chance to finish it.
 var internalSQLTimeout = owner.ManagerSessionTTL + 15
@@ -1206,6 +1206,7 @@ var (
 		upgradeToVer179,
 		upgradeToVer180,
 		upgradeToVer181,
+		upgradeToVer182,
 	}
 )
 
