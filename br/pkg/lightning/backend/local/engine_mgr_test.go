fd3c6b41a5a0648cd2152988cf49bb48b7a6c715
D3Hunter <jujj603@gmail.com>
2023-12-29 18:31:11 +0800

    test

--- br/pkg/lightning/backend/local/engine_mgr_test.go
+++ br/pkg/lightning/backend/local/engine_mgr_test.go
@@ -15,12 +15,94 @@
 package local
 
 import (
+	"context"
+	"io"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/pingcap/tidb/br/pkg/lightning/backend"
 	"github.com/pingcap/tidb/br/pkg/lightning/common"
+	"github.com/pingcap/tidb/br/pkg/lightning/config"
+	"github.com/pingcap/tidb/br/pkg/lightning/log"
+	"github.com/pingcap/tidb/br/pkg/mock/mocklocal"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
+
+func getBackendConfig(t *testing.T) BackendConfig {
+	return BackendConfig{
+		MemTableSize:                config.DefaultEngineMemCacheSize,
+		MaxOpenFiles:                1000,
+		DisableAutomaticCompactions: true,
+		LocalStoreDir:               t.TempDir(),
+		DupeDetectEnabled:           false,
+		DuplicateDetectOpt:          common.DupDetectOpt{},
+		WorkerConcurrency:           8,
+		LocalWriterMemCacheSize:     config.DefaultLocalWriterMemCacheSize,
+		CheckpointEnabled:           false,
+	}
+}
+
+func TestEngineManager(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	storeHelper := mocklocal.NewMockStoreHelper(ctrl)
+	ctx := context.Background()
+	syncMapLen := func(m *sync.Map) int {
+		count := 0
+		m.Range(func(key, value interface{}) bool {
+			count++
+			return true
+		})
+		return count
+	}
+	isEmptyDir := func(name string) bool {
+		f, err := os.Open(name)
+		require.NoError(t, err)
+		defer f.Close()
+		_, err = f.Readdirnames(1) // Or f.Readdir(1)
+		if err == io.EOF {
+			return true
+		}
+		require.Fail(t, err.Error())
+		return false
+	}
+
+	backendConfig := getBackendConfig(t)
+	em, err := newEngineManager(backendConfig, storeHelper, log.L())
+	require.NoError(t, err)
+	storeHelper.EXPECT().GetTSO(gomock.Any()).Return(int64(0), int64(0), nil)
+	engine1ID := uuid.New()
+	require.NoError(t, em.openEngine(ctx, &backend.EngineConfig{}, engine1ID))
+	require.Equal(t, 1, syncMapLen(&em.engines))
+	_, ok := em.engines.Load(engine1ID)
+	require.True(t, ok)
+	require.True(t, ctrl.Satisfied())
+	require.Len(t, em.engineFileSizes(), 1)
+
+	require.NoError(t, em.closeEngine(ctx, &backend.EngineConfig{}, engine1ID))
+	require.Equal(t, 0, int(em.getImportedKVCount(engine1ID)))
+	// close non-existent engine
+	require.ErrorContains(t, em.closeEngine(ctx, &backend.EngineConfig{}, uuid.New()), "no such file or directory")
+
+	// reset non-existent engine should work
+	require.NoError(t, em.resetEngine(ctx, uuid.New()))
+	storeHelper.EXPECT().GetTSO(gomock.Any()).Return(int64(0), int64(0), nil)
+	require.NoError(t, em.resetEngine(ctx, engine1ID))
+	require.Equal(t, 1, syncMapLen(&em.engines))
+	_, ok = em.engines.Load(engine1ID)
+	require.True(t, ok)
+	require.True(t, ctrl.Satisfied())
+
+	require.NoError(t, em.cleanupEngine(ctx, uuid.New()))
+	require.NoError(t, em.cleanupEngine(ctx, engine1ID))
+	require.Equal(t, 0, syncMapLen(&em.engines))
+
+	require.True(t, isEmptyDir(backendConfig.LocalStoreDir))
+	em.close()
+}
 
 func TestGetExternalEngineKVStatistics(t *testing.T) {
 	em := &engineManager{
