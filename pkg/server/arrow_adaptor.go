c747b801c79ff3100325b53b3767443ddba69e8f
Michiel De Backker <mail@backkem.me>
2024-01-03 17:05:22 +0100

    Apache Arrow Flight POC

---
+++ pkg/server/arrow_adaptor.go
@@ -0,0 +1,164 @@
+package server
+
+import (
+	"context"
+	"errors"
+
+	"github.com/apache/arrow/go/v14/arrow"
+	"github.com/apache/arrow/go/v14/arrow/array"
+	"github.com/apache/arrow/go/v14/arrow/memory"
+	"github.com/pingcap/tidb/pkg/parser/mysql"
+	"github.com/pingcap/tidb/pkg/parser/types"
+	"github.com/pingcap/tidb/pkg/planner/core/resolve"
+	"github.com/pingcap/tidb/pkg/util/chunk"
+	"github.com/pingcap/tidb/pkg/util/sqlexec"
+)
+
+// ResultSetRecordReader reads from a ResultSet and retuns
+// a simple RecordReader for each chunk.
+type ResultSetRecordReader struct {
+	set sqlexec.RecordSet
+
+	schema         *arrow.Schema
+	tidbAllocator  chunk.Allocator
+	arrowAllocator memory.Allocator
+	builder        *array.RecordBuilder
+	cur            *chunk.Chunk
+	err            error
+}
+
+func NewResultSetRecordReader(set sqlexec.RecordSet, tidbAllocator chunk.Allocator, arrowAllocator memory.Allocator) (*ResultSetRecordReader, error) {
+	schema, err := adaptSchema(set.Fields())
+	if err != nil {
+		return nil, err
+	}
+	if tidbAllocator == nil {
+		tidbAllocator = chunk.NewAllocator()
+	}
+	if arrowAllocator == nil {
+		arrowAllocator = memory.NewGoAllocator()
+	}
+	builder := array.NewRecordBuilder(arrowAllocator, schema)
+	return &ResultSetRecordReader{
+		set:            set,
+		schema:         schema,
+		tidbAllocator:  tidbAllocator,
+		arrowAllocator: arrowAllocator,
+		builder:        builder,
+	}, nil
+}
+
+func (r *ResultSetRecordReader) Retain() {
+	// NOP
+}
+
+func (r *ResultSetRecordReader) Release() {
+	r.cur.Reset()
+	r.cur = nil
+	r.err = r.set.Close()
+}
+
+func (r *ResultSetRecordReader) Schema() *arrow.Schema {
+	return r.schema
+}
+
+func (r *ResultSetRecordReader) Next() bool {
+	if r.cur == nil {
+		r.cur = r.set.NewChunk(r.tidbAllocator)
+	}
+
+	r.err = r.set.Next(context.Background(), r.cur)
+
+	return r.cur.NumRows() > 0
+}
+
+func (r *ResultSetRecordReader) Record() arrow.Record {
+	// Quick & dirty copy logic for MVP
+	// Should be rewritten to avoid double allocation
+	numCols := r.cur.NumCols()
+	numRows := r.cur.NumRows()
+
+	for colIdx := 0; colIdx < numCols; colIdx++ {
+		col := r.cur.Column(colIdx)
+		colInfo := r.set.Fields()[colIdx].Column
+		ft := colInfo.FieldType
+
+		dest := r.builder.Field(colIdx)
+		for rowIdx := 0; rowIdx < numRows; rowIdx++ {
+			switch ft.GetType() {
+			case mysql.TypeTiny, mysql.TypeShort, mysql.TypeInt24, mysql.TypeLong, mysql.TypeLonglong:
+				if mysql.HasUnsignedFlag(ft.GetFlag()) {
+					val := col.GetUint64(rowIdx)
+					dest.(*array.Uint64Builder).Append(val)
+				}
+				val := col.GetInt64(rowIdx)
+				dest.(*array.Int64Builder).Append(val)
+
+			case mysql.TypeFloat:
+				val := col.GetFloat32(rowIdx)
+				dest.(*array.Float32Builder).Append(val)
+
+			case mysql.TypeDouble:
+				val := col.GetFloat64(rowIdx)
+				dest.(*array.Float64Builder).Append(val)
+
+			case mysql.TypeVarString, mysql.TypeString:
+				val := col.GetString(rowIdx)
+				dest.(*array.StringBuilder).Append(val)
+			}
+		}
+	}
+
+	return r.builder.NewRecord()
+}
+
+func (r *ResultSetRecordReader) Err() error {
+	return r.err
+}
+
+func adaptSchema(fields []*resolve.ResultField) (*arrow.Schema, error) {
+	arrowFields := []arrow.Field{}
+
+	for _, field := range fields {
+		typ, err := adaptFieldType(&field.Column.FieldType)
+		if err != nil {
+			return nil, err
+		}
+		arrowField := arrow.Field{
+			Name:     field.Column.Name.String(),
+			Type:     typ,
+			Nullable: true,
+		}
+		arrowFields = append(arrowFields, arrowField)
+	}
+	schema := arrow.NewSchema(arrowFields, nil)
+
+	return schema, nil
+}
+
+func adaptFieldType(ft *types.FieldType) (arrow.DataType, error) {
+	switch ft.GetType() {
+	case mysql.TypeTiny, mysql.TypeShort, mysql.TypeInt24, mysql.TypeLong, mysql.TypeLonglong:
+		if mysql.HasUnsignedFlag(ft.GetFlag()) {
+			return arrow.PrimitiveTypes.Uint64, nil
+		}
+		return arrow.PrimitiveTypes.Int64, nil
+
+	case mysql.TypeFloat:
+		return arrow.PrimitiveTypes.Float32, nil
+
+	case mysql.TypeDouble:
+		return arrow.PrimitiveTypes.Float64, nil
+
+	// case mysql.TypeNewDecimal:
+	// 	return arrow.PrimitiveTypes.Float64, nil
+
+	// case mysql.TypeDate, mysql.TypeDatetime:
+	// 	return arrow.PrimitiveTypes.Date64, nil
+
+	case mysql.TypeVarString, mysql.TypeString:
+		return arrow.BinaryTypes.LargeString, nil
+	}
+
+	return nil, errors.ErrUnsupported
+}
---
+++ pkg/server/flightsql.go
@@ -0,0 +1,339 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/apache/arrow/go/v14/arrow"
+	"github.com/apache/arrow/go/v14/arrow/flight"
+	"github.com/apache/arrow/go/v14/arrow/flight/flightsql"
+	"github.com/apache/arrow/go/v14/arrow/memory"
+	tmysql "github.com/pingcap/tidb/pkg/parser/mysql"
+	"github.com/pingcap/tidb/pkg/util/chunk"
+	"github.com/pingcap/tidb/pkg/util/logutil"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/metadata"
+)
+
+type FlightSQLServer struct {
+	flightsql.BaseServer
+	server       *Server
+	flightServer flight.Server
+
+	tidbAllocator  chunk.Allocator
+	arrowAllocator memory.Allocator
+}
+
+func NewFlightSQLServer(server *Server) (*FlightSQLServer, error) {
+	ret := &FlightSQLServer{
+		server:         server,
+		tidbAllocator:  chunk.NewAllocator(),
+		arrowAllocator: memory.NewGoAllocator(),
+	}
+	ret.Alloc = memory.DefaultAllocator
+
+	return ret, nil
+}
+
+func (s *FlightSQLServer) Serve(l net.Listener) error {
+	server := flight.NewServerWithMiddleware([]flight.ServerMiddleware{
+		// TODO: Auth
+		// flight.CreateServerBasicAuthMiddleware(s),
+	})
+	server.RegisterFlightService(flightsql.NewFlightServer(s))
+	server.InitListener(l)
+
+	s.flightServer = server
+
+	return server.Serve()
+}
+
+func (s *FlightSQLServer) Shutdown() {
+	s.flightServer.Shutdown()
+}
+
+func (s *FlightSQLServer) GetFlightInfoStatement(ctx context.Context, cmd flightsql.StatementQuery, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	dbs := metadata.ValueFromIncomingContext(ctx, "database")
+	if len(dbs) != 1 {
+		return nil, errors.New("invalid database selection")
+	}
+
+	query, txnid := cmd.GetQuery(), cmd.GetTransactionId()
+	tkt, err := encodeTransactionQuery(query, txnid)
+	if err != nil {
+		return nil, err
+	}
+
+	return &flight.FlightInfo{
+		Endpoint:         []*flight.FlightEndpoint{{Ticket: &flight.Ticket{Ticket: tkt}}},
+		FlightDescriptor: desc,
+		TotalRecords:     -1,
+		TotalBytes:       -1,
+	}, nil
+}
+
+func (s *FlightSQLServer) GetFlightInfoSubstraitPlan(ctx context.Context, plan flightsql.StatementSubstraitPlan, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoSubstraitPlan not implemented")
+
+}
+
+func (s *FlightSQLServer) GetSchemaStatement(ctx context.Context, cmd flightsql.StatementQuery, desc *flight.FlightDescriptor) (*flight.SchemaResult, error) {
+	panic("GetSchemaStatement not implemented")
+
+}
+
+func (s *FlightSQLServer) GetSchemaSubstraitPlan(ctx context.Context, plan flightsql.StatementSubstraitPlan, desc *flight.FlightDescriptor) (*flight.SchemaResult, error) {
+	panic("GetSchemaSubstraitPlan not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetStatement(ctx context.Context, cmd flightsql.StatementQueryTicket) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	_, query, err := decodeTransactionQuery(cmd.GetStatementHandle())
+	if err != nil {
+		return nil, nil, err
+	}
+	logutil.BgLogger().Info("DoGetStatement", zap.String("query", query))
+
+	dbs := metadata.ValueFromIncomingContext(ctx, "database")
+	if len(dbs) != 1 {
+		return nil, nil, errors.New("invalid database selection")
+	}
+	dbName := dbs[0]
+
+	ct, err := s.server.driver.OpenCtx(uint64(0), 0, uint8(tmysql.DefaultCollationID), dbName, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	useStmt, err := ct.Parse(ctx, "use `"+dbName+"`")
+	if err != nil {
+		return nil, nil, err
+	}
+	_, err = ct.ExecuteStmt(ctx, useStmt[0])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	stmts, err := ct.Parse(ctx, query)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if len(stmts) != 1 {
+		return nil, nil, errors.New("run multiple statements is not supported")
+	}
+
+	rs, err := ct.Session.ExecuteStmt(ctx, stmts[0])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rdr, err := NewResultSetRecordReader(rs, s.tidbAllocator, s.arrowAllocator)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ch := make(chan flight.StreamChunk)
+	go flight.StreamChunksFromReader(rdr, ch)
+
+	return rdr.Schema(), ch, nil
+}
+
+func (s *FlightSQLServer) GetFlightInfoPreparedStatement(ctx context.Context, cmd flightsql.PreparedStatementQuery, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoPreparedStatement not implemented")
+
+}
+
+func (s *FlightSQLServer) GetSchemaPreparedStatement(ctx context.Context, cmd flightsql.PreparedStatementQuery, desc *flight.FlightDescriptor) (*flight.SchemaResult, error) {
+	panic("GetSchemaPreparedStatement not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetPreparedStatement(ctx context.Context, cmd flightsql.PreparedStatementQuery) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetPreparedStatement not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoCatalogs(ctx context.Context, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoCatalogs not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetCatalogs(ctx context.Context) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetCatalogs not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoXdbcTypeInfo(ctx context.Context, cmd flightsql.GetXdbcTypeInfo, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoXdbcTypeInfo not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetXdbcTypeInfo(ctx context.Context, cmd flightsql.GetXdbcTypeInfo) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetXdbcTypeInfo not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoSqlInfo(ctx context.Context, cmd flightsql.GetSqlInfo, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	return nil, errors.New("GetFlightInfoSqlInfo not implemented")
+}
+
+func (s *FlightSQLServer) DoGetSqlInfo(ctx context.Context, cmd flightsql.GetSqlInfo) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetSqlInfo not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoSchemas(ctx context.Context, cmd flightsql.GetDBSchemas, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoSchemas not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetDBSchemas(ctx context.Context, cmd flightsql.GetDBSchemas) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetDBSchemas not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoTables(ctx context.Context, cmd flightsql.GetTables, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoTables not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetTables(ctx context.Context, cmd flightsql.GetTables) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetTables not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoTableTypes(ctx context.Context, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoTableTypes not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetTableTypes(ctx context.Context) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetTableTypes not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoPrimaryKeys(ctx context.Context, ref flightsql.TableRef, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoPrimaryKeys not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetPrimaryKeys(ctx context.Context, ref flightsql.TableRef) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetPrimaryKeys not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoExportedKeys(ctx context.Context, ref flightsql.TableRef, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoExportedKeys not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetExportedKeys(ctx context.Context, ref flightsql.TableRef) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetExportedKeys not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoImportedKeys(ctx context.Context, ref flightsql.TableRef, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoImportedKeys not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetImportedKeys(ctx context.Context, ref flightsql.TableRef) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetImportedKeys not implemented")
+
+}
+
+func (s *FlightSQLServer) GetFlightInfoCrossReference(ctx context.Context, ref flightsql.CrossTableRef, desc *flight.FlightDescriptor) (*flight.FlightInfo, error) {
+	panic("GetFlightInfoCrossReference not implemented")
+
+}
+
+func (s *FlightSQLServer) DoGetCrossReference(ctx context.Context, ref flightsql.CrossTableRef) (*arrow.Schema, <-chan flight.StreamChunk, error) {
+	panic("DoGetCrossReference not implemented")
+
+}
+
+func (s *FlightSQLServer) DoPutCommandStatementUpdate(ctx context.Context, cmd flightsql.StatementUpdate) (int64, error) {
+	panic("DoPutCommandStatementUpdate not implemented")
+
+}
+
+func (s *FlightSQLServer) DoPutCommandSubstraitPlan(ctx context.Context, plan flightsql.StatementSubstraitPlan) (int64, error) {
+	panic("DoPutCommandSubstraitPlan not implemented")
+
+}
+
+func (s *FlightSQLServer) CreatePreparedStatement(ctx context.Context, cmd flightsql.ActionCreatePreparedStatementRequest) (flightsql.ActionCreatePreparedStatementResult, error) {
+	panic("CreatePreparedStatement not implemented")
+
+}
+
+func (s *FlightSQLServer) CreatePreparedSubstraitPlan(ctx context.Context, cmd flightsql.ActionCreatePreparedSubstraitPlanRequest) (flightsql.ActionCreatePreparedStatementResult, error) {
+	panic("CreatePreparedSubstraitPlan not implemented")
+
+}
+
+func (s *FlightSQLServer) ClosePreparedStatement(ctx context.Context, cmd flightsql.ActionClosePreparedStatementRequest) error {
+	panic("ClosePreparedStatement not implemented")
+
+}
+
+func (s *FlightSQLServer) DoPutPreparedStatementQuery(ctx context.Context, cmd flightsql.PreparedStatementQuery, r flight.MessageReader, w flight.MetadataWriter) error {
+	panic("DoPutPreparedStatementQuery not implemented")
+
+}
+
+func (s *FlightSQLServer) DoPutPreparedStatementUpdate(ctx context.Context, cmd flightsql.PreparedStatementUpdate, r flight.MessageReader) (int64, error) {
+	panic("DoPutPreparedStatementUpdate not implemented")
+
+}
+
+func (s *FlightSQLServer) BeginTransaction(ctx context.Context, cmd flightsql.ActionBeginTransactionRequest) (id []byte, err error) {
+	panic("BeginTransaction not implemented")
+
+}
+
+func (s *FlightSQLServer) BeginSavepoint(ctx context.Context, cmd flightsql.ActionBeginSavepointRequest) (id []byte, err error) {
+	panic("BeginSavepoint not implemented")
+
+}
+
+func (s *FlightSQLServer) EndSavepoint(ctx context.Context, cmd flightsql.ActionEndSavepointRequest) error {
+	panic("EndSavepoint not implemented")
+
+}
+
+func (s *FlightSQLServer) EndTransaction(ctx context.Context, cmd flightsql.ActionEndTransactionRequest) error {
+	panic("EndTransaction not implemented")
+
+}
+
+func (s *FlightSQLServer) CancelFlightInfo(ctx context.Context, cmd *flight.CancelFlightInfoRequest) (flight.CancelFlightInfoResult, error) {
+	panic("CancelFlightInfo not implemented")
+
+}
+
+func (s *FlightSQLServer) RenewFlightEndpoint(ctx context.Context, cmd *flight.RenewFlightEndpointRequest) (*flight.FlightEndpoint, error) {
+	panic("RenewFlightEndpoint not implemented")
+
+}
+
+func encodeTransactionQuery(query string, transactionID flightsql.Transaction) ([]byte, error) {
+	return flightsql.CreateStatementQueryTicket(
+		bytes.Join([][]byte{transactionID, []byte(query)}, []byte(":")))
+}
+
+func decodeTransactionQuery(ticket []byte) (txnID, query string, err error) {
+	id, queryBytes, found := bytes.Cut(ticket, []byte(":"))
+	if !found {
+		err = fmt.Errorf("%w: malformed ticket", arrow.ErrInvalid)
+		return
+	}
+
+	txnID = string(id)
+	query = string(queryBytes)
+	return
+}
