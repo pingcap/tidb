004c05f63ec5ab2722a42636049415fad292cbcd
rishabh_mittal <mittalrishabh@gmail.com>
2024-01-02 16:14:34 -0800

    fix build failure

--- br/pkg/lightning/backend/local/engine_mgr.go
+++ br/pkg/lightning/backend/local/engine_mgr.go
@@ -207,7 +207,7 @@ func (em *engineManager) openEngineDB(engineUUID uuid.UUID, readOnly bool) (*peb
 	opt.Levels = []pebble.LevelOptions{
 		{
 			TargetFileSize: 16 * units.GiB,
-			BlockSize:      int(local.BackendConfig.BlockSize),
+			BlockSize:      em.BlockSize,
 		},
 	}
 
