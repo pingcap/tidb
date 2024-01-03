e80385270c9111045859b2ab6a00db895074897f
tongjian <1045931706@qq.com>
2024-01-03 14:05:02 +0800

    metrics: add connection and fail metrics  by `resource group name` (#49424)
    ref pingcap/tidb#49318

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -2243,7 +2243,7 @@ func (s *session) ExecuteStmt(ctx context.Context, stmtNode ast.StmtNode) (sqlex
 	// current session is attached with a resource group.
 	resourceGroupName := s.GetSessionVars().StmtCtx.ResourceGroupName
 	if len(resourceGroupName) > 0 {
-		metrics.ResourceGroupQueryTotalCounter.WithLabelValues(resourceGroupName).Inc()
+		metrics.ResourceGroupQueryTotalCounter.WithLabelValues(resourceGroupName, resourceGroupName).Inc()
 	}
 
 	if err != nil {
