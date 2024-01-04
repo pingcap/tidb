1ddd08159d1dbea2f6c201a12eaafd4668fb2b1c
gmhdbjd <gmhdbjd@gmail.com>
2024-01-05 02:35:56 +0800

    fix ut

--- pkg/parser/model/ddl.go
+++ pkg/parser/model/ddl.go
@@ -461,6 +461,7 @@ func (sub *SubJob) ToProxyJob(parentJob *Job, seq int) Job {
 		Collate:         parentJob.Collate,
 		AdminOperator:   parentJob.AdminOperator,
 		TraceInfo:       parentJob.TraceInfo,
+		LocalMode:       parentJob.LocalMode,
 	}
 }
 
