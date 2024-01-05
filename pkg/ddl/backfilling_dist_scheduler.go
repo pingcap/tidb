678cb671e6397f142212d0c3d72754ab98fa4547
tangenta <tangenta@126.com>
2024-01-05 13:17:03 +0800

    ddl: skip sorting step when there are no subtasks (#49866)
    close pingcap/tidb#49770

--- pkg/ddl/backfilling_dist_scheduler.go
+++ pkg/ddl/backfilling_dist_scheduler.go
@@ -344,6 +344,10 @@ func generateGlobalSortIngestPlan(
 	if err != nil {
 		return nil, err
 	}
+	if len(startKeyFromSumm) == 0 && len(endKeyFromSumm) == 0 {
+		// Skip global sort for empty table.
+		return nil, nil
+	}
 	instanceIDs, err := scheduler.GenerateTaskExecutorNodes(ctx)
 	if err != nil {
 		return nil, err
