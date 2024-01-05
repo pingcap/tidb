d56328579ef8a4566081197b283b78d61931ae7d
D3Hunter <jujj603@gmail.com>
2024-01-05 11:28:01 +0800

    change

--- br/pkg/lightning/config/config.go
+++ br/pkg/lightning/config/config.go
@@ -332,6 +332,11 @@ func (l *Lightning) adjust(i *TikvImporter) {
 		if len(l.MetaSchemaName) == 0 {
 			l.MetaSchemaName = defaultMetaSchemaName
 		}
+		// RegionConcurrency > NumCPU is meaningless.
+		cpuCount := runtime.NumCPU()
+		if l.RegionConcurrency > cpuCount {
+			l.RegionConcurrency = cpuCount
+		}
 	}
 }
 
