6caac1cdf4f932d8e2f3f6fa9453f327d9e327f5
wjhuang2016 <huangwenjun1997@gmail.com>
2024-01-08 16:23:37 +0800

    fix
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- pkg/session/bootstrap.go
+++ pkg/session/bootstrap.go
@@ -1067,7 +1067,7 @@ const (
 
 // currentBootstrapVersion is defined as a variable, so we can modify its value for testing.
 // please make sure this is the largest version
-var currentBootstrapVersion int64 = version182
+var currentBootstrapVersion int64 = version183
 
 // DDL owner key's expired time is ManagerSessionTTL seconds, we should wait the time and give more time to have a chance to finish it.
 var internalSQLTimeout = owner.ManagerSessionTTL + 15
