8e7901ddaa1a1f9b96058269e58ff3fa10a86444
Weizhen Wang <wangweizhen@pingcap.com>
2024-01-11 16:11:09 +0800

    This is an automated cherry-pick of #50277
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/planner/core/planbuilder.go
+++ pkg/planner/core/planbuilder.go
@@ -533,7 +533,11 @@ type PlanBuilder struct {
 	colMapper map[*ast.ColumnNameExpr]int
 	// visitInfo is used for privilege check.
 	visitInfo     []visitInfo
+<<<<<<< HEAD
 	tableHintInfo []tableHintInfo
+=======
+	tableHintInfo []*hint.TableHintInfo
+>>>>>>> 0236944eab4 (planner: fix leading hint cannot take effect in UNION ALL statements (#50277))
 	// optFlag indicates the flags of the optimizer rules.
 	optFlag uint64
 	// capFlag indicates the capability flags.
