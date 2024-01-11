cf822533ef35f7754abb257a7dd0222a5b88c391
Weizhen Wang <wangweizhen@pingcap.com>
2024-01-11 17:22:46 +0800

    *
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/planbuilder.go
+++ pkg/planner/core/planbuilder.go
@@ -533,7 +533,7 @@ type PlanBuilder struct {
 	colMapper map[*ast.ColumnNameExpr]int
 	// visitInfo is used for privilege check.
 	visitInfo     []visitInfo
-	tableHintInfo []*hint.TableHintInfo
+	tableHintInfo []*tableHintInfo
 	// optFlag indicates the flags of the optimizer rules.
 	optFlag uint64
 	// capFlag indicates the capability flags.
