072af026e7a72039c05ec8fd265851f0e47fd931
qw4990 <zhangyuanjia@pingcap.com>
2024-01-17 10:50:13 +0800

    fixup

--- pkg/bindinfo/global_handle.go
+++ pkg/bindinfo/global_handle.go
@@ -122,9 +122,7 @@ type globalBindingHandle struct {
 	// This value is used to avoid reload duplicated bindings from storage.
 	lastUpdateTime atomic.Value
 
-	// invalidBindRecordMap indicates the invalid bind records found during querying.
-	// A record will be deleted from this map, after 2 bind-lease, after it is dropped from the kv.
-	invalidBindRecordMap tmpBindRecordMap
+	invalidBindings *invalidBindingCache
 }
 
 // Lease influences the duration of loading bind info and handling invalid bind.
@@ -200,13 +198,7 @@ func buildFuzzyDigestMap(bindRecords []Bindings) map[string][]string {
 // Reset is to reset the BindHandle and clean old info.
 func (h *globalBindingHandle) Reset() {
 	h.lastUpdateTime.Store(types.ZeroTimestamp)
-	h.invalidBindRecordMap.Value.Store(make(map[string]*bindRecordUpdate))
-	h.invalidBindRecordMap.flushFunc = func(binding Binding) error {
-		if _, err := h.dropGlobalBinding(binding.SQLDigest); err != nil {
-			return err
-		}
-		return nil
-	}
+	h.invalidBindings = newInvalidBindingCache()
 	h.setCache(newBindCache())
 	variable.RegisterStatistics(h)
 }
@@ -443,54 +435,38 @@ func lockBindInfoTable(sctx sessionctx.Context) error {
 	return err
 }
 
-// tmpBindRecordMap is used to temporarily save bind record changes.
-// Those changes will be flushed into store periodically.
-type tmpBindRecordMap struct {
-	sync.Mutex
-	atomic.Value
-	flushFunc func(binding Binding) error
+// invalidBindingCache is used to store invalid bindings temporarily.
+type invalidBindingCache struct {
+	mu sync.Mutex
+	m  map[string]Binding // key: sqlDigest
 }
 
-// flushToStore calls flushFunc for items in tmpBindRecordMap and removes them with a delay.
-func (tmpMap *tmpBindRecordMap) flushToStore() {
-	tmpMap.Lock()
-	defer tmpMap.Unlock()
-	newMap := copyBindRecordUpdateMap(tmpMap.Load().(map[string]*bindRecordUpdate))
-	for key, bindRecord := range newMap {
-		if bindRecord.updateTime.IsZero() {
-			err := tmpMap.flushFunc(bindRecord.binding)
-			if err != nil {
-				logutil.BgLogger().Debug("flush bind record failed", zap.String("category", "sql-bind"), zap.Error(err))
-			}
-			bindRecord.updateTime = time.Now()
-			continue
-		}
-
-		if time.Since(bindRecord.updateTime) > 6*time.Second {
-			delete(newMap, key)
-			updateMetrics(metrics.ScopeGlobal, []Binding{bindRecord.binding}, nil, false)
-		}
+func newInvalidBindingCache() *invalidBindingCache {
+	return &invalidBindingCache{
+		m: make(map[string]Binding),
 	}
-	tmpMap.Store(newMap)
 }
 
-// Add puts a Bindings into tmpBindRecordMap.
-func (tmpMap *tmpBindRecordMap) Add(binding Binding) {
-	key := binding.OriginalSQL + ":" + binding.Db + ":" + binding.ID
-	if _, ok := tmpMap.Load().(map[string]*bindRecordUpdate)[key]; ok {
-		return
+func (c *invalidBindingCache) add(binding Binding) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.m[binding.SQLDigest] = binding
+}
+
+func (c *invalidBindingCache) getAll() []Binding {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	bindings := make([]Binding, 0, len(c.m))
+	for _, binding := range c.m {
+		bindings = append(bindings, binding)
 	}
-	tmpMap.Lock()
-	defer tmpMap.Unlock()
-	if _, ok := tmpMap.Load().(map[string]*bindRecordUpdate)[key]; ok {
-		return
-	}
-	newMap := copyBindRecordUpdateMap(tmpMap.Load().(map[string]*bindRecordUpdate))
-	newMap[key] = &bindRecordUpdate{
-		binding: binding,
-	}
-	tmpMap.Store(newMap)
-	updateMetrics(metrics.ScopeGlobal, nil, []Binding{binding}, false)
+	return bindings
+}
+
+func (c *invalidBindingCache) reset() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.m = make(map[string]Binding)
 }
 
 // DropInvalidGlobalBinding executes the drop Bindings tasks.
@@ -500,12 +476,19 @@ func (h *globalBindingHandle) DropInvalidGlobalBinding() {
 			logutil.BgLogger().Warn("drop invalid global binding error", zap.Error(err))
 		}
 	}()
-	h.invalidBindRecordMap.flushToStore()
+
+	invalidBindings := h.invalidBindings.getAll()
+	h.invalidBindings.reset()
+	for _, invalidBinding := range invalidBindings {
+		if _, err := h.dropGlobalBinding(invalidBinding.SQLDigest); err != nil {
+			logutil.BgLogger().Debug("flush bind record failed", zap.String("category", "sql-bind"), zap.Error(err))
+		}
+	}
 }
 
 // AddInvalidGlobalBinding adds Bindings which needs to be deleted into invalidBindRecordMap.
 func (h *globalBindingHandle) AddInvalidGlobalBinding(invalidBinding Binding) {
-	h.invalidBindRecordMap.Add(invalidBinding)
+	h.invalidBindings.add(invalidBinding)
 }
 
 // Size returns the size of bind info cache.
@@ -729,7 +712,7 @@ func (*paramMarkerChecker) Leave(in ast.Node) (ast.Node, bool) {
 func (h *globalBindingHandle) Clear() {
 	h.setCache(newBindCache())
 	h.setLastUpdateTime(types.ZeroTimestamp)
-	h.invalidBindRecordMap.Store(make(map[string]*bindRecordUpdate))
+	h.invalidBindings.reset()
 }
 
 // FlushGlobalBindings flushes the Bindings in temp maps to storage and loads them into cache.
