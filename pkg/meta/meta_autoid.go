ed5a576a65f3dbd2e47daaa2958cdbe6e0391ec8
tiancaiamao <tiancaiamao@gmail.com>
2024-01-22 14:55:07 +0800

    meta: debug CI for pr/50621 (DNM)

--- pkg/meta/meta_autoid.go
+++ pkg/meta/meta_autoid.go
@@ -7,6 +7,7 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
+
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -99,6 +100,7 @@ type AccessorPicker interface {
 }
 
 type autoIDAccessors struct {
+	// debug for test
 	access autoIDAccessor
 }
 
