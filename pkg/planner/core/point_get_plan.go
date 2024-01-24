9dcd9e36b5b4bb6eed282bc43b6f134ca7fe2512
bb7133 <bb7133@gmail.com>
2024-01-24 14:12:29 -0800

    planner/core: Key partitioning uses an column index, which did not match value pairs in fast plan

--- pkg/planner/core/point_get_plan.go
+++ pkg/planner/core/point_get_plan.go
@@ -1864,9 +1864,14 @@ func getPartitionInfo(ctx sessionctx.Context, tbl *model.TableInfo, pairs []name
 	case model.PartitionTypeKey:
 		// The key partition table supports FastPlan when it contains only one partition column
 		if len(pi.Columns) == 1 {
+			// We need to change the partition column index!
+			col := &expression.Column{}
+			*col = *partitionExpr.KeyPartCols[0]
+			col.Index = 0
+			pe := &tables.ForKeyPruning{KeyPartCols: []*expression.Column{col}}
 			for i, pair := range pairs {
 				if pi.Columns[0].L == pair.colName {
-					pos, err := partitionExpr.LocateKeyPartition(pi.Num, []types.Datum{pair.value})
+					pos, err := pe.LocateKeyPartition(pi.Num, []types.Datum{pair.value})
 					if err != nil {
 						return nil, 0, 0, false
 					}
