3573de7b6d5d6ebd81e32012f3493b02dc55c942
Arenatlx <ailinsilence4@gmail.com>
2024-01-24 12:09:17 +0800

    Update integration_test.go

--- statistics/integration_test.go
+++ statistics/integration_test.go
@@ -789,6 +789,7 @@ func TestIssue44369(t *testing.T) {
 }
 
 func TestIssue49986(t *testing.T) {
+	// test
 	store := testkit.CreateMockStore(t)
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("use test")
