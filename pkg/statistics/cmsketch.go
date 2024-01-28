48aec1bbcccb9fe03b2e4aa40085d5ffdd2de418
Weizhen Wang <wangweizhen@pingcap.com>
2024-01-28 11:42:14 +0800

    *: Bounds Check Elimination for cm sketch
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/cmsketch.go
+++ pkg/statistics/cmsketch.go
@@ -684,12 +684,8 @@ func (c *TopN) LowerBound(d []byte) (idx int, match bool) {
 	if c == nil {
 		return 0, false
 	}
-	idx = sort.Search(len(c.TopN), func(i int) bool {
-		cmpRst := bytes.Compare(c.TopN[i].Encoded, d)
-		if cmpRst == 0 {
-			match = true
-		}
-		return cmpRst >= 0
+	idx, match = slices.BinarySearchFunc(c.TopN, d, func(a TopNMeta, b []byte) int {
+		return bytes.Compare(a.Encoded, d)
 	})
 	return idx, match
 }
