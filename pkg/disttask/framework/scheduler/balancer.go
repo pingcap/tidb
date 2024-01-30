32c1f89187069c512a25cea54bea0a7a798f098d
D3Hunter <jujj603@gmail.com>
2024-01-30 15:37:15 +0800

    change

--- pkg/disttask/framework/scheduler/balancer.go
+++ pkg/disttask/framework/scheduler/balancer.go
@@ -149,7 +149,7 @@ func (b *balancer) doBalanceSubtasks(ctx context.Context, taskID int64, eligible
 	executorWithOneMoreSubtask := make(map[string]struct{}, remainder)
 	for node, sts := range executorSubtasks {
 		if _, ok := adjustedNodeMap[node]; !ok {
-			b.logger.Info("dead node or not have enough slots, schedule subtasks away",
+			logutil.BgLogger().Info("dead node or not have enough slots, schedule subtasks away",
 				zap.Int64("task-id", taskID),
 				zap.String("node", node),
 				zap.Int("slot-capacity", b.slotMgr.getCapacity()),
