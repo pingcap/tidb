082d741a96cc6bf9b20537e9ee37b032e0198cef
lance6716 <lance6716@gmail.com>
2024-02-05 14:39:21 +0800

    address comment
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- br/pkg/lightning/backend/external/engine.go
+++ br/pkg/lightning/backend/external/engine.go
@@ -226,14 +226,12 @@ func getFilesReadConcurrency(
 		// let the stat internals cover the [startKey, endKey) since seekPropsOffsets
 		// always return an offset that is less than or equal to the key.
 		result[i] += 1
-		if result[i] > 1 {
-			logutil.Logger(ctx).Info("found hotspot file in getFilesReadConcurrency",
-				zap.String("filename", statsFiles[i]),
-				zap.Uint64("startOffset", startOffs[i]),
-				zap.Uint64("endOffset", endOffs[i]),
-				zap.Uint64("expected concurrency", result[i]),
-			)
-		}
+		logutil.Logger(ctx).Info("found hotspot file in getFilesReadConcurrency",
+			zap.String("filename", statsFiles[i]),
+			zap.Uint64("startOffset", startOffs[i]),
+			zap.Uint64("endOffset", endOffs[i]),
+			zap.Uint64("expected concurrency", result[i]),
+		)
 	}
 	return result, startOffs, nil
 }
