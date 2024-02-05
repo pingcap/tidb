a288b2e0a89c076e5ae5854007dedb16def88451
D3Hunter <jujj603@gmail.com>
2024-02-05 15:08:59 +0800

    Update pkg/disttask/framework/testutil/table_util.go
    Co-authored-by: lance6716 <lance6716@gmail.com>

--- pkg/disttask/framework/testutil/table_util.go
+++ pkg/disttask/framework/testutil/table_util.go
@@ -173,7 +173,7 @@ func GetSubtaskEndTime(ctx context.Context, mgr *storage.TaskManager, subtaskID 
 	return time.Time{}, nil
 }
 
-// GetSubtaskNodes gets subtasks running nodes for one task for test.
+// GetSubtaskNodes gets nodes that are running or have run the task for test.
 func GetSubtaskNodes(ctx context.Context, mgr *storage.TaskManager, taskID int64) ([]string, error) {
 	rs, err := mgr.ExecuteSQLWithNewSession(ctx, `
 		select distinct(exec_id) from mysql.tidb_background_subtask where task_key=%?
