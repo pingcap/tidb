7b693c8df512cc30c24d6db4db2ec586c1d6cb13
Yiding Cui <winoros@gmail.com>
2024-02-07 22:16:15 +0800

    This is an automated cherry-pick of #50834
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -234,6 +234,8 @@ func RegisterMetrics() {
 	prometheus.MustRegister(StatsCacheCounter)
 	prometheus.MustRegister(StatsCacheGauge)
 	prometheus.MustRegister(StatsHealthyGauge)
+	prometheus.MustRegister(StatsDeltaLoadHistogram)
+	prometheus.MustRegister(StatsDeltaUpdateHistogram)
 	prometheus.MustRegister(TxnStatusEnteringCounter)
 	prometheus.MustRegister(TxnDurationHistogram)
 	prometheus.MustRegister(LastCheckpoint)
