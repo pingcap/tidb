44177f4aa1ddc74deb2d1a26ab9614c3c0a3e784
Weizhen Wang <wangweizhen@pingcap.com>
2024-02-20 17:31:15 +0800

    *: recalculate the threshold of gctuner when ServerMemoryLimit is updated
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- sessionctx/variable/sysvar.go
+++ sessionctx/variable/sysvar.go
@@ -896,6 +896,8 @@ var defaultSysVars = []*SysVar{
 			}
 			memory.ServerMemoryLimitOriginText.Store(str)
 			memory.ServerMemoryLimit.Store(bt)
+			threshold := float64(bt) * GOGCTunerThreshold.Load()
+			gctuner.Tuning(uint64(threshold))
 			gctuner.GlobalMemoryLimitTuner.UpdateMemoryLimit()
 			return nil
 		},
