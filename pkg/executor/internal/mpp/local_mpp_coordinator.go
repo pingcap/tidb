245dd62364771c49c2f37f6dcef3a93a5d9d9f9d
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-02-20 11:45:26 +0800

    fix: check kill signal against 0 (#50029) (#50170)
    ref pingcap/tidb#49643

--- pkg/executor/internal/mpp/local_mpp_coordinator.go
+++ pkg/executor/internal/mpp/local_mpp_coordinator.go
@@ -668,10 +668,17 @@ func (c *localMppCoordinator) nextImpl(ctx context.Context) (resp *mppResponse, 
 		case resp, ok = <-c.respChan:
 			return
 		case <-ticker.C:
-			if c.vars != nil && c.vars.Killed != nil && atomic.LoadUint32(c.vars.Killed) == 1 {
-				err = derr.ErrQueryInterrupted
-				exit = true
-				return
+			if c.vars != nil && c.vars.Killed != nil {
+				killed := atomic.LoadUint32(c.vars.Killed)
+				if killed != 0 {
+					logutil.Logger(ctx).Info(
+						"a killed signal is received",
+						zap.Uint32("signal", killed),
+					)
+					err = derr.ErrQueryInterrupted
+					exit = true
+					return
+				}
 			}
 		case <-c.finishCh:
 			exit = true
