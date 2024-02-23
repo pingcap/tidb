c0497daaed81040f5f7a0cdc57f14939b229a744
xufei <xufeixw@mail.ustc.edu.cn>
2024-02-23 13:48:47 +0800

    refine
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

---
+++ pkg/executor/partitionedhashjoin/inner_join_probe.go
@@ -0,0 +1,140 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package partitionedhashjoin
+
+import (
+	"github.com/pingcap/tidb/pkg/expression"
+	"github.com/pingcap/tidb/pkg/util/chunk"
+)
+
+type innerJoinProbe struct {
+	baseJoinProbe
+}
+
+func (j *innerJoinProbe) probe(chk *chunk.Chunk, info *probeProcessInfo) (err error) {
+	if chk.IsFull() {
+		return nil
+	}
+	joinedChk, remainCap, err1 := j.prepareForProbe(chk, info)
+	if err1 != nil {
+		return err1
+	}
+	length := 0
+	meta := j.ctx.hashTableMeta
+
+	for remainCap > 0 && info.currentProbeRow < info.chunk.NumRows() {
+		if info.matchedRowsHeaders[info.currentProbeRow] != nil {
+			candidateRow := info.matchedRowsHeaders[info.currentProbeRow]
+			if isKeyMatched(j.ctx.keyMode, info.serializedKeys[info.currentProbeRow], candidateRow, meta) {
+				// key matched, convert row to column for build side
+				rowInfo := &rowInfo{rowStart: candidateRow, rowData: nil, currentColumnIndex: 0}
+				currentRowData := j.appendBuildRowToChunk(joinedChk, rowInfo, info)
+				if j.ctx.hasOtherCondition() {
+					j.rowIndexInfos = append(j.rowIndexInfos, rowIndexInfo{probeRowIndex: info.currentProbeRow, buildRowStart: candidateRow, buildRowData: currentRowData})
+				}
+				length++
+				remainCap--
+				joinedChk.IncNumVirtualRows()
+			}
+			info.matchedRowsHeaders[info.currentProbeRow] = getNextRowAddress(candidateRow)
+		} else {
+			if length > 0 {
+				j.offsetAndLengthArray = append(j.offsetAndLengthArray, offsetAndLength{offset: info.currentProbeRow, length: length})
+				length = 0
+			}
+			info.currentProbeRow++
+		}
+	}
+
+	if length > 0 {
+		j.offsetAndLengthArray = append(j.offsetAndLengthArray, offsetAndLength{offset: info.currentProbeRow, length: length})
+	}
+
+	j.appendProbeRowToChunk(joinedChk, info.chunk)
+
+	if j.ctx.hasOtherCondition() && joinedChk.NumRows() > 0 {
+		// eval other condition, and construct final chunk
+		j.selected = j.selected[:0]
+		j.selected, err = expression.VectorizedFilter(j.ctx.sessCtx, j.ctx.otherCondition, chunk.NewIterator4Chunk(joinedChk), j.selected)
+		if err != nil {
+			return err
+		}
+		err = j.buildResultAfterOtherCondition(chk, joinedChk, info)
+	}
+	// if there is no other condition, the joinedChk is the final result
+	return err
+}
+
+func (j *innerJoinProbe) needScanHT() bool {
+	return false
+}
+
+func (j *innerJoinProbe) scanHT(*chunk.Chunk) (err error) {
+	panic("should not reach here")
+}
+
+func (j *innerJoinProbe) buildResultAfterOtherCondition(chk *chunk.Chunk, joinedChk *chunk.Chunk, info *probeProcessInfo) (err error) {
+	// construct the return chunk based on joinedChk and selected, there are 3 kinds of columns
+	// 1. columns already in joinedChk
+	// 2. columns from build side, but not in joinedChk
+	// 3. columns from probe side, but not in joinedChk
+	probeUsedColumns, probeColOffset, probeColOffsetInJoinedChk := j.lUsed, 0, 0
+	if !j.rightAsBuildSide {
+		probeUsedColumns, probeColOffset, probeColOffsetInJoinedChk = j.rUsed, len(j.lUsed), j.ctx.hashTableMeta.totalColumnNumber
+	}
+
+	for index, colIndex := range probeUsedColumns {
+		dstCol := chk.Column(index + probeColOffset)
+		if joinedChk.Column(colIndex+probeColOffsetInJoinedChk).Rows() > 0 {
+			// probe column that is already in joinedChk
+			srcCol := joinedChk.Column(colIndex + probeColOffsetInJoinedChk)
+			chunk.CopySelectedRows(dstCol, srcCol, j.selected)
+		} else {
+			// probe column that is not in joinedChk
+			srcCol := info.chunk.Column(colIndex)
+			chunk.CopySelectedRowsWithRowIdFunc(dstCol, srcCol, j.selected, 0, len(j.selected), func(i int) int {
+				return j.rowIndexInfos[i].probeRowIndex
+			})
+		}
+	}
+	buildUsedColumns, buildColOffset, buildColOffsetInJoinedChk := j.rUsed, len(j.lUsed), info.chunk.NumCols()
+	if !j.rightAsBuildSide {
+		buildUsedColumns, buildColOffset, buildColOffsetInJoinedChk = j.lUsed, 0, 0
+	}
+	hasRemainCols := false
+	for index, colIndex := range buildUsedColumns {
+		dstCol := chk.Column(index + buildColOffset)
+		srcCol := joinedChk.Column(colIndex + buildColOffsetInJoinedChk)
+		if srcCol.Rows() > 0 {
+			// build column that is already in joinedChk
+			chunk.CopySelectedRows(dstCol, srcCol, j.selected)
+		} else {
+			hasRemainCols = true
+		}
+	}
+	if hasRemainCols {
+		// build column that is not in joinedChk
+		for index, result := range j.selected {
+			if result {
+				j.appendBuildRowToChunk(chk, &rowInfo{
+					rowStart:           j.rowIndexInfos[index].buildRowStart,
+					rowData:            j.rowIndexInfos[index].buildRowData,
+					currentColumnIndex: j.ctx.hashTableMeta.columnCountNeededForOtherCondition,
+				}, info)
+			}
+		}
+	}
+	return
+}
