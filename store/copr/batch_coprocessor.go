9c11a98ed9bc24c15de9874c2d98f40db068328e
SeaRise <hhssearise@foxmail.com>
2024-02-27 11:01:20 +0800

    init

--- store/copr/batch_coprocessor.go
+++ store/copr/batch_coprocessor.go
@@ -805,7 +805,7 @@ func buildBatchCopTasksCore(bo *backoff.Backoffer, store *kvStore, rangesForEach
 		rangesLen = 0
 		for i, ranges := range rangesForEachPhysicalTable {
 			rangesLen += ranges.Len()
-			locations, err := cache.SplitKeyRangesByLocations(bo, ranges, UnspecifiedLimit)
+			locations, err := cache.SplitKeyRangesByLocationsNew(bo, ranges)
 			if err != nil {
 				return nil, errors.Trace(err)
 			}
