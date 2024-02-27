8d86bfb4d8933f2e2d84e4478e750b04cd59eeb0
Yu Juncen <yu745514916@live.com>
2024-02-27 15:43:50 +0800

    print callstack when exiting
    Signed-off-by: Yu Juncen <yu745514916@live.com>

--- br/pkg/utils/misc.go
+++ br/pkg/utils/misc.go
@@ -16,8 +16,16 @@ package utils
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"strings"
+	"sync/atomic"
+	"syscall"
 	"time"
 
+	"github.com/docker/go-units"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/log"
@@ -156,4 +164,53 @@ func WithCleanUp(errOut *error, timeout time.Duration, fn func(context.Context) 
 	} else if err != nil {
 		log.Warn("Encountered but ignored error while cleaning up.", zap.Error(err))
 	}
+}
+
+func AllStackInfo() []byte {
+	res := make([]byte, 256*units.KiB)
+	for {
+		n := runtime.Stack(res, true)
+		if n < len(res) {
+			return res[:n]
+		}
+		res = make([]byte, len(res)*2)
+	}
+}
+
+var (
+	DumpGoroutineWhenExit atomic.Bool
+)
+
+func StartExitSingleListener(ctx context.Context) (context.Context, context.CancelFunc) {
+	cx, cancel := context.WithCancel(ctx)
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		sig := <-sc
+		dumpGoroutine := DumpGoroutineWhenExit.Load()
+		padding := strings.Repeat("=", 8)
+		printDelimate := func(s string) {
+			fmt.Printf("%s[ %s ]%s\n", padding, s, padding)
+		}
+		fmt.Println()
+		printDelimate(fmt.Sprintf("Got signal %v to exit.", sig))
+		printDelimate(fmt.Sprintf("Required Goroutine Dump = %v", dumpGoroutine))
+		if DumpGoroutineWhenExit.Load() {
+			printDelimate("Start Dumping Goroutine")
+			os.Stdout.Write(AllStackInfo())
+			printDelimate("End of Dumping Goroutine")
+		}
+		log.Warn("received signal to exit", zap.Stringer("signal", sig))
+		cancel()
+		fmt.Fprintln(os.Stderr, "gracefully shutting down, press ^C again to force exit")
+		<-sc
+		// Even user use SIGTERM to exit, there isn't any checkpoint for resuming,
+		// hence returning fail exit code.
+		os.Exit(1)
+	}()
+	return cx, cancel
 }
