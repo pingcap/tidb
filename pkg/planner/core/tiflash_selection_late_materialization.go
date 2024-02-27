70f94856f0b77a0eae2cc0b21208ff95c230ddbd
Lloyd-Pottiger <60744015+Lloyd-Pottiger@users.noreply.github.com>
2024-02-27 19:21:37 +0800

    This is an automated cherry-pick of #51355
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/planner/core/tiflash_selection_late_materialization.go
+++ pkg/planner/core/tiflash_selection_late_materialization.go
@@ -251,6 +251,7 @@ func predicatePushDownToTableScanImpl(sctx sessionctx.Context, physicalSelection
 	if len(selectedConds) == 0 {
 		return
 	}
+<<<<<<< HEAD
 	logutil.BgLogger().Debug("planner: push down conditions to table scan", zap.String("table", physicalTableScan.Table.Name.L), zap.String("conditions", string(expression.SortedExplainExpressionList(selectedConds))))
 	// remove the pushed down conditions from selection
 	removeSpecificExprsFromSelection(physicalSelection, selectedConds)
@@ -258,4 +259,19 @@ func predicatePushDownToTableScanImpl(sctx sessionctx.Context, physicalSelection
 	physicalTableScan.LateMaterializationFilterCondition = selectedConds
 	// Update the row count of table scan after pushing down the conditions.
 	physicalTableScan.StatsInfo().RowCount *= selectedSelectivity
+=======
+	logutil.BgLogger().Debug("planner: push down conditions to table scan", zap.String("table", physicalTableScan.Table.Name.L), zap.String("conditions", string(expression.SortedExplainExpressionList(sctx, selectedConds))))
+	PushedDown(physicalSelection, physicalTableScan, selectedConds, selectedSelectivity)
+}
+
+// PushedDown is used to push down the selected conditions from PhysicalSelection to PhysicalTableScan.
+// Used in unit test, so it is exported.
+func PushedDown(sel *PhysicalSelection, ts *PhysicalTableScan, selectedConds []expression.Expression, selectedSelectivity float64) {
+	// remove the pushed down conditions from selection
+	removeSpecificExprsFromSelection(sel, selectedConds)
+	// add the pushed down conditions to table scan
+	ts.LateMaterializationFilterCondition = selectedConds
+	// Update the row count of table scan after pushing down the conditions.
+	ts.StatsInfo().RowCount *= selectedSelectivity
+>>>>>>> 49fcf49283b (planner: fix flaky test TestPhysicalTableScanExtractCorrelatedCols (#51355))
 }
