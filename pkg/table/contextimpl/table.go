51c006234cf069da544b0f2bb33005770e16eef2
Chao Wang <cclcwangchao@hotmail.com>
2024-02-27 11:52:37 +0800

    rename some field

--- pkg/table/contextimpl/table.go
+++ pkg/table/contextimpl/table.go
@@ -29,12 +29,12 @@ var _ context.AllocatorContext = &TableContextImpl{}
 // TableContextImpl is used to provide context for table operations.
 type TableContextImpl struct {
 	sessionctx.Context
-	ectx exprctx.BuildContext
+	exprCtx exprctx.BuildContext
 }
 
 // NewTableContextImpl creates a new TableContextImpl.
-func NewTableContextImpl(sctx sessionctx.Context, ectx exprctx.BuildContext) *TableContextImpl {
-	return &TableContextImpl{Context: sctx, ectx: ectx}
+func NewTableContextImpl(sctx sessionctx.Context, exprCtx exprctx.BuildContext) *TableContextImpl {
+	return &TableContextImpl{Context: sctx, exprCtx: exprCtx}
 }
 
 // TxnRecordTempTable record the temporary table to the current transaction.
@@ -45,7 +45,7 @@ func (ctx *TableContextImpl) TxnRecordTempTable(tbl *model.TableInfo) tableutil.
 
 // GetExprCtx returns the ExprContext
 func (ctx *TableContextImpl) GetExprCtx() exprctx.BuildContext {
-	return ctx.ectx
+	return ctx.exprCtx
 }
 
 func (ctx *TableContextImpl) vars() *variable.SessionVars {
