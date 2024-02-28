7c9fa69c445da18a1d83ec5a1d7891bfbd8495d8
hongyunyan <649330952@qq.com>
2024-02-28 11:46:19 +0800

    Update pkg/parser/model/ddl.go
    Co-authored-by: EasonBall <592838129@qq.com>

--- pkg/parser/model/ddl.go
+++ pkg/parser/model/ddl.go
@@ -572,7 +572,7 @@ type Job struct {
 	// Only happens when tidb_ddl_version = 2.
 	LocalMode bool `json:"local_mode"`
 
-	// SQLMode for parse ddl query
+	// SQLMode for execute ddl query
 	SQLMode mysql.SQLMode `json:"sql_mode"`
 }
 
