6acc7b5152d5bf4b9ed26342bd0f7bfd93138f2d
xhe <xw897002528@gmail.com>
2024-02-29 16:42:43 +0800

    fix
    Signed-off-by: xhe <xw897002528@gmail.com>

--- pkg/ddl/table.go
+++ pkg/ddl/table.go
@@ -235,8 +235,7 @@ func onCreateTables(d *ddlCtx, t *meta.Meta, job *model.Job) (int64, error) {
 	// We don't construct jobs for every table, but only tableInfo
 	// The following loop creates a stub job for every table
 	// it clones a stub job from the ActionCreateTables job
-	stubJobT := *job
-	stubJob := &stubJobT
+	stubJob := job.Clone()
 	stubJob.Args = make([]any, 1)
 	for i := range args {
 		stubJob.TableID = args[i].ID
