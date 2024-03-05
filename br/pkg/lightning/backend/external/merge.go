8d39a697a4171da42e11f4c672322c45a1109919
lance6716 <lance6716@gmail.com>
2024-03-05 16:13:35 +0800

    global sort: fix forget to close writer when reader has error
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- br/pkg/lightning/backend/external/merge.go
+++ br/pkg/lightning/backend/external/merge.go
@@ -124,6 +124,18 @@ func mergeOverlappingFilesInternal(
 	if err != nil {
 		return nil
 	}
+	defer func() {
+		err2 := writer.Close(ctx)
+		if err2 == nil {
+			return
+		}
+		
+		if err == nil {
+			err = err2
+		} else {
+			logutil.Logger(ctx).Warn("close writer failed", zap.Error(err2))
+		}
+	}()
 
 	// currently use same goroutine to do read and write. The main advantage is
 	// there's no KV copy and iter can reuse the buffer.
@@ -138,5 +150,5 @@ func mergeOverlappingFilesInternal(
 		return err
 	}
 
-	return writer.Close(ctx)
+	return nil
 }
