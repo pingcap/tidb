7534e8120a02ac2acfdd26a1d928de3144bc1d30
Ryan Leung <rleungx@gmail.com>
2024-03-05 10:58:10 +0800

    use 503
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- pkg/infoschema/tables.go
+++ pkg/infoschema/tables.go
@@ -1989,7 +1989,7 @@ func getMicroServiceServerInfo(ctx sessionctx.Context, serviceName string) ([]Se
 		req.Header.Add("PD-Allow-follower-handle", "true")
 		resp, err := util.InternalHTTPClient().Do(req)
 		// PD is not in the microservice mode
-		if resp.StatusCode == http.StatusNotFound {
+		if resp.StatusCode == http.StatusServiceUnavailable {
 			terror.Log(resp.Body.Close())
 			return servers, nil
 		}
