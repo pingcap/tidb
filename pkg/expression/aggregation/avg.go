b3ee183ce7fc8e54bb60ae9beece4d8a389794f2
yibin <huyibin@pingcap.com>
2024-03-06 16:32:05 +0800

    Temporary changes
    Signed-off-by: yibin <huyibin@pingcap.com>

--- pkg/expression/aggregation/avg.go
+++ pkg/expression/aggregation/avg.go
@@ -82,7 +82,7 @@ func (af *avgFunction) GetResult(evalCtx *AggEvaluateContext) (d types.Datum) {
 		x := evalCtx.Value.GetMysqlDecimal()
 		y := types.NewDecFromInt(evalCtx.Count)
 		to := new(types.MyDecimal)
-		err := types.DecimalDiv(x, y, to, evalCtx.Ctx.GetSessionVars().GetDivPrecisionIncrement())
+		err := types.DecimalDiv(x, y, to, 4)
 		terror.Log(err)
 		frac := af.RetTp.GetDecimal()
 		if frac == -1 {
