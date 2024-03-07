74d9bef6093b747db06fa7fab329d5a3b32ee8af
Weizhen Wang <wangweizhen@pingcap.com>
2024-03-07 16:53:13 +0800

    domain: avoid panic in the init stats breaking the loadStatsWorker (#51579)
    close pingcap/tidb#51581

--- domain/domain.go
+++ domain/domain.go
@@ -1930,6 +1930,10 @@ func (do *Domain) newOwnerManager(prompt, ownerKey string) owner.Manager {
 func (do *Domain) initStats() {
 	statsHandle := do.StatsHandle()
 	defer func() {
+		if r := recover(); r != nil {
+			logutil.BgLogger().Error("panic when initiating stats", zap.Any("r", r),
+				zap.Stack("stack"))
+		}
 		close(statsHandle.InitStatsDone)
 	}()
 	t := time.Now()
