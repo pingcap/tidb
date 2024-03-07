2a3411acc3cb2ad08e0f1ddd79fba903af6a72d0
tangenta <tangenta@126.com>
2024-03-07 16:51:14 +0800

    ddl: fix unstable test TestCreateDropCreateTable

--- pkg/ddl/ddl.go
+++ pkg/ddl/ddl.go
@@ -795,6 +795,7 @@ func (d *ddl) Start(ctxPool *pools.ResourcePool) error {
 		if ingest.LitBackCtxMgr != nil {
 			ingest.LitBackCtxMgr.MarkJobFinish()
 		}
+		d.runningJobs = newRunningJobs()
 	})
 
 	return nil
