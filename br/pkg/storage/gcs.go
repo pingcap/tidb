3850549abafa5174fcbc34c9eea9233a655ca0ee
D3Hunter <jujj603@gmail.com>
2024-03-12 18:30:01 +0800

    change

--- br/pkg/storage/gcs.go
+++ br/pkg/storage/gcs.go
@@ -344,7 +344,7 @@ func (s *GCSStorage) Close() {
 // used in tests
 var mustReportCredErr = false
 
-const gcsClientCnt = 16
+const gcsClientCnt = 8
 
 // NewGCSStorage creates a GCS external storage implementation.
 func NewGCSStorage(ctx context.Context, gcs *backuppb.GCS, opts *ExternalStorageOptions) (*GCSStorage, error) {
