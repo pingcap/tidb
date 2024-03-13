ed22ad5f4835ce0ff1ebfee7d0fbfdb5be62b207
you06 <you1474600@gmail.com>
2024-03-13 12:18:22 +0900

    lint
    Signed-off-by: you06 <you1474600@gmail.com>

--- pkg/ttl/cache/split_test.go
+++ pkg/ttl/cache/split_test.go
@@ -104,7 +104,7 @@ func (c *mockPDClient) GetStore(_ context.Context, storeID uint64) (*metapb.Stor
 	}, nil
 }
 
-func (p *mockPDClient) GetClusterID(ctx context.Context) uint64 {
+func (c *mockPDClient) GetClusterID(ctx context.Context) uint64 {
 	return 1
 }
 
