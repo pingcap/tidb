d2c7bc8307b7179603548ff6af07fab5d5a3d7e4
lyzx2001 <rep_laureline@hotmail.com>
2024-03-14 18:10:53 +0800

    update according to comments

--- pkg/ddl/ingest/config.go
+++ pkg/ddl/ingest/config.go
@@ -63,11 +63,9 @@ func genConfig(ctx context.Context, memRoot MemRoot, jobID int64, unique bool, r
 	adjustImportMemory(ctx, memRoot, cfg)
 	cfg.Checkpoint.Enable = true
 	if unique {
-		if cfg.TikvImporter.Backend == lightning.BackendLocal {
-			cfg.Conflict.Strategy = lightning.ErrorOnDup
-		}
+		cfg.Conflict.Strategy = lightning.ErrorOnDup
 		cfg.Conflict.Threshold = math.MaxInt64
-	} else if cfg.TikvImporter.Backend == lightning.BackendLocal {
+	} else {
 		cfg.Conflict.Strategy = lightning.NoneOnDup
 	}
 	cfg.TiDB.Host = "127.0.0.1"
