c2366f97775929ad4ecd207fabecf7d89097815a
dbsid <chenhuansheng@pingcap.com>
2024-03-14 10:04:04 -0700

    expression/infer_pushdown: enable json_array_append pushdown

--- pkg/expression/infer_pushdown.go
+++ pkg/expression/infer_pushdown.go
@@ -206,7 +206,7 @@ func scalarExprSupportedByTiKV(sf *ScalarFunction) bool {
 		// json functions.
 		ast.JSONType, ast.JSONExtract, ast.JSONObject, ast.JSONArray, ast.JSONMerge, ast.JSONSet,
 		ast.JSONInsert, ast.JSONReplace, ast.JSONRemove, ast.JSONLength,
-		ast.JSONUnquote, ast.JSONContains, ast.JSONValid, ast.JSONMemberOf,
+		ast.JSONUnquote, ast.JSONContains, ast.JSONValid, ast.JSONMemberOf, ast.JSONArrayAppend,
 
 		// date functions.
 		ast.Date, ast.Week /* ast.YearWeek, ast.ToSeconds */, ast.DateDiff,
