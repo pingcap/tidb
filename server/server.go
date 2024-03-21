c5e07dbb85bbfda726c9913e35cd10050bb145c8
Weizhen Wang <wangweizhen@pingcap.com>
2024-03-21 13:58:24 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- server/server.go
+++ server/server.go
@@ -209,27 +209,15 @@ func (s *Server) newConn(conn net.Conn) *clientConn {
 // NewServer creates a new Server.
 func NewServer(cfg *config.Config, driver IDriver) (*Server, error) {
 	s := &Server{
-<<<<<<< HEAD:server/server.go
 		cfg:               cfg,
 		driver:            driver,
 		concurrentLimiter: NewTokenLimiter(cfg.TokenLimit),
 		clients:           make(map[uint64]*clientConn),
 		globalConnID:      util.NewGlobalConnID(0, true),
 		internalSessions:  make(map[interface{}]struct{}, 100),
-		health:            uatomic.NewBool(true),
+		health:            uatomic.NewBool(false),
 		inShutdownMode:    uatomic.NewBool(false),
 		printMDLLogTime:   time.Now(),
-=======
-		cfg:                    cfg,
-		driver:                 driver,
-		concurrentLimiter:      NewTokenLimiter(cfg.TokenLimit),
-		clients:                make(map[uint64]*clientConn),
-		ConnNumByResourceGroup: make(map[string]int),
-		internalSessions:       make(map[any]struct{}, 100),
-		health:                 uatomic.NewBool(false),
-		inShutdownMode:         uatomic.NewBool(false),
-		printMDLLogTime:        time.Now(),
->>>>>>> d3a9c1b8677 (server: fix incorrect setting of health status (#51595)):pkg/server/server.go
 	}
 	s.capability = defaultCapability
 	setTxnScope()
--- server/server_test.go
+++ server/server_test.go
@@ -2655,3 +2655,25 @@ func (cli *testServerClient) RunTestStmtCountLimit(t *testing.T) {
 		require.Equal(t, 5, count)
 	})
 }
+
+func TestSeverHealth(t *testing.T) {
+	RunInGoTestChan = make(chan struct{})
+	RunInGoTest = true
+	store := testkit.CreateMockStore(t)
+	tidbdrv := NewTiDBDriver(store)
+	cfg := newTestConfig()
+	cfg.Port, cfg.Status.StatusPort = 0, 0
+	cfg.Status.ReportStatus = false
+	server, err := NewServer(cfg, tidbdrv)
+	require.NoError(t, err)
+	require.False(t, server.health.Load(), "server should not be healthy")
+	go func() {
+		err = server.Run(nil)
+		require.NoError(t, err)
+	}()
+	defer server.Close()
+	for range RunInGoTestChan {
+		// wait for server to be healthy
+	}
+	require.True(t, server.health.Load(), "server should be healthy")
+}
