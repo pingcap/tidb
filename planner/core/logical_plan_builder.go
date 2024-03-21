c11167f2b46b0f2610b463cc7158f81f209e0339
Weizhen Wang <wangweizhen@pingcap.com>
2024-03-21 09:00:12 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -2411,7 +2411,7 @@ func (a *havingWindowAndOrderbyExprResolver) resolveFromPlan(v *ast.ColumnNameEx
 	//             JOIN `t2`
 	//             WHERE ISNULL(`t2`.`col_3`) HAVING `t1`.`col_6`>1951988)
 	//     ) ;
-	if resolveFieldsFirst && a.curClause == havingClause && a.gbyItems == nil {
+	if resolveFieldsFirst && a.curClause == havingClause {
 		return -1, nil
 	}
 	sf := &ast.SelectField{
