363d059b4891b9a58ae7d0f3687243fcdadc0547
Leavrth <jianjun.liao@outlook.com>
2024-03-22 13:57:57 +0800

    fix typos
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- br/tests/br_pitr/check/check_ingest_repair.sh
+++ br/tests/br_pitr/check/check_ingest_repair.sh
@@ -27,11 +27,11 @@ check_contains "Column_name: y"
 
 run_sql "SHOW INDEX FROM test.pairs WHERE Key_name = 'i2' AND Index_type = 'BTREE' AND Index_comment = '123' AND Visible = 'YES';"
 check_contains "Column_name: y"
-check_contains "Expression: `z` + 1"
+check_contains "Expression: \`z\` + 1"
 
 run_sql "SHOW INDEX FROM test.pairs WHERE Key_name = 'u2' AND Index_type = 'HASH' AND Index_comment = '243' AND Visible = 'YES';"
 check_contains "Column_name: x"
-check_contains "Expression: `y` + 1"
+check_contains "Expression: \`y\` + 1"
 
 ## check table test.pairs2
 run_sql "SHOW INDEX FROM test.pairs2 WHERE Key_name = 'i1' AND Visible = 'YES';"
@@ -43,7 +43,7 @@ check_contains "Sub_part: 10"
 run_sql "SHOW INDEX FROM test.pairs2 WHERE Key_name = 'u1' AND Index_type = 'RTREE' AND Visible = 'YES';"
 check_contains "Column_name: y"
 check_contains "Column_name: z"
-check_contains "Expression: `y` * 2"
+check_contains "Expression: \`y\` * 2"
 run_sql "SHOW INDEX FROM test.pairs2 WHERE Column_name = 'z' AND Key_name = 'u1' AND Index_type = 'RTREE' AND Visible = 'YES';"
 check_contains "Sub_part: 10"
 
@@ -52,7 +52,7 @@ check_contains "Column_name: x"
 
 ## check table test.pairs3
 run_sql "SHOW INDEX FROM test.pairs3 WHERE Key_name = 'zips2' AND Index_type = 'BTREE' AND Visible = 'YES';"
-check_contains "Expression: cast(json_extract(`custinfo`, _utf8'$.zipcode') as unsigned array)"
+check_contains "Expression: cast(json_extract(\`custinfo\`, _utf8'$.zipcode') as unsigned array)"
 
 ## check table test.pairs4
 run_sql "SHOW INDEX FROM test.pairs4 WHERE Key_name != 'PRIMARY';"
@@ -64,11 +64,11 @@ check_not_contains "1" ## the result should be empty
 
 ## check table test.pairs7
 run_sql "SHOW INDEX FROM test.pairs7 WHERE Key_name = 'zips2' AND Visible = 'YES';"
-check_contains "Expression: cast(json_extract(`cust``;info`, _utf8'$.zipcode') as unsigned array)"
+check_contains "Expression: cast(json_extract(\`cust\`\`;info\`, _utf8'$.zipcode') as unsigned array)"
 run_sql "SHOW INDEX FROM test.pairs7 WHERE Key_name = 'i2' AND Seq_in_index = 1 AND Visible = 'YES';"
-check_contains "Column_name: nam``;e"
+check_contains "Column_name: nam\`;e"
 run_sql "SHOW INDEX FROM test.pairs7 WHERE Key_name = 'i2' AND Seq_in_index = 2 AND Visible = 'YES';"
-check_contains "Expression: `nam``;e` * 2"
+check_contains "Expression: \`nam\`\`;e\` * 2"
 
 ## check table test.pairs10
 run_sql "SELECT count(*) AS RESCNT FROM INFORMATION_SCHEMA.TIDB_INDEXES WHERE INDEX_ID = 2 AND TABLE_NAME = 'pairs11' AND KEY_NAME = 'u1';"
@@ -76,7 +76,6 @@ check_contains "RESCNT: 2"
 
 # adjust some index to be visible
 run_sql "ALTER TABLE test.pairs ALTER INDEX i1 VISIBLE;"
-run_sql "ALTER TABLE test.pairs4 ALTER INDEX i1 VISIBLE;"
 
 # check index data
 run_sql "select count(*) AS RESCNT from test.pairs use index(i1) where y = 0 and z = 0;"
@@ -95,9 +94,9 @@ run_sql "select count(*) AS RESCNT from test.pairs2 use index(PRIMARY) where x =
 check_not_contains "RESCNT: 0"
 run_sql "select count(*) AS RESCNT from test.pairs3 use index(zips2) where custinfo->'$.zipcode' = json_array(1,2);"
 check_not_contains "RESCNT: 0"
-run_sql "select count(*) AS RESCNT from test.pairs7 use index(zips2) where `cust``;info`->'$.zipcode' =json_array(1,2);"
+run_sql "select count(*) AS RESCNT from test.pairs7 use index(zips2) where \`cust\`\`;info\`->'$.zipcode' =json_array(1,2);"
 check_not_contains "RESCNT: 0"
-run_sql "select count(*) AS RESCNT from test.pairs7 use index(i2) where `nam``;e` = 1 and `nam``;e` * 2 = 2;"
+run_sql "select count(*) AS RESCNT from test.pairs7 use index(i2) where \`nam\`\`;e\` = 1 and \`nam\`\`;e\` * 2 = 2;"
 check_not_contains "RESCNT: 0"
 run_sql "select count(*) AS RESCNT from test.pairs8 use index(i1) where y = '1';"
 check_not_contains "RESCNT: 0"
