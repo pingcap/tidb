4a4b8d625efb01ccf8b901fc88f22e1fdd50557f
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-03-25 12:06:46 +0800

    planner: Comparing binary string with int caused issue in partition pruning (#52041) (#52050)
    close pingcap/tidb#52023

--- pkg/planner/core/casetest/integration_test.go
+++ pkg/planner/core/casetest/integration_test.go
@@ -385,3 +385,38 @@ func TestFixControl45132(t *testing.T) {
 	tk.MustExec(`set @@tidb_opt_fix_control = "45132:0"`)
 	tk.MustHavePlan(`select * from t where a=2`, `TableFullScan`)
 }
+
+func TestIssue52023(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec(`use test`)
+	tk.MustExec(`CREATE TABLE t (
+			a binary(1) NOT NULL,
+			PRIMARY KEY (a)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+		PARTITION BY RANGE COLUMNS(a)
+		(PARTITION P0 VALUES LESS THAN (_binary 0x03),
+		PARTITION P4 VALUES LESS THAN (_binary 0xc0),
+		PARTITION PMX VALUES LESS THAN (MAXVALUE))`)
+	tk.MustExec(`insert into t values (0x5)`)
+	tk.MustExec(`analyze table t`)
+	tk.MustQuery(`select * from t`).Check(testkit.Rows("\u0005"))
+	tk.MustQuery(`select * from t where a = 0x5`).Check(testkit.Rows("\u0005"))
+	tk.MustQuery(`select * from t where a = 5`).Check(testkit.Rows())
+	tk.MustQuery(`select * from t where a IN (5,55)`).Check(testkit.Rows())
+	tk.MustQuery(`select * from t where a IN (0x5,55)`).Check(testkit.Rows("\u0005"))
+	tk.MustQuery(`explain select * from t where a = 0x5`).Check(testkit.Rows("Point_Get_1 1.00 root table:t, partition:P4, clustered index:PRIMARY(a) "))
+	tk.MustQuery(`explain format='brief' select * from t where a = 5`).Check(testkit.Rows(""+
+		"TableReader 0.80 root partition:all data:Selection",
+		"└─Selection 0.80 cop[tikv]  eq(cast(test.t.a, double BINARY), 5)",
+		"  └─TableFullScan 1.00 cop[tikv] table:t keep order:false"))
+	tk.MustQuery(`explain format='brief' select * from t where a IN (5,55)`).Check(testkit.Rows(""+
+		"TableReader 0.96 root partition:all data:Selection",
+		"└─Selection 0.96 cop[tikv]  or(eq(cast(test.t.a, double BINARY), 5), eq(cast(test.t.a, double BINARY), 55))",
+		"  └─TableFullScan 1.00 cop[tikv] table:t keep order:false"))
+	tk.MustQuery(`explain format='brief' select * from t where a IN (0x5,55)`).Check(testkit.Rows(""+
+		"TableReader 1.00 root partition:all data:Selection",
+		"└─Selection 1.00 cop[tikv]  or(eq(test.t.a, \"0x05\"), eq(cast(test.t.a, double BINARY), 55))",
+		"  └─TableFullScan 1.00 cop[tikv] table:t keep order:false"))
+}
