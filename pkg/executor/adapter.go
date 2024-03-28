22c7e1a9f990cbdbc4939cbd676f1be464b1d6ef
Jack Yu <jackysp@gmail.com>
2024-03-28 21:43:53 +0800

    fix panic
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- pkg/executor/adapter.go
+++ pkg/executor/adapter.go
@@ -1056,7 +1056,9 @@ func (a *ExecStmt) handlePessimisticDML(ctx context.Context, e exec.Executor) (e
 		startLocking := time.Now()
 		err = txn.LockKeys(ctx, lockCtx, keys...)
 		a.phaseLockDurations[0] += time.Since(startLocking)
-		e.RuntimeStats().Record(time.Since(startLocking), 0)
+		if e.RuntimeStats() != nil {
+			e.RuntimeStats().Record(time.Since(startLocking), 0)
+		}
 		if lockKeyStats != nil {
 			seVars.StmtCtx.MergeLockKeysExecDetails(lockKeyStats)
 		}
