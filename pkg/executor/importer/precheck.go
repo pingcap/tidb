e1e7c5cc2ca93ff126193bd02abc2edc099759f6
D3Hunter <jujj603@gmail.com>
2024-04-01 12:01:08 +0800

    change

--- pkg/executor/importer/precheck.go
+++ pkg/executor/importer/precheck.go
@@ -63,10 +63,10 @@ func (e *LoadDataController) CheckRequirements(ctx context.Context, conn sqlexec
 		if err := e.checkTotalFileSize(); err != nil {
 			return err
 		}
-		// run global sort with < 16 thread might OOM on merge step
+		// run global sort with < 8 thread might OOM on merge step
 		// TODO: remove this limit after control memory usage.
-		if e.IsGlobalSort() && e.ThreadCnt < 16 {
-			return exeerrors.ErrLoadDataPreCheckFailed.FastGenByArgs("global sort requires at least 16 threads")
+		if e.IsGlobalSort() && e.ThreadCnt < 8 {
+			return exeerrors.ErrLoadDataPreCheckFailed.FastGenByArgs("global sort requires at least 8 threads")
 		}
 	}
 	if err := e.checkTableEmpty(ctx, conn); err != nil {
