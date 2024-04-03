12870d3ccd7a1c1bfd0f560e3db69b8106e25495
y_static_y@sina.com <y_static_y@sina.com>
2024-04-03 17:07:51 +0800

    code format
    Signed-off-by: y_static_y@sina.com <y_static_y@sina.com>

--- pkg/store/driver/tikv_driver.go
+++ pkg/store/driver/tikv_driver.go
@@ -430,10 +430,6 @@ func (s *tikvStore) CurrentVersion(txnScope string) (kv.Version, error) {
 // CurrentMinTimestamp returns current minimum timestamp across all keyspace groups.
 func (s *tikvStore) CurrentAllTSOKeyspaceGroupMinTs() (uint64, error) {
 	ts, err := s.KVStore.CurrentAllTSOKeyspaceGroupMinTs()
-
-	if err != nil && strings.Contains(err.Error(), "Unimplemented") {
-		ts, err = s.KVStore.CurrentTimestamp(kv.GlobalTxnScope)
-	}
 	return ts, derr.ToTiDBErr(err)
 }
 
