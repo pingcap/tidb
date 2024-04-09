4164b7afdc7dae16e3005c17ca6c6094c9efa16d
hi-rustin <rustin.liu@gmail.com>
2024-04-09 11:58:31 +0800

    test: best position

--- pkg/planner/core/plan_test.go
+++ pkg/planner/core/plan_test.go
@@ -735,3 +735,59 @@ func TestImportIntoBuildPlan(t *testing.T) {
 	require.ErrorIs(t, tk.ExecToErr("IMPORT INTO t3 FROM select * from t2"),
 		infoschema.ErrTableNotExists)
 }
+
+func TestIssue42734(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+
+	tk.MustExec("use test")
+	tk.MustExec("CREATE TABLE temperature_data (temperature double);")
+	tk.MustExec("CREATE TABLE humidity_data (humidity double);")
+	tk.MustExec("CREATE TABLE weather_report (report_id double, report_date varchar(100));")
+
+	tk.MustExec("INSERT INTO temperature_data VALUES (1.0);")
+	tk.MustExec("INSERT INTO humidity_data VALUES (0.5);")
+	tk.MustExec("INSERT INTO weather_report VALUES (2.0, 'test');")
+
+	result := tk.MustQuery(`
+   SELECT
+     EXISTS (
+       SELECT
+         FIRST_VALUE(temp_data.temperature) OVER weather_window AS first_temperature,
+         MIN(report_data.report_id) OVER weather_window AS min_report_id
+       FROM
+         temperature_data AS temp_data
+       WINDOW weather_window AS (
+         PARTITION BY EXISTS (
+           SELECT
+             report_data.report_date AS report_date
+           FROM
+             humidity_data AS humidity_data
+           WHERE temp_data.temperature >= humidity_data.humidity
+         )
+       )
+     ) AS is_exist
+   FROM
+     weather_report AS report_data;
+ `)
+
+	result.Check(testkit.Rows("1"))
+
+	result = tk.MustQuery(`
+   SELECT
+     EXISTS (
+       SELECT
+         FIRST_VALUE(temp_data.temperature) OVER weather_window AS first_temperature,
+         MIN(report_data.report_id) OVER weather_window AS min_report_id
+       FROM
+         temperature_data AS temp_data
+       WINDOW weather_window AS (
+         PARTITION BY temp_data.temperature 
+       )
+     ) AS is_exist
+   FROM
+     weather_report AS report_data;
+ `)
+
+	result.Check(testkit.Rows("1"))
+}
