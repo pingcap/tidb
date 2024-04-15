658a94e194f0e70594a01b0d50b693041fcc0f98
guo-shaoge <shaoge1994@163.com>
2024-04-15 16:48:43 +0800

    fix
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- pkg/executor/explain.go
+++ pkg/executor/explain.go
@@ -105,13 +105,6 @@ func (e *ExplainExec) executeAnalyzeExec(ctx context.Context) (err error) {
 					err = err1
 				}
 			}
-			if e.explain.Analyze && e.analyzeExec != nil && e.executed {
-				ruDetailsRaw := ctx.Value(clientutil.RUDetailsCtxKey)
-				if coll := e.Ctx().GetSessionVars().StmtCtx.RuntimeStatsColl; coll != nil && ruDetailsRaw != nil {
-					ruDetails := ruDetailsRaw.(*clientutil.RUDetails)
-					coll.RegisterStats(e.explain.TargetPlan.ID(), &ruRuntimeStats{ruDetails})
-				}
-			}
 		}()
 		if minHeapInUse, alarmRatio := e.Ctx().GetSessionVars().MemoryDebugModeMinHeapInUse, e.Ctx().GetSessionVars().MemoryDebugModeAlarmRatio; minHeapInUse != 0 && alarmRatio != 0 {
 			memoryDebugModeCtx, cancel := context.WithCancel(ctx)
@@ -140,14 +133,14 @@ func (e *ExplainExec) executeAnalyzeExec(ctx context.Context) (err error) {
 			}
 		}
 	}
-	// // Register the RU runtime stats to the runtime stats collection after the analyze executor has been executed.
-	// if e.explain.Analyze && e.analyzeExec != nil && e.executed {
-	// 	ruDetailsRaw := ctx.Value(clientutil.RUDetailsCtxKey)
-	// 	if coll := e.Ctx().GetSessionVars().StmtCtx.RuntimeStatsColl; coll != nil && ruDetailsRaw != nil {
-	// 		ruDetails := ruDetailsRaw.(*clientutil.RUDetails)
-	// 		coll.RegisterStats(e.explain.TargetPlan.ID(), &ruRuntimeStats{ruDetails})
-	// 	}
-	// }
+	// Register the RU runtime stats to the runtime stats collection after the analyze executor has been executed.
+	if e.explain.Analyze && e.analyzeExec != nil && e.executed {
+		ruDetailsRaw := ctx.Value(clientutil.RUDetailsCtxKey)
+		if coll := e.Ctx().GetSessionVars().StmtCtx.RuntimeStatsColl; coll != nil && ruDetailsRaw != nil {
+			ruDetails := ruDetailsRaw.(*clientutil.RUDetails)
+			coll.RegisterStats(e.explain.TargetPlan.ID(), &ruRuntimeStats{ruDetails})
+		}
+	}
 	return err
 }
 
