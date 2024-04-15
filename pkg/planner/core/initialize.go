e754148ed49eab1f174e2afbcd21ab32969b4ae1
AilinKid <314806019@qq.com>
2024-04-15 22:30:16 +0800

    remove internal pkg and move base code to certain place
    Signed-off-by: AilinKid <314806019@qq.com>

--- pkg/planner/core/initialize.go
+++ pkg/planner/core/initialize.go
@@ -18,7 +18,7 @@ import (
 	"github.com/pingcap/tidb/pkg/expression"
 	"github.com/pingcap/tidb/pkg/kv"
 	base2 "github.com/pingcap/tidb/pkg/planner/core/base"
-	"github.com/pingcap/tidb/pkg/planner/core/internal/base"
+	"github.com/pingcap/tidb/pkg/planner/core/operator/base"
 	"github.com/pingcap/tidb/pkg/planner/property"
 	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/util/plancodec"
