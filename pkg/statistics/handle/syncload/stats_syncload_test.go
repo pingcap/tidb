59d38e63eb1e2b747123992832acfe21dcf49eee
Yiding Cui <winoros@gmail.com>
2024-04-15 23:39:51 +0800

    fix unexpected reset

--- pkg/statistics/handle/syncload/stats_syncload_test.go
+++ pkg/statistics/handle/syncload/stats_syncload_test.go
@@ -186,6 +186,8 @@ func TestConcurrentLoadHistWithPanicAndFail(t *testing.T) {
 			inTerms:  "return(true)",
 		},
 	}
+	sc := testKit.Session().(sessionctx.Context).GetSessionVars().StmtCtx
+	sc.SetTypeFlags(sc.TypeFlags().WithIgnoreInvalidDateErr(true).WithIgnoreZeroInDate(true))
 
 	for _, fp := range failpoints {
 		// clear statsCache
