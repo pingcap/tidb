27888dff3d519bc06d5159e81d27e22f5c25cf85
山岚 <36239017+YuJuncen@users.noreply.github.com>
2024-04-18 15:03:14 +0800

    This is an automated cherry-pick of #52671
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- br/pkg/streamhelper/advancer.go
+++ br/pkg/streamhelper/advancer.go
@@ -712,3 +712,12 @@ func (c *CheckpointAdvancer) asyncResolveLocksForRanges(ctx context.Context, tar
 		c.inResolvingLock.Store(false)
 	}()
 }
+
+func (c *CheckpointAdvancer) TEST_registerCallbackForSubscriptions(f func()) int {
+	cnt := 0
+	for _, sub := range c.subscriber.subscriptions {
+		sub.onDaemonExit = f
+		cnt += 1
+	}
+	return cnt
+}
