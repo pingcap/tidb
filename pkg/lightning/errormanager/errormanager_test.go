51791a328c97b130dd15c4428079810d590cd75a
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-04-18 17:07:37 +0800

    lightning/importinto: fix insert err after import for AUTO_ID_CACHE=1 and SHARD_ROW_ID_BITS (#52712) (#52724)
    close pingcap/tidb#52654

--- pkg/lightning/errormanager/errormanager_test.go
+++ pkg/lightning/errormanager/errormanager_test.go
@@ -215,7 +215,7 @@ func TestReplaceConflictOneKey(t *testing.T) {
 		State:      model.StatePublic,
 	}
 
-	tbl, err := tables.TableFromMeta(tidbkv.NewPanickingAllocators(0), table)
+	tbl, err := tables.TableFromMeta(tidbkv.NewPanickingAllocators(table.SepAutoInc(), 0), table)
 	require.NoError(t, err)
 
 	sessionOpts := encode.SessionOptions{
@@ -397,7 +397,7 @@ func TestReplaceConflictOneUniqueKey(t *testing.T) {
 		State:      model.StatePublic,
 	}
 
-	tbl, err := tables.TableFromMeta(tidbkv.NewPanickingAllocators(0), table)
+	tbl, err := tables.TableFromMeta(tidbkv.NewPanickingAllocators(table.SepAutoInc(), 0), table)
 	require.NoError(t, err)
 
 	sessionOpts := encode.SessionOptions{
