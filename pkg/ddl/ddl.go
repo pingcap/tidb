3fe531edc734f498787150085eaf95115b26a0d1
D3Hunter <jujj603@gmail.com>
2024-04-18 16:12:50 +0800

    change

--- pkg/ddl/ddl.go
+++ pkg/ddl/ddl.go
@@ -368,7 +368,7 @@ type ddlCtx struct {
 	stateSyncer  syncer.StateSyncer
 	ddlJobDoneCh chan struct{}
 	ddlEventCh   chan<- *statsutil.DDLEvent
-	lease        time.Duration        // lease is schema lease.
+	lease        time.Duration        // lease is schema lease, default 45s.
 	binlogCli    *pumpcli.PumpsClient // binlogCli is used for Binlog.
 	infoCache    *infoschema.InfoCache
 	statsHandle  *handle.Handle
