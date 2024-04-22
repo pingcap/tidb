05767c8bc18bfa264b5fdb2c4f91423050a2b07b
tiancaiamao <tiancaiamao@gmail.com>
2024-04-22 14:50:12 +0800

    make lint happy

--- pkg/ddl/db_integration_test.go
+++ pkg/ddl/db_integration_test.go
@@ -3048,7 +3048,6 @@ func TestIssue52680(t *testing.T) {
 		{sql: "drop table issue52680", expect: meta.AutoIDGroup{RowID: 0, IncrementID: 0, RandomID: 0}},
 		{sql: "recover table issue52680", expect: meta.AutoIDGroup{RowID: 0, IncrementID: 4000, RandomID: 0}},
 	}
-
 	for _, step := range testSteps {
 		if step.sql != "" {
 			tk.MustExec(step.sql)
