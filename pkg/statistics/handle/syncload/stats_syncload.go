51510d87b9e64b3132a948c21a6f98be683dbb30
Weizhen Wang <wangweizhen@pingcap.com>
2024-04-23 12:21:15 +0800

    *: avoid concurrently using the session in the syncload
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/syncload/stats_syncload.go
+++ pkg/statistics/handle/syncload/stats_syncload.go
@@ -231,7 +231,7 @@ func (s *statsSyncLoad) HandleOneTask(sctx sessionctx.Context, lastTask *statsty
 	}
 	result := stmtctx.StatsLoadResult{Item: task.Item.TableItemID}
 	resultChan := s.StatsLoad.Singleflight.DoChan(task.Item.Key(), func() (any, error) {
-		err := s.handleOneItemTask(sctx, task)
+		err := s.handleOneItemTask(task)
 		return nil, err
 	})
 	timeout := time.Until(task.ToTimeout)
@@ -264,14 +264,22 @@ func isVaildForRetry(task *statstypes.NeededItemTask) bool {
 	return task.Retry <= RetryCount
 }
 
-func (s *statsSyncLoad) handleOneItemTask(sctx sessionctx.Context, task *statstypes.NeededItemTask) (err error) {
+func (s *statsSyncLoad) handleOneItemTask(task *statstypes.NeededItemTask) (err error) {
+	se, err := s.statsHandle.SPool().Get()
+	if err != nil {
+		return err
+	}
 	defer func() {
 		// recover for each task, worker keeps working
 		if r := recover(); r != nil {
 			logutil.BgLogger().Error("handleOneItemTask panicked", zap.Any("recover", r), zap.Stack("stack"))
 			err = errors.Errorf("stats loading panicked: %v", r)
 		}
+		if err == nil { // only recycle when no error
+			s.statsHandle.SPool().Put(se)
+		}
 	}()
+	sctx := se.(sessionctx.Context)
 	item := task.Item.TableItemID
 	tbl, ok := s.statsHandle.Get(item.TableID)
 	if !ok {
