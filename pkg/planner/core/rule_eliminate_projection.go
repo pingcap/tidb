111abf32b4e6a2b0546320fe82f7c6ecf949b0bd
yibin <huyibin@pingcap.com>
2024-04-24 11:40:30 +0800

    Add new todo comment
    Signed-off-by: yibin <huyibin@pingcap.com>

--- pkg/planner/core/rule_eliminate_projection.go
+++ pkg/planner/core/rule_eliminate_projection.go
@@ -141,6 +141,7 @@ func doPhysicalProjectionElimination(p base.PhysicalPlan) base.PhysicalPlan {
 	child := p.Children()[0]
 	if childProj, ok := child.(*PhysicalProjection); ok {
 		// when current projection is an empty projection(schema pruned by column pruner), no need to reset child's schema
+		// TODO: avoid producing empty projection in column pruner.
 		if p.Schema().Len() != 0 {
 			childProj.SetSchema(p.Schema())
 		}
