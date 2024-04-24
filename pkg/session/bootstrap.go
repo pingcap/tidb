cb9cf9abbf6c67c8fd3dbec2bfdaf0628ab13b03
ywqzzy <592838129@qq.com>
2024-04-24 11:00:51 +0800

    u

--- pkg/session/bootstrap.go
+++ pkg/session/bootstrap.go
@@ -604,7 +604,7 @@ const (
 		meta LONGBLOB,
 		concurrency INT(11),
 		step INT(11),
-		target_scope VARCHAR(256),
+		target_scope VARCHAR(256) DEFAULT "",
 		error BLOB,
 		key(state),
       	UNIQUE KEY task_key(task_key)
