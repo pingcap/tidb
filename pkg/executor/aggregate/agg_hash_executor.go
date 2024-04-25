3e8ea59c1af162d230f235791efffa769649c658
wshwsh12 <793703860@qq.com>
2024-04-25 11:57:03 +0800

    add

--- pkg/executor/aggregate/agg_hash_executor.go
+++ pkg/executor/aggregate/agg_hash_executor.go
@@ -212,6 +212,8 @@ func (e *HashAggExec) Close() error {
 			e.parallelAggSpillAction = nil
 			e.spillHelper.close()
 		}
+		e.finalWorkers = nil
+		e.partialWorkers = nil
 	}
 
 	err := e.BaseExecutor.Close()
