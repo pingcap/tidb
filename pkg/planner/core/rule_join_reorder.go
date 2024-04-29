247039a1746c4cd4d84efc49ea73a93819ec63f3
qw4990 <zhangyuanjia@pingcap.com>
2024-04-29 18:25:36 +0800

    fixup

--- pkg/planner/core/rule_join_reorder.go
+++ pkg/planner/core/rule_join_reorder.go
@@ -515,6 +515,8 @@ func (s *baseSingleGroupJoinOrderSolver) checkConnection(leftPlan, rightPlan bas
 			} else {
 				newSf := expression.NewFunctionInternal(s.ctx.GetExprCtx(), ast.EQ, edge.GetType(), rCol, lCol).(*expression.ScalarFunction)
 
+				// after creating the new EQ function, the 2 args might not be column anymore, which breaks the assumption that
+				// join eq keys must be `col=col`, to handle this, inject 2 projections.
 				_, isCol0 := newSf.GetArgs()[0].(*expression.Column)
 				_, isCol1 := newSf.GetArgs()[1].(*expression.Column)
 				if !isCol0 || !isCol1 {
