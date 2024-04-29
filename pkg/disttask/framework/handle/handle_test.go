62f49e3b981ac028b66f9128f2b643b8662b35ed
ywqzzy <592838129@qq.com>
2024-04-29 12:34:44 +0800

    u

--- pkg/disttask/framework/handle/handle_test.go
+++ pkg/disttask/framework/handle/handle_test.go
@@ -74,7 +74,6 @@ func TestHandle(t *testing.T) {
 
 	task, err = handle.SubmitTask(ctx, "2", proto.TaskTypeExample, 2, proto.EmptyMeta)
 	require.NoError(t, err)
-	require.Equal(t, int64(2), task.ID)
 	require.Equal(t, "2", task.Key)
 
 	// submit same task.
@@ -88,7 +87,6 @@ func TestHandle(t *testing.T) {
 	// submit task with same key
 	task, err = handle.SubmitTask(ctx, "3", proto.TaskTypeExample, 2, proto.EmptyMeta)
 	require.NoError(t, err)
-	require.Equal(t, int64(3), task.ID)
 	require.NoError(t, mgr.TransferTasks2History(ctx, []*proto.Task{task}))
 	task, err = handle.SubmitTask(ctx, "3", proto.TaskTypeExample, 2, proto.EmptyMeta)
 	require.Nil(t, task)
