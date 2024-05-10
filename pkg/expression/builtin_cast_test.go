f0a66f0ef42786847cf8a466dd019540ed3196f5
Yang Keao <yangkeao@chunibyo.icu>
2024-05-10 13:09:17 +0800

    fix charset conversion warning/error
    Signed-off-by: Yang Keao <yangkeao@chunibyo.icu>

--- pkg/expression/builtin_cast_test.go
+++ pkg/expression/builtin_cast_test.go
@@ -1448,7 +1448,7 @@ func TestWrapWithCastAsString(t *testing.T) {
 
 	cases := []struct {
 		expr Expression
-		err  bool
+		warn bool
 		ret  string
 	}{
 		{
@@ -1487,11 +1487,14 @@ func TestWrapWithCastAsString(t *testing.T) {
 			"-127",
 		},
 	}
+	lastWarningLen := 0
 	for _, c := range cases {
 		expr := BuildCastFunction(ctx, c.expr, types.NewFieldType(mysql.TypeVarString))
-		res, _, err := expr.EvalString(ctx, chunk.Row{})
-		if c.err {
-			require.Error(t, err)
+		res, _, _ := expr.EvalString(ctx, chunk.Row{})
+		if c.warn {
+			warns := ctx.GetSessionVars().StmtCtx.GetWarnings()
+			require.Greater(t, len(warns), lastWarningLen)
+			lastWarningLen = len(warns)
 		} else {
 			require.Equal(t, c.ret, res)
 		}
