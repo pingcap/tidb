10a3b208b35b9d6af89caa9e6138b5889d6d0a11
yibin <huyibin@pingcap.com>
2024-05-10 11:01:09 +0800

    This is an automated cherry-pick of #53097
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/executor/builder.go
+++ pkg/executor/builder.go
@@ -2958,6 +2958,21 @@ func (*executorBuilder) corColInDistPlan(plans []plannercore.PhysicalPlan) bool 
 					return true
 				}
 			}
+<<<<<<< HEAD
+=======
+		case *plannercore.PhysicalProjection:
+			for _, expr := range x.Exprs {
+				if len(expression.ExtractCorColumns(expr)) > 0 {
+					return true
+				}
+			}
+		case *plannercore.PhysicalTopN:
+			for _, byItem := range x.ByItems {
+				if len(expression.ExtractCorColumns(byItem.Expr)) > 0 {
+					return true
+				}
+			}
+>>>>>>> 203e5f2cff1 (executor: Fix push downed topN won't replace correlated column problem (#53097))
 		case *plannercore.PhysicalTableScan:
 			for _, cond := range x.LateMaterializationFilterCondition {
 				if len(expression.ExtractCorColumns(cond)) > 0 {
