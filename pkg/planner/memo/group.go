c9a2d48bdd4b6f40846b03ec23c07720d99f79ba
AilinKid <314806019@qq.com>
2024-05-12 22:40:12 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

--- pkg/planner/memo/group.go
+++ pkg/planner/memo/group.go
@@ -19,12 +19,12 @@ import (
 	"fmt"
 
 	"github.com/pingcap/tidb/pkg/expression"
-	"github.com/pingcap/tidb/pkg/planner/util/utilfuncp"
 	// import core pkg first to call its init func.
 	_ "github.com/pingcap/tidb/pkg/planner/core"
 	"github.com/pingcap/tidb/pkg/planner/core/base"
 	"github.com/pingcap/tidb/pkg/planner/pattern"
 	"github.com/pingcap/tidb/pkg/planner/property"
+	"github.com/pingcap/tidb/pkg/planner/util/utilfuncp"
 )
 
 // ExploreMark is uses to mark whether a Group or GroupExpr has
