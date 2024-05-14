3c6725cfa69cfc82293bafce2121cf86deca1893
Mattias Jonsson <mattias.jonsson@pingcap.com>
2024-05-14 17:21:36 +0200

    Put back a line that was mistakenly removed

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -1461,6 +1461,7 @@ var (
 	ColumnElementKey ElementKeyType = []byte("_col_")
 	// IndexElementKey is the key for index element.
 	IndexElementKey ElementKeyType = []byte("_idx_")
+)
 
 const elementKeyLen = 5
 
