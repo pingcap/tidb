d774c7399c3143771e41de4b8808fb30fa9683cd
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-05-15 12:47:42 +0800

    expression: fix charset conversion warning and error behavior (#51191) (#53227)
    close pingcap/tidb#50295

--- pkg/expression/builtin_cast_test.go
+++ pkg/expression/builtin_cast_test.go
@@ -1393,7 +1393,7 @@ func TestWrapWithCastAsString(t *testing.T) {
 
 	cases := []struct {
 		expr Expression
-		err  bool
+		warn bool
 		ret  string
 	}{
 		{
@@ -1432,11 +1432,14 @@ func TestWrapWithCastAsString(t *testing.T) {
 			"-127",
 		},
 	}
+	lastWarningLen := 0
 	for _, c := range cases {
 		expr := BuildCastFunction(ctx, c.expr, types.NewFieldType(mysql.TypeVarString))
-		res, _, err := expr.EvalString(ctx, chunk.Row{})
-		if c.err {
-			require.Error(t, err)
+		res, _, _ := expr.EvalString(ctx, chunk.Row{})
+		if c.warn {
+			warns := ctx.GetSessionVars().StmtCtx.GetWarnings()
+			require.Greater(t, len(warns), lastWarningLen)
+			lastWarningLen = len(warns)
 		} else {
 			require.Equal(t, c.ret, res)
 		}
