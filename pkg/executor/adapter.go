bcbcae0d0c9f421661190dd4f596edb2b3e7fb6a
xhe <xw897002528@gmail.com>
2024-05-17 13:32:16 +0800

    *: only output log when error is non-nil
    Signed-off-by: xhe <xw897002528@gmail.com>

--- pkg/executor/adapter.go
+++ pkg/executor/adapter.go
@@ -1855,9 +1855,11 @@ func (a *ExecStmt) SummaryStmt(succ bool) {
 	planGenerator := func() (p string, h string, e any) {
 		defer func() {
 			e = recover()
-			logutil.BgLogger().Warn("fail to generate plan info",
-				zap.Stack("backtrace"),
-				zap.Any("error", e))
+			if e != nil {
+				logutil.BgLogger().Warn("fail to generate plan info",
+					zap.Stack("backtrace"),
+					zap.Any("error", e))
+			}
 		}()
 		p, h = getEncodedPlan(stmtCtx, !sessVars.InRestrictedSQL)
 		return
