4014a25d3dfeb7a2f30cbe28d5280211f55b51d8
xufei <xufeixw@mail.ustc.edu.cn>
2024-05-17 16:37:24 +0800

    avoid data race
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- pkg/executor/builder.go
+++ pkg/executor/builder.go
@@ -1664,7 +1664,7 @@ func (b *executorBuilder) buildHashJoinV2(v *plannercore.PhysicalHashJoin) exec.
 }
 
 func (b *executorBuilder) buildHashJoin(v *plannercore.PhysicalHashJoin) exec.Executor {
-	if join.EnableHashJoinV2 && v.CanUseHashJoinV2() {
+	if join.EnableHashJoinV2.Load() && v.CanUseHashJoinV2() {
 		return b.buildHashJoinV2(v)
 	}
 	leftExec := b.build(v.Children()[0])
