6f096d7bde545b93042fb19e787f5050da83fce9
Mattias Jonsson <mattias.jonsson@pingcap.com>
2024-05-17 10:38:39 +0200

    Updated tests

--- pkg/ddl/tests/partition/db_partition_test.go
+++ pkg/ddl/tests/partition/db_partition_test.go
@@ -3470,8 +3470,8 @@ func TestReorgPartitionGlobalIndex(t *testing.T) {
 	}()
 	tk.MustExec("drop table if exists t")
 	// TODO: Also test with extended primary key!
-	tk.MustExec(`create table t ( a int, b int, c int)
-	partition by range( a ) (
+	tk.MustExec(`create table t (a int, b int, c int)
+	partition by range (a) (
 		partition p1 values less than (10),
 		partition p2 values less than (20)
 	)`)
@@ -3526,11 +3526,11 @@ func TestReorgPartitionGlobalIndex(t *testing.T) {
 	idxInfo = tt.Meta().FindIndexByName("idx_c")
 	require.True(t, idxInfo.Global)
 	require.True(t, idxInfo.Unique)
-	tk.MustExec(`alter table t partition by hash (a) partitions 3`)
+	tk.MustExec(`alter table t partition by hash (b) partitions 3`)
 	tt = external.GetTableByName(t, tk, "test", "t")
 	require.Equal(t, 2, len(tt.Meta().Indices))
 	idxInfo = tt.Meta().FindIndexByName("idx_b")
-	require.True(t, idxInfo.Global)
+	require.False(t, idxInfo.Global)
 	require.True(t, idxInfo.Unique)
 	idxInfo = tt.Meta().FindIndexByName("idx_c")
 	require.True(t, idxInfo.Global)
