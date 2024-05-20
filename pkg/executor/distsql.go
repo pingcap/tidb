57e65e6903a3921aa9885fe39658630c89f468ef
you06 <you1474600@gmail.com>
2024-05-20 22:52:22 +0900

    remove ballast memory in executor
    Signed-off-by: you06 <you1474600@gmail.com>

--- pkg/executor/distsql.go
+++ pkg/executor/distsql.go
@@ -18,7 +18,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"runtime"
 	"runtime/trace"
 	"slices"
 	"sort"
@@ -701,10 +700,6 @@ func (e *IndexLookUpExecutor) startIndexWorker(ctx context.Context, workCh chan<
 	idxID := e.getIndexPlanRootID()
 	e.idxWorkerWg.Add(1)
 	go func() {
-		// 32KB ballast helps grow the stack to the requirement of index worker.
-		// This reduces the `morestack` call during the execution, thus improvement the efficiency of TiDB.
-		// TODO: remove ballast after global pool is applied
-		ballast := make([]byte, 32*size.KB)
 		defer trace.StartRegion(ctx, "IndexLookUpIndexWorker").End()
 		worker := &indexWorker{
 			idxLookup:       e,
@@ -782,7 +777,6 @@ func (e *IndexLookUpExecutor) startIndexWorker(ctx context.Context, workCh chan<
 		close(workCh)
 		close(e.resultCh)
 		e.idxWorkerWg.Done()
-		runtime.KeepAlive(ballast)
 	}()
 	return nil
 }
@@ -1194,7 +1188,6 @@ func (w *tableWorker) pickAndExecTask(ctx context.Context) {
 			task.doneCh <- errors.Errorf("%v", r)
 		}
 	}()
-	var ballast []byte
 	for {
 		// Don't check ctx.Done() on purpose. If background worker get the signal and all
 		// exit immediately, session's goroutine doesn't know this and still calling Next(),
@@ -1207,13 +1200,6 @@ func (w *tableWorker) pickAndExecTask(ctx context.Context) {
 		case <-w.finished:
 			return
 		}
-		if ballast == nil {
-			// 32KB ballast helps grow the stack to the requirement of table worker.
-			// This reduces the `morestack` call during the execution, thus improvement the efficiency of TiDB.
-			// Only allocate ballast when task is received.
-			// TODO: remove ballast after global pool is applied
-			ballast = make([]byte, 32*size.KB)
-		}
 		startTime := time.Now()
 		err := w.executeTask(ctx, task)
 		if w.idxLookup.stats != nil {
@@ -1222,7 +1208,6 @@ func (w *tableWorker) pickAndExecTask(ctx context.Context) {
 		}
 		task.doneCh <- err
 	}
-	runtime.KeepAlive(ballast)
 }
 
 func (e *IndexLookUpExecutor) getHandle(row chunk.Row, handleIdx []int,
