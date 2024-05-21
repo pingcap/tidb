8f21019eee545f6be8e052c41f2b2bdc2c79336f
y_static_y@sina.com <y_static_y@sina.com>
2024-05-21 19:21:47 +0800

    support skip first wait in GC
    Signed-off-by: y_static_y@sina.com <y_static_y@sina.com>

--- pkg/store/gcworker/gc_worker.go
+++ pkg/store/gcworker/gc_worker.go
@@ -32,7 +32,6 @@ import (
 	"github.com/pingcap/kvproto/pkg/errorpb"
 	"github.com/pingcap/kvproto/pkg/kvrpcpb"
 	"github.com/pingcap/kvproto/pkg/metapb"
-	"github.com/pingcap/tidb/pkg/config"
 	"github.com/pingcap/tidb/pkg/ddl"
 	"github.com/pingcap/tidb/pkg/ddl/label"
 	"github.com/pingcap/tidb/pkg/ddl/placement"
@@ -425,10 +424,10 @@ func (w *GCWorker) leaderTick(ctx context.Context) error {
 }
 
 func (w *GCWorker) isNeedToWait() bool {
-	if config.GetGlobalConfig().EnableGCFastStart {
-		return time.Since(w.lastFinish) < gcWaitTime && w.isFirstTickFinished
-	}
-	return time.Since(w.lastFinish) < gcWaitTime
+	//if config.GetGlobalConfig().EnableGCFastStart {
+	return time.Since(w.lastFinish) < gcWaitTime && w.isFirstTickFinished
+	//}
+	//return time.Since(w.lastFinish) < gcWaitTime
 }
 
 func (w *GCWorker) runKeyspaceGCJob(ctx context.Context, concurrency int) error {
