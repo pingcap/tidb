a1c36c241a9a8541c1ae90135fc936f4e4d579ca
Weizhen Wang <wangweizhen@pingcap.com>
2024-05-23 19:28:18 +0800

    This is an automated cherry-pick of #53501
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- util/ranger/ranger.go
+++ util/ranger/ranger.go
@@ -169,8 +169,8 @@ func convertPoint(sctx sessionctx.Context, point *point, tp *types.FieldType) (*
 			// see issue #20101: overflow when converting integer to year
 		} else if tp.GetType() == mysql.TypeBit && terror.ErrorEqual(err, types.ErrDataTooLong) {
 			// see issue #19067: we should ignore the types.ErrDataTooLong when we convert value to TypeBit value
-		} else if (tp.GetType() == mysql.TypeNewDecimal || tp.GetType() == mysql.TypeLonglong) && terror.ErrorEqual(err, types.ErrOverflow) {
-			// Ignore the types.ErrOverflow when we convert TypeNewDecimal/TypeLonglong values.
+		} else if (newTp.GetType() == mysql.TypeNewDecimal || mysql.IsIntegerType(newTp.GetType()) || newTp.GetType() == mysql.TypeFloat) && terror.ErrorEqual(err, types.ErrOverflow) {
+			// Ignore the types.ErrOverflow when we convert TypeNewDecimal/TypeTiny/TypeShort/TypeInt24/TypeLong/TypeLonglong/TypeFloat values.
 			// A trimmed valid boundary point value would be returned then. Accordingly, the `excl` of the point
 			// would be adjusted. Impossible ranges would be skipped by the `validInterval` call later.
 			// tests in TestIndexRange/TestIndexRangeForDecimal
