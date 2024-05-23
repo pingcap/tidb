917ac2014765ccdf4e3561ab03bca0c5a449f0e6
terry1purcell <terry.purcell@pingcap.com>
2024-05-22 21:09:50 -0500

    review testing 25

--- pkg/statistics/builder.go
+++ pkg/statistics/builder.go
@@ -296,6 +296,10 @@ func BuildHistAndTopN(
 	sampleNum := int64(len(samples))
 	// As we use samples to build the histogram, the bucket number and repeat should multiply a factor.
 	sampleFactor := float64(count) / float64(len(samples))
+	doNotPrune := false
+	if numTopN != 100 {
+		doNotPrune = true
+	}
 
 	// Step1: collect topn from samples
 
@@ -327,7 +331,7 @@ func BuildHistAndTopN(
 		}
 		// case 2, meet a different value: counting for the "current" is complete
 		// case 2-1, do not add a count of 1 if we're sampling
-		if curCnt == 1 && sampleFactor > 1 {
+		if curCnt == 1 && (sampleFactor > 1 || doNotPrune) {
 			cur, curCnt = sampleBytes, 1
 			continue
 		}
@@ -365,7 +369,7 @@ func BuildHistAndTopN(
 
 	// Handle the counting for the last value. Basically equal to the case 2 above.
 	// now topn is empty: append the "current" count directly
-	if numTopN != 0 && (sampleFactor <= 1 || curCnt > 1) {
+	if numTopN != 0 && (doNotPrune || sampleFactor <= 1 || curCnt > 1) {
 		if len(topNList) == 0 {
 			topNList = append(topNList, TopNMeta{Encoded: cur, Count: uint64(curCnt)})
 		} else if len(topNList) < numTopN || uint64(curCnt) > topNList[len(topNList)-1].Count {
@@ -385,7 +389,9 @@ func BuildHistAndTopN(
 		}
 	}
 
-	topNList = pruneTopNItem(topNList, ndv, nullCount, sampleNum, count)
+	if !doNotPrune {
+		topNList = pruneTopNItem(topNList, ndv, nullCount, sampleNum, count)
+	}
 
 	// Step2: exclude topn from samples
 	if numTopN != 0 {
