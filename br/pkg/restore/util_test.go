ecf90607d378f765848c0c4e80f7f6cb93198969
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-05-24 11:49:18 +0800

    txn: restore failed due to file end key is empty (#52572) (#53449)
    close pingcap/tidb#52574

--- br/pkg/restore/util_test.go
+++ br/pkg/restore/util_test.go
@@ -22,6 +22,65 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestGetKeyRangeByMode(t *testing.T) {
+	file := &backuppb.File{
+		Name:     "file_write.sst",
+		StartKey: []byte("t1a"),
+		EndKey:   []byte("t1ccc"),
+	}
+	endFile := &backuppb.File{
+		Name:     "file_write.sst",
+		StartKey: []byte("t1a"),
+		EndKey:   []byte(""),
+	}
+	rule := &restore.RewriteRules{
+		Data: []*import_sstpb.RewriteRule{
+			{
+				OldKeyPrefix: []byte("t1"),
+				NewKeyPrefix: []byte("t2"),
+			},
+		},
+	}
+	// raw kv
+	testRawFn := restore.GetKeyRangeByMode(restore.Raw)
+	start, end, err := testRawFn(file, rule)
+	require.NoError(t, err)
+	require.Equal(t, []byte("t1a"), start)
+	require.Equal(t, []byte("t1ccc"), end)
+
+	start, end, err = testRawFn(endFile, rule)
+	require.NoError(t, err)
+	require.Equal(t, []byte("t1a"), start)
+	require.Equal(t, []byte(""), end)
+
+	// txn kv: the keys must be encoded.
+	testTxnFn := restore.GetKeyRangeByMode(restore.Txn)
+	start, end, err = testTxnFn(file, rule)
+	require.NoError(t, err)
+	require.Equal(t, codec.EncodeBytes(nil, []byte("t1a")), start)
+	require.Equal(t, codec.EncodeBytes(nil, []byte("t1ccc")), end)
+
+	start, end, err = testTxnFn(endFile, rule)
+	require.NoError(t, err)
+	require.Equal(t, codec.EncodeBytes(nil, []byte("t1a")), start)
+	require.Equal(t, []byte(""), end)
+
+	// normal kv: the keys must be encoded.
+	testFn := restore.GetKeyRangeByMode(restore.TiDB)
+	start, end, err = testFn(file, rule)
+	require.NoError(t, err)
+	require.Equal(t, codec.EncodeBytes(nil, []byte("t2a")), start)
+	require.Equal(t, codec.EncodeBytes(nil, []byte("t2ccc")), end)
+
+	// TODO maybe fix later
+	// current restore does not support rewrite empty endkey.
+	// because backup guarantees that the end key is not empty.
+	// start, end, err = testFn(endFile, rule)
+	// require.NoError(t, err)
+	// require.Equal(t, codec.EncodeBytes(nil, []byte("t2a")), start)
+	// require.Equal(t, []byte(""), end)
+}
+
 func TestParseQuoteName(t *testing.T) {
 	schema, table := restore.ParseQuoteName("`a`.`b`")
 	require.Equal(t, "a", schema)
