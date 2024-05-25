2a690661dcf93b7b167e42b4340f9980280d570c
Mattias Jonsson <mjonss@gmail.com>
2024-05-25 08:43:20 +0100

    Linting

--- pkg/ddl/ddl_api.go
+++ pkg/ddl/ddl_api.go
@@ -3161,13 +3161,13 @@ func (d *ddl) FlashbackCluster(ctx sessionctx.Context, flashbackTS uint64) error
 		Args: []any{
 			flashbackTS,
 			map[string]any{},
-			true,           /* tidb_gc_enable */
-			variable.On,    /* tidb_enable_auto_analyze */
-			variable.Off,   /* tidb_super_read_only */
-			0,              /* totalRegions */
-			0,              /* startTS */
-			0,              /* commitTS */
-			variable.On,    /* tidb_ttl_job_enable */
+			true,         /* tidb_gc_enable */
+			variable.On,  /* tidb_enable_auto_analyze */
+			variable.Off, /* tidb_super_read_only */
+			0,            /* totalRegions */
+			0,            /* startTS */
+			0,            /* commitTS */
+			variable.On,  /* tidb_ttl_job_enable */
 			[]kv.KeyRange{} /* flashback key_ranges */},
 		CDCWriteSource: ctx.GetSessionVars().CDCWriteSource,
 		// FLASHBACK CLUSTER affects all schemas and tables.
