048d284c9a4fc2a685315168ae39b482116bd65f
Jack Yu <jackysp@gmail.com>
2024-05-30 21:12:57 +0800

    once more
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- server/conn.go
+++ server/conn.go
@@ -2058,6 +2058,7 @@ func (cc *clientConn) prefetchPointPlanKeys(ctx context.Context, stmts []ast.Stm
 		}
 		// TODO: the preprocess is run twice, we should find some way to avoid do it again.
 		if err = plannercore.Preprocess(ctx, cc.getCtx(), stmt); err != nil {
+			// error might happen, see https://github.com/pingcap/tidb/issues/39664
 			return nil, nil
 		}
 		p := plannercore.TryFastPlan(cc.ctx.Session, stmt)
