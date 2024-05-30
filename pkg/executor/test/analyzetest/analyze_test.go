74b7d4cb0c2fa3fb495e271392777027665c3f97
terry1purcell <terry.purcell@pingcap.com>
2024-05-30 15:43:17 -0700

    review may 30 test2

--- pkg/executor/test/analyzetest/analyze_test.go
+++ pkg/executor/test/analyzetest/analyze_test.go
@@ -1916,7 +1916,7 @@ func testKillAutoAnalyze(t *testing.T, ver int) {
 	if ver == 1 {
 		jobInfo += "columns"
 	} else {
-		jobInfo += "table all columns with 256 buckets, 500 topn, 1 samplerate"
+		jobInfo += "table all columns with 256 buckets, 100 topn, 1 samplerate"
 	}
 	// kill auto analyze when it is pending/running/finished
 	for _, status := range []string{
@@ -2041,7 +2041,7 @@ func TestAnalyzeJob(t *testing.T) {
 			DBName:        "test",
 			TableName:     "t",
 			PartitionName: "",
-			JobInfo:       "table all columns with 256 buckets, 500 topn, 1 samplerate",
+			JobInfo:       "table all columns with 256 buckets, 100 topn, 1 samplerate",
 		}
 		executor.AddNewAnalyzeJob(se, job)
 		require.NotNil(t, job.ID)
@@ -2133,7 +2133,7 @@ func TestInsertAnalyzeJobWithLongInstance(t *testing.T) {
 		DBName:        "test",
 		TableName:     "t",
 		PartitionName: "",
-		JobInfo:       "table all columns with 256 buckets, 500 topn, 1 samplerate",
+		JobInfo:       "table all columns with 256 buckets, 100 topn, 1 samplerate",
 	}
 	h := dom.StatsHandle()
 	instance := "xxxtidb-tidb-0.xxxtidb-tidb-peer.xxxx-xx-1234-xxx-123456-1-321.xyz:4000"
