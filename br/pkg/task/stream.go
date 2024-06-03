383116cd5bd031230850643b9b5823a6c2b7be9d
RidRisR <79858083+RidRisR@users.noreply.github.com>
2024-06-03 16:36:52 +0800

    refactor

--- br/pkg/task/stream.go
+++ br/pkg/task/stream.go
@@ -1412,10 +1412,6 @@ func restoreStream(
 	downstreamIdset := make(map[int64]struct{})
 	for upstreamId, rule := range rewriteRules {
 		downstreamId := restoreutils.GetRewriteTableID(upstreamId, rule)
-		info, ok := mgr.GetDomain().InfoSchema().TableInfoByID(downstreamId)
-		if ok && info.TiFlashReplica != nil && info.TiFlashReplica.Count > 0 {
-			return errors.Errorf("table %s has tiflash replica, please remove it before restore", info.Name)
-		}
 		idrules[upstreamId] = downstreamId
 		downstreamIdset[downstreamId] = struct{}{}
 	}
