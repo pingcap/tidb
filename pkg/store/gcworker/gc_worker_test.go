1c3c427f7cb812b9836530f0fac0affb6ab4a3d5
ystaticy <y_static_y@sina.com>
2024-06-06 15:26:56 +0800

    GC : fix issue of delete range could not be executed again if the cleaning rules failed (#53368)
    ref pingcap/tidb#53369

--- pkg/store/gcworker/gc_worker_test.go
+++ pkg/store/gcworker/gc_worker_test.go
@@ -688,6 +688,11 @@ func TestDeleteRangesFailure(t *testing.T) {
 				require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJobForGC"))
 			}()
 
+			require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJob", "return(\"schema/d1/t1\")"))
+			defer func() {
+				require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJob"))
+			}()
+
 			// Put some delete range tasks.
 			se := createSession(s.gcWorker.store)
 			defer se.Close()
@@ -846,6 +851,16 @@ Loop:
 
 func TestUnsafeDestroyRangeForRaftkv2(t *testing.T) {
 	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/ddl/util/IsRaftKv2", "return(true)"))
+
+	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJobForGC", "return(1)"))
+	defer func() {
+		require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJobForGC"))
+	}()
+
+	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJob", "return(\"schema/d1/t1\")"))
+	defer func() {
+		require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/store/gcworker/mockHistoryJob"))
+	}()
 
 	s := createGCWorkerSuite(t)
 	// Put some delete range tasks.
