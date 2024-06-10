35fe5da461d80788576c3d8f34516fbaa2f828d9
Weizhen Wang <wangweizhen@pingcap.com>
2024-06-10 12:45:18 +0800

    *
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/syncload/batch_read_storage.go
+++ pkg/statistics/handle/syncload/batch_read_storage.go
@@ -18,8 +18,12 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/sessionctx"
+	"github.com/pingcap/tidb/pkg/statistics"
 	"github.com/pingcap/tidb/pkg/statistics/handle/util"
+	"github.com/pingcap/tidb/pkg/types"
+	"github.com/pingcap/tidb/pkg/util/chunk"
 )
 
 func generateMetaPredict(conditions []*batchSyncLoadTask) string {
@@ -48,4 +52,58 @@ func BatchHistMetaFromStorageWithHighPriority(sctx sessionctx.Context, bc batchC
 	if len(rows) == 0 {
 		return false, nil
 	}
+	for _, row := range rows {
+		id := rows[0].GetInt64(8)
+		hist_id := rows[0].GetInt64(9)
+		is_index := rows[0].GetInt64(10)
+		hist := statistics.NewHistogram(id, rows[0].GetInt64(0), rows[0].GetInt64(2), rows[0].GetUint64(1), tp, chunk.InitialCapacity, rows[0].GetInt64(3))
+		hist.Correlation = rows[0].GetFloat64(5)
+		lastPos := rows[0].GetDatum(7, types.NewFieldType(mysql.TypeBlob))
+
+		lastAnalyzePos := &lastPos
+		statsVer := rows[0].GetInt64(4)
+		flag := rows[0].GetInt64(6)
+		if is_index == 1 {
+			idxHist := &statistics.Index{
+				Histogram:  *hist,
+				CMSketch:   nil,
+				TopN:       nil,
+				FMSketch:   nil,
+				Info:       nil,
+				StatsVer:   statsVer,
+				Flag:       flag,
+				PhysicalID: id,
+			}
+			if statsVer != statistics.Version0 {
+				if fullLoad {
+					idxHist.StatsLoadedStatus = statistics.NewStatsFullLoadStatus()
+				} else {
+					idxHist.StatsLoadedStatus = statistics.NewStatsAllEvictedStatus()
+				}
+			}
+			lastAnalyzePos.Copy(&idxHist.LastAnalyzePos)
+		} else {
+			colHist := &statistics.Column{
+				PhysicalID: id,
+				Histogram:  *hist,
+				Info:       nil,
+				CMSketch:   nil,
+				TopN:       nil,
+				FMSketch:   nil,
+				IsHandle:   isPkIsHandle && mysql.HasPriKeyFlag(w.colInfo.GetFlag()),
+				StatsVer:   statsVer,
+			}
+			if colHist.StatsAvailable() {
+				if fullLoad {
+					colHist.StatsLoadedStatus = statistics.NewStatsFullLoadStatus()
+				} else {
+					colHist.StatsLoadedStatus = statistics.NewStatsAllEvictedStatus()
+				}
+			}
+			w.col = colHist
+		}
+		bc.tables[id]
+
+	}
+
 }
