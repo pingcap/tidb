4cff319c8d5c667f942411dce16e75e7e6681dcf
qw4990 <zhangyuanjia@pingcap.com>
2024-06-11 16:38:19 +0800

    fixup

--- pkg/planner/core/common_unity_test.go
+++ pkg/planner/core/common_unity_test.go
@@ -23,6 +23,24 @@ func TestUnity(t *testing.T) {
 	formatPrint(tk, `explain format='unity' select a from t5`)
 }
 
+func TestUnityMCV(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec(`create table t1 (a int, b int, c int, key(a))`)
+	for i := 0; i < 1024; i++ {
+		tk.MustExec(fmt.Sprintf(`insert into t1 values (%v, %v, %v)`, i, i, i))
+	}
+	for i := 0; i < 10; i++ {
+		for j := 0; j <= i; j++ {
+			tk.MustExec(fmt.Sprintf(`insert into t1 values (%v, %v, %v)`, i, i, i))
+		}
+	}
+	tk.MustExec(`analyze table t1 with 10 topn`)
+	formatPrint(tk, `explain format='unity' select a from t1`)
+	formatPrint(tk, `explain format='unity' select a from t1`)
+}
+
 func formatPrint(tk *testkit.TestKit, sql string) {
 	data := tk.MustQuery(sql).Rows()[0][0]
 	jsonData := data.(string)
