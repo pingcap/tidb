bf79dc67c800d0d17fbc1871faaf3ad7b222decc
BornChanger <97348524+BornChanger@users.noreply.github.com>
2024-06-12 07:29:46 +0800

    Update advancer.go

--- br/pkg/streamhelper/advancer.go
+++ br/pkg/streamhelper/advancer.go
@@ -438,10 +438,6 @@ func (c *CheckpointAdvancer) onTaskEvent(ctx context.Context, e TaskEvent) error
 		}
 		log.Info("get global checkpoint", zap.Uint64("checkpoint", globalCheckpointTs))
 		c.lastCheckpoint = newCheckpointWithTS(globalCheckpointTs)
-		p, err := c.env.BlockGCUntil(ctx, globalCheckpointTs)
-		if err != nil {
-			log.Warn("failed to upload service GC safepoint, skipping.", logutil.ShortError(err))
-		}
 		log.Info("added event", zap.Stringer("task", e.Info),
 			zap.Stringer("ranges", logutil.StringifyKeys(c.taskRange)))
 	case EventDel:
