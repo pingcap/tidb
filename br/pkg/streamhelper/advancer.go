378db51a5c6fdcf17c4454b13192ad92ce5a20c9
ris <79858083+RidRisR@users.noreply.github.com>
2024-06-11 23:46:30 +0800

    This is an automated cherry-pick of #53695
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- br/pkg/streamhelper/advancer.go
+++ br/pkg/streamhelper/advancer.go
@@ -428,7 +428,24 @@ func (c *CheckpointAdvancer) onTaskEvent(ctx context.Context, e TaskEvent) error
 		c.task = e.Info
 		c.taskRange = spans.Collapse(len(e.Ranges), func(i int) kv.KeyRange { return e.Ranges[i] })
 		c.setCheckpoints(spans.Sorted(spans.NewFullWith(e.Ranges, 0)))
+<<<<<<< HEAD
 		c.lastCheckpoint = newCheckpointWithTS(e.Info.StartTs)
+=======
+		globalCheckpointTs, err := c.env.GetGlobalCheckpointForTask(ctx, e.Name)
+		if err != nil {
+			log.Error("failed to get global checkpoint, skipping.", logutil.ShortError(err))
+			return err
+		}
+		if globalCheckpointTs < c.task.StartTs {
+			globalCheckpointTs = c.task.StartTs
+		}
+		log.Info("get global checkpoint", zap.Uint64("checkpoint", globalCheckpointTs))
+		c.lastCheckpoint = newCheckpointWithTS(globalCheckpointTs)
+		p, err := c.env.BlockGCUntil(ctx, globalCheckpointTs)
+		if err != nil {
+			log.Warn("failed to upload service GC safepoint, skipping.", logutil.ShortError(err))
+		}
+>>>>>>> 64f5427448b (br: fix Log Backup unexpected paused when adding a already long-running task (#53695))
 		log.Info("added event", zap.Stringer("task", e.Info),
 			zap.Stringer("ranges", logutil.StringifyKeys(c.taskRange)))
 	case EventDel:
