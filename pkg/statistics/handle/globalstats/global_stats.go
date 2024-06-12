527a04aa9c162c15a45d3fcfcd680efeb9594be3
hi-rustin <rustin.liu@gmail.com>
2024-06-12 21:08:28 +0800

    statistics: update some vars for internal sessions correctly
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- pkg/statistics/handle/globalstats/global_stats.go
+++ pkg/statistics/handle/globalstats/global_stats.go
@@ -94,6 +94,7 @@ func MergePartitionStats2GlobalStats(
 	histIDs []int64,
 ) (globalStats *GlobalStats, err error) {
 	if sc.GetSessionVars().EnableAsyncMergeGlobalStats {
+		logutil.BgLogger().Info("use async merge global stats", zap.String("table", globalTableInfo.Name.L))
 		worker, err := NewAsyncMergePartitionStats2GlobalStats(statsHandle, globalTableInfo, histIDs, is)
 		if err != nil {
 			return nil, errors.Trace(err)
@@ -104,6 +105,7 @@ func MergePartitionStats2GlobalStats(
 		}
 		return worker.Result(), nil
 	}
+	logutil.BgLogger().Info("use blocking merge global stats", zap.String("table", globalTableInfo.Name.L))
 	return blockingMergePartitionStats2GlobalStats(sc, statsHandle.GPool(), opts, is, globalTableInfo, isIndex, histIDs, nil, statsHandle)
 }
 
