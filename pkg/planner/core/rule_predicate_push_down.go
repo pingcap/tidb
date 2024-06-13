398066e2e69cdf564613afb6e7ba097e90ade91e
qw4990 <zhangyuanjia@pingcap.com>
2024-06-13 09:27:30 +0800

    fixup

--- pkg/planner/core/rule_predicate_push_down.go
+++ pkg/planner/core/rule_predicate_push_down.go
@@ -117,8 +117,8 @@ func (p *LogicalSelection) PredicatePushDown(predicates []expression.Expression,
 // PredicatePushDown implements base.LogicalPlan PredicatePushDown interface.
 func (p *LogicalUnionScan) PredicatePushDown(predicates []expression.Expression, opt *optimizetrace.LogicalOptimizeOp) ([]expression.Expression, base.LogicalPlan) {
 	if expression.ContainVirtualColumn(predicates) {
-		// predicates with virtual columns can't be pushed down to TiKV/TiFlash then they'll be push into a Projection
-		// under the UnionScan, but the current UnionScan doesn't support placing Projection below it, see #53951.
+		// predicates with virtual columns can't be pushed down to TiKV/TiFlash so they'll be put into a Projection
+		// below the UnionScan, but the current UnionScan doesn't support placing Projection below it, see #53951.
 		return predicates, p
 	}
 	retainedPredicates, _ := p.Children()[0].PredicatePushDown(predicates, opt)
