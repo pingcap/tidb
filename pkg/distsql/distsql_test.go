e18d0d96ba6dea8c0ced59da12950d27fd28afb4
crazycs520 <crazycs520@gmail.com>
2024-06-13 18:01:07 +0800

    fix test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/distsql/distsql_test.go
+++ pkg/distsql/distsql_test.go
@@ -119,9 +119,9 @@ func TestSelectResultRuntimeStats(t *testing.T) {
 	s1.procKeys.Add(100)
 	s1.procKeys.Add(200)
 
-	s2 := *s1
-	stmtStats.RegisterStats(1, s1)
-	stmtStats.RegisterStats(1, &s2)
+	s2 := s1.Clone()
+	stmtStats.RegisterStats(1, s1.Clone())
+	stmtStats.RegisterStats(1, s2)
 	stats := stmtStats.GetRootStats(1)
 	expect := "time:1s, open:0s, close:0s, loops:1, cop_task: {num: 4, max: 1s, min: 1ms, avg: 500.5ms, p95: 1s, max_proc_keys: 200, p95_proc_keys: 200, tot_proc: 2s, tot_wait: 2s, copr_cache_hit_ratio: 0.00, max_distsql_concurrency: 15}, backoff{RegionMiss: 2ms}"
 	require.Equal(t, expect, stats.String())
@@ -134,7 +134,7 @@ func TestSelectResultRuntimeStats(t *testing.T) {
 	}
 	s1.reqStat.RecordRPCErrorStats("server_is_busy")
 	s1.reqStat.RecordRPCErrorStats("server_is_busy")
-	stmtStats.RegisterStats(2, s1)
+	stmtStats.RegisterStats(2, s1.Clone())
 	stats = stmtStats.GetRootStats(2)
 	expect = "cop_task: {num: 2, max: 1s, min: 1ms, avg: 500.5ms, p95: 1s, max_proc_keys: 200, p95_proc_keys: 200, tot_proc: 1s, tot_wait: 1s, copr_cache_hit_ratio: 0.00, max_distsql_concurrency: 15}, rpc_info:{Cop:{num_rpc:1, total_time:1s}, rpc_errors:{server_is_busy:2}}, backoff{RegionMiss: 1ms}"
 	require.Equal(t, expect, stats.String())
