2c4467372de0fb744a2126a248b658c32864a12f
lance6716 <lance6716@gmail.com>
2024-06-13 17:23:38 +0800

    remove a TODO
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- pkg/ddl/ddl_api.go
+++ pkg/ddl/ddl_api.go
@@ -3202,13 +3202,13 @@ func (d *ddl) FlashbackCluster(ctx sessionctx.Context, flashbackTS uint64) error
 		Args: []any{
 			flashbackTS,
 			map[string]any{},
-			true,           /* tidb_gc_enable */
-			variable.On,    /* tidb_enable_auto_analyze */
-			variable.Off,   /* tidb_super_read_only */
-			0,              /* totalRegions */
-			0,              /* startTS */
-			0,              /* commitTS */
-			variable.On,    /* tidb_ttl_job_enable */
+			true,         /* tidb_gc_enable */
+			variable.On,  /* tidb_enable_auto_analyze */
+			variable.Off, /* tidb_super_read_only */
+			0,            /* totalRegions */
+			0,            /* startTS */
+			0,            /* commitTS */
+			variable.On,  /* tidb_ttl_job_enable */
 			[]kv.KeyRange{} /* flashback key_ranges */},
 		CDCWriteSource: ctx.GetSessionVars().CDCWriteSource,
 		// FLASHBACK CLUSTER affects all schemas and tables.
@@ -9453,7 +9453,6 @@ func (d *ddl) CreateCheckConstraint(ctx sessionctx.Context, ti ast.Ident, constr
 	if err := table.IfCheckConstraintExprBoolType(ctx.GetExprCtx().GetEvalCtx(), constraintInfo, tblInfo); err != nil {
 		return err
 	}
-	// TODO(lance6716): InvolvingSchemaInfo for constraint related DDL?
 	job := &model.Job{
 		SchemaID:       schema.ID,
 		TableID:        tblInfo.ID,
