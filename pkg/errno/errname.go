f2f97370f3e998854ec48403ab24ee9b9eedcecf
disksing <i@disksing.com>
2024-06-14 15:58:23 +0800

    add error message
    Signed-off-by: disksing <i@disksing.com>

--- pkg/errno/errname.go
+++ pkg/errno/errname.go
@@ -1166,4 +1166,6 @@ var MySQLErrName = map[uint16]*mysql.ErrMessage{
 	ErrCannotResumeDDLJob: mysql.Message("Job [%v] can't be resumed: %s", nil),
 	ErrPausedDDLJob:       mysql.Message("Job [%v] has already been paused", nil),
 	ErrBDRRestrictedDDL:   mysql.Message("The operation is not allowed while the bdr role of this cluster is set to %s.", nil),
+
+	ErrUsernameFormat: mysql.Message("Username is not valid", nil),
 }
