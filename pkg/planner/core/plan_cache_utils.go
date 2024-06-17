d270211acba100057de8f833d102341062e8dc00
qw4990 <zhangyuanjia@pingcap.com>
2024-06-17 17:34:34 +0800

    fixup

--- pkg/planner/core/plan_cache_utils.go
+++ pkg/planner/core/plan_cache_utils.go
@@ -751,7 +751,7 @@ func parseParamTypes(sctx sessionctx.Context, params []expression.Expression) (p
 	return
 }
 
-// PickPlanFromBucket pick one plan from bucket
+// matchCachedPlan checks whether this plan is matched with these match-options.
 func matchCachedPlan(sctx sessionctx.Context, value *PlanCacheValue, matchOpts *utilpc.PlanCacheMatchOpts) bool {
 	if matchOpts == nil { // if PointGet, the matchOpts is nil
 		return true
