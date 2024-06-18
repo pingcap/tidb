f578180d30132103dc7d9c2379b7736cc627f4b3
qw4990 <zhangyuanjia@pingcap.com>
2024-06-18 14:45:20 +0800

    fixup

--- pkg/bindinfo/session_handle_test.go
+++ pkg/bindinfo/session_handle_test.go
@@ -396,10 +396,10 @@ func TestIssue53834(t *testing.T) {
 		tk.MustExec(fmt.Sprintf(`set global tidb_mem_oom_action='%v'`, oomAction))
 	}()
 
+	tk.MustExec(`set global tidb_mem_oom_action='cancel'`)
 	err := tk.ExecToErr(`replace into t select /*+ memory_quota(1 mb) */ * from t`)
 	require.ErrorContains(t, err, "cancelled due to exceeding the allowed memory limit")
 
-	tk.MustExec(`set global tidb_mem_oom_action='cancel'`)
 	tk.MustExec(`create binding using replace into t select /*+ memory_quota(1 mb) */ * from t`)
 	err = tk.ExecToErr(`replace into t select * from t`)
 	require.ErrorContains(t, err, "cancelled due to exceeding the allowed memory limit")
