d39517f6fe2beac922d4ec7c922a3efb6a29f065
qw4990 <zhangyuanjia@pingcap.com>
2024-06-18 22:43:47 +0800

    fixup

--- pkg/planner/core/plan_cache_instance_test.go
+++ pkg/planner/core/plan_cache_instance_test.go
@@ -22,8 +22,25 @@ import (
 	"time"
 
 	"github.com/pingcap/tidb/pkg/domain"
+	"github.com/pingcap/tidb/pkg/sessionctx"
 	"github.com/stretchr/testify/require"
 )
+
+func _put(sctx sessionctx.Context, pc InstancePlanCache, testKey, memUsage, statsHash int64) (succ bool) {
+	v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
+	return pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+}
+
+func _hit(t *testing.T, sctx sessionctx.Context, pc InstancePlanCache, testKey, statsHash int) {
+	v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+	require.True(t, ok)
+	require.Equal(t, v.(*PlanCacheValue).testKey, int64(testKey))
+}
+
+func _miss(t *testing.T, sctx sessionctx.Context, pc InstancePlanCache, testKey, statsHash int) {
+	_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+	require.False(t, ok)
+}
 
 func TestInstancePlanCacheBasic(t *testing.T) {
 	sctx := MockContext()
@@ -31,74 +48,59 @@ func TestInstancePlanCacheBasic(t *testing.T) {
 		domain.GetDomain(sctx).StatsHandle().Close()
 	}()
 
-	var pc InstancePlanCache
-	put := func(testKey, memUsage int64) {
-		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage}
-		pc.Put(sctx, fmt.Sprintf("%v", testKey), v, nil)
-	}
-	hit := func(testKey int64) {
-		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), nil)
-		require.True(t, ok)
-		require.Equal(t, v.(*PlanCacheValue).testKey, testKey)
-	}
-	miss := func(testKey int) {
-		_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), nil)
-		require.False(t, ok)
-	}
-
-	pc = NewInstancePlanCache(1000, 1000)
-	put(1, 100)
-	put(2, 100)
-	put(3, 100)
+	pc := NewInstancePlanCache(1000, 1000)
+	_put(sctx, pc, 1, 100, 0)
+	_put(sctx, pc, 2, 100, 0)
+	_put(sctx, pc, 3, 100, 0)
 	require.Equal(t, pc.MemUsage(sctx), int64(300))
-	hit(1)
-	hit(2)
-	hit(3)
+	_hit(t, sctx, pc, 1, 0)
+	_hit(t, sctx, pc, 2, 0)
+	_hit(t, sctx, pc, 3, 0)
 
 	// exceed the hard limit during Put
 	pc = NewInstancePlanCache(250, 250)
-	put(1, 100)
-	put(2, 100)
-	put(3, 100)
+	_put(sctx, pc, 1, 100, 0)
+	_put(sctx, pc, 2, 100, 0)
+	_put(sctx, pc, 3, 100, 0)
 	require.Equal(t, pc.MemUsage(sctx), int64(200))
-	hit(1)
-	hit(2)
-	miss(3)
+	_hit(t, sctx, pc, 1, 0)
+	_hit(t, sctx, pc, 2, 0)
+	_miss(t, sctx, pc, 3, 0)
 
 	// can't Put 2 same values
 	pc = NewInstancePlanCache(250, 250)
-	put(1, 100)
-	put(1, 101)
+	_put(sctx, pc, 1, 100, 0)
+	_put(sctx, pc, 1, 101, 0)
 	require.Equal(t, pc.MemUsage(sctx), int64(100)) // the second one will be ignored
 
 	// eviction
 	pc = NewInstancePlanCache(320, 500)
-	put(1, 100)
-	put(2, 100)
-	put(3, 100)
-	put(4, 100)
-	put(5, 100)
-	hit(1) // access 1-3 to refresh their last_used
-	hit(2)
-	hit(3)
+	_put(sctx, pc, 1, 100, 0)
+	_put(sctx, pc, 2, 100, 0)
+	_put(sctx, pc, 3, 100, 0)
+	_put(sctx, pc, 4, 100, 0)
+	_put(sctx, pc, 5, 100, 0)
+	_hit(t, sctx, pc, 1, 0) // access 1-3 to refresh their last_used
+	_hit(t, sctx, pc, 2, 0)
+	_hit(t, sctx, pc, 3, 0)
 	require.Equal(t, pc.Evict(sctx), true)
 	require.Equal(t, pc.MemUsage(sctx), int64(300))
-	hit(1) // access 1-3 to refresh their last_used
-	hit(2)
-	hit(3)
-	miss(4) // 4-5 have been evicted
-	miss(5)
+	_hit(t, sctx, pc, 1, 0) // access 1-3 to refresh their last_used
+	_hit(t, sctx, pc, 2, 0)
+	_hit(t, sctx, pc, 3, 0)
+	_miss(t, sctx, pc, 4, 0) // 4-5 have been evicted
+	_miss(t, sctx, pc, 5, 0)
 
 	// no need to eviction if mem < softLimit
 	pc = NewInstancePlanCache(320, 500)
-	put(1, 100)
-	put(2, 100)
-	put(3, 100)
+	_put(sctx, pc, 1, 100, 0)
+	_put(sctx, pc, 2, 100, 0)
+	_put(sctx, pc, 3, 100, 0)
 	require.Equal(t, pc.Evict(sctx), false)
 	require.Equal(t, pc.MemUsage(sctx), int64(300))
-	hit(1)
-	hit(2)
-	hit(3)
+	_hit(t, sctx, pc, 1, 0)
+	_hit(t, sctx, pc, 2, 0)
+	_hit(t, sctx, pc, 3, 0)
 }
 
 func TestInstancePlanCacheWithMatchOpts(t *testing.T) {
@@ -108,75 +110,60 @@ func TestInstancePlanCacheWithMatchOpts(t *testing.T) {
 	}()
 	sctx.GetSessionVars().PlanCacheInvalidationOnFreshStats = true
 
-	var pc InstancePlanCache
-	put := func(testKey, memUsage, statsHash int64) {
-		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
-		pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-	}
-	hit := func(testKey, statsHash int64) {
-		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-		require.True(t, ok)
-		require.Equal(t, v.(*PlanCacheValue).testKey, testKey)
-	}
-	miss := func(testKey, statsHash int) {
-		_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-		require.False(t, ok)
-	}
-
 	// same key with different statsHash
-	pc = NewInstancePlanCache(1000, 1000)
-	put(1, 100, 1)
-	put(1, 100, 2)
-	put(1, 100, 3)
-	hit(1, 1)
-	hit(1, 2)
-	hit(1, 3)
-	miss(1, 4)
-	miss(2, 1)
+	pc := NewInstancePlanCache(1000, 1000)
+	_put(sctx, pc, 1, 100, 1)
+	_put(sctx, pc, 1, 100, 2)
+	_put(sctx, pc, 1, 100, 3)
+	_hit(t, sctx, pc, 1, 1)
+	_hit(t, sctx, pc, 1, 2)
+	_hit(t, sctx, pc, 1, 3)
+	_miss(t, sctx, pc, 1, 4)
+	_miss(t, sctx, pc, 2, 1)
 
 	// multiple keys with same statsHash
 	pc = NewInstancePlanCache(1000, 1000)
-	put(1, 100, 1)
-	put(1, 100, 2)
-	put(2, 100, 1)
-	put(2, 100, 2)
-	hit(1, 1)
-	hit(1, 2)
-	miss(1, 3)
-	hit(2, 1)
-	hit(2, 2)
-	miss(2, 3)
-	miss(3, 1)
-	miss(3, 2)
-	miss(3, 3)
+	_put(sctx, pc, 1, 100, 1)
+	_put(sctx, pc, 1, 100, 2)
+	_put(sctx, pc, 2, 100, 1)
+	_put(sctx, pc, 2, 100, 2)
+	_hit(t, sctx, pc, 1, 1)
+	_hit(t, sctx, pc, 1, 2)
+	_miss(t, sctx, pc, 1, 3)
+	_hit(t, sctx, pc, 2, 1)
+	_hit(t, sctx, pc, 2, 2)
+	_miss(t, sctx, pc, 2, 3)
+	_miss(t, sctx, pc, 3, 1)
+	_miss(t, sctx, pc, 3, 2)
+	_miss(t, sctx, pc, 3, 3)
 
 	// hard limit can take effect in this case
 	pc = NewInstancePlanCache(200, 200)
-	put(1, 100, 1)
-	put(1, 100, 2)
-	put(1, 100, 3) // the third one will be ignored
+	_put(sctx, pc, 1, 100, 1)
+	_put(sctx, pc, 1, 100, 2)
+	_put(sctx, pc, 1, 100, 3) // the third one will be ignored
 	require.Equal(t, pc.MemUsage(sctx), int64(200))
-	hit(1, 1)
-	hit(1, 2)
-	miss(1, 3)
+	_hit(t, sctx, pc, 1, 1)
+	_hit(t, sctx, pc, 1, 2)
+	_miss(t, sctx, pc, 1, 3)
 
 	// eviction this case
 	pc = NewInstancePlanCache(300, 500)
-	put(1, 100, 1)
-	put(1, 100, 2)
-	put(1, 100, 3)
-	put(1, 100, 4)
-	put(1, 100, 5)
-	hit(1, 1) // refresh 1-3's last_used
-	hit(1, 2)
-	hit(1, 3)
+	_put(sctx, pc, 1, 100, 1)
+	_put(sctx, pc, 1, 100, 2)
+	_put(sctx, pc, 1, 100, 3)
+	_put(sctx, pc, 1, 100, 4)
+	_put(sctx, pc, 1, 100, 5)
+	_hit(t, sctx, pc, 1, 1) // refresh 1-3's last_used
+	_hit(t, sctx, pc, 1, 2)
+	_hit(t, sctx, pc, 1, 3)
 	require.True(t, pc.Evict(sctx))
 	require.Equal(t, pc.MemUsage(sctx), int64(300))
-	hit(1, 1)
-	hit(1, 2)
-	hit(1, 3)
-	miss(1, 4)
-	miss(1, 5)
+	_hit(t, sctx, pc, 1, 1)
+	_hit(t, sctx, pc, 1, 2)
+	_hit(t, sctx, pc, 1, 3)
+	_miss(t, sctx, pc, 1, 4)
+	_miss(t, sctx, pc, 1, 5)
 }
 
 func TestInstancePlanCacheConcurrentRead(t *testing.T) {
@@ -186,27 +173,12 @@ func TestInstancePlanCacheConcurrentRead(t *testing.T) {
 	}()
 	sctx.GetSessionVars().PlanCacheInvalidationOnFreshStats = true
 
-	var pc InstancePlanCache
-	put := func(testKey, memUsage, statsHash int64) {
-		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
-		pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-	}
-	hit := func(testKey, statsHash int) {
-		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-		require.True(t, ok)
-		require.Equal(t, v.(*PlanCacheValue).testKey, int64(testKey))
-	}
-	miss := func(testKey, statsHash int) {
-		_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-		require.False(t, ok)
-	}
-
-	pc = NewInstancePlanCache(300, 100000)
+	pc := NewInstancePlanCache(300, 100000)
 	var flag [100][100]bool
 	for k := 0; k < 100; k++ {
 		for statsHash := 0; statsHash < 100; statsHash++ {
 			if rand.Intn(10) < 7 {
-				put(int64(k), 1, int64(statsHash))
+				_put(sctx, pc, int64(k), 1, int64(statsHash))
 				flag[k][statsHash] = true
 			}
 		}
@@ -220,9 +192,9 @@ func TestInstancePlanCacheConcurrentRead(t *testing.T) {
 			for i := 0; i < 10000; i++ {
 				k, statsHash := rand.Intn(100), rand.Intn(100)
 				if flag[k][statsHash] {
-					hit(k, statsHash)
+					_hit(t, sctx, pc, k, statsHash)
 				} else {
-					miss(k, statsHash)
+					_miss(t, sctx, pc, k, statsHash)
 				}
 				time.Sleep(time.Nanosecond * 10)
 			}
@@ -237,20 +209,8 @@ func TestInstancePlanCacheConcurrentWriteRead(t *testing.T) {
 		domain.GetDomain(sctx).StatsHandle().Close()
 	}()
 	sctx.GetSessionVars().PlanCacheInvalidationOnFreshStats = true
-
-	var pc InstancePlanCache
-	put := func(testKey, memUsage, statsHash int64) (succ bool) {
-		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
-		return pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-	}
-	hit := func(testKey, statsHash int64) {
-		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
-		require.True(t, ok)
-		require.Equal(t, v.(*PlanCacheValue).testKey, testKey)
-	}
-
 	var flag [100][100]atomic.Bool
-	pc = NewInstancePlanCache(300, 100000)
+	pc := NewInstancePlanCache(300, 100000)
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ { // writers
 		wg.Add(1)
@@ -258,7 +218,7 @@ func TestInstancePlanCacheConcurrentWriteRead(t *testing.T) {
 			defer wg.Done()
 			for i := 0; i < 1000; i++ {
 				k, statsHash := rand.Intn(100), rand.Intn(100)
-				if put(int64(k), 1, int64(statsHash)) {
+				if _put(sctx, pc, int64(k), 1, int64(statsHash)) {
 					flag[k][statsHash].Store(true)
 				}
 				time.Sleep(time.Nanosecond * 10)
@@ -272,7 +232,7 @@ func TestInstancePlanCacheConcurrentWriteRead(t *testing.T) {
 			for i := 0; i < 2000; i++ {
 				k, statsHash := rand.Intn(100), rand.Intn(100)
 				if flag[k][statsHash].Load() {
-					hit(int64(k), int64(statsHash))
+					_hit(t, sctx, pc, k, statsHash)
 				}
 				time.Sleep(time.Nanosecond * 5)
 			}
