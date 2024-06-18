f77877593bfd2f4445c72e0e7a3c6c788f72d9cf
qw4990 <zhangyuanjia@pingcap.com>
2024-06-18 17:55:59 +0800

    fixup

---
+++ pkg/planner/core/plan_cache_instance.go
@@ -0,0 +1,238 @@
+// Package core Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+import (
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/pkg/sessionctx"
+	"github.com/pingcap/tidb/pkg/util/kvcache"
+	"go.uber.org/atomic"
+)
+
+// InstancePlanCache ...
+type InstancePlanCache interface {
+	Get(sctx sessionctx.Context, key string, opts any) (value any, ok bool)
+	Put(sctx sessionctx.Context, key string, value, opts any) (succ bool)
+	Evict(sctx sessionctx.Context) (evicted bool)
+	MemUsage(sctx sessionctx.Context) int64
+	SetHardLimit(hardMemLimit int64) error
+	SetSoftLimit(softMemLimit int64) error
+}
+
+func NewInstancePlanCache(softMemLimit, hardMemLimit int64) InstancePlanCache {
+	planCache := new(instancePlanCache)
+	planCache.softMemLimit.Store(softMemLimit)
+	planCache.hardMemLimit.Store(hardMemLimit)
+	return planCache
+}
+
+type instancePCNode struct {
+	value    any // the underlying value, which should be (*PlanCacheValue)
+	lastUsed atomic.Time
+	next     atomic.Pointer[instancePCNode]
+}
+
+// instancePlanCache is the instance level plan cache.
+// [key1] --> [headNode1] --> [node1] --> [node2] --> [node3]
+// [key2] --> [headNode2] --> [node4] --> [node5]
+// [key3] --> [headNode3] --> [node6] --> [node7] --> [node8]
+// headNode.value is always empty, headNode is designed to make it easier to implement.
+type instancePlanCache struct {
+	heads   sync.Map
+	totCost atomic.Int64
+
+	evictMutex   sync.Mutex
+	softMemLimit atomic.Int64
+	hardMemLimit atomic.Int64
+}
+
+func (pc *instancePlanCache) getHead(key string, create bool) *instancePCNode {
+	headNode, ok := pc.heads.Load(key)
+	if ok { // cache hit
+		return headNode.(*instancePCNode)
+	}
+	if !create { // cache miss
+		return nil
+	}
+	newHeadNode := pc.createNode(nil)
+	actual, _ := pc.heads.LoadOrStore(key, newHeadNode)
+	if headNode, ok := actual.(*instancePCNode); ok { // for safety
+		return headNode
+	}
+	return nil
+}
+
+func (pc *instancePlanCache) Get(sctx sessionctx.Context, key string, opts any) (value any, ok bool) {
+	headNode := pc.getHead(key, false)
+	if headNode == nil { // cache miss
+		return nil, false
+	}
+	return pc.getPlanFromList(sctx, headNode, opts)
+}
+
+func (pc *instancePlanCache) getPlanFromList(sctx sessionctx.Context, headNode *instancePCNode, opts any) (any, bool) {
+	for node := headNode.next.Load(); node != nil; node = node.next.Load() {
+		var matchOpts *PlanCacheMatchOpts
+		if opts != nil {
+			matchOpts = opts.(*PlanCacheMatchOpts)
+		}
+		if matchCachedPlan(sctx, node.value.(*PlanCacheValue), matchOpts) { // v.Plan is read-only, no need to lock
+			node.lastUsed.Store(time.Now()) // atomically update the lastUsed field
+			return node.value, true
+		}
+	}
+	return nil, false
+}
+
+func (pc *instancePlanCache) Put(sctx sessionctx.Context, key string, value, opts any) (succ bool) {
+	vMem := value.(*PlanCacheValue).MemoryUsage()
+	if vMem+pc.totCost.Load() > pc.hardMemLimit.Load() {
+		return // do nothing if it exceeds the hard limit
+	}
+	headNode := pc.getHead(key, true)
+	if _, ok := pc.getPlanFromList(sctx, headNode, opts); ok {
+		return // some other thread has inserted the same plan before
+	}
+
+	firstNode := headNode.next.Load()
+	currNode := pc.createNode(value)
+	currNode.next.Store(firstNode)
+	if headNode.next.CompareAndSwap(firstNode, currNode) { // if failed, some other thread has updated this node,
+		pc.totCost.Add(vMem) // then skip this Put and wait for the next time.
+		succ = true
+	}
+	return
+}
+
+// Evict evicts some values. There should be a background thread to perform the eviction.
+// step 1: iterate all values to collects their last_used
+// step 2: estimate a eviction threshold time based on all last_used values
+// step 3: iterate all values again and evict qualified values
+func (pc *instancePlanCache) Evict(_ sessionctx.Context) (evicted bool) {
+	pc.evictMutex.Lock() // make sure only one thread to trigger eviction for safety
+	defer pc.evictMutex.Unlock()
+	if pc.totCost.Load() < pc.softMemLimit.Load() {
+		return // do nothing
+	}
+	lastUsedTimes := make([]time.Time, 0, 64)
+	pc.foreach(func(_, this *instancePCNode) bool { // step 1
+		lastUsedTimes = append(lastUsedTimes, this.lastUsed.Load())
+		return false
+	})
+	threshold := pc.calcEvictionThreshold(lastUsedTimes) // step 2
+	pc.foreach(func(prev, this *instancePCNode) bool {   // step 3
+		if this.lastUsed.Load().Before(threshold) { // evict this value
+			if prev.next.CompareAndSwap(this, this.next.Load()) { // have to use CAS since
+				pc.totCost.Sub(this.value.(*PlanCacheValue).MemoryUsage()) //  it might have been updated by other thread
+				evicted = true
+				return true
+			}
+		}
+		return false
+	})
+	pc.clearEmptyHead()
+	return
+}
+
+func (pc *instancePlanCache) MemUsage(_ sessionctx.Context) int64 {
+	return pc.totCost.Load()
+}
+
+func (pc *instancePlanCache) SetHardLimit(hardMemLimit int64) error {
+	currentSoft, currentHard := pc.softMemLimit.Load(), pc.hardMemLimit.Load()
+	if hardMemLimit < 0 || hardMemLimit < currentSoft {
+		return errors.NewNoStackErrorf("Plan Cache hard memory limit shouldn't be less than 0 or less than soft memory limit(%v)", currentSoft)
+	}
+	ok := pc.hardMemLimit.CompareAndSwap(currentHard, hardMemLimit)
+	if !ok {
+		return errors.NewNoStackError("some other threads are updating hard memory limit simultaneously")
+	}
+	return nil
+}
+
+func (pc *instancePlanCache) SetSoftLimit(softMemLimit int64) error {
+	currentSoft, currentHard := pc.softMemLimit.Load(), pc.hardMemLimit.Load()
+	if softMemLimit < 0 || softMemLimit > currentHard {
+		return errors.NewNoStackErrorf("Plan Cache soft memory limit shouldn't be less than 0 or greater than hard memory limit(%v)", currentHard)
+	}
+	ok := pc.softMemLimit.CompareAndSwap(currentSoft, softMemLimit)
+	if !ok {
+		return errors.NewNoStackError("some other threads are updating soft memory limit simultaneously")
+	}
+	return nil
+}
+
+func (pc *instancePlanCache) calcEvictionThreshold(lastUsedTimes []time.Time) (t time.Time) {
+	avgPerPlan := pc.totCost.Load() / int64(len(lastUsedTimes))
+	if avgPerPlan <= 0 {
+		return
+	}
+	numToEvict := (pc.totCost.Load() - pc.softMemLimit.Load() + avgPerPlan - 1) / avgPerPlan
+	if numToEvict <= 0 {
+		return
+	}
+	sort.Slice(lastUsedTimes, func(i, j int) bool {
+		return lastUsedTimes[i].Before(lastUsedTimes[j])
+	})
+	if len(lastUsedTimes) <= int(numToEvict) {
+		return
+	}
+	return lastUsedTimes[numToEvict]
+}
+
+func (pc *instancePlanCache) foreach(callback func(prev, this *instancePCNode) (thisRemoved bool)) {
+	_, headNodes := pc.headNodes()
+	for _, headNode := range headNodes {
+		for prev, this := headNode, headNode.next.Load(); this != nil; {
+			thisRemoved := callback(prev, this)
+			if !thisRemoved {
+				prev, this = this, this.next.Load()
+			} else {
+				this = this.next.Load()
+			}
+		}
+	}
+}
+
+func (pc *instancePlanCache) headNodes() ([]string, []*instancePCNode) {
+	keys := make([]string, 0, 64)
+	headNodes := make([]*instancePCNode, 0, 64)
+	pc.heads.Range(func(k, v any) bool {
+		keys = append(keys, k.(string))
+		headNodes = append(headNodes, v.(*instancePCNode))
+		return true
+	})
+	return keys, headNodes
+}
+
+func (pc *instancePlanCache) clearEmptyHead() {
+	keys, headNodes := pc.headNodes()
+	for i, headNode := range headNodes {
+		if headNode.next.Load() == nil {
+			pc.heads.Delete(keys[i])
+		}
+	}
+}
+
+func (pc *instancePlanCache) createNode(value kvcache.Value) *instancePCNode {
+	node := new(instancePCNode)
+	node.value = value
+	node.lastUsed.Store(time.Now())
+	return node
+}
---
+++ pkg/planner/core/plan_cache_instance_test.go
@@ -0,0 +1,303 @@
+// Package core Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/pingcap/tidb/pkg/domain"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInstancePlanCacheBasic(t *testing.T) {
+	sctx := MockContext()
+	defer func() {
+		domain.GetDomain(sctx).StatsHandle().Close()
+	}()
+
+	var pc InstancePlanCache
+	put := func(testKey, memUsage int64) {
+		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage}
+		pc.Put(sctx, fmt.Sprintf("%v", testKey), v, nil)
+	}
+	hit := func(testKey int64) {
+		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), nil)
+		require.True(t, ok)
+		require.Equal(t, v.(*PlanCacheValue).testKey, testKey)
+	}
+	miss := func(testKey int) {
+		_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), nil)
+		require.False(t, ok)
+	}
+
+	pc = NewInstancePlanCache(1000, 1000)
+	put(1, 100)
+	put(2, 100)
+	put(3, 100)
+	require.Equal(t, pc.MemUsage(sctx), int64(300))
+	hit(1)
+	hit(2)
+	hit(3)
+
+	// exceed the hard limit during Put
+	pc = NewInstancePlanCache(250, 250)
+	put(1, 100)
+	put(2, 100)
+	put(3, 100)
+	require.Equal(t, pc.MemUsage(sctx), int64(200))
+	hit(1)
+	hit(2)
+	miss(3)
+
+	// can't Put 2 same values
+	pc = NewInstancePlanCache(250, 250)
+	put(1, 100)
+	put(1, 101)
+	require.Equal(t, pc.MemUsage(sctx), int64(100)) // the second one will be ignored
+
+	// update the hard limit after exceeding it
+	pc = NewInstancePlanCache(250, 250)
+	put(1, 100)
+	put(2, 100)
+	put(3, 100)
+	require.Equal(t, pc.MemUsage(sctx), int64(200))
+	pc.SetHardLimit(300)
+	put(3, 100)
+	require.Equal(t, pc.MemUsage(sctx), int64(300))
+	hit(1)
+	hit(2)
+	hit(3)
+
+	// invalid hard or soft limit
+	pc = NewInstancePlanCache(250, 250)
+	require.Error(t, pc.SetHardLimit(-1))
+	require.Error(t, pc.SetHardLimit(200))
+	require.Error(t, pc.SetSoftLimit(-1))
+	require.Error(t, pc.SetSoftLimit(300))
+
+	// eviction
+	pc = NewInstancePlanCache(320, 500)
+	put(1, 100)
+	put(2, 100)
+	put(3, 100)
+	put(4, 100)
+	put(5, 100)
+	hit(1) // access 1-3 to refresh their last_used
+	hit(2)
+	hit(3)
+	require.Equal(t, pc.Evict(sctx), true)
+	require.Equal(t, pc.MemUsage(sctx), int64(300))
+	hit(1) // access 1-3 to refresh their last_used
+	hit(2)
+	hit(3)
+	miss(4) // 4-5 have been evicted
+	miss(5)
+
+	// no need to eviction if mem < softLimit
+	pc = NewInstancePlanCache(320, 500)
+	put(1, 100)
+	put(2, 100)
+	put(3, 100)
+	require.Equal(t, pc.Evict(sctx), false)
+	require.Equal(t, pc.MemUsage(sctx), int64(300))
+	hit(1)
+	hit(2)
+	hit(3)
+}
+
+func TestInstancePlanCacheWithMatchOpts(t *testing.T) {
+	sctx := MockContext()
+	defer func() {
+		domain.GetDomain(sctx).StatsHandle().Close()
+	}()
+	sctx.GetSessionVars().PlanCacheInvalidationOnFreshStats = true
+
+	var pc InstancePlanCache
+	put := func(testKey, memUsage, statsHash int64) {
+		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
+		pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+	}
+	hit := func(testKey, statsHash int64) {
+		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+		require.True(t, ok)
+		require.Equal(t, v.(*PlanCacheValue).testKey, testKey)
+	}
+	miss := func(testKey, statsHash int) {
+		_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+		require.False(t, ok)
+	}
+
+	// same key with different statsHash
+	pc = NewInstancePlanCache(1000, 1000)
+	put(1, 100, 1)
+	put(1, 100, 2)
+	put(1, 100, 3)
+	hit(1, 1)
+	hit(1, 2)
+	hit(1, 3)
+	miss(1, 4)
+	miss(2, 1)
+
+	// multiple keys with same statsHash
+	pc = NewInstancePlanCache(1000, 1000)
+	put(1, 100, 1)
+	put(1, 100, 2)
+	put(2, 100, 1)
+	put(2, 100, 2)
+	hit(1, 1)
+	hit(1, 2)
+	miss(1, 3)
+	hit(2, 1)
+	hit(2, 2)
+	miss(2, 3)
+	miss(3, 1)
+	miss(3, 2)
+	miss(3, 3)
+
+	// hard limit can take effect in this case
+	pc = NewInstancePlanCache(200, 200)
+	put(1, 100, 1)
+	put(1, 100, 2)
+	put(1, 100, 3) // the third one will be ignored
+	require.Equal(t, pc.MemUsage(sctx), int64(200))
+	hit(1, 1)
+	hit(1, 2)
+	miss(1, 3)
+
+	// eviction this case
+	pc = NewInstancePlanCache(300, 500)
+	put(1, 100, 1)
+	put(1, 100, 2)
+	put(1, 100, 3)
+	put(1, 100, 4)
+	put(1, 100, 5)
+	hit(1, 1) // refresh 1-3's last_used
+	hit(1, 2)
+	hit(1, 3)
+	require.True(t, pc.Evict(sctx))
+	require.Equal(t, pc.MemUsage(sctx), int64(300))
+	hit(1, 1)
+	hit(1, 2)
+	hit(1, 3)
+	miss(1, 4)
+	miss(1, 5)
+}
+
+func TestInstancePlanCacheConcurrentRead(t *testing.T) {
+	sctx := MockContext()
+	defer func() {
+		domain.GetDomain(sctx).StatsHandle().Close()
+	}()
+	sctx.GetSessionVars().PlanCacheInvalidationOnFreshStats = true
+
+	var pc InstancePlanCache
+	put := func(testKey, memUsage, statsHash int64) {
+		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
+		pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+	}
+	hit := func(testKey, statsHash int) {
+		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+		require.True(t, ok)
+		require.Equal(t, v.(*PlanCacheValue).testKey, int64(testKey))
+	}
+	miss := func(testKey, statsHash int) {
+		_, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+		require.False(t, ok)
+	}
+
+	pc = NewInstancePlanCache(300, 100000)
+	var flag [100][100]bool
+	for k := 0; k < 100; k++ {
+		for statsHash := 0; statsHash < 100; statsHash++ {
+			if rand.Intn(10) < 7 {
+				put(int64(k), 1, int64(statsHash))
+				flag[k][statsHash] = true
+			}
+		}
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				k, statsHash := rand.Intn(100), rand.Intn(100)
+				if flag[k][statsHash] {
+					hit(k, statsHash)
+				} else {
+					miss(k, statsHash)
+				}
+				time.Sleep(time.Nanosecond * 10)
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func TestInstancePlanCacheConcurrentWriteRead(t *testing.T) {
+	sctx := MockContext()
+	defer func() {
+		domain.GetDomain(sctx).StatsHandle().Close()
+	}()
+	sctx.GetSessionVars().PlanCacheInvalidationOnFreshStats = true
+
+	var pc InstancePlanCache
+	put := func(testKey, memUsage, statsHash int64) (succ bool) {
+		v := &PlanCacheValue{testKey: testKey, memoryUsage: memUsage, matchOpts: &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)}}
+		return pc.Put(sctx, fmt.Sprintf("%v", testKey), v, &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+	}
+	hit := func(testKey, statsHash int64) {
+		v, ok := pc.Get(sctx, fmt.Sprintf("%v", testKey), &PlanCacheMatchOpts{StatsVersionHash: uint64(statsHash)})
+		require.True(t, ok)
+		require.Equal(t, v.(*PlanCacheValue).testKey, testKey)
+	}
+
+	var flag [100][100]atomic.Bool
+	pc = NewInstancePlanCache(300, 100000)
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ { // writers
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 1000; i++ {
+				k, statsHash := rand.Intn(100), rand.Intn(100)
+				if put(int64(k), 1, int64(statsHash)) {
+					flag[k][statsHash].Store(true)
+				}
+				time.Sleep(time.Nanosecond * 10)
+			}
+		}()
+	}
+	for i := 0; i < 5; i++ { // readers
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 2000; i++ {
+				k, statsHash := rand.Intn(100), rand.Intn(100)
+				if flag[k][statsHash].Load() {
+					hit(int64(k), int64(statsHash))
+				}
+				time.Sleep(time.Nanosecond * 5)
+			}
+		}()
+	}
+	wg.Wait()
+}
