3db10f832d610ef9ff6c4f7587c610130244d7c8
gmhdbjd <gmhdbjd@gmail.com>
2024-06-24 16:10:19 +0800

    fix

--- pkg/ddl/job_table.go
+++ pkg/ddl/job_table.go
@@ -523,7 +523,7 @@ func (d *ddl) delivery2LocalWorker(pool *workerPool, task *limitJobTask) {
 			if err == nil || !isRetryableError(err) {
 				break
 			}
-			logutil.Logger(context.Background()).Warn("handle local ddl job", zap.Int("retry times", i), zap.Error(err))
+			logutil.DDLLogger().Warn("handle local ddl job", zap.Int("retry times", i), zap.Error(err))
 			time.Sleep(time.Second)
 		}
 		pool.put(wk)
