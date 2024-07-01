0a2fd0ab661b0b731ac456920d0bcedf3b79b19d
sugimoto_e <eikun1014@gmail.com>
2024-07-01 14:47:50 +0900

    server: modify test of initial hand shake

--- pkg/server/conn_test.go
+++ pkg/server/conn_test.go
@@ -314,7 +314,7 @@ func TestInitialHandshake(t *testing.T) {
 	expected.WriteByte(0x15)                                                                             // Authentication Plugin Length
 	expected.Write([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})                   // Unused
 	expected.Write([]byte{0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x00}) // Salt
-	expected.WriteString("mysql_native_password")                                                        // Authentication Plugin
+	expected.WriteString("caching_sha2_password")                                                        // Authentication Plugin
 	expected.WriteByte(0x00)                                                                             // NULL
 	require.Equal(t, expected.Bytes(), outBuffer.Bytes()[4:])
 }
