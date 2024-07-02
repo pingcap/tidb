4c8bd24ddc1169750e78f2cc23a0e5940ffa04ca
guo-shaoge <shaoge1994@163.com>
2024-07-02 14:44:02 +0800

    fix main_test.go
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- pkg/executor/test/cte/main_test.go
+++ pkg/executor/test/cte/main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 PingCAP, Inc.
+// Copyright 2024 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,58 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package executor_test
+package ctetest
 
 import (
 	"testing"
 
 	"github.com/pingcap/tidb/pkg/config"
-	"github.com/pingcap/tidb/pkg/meta/autoid"
-	"github.com/pingcap/tidb/pkg/sessionctx/variable"
-	"github.com/pingcap/tidb/pkg/testkit/testdata"
-	"github.com/pingcap/tidb/pkg/testkit/testmain"
 	"github.com/pingcap/tidb/pkg/testkit/testsetup"
-	"github.com/tikv/client-go/v2/tikv"
 	"go.uber.org/goleak"
 )
 
-var testDataMap = make(testdata.BookKeeper)
-var prepareMergeSuiteData testdata.TestData
-var slowQuerySuiteData testdata.TestData
-
 func TestMain(m *testing.M) {
 	testsetup.SetupForCommonTest()
-	testDataMap.LoadTestSuiteData("testdata", "prepare_suite")
-	testDataMap.LoadTestSuiteData("testdata", "slow_query_suite")
-	prepareMergeSuiteData = testDataMap["prepare_suite"]
-	slowQuerySuiteData = testDataMap["slow_query_suite"]
-
-	autoid.SetStep(5000)
 	config.UpdateGlobal(func(conf *config.Config) {
-		conf.Instance.SlowThreshold = 30000 // 30s
 		conf.TiKVClient.AsyncCommit.SafeWindow = 0
 		conf.TiKVClient.AsyncCommit.AllowedClockDrift = 0
 		conf.Experimental.AllowsExpressionIndex = true
+		conf.Performance.EnableStatsCacheMemQuota = true
 	})
-	tikv.EnableFailpoints()
-	variable.StatsCacheMemQuota.Store(5000)
-
 	opts := []goleak.Option{
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
 		goleak.IgnoreTopFunction("github.com/bazelbuild/rules_go/go/tools/bzltestutil.RegisterTimeoutHandler.func1"),
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
-		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
-		goleak.IgnoreTopFunction("gopkg.in/natefinch/lumberjack%2ev2.(*Logger).millRun"),
-		goleak.IgnoreTopFunction("github.com/tikv/client-go/v2/txnkv/transaction.keepAlive"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
-		goleak.IgnoreTopFunction("github.com/pingcap/tidb/pkg/ttl/ttlworker.(*ttlScanWorker).loop"),
-		goleak.IgnoreTopFunction("github.com/pingcap/tidb/pkg/ttl/client.(*mockClient).WatchCommand.func1"),
-		goleak.IgnoreTopFunction("github.com/pingcap/tidb/pkg/ttl/ttlworker.(*JobManager).jobLoop"),
 	}
-	callback := func(i int) int {
-		testDataMap.GenerateOutputIfNeeded()
-		return i
-	}
-
-	goleak.VerifyTestMain(testmain.WrapTestingM(m, callback), opts...)
+	goleak.VerifyTestMain(m, opts...)
 }
