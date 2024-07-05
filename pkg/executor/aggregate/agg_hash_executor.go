ed19546a5ef4df0c52fb9df3f28449f1f05d11a8
xzhangxian1008 <xzhangxian@foxmail.com>
2024-07-05 16:46:12 +0800

    save

--- pkg/executor/aggregate/agg_hash_executor.go
+++ pkg/executor/aggregate/agg_hash_executor.go
@@ -199,7 +199,9 @@ func (e *HashAggExec) Close() error {
 			channel.Clear(ch)
 		}
 		for _, ch := range e.partialInputChs {
-			channel.Clear(ch)
+			for chk := range ch {
+				chk.Destroy(e.MaxChunkSize(), e.Children(0).RetFieldTypes())
+			}
 		}
 		channel.Clear(e.finalOutputCh)
 		e.executed.Store(false)
@@ -211,6 +213,20 @@ func (e *HashAggExec) Close() error {
 			e.parallelAggSpillAction.SetFinished()
 			e.parallelAggSpillAction = nil
 			e.spillHelper.close()
+		}
+		for i := range e.partialWorkers {
+			e.partialWorkers[i].chk.Destroy(e.MaxChunkSize(), e.Children(0).RetFieldTypes())
+		}
+		for i := range e.finalWorkers {
+			// Commonly, finalResultHolderCh should not be nil. Check it just in case.
+			if e.finalWorkers[i].finalResultHolderCh == nil {
+				continue
+			}
+
+			close(e.finalWorkers[i].finalResultHolderCh)
+			for chk := range e.finalWorkers[i].finalResultHolderCh {
+				chk.Destroy(e.MaxChunkSize(), e.RetFieldTypes())
+			}
 		}
 	}
 
@@ -308,7 +324,7 @@ func (e *HashAggExec) initPartialWorkers(partialConcurrency int, finalConcurrenc
 			globalOutputCh:       e.finalOutputCh,
 			partialResultsMap:    partialResultsMap,
 			groupByItems:         e.GroupByItems,
-			chk:                  e.NewChunkWithCapacity(e.Children(0).RetFieldTypes(), 0, e.MaxChunkSize()),
+			chk:                  e.NewChunkWithCapacity(e.Children(0).RetFieldTypes(), 0, e.MaxChunkSize()), // TODO
 			groupKeyBuf:          *groupKeyBuf,
 			serializeHelpers:     aggfuncs.NewSerializeHelper(),
 			isSpillPrepared:      false,
@@ -330,7 +346,7 @@ func (e *HashAggExec) initPartialWorkers(partialConcurrency int, finalConcurrenc
 		}
 		e.memTracker.Consume(e.partialWorkers[i].chk.GetMemoryUsageCap(e.MaxChunkSize()))
 		input := &HashAggInput{
-			chk:        chunk.New(e.Children(0).RetFieldTypes(), 0, e.MaxChunkSize()),
+			chk:        chunk.New(e.Children(0).RetFieldTypes(), 0, e.MaxChunkSize()), // TODO
 			giveBackCh: e.partialWorkers[i].inputCh,
 		}
 		e.memTracker.Consume(input.chk.GetMemoryUsageCap(e.MaxChunkSize()))
@@ -356,7 +372,7 @@ func (e *HashAggExec) initFinalWorkers(finalConcurrency int) {
 			e.finalWorkers[i].stats = &AggWorkerStat{}
 			e.stats.FinalStats = append(e.stats.FinalStats, e.finalWorkers[i].stats)
 		}
-		e.finalWorkers[i].finalResultHolderCh <- chunk.New(e.RetFieldTypes(), 0, e.MaxChunkSize())
+		e.finalWorkers[i].finalResultHolderCh <- chunk.New(e.RetFieldTypes(), 0, e.MaxChunkSize()) // TODO
 	}
 }
 
