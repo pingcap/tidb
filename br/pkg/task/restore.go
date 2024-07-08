a4db8b6a8eeec5537d4b5e85f9e14544aa37fed6
3pointer <luancheng@pingcap.com>
2024-07-08 12:48:08 +0800

    This is an automated cherry-pick of #54430
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- br/pkg/task/restore.go
+++ br/pkg/task/restore.go
@@ -645,12 +645,17 @@ func runRestore(c context.Context, g glue.Glue, cmdName string, cfg *RestoreConf
 		return errors.Trace(err)
 	}
 
+<<<<<<< HEAD
 	var newTS uint64
 	if client.IsIncremental() {
 		newTS = restoreTS
 	}
 	ddlJobs := restore.FilterDDLJobs(client.GetDDLJobs(), tables)
 	ddlJobs = restore.FilterDDLJobByRules(ddlJobs, restore.DDLJobBlockListRule)
+=======
+	ddlJobs := FilterDDLJobs(client.GetDDLJobs(), tables)
+	ddlJobs = FilterDDLJobByRules(ddlJobs, DDLJobBlockListRule)
+>>>>>>> 08147e7e258 (Incremental restore: fix the issue that backfill data is not covered by newTS (#54430))
 
 	err = client.PreCheckTableTiFlashReplica(ctx, tables, cfg.tiflashRecorder)
 	if err != nil {
@@ -704,6 +709,17 @@ func runRestore(c context.Context, g glue.Glue, cmdName string, cfg *RestoreConf
 		}
 	}
 
+	var newTS uint64
+	if client.IsIncremental() {
+		// we need to get the new ts after execDDL
+		// or backfilled data in upstream may not be covered by
+		// the new ts.
+		// see https://github.com/pingcap/tidb/issues/54426
+		newTS, err = restore.GetTSWithRetry(ctx, mgr.GetPDClient())
+		if err != nil {
+			return errors.Trace(err)
+		}
+	}
 	// We make bigger errCh so we won't block on multi-part failed.
 	errCh := make(chan error, 32)
 
