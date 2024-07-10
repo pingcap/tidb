903120577dd630cce95e4ebf3095825d255da748
D3Hunter <jujj603@gmail.com>
2024-07-10 10:29:28 +0800

    change

--- pkg/ddl/executor_test.go
+++ pkg/ddl/executor_test.go
@@ -16,7 +16,11 @@ package ddl_test
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/pingcap/tidb/pkg/ddl"
 	"github.com/pingcap/tidb/pkg/domain"
@@ -36,6 +40,13 @@ func TestGenIDAndInsertJobsWithRetry(t *testing.T) {
 	dom.DDL().OwnerManager().CampaignCancel()
 	ctx := kv.WithInternalSourceType(context.Background(), kv.InternalTxnDDL)
 
+	// avoid outer retry
+	bak := kv.MaxRetryCnt
+	kv.MaxRetryCnt = 1
+	t.Cleanup(func() {
+		kv.MaxRetryCnt = bak
+	})
+
 	jobs := []*model.Job{
 		{
 			Type:       model.ActionCreateTable,
@@ -43,21 +54,75 @@ func TestGenIDAndInsertJobsWithRetry(t *testing.T) {
 			TableName:  "t1",
 		},
 	}
-	barrier := make(chan struct{})
 	var wg util.WaitGroupWrapper
 	for i := 0; i < 10; i++ {
 		wg.Run(func() {
-			<-barrier
 			kit := testkit.NewTestKit(t, store)
 			for i := 0; i < 1000; i++ {
 				require.NoError(t, ddl.GenIDAndInsertJobsWithRetry(ctx, kit.Session(), jobs))
 			}
 		})
 	}
-	close(barrier)
 	wg.Wait()
 
 	jobs, err := ddl.GetAllDDLJobs(tk.Session())
 	require.NoError(t, err)
 	require.Len(t, jobs, 10000)
+}
+
+func TestGenIDAndInsertJobsWithRetryQPS(t *testing.T) {
+	store := testkit.CreateMockStore(t, mockstore.WithStoreType(mockstore.EmbedUnistore))
+	// disable DDL to avoid it interfere the test
+	tk := testkit.NewTestKit(t, store)
+	dom := domain.GetDomain(tk.Session())
+	dom.DDL().OwnerManager().CampaignCancel()
+	ctx := kv.WithInternalSourceType(context.Background(), kv.InternalTxnDDL)
+
+	jobs := []*model.Job{
+		{
+			Type:       model.ActionCreateTable,
+			SchemaName: "test",
+			TableName:  "t1",
+		},
+	}
+	thread := 1000
+	iterationPerThread := 30000
+	counters := make([]atomic.Int64, thread+1)
+	var wg util.WaitGroupWrapper
+	for i := 0; i < thread; i++ {
+		index := i
+		wg.Run(func() {
+			kit := testkit.NewTestKit(t, store)
+			for i := 0; i < iterationPerThread; i++ {
+				require.NoError(t, ddl.GenIDAndInsertJobsWithRetry(ctx, kit.Session(), jobs))
+
+				counters[0].Add(1)
+				counters[index+1].Add(1)
+			}
+		})
+	}
+	go func() {
+		getCounts := func() []int64 {
+			res := make([]int64, len(counters))
+			for i := range counters {
+				res[i] = counters[i].Load()
+			}
+			return res
+		}
+		lastCnt := getCounts()
+		for {
+			select {
+			case <-time.After(5 * time.Second):
+			}
+			currCnt := getCounts()
+			var sb strings.Builder
+			sb.WriteString(fmt.Sprintf("QPS - total:%.0f", float64(currCnt[0]-lastCnt[0])/5))
+			for i := 1; i < min(len(counters), 100); i++ {
+				sb.WriteString(fmt.Sprintf(", thread-%d: %.0f", i, float64(currCnt[i]-lastCnt[i])/5))
+			}
+			lastCnt = currCnt
+			fmt.Println(sb.String())
+		}
+	}()
+	wg.Wait()
 }
