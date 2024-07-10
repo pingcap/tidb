8381da8c9239b77be62e86b004eb38621ee26ce5
Arenatlx <314806019@qq.com>
2024-07-11 00:57:04 +0800

    planner: classify logical cte table into a separate file for later pkg move (#54542)
    ref pingcap/tidb#51664, ref pingcap/tidb#52714

--- pkg/planner/core/logical_initialize.go
+++ pkg/planner/core/logical_initialize.go
@@ -43,9 +43,3 @@ func (p LogicalExpand) Init(ctx base.PlanContext, offset int) *LogicalExpand {
 	p.BaseLogicalPlan = logicalop.NewBaseLogicalPlan(ctx, plancodec.TypeExpand, &p, offset)
 	return &p
 }
-
-// Init only assigns type and context.
-func (p LogicalCTETable) Init(ctx base.PlanContext, offset int) *LogicalCTETable {
-	p.BaseLogicalPlan = logicalop.NewBaseLogicalPlan(ctx, plancodec.TypeCTETable, &p, offset)
-	return &p
-}
