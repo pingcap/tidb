b876270468c28dcbea015fb35c98458d116ab2da
Jianjun Liao <jianjun.liao@outlook.com>
2024-07-11 16:54:57 +0800

    resolve conflicts
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- br/pkg/conn/conn.go
+++ br/pkg/conn/conn.go
@@ -7,6 +7,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -332,17 +333,25 @@ func (mgr *Mgr) GetMergeRegionSizeAndCount(ctx context.Context, client *http.Cli
 // IsLogBackupEnabled is used for br to check whether tikv has enabled log backup.
 func (mgr *Mgr) IsLogBackupEnabled(ctx context.Context, client *http.Client) (bool, error) {
 	logbackupEnable := true
+	type logbackup struct {
+		Enable bool `json:"enable"`
+	}
+	type config struct {
+		LogBackup logbackup `json:"log-backup"`
+	}
 	err := mgr.GetConfigFromTiKV(ctx, client, func(resp *http.Response) error {
 		respBytes, err := io.ReadAll(resp.Body)
 		if err != nil {
 			return err
 		}
-		enable, err := kvconfig.ParseLogBackupEnableFromConfig(respBytes)
+
+		var c config
+		err = json.Unmarshal(respBytes, &c)
 		if err != nil {
 			log.Warn("Failed to parse log-backup enable from config", logutil.ShortError(err))
 			return err
 		}
-		logbackupEnable = logbackupEnable && enable
+		logbackupEnable = logbackupEnable && c.LogBackup.Enable
 		return nil
 	})
 	return logbackupEnable, errors.Trace(err)
