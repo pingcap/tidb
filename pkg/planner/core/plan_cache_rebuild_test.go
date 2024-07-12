bca1e014e1b5ba06258f3469b1d2e1f21a51b694
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2024-07-12 09:03:04 +0800

    planner: add more test cases for Plan.Clone (#54584)
    ref pingcap/tidb#54057

--- pkg/planner/core/plan_cache_rebuild_test.go
+++ pkg/planner/core/plan_cache_rebuild_test.go
@@ -18,8 +18,11 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"reflect"
 	"testing"
+	"unsafe"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/pkg/expression"
 	"github.com/pingcap/tidb/pkg/planner/core"
 	"github.com/pingcap/tidb/pkg/planner/core/base"
@@ -95,49 +98,49 @@ func testCachedPlanClone(t *testing.T, tk1, tk2 *testkit.TestKit, prep, set, exe
 	require.True(t, checked)
 }
 
-func TestCheckPlanDeepClone(t *testing.T) {
+func TestCheckPlanClone(t *testing.T) {
 	// totally same pointer
 	ts1 := &core.PhysicalTableScan{}
-	require.Equal(t, core.CheckPlanDeepClone(ts1, ts1).Error(), "same pointer, path *PhysicalTableScan")
+	require.Equal(t, checkUnclearPlanCacheClone(ts1, ts1).Error(), "same pointer, path *core.PhysicalTableScan")
 
 	// share the same slice
 	ts2 := &core.PhysicalTableScan{}
 	ts1.AccessCondition = make([]expression.Expression, 10)
 	ts2.AccessCondition = ts1.AccessCondition
-	require.Equal(t, core.CheckPlanDeepClone(ts1, ts2).Error(), "same slice pointers, path *PhysicalTableScan.AccessCondition")
+	require.Equal(t, checkUnclearPlanCacheClone(ts1, ts2).Error(), "same slice pointers, path *core.PhysicalTableScan.AccessCondition")
 
 	// same slice element
 	ts2.AccessCondition = make([]expression.Expression, 10)
 	expr := &expression.Column{}
 	ts1.AccessCondition[0] = expr
 	ts2.AccessCondition[0] = expr
-	require.Equal(t, core.CheckPlanDeepClone(ts1, ts2).Error(), "same pointer, path *PhysicalTableScan.AccessCondition[0]")
+	require.Equal(t, checkUnclearPlanCacheClone(ts1, ts2).Error(), "same pointer, path *core.PhysicalTableScan.AccessCondition[0](*expression.Column)")
 
 	// same slice[0].pointer.pointer
 	ts2.AccessCondition[0] = new(expression.Column)
 	ts1.AccessCondition[0].(*expression.Column).RetType = new(types.FieldType)
 	ts2.AccessCondition[0].(*expression.Column).RetType = ts1.AccessCondition[0].(*expression.Column).RetType
-	require.Equal(t, core.CheckPlanDeepClone(ts1, ts2).Error(), "same pointer, path *PhysicalTableScan.AccessCondition[0].RetType")
+	require.Equal(t, checkUnclearPlanCacheClone(ts1, ts2).Error(), "same pointer, path *core.PhysicalTableScan.AccessCondition[0](*expression.Column).RetType")
 
 	// same interface
 	child := &core.PhysicalTableScan{}
 	ts1.SetProbeParents([]base.PhysicalPlan{child})
 	ts2.SetProbeParents([]base.PhysicalPlan{child})
-	require.Equal(t, core.CheckPlanDeepClone(ts1, ts2).Error(), "same pointer, path *PhysicalTableScan.physicalSchemaProducer.basePhysicalPlan.probeParents[0]")
+	require.Equal(t, checkUnclearPlanCacheClone(ts1, ts2).Error(), "same pointer, path *core.PhysicalTableScan.physicalSchemaProducer.basePhysicalPlan.probeParents[0](*core.PhysicalTableScan)")
 
 	// same map
 	l1 := &core.PhysicalLock{}
 	l2 := &core.PhysicalLock{}
 	l1.TblID2Handle = make(map[int64][]util.HandleCols)
 	l2.TblID2Handle = l1.TblID2Handle
-	require.Equal(t, core.CheckPlanDeepClone(l1, l2).Error(), "same map pointers, path *PhysicalLock.TblID2Handle")
+	require.Equal(t, checkUnclearPlanCacheClone(l1, l2).Error(), "same map pointers, path *core.PhysicalLock.TblID2Handle")
 
 	// same pointer in map
 	l2.TblID2Handle = make(map[int64][]util.HandleCols)
 	cols := make([]util.HandleCols, 10)
 	l1.TblID2Handle[1] = cols
 	l2.TblID2Handle[1] = cols
-	require.Equal(t, core.CheckPlanDeepClone(l1, l2).Error(), "same slice pointers, path *PhysicalLock.TblID2Handle[int64]")
+	require.Equal(t, checkUnclearPlanCacheClone(l1, l2).Error(), "same slice pointers, path *core.PhysicalLock.TblID2Handle[int64]")
 
 	// same sctx
 	l1.TblID2Handle[1] = nil
@@ -146,5 +149,152 @@ func TestCheckPlanDeepClone(t *testing.T) {
 	defer ctx.Close()
 	l1.SetSCtx(ctx)
 	l2.SetSCtx(ctx)
-	require.Equal(t, core.CheckPlanDeepClone(l1, l2).Error(), "same pointer, path *PhysicalLock.basePhysicalPlan.Plan.ctx")
+	require.Equal(t, checkUnclearPlanCacheClone(l1, l2).Error(), "same pointer, path *core.PhysicalLock.basePhysicalPlan.Plan.ctx(*mock.Context)")
+
+	// test tag
+	type S struct {
+		p1 *int `plan-cache-shallow-clone:"true"`
+		p2 *int
+	}
+	s1 := new(S)
+	s2 := new(S)
+	s1.p2 = new(int)
+	s2.p2 = s1.p2
+	require.Equal(t, checkUnclearPlanCacheClone(s1, s2).Error(), "same pointer, path *core_test.S.p2")
+	s2.p2 = new(int)
+	s1.p1 = new(int)
+	s2.p1 = s1.p1
+	require.NoError(t, checkUnclearPlanCacheClone(s1, s2))
+}
+
+// checkUnclearPlanCacheClone checks whether this cloned plan is safe for instance plan cache.
+// All fields in the plan should be deeply cloned except the fields with tag "plan-cache-shallow-clone:'true'".
+func checkUnclearPlanCacheClone(plan, cloned any) error {
+	return planCacheUnclearCloneCheck(reflect.ValueOf(plan), reflect.ValueOf(cloned), reflect.TypeOf(plan).String(), nil)
+}
+
+func planCacheUnclearCloneCheck(v1, v2 reflect.Value, path string, visited map[visit]bool) error {
+	if !v1.IsValid() || !v2.IsValid() {
+		if v1.IsValid() != v2.IsValid() {
+			return errors.Errorf("invalid")
+		}
+		return nil
+	}
+
+	if v1.Type() != v2.Type() {
+		return errors.Errorf("different type %v, %v, path %v", v1.Type(), v2.Type(), path)
+	}
+
+	if visited == nil {
+		visited = make(map[visit]bool)
+	}
+	hard := func(k reflect.Kind) bool {
+		switch k {
+		case reflect.Map, reflect.Slice, reflect.Ptr, reflect.Interface:
+			return true
+		}
+		return false
+	}
+	if v1.CanAddr() && v2.CanAddr() && hard(v1.Kind()) { // avoid dead loop
+		addr1 := unsafe.Pointer(v1.UnsafeAddr())
+		addr2 := unsafe.Pointer(v2.UnsafeAddr())
+		if uintptr(addr1) > uintptr(addr2) {
+			addr1, addr2 = addr2, addr1
+		}
+		typ := v1.Type()
+		v := visit{addr1, addr2, typ}
+		if visited[v] {
+			return nil
+		}
+		visited[v] = true
+	}
+
+	switch v1.Kind() {
+	case reflect.Array:
+		for i := 0; i < v1.Len(); i++ {
+			if err := planCacheUnclearCloneCheck(v1.Index(i), v2.Index(i), fmt.Sprintf("%v[%v]", path, i), visited); err != nil {
+				return err
+			}
+		}
+	case reflect.Slice:
+		if (v1.IsNil() && v2.IsNil()) || (v1.Len() == 0 && v2.Len() == 0) {
+			return nil
+		}
+		if v1.Len() != v2.Len() {
+			return errors.Errorf("different slice lengths, len %v, %v, path %v", v1.Len(), v2.Len(), path)
+		}
+		if v1.IsNil() != v2.IsNil() {
+			if v1.Len() == 0 && v2.Len() == 0 {
+				return nil // nil and an empty slice are accepted
+			}
+			return errors.Errorf("different slices nil %v, %v, path %v", v1.IsNil(), v2.IsNil(), path)
+		}
+		if v1.Pointer() == v2.Pointer() {
+			return errors.Errorf("same slice pointers, path %v", path)
+		}
+		for i := 0; i < v1.Len(); i++ {
+			if err := planCacheUnclearCloneCheck(v1.Index(i), v2.Index(i), fmt.Sprintf("%v[%v]", path, i), visited); err != nil {
+				return err
+			}
+		}
+	case reflect.Interface:
+		if v1.IsNil() && v2.IsNil() {
+			return nil
+		}
+		if v1.IsNil() != v2.IsNil() {
+			return errors.Errorf("invalid interfaces, path %v", path)
+		}
+		return planCacheUnclearCloneCheck(v1.Elem(), v2.Elem(), fmt.Sprintf("%v(%v)", path, v1.Elem().Type().String()), visited)
+	case reflect.Ptr:
+		if v1.IsNil() && v2.IsNil() {
+			return nil
+		}
+		if v1.Pointer() == v2.Pointer() {
+			return errors.Errorf("same pointer, path %v", path)
+		}
+		return planCacheUnclearCloneCheck(v1.Elem(), v2.Elem(), path, visited)
+	case reflect.Struct:
+		for i, n := 0, v1.NumField(); i < n; i++ {
+			tag := v1.Type().Field(i).Tag.Get("plan-cache-shallow-clone")
+			if tag == "true" {
+				continue
+			}
+			fieldName := v1.Type().Field(i).Name
+			if err := planCacheUnclearCloneCheck(v1.Field(i), v2.Field(i), fmt.Sprintf("%v.%v", path, fieldName), visited); err != nil {
+				return err
+			}
+		}
+	case reflect.Map:
+		if v1.IsNil() && v2.IsNil() {
+			return nil
+		}
+		if v1.IsNil() != v2.IsNil() || v1.Len() != v2.Len() {
+			return errors.Errorf("different maps nil: %v, %v, len: %v, %v, path: %v", v1.IsNil(), v2.IsNil(), v1.Len(), v2.Len(), path)
+		}
+		if v1.Pointer() == v2.Pointer() {
+			return errors.Errorf("same map pointers, path %v", path)
+		}
+		if len(v1.MapKeys()) != len(v2.MapKeys()) {
+			return errors.Errorf("invalid map")
+		}
+		for _, k := range v1.MapKeys() {
+			val1 := v1.MapIndex(k)
+			val2 := v2.MapIndex(k)
+			if !val1.IsValid() || !val2.IsValid() {
+				return errors.Errorf("invalid map value at %v", fmt.Sprintf("%v[%v]", path, k.Type().Name()))
+			}
+			if err := planCacheUnclearCloneCheck(val1, val2, fmt.Sprintf("%v[%v]", path, k.Type().Name()), visited); err != nil {
+				return err
+			}
+		}
+	default:
+		return nil
+	}
+	return nil
+}
+
+type visit struct {
+	a1  unsafe.Pointer
+	a2  unsafe.Pointer
+	typ reflect.Type
 }
