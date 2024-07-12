f64b039e2dc5205a0806484f43c88b940a3f636b
qw4990 <zhangyuanjia@pingcap.com>
2024-07-12 18:01:11 +0800

    fixup

--- pkg/planner/core/plan_clone_generator.go
+++ pkg/planner/core/plan_clone_generator.go
@@ -144,6 +144,7 @@ const _PREFIX = `// Copyright 2024 PingCAP, Inc.
 package core
 
 import (
+	"github.com/pingcap/tidb/pkg/expression"
 	"github.com/pingcap/tidb/pkg/planner/core/base"
 	"github.com/pingcap/tidb/pkg/planner/util"
 )
