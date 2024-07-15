83c543c2f6fa72e3d1c0e921786f64a0c2ceaa5e
yibin <huyibin@pingcap.cn>
2024-07-15 17:05:53 +0800

    little fix
    Signed-off-by: yibin <huyibin@pingcap.cn>

--- executor/slow_query.go
+++ executor/slow_query.go
@@ -607,7 +607,7 @@ func splitByColon(line string) (fields []string, values []string) {
 					current++
 				}
 			}
-			values = append(values, line[start:current])
+			values = append(values, line[start:min(current, len(line))])
 			parseKey = true
 		}
 	}
