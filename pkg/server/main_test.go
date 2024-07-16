7dc1d22c425549c87908ab37d64c43624f85caa4
Weizhen Wang <wangweizhen@pingcap.com>
2024-07-16 13:39:48 +0800

    server: ignore goleak for ristretto.(*defaultPolicy).processItems
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/server/main_test.go
+++ pkg/server/main_test.go
@@ -62,6 +62,7 @@ func TestMain(m *testing.M) {
 	testDataMap.LoadTestSuiteData("testdata", "optimizer_suite")
 
 	opts := []goleak.Option{
+		goleak.IgnoreTopFunction("github.com/dgraph-io/ristretto.(*defaultPolicy).processItems"),
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
 		goleak.IgnoreTopFunction("github.com/bazelbuild/rules_go/go/tools/bzltestutil.RegisterTimeoutHandler.func1"),
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
