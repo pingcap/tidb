3f5ab5fd4d52c8c7826b8f4f3c7e0104bcfbd8dd
Weizhen Wang <wangweizhen@pingcap.com>
2024-07-17 12:45:00 +0800

    This is an automated cherry-pick of #54450
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/server/main_test.go
+++ pkg/server/main_test.go
@@ -62,6 +62,11 @@ func TestMain(m *testing.M) {
 	testDataMap.LoadTestSuiteData("testdata", "optimizer_suite")
 
 	opts := []goleak.Option{
+<<<<<<< HEAD
+=======
+		goleak.IgnoreTopFunction("github.com/dgraph-io/ristretto.(*defaultPolicy).processItems"),
+		goleak.IgnoreTopFunction("github.com/dgraph-io/ristretto.(*Cache).processItems"),
+>>>>>>> 156f23aacb9 (planer: fix invalid pointer caused by a recursive CTE query (#54450))
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
 		goleak.IgnoreTopFunction("github.com/bazelbuild/rules_go/go/tools/bzltestutil.RegisterTimeoutHandler.func1"),
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
