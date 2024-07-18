62b39d0d4827a8c57a50ba7f0609274ef3ab4c7f
Wenqi Mou <wenqimou@gmail.com>
2024-07-18 13:14:23 -0400

    use goimports, add license
    Signed-off-by: Wenqi Mou <wenqimou@gmail.com>

--- br/pkg/utils/error_handling.go
+++ br/pkg/utils/error_handling.go
@@ -1,12 +1,27 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package utils
 
 import (
 	"fmt"
+	"strings"
+	"sync"
+
 	backuppb "github.com/pingcap/kvproto/pkg/brpb"
 	"github.com/pingcap/log"
 	"go.uber.org/zap"
-	"strings"
-	"sync"
 )
 
 // UNSAFE! TODO: remove and map them to error types
