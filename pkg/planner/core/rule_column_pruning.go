1cd80eb4c1050a4e91c67ea2a635cc9a49cd91c3
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2024-07-23 11:10:33 +0800

    planner: fix the issue accessing unnecessary table side caused by column pruning (#54815)
    close pingcap/tidb#54213

--- pkg/planner/core/rule_column_pruning.go
+++ pkg/planner/core/rule_column_pruning.go
@@ -119,6 +119,10 @@ func (p *LogicalProjection) PruneColumns(parentUsedCols []*expression.Column, op
 	if err != nil {
 		return nil, err
 	}
+	// If its columns are all pruned, we directly use its child. The child will output at least one column.
+	if p.Schema().Len() == 0 {
+		return p.Children()[0], nil
+	}
 	return p, nil
 }
 
@@ -628,10 +632,6 @@ func (p *LogicalWindow) extractUsedCols(parentUsedCols []*expression.Column) []*
 
 // PruneColumns implements LogicalPlan interface.
 func (p *LogicalLimit) PruneColumns(parentUsedCols []*expression.Column, opt *logicalOptimizeOp) (LogicalPlan, error) {
-	if len(parentUsedCols) == 0 { // happens when LIMIT appears in UPDATE.
-		return p, nil
-	}
-
 	savedUsedCols := make([]*expression.Column, len(parentUsedCols))
 	copy(savedUsedCols, parentUsedCols)
 
--- pkg/planner/core/util.go
+++ pkg/planner/core/util.go
@@ -16,6 +16,7 @@ package core
 
 import (
 	"fmt"
+	"math"
 	"slices"
 	"sort"
 	"strings"
@@ -139,6 +140,22 @@ func (s *logicalSchemaProducer) setSchemaAndNames(schema *expression.Schema, nam
 func (s *logicalSchemaProducer) inlineProjection(parentUsedCols []*expression.Column, opt *logicalOptimizeOp) {
 	prunedColumns := make([]*expression.Column, 0)
 	used := expression.GetUsedList(parentUsedCols, s.Schema())
+	if len(parentUsedCols) == 0 {
+		// When this operator output no columns, we return its smallest column for safety.
+		minColLen := math.MaxInt
+		chosenPos := 0
+		for i, col := range s.schema.Columns {
+			flen := col.GetType().GetFlen()
+			if flen < minColLen {
+				chosenPos = i
+				minColLen = flen
+			}
+		}
+		// It should be always true.
+		if len(used) > 0 {
+			used[chosenPos] = true
+		}
+	}
 	for i := len(used) - 1; i >= 0; i-- {
 		if !used[i] {
 			prunedColumns = append(prunedColumns, s.Schema().Columns[i])
