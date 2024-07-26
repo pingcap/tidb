d3d4fc9d59746d9121eeae61358e66b6353fe241
Yiding <winoros@gmail.com>
2024-07-26 19:25:45 +0800

    address comments

--- pkg/util/disjointset/set.go
+++ pkg/util/disjointset/set.go
@@ -14,56 +14,53 @@
 
 package disjointset
 
-// Set is the universal implementation of disjoint set.
-// It's designed for sparse case or not integer type.
-// If you are dealing with continuous integer, you should use SimpleIntSet to avoid the cost of hash map.
+// Set is the universal implementation of a disjoint set.
+// It's designed for sparse cases or non-integer types.
+// If you are dealing with continuous integers, you should use SimpleIntSet to avoid the cost of a hash map.
 // We hash the original value to an integer index and then apply the core disjoint set algorithm.
-// Time complexity: the union operation is inverse ackermann function, which is very close to O(1).
+// Time complexity: the union operation has an inverse Ackermann function time complexity, which is very close to O(1).
 type Set[T comparable] struct {
 	parent  []int
-	val2idx map[T]int
+	val2Idx map[T]int
 	tailIdx int
 }
 
-// NewSet creates the disjoint set.
+// NewSet creates a disjoint set.
 func NewSet[T comparable](size int) *Set[T] {
 	return &Set[T]{
 		parent:  make([]int, 0, size),
-		val2idx: make(map[T]int),
+		val2Idx: make(map[T]int, size),
 		tailIdx: 0,
 	}
 }
-
-func (s *Set[T]) findRootOrigialVal(a T) int {
-	idx, ok := s.val2idx[a]
+func (s *Set[T]) findRootOriginalVal(a T) int {
+	idx, ok := s.val2Idx[a]
 	if !ok {
 		s.parent = append(s.parent, s.tailIdx)
-		s.val2idx[a] = s.tailIdx
+		s.val2Idx[a] = s.tailIdx
 		s.tailIdx++
 		return s.tailIdx - 1
 	}
-	s.parent[idx] = s.findRoot(s.parent[idx])
-	return s.parent[idx]
+	return s.findRoot(idx)
 }
 
-// findRoot is internal impl. Call it inside the findRootOrig.
+// findRoot is an internal implementation. Call it inside findRootOriginalVal.
 func (s *Set[T]) findRoot(a int) int {
-	if s.parent[a] == a {
-		return a
+	if s.parent[a] != a {
+		s.parent[a] = s.findRoot(s.parent[a])
 	}
-	s.parent[a] = s.findRoot(s.parent[a])
 	return s.parent[a]
 }
 
 // InSameGroup checks whether a and b are in the same group.
-func (s *Set[T]) InSameGroup(a T, b T) bool {
-	return s.findRootOrigialVal(a) == s.findRootOrigialVal(b)
+func (s *Set[T]) InSameGroup(a, b T) bool {
+	return s.findRootOriginalVal(a) == s.findRootOriginalVal(b)
 }
 
-// Union unions two sets in disjoint set.
-func (s *Set[T]) Union(a T, b T) {
-	rootA := s.findRootOrigialVal(a)
-	rootB := s.findRootOrigialVal(b)
+// Union joins two sets in the disjoint set.
+func (s *Set[T]) Union(a, b T) {
+	rootA := s.findRootOriginalVal(a)
+	rootB := s.findRootOriginalVal(b)
 	if rootA != rootB {
 		s.parent[rootA] = rootB
 	}
