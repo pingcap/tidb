3c8f2608afe70627bb3c9cac9f1736b0b5c26065
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-08-01 21:28:20 +0800

    planner: fix the issue that statement-level hints in sub-queries of Insert/Replace can not take effect (#54083) (#54954)
    close pingcap/tidb#53834

--- pkg/util/hint/hint_processor.go
+++ pkg/util/hint/hint_processor.go
@@ -91,7 +91,18 @@ func ExtractTableHintsFromStmtNode(node ast.Node, warnHandler hintWarnHandler) [
 	case *ast.InsertStmt:
 		// check duplicated hints
 		checkInsertStmtHintDuplicated(node, warnHandler)
-		return x.TableHints
+		result := make([]*ast.TableOptimizerHint, 0, len(x.TableHints))
+		result = append(result, x.TableHints...)
+		if x.Select != nil {
+			// support statement-level hint in sub-select: "insert into t select /* ... */ ..."
+			// TODO: support this for Update and Delete as well
+			for _, h := range ExtractTableHintsFromStmtNode(x.Select, warnHandler) {
+				if isStmtHint(h) {
+					result = append(result, h)
+				}
+			}
+		}
+		return result
 	case *ast.SetOprStmt:
 		var result []*ast.TableOptimizerHint
 		if x.SelectList == nil {
