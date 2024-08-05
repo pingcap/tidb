91190c6ee7f33d5758b803ea9b6034c2b48ba6c0
“EricZequan” <zequany33@gmail.com>
2024-08-05 11:26:27 +0800

    fix TestVectorColumnInfo fail
    Signed-off-by: “EricZequan” <zequany33@gmail.com>

--- pkg/planner/core/preprocess.go
+++ pkg/planner/core/preprocess.go
@@ -1457,6 +1457,12 @@ func checkColumn(colDef *ast.ColumnDef) error {
 		if tp.GetFlen() > mysql.MaxBitDisplayWidth {
 			return types.ErrTooBigDisplayWidth.GenWithStackByArgs(colDef.Name.Name.O, mysql.MaxBitDisplayWidth)
 		}
+	case mysql.TypeTiDBVectorFloat32:
+		if tp.GetFlen() != types.UnspecifiedLength {
+			if err := types.CheckVectorDimValid(tp.GetFlen()); err != nil {
+				return err
+			}
+		}
 	default:
 		// TODO: Add more types.
 	}
