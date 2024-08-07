5475bd4fb52eb3fde7e06d8bf47cac905546980d
Ruihao Chen <ruihao.chen@pingcap.cn>
2024-08-07 16:56:54 +0800

    Update comments

--- pkg/executor/infoschema_reader.go
+++ pkg/executor/infoschema_reader.go
@@ -3681,7 +3681,6 @@ func (e *memtableRetriever) setDataFromKeywords() error {
 	return nil
 }
 
-// ClusterIndexUsage cannot be pushed down, use the previous logic
 func (e *memtableRetriever) setDataForClusterIndexUsage(ctx context.Context, sctx sessionctx.Context, schemas []model.CIStr) error {
 	dom := domain.GetDomain(sctx)
 	rows := make([][]types.Datum, 0, 100)
@@ -3795,6 +3794,7 @@ func (e *memtableRetriever) setDataFromIndexUsage(ctx context.Context, sctx sess
 	return nil
 }
 
+// Currently, ClusterIndexUsage will not be be pushed down, so just use the previous logic
 func (e *memtableRetriever) setDataFromClusterIndexUsage(ctx context.Context, sctx sessionctx.Context, schemas []model.CIStr) error {
 	err := e.setDataForClusterIndexUsage(ctx, sctx, schemas)
 	if err != nil {
