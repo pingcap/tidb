14dc43d863807dfbc240720d59820b807b7563c3
Jason Mo <mohangjie1995@gmail.com>
2024-08-07 14:03:53 +0800

    update

--- pkg/parser/keywords.go
+++ pkg/parser/keywords.go
@@ -622,7 +622,6 @@ var Keywords = []KeywordsType{
 	{"VALIDATION", false, "unreserved"},
 	{"VALUE", false, "unreserved"},
 	{"VARIABLES", false, "unreserved"},
-	{"VECTOR", false, "unreserved"},
 	{"VIEW", false, "unreserved"},
 	{"VISIBLE", false, "unreserved"},
 	{"WAIT", false, "unreserved"},
