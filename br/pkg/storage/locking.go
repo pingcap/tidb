e3a03793f08065b673c3cbd9dc55d902bfe21b4b
hillium <yujuncen@pingcap.com>
2024-08-08 14:11:54 +0800

    initial implementation
    Signed-off-by: hillium <yujuncen@pingcap.com>

--- br/pkg/storage/locking.go
+++ br/pkg/storage/locking.go
@@ -3,23 +3,115 @@
 package storage
 
 import (
+	"bytes"
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
+	"path"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
 	"github.com/pingcap/tidb/br/pkg/logutil"
+	"go.uber.org/multierr"
 	"go.uber.org/zap"
 )
+
+// ExclusiveWrite is a write that in a strong consistency storage, it will either:
+// - The written file won't be override by other `ExclusiveWrite` writers.
+// - Or it files.
+type ExclusiveWrite struct {
+	// Target is the target file of this txn.
+	// There shouldn't be other files shares this prefix with this file, or the txn will fail.
+	Target string
+	// Content is the content that needed to be written to that file.
+	Content func(txnID uuid.UUID) []byte
+	// Verify allows you add other preconditions to the write.
+	// This will be called when the write is allowed and about to be performed.
+	// If `Verify()` returns an error, the write will be aborted.
+	Verify func(ctx VerifyWriteContext) error
+}
+
+type VerifyWriteContext struct {
+	context.Context
+	Target  string
+	Storage ExternalStorage
+	TxnID   uuid.UUID
+}
+
+func (c *VerifyWriteContext) IntentFileName() string {
+	return fmt.Sprintf("%s.INTENT.%s", c.Target, hex.EncodeToString(c.TxnID[:]))
+}
+
+// CommitTo commits the write to the external storage.
+// It contains two phases:
+// - Intention phase, it will write an "intention" file named "$Target_$TxnID".
+// - Put phase, here it actually write the "$Target" down.
+//
+// In each phase, before writing, it will verify whether the storage is suitable for writing, that is:
+// - There shouldn't be any other intention files.
+func (w ExclusiveWrite) CommitTo(ctx context.Context, s ExternalStorage) (uuid.UUID, error) {
+	txnID := uuid.New()
+	cx := VerifyWriteContext{
+		Context: ctx,
+		Target:  w.Target,
+		Storage: s,
+		TxnID:   txnID,
+	}
+	intentFileName := cx.IntentFileName()
+	checkConflict := func() error {
+		var err error
+		if w.Verify != nil {
+			err = multierr.Append(err, w.Verify(cx))
+		}
+		return multierr.Append(err, assertNoOtherFileOfPrefix(cx, s, w.Target, intentFileName))
+	}
+
+	if err := checkConflict(); err != nil {
+		return uuid.UUID{}, errors.Annotate(err, "during initial check")
+	}
+
+	if err := s.WriteFile(cx, intentFileName, []byte{}); err != nil {
+		return uuid.UUID{}, errors.Annotate(err, "during writing intention file")
+	}
+
+	deleteIntentionFile := func() {
+		if err := s.DeleteFile(cx, intentFileName); err != nil {
+			log.Warn("Cannot delete the intention file, you may delete it manually.", zap.String("file", intentFileName), logutil.ShortError(err))
+		}
+	}
+	defer deleteIntentionFile()
+	if err := checkConflict(); err != nil {
+		return uuid.UUID{}, errors.Annotate(err, "during checking whether there are other intentions")
+	}
+
+	return txnID, s.WriteFile(cx, w.Target, w.Content(txnID))
+}
+
+func assertNoOtherFileOfPrefix(ctx context.Context, s ExternalStorage, pfx string, expect string) error {
+	fileName := path.Base(pfx)
+	dirName := path.Dir(pfx)
+	return s.WalkDir(ctx, &WalkOption{
+		SubDir:    dirName,
+		ObjPrefix: fileName,
+	}, func(path string, size int64) error {
+		if path != expect {
+			return fmt.Errorf("there is conflict file %s", path)
+		}
+		return nil
+	})
+}
 
 // LockMeta is the meta information of a lock.
 type LockMeta struct {
 	LockedAt   time.Time `json:"locked_at"`
 	LockerHost string    `json:"locker_host"`
 	LockerPID  int       `json:"locker_pid"`
+	TxnID      []byte    `json:"txn_id"`
 	Hint       string    `json:"hint"`
 }
 
@@ -67,16 +159,10 @@ func readLockMeta(ctx context.Context, storage ExternalStorage, path string) (Lo
 	return meta, nil
 }
 
-func putLockMeta(ctx context.Context, storage ExternalStorage, path string, meta LockMeta) error {
-	file, err := json.Marshal(meta)
-	if err != nil {
-		return errors.Annotatef(err, "failed to marshal lock meta %s", path)
-	}
-	err = storage.WriteFile(ctx, path, file)
-	if err != nil {
-		return errors.Annotatef(err, "failed to write lock meta at %s", path)
-	}
-	return nil
+type RemoteLock struct {
+	txnID   uuid.UUID
+	storage ExternalStorage
+	path    string
 }
 
 // TryLockRemote tries to create a "lock file" at the external storage.
@@ -84,30 +170,34 @@ func putLockMeta(ctx context.Context, storage ExternalStorage, path string, meta
 // Will return a `ErrLocked` if there is another process already creates the lock file.
 // This isn't a strict lock like flock in linux: that means, the lock might be forced removed by
 // manually deleting the "lock file" in external storage.
-func TryLockRemote(ctx context.Context, storage ExternalStorage, path, hint string) (err error) {
-	defer func() {
-		log.Info("Trying lock remote file.", zap.String("path", path), zap.String("hint", hint), logutil.ShortError(err))
-	}()
-	exists, err := storage.FileExists(ctx, path)
-	if err != nil {
-		return errors.Annotatef(err, "failed to check lock file %s exists", path)
-	}
-	if exists {
-		meta, err := readLockMeta(ctx, storage, path)
-		if err != nil {
-			return err
-		}
-		return ErrLocked{Meta: meta}
+func TryLockRemote(ctx context.Context, storage ExternalStorage, path, hint string) (lock RemoteLock, err error) {
+	writer := ExclusiveWrite{
+		Target: path,
+		Content: func(txnID uuid.UUID) []byte {
+			meta := MakeLockMeta(hint)
+			meta.TxnID = txnID[:]
+			res, err := json.Marshal(meta)
+			if err != nil {
+				log.Panic(
+					"Unreachable: a trivial object cannot be marshaled to JSON.",
+					zap.String("path", path),
+					logutil.ShortError(err),
+				)
+			}
+			return res
+		},
 	}
 
-	meta := MakeLockMeta(hint)
-	return putLockMeta(ctx, storage, path, meta)
+	lock.storage = storage
+	lock.path = path
+	lock.txnID, err = writer.CommitTo(ctx, storage)
+	return
 }
 
 // UnlockRemote removes the lock file at the specified path.
 // Removing that file will release the lock.
-func UnlockRemote(ctx context.Context, storage ExternalStorage, path string) error {
-	meta, err := readLockMeta(ctx, storage, path)
+func (l RemoteLock) Unlock(ctx context.Context) error {
+	meta, err := readLockMeta(ctx, l.storage, l.path)
 	if err != nil {
 		return err
 	}
@@ -115,10 +205,69 @@ func UnlockRemote(ctx context.Context, storage ExternalStorage, path string) err
 	// operation in our ExternalStorage abstraction.
 	// So, once our lock has been overwritten or we are overwriting other's lock,
 	// this information will be useful for troubleshooting.
-	log.Info("Releasing lock.", zap.Stringer("meta", meta), zap.String("path", path))
-	err = storage.DeleteFile(ctx, path)
+	if !bytes.Equal(l.txnID[:], meta.TxnID) {
+		return errors.Errorf("Txn ID mismatch: remote is %v, our is %v", meta.TxnID, l.txnID)
+	}
+
+	log.Info("Releasing lock.", zap.Stringer("meta", meta), zap.String("path", l.path))
+	err = l.storage.DeleteFile(ctx, l.path)
 	if err != nil {
-		return errors.Annotatef(err, "failed to delete lock file %s", path)
+		return errors.Annotatef(err, "failed to delete lock file %s", l.path)
 	}
 	return nil
+}
+
+func TryLockRemoteWrite(ctx context.Context, storage ExternalStorage, path, hint string) (lock RemoteLock, err error) {
+	writer := ExclusiveWrite{
+		Target: fmt.Sprintf("%s.WRIT", path),
+		Content: func(txnID uuid.UUID) []byte {
+			meta := MakeLockMeta(hint)
+			meta.TxnID = txnID[:]
+			res, err := json.Marshal(meta)
+			if err != nil {
+				log.Panic(
+					"Unreachable: a trivial object cannot be marshaled to JSON.",
+					zap.String("path", path),
+					logutil.ShortError(err),
+				)
+			}
+			return res
+		},
+		Verify: func(ctx VerifyWriteContext) error {
+			return assertNoOtherFileOfPrefix(ctx, ctx.Storage, ctx.Target, ctx.IntentFileName())
+		},
+	}
+
+	lock.storage = storage
+	lock.path = path
+	lock.txnID, err = writer.CommitTo(ctx, storage)
+	return
+}
+
+func TryLockRemoteRead(ctx context.Context, storage ExternalStorage, path, hint string) (lock RemoteLock, err error) {
+	readSpec := rand.Int63()
+	writer := ExclusiveWrite{
+		Target: fmt.Sprintf("%s.READ.%016x", path, readSpec),
+		Content: func(txnID uuid.UUID) []byte {
+			meta := MakeLockMeta(hint)
+			meta.TxnID = txnID[:]
+			res, err := json.Marshal(meta)
+			if err != nil {
+				log.Panic(
+					"Unreachable: a trivial object cannot be marshaled to JSON.",
+					zap.String("path", path),
+					logutil.ShortError(err),
+				)
+			}
+			return res
+		},
+		Verify: func(ctx VerifyWriteContext) error {
+			return assertNoOtherFileOfPrefix(ctx, ctx.Storage, fmt.Sprintf("%s.WRIT", ctx.Target), "")
+		},
+	}
+
+	lock.storage = storage
+	lock.path = path
+	lock.txnID, err = writer.CommitTo(ctx, storage)
+	return
 }
--- br/pkg/storage/locking_test.go
+++ br/pkg/storage/locking_test.go
@@ -39,10 +39,10 @@ func requireFileNotExists(t *testing.T, path string) {
 func TestTryLockRemote(t *testing.T) {
 	ctx := context.Background()
 	strg, pth := createMockStorage(t)
-	err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
+	lock, err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
 	require.NoError(t, err)
 	requireFileExists(t, filepath.Join(pth, "test.lock"))
-	err = storage.UnlockRemote(ctx, strg, "test.lock")
+	err = lock.Unlock(ctx)
 	require.NoError(t, err)
 	requireFileNotExists(t, filepath.Join(pth, "test.lock"))
 }
@@ -50,12 +50,30 @@ func TestTryLockRemote(t *testing.T) {
 func TestConflictLock(t *testing.T) {
 	ctx := context.Background()
 	strg, pth := createMockStorage(t)
-	err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
+	lock, err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
 	require.NoError(t, err)
-	err = storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
-	require.ErrorContains(t, err, "locked, meta = Locked")
+	_, err = storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
+	require.ErrorContains(t, err, "conflict file test.lock")
 	requireFileExists(t, filepath.Join(pth, "test.lock"))
-	err = storage.UnlockRemote(ctx, strg, "test.lock")
+	err = lock.Unlock(ctx)
 	require.NoError(t, err)
 	requireFileNotExists(t, filepath.Join(pth, "test.lock"))
+}
+
+func TestRWLock(t *testing.T) {
+	ctx := context.Background()
+	strg, path := createMockStorage(t)
+	lock, err := storage.TryLockRemoteRead(ctx, strg, "test.lock", "I wanna read it!")
+	require.NoError(t, err)
+	lock2, err := storage.TryLockRemoteRead(ctx, strg, "test.lock", "I wanna read it too!")
+	require.NoError(t, err)
+	_, err = storage.TryLockRemoteWrite(ctx, strg, "test.lock", "I wanna write it, you gotta out!")
+	require.Error(t, err)
+	require.NoError(t, lock.Unlock(ctx))
+	require.NoError(t, lock2.Unlock(ctx))
+	l, err := storage.TryLockRemoteWrite(ctx, strg, "test.lock", "Can I have a write lock?")
+	require.NoError(t, err)
+	requireFileExists(t, filepath.Join(path, "test.lock.WRIT"))
+	require.NoError(t, l.Unlock(ctx))
+	requireFileNotExists(t, filepath.Join(path, "test.lock.WRIT"))
 }
