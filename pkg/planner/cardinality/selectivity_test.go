a6132fc72906a720f47a15846fa4bb2aa2fac1b0
tpp <terry.purcell@pingcap.com>
2024-08-10 21:23:57 -0700

    testcase updates after code change1

--- pkg/planner/cardinality/selectivity_test.go
+++ pkg/planner/cardinality/selectivity_test.go
@@ -427,7 +427,7 @@ func TestSelectivity(t *testing.T) {
 		{
 			exprs:                    "a > 1 and b < 2 and c > 3 and d < 4 and e > 5",
 			selectivity:              0.00099451303,
-			selectivityAfterIncrease: 1.51329827770157e-05,
+			selectivityAfterIncrease: 3.772290809327846e-05,
 		},
 		{
 			exprs:                    longExpr,
