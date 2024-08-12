d949ae9ddb9413132b880d51fdb7a9ebb325571a
Ruihao Chen <ruihao.chen@pingcap.cn>
2024-08-12 11:37:56 +0800

    Add test

--- pkg/executor/infoschema_reader_test.go
+++ pkg/executor/infoschema_reader_test.go
@@ -541,6 +541,8 @@ func TestColumnTable(t *testing.T) {
 				where TABLE_SCHEMA = 'test' and TABLE_NAME = 'tbl1' and COLUMN_NAME = 'col_2';`).Check(
 		testkit.RowsWithSep("|",
 			"test|tbl1|col_2"))
+	tk.MustQuery(`select count(*) from information_schema.columns;`).Check(
+		testkit.RowsWithSep("|", "4923"))
 }
 
 func TestIndexUsageTable(t *testing.T) {
@@ -586,6 +588,8 @@ func TestIndexUsageTable(t *testing.T) {
 				where TABLE_SCHEMA = 'test' and TABLE_NAME = 'idt2' and INDEX_NAME = 'idx_4';`).Check(
 		testkit.RowsWithSep("|",
 			"test|idt2|idx_4"))
+	tk.MustQuery(`select count(*) from information_schema.tidb_index_usage;`).Check(
+		testkit.RowsWithSep("|", "72"))
 
 	tk.MustQuery(`select TABLE_SCHEMA, TABLE_NAME, INDEX_NAME from information_schema.tidb_index_usage
 				where TABLE_SCHEMA = 'test1';`).Check(testkit.Rows())
