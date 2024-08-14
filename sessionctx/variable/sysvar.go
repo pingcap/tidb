d41d6946e395684ba4aed2ed9cfd666ed03139e0
Hangjie Mo <mohangjie1995@gmail.com>
2024-08-14 15:05:27 +0800

    update

--- sessionctx/variable/sysvar.go
+++ sessionctx/variable/sysvar.go
@@ -749,7 +749,7 @@ var defaultSysVars = []*SysVar{
 			if err != nil {
 				return err
 			}
-			VarTiDBSuperReadOnly.Store(on)
+			GetSysVar(TiDBSuperReadOnly).SetGlobal(context.Background(), s, "ON")
 		}
 		RestrictedReadOnly.Store(on)
 		return nil
