fe1f54f6b334a40ef9a20cc0c9c4478214c1a63e
lance6716 <lance6716@gmail.com>
2024-08-15 17:41:06 +0800

    debug CI
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- pkg/ddl/ingest/config.go
+++ pkg/ddl/ingest/config.go
@@ -45,6 +45,7 @@ func genConfig(
 	resourceGroup string,
 	concurrency int,
 ) (*local.BackendConfig, error) {
+	tidbCfg := tidb.GetGlobalConfig()
 	cfg := &local.BackendConfig{
 		LocalStoreDir:     jobSortPath,
 		ResourceGroupName: resourceGroup,
