c1ef05ce5b71b63836eddec31fb7a17d2051908b
Weizhen Wang <wangweizhen@pingcap.com>
2024-08-15 19:29:17 +0800

    *: fix cannot get column info from generate column
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/executor/test/analyzetest/analyze_test.go
+++ pkg/executor/test/analyzetest/analyze_test.go
@@ -3128,3 +3128,12 @@ func TestAnalyzePartitionVerify(t *testing.T) {
 		}
 	}
 }
+
+func TestIssue55438(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("CREATE TABLE t0(c0 NUMERIC , c1 BIGINT UNSIGNED  AS ((CASE 0 WHEN false THEN 1358571571 ELSE TRIM(c0) END )));")
+	tk.MustExec("CREATE INDEX i0 ON t0(c1);")
+	tk.MustExec("analyze table t0")
+}
