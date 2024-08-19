0a8195be577367c6ed2535d2476d9fe5e70a3bde
lance6716 <lance6716@gmail.com>
2024-08-19 15:46:34 +0800

    --wip-- [skip ci]

--- tests/realtikvtest/importintotest/import_into_test.go
+++ tests/realtikvtest/importintotest/import_into_test.go
@@ -145,6 +145,12 @@ func (s *mockGCSSuite) TestBasicImportInto() {
 			"6,test6,66"),
 	})
 	s.prepareAndUseDB("import_into")
+	// IMPORT INTO does not support subquery in SET
+	s.tk.MustExec("drop table if exists t")
+	s.tk.MustExec("create table t (a bigint, b varchar(100), c int);")
+	sql := fmt.Sprintf(`IMPORT INTO t(a, @, c) SET b=(SELECT 'subquery') FROM 'gs://test-multi-load/db.tbl.001.csv?endpoint=%s'
+		with thread=1`, gcsEndpoint)
+	s.tk.MustGetErrMsg(sql, "lance test")
 
 	allData := []string{"1 test1 11", "2 test2 22", "3 test3 33", "4 test4 44", "5 test5 55", "6 test6 66"}
 	cases := []struct {
