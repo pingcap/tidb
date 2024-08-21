c4d7d6faa12c1e4e2b72a1088d09ec73300ad0c5
yibin <huyibin@pingcap.cn>
2024-08-21 16:32:03 +0800

    Little update
    Signed-off-by: yibin <huyibin@pingcap.cn>

--- pkg/util/profileProcess/profile_process_cpu_test.go
+++ pkg/util/profileProcess/profile_process_cpu_test.go
@@ -16,15 +16,15 @@ package profileprocess
 
 import (
 	"context"
-	"github.com/pingcap/tidb/pkg/util/logutil"
-	"go.uber.org/zap"
 	"testing"
 	"time"
 
 	"github.com/pingcap/tidb/pkg/util/cpuprofile"
 	"github.com/pingcap/tidb/pkg/util/cpuprofile/testutil"
+	"github.com/pingcap/tidb/pkg/util/logutil"
 	topsqlstate "github.com/pingcap/tidb/pkg/util/topsql/state"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
 func TestPProfCPUProfile(t *testing.T) {
@@ -45,30 +45,8 @@ func TestPProfCPUProfile(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	testutil.MockCPULoad(ctx, "sql_global_uid")
+	testutil.MockCPULoad(ctx, "sql_global")
 	time.Sleep(interval * 2)
-	/*// Test disable then re-enable.
-	topsqlstate.DisableTopSQL()
-	time.Sleep(interval * 2)
-	dataChLen := len(mc.dataCh)
-	deltaLen := 0
-	topsqlstate.EnableTopSQL()
-	for i := 0; i < 10; i++ {
-		t1 := time.Now()
-		data = <-mc.dataCh
-		require.True(t, time.Since(t1) < interval*4)
-		if len(data) > 0 {
-			deltaLen++
-			if deltaLen > dataChLen {
-				// Here we can ensure that we receive new data after "re-enable".
-				break
-			}
-		}
-	}
-	require.True(t, len(data) > 0)
-	require.True(t, deltaLen > dataChLen)
-	require.Equal(t, []byte("sql_digest value"), data[0].SQLDigest)
-	*/
 }
 
 type mockUpdater struct {
