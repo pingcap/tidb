0c0e5910c61b04588ea9cb08a38295d79dfc10fb
AilinKid <314806019@qq.com>
2024-08-21 17:39:27 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

---
+++ pkg/planner/cascades/canonical.go
@@ -0,0 +1,204 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cascades
+
+import (
+	"bytes"
+	"math"
+)
+
+const (
+	// both offset and prime are used to compute the fnv-1a's
+	// hash value which is more unique efficient than fnv-1.
+	//
+	// offset64 is ported from fnv.go from go library.
+	offset64 = 14695981039346656037
+
+	// prime64 is ported from fnv.go from go library.
+	prime64 = 1099511628211
+)
+
+type Hash64a uint64
+
+// Hasher is a helper struct that's used for computing **fnv-1a** hash values and tell
+// the equivalence on expression/operators. To use, first call the init method, then
+// a series of hash methods. The final value is stored in the hash64a field.
+type Hasher struct {
+	// hash stores the hash value as it is incrementally computed.
+	hash64a Hash64a
+}
+
+// NewHasher creates a new Hasher.
+func NewHasher() *Hasher {
+	return &Hasher{
+		hash64a: offset64,
+	}
+}
+
+// Reset resets the Hasher to its initial state, reusing the internal bytes slice.
+func (h *Hasher) Reset() {
+	h.hash64a = offset64
+}
+
+// ------------------------------ Hash functions ----------------------------------------
+// Previously, expressions' hashcode are computed by encoding meta layer by layer from the
+// bottom up. This is not efficient and oom risky because each expression has cached numerous
+// hash bytes on their own.
+//
+// The new hash function is based on the fnv-1a hash algorithm, outputting the uint64 only.
+// To avoid the OOM during the hash computation, we use a shared bytes slice to take in primitive
+// types from targeted expressions/operators. The bytes slice is reused and reset after each
+// usage of them.
+//
+// The standardized fnv-1a lib only takes in bytes slice as input, so we need to convert every
+// primitive type to bytes slice inside Hash function implementation of every expression/operators
+// by allocating some temporary slice. This is undesirable, and we just made the Hasher to take in
+// primitive type directly.
+// ---------------------------------------------------------------------------------------
+
+// HashBool hashes a Boolean value.
+func (h *Hasher) HashBool(val bool) {
+	i := 0
+	if val {
+		i = 1
+	}
+	h.hash64a ^= Hash64a(i)
+	h.hash64a *= prime64
+}
+
+// HashInt hashes an integer value.
+func (h *Hasher) HashInt(val int) {
+	h.hash64a ^= Hash64a(val)
+	h.hash64a *= prime64
+}
+
+// HashInt64 hashes an int64 value.
+func (h *Hasher) HashInt64(val int64) {
+	h.hash64a ^= Hash64a(val)
+	h.hash64a *= prime64
+}
+
+// HashUint64 hashes a uint64 value.
+func (h *Hasher) HashUint64(val uint64) {
+	h.hash64a ^= Hash64a(val)
+	h.hash64a *= prime64
+}
+
+// HashFloat64 hashes a float64 value.
+func (h *Hasher) HashFloat64(val float64) {
+	h.hash64a ^= Hash64a(math.Float64bits(val))
+	h.hash64a *= prime64
+}
+
+// HashRune hashes a rune value.
+func (h *Hasher) HashRune(val rune) {
+	h.hash64a ^= Hash64a(val)
+	h.hash64a *= prime64
+}
+
+// HashString hashes a string value.
+// eg: "我是谁" is with 3 rune inside, each rune of them takes up 3-4 bytes.
+func (h *Hasher) HashString(val string) {
+	for _, c := range val {
+		h.HashRune(c)
+	}
+}
+
+// HashByte hashes a byte value.
+// a byte can be treated as a simple rune as well.
+func (h *Hasher) HashByte(val byte) {
+	h.HashRune(rune(val))
+}
+
+// HashBytes hashes a byte slice value.
+func (h *Hasher) HashBytes(val []byte) {
+	for _, c := range val {
+		h.HashByte(c)
+	}
+}
+
+// ------------------------------ Equal functions ----------------------------------------
+// The equal functions are used to compare the equivalence of two primitive types. The
+// implementation is straightforward and simple.
+//
+// For composed and complex user defined structure or types, the implementor should implement
+// the equal functions for them. The equal functions should be implemented in a way that
+// the hash functions are implemented.
+//
+// Equal functions is called from the hash collision detection algorithm to determine whether
+// two expressions/operators are equivalent. For primitive types, we can directly compare them.
+// while for composed and complex user defined structure or types, we should compare them field
+// by field and recursively call down of them if it's embedded.
+// ---------------------------------------------------------------------------------------
+
+func (h *Hasher) EqualBool(l, r bool) bool {
+	return l == r
+}
+
+func (h *Hasher) EqualInt(l, r int) bool {
+	return l == r
+}
+
+func (h *Hasher) EqualInt64(l, r int64) bool {
+	return l == r
+}
+
+func (h *Hasher) EqualFloat64(l, r float64) bool {
+	// Compare bit representations so that NaN == NaN and 0 != -0.
+	return math.Float64bits(l) == math.Float64bits(r)
+}
+
+func (h *Hasher) EqualRune(l, r rune) bool {
+	return l == r
+}
+
+func (h *Hasher) EqualString(l, r string) bool {
+	return l == r
+}
+
+func (h *Hasher) EqualByte(l, r byte) bool {
+	return l == r
+}
+
+func (h *Hasher) EqualBytes(l, r []byte) bool {
+	return bytes.Equal(l, r)
+}
+
+// ------------------------------ Object Implementation -------------------------------------
+// For primitive type, we can directly hash them and compare them. Based on the primitive
+// interface call listed here, we can easily implement the hash and equal functions for other
+// composed and complex user defined structure or types.
+//
+// Say we have a structure like this:
+// type MyStruct struct {
+//     a int
+// 	   b string
+//     c OtherStruct
+//     d Pointer
+// }
+// so we can implement the hash and equal functions like this:
+// func (h *Hasher) Hash64(val MyStruct) {
+//     h.HashInt(val.a)
+//     h.HashString(val.b)
+// 	   // for c here, it calls for the hash function of OtherStruct implementor.
+//     c.Hash64(h)
+//	   // for pointer, how it could be hashed is up to the implementor.
+//     h.HashUint64(uint64(val.d))
+// }
+//
+// func (h *Hasher) Equal(val1, val2 MyStruct) bool {
+//     return h.EqualInt(val1.a, val2.a) && h.EqualString(val1.b, val2.b) && val1.c.Equal(val2.c) && val1.d == val2.d
+// }
+// ------------------------------------------------------------------------------------------
---
+++ pkg/planner/cascades/canonical_test.go
@@ -0,0 +1,59 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cascades
+
+import (
+	"testing"
+	
+	"github.com/stretchr/testify/require"
+)
+
+func TestHash64a(t *testing.T) {
+	hasher1 := NewHasher()
+	hasher2 := NewHasher()
+	hasher1.HashBool(true)
+	hasher2.HashBool(true)
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashBool(false)
+	hasher2.HashBool(false)
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashInt(199)
+	hasher2.HashInt(199)
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashInt64(13534523462346)
+	hasher2.HashInt64(13534523462346)
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashUint64(13534523462346)
+	hasher2.HashUint64(13534523462346)
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashString("hello")
+	hasher2.HashString("hello")
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashBytes([]byte("world"))
+	hasher2.HashBytes([]byte("world"))
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.HashRune('我')
+	hasher1.HashRune('是')
+	hasher1.HashRune('谁')
+	hasher2.HashRune('我')
+	hasher2.HashRune('是')
+	hasher2.HashRune('谁')
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+	hasher1.Reset()
+	hasher2.Reset()
+	hasher1.HashString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	hasher2.HashString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	require.Equal(t, hasher1.hash64a, hasher2.hash64a)
+}
