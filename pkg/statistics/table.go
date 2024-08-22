5ced99722f5dd326a4462ef9b81b91e8de23dcc9
Weizhen Wang <wangweizhen@pingcap.com>
2024-08-22 18:54:32 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/table.go
+++ pkg/statistics/table.go
@@ -416,28 +416,6 @@ func (t *Table) GetStatsInfo(id int64, isIndex bool, needCopy bool) (*Histogram,
 	return nil, nil, nil, nil, false
 }
 
-<<<<<<< HEAD
-=======
-// IsAnalyzed checks whether the table is analyzed or not by checking its last analyze's timestamp value.
-// A valid timestamp must be greater than 0.
-func (t *Table) IsAnalyzed() bool {
-	return t.LastAnalyzeVersion > 0
-}
-
-// IsEligibleForAnalysis checks whether the table is eligible for analysis.
-func (t *Table) IsEligibleForAnalysis() bool {
-	// 1. If the statistics are either not loaded or are classified as pseudo, there is no need for analyze.
-	//    Pseudo statistics can be created by the optimizer, so we need to double check it.
-	// 2. If the table is too small, we don't want to waste time to analyze it.
-	//    Leave the opportunity to other bigger tables.
-	if t == nil || t.Pseudo || t.RealtimeCount < AutoAnalyzeMinCnt {
-		return false
-	}
-
-	return true
-}
-
->>>>>>> 7e73ddc91b5 (statistics: add metrics for unneeded analyze table (#54822))
 // GetAnalyzeRowCount tries to get the row count of a column or an index if possible.
 // This method is useful because this row count doesn't consider the modify count.
 func (coll *HistColl) GetAnalyzeRowCount() float64 {
