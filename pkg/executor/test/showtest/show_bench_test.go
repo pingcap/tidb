ff32b657b2ef7afebccec897b966dc0895d14761
Weizhen Wang <wangweizhen@pingcap.com>
2024-08-23 16:45:56 +0800

    executor: add benchmark for the show
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ pkg/executor/test/showtest/show_bench_test.go
@@ -0,0 +1,39 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package showtest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/pingcap/tidb/pkg/testkit"
+)
+
+func BenchmarkShowGrants(b *testing.B) {
+	store := testkit.CreateMockStore(b)
+
+	tk := testkit.NewTestKit(b, store)
+	tk.MustExec("use test")
+	for i := 0; i < 1000; i++ {
+		tk.MustExec(fmt.Sprintf("create table t%d(a int);", i))
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tk.MustQuery("SHOW TABLES;")
+	}
+	b.StopTimer()
+
+}
