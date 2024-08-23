2e688f293b3aef97f0ec3e66baa36538ce2bed45
cbcwestwolf <1004626265@qq.com>
2024-08-23 15:02:47 +0800

    finish TestMemtableInfoschemaExtractor

--- pkg/planner/core/memtable_infoschema_extractor_test.go
+++ pkg/planner/core/memtable_infoschema_extractor_test.go
@@ -100,11 +100,11 @@ func (cp colPredicates) enumerateAll() (res []string) {
 
 type testCase struct {
 	memTableName string
-	prepareData  func(t *testing.T, tk *testkit.TestKit) (names []colPredicates)
-	cleanData    func(t *testing.T, tk *testkit.TestKit)
+	prepareData  func(tk *testkit.TestKit) (names []colPredicates)
+	cleanData    func(tk *testkit.TestKit)
 }
 
-func prepareDataTables(_ *testing.T, tk *testkit.TestKit) (names []colPredicates) {
+func prepareDataTables(tk *testkit.TestKit) (names []colPredicates) {
 	schemaNames := []string{"schema_tables1", "schema_tables2"}
 	tableNames := []string{"t1", "t2"}
 	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
@@ -122,12 +122,12 @@ func prepareDataTables(_ *testing.T, tk *testkit.TestKit) (names []colPredicates
 	return
 }
 
-func cleanDataTables(t *testing.T, tk *testkit.TestKit) {
+func cleanDataTables(tk *testkit.TestKit) {
 	tk.MustExec("drop database schema_tables1")
 	tk.MustExec("drop database schema_tables2")
 }
 
-func prepareDataTiDBIndexes(_ *testing.T, tk *testkit.TestKit) (names []colPredicates) {
+func prepareDataTiDBIndexes(tk *testkit.TestKit) (names []colPredicates) {
 	schemaNames := []string{"schema_tidb_indexes1", "schema_tidb_indexes2", "schema_tidb_indexes3"}
 	tableNames := []string{"t1", "t2", "t3"}
 	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
@@ -142,16 +142,207 @@ func prepareDataTiDBIndexes(_ *testing.T, tk *testkit.TestKit) (names []colPredi
 	return
 }
 
-func cleanDataTiDBIndexes(t *testing.T, tk *testkit.TestKit) {
+func cleanDataTiDBIndexes(tk *testkit.TestKit) {
 	tk.MustExec("drop database schema_tidb_indexes1")
 	tk.MustExec("drop database schema_tidb_indexes2")
 	tk.MustExec("drop database schema_tidb_indexes3")
+}
+
+func prepareDataViews(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_views1", "schema_views2", "schema_views3"}
+	viewNames := []string{"v1", "v2", "v3"}
+	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.TableName, values: viewNames})
+
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		tk.MustExec(fmt.Sprintf("create table %s.t (c int)", schemaName))
+		for _, viewName := range viewNames {
+			tk.MustExec(fmt.Sprintf("create view %s.%s as select * from %s.t", schemaName, viewName, schemaName))
+		}
+	}
+	return
+}
+
+func cleanDataViews(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_views1")
+	tk.MustExec("drop database schema_views2")
+	tk.MustExec("drop database schema_views3")
+}
+
+func prepareDataKeyColumnUsage(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_key_column_usage1", "schema_key_column_usage2"}
+	tableNames := []string{"t1", "t2"}
+	constraintNames := []string{"c1", "c2"}
+	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.TableName, values: tableNames})
+	names = append(names, colPredicates{colName: core.ConstraintSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.ConstraintName, values: constraintNames})
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		for _, tableName := range tableNames {
+			tk.MustExec(fmt.Sprintf("create table %s.%s (a int)", schemaName, tableName))
+			for _, constraintName := range constraintNames {
+				tk.MustExec(fmt.Sprintf("alter table %s.%s add constraint %s unique(a)", schemaName, tableName, constraintName))
+			}
+		}
+	}
+	return
+}
+
+func cleanDataKeyColumnUsage(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_key_column_usage1")
+	tk.MustExec("drop database schema_key_column_usage2")
+}
+
+func prepareDataPartitions(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_partition1", "schema_partition2"}
+	tableNames := []string{"t1", "t2"}
+	partitionNames := []string{"p1", "p2"}
+
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		for _, tableName := range tableNames {
+			tk.MustExec(fmt.Sprintf("create table %s.%s (a int) partition by list (a) (partition p1 values in (1, 2, 3), partition p2 default)", schemaName, tableName))
+		}
+	}
+
+	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.TableName, values: tableNames})
+	names = append(names, colPredicates{colName: core.PartitionName, values: partitionNames})
+	return
+}
+
+func cleanDataPartitions(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_partition1")
+	tk.MustExec("drop database schema_partition2")
+}
+
+func prepareDataStatistics(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_statistics1", "schema_statistics2"}
+	tableNames := []string{"t1", "t2"}
+	indexNames := []string{"i1", "i2"}
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		for _, tableName := range tableNames {
+			tk.MustExec(fmt.Sprintf("create table %s.%s (i1 int, i2 int, unique key(i1), unique key(i2))", schemaName, tableName))
+		}
+	}
+	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.TableName, values: tableNames})
+	names = append(names, colPredicates{colName: core.IndexName, values: indexNames})
+	return
+}
+
+func cleanDataStatistics(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_statistics1")
+	tk.MustExec("drop database schema_statistics2")
+}
+
+func prepareDataSchemata(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_schemata1", "schema_schemata2", "schema_schemata3"}
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+	}
+	names = append(names, colPredicates{colName: core.SchemaName, values: schemaNames})
+	return
+}
+
+func cleanDataSchemata(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_schemata1")
+	tk.MustExec("drop database schema_schemata2")
+	tk.MustExec("drop database schema_schemata3")
+}
+
+func prepareDataCheckConstraints(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_check_constraints1", "schema_check_constraints2", "schema_check_constraints3"}
+	constrainNames := []string{"c1", "c2", "c3"}
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		tk.MustExec(fmt.Sprintf("create table %s.t (a int, constraint c1 check (a > 0), constraint c2 check (a = 0), constraint c3 check (a < 0))", schemaName))
+	}
+	names = append(names, colPredicates{colName: core.ConstraintSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.ConstraintName, values: constrainNames})
+	return
+}
+
+func cleanDataCheckConstraints(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_check_constraints1")
+	tk.MustExec("drop database schema_check_constraints2")
+	tk.MustExec("drop database schema_check_constraints3")
+}
+
+func prepareDataTidbCheckConstraints(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_tidb_check_constraints1", "schema_tidb_check_constraints2"}
+	tableNames := []string{"t1", "t2"}
+	constrainNames := []string{"t1_c", "t2_c"}
+	tableIDs := []string{}
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		for _, tableName := range tableNames {
+			tk.MustExec(fmt.Sprintf("create table %s.%s (a int, constraint %s_c check (a != 0))", schemaName, tableName, tableName))
+			sql := fmt.Sprintf("select distinct table_id from information_schema.tidb_check_constraints where CONSTRAINT_SCHEMA = '%s' and TABLE_NAME = '%s'", schemaName, tableName)
+			id := tk.MustQuery(sql).String()
+			tableIDs = append(tableIDs, id)
+		}
+	}
+	names = append(names, colPredicates{colName: core.ConstraintSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.TableName, values: tableNames})
+	names = append(names, colPredicates{colName: core.ConstraintName, values: constrainNames})
+	names = append(names, colPredicates{colName: core.TableID, values: tableIDs, isInteger: true})
+	return
+}
+
+func cleanDataTidbCheckConstraints(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_tidb_check_constraints1")
+	tk.MustExec("drop database schema_tidb_check_constraints2")
+}
+
+func prepareDataSequences(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_sequences1", "schema_sequences2"}
+	sequenceNames := []string{"s1", "s2"}
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		for _, sequenceName := range sequenceNames {
+			tk.MustExec(fmt.Sprintf("create sequence %s.%s", schemaName, sequenceName))
+		}
+	}
+	names = append(names, colPredicates{colName: core.SequenceSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.SequenceName, values: sequenceNames})
+	return
+}
+
+func cleanDataSequences(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_sequences1")
+	tk.MustExec("drop database schema_sequences2")
+}
+
+func prepareDataColumns(tk *testkit.TestKit) (names []colPredicates) {
+	schemaNames := []string{"schema_columns1", "schema_columns2"}
+	tableNames := []string{"t1", "t2"}
+	columnNames := []string{"c1", "c2"}
+	for _, schemaName := range schemaNames {
+		tk.MustExec(fmt.Sprintf("create database %s", schemaName))
+		for _, tableName := range tableNames {
+			tk.MustExec(fmt.Sprintf("create table %s.%s (c1 int, c2 int)", schemaName, tableName))
+		}
+	}
+	names = append(names, colPredicates{colName: core.TableSchema, values: schemaNames})
+	names = append(names, colPredicates{colName: core.TableName, values: tableNames})
+	names = append(names, colPredicates{colName: core.ColumnName, values: columnNames})
+	return
+}
+
+func cleanDataColumns(tk *testkit.TestKit) {
+	tk.MustExec("drop database schema_columns1")
+	tk.MustExec("drop database schema_columns2")
 }
 
 func TestMemtableInfoschemaExtractor(t *testing.T) {
 	store := testkit.CreateMockStore(t)
 	tk := testkit.NewTestKit(t, store)
 
+	tk.MustExec("set global tidb_enable_check_constraint = true")
 	tcs := []testCase{
 		{
 			memTableName: infoschema.TableTiDBIndexes,
@@ -163,13 +354,61 @@ func TestMemtableInfoschemaExtractor(t *testing.T) {
 			prepareData:  prepareDataTables,
 			cleanData:    cleanDataTables,
 		},
-
-		// TODO: add more infoschema tables
+		{
+			memTableName: infoschema.TableViews,
+			prepareData:  prepareDataViews,
+			cleanData:    cleanDataViews,
+		},
+		{
+			memTableName: infoschema.TableKeyColumn,
+			prepareData:  prepareDataKeyColumnUsage,
+			cleanData:    cleanDataKeyColumnUsage,
+		},
+		{
+			memTableName: infoschema.TableConstraints,
+			prepareData:  prepareDataKeyColumnUsage,
+			cleanData:    cleanDataKeyColumnUsage,
+		},
+		{
+			memTableName: infoschema.TablePartitions,
+			prepareData:  prepareDataPartitions,
+			cleanData:    cleanDataPartitions,
+		},
+		{
+			memTableName: infoschema.TableStatistics,
+			prepareData:  prepareDataStatistics,
+			cleanData:    cleanDataStatistics,
+		},
+		{
+			memTableName: infoschema.TableSchemata,
+			prepareData:  prepareDataSchemata,
+			cleanData:    cleanDataSchemata,
+		},
+		{
+			memTableName: infoschema.TableCheckConstraints,
+			prepareData:  prepareDataCheckConstraints,
+			cleanData:    cleanDataCheckConstraints,
+		},
+		{
+			memTableName: infoschema.TableTiDBCheckConstraints,
+			prepareData:  prepareDataTidbCheckConstraints,
+			cleanData:    cleanDataTidbCheckConstraints,
+		},
+		{
+			memTableName: infoschema.TableSequences,
+			prepareData:  prepareDataSequences,
+			cleanData:    cleanDataSequences,
+		},
+		{
+			memTableName: infoschema.TableTiDBIndexUsage,
+			prepareData:  prepareDataStatistics,
+			cleanData:    cleanDataStatistics,
+		},
 	}
 
 	countSQL := 0
 	for _, tc := range tcs {
-		names := tc.prepareData(t, tk)
+		names := tc.prepareData(tk)
 		conditions := []string{}
 		for i := len(names) - 1; i >= 0; i-- {
 			all := names[i].enumerateAll()
@@ -192,7 +431,7 @@ func TestMemtableInfoschemaExtractor(t *testing.T) {
 			tk.MustQuery(sql)
 		}
 
-		tc.cleanData(t, tk)
+		tc.cleanData(tk)
 	}
 	fmt.Printf("Total SQLs: %d\n", countSQL)
 }
