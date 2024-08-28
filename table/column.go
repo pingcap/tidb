2f8402fd224da970f640aeda6e9aae02b59df2cf
Jason Mo <mohangjie1995@gmail.com>
2024-08-28 10:57:21 +0800

    update

--- table/column.go
+++ table/column.go
@@ -645,7 +645,7 @@ func getColDefaultValueFromNil(ctx sessionctx.Context, col *model.ColumnInfo, ar
 		sc.AppendWarning(err)
 		return GetZeroValue(col), nil
 	}
-	return types.Datum{}, ErrNoDefaultValue.FastGenByArgs(col.Name)
+	return types.Datum{}, ErrNoDefaultValue.GenWithStackByArgs(col.Name)
 }
 
 // GetZeroValue gets zero value for given column type.
