5585cdc1f38d742e1998c435e42bc952d7d44234
Lynn <zimu_xia@126.com>
2024-08-30 10:56:16 +0800

    pkt/parser: tiny update

--- pkg/parser/keywords.go
+++ pkg/parser/keywords.go
@@ -623,7 +623,7 @@ var Keywords = []KeywordsType{
 	{"VALIDATION", false, "unreserved"},
 	{"VALUE", false, "unreserved"},
 	{"VARIABLES", false, "unreserved"},
-	{"VECTOR", false, "reserved"},
+	{"VECTOR", false, "unreserved"},
 	{"VIEW", false, "unreserved"},
 	{"VISIBLE", false, "unreserved"},
 	{"WAIT", false, "unreserved"},
