1d788a0b72e0d43f65868767c7aaed826db77002
lance6716 <lance6716@gmail.com>
2024-09-03 09:53:05 +0800

    fix a UT
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- pkg/lightning/backend/local/local_test.go
+++ pkg/lightning/backend/local/local_test.go
@@ -1341,6 +1341,7 @@ func TestCheckPeersBusy(t *testing.T) {
 }
 
 func TestNotLeaderErrorNeedUpdatePeers(t *testing.T) {
+	log.InitLogger(&log.Config{Level: "debug"}, "")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -1387,7 +1388,7 @@ func TestNotLeaderErrorNeedUpdatePeers(t *testing.T) {
 	jobCh := make(chan *regionJob, 10)
 
 	staleJob := &regionJob{
-		keyRange: common.Range{Start: []byte("a"), End: []byte("")},
+		keyRange: common.Range{Start: []byte("a"), End: []byte("b")},
 		region: &split.RegionInfo{
 			Region: &metapb.Region{
 				Id: 1,
