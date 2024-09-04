b91cb0191799a2f055793894c5172f838ed77afa
wjHuang <huangwenjun1997@gmail.com>
2024-09-04 19:23:09 +0800

    core: fix missing update on nested generated column (#55829)
    close pingcap/tidb#53967

--- pkg/planner/core/logical_plan_builder.go
+++ pkg/planner/core/logical_plan_builder.go
@@ -5614,6 +5614,9 @@ func (b *PlanBuilder) buildUpdateLists(ctx context.Context, tableList []*ast.Tab
 					break
 				}
 			}
+			if isModified {
+				dependentColumnsModified[col.UniqueID] = true
+			}
 			// skip unmodified generated columns
 			if !isModified {
 				continue
