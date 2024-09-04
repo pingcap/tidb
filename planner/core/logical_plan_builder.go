434c08d98b9aaf69964cf73826edfc4f1a61f874
wjHuang <huangwenjun1997@gmail.com>
2024-09-04 19:23:09 +0800

    This is an automated cherry-pick of #55829
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- planner/core/logical_plan_builder.go
+++ planner/core/logical_plan_builder.go
@@ -5981,6 +5981,9 @@ func (b *PlanBuilder) buildUpdateLists(ctx context.Context, tableList []*ast.Tab
 					break
 				}
 			}
+			if isModified {
+				dependentColumnsModified[col.UniqueID] = true
+			}
 			// skip unmodified generated columns
 			if !isModified {
 				continue
