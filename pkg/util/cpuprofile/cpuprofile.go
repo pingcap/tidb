d42c7d71fa7eeef941d21f1ccf3ceb4f66cb6c3e
yibin <huyibin@pingcap.cn>
2024-09-09 16:55:24 +0800

    Improve cpu profile frequency
    Signed-off-by: yibin <huyibin@pingcap.cn>

--- pkg/util/cpuprofile/cpuprofile.go
+++ pkg/util/cpuprofile/cpuprofile.go
@@ -29,7 +29,7 @@ import (
 )
 
 // DefProfileDuration exports for testing.
-var DefProfileDuration = time.Second
+var DefProfileDuration = 100 * time.Millisecond
 
 // globalCPUProfiler is the global CPU profiler.
 var globalCPUProfiler = newParallelCPUProfiler()
