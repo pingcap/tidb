ca05911994caa93da38c00064a4c706c59e715a0
Lynn <zimu_xia@126.com>
2024-09-10 10:06:46 +0800

    *: tiny update

--- server/server_test.go
+++ server/server_test.go
@@ -2717,11 +2717,10 @@ func (cli *testServerClient) runTestIssue53634(t *testing.T, ts *tidbTestSuite, 
 	}, "MDL", func(dbt *testkit.DBTestKit) {
 		ctx := context.Background()
 
-		variable.EnableMDL.Store(true)
-		t.Logf("set enable mdl:%v", variable.EnableMDL.Load())
 		conn, err := dbt.GetDB().Conn(ctx)
 		require.NoError(t, err)
 		MustExec(ctx, t, conn, "set global tidb_enable_metadata_lock=1")
+		t.Logf("enable mdl:%v", variable.EnableMDL.Load())
 		MustExec(ctx, t, conn, "create database test_db_state default charset utf8 default collate utf8_bin")
 		MustExec(ctx, t, conn, "use test_db_state")
 		MustExec(ctx, t, conn, `CREATE TABLE stock (
@@ -2833,7 +2832,6 @@ func runTestInSchemaState(
 	} else {
 		callback.OnJobRunBeforeExported = cbFunc1
 	}
-	t.Logf("enable mdl:%v", variable.EnableMDL.Load())
 	d := dom.DDL()
 	originalCallback := d.GetHook()
 	d.SetHook(callback)
