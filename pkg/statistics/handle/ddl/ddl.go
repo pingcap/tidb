ece1cd2e5105575bc61a33c6767d56f2a028ff97
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2024-09-10 16:31:26 +0800

    ||

--- pkg/statistics/handle/ddl/ddl.go
+++ pkg/statistics/handle/ddl/ddl.go
@@ -182,7 +182,7 @@ func (h *ddlHandlerImpl) HandleDDLEvent(s *ddlutil.SchemaChangeEvent) error {
 	case model.ActionFlashbackCluster:
 		return h.statsWriter.UpdateStatsVersion()
 	default:
-		errors.Trace(errors.Errorf("unhandled DDL type %v", s.GetType()))
+		return errors.Trace(errors.Errorf("unhandled DDL type %v", s.GetType()))
 	}
 	return nil
 }
