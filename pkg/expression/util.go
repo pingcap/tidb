e47143360127802a3ed69597261bbb03e44d4a5d
victorgao <kafka1991@163.com>
2024-09-13 17:21:19 +0800

    *: Refactor param.ExecArgs from pkg param to pkg expression. (#55784)

--- pkg/expression/util.go
+++ pkg/expression/util.go
@@ -17,6 +17,8 @@ package expression
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
+	"fmt"
 	"math"
 	"strconv"
 	"strings"
@@ -27,6 +29,7 @@ import (
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/expression/contextopt"
 	"github.com/pingcap/tidb/pkg/kv"
+	"github.com/pingcap/tidb/pkg/param"
 	"github.com/pingcap/tidb/pkg/parser/ast"
 	"github.com/pingcap/tidb/pkg/parser/charset"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
@@ -36,6 +39,7 @@ import (
 	driver "github.com/pingcap/tidb/pkg/types/parser_driver"
 	"github.com/pingcap/tidb/pkg/util/chunk"
 	"github.com/pingcap/tidb/pkg/util/collate"
+	"github.com/pingcap/tidb/pkg/util/hack"
 	"github.com/pingcap/tidb/pkg/util/intset"
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"go.uber.org/zap"
@@ -1919,4 +1923,242 @@ func ExprHasSetVarOrSleep(expr Expression) bool {
 		}
 	}
 	return false
+}
+
+// ExecBinaryParam parse execute binary param arguments to datum slice.
+func ExecBinaryParam(typectx types.Context, binaryParams []param.BinaryParam) (params []Expression, err error) {
+	var (
+		tmp any
+	)
+
+	params = make([]Expression, len(binaryParams))
+	args := make([]types.Datum, len(binaryParams))
+	for i := 0; i < len(args); i++ {
+		tp := binaryParams[i].Tp
+		isUnsigned := binaryParams[i].IsUnsigned
+
+		switch tp {
+		case mysql.TypeNull:
+			var nilDatum types.Datum
+			nilDatum.SetNull()
+			args[i] = nilDatum
+			continue
+
+		case mysql.TypeTiny:
+			if isUnsigned {
+				args[i] = types.NewUintDatum(uint64(binaryParams[i].Val[0]))
+			} else {
+				args[i] = types.NewIntDatum(int64(int8(binaryParams[i].Val[0])))
+			}
+			continue
+
+		case mysql.TypeShort, mysql.TypeYear:
+			valU16 := binary.LittleEndian.Uint16(binaryParams[i].Val)
+			if isUnsigned {
+				args[i] = types.NewUintDatum(uint64(valU16))
+			} else {
+				args[i] = types.NewIntDatum(int64(int16(valU16)))
+			}
+			continue
+
+		case mysql.TypeInt24, mysql.TypeLong:
+			valU32 := binary.LittleEndian.Uint32(binaryParams[i].Val)
+			if isUnsigned {
+				args[i] = types.NewUintDatum(uint64(valU32))
+			} else {
+				args[i] = types.NewIntDatum(int64(int32(valU32)))
+			}
+			continue
+
+		case mysql.TypeLonglong:
+			valU64 := binary.LittleEndian.Uint64(binaryParams[i].Val)
+			if isUnsigned {
+				args[i] = types.NewUintDatum(valU64)
+			} else {
+				args[i] = types.NewIntDatum(int64(valU64))
+			}
+			continue
+
+		case mysql.TypeFloat:
+			args[i] = types.NewFloat32Datum(math.Float32frombits(binary.LittleEndian.Uint32(binaryParams[i].Val)))
+			continue
+
+		case mysql.TypeDouble:
+			args[i] = types.NewFloat64Datum(math.Float64frombits(binary.LittleEndian.Uint64(binaryParams[i].Val)))
+			continue
+
+		case mysql.TypeDate, mysql.TypeTimestamp, mysql.TypeDatetime:
+			switch len(binaryParams[i].Val) {
+			case 0:
+				tmp = types.ZeroDatetimeStr
+			case 4:
+				_, tmp = binaryDate(0, binaryParams[i].Val)
+			case 7:
+				_, tmp = binaryDateTime(0, binaryParams[i].Val)
+			case 11:
+				_, tmp = binaryTimestamp(0, binaryParams[i].Val)
+			case 13:
+				_, tmp = binaryTimestampWithTZ(0, binaryParams[i].Val)
+			default:
+				err = mysql.ErrMalformPacket
+				return
+			}
+			// TODO: generate the time datum directly
+			var parseTime func(types.Context, string) (types.Time, error)
+			switch tp {
+			case mysql.TypeDate:
+				parseTime = types.ParseDate
+			case mysql.TypeDatetime:
+				parseTime = types.ParseDatetime
+			case mysql.TypeTimestamp:
+				// To be compatible with MySQL, even the type of parameter is
+				// TypeTimestamp, the return type should also be `Datetime`.
+				parseTime = types.ParseDatetime
+			}
+			var time types.Time
+			time, err = parseTime(typectx, tmp.(string))
+			err = typectx.HandleTruncate(err)
+			if err != nil {
+				return
+			}
+			args[i] = types.NewDatum(time)
+			continue
+
+		case mysql.TypeDuration:
+			fsp := 0
+			switch len(binaryParams[i].Val) {
+			case 0:
+				tmp = "0"
+			case 8:
+				isNegative := binaryParams[i].Val[0]
+				if isNegative > 1 {
+					err = mysql.ErrMalformPacket
+					return
+				}
+				_, tmp = binaryDuration(1, binaryParams[i].Val, isNegative)
+			case 12:
+				isNegative := binaryParams[i].Val[0]
+				if isNegative > 1 {
+					err = mysql.ErrMalformPacket
+					return
+				}
+				_, tmp = binaryDurationWithMS(1, binaryParams[i].Val, isNegative)
+				fsp = types.MaxFsp
+			default:
+				err = mysql.ErrMalformPacket
+				return
+			}
+			// TODO: generate the duration datum directly
+			var dur types.Duration
+			dur, _, err = types.ParseDuration(typectx, tmp.(string), fsp)
+			err = typectx.HandleTruncate(err)
+			if err != nil {
+				return
+			}
+			args[i] = types.NewDatum(dur)
+			continue
+		case mysql.TypeNewDecimal:
+			if binaryParams[i].IsNull {
+				args[i] = types.NewDecimalDatum(nil)
+			} else {
+				var dec types.MyDecimal
+				err = typectx.HandleTruncate(dec.FromString(binaryParams[i].Val))
+				if err != nil {
+					return nil, err
+				}
+				args[i] = types.NewDecimalDatum(&dec)
+			}
+			continue
+		case mysql.TypeBlob, mysql.TypeTinyBlob, mysql.TypeMediumBlob, mysql.TypeLongBlob:
+			if binaryParams[i].IsNull {
+				args[i] = types.NewBytesDatum(nil)
+			} else {
+				args[i] = types.NewBytesDatum(binaryParams[i].Val)
+			}
+			continue
+		case mysql.TypeUnspecified, mysql.TypeVarchar, mysql.TypeVarString, mysql.TypeString,
+			mysql.TypeEnum, mysql.TypeSet, mysql.TypeGeometry, mysql.TypeBit:
+			if !binaryParams[i].IsNull {
+				tmp = string(hack.String(binaryParams[i].Val))
+			} else {
+				tmp = nil
+			}
+			args[i] = types.NewDatum(tmp)
+			continue
+		default:
+			err = param.ErrUnknownFieldType.GenWithStack("stmt unknown field type %d", tp)
+			return
+		}
+	}
+
+	for i := range params {
+		ft := new(types.FieldType)
+		types.InferParamTypeFromUnderlyingValue(args[i].GetValue(), ft)
+		params[i] = &Constant{Value: args[i], RetType: ft}
+	}
+	return
+}
+
+func binaryDate(pos int, paramValues []byte) (int, string) {
+	year := binary.LittleEndian.Uint16(paramValues[pos : pos+2])
+	pos += 2
+	month := paramValues[pos]
+	pos++
+	day := paramValues[pos]
+	pos++
+	return pos, fmt.Sprintf("%04d-%02d-%02d", year, month, day)
+}
+
+func binaryDateTime(pos int, paramValues []byte) (int, string) {
+	pos, date := binaryDate(pos, paramValues)
+	hour := paramValues[pos]
+	pos++
+	minute := paramValues[pos]
+	pos++
+	second := paramValues[pos]
+	pos++
+	return pos, fmt.Sprintf("%s %02d:%02d:%02d", date, hour, minute, second)
+}
+
+func binaryTimestamp(pos int, paramValues []byte) (int, string) {
+	pos, dateTime := binaryDateTime(pos, paramValues)
+	microSecond := binary.LittleEndian.Uint32(paramValues[pos : pos+4])
+	pos += 4
+	return pos, fmt.Sprintf("%s.%06d", dateTime, microSecond)
+}
+
+func binaryTimestampWithTZ(pos int, paramValues []byte) (int, string) {
+	pos, timestamp := binaryTimestamp(pos, paramValues)
+	tzShiftInMin := int16(binary.LittleEndian.Uint16(paramValues[pos : pos+2]))
+	tzShiftHour := tzShiftInMin / 60
+	tzShiftAbsMin := tzShiftInMin % 60
+	if tzShiftAbsMin < 0 {
+		tzShiftAbsMin = -tzShiftAbsMin
+	}
+	pos += 2
+	return pos, fmt.Sprintf("%s%+02d:%02d", timestamp, tzShiftHour, tzShiftAbsMin)
+}
+
+func binaryDuration(pos int, paramValues []byte, isNegative uint8) (int, string) {
+	sign := ""
+	if isNegative == 1 {
+		sign = "-"
+	}
+	days := binary.LittleEndian.Uint32(paramValues[pos : pos+4])
+	pos += 4
+	hours := paramValues[pos]
+	pos++
+	minutes := paramValues[pos]
+	pos++
+	seconds := paramValues[pos]
+	pos++
+	return pos, fmt.Sprintf("%s%d %02d:%02d:%02d", sign, days, hours, minutes, seconds)
+}
+
+func binaryDurationWithMS(pos int, paramValues []byte,
+	isNegative uint8) (int, string) {
+	pos, dur := binaryDuration(pos, paramValues, isNegative)
+	microSecond := binary.LittleEndian.Uint32(paramValues[pos : pos+4])
+	pos += 4
+	return pos, fmt.Sprintf("%s.%06d", dur, microSecond)
 }
