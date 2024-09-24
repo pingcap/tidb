42c6e241271601f1121cabc538e73a9b271127a1
tangenta <tangenta@126.com>
2024-09-24 22:42:56 +0800

    ddl: add lease for ingest distributed lock (#56184)
    close pingcap/tidb#55917

--- pkg/owner/manager.go
+++ pkg/owner/manager.go
@@ -499,3 +499,38 @@ func init() {
 		logutil.BgLogger().Warn("set manager session TTL failed", zap.Error(err))
 	}
 }
+
+// AcquireDistributedLock creates a mutex with ETCD client, and returns a mutex release function.
+func AcquireDistributedLock(
+	ctx context.Context,
+	cli *clientv3.Client,
+	key string,
+	ttlInSec int,
+) (release func(), err error) {
+	se, err := concurrency.NewSession(cli, concurrency.WithTTL(ttlInSec))
+	if err != nil {
+		return nil, err
+	}
+	mu := concurrency.NewMutex(se, key)
+	err = mu.Lock(ctx)
+	if err != nil {
+		err1 := se.Close()
+		if err1 != nil {
+			logutil.Logger(ctx).Warn("close session error", zap.Error(err1))
+		}
+		return nil, err
+	}
+	logutil.Logger(ctx).Info("acquire distributed flush lock success", zap.String("key", key))
+	return func() {
+		err = mu.Unlock(ctx)
+		if err != nil {
+			logutil.Logger(ctx).Warn("release distributed flush lock error", zap.Error(err), zap.String("key", key))
+		} else {
+			logutil.Logger(ctx).Info("release distributed flush lock success", zap.String("key", key))
+		}
+		err = se.Close()
+		if err != nil {
+			logutil.Logger(ctx).Warn("close session error", zap.Error(err))
+		}
+	}, nil
+}
