2c8f49c92c98cb5861e3acac8c8657d657f4ffa6
Weizhen Wang <wangweizhen@pingcap.com>
2024-09-26 00:06:50 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/syncload/stats_syncload.go
+++ pkg/statistics/handle/syncload/stats_syncload.go
@@ -548,7 +548,9 @@ func (s *statsSyncLoad) updateCachedItem(tblInfo table.Table, item model.TableIt
 	if !ok {
 		return false
 	}
-	if !tbl.ColAndIdxExistenceMap.Checked() {
+	if !tbl.ColAndIdxExistenceMap.Checked() ||
+		// Randomly check the existence map to avoid the map has missing information.
+		rand.Intn(10_000) == 1 {
 		tbl = tbl.Copy()
 		for _, col := range tbl.HistColl.GetColSlice() {
 			if tblInfo.Meta().FindColumnByID(col.ID) == nil {
