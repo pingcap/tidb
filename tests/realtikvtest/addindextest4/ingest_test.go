f591d0dff7598e9a1e24f5172bfbd53a822a64cf
lance6716 <lance6716@gmail.com>
2024-09-26 18:17:10 +0800

    lightning: fix forget to set lastRetryableErr when ingest RPC fail(#56345)
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- tests/realtikvtest/addindextest4/ingest_test.go
+++ tests/realtikvtest/addindextest4/ingest_test.go
@@ -580,3 +580,30 @@ func TestFirstLitSlowStart(t *testing.T) {
 	}()
 	wg.Wait()
 }
+
+func TestIssue55808(t *testing.T) {
+	store := realtikvtest.CreateMockStoreAndSetup(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("drop database if exists addindexlit;")
+	tk.MustExec("create database addindexlit;")
+	tk.MustExec("use addindexlit;")
+	tk.MustExec(`set global tidb_ddl_enable_fast_reorg=on;`)
+	tk.MustExec("set global tidb_enable_dist_task = off;")
+
+	backup := local.MaxWriteAndIngestRetryTimes
+	local.MaxWriteAndIngestRetryTimes = 1
+	t.Cleanup(func() {
+		local.MaxWriteAndIngestRetryTimes = backup
+	})
+
+	tk.MustExec("create table t (a int primary key, b int);")
+
+	for i := 0; i < 4; i++ {
+		tk.MustExec(fmt.Sprintf("insert into t values (%d, %d);", i*10000, i*10000))
+	}
+
+	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/br/pkg/lightning/backend/local/doIngestFailed", "return()"))
+	tk.MustExec("alter table t add index idx(a);")
+	tk.MustExec("admin check table t;")
+	require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/br/pkg/lightning/backend/local/doIngestFailed"))
+}
