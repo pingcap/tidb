badf610a86826dea231b9b3d1cf00268184d3e26
Ruihao Chen <ruihao.chen@pingcap.cn>
2024-09-26 17:32:46 +0800

    ddl: fix flaky TestRenameTableAutoIDs (#56327)
    ref pingcap/tidb#55446

--- pkg/ddl/schema_test.go
+++ pkg/ddl/schema_test.go
@@ -387,6 +387,7 @@ func TestRenameTableAutoIDs(t *testing.T) {
 	tk1 := testkit.NewTestKit(t, store)
 	tk2 := testkit.NewTestKit(t, store)
 	tk3 := testkit.NewTestKit(t, store)
+	tk4 := testkit.NewTestKit(t, store)
 	dbName := "RenameTableAutoIDs"
 	tk1.MustExec(`create schema ` + dbName)
 	tk1.MustExec(`create schema ` + dbName + "2")
@@ -400,8 +401,6 @@ func TestRenameTableAutoIDs(t *testing.T) {
 
 	waitFor := func(col int, tableName, s string) {
 		for {
-			tk4 := testkit.NewTestKit(t, store)
-			tk4.MustExec(`use test`)
 			sql := `admin show ddl jobs where db_name like '` + strings.ToLower(dbName) + `%' and table_name like '` + tableName + `%' and job_type = 'rename table'`
 			res := tk4.MustQuery(sql).Rows()
 			if len(res) == 1 && res[0][col] == s {
