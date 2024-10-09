19d2760deadc1304338bc2bece175a6e88025673
Lynn <zimu_xia@126.com>
2024-10-09 15:15:51 +0800

    ddl: support add nonymous vector index when creating table (#56499)
    close pingcap/tidb#56491

--- pkg/ddl/create_table.go
+++ pkg/ddl/create_table.go
@@ -976,7 +976,7 @@ func setEmptyConstraintName(namesMap map[string]bool, constr *ast.Constraint) {
 		var colName string
 		for _, keyPart := range constr.Keys {
 			if keyPart.Expr != nil {
-				colName = "expression_index"
+				colName = getAnonymousIndexPrefix(constr.Tp == ast.ConstraintVector)
 			}
 		}
 		if colName == "" {
