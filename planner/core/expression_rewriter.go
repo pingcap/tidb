4065837a7d7787b1bc8957c769c103a1a4643a81
dash12653 <35824102+dash12653@users.noreply.github.com>
2024-10-10 03:16:40 -0400

    This is an automated cherry-pick of #54334
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- planner/core/expression_rewriter.go
+++ planner/core/expression_rewriter.go
@@ -1017,6 +1017,11 @@ func (er *expressionRewriter) handleInSubquery(ctx context.Context, v *ast.Patte
 		copy(join.names, er.p.OutputNames())
 		copy(join.names[er.p.Schema().Len():], agg.OutputNames())
 		join.AttachOnConds(expression.SplitCNFItems(checkCondition))
+		// set FullSchema and FullNames for this join
+		if left, ok := planCtx.plan.(*logicalop.LogicalJoin); ok && left.FullSchema != nil {
+			join.FullSchema = left.FullSchema
+			join.FullNames = left.FullNames
+		}
 		// Set join hint for this join.
 		if er.b.TableHints() != nil {
 			join.setPreferredJoinTypeAndOrder(er.b.TableHints())
