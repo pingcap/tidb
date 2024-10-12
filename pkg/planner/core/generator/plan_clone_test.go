025c2a3c5b72557b1a4fc2df7c0ee7be8f551957
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2024-10-12 15:41:55 +0800

    planner: use go:generate go generate Plan Clone code (#56570)
    ref pingcap/tidb#54057

--- pkg/planner/core/generator/plan_clone_test.go
+++ pkg/planner/core/generator/plan_clone_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package main
 
 import (
 	"bytes"
@@ -21,29 +21,17 @@ import (
 )
 
 func TestPlanClone(t *testing.T) {
-	updatedCode, err := genPlanCloneForPlanCacheCode()
+	updatedCode, err := GenPlanCloneForPlanCacheCode()
 	if err != nil {
 		t.Errorf("Generate CloneForPlanCache code error: %v", err)
 		return
 	}
-	currentCode, err := os.ReadFile("./plan_clone_generated.go")
+	currentCode, err := os.ReadFile("../plan_clone_generated.go")
 	if err != nil {
 		t.Errorf("Read current plan_clone_generated.go code error: %v", err)
 		return
 	}
 	if !bytes.Equal(updatedCode, currentCode) {
-		t.Errorf("plan_clone_generated.go should be updated, please run TestUpdatePlanCloneCode manually to update it.")
-	}
-}
-
-func TestUpdatePlanCloneCode(t *testing.T) {
-	t.Skip("only run this test manually to update plan_clone_generated.go")
-	updatedCode, err := genPlanCloneForPlanCacheCode()
-	if err != nil {
-		t.Errorf("Generate CloneForPlanCache code error: %v", err)
-		return
-	}
-	if err := os.WriteFile("./plan_clone_generated.go", updatedCode, 0644); err != nil {
-		t.Errorf("Write plan_clone_generated.go error: %v", err)
+		t.Errorf("plan_clone_generated.go should be updated, please run 'make gogenerate' to update it.")
 	}
 }
