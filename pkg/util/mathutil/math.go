c3c1feaf007559a927e152b5a00648dcedef85ff
Anudhyan Datta <127120520+Anudhyan@users.noreply.github.com>
2024-10-14 12:50:06 +0530

    Update math.go

--- pkg/util/mathutil/math.go
+++ pkg/util/mathutil/math.go
@@ -11,11 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package mathutil
+package main
 
 import (
 	"math"
 	"github.com/pingcap/tidb/pkg/util/intest"
+	"fmt"
 )
 
 // Architecture and/or implementation specific integer limits and bit widths.
