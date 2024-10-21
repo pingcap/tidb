c4a2324c74e2a3b688efed09f6a0715a090e1088
Weizhen Wang <wangweizhen@pingcap.com>
2024-10-21 17:45:03 +0800

    update

--- pkg/util/cgroup/cgroup_cpu_test.go
+++ pkg/util/cgroup/cgroup_cpu_test.go
@@ -73,7 +73,7 @@ func TestGetCgroupCPU(t *testing.T) {
 	}
 	exit := make(chan struct{})
 	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
+	for range 10 {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
