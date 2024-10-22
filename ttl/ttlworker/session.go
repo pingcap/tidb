c6ec24e52bdce6f7bc18f0f812f5f478e121ceb1
YangKeao <yangkeao@chunibyo.icu>
2024-10-22 13:26:23 +0800

    This is an automated cherry-pick of #56604
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- ttl/ttlworker/session.go
+++ ttl/ttlworker/session.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/ngaut/pools"
 	"github.com/pingcap/errors"
+<<<<<<< HEAD:ttl/ttlworker/session.go
 	"github.com/pingcap/tidb/parser/terror"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/sessionctx/variable"
@@ -30,6 +31,20 @@ import (
 	"github.com/pingcap/tidb/util/chunk"
 	"github.com/pingcap/tidb/util/logutil"
 	"github.com/pingcap/tidb/util/sqlexec"
+=======
+	"github.com/pingcap/tidb/pkg/kv"
+	"github.com/pingcap/tidb/pkg/parser/terror"
+	"github.com/pingcap/tidb/pkg/sessionctx"
+	"github.com/pingcap/tidb/pkg/sessionctx/variable"
+	"github.com/pingcap/tidb/pkg/ttl/cache"
+	"github.com/pingcap/tidb/pkg/ttl/metrics"
+	"github.com/pingcap/tidb/pkg/ttl/session"
+	"github.com/pingcap/tidb/pkg/util"
+	"github.com/pingcap/tidb/pkg/util/chunk"
+	"github.com/pingcap/tidb/pkg/util/intest"
+	"github.com/pingcap/tidb/pkg/util/logutil"
+	"github.com/pingcap/tidb/pkg/util/sqlexec"
+>>>>>>> 670e970b224 (ttl: always enable all read engines for TTL sessions (#56604)):pkg/ttl/ttlworker/session.go
 	"go.uber.org/zap"
 )
 
@@ -54,12 +69,22 @@ var DetachStatsCollector = func(s sqlexec.SQLExecutor) sqlexec.SQLExecutor {
 	return s
 }
 
+<<<<<<< HEAD:ttl/ttlworker/session.go
 type sessionPool interface {
 	Get() (pools.Resource, error)
 	Put(pools.Resource)
 }
 
 func getSession(pool sessionPool) (session.Session, error) {
+=======
+var allIsolationReadEngines = map[kv.StoreType]struct{}{
+	kv.TiKV:    {},
+	kv.TiFlash: {},
+	kv.TiDB:    {},
+}
+
+func getSession(pool util.SessionPool) (session.Session, error) {
+>>>>>>> 670e970b224 (ttl: always enable all read engines for TTL sessions (#56604)):pkg/ttl/ttlworker/session.go
 	resource, err := pool.Get()
 	if err != nil {
 		return nil, err
@@ -85,6 +110,12 @@ func getSession(pool sessionPool) (session.Session, error) {
 	originalRetryLimit := sctx.GetSessionVars().RetryLimit
 	originalEnable1PC := sctx.GetSessionVars().Enable1PC
 	originalEnableAsyncCommit := sctx.GetSessionVars().EnableAsyncCommit
+<<<<<<< HEAD:ttl/ttlworker/session.go
+=======
+	originalTimeZone, restoreTimeZone := "", false
+	originalIsolationReadEngines, restoreIsolationReadEngines := "", false
+
+>>>>>>> 670e970b224 (ttl: always enable all read engines for TTL sessions (#56604)):pkg/ttl/ttlworker/session.go
 	se := session.NewSession(sctx, exec, func(se session.Session) {
 		_, err = se.ExecuteSQL(context.Background(), fmt.Sprintf("set tidb_retry_limit=%d", originalRetryLimit))
 		if err != nil {
@@ -98,6 +129,12 @@ func getSession(pool sessionPool) (session.Session, error) {
 
 		if !originalEnableAsyncCommit {
 			_, err = se.ExecuteSQL(context.Background(), "set tidb_enable_async_commit=OFF")
+			terror.Log(err)
+		}
+
+		if restoreIsolationReadEngines {
+			_, err = se.ExecuteSQL(context.Background(), "set tidb_isolation_read_engines=%?", originalIsolationReadEngines)
+			intest.AssertNoError(err)
 			terror.Log(err)
 		}
 
@@ -135,6 +172,55 @@ func getSession(pool sessionPool) (session.Session, error) {
 		return nil, err
 	}
 
+<<<<<<< HEAD:ttl/ttlworker/session.go
+=======
+	// set the time zone to UTC
+	rows, err := se.ExecuteSQL(context.Background(), "select @@time_zone")
+	if err != nil {
+		se.Close()
+		return nil, err
+	}
+
+	if len(rows) == 0 || rows[0].Len() == 0 {
+		se.Close()
+		return nil, errors.New("failed to get time_zone variable")
+	}
+	originalTimeZone = rows[0].GetString(0)
+
+	_, err = se.ExecuteSQL(context.Background(), "set @@time_zone='UTC'")
+	if err != nil {
+		se.Close()
+		return nil, err
+	}
+	restoreTimeZone = true
+
+	// allow the session in TTL to use all read engines.
+	_, hasTiDBEngine := se.GetSessionVars().IsolationReadEngines[kv.TiDB]
+	_, hasTiKVEngine := se.GetSessionVars().IsolationReadEngines[kv.TiKV]
+	_, hasTiFlashEngine := se.GetSessionVars().IsolationReadEngines[kv.TiFlash]
+	if !hasTiDBEngine || !hasTiKVEngine || !hasTiFlashEngine {
+		rows, err := se.ExecuteSQL(context.Background(), "select @@tidb_isolation_read_engines")
+		if err != nil {
+			se.Close()
+			return nil, err
+		}
+
+		if len(rows) == 0 || rows[0].Len() == 0 {
+			se.Close()
+			return nil, errors.New("failed to get tidb_isolation_read_engines variable")
+		}
+		originalIsolationReadEngines = rows[0].GetString(0)
+
+		_, err = se.ExecuteSQL(context.Background(), "set tidb_isolation_read_engines='tikv,tiflash,tidb'")
+		if err != nil {
+			se.Close()
+			return nil, err
+		}
+
+		restoreIsolationReadEngines = true
+	}
+
+>>>>>>> 670e970b224 (ttl: always enable all read engines for TTL sessions (#56604)):pkg/ttl/ttlworker/session.go
 	return se, nil
 }
 
