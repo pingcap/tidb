fec2beab7bb4bf0858bad40c35c43fe410ec9560
Weizhen Wang <wangweizhen@pingcap.com>
2024-10-28 13:21:22 +0800

    update

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -179,7 +179,7 @@ func PrintableASCII(b byte) bool {
 func FmtNonASCIIPrintableCharToHex(str string) string {
 	var b bytes.Buffer
 	b.Grow(len(str) * 2)
-	for i := 0; i < len(str); i++ {
+	for i := range len(str) {
 		if PrintableASCII(str[i]) {
 			b.WriteByte(str[i])
 			continue
