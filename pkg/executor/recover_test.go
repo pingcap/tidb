1f4735f4c60c8f19363ded7d042eb4f1b7e800ca
crazycs520 <crazycs520@gmail.com>
2024-10-29 19:44:50 +0800

    refine test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/executor/recover_test.go
+++ pkg/executor/recover_test.go
@@ -136,22 +136,17 @@ func TestRecoverTable(t *testing.T) {
 	tk.MustExec("drop table if exists t_recover2")
 	tk.MustExec("create table t_recover2 (a int);")
 	tk.MustExec("insert into t_recover2 values (1),(2),(3)")
-	tk2 := testkit.NewTestKit(t, store)
-	tk2.MustExec("use test_recover")
 	jobID := int64(0)
 	testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/ddl/onJobRunBefore", func(job *model.Job) {
 		if job.Type == model.ActionDropTable && jobID == 0 {
-			tk2.MustExec(fmt.Sprintf("admin cancel ddl jobs %d", job.ID))
 			jobID = job.ID
 		}
 	})
-	defer testfailpoint.Disable(t, "github.com/pingcap/tidb/pkg/ddl/onJobRunBefore")
-	err = tk.ExecToErr("drop table t_recover2")
-	require.Error(t, err)
+	tk.MustExec("drop table t_recover2")
+	tk.MustExec("recover table by job " + strconv.Itoa(int(jobID)))
 	err = tk.ExecToErr("recover table by job " + strconv.Itoa(int(jobID)))
 	require.Error(t, err)
 	require.Equal(t, "[schema:1050]Table 't_recover2' already been recover to 't_recover2', can't be recover repeatedly", err.Error())
-	tk.MustQuery("select * from t_recover2 order by a").Check(testkit.Rows("1", "2", "3"))
 
 	gcEnable, err := gcutil.CheckGCEnable(tk.Session())
 	require.NoError(t, err)
