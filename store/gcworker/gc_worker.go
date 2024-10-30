723ca4805c498d7cca2ef64cffc88bc34c1c3996
y_static_y@sina.com <y_static_y@sina.com>
2024-10-30 12:24:00 +0800

    fix delete range logic
    Signed-off-by: y_static_y@sina.com <y_static_y@sina.com>

--- store/gcworker/gc_worker.go
+++ store/gcworker/gc_worker.go
@@ -707,9 +707,6 @@ func (w *GCWorker) deleteRanges(ctx context.Context, safePoint uint64, concurren
 	startTime := time.Now()
 	for _, r := range ranges {
 		startKey, endKey := r.Range()
-		logutil.Logger(ctx).Info("[test] test range",
-			zap.Stringer("startKey", startKey),
-			zap.Stringer("endKey", endKey))
 		err = w.doUnsafeDestroyRangeRequest(ctx, startKey, endKey, concurrency)
 		failpoint.Inject("ignoreDeleteRangeFailed", func() {
 			err = nil
@@ -731,7 +728,6 @@ func (w *GCWorker) deleteRanges(ctx context.Context, safePoint uint64, concurren
 				zap.Error(err))
 			continue
 		}
-
 		if err := w.doGCLabelRules(r); err != nil {
 			logutil.Logger(ctx).Error("[gc worker] gc label rules failed on range",
 				zap.String("uuid", w.uuid),
@@ -1883,8 +1879,6 @@ func (w *GCWorker) doGCPlacementRules(safePoint uint64, dr util.DelRangeTask, gc
 			RawArgs: args,
 		}
 	})
-	logutil.BgLogger().Info("[test] doGCPlacementRules",
-		zap.Any("historyJob", historyJob))
 	if historyJob == nil {
 		err = kv.RunInNewTxn(context.Background(), w.store, false, func(ctx context.Context, txn kv.Transaction) error {
 			var err1 error
