b911382920d4deeb7020aaf85bf510a3e7e35cc7
ekexium <eke@fastmail.com>
2024-10-31 17:37:55 +0800

    fix conflict
    Signed-off-by: ekexium <eke@fastmail.com>

--- executor/replace.go
+++ executor/replace.go
@@ -185,12 +185,7 @@ func (e *ReplaceExec) exec(ctx context.Context, newRows [][]types.Datum) error {
 			return err
 		}
 	}
-<<<<<<< HEAD:executor/replace.go
-	e.memTracker.Consume(int64(txn.Size() - txnSize))
 	return nil
-=======
-	return txn.MayFlush()
->>>>>>> 9960ae746a3 (executor: do not track membuffer's memory in executor (#53985)):pkg/executor/replace.go
 }
 
 // Next implements the Executor Next interface.
