460d545d483a1dbeda18bbfd458971aee311e790
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-10-31 17:56:13 +0800

    infoschema: fix `sub_part` in `information_schema.statistics` table (#55814) (#56209)
    close pingcap/tidb#55812

--- executor/infoschema_reader.go
+++ executor/infoschema_reader.go
@@ -588,6 +588,11 @@ func (e *memtableRetriever) setDataForStatisticsInTable(schema *model.DBInfo, ta
 				expression = tblCol.GeneratedExprString
 			}
 
+			var subPart any
+			if key.Length != types.UnspecifiedLength {
+				subPart = key.Length
+			}
+
 			record := types.MakeDatums(
 				infoschema.CatalogVal, // TABLE_CATALOG
 				schema.Name.O,         // TABLE_SCHEMA
@@ -599,7 +604,7 @@ func (e *memtableRetriever) setDataForStatisticsInTable(schema *model.DBInfo, ta
 				colName,               // COLUMN_NAME
 				"A",                   // COLLATION
 				0,                     // CARDINALITY
-				nil,                   // SUB_PART
+				subPart,               // SUB_PART
 				nil,                   // PACKED
 				nullable,              // NULLABLE
 				"BTREE",               // INDEX_TYPE
