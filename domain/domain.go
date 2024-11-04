0d1cd1f45e35d5f6e060430bbc2ff68df614f8f2
Weizhen Wang <wangweizhen@pingcap.com>
2024-11-04 11:56:30 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- domain/domain.go
+++ domain/domain.go
@@ -2367,12 +2367,8 @@ func (do *Domain) updateStatsWorker(ctx sessionctx.Context, owner owner.Manager)
 	loadFeedbackTicker := time.NewTicker(5 * lease)
 	loadLockedTablesTicker := time.NewTicker(5 * lease)
 	dumpColStatsUsageTicker := time.NewTicker(100 * lease)
-<<<<<<< HEAD:domain/domain.go
-	readMemTricker := time.NewTicker(memory.ReadMemInterval)
-=======
 	updateStatsHealthyTicker := time.NewTicker(20 * lease)
 	readMemTicker := time.NewTicker(memory.ReadMemInterval)
->>>>>>> 0d5e0e921f6 (domain: move UpdateStatsHealthyMetrics into updateStatsWorker (#55386)):pkg/domain/domain.go
 	statsHandle := do.StatsHandle()
 	defer func() {
 		dumpColStatsUsageTicker.Stop()
@@ -2380,12 +2376,8 @@ func (do *Domain) updateStatsWorker(ctx sessionctx.Context, owner owner.Manager)
 		dumpFeedbackTicker.Stop()
 		gcStatsTicker.Stop()
 		deltaUpdateTicker.Stop()
-<<<<<<< HEAD:domain/domain.go
-		readMemTricker.Stop()
-=======
 		readMemTicker.Stop()
 		updateStatsHealthyTicker.Stop()
->>>>>>> 0d5e0e921f6 (domain: move UpdateStatsHealthyMetrics into updateStatsWorker (#55386)):pkg/domain/domain.go
 		do.SetStatsUpdating(false)
 		logutil.BgLogger().Info("updateStatsWorker exited.")
 	}()
@@ -2434,12 +2426,7 @@ func (do *Domain) updateStatsWorker(ctx sessionctx.Context, owner owner.Manager)
 			if err != nil {
 				logutil.BgLogger().Debug("dump column stats usage failed", zap.Error(err))
 			}
-<<<<<<< HEAD:domain/domain.go
-
-		case <-readMemTricker.C:
-=======
 		case <-readMemTicker.C:
->>>>>>> 0d5e0e921f6 (domain: move UpdateStatsHealthyMetrics into updateStatsWorker (#55386)):pkg/domain/domain.go
 			memory.ForceReadMemStats()
 		case <-updateStatsHealthyTicker.C:
 			statsHandle.UpdateStatsHealthyMetrics()
