3dea90c4fc613b9be53f0836c55e94d6c963892c
fzzf678 <daery_7@163.com>
2024-11-05 20:15:52 +0800

    log

--- pkg/disttask/framework/scheduler/collector.go
+++ pkg/disttask/framework/scheduler/collector.go
@@ -103,6 +103,10 @@ func (c *collector) Collect(ch chan<- prometheus.Metric) {
 			}
 		}
 	}
+	if len(subtaskCnt) == 0 {
+		ch <- prometheus.MustNewConstMetric(c.subtasks, prometheus.GaugeValue, 0, "", "", "", "")
+		logutil.DDLLogger().Info("set subtask count to 0")
+	}
 	logutil.DDLLogger().Info("subTask count collect:", zap.String("subtaskCnt", fmt.Sprint(subtaskCnt)))
 }
 
