7e83048172ab7ef3e055bce3ddfa81075a1b4458
yibin <huyibin@pingcap.cn>
2024-11-06 11:41:31 +0800

    Little refact
    Signed-off-by: yibin <huyibin@pingcap.cn>

--- pkg/executor/internal/mpp/local_mpp_coordinator.go
+++ pkg/executor/internal/mpp/local_mpp_coordinator.go
@@ -177,11 +177,12 @@ func NewLocalMPPCoordinator(ctx context.Context, sctx sessionctx.Context, is inf
 
 	value := sctx.GetSessionVars().StmtCtx.GetPlan()
 	if value != nil {
-		p := value.(base.Plan)
-		pp := getActualPhysicalPlan(p)
-		if pp != nil {
-			if len(coordinatorAddr) > 0 && needReportExecutionSummary(pp, coord.originalPlan.ID(), false) {
-				coord.reportExecutionInfo = true
+		if p, ok := value.(base.Plan); ok {
+			pp := getActualPhysicalPlan(p)
+			if pp != nil {
+				if len(coordinatorAddr) > 0 && needReportExecutionSummary(pp, coord.originalPlan.ID(), false) {
+					coord.reportExecutionInfo = true
+				}
 			}
 		}
 	}
