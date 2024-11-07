6bbc95207518d3e5648cd80be34133c15d18c5bc
3pointer <luancheng@pingcap.com>
2024-11-07 13:23:22 +0800

    Update br/pkg/streamhelper/basic_lib_for_test.go
    Co-authored-by: 山岚 <36239017+YuJuncen@users.noreply.github.com>

--- br/pkg/streamhelper/basic_lib_for_test.go
+++ br/pkg/streamhelper/basic_lib_for_test.go
@@ -865,7 +865,7 @@ func (p *mockPDClient) GetAllStores(ctx context.Context, opts ...pd.GetStoreOpti
 	return []*metapb.Store{
 		{
 			Id:      1,
-			Address: fmt.Sprintf("127.0.0.%d", 1),
+			Address: "127.0.0.1"
 		},
 	}, nil
 }
