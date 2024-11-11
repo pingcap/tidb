c03cac8e63c473958e9769ff398fd9c2308e1571
3pointer <luancheng@pingcap.com>
2024-11-11 17:10:01 +0800

    fix build

--- br/pkg/streamhelper/advancer.go
+++ br/pkg/streamhelper/advancer.go
@@ -6,7 +6,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"math"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -22,6 +21,7 @@ import (
 	"github.com/pingcap/tidb/br/pkg/utils"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/metrics"
+	"github.com/pingcap/tidb/util/mathutil"
 	tikvstore "github.com/tikv/client-go/v2/kv"
 	"github.com/tikv/client-go/v2/oracle"
 	"github.com/tikv/client-go/v2/tikv"
@@ -685,7 +685,7 @@ func (c *CheckpointAdvancer) asyncResolveLocksForRanges(ctx context.Context, tar
 		failpoint.Inject("AsyncResolveLocks", func() {})
 		maxTs := uint64(0)
 		for _, t := range targets {
-			maxTs = math.Max(maxTs, t.Value)
+			maxTs = mathutil.Max(maxTs, t.Value)
 		}
 		handler := func(ctx context.Context, r tikvstore.KeyRange) (rangetask.TaskStat, error) {
 			// we will scan all locks and try to resolve them by check txn status.
