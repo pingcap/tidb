488b3fbc2757d83d0758518a378a1df8c492469c
Ruihao Chen <ruihao.chen@pingcap.cn>
2024-11-11 10:25:21 +0800

    Fix

--- br/pkg/lightning/backend/local/local.go
+++ br/pkg/lightning/backend/local/local.go
@@ -1085,7 +1085,7 @@ func (local *Backend) prepareAndSendJob(
 	failpoint.Inject("failToSplit", func(_ failpoint.Value) {
 		needSplit = true
 	})
-	logger := log.FromContext(ctx).With(zap.String("uuid", engine.ID())).Begin(zap.InfoLevel, "split and scatter ranges")
+	logger := log.FromContext(ctx).With(zap.Stringer("uuid", engine.UUID)).Begin(zap.InfoLevel, "split and scatter ranges")
 	backOffTime := 10 * time.Second
 	maxbackoffTime := 120 * time.Second
 	for i := 0; i < maxRetryTimes; i++ {
