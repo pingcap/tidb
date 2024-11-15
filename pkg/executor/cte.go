e29970c61dfa9663f3cb9291b62d0411fec38d5a
xufei <xufei@pingcap.com>
2024-11-15 13:54:35 +0800

    Update cte.go

--- pkg/executor/cte.go
+++ pkg/executor/cte.go
@@ -258,13 +258,8 @@ func (p *cteProducer) openProducerExecutor(ctx context.Context, cteExec *CTEExec
 	return nil
 }
 
-<<<<<<< HEAD
-func (p *cteProducer) closeProducer() (firstErr error) {
-	err := p.seedExec.Close()
-=======
 func (p *cteProducer) closeProducerExecutor() (firstErr error) {
-	err := exec.Close(p.seedExec)
->>>>>>> 738adb9934c (executor: fix random cte error under apply (#57294))
+	err := p.seedExec.Close()
 	firstErr = setFirstErr(firstErr, err, "close seedExec err")
 	if p.recursiveExec != nil {
 		err = p.recursiveExec.Close()
@@ -572,21 +567,6 @@ func (p *cteProducer) resetTracker() {
 	}
 }
 
-<<<<<<< HEAD
-func (p *cteProducer) reopenTbls() (err error) {
-	if p.isDistinct {
-		p.hashTbl = newConcurrentMapHashTable()
-	}
-	// Normally we need to setup tracker after calling Reopen(),
-	// But reopen resTbl means we need to call produce() again, it will setup tracker.
-	if err := p.resTbl.Reopen(); err != nil {
-		return err
-	}
-	return p.iterInTbl.Reopen()
-}
-
-=======
->>>>>>> 738adb9934c (executor: fix random cte error under apply (#57294))
 // Check if tbl meets the requirement of limit.
 func (p *cteProducer) limitDone(tbl cteutil.Storage) bool {
 	return p.hasLimit && uint64(tbl.NumRows()) >= p.limitEnd
