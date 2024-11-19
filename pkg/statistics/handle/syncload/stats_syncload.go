6659b616dae361ca30a53507cd799f32c3308b97
Weizhen Wang <wangweizhen@pingcap.com>
2024-11-19 13:53:49 +0800

    fix
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/syncload/stats_syncload.go
+++ pkg/statistics/handle/syncload/stats_syncload.go
@@ -307,11 +307,14 @@ func (s *statsSyncLoad) handleOneItemTask(task *statstypes.NeededItemTask) (err 
 			s.statsHandle.SPool().Destroy(se)
 		}
 	}()
+	var skipTypes map[string]struct{}
 	val, err := sctx.GetSessionVars().GlobalVarsAccessor.GetGlobalSysVar(variable.TiDBAnalyzeSkipColumnTypes)
 	if err != nil {
 		logutil.BgLogger().Warn("failed to get global variable", zap.Error(err))
+	} else {
+		skipTypes = variable.ParseAnalyzeSkipColumnTypes(val)
 	}
-	skipTypes := variable.ParseAnalyzeSkipColumnTypes(val)
+
 	item := task.Item.TableItemID
 	statsTbl, ok := s.statsHandle.Get(item.TableID)
 
@@ -348,10 +351,13 @@ func (s *statsSyncLoad) handleOneItemTask(task *statstypes.NeededItemTask) (err 
 			// so we have to get the column info from the domain.
 			wrapper.colInfo = tblInfo.GetColumnByID(item.ID)
 		}
-		_, skip := skipTypes[types.TypeToStr(wrapper.colInfo.FieldType.GetType(), wrapper.colInfo.FieldType.GetCharset())]
-		if skip {
-			return nil
+		if skipTypes != nil {
+			_, skip := skipTypes[types.TypeToStr(wrapper.colInfo.FieldType.GetType(), wrapper.colInfo.FieldType.GetCharset())]
+			if skip {
+				return nil
+			}
 		}
+
 		// If this column is not analyzed yet and we don't have it in memory.
 		// We create a fake one for the pseudo estimation.
 		// Otherwise, it will trigger the sync/async load again, even if the column has not been analyzed.
