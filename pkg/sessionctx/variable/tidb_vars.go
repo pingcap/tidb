faf322cd6965ed577c9b2dc2da737db9de3ae1ec
xufei <xufeixw@mail.ustc.edu.cn>
2024-11-19 09:06:09 +0800

    executor: disable hash join v2 in release-8.5 by default (#57443)
    ref pingcap/tidb#53127, close pingcap/tidb#57444

--- pkg/sessionctx/variable/tidb_vars.go
+++ pkg/sessionctx/variable/tidb_vars.go
@@ -1548,7 +1548,7 @@ const (
 	DefTiDBEnableCheckConstraint                      = false
 	DefTiDBSkipMissingPartitionStats                  = true
 	DefTiDBOptEnableHashJoin                          = true
-	DefTiDBHashJoinVersion                            = joinversion.HashJoinVersionOptimized
+	DefTiDBHashJoinVersion                            = joinversion.HashJoinVersionLegacy
 	DefTiDBOptObjective                               = OptObjectiveModerate
 	DefTiDBSchemaVersionCacheLimit                    = 16
 	DefTiDBIdleTransactionTimeout                     = 0
