376a78a28049b074a075dbba4c1735f972a239cd
山岚 <36239017+YuJuncen@users.noreply.github.com>
2024-11-20 15:50:44 +0800

    external_storage: implement locking (#56597)
    close pingcap/tidb#56523

--- br/pkg/storage/locking_test.go
+++ br/pkg/storage/locking_test.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/pingcap/failpoint"
 	backup "github.com/pingcap/kvproto/pkg/brpb"
 	"github.com/pingcap/tidb/br/pkg/storage"
 	"github.com/stretchr/testify/require"
@@ -39,10 +40,10 @@ func requireFileNotExists(t *testing.T, path string) {
 func TestTryLockRemote(t *testing.T) {
 	ctx := context.Background()
 	strg, pth := createMockStorage(t)
-	err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
+	lock, err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
 	require.NoError(t, err)
 	requireFileExists(t, filepath.Join(pth, "test.lock"))
-	err = storage.UnlockRemote(ctx, strg, "test.lock")
+	err = lock.Unlock(ctx)
 	require.NoError(t, err)
 	requireFileNotExists(t, filepath.Join(pth, "test.lock"))
 }
@@ -50,12 +51,65 @@ func TestTryLockRemote(t *testing.T) {
 func TestConflictLock(t *testing.T) {
 	ctx := context.Background()
 	strg, pth := createMockStorage(t)
-	err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
+	lock, err := storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
 	require.NoError(t, err)
-	err = storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
-	require.ErrorContains(t, err, "locked, meta = Locked")
+	_, err = storage.TryLockRemote(ctx, strg, "test.lock", "This file is mine!")
+	require.ErrorContains(t, err, "conflict file test.lock")
 	requireFileExists(t, filepath.Join(pth, "test.lock"))
-	err = storage.UnlockRemote(ctx, strg, "test.lock")
+	err = lock.Unlock(ctx)
 	require.NoError(t, err)
 	requireFileNotExists(t, filepath.Join(pth, "test.lock"))
+}
+
+func TestRWLock(t *testing.T) {
+	ctx := context.Background()
+	strg, path := createMockStorage(t)
+	lock, err := storage.TryLockRemoteRead(ctx, strg, "test.lock", "I wanna read it!")
+	require.NoError(t, err)
+	lock2, err := storage.TryLockRemoteRead(ctx, strg, "test.lock", "I wanna read it too!")
+	require.NoError(t, err)
+	_, err = storage.TryLockRemoteWrite(ctx, strg, "test.lock", "I wanna write it, you get out!")
+	require.Error(t, err)
+	require.NoError(t, lock.Unlock(ctx))
+	require.NoError(t, lock2.Unlock(ctx))
+	l, err := storage.TryLockRemoteWrite(ctx, strg, "test.lock", "Can I have a write lock?")
+	require.NoError(t, err)
+	requireFileExists(t, filepath.Join(path, "test.lock.WRIT"))
+	require.NoError(t, l.Unlock(ctx))
+	requireFileNotExists(t, filepath.Join(path, "test.lock.WRIT"))
+}
+
+func TestConcurrentLock(t *testing.T) {
+	ctx := context.Background()
+	strg, path := createMockStorage(t)
+
+	errChA := make(chan error, 1)
+	errChB := make(chan error, 1)
+
+	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/br/pkg/storage/exclusive-write-commit-to-1", "1*pause"))
+	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/br/pkg/storage/exclusive-write-commit-to-2", "1*pause"))
+
+	go func() {
+		_, err := storage.TryLockRemote(ctx, strg, "test.lock", "I wanna read it, but I hesitated before send my intention!")
+		errChA <- err
+	}()
+
+	go func() {
+		_, err := storage.TryLockRemote(ctx, strg, "test.lock", "I wanna read it too, but I hesitated before committing!")
+		errChB <- err
+	}()
+
+	failpoint.Disable("github.com/pingcap/tidb/br/pkg/storage/exclusive-write-commit-to-1")
+	failpoint.Disable("github.com/pingcap/tidb/br/pkg/storage/exclusive-write-commit-to-2")
+
+	// There is exactly one error.
+	errA := <-errChA
+	errB := <-errChB
+	if errA == nil {
+		require.Error(t, errB)
+	} else {
+		require.NoError(t, errB)
+	}
+
+	requireFileExists(t, filepath.Join(path, "test.lock"))
 }
