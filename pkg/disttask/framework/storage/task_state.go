d6b111f0d52ff69bc678b6c14300206776845742
D3Hunter <jujj603@gmail.com>
2024-11-21 12:20:22 +0800

    change

--- pkg/disttask/framework/storage/task_state.go
+++ pkg/disttask/framework/storage/task_state.go
@@ -199,6 +199,31 @@ func (mgr *TaskManager) ModifyTaskByID(ctx context.Context, taskID int64, param 
 	})
 }
 
+// ModifiedTask implements the scheduler.TaskManager interface.
+func (mgr *TaskManager) ModifiedTask(ctx context.Context, task *proto.Task) error {
+	prevState := task.ModifyParam.PrevState
+	return mgr.WithNewTxn(ctx, func(se sessionctx.Context) error {
+		_, err := sqlexec.ExecSQL(ctx, se.GetSQLExecutor(), `
+			update mysql.tidb_global_task
+			set state = %?,
+			    concurrency = %?,
+				meta = %?,
+				state_update_time = CURRENT_TIMESTAMP()
+			where id = %? and state = %?`,
+			prevState, task.Concurrency, task.Meta, task.ID, proto.TaskStateModifying,
+		)
+		if se.GetSessionVars().StmtCtx.AffectedRows() == 0 {
+			// might be handled by other owner nodes, skip.
+			return nil
+		}
+		_, err = sqlexec.ExecSQL(ctx, se.GetSQLExecutor(), `
+			update mysql.tidb_background_subtask
+			set concurrency = %?
+			where task_key = %?`, task.Concurrency, task.ID)
+		return err
+	})
+}
+
 // SucceedTask update task state from running to succeed.
 func (mgr *TaskManager) SucceedTask(ctx context.Context, taskID int64) error {
 	return mgr.WithNewSession(func(se sessionctx.Context) error {
