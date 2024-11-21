42218ca61b8cff0a694bba4098e34e8bffd43758
Hangjie Mo <mohangjie1995@gmail.com>
2024-11-21 16:44:46 +0800

    partition: set AddRecordOpt.isUpdate = true when update partition records

--- pkg/ddl/partition_test.go
+++ pkg/ddl/partition_test.go
@@ -250,3 +250,24 @@ func TestReorganizePartitionRollback(t *testing.T) {
 	// test then add index should success
 	tk.MustExec("alter table t1 add index idx_kc (k, c)")
 }
+
+func TestUpdateDuringAddColumn(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("create table t1 (c1 int, c2 int) partition by hash (c1) partitions 16")
+	tk.MustExec("insert t1 values (1, 1), (2, 1)")
+	tk2 := testkit.NewTestKit(t, store)
+	tk2.MustExec("use test")
+
+	testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/ddl/onJobUpdated", func(job *model.Job) {
+		if job.SchemaState == model.StateWriteOnly {
+			tk2.MustExec("update t1 set t1.c1 = 8 where t1.c2 = 1")
+			tk2.MustQuery("select * from t1").Sort().Check(testkit.Rows("8 1", "8 1"))
+		}
+	})
+
+	tk.MustExec("alter table t1 add column c3 bigint default 9")
+
+	tk.MustQuery("select * from t1").Sort().Check(testkit.Rows("8 1 9", "8 1 9"))
+}
