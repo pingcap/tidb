707ac54544aa1cb70dca556c468bca75194ba83a
D3Hunter <jujj603@gmail.com>
2024-11-22 18:56:00 +0800

    test

--- pkg/disttask/framework/integrationtests/modify_test.go
+++ pkg/disttask/framework/integrationtests/modify_test.go
@@ -39,14 +39,53 @@ func TestModifyTaskConcurrency(t *testing.T) {
 	subtaskCh := make(chan struct{})
 	testutil.RegisterTaskMeta(t, c.MockCtrl, schedulerExt, c.TestContext,
 		func(ctx context.Context, subtask *proto.Subtask) error {
-			<-subtaskCh
+			select {
+			case <-subtaskCh:
+			case <-ctx.Done():
+				return ctx.Err()
+			}
 			return nil
 		},
 	)
 
-	t.Run("modify task concurrency at step two", func(t *testing.T) {
+	t.Run("modify pending task concurrency", func(t *testing.T) {
 		var once sync.Once
-		modifiySyncCh := make(chan struct{})
+		modifySyncCh := make(chan struct{})
+		var theTask *proto.Task
+		testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/disttask/framework/scheduler/beforeGetSchedulableTasks", func() {
+			once.Do(func() {
+				task, err := handle.SubmitTask(c.Ctx, "k1", proto.TaskTypeExample, 3, "", nil)
+				require.NoError(t, err)
+				require.Equal(t, 3, task.Concurrency)
+				require.NoError(t, c.TaskMgr.ModifyTaskByID(c.Ctx, task.ID, &proto.ModifyParam{
+					PrevState: proto.TaskStatePending,
+					Modifications: []proto.Modification{
+						{Type: proto.ModifyConcurrency, To: 7},
+					},
+				}))
+				theTask = task
+				gotTask, err := c.TaskMgr.GetTaskBaseByID(c.Ctx, theTask.ID)
+				require.NoError(t, err)
+				require.Equal(t, proto.TaskStateModifying, gotTask.State)
+				require.Equal(t, 3, gotTask.Concurrency)
+				<-modifySyncCh
+			})
+		})
+		modifySyncCh <- struct{}{}
+		// finish subtasks
+		subtaskCh <- struct{}{}
+		subtaskCh <- struct{}{}
+		task2Base := testutil.WaitTaskDone(c.Ctx, t, theTask.Key)
+		require.Equal(t, proto.TaskStateSucceed, task2Base.State)
+		checkSubtaskConcurrency(t, c, theTask.ID, map[proto.Step]int{
+			proto.StepOne: 7,
+			proto.StepTwo: 7,
+		})
+	})
+
+	t.Run("modify running task concurrency at step two", func(t *testing.T) {
+		var once sync.Once
+		modifySyncCh := make(chan struct{})
 		testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/disttask/framework/scheduler/beforeRefreshTask", func(task *proto.Task) {
 			if task.State != proto.TaskStateRunning && task.Step != proto.StepTwo {
 				return
@@ -58,15 +97,16 @@ func TestModifyTaskConcurrency(t *testing.T) {
 						{Type: proto.ModifyConcurrency, To: 7},
 					},
 				}))
-				<-modifiySyncCh
+				<-modifySyncCh
 			})
 		})
-		task, err := handle.SubmitTask(c.Ctx, "k1", proto.TaskTypeExample, 3, "", nil)
+		task, err := handle.SubmitTask(c.Ctx, "k2", proto.TaskTypeExample, 3, "", nil)
 		require.NoError(t, err)
+		require.Equal(t, 3, task.Concurrency)
 		// finish StepOne
 		subtaskCh <- struct{}{}
 		// wait task move to 'modifying' state
-		modifiySyncCh <- struct{}{}
+		modifySyncCh <- struct{}{}
 		// wait task move back to 'running' state
 		require.Eventually(t, func() bool {
 			gotTask, err2 := c.TaskMgr.GetTaskByID(c.Ctx, task.ID)
@@ -75,8 +115,100 @@ func TestModifyTaskConcurrency(t *testing.T) {
 		}, 10*time.Second, 100*time.Millisecond)
 		// finish StepTwo
 		subtaskCh <- struct{}{}
+		task2Base := testutil.WaitTaskDone(c.Ctx, t, task.Key)
+		require.Equal(t, proto.TaskStateSucceed, task2Base.State)
 		checkSubtaskConcurrency(t, c, task.ID, map[proto.Step]int{
 			proto.StepOne: 3,
+			proto.StepTwo: 7,
+		})
+	})
+
+	t.Run("modify paused task concurrency", func(t *testing.T) {
+		var once sync.Once
+		syncCh := make(chan struct{})
+		var theTask *proto.Task
+		testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/disttask/framework/scheduler/beforeGetSchedulableTasks", func() {
+			once.Do(func() {
+				task, err := handle.SubmitTask(c.Ctx, "k3", proto.TaskTypeExample, 3, "", nil)
+				require.NoError(t, err)
+				require.Equal(t, 3, task.Concurrency)
+				found, err := c.TaskMgr.PauseTask(c.Ctx, task.Key)
+				require.NoError(t, err)
+				require.True(t, found)
+				theTask = task
+				<-syncCh
+			})
+		})
+		syncCh <- struct{}{}
+		taskBase := testutil.WaitTaskDoneOrPaused(c.Ctx, t, theTask.Key)
+		require.Equal(t, proto.TaskStatePaused, taskBase.State)
+		require.NoError(t, c.TaskMgr.ModifyTaskByID(c.Ctx, theTask.ID, &proto.ModifyParam{
+			PrevState: proto.TaskStatePaused,
+			Modifications: []proto.Modification{
+				{Type: proto.ModifyConcurrency, To: 7},
+			},
+		}))
+		taskBase = testutil.WaitTaskDoneOrPaused(c.Ctx, t, theTask.Key)
+		require.Equal(t, proto.TaskStatePaused, taskBase.State)
+		found, err := c.TaskMgr.ResumeTask(c.Ctx, theTask.Key)
+		require.NoError(t, err)
+		require.True(t, found)
+		// finish subtasks
+		subtaskCh <- struct{}{}
+		subtaskCh <- struct{}{}
+		task2Base := testutil.WaitTaskDone(c.Ctx, t, theTask.Key)
+		require.Equal(t, proto.TaskStateSucceed, task2Base.State)
+		checkSubtaskConcurrency(t, c, theTask.ID, map[proto.Step]int{
+			proto.StepOne: 7,
+			proto.StepTwo: 7,
+		})
+	})
+
+	t.Run("modify pending task concurrency, but other owner already done it", func(t *testing.T) {
+		var once sync.Once
+		modifySyncCh := make(chan struct{})
+		var theTask *proto.Task
+		testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/disttask/framework/scheduler/beforeGetSchedulableTasks", func() {
+			once.Do(func() {
+				task, err := handle.SubmitTask(c.Ctx, "k4", proto.TaskTypeExample, 3, "", nil)
+				require.NoError(t, err)
+				require.Equal(t, 3, task.Concurrency)
+				require.NoError(t, c.TaskMgr.ModifyTaskByID(c.Ctx, task.ID, &proto.ModifyParam{
+					PrevState: proto.TaskStatePending,
+					Modifications: []proto.Modification{
+						{Type: proto.ModifyConcurrency, To: 7},
+					},
+				}))
+				theTask = task
+				gotTask, err := c.TaskMgr.GetTaskBaseByID(c.Ctx, theTask.ID)
+				require.NoError(t, err)
+				require.Equal(t, proto.TaskStateModifying, gotTask.State)
+				require.Equal(t, 3, gotTask.Concurrency)
+			})
+		})
+		var onceForRefresh sync.Once
+		testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/disttask/framework/scheduler/afterRefreshTask",
+			func(task *proto.Task) {
+				onceForRefresh.Do(func() {
+					require.Equal(t, proto.TaskStateModifying, task.State)
+					taskClone := *task
+					taskClone.Concurrency = 7
+					require.NoError(t, c.TaskMgr.ModifiedTask(c.Ctx, &taskClone))
+					gotTask, err := c.TaskMgr.GetTaskBaseByID(c.Ctx, task.ID)
+					require.NoError(t, err)
+					require.Equal(t, proto.TaskStatePending, gotTask.State)
+					<-modifySyncCh
+				})
+			},
+		)
+		modifySyncCh <- struct{}{}
+		// finish subtasks
+		subtaskCh <- struct{}{}
+		subtaskCh <- struct{}{}
+		task2Base := testutil.WaitTaskDone(c.Ctx, t, theTask.Key)
+		require.Equal(t, proto.TaskStateSucceed, task2Base.State)
+		checkSubtaskConcurrency(t, c, theTask.ID, map[proto.Step]int{
+			proto.StepOne: 7,
 			proto.StepTwo: 7,
 		})
 	})
