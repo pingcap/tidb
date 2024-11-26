e3dc1ba9f88010fd4e6772f44429f6fcca1e704a
qw4990 <zhangyuanjia@pingcap.com>
2024-11-26 10:31:21 +0800

    fixup

--- pkg/planner/core/logical_plan_builder.go
+++ pkg/planner/core/logical_plan_builder.go
@@ -4878,8 +4878,6 @@ func (b *PlanBuilder) buildMemTable(_ context.Context, dbName pmodel.CIStr, tabl
 			p.Extractor = NewInfoSchemaTableConstraintsExtractor()
 		case infoschema.TableTiKVRegionStatus:
 			p.Extractor = &TiKVRegionStatusExtractor{tablesID: make([]int64, 0)}
-		case infoschema.TableTiDBPlanCache:
-			p.Extractor = nil // TODO
 		}
 	}
 	return p, nil
