4985d52aa0fe10bae363aeccb0ffa6b546053393
Weizhen Wang <wangweizhen@pingcap.com>
2024-11-27 13:37:34 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/handletest/initstats/load_stats_test.go
+++ pkg/statistics/handle/handletest/initstats/load_stats_test.go
@@ -20,8 +20,9 @@ import (
 	"testing"
 
 	"github.com/pingcap/tidb/pkg/config"
+	"github.com/pingcap/tidb/pkg/parser/model"
 	"github.com/pingcap/tidb/pkg/statistics/handle"
-	statstypes "github.com/pingcap/tidb/pkg/statistics/handle/types"
+	"github.com/pingcap/tidb/pkg/statistics/handle/types"
 	"github.com/pingcap/tidb/pkg/testkit"
 	"github.com/stretchr/testify/require"
 )
@@ -33,17 +34,34 @@ func TestConcurrentlyInitStatsWithMemoryLimit(t *testing.T) {
 		conf.Performance.LiteInitStats = false
 		conf.Performance.ConcurrentlyInitStats = true
 	})
-	handle.IsFullCacheFunc = func(cache statstypes.StatsCache, total uint64) bool {
+	handle.IsFullCacheFunc = func(cache types.StatsCache, total uint64) bool {
 		return true
 	}
+	testConcurrentlyInitStats(t)
+}
+
+func TestConcurrentlyInitStatsWithoutMemoryLimit(t *testing.T) {
+	restore := config.RestoreFunc()
+	defer restore()
+	config.UpdateGlobal(func(conf *config.Config) {
+		conf.Performance.LiteInitStats = false
+		conf.Performance.ConcurrentlyInitStats = true
+	})
+	handle.IsFullCacheFunc = func(cache types.StatsCache, total uint64) bool {
+		return false
+	}
+	testConcurrentlyInitStats(t)
+}
+
+func testConcurrentlyInitStats(t *testing.T) {
 	store, dom := testkit.CreateMockStoreAndDomain(t)
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("use test")
-	tk.MustExec("create table t1 (a int, b int, c int, primary key(a), key idx(b))")
+	tk.MustExec("create table t1 (a int, b int, c int, primary key(c))")
 	tk.MustExec("insert into t1 values (1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5),(6,7,8)")
 	tk.MustExec("analyze table t1")
 	for i := 2; i < 10; i++ {
-		tk.MustExec(fmt.Sprintf("create table t%v (a int, b int, c int, primary key(a), key idx(b))", i))
+		tk.MustExec(fmt.Sprintf("create table t%v (a int, b int, c int, primary key(c))", i))
 		tk.MustExec(fmt.Sprintf("insert into t%v select * from t1", i))
 		tk.MustExec(fmt.Sprintf("analyze table t%v", i))
 	}
@@ -52,8 +70,13 @@ func TestConcurrentlyInitStatsWithMemoryLimit(t *testing.T) {
 	h.Clear()
 	require.Equal(t, h.MemConsumed(), int64(0))
 	require.NoError(t, h.InitStats(context.Background(), is))
-	for _, val := range h.StatsCache.Values() {
-		for _, col := range val.HistColl.GetColSlice() {
+	for i := 1; i < 10; i++ {
+		tbl, err := is.TableByName(context.Background(), model.NewCIStr("test"), model.NewCIStr(fmt.Sprintf("t%v", i)))
+		require.NoError(t, err)
+		stats, ok := h.StatsCache.Get(tbl.Meta().ID)
+		require.True(t, ok)
+		for _, col := range stats.GetColSlice() {
+			require.True(t, col.IsAllEvicted())
 			require.False(t, col.IsFullLoad())
 		}
 	}
@@ -66,8 +89,13 @@ func TestConcurrentlyInitStatsWithMemoryLimit(t *testing.T) {
 	for i := 1; i < 10; i++ {
 		tk.MustQuery(fmt.Sprintf("explain select * from t%v where c = 1", i)).CheckNotContain("pseudo")
 	}
-	for _, val := range h.StatsCache.Values() {
-		for _, col := range val.HistColl.GetColSlice() {
+	for i := 1; i < 10; i++ {
+		tbl, err := is.TableByName(context.Background(), model.NewCIStr("test"), model.NewCIStr(fmt.Sprintf("t%v", i)))
+		require.NoError(t, err)
+		stats, ok := h.StatsCache.Get(tbl.Meta().ID)
+		require.True(t, ok)
+		for _, col := range stats.GetColSlice() {
+			require.True(t, col.IsFullLoad())
 			require.False(t, col.IsAllEvicted())
 		}
 	}
