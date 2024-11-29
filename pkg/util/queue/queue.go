3254bc529a1e14eadb4195aadaaeb2d92142441b
xzhangxian1008 <xzhangxian@foxmail.com>
2024-11-29 11:01:29 +0800

    add comment

--- pkg/util/queue/queue.go
+++ pkg/util/queue/queue.go
@@ -79,6 +79,7 @@ func (r *Queue[T]) Clear() {
 	r.size = 0
 }
 
+// ClearAndExpandIfNeed clear the queue and try to expand the elements
 func (r *Queue[T]) ClearAndExpandIfNeed(size int) {
 	r.Clear()
 
