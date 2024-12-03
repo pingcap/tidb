30195708a5845559844f01b9f6f1f7f414f37946
Wenqi Mou <wenqimou@gmail.com>
2024-12-02 21:30:20 -0700

    This is an automated cherry-pick of #57742
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -291,6 +291,16 @@ func (do *Domain) loadInfoSchema(startTS uint64) (infoschema.InfoSchema, bool, i
 		// We can fall back to full load, don't need to return the error.
 		logutil.BgLogger().Error("failed to load schema diff", zap.Error(err))
 	}
+
+	// add failpoint to simulate long-running schema loading scenario
+	failpoint.Inject("mock-load-schema-long-time", func(val failpoint.Value) {
+		if val.(bool) {
+			// not ideal to use sleep, but not sure if there is a better way
+			logutil.BgLogger().Error("sleep before doing a full load")
+			time.Sleep(15 * time.Second)
+		}
+	})
+
 	// full load.
 	schemas, err := do.fetchAllSchemasWithTables(m)
 	if err != nil {
@@ -1328,6 +1338,19 @@ func (do *Domain) Init(
 	return nil
 }
 
+<<<<<<< HEAD
+=======
+// GetSchemaLease return the schema lease.
+func (do *Domain) GetSchemaLease() time.Duration {
+	return do.schemaLease
+}
+
+// IsLeaseExpired returns whether lease has expired
+func (do *Domain) IsLeaseExpired() bool {
+	return do.SchemaValidator.IsLeaseExpired()
+}
+
+>>>>>>> 8fe061897e8 (br: fix insert gc failed due to slow schema reload (#57742))
 // InitInfo4Test init infosync for distributed execution test.
 func (do *Domain) InitInfo4Test() {
 	infosync.MockGlobalServerInfoManagerEntry.Add(do.ddl.GetID(), do.ServerID)
