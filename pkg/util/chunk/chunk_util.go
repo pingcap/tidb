8e7dcf459fc568e178f8fe1c298b14db87bac08e
xzhangxian1008 <xzhangxian@foxmail.com>
2024-12-03 10:40:36 +0800

    address comments

--- pkg/util/chunk/chunk_util.go
+++ pkg/util/chunk/chunk_util.go
@@ -110,9 +110,13 @@ func CopySelectedRows(dstCol *Column, srcCol *Column, selected []bool) {
 
 // CopySelectedRowsWithRowIDFunc copies the selected rows in srcCol to dstCol
 func CopySelectedRowsWithRowIDFunc(dstCol *Column, srcCol *Column, selected []bool, start int, end int, rowIDFunc func(int) int) {
+	if selected == nil {
+		return
+	}
+
 	if srcCol.isFixed() {
 		for i := start; i < end; i++ {
-			if selected != nil && !selected[i] {
+			if !selected[i] {
 				continue
 			}
 			rowID := rowIDFunc(i)
@@ -125,7 +129,7 @@ func CopySelectedRowsWithRowIDFunc(dstCol *Column, srcCol *Column, selected []bo
 		}
 	} else {
 		for i := start; i < end; i++ {
-			if selected != nil && !selected[i] {
+			if !selected[i] {
 				continue
 			}
 			rowID := rowIDFunc(i)
