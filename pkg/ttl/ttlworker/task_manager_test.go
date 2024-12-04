0392cdda7677eea870ff89c92bdc7e63a89c823e
YangKeao <yangkeao@chunibyo.icu>
2024-12-04 17:59:29 +0800

    ttl: fix the issue that one task losing heartbeat will block other tasks (#57919)
    close pingcap/tidb#57915

--- pkg/ttl/ttlworker/task_manager_test.go
+++ pkg/ttl/ttlworker/task_manager_test.go
@@ -112,8 +112,13 @@ func (m *taskManager) CheckInvalidTask(se session.Session) {
 }
 
 // UpdateHeartBeat is an exported version of updateHeartBeat
-func (m *taskManager) UpdateHeartBeat(ctx context.Context, se session.Session, now time.Time) error {
-	return m.updateHeartBeat(ctx, se, now)
+func (m *taskManager) UpdateHeartBeat(ctx context.Context, se session.Session, now time.Time) {
+	m.updateHeartBeat(ctx, se, now)
+}
+
+// UpdateHeartBeatForTask is an exported version of updateHeartBeatForTask
+func (m *taskManager) UpdateHeartBeatForTask(ctx context.Context, se session.Session, now time.Time, task *runningScanTask) error {
+	return m.updateHeartBeatForTask(ctx, se, now, task)
 }
 
 func TestResizeWorkers(t *testing.T) {
