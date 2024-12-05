d3ab1bd74613021a1d05bc146bfe0577459809e5
lance6716 <lance6716@gmail.com>
2024-12-05 21:35:05 +0800

    address comment
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- tests/realtikvtest/addindextest3/ingest_test.go
+++ tests/realtikvtest/addindextest3/ingest_test.go
@@ -451,12 +451,19 @@ func TestAddIndexMockFlushError(t *testing.T) {
 func TestAddIndexDiskQuotaTS(t *testing.T) {
 	store := realtikvtest.CreateMockStoreAndSetup(t)
 	tk := testkit.NewTestKit(t, store)
+
+	tk.MustExec("set @@global.tidb_enable_dist_task = 0;")
+	testAddIndexDiskQuotaTS(t, tk)
+	tk.MustExec("set @@global.tidb_enable_dist_task = 1;")
+	testAddIndexDiskQuotaTS(t, tk)
+}
+
+func testAddIndexDiskQuotaTS(t *testing.T, tk *testkit.TestKit) {
 	tk.MustExec("drop database if exists addindexlit;")
 	tk.MustExec("create database addindexlit;")
 	tk.MustExec("use addindexlit;")
 	tk.MustExec(`set global tidb_ddl_enable_fast_reorg=on;`)
 	tk.MustExec("set @@tidb_ddl_reorg_worker_cnt=1;")
-	tk.MustExec("set @@global.tidb_enable_dist_task = 0;")
 
 	tk.MustExec("create table t(id int primary key, b int, k int);")
 	tk.MustQuery("split table t by (30000);").Check(testkit.Rows("1 1"))
