ffce3b1564a88a16087ef5f27edacd0b462148f4
qw4990 <zhangyuanjia@pingcap.com>
2024-12-05 19:47:55 +0800

    handle panic when loading bindings when restaring

--- pkg/planner/core/optimizer.go
+++ pkg/planner/core/optimizer.go
@@ -21,7 +21,6 @@ import (
 	"math"
 	"slices"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/pingcap/errors"
@@ -284,11 +283,6 @@ func doOptimize(
 	logic LogicalPlan,
 ) (LogicalPlan, PhysicalPlan, float64, error) {
 	sessVars := sctx.GetSessionVars()
-
-	if strings.Contains(sessVars.StmtCtx.OriginalSQL, "EXPLAIN FORMAT='hint'") {
-		panic("parsing hints panic")
-	}
-
 	flag = adjustOptimizationFlags(flag, logic)
 	logic, err := logicalOptimize(ctx, flag, logic)
 	if err != nil {
