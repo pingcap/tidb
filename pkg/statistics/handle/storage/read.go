ce902ba4074f134cfabbe4bef79d6e7c4d028849
Weizhen Wang <wangweizhen@pingcap.com>
2024-12-06 19:42:18 +0800

    statistics: trigger

--- pkg/statistics/handle/storage/read.go
+++ pkg/statistics/handle/storage/read.go
@@ -516,15 +516,11 @@ func TableStatsFromStorage(sctx sessionctx.Context, snapshot uint64, tableInfo *
 	table.RealtimeCount = realtimeCount
 
 	rows, _, err := util.ExecRows(sctx, "select table_id, is_index, hist_id, distinct_count, version, null_count, tot_col_size, stats_ver, flag, correlation, last_analyze_pos from mysql.stats_histograms where table_id = %?", tableID)
-<<<<<<< HEAD
-	if err != nil || len(rows) == 0 {
-=======
 	if err != nil {
 		return nil, err
 	}
 	// Check deleted table.
 	if len(rows) == 0 {
->>>>>>> 1521bf723dd (statistics: right deal with error for reading stats from storage (#58048))
 		return nil, nil
 	}
 	for _, row := range rows {
