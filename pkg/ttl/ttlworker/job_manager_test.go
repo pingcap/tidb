79f31de9081c3a4d7eed9f2f2e1748a66d097cd1
Yang Keao <yangkeao@chunibyo.icu>
2024-12-10 01:37:22 +0800

    add integration test with fault for TTL
    Signed-off-by: Yang Keao <yangkeao@chunibyo.icu>

--- pkg/ttl/ttlworker/job_manager_test.go
+++ pkg/ttl/ttlworker/job_manager_test.go
@@ -210,6 +210,11 @@ func (m *JobManager) ReportMetrics(se session.Session) {
 	m.reportMetrics(se)
 }
 
+// ID returns the id of JobManager
+func (m *JobManager) ID() string {
+	return m.id
+}
+
 // CheckFinishedJob is an exported version of checkFinishedJob
 func (m *JobManager) CheckFinishedJob(se session.Session) {
 	m.checkFinishedJob(se)
@@ -694,4 +699,11 @@ func TestSplitCnt(t *testing.T) {
 			require.Equal(t, int(i), getScanSplitCnt(s))
 		}
 	}
+}
+
+// SetTimeFormat sets the time format used by the test.
+// Some tests require a greater precision than the default time format. We don't change it globally to avoid potential compatibility issues.
+// Therefore, the format for most tests are also not changed, to make sure the tests can represent the real-world scenarios.
+func SetTimeFormat(format string) {
+	timeFormat = format
 }
