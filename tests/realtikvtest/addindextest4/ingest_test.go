1f09db4a13099c4538427cba6c9d5b103c3fd4c7
lance6716 <lance6716@gmail.com>
2024-12-11 16:52:37 +0800

    revert change after test
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- tests/realtikvtest/addindextest4/ingest_test.go
+++ tests/realtikvtest/addindextest4/ingest_test.go
@@ -669,4 +669,7 @@ func TestIssue55808(t *testing.T) {
 	err := tk.ExecToErr("alter table t add index idx(a);")
 	require.ErrorContains(t, err, "injected error")
 	require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/lightning/backend/local/doIngestFailed"))
+
+	tk.MustExec("set global tidb_enable_dist_task = default;")
+	tk.MustExec("set global tidb_ddl_error_count_limit = default")
 }
