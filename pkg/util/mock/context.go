eb48ce0769aff4e1c84e7a3060207269093cf8e9
MyonKeminta <MyonKeminta@users.noreply.github.com>
2024-12-12 17:23:53 +0800

    Add comment
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- pkg/util/mock/context.go
+++ pkg/util/mock/context.go
@@ -426,6 +426,8 @@ func (*fakeTxn) SetDiskFullOpt(_ kvrpcpb.DiskFullOpt) {}
 func (*fakeTxn) SetOption(_ int, _ any) {}
 
 func (*fakeTxn) Get(ctx context.Context, k kv.Key) ([]byte, error) {
+	// Check your implementation if you meet this error. It's dangerous if some calculation relies on the data but the
+	// read result is faked.
 	logutil.Logger(ctx).Warn("mock.Context: No store is specified but trying to access data from a transaction.")
 	return nil, nil
 }
