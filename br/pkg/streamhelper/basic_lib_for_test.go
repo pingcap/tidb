a770177f92f4e448ec7b3906a06a6cd81926f3d5
3pointer <luancheng@pingcap.com>
2024-12-13 10:58:38 +0800

    fix lint

--- br/pkg/streamhelper/basic_lib_for_test.go
+++ br/pkg/streamhelper/basic_lib_for_test.go
@@ -769,7 +769,7 @@ func (t *testEnv) putTask() {
 
 func (t *testEnv) ScanLocksInOneRegion(bo *tikv.Backoffer, key []byte, maxVersion uint64, limit uint32) ([]*txnlock.Lock, *tikv.KeyLocation, error) {
 	t.mu.Lock()
- 	defer t.mu.Unlock()
+	defer t.mu.Unlock()
 	if t.maxTs != maxVersion {
 		return nil, nil, errors.Errorf("unexpect max version in scan lock, expected %d, actual %d", t.maxTs, maxVersion)
 	}
@@ -792,7 +792,7 @@ func (t *testEnv) ScanLocksInOneRegion(bo *tikv.Backoffer, key []byte, maxVersio
 
 func (t *testEnv) ResolveLocksInOneRegion(bo *tikv.Backoffer, locks []*txnlock.Lock, loc *tikv.KeyLocation) (*tikv.KeyLocation, error) {
 	t.mu.Lock()
- 	defer t.mu.Unlock()
+	defer t.mu.Unlock()
 	for _, r := range t.regions {
 		if loc != nil && loc.Region.GetID() == r.id {
 			// reset locks
