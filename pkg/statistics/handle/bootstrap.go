186a295baa530ef51b7fec75565a6854ce6c7641
Weizhen Wang <wangweizhen@pingcap.com>
2024-12-18 16:42:53 +0800

    statistics: skip non-exicted table when to init stats
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/bootstrap.go
+++ pkg/statistics/handle/bootstrap.go
@@ -185,7 +185,11 @@ func (h *Handle) initStatsHistograms4Chunk(is infoschema.InfoSchema, cache stats
 		}
 		id, ndv, nullCount, version, totColSize := row.GetInt64(2), row.GetInt64(3), row.GetInt64(5), row.GetUint64(4), row.GetInt64(7)
 		lastAnalyzePos := row.GetDatum(11, types.NewFieldType(mysql.TypeBlob))
-		tbl, _ := h.TableInfoByID(is, table.PhysicalID)
+		tbl, ok := h.TableInfoByID(is, table.PhysicalID)
+		if !ok {
+			logutil.BgLogger().Warn("cannot find this table when to init stats", zap.Int64("tableID", table.PhysicalID))
+			continue
+		}
 		if row.GetInt64(1) > 0 {
 			var idxInfo *model.IndexInfo
 			for _, idx := range tbl.Meta().Indices {
