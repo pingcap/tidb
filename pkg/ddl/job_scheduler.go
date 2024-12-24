4f8974a2116a828dd98ed5dce2460ac179953a2f
fzzf678 <daery_7@163.com>
2024-12-24 15:54:44 +0800

    Update job_scheduler.go

--- pkg/ddl/job_scheduler.go
+++ pkg/ddl/job_scheduler.go
@@ -19,7 +19,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"runtime"
 	"strconv"
 	"strings"
 	"sync/atomic"
@@ -177,7 +176,7 @@ func (s *jobScheduler) start() {
 		}
 	}
 	// reorg worker count at least 1 at most 10.
-	reorgCnt := min(max(runtime.GOMAXPROCS(0)/4, 1), reorgWorkerCnt)
+	reorgCnt := reorgWorkerCnt * 2
 	s.reorgWorkerPool = newDDLWorkerPool(pools.NewResourcePool(workerFactory(addIdxWorker), reorgCnt, reorgCnt, 0), jobTypeReorg)
 	s.generalDDLWorkerPool = newDDLWorkerPool(pools.NewResourcePool(workerFactory(generalWorker), generalWorkerCnt, generalWorkerCnt, 0), jobTypeGeneral)
 	s.wg.RunWithLog(s.scheduleLoop)
