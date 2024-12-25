2a72e7f012bcebca7dc273ed4cc8d0163c82a100
YangKeao <yangkeao@chunibyo.icu>
2024-12-25 20:25:15 +0800

    ttl: set the job history status to `cancelled` if it's removed in GC and it's still running (#58539)
    close pingcap/tidb#58510

--- pkg/ttl/ttlworker/job_manager_test.go
+++ pkg/ttl/ttlworker/job_manager_test.go
@@ -215,6 +215,11 @@ func (m *JobManager) ID() string {
 	return m.id
 }
 
+// CheckNotOwnJob is an exported version of checkNotOwnJob
+func (m *JobManager) CheckNotOwnJob() {
+	m.checkNotOwnJob()
+}
+
 // CheckFinishedJob is an exported version of checkFinishedJob
 func (m *JobManager) CheckFinishedJob(se session.Session) {
 	m.checkFinishedJob(se)
