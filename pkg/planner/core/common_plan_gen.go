d1ef036e8db9c62139e274368b3644f88c3a4dec
qw4990 <zhangyuanjia@pingcap.com>
2024-12-30 18:25:57 +0800

    fixup

--- pkg/planner/core/common_plan_gen.go
+++ pkg/planner/core/common_plan_gen.go
@@ -14,7 +14,7 @@ explain analyze format='unity_plan' select * from t;
 	}
 */
 func (e *Explain) unityPlan() (string, error) {
-	up := new(unityPlan)
+	up := new(UnityPlan)
 	up.PlanDigest = planDigest(e.TargetPlan)
 	rootStats, _, memTracker, _ := getRuntimeInfo(e.SCtx(), e.TargetPlan, e.RuntimeStatsColl)
 	basicStats, _ := rootStats.MergeStats()
@@ -24,10 +24,29 @@ func (e *Explain) unityPlan() (string, error) {
 	return string(data), err
 }
 
-type unityPlan struct {
-	PlanDigest string
-	TimeInMS   float64
-	MemInByte  int64
+func (e *Explain) unitySubPlan() {
+
+}
+
+type UnityPlanNode struct {
+	ID                  string                  `json:"id"`
+	EstRows             string                  `json:"estRows"`
+	ActRows             string                  `json:"actRows,omitempty"`
+	TaskType            string                  `json:"taskType"`
+	AccessObject        string                  `json:"accessObject,omitempty"`
+	ExecuteInfo         string                  `json:"executeInfo,omitempty"`
+	OperatorInfo        string                  `json:"operatorInfo,omitempty"`
+	EstCost             string                  `json:"estCost,omitempty"`
+	MemoryInfo          string                  `json:"memoryInfo,omitempty"`
+	TotalMemoryConsumed string                  `json:"totalMemoryConsumed,omitempty"`
+	SubOperators        []*ExplainInfoForEncode `json:"subOperators,omitempty"`
+}
+
+type UnityPlan struct {
+	PlanDigest string           `json:"plan_digest"`
+	TimeInMS   float64          `json:"time_in_ms"`
+	MemInByte  int64            `json:"mem_in_byte"`
+	SubPlans   []*UnityPlanNode `json:"sub_plans"`
 }
 
 func planDigest(p base.Plan) string {
