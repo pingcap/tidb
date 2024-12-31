cdb786f40f7a4e70eff3370401c602b0a1945b6a
crazycs520 <crazycs520@gmail.com>
2024-12-31 18:56:37 +0800

    init test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/store/copr/copr_test/coprocessor_test.go
+++ pkg/store/copr/copr_test/coprocessor_test.go
@@ -304,6 +304,9 @@ func TestQueryWithConcurrentSmallCop(t *testing.T) {
 	tk.MustExec("insert into t2 values (1,1), (18446744073709551615,2)")
 	tk.MustExec("set @@tidb_distsql_scan_concurrency=15")
 	tk.MustExec("set @@tidb_executor_concurrency=15")
+	tk.MustExec("create table t3 (id bigint key, b int, c int);")
+	tk.MustExec("insert into t3 select * from t1")
+	tk.MustQuery("split table t3 by (0), (1), (2), (3), (4), (5), (6), (7), (8), (9);")
 	require.NoError(t, failpoint.Enable("github.com/pingcap/tidb/pkg/store/mockstore/unistore/unistoreRPCSlowCop", `return(100)`))
 	// Test for https://github.com/pingcap/tidb/pull/57522#discussion_r1875515863
 	start := time.Now()
@@ -321,5 +324,11 @@ func TestQueryWithConcurrentSmallCop(t *testing.T) {
 	start = time.Now()
 	tk.MustQuery("select * from t2 where id >= 1 and id <= 18446744073709551615 order by id;")
 	require.Less(t, time.Since(start), time.Millisecond*150)
+
+	// Test
+	start = time.Now()
+	tk.MustQuery("update t3 set b=b+1 where id >= 0")
+	fmt.Printf("update cost %v------------\n\n\n", time.Since(start))
 	require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/store/mockstore/unistore/unistoreRPCSlowCop"))
+	//tk.MustQuery("select * from t3").Check(testkit.Rows(""))
 }
