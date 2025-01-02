f8f1070618a43fd7617b222f501c9ed589f7bfdc
RidRisR <79858083+RidRisR@users.noreply.github.com>
2025-01-02 10:30:57 +0100

    mod

--- br/pkg/restore/snap_client/systable_restore.go
+++ br/pkg/restore/snap_client/systable_restore.go
@@ -45,16 +45,16 @@ var statsTables = map[string]map[string]struct{}{
 }
 
 // tables in this map is restored when fullClusterRestore=true
-var sysPrivilegeTableMap = map[string]string{
-	"user":          "(user = '%s' and host = '%%')",       // since v1.0.0
-	"db":            "(user = '%s' and host = '%%')",       // since v1.0.0
-	"tables_priv":   "(user = '%s' and host = '%%')",       // since v1.0.0
-	"columns_priv":  "(user = '%s' and host = '%%')",       // since v1.0.0
-	"default_roles": "(user = '%s' and host = '%%')",       // since v3.0.0
-	"role_edges":    "(to_user = '%s' and to_host = '%%')", // since v3.0.0
-	"global_priv":   "(user = '%s' and host = '%%')",       // since v3.0.8
-	"global_grants": "(user = '%s' and host = '%%')",       // since v5.0.3
-	"bind_info":     "(user = '%s' and host = '%%')",       // since v7.6.0
+var sysPrivilegeTableMap = map[string]struct{}{
+	"user":          {}, // since v1.0.0
+	"db":            {}, // since v1.0.0
+	"tables_priv":   {}, // since v1.0.0
+	"columns_priv":  {}, // since v1.0.0
+	"default_roles": {}, // since v3.0.0
+	"role_edges":    {}, // since v3.0.0
+	"global_priv":   {}, // since v3.0.8
+	"global_grants": {}, // since v5.0.3
+	"bind_info":     {}, // since v7.6.0
 }
 
 var unRecoverableTable = map[string]map[string]struct{}{
