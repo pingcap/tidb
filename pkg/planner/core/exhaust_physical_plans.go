475a4c6d0e266480fdf93eaa0fb923617fde82b3
Rustin170506 <techregister@pm.me>
2025-01-02 12:28:53 +0800

    test: update broken tests
    Signed-off-by: Rustin170506 <techregister@pm.me>

--- pkg/planner/core/exhaust_physical_plans.go
+++ pkg/planner/core/exhaust_physical_plans.go
@@ -432,7 +432,7 @@ func getHashJoins(p *logicalop.LogicalJoin, prop *property.PhysicalProperty) (jo
 	forceRightToBuild := ((p.PreferJoinType & h.PreferRightAsHJBuild) > 0) || ((p.PreferJoinType & h.PreferLeftAsHJProbe) > 0)
 	if forceLeftToBuild && forceRightToBuild {
 		p.SCtx().GetSessionVars().StmtCtx.SetHintWarning("Conflicting HASH_JOIN_BUILD and HASH_JOIN_PROBE hints detected. " +
-			"Both sides cannot be specified to use the same table. Please review the hints.")
+			"Both sides cannot be specified to use the same table. Please review the hints")
 		forceLeftToBuild = false
 		forceRightToBuild = false
 	}
@@ -453,7 +453,7 @@ func getHashJoins(p *logicalop.LogicalJoin, prop *property.PhysicalProperty) (jo
 			if forceLeftToBuild || forceRightToBuild {
 				p.SCtx().GetSessionVars().StmtCtx.SetHintWarning(fmt.Sprintf(
 					"The HASH_JOIN_BUILD and HASH_JOIN_PROBE hints are not supported for %s with hash join version 1. "+
-						"Please remove these hints.",
+						"Please remove these hints",
 					p.JoinType))
 				forceLeftToBuild = false
 				forceRightToBuild = false
@@ -464,7 +464,7 @@ func getHashJoins(p *logicalop.LogicalJoin, prop *property.PhysicalProperty) (jo
 		if forceLeftToBuild || forceRightToBuild {
 			p.SCtx().GetSessionVars().StmtCtx.SetHintWarning(fmt.Sprintf(
 				"HASH_JOIN_BUILD and HASH_JOIN_PROBE hints are not supported for %s because the build side is fixed. "+
-					"The build side will always be the right table. Please remove these hints.",
+					"The build side will always be the right table. Please remove these hints",
 				p.JoinType))
 			forceLeftToBuild = false
 			forceRightToBuild = false
