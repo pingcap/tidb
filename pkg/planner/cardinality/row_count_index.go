8a4df9f77b06ab52ae36b72a60e443a734090d31
Weizhen Wang <wangweizhen@pingcap.com>
2025-01-02 17:00:31 +0800

    planner: fix wrong count when is out of range with datetime
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/cardinality/row_count_index.go
+++ pkg/planner/cardinality/row_count_index.go
@@ -16,6 +16,7 @@ package cardinality
 
 import (
 	"bytes"
+	"fmt"
 	"math"
 	"slices"
 	"strings"
@@ -214,9 +215,19 @@ func isSingleColIdxNullRange(idx *statistics.Index, ran *ranger.Range) bool {
 	return false
 }
 
+func Shortening(lb []byte, rb []byte, kind byte) {
+	if kind != types.KindMysqlTime {
+		return
+	}
+
+}
+
 // It uses the modifyCount to validate, and realtimeRowCount to adjust the influence of modifications on the table.
 func getIndexRowCountForStatsV2(sctx planctx.PlanContext, idx *statistics.Index, coll *statistics.HistColl, indexRanges []*ranger.Range, realtimeRowCount, modifyCount int64) (float64, error) {
 	sc := sctx.GetSessionVars().StmtCtx
+	if !sctx.GetSessionVars().InRestrictedSQL {
+		fmt.Println("here")
+	}
 	debugTrace := sc.EnableOptimizerDebugTrace
 	if debugTrace {
 		debugtrace.EnterContextCommon(sctx)
