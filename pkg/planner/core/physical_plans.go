b015dd53e67b1e406110e9c77f26e1ff4e6baca0
dash12653 <35824102+dash12653@users.noreply.github.com>
2025-01-05 14:43:47 -0500

    modify some code

--- pkg/planner/core/physical_plans.go
+++ pkg/planner/core/physical_plans.go
@@ -1063,19 +1063,21 @@ func ExpandVirtualColumn(columns []*model.ColumnInfo, schema *expression.Schema,
 
 	oldNumColumns := len(schema.Columns)
 	numExtraColumns := 0
-	if oldNumColumns > 1 {
-		for i := oldNumColumns - 1; i >= 0; i-- {
-			cid := schema.Columns[i].ID
-			// Move extra columns to the end.
-			// ExtraRowChecksumID is ignored here since it's treated as an ordinary column.
-			// https://github.com/pingcap/tidb/blob/3c407312a986327bc4876920e70fdd6841b8365f/pkg/util/rowcodec/decoder.go#L206-L222
-			if cid != model.ExtraHandleID && cid != model.ExtraPhysTblID {
-				break
-			}
-			numExtraColumns++
+	ordinaryColumnExists := false
+	for i := oldNumColumns - 1; i >= 0; i-- {
+		cid := schema.Columns[i].ID
+		// Move extra columns to the end.
+		// ExtraRowChecksumID is ignored here since it's treated as an ordinary column.
+		// https://github.com/pingcap/tidb/blob/3c407312a986327bc4876920e70fdd6841b8365f/pkg/util/rowcodec/decoder.go#L206-L222
+		if cid != model.ExtraHandleID && cid != model.ExtraPhysTblID {
+			ordinaryColumnExists = true
+			break
 		}
+		numExtraColumns++
 	}
-
+	if !ordinaryColumnExists {
+		numExtraColumns = 0
+	}
 	extraColumns := make([]*expression.Column, numExtraColumns)
 	copy(extraColumns, schema.Columns[oldNumColumns-numExtraColumns:])
 	schema.Columns = schema.Columns[:oldNumColumns-numExtraColumns]
