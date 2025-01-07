2b2f62972c601709b5785bd154c48b47706f6f50
Weizhen Wang <wangweizhen@pingcap.com>
2025-01-07 16:44:19 +0800

    This is an automated cherry-pick of #58739
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/statistics/handle/usage/session_stats_collect.go
+++ pkg/statistics/handle/usage/session_stats_collect.go
@@ -82,6 +82,7 @@ func (s *statsUsageImpl) needDumpStatsDelta(is infoschema.InfoSchema, dumpAll bo
 // DumpStatsDeltaToKV sweeps the whole list and updates the global map, then we dumps every table that held in map to KV.
 // If the mode is `DumpDelta`, it will only dump that delta info that `Modify Count / Table Count` greater than a ratio.
 func (s *statsUsageImpl) DumpStatsDeltaToKV(dumpAll bool) error {
+	defer util.Recover(metrics.LabelStats, "DumpStatsDeltaToKV", nil, false)
 	start := time.Now()
 	defer func() {
 		dur := time.Since(start)
@@ -214,9 +215,13 @@ func (s *statsUsageImpl) dumpTableStatCountToKV(is infoschema.InfoSchema, physic
 
 // DumpColStatsUsageToKV sweeps the whole list, updates the column stats usage map and dumps it to KV.
 func (s *statsUsageImpl) DumpColStatsUsageToKV() error {
+<<<<<<< HEAD
 	if !variable.EnableColumnTracking.Load() {
 		return nil
 	}
+=======
+	defer util.Recover(metrics.LabelStats, "DumpColStatsUsageToKV", nil, false)
+>>>>>>> 23ed0dfd6ed (statistics: add recover to protect background task (#58739))
 	s.SweepSessionStatsList()
 	colMap := s.SessionStatsUsage().GetUsageAndReset()
 	defer func() {
