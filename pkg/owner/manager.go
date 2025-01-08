2fff0626971327e00e9c986eec70ab61337079cb
fzzf678 <daery_7@163.com>
2025-01-09 00:12:07 +0800

    log the duration of getting distributed lock

--- pkg/owner/manager.go
+++ pkg/owner/manager.go
@@ -638,6 +638,7 @@ func AcquireDistributedLock(
 	}
 	mu := concurrency.NewMutex(se, key)
 	maxRetryCnt := 10
+	startTime := time.Now()
 	err = util2.RunWithRetry(maxRetryCnt, util2.RetryInterval, func() (bool, error) {
 		err = mu.Lock(ctx)
 		if err != nil {
@@ -655,7 +656,8 @@ func AcquireDistributedLock(
 		}
 		return nil, err
 	}
-	logutil.Logger(ctx).Info("acquire distributed lock success", zap.String("key", key))
+	logutil.Logger(ctx).Info("acquire distributed lock success", zap.String("key", key),
+		zap.Duration("cost time", time.Since(startTime)))
 	return func() {
 		err = mu.Unlock(ctx)
 		if err != nil {
