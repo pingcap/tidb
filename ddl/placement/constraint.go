fb4dc8b66ca8e30cc71df4f22cbbbd0e5602c48e
JaySon <tshent@qq.com>
2025-01-09 17:08:37 +0800

    This is an automated cherry-pick of #58637
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- ddl/placement/constraint.go
+++ ddl/placement/constraint.go
@@ -75,7 +75,12 @@ func NewConstraint(label string) (Constraint, error) {
 		return r, fmt.Errorf("%w: %s", ErrInvalidConstraintFormat, label)
 	}
 
+<<<<<<< HEAD:ddl/placement/constraint.go
 	if op == In && key == EngineLabelKey && strings.ToLower(val) == EngineLabelTiFlash {
+=======
+	// Does not allow adding rule of tiflash.
+	if op == pd.In && key == EngineLabelKey && strings.ToLower(val) == EngineLabelTiFlash {
+>>>>>>> 4240ce4acc9 (ddl: Remove explicit exclude for "engine" notIn "tiflash" (#58637)):pkg/ddl/placement/constraint.go
 		return r, fmt.Errorf("%w: %s", ErrUnsupportedConstraint, label)
 	}
 
