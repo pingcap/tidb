b59ff0bc19b6a0b52adfa1b0225e6558dd80f3a2
Rustin170506 <techregister@pm.me>
2025-01-10 15:49:08 +0800

    refactor: remove the projection as well

--- pkg/statistics/handle/storage/read.go
+++ pkg/statistics/handle/storage/read.go
@@ -529,7 +529,7 @@ func TableStatsFromStorage(sctx sessionctx.Context, snapshot uint64, tableInfo *
 	table.ModifyCount = modidyCount
 	table.RealtimeCount = realtimeCount
 
-	rows, _, err := util.ExecRows(sctx, "select table_id, is_index, hist_id, distinct_count, version, null_count, tot_col_size, stats_ver, correlation, last_analyze_pos from mysql.stats_histograms where table_id = %?", tableID)
+	rows, _, err := util.ExecRows(sctx, "select table_id, is_index, hist_id, distinct_count, version, null_count, tot_col_size, stats_ver, correlation from mysql.stats_histograms where table_id = %?", tableID)
 	if err != nil {
 		return nil, err
 	}
