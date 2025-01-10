aa672ac0bae8033e5ceafa49e14fb380fa9c00e2
Hangjie Mo <mohangjie1995@gmail.com>
2025-01-10 11:10:52 +0800

    update

--- planner/core/task.go
+++ planner/core/task.go
@@ -19,9 +19,9 @@ import (
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
-<<<<<<< HEAD:planner/core/task.go
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/expression/aggregation"
+	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/charset"
@@ -41,29 +41,6 @@ import (
 	"github.com/pingcap/tidb/util/plancodec"
 	"github.com/pingcap/tidb/util/size"
 	"github.com/pingcap/tipb/go-tipb"
-=======
-	"github.com/pingcap/tidb/pkg/expression"
-	"github.com/pingcap/tidb/pkg/expression/aggregation"
-	"github.com/pingcap/tidb/pkg/infoschema"
-	"github.com/pingcap/tidb/pkg/kv"
-	"github.com/pingcap/tidb/pkg/parser/ast"
-	"github.com/pingcap/tidb/pkg/parser/charset"
-	"github.com/pingcap/tidb/pkg/parser/mysql"
-	"github.com/pingcap/tidb/pkg/planner/cardinality"
-	"github.com/pingcap/tidb/pkg/planner/core/base"
-	"github.com/pingcap/tidb/pkg/planner/core/cost"
-	"github.com/pingcap/tidb/pkg/planner/core/operator/baseimpl"
-	"github.com/pingcap/tidb/pkg/planner/core/operator/logicalop"
-	"github.com/pingcap/tidb/pkg/planner/property"
-	"github.com/pingcap/tidb/pkg/planner/util"
-	"github.com/pingcap/tidb/pkg/planner/util/fixcontrol"
-	"github.com/pingcap/tidb/pkg/types"
-	"github.com/pingcap/tidb/pkg/util/chunk"
-	"github.com/pingcap/tidb/pkg/util/collate"
-	"github.com/pingcap/tidb/pkg/util/logutil"
-	"github.com/pingcap/tidb/pkg/util/paging"
-	"github.com/pingcap/tidb/pkg/util/plancodec"
->>>>>>> 5301602262c (planner: correct plan when scan tidb related cluster table with KeepOrder (#51922)):pkg/planner/core/task.go
 	"go.uber.org/zap"
 )
 
@@ -1079,16 +1056,12 @@ func (p *PhysicalTopN) canPushDownToTiFlash(mppTask *mppTask) bool {
 	return true
 }
 
-<<<<<<< HEAD:planner/core/task.go
-func (p *PhysicalTopN) attach2Task(tasks ...task) task {
-	t := tasks[0].copy()
-=======
 // For https://github.com/pingcap/tidb/issues/51723,
 // This function only supports `CLUSTER_SLOW_QUERY`,
 // it will change plan from
 // TopN -> TableReader -> TableFullScan[cop] to
 // TopN -> TableReader -> Limit[cop] -> TableFullScan[cop] + keepOrder
-func (p *PhysicalTopN) pushLimitDownToTiDBCop(copTsk *CopTask) (base.Task, bool) {
+func (p *PhysicalTopN) pushLimitDownToTiDBCop(copTsk *copTask) (task, bool) {
 	if copTsk.indexPlan != nil || copTsk.tablePlan == nil {
 		return nil, false
 	}
@@ -1101,7 +1074,7 @@ func (p *PhysicalTopN) pushLimitDownToTiDBCop(copTsk *CopTask) (base.Task, bool)
 		ok             bool
 	)
 
-	copTsk.tablePlan, err = copTsk.tablePlan.Clone(p.SCtx())
+	copTsk.tablePlan, err = copTsk.tablePlan.Clone()
 	if err != nil {
 		return nil, false
 	}
@@ -1124,57 +1097,51 @@ func (p *PhysicalTopN) pushLimitDownToTiDBCop(copTsk *CopTask) (base.Task, bool)
 	if !ok {
 		return nil, false
 	}
-	if len(colsProp.SortItems) != 1 || !colsProp.SortItems[0].Col.Equal(p.SCtx().GetExprCtx().GetEvalCtx(), tblScan.HandleCols.GetCol(0)) {
+	if len(colsProp.SortItems) != 1 || !colsProp.SortItems[0].Col.Equal(p.SCtx(), tblScan.HandleCols.GetCol(0)) {
 		return nil, false
 	}
-	if selOnTblScan != nil && tblScan.StatsInfo().RowCount > 0 {
-		selSelectivity = selOnTblScan.StatsInfo().RowCount / tblScan.StatsInfo().RowCount
+	if selOnTblScan != nil && tblScan.statsInfo().RowCount > 0 {
+		selSelectivity = selOnTblScan.statsInfo().RowCount / tblScan.statsInfo().RowCount
 	}
 	tblScan.Desc = colsProp.SortItems[0].Desc
 	tblScan.KeepOrder = true
 
-	childProfile := copTsk.Plan().StatsInfo()
+	childProfile := copTsk.plan().statsInfo()
 	newCount := p.Offset + p.Count
-	stats := util.DeriveLimitStats(childProfile, float64(newCount))
+	stats := deriveLimitStats(childProfile, float64(newCount))
 	pushedLimit := PhysicalLimit{
 		Count: newCount,
-	}.Init(p.SCtx(), stats, p.QueryBlockOffset())
+	}.Init(p.SCtx(), stats, p.SelectBlockOffset())
 	pushedLimit.SetSchema(copTsk.tablePlan.Schema())
-	copTsk = attachPlan2Task(pushedLimit, copTsk).(*CopTask)
+	copTsk = attachPlan2Task(pushedLimit, copTsk).(*copTask)
 	child := pushedLimit.Children()[0]
-	child.SetStats(child.StatsInfo().ScaleByExpectCnt(float64(newCount)))
+	child.SetStats(child.statsInfo().ScaleByExpectCnt(float64(newCount)))
 	if selSelectivity > 0 && selSelectivity < 1 {
-		scaledRowCount := child.StatsInfo().RowCount / selSelectivity
-		tblScan.SetStats(tblScan.StatsInfo().ScaleByExpectCnt(scaledRowCount))
+		scaledRowCount := child.statsInfo().RowCount / selSelectivity
+		tblScan.SetStats(tblScan.statsInfo().ScaleByExpectCnt(scaledRowCount))
 	}
-	rootTask := copTsk.ConvertToRootTask(p.SCtx())
+	rootTask := copTsk.convertToRootTask(p.SCtx())
 	return attachPlan2Task(p, rootTask), true
 }
 
-// Attach2Task implements the PhysicalPlan interface.
-func (p *PhysicalTopN) Attach2Task(tasks ...base.Task) base.Task {
-	t := tasks[0].Copy()
->>>>>>> 5301602262c (planner: correct plan when scan tidb related cluster table with KeepOrder (#51922)):pkg/planner/core/task.go
+func (p *PhysicalTopN) attach2Task(tasks ...task) task {
+	t := tasks[0].copy()
 	cols := make([]*expression.Column, 0, len(p.ByItems))
 	for _, item := range p.ByItems {
 		cols = append(cols, expression.ExtractColumns(item.Expr)...)
 	}
 	needPushDown := len(cols) > 0
-<<<<<<< HEAD:planner/core/task.go
-	if copTask, ok := t.(*copTask); ok && needPushDown && p.canPushDownToTiKV(copTask) && len(copTask.rootTaskConds) == 0 {
-		newTask, changed := p.pushPartialTopNDownToCop(copTask)
-		if changed {
-			return newTask
-		}
-=======
-	if copTask, ok := t.(*CopTask); ok && needPushDown && copTask.getStoreType() == kv.TiDB && len(copTask.rootTaskConds) == 0 {
+	if copTask, ok := t.(*copTask); ok && needPushDown && copTask.getStoreType() == kv.TiDB && len(copTask.rootTaskConds) == 0 {
 		newTask, changed := p.pushLimitDownToTiDBCop(copTask)
 		if changed {
 			return newTask
 		}
 	}
-	if copTask, ok := t.(*CopTask); ok && needPushDown && p.canPushDownToTiKV(copTask) && len(copTask.rootTaskConds) == 0 {
->>>>>>> 5301602262c (planner: correct plan when scan tidb related cluster table with KeepOrder (#51922)):pkg/planner/core/task.go
+	if copTask, ok := t.(*copTask); ok && needPushDown && p.canPushDownToTiKV(copTask) && len(copTask.rootTaskConds) == 0 {
+		newTask, changed := p.pushPartialTopNDownToCop(copTask)
+		if changed {
+			return newTask
+		}
 		// If all columns in topN are from index plan, we push it to index plan, otherwise we finish the index plan and
 		// push it to table plan.
 		var pushedDownTopN *PhysicalTopN
