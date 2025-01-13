3b3fc7dd1e6cf0c0d00c4670af400617b10896a6
qw4990 <zhangyuanjia@pingcap.com>
2025-01-13 16:49:18 +0800

    fixup

--- pkg/planner/core/common_unity_offline.go
+++ pkg/planner/core/common_unity_offline.go
@@ -26,13 +26,14 @@ func (e *Explain) UnityOffline() string {
 	tableNames := collectTableNames(e.SCtx().GetSessionVars().CurrentDB, stmt)
 	leadingHints := e.unityOfflineIterateLeadingHints(tableNames)
 
-	//indexHints := make([][]string, len(tableNames))
-	//for i, t := range tableNames {
-	//	indexHints[i] = e.unityOfflineIterateIndexHints(t)
-	//}
-	//
-	//allPossibleHintSets := e.unityOfflineIterateHints(leadingHints, indexHints)
 	allPossibleHintSets := leadingHints
+	if len(tableNames) <= 4 {
+		indexHints := make([][]string, len(tableNames))
+		for i, t := range tableNames {
+			indexHints[i] = e.unityOfflineIterateIndexHints(t)
+		}
+		allPossibleHintSets = e.unityOfflineIterateHints(leadingHints, indexHints)
+	}
 
 	planDigestMap := make(map[string]struct{})
 	sctx := e.SCtx()
