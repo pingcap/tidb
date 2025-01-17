738a5f3e6e30ebb08393799729d71f7012c3a240
Wenqi Mou <wenqimou@gmail.com>
2025-01-16 21:33:15 -0500

    fix format
    Signed-off-by: Wenqi Mou <wenqimou@gmail.com>

--- br/pkg/restore/log_client/client_test.go
+++ br/pkg/restore/log_client/client_test.go
@@ -2030,7 +2030,6 @@ func TestPutRawKvWithRetry(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			mockRawClient := &mockRawKVClient{
 				errThreshold: tt.errThreshold,
