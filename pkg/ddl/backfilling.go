372eb9a540b4ef2d891e851e6f959acbdebfc1b4
D3Hunter <jujj603@gmail.com>
2025-01-17 21:35:34 +0800

    ddl: fix context usage and wait adjust worker count routine exit before return (#59020)
    ref pingcap/tidb#57229, close pingcap/tidb#59016

--- pkg/ddl/backfilling.go
+++ pkg/ddl/backfilling.go
@@ -856,11 +856,9 @@ func executeAndClosePipeline(ctx *OperatorCtx, pipe *operator.AsyncPipeline, job
 	var wg util.WaitGroupWrapper
 	adjustCtx, cancel := context.WithCancel(ctx)
 	if job != nil {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
+		wg.RunWithLog(func() {
 			adjustWorkerCntAndMaxWriteSpeed(adjustCtx, pipe, job, bcCtx, avgRowSize)
-		}()
+		})
 	}
 
 	err = pipe.Close()
@@ -1013,6 +1011,7 @@ func (dc *ddlCtx) writePhysicalTableRecord(
 	})
 
 	// generate task goroutine
+	doneCh := make(chan struct{})
 	eg.Go(func() error {
 		// we will modify the startKey in this goroutine, so copy them to avoid race.
 		start, end := startKey, endKey
@@ -1044,17 +1043,21 @@ func (dc *ddlCtx) writePhysicalTableRecord(
 		}
 
 		exec.close(false)
+		close(doneCh)
 		return nil
 	})
 
 	// update the worker cnt goroutine
-	go func() {
+	eg.Go(func() error {
 		ticker := time.NewTicker(UpdateDDLJobReorgCfgInterval)
 		defer ticker.Stop()
+	outer:
 		for {
 			select {
-			case <-ctx.Done():
-				return
+			case <-egCtx.Done():
+				return egCtx.Err()
+			case <-doneCh:
+				break outer
 			case <-ticker.C:
 				currentWorkerCnt := exec.currentWorkerSize()
 				targetWorkerCnt := reorgInfo.ReorgMeta.GetConcurrencyOrDefault(int(variable.GetDDLReorgWorkerCounter()))
@@ -1070,7 +1073,8 @@ func (dc *ddlCtx) writePhysicalTableRecord(
 				}
 			}
 		}
-	}()
+		return nil
+	})
 
 	return eg.Wait()
 }
