85bd16e601e69fe5b71004cad3c29367fcae6d36
elsa0520 <elsa0520@163.com>
2025-01-22 17:39:49 +0800

    Step5: Save the metrics by batch

--- pkg/kv/option.go
+++ pkg/kv/option.go
@@ -197,6 +197,8 @@ const (
 	InternalTxnStats = "stats"
 	// InternalTxnBindInfo is the type of bind info txn.
 	InternalTxnBindInfo = InternalTxnOthers
+	// InternalTxnWorkloadLearning is the type of workload-based learning txn.
+	InternalTxnWorkloadLearning = "WorkloadLearning"
 	// InternalTxnSysVar is the type of sys var txn.
 	InternalTxnSysVar = InternalTxnOthers
 	// InternalTxnAdmin is the type of admin operations.
