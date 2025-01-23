830a64dd6cab514768b1a94d752d6180663bcb35
Weizhen Wang <wangweizhen@pingcap.com>
2025-01-23 20:55:56 +0800

    domain: move deltaUpdateTicker/gcstats into a new goroutine (#58926)
    ref pingcap/tidb#58920

--- pkg/domain/domain_test.go
+++ pkg/domain/domain_test.go
@@ -182,7 +182,7 @@ func TestStatWorkRecoverFromPanic(t *testing.T) {
 	metrics.PanicCounter.Reset()
 	// Since the stats lease is 0 now, so create a new ticker will panic.
 	// Test that they can recover from panic correctly.
-	dom.updateStatsWorker(mock.NewContext())
+	dom.gcStatsWorker()
 	dom.autoAnalyzeWorker()
 	counter := metrics.PanicCounter.WithLabelValues(metrics.LabelDomain)
 	pb := &dto.Metric{}
