ce948d1288dceecd7ff1fcd74aa71be469fd859b
D3Hunter <jujj603@gmail.com>
2025-01-24 14:39:00 +0800

    test

--- tests/realtikvtest/addindextest1/disttask_test.go
+++ tests/realtikvtest/addindextest1/disttask_test.go
@@ -16,7 +16,10 @@ package addindextest
 
 import (
 	"context"
+	goerrors "errors"
 	"fmt"
+	"io/fs"
+	"os"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -43,6 +46,24 @@ func init() {
 	config.UpdateGlobal(func(conf *config.Config) {
 		conf.Path = "127.0.0.1:2379"
 	})
+}
+
+func checkTmpDDLDir(t *testing.T) {
+	tmpDir := config.GetGlobalConfig().TempDir
+	fmt.Println(tmpDir)
+	require.NoError(t, fs.WalkDir(os.DirFS(tmpDir), ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !strings.Contains(path, "/tmp_ddl-") {
+			return nil
+		}
+		// we only checks whether there is a stale file, empty dir is allowed.
+		if !d.IsDir() {
+			return goerrors.New("stale file found")
+		}
+		return nil
+	}))
 }
 
 func TestAddIndexDistBasic(t *testing.T) {
@@ -118,6 +139,7 @@ func TestAddIndexDistBasic(t *testing.T) {
 	require.NoError(t, failpoint.Disable("github.com/pingcap/tidb/pkg/ddl/injectPanicForIndexIngest"))
 
 	tk.MustExec(`set global tidb_enable_dist_task=0;`)
+	checkTmpDDLDir(t)
 }
 
 func TestAddIndexDistCancelWithPartition(t *testing.T) {
