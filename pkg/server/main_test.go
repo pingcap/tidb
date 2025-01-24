94898453a66214660d475f38221d5e3a7dd376a8
Weizhen Wang <wangweizhen@pingcap.com>
2025-01-24 15:51:53 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/server/main_test.go
+++ pkg/server/main_test.go
@@ -74,7 +74,6 @@ func TestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("github.com/go-sql-driver/mysql.(*mysqlConn).startWatcher.func1"),
 		goleak.IgnoreTopFunction("github.com/pingcap/tidb/pkg/util/timeutil.Sleep"),
 		goleak.IgnoreTopFunction("github.com/pingcap/tidb/pkg/ddl/notifier.(*DDLNotifier).start"),
-			),
 	}
 
 	callback := func(i int) int {
