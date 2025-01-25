31f6ede157b96101d315dc9410f2d88be4c3d5b9
tpp <terry.purcell@pingcap.com>
2025-01-24 19:04:59 -0800

    testcase2

--- pkg/planner/core/find_best_task.go
+++ pkg/planner/core/find_best_task.go
@@ -768,7 +768,7 @@ func compareCandidates(sctx base.PlanContext, statsTbl *statistics.Table, tableI
 	accessResult, comparable1 := util.CompareCol2Len(lhs.accessCondsColMap, rhs.accessCondsColMap)
 	scanResult, comparable2 := compareIndexBack(lhs, rhs)
 	corrResult := 0
-	if lhsPseudo == rhsPseudo {
+	if lhsPseudo == rhsPseudo && !lhsFullScan && !rhsFullScan {
 		// corrResult returns the left vs right comparison as a boolean, but also the actual ratio - which will be used in future
 		corrResult, _ = compareCorrRatio(lhs, rhs)
 	}
