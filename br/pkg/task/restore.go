ff3b7a9908379f02a2a21f1b6fa4f23753c92061
Wenqi Mou <wenqimou@gmail.com>
2025-02-10 12:55:57 -0500

    more resolve
    Signed-off-by: Wenqi Mou <wenqimou@gmail.com>

--- br/pkg/task/restore.go
+++ br/pkg/task/restore.go
@@ -590,7 +590,7 @@ func runRestore(c context.Context, g glue.Glue, cmdName string, cfg *RestoreConf
 	}
 
 	reader := metautil.NewMetaReader(backupMeta, s, &cfg.CipherInfo)
-	if err = client.InitBackupMeta(c, backupMeta, u, reader); err != nil {
+	if err = client.LoadSchemaIfNeededAndInitClient(c, backupMeta, u, reader); err != nil {
 		return errors.Trace(err)
 	}
 
