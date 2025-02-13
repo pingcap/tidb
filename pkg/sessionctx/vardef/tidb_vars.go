a0199b5938f40807223607a4587949ab75b1b142
xufei <xufeixw@mail.ustc.edu.cn>
2025-02-13 13:43:28 +0800

    save work
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- pkg/sessionctx/vardef/tidb_vars.go
+++ pkg/sessionctx/vardef/tidb_vars.go
@@ -1554,7 +1554,7 @@ const (
 	DefTiDBEnableCheckConstraint                      = false
 	DefTiDBSkipMissingPartitionStats                  = true
 	DefTiDBOptEnableHashJoin                          = true
-	DefTiDBHashJoinVersion                            = joinversion.HashJoinVersionLegacy
+	DefTiDBHashJoinVersion                            = joinversion.DefaultHashJoinVersion
 	DefTiDBOptObjective                               = OptObjectiveModerate
 	DefTiDBSchemaVersionCacheLimit                    = 16
 	DefTiDBIdleTransactionTimeout                     = 0
