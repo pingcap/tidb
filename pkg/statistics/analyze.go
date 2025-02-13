da371fa14788dd917bc31161afa2e0c4c4be99b3
elsa0520 <elsa0520@163.com>
2025-02-13 14:32:53 +0800

    stats: Supplement some comments on analyze result struct

--- pkg/statistics/analyze.go
+++ pkg/statistics/analyze.go
@@ -61,6 +61,7 @@ func (h *AnalyzeTableID) Equals(t *AnalyzeTableID) bool {
 }
 
 // AnalyzeResult is used to represent analyze result.
+// AnalyzeResult represents the analyze result of one column/index.
 type AnalyzeResult struct {
 	Hist    []*Histogram
 	Cms     []*CMSketch
@@ -83,6 +84,7 @@ func (a *AnalyzeResult) DestroyAndPutToPool() {
 type AnalyzeResults struct {
 	Err      error
 	ExtStats *ExtendedStatsColl
+	// The analyze results of each column/index.
 	Job      *AnalyzeJob
 	Ars      []*AnalyzeResult
 	TableID  AnalyzeTableID
