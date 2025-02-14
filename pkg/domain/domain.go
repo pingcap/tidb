98c8df26f0a21babc2fffeb97fd25ea616899c21
Rustin170506 <techregister@pm.me>
2025-02-14 18:00:17 +0800

    style: update logging in asyncLoadHistogram to use ErrVerboseLogger
    Signed-off-by: Rustin170506 <techregister@pm.me>

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -2616,7 +2616,7 @@ func (do *Domain) asyncLoadHistogram() {
 		case <-cleanupTicker.C:
 			err = statsHandle.LoadNeededHistograms(do.InfoSchema())
 			if err != nil {
-				logutil.BgLogger().Warn("load histograms failed", zap.Error(err), zap.Stack("stack"))
+				logutil.ErrVerboseLogger().Warn("load histograms failed", zap.Error(err))
 			}
 		case <-do.exit:
 			return
