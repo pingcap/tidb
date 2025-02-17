b73d1110e9fba49e761898c4817173ae4a704e34
wshwsh12 <793703860@qq.com>
2025-02-17 15:12:49 +0800

    disable_str_to_date pushdown

--- pkg/expression/expr_to_pb_test.go
+++ pkg/expression/expr_to_pb_test.go
@@ -1845,26 +1845,26 @@ func TestExprPushDownToTiKV(t *testing.T) {
 			retType:      types.NewFieldType(mysql.TypeString),
 			args:         []Expression{decimalColumn, stringColumn},
 		},
-		{
-			functionName: ast.StrToDate,
-			retType:      types.NewFieldType(mysql.TypeDatetime),
-			args:         []Expression{stringColumn, stringColumn},
-		},
-		{
-			functionName: ast.StrToDate,
-			retType:      types.NewFieldType(mysql.TypeDuration),
-			args:         []Expression{stringColumn, NewStrConst("%h")},
-		},
-		{
-			functionName: ast.StrToDate,
-			retType:      types.NewFieldType(mysql.TypeDate),
-			args:         []Expression{stringColumn, NewStrConst("%y")},
-		},
-		{
-			functionName: ast.StrToDate,
-			retType:      types.NewFieldType(mysql.TypeDatetime),
-			args:         []Expression{stringColumn, NewStrConst("%h%y")},
-		},
+		//{
+		//	functionName: ast.StrToDate,
+		//	retType:      types.NewFieldType(mysql.TypeDatetime),
+		//	args:         []Expression{stringColumn, stringColumn},
+		//},
+		//{
+		//	functionName: ast.StrToDate,
+		//	retType:      types.NewFieldType(mysql.TypeDuration),
+		//	args:         []Expression{stringColumn, NewStrConst("%h")},
+		//},
+		//{
+		//	functionName: ast.StrToDate,
+		//	retType:      types.NewFieldType(mysql.TypeDate),
+		//	args:         []Expression{stringColumn, NewStrConst("%y")},
+		//},
+		//{
+		//	functionName: ast.StrToDate,
+		//	retType:      types.NewFieldType(mysql.TypeDatetime),
+		//	args:         []Expression{stringColumn, NewStrConst("%h%y")},
+		//},
 		{
 			functionName: ast.TimestampDiff,
 			retType:      types.NewFieldType(mysql.TypeLong),
