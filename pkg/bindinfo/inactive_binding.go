c8be51d8cd8dccf898e0888f7ed27b13adf45d0e
qw4990 <zhangyuanjia@pingcap.com>
2025-02-17 17:11:11 +0800

    fixup

--- pkg/bindinfo/inactive_binding.go
+++ pkg/bindinfo/inactive_binding.go
@@ -2,6 +2,10 @@ package bindinfo
 
 import (
 	"fmt"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/pkg/parser"
+	"github.com/pingcap/tidb/pkg/parser/ast"
+	utilparser "github.com/pingcap/tidb/pkg/util/parser"
 
 	"github.com/pingcap/tidb/pkg/sessionctx"
 	"github.com/pingcap/tidb/pkg/util/chunk"
@@ -22,20 +26,32 @@ func (h *globalBindingHandle) RecordInactiveBindings() (err error) {
 
 	for _, row := range rows {
 		digest := row.GetString(0)
-		sql := row.GetString(1)
+		query := row.GetString(1)
 		charset := row.GetString(2)
 		collation := row.GetString(3)
 		hint := row.GetString(4)
 		planDigest := row.GetString(5)
-		defaultDB := row.GetString(6)
-		fmt.Println(">>>>>>>>>>>>", digest, sql, charset, collation, hint)
+		schema := row.GetString(6)
 
-		originalSQL := sql
-		bindSQL := sql
-		sqlDigest := ""
+		parser4binding := parser.New()
+		originNode, err := parser4binding.ParseOneStmt(query, charset, collation)
+		if err != nil {
+			// TODO:
+		}
+		bindSQL := GenerateBindingSQL(originNode, hint, schema)
+		var hintNode ast.StmtNode
+		hintNode, err = parser4binding.ParseOneStmt(bindSQL, charset, collation)
+		if err != nil {
+			// TODO:
+		}
+		restoredSQL := utilparser.RestoreWithDefaultDB(originNode, schema, query)
+		bindSQL = utilparser.RestoreWithDefaultDB(hintNode, schema, hintNode.Text())
+		db := utilparser.GetDefaultDB(originNode, schema)
+		originalSQL, sqlDigest := parser.NormalizeDigestForBinding(restoredSQL)
+
 
 		stmtInsert := fmt.Sprintf("insert ignore into mysql.bind_info values ('%s', '%s', '%s', 'disabled', NOW(), NOW(), '%s', '%s', 'auto', '%s', '%s')",
-			originalSQL, bindSQL, defaultDB, charset, collation, sqlDigest, planDigest)
+			originalSQL, bindSQL, db, charset, collation, sqlDigest, planDigest)
 		err = h.callWithSCtx(true, func(sctx sessionctx.Context) error {
 			_, err = exec(sctx, stmtInsert)
 			return err
