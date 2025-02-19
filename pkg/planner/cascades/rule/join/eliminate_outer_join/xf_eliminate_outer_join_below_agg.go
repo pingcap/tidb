c1c311762318136753f4275fef50b936e8b70613
AilinKid <314806019@qq.com>
2025-02-19 17:53:16 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

---
+++ pkg/planner/cascades/rule/join/eliminate_outer_join/xf_eliminate_outer_join_below_agg.go
@@ -0,0 +1,117 @@
+// Copyright 2025 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package eoj
+
+import (
+	"github.com/pingcap/tidb/pkg/expression"
+	"github.com/pingcap/tidb/pkg/planner/cascades/memo"
+	"github.com/pingcap/tidb/pkg/planner/cascades/pattern"
+	"github.com/pingcap/tidb/pkg/planner/cascades/rule"
+	corebase "github.com/pingcap/tidb/pkg/planner/core/base"
+	"github.com/pingcap/tidb/pkg/planner/core/operator/logicalop"
+	ruleutil "github.com/pingcap/tidb/pkg/planner/core/rule/util"
+	"github.com/pingcap/tidb/pkg/util/intset"
+)
+
+var _ rule.Rule = &XFEliminateOuterJoinBelowAgg{}
+
+// XFEliminateOuterJoinBelowAgg eliminate an outer join below an Aggregation.
+type XFEliminateOuterJoinBelowAgg struct {
+	outerJoinEliminator
+	*rule.BaseRule
+}
+
+// NewXFEliminateOuterJoinBelowAgg creates a new XFEliminateOuterJoinBelowAgg rule.
+func NewXFEliminateOuterJoinBelowAgg() *XFEliminateOuterJoinBelowAgg {
+	pa1 := pattern.NewPattern(pattern.OperandAggregation, pattern.EngineTiDBOnly)
+	pa2 := pattern.NewPattern(pattern.OperandJoin, pattern.EngineTiDBOnly)
+	pa2.SetChildren(pattern.NewPattern(pattern.OperandAny, pattern.EngineTiDBOnly), pattern.NewPattern(pattern.OperandAny, pattern.EngineTiDBOnly))
+	pa1.SetChildren(pa2)
+	return &XFEliminateOuterJoinBelowAgg{
+		BaseRule: rule.NewBaseRule(rule.XFEliminateOuterJoinBelowAgg, pa1),
+	}
+}
+
+// ID implement the Rule interface.
+func (*XFEliminateOuterJoinBelowAgg) ID() uint {
+	return uint(rule.XFEliminateOuterJoinBelowAgg)
+}
+
+// PreCheck implements the Rule interface.
+func (*XFEliminateOuterJoinBelowAgg) PreCheck(projGE corebase.LogicalPlan) bool {
+	joinOp := projGE.Children()[0].GetWrappedLogicalPlan().(*logicalop.LogicalJoin)
+	return joinOp.JoinType == logicalop.LeftOuterJoin || joinOp.JoinType == logicalop.RightOuterJoin
+}
+
+// XForm implements the Rule interface.
+func (xf *XFEliminateOuterJoinBelowAgg) XForm(aggGE corebase.LogicalPlan) ([]corebase.LogicalPlan, bool, error) {
+	aggOp := aggGE.GetWrappedLogicalPlan().(*logicalop.LogicalAggregation)
+	joinGE := aggGE.Children()[0]
+	joinOp := joinGE.GetWrappedLogicalPlan().(*logicalop.LogicalJoin)
+	ok, innerChildIdx, outerGE, innerGE, outerUniqueIDs := xf.prepareForEliminateOuterJoin(joinGE)
+	if !ok {
+		return nil, false, nil
+	}
+	// only when proj only use the columns from outer table can eliminate outer join.
+	if !ruleutil.IsColsAllFromOuterTable(aggOp.GetUsedCols(), &outerUniqueIDs) {
+		return nil, false, nil
+	}
+	// check whether join inner side covers the unique key.
+	innerJoinKeys := joinOp.ExtractJoinKeys(innerChildIdx)
+	if xf.isInnerJoinKeysContainUniqueKey(innerGE, innerJoinKeys) {
+		clonedProjOp := projOp.LogicalProjectionShallowRef()
+		clonedProjOp.SetChildren(outerGE)
+		// since eliminate outer join is always good, eliminate the old one.
+		return []corebase.LogicalPlan{clonedProjOp}, true, nil
+	}
+	return nil, false, nil
+}
+
+func (*XFEliminateOuterJoinBelowProjection) prepareForEliminateOuterJoin(joinGE corebase.LogicalPlan) (ok bool, innerChildIdx int, outerGE, innerGE corebase.LogicalPlan, outerUniqueIDs intset.FastIntSet) {
+	joinOp := joinGE.GetWrappedLogicalPlan().(*logicalop.LogicalJoin)
+	switch joinOp.JoinType {
+	case logicalop.LeftOuterJoin:
+		innerChildIdx = 1
+	case logicalop.RightOuterJoin:
+		innerChildIdx = 0
+	default:
+		ok = false
+		return
+	}
+	outerGE = joinOp.Children()[1^innerChildIdx]
+	innerGE = joinOp.Children()[innerChildIdx]
+
+	outerUniqueIDs = intset.NewFastIntSet()
+	for _, outerCol := range outerGE.Schema().Columns {
+		outerUniqueIDs.Insert(int(outerCol.UniqueID))
+	}
+	ok = true
+	return
+}
+
+// check whether one of unique keys sets is contained by inner join keys.
+func (*XFEliminateOuterJoinBelowProjection) isInnerJoinKeysContainUniqueKey(innerGE corebase.LogicalPlan, joinKeys *expression.Schema) bool {
+	// builds UniqueKey info of innerGroup.
+	fds := innerGE.(*memo.GroupExpression).GetGroup().GetLogicalProperty().FD
+	// PK or UK(without nullability) can be used as strict key.
+	ids := intset.NewFastIntSet()
+	for _, key := range joinKeys.Columns {
+		ids.Insert(int(key.UniqueID))
+	}
+	if fds.StrictKeyCovered(ids) {
+		return true
+	}
+	return false
+}
