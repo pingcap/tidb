8b1c9cc9ad4e507b883f0b1f7457c065f8b10031
fzzf678 <daery_7@163.com>
2025-02-20 00:15:59 +0800

    Update main.go

--- tools/csv_writer/main.go
+++ tools/csv_writer/main.go
@@ -643,7 +643,7 @@ func checkCSVUniqueness(credentialPath, f string) {
 	// WaitGroup to ensure all goroutines complete
 	var wg sync.WaitGroup
 	// Limit the maximum number of concurrent goroutines
-	maxGoroutines := 1000
+	maxGoroutines := 200
 	sem := make(chan struct{}, maxGoroutines)
 	var duplicateFound int32
 
