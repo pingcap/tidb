241356e887c305fcbeecbd36d4c43f5433043d98
Jiaqiang Huang <96465211+River2000i@users.noreply.github.com>
2025-02-20 14:22:48 +0800

    add comment

--- pkg/lightning/mydump/router.go
+++ pkg/lightning/mydump/router.go
@@ -442,6 +442,8 @@ type RouteResult struct {
 	Type        SourceType
 }
 
+// ParseFormat will traverse all matching files based on the given path and pick arbitrary one file to detect the format.
+// If no files are matched, it will default to returning the CSV format.
 func ParseFormat(ctx context.Context, path string) (string, error) {
 	format := TypeCSV
 	u, err := storage.ParseRawURL(path)
