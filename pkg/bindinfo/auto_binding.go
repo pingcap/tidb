9041e3c6f89b92fcf00569d5dd6f107133ebe11d
qw4990 <zhangyuanjia@pingcap.com>
2025-02-24 15:10:56 +0800

    fixup

--- pkg/bindinfo/auto_binding.go
+++ pkg/bindinfo/auto_binding.go
@@ -12,54 +12,39 @@ import (
 	"go.uber.org/zap"
 )
 
-func (h *globalBindingHandle) AutoRecordBindings(since time.Time) (err error) {
-	// TODO: improve the scan performance
-	stmtQuery := fmt.Sprintf(`select digest, query_sample_text, charset, collation, plan_hint, plan_digest, schema_name
-				from information_schema.statements_summary
-				where stmt_type='Select' and exec_count > 0 and plan_hint != "" and summary_begin_time >= '%s'`,
-		since.Add(-time.Minute*30).Format("2006-01-02 15:04:05"))
-	var rows []chunk.Row
-	err = h.callWithSCtx(false, func(sctx sessionctx.Context) error {
-		rows, _, err = execRows(sctx, stmtQuery)
-		return err
-	})
+func (h *globalBindingHandle) AutoRecordBindings(beginTime time.Time) (err error) {
+	// TODO: implement h.getStmtStats to get data by using in-memory function call instead of SQL to improve performance.
+	stmts, err := h.getStmtStatsTemp(2, beginTime)
 	if err != nil {
 		return err
 	}
 
-	logutil.BgLogger().Error("RecordInactiveBindings", zap.Int("rows", len(rows)))
+	logutil.BgLogger().Error("RecordInactiveBindings", zap.Int("rows", len(stmts)))
 
-	for _, row := range rows {
-		query := row.GetString(1)
-		charset := row.GetString(2)
-		collation := row.GetString(3)
-		hint := row.GetString(4)
-		planDigest := row.GetString(5)
-		schema := row.GetString(6)
-
+	for _, stmt := range stmts {
 		parser4binding := parser.New()
-		originNode, err := parser4binding.ParseOneStmt(query, charset, collation)
+		originNode, err := parser4binding.ParseOneStmt(stmt.QuerySampleText, stmt.Charset, stmt.Collation)
 		if err != nil {
 			// TODO:
 		}
-		bindSQL := GenerateBindingSQL(originNode, hint, schema)
+		bindSQL := GenerateBindingSQL(originNode, stmt.PlanHint, stmt.SchemaName)
 		var hintNode ast.StmtNode
-		hintNode, err = parser4binding.ParseOneStmt(bindSQL, charset, collation)
+		hintNode, err = parser4binding.ParseOneStmt(bindSQL, stmt.Charset, stmt.Collation)
 		if err != nil {
 			// TODO:
 		}
-		restoredSQL := RestoreDBForBinding(originNode, schema)
-		bindSQL = RestoreDBForBinding(hintNode, schema)
+		restoredSQL := RestoreDBForBinding(originNode, stmt.SchemaName)
+		bindSQL = RestoreDBForBinding(hintNode, stmt.SchemaName)
 		originalSQL, sqlDigest := parser.NormalizeDigestForBinding(restoredSQL)
 
 		// TODO: calculate binding digest
-		bindingDigest := planDigest
+		bindingDigest := stmt.PlanDigest
 
 		// TODO: improve the write performance
 		stmtInsert := fmt.Sprintf(`insert ignore into mysql.bind_info
     (binding_digest, original_sql, bind_sql, default_db, status, create_time, update_time, charset, collation, source, sql_digest, plan_digest) values
     ('%s', '%s', '%s', '%s', 'disabled', NOW(), NOW(), '%s', '%s', 'auto', '%s', '%s')`,
-			bindingDigest, originalSQL, bindSQL, schema, charset, collation, sqlDigest, planDigest)
+			bindingDigest, originalSQL, bindSQL, stmt.SchemaName, stmt.Charset, stmt.Collation, sqlDigest, stmt.PlanDigest)
 		err = h.callWithSCtx(true, func(sctx sessionctx.Context) error {
 			_, err = exec(sctx, stmtInsert)
 			return err
@@ -68,7 +53,10 @@ func (h *globalBindingHandle) AutoRecordBindings(since time.Time) (err error) {
 	return nil
 }
 
-type BindingExecInfo struct {
+type AutoBindingInfo struct {
+	*Binding
+
+	// Info from StmtStats
 	Plan                 string
 	AvgLatency           float64
 	ExecTimes            int64
@@ -76,11 +64,6 @@ type BindingExecInfo struct {
 	AvgReturnedRows      float64
 	LatencyPerReturnRow  float64
 	ScanRowsPerReturnRow float64
-}
-
-type AutoBindingInfo struct {
-	*Binding
-	*BindingExecInfo
 }
 
 func (h *globalBindingHandle) AutoBindingsForSQL(stmtOrDigest string) ([]*AutoBindingInfo, error) {
@@ -117,4 +100,58 @@ func (h *globalBindingHandle) normalizeSQLForBinding(SQLOrDigest string) (normSt
 	}
 	normStmt, _ = NormalizeStmtForBinding(stmt, "test", false)
 	return normStmt, true
+}
+
+// TODO: expose statement_summary.go:stmtSummaryStats and use it directly? Is it thread-safe?
+type StmtStats struct {
+	Digest          string
+	QuerySampleText string
+	Charset         string
+	Collation       string
+	PlanHint        string
+	PlanDigest      string
+	SchemaName      string
+}
+
+func (h *globalBindingHandle) getStmtStats(execCountThreshold int, beginTime time.Time) ([]*StmtStats, error) {
+	/* select * from information_schema.tidb_statements_stats
+	where begin_time >= ? and exec_count > ? and
+		plan_hint != "" and plan_in_binding = 0 */
+	return nil, nil
+}
+
+func (h *globalBindingHandle) getStmtStatsTemp(execCountThreshold int, beginTime time.Time) (stmts []*StmtStats, err error) {
+	stmtQuery := fmt.Sprintf(`select digest, query_sample_text, charset, collation, plan_hint, plan_digest, schema_name
+				from information_schema.statements_summary
+				where stmt_type='Select' and exec_count > %v and plan_hint != "" and summary_begin_time >= '%s'`,
+		execCountThreshold,
+		beginTime.Add(-time.Minute*30).Format("2006-01-02 15:04:05"))
+	var rows []chunk.Row
+	err = h.callWithSCtx(false, func(sctx sessionctx.Context) error {
+		rows, _, err = execRows(sctx, stmtQuery)
+		return err
+	})
+	if err != nil {
+		return
+	}
+
+	logutil.BgLogger().Error("RecordInactiveBindings", zap.Int("rows", len(rows)))
+
+	stmts = make([]*StmtStats, 0, len(rows))
+	for _, row := range rows {
+		stmts = append(stmts, &StmtStats{
+			Digest:          row.GetString(0),
+			QuerySampleText: row.GetString(1),
+			Charset:         row.GetString(2),
+			Collation:       row.GetString(3),
+			PlanHint:        row.GetString(4),
+			PlanDigest:      row.GetString(5),
+			SchemaName:      row.GetString(6),
+		})
+	}
+	return
+}
+
+func (h *globalBindingHandle) getStmtStatsByDigest(digest string) ([]*StmtStats, error) {
+	return nil, nil
 }
