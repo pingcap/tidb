5feb68fabfa347b3c74a23e0b5991f3a386d3f10
Daniël van Eeden <git@myname.nl>
2025-02-26 10:45:02 +0100

    fixup

--- pkg/ddl/tests/serial/serial_test.go
+++ pkg/ddl/tests/serial/serial_test.go
@@ -219,7 +219,7 @@ func TestCreateTableWithLike(t *testing.T) {
 	tk.MustExec("drop database ctwl_db")
 	tk.MustExec("drop database ctwl_db1")
 
-	// Test information_schema.columns compatibility.
+	// Test information_schema.columns copiability.
 	// See https://github.com/pingcap/tidb/issues/42030.
 	tk.MustExec("use test")
 	tk.MustExec("create table cc like information_schema.columns;")
