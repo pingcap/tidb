a411a471f5a8f5bd9ae01002cb85f41f6fd69990
Jiaqiang Huang <96465211+River2000i@users.noreply.github.com>
2025-02-26 17:38:42 +0800

    fix test

--- tests/realtikvtest/importintotest/import_into_test.go
+++ tests/realtikvtest/importintotest/import_into_test.go
@@ -493,7 +493,7 @@ func (s *mockGCSSuite) TestLoadSQLDump() {
 	sql := fmt.Sprintf(`IMPORT INTO load_csv.t FROM 'gs://test-load-parquet/p?endpoint=%s' FORMAT 'SQL';`, gcsEndpoint)
 	s.tk.MustQuery(sql)
 	s.tk.MustQuery("SELECT * FROM load_csv.t;").Check(testkit.Rows("1 a", "2 b"))
-	s.tk.MustQuery("TRUNCATE TABLE load_csv.t")
+	s.tk.MustExec("TRUNCATE TABLE load_csv.t")
 	sql = fmt.Sprintf(`IMPORT INTO load_csv.t FROM 'gs://test-load-parquet/p?endpoint=%s';`, gcsEndpoint)
 	s.tk.MustQuery(sql)
 	s.tk.MustQuery("SELECT * FROM load_csv.t;").Check(testkit.Rows("1 a", "2 b"))
