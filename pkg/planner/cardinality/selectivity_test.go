39e3ecb6cf8a13a02134a544beeae9ee81db71d0
tpp <terry.purcell@pingcap.com>
2025-02-27 11:56:57 -0800

    testcase5

--- pkg/planner/cardinality/selectivity_test.go
+++ pkg/planner/cardinality/selectivity_test.go
@@ -511,12 +511,12 @@ func TestSelectivity(t *testing.T) {
 		{
 			exprs:                    "a >= 1 and c > 1 and a < 2",
 			selectivity:              0.00617283950,
-			selectivityAfterIncrease: 0.006378600823045267,
+			selectivityAfterIncrease: 0.014506172839506172,
 		},
 		{
 			exprs:                    "a >= 1 and c >= 1 and a < 2",
 			selectivity:              0.01234567901,
-			selectivityAfterIncrease: 0.012551440329218106,
+			selectivityAfterIncrease: 0.018518518518518517,
 		},
 		{
 			exprs:                    "d = 0 and e = 1",
@@ -531,7 +531,7 @@ func TestSelectivity(t *testing.T) {
 		{
 			exprs:                    "a > 1 and b < 2 and c > 3 and d < 4 and e > 5",
 			selectivity:              5.870830440255832e-05,
-			selectivityAfterIncrease: 0.000907978966620942,
+			selectivityAfterIncrease: 0.21916666666666668,
 		},
 		{
 			exprs:                    longExpr,
--- pkg/statistics/histogram.go
+++ pkg/statistics/histogram.go
@@ -1079,15 +1079,18 @@ func (hg *Histogram) OutOfRangeRowCount(
 	}
 
 	// If the realtimeRowCount is larger than the original table rows, then any out of range estimate is unreliable.
-	// Assume at least 1/NDV is returned
 	addedRows := float64(realtimeRowCount) - hg.TotalRowCount()
-	if addedRows > 1 && rowCount < upperBound {
+	addedPct := addedRows / float64(realtimeRowCount)
+	if addedPct > totalPercent {
 		// Conservatively - use the larger of the left or right percent - since we are working with
 		// changes to the table since last Analyze - any out of range estimate is unreliable.
 		// if the histogram range is invalid (too small/large - histInvalid) - totalPercent is zero
-		// and we will set rowCount to min of upperbound and added rows
-		totalPercent = min(0.5, max(leftPercent, rightPercent))
-		rowCount = max(totalPercent*addedRows, upperBound)
+		// So we need a final max to include upperBound
+		if totalPercent == 0 {
+			totalPercent = min(addedPct, 0.5)
+		}
+		outOfRangeAdded := addedRows * totalPercent
+		rowCount = max(max(rowCount, outOfRangeAdded), upperBound)
 	}
 
 	// Use modifyCount as a final bound
