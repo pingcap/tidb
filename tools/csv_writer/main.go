172b4727782a04186c3201080ae77a2dac584273
fzzf678 <daery_7@163.com>
2025-02-27 14:03:55 +0800

    Update main.go

--- tools/csv_writer/main.go
+++ tools/csv_writer/main.go
@@ -313,12 +313,7 @@ func generateDecimal(num int, res []string) {
 
 func generateBigintWithNoLimit(num int, res []string, colName string) {
 	for i := 0; i < num; i++ {
-		if strings.Contains(colName, "datetime") && faker.Number(1, 10) <= 8 {
-			// 80% null value
-			res[i] = "\\N"
-		} else {
-			res[i] = strconv.Itoa(faker.Number(math.MinInt64, math.MaxInt64)) // https://docs.pingcap.com/zh/tidb/stable/data-type-numeric#bigint-%E7%B1%BB%E5%9E%8B)
-		}
+		res[i] = strconv.Itoa(faker.Number(math.MinInt64, math.MaxInt64)) // https://docs.pingcap.com/zh/tidb/stable/data-type-numeric#bigint-%E7%B1%BB%E5%9E%8B)
 	}
 }
 
