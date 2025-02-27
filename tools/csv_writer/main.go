e4dafb27c9d952f00e296eebf3dca164e617bf4c
fzzf678 <daery_7@163.com>
2025-02-27 13:46:55 +0800

    Update main.go

--- tools/csv_writer/main.go
+++ tools/csv_writer/main.go
@@ -246,7 +246,7 @@ func (t *Task) generateValueByCol(col Column, num int, res []string) {
 		if col.IsUnique {
 			generateBigint(res, col.Order, t.begin, t.end)
 		} else {
-			generateBigintWithNoLimit(num, res)
+			generateBigintWithNoLimit(num, res, col.Name)
 		}
 	case strings.HasPrefix(col.Type, "TINYINT"):
 		generateTinyint1(num, res)
@@ -311,9 +311,14 @@ func generateDecimal(num int, res []string) {
 	}
 }
 
-func generateBigintWithNoLimit(num int, res []string) {
+func generateBigintWithNoLimit(num int, res []string, colName string) {
 	for i := 0; i < num; i++ {
-		res[i] = strconv.Itoa(faker.Number(math.MinInt64, math.MaxInt64)) // https://docs.pingcap.com/zh/tidb/stable/data-type-numeric#bigint-%E7%B1%BB%E5%9E%8B)
+		if strings.Contains(colName, "datetime") && faker.Number(1, 10) <= 8 {
+			// 80% null value
+			res[i] = "\\N"
+		} else {
+			res[i] = strconv.Itoa(faker.Number(math.MinInt64, math.MaxInt64)) // https://docs.pingcap.com/zh/tidb/stable/data-type-numeric#bigint-%E7%B1%BB%E5%9E%8B)
+		}
 	}
 }
 
