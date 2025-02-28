f7aa877a1ea6056aa54ff8d56ea80e399ad53068
fzzf678 <daery_7@163.com>
2025-02-28 22:00:26 +0800

    Update main.go

--- tools/csv_writer/main.go
+++ tools/csv_writer/main.go
@@ -432,7 +432,7 @@ func generateVarbinary(num, len int, res []string, unique bool) {
 				res[i] = "\\N"
 			} else {
 				// todo: remove 1024
-				res[i] = generateLetterWithNum(128, true)
+				res[i] = generateLetterWithNum(256, true)
 			}
 		}
 	}
