a9477b16091e3763be7749e7d7e8c67201aba0d9
qw4990 <zhangyuanjia@pingcap.com>
2025-03-04 15:42:33 +0800

    fixup

--- pkg/bindinfo/binding_cache.go
+++ pkg/bindinfo/binding_cache.go
@@ -128,6 +128,7 @@ func (u *bindingCacheUpdater) readBindingsFromStorage(condition string) (binding
 	return
 }
 
+// NewBindingCacheUpdater creates a new BindingCacheUpdater.
 func NewBindingCacheUpdater(sPool util.DestroyableSessionPool) BindingCacheUpdater {
 	u := new(bindingCacheUpdater)
 	u.lastUpdateTime.Store(types.ZeroTimestamp)
