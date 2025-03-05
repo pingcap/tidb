8f03ce0dc78dbbb7959a867aab1c8859a87f3450
Weizhen Wang <wangweizhen@pingcap.com>
2025-03-05 11:51:04 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/find_best_task.go
+++ pkg/planner/core/find_best_task.go
@@ -189,10 +189,10 @@ func enumeratePhysicalPlans4Task(
 	}
 	var fd *fd.FDSet
 	// while for join's both child, anyway and currently we should keep exchanger within the MPP for now
-	// TODO(hawkingrei): FD should be maintained as logical prop instead of constructing it in physical phase
 	if !prop.IsParentPhyscicalHashJoin {
 		if joinP, ok := p.Self().(*logicalop.LogicalJoin); ok {
 			if joinP.JoinType == logicalop.InnerJoin {
+				// TODO(hawkingrei): FD should be maintained as logical prop instead of constructing it in physical phase
 				fd = joinP.ExtractFD()
 			}
 		}
