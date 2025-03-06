6043ea9c1d5f39dbbecef55e027d226f7f985a85
Yiding <winoros@gmail.com>
2025-03-07 02:57:39 +0800

    fix static check and address comments

--- pkg/planner/core/operator/logicalop/logical_join.go
+++ pkg/planner/core/operator/logicalop/logical_join.go
@@ -389,13 +389,14 @@ func (p *LogicalJoin) PushDownTopN(topNLogicalPlan base.LogicalPlan, opt *optimi
 	// The LogicalJoin may be also a LogicalApply. So we must use self to set parents.
 	if topN != nil {
 		if topnEliminated {
+			// Add a sort if the topN has order by items.
 			if len(topN.ByItems) > 0 {
 				sort := LogicalSort{ByItems: topN.ByItems}.Init(p.SCtx(), p.QueryBlockOffset())
 				sort.SetChildren(p.Self())
 				return sort
-			} else {
-				return p.Self()
 			}
+			// If the topN has no order by items, simply return the join itself.
+			return p.Self()
 		}
 		return topN.AttachChild(p.Self(), opt)
 	}
