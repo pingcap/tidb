0d8b4e507e5f8a5053f71d5603100afc75c81562
Mattias Jonsson <mattias.jonsson@pingcap.com>
2025-03-07 15:42:08 +0100

    Rolled back some error handling.

--- pkg/table/tables/partition.go
+++ pkg/table/tables/partition.go
@@ -1422,9 +1422,6 @@ func (t *partitionedTable) locatePartitionCommon(ctx expression.EvalContext, tp 
 			idx, err = t.locateRangePartition(ctx, partitionExpr, r)
 		}
 		if err != nil {
-			if table.ErrNoPartitionForGivenValue.Equal(err) {
-				return -1, nil
-			}
 			return -1, err
 		}
 		pi := t.Meta().Partition
@@ -1452,9 +1449,6 @@ func (t *partitionedTable) locatePartitionCommon(ctx expression.EvalContext, tp 
 		idx = 0
 	}
 	if err != nil {
-		if table.ErrNoPartitionForGivenValue.Equal(err) {
-			return -1, nil
-		}
 		return -1, errors.Trace(err)
 	}
 	return idx, nil
