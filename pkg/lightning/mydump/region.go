6fb09b8cdb780dfad8a2c701dc765311ca8cbadf
Ruihao Chen <ruihao.chen@pingcap.cn>
2025-03-07 09:54:03 +0800

    [test] skip preprocess for test

--- pkg/lightning/mydump/region.go
+++ pkg/lightning/mydump/region.go
@@ -362,18 +362,20 @@ func MakeSourceFileRegion(
 // because parquet files can't seek efficiently, there is no benefit in split.
 // parquet file are column orient, so the offset is read line number
 func makeParquetFileRegion(
-	ctx context.Context,
+	_ context.Context,
 	cfg *DataDivideConfig,
 	dataFile FileInfo,
 ) ([]*TableRegion, []float64, error) {
 	numberRows := dataFile.FileMeta.ParquetMeta.Rows
-	var err error
+	// var err error
 	// for safety
 	if numberRows <= 0 {
-		numberRows, err = ReadParquetFileRowCountByFile(ctx, cfg.Store, dataFile.FileMeta)
-		if err != nil {
-			return nil, nil, err
-		}
+		// TODO(joechenrh): only for test
+		numberRows = 1600000
+		// numberRows, err = ReadParquetFileRowCountByFile(ctx, cfg.Store, dataFile.FileMeta)
+		// if err != nil {
+		// 	return nil, nil, err
+		// }
 	}
 	region := &TableRegion{
 		DB:       cfg.TableMeta.DB,
