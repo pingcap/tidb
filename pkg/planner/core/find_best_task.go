34077c220fdec50643715929284aed14f4e995d8
Yang Keao <yangkeao@chunibyo.icu>
2025-03-10 22:52:37 +0800

    remove no_null_index if the condition is not valid in skyline pruning
    Signed-off-by: Yang Keao <yangkeao@chunibyo.icu>

--- pkg/planner/core/find_best_task.go
+++ pkg/planner/core/find_best_task.go
@@ -1174,6 +1174,30 @@ func (ds *DataSource) skylinePruning(prop *property.PhysicalProperty) []*candida
 			candidates = append(candidates, ds.getIndexMergeCandidate(path, prop))
 			continue
 		}
+		// If the index contains NO_NULL_INDEX column, it may not contain all rows
+		if path.Index != nil && len(path.Index.NoNullIdxColOffsets) > 0 {
+			includeNullRange := false
+
+			for _, col := range path.Index.NoNullIdxColOffsets {
+				for _, ran := range path.Ranges {
+					if len(ran.LowVal) <= col || len(ran.HighVal) <= col ||
+						ran.LowVal[col].IsNull() || ran.HighVal[col].IsNull() {
+						includeNullRange = true
+						break
+					}
+				}
+
+				if includeNullRange {
+					break
+				}
+			}
+
+			// TODO: Cover more cases: the index filter/table filter/access condition may hace the
+			// conditions that the column is not null
+			if includeNullRange {
+				continue
+			}
+		}
 		// if we already know the range of the scan is empty, just return a TableDual
 		if len(path.Ranges) == 0 {
 			return []*candidatePath{{path: path}}
