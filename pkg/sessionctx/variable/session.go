706a8b995f0e49149a78148f21ed2d32709917b3
mohangjie <mohangjie1995@gmail.com>
2025-03-11 12:39:34 +0800

    update

--- pkg/sessionctx/variable/session.go
+++ pkg/sessionctx/variable/session.go
@@ -1804,8 +1804,11 @@ func (s *SessionVars) InitStatementContext() *stmtctx.StatementContext {
 		sc = &s.cachedStmtCtx[1]
 	}
 	if s.RefCountOfStmtCtx.TryFreeze() {
-		sc.Reset()
+		succ := sc.Reset()
 		s.RefCountOfStmtCtx.UnFreeze()
+		if !succ {
+			sc = stmtctx.NewStmtCtx()
+		}
 	} else {
 		sc = stmtctx.NewStmtCtx()
 	}
