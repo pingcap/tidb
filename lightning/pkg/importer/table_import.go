0e77aaa15bff347e2fd9e4d814947d7067d8f95d
OliverS929 <182192954+OliverS929@users.noreply.github.com>
2025-03-17 18:09:43 +0800

    Make sure that data engine use the configured value as well.

--- lightning/pkg/importer/table_import.go
+++ lightning/pkg/importer/table_import.go
@@ -658,6 +658,7 @@ func (tr *TableImporter) preprocessEngine(
 		dataEngineCfg.Local.Compact = true
 		dataEngineCfg.Local.CompactConcurrency = 4
 		dataEngineCfg.Local.CompactThreshold = local.CompactionUpperThreshold
+		dataEngineCfg.Local.BlockSize = int(rc.cfg.TikvImporter.BlockSize)
 	}
 	dataEngine, err := rc.engineMgr.OpenEngine(ctx, dataEngineCfg, tr.tableName, engineID)
 	if err != nil {
