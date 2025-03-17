d2da32e39cb9a33bc0ef1d318b163407115b673f
qw4990 <zhangyuanjia@pingcap.com>
2025-03-17 17:33:22 +0800

    fixup

--- pkg/planner/core/physical_plans.go
+++ pkg/planner/core/physical_plans.go
@@ -16,6 +16,7 @@ package core
 
 import (
 	"fmt"
+	"github.com/pingcap/tidb/pkg/util/intest"
 	"strconv"
 	"strings"
 	"unsafe"
@@ -1620,6 +1621,30 @@ type PhysicalIndexJoin struct {
 	InnerHashKeys []*expression.Column
 }
 
+// Clone implements op.PhysicalPlan interface.
+func (p *PhysicalIndexJoin) Clone(newCtx base.PlanContext) (base.PhysicalPlan, error) {
+	cloned := new(PhysicalIndexJoin)
+	cloned.SetSCtx(newCtx)
+	base, err := p.basePhysicalJoin.cloneWithSelf(newCtx, cloned)
+	if err != nil {
+		return nil, err
+	}
+	cloned.basePhysicalJoin = *base
+	cloned.innerPlan, err = p.innerPlan.Clone(newCtx)
+	if err != nil {
+		return nil, err
+	}
+	cloned.Ranges = p.Ranges.CloneForPlanCache() // this clone is deep copy
+	cloned.KeyOff2IdxOff = make([]int, len(p.KeyOff2IdxOff))
+	copy(cloned.KeyOff2IdxOff, p.KeyOff2IdxOff)
+	cloned.IdxColLens = make([]int, len(p.IdxColLens))
+	copy(cloned.IdxColLens, p.IdxColLens)
+	cloned.CompareFilters = p.CompareFilters.Copy()
+	cloned.OuterHashKeys = util.CloneCols(p.OuterHashKeys)
+	cloned.InnerHashKeys = util.CloneCols(p.InnerHashKeys)
+	return cloned, nil
+}
+
 // MemoryUsage return the memory usage of PhysicalIndexJoin
 func (p *PhysicalIndexJoin) MemoryUsage() (sum int64) {
 	if p == nil {
@@ -1678,6 +1703,26 @@ type PhysicalIndexHashJoin struct {
 	// KeepOuterOrder indicates whether keeping the output result order as the
 	// outer side.
 	KeepOuterOrder bool
+}
+
+// Clone implements op.PhysicalPlan interface.
+func (p *PhysicalIndexHashJoin) Clone(newCtx base.PlanContext) (base.PhysicalPlan, error) {
+	cloned := new(PhysicalIndexHashJoin)
+	cloned.SetSCtx(newCtx)
+	base, err := p.basePhysicalJoin.cloneWithSelf(newCtx, cloned)
+	if err != nil {
+		return nil, err
+	}
+	cloned.basePhysicalJoin = *base
+	physicalIndexJoin, err := p.PhysicalIndexJoin.Clone(newCtx)
+	if err != nil {
+		return nil, err
+	}
+	indexJoin, ok := physicalIndexJoin.(*PhysicalIndexJoin)
+	intest.Assert(ok)
+	cloned.PhysicalIndexJoin = *indexJoin
+	cloned.KeepOuterOrder = p.KeepOuterOrder
+	return cloned, nil
 }
 
 // MemoryUsage return the memory usage of PhysicalIndexHashJoin
