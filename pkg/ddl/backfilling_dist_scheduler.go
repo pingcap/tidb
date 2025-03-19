ae95c3ac44c335d7163f2dcd966d42d87e0b3e16
gmhdbjd <gmhdbjd@gmail.com>
2025-03-19 21:12:07 +0800

    Revert "force mergesort"
    This reverts commit d9d5e7c9031385e574dd68cace98db5c12444364.

--- pkg/ddl/backfilling_dist_scheduler.go
+++ pkg/ddl/backfilling_dist_scheduler.go
@@ -177,12 +177,11 @@ func (sch *LitBackfillScheduler) GetNextStep(task *proto.TaskBase) proto.Step {
 	}
 }
 
-func skipMergeSort(_ []external.MultipleFilesStat) bool {
-	return false
-	//failpoint.Inject("forceMergeSort", func() {
-	//	failpoint.Return(false)
-	//})
-	//return external.GetMaxOverlappingTotal(stats) <= external.MergeSortOverlapThreshold
+func skipMergeSort(stats []external.MultipleFilesStat) bool {
+	failpoint.Inject("forceMergeSort", func() {
+		failpoint.Return(false)
+	})
+	return external.GetMaxOverlappingTotal(stats) <= external.MergeSortOverlapThreshold
 }
 
 // OnDone implements scheduler.Extension interface.
