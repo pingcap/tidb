b8768e7a5868ba002f9403d73e618e07591ca649
Weizhen Wang <wangweizhen@pingcap.com>
2025-03-20 19:59:49 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/exhaust_physical_plans.go
+++ pkg/planner/core/exhaust_physical_plans.go
@@ -1535,9 +1535,15 @@ func constructIndexJoinInnerSideTaskWithAggCheck(p *logicalop.LogicalJoin, prop 
 
 	// build hash agg, when the stream agg is illegal such as the order by prop is not matched
 	if aggTask == nil {
+		stats := la.StatsInfo()
+		if dsCopTask.indexPlan != nil {
+			stats.ScaleByExpectCnt(dsCopTask.indexPlan.StatsCount())
+		} else if dsCopTask.tablePlan != nil {
+			stats.ScaleByExpectCnt(dsCopTask.tablePlan.StatsCount())
+		}
 		physicalHashAgg := NewPhysicalHashAgg(
 			la,
-			la.StatsInfo().ScaleByExpectCnt(dsCopTask.indexPlan.StatsCount()),
+			stats,
 			prop)
 		physicalHashAgg.SetSchema(la.Schema().Clone())
 		aggTask = physicalHashAgg.Attach2Task(dsCopTask)
