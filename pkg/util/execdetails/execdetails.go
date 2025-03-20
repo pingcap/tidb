e5dc26c2e058f41ac656a27726afb7b15e292949
yibin87 <huyibin@pingcap.com>
2025-03-20 14:26:30 +0800

    Remove clone function call
    Signed-off-by: yibin87 <huyibin@pingcap.com>

--- pkg/util/execdetails/execdetails.go
+++ pkg/util/execdetails/execdetails.go
@@ -1711,7 +1711,7 @@ func (e *RuntimeStatsColl) RegisterStats(planID int, info RuntimeStats) {
 		}
 	}
 	if !found {
-		stats.groupRss = append(stats.groupRss, info.Clone())
+		stats.groupRss = append(stats.groupRss, info)
 	}
 }
 
