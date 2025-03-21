8db39cb9d13a72048ab43a05cddcc0699873e915
BornChanger <97348524+BornChanger@users.noreply.github.com>
2025-03-21 16:11:57 +0800

    Update br/pkg/stream/table_mapping.go
    Co-authored-by: Jianjun Liao <36503113+Leavrth@users.noreply.github.com>

--- br/pkg/stream/table_mapping.go
+++ br/pkg/stream/table_mapping.go
@@ -293,7 +293,8 @@ func (tm *TableMappingManager) MergeBaseDBReplace(baseMap map[UpstreamID]*DBRepl
 			existingDBReplace.DbID = newID
 		}
 
-		// update db name if empty and exists in baseMap
+		// db replace in `TableMappingManager` has no name yet, it is determined by baseMap.
+		// TODO: update the name of the db replace that is not exists in baseMap. Now it is OK because user tables' name is not used.
 		if existingDBReplace.Name == "" {
 			if baseDBReplace, exists := baseMap[upDBID]; exists && baseDBReplace.Name != "" {
 				existingDBReplace.Name = baseDBReplace.Name
