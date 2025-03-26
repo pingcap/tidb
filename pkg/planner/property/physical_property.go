f212c80a46520ce6387438de90224a9dfc3b4be0
Weizhen Wang <wangweizhen@pingcap.com>
2025-03-26 19:08:46 +0800

    Update pkg/planner/property/physical_property.go
    Co-authored-by: Elsa <111482174+elsa0520@users.noreply.github.com>

--- pkg/planner/property/physical_property.go
+++ pkg/planner/property/physical_property.go
@@ -310,7 +310,7 @@ func (p *PhysicalProperty) IsSubsetOf(keys []*MPPPartitionColumn) []int {
 //  3. FD: (1)-->(2-6,8), ()-->(7), (9)-->(10-17), (1,10)==(1,10), (18,21)-->(19,20,22-33), (9,18)==(9,18)
 //     In this case, we can see that the child supplied partition keys is subset of parent required partition cols.
 func (p *PhysicalProperty) NeedMPPExchangeByEquivalence(
-	hashCols []*MPPPartitionColumn, fd *funcdep.FDSet) bool {
+currentPartitionColumn []*MPPPartitionColumn, fd *funcdep.FDSet) bool {
 	uniqueID2MppCol := make(map[*MPPPartitionColumn]intset.FastIntSet, len(p.MPPPartitionCols))
 	// for each partition column, we calculate the equivalence alternative closure of it.
 	for _, pCol := range p.MPPPartitionCols {
