50b9da5f8c102c2a74ae26955f434d5e0107f169
Weizhen Wang <wangweizhen@pingcap.com>
2025-03-28 16:02:06 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/optimizer.go
+++ pkg/planner/core/optimizer.go
@@ -451,6 +451,9 @@ func mergeContinuousSelections(p base.PhysicalPlan) {
 }
 
 func postOptimize(ctx context.Context, sctx base.PlanContext, plan base.PhysicalPlan) base.PhysicalPlan {
+	if !sctx.GetSessionVars().InRestrictedSQL {
+		fmt.Println("wwz")
+	}
 	// some cases from update optimize will require avoiding projection elimination.
 	// see comments ahead of call of DoOptimize in function of buildUpdate().
 	plan = eliminatePhysicalProjection(plan)
