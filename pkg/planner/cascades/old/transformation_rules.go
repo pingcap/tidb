4b06117ea6418d8e1f5b9de696bc1b8d4b1c9cf9
Mattias Jonsson <mattias.jonsson@pingcap.com>
2025-03-29 18:43:53 +0100

    Fixed code changes that lead to a failing test.

--- pkg/planner/cascades/old/transformation_rules.go
+++ pkg/planner/cascades/old/transformation_rules.go
@@ -1316,7 +1316,10 @@ func (*PushTopNDownProjection) OnTransform(old *memo.ExprIter) (newExprs []*memo
 	for i := len(newTopN.ByItems) - 1; i >= 0; i-- {
 		switch newTopN.ByItems[i].Expr.(type) {
 		case *expression.Constant, *expression.CorrelatedColumn:
-			topN.ByItems = slices.Delete(newTopN.ByItems, i, i+1)
+			topN.ByItems = append(newTopN.ByItems[:i], newTopN.ByItems[i+1:]...)
+			// TODO: Investigate why this does not work instead?
+			// Is it the clear() of the last part of the slice cause an issue?
+			//topN.ByItems = slices.Delete(newTopN.ByItems, i, i+1)
 		}
 	}
 	projExpr := memo.NewGroupExpr(proj)
