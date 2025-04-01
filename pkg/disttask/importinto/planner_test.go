1c408155ee3af58fea671cccaf1c1f6a3f15138c
GMHDBJD <35025882+GMHDBJD@users.noreply.github.com>
2025-04-01 17:04:15 +0800

    importinto: fix error not returned when writing external meta (#60350)
    close pingcap/tidb#60083

--- pkg/disttask/importinto/planner_test.go
+++ pkg/disttask/importinto/planner_test.go
@@ -125,6 +125,14 @@ func TestToPhysicalPlan(t *testing.T) {
 	bs, err = json.Marshal(subtaskMeta2)
 	require.NoError(t, err)
 	require.Equal(t, [][]byte{bs}, subtaskMetas2)
+
+	planCtx = planner.PlanCtx{
+		NextTaskStep: proto.ImportStepImport,
+		GlobalSort:   true,
+	}
+	_, err = logicalPlan.ToPhysicalPlan(planCtx)
+	// error when build controller plan
+	require.ErrorContains(t, err, "table tb can't be in none state")
 }
 
 func genEncodeStepMetas(t *testing.T, cnt int) [][]byte {
