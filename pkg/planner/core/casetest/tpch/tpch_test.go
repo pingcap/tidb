8d3a0b4173d267077dc81ddfdfd26fa9791c1ae7
Weizhen Wang <wangweizhen@pingcap.com>
2025-04-02 18:44:51 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/casetest/tpch/tpch_test.go
+++ pkg/planner/core/casetest/tpch/tpch_test.go
@@ -199,70 +199,24 @@ CREATE TABLE lineitem (
 	testkit.SetTiFlashReplica(t, dom, "test", "customer")
 	testkit.SetTiFlashReplica(t, dom, "test", "orders")
 	testkit.SetTiFlashReplica(t, dom, "test", "lineitem")
-	tk.MustQuery(`explain format="brief" select
-	c_name,
-	c_custkey,
-	o_orderkey,
-	o_orderdate,
-	o_totalprice,
-	sum(l_quantity)
-from
-	customer,
-	orders,
-	lineitem
-where
-	o_orderkey in (
-		select
-			l_orderkey
-		from
-			lineitem
-		group by
-			l_orderkey having
-				sum(l_quantity) > 314
+	tk.MustExec("set @@session.tidb_broadcast_join_threshold_size = 0")
+	tk.MustExec("set @@session.tidb_broadcast_join_threshold_count = 0")
+	integrationSuiteData := GetTPCHSuiteData()
+	var (
+		input  []string
+		output []struct {
+			SQL    string
+			Result []string
+		}
 	)
-	and c_custkey = o_custkey
-	and o_orderkey = l_orderkey
-group by
-	c_name,
-	c_custkey,
-	o_orderkey,
-	o_orderdate,
-	o_totalprice
-order by
-	o_totalprice desc,
-	o_orderdate
-limit 100;`).Check(testkit.Rows(
-		"Projection 100.00 root  test.customer.c_name, test.customer.c_custkey, test.orders.o_orderkey, test.orders.o_orderdate, test.orders.o_totalprice, Column#52",
-		"└─TopN 100.00 root  test.orders.o_totalprice:desc, test.orders.o_orderdate, offset:0, count:100",
-		"  └─TableReader 100.00 root  MppVersion: 3, data:ExchangeSender",
-		"    └─ExchangeSender 100.00 mpp[tiflash]  ExchangeType: PassThrough",
-		"      └─TopN 100.00 mpp[tiflash]  test.orders.o_totalprice:desc, test.orders.o_orderdate, offset:0, count:100",
-		"        └─Projection 8000.00 mpp[tiflash]  Column#52, test.customer.c_custkey, test.customer.c_name, test.orders.o_orderkey, test.orders.o_totalprice, test.orders.o_orderdate",
-		"          └─HashAgg 8000.00 mpp[tiflash]  group by:test.customer.c_custkey, test.customer.c_name, test.orders.o_orderdate, test.orders.o_orderkey, test.orders.o_totalprice, funcs:sum(Column#85)->Column#52, funcs:firstrow(test.customer.c_custkey)->test.customer.c_custkey, funcs:firstrow(test.customer.c_name)->test.customer.c_name, funcs:firstrow(test.orders.o_orderkey)->test.orders.o_orderkey, funcs:firstrow(test.orders.o_totalprice)->test.orders.o_totalprice, funcs:firstrow(test.orders.o_orderdate)->test.orders.o_orderdate",
-		"            └─ExchangeReceiver 8000.00 mpp[tiflash]  ",
-		"              └─ExchangeSender 8000.00 mpp[tiflash]  ExchangeType: HashPartition, Compression: FAST, Hash Cols: [name: test.customer.c_name, collate: utf8mb4_bin], [name: test.customer.c_custkey, collate: binary], [name: test.orders.o_orderkey, collate: binary], [name: test.orders.o_orderdate, collate: binary], [name: test.orders.o_totalprice, collate: binary]",
-		"                └─HashAgg 8000.00 mpp[tiflash]  group by:test.customer.c_custkey, test.customer.c_name, test.orders.o_orderdate, test.orders.o_orderkey, test.orders.o_totalprice, funcs:sum(test.lineitem.l_quantity)->Column#85",
-		"                  └─Projection 12500.00 mpp[tiflash]  test.customer.c_custkey, test.customer.c_name, test.orders.o_orderkey, test.orders.o_totalprice, test.orders.o_orderdate, test.lineitem.l_quantity",
-		"                    └─HashJoin 12500.00 mpp[tiflash]  inner join, equal:[eq(test.orders.o_orderkey, test.lineitem.l_orderkey)]",
-		"                      ├─ExchangeReceiver(Build) 6400.00 mpp[tiflash]  ",
-		"                      │ └─ExchangeSender 6400.00 mpp[tiflash]  ExchangeType: Broadcast, Compression: FAST",
-		"                      │   └─Selection 6400.00 mpp[tiflash]  gt(Column#50, 314)",
-		"                      │     └─Projection 8000.00 mpp[tiflash]  Column#50, test.lineitem.l_orderkey",
-		"                      │       └─HashAgg 8000.00 mpp[tiflash]  group by:test.lineitem.l_orderkey, funcs:sum(Column#57)->Column#50, funcs:firstrow(test.lineitem.l_orderkey)->test.lineitem.l_orderkey",
-		"                      │         └─ExchangeReceiver 8000.00 mpp[tiflash]  ",
-		"                      │           └─ExchangeSender 8000.00 mpp[tiflash]  ExchangeType: HashPartition, Compression: FAST, Hash Cols: [name: test.lineitem.l_orderkey, collate: binary]",
-		"                      │             └─HashAgg 8000.00 mpp[tiflash]  group by:test.lineitem.l_orderkey, funcs:sum(test.lineitem.l_quantity)->Column#57",
-		"                      │               └─TableFullScan 10000.00 mpp[tiflash] table:lineitem keep order:false, stats:pseudo",
-		"                      └─Projection(Probe) 15625.00 mpp[tiflash]  test.customer.c_custkey, test.customer.c_name, test.orders.o_orderkey, test.orders.o_totalprice, test.orders.o_orderdate, test.lineitem.l_quantity",
-		"                        └─HashJoin 15625.00 mpp[tiflash]  inner join, equal:[eq(test.orders.o_orderkey, test.lineitem.l_orderkey)]",
-		"                          ├─ExchangeReceiver(Build) 10000.00 mpp[tiflash]  ",
-		"                          │ └─ExchangeSender 10000.00 mpp[tiflash]  ExchangeType: Broadcast, Compression: FAST",
-		"                          │   └─TableFullScan 10000.00 mpp[tiflash] table:lineitem keep order:false, stats:pseudo",
-		"                          └─Projection(Probe) 12500.00 mpp[tiflash]  test.customer.c_custkey, test.customer.c_name, test.orders.o_orderkey, test.orders.o_totalprice, test.orders.o_orderdate",
-		"                            └─HashJoin 12500.00 mpp[tiflash]  inner join, equal:[eq(test.customer.c_custkey, test.orders.o_custkey)]",
-		"                              ├─ExchangeReceiver(Build) 10000.00 mpp[tiflash]  ",
-		"                              │ └─ExchangeSender 10000.00 mpp[tiflash]  ExchangeType: Broadcast, Compression: FAST",
-		"                              │   └─TableFullScan 10000.00 mpp[tiflash] table:customer keep order:false, stats:pseudo",
-		"                              └─TableFullScan(Probe) 10000.00 mpp[tiflash] table:orders keep order:false, stats:pseudo",
-	))
+	integrationSuiteData.LoadTestCases(t, &input, &output)
+	for i := 0; i < len(input); i++ {
+		testdata.OnRecord(func() {
+			output[i].SQL = input[i]
+		})
+		testdata.OnRecord(func() {
+			output[i].Result = testdata.ConvertRowsToStrings(tk.MustQuery(input[i]).Rows())
+		})
+		tk.MustQuery(input[i]).Check(testkit.Rows(output[i].Result...))
+	}
 }
