b1e3ed072e80548ce2d7d2c86511516affddbb4b
RidRisR <79858083+RidRisR@users.noreply.github.com>
2025-04-08 10:29:50 +0200

    new alloc

--- br/pkg/restore/internal/prealloc_table_id/alloc.go
+++ br/pkg/restore/internal/prealloc_table_id/alloc.go
@@ -13,14 +13,14 @@ import (
 )
 
 const (
-	// insaneTableIDThreshold is the threshold for "normal" table ID.
+	// InsaneTableIDThreshold is the threshold for "normal" table ID.
 	// Sometimes there might be some tables with huge table ID.
 	// For example, DDL metadata relative tables may have table ID up to 1 << 48.
 	// When calculating the max table ID, we would ignore tables with table ID greater than this.
 	// NOTE: In fact this could be just `1 << 48 - 1000` (the max available global ID),
 	// however we are going to keep some gap here for some not-yet-known scenario, which means
 	// at least, BR won't exhaust all global IDs.
-	insaneTableIDThreshold = math.MaxUint32
+	InsaneTableIDThreshold = math.MaxUint32
 )
 
 // Allocator is the interface needed to allocate table IDs.
@@ -31,12 +31,13 @@ type Allocator interface {
 
 // PreallocIDs mantains the state of preallocated table IDs.
 type PreallocIDs struct {
-	mu              sync.Mutex
-	start           int64
-	rewriteBoundary int64
-	end             int64
-	used            map[int64]struct{}
-	next            int64
+	mu             sync.Mutex
+	start          int64
+	reusableBorder int64
+	end            int64
+	count          int64
+	used           map[int64]struct{}
+	next           int64
 }
 
 // New collects the requirement of prealloc IDs and return a
@@ -49,32 +50,31 @@ func New(tables []*metautil.Table) *PreallocIDs {
 	}
 
 	maxID := int64(0)
-	count := int64(0)
+	count := int64(len(tables))
 
 	for _, t := range tables {
-		count += 1
-		if t.Info.ID > maxID && t.Info.ID < insaneTableIDThreshold {
+		if t.Info.ID > maxID && t.Info.ID < InsaneTableIDThreshold {
 			maxID = t.Info.ID
 		}
 
 		if t.Info.Partition != nil && t.Info.Partition.Definitions != nil {
 			count += int64(len(t.Info.Partition.Definitions))
 			for _, part := range t.Info.Partition.Definitions {
-				if part.ID > maxID && part.ID < insaneTableIDThreshold {
+				if part.ID > maxID && part.ID < InsaneTableIDThreshold {
 					maxID = part.ID
 				}
 			}
 		}
 	}
-	if maxID+1 > insaneTableIDThreshold || maxID+count+1 > insaneTableIDThreshold {
+	if maxID+count+1 > InsaneTableIDThreshold {
 		return nil
 	}
 	return &PreallocIDs{
-		start:           math.MaxInt64,
-		rewriteBoundary: maxID + 1,
-		end:             maxID + count + 1,
-		used:            make(map[int64]struct{}, count),
-		next:            math.MaxInt64,
+		start:          math.MaxInt64,
+		reusableBorder: maxID + 1,
+		count:          count,
+		used:           make(map[int64]struct{}, count),
+		next:           math.MaxInt64,
 	}
 }
 
@@ -100,20 +100,28 @@ func (p *PreallocIDs) Alloc(m Allocator) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
-	if p.end == 0 {
+	if p.count == 0 {
 		return nil
 	}
 	if p.start < p.end {
-		return errors.Errorf("prealloc table ID should be empty")
+		return errors.Errorf("table ID should only be allocated once")
 	}
 
-	alloced, err := m.AdvanceGlobalIDs(int(p.end))
+	currentID, err := m.GetGlobalID()
 	if err != nil {
 		return err
 	}
-	p.start = alloced + 1
-	p.end += p.start
-	p.next = p.start
+	if p.reusableBorder <= currentID+1 {
+		p.reusableBorder = currentID + 1
+	}
+	idRnage := p.reusableBorder - (currentID + 1) + p.count
+	if _, err := m.AdvanceGlobalIDs(int(idRnage)); err != nil {
+		return err
+	}
+
+	p.start = currentID + 1
+	p.end = p.start + idRnage
+	p.next = p.reusableBorder
 	return nil
 }
 
@@ -122,7 +130,7 @@ func (p *PreallocIDs) allocID(originalID int64) (int64, error) {
 		return 0, errors.Errorf("no available IDs")
 	}
 
-	if originalID >= p.start && originalID < p.rewriteBoundary {
+	if originalID >= p.start && originalID < p.reusableBorder {
 		if _, exists := p.used[originalID]; !exists {
 			p.used[originalID] = struct{}{}
 			return originalID, nil
@@ -132,7 +140,7 @@ func (p *PreallocIDs) allocID(originalID int64) (int64, error) {
 	start := p.next
 	for {
 		current := p.next
-		p.next = (current+1-p.start)%(p.end-p.start) + p.start
+		p.next = (current+1-p.reusableBorder)%(p.end-p.reusableBorder) + p.reusableBorder
 
 		if _, exists := p.used[current]; !exists {
 			p.used[current] = struct{}{}
