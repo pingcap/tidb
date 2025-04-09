6bdd8a2ac7b10dc338c80fab3e835e2f0921d334
zeminzhou <zhouzemin@pingcap.com>
2025-04-09 14:56:59 +0800

    fix
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- lightning/pkg/importer/get_pre_info.go
+++ lightning/pkg/importer/get_pre_info.go
@@ -231,7 +231,7 @@ func (g *TargetInfoGetterImpl) IsTableEmpty(ctx context.Context, schemaName stri
 // It implements the TargetInfoGetter interface.
 // It uses the SQL to fetch sys variables from the target.
 func (g *TargetInfoGetterImpl) GetTargetSysVariablesForImport(ctx context.Context, _ ...ropts.GetPreInfoOption) map[string]string {
-	sysVars := ObtainImportantVariables(ctx, g.db, !g.cfg.IsTiDBBackend())
+	sysVars := ObtainImportantVariables(ctx, g.db, !g.cfg.TikvImporter.IsTiDBBackend())
 	// override by manually set vars
 	maps.Copy(sysVars, g.cfg.TiDB.Vars)
 	return sysVars
@@ -556,7 +556,7 @@ func (p *PreImportInfoGetterImpl) EstimateSourceDataSize(ctx context.Context, op
 				tableSize := tbl.TotalSize
 				// Do not sample small table because there may a large number of small table and it will take a long
 				// time to sample data for all of them.
-				if p.cfg.IsTiDBBackend() || tbl.TotalSize < int64(config.SplitRegionSize) {
+				if p.cfg.TikvImporter.IsTiDBBackend() || tbl.TotalSize < int64(config.SplitRegionSize) {
 					tbl.IndexRatio = 1.0
 					tbl.IsRowOrdered = false
 				} else {
@@ -583,7 +583,7 @@ func (p *PreImportInfoGetterImpl) EstimateSourceDataSize(ctx context.Context, op
 		}
 	}
 
-	if p.cfg.IsLocalBackend() {
+	if p.cfg.TikvImporter.IsLocalBackend() {
 		sizeWithIndex = int64(float64(sizeWithIndex) * compressionRatio)
 		tiflashSize = int64(float64(tiflashSize) * compressionRatio)
 	}
