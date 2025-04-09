e19325ba7cfa1474d5e08ec8c644e5ec2701596b
fzzf678 <daery_7@163.com>
2025-04-09 19:29:18 +0800

    debug

--- pkg/lightning/backend/external/reader.go
+++ pkg/lightning/backend/external/reader.go
@@ -84,7 +84,7 @@ func readAllData(
 	}
 
 	eg, egCtx := util.NewErrorGroupWithRecoverWithCtx(ctx)
-	readConn := 4
+	readConn := 1000
 	readConn = min(readConn, len(dataFiles))
 	taskCh := make(chan int)
 	output.memKVBuffers = make([]*membuf.Buffer, readConn*2)
