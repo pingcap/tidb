b33abd3173c2f5aaa3f85cf1404162dcf61947ed
fzzf678 <daery_7@163.com>
2025-04-10 19:50:21 +0800

    Update reader.go

--- pkg/lightning/backend/external/reader.go
+++ pkg/lightning/backend/external/reader.go
@@ -72,7 +72,7 @@ func readAllData(
 		task.End(zap.ErrorLevel, err)
 	}()
 
-	concurrences, startOffsets, err, skipOpen, readConn1 := getFilesReadConcurrency(
+	concurrences, startOffsets, err, _, _ := getFilesReadConcurrency(
 		ctx,
 		store,
 		statsFiles,
@@ -85,7 +85,8 @@ func readAllData(
 
 	eg, egCtx := util.NewErrorGroupWithRecoverWithCtx(ctx)
 	readConn := 1000
-	readConn = min(readConn, readConn1)
+	//readConn = min(readConn, readConn1)
+	readConn = min(readConn, len(dataFiles))
 	taskCh := make(chan int)
 	output.memKVBuffers = make([]*membuf.Buffer, readConn*2)
 	task.Info("read file go routine num", zap.Int("readConn", readConn))
@@ -126,9 +127,9 @@ func readAllData(
 	}
 
 	for fileIdx := range dataFiles {
-		if skipOpen[fileIdx] {
-			continue
-		}
+		//if skipOpen[fileIdx] {
+		//	continue
+		//}
 		select {
 		case <-egCtx.Done():
 			return eg.Wait()
