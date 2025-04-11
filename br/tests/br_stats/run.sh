fb07a46c30804af9f05380141a6d87b6f23550d1
Jianjun Liao <jianjun.liao@outlook.com>
2025-04-11 15:24:51 +0800

    add integration test
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- br/tests/br_stats/run.sh
+++ br/tests/br_stats/run.sh
@@ -26,6 +26,11 @@ for i in $(seq $DB_COUNT); do
     go-ycsb load mysql -P $CUR/workload -p mysql.host=$TIDB_IP -p mysql.port=$TIDB_PORT -p mysql.user=root -p mysql.db=$DB${i}
 done
 
+run_sql "CREATE CREATE ${DB}1.br_stats_partition (id INT NOT NULL, store_id INT NOT NULL, KEY i1(id)) PARTITION BY RANGE (store_id) (PARTITION p0 VALUES LESS THAN (6), PARTITION p1 VALUES LESS THAN MAXVALUE);"
+for j in $(seq 9); do
+    run_sql "INSERT INTO ${DB}1.br_stats_partition values ($j, $j);"
+done
+
 unset BR_LOG_TO_TERM
 run_br --pd $PD_ADDR backup full -s "local://$TEST_DIR/$DB" --log-file $LOG --ignore-stats=false --filter "${DB}1.*" --filter "${DB}2.*" || cat $LOG
 dump_cnt=$(cat $LOG | grep "dump stats to json" | wc -l)
@@ -54,5 +59,27 @@ echo "load stats count: ${load_cnt}; db1 count: ${load_db1_cnt}; load mark: ${lo
 if [ "${load_mark}" -ne "11" ]; then
     echo "TEST: [$TEST_NAME] fail on load stats"
     echo $(cat $LOG | grep "restore statistic data done")
+    exit 1
+fi
+
+for i in $(seq $DB_COUNT); do
+    run_sql "DROP DATABASE $DB${i};"
+done
+
+rm -f $LOG
+run_br --pd $PD_ADDR restore full -s "local://$TEST_DIR/$DB" --log-file $LOG --load-stats=false || cat $LOG
+table_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.TABLES tables ON meta.table_id = tables.TIDB_TABLE_ID WHERE tables.TABLE_SCHEMA = '${DB}1' and modify_count = 0;" | awk '/count/{print $2}')
+if [ "$count" -ne 9 ]; then
+    echo "table stats meta count does not equal to 9, but $count instead"
+    exit 1
+fi
+p0_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.PARTITIONS parts ON meta.table_id = parts.TIDB_PARTITION_ID WHERE parts.TABLE_SCHEMA = '${DB}1' and parts.PARTITION_NAME = 'p0' and modify_count = 0;" | awk '/count/{print $2}')
+if [ "p0_count" -ne 5 ]; then
+    echo "partition p0 stats meta count does not equal to 5, but $p0_count instead"
+    exit 1
+fi
+p1_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.PARTITIONS parts ON meta.table_id = parts.TIDB_PARTITION_ID WHERE parts.TABLE_SCHEMA = '${DB}1' and parts.PARTITION_NAME = 'p1' and modify_count = 0;" | awk '/count/{print $2}')
+if [ "p1_count" -ne 4 ]; then
+    echo "partition p1 stats meta count does not equal to 4, but $p1_count instead"
     exit 1
 fi
