0203fa45b21aee0a15d50848db9814cef7217609
ris <79858083+RidRisR@users.noreply.github.com>
2025-04-14 12:47:07 +0800

    br: Pre-alloc all table ids for restore (#60343)
    close pingcap/tidb#60345

--- br/pkg/restore/snap_client/export_test.go
+++ br/pkg/restore/snap_client/export_test.go
@@ -86,6 +86,7 @@ func (rc *SnapClient) CreateTablesTest(
 	for i, t := range tables {
 		tbMapping[t.Info.Name.String()] = i
 	}
+	rc.AllocTableIDs(context.Background(), tables)
 	createdTables, err := rc.CreateTables(context.TODO(), tables, newTS)
 	if err != nil {
 		return nil, nil, err
