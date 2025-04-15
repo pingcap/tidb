df1c39f8107845066ca874e3353580c69c2f08e6
xufei <xufei@pingcap.com>
2025-04-15 09:23:07 +0800

    Update physical_property.go

--- pkg/planner/property/physical_property.go
+++ pkg/planner/property/physical_property.go
@@ -93,8 +93,6 @@ type MPPPartitionColumn struct {
 	CollateID int32
 }
 
-<<<<<<< HEAD
-=======
 // ResolveIndices resolve index for MPPPartitionColumn
 func (partitionCol *MPPPartitionColumn) ResolveIndices(schema *expression.Schema) (*MPPPartitionColumn, error) {
 	newColExpr, err := partitionCol.Col.ResolveIndices(schema)
@@ -108,15 +106,6 @@ func (partitionCol *MPPPartitionColumn) ResolveIndices(schema *expression.Schema
 	}, nil
 }
 
-// Clone makes a copy of MPPPartitionColumn.
-func (partitionCol *MPPPartitionColumn) Clone() *MPPPartitionColumn {
-	return &MPPPartitionColumn{
-		Col:       partitionCol.Col.Clone().(*expression.Column),
-		CollateID: partitionCol.CollateID,
-	}
-}
-
->>>>>>> 43d47e4e1d7 (planner: fix bug in `PhysicalExchangeSender::ResolveIndicesItself` (#60520))
 func (partitionCol *MPPPartitionColumn) hashCode() []byte {
 	hashcode := partitionCol.Col.HashCode()
 	if partitionCol.CollateID < 0 {
