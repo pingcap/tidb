f17f76067bb6a9b1d7c7b2b52e186c63b2e0edf1
Jianjun Liao <jianjun.liao@outlook.com>
2025-04-16 12:06:47 +0800

    batch save stats meta
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- pkg/statistics/handle/storage/save.go
+++ pkg/statistics/handle/storage/save.go
@@ -28,6 +28,7 @@ import (
 	"github.com/pingcap/tidb/pkg/sessionctx"
 	"github.com/pingcap/tidb/pkg/statistics"
 	statslogutil "github.com/pingcap/tidb/pkg/statistics/handle/logutil"
+	statstypes "github.com/pingcap/tidb/pkg/statistics/handle/types"
 	"github.com/pingcap/tidb/pkg/statistics/handle/util"
 	"github.com/pingcap/tidb/pkg/table"
 	"github.com/pingcap/tidb/pkg/types"
@@ -380,22 +381,32 @@ func SaveColOrIdxStatsToStorage(
 	return
 }
 
-// SaveMetaToStorage will save stats_meta to storage and update last stats histograms version.
-func SaveMetaToStorage(
+// SaveMetasToStorage will batch save stats_meta to storage.
+func SaveMetasToStorage(
 	sctx sessionctx.Context,
-	tableID, count, modifyCount int64,
 	refreshLastHistVer bool,
-) (statsVer uint64, err error) {
+	metaUpdates []statstypes.MetaUpdate,
+) (err error) {
 	version, err := util.GetStartTS(sctx)
 	if err != nil {
-		return 0, errors.Trace(err)
+		return errors.Trace(err)
 	}
+	var sql string
+	values := make([]string, 0, len(metaUpdates))
 	if refreshLastHistVer {
-		_, err = util.Exec(sctx, "replace into mysql.stats_meta (version, table_id, count, modify_count, last_stats_histograms_version) values (%?, %?, %?, %?, %?)", version, tableID, count, modifyCount, version)
+		for _, metaUpdate := range metaUpdates {
+			values = append(values, fmt.Sprintf("(%d, %d, %d, %d, %d)", version, metaUpdate.PhysicalID, metaUpdate.Count, metaUpdate.ModifyCount, version))
+		}
+		sql = fmt.Sprintf("insert into mysql.stats_meta (version, table_id, count, modify_count, last_stats_histograms_version) values %s "+
+			"on duplicate key update version = values(version), modify_count = values(modify_count), count = values(count), last_stats_histograms_version = values(last_stats_histograms_version)", strings.Join(values, ","))
 	} else {
-		_, err = util.Exec(sctx, "replace into mysql.stats_meta (version, table_id, count, modify_count) values (%?, %?, %?, %?)", version, tableID, count, modifyCount)
+		for _, metaUpdate := range metaUpdates {
+			values = append(values, fmt.Sprintf("(%d, %d, %d, %d)", version, metaUpdate.PhysicalID, metaUpdate.Count, metaUpdate.ModifyCount))
+		}
+		sql = fmt.Sprintf("insert into mysql.stats_meta (version, table_id, count, modify_count) values %s "+
+			"on duplicate key update version = values(version), modify_count = values(modify_count), count = values(count)", strings.Join(values, ","))
 	}
-	statsVer = version
+	_, err = util.Exec(sctx, sql)
 	return
 }
 
--- pkg/statistics/handle/types/interfaces.go
+++ pkg/statistics/handle/types/interfaces.go
@@ -324,6 +324,13 @@ type PartitionStatisticLoadTask struct {
 // PersistFunc is used to persist JSONTable in the partition level.
 type PersistFunc func(ctx context.Context, jsonTable *statsutil.JSONTable, physicalID int64) error
 
+// MetaUpdate records a partition-level meta update.
+type MetaUpdate struct {
+	PhysicalID  int64
+	Count       int64
+	ModifyCount int64
+}
+
 // StatsReadWriter is used to read and write stats to the storage.
 // TODO: merge and remove some methods.
 type StatsReadWriter interface {
@@ -349,10 +356,10 @@ type StatsReadWriter interface {
 	// SaveAnalyzeResultToStorage saves the analyze result to the storage.
 	SaveAnalyzeResultToStorage(results *statistics.AnalyzeResults, analyzeSnapshot bool, source string) (err error)
 
-	// SaveMetaToStorage saves the stats meta of a table to storage.
+	// SaveMetasToStorage saves the stats meta of a table to storage.
 	// Use the param `refreshLastHistVer` to indicate whether we need to update the last_histograms_versions in stats_meta table.
 	// Set it to true if the column/index stats is updated.
-	SaveMetaToStorage(tableID, count, modifyCount int64, source string, needRefreshLastHistVer bool) (err error)
+	SaveMetasToStorage(needRefreshLastHistVer bool, metaUpdates ...MetaUpdate) (err error)
 
 	// UpdateStatsMetaVersionForGC updates the version of mysql.stats_meta,
 	// ensuring it is greater than the last garbage collection (GC) time.
