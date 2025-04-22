9f4a8a4ea63a7a8a245ac63947a5e72cdfd5e6a0
Weizhen Wang <wangweizhen@pingcap.com>
2025-04-22 17:37:47 +0800

    collate: latin1_bin has the same collate with utf8mb4_bin,utf8_bin
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/optimizer.go
+++ pkg/planner/core/optimizer.go
@@ -343,9 +343,6 @@ func CascadesOptimize(ctx context.Context, sctx base.PlanContext, flag uint64, l
 func VolcanoOptimize(ctx context.Context, sctx base.PlanContext, flag uint64, logic base.LogicalPlan) (base.LogicalPlan, base.PhysicalPlan, float64, error) {
 	sessVars := sctx.GetSessionVars()
 	flag = adjustOptimizationFlags(flag, logic)
-	if !sctx.GetSessionVars().InRestrictedSQL {
-		fmt.Println("wwz")
-	}
 	logic, err := logicalOptimize(ctx, flag, logic)
 	if err != nil {
 		return nil, nil, 0, err
