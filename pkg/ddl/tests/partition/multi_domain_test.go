d42a2196ad642da5bc01f44a3fc4dade7a11eba6
Mattias Jonsson <mattias.jonsson@pingcap.com>
2025-04-29 00:27:24 +0200

    Updated test to accept more index errors

--- pkg/ddl/tests/partition/multi_domain_test.go
+++ pkg/ddl/tests/partition/multi_domain_test.go
@@ -247,7 +247,7 @@ func TestMultiSchemaDropListColumnsDefaultPartition(t *testing.T) {
 			tkO.MustExec(`insert into t values (3,3,3)`)
 			tkO.MustContainErrMsg(`insert into t values (1,1,2)`, "[kv:1062]Duplicate entry '1' for key 't.a_2")
 			tkNO.MustContainErrMsg(`insert into t values (3,3,3)`, "[table:1526]Table has no partition for value matching a partition being dropped, 'p0'")
-			tkO.MustContainErrMsg(`insert into t values (101,101,101)`, "[kv:1062]Duplicate entry '101' for key 't.a_2'")
+			tkO.MustContainErrMsg(`insert into t values (101,101,101)`, "[kv:1062]Duplicate entry '101")
 			tkNO.MustContainErrMsg(`insert into t values (101,101,101)`, "[kv:1062]Duplicate entry '101")
 			tkNO.MustQuery(`select * from t`).Sort().Check(testkit.Rows("101 101 101", "102 102 102", "3 3 3"))
 			tkO.MustQuery(`select * from t`).Sort().Check(testkit.Rows("101 101 101", "102 102 102", "3 3 3"))
