746e6dd2a587ed7e8b0f75760e903b1484c7a4c1
cbcwestwolf <1004626265@qq.com>
2025-05-06 14:52:11 +0800

    manually pick https://github.com/pingcap/tidb/pull/60950

--- pkg/lightning/backend/external/iter.go
+++ pkg/lightning/backend/external/iter.go
@@ -467,6 +467,10 @@ func (p *kvPair) len() int {
 	return len(p.key) + len(p.value)
 }
 
+func getPairKey(p *kvPair) []byte {
+	return p.key
+}
+
 type kvReaderProxy struct {
 	p string
 	r *kvReader
