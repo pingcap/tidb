940c95026f3c181dff3ecf405008728dafe19f07
Weizhen Wang <wangweizhen@pingcap.com>
2025-05-07 09:11:05 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/executor/infoschema_reader_test.go
+++ pkg/executor/infoschema_reader_test.go
@@ -982,7 +982,7 @@ func TestInfoSchemaDDLJobs(t *testing.T) {
 	))
 
 	// Test explain output, since the output may change in future.
-	tk.MustQuery(`EXPLAIN FORMAT='brieffail' SELECT * FROM information_schema.ddl_jobs where db_name = "test2" limit 10;`).Check(testkit.Rows(
+	tk.MustQuery(`EXPLAIN FORMAT='brief' SELECT * FROM information_schema.ddl_jobs where db_name = "test2" limit 10;`).Check(testkit.Rows(
 		`Limit 10.00 root  offset:0, count:10`,
 		`└─Selection 10.00 root  eq(Column#2, "test2")`,
 		`  └─MemTableScan 10000.00 root table:DDL_JOBS db_name:["test2"]`,
