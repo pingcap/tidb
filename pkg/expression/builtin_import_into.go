c11c3a9fde807d0753e142b388af07fe32eb59ea
joccau <zak.zhao@pingcap.cn>
2025-05-08 19:51:36 +0800

    get data filepath with table
    Signed-off-by: joccau <zak.zhao@pingcap.cn>

--- pkg/expression/builtin_import_into.go
+++ pkg/expression/builtin_import_into.go
@@ -19,8 +19,17 @@
 package expression
 
 import (
+	"context"
+	"path/filepath"
+	"strings"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tidb/br/pkg/storage"
+	"github.com/pingcap/tidb/pkg/lightning/log"
+	"github.com/pingcap/tidb/pkg/lightning/mydump"
 	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/util/chunk"
+	"github.com/pingcap/tidb/pkg/util/dbutil"
 )
 
 var (
@@ -57,21 +66,59 @@ func (b *builtinFileRouteSig) Clone() builtinFunc {
 
 func (b *builtinFileRouteSig) evalJSON(ctx EvalContext, row chunk.Row) (res types.BinaryJSON, isNull bool, err error) {
 	// get the path from the args[0]
-	dir, isNULL, err := b.args[0].EvalString(ctx, row)
+	soureURL, isNULL, err := b.args[0].EvalString(ctx, row)
 	if err != nil || isNULL {
 		return res, true, err
 	}
 
-	// find table and files from dir
-	if len(dir) == 0 {
-		return res, true, nil
+	// find the map: table -> data files path
+	tableRouteFiles, err := getDatafilePath(soureURL)
+	if err != nil {
+		return res, true, errors.Trace(err)
 	}
 
 	// create binary json
-	tableRoutePath := make(map[string]string, 0)
-	bj, err := types.CreateBinaryJSONWithCheck(tableRoutePath)
+	bj, err := types.CreateBinaryJSONWithCheck(tableRouteFiles)
 	if err != nil {
 		return res, true, err
 	}
 	return bj, false, nil
+}
+
+func getDatafilePath(soureURL string) (map[string]string, error) {
+	ctx := context.TODO()
+
+	u, err := storage.ParseBackend(soureURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := storage.New(ctx, u, &storage.ExternalStorageOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	fileRouter, err := mydump.NewDefaultFileRouter(log.FromContext(ctx))
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	tableRouteFiles := make(map[string]string, 0)
+	fileIter := mydump.NewAllFileIterator(s, 0)
+	err = fileIter.IterateFiles(ctx, func(_ context.Context, path string, size int64) error {
+		// only handle the file about SQL for creating table.
+		if !strings.HasSuffix(path, "-schema.sql") {
+			return nil
+		}
+
+		res, err := fileRouter.Route(filepath.ToSlash(path))
+		if err != nil {
+			return err
+		}
+
+		tableName := dbutil.TableName(res.Schema, res.Name)
+		tableRouteFiles[tableName] = strings.ReplaceAll(path, "-schema.sql", ".*")
+		return nil
+	})
+	return tableRouteFiles, nil
 }
