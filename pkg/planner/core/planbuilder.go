054225f7f8ab102b21497c8fb680262652a9f7d5
qw4990 <zhangyuanjia@pingcap.com>
2025-05-08 14:36:08 -0700

    use_index to support tiflash

--- pkg/planner/core/planbuilder.go
+++ pkg/planner/core/planbuilder.go
@@ -1094,6 +1094,9 @@ func getPathByIndexName(paths []*util.AccessPath, idxName ast.CIStr, tblInfo *mo
 	var indexPrefixPath *util.AccessPath
 	prefixMatches := 0
 	for _, path := range paths {
+		if path.IsTiFlashSimpleTablePath() && idxName.L == "tiflash" {
+			return path
+		}
 		// Only accept tikv's primary key table path.
 		if path.IsTiKVTablePath() && isPrimaryIndex(idxName) && tblInfo.HasClusteredIndex() {
 			return path
@@ -1363,6 +1366,12 @@ func getPossibleAccessPaths(ctx base.PlanContext, tableHints *hint.PlanHints, in
 	}
 	if allMVIIndexPath {
 		available = append(available, tablePath)
+	}
+
+	if strings.Contains(ctx.GetSessionVars().StmtCtx.OriginalSQL, "txx") {
+		for _, p := range available {
+			fmt.Println(">>> ", p.IsTablePath(), p.StoreType, *p)
+		}
 	}
 
 	return available, nil
