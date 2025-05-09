5ae458a231c381503fb0eb4d543a96e88ffeab15
Jianjun Liao <jianjun.liao@outlook.com>
2025-05-09 13:51:36 +0800

    merge auto analyze and load stats
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- br/pkg/restore/snap_client/pipeline_items.go
+++ br/pkg/restore/snap_client/pipeline_items.go
@@ -90,7 +90,6 @@ type PipelineContext struct {
 	LogProgress         bool
 	ChecksumConcurrency uint
 	StatsConcurrency    uint
-	AutoAnalyze         bool
 
 	// pipeline item tool client
 	KvClient   kv.Client
@@ -108,7 +107,9 @@ func (rc *SnapClient) RestorePipeline(ctx context.Context, plCtx PipelineContext
 	if plCtx.Checksum {
 		progressLen += int64(len(createdTables))
 	}
-	progressLen += int64(len(createdTables)) // for pipeline item - update stats meta
+	if plCtx.LoadStats {
+		progressLen += int64(len(createdTables))
+	}
 	if plCtx.WaitTiflashReady {
 		progressLen += int64(len(createdTables))
 	}
@@ -124,7 +125,9 @@ func (rc *SnapClient) RestorePipeline(ctx context.Context, plCtx PipelineContext
 	}
 
 	// pipeline update meta and load stats
-	rc.registerUpdateMetaAndLoadStats(handlerBuilder, plCtx.ExtStorage, updateCh, plCtx.StatsConcurrency, plCtx.AutoAnalyze, plCtx.LoadStats)
+	if plCtx.LoadStats {
+		rc.registerUpdateMetaAndLoadStats(handlerBuilder, plCtx.ExtStorage, updateCh, plCtx.StatsConcurrency)
+	}
 
 	// pipeline wait Tiflash synced
 	if plCtx.WaitTiflashReady {
@@ -367,8 +370,6 @@ func (rc *SnapClient) registerUpdateMetaAndLoadStats(
 	s storage.ExternalStorage,
 	updateCh glue.Progress,
 	statsConcurrency uint,
-	autoAnalyze bool,
-	loadStats bool,
 ) {
 	statsHandler := rc.dom.StatsHandle()
 	buffer := NewStatsMetaItemBuffer()
@@ -376,7 +377,7 @@ func (rc *SnapClient) registerUpdateMetaAndLoadStats(
 	builder.RegisterPipelineTask("Update Stats", statsConcurrency, func(c context.Context, tbl *CreatedTable) error {
 		oldTable := tbl.OldTable
 		var statsErr error = nil
-		if loadStats && oldTable.Stats != nil {
+		if oldTable.Stats != nil {
 			log.Info("start loads analyze after validate checksum",
 				zap.Int64("old id", oldTable.Info.ID),
 				zap.Int64("new id", tbl.Table.ID),
@@ -390,7 +391,7 @@ func (rc *SnapClient) registerUpdateMetaAndLoadStats(
 				zap.Stringer("table", oldTable.Info.Name),
 				zap.Stringer("db", oldTable.DB.Name),
 				zap.Duration("cost", time.Since(start)))
-		} else if loadStats && len(oldTable.StatsFileIndexes) > 0 {
+		} else if len(oldTable.StatsFileIndexes) > 0 {
 			log.Info("start to load statistic data for each partition",
 				zap.Int64("old id", oldTable.Info.ID),
 				zap.Int64("new id", tbl.Table.ID),
@@ -406,7 +407,7 @@ func (rc *SnapClient) registerUpdateMetaAndLoadStats(
 				zap.Duration("cost", time.Since(start)))
 		}
 
-		if autoAnalyze && (statsErr != nil || !loadStats || (oldTable.Stats == nil && len(oldTable.StatsFileIndexes) == 0)) {
+		if statsErr != nil || (oldTable.Stats == nil && len(oldTable.StatsFileIndexes) == 0) {
 			// Not need to return err when failed because of update analysis-meta
 			log.Info("start update metas", zap.Stringer("table", oldTable.Info.Name), zap.Stringer("db", oldTable.DB.Name))
 			// get the the number of rows of each partition
