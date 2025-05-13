78c20a04ac04275b299512a93c0381d08fb85d71
Jianjun Liao <36503113+Leavrth@users.noreply.github.com>
2025-05-13 17:44:30 +0800

    br: adjust auto analyze (#60994)
    ref pingcap/tidb#60374

--- br/tests/br_stats/run.sh
+++ br/tests/br_stats/run.sh
@@ -45,6 +45,8 @@ if [ "${dump_mark}" -ne "123" ]; then
     exit 1
 fi
 
+run_br --pd $PD_ADDR backup full -s "local://$TEST_DIR/${DB}2" --log-file $LOG --ignore-stats=true --filter "${DB}1.*"
+
 for i in $(seq $DB_COUNT); do
     run_sql "DROP DATABASE $DB${i};"
 done
@@ -62,31 +64,11 @@ if [ "${load_mark}" -ne "22" ]; then
     exit 1
 fi
 
+# test load stats is true but statistic data is not backed up
 run_sql "DROP DATABASE ${DB}1;"
 
 rm -f $LOG
-run_br --pd $PD_ADDR restore full -s "local://$TEST_DIR/$DB" --log-file $LOG --load-stats=false --filter "${DB}1.*" || cat $LOG
-table_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.TABLES tables ON meta.table_id = tables.TIDB_TABLE_ID WHERE tables.TABLE_SCHEMA = '${DB}1' and modify_count = 0;" | awk '/count/{print $2}')
-if [ "${table_count}" -ne 9 ]; then
-    echo "table stats meta count does not equal to 9, but $count instead"
-    exit 1
-fi
-p0_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.PARTITIONS parts ON meta.table_id = parts.TIDB_PARTITION_ID WHERE parts.TABLE_SCHEMA = '${DB}1' and parts.PARTITION_NAME = 'p0' and modify_count = 0;" | awk '/count/{print $2}')
-if [ "${p0_count}" -ne 5 ]; then
-    echo "partition p0 stats meta count does not equal to 5, but $p0_count instead"
-    exit 1
-fi
-p1_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.PARTITIONS parts ON meta.table_id = parts.TIDB_PARTITION_ID WHERE parts.TABLE_SCHEMA = '${DB}1' and parts.PARTITION_NAME = 'p1' and modify_count = 0;" | awk '/count/{print $2}')
-if [ "${p1_count}" -ne 4 ]; then
-    echo "partition p1 stats meta count does not equal to 4, but $p1_count instead"
-    exit 1
-fi
-
-# test auto analyze
-run_sql "DROP DATABASE ${DB}1;"
-
-rm -f $LOG
-run_br --pd $PD_ADDR restore full -s "local://$TEST_DIR/$DB" --log-file $LOG --load-stats=false --filter "${DB}1.*" --auto-analyze || cat $LOG
+run_br --pd $PD_ADDR restore full -s "local://$TEST_DIR/${DB}2" --log-file $LOG --load-stats=true --filter "${DB}1.br_stats_partition" || cat $LOG
 table_count=$(run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.TABLES tables ON meta.table_id = tables.TIDB_TABLE_ID WHERE tables.TABLE_SCHEMA = '${DB}1' and modify_count = count and count > 0;" | awk '/count/{print $2}')
 if [ "${table_count}" -ne 9 ]; then
     echo "table stats meta count does not equal to 9, but $count instead"
@@ -102,3 +84,14 @@ if [ "${p1_count}" -ne 4 ]; then
     echo "partition p1 stats meta count does not equal to 4, but $p1_count instead"
     exit 1
 fi
+
+# test load stats is false
+run_sql "DROP DATABASE ${DB}1;"
+
+rm -f $LOG
+run_br --pd $PD_ADDR restore full -s "local://$TEST_DIR/$DB" --log-file $LOG --load-stats=false --filter "${DB}1.br_stats_partition" || cat $LOG
+run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.TABLES tables ON meta.table_id = tables.TIDB_TABLE_ID WHERE tables.TABLE_SCHEMA = '${DB}1' and modify_count = count and count > 0;"
+check_not_contains "1. row"
+
+run_sql "SELECT meta.count as count FROM mysql.stats_meta meta JOIN INFORMATION_SCHEMA.PARTITIONS parts ON meta.table_id = parts.TIDB_PARTITION_ID WHERE parts.TABLE_SCHEMA = '${DB}1' and modify_count = count and count > 0;"
+check_not_contains "1. row"
