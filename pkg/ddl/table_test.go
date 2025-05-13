b2a7b3165f81fbbd78e88cbf06d9e430c907c9bb
Jiaqiang Huang <96465211+River2000i@users.noreply.github.com>
2025-05-13 17:10:07 +0800

    add test for drop table

--- pkg/ddl/table_test.go
+++ pkg/ddl/table_test.go
@@ -866,14 +866,21 @@ func TestRefreshMetaBasic(t *testing.T) {
 	_, err = domain.InfoSchema().TableByName(context.Background(), ast.NewCIStr("test"), ast.NewCIStr("t2"))
 	require.NoError(t, err)
 
-	// drop table t3 by txn
 	tk.MustExec("create table t3(id int)")
 	require.True(t, ok)
 	t3TableInfo := getClonedTableInfoFromDomain(t, "test", "t3", domain)
 	txn, err := store.Begin()
 	require.NoError(t, err)
+	// drop table t3 by txn
 	err = meta.NewMutator(txn).DropTableOrView(dbInfo.ID, t3TableInfo.ID)
 	require.NoError(t, err)
+	txn.Commit(context.Background())
+	txn, err = store.Begin()
+	require.NoError(t, err)
+	newT3TableInfo, err := meta.NewMutator(txn).GetTable(dbInfo.ID, t3TableInfo.ID)
+	require.NoError(t, err)
+	require.Nil(t, newT3TableInfo)
+	// t3 table info store in infoschema, after refresh meta, t3 table info should be nil
 	_, ok = domain.InfoSchema().TableByID(context.Background(), t3TableInfo.ID)
 	require.True(t, ok)
 	testutil.RefreshMeta(sctx, t, de, dbInfo.ID, t3TableInfo.ID)
