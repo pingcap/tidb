9fbd12a9246d9972bde1937d7d2eb031ec9f9f36
qts <2100012999@stu.pku.edu.cn>
2025-05-23 15:02:15 +0800

    remove commented code

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -3040,8 +3040,6 @@ func (do *Domain) notifyUpdatePrivilege(event PrivilegeEvent) error {
 		if uint64(len(data)) > size.MB {
 			logutil.BgLogger().Warn("notify update privilege message too large", zap.ByteString("value", data))
 		}
-		//row := do.etcdClient.KV
-		//_, err = row.Put(do.ctx, privilegeKey, string(data))
 		err = ddlutil.PutKVToEtcd(do.ctx, do.etcdClient, ddlutil.KeyOpDefaultRetryCnt, privilegeKey, string(data))
 		if err != nil {
 			logutil.BgLogger().Warn("notify update privilege failed", zap.Error(err))
