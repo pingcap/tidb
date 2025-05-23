c62133891bedbfb55a397e39b3808494970c7f12
“EricZequan” <zequany33@gmail.com>
2025-05-23 15:41:54 +0800

    update
    Signed-off-by: “EricZequan” <zequany33@gmail.com>

--- tests/clusterintegrationtest/_include.sh
+++ tests/clusterintegrationtest/_include.sh
@@ -37,8 +37,6 @@ function start_tidb() {
   else
     tiup playground nightly --db=1 --kv=1 --tiflash=1 --db.binpath=../../bin/tidb-server --db.config=./config.toml &
   fi
-
-  sleep 30
 }
 
 function check_and_prepare_datasets() {
@@ -78,8 +76,6 @@ function start_tidb_fixed_version() {
 
   echo "Starting TiUP Playground in the background..."
   tiup playground v8.5.1 --db=1 --kv=1 --tiflash=1 --db.config=./config.toml &
-
-  sleep 30
 }
 
 function build_mysql_tester() {
