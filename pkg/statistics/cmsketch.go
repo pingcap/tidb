d7ca2e4e7f8f2005316bf14f07b85acfbb467f46
Weizhen Wang <wangweizhen@pingcap.com>
2025-05-27 22:57:47 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/cmsketch.go
+++ pkg/statistics/cmsketch.go
@@ -532,10 +532,19 @@ type TopN struct {
 // Scale scales the TopN by the given factor and return the total row count. It is only used when to build topN from sampled data during analyze.
 func (c *TopN) Scale(scaleFactor float64) uint64 {
 	var total uint64
-	for i := range c.TopN {
-		c.TopN[i].Count = uint64(float64(c.TopN[i].Count) * scaleFactor)
-		total += c.TopN[i].Count
+	if len(c.TopN) == 0 {
+		return 0
 	}
+	c.once.Do(func() {
+		minCount := uint64(float64(c.TopN[0].Count) * scaleFactor)
+		for i := range c.TopN {
+			c.TopN[i].Count = uint64(float64(c.TopN[i].Count) * scaleFactor)
+			minCount = min(minCount, c.TopN[i].Count)
+			total += c.TopN[i].Count
+		}
+		c.minCount = minCount
+		c.totalCount = total
+	})
 	return total
 }
 
