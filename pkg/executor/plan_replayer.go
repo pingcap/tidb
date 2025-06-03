7aa0067644ddcaa80c858f1c9b058eebb4507d01
Weizhen Wang <wangweizhen@pingcap.com>
2025-06-03 17:44:26 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/executor/plan_replayer.go
+++ pkg/executor/plan_replayer.go
@@ -455,8 +455,11 @@ func loadStats(ctx sessionctx.Context, f *zip.File) error {
 	buf := new(bytes.Buffer)
 	_, err = buf.ReadFrom(r)
 	if err != nil {
-
-		ctx.GetSessionVars().StmtCtx.AppendWarning(errors.Join(fmt.Errorf("fail to read stats file %s", f.Name), err))
+		if f == nil || f.Name == "" {
+			ctx.GetSessionVars().StmtCtx.AppendWarning(errors.Join(fmt.Errorf("fail to read stats file %s", f.Name), err))
+			return nil
+		}
+		ctx.GetSessionVars().StmtCtx.AppendWarning(errors.Join(errors.New("fail to read stats file"), err))
 		return nil
 	}
 	if err := json.Unmarshal(buf.Bytes(), jsonTbl); err != nil {
