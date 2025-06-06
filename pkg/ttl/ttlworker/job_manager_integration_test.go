70e7f5941e0073a79839668bfb023d17b3b7b2a6
YangKeao <yangkeao@chunibyo.icu>
2025-06-06 16:10:56 +0800

    This is an automated cherry-pick of #61518
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/ttl/ttlworker/job_manager_integration_test.go
+++ pkg/ttl/ttlworker/job_manager_integration_test.go
@@ -1763,3 +1763,39 @@ func TestIterationOfRunningJob(t *testing.T) {
 	// Now all the jobs should have been removed
 	require.Len(t, m.RunningJobs(), 0)
 }
+
+func TestTTLSummaryForTimeoutJob(t *testing.T) {
+	store, dom := testkit.CreateMockStoreAndDomain(t)
+	waitAndStopTTLManager(t, dom)
+	sessionFactory := sessionFactory(t, dom)
+
+	tk := testkit.NewTestKit(t, store)
+	m := ttlworker.NewJobManager("test-job-manager", dom.AdvancedSysSessionPool(), store, nil, func() bool { return true })
+
+	se, closeSe := sessionFactory()
+	defer closeSe()
+
+	testTable := &cache.PhysicalTable{ID: 1, TableInfo: &model.TableInfo{ID: 1, TTLInfo: &model.TTLInfo{IntervalExprStr: "1", IntervalTimeUnit: int(ast.TimeUnitDay), JobInterval: "1h"}}}
+	m.InfoSchemaCache().Tables[testTable.ID] = testTable
+	jobID := uuid.NewString()
+	_, err := m.LockJob(context.Background(), se, testTable, se.Now(), jobID, false)
+	require.NoError(t, err)
+	tk.MustQuery("SELECT current_job_id, current_job_owner_id FROM mysql.tidb_ttl_table_status WHERE table_id = ?", 1).Check(testkit.Rows(fmt.Sprintf("%s %s", jobID, m.ID())))
+
+	// insert some finished task
+	tk.MustExec("INSERT INTO mysql.tidb_ttl_task (scan_id, job_id, table_id, status, state, expire_time, created_time)"+
+		" VALUES (1, ?, 1, 'finished', ?, NOW(), NOW())", jobID, `{"total_rows": 100 ,"success_rows": 100}`)
+
+	// report timeout
+	err = m.UpdateHeartBeatForJob(context.Background(), se, se.Now().Add(8*time.Hour), m.RunningJobs()[0])
+	require.NoError(t, err)
+
+	// the job should contain summary
+	rows := tk.MustQuery("SELECT last_job_summary FROM mysql.tidb_ttl_table_status WHERE table_id = ?", 1).Rows()
+	summary := &ttlworker.TTLSummary{}
+	require.NoError(t, json.Unmarshal([]byte(rows[0][0].(string)), summary))
+	require.Equal(t, uint64(100), summary.TotalRows)
+	require.Equal(t, uint64(100), summary.SuccessRows)
+	require.Equal(t, uint64(0), summary.ErrorRows)
+	require.Equal(t, "job is timeout", summary.ScanTaskErr)
+}
