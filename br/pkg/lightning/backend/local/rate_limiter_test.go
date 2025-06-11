b138f37ee6f15f330dec37627d6fac97e48eea0a
tangenta <tangenta@126.com>
2025-06-11 12:24:10 +0800

    add test TestIngestLimiterBurst
    Signed-off-by: tangenta <tangenta@126.com>

--- br/pkg/lightning/backend/local/rate_limiter_test.go
+++ br/pkg/lightning/backend/local/rate_limiter_test.go
@@ -16,6 +16,7 @@ package local
 
 import (
 	"context"
+	"math"
 	"testing"
 	"time"
 
@@ -131,4 +132,25 @@ func TestContextCancelDuringRateWait(t *testing.T) {
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("timed out waiting for error")
 	}
+}
+
+func TestIngestLimiterBurst(t *testing.T) {
+	ctx := context.Background()
+	l := newIngestLimiter(ctx, 0, 0)
+	require.Equal(t, math.MaxInt, l.Burst())
+
+	l = newIngestLimiter(ctx, 0, 1000)
+	require.Equal(t, 1000, l.Burst())
+
+	l = newIngestLimiter(ctx, 1000, 0)
+	require.Equal(t, 1000, l.Burst())
+
+	l = newIngestLimiter(ctx, 1000, 1000)
+	require.Equal(t, 1000, l.Burst())
+
+	l = newIngestLimiter(ctx, 1000, 567)
+	require.Equal(t, 567, l.Burst())
+
+	l = newIngestLimiter(ctx, 567, 1000)
+	require.Equal(t, 567, l.Burst())
 }
