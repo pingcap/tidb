8b9880bada9822bf745e4d759b413bd514efc030
Weizhen Wang <wangweizhen@pingcap.com>
2025-06-13 18:23:39 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/expression/constant.go
+++ pkg/expression/constant.go
@@ -197,7 +197,6 @@ func (c *Constant) StringWithCtx(ctx ParamValues, redact string) string {
 // Clone implements Expression interface.
 func (c *Constant) Clone() Expression {
 	con := *c
-	con.Value = *c.Value.Clone()
 	con.RetType = c.RetType.Clone()
 	if c.ParamMarker != nil {
 		con.ParamMarker = &ParamMarker{order: c.ParamMarker.order}
