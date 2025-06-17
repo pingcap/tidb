8ab31e4a60092fa3b34d380d4313d6d9ae8dca61
cbcwestwolf <1004626265@qq.com>
2025-06-17 16:47:28 +0800

    infoschema: introduces privilege view in information_schema

--- pkg/executor/builder.go
+++ pkg/executor/builder.go
@@ -2279,7 +2279,10 @@ func (b *executorBuilder) buildMemTable(v *plannercore.PhysicalMemTable) exec.Ex
 			strings.ToLower(infoschema.TableTiDBCheckConstraints),
 			strings.ToLower(infoschema.TableKeywords),
 			strings.ToLower(infoschema.TableTiDBIndexUsage),
-			strings.ToLower(infoschema.ClusterTableTiDBIndexUsage):
+			strings.ToLower(infoschema.ClusterTableTiDBIndexUsage),
+			strings.ToLower(infoschema.TableColumnPrivileges),
+			strings.ToLower(infoschema.TableTablePrivileges),
+			strings.ToLower(infoschema.TableSchemaPrivileges):
 			memTracker := memory.NewTracker(v.ID(), -1)
 			memTracker.AttachTo(b.ctx.GetSessionVars().StmtCtx.MemTracker)
 			return &MemTableReaderExec{
