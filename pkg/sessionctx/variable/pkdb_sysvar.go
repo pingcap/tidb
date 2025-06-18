3b4fa050229960a2d1c77116f863cf8be24e8bb5
tianyuan <tianyuan@pingcap.cn>
2025-06-18 09:52:18 +0800

    add a session var to enable PD IPC (#70)
    Co-authored-by: Zhou Fan <zhou.fan@pingcap.cn>
    Co-authored-by: crazycs520 <chenshuang@pingcap.cn>
    Reviewed-on: https://git.pingcap.net/pingkai/tidb/pulls/70
    Reviewed-by: wink.yao <wink.yao@pingcap.cn>
    Co-authored-by: tianyuan <tianyuan@pingcap.cn>
    Co-committed-by: tianyuan <tianyuan@pingcap.cn>

--- pkg/sessionctx/variable/pkdb_sysvar.go
+++ pkg/sessionctx/variable/pkdb_sysvar.go
@@ -5,7 +5,9 @@ package variable
 
 import (
 	"context"
+	"sync/atomic"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/tikv/client-go/v2/tikvrpc"
 	"go.uber.org/zap"
@@ -14,17 +16,19 @@ import (
 // FFI-dependent system variables
 var ffiSysVars = []*SysVar{
 	{Scope: ScopeGlobal, Name: TiDBXEnableTiKVLocalCall, Value: BoolToOnOff(DefTiDBXEnableLocalRPCOpt), Type: TypeBool,
-		SetGlobal: func(_ context.Context, vars *SessionVars, s string) error {
+		SetGlobal: func(_ context.Context, _ *SessionVars, s string) error {
 			if TiDBOptOn(s) != tikvrpc.EnableTiKVLocalCall.Load() {
 				tikvrpc.EnableTiKVLocalCall.Store(TiDBOptOn(s))
-				logutil.BgLogger().Info("set enable local rpc opt", zap.Bool("enable", TiDBOptOn(s)))
+				logutil.BgLogger().Info("set enable local rpc opt",
+					zap.String("variable", TiDBXEnableTiKVLocalCall),
+					zap.Bool("enable", TiDBOptOn(s)))
 			}
 			return nil
-		}, GetGlobal: func(_ context.Context, vars *SessionVars) (string, error) {
-			return BoolToOnOff(tikvrpc.EnableTiKVLocalCall.Load()), nil
-		}},
+		}, GetGlobal: func(_ context.Context, _ *SessionVars) (string, error) {
+		return BoolToOnOff(tikvrpc.EnableTiKVLocalCall.Load()), nil
+	}},
 	{Scope: ScopeGlobal, Name: TiDBXEnableScheduleLeaderRule, Value: BoolToOnOff(DefTiDBXEnableScheduleLeaderRule), Type: TypeBool,
-		SetGlobal: func(_ context.Context, vars *SessionVars, s string) error {
+		SetGlobal: func(_ context.Context, _ *SessionVars, s string) error {
 			v := TiDBOptOn(s)
 			if v != EnableScheduleLeaderRule.Load() {
 				EnableScheduleLeaderRule.Store(v)
@@ -33,10 +37,33 @@ var ffiSysVars = []*SysVar{
 				}
 			}
 			return nil
-		}, GetGlobal: func(_ context.Context, vars *SessionVars) (string, error) {
-			return BoolToOnOff(EnableScheduleLeaderRule.Load()), nil
-		}},
+		}, GetGlobal: func(context.Context, *SessionVars) (string, error) {
+		return BoolToOnOff(EnableScheduleLeaderRule.Load()), nil
+	}},
+	{Scope: ScopeGlobal, Name: TiDBXEnablePDFusion, Value: BoolToOnOff(DefTiDBXEnableLocalRPCOpt), Type: TypeBool,
+		SetGlobal: func(_ context.Context, _ *SessionVars, s string) error {
+			if PDFusionVar == nil {
+				return errors.Errorf("%s is not initialized. Please check if this is fusion mode", TiDBXEnablePDFusion)
+			}
+			if TiDBOptOn(s) != (*PDFusionVar).Load() {
+				(*PDFusionVar).Store(TiDBOptOn(s))
+				logutil.BgLogger().Info("set enable local rpc opt",
+					zap.String("variable", TiDBXEnablePDFusion),
+					zap.Bool("enable", TiDBOptOn(s)))
+			}
+			return nil
+		}, GetGlobal: func(context.Context, *SessionVars) (string, error) {
+		if PDFusionVar == nil {
+			return "", errors.Errorf("%s is not initialized. Please check if this is fusion mode", TiDBXEnablePDFusion)
+		}
+		return BoolToOnOff((*PDFusionVar).Load()), nil
+	}},
 }
+
+// PDFusionVar will be set by the upper package tidbx-server to point to pd-server's
+// EnableIPC. This is to break the dependency cycle between tidbx-server and
+// pd-server.
+var PDFusionVar *atomic.Bool
 
 func init() {
 	defaultSysVars = append(defaultSysVars, ffiSysVars...)
--- pkg/sessionctx/variable/pkdb_tidb_vars.go
+++ pkg/sessionctx/variable/pkdb_tidb_vars.go
@@ -2,16 +2,13 @@ package variable
 
 import "go.uber.org/atomic"
 
-// TiDB system variable names that only in session scope.
 const (
 	// TiDBXEnableScheduleLeaderRule indicates whether to enable region leader in one store.
 	TiDBXEnableScheduleLeaderRule = "tidbx_enable_schedule_leader_rule"
-)
-
-// TiDB system variable names that both in session and global scope.
-const (
 	// TiDBXEnableTiKVLocalCall indicates whether to enable TiKV local calls.
 	TiDBXEnableTiKVLocalCall = "tidbx_enable_tikv_local_call"
+	// TiDBXEnablePDFusion indicates whether to use Inter-Process Call for PD.
+	TiDBXEnablePDFusion = "tidbx_enable_pd_fusion"
 )
 
 // Default TiDB system variable values.
