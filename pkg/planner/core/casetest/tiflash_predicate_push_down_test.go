869010f45344fa7a2e2e1bb4b4ae04fc8941d00f
Weizhen Wang <wangweizhen@pingcap.com>
2025-06-30 11:44:13 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/casetest/tiflash_predicate_push_down_test.go
+++ pkg/planner/core/casetest/tiflash_predicate_push_down_test.go
@@ -41,6 +41,7 @@ func TestTiFlashLateMaterialization(t *testing.T) {
 		tk.MustExec("insert into t1(a,b,c,t) select a,b,c,t from t1;")
 	}
 	tk.MustExec("analyze table t1 all columns;")
+	tk.MustExec("set @@session.tidb_allow_tiflash_cop=ON")
 
 	// Create virtual `tiflash` replica info.
 	testkit.SetTiFlashReplica(t, dom, "test", "t1")
