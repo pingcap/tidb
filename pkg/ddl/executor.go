2ef73fe3476e4ef232d1347d6e1e4d6d21088bcf
River2000i <lzzziver@gmail.com>
2025-07-03 23:13:29 -0400

    revert 	involvingSchemaInfo

--- pkg/ddl/executor.go
+++ pkg/ddl/executor.go
@@ -5766,17 +5766,14 @@ func (e *executor) AlterTableMode(sctx sessionctx.Context, args *model.AlterTabl
 		return nil
 	}
 
-	involvingSchemaInfo := []model.InvolvingSchemaInfo{{Database: schema.Name.L, Table: table.Meta().Name.L}}
-
 	job := &model.Job{
-		Version:             model.JobVersion2,
-		SchemaID:            args.SchemaID,
-		TableID:             args.TableID,
-		Type:                model.ActionAlterTableMode,
-		BinlogInfo:          &model.HistoryInfo{},
-		InvolvingSchemaInfo: involvingSchemaInfo,
-		CDCWriteSource:      sctx.GetSessionVars().CDCWriteSource,
-		SQLMode:             sctx.GetSessionVars().SQLMode,
+		Version:        model.JobVersion2,
+		SchemaID:       args.SchemaID,
+		TableID:        args.TableID,
+		Type:           model.ActionAlterTableMode,
+		BinlogInfo:     &model.HistoryInfo{},
+		CDCWriteSource: sctx.GetSessionVars().CDCWriteSource,
+		SQLMode:        sctx.GetSessionVars().SQLMode,
 	}
 	sctx.SetValue(sessionctx.QueryString, "skip")
 	err := e.doDDLJob2(sctx, job, args)
