852c0a110107f195c869d934b45404c2e5570a0b
tiancaiamao <tiancaiamao@gmail.com>
2025-07-11 14:23:28 +0800

    resolve conflict

--- pkg/distsql/request_builder.go
+++ pkg/distsql/request_builder.go
@@ -28,12 +28,8 @@ import (
 	"github.com/pingcap/tidb/pkg/infoschema"
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
-<<<<<<< HEAD
 	"github.com/pingcap/tidb/pkg/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/pkg/sessionctx/variable"
-=======
-	"github.com/pingcap/tidb/pkg/sessionctx/vardef"
->>>>>>> a206b0b121a (distsql: do not change concurrency for keep order request when @@tidb_distsql_scan_concurrency is set (#60803))
 	"github.com/pingcap/tidb/pkg/tablecodec"
 	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/util/codec"
@@ -92,7 +88,7 @@ func (builder *RequestBuilder) Build() (*kv.Request, error) {
 	if dag := builder.dag; dag != nil {
 		if execCnt := len(dag.Executors); execCnt == 1 {
 			// select * from t order by id
-			if builder.Request.KeepOrder && builder.Request.Concurrency == vardef.DefDistSQLScanConcurrency {
+			if builder.Request.KeepOrder && builder.Request.Concurrency == variable.DefDistSQLScanConcurrency {
 				// When the DAG is just simple scan and keep order, set concurrency to 2.
 				// If a lot data are returned to client, mysql protocol is the bottleneck so concurrency 2 is enough.
 				// If very few data are returned to client, the speed is not optimal but good enough.
