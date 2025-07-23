c4b12fe27c3e566ad1463b64a0787e5c475a96e4
Weizhen Wang <wangweizhen@pingcap.com>
2025-07-23 13:38:09 +0800

    planner: move BasePhysicalJoin related logic into physical op dir
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/generator/plan_cache/plan_clone_generator.go
+++ pkg/planner/core/generator/plan_cache/plan_clone_generator.go
@@ -98,7 +98,7 @@ func genPlanCloneForPlanCache(x any) ([]byte, error) {
 		case "[]int", "[]byte", "[]float", "[]bool": // simple slice
 			c.write("cloned.%v = make(%v, len(op.%v))", f.Name, f.Type, f.Name)
 			c.write("copy(cloned.%v, op.%v)", f.Name, f.Name)
-		case "core.basePhysicalAgg", "core.BasePhysicalJoin":
+		case "core.basePhysicalAgg", "physicalop.BasePhysicalJoin":
 			fieldName := strings.Split(f.Type.String(), ".")[1]
 			c.write(`basePlan, baseOK := op.%v.CloneForPlanCacheWithSelf(newCtx, cloned)
 							if !baseOK {return nil, false}
