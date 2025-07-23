d9987dd6fcfa284785d9e00ad176b732faa27089
0xPoe <techregister@pm.me>
2025-07-23 12:21:01 +0200

    domain, statistics: update auto analyze time window handling to return formatted strings
    Signed-off-by: 0xPoe <techregister@pm.me>

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -482,8 +482,8 @@ func (do *Domain) CheckAutoAnalyzeWindows() {
 			statslogutil.StatsLogger().Warn("Kill auto analyze process because it exceeded the window",
 				zap.Uint64("processID", id),
 				zap.Time("now", time.Now()),
-				zap.Time("start", start),
-				zap.Time("end", end),
+				zap.String("start", start),
+				zap.String("end", end),
 			)
 			do.SysProcTracker().KillSysProcess(id)
 		}
