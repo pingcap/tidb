2f2e883f687fc3e8a313121a4eb3ac76ce8dae23
stevenayu <stevenayu@126.com>
2025-07-24 14:24:46 +0800

    Update main_test.go

--- tests/readonlytest/main_test.go
+++ tests/readonlytest/main_test.go
@@ -8,7 +8,6 @@
 //
 //
 //
-//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
