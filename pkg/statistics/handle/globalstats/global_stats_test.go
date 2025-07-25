eb617c22263084f523accbc9e834cd808c7d48a0
Isabel Chen <92058925+ichen032@users.noreply.github.com>
2025-07-24 19:13:13 -0700

    This is an automated cherry-pick of #62363
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/statistics/handle/globalstats/global_stats_test.go
+++ pkg/statistics/handle/globalstats/global_stats_test.go
@@ -890,10 +890,13 @@ func TestGlobalIndexStatistics(t *testing.T) {
 		require.Nil(t, h.Update(context.Background(), dom.InfoSchema()))
 		tk.MustQuery("SELECT b FROM t use index(idx) WHERE b < 16 ORDER BY b").
 			Check(testkit.Rows("1", "2", "3", "15"))
+		expectedRows := "4.06"
+		if i != 0 {
+			expectedRows = "4.00"
+		}
 		tk.MustQuery("EXPLAIN format='brief' SELECT b FROM t use index(idx) WHERE b < 16 ORDER BY b").
-			Check(testkit.Rows("IndexReader 4.00 root partition:all index:IndexRangeScan",
-				"└─IndexRangeScan 4.00 cop[tikv] table:t, index:idx(b) range:[-inf,16), keep order:true"))
-
+			Check(testkit.Rows("IndexReader "+expectedRows+" root partition:all index:IndexRangeScan",
+				"└─IndexRangeScan "+expectedRows+" cop[tikv] table:t, index:idx(b) range:[-inf,16), keep order:true"))
 		// analyze table t index idx
 		tk.MustExec("drop table if exists t")
 		err = statstestutil.HandleNextDDLEventWithTxn(h)
@@ -913,7 +916,7 @@ func TestGlobalIndexStatistics(t *testing.T) {
 		tk.MustExec("analyze table t index idx")
 		require.Nil(t, h.Update(context.Background(), dom.InfoSchema()))
 		rows := tk.MustQuery("EXPLAIN SELECT b FROM t use index(idx) WHERE b < 16 ORDER BY b;").Rows()
-		require.Equal(t, "4.00", rows[0][1])
+		require.Equal(t, expectedRows, rows[0][1])
 
 		// analyze table t index
 		tk.MustExec("drop table if exists t")
@@ -934,8 +937,8 @@ func TestGlobalIndexStatistics(t *testing.T) {
 		tk.MustExec("analyze table t index")
 		require.Nil(t, h.Update(context.Background(), dom.InfoSchema()))
 		tk.MustQuery("EXPLAIN format='brief' SELECT b FROM t use index(idx) WHERE b < 16 ORDER BY b;").
-			Check(testkit.Rows("IndexReader 4.00 root partition:all index:IndexRangeScan",
-				"└─IndexRangeScan 4.00 cop[tikv] table:t, index:idx(b) range:[-inf,16), keep order:true"))
+			Check(testkit.Rows("IndexReader "+expectedRows+" root partition:all index:IndexRangeScan",
+				"└─IndexRangeScan "+expectedRows+" cop[tikv] table:t, index:idx(b) range:[-inf,16), keep order:true"))
 	}
 }
 
