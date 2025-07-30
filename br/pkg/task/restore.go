af7316cb70cf40cb3cbcbc59905c316ec81e0086
Jianjun Liao <jianjun.liao@outlook.com>
2025-07-30 14:20:24 +0800

    fix version compatibility
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- br/pkg/task/restore.go
+++ br/pkg/task/restore.go
@@ -937,7 +937,7 @@ func RunRestore(c context.Context, g glue.Glue, cmdName string, cfg *RestoreConf
 	defer printRestoreMetrics()
 
 	// build restore registry
-	restoreRegistry, err := registry.NewRestoreRegistry(g, mgr.GetDomain())
+	restoreRegistry, err := registry.NewRestoreRegistry(c, g, mgr.GetDomain())
 	if err != nil {
 		return errors.Trace(err)
 	}
@@ -2638,7 +2638,7 @@ func RunRestoreAbort(c context.Context, g glue.Glue, cmdName string, cfg *Restor
 	}
 
 	// build restore registry
-	restoreRegistry, err := registry.NewRestoreRegistry(g, mgr.GetDomain())
+	restoreRegistry, err := registry.NewRestoreRegistry(ctx, g, mgr.GetDomain())
 	if err != nil {
 		return errors.Trace(err)
 	}
