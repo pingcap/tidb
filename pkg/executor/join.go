c9c39414e7118024721715b3f2fc397ebdf11b58
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-07-30 16:58:19 +0800

    executor: Change log level to `Warning` in the `Close` of join (#62656) (#62676)
    close pingcap/tidb#62652

--- pkg/executor/join.go
+++ pkg/executor/join.go
@@ -183,7 +183,7 @@ func (e *HashJoinExec) Close() error {
 		util.WithRecovery(func() {
 			err := e.rowContainer.Close()
 			if err != nil {
-				logutil.BgLogger().Error("RowContainer encounters error",
+				logutil.BgLogger().Warn("RowContainer encounters error",
 					zap.Error(err),
 					zap.Stack("stack trace"))
 			}
