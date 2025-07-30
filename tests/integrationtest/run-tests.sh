38b6f5b3d04832e93c6d9ffd09578aa64982da3a
wuhuizuo <wuhuizuo@126.com>
2025-07-30 20:32:31 +0800

    Fix script paths to use absolute locations in test runners

--- tests/integrationtest/run-tests.sh
+++ tests/integrationtest/run-tests.sh
@@ -215,9 +215,10 @@ status=${ports[1]}
 
 function start_tidb_server()
 {
-    config_file="config.toml"
+    self_dir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+    config_file="${self_dir}/config.toml"
     if [[ $enabled_new_collation = 0 ]]; then
-        config_file="disable_new_collation.toml"
+        config_file="${self_dir}/disable_new_collation.toml"
     fi
     echo "start tidb-server, log file: $mysql_tester_log"
     if [ "${TIDB_TEST_STORE_NAME}" = "tikv" ]; then
