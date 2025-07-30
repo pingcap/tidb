18974771eb4324e124e82d49e19e9ae2497f2d1b
AilinKid <314806019@qq.com>
2025-07-30 13:44:20 +0800

    .;
    Signed-off-by: AilinKid <314806019@qq.com>

--- pkg/planner/core/generator/plan_cache/plan_clone_generator.go
+++ pkg/planner/core/generator/plan_cache/plan_clone_generator.go
@@ -99,7 +99,7 @@ func genPlanCloneForPlanCache(x any) ([]byte, error) {
 		case "[]int", "[]byte", "[]float", "[]bool": // simple slice
 			c.write("cloned.%v = make(%v, len(op.%v))", f.Name, f.Type, f.Name)
 			c.write("copy(cloned.%v, op.%v)", f.Name, f.Name)
-		case "core.basePhysicalAgg":
+		case "physicalop.basePhysicalAgg":
 			fieldName := strings.Split(f.Type.String(), ".")[1]
 			c.write(`basePlan, baseOK := op.%v.CloneForPlanCacheWithSelf(newCtx, cloned)
 							if !baseOK {return nil, false}
