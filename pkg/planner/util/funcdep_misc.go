6e1a35340cd8fe8a0fbf13806a0c214980a354ff
Weizhen Wang <wangweizhen@pingcap.com>
2025-08-04 21:00:27 +0800

    This is an automated cherry-pick of #62791
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/planner/util/funcdep_misc.go
+++ pkg/planner/util/funcdep_misc.go
@@ -37,8 +37,7 @@ func ExtractNotNullFromConds(conditions []expression.Expression, p base.LogicalP
 	// As a result,	`a` will be extracted as not-null column to abound the FDSet.
 	notnullColsUniqueIDs := intset.NewFastIntSet()
 	for _, condition := range conditions {
-		var cols []*expression.Column
-		cols = expression.ExtractColumnsFromExpressions(cols, []expression.Expression{condition}, nil)
+		cols := expression.ExtractColumnsFromExpressions([]expression.Expression{condition}, nil)
 		if len(cols) == 0 {
 			continue
 		}
