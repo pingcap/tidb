db45d0f093b12f6e0af7633b948bb13913687cb9
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-08-05 15:08:54 +0800

    executor: replace `Call` with `WithRecovery in the close of hash join v1 (#61868) (#62827)
    close pingcap/tidb#60926

--- executor/hash_table.go
+++ executor/hash_table.go
@@ -23,6 +23,7 @@ import (
 	"unsafe"
 
 	"github.com/pingcap/errors"
+	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/types"
@@ -501,6 +502,8 @@ func (c *hashRowContainer) Len() uint64 {
 }
 
 func (c *hashRowContainer) Close() error {
+	failpoint.Inject("issue60926", nil)
+
 	defer c.memTracker.Detach()
 	c.chkBuf = nil
 	return c.rowContainer.Close()
