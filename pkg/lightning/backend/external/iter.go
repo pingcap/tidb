583502967662ab1ac09018eec651ae1f6604d36c
fzzf678 <daery_7@163.com>
2025-08-06 20:31:31 +0800

    100s

--- pkg/lightning/backend/external/iter.go
+++ pkg/lightning/backend/external/iter.go
@@ -568,7 +568,7 @@ func (i *MergeKVIter) Next() bool {
 	i.lastSize += len(i.Key()) + len(i.Value())
 	i.lastKeyNum++
 	d := time.Since(i.lastTime)
-	if d > 2*time.Second {
+	if d > 100*time.Second {
 		sz := i.lastSize
 		keyNum := i.lastKeyNum
 
