2d07f5c7167eaea299c6f093fa35309dea0ee236
D3Hunter <jujj603@gmail.com>
2025-08-07 14:11:21 +0800

    mdl: always enable and make it read only in nextgen (#62865)
    ref pingcap/tidb#61702

--- pkg/ddl/schemaver/syncer_test.go
+++ pkg/ddl/schemaver/syncer_test.go
@@ -39,7 +39,7 @@ import (
 const minInterval = 10 * time.Nanosecond // It's used to test timeout.
 
 func TestSyncerSimple(t *testing.T) {
-	vardef.EnableMDL.Store(false)
+	vardef.SetEnableMDL(false)
 	if runtime.GOOS == "windows" {
 		t.Skip("integration.NewClusterV3 will create file contains a colon which is not allowed on Windows")
 	}
