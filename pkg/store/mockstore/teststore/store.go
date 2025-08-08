e65d744aa3cc2b192e95e4dda847a907c2eaf94e
tangenta <tangenta@126.com>
2025-08-08 15:25:31 +0800

    add license header
    Signed-off-by: tangenta <tangenta@126.com>

--- pkg/store/mockstore/teststore/store.go
+++ pkg/store/mockstore/teststore/store.go
@@ -1,3 +1,17 @@
+// Copyright 2025 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package teststore
 
 import (
@@ -12,6 +26,7 @@ import (
 )
 
 // NewMockStoreWithoutBootstrap creates a mock store without bootstrap.
+// This is a wrapper function for mockstore.NewMockStore to avoid potential circular dependency.
 // If you need to bootstrap the store, use `testkit.CreateMockStore` or `testkit.CreateMockStoreAndDomain` instead.
 func NewMockStoreWithoutBootstrap(t testing.TB, opts ...mockstore.MockTiKVStoreOption) kv.Storage {
 	store, err := mockstore.NewMockStore(opts...)
