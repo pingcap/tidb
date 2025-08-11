98d0b073fc3d3b860d9c1b069d7cf8a06fc89e24
Dongpo Liu <techregister@pm.me>
2025-08-11 12:22:54 +0200

    This is an automated cherry-pick of #62790
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    
    fix: resolve conflicts
    
    fix: remove tests
    
    fix: make it compile
    
    fix: remove tests
    
    fix: remove tests

--- pkg/executor/analyze_test.go
+++ pkg/executor/analyze_test.go
@@ -80,7 +80,7 @@ func TestAnalyzeIndexExtractTopN(t *testing.T) {
 	table, err := is.TableByName(context.Background(), model.NewCIStr("test_index_extract_topn"), model.NewCIStr("t"))
 	require.NoError(t, err)
 	tableInfo := table.Meta()
-	tbl := dom.StatsHandle().GetTableStats(tableInfo)
+	tbl := dom.StatsHandle().GetPhysicalTableStats(tableInfo.ID, tableInfo)
 
 	// Construct TopN, should be (1, 1) -> 2 and (1, 2) -> 2
 	topn := statistics.NewTopN(2)
