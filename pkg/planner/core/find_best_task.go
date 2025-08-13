4a09f68d7eff1f3c64c102b69e6881234583e697
tpp <terry.purcell@pingcap.com>
2025-08-13 11:31:52 -0700

    fix github merge error

--- pkg/planner/core/find_best_task.go
+++ pkg/planner/core/find_best_task.go
@@ -1601,7 +1601,6 @@ func skylinePruning(ds *logicalop.DataSource, prop *property.PhysicalProperty) [
 			if !c.path.IsFullScanRange(ds.TableInfo) {
 				// Preference plans with equals/IN predicates or where there is more filtering in the index than against the table
 				indexFilters := c.path.EqOrInCondCount > 0 || len(c.path.TableFilters) < len(c.path.IndexFilters)
-				isDNFOnlyEqualPredicates := ds.TableInfo.GetPartitionInfo() == nil && c.hasOnlyEqualPredicatesInDNF()
 				isDNFOnlyEquals := ds.TableInfo.GetPartitionInfo() == nil && c.hasOnlyEqualPredicatesInDNF()
 				if preferMerge || isDNFOnlyEquals || (indexFilters && (prop.IsSortItemEmpty() || c.isMatchProp)) {
 					preferredPaths = append(preferredPaths, c)
