f38b27e4bed1cca510ddeb008d33b14f0a92db9b
D3Hunter <jujj603@gmail.com>
2025-08-19 14:37:20 +0800

    change
    change
    
    change
    
    change
    
    change
    
    change
    
    test

---
+++ pkg/disttask/framework/handle/status_testkit_test.go
@@ -0,0 +1,114 @@
+// Copyright 2025 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handle_test
+
+import (
+	"cmp"
+	"context"
+	"slices"
+	"testing"
+	"time"
+
+	"github.com/ngaut/pools"
+	"github.com/pingcap/tidb/pkg/disttask/framework/handle"
+	"github.com/pingcap/tidb/pkg/disttask/framework/proto"
+	"github.com/pingcap/tidb/pkg/disttask/framework/schstatus"
+	"github.com/pingcap/tidb/pkg/disttask/framework/storage"
+	"github.com/pingcap/tidb/pkg/sessionctx"
+	"github.com/pingcap/tidb/pkg/testkit"
+	"github.com/pingcap/tidb/pkg/testkit/testfailpoint"
+	"github.com/pingcap/tidb/pkg/util/sqlexec"
+	"github.com/stretchr/testify/require"
+	"github.com/tikv/client-go/v2/util"
+)
+
+func TestNodeInfoAndBusyNodes(t *testing.T) {
+	testfailpoint.Enable(t, "github.com/pingcap/tidb/pkg/domain/MockDisableDistTask", "return(true)")
+	testfailpoint.Enable(t, "github.com/pingcap/tidb/pkg/util/cpu/mockNumCpu", "return(16)")
+	store := testkit.CreateMockStore(t)
+	gtk := testkit.NewTestKit(t, store)
+	pool := pools.NewResourcePool(func() (pools.Resource, error) {
+		return gtk.Session(), nil
+	}, 1, 1, time.Second)
+	defer pool.Close()
+	ctx := util.WithInternalSourceType(context.Background(), "handle_test")
+	taskMgr := storage.NewTaskManager(pool)
+	nodeCount, cpuCount, err := handle.GetNodesInfo(ctx, taskMgr)
+	require.NoError(t, err)
+	require.Zero(t, nodeCount)
+	require.Equal(t, 16, cpuCount)
+	// add one node
+	require.NoError(t, taskMgr.InitMeta(ctx, "node1", ""))
+	nodeCount, cpuCount, err = handle.GetNodesInfo(ctx, taskMgr)
+	require.NoError(t, err)
+	require.Equal(t, 1, nodeCount)
+	require.Equal(t, 16, cpuCount)
+
+	// busy nodes include owner node
+	busyNodes, err := handle.GetBusyNodes(ctx, taskMgr)
+	require.NoError(t, err)
+	require.EqualValues(t, []schstatus.Node{{ID: ":4000", IsOwner: true}}, busyNodes)
+	// busy nodes include node with subtasks
+	require.NoError(t, taskMgr.WithNewSession(func(se sessionctx.Context) error {
+		_, err2 := sqlexec.ExecSQL(ctx, se.GetSQLExecutor(), `
+			insert into mysql.tidb_background_subtask(`+storage.InsertSubtaskColumns+`) values`+
+			`(%?, %?, %?, %?, %?, %?, %?, NULL, CURRENT_TIMESTAMP(), '{}', '{}')`,
+			1, "1", ":4000", "{}", proto.SubtaskStatePending, 1, 1)
+		require.NoError(t, err2)
+		_, err2 = sqlexec.ExecSQL(ctx, se.GetSQLExecutor(), `
+			insert into mysql.tidb_background_subtask(`+storage.InsertSubtaskColumns+`) values`+
+			`(%?, %?, %?, %?, %?, %?, %?, NULL, CURRENT_TIMESTAMP(), '{}', '{}')`,
+			1, "1", ":4001", "{}", proto.SubtaskStateRunning, 1, 1)
+		require.NoError(t, err2)
+		return nil
+	}))
+	busyNodes, err = handle.GetBusyNodes(ctx, taskMgr)
+	require.NoError(t, err)
+	slices.SortFunc(busyNodes, func(a, b schstatus.Node) int {
+		return cmp.Compare(a.ID, b.ID)
+	})
+	require.EqualValues(t, []schstatus.Node{{ID: ":4000", IsOwner: true}, {ID: ":4001"}}, busyNodes)
+}
+
+func TestScheduleFlag(t *testing.T) {
+	testkit.CreateMockStore(t)
+	ctx := util.WithInternalSourceType(context.Background(), "handle_test")
+	taskMgr, err := storage.GetTaskManager()
+	require.NoError(t, err)
+	// not set
+	flags, err := handle.GetScheduleFlags(ctx, taskMgr)
+	require.NoError(t, err)
+	require.Empty(t, flags)
+	// enabled flag
+	expireTime := time.Unix(time.Now().Add(time.Hour).Unix(), 0)
+	flag := schstatus.TTLFlag{Enabled: true, TTL: time.Hour, ExpireTime: expireTime}
+	require.NoError(t, handle.UpdatePauseScaleInFlag(ctx, &flag))
+	flags, err = handle.GetScheduleFlags(ctx, taskMgr)
+	require.NoError(t, err)
+	require.Len(t, flags, 1)
+	require.EqualValues(t, flag, flags[schstatus.PauseScaleInFlag])
+	// expired flag
+	flag.ExpireTime = time.Now().Add(-time.Hour)
+	require.NoError(t, handle.UpdatePauseScaleInFlag(ctx, &flag))
+	flags, err = handle.GetScheduleFlags(ctx, taskMgr)
+	require.NoError(t, err)
+	require.Empty(t, flags)
+	// disabled flag
+	flag = schstatus.TTLFlag{Enabled: false}
+	require.NoError(t, handle.UpdatePauseScaleInFlag(ctx, &flag))
+	flags, err = handle.GetScheduleFlags(ctx, taskMgr)
+	require.NoError(t, err)
+	require.Empty(t, flags)
+}
