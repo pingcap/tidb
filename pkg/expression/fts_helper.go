e93295fa6f259f39506ea4d32f59cd0f261148f5
Yiding <winoros@gmail.com>
2025-08-22 12:58:02 +0800

    fix static check

--- pkg/expression/fts_helper.go
+++ pkg/expression/fts_helper.go
@@ -24,6 +24,7 @@ type FTSInfo struct {
 	Column *Column
 }
 
+// FTSFuncMap stores the functions related to fulltext search.
 var FTSFuncMap map[string]struct{} = map[string]struct{}{
 	ast.FTSMatchWord:   {},
 	ast.FTSMatchPrefix: {},
