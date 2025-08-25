53e2cd0690c8bdc3b2b37e64eaab840a75621b22
tpp <terry.purcell@pingcap.com>
2025-08-25 16:37:07 -0700

    planner: Add new risk variables to plan generation

--- pkg/bindinfo/binding_plan_generation.go
+++ pkg/bindinfo/binding_plan_generation.go
@@ -338,6 +338,8 @@ func genPlanUnderState(sctx sessionctx.Context, stmt ast.StmtNode, state *state)
 			sctx.GetSessionVars().OptOrderingIdxSelRatio = state.varValues[i].(float64)
 		case vardef.TiDBOptRiskEqSkewRatio:
 			sctx.GetSessionVars().RiskEqSkewRatio = state.varValues[i].(float64)
+		case vardef.TiDBOptRiskRangeSkewRatio:
+			sctx.GetSessionVars().RiskRangeSkewRatio = state.varValues[i].(float64)
 		case vardef.TiDBOptRiskGroupNDVSkewRatio:
 			sctx.GetSessionVars().RiskGroupNDVSkewRatio = state.varValues[i].(float64)
 		case vardef.TiDBOptPreferRangeScan:
@@ -409,7 +411,7 @@ func adjustVar(varName string, varVal any) (newVarVal any, err error) {
 			return v, nil
 		}
 		return v * 5, nil
-	case vardef.TiDBOptOrderingIdxSelRatio, vardef.TiDBOptRiskEqSkewRatio, vardef.TiDBOptRiskGroupNDVSkewRatio: // range [0, 1], "<=0" means disable
+	case vardef.TiDBOptOrderingIdxSelRatio, vardef.TiDBOptRiskEqSkewRatio, vardef.TiDBOptRiskRangeSkewRatio, vardef.TiDBOptRiskGroupNDVSkewRatio: // range [0, 1], "<=0" means disable
 		v := varVal.(float64)
 		if v <= 0 {
 			return 0.1, nil
@@ -491,6 +493,8 @@ func getStartState(vars []string, fixes []uint64) (*state, error) {
 			s.varValues = append(s.varValues, vardef.DefTiDBOptOrderingIdxSelRatio)
 		case vardef.TiDBOptRiskEqSkewRatio:
 			s.varValues = append(s.varValues, vardef.DefOptRiskEqSkewRatio)
+		case vardef.TiDBOptRiskRangeSkewRatio:
+			s.varValues = append(s.varValues, vardef.DefOptRiskRangeSkewRatio)
 		case vardef.TiDBOptRiskGroupNDVSkewRatio:
 			s.varValues = append(s.varValues, vardef.DefOptRiskGroupNDVSkewRatio)
 		case vardef.TiDBOptPreferRangeScan:
