a1cb9a719c69b4a4dc47117a65d56ad477d4b2db
tpp <146148086+terry1purcell@users.noreply.github.com>
2025-08-26 07:41:21 -0700

    Remove duplicate case for risk skew ratio options

--- pkg/bindinfo/binding_plan_generation.go
+++ pkg/bindinfo/binding_plan_generation.go
@@ -414,8 +414,7 @@ func adjustVar(varName string, varVal any) (newVarVal any, err error) {
 		}
 		return v * 5, nil
 	case vardef.TiDBOptOrderingIdxSelRatio, vardef.TiDBOptRiskEqSkewRatio, vardef.TiDBOptRiskRangeSkewRatio, 
-    vardef.TiDBOptRiskGroupNDVSkewRatio, vardef.TiDBOptSelectivityFactor: // range [0, 1], "<=0" means disable
-	case vardef.TiDBOptOrderingIdxSelRatio, vardef.TiDBOptRiskEqSkewRatio, vardef.TiDBOptRiskGroupNDVSkewRatio, vardef.TiDBOptSelectivityFactor: // range [0, 1], "<=0" means disable
+        vardef.TiDBOptRiskGroupNDVSkewRatio, vardef.TiDBOptSelectivityFactor: // range [0, 1], "<=0" means disable
 		v := varVal.(float64)
 		if v <= 0 {
 			return 0.1, nil
