f7f59edeec8697998ebd08ea5671527082a1a62e
Ruihao Chen <joechenrh@gmail.com>
2025-08-27 06:40:47 -0400

    update
    Signed-off-by: Ruihao Chen <joechenrh@gmail.com>

--- tests/realtikvtest/addindextest2/alter_job_test.go
+++ tests/realtikvtest/addindextest2/alter_job_test.go
@@ -90,7 +90,7 @@ func TestAlterJobOnDXF(t *testing.T) {
 	tk.MustExec("set @@tidb_ddl_reorg_worker_cnt = 1")
 	tk.MustExec("set @@tidb_ddl_reorg_batch_size = 32")
 	// tk.MustExec(fmt.Sprintf(`set @@global.tidb_cloud_storage_uri = "%s"`, cloudStorageURI))
-	tk.MustExec(fmt.Sprintf(`set @@global.tidb_cloud_storage_uri = ""`))
+	tk.MustExec(`set @@global.tidb_cloud_storage_uri = ""`)
 	if kerneltype.IsClassic() {
 		tk.MustExec("set global tidb_ddl_reorg_max_write_speed = 16")
 		t.Cleanup(func() {
