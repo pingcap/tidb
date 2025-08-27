6e774b89497ecab5e2a3eaff191d58e68b94d5fe
tpp <terry.purcell@pingcap.com>
2025-08-26 17:24:56 -0700

    add new query to testcase

--- pkg/planner/core/logical_plans_test.go
+++ pkg/planner/core/logical_plans_test.go
@@ -1970,6 +1970,10 @@ func TestSkylinePruning(t *testing.T) {
 			result: "c_d_e_str,e_d_c_str_prefix", // TODO: Refine skyline pruning for string prefix indexes
 		},
 		{
+			sql:    "select * from t where c_str = 'a' and d_str = 'b' and e_str = '1234567890a'",
+			result: "c_d_e_str,e_d_c_str_prefix", // TODO: Refine skyline pruning for string prefix indexes
+		},
+		{
 			sql:    "select * from t where (f = 1 and g = 1) or (f = 2 and g = 2)",
 			result: "f_g",
 		},
