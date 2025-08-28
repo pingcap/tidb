75e5b77b222abb2b051accc28d3c769c2495e583
Lynn <zimu_xia@126.com>
2025-08-28 16:27:15 +0800

    *: remove mustParseDuration and copyImage

--- pkg/sessionctx/vardef/tidb_vars.go
+++ pkg/sessionctx/vardef/tidb_vars.go
@@ -1832,15 +1832,6 @@ func serverMemoryLimitDefaultValue() string {
 	return "0"
 }
 
-func mustParseDuration(str string) time.Duration {
-	duration, err := time.ParseDuration(str)
-	if err != nil {
-		panic(fmt.Sprintf("%s is not a duration", str))
-	}
-
-	return duration
-}
-
 func mustParseTime(layout string, str string) time.Time {
 	time, err := time.ParseInLocation(layout, str, time.UTC)
 	if err != nil {
