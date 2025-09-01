bd009490309865ae381e42d75f8ca38c493159cc
D3Hunter <jujj603@gmail.com>
2025-09-02 00:15:34 +0800

    This is an automated cherry-pick of #63300
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/util/naming/naming.go
+++ pkg/util/naming/naming.go
@@ -19,15 +19,44 @@ import (
 	"regexp"
 )
 
+<<<<<<< HEAD
 var nameRe = regexp.MustCompile(`^[a-zA-Z0-9_-]{0,64}$`)
+=======
+const (
+	// mostly we use an uint64 as the keyspace name, the max value is 20 characters.
+	// And there are at most 16,777,215 keyspace ID in a single physical cluster,
+	// as keyspace ID is encoded into KV, it's very hard to extend its length, so
+	// current keyspace name limit is more than enough for current usage and later
+	// extension.
+	maxKeyspaceNameLength = 20
+)
+>>>>>>> 0c7ebbae674 (config: limit keyspace name to at most 20 chars (#63300))
 
 // Check if the name is valid.
 // Valid name must be 64 characters or fewer and consist only of letters (a-z, A-Z),
 // numbers (0-9), hyphens (-), and underscores (_).
 // currently, we enforce this rule to tidb_service_scope and keyspace_name
+<<<<<<< HEAD
 func Check(scope string) error {
 	if !nameRe.MatchString(scope) {
 		return fmt.Errorf("the value '%s' is invalid. It must be 64 characters or fewer and consist only of letters (a-z, A-Z), numbers (0-9), hyphens (-), and underscores (_)", scope)
+=======
+func Check(name string) error {
+	return CheckWithMaxLen(name, 64)
+}
+
+// CheckKeyspaceName checks if the keyspace name is valid.
+func CheckKeyspaceName(name string) error {
+	return CheckWithMaxLen(name, maxKeyspaceNameLength)
+}
+
+// CheckWithMaxLen checks if the name is valid with the specified maximum length.
+func CheckWithMaxLen(name string, maxLen int) error {
+	namePattern := fmt.Sprintf(`^[a-zA-Z0-9_-]{0,%d}$`, maxLen)
+	nameRe := regexp.MustCompile(namePattern)
+	if !nameRe.MatchString(name) {
+		return fmt.Errorf("the value '%s' is invalid. It must be %d characters or fewer and consist only of letters (a-z, A-Z), numbers (0-9), hyphens (-), and underscores (_)", name, maxLen)
+>>>>>>> 0c7ebbae674 (config: limit keyspace name to at most 20 chars (#63300))
 	}
 	return nil
 }
