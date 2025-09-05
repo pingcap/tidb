4ec5a9fdca755a44fba6cc2ce8761afb16f04725
Weizhen Wang <wangweizhen@pingcap.com>
2025-09-05 09:42:02 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/logical_plan_builder.go
+++ pkg/planner/core/logical_plan_builder.go
@@ -2937,9 +2937,6 @@ func (g *gbyResolver) Leave(inNode ast.Node) (ast.Node, bool) {
 						break
 					}
 				}
-				if pos == 0 {
-					return inNode, false
-				}
 			}
 		}
 		if pos < 1 || pos > len(g.fields) {
