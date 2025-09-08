6f6718eabde8daa7d0f1fd6e37bd083fc790543d
Weizhen Wang <wangweizhen@pingcap.com>
2025-09-08 17:22:28 +0800

    bindinfo: add last_used_time to track bindinfo usage frequency
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/bindinfo/binding_cache.go
+++ pkg/bindinfo/binding_cache.go
@@ -38,6 +38,9 @@ type BindingCacheUpdater interface {
 	// LoadFromStorageToCache loads global bindings from storage to the memory cache.
 	LoadFromStorageToCache(fullLoad bool) (err error)
 
+	// UpdateBindingUsageInfoToStorage is to update the binding usage info into storage
+	UpdateBindingUsageInfoToStorage()
+
 	// LastUpdateTime returns the last update time.
 	LastUpdateTime() types.Time
 }
@@ -94,6 +97,15 @@ func (u *bindingCacheUpdater) LoadFromStorageToCache(fullLoad bool) (err error) 
 	metrics.BindingCacheMemLimit.Set(float64(u.GetMemCapacity()))
 	metrics.BindingCacheNumBindings.Set(float64(len(u.GetAllBindings())))
 	return nil
+}
+
+// UpdateBindingUsageInfoToStorage is to update the binding usage info into storage
+func (u *bindingCacheUpdater) UpdateBindingUsageInfoToStorage() {
+	bindings := u.GetAllBindings()
+	err := UpdateBindingUsageInfoToStorage(u.sPool, bindings)
+	if err != nil {
+		bindingLogger().Warn("BindingHandle.UpdateBindingUsageInfoToStorage", zap.Error(err))
+	}
 }
 
 // LastUpdateTime returns the last update time.
