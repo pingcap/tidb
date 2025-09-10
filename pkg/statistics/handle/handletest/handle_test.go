35944dc51e1606ba24cabfb35bd771ea5fc8d500
Weizhen Wang <wangweizhen@pingcap.com>
2025-09-10 15:59:58 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/handle/handletest/handle_test.go
+++ pkg/statistics/handle/handletest/handle_test.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/config"
+	"github.com/pingcap/tidb/pkg/config/kerneltype"
 	"github.com/pingcap/tidb/pkg/domain"
 	"github.com/pingcap/tidb/pkg/parser/ast"
 	"github.com/pingcap/tidb/pkg/planner/cardinality"
