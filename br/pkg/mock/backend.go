10a43610c05818d2553898ff128940aa0b9752e1
cbcwestwolf <1004626265@qq.com>
2025-09-11 16:10:26 +0800

    backend: adapt `AddPartitionRange` api for importing

--- br/pkg/mock/backend.go
+++ br/pkg/mock/backend.go
@@ -90,6 +90,21 @@ func (mr *MockBackendMockRecorder) CloseEngine(arg0, arg1, arg2 any) *gomock.Cal
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CloseEngine", reflect.TypeOf((*MockBackend)(nil).CloseEngine), arg0, arg1, arg2)
 }
 
+// AddPartitionRangeForTable mocks base method.
+func (m *MockBackend) AddPartitionRangeForTable(arg0 context.Context, arg1 int64) (func(), error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "AddPartitionRangeForTable", arg0, arg1)
+	ret0, _ := ret[0].(func())
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// AddPartitionRangeForTable indicates an expected call of AddPartitionRangeForTable.
+func (mr *MockBackendMockRecorder) AddPartitionRangeForTable(arg0, arg1 any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddPartitionRangeForTable", reflect.TypeOf((*MockBackend)(nil).AddPartitionRangeForTable), arg0, arg1)
+}
+
 // FlushAllEngines mocks base method.
 func (m *MockBackend) FlushAllEngines(arg0 context.Context) error {
 	m.ctrl.T.Helper()
