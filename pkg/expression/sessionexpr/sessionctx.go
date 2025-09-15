a6ddde324b9ec5438064c44202896ae0814bf196
x-ainger <xianganjie@gmail.com>
2025-09-15 10:21:11 +0800

    *: support the variable of `tidb_default_auto_id_cache` and corresponding behaviors fix

--- pkg/expression/sessionexpr/sessionctx.go
+++ pkg/expression/sessionexpr/sessionctx.go
@@ -79,7 +79,7 @@ func (ctx *ExprContext) NewCollationEnabled() bool {
 	return collate.NewCollationEnabled()
 }
 
-// GetTiDBDefaultAutoIDCache returns the value of the 'tidb_auto_id_cache' system variable.
+// GetTiDBDefaultAutoIDCache returns the value of the 'tidb_default_auto_id_cache' system variable.
 func (ctx *ExprContext) GetTiDBDefaultAutoIDCache() int {
 	defaultAutoIDCache, _ := ctx.sctx.GetSessionVars().GetSystemVar(vardef.TiDBDefaultAutoIDCache)
 	return variable.TidbOptInt(defaultAutoIDCache, vardef.DefTiDBAutoIDCache)
