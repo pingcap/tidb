9d32a23df8197c6f297dfb9e61aa6803dd85e711
tpp <146148086+terry1purcell@users.noreply.github.com>
2025-09-16 14:10:43 -0700

    Update expression_rewriter.go

--- pkg/planner/core/expression_rewriter.go
+++ pkg/planner/core/expression_rewriter.go
@@ -1258,7 +1258,6 @@ func (er *expressionRewriter) handleInSubquery(ctx context.Context, planCtx *exp
 
 func isNoDecorrelate(planCtx *exprRewriterPlanCtx, corCols []*expression.CorrelatedColumn, hintFlags uint64) bool {
 	noDecorrelate := hintFlags&hint.HintFlagNoDecorrelate > 0
-
 	if noDecorrelate && len(corCols) == 0 {
 		planCtx.builder.ctx.GetSessionVars().StmtCtx.SetHintWarning(
 			"NO_DECORRELATE() is inapplicable because there are no correlated columns.")
