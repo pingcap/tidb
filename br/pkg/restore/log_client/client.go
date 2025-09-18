2b02b123c52aa946d3c3fa5e30bda367f99538db
Jianjun Liao <36503113+Leavrth@users.noreply.github.com>
2025-09-18 14:48:56 +0800

    br: concurrently set tiflash replicas (#63360)
    close pingcap/tidb#63177

--- br/pkg/restore/log_client/client.go
+++ br/pkg/restore/log_client/client.go
@@ -1609,6 +1609,27 @@ func WrapLogFilesIterWithCheckpointFailpoint(
 	return logIter, nil
 }
 
+// ResetTiflashReplicas set tiflash replicas by given sqls concurrently.
+func (rc *LogClient) ResetTiflashReplicas(ctx context.Context, sqls []string, g glue.Glue) error {
+	resetSessions, err := createSessions(ctx, g, rc.dom.Store(), 16)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	defer func() {
+		closeSessions(resetSessions)
+	}()
+	workerpool := tidbutil.NewWorkerPool(16, "repair ingest index")
+	eg, ectx := errgroup.WithContext(ctx)
+	for _, sql := range sqls {
+		workerpool.ApplyWithIDInErrorGroup(eg, func(id uint64) error {
+			resetSession := resetSessions[id%uint64(len(resetSessions))]
+			log.Info("reset tiflash replica", zap.String("sql", sql))
+			return resetSession.ExecuteInternal(ectx, sql)
+		})
+	}
+	return eg.Wait()
+}
+
 const (
 	alterTableDropIndexSQL         = "ALTER TABLE %n.%n DROP INDEX %n"
 	alterTableAddIndexFormat       = "ALTER TABLE %%n.%%n ADD INDEX %%n(%s)"
