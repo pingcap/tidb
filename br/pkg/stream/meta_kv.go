49bc537c4e582c347c5b8546fba6539fece8a4cf
ris <79858083+RidRisR@users.noreply.github.com>
2025-09-24 16:48:25 +0800

    br: cp overflow fix (#63717)
    close pingcap/tidb#63663

--- br/pkg/stream/meta_kv.go
+++ br/pkg/stream/meta_kv.go
@@ -173,13 +173,19 @@ l_for:
 	for len(data) > 0 {
 		switch data[0] {
 		case flagShortValuePrefix:
-			vlen := data[1]
-			if len(data[2:]) < int(vlen) {
+			if len(data) < 2 {
 				return errors.Annotatef(berrors.ErrInvalidArgument,
-					"the length of short value is invalid, vlen: %v", int(vlen))
+					"insufficient data for short value header, need at least 2 bytes but only have %d",
+					len(data))
 			}
-			v.shortValue = data[2 : vlen+2]
-			data = data[vlen+2:]
+			vlen := data[1]
+			if len(data) < int(vlen)+2 {
+				return errors.Annotatef(berrors.ErrInvalidArgument,
+					"insufficient data for short value, need %d bytes but only have %d",
+					int(vlen)+2, len(data))
+			}
+			v.shortValue = data[2 : int(vlen)+2]
+			data = data[int(vlen)+2:]
 		case flagOverlappedRollback:
 			v.hasOverlappedRollback = true
 			data = data[1:]
