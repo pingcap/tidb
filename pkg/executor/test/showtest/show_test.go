375536ad3551752e5f20b5d52555aeabfbed50f0
cbcwestwolf <1004626265@qq.com>
2025-09-24 12:30:31 +0800

    privilege: fix a bug for `show grants` with roles

--- pkg/executor/test/showtest/show_test.go
+++ pkg/executor/test/showtest/show_test.go
@@ -1112,3 +1112,27 @@ func TestShowLimitReturnRow(t *testing.T) {
 	rows = result.Rows()
 	require.Equal(t, rows[0][2], "idx_b")
 }
+
+func TestShowGrantsWithRoles(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("CREATE USER u")
+	tk.MustExec("CREATE USER u@'r1'")
+	tk.MustExec("CREATE USER u@'r2'")
+	tk.MustExec("CREATE TABLE t (c1 int, c2 int)")
+
+	tk.MustExec("GRANT select(c1, c2) ON test.t TO u@'r1'")
+	tk.MustExec("GRANT select(c1, c2) ON test.t TO u@'r2'")
+	tk.MustExec("GRANT u@'r1' TO u")
+	tk.MustExec("GRANT u@'r2' TO u")
+	tk.MustExec("SET DEFAULT ROLE ALL to u")
+
+	usertk := testkit.NewTestKit(t, store)
+	require.NoError(t, usertk.Session().Auth(&auth.UserIdentity{Username: "u", Hostname: "%"}, nil, nil, nil))
+	usertk.MustQuery("show grants").Check(testkit.Rows(
+		"GRANT USAGE ON *.* TO 'u'@'%'",
+		"GRANT SELECT(c1, c2) ON `test`.`t` TO 'u'@'%'",
+		"GRANT 'u'@'r1', 'u'@'r2' TO 'u'@'%'",
+	))
+}
