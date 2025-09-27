34a418d8d6cfd6217350d6bd4cc344e7e1466753
ChangRui-Ryan <changrui82@gmail.com>
2025-09-27 13:04:39 +0800

    incremental probing for IndexHashJoin

--- pkg/planner/core/base/plan_base.go
+++ pkg/planner/core/base/plan_base.go
@@ -366,6 +366,11 @@ func (tp JoinType) IsInnerJoin() bool {
 	return tp == InnerJoin
 }
 
+// SupportIncrementalLookUp returns if this joiner supports incremental look up.
+func (tp JoinType) SupportIncrementalLookUp() bool {
+	return tp == InnerJoin || tp == LeftOuterJoin
+}
+
 func (tp JoinType) String() string {
 	switch tp {
 	case InnerJoin:
