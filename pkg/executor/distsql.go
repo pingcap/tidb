979f96533c9d7f93b7dec86fdb1e9c189520ba72
wshwsh12 <793703860@qq.com>
2025-09-29 14:11:56 +0800

    fix

--- pkg/executor/distsql.go
+++ pkg/executor/distsql.go
@@ -1174,7 +1174,7 @@ func (w *indexWorker) extractTaskHandles(ctx context.Context, chk *chunk.Chunk, 
 	}
 	handleOffset := make([]int, 0, len(w.idxLookup.handleCols))
 	for i := range w.idxLookup.handleCols {
-		if handleVersionMap == nil {
+		if !w.idxLookup.isVersionAware {
 			handleOffset = append(handleOffset, numColsWithoutPid-len(w.idxLookup.handleCols)+i)
 		} else {
 			handleOffset = append(handleOffset, numColsWithoutPid-1-len(w.idxLookup.handleCols)+i)
