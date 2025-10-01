375a0a3d6db3cf1cf621317992bfb42b50b8f775
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-10-01 10:34:04 +0800

    executor: replace Call with `WithRecovery in the close of hash join v1 (#63782)
    close pingcap/tidb#60926

--- pkg/executor/join/hash_join_v1.go
+++ pkg/executor/join/hash_join_v1.go
@@ -42,8 +42,13 @@ import (
 	"github.com/pingcap/tidb/pkg/util/dbterror/exeerrors"
 	"github.com/pingcap/tidb/pkg/util/disk"
 	"github.com/pingcap/tidb/pkg/util/execdetails"
+	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/pingcap/tidb/pkg/util/memory"
+	"go.uber.org/zap"
 )
+
+// IsChildCloseCalledForTest is used for test
+var IsChildCloseCalledForTest = false
 
 var (
 	_ exec.Executor = &HashJoinV1Exec{}
@@ -114,7 +119,7 @@ type HashJoinV1Exec struct {
 }
 
 // Close implements the Executor Close interface.
-func (e *HashJoinV1Exec) Close() error {
+func (e *HashJoinV1Exec) Close() (err error) {
 	if e.closeCh != nil {
 		close(e.closeCh)
 	}
@@ -138,7 +143,14 @@ func (e *HashJoinV1Exec) Close() error {
 			channel.Clear(e.ProbeWorkers[i].joinChkResourceCh)
 		}
 		e.ProbeSideTupleFetcher.probeChkResourceCh = nil
-		terror.Call(e.RowContainer.Close)
+		util.WithRecovery(func() {
+			err := e.RowContainer.Close()
+			if err != nil {
+				logutil.BgLogger().Warn("RowContainer encounters error",
+					zap.Error(err),
+					zap.Stack("stack trace"))
+			}
+		}, nil)
 		e.HashJoinCtxV1.SessCtx.GetSessionVars().MemTracker.UnbindActionFromHardLimit(e.RowContainer.ActionSpill())
 		e.waiterWg.Wait()
 	}
@@ -159,8 +171,9 @@ func (e *HashJoinV1Exec) Close() error {
 	if e.stats != nil {
 		defer e.Ctx().GetSessionVars().StmtCtx.RuntimeStatsColl.RegisterStats(e.ID(), e.stats)
 	}
-	err := e.BaseExecutor.Close()
-	return err
+
+	IsChildCloseCalledForTest = true
+	return e.BaseExecutor.Close()
 }
 
 // Open implements the Executor Open interface.
