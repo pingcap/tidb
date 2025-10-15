487aef3b7e2dbaa5a3b12dd869e417953a7f0247
AilinKid <314806019@qq.com>
2025-10-15 15:07:48 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

--- pkg/planner/core/operator/physicalop/enforce.go
+++ pkg/planner/core/operator/physicalop/enforce.go
@@ -1,6 +1,22 @@
+// Copyright 2025 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package physicalop
 
 import (
+	"math"
+
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/planner/core/base"
 	"github.com/pingcap/tidb/pkg/planner/funcdep"
@@ -8,7 +24,6 @@ import (
 	"github.com/pingcap/tidb/pkg/planner/util"
 	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/util/collate"
-	"math"
 )
 
 // EnforceProperty act as portal to enforce a physical property to a task.
