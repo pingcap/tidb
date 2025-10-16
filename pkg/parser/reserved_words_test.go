c022ab949f9d8e00ada5f22a58e09a644f233d51
Weizhen Wang <wangweizhen@pingcap.com>
2025-10-16 10:09:44 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/parser/reserved_words_test.go
+++ pkg/parser/reserved_words_test.go
@@ -12,7 +12,6 @@
 // limitations under the License.
 
 //go:build reserved_words_test
-// +build reserved_words_test
 
 // This file ensures that the set of reserved keywords is the same as that of
 // MySQL. To run:
