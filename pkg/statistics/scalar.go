f0e502412eadf813538d9c50cad120b4f421c8a2
Weizhen Wang <wangweizhen@pingcap.com>
2025-10-18 15:50:21 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/statistics/scalar.go
+++ pkg/statistics/scalar.go
@@ -174,8 +174,13 @@ func commonPrefixLength(strs ...[]byte) int {
 
 func convertBytesToScalar(value []byte) float64 {
 	// Bytes type is viewed as a base-256 value, so we only consider at most 8 bytes.
-	if len(value) == 1 {
+	n := len(value)
+	if n == 1 {
 		return float64(value[0])
+	} else if n < 8 {
+		var buf [8]byte
+		copy(buf[:], value)
+		return float64(binary.BigEndian.Uint64(buf[:]))
 	}
 	return float64(binary.BigEndian.Uint64(value))
 }
