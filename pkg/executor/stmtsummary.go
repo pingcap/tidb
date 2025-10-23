f77f9b2758a0c4eb8a43d4924d43462cf4742d5f
cpd <cpdprivate007@gmail.com>
2025-10-23 16:25:06 +0530

    planner: user filtering based in session and priviledge

--- pkg/executor/stmtsummary.go
+++ pkg/executor/stmtsummary.go
@@ -166,6 +166,9 @@ func (e *stmtSummaryRetriever) initSummaryRowsReader(sctx sessionctx.Context) (*
 	} else if isHistoryTable(e.table.Name.O) {
 		rows = reader.GetStmtSummaryHistoryRows()
 	}
+
+	rows = filterRowsByUserAndPriv(rows, columns, user.Username, priv)
+
 	return newSimpleRowsReader(rows), nil
 }
 
@@ -259,6 +262,8 @@ func (r *stmtSummaryRetrieverV2) initSummaryRowsReader(ctx context.Context, sctx
 
 	var rowsReader *rowsReader
 	if isCurrentTable(r.table.Name.O) {
+		// filter for ordinary users
+		memRows = filterRowsByUserAndPriv(memRows, columns, user.Username, priv)
 		rowsReader = newSimpleRowsReader(memRows)
 	}
 	if isHistoryTable(r.table.Name.O) {
@@ -268,6 +273,7 @@ func (r *stmtSummaryRetrieverV2) initSummaryRowsReader(ctx context.Context, sctx
 		if err != nil {
 			return nil, err
 		}
+		memRows = filterRowsByUserAndPriv(memRows, columns, user.Username, priv)
 		rowsReader = newRowsReader(memRows, history)
 	}
 
@@ -417,4 +423,29 @@ func buildTimeRanges(tr *plannercore.TimeRange) []*stmtsummaryv2.StmtTimeRange {
 		Begin: tr.StartTime.Unix(),
 		End:   tr.EndTime.Unix(),
 	}}
+}
+
+// filter rows based on current session and its priviledges.
+func filterRowsByUserAndPriv(rows [][]types.Datum, columns []*model.ColumnInfo, username string, privileged bool) [][]types.Datum {
+	if privileged {
+		return rows
+	}
+	sampleUserCol := -1
+	for i, col := range columns {
+		if col.Name.O == "SAMPLE_USER" {
+			sampleUserCol = i
+			break
+		}
+	}
+	if sampleUserCol < 0 {
+		return rows
+	}
+
+	filtered := rows[:0]
+	for _, r := range rows {
+		if r[sampleUserCol].GetString() == username {
+			filtered = append(filtered, r)
+		}
+	}
+	return filtered
 }
