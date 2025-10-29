b2d7adf1fa0585dddc751e94fb82afb6572b9d0c
qw4990 <zhangyuanjia@pingcap.com>
2025-10-29 15:20:26 +0800

    fixup

--- pkg/planner/cardinality/selectivity.go
+++ pkg/planner/cardinality/selectivity.go
@@ -1172,8 +1172,7 @@ func outOfRangeEQSelectivity(sctx planctx.PlanContext, ndv, realtimeRowCount, co
 
 // outOfRangeFullNDV estimates the number of qualified rows when the topN represents all NDV values
 // and the searched value does not appear in the topN
-func outOfRangeFullNDV(ndv, origRowCount, notNullCount, realtimeRowCount, increaseFactor float64,
-	modifyCount int64, emptyHist bool) (result float64) {
+func outOfRangeFullNDV(ndv, origRowCount, notNullCount, realtimeRowCount, increaseFactor float64, modifyCount int64) (result float64) {
 	// If the table hasn't been modified, it's safe to return 0.
 	if modifyCount == 0 {
 		return 0
@@ -1192,13 +1191,20 @@ func outOfRangeFullNDV(ndv, origRowCount, notNullCount, realtimeRowCount, increa
 	}
 	// if no NDV - derive an NDV using sqrt
 	// if the Hist is empty and the value can't hit any values in TopN - derive an NDV using sqrt
-	if ndv <= 0 || emptyHist {
+	if ndv <= 0 {
 		ndv = math.Sqrt(max(notNullCount, realtimeRowCount))
 	} else {
 		// We need to increase the ndv by increaseFactor because the estimate will be increased by
 		// the caller of the function
 		ndv *= increaseFactor
 	}
+	// If topN represents all NDV values, the NDV should be relatively small.
+	// Small NDV could cause extremely inaccurate result, use `outOfRangeBetweenRate` to smooth the result.
+	// For example, TopN = {(value:1, rows: 10000), (2, 10000), (3, 10000)} and newRows = 15000, we should assume most
+	// newly added rows are 1, 2 or 3. Then for an out-of-range estimation like `where col=9999`, the result should be
+	// close to 0, but if we still use the original NDV, the result could be extremely large: 15000/3 = 5000.
+	// See #64137 for a concrete example.
+	ndv = max(ndv, float64(outOfRangeBetweenRate)) // avoid inaccurate estimate caused by small NDV
 	return max(1, newRows/ndv)
 }
 
