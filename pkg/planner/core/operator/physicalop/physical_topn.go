81e8b474e11f9dbdc38a8c09cce1ec1156fc0ff2
guo-shaoge <shaoge1994@163.com>
2025-11-05 14:09:57 +0800

    refine
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- pkg/planner/core/operator/physicalop/physical_topn.go
+++ pkg/planner/core/operator/physicalop/physical_topn.go
@@ -46,6 +46,10 @@ type PhysicalTopN struct {
 // ExhaustPhysicalPlans4LogicalTopN exhausts PhysicalTopN plans from LogicalTopN.
 func ExhaustPhysicalPlans4LogicalTopN(lt *logicalop.LogicalTopN, prop *property.PhysicalProperty) ([][]base.PhysicalPlan, bool, error) {
 	if MatchItems(prop, lt.ByItems) {
+		// PhysicalTopN and PhysicalLimit are in different slices
+		// so we can support prefering a specific LIMIT or TopN within one slice.
+		// For example, among all LIMIT tasks, we prefer the one pushed down to TiKV.
+		// Then we compare the preferred task from each slice by their actual cost.
 		return [][]base.PhysicalPlan{getPhysTopN(lt, prop), getPhysLimits(lt, prop)}, true, nil
 	}
 	return nil, true, nil
