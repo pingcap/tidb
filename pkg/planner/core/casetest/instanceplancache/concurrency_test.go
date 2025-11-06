3cd68033c092f8f90b57840a56ebe2c68bfb6150
Weizhen Wang <wangweizhen@pingcap.com>
2025-11-06 21:10:36 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/casetest/instanceplancache/concurrency_test.go
+++ pkg/planner/core/casetest/instanceplancache/concurrency_test.go
@@ -889,7 +889,7 @@ func TestInstancePlanCacheConcurrencyComp(t *testing.T) {
 	}
 	stmts = append(stmts, &testStmt{normalStmt: "commit"})
 
-	nConcurrency := 12
+	nConcurrency := 10
 	TKs := make([]*testkit.TestKit, nConcurrency)
 	for i := range TKs {
 		TKs[i] = testkit.NewTestKit(t, store)
