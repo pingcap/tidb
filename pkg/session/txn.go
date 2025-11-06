530e8f14dc34d022410f50e93c239b6aed30b686
fzzf678 <daery_7@163.com>
2025-11-06 15:21:12 +0800

    Update txn.go

--- pkg/session/txn.go
+++ pkg/session/txn.go
@@ -652,6 +652,14 @@ func KeyNeedToLock(k, v []byte, flags kv.KeyFlags) bool {
 	}
 
 	if tablecodec.IsTempIndexKey(k) {
+		tmpVal, err := tablecodec.DecodeTempIndexValue(v)
+		if err != nil {
+			logutil.BgLogger().Warn("decode temp index value failed", zap.Error(err))
+			return false
+		}
+		current := tmpVal.Current()
+		if current.Handle != nil || tablecodec.IndexKVIsUnique(current.Value) {
+		}
 		return true
 	}
 
