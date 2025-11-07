75d5b160ef8739ca49b6dd6b0e7a9e78822bae2e
Ruihao Chen <joechenrh@gmail.com>
2025-11-07 03:44:16 -0500

    fix test
    Signed-off-by: Ruihao Chen <joechenrh@gmail.com>

--- pkg/meta/model/job_test.go
+++ pkg/meta/model/job_test.go
@@ -163,7 +163,7 @@ func TestJobSize(t *testing.T) {
 - SubJob.ToProxyJob()
 `
 	require.Equal(t, 400, int(unsafe.Sizeof(Job{})), msg)
-	require.Equal(t, 160, int(unsafe.Sizeof(SubJob{})), msg)
+	require.Equal(t, 144, int(unsafe.Sizeof(SubJob{})), msg)
 }
 
 func TestBackfillMetaCodec(t *testing.T) {
