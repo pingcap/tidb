430b8b0916c1e714fe58e0af5011956f4cf47f77
D3Hunter <jujj603@gmail.com>
2025-11-12 14:42:02 +0800

    change

--- pkg/lightning/common/retry.go
+++ pkg/lightning/common/retry.go
@@ -19,6 +19,7 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	goerrors "errors"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -32,6 +33,8 @@ import (
 	tmysql "github.com/pingcap/tidb/pkg/errno"
 	"github.com/pingcap/tidb/pkg/ingestor/errdef"
 	drivererr "github.com/pingcap/tidb/pkg/store/driver/error"
+	"github.com/pingcap/tidb/pkg/util/logutil"
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -69,6 +72,10 @@ func isRetryableFromErrorMessage(err error) bool {
 func IsRetryableError(err error) bool {
 	for _, singleError := range errors.Errors(err) {
 		if !isSingleRetryableError(singleError) {
+			// we want to log its type and other info for better diagnosing and
+			// direct us to determine how to add to the retryable error list.
+			logutil.BgLogger().Info("meet un-retryable error", zap.Error(singleError),
+				zap.String("info", fmt.Sprintf("type: %T, value: %#v", singleError, singleError)))
 			return false
 		}
 	}
