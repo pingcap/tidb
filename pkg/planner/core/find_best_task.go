2c1ae56691316d2b63d385c7d0da07f356c3afe0
tpp <terry.purcell@pingcap.com>
2025-11-14 12:37:57 -0800

    testcase4

--- pkg/planner/core/find_best_task.go
+++ pkg/planner/core/find_best_task.go
@@ -1493,7 +1493,7 @@ func skylinePruning(ds *logicalop.DataSource, prop *property.PhysicalProperty) [
 			}
 			// Preference plans with equals/IN predicates or where there is more filtering in the index than against the table
 			indexFilters := c.equalPredicateCount() > 0 || len(c.path.TableFilters) < len(c.path.IndexFilters)
-			if preferMerge || (indexFilters && (prop.IsSortItemEmpty() || c.matchPropResult.Matched())) {
+			if preferMerge || c.path.IsSingleScan || (indexFilters && (prop.IsSortItemEmpty() || c.matchPropResult.Matched())) {
 				if !c.path.IsFullScanRange(ds.TableInfo) {
 					preferredPaths = append(preferredPaths, c)
 					hasRangeScanPath = true
