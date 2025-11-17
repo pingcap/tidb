ba58ccf5d5b53fb4e6946a4745e1808b44a0d1f6
tpp <terry.purcell@pingcap.com>
2025-11-17 12:20:31 -0800

    testcase4

--- pkg/statistics/histogram.go
+++ pkg/statistics/histogram.go
@@ -1138,38 +1138,6 @@ func (hg *Histogram) OutOfRangeRowCount(
 	// but deleted from the other, resulting in qualifying out of range rows even though
 	// realtimeRowCount is less than histogram count
 	addedRows := hg.AbsRowCountDifference(realtimeRowCount)
-	// We may have missed the true lowest/highest values due to sampling OR there could be a delay in
-	// updates to modifyCount (meaning modifyCount is incorrectly set to 0). So ensure we always
-	// account for at least 1% of the total row count as a worst case for "addedRows".
-	// We inflate this here so ONLY to impact the MaxEst value.
-	if modifyCount == 0 || addedRows == 0 {
-		if realtimeRowCount <= 0 {
-			realtimeRowCount = int64(hg.TotalRowCount())
-		}
-		// Use outOfRangeBetweenRate as a divisor to get a small percentage of the approximate
-		// modifyCount (since outOfRangeBetweenRate has a default value of 100).
-		addedRows = max(addedRows, float64(realtimeRowCount)/outOfRangeBetweenRate)
-	}
-	modifyCount = max(int64(addedRows), modifyCount)
-
-	// make sure l < r
-	if l > r {
-		return DefaultRowEst(oneValue)
-	}
-	if l == r {
-		returnEst := oneValue
-		returnMax := addedRows
-		if highIsOpenEnded { // if the high is open ended - there's a high probability that we're searching a large range
-			returnEst = addedRows * 0.5
-			returnMax = float64(modifyCount)
-		}
-		// if l ==r, it means our byte comparison is too short.
-		return RowEstimate{
-			Est:    returnEst,
-			MinEst: oneValue, // Assume a minimum of 1 row qualifies
-			MaxEst: returnMax,
-		}
-	}
 
 	// Convert the lower and upper bound of the histogram to scalar value(float64)
 	histL := convertDatumToScalar(hg.GetLower(0), commonPrefix)
@@ -1202,11 +1170,19 @@ func (hg *Histogram) OutOfRangeRowCount(
 		}()
 	}
 
-	// keep l and r unchanged, use actualL and actualR to calculate.
-	actualL := l
-	actualR := r
+	// make sure l < r
+	if l > r {
+		return DefaultRowEst(oneValue)
+	}
+	if l == r {
+		histInvalid = true
+	}
+
 	// Only attempt to calculate the ranges if the histogram is valid
 	if !histInvalid {
+		// keep l and r unchanged, use actualL and actualR to calculate.
+		actualL := l
+		actualR := r
 		// If the range overlaps with (boundL,histL), we need to handle the out-of-range part on the left of the histogram range
 		if actualL < histL && actualR > boundL {
 			// make sure boundL <= actualL < actualR <= histL
@@ -1245,6 +1221,32 @@ func (hg *Histogram) OutOfRangeRowCount(
 	// half are distributed out of range according to the diagram in the function description.
 	avgRowCount = (addedRows * addedOutOfRangePct) * totalPercent
 
+	// Use oneValue as lower bound and provide meaningful min/max estimates
+	finalEst := max(avgRowCount, oneValue)
+	// We may have missed the true lowest/highest values due to sampling OR there could be a delay in
+	// updates to modifyCount (meaning modifyCount is incorrectly set to 0). So ensure we always
+	// account for at least 1% of the total row count as a worst case for "addedRows".
+	// We inflate this here so ONLY to impact the MaxEst value.
+	if modifyCount == 0 || addedRows == 0 {
+		if realtimeRowCount <= 0 {
+			realtimeRowCount = int64(hg.TotalRowCount())
+		}
+		// Use outOfRangeBetweenRate as a divisor to get a small percentage of the approximate
+		// modifyCount (since outOfRangeBetweenRate has a default value of 100).
+		addedRows = max(addedRows, float64(realtimeRowCount)/outOfRangeBetweenRate)
+	}
+	// Maximum could be as high as all added rows.
+	maxEst := max(finalEst, addedRows)
+
+	if histInvalid {
+		modifyCount = max(modifyCount, int64(addedRows))
+		maxEst = addedRows
+		if highIsOpenEnded { // if the high is open ended - there's a high probability that we're searching a large range
+			finalEst = addedRows * 0.5
+			maxEst = float64(modifyCount)
+		}
+	}
+
 	skewRatio := sctx.GetSessionVars().RiskRangeSkewRatio
 	sctx.GetSessionVars().RecordRelevantOptVar(vardef.TiDBOptRiskRangeSkewRatio)
 	if skewRatio > 0 {
@@ -1255,11 +1257,6 @@ func (hg *Histogram) OutOfRangeRowCount(
 		result.MaxEst = max(result.Est, addedRows)
 		return result
 	}
-
-	// Use oneValue as lower bound and provide meaningful min/max estimates
-	finalEst := max(avgRowCount, oneValue)
-	// Maximum could be as high as all added rows.
-	maxEst := max(finalEst, addedRows)
 
 	return RowEstimate{
 		Est:    finalEst,
