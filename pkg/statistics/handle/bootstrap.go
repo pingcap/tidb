82e7b883c82b4fe7bcfcb9cf966073e9a9258fcf
0xPoe <techregister@pm.me>
2025-11-18 18:25:36 +0100

    fix: resolve confilcts

--- pkg/statistics/handle/bootstrap.go
+++ pkg/statistics/handle/bootstrap.go
@@ -196,10 +196,7 @@ func (h *Handle) initStatsHistograms4Chunk(is infoschema.InfoSchema, cache stats
 			if !ok {
 				continue
 			}
-<<<<<<< HEAD
 			table = table.Copy()
-=======
-			table = table.CopyAs(statistics.BothMapsWritable)
 			// Fetch table info only once per table instead of once per row
 			tblInfo, ok = h.TableInfoByIDForInitStats(is, tblID)
 			if !ok {
@@ -213,23 +210,12 @@ func (h *Handle) initStatsHistograms4Chunk(is infoschema.InfoSchema, cache stats
 		// This happens when a table is dropped but its stats metadata is not yet garbage collected.
 		if !tblInfoValid {
 			continue
->>>>>>> 9b249401c0e (statistics: cache partition IDs to global IDs map when getting the table info (#64521))
 		}
 		// All the objects in the table share the same stats version.
 		if statsVer != statistics.Version0 {
 			table.StatsVer = int(statsVer)
 		}
 		id, ndv, nullCount, version, totColSize := row.GetInt64(2), row.GetInt64(3), row.GetInt64(5), row.GetUint64(4), row.GetInt64(7)
-<<<<<<< HEAD
-		lastAnalyzePos := row.GetDatum(11, types.NewFieldType(mysql.TypeBlob))
-		tbl, ok := h.TableInfoByID(is, table.PhysicalID)
-		if !ok {
-			// this table has been dropped. but stats meta still exists and wait for being deleted.
-			logutil.BgLogger().Warn("cannot find this table when to init stats", zap.Int64("tableID", table.PhysicalID))
-			continue
-		}
-=======
->>>>>>> 9b249401c0e (statistics: cache partition IDs to global IDs map when getting the table info (#64521))
 		if row.GetInt64(1) > 0 {
 			var idxInfo *model.IndexInfo
 			for _, idx := range tblInfo.Meta().Indices {
