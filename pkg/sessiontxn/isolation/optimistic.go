5076f2eb91a7f527995bc214747179433e3aeefc
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-11-28 15:24:11 +0800

    plugin: add retry information in the plugin completed event. (#64692) (#64725)
    close pingcap/tidb#64691

--- pkg/sessiontxn/isolation/optimistic.go
+++ pkg/sessiontxn/isolation/optimistic.go
@@ -17,6 +17,7 @@ package isolation
 import (
 	"math"
 
+	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	plannercore "github.com/pingcap/tidb/pkg/planner/core"
@@ -48,6 +49,9 @@ func (p *OptimisticTxnContextProvider) ResetForNewTxn(sctx sessionctx.Context, c
 func (p *OptimisticTxnContextProvider) onTxnActive(_ kv.Transaction, tp sessiontxn.EnterNewTxnType) {
 	sessVars := p.sctx.GetSessionVars()
 	sessVars.TxnCtx.CouldRetry = isOptimisticTxnRetryable(sessVars, tp)
+	failpoint.Inject("injectOptimisticTxnRetryable", func(val failpoint.Value) {
+		sessVars.TxnCtx.CouldRetry = val.(bool)
+	})
 }
 
 // isOptimisticTxnRetryable (if returns true) means the transaction could retry.
