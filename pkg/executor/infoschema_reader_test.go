c6266c6e316b28a01ad4f3faf8575d9dff7b1233
Jianjun Liao <jianjun.liao@outlook.com>
2025-12-02 18:17:22 +0800

    fix unit test
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- pkg/executor/infoschema_reader_test.go
+++ pkg/executor/infoschema_reader_test.go
@@ -665,7 +665,7 @@ func TestColumnTable(t *testing.T) {
 		testkit.RowsWithSep("|",
 			"test|tbl1|col_2"))
 	tk.MustQuery(`select count(*) from information_schema.columns;`).Check(
-		testkit.RowsWithSep("|", "4986"))
+		testkit.RowsWithSep("|", "4987"))
 }
 
 func TestIndexUsageTable(t *testing.T) {
