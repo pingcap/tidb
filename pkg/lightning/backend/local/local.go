3fc27f03e1c1d6a552f22fbf90871a53334c1b10
wjhuang2016 <huangwenjun1997@gmail.com>
2025-12-03 20:19:46 +0800

    debug
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- pkg/lightning/backend/local/local.go
+++ pkg/lightning/backend/local/local.go
@@ -1196,6 +1196,12 @@ func (local *Backend) generateJobForRange(
 
 	startKey := codec.EncodeBytes([]byte{}, pairStart)
 	endKey := codec.EncodeBytes([]byte{}, nextKey(pairEnd))
+	for _, rangeKey := range sortedJobRanges {
+		tidblogutil.Logger(ctx).Info("key range",
+			logutil.Key("startKey", rangeKey.Start),
+			logutil.Key("endKey", rangeKey.End),
+		)
+	}
 	regions, err := split.PaginateScanRegion(ctx, local.splitCli, startKey, endKey, scanRegionLimit)
 	if err != nil {
 		tidblogutil.Logger(ctx).Error("scan region failed",
@@ -1203,6 +1209,12 @@ func (local *Backend) generateJobForRange(
 			logutil.Key("startKey", startKey),
 			logutil.Key("endKey", endKey))
 		return nil, err
+	}
+	for _, region := range regions {
+		tidblogutil.Logger(ctx).Info("region",
+			logutil.Key("startKey", region.Region.GetStartKey()),
+			logutil.Key("endKey", region.Region.GetEndKey()),
+		)
 	}
 
 	jobs := newRegionJobs(regions, data, sortedJobRanges, regionSplitSize, regionSplitKeys, local.metrics)
@@ -1213,6 +1225,12 @@ func (local *Backend) generateJobForRange(
 		zap.String("startKeyOfFirstRegion", hex.EncodeToString(regions[0].Region.GetStartKey())),
 		zap.String("endKeyOfLastRegion", hex.EncodeToString(regions[len(regions)-1].Region.GetEndKey())),
 	)
+	for _, job := range jobs {
+		tidblogutil.Logger(ctx).Info("job",
+			logutil.Key("startKey", job.keyRange.Start),
+			logutil.Key("endKey", job.keyRange.End),
+		)
+	}
 	return jobs, nil
 }
 
