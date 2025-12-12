f8fe30ffd87658795d3fa43e1df6dbc23f12b872
Ruihao Chen <joechenrh@gmail.com>
2025-12-11 22:07:25 -0500

    fixup
    Signed-off-by: Ruihao Chen <joechenrh@gmail.com>

--- pkg/statistics/handle/cache/internal/lfu/lfu_cache.go
+++ pkg/statistics/handle/cache/internal/lfu/lfu_cache.go
@@ -15,7 +15,6 @@
 package lfu
 
 import (
-	"math/rand/v2"
 	"sync"
 	"sync/atomic"
 
@@ -27,6 +26,7 @@ import (
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/pingcap/tidb/pkg/util/memory"
 	"go.uber.org/zap"
+	"golang.org/x/exp/rand"
 )
 
 // LFU is a LFU based on the ristretto.Cache
