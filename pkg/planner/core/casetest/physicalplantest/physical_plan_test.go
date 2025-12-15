d6a8d68115ffaf6a35e7272026aba30174558ffb
Weizhen Wang <wangweizhen@pingcap.com>
2025-12-15 13:23:02 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/casetest/physicalplantest/physical_plan_test.go
+++ pkg/planner/core/casetest/physicalplantest/physical_plan_test.go
@@ -248,18 +248,15 @@ func TestIssue37520(t *testing.T) {
 }
 
 func TestMPPHints(t *testing.T) {
-	store := testkit.CreateMockStore(t, mockstore.WithMockTiFlash(2))
+	store, dom := testkit.CreateMockStoreAndDomain(t)
 	tk := testkit.NewTestKit(t, store)
 	tk.MustExec("use test")
 	tk.MustExec("set tidb_cost_model_version=2")
 	tk.MustExec("create table t (a int, b int, c int, index idx_a(a), index idx_b(b))")
-	tk.MustExec("alter table t set tiflash replica 1")
 	tk.MustExec("set @@session.tidb_allow_mpp=ON")
 	tk.MustExec("create definer='root'@'localhost' view v as select a, sum(b) from t group by a, c;")
 	tk.MustExec("create definer='root'@'localhost' view v1 as select t1.a from t t1, t t2 where t1.a=t2.a;")
-	tb := external.GetTableByName(t, tk, "test", "t")
-	err := domain.GetDomain(tk.Session()).DDLExecutor().UpdateTableReplicaInfo(tk.Session(), tb.Meta().ID, true)
-	require.NoError(t, err)
+	testkit.SetTiFlashReplica(t, dom, "test", "t")
 
 	var input []string
 	var output []struct {
@@ -1549,4 +1546,23 @@ func TestDisableReuseChunk(t *testing.T) {
 	core.MaxMemoryLimitForOverlongType = 500 * size.GB
 	tk.MustQuery(` select * from t1 where c1 = 1 and c2 = "abc";`).Check(testkit.Rows("1 abc"))
 	tk.MustQuery(`select @@last_sql_use_alloc`).Check(testkit.Rows("0"))
+}
+
+func TestSemiJoinRewriter(t *testing.T) {
+	testkit.RunTestUnderCascades(t, func(t *testing.T, tk *testkit.TestKit, cascades, caller string) {
+		tk.MustExec("use test")
+		tk.MustExec(`set @@tidb_opt_enable_semi_join_rewrite=on;`)
+		tk.MustExec(`create table t1(a int);`)
+		tk.MustExec(`create table t2(a varchar(10));`)
+		tk.MustExec(`create table t3(a int);`)
+		tk.MustQuery(`explain format = 'plan_tree' select * from t1 where exists(select 1 from t2 where t1.a=t2.a);`).Check(testkit.Rows(
+			`HashJoin root  inner join, equal:[eq(Column#6, Column#7)]`,
+			`├─HashAgg(Build) root  group by:Column#7, funcs:firstrow(Column#7)->Column#7`,
+			`│ └─Projection root  cast(test.t2.a, double BINARY)->Column#7`,
+			`│   └─TableReader root  data:TableFullScan`,
+			`│     └─TableFullScan cop[tikv] table:t2 keep order:false, stats:pseudo`,
+			`└─Projection(Probe) root  test.t1.a, cast(test.t1.a, double BINARY)->Column#6`,
+			`  └─TableReader root  data:TableFullScan`,
+			`    └─TableFullScan cop[tikv] table:t1 keep order:false, stats:pseudo`))
+	})
 }
