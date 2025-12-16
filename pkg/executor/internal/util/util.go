8e995d147b5606dee33e1e9205bf0a98909d0689
Weizhen Wang <wangweizhen@pingcap.com>
2025-12-16 13:27:51 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/executor/internal/util/util.go
+++ pkg/executor/internal/util/util.go
@@ -24,7 +24,6 @@ import (
 	"testing"
 
 	"github.com/pingcap/log"
-	"github.com/pingcap/tidb/pkg/config"
 	"github.com/stretchr/testify/require"
 )
 
@@ -37,8 +36,7 @@ func GetFunctionName() string {
 // CheckNoLeakFiles checks if there are file leaks
 func CheckNoLeakFiles(t *testing.T, fileNamePrefixForTest string) {
 	path := t.TempDir()
-	log.Info(fmt.Sprintf("path: %s", path)
-
+	log.Info(fmt.Sprintf("path: %s", path))
 	err := filepath.WalkDir(path, func(_ string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
