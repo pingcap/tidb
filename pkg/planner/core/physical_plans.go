762e2422853fe36197236ee9acea983a07871a50
Chao Wang <cclcwangchao@hotmail.com>
2025-12-22 18:39:50 +0800

    *: fix index lookup pushdown error for partition table + uncomitted rows (#65162)
    close pingcap/tidb#65155

--- pkg/planner/core/physical_plans.go
+++ pkg/planner/core/physical_plans.go
@@ -3130,7 +3130,24 @@ func buildPushDownIndexLookUpPlan(
 		// - If common handle, we don't need to set the indexHandleOffsets to build the common handle key
 		// which can be read from the index value directly.
 		// - If int handle, it is the last column in the index schema.
-		indexHandleOffsets = []uint32{uint32(indexPlan.Schema().Len()) - 1}
+		//   - If the last column is ExtraHandleID, or a non-negative column ID, handle is the last column.
+		//   - Otherwise, we need to find the last column whose ID is not ExtraHandleID and is negative.
+		//     For example, when a partition table needs to append ExtraPhysTblID
+		//     to the end for the upper UnionScanExec.
+		offset := indexPlan.Schema().Len() - 1
+		for offset >= 0 {
+			col := indexPlan.Schema().Columns[offset]
+			if col.ID >= 0 || col.ID == model.ExtraHandleID {
+				break
+			}
+			offset--
+			intest.Assert(offset >= 0, "cannot find handle column in index schema")
+		}
+
+		if offset < 0 {
+			return nil, errors.New("cannot find handle column in index schema")
+		}
+		indexHandleOffsets = []uint32{uint32(offset)}
 	}
 
 	tableScanPlan, parentOfTableScan := detachRootTableScanPlan(tablePlan)
