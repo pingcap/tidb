715c37cf9c7cf41592746fc2bb71245fbc8e2875
Ruihao Chen <joechenrh@gmail.com>
2025-12-25 09:19:46 -0500

    redact url
    Signed-off-by: Ruihao Chen <joechenrh@gmail.com>

--- pkg/parser/ast/misc_test.go
+++ pkg/parser/ast/misc_test.go
@@ -372,6 +372,9 @@ func TestRedactURL(t *testing.T) {
 		// underline
 		{args{"s3://bucket/file?access_key=123"}, "s3://bucket/file?access_key=xxxxxx"},
 		{args{"s3://bucket/file?secret_access_key=123"}, "s3://bucket/file?secret_access_key=xxxxxx"},
+		{args{"azure://bucket/file?sas-token=123"}, "azure://bucket/file?sas-token=xxxxxx"},
+		{args{"azblob://bucket/file?sas-token=123"}, "azblob://bucket/file?sas-token=xxxxxx"},
+		{args{"azure://bucket/file?account-name=test&sas_token=123"}, "azure://bucket/file?account-name=test&sas_token=xxxxxx"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.args.str, func(t *testing.T) {
