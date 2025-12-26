641629877fb414131568637a6cd1be22eade8ca9
Ruihao Chen <joechenrh@gmail.com>
2025-12-25 22:11:54 -0500

    chore: change bucket to container
    Signed-off-by: Ruihao Chen <joechenrh@gmail.com>

--- pkg/parser/ast/misc_test.go
+++ pkg/parser/ast/misc_test.go
@@ -395,8 +395,8 @@ func TestRedactURL(t *testing.T) {
 		{args{"s3://bucket/file?access_key=123"}, "s3://bucket/file?access_key=xxxxxx"},
 		{args{"s3://bucket/file?secret_access_key=123"}, "s3://bucket/file?secret_access_key=xxxxxx"},
 		{args{"azure://bucket/file?sas-token=123"}, "azure://bucket/file?sas-token=xxxxxx"},
-		{args{"azblob://bucket/file?sas-token=123"}, "azblob://bucket/file?sas-token=xxxxxx"},
-		{args{"azure://bucket/file?account-name=test&sas_token=123"}, "azure://bucket/file?account-name=test&sas_token=xxxxxx"},
+		{args{"azblob://container/file?sas-token=123"}, "azblob://container/file?sas-token=xxxxxx"},
+		{args{"azure://container/file?account-name=test&sas_token=123"}, "azure://container/file?account-name=test&sas_token=xxxxxx"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.args.str, func(t *testing.T) {
