327344f629a36e261d6cede7a65e84473544d563
Weizhen Wang <wangweizhen@pingcap.com>
2025-12-26 18:55:28 +0800

    chore: bazel mirror
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/logical_plan_builder.go
+++ pkg/planner/core/logical_plan_builder.go
@@ -522,7 +522,7 @@ func setPreferredStoreType(ds *logicalop.DataSource, hintInfo *h.PlanHints) {
 			ds.SCtx().GetSessionVars().RaiseWarningWhenMPPEnforced("MPP mode may be blocked because you have set a hint to read table `" + hintTbl.TblName.O + "` from TiKV.")
 		}
 	}
-	if hintTbl := hintInfo.IfPreferTiFlash(alias); hintTbl != nil || ds.SCtx().GetSessionVars().IsMPPEnforced() {
+	if hintTbl := hintInfo.IfPreferTiFlash(alias); hintTbl != nil {
 		// `ds.PreferStoreType != 0`, which means there's a hint hit the both TiKV value and TiFlash value for table.
 		// We can't support read a table from two different storages, even partition table.
 		if ds.PreferStoreType != 0 {
@@ -535,9 +535,7 @@ func setPreferredStoreType(ds *logicalop.DataSource, hintInfo *h.PlanHints) {
 		for _, path := range ds.AllPossibleAccessPaths {
 			if path.StoreType == kv.TiFlash {
 				ds.PreferStoreType |= h.PreferTiFlash
-				if hintTbl != nil {
-					ds.PreferPartitions[h.PreferTiFlash] = hintTbl.Partitions
-				}
+				ds.PreferPartitions[h.PreferTiFlash] = hintTbl.Partitions
 				break
 			}
 		}
