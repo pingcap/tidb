1a92fa620b421aabd81ab7a49325197ab277ce27
qw4990 <zhangyuanjia@pingcap.com>
2026-01-06 21:16:51 +0800

    fixup

--- pkg/planner/core/rule_eliminate_projection.go
+++ pkg/planner/core/rule_eliminate_projection.go
@@ -176,7 +176,8 @@ func (pe *ProjectionEliminator) eliminate(p base.LogicalPlan, replace map[string
 		p.Children()[i] = pe.eliminate(child, replace, childFlag, opt)
 	}
 
-	// Replace all columns in the schema with the replaced columns.	switch x := p.(type) {
+	// Replace all columns in the schema with the replaced columns.
+	switch x := p.(type) {
 	case *logicalop.LogicalApply:
 		x.SetSchema(logicalop.BuildLogicalJoinSchema(x.JoinType, x))
 	default:
