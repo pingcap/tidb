07d345f5d6597f6337b2a6fcb6ec70579e4ea9c5
Weizhen Wang <wangweizhen@pingcap.com>
2026-01-12 13:59:16 +0800

    variable: ignore internal/poll.runtime_pollWait in the goleak
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/sessionctx/variable/tests/main_test.go
+++ pkg/sessionctx/variable/tests/main_test.go
@@ -29,7 +29,6 @@ func TestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
-		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
 	}
 	goleak.VerifyTestMain(m, opts...)
 }
