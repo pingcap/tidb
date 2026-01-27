82a5c1e4ddf4f074a7709076b8768e1b2bf06947
0xPoe <techregister@pm.me>
2026-01-27 20:53:06 +0100

    fix: rename

--- pkg/statistics/handle/autoanalyze/priorityqueue/intervaltimezone/main_test.go
+++ pkg/statistics/handle/autoanalyze/priorityqueue/intervaltimezone/main_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package interval_timezone_test
+package intervaltimezone_test
 
 import (
 	"testing"
