876b1ad105104220c087ec1e0d7292e5fb82593a
Yiding Cui <winoros@gmail.com>
2026-01-29 03:24:11 +0800

    Update worker pool size expectations in global_sort_test

--- tests/realtikvtest/addindextest2/global_sort_test.go
+++ tests/realtikvtest/addindextest2/global_sort_test.go
@@ -634,8 +634,8 @@ func TestAlterJobOnDXFWithGlobalSort(t *testing.T) {
 	testfailpoint.EnableCall(t, "github.com/pingcap/tidb/pkg/ddl/afterPipeLineClose", func(pipe *operator.AsyncPipeline) {
 		pipeClosed = true
 		reader, writer := pipe.GetReaderAndWriter()
-		require.EqualValues(t, 4, reader.GetWorkerPoolSize())
-		require.EqualValues(t, 6, writer.GetWorkerPoolSize())
+		require.EqualValues(t, 8, reader.GetWorkerPoolSize())
+		require.EqualValues(t, 8, writer.GetWorkerPoolSize())
 	})
 
 	// Change the batch size and concurrency during table scanning and check the modified parameters.
