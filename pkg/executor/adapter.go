c09ef969de86a628bc3abaf2e6199e1abe70cde2
Lynn <zimu_xia@126.com>
2026-02-03 12:01:34 +0800

    executor: add a comment

--- pkg/executor/adapter.go
+++ pkg/executor/adapter.go
@@ -2173,6 +2173,7 @@ func (a *ExecStmt) observeStmtBeginForTopProfiling(ctx context.Context) context.
 	stats := a.Ctx.GetStmtStats()
 	if !topsqlstate.TopProfilingEnabled() {
 		// Always attach the SQL and plan info uses to catch the running SQL when Top Profiling is enabled in execution.
+		// Note: Goroutine labels for CPU profiling are only set when TopSQL is enabled.
 		if stats != nil {
 			stats.OnExecutionBegin(sqlDigestByte, planDigestByte, vars.InPacketBytes.Load())
 		}
