98576f04986451b04f66a7ec610662a6b3e22505
wjhuang2016 <huangwenjun1997@gmail.com>
2026-02-06 03:42:16 +0800

    tests: update infoschema.columns count after mview system tables

--- pkg/executor/infoschema_reader_test.go
+++ pkg/executor/infoschema_reader_test.go
@@ -661,7 +661,7 @@ func TestColumnTable(t *testing.T) {
 		testkit.RowsWithSep("|",
 			"test|tbl1|col_2"))
 	tk.MustQuery(`select count(*) from information_schema.columns;`).Check(
-		testkit.RowsWithSep("|", "4986"))
+		testkit.RowsWithSep("|", "5015"))
 }
 
 func TestIndexUsageTable(t *testing.T) {
