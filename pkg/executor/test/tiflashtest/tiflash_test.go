c70264a78a0a98acaa3410719dad39f822483b0c
time-and-fate <25057648+time-and-fate@users.noreply.github.com>
2026-02-06 09:00:48 +0800

    update

--- pkg/executor/test/tiflashtest/tiflash_test.go
+++ pkg/executor/test/tiflashtest/tiflash_test.go
@@ -2018,20 +2018,20 @@ func TestMppAggShouldAlignFinalMode(t *testing.T) {
 	tk.MustQuery("explain format='brief' select 1 from (" +
 		"  select /*+ read_from_storage(tiflash[t]) */ sum(1)" +
 		"  from t where d BETWEEN '2023-07-01' and '2023-07-03' group by d" +
-		") total;").Check(testkit.Rows("Projection 400.00 root  1->Column#4",
-		"└─HashAgg 400.00 root  group by:test.t.d, funcs:count(complete,1)->Column#8",
+		") total;").Check(testkit.Rows("Projection 400.00 root  1->Column#5",
+		"└─HashAgg 400.00 root  group by:test.t.d, funcs:count(complete,1)->Column#9",
 		"  └─PartitionUnion 400.00 root  ",
 		"    ├─Projection 200.00 root  test.t.d",
-		"    │ └─HashAgg 200.00 root  group by:test.t.d, funcs:firstrow(partial2,test.t.d)->test.t.d, funcs:count(final,Column#12)->Column#9",
+		"    │ └─HashAgg 200.00 root  group by:test.t.d, funcs:firstrow(partial2,test.t.d)->test.t.d, funcs:count(final,Column#13)->Column#10",
 		"    │   └─TableReader 200.00 root  MppVersion: 2, data:ExchangeSender",
 		"    │     └─ExchangeSender 200.00 mpp[tiflash]  ExchangeType: PassThrough",
 		"    │       └─HashAgg 200.00 mpp[tiflash]  group by:test.t.d, funcs:count(partial1,1)->Column#12",
 		"    │         └─TableRangeScan 250.00 mpp[tiflash] table:t, partition:p1 range:[2023-07-01,2023-07-03], keep order:false, stats:pseudo",
 		"    └─Projection 200.00 root  test.t.d",
-		"      └─HashAgg 200.00 root  group by:test.t.d, funcs:firstrow(partial2,test.t.d)->test.t.d, funcs:count(final,Column#14)->Column#10",
+		"      └─HashAgg 200.00 root  group by:test.t.d, funcs:firstrow(partial2,test.t.d)->test.t.d, funcs:count(final,Column#15)->Column#11",
 		"        └─TableReader 200.00 root  MppVersion: 2, data:ExchangeSender",
 		"          └─ExchangeSender 200.00 mpp[tiflash]  ExchangeType: PassThrough",
-		"            └─HashAgg 200.00 mpp[tiflash]  group by:test.t.d, funcs:count(partial1,1)->Column#14",
+		"            └─HashAgg 200.00 mpp[tiflash]  group by:test.t.d, funcs:count(partial1,1)->Column#15",
 		"              └─TableRangeScan 250.00 mpp[tiflash] table:t, partition:p2 range:[2023-07-01,2023-07-03], keep order:false, stats:pseudo"))
 
 	err = failpoint.Disable("github.com/pingcap/tidb/pkg/expression/aggregation/show-agg-mode")
