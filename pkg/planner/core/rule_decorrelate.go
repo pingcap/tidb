2d7e704b5ef1fc921ab3a7b63e21a38dd1ab8a80
AilinKid <314806019@qq.com>
2026-02-11 15:33:09 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

--- pkg/planner/core/rule_decorrelate.go
+++ pkg/planner/core/rule_decorrelate.go
@@ -231,7 +231,6 @@ func (s *DecorrelateSolver) optimize(ctx context.Context, p base.LogicalPlan, gr
 			join := &apply.LogicalJoin
 			join.SetSelf(join)
 			join.SetTP(plancodec.TypeJoin)
-			join.SetFlag(logicalop.JoinGenFromApplyFlag)
 			p = join
 		} else if apply.NoDecorrelate {
 			goto NoOptimize
