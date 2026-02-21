77d299b8626b1223b5d5b67f69f0f19c26f1e673
Qi Liu <qiliu@Qis-MacBook-Pro.local>
2026-02-21 17:03:42 +0800

    parser: rename ddl_stmt_parser.go, extract lexer literal converters

--- pkg/parser/parser_api.go
+++ pkg/parser/parser_api.go
@@ -15,16 +15,13 @@ package parser
 
 import (
 	"fmt"
-	"math"
 	"regexp"
-	"strconv"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/pkg/parser/ast"
 	"github.com/pingcap/tidb/pkg/parser/charset"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/parser/terror"
-	"github.com/pingcap/tidb/pkg/parser/types"
 )
 
 var (
@@ -246,80 +243,6 @@ func (parser *Parser) parseHint(input string) ([]*ast.TableOptimizerHint, []erro
 		parser.hintParser = newHintParser()
 	}
 	return parser.hintParser.parse(input, parser.lexer.GetSQLMode(), parser.lexer.lastHintPos)
-}
-
-// ---------- Literal conversion helpers (used by Scanner.Lex in lexer.go) ----------
-
-func toInt(l Lexer, lval *Token, str string) int {
-	n, err := strconv.ParseUint(str, 10, 64)
-	if err != nil {
-		e := err.(*strconv.NumError)
-		if e.Err == strconv.ErrRange {
-			return toDecimal(l, lval, str)
-		}
-		l.AppendError(l.Errorf("integer literal: %v", err))
-		return invalid
-	}
-
-	switch {
-	case n <= math.MaxInt64:
-		lval.Item = int64(n)
-	default:
-		lval.Item = n
-	}
-	return intLit
-}
-
-func toDecimal(l Lexer, lval *Token, str string) int {
-	dec, err := ast.NewDecimal(str)
-	if err != nil {
-		if terror.ErrorEqual(err, types.ErrDataOutOfRange) {
-			l.AppendWarn(types.ErrTruncatedWrongValue.FastGenByArgs("DECIMAL", dec))
-			dec, _ = ast.NewDecimal(mysql.DefaultDecimal)
-		} else {
-			l.AppendError(l.Errorf("decimal literal: %v", err))
-		}
-	}
-	lval.Item = dec
-	return decLit
-}
-
-func toFloat(l Lexer, lval *Token, str string) int {
-	n, err := strconv.ParseFloat(str, 64)
-	if err != nil {
-		e := err.(*strconv.NumError)
-		if e.Err == strconv.ErrRange {
-			l.AppendError(types.ErrIllegalValueForType.GenWithStackByArgs("double", str))
-			return invalid
-		}
-		l.AppendError(l.Errorf("float literal: %v", err))
-		return invalid
-	}
-
-	lval.Item = n
-	return floatLit
-}
-
-// See https://dev.mysql.com/doc/refman/5.7/en/hexadecimal-literals.html
-func toHex(l Lexer, lval *Token, str string) int {
-	h, err := ast.NewHexLiteral(str)
-	if err != nil {
-		l.AppendError(l.Errorf("hex literal: %v", err))
-		return invalid
-	}
-	lval.Item = h
-	return hexLit
-}
-
-// See https://dev.mysql.com/doc/refman/5.7/en/bit-type.html
-func toBit(l Lexer, lval *Token, str string) int {
-	b, err := ast.NewBitLiteral(str)
-	if err != nil {
-		l.AppendError(l.Errorf("bit literal: %v", err))
-		return invalid
-	}
-	lval.Item = b
-	return bitLit
 }
 
 // ---------- ParseParam types ----------
