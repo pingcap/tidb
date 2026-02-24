92beac71e6ac9c523d46e06dcf86d20d6355f1b4
gmhdbjd <gmhdbjd@gmail.com>
2026-02-24 22:03:56 +0800

    planner: add raw schema for SHOW IMPORT JOBS

--- pkg/planner/core/operator/logicalop/logical_show.go
+++ pkg/planner/core/operator/logicalop/logical_show.go
@@ -57,6 +57,7 @@ type ShowContents struct {
 	Limit       *ast.Limit // Used for limit Result Set row number.
 
 	ImportJobID       *int64 // Used for SHOW LOAD DATA JOB <jobID>
+	ImportJobRaw      bool   // Used for SHOW RAW IMPORT JOB(S)
 	ImportGroupKey    string // Used for SHOW IMPORT GROUP <GROUP_KEY>
 	DistributionJobID *int64 // Used for SHOW DISTRIBUTION JOB <JobID>
 }
