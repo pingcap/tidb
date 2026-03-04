0dabfc6f2e9dc15b5f3f976f6882114b7e5d902f
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2026-03-04 23:52:18 +0800

    planner: enable tidb_ignore_inlist_plan_digest by default to improve user experience and add more test cases (#66624)
    close pingcap/tidb#66623

--- pkg/sessionctx/variable/tidb_vars.go
+++ pkg/sessionctx/variable/tidb_vars.go
@@ -1465,7 +1465,7 @@ const (
 	DefTiDBStmtSummaryMaxStmtCount                    = 3000
 	DefTiDBStmtSummaryMaxSQLLength                    = 4096
 	DefTiDBCapturePlanBaseline                        = Off
-	DefTiDBIgnoreInlistPlanDigest                     = false
+	DefTiDBIgnoreInlistPlanDigest                     = true
 	DefTiDBEnableIndexMerge                           = true
 	DefEnableLegacyInstanceScope                      = true
 	DefTiDBTableCacheLease                            = 3 // 3s
