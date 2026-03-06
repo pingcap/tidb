b63acd2a42bb6dfbaebf92577ebde33edc7b6360
muyun.pan <muyun.pan@pingcap.cn>
2026-03-06 20:08:04 +0800

    !214 [dumpling]: 增加 eal2 功能 - 数据导出带权限数据 (!580)
    Reviewed-on: https://git.pingcap.net/pingkai/tidb/pulls/580
    Reviewed-by: crazycs520 <chenshuang@pingcap.cn>
    Co-authored-by: muyun.pan <muyun.pan@pingcap.cn>
    Co-committed-by: muyun.pan <muyun.pan@pingcap.cn>

--- dumpling/export/dump.go
+++ dumpling/export/dump.go
@@ -312,6 +312,12 @@ func (d *Dumper) Dump() (dumpErr error) {
 	})
 	baseConn := newBaseConn(metaConn, true, rebuildMetaConn)
 
+	am := newAccessMeta(conf)
+	err = am.getUserGrants(metaConn)
+	if err != nil {
+		return err
+	}
+
 	if conf.SQL == "" {
 		if err = d.dumpDatabases(writerCtx, baseConn, taskIn); err != nil && !errors.ErrorEqual(err, context.Canceled) {
 			return err
@@ -333,6 +339,12 @@ func (d *Dumper) Dump() (dumpErr error) {
 
 	summary.SetSuccessStatus(true)
 	m.recordFinishTime(time.Now())
+	am.setDumpEndTime()
+	err = am.writeAccessMeta()
+	if err != nil {
+		tctx.L().Error("write access meta fail ," + err.Error())
+		return err
+	}
 	return nil
 }
 
