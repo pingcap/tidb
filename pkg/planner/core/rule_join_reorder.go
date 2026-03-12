f3f06c62c55d9c402679b6a3d6af8f8cd5ca9018
guo-shaoge <shaoge1994@163.com>
2026-03-12 09:06:35 +0800

    sysvar
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- pkg/planner/core/rule_join_reorder.go
+++ pkg/planner/core/rule_join_reorder.go
@@ -288,7 +288,7 @@ type joinTypeWithExtMsg struct {
 
 // Optimize implements the base.LogicalOptRule.<0th> interface.
 func (s *JoinReOrderSolver) Optimize(_ context.Context, p base.LogicalPlan) (base.LogicalPlan, bool, error) {
-	if p.SCtx().GetSessionVars().OptEnableAdvancedJoinReorder {
+	if p.SCtx().GetSessionVars().TiDBOptEnableAdvancedJoinReorder {
 		p, err := joinorder.Optimize(p)
 		return p, false, err
 	}
