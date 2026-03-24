ed495d6f2698464a3aa4e6316452d27838963f22
OliverS929 <182192954+OliverS929@users.noreply.github.com>
2026-03-24 14:16:18 +0800

    objstore: disable HTTP/2 for GCS clients (#67135)
    ref pingcap-inc/tici#868, ref pingcap/tidb#63937

--- pkg/objstore/gcs.go
+++ pkg/objstore/gcs.go
@@ -17,6 +17,7 @@ package objstore
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	goerrors "errors"
 	"fmt"
 	"io"
@@ -449,33 +450,27 @@ skipHandleCred:
 		clientOps = append(clientOps, option.WithEndpoint(gcs.Endpoint))
 	}
 
-	httpClient := opts.HTTPClient
+	httpClient := prepareGCSHTTPClient(opts.HTTPClient)
 	if opts.AccessRecording != nil {
-		if httpClient == nil {
-			transport, _ := http.DefaultTransport.(*http.Transport)
-			httpClient = &http.Client{Transport: transport.Clone()}
-		}
 		httpClient.Transport = &roundTripperWrapper{
 			RoundTripper: httpClient.Transport,
 			accessRec:    opts.AccessRecording,
 		}
 	}
-	if httpClient != nil {
-		// see https://github.com/pingcap/tidb/issues/47022#issuecomment-1722913455
-		// https://www.googleapis.com/auth/cloud-platform must be set to use service_account
-		// type of credential-file.
-		newTransport, err := htransport.NewTransport(ctx, httpClient.Transport,
-			append(clientOps, option.WithScopes(storage.ScopeFullControl, "https://www.googleapis.com/auth/cloud-platform"))...)
-		if err != nil {
-			if intest.InTest && !mustReportCredErr {
-				goto skipHandleTransport
-			}
-			return nil, errors.Trace(err)
+	// see https://github.com/pingcap/tidb/issues/47022#issuecomment-1722913455
+	// https://www.googleapis.com/auth/cloud-platform must be set to use service_account
+	// type of credential-file.
+	newTransport, err := htransport.NewTransport(ctx, httpClient.Transport,
+		append(clientOps, option.WithScopes(storage.ScopeFullControl, "https://www.googleapis.com/auth/cloud-platform"))...)
+	if err != nil {
+		if intest.InTest && !mustReportCredErr {
+			goto skipHandleTransport
 		}
-		httpClient.Transport = newTransport
-	skipHandleTransport:
-		clientOps = append(clientOps, option.WithHTTPClient(httpClient))
+		return nil, errors.Trace(err)
 	}
+	httpClient.Transport = newTransport
+skipHandleTransport:
+	clientOps = append(clientOps, option.WithHTTPClient(httpClient))
 
 	if !opts.SendCredentials {
 		// Clear the credentials if exists so that they will not be sent to TiKV
@@ -493,6 +488,96 @@ skipHandleCred:
 		return nil, errors.Trace(err)
 	}
 	return ret, nil
+}
+
+func prepareGCSHTTPClient(base *http.Client) *http.Client {
+	if base == nil {
+		return &http.Client{Transport: newGCSHTTPTransport(nil)}
+	}
+	cloned := *base
+	switch transport := cloned.Transport.(type) {
+	case nil:
+		cloned.Transport = newGCSHTTPTransport(nil)
+	case *http.Transport:
+		cloned.Transport = newGCSHTTPTransport(transport)
+	}
+	return &cloned
+}
+
+func newGCSHTTPTransport(base *http.Transport) *http.Transport {
+	// Avoid Transport.Clone here: disabling HTTP/2 on a cloned default transport
+	// can trigger malformed HTTP/1.x responses against storage.googleapis.com.
+	if base == nil {
+		baseTransport, _ := http.DefaultTransport.(*http.Transport)
+		base = baseTransport
+	}
+
+	transport := &http.Transport{
+		Proxy:               http.ProxyFromEnvironment,
+		MaxIdleConnsPerHost: http.DefaultMaxIdleConnsPerHost,
+	}
+	if base != nil {
+		transport = &http.Transport{
+			Proxy:                  base.Proxy,
+			OnProxyConnectResponse: base.OnProxyConnectResponse,
+			DialContext:            base.DialContext,
+			Dial:                   base.Dial,
+			DialTLSContext:         base.DialTLSContext,
+			DialTLS:                base.DialTLS,
+			TLSClientConfig:        cloneGCSHTTPClientTLSConfig(base.TLSClientConfig),
+			TLSHandshakeTimeout:    base.TLSHandshakeTimeout,
+			DisableKeepAlives:      base.DisableKeepAlives,
+			DisableCompression:     base.DisableCompression,
+			MaxIdleConns:           base.MaxIdleConns,
+			MaxIdleConnsPerHost:    base.MaxIdleConnsPerHost,
+			MaxConnsPerHost:        base.MaxConnsPerHost,
+			IdleConnTimeout:        base.IdleConnTimeout,
+			ResponseHeaderTimeout:  base.ResponseHeaderTimeout,
+			ExpectContinueTimeout:  base.ExpectContinueTimeout,
+			ProxyConnectHeader:     base.ProxyConnectHeader.Clone(),
+			GetProxyConnectHeader:  base.GetProxyConnectHeader,
+			MaxResponseHeaderBytes: base.MaxResponseHeaderBytes,
+			WriteBufferSize:        base.WriteBufferSize,
+			ReadBufferSize:         base.ReadBufferSize,
+		}
+	}
+	disableGCSHTTP2(transport)
+	return transport
+}
+
+func cloneGCSHTTPClientTLSConfig(cfg *tls.Config) *tls.Config {
+	if cfg == nil {
+		return nil
+	}
+	cloned := cfg.Clone()
+	if len(cloned.NextProtos) == 0 {
+		return cloned
+	}
+	filtered := make([]string, 0, len(cloned.NextProtos))
+	hasHTTP1 := false
+	for _, proto := range cloned.NextProtos {
+		if proto == "h2" {
+			continue
+		}
+		if proto == "http/1.1" {
+			hasHTTP1 = true
+		}
+		filtered = append(filtered, proto)
+	}
+	if !hasHTTP1 {
+		filtered = append(filtered, "http/1.1")
+	}
+	cloned.NextProtos = filtered
+	return cloned
+}
+
+func disableGCSHTTP2(transport *http.Transport) {
+	transport.ForceAttemptHTTP2 = false
+	protocols := new(http.Protocols)
+	protocols.SetHTTP1(true)
+	protocols.SetHTTP2(false)
+	transport.Protocols = protocols
+	transport.TLSNextProto = map[string]func(string, *tls.Conn) http.RoundTripper{}
 }
 
 // Reset resets the GCS storage. Reset should not be used concurrently with
