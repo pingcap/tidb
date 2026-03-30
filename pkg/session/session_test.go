ff5aef911f0069e15c9198535876b919ae876b40
xufei <xufei@pingcap.com>
2026-03-30 18:57:13 +0800

    session: fix advisory lock owner for internal sessions (#38)
    (cherry picked from commit 0c12258356aa03285e3c92624edb548758dfcc8c)

--- pkg/session/session_test.go
+++ pkg/session/session_test.go
@@ -15,9 +15,12 @@
 package session
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/pingcap/tidb/pkg/ddl"
+	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,4 +29,45 @@ func TestGetStartMode(t *testing.T) {
 	require.Equal(t, ddl.Normal, getStartMode(currentBootstrapVersion+1))
 	require.Equal(t, ddl.Upgrade, getStartMode(currentBootstrapVersion-1))
 	require.Equal(t, ddl.Bootstrap, getStartMode(0))
+}
+
+func TestAdvisoryLockWithoutProcessInfo(t *testing.T) {
+	store, dom := CreateStoreAndBootstrap(t)
+	defer func() { require.NoError(t, store.Close()) }()
+	defer dom.Close()
+
+	se, err := createSession(store)
+	require.NoError(t, err)
+
+	require.Nil(t, se.ShowProcess())
+	lockName := fmt.Sprintf("%s-lock", t.Name())
+
+	require.NoError(t, se.GetAdvisoryLock(lockName, 1))
+	require.NotZero(t, se.advisoryLockOwnerID.Load())
+	require.Equal(t, se.advisoryLockOwnerID.Load(), se.IsUsedAdvisoryLock(lockName))
+	require.True(t, se.ReleaseAdvisoryLock(lockName))
+	require.Zero(t, se.IsUsedAdvisoryLock(lockName))
+
+	se.Close()
+	require.Zero(t, se.advisoryLockOwnerID.Load())
+}
+
+func TestAdvisoryLockUsesProcessInfoOwner(t *testing.T) {
+	store, dom := CreateStoreAndBootstrap(t)
+	defer func() { require.NoError(t, store.Close()) }()
+	defer dom.Close()
+
+	se, err := createSession(store)
+	require.NoError(t, err)
+	defer se.Close()
+
+	const connID uint64 = 12345
+	lockName := fmt.Sprintf("%s-processinfo-lock", t.Name())
+	se.SetConnectionID(connID)
+	se.SetProcessInfo("select 1", time.Now(), mysql.ComQuery, 0)
+
+	require.NoError(t, se.GetAdvisoryLock(lockName, 1))
+	require.Equal(t, connID, se.IsUsedAdvisoryLock(lockName))
+	require.Equal(t, connID, se.advisoryLockOwnerID.Load())
+	require.True(t, se.ReleaseAdvisoryLock(lockName))
 }
