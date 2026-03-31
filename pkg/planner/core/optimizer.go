ff12f123c34869197e6c4c8f7963adde79546c5f
AilinKid <314806019@qq.com>
2026-03-31 16:18:38 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

--- pkg/planner/core/optimizer.go
+++ pkg/planner/core/optimizer.go
@@ -352,6 +352,10 @@ func adjustOptimizationFlags(flag uint64, logic base.LogicalPlan) uint64 {
 	if checkStableResultMode(logic.SCtx()) {
 		flag |= rule.FlagStabilizeResults
 	}
+	if logic.SCtx().GetSessionVars().StmtCtx.StraightJoinOrder {
+		// When we use the straight Join Order hint, we should disable the join reorder optimization.
+		flag &= ^rule.FlagJoinReOrder
+	}
 	// InternalSQLScanUserTable is for ttl scan.
 	if !logic.SCtx().GetSessionVars().InRestrictedSQL || logic.SCtx().GetSessionVars().InternalSQLScanUserTable {
 		flag |= rule.FlagCollectPredicateColumnsPoint
