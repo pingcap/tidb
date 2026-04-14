319293ae77418a7ba343c72942ce03a4c34e9551
Zhigao TONG <tongzhigao@pingcap.com>
2026-04-14 18:08:55 +0800

    remove modifications about planner

--- pkg/statistics/handle/ddl/subscriber.go
+++ pkg/statistics/handle/ddl/subscriber.go
@@ -262,10 +262,6 @@ func (h subscriber) handle(
 				)
 			}
 		}
-	case model.ActionCreateMaterializedViewLog,
-		model.ActionCreateMaterializedView:
-		// MV refresh/attributes/mvlog purge DDL updates only MV runtime metadata.
-		// They don't change table data/partition topology, so stats meta should stay unchanged.
 	default:
 		intest.Assert(false)
 		logutil.StatsLogger().Error("Unhandled schema change event",
