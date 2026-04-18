3f03f9fb047f765bec108819640d6a82309e6dfe
guo-shaoge <shaoge1994@163.com>
2026-04-18 17:23:03 +0800

    *: support cache prepared statement in plan cache (#67857)
    close pingcap/tidb#67815

--- pkg/sessionctx/variable/tidb_vars.go
+++ pkg/sessionctx/variable/tidb_vars.go
@@ -1262,6 +1262,9 @@ const (
 
 	// MaxPreSplitRegions is the maximum number of regions that can be pre-split.
 	MaxPreSplitRegions = 15
+
+	// TiDBEnableCachePrepareStmt indicates whether to support cache prepare stmt in plan cache.
+	TiDBEnableCachePrepareStmt = "tidb_enable_cache_prepare_stmt"
 )
 
 // Default TiDB system variable values.
@@ -1618,6 +1621,7 @@ const (
 	DefTiDBEnableSharedLockPromotion                  = false
 	DefTiDBTSOClientRPCMode                           = TSOClientRPCModeDefault
 	DefTiDBLoadBindingTimeout                         = 200
+	DefEnableCachePrepareStmt                         = true
 )
 
 // Process global variables.
