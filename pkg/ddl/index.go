63f6ed2a1fdea31460e842656572419a5b9bb3d7
fzzf678 <108643977+fzzf678@users.noreply.github.com>
2026-04-20 21:18:15 +0800

    This is an automated cherry-pick of #67713
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/ddl/index.go
+++ pkg/ddl/index.go
@@ -1757,10 +1757,10 @@ func isRetryableJobError(err error, jobErrCnt int64) bool {
 	if jobErrCnt+1 >= variable.GetDDLErrorCountLimit() {
 		return false
 	}
-	return isRetryableError(err)
+	return isRetryableError(err, true)
 }
 
-func isRetryableError(err error) bool {
+func isRetryableError(err error, retryUnknown bool) bool {
 	errMsg := err.Error()
 	for _, m := range dbterror.ReorgRetryableErrMsgs {
 		if strings.Contains(errMsg, m) {
@@ -1773,8 +1773,7 @@ func isRetryableError(err error) bool {
 		_, ok := dbterror.ReorgRetryableErrCodes[sqlErr.Code]
 		return ok
 	}
-	// For the unknown errors, we should retry.
-	return true
+	return retryUnknown
 }
 
 func runReorgJobAndHandleErr(
