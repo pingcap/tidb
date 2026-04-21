09dcfe0c422eefd348cf9932a6406e57bfa5c067
gengliqi <gengliqiii@gmail.com>
2026-04-21 12:45:21 +0800

    u
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- pkg/store/copr/coprocessor.go
+++ pkg/store/copr/coprocessor.go
@@ -254,13 +254,10 @@ func (c *CopClient) BuildCopIterator(ctx context.Context, req *kv.Request, vars 
 		if it.smallTaskConcurrency > 20 {
 			it.smallTaskConcurrency = 20
 		}
+		it.sendRate = util.NewRateLimit(2 * (it.concurrency + it.smallTaskConcurrency))
 		it.respChan = nil
 	} else {
 		it.respChan = make(chan *copResponse)
-	}
-	if it.req.KeepOrder {
-		it.sendRate = util.NewRateLimit(2 * (it.concurrency + it.smallTaskConcurrency))
-	} else {
 		it.sendRate = util.NewRateLimit(it.concurrency + it.smallTaskConcurrency)
 	}
 	if option.EnableCollectExecutionInfo {
