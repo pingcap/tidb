20176b9e129cd674fa11213d351157f9b8347a8f
tiancaiamao <tiancaiamao@gmail.com>
2026-04-22 08:36:01 +0800

    test, session: retry schema-expired updates in bootstrap downgrade

--- pkg/session/bootstrap_test.go
+++ pkg/session/bootstrap_test.go
@@ -299,11 +299,31 @@ func TestDDLTableCreateDDLNotifierTable(t *testing.T) {
 }
 
 func revertVersionAndVariables(t *testing.T, se sessiontypes.Session, ver int) {
-	MustExec(t, se, fmt.Sprintf("update mysql.tidb set variable_value='%d' where variable_name='tidb_server_version'", ver))
+	mustExecWithInfoSchemaRetry(t, se, fmt.Sprintf("update mysql.tidb set variable_value='%d' where variable_name='tidb_server_version'", ver))
 	if ver <= version195 {
 		// for version <= version195, tidb_enable_dist_task should be disabled before upgrade
-		MustExec(t, se, "update mysql.global_variables set variable_value='off' where variable_name='tidb_enable_dist_task'")
+		mustExecWithInfoSchemaRetry(t, se, "update mysql.global_variables set variable_value='off' where variable_name='tidb_enable_dist_task'")
 	}
+}
+
+func mustExecWithInfoSchemaRetry(t *testing.T, se sessiontypes.Session, sql string) {
+	var err error
+	for range 10 {
+		rs, execErr := exec(se, sql)
+		if execErr == nil && rs != nil {
+			execErr = rs.Close()
+		}
+		if execErr == nil {
+			return
+		}
+		if !domain.ErrInfoSchemaExpired.Equal(execErr) && !domain.ErrInfoSchemaChanged.Equal(execErr) {
+			err = execErr
+			break
+		}
+		err = execErr
+		time.Sleep(20 * time.Millisecond)
+	}
+	require.NoError(t, err, "sql: %s", sql)
 }
 
 // TestUpgrade tests upgrading
