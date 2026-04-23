e5a07490f0f2d4c98e77b494e1144ca692740cda
D3Hunter <jujj603@gmail.com>
2026-04-23 16:06:28 +0800

    import: honor Spark legacy Parquet datetime metadata (#67908)
    close pingcap/tidb#67849

--- pkg/lightning/mydump/parquet_parser.go
+++ pkg/lightning/mydump/parquet_parser.go
@@ -215,6 +215,12 @@ type convertedType struct {
 
 	// See https://github.com/apache/parquet-format/blob/master/LogicalTypes.md#temporal-types
 	IsAdjustedToUTC bool
+
+	// sparkRebaseMicros is non-empty when the file footer says the column was
+	// written by a Spark release that used the legacy hybrid Julian/Gregorian
+	// calendar for ancient DATE/TIMESTAMP values. It also caches the generated
+	// Spark timezone rebase table for TIMESTAMP and INT96 value conversion.
+	sparkRebaseMicros sparkRebaseMicrosLookup
 }
 
 // rowGroupParser parses rows from one parquet row group.
@@ -615,12 +621,17 @@ func NewParquetParser(
 		return nil, errors.Trace(err)
 	}
 
-	fileSchema := reader.MetaData().Schema
+	fileMeta := reader.MetaData()
+	fileSchema := fileMeta.Schema
 	colTypes := make([]convertedType, fileSchema.NumColumns())
 	colNames := make([]string, 0, fileSchema.NumColumns())
+	effectiveLoc := meta.Loc
+	if effectiveLoc == nil {
+		effectiveLoc = timeutil.SystemLocation()
+	}
 
 	for i := range colTypes {
-		desc := reader.MetaData().Schema.Column(i)
+		desc := fileSchema.Column(i)
 		colNames = append(colNames, strings.ToLower(desc.Name()))
 
 		logicalType := desc.LogicalType()
@@ -641,6 +652,32 @@ func NewParquetParser(
 		if _, ok := unsupportedParquetTypes[colTypes[i].converted]; ok {
 			return nil, errors.Errorf("unsupported parquet logical type %s", colTypes[i].converted.String())
 		}
+
+		switch desc.PhysicalType() {
+		case parquet.Types.Int32:
+			if colTypes[i].converted == schema.ConvertedTypes.Date {
+				colTypes[i].sparkRebaseMicros, err = sparkRebaseMicrosFromMetadata(
+					fileMeta, sparkDatetimeRebaseCutoff, sparkLegacyDateTimeMetadataKey, effectiveLoc)
+				if err != nil {
+					return nil, errors.Trace(err)
+				}
+			}
+		case parquet.Types.Int64:
+			if colTypes[i].converted == schema.ConvertedTypes.TimestampMillis ||
+				colTypes[i].converted == schema.ConvertedTypes.TimestampMicros {
+				colTypes[i].sparkRebaseMicros, err = sparkRebaseMicrosFromMetadata(
+					fileMeta, sparkDatetimeRebaseCutoff, sparkLegacyDateTimeMetadataKey, effectiveLoc)
+				if err != nil {
+					return nil, errors.Trace(err)
+				}
+			}
+		case parquet.Types.Int96:
+			colTypes[i].sparkRebaseMicros, err = sparkRebaseMicrosFromMetadata(
+				fileMeta, sparkINT96RebaseCutoff, sparkLegacyINT96MetadataKey, effectiveLoc)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+		}
 	}
 
 	numColumns := len(colTypes)
@@ -649,7 +686,7 @@ func NewParquetParser(
 	})
 
 	parser := &ParquetParser{
-		fileMeta: reader.MetaData(),
+		fileMeta: fileMeta,
 		colTypes: colTypes,
 		colNames: colNames,
 		ctx:      ctx,
@@ -660,7 +697,7 @@ func NewParquetParser(
 		logger:   logger,
 		rowPool:  &pool,
 	}
-	if err := parser.Init(meta.Loc); err != nil {
+	if err := parser.Init(effectiveLoc); err != nil {
 		return nil, errors.Trace(err)
 	}
 
