2bb4f3964e3a7d692e76e96712fa3fdfff778b59
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2026-04-27 15:01:00 +0800

    BR: support detect unrecognized pattern (#67408) (#67905)
    close pingcap/tidb#67016

--- br/tests/br_full_cluster_restore/run.sh
+++ br/tests/br_full_cluster_restore/run.sh
@@ -128,7 +128,8 @@ run_sql_as user2 "123456" "select count(*) from db2.t1" || true
 check_contains "SELECT command denied to user"
 # user3 can only query db1.t1 using ssl
 # ci env uses mariadb client, ssl flag is different with mysql client
-run_sql_as user3 "123456" "select count(*) from db1.t1" || true
+# enforce a non-SSL attempt first; some clients may auto-negotiate TLS by default.
+run_sql_as user3 "123456" "select count(*) from db1.t1" --skip-ssl || true
 check_contains "Access denied for user"
 run_sql_as user3 "123456" "select count(*) from db1.t1" --ssl
 check_contains "count(*): 2"
