29581a3d42e58ad4bed0f933ef38efcfa2c9514f
Liqi Geng <gengliqiii@gmail.com>
2026-05-16 01:45:26 +0800

    This is an automated cherry-pick of #67676
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/store/copr/copr_test/coprocessor_test.go
+++ pkg/store/copr/copr_test/coprocessor_test.go
@@ -33,6 +33,11 @@ import (
 	"github.com/pingcap/tidb/pkg/testkit"
 	"github.com/stretchr/testify/require"
 	"github.com/tikv/client-go/v2/testutils"
+<<<<<<< HEAD
+=======
+	"github.com/tikv/client-go/v2/tikvrpc"
+	tikvutil "github.com/tikv/client-go/v2/util"
+>>>>>>> dd8253cf5da (executor: bound partition fan-out in IndexLookUp to prevent cop request burst (#67676))
 	pd "github.com/tikv/pd/client"
 	rmclient "github.com/tikv/pd/client/resource_group/controller"
 )
@@ -109,6 +114,44 @@ func TestBuildCopIteratorWithRowCountHint(t *testing.T) {
 	require.Equal(t, conc, 4)
 	require.Equal(t, smallConc, 0)
 	require.Equal(t, rateLimit.GetCapacity(), 4)
+}
+
+func TestBuildCopIteratorWithSharedRequestRateLimit(t *testing.T) {
+	store, err := mockstore.NewMockStore()
+	require.NoError(t, err)
+	defer require.NoError(t, store.Close())
+
+	copClient := store.GetClient().(*copr.CopClient)
+	ctx := context.Background()
+	killed := uint32(0)
+	vars := kv.NewVariables(&killed)
+	opt := &kv.ClientSendOption{}
+	ranges := copr.BuildKeyRanges("a", "z")
+
+	testCases := []struct {
+		name      string
+		keepOrder bool
+	}{
+		{name: "keep-order", keepOrder: true},
+		{name: "non-keep-order", keepOrder: false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			shared := tikvutil.NewRateLimit(7)
+			req := &kv.Request{
+				Tp:                   kv.ReqTypeDAG,
+				KeyRanges:            kv.NewNonPartitionedKeyRanges(ranges),
+				Concurrency:          15,
+				KeepOrder:            tc.keepOrder,
+				CoprRequestRateLimit: shared,
+			}
+			it, errRes := copClient.BuildCopIterator(ctx, req, vars, opt)
+			require.Nil(t, errRes)
+			require.Same(t, shared, it.GetRequestRateLimit())
+			require.Equal(t, 7, it.GetRequestRateLimit().GetCapacity())
+		})
+	}
 }
 
 func TestBuildCopIteratorWithBatchStoreCopr(t *testing.T) {
