d1649a78bf1e10db5abc7c956e73ca867ff242e8
Yiding Cui <winoros@gmail.com>
2026-05-20 23:20:39 +0800

    expression: add fulltext analyzers

---
+++ pkg/expression/fulltext/analyzer.go
@@ -0,0 +1,318 @@
+// Copyright 2026 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fulltext
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+
+	"github.com/pingcap/tidb/pkg/sessionctx"
+	"github.com/pingcap/tidb/pkg/sessionctx/vardef"
+	"github.com/pingcap/tidb/pkg/sessionctx/variable"
+)
+
+// Token is the analyzed fulltext token. OffsetFrom and OffsetTo are UTF-8 byte
+// offsets in the original input text.
+type Token struct {
+	Text           string
+	Position       int
+	OffsetFrom     int
+	OffsetTo       int
+	PositionLength int
+}
+
+// PreserveUnderscoreTokenize tokenizes text with TiCI's PreserveUnderscore
+// tokenizer semantics: Unicode alphanumeric characters and '_' form tokens;
+// every other character is a delimiter.
+func PreserveUnderscoreTokenize(text string) []Token {
+	tokens := make([]Token, 0)
+	pos := 0
+	for i := 0; i < len(text); {
+		ch, next := runeAtByte(text, i)
+		if !isTokenChar(ch) {
+			i = next
+			continue
+		}
+
+		start := i
+		j := next
+		for j < len(text) {
+			ch, next = runeAtByte(text, j)
+			if !isTokenChar(ch) {
+				break
+			}
+			j = next
+		}
+		tokens = append(tokens, Token{
+			Text:           text[start:j],
+			Position:       pos,
+			OffsetFrom:     start,
+			OffsetTo:       j,
+			PositionLength: 1,
+		})
+		pos++
+		i = j
+	}
+	return tokens
+}
+
+// AnalyzeStandardV1 runs the STANDARD_V1 analyzer:
+// PreserveUnderscore tokenizer, length filter, lower-case filter, and optional
+// stopword filter.
+func AnalyzeStandardV1(sctx sessionctx.Context, text string) ([]Token, error) {
+	parserInfo, err := parserInfoFromSessionContext(sctx)
+	if err != nil {
+		return nil, err
+	}
+	return analyzeStandardV1(text, parserInfo), nil
+}
+
+func analyzeStandardV1(text string, parserInfo parserInfo) []Token {
+	tokens := PreserveUnderscoreTokenize(text)
+	tokens = lengthFilter(tokens, parserInfo.innodbFtMinTokenSize, parserInfo.innodbFtMaxTokenSize)
+	tokens = lowerFilter(tokens)
+	tokens = stopwordFilter(tokens, parserInfo)
+	return tokens
+}
+
+// AnalyzeNgramV1 runs the NGRAM_V1 analyzer:
+// PreserveUnderscore tokenizer, fixed-size ngram filter, and lower-case filter.
+func AnalyzeNgramV1(sctx sessionctx.Context, text string) ([]Token, error) {
+	parserInfo, err := parserInfoFromSessionContext(sctx)
+	if err != nil {
+		return nil, err
+	}
+	return analyzeNgramV1(text, parserInfo), nil
+}
+
+func analyzeNgramV1(text string, parserInfo parserInfo) []Token {
+	tokens := PreserveUnderscoreTokenize(text)
+	tokens = ngramFilter(tokens, parserInfo.ngramTokenSize, parserInfo.ngramTokenSize)
+	tokens = lowerFilter(tokens)
+	return tokens
+}
+
+type parserInfo struct {
+	innodbFtMinTokenSize int
+	innodbFtMaxTokenSize int
+	ngramTokenSize       int
+	stopwords            map[string]struct{}
+}
+
+func parserInfoFromSessionContext(sctx sessionctx.Context) (parserInfo, error) {
+	if sctx == nil || sctx.GetSessionVars() == nil {
+		return parserInfo{}, fmt.Errorf("missing session context for fulltext analyzer")
+	}
+	sessVars := sctx.GetSessionVars()
+
+	enableStopword, err := getFulltextSysVar(sessVars, vardef.InnodbFtEnableStopword)
+	if err != nil {
+		return parserInfo{}, err
+	}
+	minTokenSize, err := getFulltextIntSysVar(sessVars, vardef.InnodbFtMinTokenSize)
+	if err != nil {
+		return parserInfo{}, err
+	}
+	maxTokenSize, err := getFulltextIntSysVar(sessVars, vardef.InnodbFtMaxTokenSize)
+	if err != nil {
+		return parserInfo{}, err
+	}
+	ngramTokenSize, err := getFulltextIntSysVar(sessVars, vardef.NgramTokenSize)
+	if err != nil {
+		return parserInfo{}, err
+	}
+	serverStopwordTable, err := getFulltextSysVar(sessVars, vardef.InnodbFtServerStopwordTable)
+	if err != nil {
+		return parserInfo{}, err
+	}
+	userStopwordTable, err := getFulltextSysVar(sessVars, vardef.InnodbFtUserStopwordTable)
+	if err != nil {
+		return parserInfo{}, err
+	}
+	stopwords := stopwordSetFromSysVars(enableStopword, serverStopwordTable, userStopwordTable)
+
+	return parserInfo{
+		innodbFtMinTokenSize: minTokenSize,
+		innodbFtMaxTokenSize: maxTokenSize,
+		ngramTokenSize:       ngramTokenSize,
+		stopwords:            stopwords,
+	}, nil
+}
+
+func stopwordSetFromSysVars(enableStopword, serverStopwordTable, userStopwordTable string) map[string]struct{} {
+	if !variable.TiDBOptOn(enableStopword) {
+		return nil
+	}
+	if strings.TrimSpace(serverStopwordTable) != "" || strings.TrimSpace(userStopwordTable) != "" {
+		return map[string]struct{}{}
+	}
+	return englishStopwords
+}
+
+func getFulltextSysVar(sessVars *variable.SessionVars, name string) (string, error) {
+	val, err := sessVars.GetSessionOrGlobalSystemVar(context.Background(), name)
+	if err != nil {
+		return "", fmt.Errorf("get %s for fulltext analyzer: %w", name, err)
+	}
+	return val, nil
+}
+
+func getFulltextIntSysVar(sessVars *variable.SessionVars, name string) (int, error) {
+	val, err := getFulltextSysVar(sessVars, name)
+	if err != nil {
+		return 0, err
+	}
+	n, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, fmt.Errorf("parse %s for fulltext analyzer: %w", name, err)
+	}
+	return n, nil
+}
+
+func runeAtByte(text string, offset int) (rune, int) {
+	ch, size := utf8.DecodeRuneInString(text[offset:])
+	if size == 0 {
+		return 0, len(text)
+	}
+	return ch, offset + size
+}
+
+func isTokenChar(ch rune) bool {
+	return unicode.IsLetter(ch) || unicode.IsNumber(ch) || ch == '_'
+}
+
+func lengthFilter(tokens []Token, minLen, maxLen int) []Token {
+	if minLen > maxLen {
+		return nil
+	}
+
+	out := make([]Token, 0, len(tokens))
+	for _, token := range tokens {
+		n := charLen(token.Text)
+		if minLen <= n && n <= maxLen {
+			out = append(out, token)
+		}
+	}
+	return out
+}
+
+func charLen(s string) int {
+	n := 0
+	for range s {
+		n++
+	}
+	return n
+}
+
+func lowerFilter(tokens []Token) []Token {
+	for i := range tokens {
+		tokens[i].Text = strings.ToLower(tokens[i].Text)
+	}
+	return tokens
+}
+
+func stopwordFilter(tokens []Token, parserInfo parserInfo) []Token {
+	if parserInfo.stopwords == nil {
+		return tokens
+	}
+
+	out := make([]Token, 0, len(tokens))
+	for _, token := range tokens {
+		if _, ok := parserInfo.stopwords[token.Text]; !ok {
+			out = append(out, token)
+		}
+	}
+	return out
+}
+
+func stopwordSet(words ...string) map[string]struct{} {
+	set := make(map[string]struct{}, len(words))
+	for _, word := range words {
+		set[word] = struct{}{}
+	}
+	return set
+}
+
+func ngramFilter(tokens []Token, minGram, maxGram int) []Token {
+	if minGram <= 0 || maxGram < minGram {
+		return nil
+	}
+
+	out := make([]Token, 0)
+	nextPositionBase := 0
+	for _, token := range tokens {
+		basePosition := max(token.Position, nextPositionBase)
+		spans := utf8CharSpans(token.Text)
+		charCount := len(spans)
+		if charCount < minGram {
+			nextPositionBase = max(nextPositionBase, token.Position+1)
+			continue
+		}
+
+		maxLimit := min(maxGram, charCount)
+		for startIdx := range charCount {
+			for gramLen := minGram; gramLen <= maxLimit; gramLen++ {
+				endIdx := startIdx + gramLen
+				if endIdx > charCount {
+					break
+				}
+
+				startByte := spans[startIdx].byteStart
+				endByte := spans[endIdx-1].byteEnd
+				out = append(out, Token{
+					Text:           token.Text[startByte:endByte],
+					Position:       basePosition + startIdx,
+					OffsetFrom:     token.OffsetFrom + startByte,
+					OffsetTo:       token.OffsetFrom + endByte,
+					PositionLength: token.PositionLength,
+				})
+			}
+		}
+		nextPositionBase = basePosition + charCount - minGram + 1
+	}
+	return out
+}
+
+type charSpan struct {
+	byteStart int
+	byteEnd   int
+}
+
+func utf8CharSpans(text string) []charSpan {
+	spans := make([]charSpan, 0, len(text))
+	for start, ch := range text {
+		size := utf8.RuneLen(ch)
+		if size < 0 {
+			size = 1
+		}
+		spans = append(spans, charSpan{
+			byteStart: start,
+			byteEnd:   start + size,
+		})
+	}
+	return spans
+}
+
+var englishStopwords = map[string]struct{}{
+	"a": {}, "an": {}, "and": {}, "are": {}, "as": {}, "at": {}, "be": {}, "but": {},
+	"by": {}, "for": {}, "if": {}, "in": {}, "into": {}, "is": {}, "it": {}, "no": {},
+	"not": {}, "of": {}, "on": {}, "or": {}, "such": {}, "that": {}, "the": {},
+	"their": {}, "then": {}, "there": {}, "these": {}, "they": {}, "this": {}, "to": {},
+	"was": {}, "will": {}, "with": {},
+}
---
+++ pkg/expression/fulltext/analyzer_test.go
@@ -0,0 +1,134 @@
+// Copyright 2026 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fulltext
+
+import (
+	"context"
+	"testing"
+
+	"github.com/pingcap/tidb/pkg/sessionctx/vardef"
+	"github.com/pingcap/tidb/pkg/sessionctx/variable"
+	"github.com/pingcap/tidb/pkg/util/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPreserveUnderscoreTokenize(t *testing.T) {
+	tokens := PreserveUnderscoreTokenize("abc_def,ghi 你好-world")
+	require.Equal(t, []Token{
+		{Text: "abc_def", Position: 0, OffsetFrom: 0, OffsetTo: 7, PositionLength: 1},
+		{Text: "ghi", Position: 1, OffsetFrom: 8, OffsetTo: 11, PositionLength: 1},
+		{Text: "你好", Position: 2, OffsetFrom: 12, OffsetTo: 18, PositionLength: 1},
+		{Text: "world", Position: 3, OffsetFrom: 19, OffsetTo: 24, PositionLength: 1},
+	}, tokens)
+}
+
+func TestAnalyzeStandardV1(t *testing.T) {
+	sctx := newFulltextTestContext(t)
+
+	tokens, err := AnalyzeStandardV1(sctx, "The foo_bar, a 好")
+	require.NoError(t, err)
+	require.Equal(t, []Token{
+		{Text: "foo_bar", Position: 1, OffsetFrom: 4, OffsetTo: 11, PositionLength: 1},
+	}, tokens)
+
+	require.NoError(t, sctx.GetSessionVars().SetSystemVar(vardef.InnodbFtEnableStopword, vardef.Off))
+	tokens, err = AnalyzeStandardV1(sctx, "The cat")
+	require.NoError(t, err)
+	require.Equal(t, []Token{
+		{Text: "the", Position: 0, OffsetFrom: 0, OffsetTo: 3, PositionLength: 1},
+		{Text: "cat", Position: 1, OffsetFrom: 4, OffsetTo: 7, PositionLength: 1},
+	}, tokens)
+}
+
+func TestAnalyzeStandardV1ReadsTokenSizesFromSessionContext(t *testing.T) {
+	sctx := newFulltextTestContext(t)
+	setGlobalSysVar(t, sctx, vardef.InnodbFtMinTokenSize, "1")
+	require.NoError(t, sctx.GetSessionVars().SetSystemVar(vardef.InnodbFtEnableStopword, vardef.Off))
+
+	tokens, err := AnalyzeStandardV1(sctx, "A 好 xy")
+	require.NoError(t, err)
+	require.Equal(t, []Token{
+		{Text: "a", Position: 0, OffsetFrom: 0, OffsetTo: 1, PositionLength: 1},
+		{Text: "好", Position: 1, OffsetFrom: 2, OffsetTo: 5, PositionLength: 1},
+		{Text: "xy", Position: 2, OffsetFrom: 6, OffsetTo: 8, PositionLength: 1},
+	}, tokens)
+}
+
+func TestAnalyzeNgramV1(t *testing.T) {
+	sctx := newFulltextTestContext(t)
+
+	tokens, err := AnalyzeNgramV1(sctx, "Hi世界 foo-bar")
+	require.NoError(t, err)
+	require.Equal(t, []Token{
+		{Text: "hi", Position: 0, OffsetFrom: 0, OffsetTo: 2, PositionLength: 1},
+		{Text: "i世", Position: 1, OffsetFrom: 1, OffsetTo: 5, PositionLength: 1},
+		{Text: "世界", Position: 2, OffsetFrom: 2, OffsetTo: 8, PositionLength: 1},
+		{Text: "fo", Position: 3, OffsetFrom: 9, OffsetTo: 11, PositionLength: 1},
+		{Text: "oo", Position: 4, OffsetFrom: 10, OffsetTo: 12, PositionLength: 1},
+		{Text: "ba", Position: 5, OffsetFrom: 13, OffsetTo: 15, PositionLength: 1},
+		{Text: "ar", Position: 6, OffsetFrom: 14, OffsetTo: 16, PositionLength: 1},
+	}, tokens)
+}
+
+func TestAnalyzeNgramV1ShortTokenAdvancesPositionBase(t *testing.T) {
+	sctx := newFulltextTestContext(t)
+
+	tokens, err := AnalyzeNgramV1(sctx, "abc x def")
+	require.NoError(t, err)
+	require.Equal(t, []Token{
+		{Text: "ab", Position: 0, OffsetFrom: 0, OffsetTo: 2, PositionLength: 1},
+		{Text: "bc", Position: 1, OffsetFrom: 1, OffsetTo: 3, PositionLength: 1},
+		{Text: "de", Position: 2, OffsetFrom: 6, OffsetTo: 8, PositionLength: 1},
+		{Text: "ef", Position: 3, OffsetFrom: 7, OffsetTo: 9, PositionLength: 1},
+	}, tokens)
+}
+
+func TestAnalyzeNgramV1ReadsTokenSizeFromSessionContext(t *testing.T) {
+	sctx := newFulltextTestContext(t)
+	setGlobalSysVar(t, sctx, vardef.NgramTokenSize, "3")
+
+	tokens, err := AnalyzeNgramV1(sctx, "abcd")
+	require.NoError(t, err)
+	require.Equal(t, []Token{
+		{Text: "abc", Position: 0, OffsetFrom: 0, OffsetTo: 3, PositionLength: 1},
+		{Text: "bcd", Position: 1, OffsetFrom: 1, OffsetTo: 4, PositionLength: 1},
+	}, tokens)
+}
+
+func TestAnalyzeStandardV1CustomStopwords(t *testing.T) {
+	tokens := analyzeStandardV1("foo the bar", parserInfo{
+		innodbFtMinTokenSize: 3,
+		innodbFtMaxTokenSize: 84,
+		stopwords:            stopwordSet("foo"),
+	})
+	require.Equal(t, []Token{
+		{Text: "the", Position: 1, OffsetFrom: 4, OffsetTo: 7, PositionLength: 1},
+		{Text: "bar", Position: 2, OffsetFrom: 8, OffsetTo: 11, PositionLength: 1},
+	}, tokens)
+}
+
+func newFulltextTestContext(t *testing.T) *mock.Context {
+	sctx := mock.NewContext()
+	globalAccessor := variable.NewMockGlobalAccessor4Tests()
+	globalAccessor.SessionVars = sctx.GetSessionVars()
+	sctx.GetSessionVars().GlobalVarsAccessor = globalAccessor
+	return sctx
+}
+
+func setGlobalSysVar(t *testing.T, sctx *mock.Context, name, value string) {
+	globalAccessor, ok := sctx.GetSessionVars().GlobalVarsAccessor.(*variable.MockGlobalAccessor)
+	require.True(t, ok)
+	require.NoError(t, globalAccessor.SetGlobalSysVarOnly(context.Background(), name, value, false))
+}
