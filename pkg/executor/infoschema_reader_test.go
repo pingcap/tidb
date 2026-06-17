d145323f0680fe7defc3b142bc92b560f8db8079
xufei <xufeixw@mail.ustc.edu.cn>
2026-06-17 13:56:51 +0800

    fix ut
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- pkg/executor/infoschema_reader_test.go
+++ pkg/executor/infoschema_reader_test.go
@@ -661,7 +661,7 @@ func TestColumnTable(t *testing.T) {
 		testkit.RowsWithSep("|",
 			"test|tbl1|col_2"))
 	tk.MustQuery(`select count(*) from information_schema.columns;`).Check(
-		testkit.RowsWithSep("|", "5032"))
+		testkit.RowsWithSep("|", "5064"))
 }
 
 func TestIndexUsageTable(t *testing.T) {
