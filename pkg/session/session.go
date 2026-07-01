77bb41b5eb4297ad0a3e6147de4bb37e9407530d
D3Hunter <jujj603@gmail.com>
2026-07-02 00:14:46 +0800

    change
    change
    
    change

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -137,7 +137,6 @@ import (
 	"github.com/pingcap/tidb/pkg/util/sqlescape"
 	"github.com/pingcap/tidb/pkg/util/sqlexec"
 	"github.com/pingcap/tidb/pkg/util/syncutil"
-	"github.com/pingcap/tidb/pkg/util/timeutil"
 	"github.com/pingcap/tidb/pkg/util/topsql"
 	topsqlstate "github.com/pingcap/tidb/pkg/util/topsql/state"
 	"github.com/pingcap/tidb/pkg/util/topsql/stmtstats"
@@ -4351,6 +4350,9 @@ func bootstrapSessionImpl(ctx context.Context, store kv.Storage, createSessionsI
 			return nil, err
 		}
 	}
+	if err = initGlobalVarFromSystemDB(ctx, store); err != nil {
+		return nil, err
+	}
 
 	// initiate disttask framework components which need a store
 	scheduler.RegisterSchedulerFactory(
@@ -4374,7 +4376,6 @@ func bootstrapSessionImpl(ctx context.Context, store kv.Storage, createSessionsI
 	if concurrency < 0 { // it is only for test, in the production, negative value is illegal.
 		concurrency = 0
 	}
-
 	ses, err := createSessionsImpl(store, 10)
 	if err != nil {
 		return nil, err
@@ -4392,20 +4393,6 @@ func bootstrapSessionImpl(ctx context.Context, store kv.Storage, createSessionsI
 	for i := range ses {
 		ses[i].GetSessionVars().InRestrictedSQL = true
 	}
-
-	// get system tz from mysql.tidb
-	tz, err := ses[0].getTableValue(ctx, mysql.TiDBTable, tidbSystemTZ)
-	if err != nil {
-		return nil, err
-	}
-	timeutil.SetSystemTZ(tz)
-
-	// get the flag from `mysql`.`tidb` which indicating if new collations are enabled.
-	newCollationEnabled, err := loadCollationParameter(ctx, ses[0])
-	if err != nil {
-		return nil, err
-	}
-	collate.SetNewCollationEnabledForTest(newCollationEnabled)
 
 	// only start the domain after we have initialized some global variables.
 	dom := domain.GetDomain(ses[0])
