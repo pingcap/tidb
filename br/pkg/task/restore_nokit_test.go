cff357e0d75ac9c1d46a166d2b3934a995a7b214
Juncen Yu <yujuncen@pingcap.com>
2026-07-01 17:31:57 +0800

    br: make restore scheduler pause range keyspace-aware (#69560)
    close pingcap/tidb#69558

--- br/pkg/task/restore_nokit_test.go
+++ br/pkg/task/restore_nokit_test.go
@@ -21,11 +21,15 @@ import (
 	"github.com/gogo/protobuf/proto"
 	backuppb "github.com/pingcap/kvproto/pkg/brpb"
 	"github.com/pingcap/kvproto/pkg/encryptionpb"
+	"github.com/pingcap/kvproto/pkg/keyspacepb"
 	"github.com/pingcap/tidb/br/pkg/metautil"
 	"github.com/pingcap/tidb/pkg/config/kerneltype"
 	"github.com/pingcap/tidb/pkg/objstore"
+	"github.com/pingcap/tidb/pkg/tablecodec"
+	"github.com/pingcap/tidb/pkg/util/codec"
 	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/require"
+	"github.com/tikv/client-go/v2/tikv"
 )
 
 func TestPhysicalRestoreSysTables(t *testing.T) {
@@ -92,6 +96,39 @@ func TestSplitRegionIndexStepFlag(t *testing.T) {
 		require.ErrorContains(t, err, FlagSplitRegionIndexStep)
 		require.ErrorContains(t, err, "greater than 0")
 	})
+}
+
+func TestRewriteKeyRangesUsesStorageCodec(t *testing.T) {
+	preAlloced := [2]int64{11, 89}
+	tableStart := tablecodec.EncodeTablePrefix(preAlloced[0])
+	tableEnd := tablecodec.EncodeTablePrefix(preAlloced[1])
+	legacyStart := codec.EncodeBytes(nil, tableStart)
+	legacyEnd := codec.EncodeBytes(nil, tableEnd)
+
+	ranges := rewriteKeyRanges(tikv.NewCodecV1(tikv.ModeTxn), preAlloced)
+	require.Len(t, ranges, 1)
+	require.Equal(t, legacyStart, []byte(ranges[0][0]))
+	require.Equal(t, legacyEnd, []byte(ranges[0][1]))
+
+	v2Codec, err := tikv.NewCodecV2(tikv.ModeTxn, &keyspacepb.KeyspaceMeta{
+		Id:   1,
+		Name: "user-keyspace",
+	})
+	require.NoError(t, err)
+
+	ranges = rewriteKeyRanges(v2Codec, preAlloced)
+	require.Len(t, ranges, 1)
+	require.NotEqual(t, legacyStart, []byte(ranges[0][0]))
+	require.NotEqual(t, legacyEnd, []byte(ranges[0][1]))
+
+	decodedStart, decodedEnd, err := v2Codec.DecodeRegionRange(ranges[0][0], ranges[0][1])
+	require.NoError(t, err)
+	require.Equal(t, []byte(tableStart), decodedStart)
+	require.Equal(t, []byte(tableEnd), decodedEnd)
+
+	_, _, err = v2Codec.DecodeRegionRange(legacyStart, legacyEnd)
+	require.Error(t, err)
+	require.Nil(t, rewriteKeyRanges(v2Codec, [2]int64{}))
 }
 
 func TestCheckSnapshotRestoreModeRejectsRawBackup(t *testing.T) {
