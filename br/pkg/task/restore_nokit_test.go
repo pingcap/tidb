5764bc7f55143796ad1b1bbcc2c4415af802a374
Juncen Yu <yujuncen@pingcap.com>
2026-07-02 07:19:40 +0000

    resolve conflicts

--- br/pkg/task/restore_nokit_test.go
+++ br/pkg/task/restore_nokit_test.go
@@ -17,20 +17,10 @@ package task
 import (
 	"testing"
 
-<<<<<<< HEAD
-	"github.com/pingcap/tidb/pkg/config/kerneltype"
-=======
-	"github.com/gogo/protobuf/proto"
-	backuppb "github.com/pingcap/kvproto/pkg/brpb"
-	"github.com/pingcap/kvproto/pkg/encryptionpb"
 	"github.com/pingcap/kvproto/pkg/keyspacepb"
-	"github.com/pingcap/tidb/br/pkg/metautil"
 	"github.com/pingcap/tidb/pkg/config/kerneltype"
-	"github.com/pingcap/tidb/pkg/objstore"
 	"github.com/pingcap/tidb/pkg/tablecodec"
 	"github.com/pingcap/tidb/pkg/util/codec"
-	"github.com/spf13/pflag"
->>>>>>> cff357e0d75 (br: make restore scheduler pause range keyspace-aware (#69560))
 	"github.com/stretchr/testify/require"
 	"github.com/tikv/client-go/v2/tikv"
 )
@@ -52,55 +42,6 @@ func TestPhysicalRestoreSysTables(t *testing.T) {
 		require.True(t, loadSysTablePhysical)
 		require.True(t, loadStatsPhysical)
 	}
-}
-<<<<<<< HEAD
-=======
-
-func TestRestorePhaseRequiresCheckpoint(t *testing.T) {
-	flags := pflag.NewFlagSet("restore", pflag.ContinueOnError)
-	DefineRestoreFlags(flags)
-	require.NoError(t, flags.Set(FlagRestorePhase, "1"))
-	require.NoError(t, flags.Set(flagUseCheckpoint, "false"))
-
-	cfg := &RestoreConfig{}
-	err := cfg.ParseFromFlags(flags, true)
-	require.Error(t, err)
-	require.ErrorContains(t, err, FlagRestorePhase)
-	require.ErrorContains(t, err, flagUseCheckpoint)
-}
-
-func TestSplitRegionIndexStepFlag(t *testing.T) {
-	t.Run("coarse scatter", func(t *testing.T) {
-		flags := pflag.NewFlagSet("restore", pflag.ContinueOnError)
-		DefineRestoreFlags(flags)
-		require.NoError(t, flags.Set("coarse-scatter", "true"))
-
-		cfg := &RestoreConfig{}
-		require.NoError(t, cfg.ParseFromFlags(flags, true))
-		require.True(t, cfg.CoarseScatter)
-	})
-
-	t.Run("custom", func(t *testing.T) {
-		flags := pflag.NewFlagSet("restore", pflag.ContinueOnError)
-		DefineRestoreFlags(flags)
-		require.NoError(t, flags.Set(FlagSplitRegionIndexStep, "64"))
-
-		cfg := &RestoreConfig{}
-		require.NoError(t, cfg.ParseFromFlags(flags, true))
-		require.Equal(t, uint(64), cfg.SplitRegionIndexStep)
-	})
-
-	t.Run("zero", func(t *testing.T) {
-		flags := pflag.NewFlagSet("restore", pflag.ContinueOnError)
-		DefineRestoreFlags(flags)
-		require.NoError(t, flags.Set(FlagSplitRegionIndexStep, "0"))
-
-		cfg := &RestoreConfig{}
-		err := cfg.ParseFromFlags(flags, true)
-		require.Error(t, err)
-		require.ErrorContains(t, err, FlagSplitRegionIndexStep)
-		require.ErrorContains(t, err, "greater than 0")
-	})
 }
 
 func TestRewriteKeyRangesUsesStorageCodec(t *testing.T) {
@@ -135,31 +76,3 @@ func TestRewriteKeyRangesUsesStorageCodec(t *testing.T) {
 	require.Error(t, err)
 	require.Nil(t, rewriteKeyRanges(v2Codec, [2]int64{}))
 }
-
-func TestCheckSnapshotRestoreModeRejectsRawBackup(t *testing.T) {
-	ctx := context.Background()
-	store, err := objstore.NewLocalStorage(t.TempDir())
-	require.NoError(t, err)
-
-	meta := &backuppb.BackupMeta{
-		IsRawKv:             true,
-		BackupSchemaVersion: backuppb.BackupSchemaVersion,
-	}
-	data, err := proto.Marshal(meta)
-	require.NoError(t, err)
-	require.NoError(t, store.WriteFile(ctx, metautil.MetaFile, data))
-
-	cfg := &RestoreConfig{
-		Config: Config{
-			Storage: "local://" + store.URI(),
-			CipherInfo: backuppb.CipherInfo{
-				CipherType: encryptionpb.EncryptionMethod_PLAINTEXT,
-			},
-		},
-	}
-
-	err = checkSnapshotRestoreMode(ctx, cfg)
-	require.Error(t, err)
-	require.ErrorContains(t, err, "restore mode mismatch")
-}
->>>>>>> cff357e0d75 (br: make restore scheduler pause range keyspace-aware (#69560))
