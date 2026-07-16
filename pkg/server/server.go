df697dc23cc38e0c9b3735b27554cafdb3d305e8
OliverS929 <182192954+OliverS929@users.noreply.github.com>
2026-07-16 03:40:48 +0000

    Check the advertised status endpoint on startup

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -497,6 +497,9 @@ func (s *Server) Run(dom *domain.Domain) error {
 	terror.RegisterFinish()
 	go s.startNetworkListener(s.listener, false, errChan)
 	go s.startNetworkListener(s.socket, true, errChan)
+	advertisedStatusEndpointCheckCtx, cancelAdvertisedStatusEndpointCheck := context.WithCancel(context.Background())
+	defer cancelAdvertisedStatusEndpointCheck()
+	s.startAdvertisedStatusEndpointCheck(advertisedStatusEndpointCheckCtx)
 	if RunInGoTest && !isClosed(RunInGoTestChan) {
 		close(RunInGoTestChan)
 	}
