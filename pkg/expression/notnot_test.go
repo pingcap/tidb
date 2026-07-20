80b669d2b92f713ea0f34c1b8bc40f3684bd149b
sijie-Z <1683039482@qq.com>
2026-07-20 11:55:16 +0800

    fixup: fix imports in test file

--- pkg/expression/notnot_test.go
+++ pkg/expression/notnot_test.go
@@ -2,9 +2,11 @@ package expression
 
 import (
 	"testing"
+
 	"github.com/pingcap/tidb/pkg/parser/ast"
-	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
+	"github.com/pingcap/tidb/pkg/types"
+	"github.com/pingcap/tidb/pkg/util/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,7 +21,11 @@ func TestPushDownNotDoubleNegation(t *testing.T) {
 
 	// PushDownNot should simplify NOT NOT (a=1) -> a=1
 	ret := PushDownNot(ctx, notOuter)
-	t.Logf("Input: %s", notOuter.StringWithCtx(ctx.GetEvalCtx(), errors.RedactLogDisable))
-	t.Logf("Output: %s", ret.StringWithCtx(ctx.GetEvalCtx(), errors.RedactLogDisable))
 	require.True(t, ret.Equal(ctx, eqFunc), "NOT NOT (a=1) should simplify to a=1")
+
+	// Also verify: NOT NOT NOT (a=1) -> NOT (a=1) = (a!=1)
+	notOuter3 := newFunctionWithMockCtx(ast.UnaryNot, notOuter)
+	ret3 := PushDownNot(ctx, notOuter3)
+	neFunc := newFunctionWithMockCtx(ast.NE, col, NewOne())
+	require.True(t, ret3.Equal(ctx, neFunc), "NOT NOT NOT (a=1) should simplify to a!=1")
 }
