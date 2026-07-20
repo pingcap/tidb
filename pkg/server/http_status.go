78afdafa939adda373b471e2aa4dbba4a5ed5106
gengliqi <gengliqiii@gmail.com>
2026-07-20 15:36:51 +0800

    u
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- pkg/server/http_status.go
+++ pkg/server/http_status.go
@@ -326,85 +326,12 @@ func (s *Server) startHTTPServer() {
 		router.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(static.Data)))
 	}
 
-<<<<<<< HEAD
-	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	router.HandleFunc("/debug/pprof/profile", cpuprofile.ProfileHTTPHandler)
-	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	router.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	// Other /debug/pprof paths not covered above are redirected to pprof.Index.
-	router.PathPrefix("/debug/pprof/").HandlerFunc(pprof.Index)
-=======
-	if s.StandbyController != nil {
-		path, handler := s.StandbyController.Handler(s)
-		router.PathPrefix(path).Handler(handler)
-	}
-
 	router.HandleFunc("/debug/pprof/cmdline", withProfilingRequestLog(pprof.Cmdline))
 	router.HandleFunc("/debug/pprof/profile", withProfilingRequestLog(cpuprofile.ProfileHTTPHandler))
 	router.HandleFunc("/debug/pprof/symbol", withProfilingRequestLog(pprof.Symbol))
 	router.HandleFunc("/debug/pprof/trace", withProfilingRequestLog(pprof.Trace))
 	// Other /debug/pprof paths not covered above are redirected to pprof.Index.
 	router.PathPrefix("/debug/pprof/").HandlerFunc(withProfilingRequestLog(pprof.Index))
-	router.HandleFunc("/debug/traceevent", traceeventHandler)
-
-	router.HandleFunc("/covdata", func(writer http.ResponseWriter, _ *http.Request) {
-		writer.Header().Set("Content-Type", "application/zip")
-		writer.Header().Set("Content-Disposition", "attachment; filename=files.zip")
-
-		dir := os.Getenv("TIDB_GOCOVERDIR")
-		if dir == "" {
-			serveError(writer, http.StatusInternalServerError, "TIDB_GOCOVERDIR is not set")
-			return
-		}
-		err := coverage.WriteMetaDir(dir)
-		if err != nil {
-			logutil.BgLogger().Warn("write coverage meta failed", zap.Error(err))
-			serveError(writer, http.StatusInternalServerError, "write coverage meta failed")
-			return
-		}
-		err = coverage.WriteCountersDir(dir)
-		if err != nil {
-			logutil.BgLogger().Warn("write coverage counters failed", zap.Error(err))
-			serveError(writer, http.StatusInternalServerError, "write coverage counters failed")
-			return
-		}
-
-		zipWriter := zip.NewWriter(writer)
-
-		err = filepath.Walk(dir, func(file string, fi os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if fi.IsDir() {
-				return nil
-			}
-			relPath, err := filepath.Rel(dir, file)
-			if err != nil {
-				return err
-			}
-			writer, err := zipWriter.Create(relPath)
-			if err != nil {
-				return err
-			}
-			srcFile, err := os.Open(filepath.Clean(file))
-			if err != nil {
-				return err
-			}
-			defer func() {
-				_ = srcFile.Close()
-			}()
-			_, err = io.Copy(writer, srcFile)
-			return err
-		})
-		if err != nil {
-			logutil.BgLogger().Warn("zip coverage files failed", zap.Error(err))
-			serveError(writer, http.StatusInternalServerError, "zip coverage files failed")
-			return
-		}
-		err = zipWriter.Close()
-		terror.Log(err)
-	})
->>>>>>> 3332762af45 (server: add logging for profiling requests (#69897))
 
 	ballast := newBallast(s.cfg.MaxBallastObjectSize)
 	{
