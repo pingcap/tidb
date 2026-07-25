3965793f677f8457d97077f7b24da693213544b5
cian.z <hi_leo@yeah.net>
2026-07-26 00:42:23 +0800

    test: fix gci formatting in parser_test.go

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -7232,21 +7232,21 @@ func TestStartTransaction(t *testing.T) {
 }
 
 func TestJSONTable(t *testing.T) {
-	cases := []testCase {
+	cases := []testCase{
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (id FOR ORDINALITY)) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` FOR ORDINALITY)) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` FOR ORDINALITY)) AS `jt`"},
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (id INT PATH '$.a')) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a')) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a')) AS `jt`"},
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (id INT EXISTS PATH '$.a')) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT EXISTS PATH '$.a')) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT EXISTS PATH '$.a')) AS `jt`"},
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (NESTED PATH '$.b' COLUMNS (c INT PATH '$.c'))) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (NESTED PATH '$.b' COLUMNS (`c` INT PATH '$.c'))) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (NESTED PATH '$.b' COLUMNS (`c` INT PATH '$.c'))) AS `jt`"},
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (id INT PATH '$.a' NULL ON EMPTY)) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a' NULL ON EMPTY)) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a' NULL ON EMPTY)) AS `jt`"},
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (id INT PATH '$.a' ERROR ON ERROR)) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a' ERROR ON ERROR)) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a' ERROR ON ERROR)) AS `jt`"},
 		{`SELECT * FROM JSON_TABLE('{"a":1}', '$' COLUMNS (id INT PATH '$.a' NULL ON EMPTY ERROR ON ERROR)) jt`, true,
-		"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a' NULL ON EMPTY ERROR ON ERROR)) AS `jt`"},
+			"SELECT * FROM JSON_TABLE(_UTF8MB4'{\"a\":1}', '$' COLUMNS (`id` INT PATH '$.a' NULL ON EMPTY ERROR ON ERROR)) AS `jt`"},
 	}
 	RunTest(t, cases, false, false)
 }
