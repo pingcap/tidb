3ad4b699e363a2f3cac97473508712748131f1bb
Chao Wang <cclcwangchao@hotmail.com>
2026-07-25 10:39:02 +0800

    fix conflict

--- pkg/infoschema/test/clustertablestest/tables_test.go
+++ pkg/infoschema/test/clustertablestest/tables_test.go
@@ -445,13 +445,7 @@ func TestSlowQuery(t *testing.T) {
 			"10",
 			"10",
 			"100",
-<<<<<<< HEAD
-=======
-			"0",
-			"0",
-			"0",
 			"",
->>>>>>> 05b396fb663 (executor: expose read pool task details in diagnostics (#69971))
 			"test",
 			"",
 			"0",
@@ -548,13 +542,7 @@ func TestSlowQuery(t *testing.T) {
 			"0",
 			"0",
 			"0",
-<<<<<<< HEAD
-=======
-			"0",
-			"0",
-			"0",
 			"",
->>>>>>> 05b396fb663 (executor: expose read pool task details in diagnostics (#69971))
 			"rtdb",
 			"",
 			"0",
