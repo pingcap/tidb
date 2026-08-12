f5c07ca759b13870e8d2eb1ff10d1636deba123f
D3Hunter <jujj603@gmail.com>
2026-08-12 17:01:32 +0800

    This is an automated cherry-pick of #70427
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/executor/importer/precheck.go
+++ pkg/executor/importer/precheck.go
@@ -38,6 +38,7 @@ var GetEtcdClient = store.NewEtcdCli
 
 // CheckRequirements checks the requirements for IMPORT INTO.
 // we check the following things here:
+//   - target table should not have TTL enabled
 //   - when import from file
 //     1. there is no active job on the target table
 //     2. the total file size > 0
@@ -47,6 +48,31 @@ var GetEtcdClient = store.NewEtcdCli
 //
 // we check them one by one, and return the first error we meet.
 func (e *LoadDataController) CheckRequirements(ctx context.Context, se sessionctx.Context) error {
+<<<<<<< HEAD
+=======
+	return e.checkRequirements(ctx, se, true)
+}
+
+// CheckRequirementsBeforeInitDataFiles checks requirements that don't depend on
+// discovered data files, and is used by async-prepare submit path.
+func (e *LoadDataController) CheckRequirementsBeforeInitDataFiles(ctx context.Context, se sessionctx.Context) error {
+	return e.checkRequirements(ctx, se, false)
+}
+
+func (e *LoadDataController) checkRequirements(ctx context.Context, se sessionctx.Context, checkTotalFileSize bool) error {
+	tableInfo := e.Plan.TableInfo
+	// Import table mode can prevent TTL from deleting data and causing a checksum
+	// mismatch. However, because TTL jobs run asynchronously, a job that races with the
+	// switch to import mode may still report a protected-table error. This precheck is
+	// also needed on versions that do not support table mode. Therefore, IMPORT INTO
+	// forbids importing into a table with TTL enabled. If later testing shows that IMPORT
+	// INTO can always switch table mode without an opt-out, TTL can check table mode before
+	// starting a job and this precheck can be removed.
+	if tableInfo.TTLInfo != nil && tableInfo.TTLInfo.Enable {
+		return exeerrors.ErrLoadDataPreCheckFailed.FastGenByArgs("target table has TTL enabled, please disable TTL before IMPORT INTO")
+	}
+
+>>>>>>> d5f9ca5690c (importinto: forbid importing into TTL-enabled tables (#70427))
 	conn := se.GetSQLExecutor()
 	if e.DataSourceType == DataSourceTypeFile {
 		cnt, err := GetActiveJobCnt(ctx, conn, e.Plan.DBName, e.Plan.TableInfo.Name.L)
