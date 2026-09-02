aff8abec3bc99012dd0a3fa16d14559e49746b41
Yang Keao <yangkeao@chunibyo.icu>
2026-09-02 23:31:17 +0800

    ttl: keep SET clustered primary key compatibility

--- pkg/errno/errname.go
+++ pkg/errno/errname.go
@@ -1066,7 +1066,7 @@ var MySQLErrName = map[uint16]*mysql.ErrMessage{
 	ErrSetTTLOptionForNonTTLTable:       mysql.Message("Cannot set %s on a table without TTL config", nil),
 	ErrTempTableNotAllowedWithTTL:       mysql.Message("Set TTL for temporary table is not allowed", nil),
 	ErrUnsupportedTTLReferencedByFK:     mysql.Message("Set TTL for a table referenced by foreign key is not allowed", nil),
-	ErrUnsupportedPrimaryKeyTypeWithTTL: mysql.Message("Unsupported clustered primary key type FLOAT/DOUBLE/SET for TTL", nil),
+	ErrUnsupportedPrimaryKeyTypeWithTTL: mysql.Message("Unsupported clustered primary key type FLOAT/DOUBLE for TTL", nil),
 	ErrLoadDataFromServerDisk:           mysql.Message("Don't support load data from tidb-server's disk. Or if you want to load local data via client, the path of INFILE '%s' needs to specify the clause of LOCAL first", nil),
 	ErrLoadParquetFromLocal:             mysql.Message("Do not support loading parquet files from local. Please try to load the parquet files from the cloud storage", nil),
 	ErrLoadDataEmptyPath:                mysql.Message("The value of INFILE must not be empty when LOAD DATA from LOCAL", nil),
