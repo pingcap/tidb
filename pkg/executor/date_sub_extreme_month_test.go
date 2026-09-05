a21f5fd7358124874d1f8f62baffa8367f432ad4
Alignyx <317810310+Alignyx@users.noreply.github.com>
2026-09-05 12:37:33 +0800

    executor: cover DATE_ADD year-zero arithmetic

--- pkg/executor/date_sub_extreme_month_test.go
+++ pkg/executor/date_sub_extreme_month_test.go
@@ -1,6 +1,16 @@
 // Copyright 2026 PingCAP, Inc.
 //
-// Licensed under the Apache License, Version 2.0.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package executor_test
 
@@ -29,6 +39,9 @@ func TestDateSubExtremeMonth(t *testing.T) {
 			))
 			tk.MustQuery("SELECT DATE_ADD(col1, INTERVAL -2 HOUR) FROM lrr_test").Check(testkit.Rows(
 				"0000-00-00 22:00:00",
+			))
+			tk.MustQuery("SELECT DATE_ADD(col1, INTERVAL -10 MONTH) FROM lrr_test").Check(testkit.Rows(
+				"0000-03-01 00:00:00",
 			))
 		})
 	}
