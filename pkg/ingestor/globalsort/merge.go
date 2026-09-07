50ed4ba43f9e6c02f3f9ae47317c14dbdb28b7bb
Ruihao Chen <joechenrh@gmail.com>
2026-09-07 19:00:54 +0800

    ingestor: raise merge upload part size floor

--- pkg/ingestor/globalsort/merge.go
+++ pkg/ingestor/globalsort/merge.go
@@ -44,7 +44,8 @@ var (
 )
 
 const (
-	maxMergeReaderMemoryPerCore = 256 * units.MiB
+	maxMergeReaderMemoryPerCore       = 256 * units.MiB
+	minMergePartSize            int64 = 16 * units.MiB
 )
 
 var _ execute.Collector = &mergeCollector{}
@@ -389,5 +390,5 @@ func getMergePartSize(inputSize int64, fileCount, blockSize int) int64 {
 	if maxOutputSize%simplesst.MaxUploadPartCount != 0 {
 		partSize++
 	}
-	return max(simplesst.MinUploadPartSize, partSize)
+	return max(minMergePartSize, partSize)
 }
