c7caa8c539bc8237fd61eb7eb141e160cbefd51a
xufei <xufeixw@mail.ustc.edu.cn>
2026-09-08 15:16:57 +0800

    ddl: validate table info after adding MLog column

--- pkg/ddl/add_column.go
+++ pkg/ddl/add_column.go
@@ -141,7 +141,8 @@ func (w *worker) onAddColumn(jobCtx *jobContext, job *model.Job) (ver int64, err
 		tblInfo.MoveColumnInfo(columnInfo.Offset, offset)
 		columnInfo.State = model.StatePublic
 		syncMLogColumnsAfterAddColumn(tblInfo)
-		ver, err = updateVersionAndTableInfo(jobCtx, job, tblInfo, originalState != columnInfo.State)
+		// Validate the final public table info before publishing its schema version.
+		ver, err = updateVersionAndTableInfoWithCheck(jobCtx, job, tblInfo, originalState != columnInfo.State)
 		if err != nil {
 			return ver, errors.Trace(err)
 		}
