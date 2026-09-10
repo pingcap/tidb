383c360baaeb73004a7822c843a829352354b8c3
D3Hunter <jujj603@gmail.com>
2026-09-10 12:54:07 +0800

    *: migrate non-replacing AST visitors to in-place traversal part 3 (#70935)
    ref pingcap/tidb#70622

--- pkg/parser/ast/expressions_test.go
+++ pkg/parser/ast/expressions_test.go
@@ -24,19 +24,19 @@ import (
 
 type checkVisitor struct{}
 
-func (v checkVisitor) Enter(in Node) (Node, bool) {
+func (v checkVisitor) Enter(in Node) bool {
 	if e, ok := in.(*checkExpr); ok {
 		e.enterCnt++
-		return in, true
+		return true
 	}
-	return in, false
+	return false
 }
 
-func (v checkVisitor) Leave(in Node) (Node, bool) {
+func (v checkVisitor) Leave(in Node) bool {
 	if e, ok := in.(*checkExpr); ok {
 		e.leaveCnt++
 	}
-	return in, true
+	return true
 }
 
 type checkExpr struct {
@@ -98,10 +98,10 @@ func TestExpresionsVisitorCover(t *testing.T) {
 
 	for _, v := range stmts {
 		ce.reset()
-		v.node.Accept(checkVisitor{})
+		Walk(v.node, checkVisitor{})
 		require.Equal(t, v.expectedEnterCnt, ce.enterCnt)
 		require.Equal(t, v.expectedLeaveCnt, ce.leaveCnt)
-		v.node.Accept(visitor1{})
+		Walk(v.node, visitor1{})
 	}
 }
 
