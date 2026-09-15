de84fc981cfbb125e1edc6913d5f0dbb27edb605
JmPotato <github@ipotato.me>
2026-09-15 12:36:00 +0800

    session: apply global paging byte budgets to existing sessions
    Signed-off-by: JmPotato <github@ipotato.me>

--- pkg/sessionctx/variable/session.go
+++ pkg/sessionctx/variable/session.go
@@ -1780,10 +1780,6 @@ type SessionVars struct {
 	// NOTE: please don't change it directly. Use `SetResourceGroupName`, because it'll need to inc/dec the metrics
 	ResourceGroupName string
 
-	// PagingSizeBytes is the byte budget per page.
-	// 0 means disabled.
-	PagingSizeBytes int
-
 	// PessimisticTransactionFairLocking controls whether fair locking for pessimistic transaction
 	// is enabled.
 	PessimisticTransactionFairLocking bool
@@ -2511,7 +2507,6 @@ func NewSessionVars(hctx HookContext) *SessionVars {
 		EnableLateMaterialization:        vardef.DefTiDBOptEnableLateMaterialization,
 		TiFlashComputeDispatchPolicy:     tiflashcompute.DispatchPolicyConsistentHash,
 		ResourceGroupName:                resourcegroup.DefaultResourceGroupName,
-		PagingSizeBytes:                  vardef.DefPagingSizeBytes,
 		DefaultCollationForUTF8MB4:       mysql.DefaultCollationName,
 		GroupConcatMaxLen:                vardef.DefGroupConcatMaxLen,
 		EnableRedactLog:                  vardef.DefTiDBRedactLog,
