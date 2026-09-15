c8a4febc5e072a2bf6648282fec8629326961c89
wjhuang2016 <huangwenjun1997@gmail.com>
2026-09-15 11:02:09 +0800

    ddl: test disk-full at DDL job retry gate

--- pkg/ddl/backfilling_test.go
+++ pkg/ddl/backfilling_test.go
@@ -113,8 +113,7 @@ func TestBackfillRetryableErrors(t *testing.T) {
 			require.False(t, isRetryableJobError(err, vardef.GetDDLErrorCountLimit()-1))
 		}
 
-		require.False(t, isRetryableError(errdef.ErrKVEpochNotMatch.GenWithStack("epoch mismatch"), false))
-		require.False(t, isRetryableError(errdef.ErrKVDiskFull.GenWithStack("store disk full"), false))
+		require.False(t, isRetryableJobError(errdef.ErrKVDiskFull.GenWithStack("store disk full"), 0))
 	})
 }
 
