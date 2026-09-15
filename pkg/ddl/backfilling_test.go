c4bd6b94c5ccccd31876a3e12b16c239b4005f05
wjhuang2016 <huangwenjun1997@gmail.com>
2026-09-15 10:10:56 +0800

    ddl: retry transient PD TSO stream errors during reorg
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- pkg/ddl/backfilling_test.go
+++ pkg/ddl/backfilling_test.go
@@ -49,6 +49,7 @@ import (
 	"github.com/pingcap/tidb/pkg/util/mock"
 	"github.com/pingcap/tidb/pkg/util/timeutil"
 	"github.com/stretchr/testify/require"
+	pderr "github.com/tikv/pd/client/errs"
 )
 
 func TestDoneTaskKeeper(t *testing.T) {
@@ -98,6 +99,25 @@ func TestBackfillRetryableErrors(t *testing.T) {
 	t.Run("confirmed insufficient local-sort disk is not retryable", func(t *testing.T) {
 		err := dbterror.ErrIngestCheckEnvFailed.FastGenByArgs("mock insufficient local sort disk space")
 		require.False(t, (&backfillDistExecutor{}).IsRetryableError(err))
+	})
+
+	t.Run("PD TSO stream errors are retryable", func(t *testing.T) {
+		errs := []error{
+			pderr.ErrClientCreateTSOStream.FastGenByArgs(pderr.RetryTimeoutErr),
+			pderr.ErrClientTSOStreamClosed.GenWithStackByArgs(),
+		}
+		for _, err := range errs {
+			require.True(t, isRetryableError(err, false))
+			require.True(t, isRetryableError(errors.Annotate(err, "wrapped"), false))
+			require.True(t, (&backfillDistExecutor{}).IsRetryableError(err))
+			require.True(t, isRetryableJobError(err, 0))
+			require.False(t, isRetryableJobError(err, vardef.GetDDLErrorCountLimit()-1))
+		}
+	})
+
+	t.Run("unrelated PD errors are not made retryable", func(t *testing.T) {
+		err := pderr.ErrClientProtoUnmarshal.FastGenByArgs("invalid response")
+		require.False(t, isRetryableError(err, false))
 	})
 }
 
