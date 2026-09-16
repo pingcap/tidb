7076cdca867f75c5cd84161004eb2fb05dcccc72
siyxo <siyxo511@outlook.com>
2026-09-17 01:01:50 +0800

    fix(executor): 修复导出文件遗漏行前缀

--- pkg/executor/select_into.go
+++ pkg/executor/select_into.go
@@ -140,7 +140,7 @@ func (s *SelectIntoExec) dumpToOutfile() error {
 	cols := s.Children(0).Schema().Columns
 	for i := range s.chk.NumRows() {
 		row := s.chk.GetRow(i)
-		s.lineBuf = s.lineBuf[:0]
+		s.lineBuf = append(s.lineBuf[:0], s.LinesStartingBy...)
 		for j, col := range cols {
 			if j != 0 {
 				s.lineBuf = append(s.lineBuf, s.FieldsTerminatedBy...)
