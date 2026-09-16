02a578f0c91193b3a9ff8e6eb48a4d64196acc26
wjhuang2016 <huangwenjun1997@gmail.com>
2026-09-17 04:38:41 +0800

    variable: initialize adaptive closest-read threshold

--- pkg/sessionctx/variable/session.go
+++ pkg/sessionctx/variable/session.go
@@ -2373,6 +2373,7 @@ func NewSessionVars(hctx HookContext) *SessionVars {
 		AutoIncrementOffset:              vardef.DefAutoIncrementOffset,
 		StmtCtx:                          stmtctx.NewStmtCtx(),
 		AllowAggPushDown:                 false,
+		ReplicaClosestReadThreshold:      vardef.DefAdaptiveClosestReadThreshold,
 		AllowCartesianBCJ:                vardef.DefOptCartesianBCJ,
 		MPPOuterJoinFixedBuildSide:       vardef.DefOptMPPOuterJoinFixedBuildSide,
 		BroadcastJoinThresholdSize:       vardef.DefBroadcastJoinThresholdSize,
