362968c61fb9a37024889341c28abd1c463640da
wjhuang2016 <huangwenjun1997@gmail.com>
2026-09-17 04:25:04 +0800

    parser: preserve MOD function argument boundaries on restore

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -20450,7 +20450,7 @@ yynewstate:
 		}
 	case 1686:
 		{
-			parser.yyVAL.expr = &ast.BinaryOperationExpr{Op: opcode.Mod, L: yyS[yypt-3].expr, R: yyS[yypt-1].expr}
+			parser.yyVAL.expr = &ast.FuncCallExpr{FnName: ast.NewCIStr(ast.Mod), Args: []ast.ExprNode{yyS[yypt-3].expr, yyS[yypt-1].expr}}
 		}
 	case 1687:
 		{
