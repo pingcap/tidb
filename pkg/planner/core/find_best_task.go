a2e369b56477d88ac7bba160872d57fe877beca7
Yuanjia Zhang <qw4990@163.com>
2026-09-18 14:59:26 +0800

    Revert "planner: support late materialization in CBO (#70728)"
    This reverts commit 43dee54109aef14ca5f01d7ab5613feeb08de8c8.

--- pkg/planner/core/find_best_task.go
+++ pkg/planner/core/find_best_task.go
@@ -2974,23 +2974,6 @@ func convertToBatchPointGet(ds *logicalop.DataSource, prop *property.PhysicalPro
 	return rTsk
 }
 
-func (ts *PhysicalTableScan) buildPushedDownSelection(stats *property.StatsInfo) *PhysicalSelection {
-	if ts.StoreType == kv.TiFlash {
-		pushDownLateMaterializationFilters(ts.SCtx(), ts)
-		conditions := make([]expression.Expression, 0, len(ts.filterCondition))
-		for _, cond := range ts.filterCondition {
-			if !expression.Contains(ts.SCtx().GetExprCtx().GetEvalCtx(), ts.LateMaterializationFilterCondition, cond) {
-				conditions = append(conditions, cond)
-			}
-		}
-		if len(conditions) == 0 {
-			return nil
-		}
-		return PhysicalSelection{Conditions: conditions}.Init(ts.SCtx(), stats, ts.QueryBlockOffset())
-	}
-	return PhysicalSelection{Conditions: ts.filterCondition}.Init(ts.SCtx(), stats, ts.QueryBlockOffset())
-}
-
 func (ts *PhysicalTableScan) addPushedDownSelectionToMppTask(mpp *MppTask, stats *property.StatsInfo) *MppTask {
 	filterCondition, rootTaskConds := SplitSelCondsWithVirtualColumn(ts.filterCondition)
 	var newRootConds []expression.Expression
@@ -3000,12 +2983,9 @@ func (ts *PhysicalTableScan) addPushedDownSelectionToMppTask(mpp *MppTask, stats
 	ts.filterCondition = filterCondition
 	// Add filter condition to table plan now.
 	if len(ts.filterCondition) > 0 {
-		if sel := ts.buildPushedDownSelection(stats); sel != nil {
-			sel.SetChildren(ts)
-			mpp.p = sel
-		} else {
-			mpp.p = ts
-		}
+		sel := PhysicalSelection{Conditions: ts.filterCondition}.Init(ts.SCtx(), stats, ts.QueryBlockOffset())
+		sel.SetChildren(ts)
+		mpp.p = sel
 	}
 	return mpp
 }
@@ -3018,13 +2998,9 @@ func (ts *PhysicalTableScan) addPushedDownSelection(copTask *CopTask, stats *pro
 
 	// Add filter condition to table plan now.
 	if len(ts.filterCondition) > 0 {
-		sel := ts.buildPushedDownSelection(stats)
-		if sel == nil {
-			copTask.tablePlan = ts
-			return
-		}
+		sel := PhysicalSelection{Conditions: ts.filterCondition}.Init(ts.SCtx(), stats, ts.QueryBlockOffset())
 		if len(copTask.rootTaskConds) != 0 {
-			selectivity, _, err := cardinality.Selectivity(ts.SCtx(), copTask.tblColHists, sel.Conditions, nil)
+			selectivity, _, err := cardinality.Selectivity(ts.SCtx(), copTask.tblColHists, ts.filterCondition, nil)
 			if err != nil {
 				logutil.BgLogger().Debug("calculate selectivity failed, use selection factor", zap.Error(err))
 				selectivity = cost.SelectionFactor
