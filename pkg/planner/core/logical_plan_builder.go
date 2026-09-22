1f2854dcc0404c73ec6b21d9ec1b5727dcebcd53
xufei <xufeixw@mail.ustc.edu.cn>
2026-09-22 20:30:44 +0800

    Revert "executor, ddl: support base table DML with materialized view logs (#70941)"
    This reverts commit 91e2f28ac7b0ed655b62d0482cf87336f2fb2052.

--- pkg/planner/core/logical_plan_builder.go
+++ pkg/planner/core/logical_plan_builder.go
@@ -5970,8 +5970,7 @@ func pruneAndBuildColPositionInfoForDelete(
 		// Use a very relax check for foreign key cascades and checks.
 		// If there's one table containing foreign keys, all of the tables would not do pruning.
 		// It should be strict in the future or just support pruning column when there is foreign key.
-		hasMLog := tblInfo.MaterializedViewBase != nil && tblInfo.MaterializedViewBase.MLogID != 0
-		skipPruning := tblInfo.GetPartitionInfo() != nil || hasFK || nonPruned == nil || hasMLog
+		skipPruning := tblInfo.GetPartitionInfo() != nil || hasFK || nonPruned == nil
 		for _, idx := range tblInfo.Indices {
 			if len(idx.ConditionExprString) > 0 {
 				// If the index has a partial index condition, we can't prune the columns.
