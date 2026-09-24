f87e193448bf6709023ba68cff0740fcbf8fd0de
YangKeao <yangkeao@chunibyo.icu>
2026-09-24 18:18:51 +0800

    metricsutil: centralize database metric labels and move their tests

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -131,6 +131,7 @@ import (
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/pingcap/tidb/pkg/util/logutil/consistency"
 	"github.com/pingcap/tidb/pkg/util/memory"
+	"github.com/pingcap/tidb/pkg/util/metricsutil"
 	parserutil "github.com/pingcap/tidb/pkg/util/parser"
 	rangerctx "github.com/pingcap/tidb/pkg/util/ranger/context"
 	"github.com/pingcap/tidb/pkg/util/redact"
@@ -2218,7 +2219,7 @@ func (s *session) ExecRestrictedStmt(ctx context.Context, stmtNode ast.StmtNode,
 
 	vars := se.GetSessionVars()
 	cost := time.Since(startTime).Seconds()
-	for _, dbName := range GetDBNames(vars) {
+	for _, dbName := range metricsutil.GetDBNames(vars) {
 		metrics.QueryDurationHistogram.WithLabelValues(metrics.LblInternal, dbName, vars.StmtCtx.ResourceGroupName).Observe(cost)
 		metrics.CommandDurationHistogram.WithLabelValues(metrics.LblInternal, dbName, vars.StmtCtx.ResourceGroupName).Observe(cost)
 	}
@@ -2407,7 +2408,7 @@ func (s *session) ExecRestrictedSQL(ctx context.Context, opts []sqlexec.OptionFu
 
 		vars := se.GetSessionVars()
 		cost := time.Since(startTime).Seconds()
-		for _, dbName := range GetDBNames(vars) {
+		for _, dbName := range metricsutil.GetDBNames(vars) {
 			metrics.QueryDurationHistogram.WithLabelValues(metrics.LblInternal, dbName, vars.StmtCtx.ResourceGroupName).Observe(cost)
 			metrics.CommandDurationHistogram.WithLabelValues(metrics.LblInternal, dbName, vars.StmtCtx.ResourceGroupName).Observe(cost)
 		}
@@ -6016,11 +6017,6 @@ func (s *session) usePipelinedDmlOrWarn(ctx context.Context) bool {
 		)
 	}
 	return true
-}
-
-// GetDBNames gets the sql layer database names from the session.
-func GetDBNames(seVar *variable.SessionVars) []string {
-	return seVar.GetMetricDBNames()
 }
 
 // GetCursorTracker returns the internal `cursor.Tracker`
