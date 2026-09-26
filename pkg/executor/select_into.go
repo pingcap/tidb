da24174ab619d27c9f4e2db4190d4eab4175f148
siyxo <siyxo511@outlook.com>
2026-09-26 21:37:51 +0800

    perf(executor): reuse OUTFILE line prefix across rows
    Signed-off-by: siyxo <siyxo511@outlook.com>

--- pkg/executor/select_into.go
+++ pkg/executor/select_into.go
@@ -140,9 +140,10 @@ func (s *SelectIntoExec) dumpToOutfile() error {
 	}
 
 	cols := s.Children(0).Schema().Columns
+	s.lineBuf = append(s.lineBuf[:0], s.LinesStartingBy...)
 	for i := range s.chk.NumRows() {
 		row := s.chk.GetRow(i)
-		s.lineBuf = append(s.lineBuf[:0], s.LinesStartingBy...)
+		s.lineBuf = s.lineBuf[:len(s.LinesStartingBy)]
 		for j, col := range cols {
 			if j != 0 {
 				s.lineBuf = append(s.lineBuf, s.FieldsTerminatedBy...)
